5f18d2ad8ce7892346f1b9db1b46e90d7ffa9053
yunheehyeon <ok94life@daum.net>
2019-06-14 15:03:19 +0900

    feat : 체스판 출력하기

---
+++ src/main/java/chess/CUIChessApp.java
@@ -0,0 +1,10 @@
+package chess;
+
+import chess.view.OutputVIew;
+
+public class CUIChessApp {
+
+    public static void main(String[] args) {
+        OutputVIew.printBoardState();
+    }
+}
---
+++ src/main/java/chess/view/OutputVIew.java
@@ -0,0 +1,16 @@
+package chess.view;
+
+public class OutputVIew {
+
+    public static void printBoardState() {
+        System.out.println("RNBQKBNR");
+        System.out.println("PPPPPPPP");
+        System.out.println("........");
+        System.out.println("........");
+        System.out.println("........");
+        System.out.println("........");
+        System.out.println("pppppppp");
+        System.out.println("rnbqkbnr");
+    }
+
+}
