af161f2d4fecd278833bc7774a2c8f2e380d4881
yunheehyeon <ok94life@daum.net>
2019-06-14 15:15:23 +0900

    feat : 사용자로부터 입력을 받고 게임 시작과 종료를 처리

--- src/main/java/chess/CUIChessApp.java
+++ src/main/java/chess/CUIChessApp.java
@@ -1,10 +1,27 @@
 package chess;
 
+import chess.view.InputView;
 import chess.view.OutputVIew;
 
 public class CUIChessApp {
 
     public static void main(String[] args) {
-        OutputVIew.printBoardState();
+        OutputVIew.printInputGuide();
+        String input = InputView.promptUserSelection();
+        while (!handleUserSelection(input)) {
+            input = InputView.promptUserSelection();
+        }
+    }
+
+    private static boolean handleUserSelection(String input) {
+        if (input.equals("start")) {
+            OutputVIew.printBoardState();
+            return false;
+        }
+        if (input.equals("end")) {
+            System.exit(0);
+            return true;
+        }
+        throw new IllegalArgumentException("알 수 없는 명령입니다");
     }
 }
--- src/main/java/chess/view/OutputVIew.java
+++ src/main/java/chess/view/OutputVIew.java
@@ -2,6 +2,11 @@ package chess.view;
 
 public class OutputVIew {
 
+    public static void printInputGuide() {
+        System.out.println("체스 게임을 시작합니다.");
+        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+    }
+
     public static void printBoardState() {
         System.out.println("RNBQKBNR");
         System.out.println("PPPPPPPP");
