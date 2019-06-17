a1b895624e5343ecaa850dff58a644071c2e8847
Jo Jinsik <kunggom@gmail.com>
2019-06-17 18:34:23 +0900

    작성: ConsoleOutput.getBoardString()
    Board 인스턴스를 받아 콘솔 문자열로 출력하는 정적 메소드. 테스트도 작성하였다.

---
+++ src/main/java/chess/view/ConsoleOutput.java
@@ -0,0 +1,66 @@
+package chess.view;
+
+import chess.model.Column;
+import chess.model.Row;
+import chess.model.board.Board;
+import chess.model.unit.Piece;
+import chess.model.unit.Side;
+import chess.model.unit.UnitClass;
+
+import java.util.*;
+
+public class ConsoleOutput {
+    private static final Map<UnitClass, String> PIECE_SYMBOL = new HashMap<>();
+    private static final String EMPTY_SQUARE = ".";
+    private static final String NEW_LINE = "\n";
+
+    static {
+        PIECE_SYMBOL.put(UnitClass.KING, "K");
+        PIECE_SYMBOL.put(UnitClass.QUEEN, "Q");
+        PIECE_SYMBOL.put(UnitClass.ROOK, "R");
+        PIECE_SYMBOL.put(UnitClass.BISHOP, "B");
+        PIECE_SYMBOL.put(UnitClass.KNIGHT, "N");
+        PIECE_SYMBOL.put(UnitClass.PAWN, "P");
+    }
+
+    private static String getPieceSymbol(final Board board, final Column column, final Row row) {
+        String symbol = EMPTY_SQUARE;
+        try {
+            final Piece piece = board.getPiece(column, row);
+            symbol = getSymbolBySide(piece);
+        } catch (Exception e) {
+            // pass
+        }
+        return symbol;
+    }
+
+    private static String getSymbolBySide(final Piece piece) {
+        String symbol = PIECE_SYMBOL.get(piece.getUnitClass());
+        if (piece.getSide().equals(Side.WHITE)) {
+            symbol = symbol.toLowerCase();
+        }
+        return symbol;
+    }
+
+    public static String getBoardString(final Board board) {
+        final StringBuilder result = new StringBuilder();
+        final List<StringBuilder> sbList = new ArrayList<>();
+        for (Row row : Row.values()) {
+            sbList.add(boardRow(board, row));
+        }
+        Collections.reverse(sbList);
+        for (StringBuilder sb : sbList) {
+            result.append(sb);
+            result.append(NEW_LINE);
+        }
+        return result.toString();
+    }
+
+    private static StringBuilder boardRow(final Board board, final Row row) {
+        StringBuilder sb = new StringBuilder();
+        for (Column column : Column.values()) {
+            sb.append(getPieceSymbol(board, column, row));
+        }
+        return sb;
+    }
+}
