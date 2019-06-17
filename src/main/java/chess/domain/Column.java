7eb3ebf2a3665c53daa444b4c43d9a801c8e216b
DaejunBae <dpudpu11@gmail.com>
2019-06-17 15:21:56 +0900

    feat: Column.class 추가

---
+++ src/main/java/chess/domain/Column.java
@@ -0,0 +1,44 @@
+package chess.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Column {
+    private static final Map<String, Column> COLUMNS = new HashMap<>();
+    static final char MIN = 'a';
+    static final char MAX = 'h';
+
+    private final String column;
+
+
+    static {
+        for (int i = MIN; i < MAX; i++) {
+            String columnName = String.valueOf((char) i);
+            COLUMNS.put(columnName, new Column(columnName));
+        }
+    }
+
+    private Column(final String column) {
+        this.column = column;
+    }
+
+    static Column from(final String column) {
+        Optional<Column> optColumn = Optional.ofNullable(COLUMNS.get(column));
+        return optColumn.orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Column column1 = (Column) o;
+        return Objects.equals(column, column1.column);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column);
+    }
+}
---
+++ src/test/java/chess/domain/ColumnTest.java
@@ -0,0 +1,32 @@
+package chess.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ColumnTest {
+
+    @Test
+    public void 동치성_테스트() {
+        Column column1 = Column.from("a");
+        Column column2 = Column.from("a");
+
+        assertThat(column1).isEqualTo(column2);
+    }
+
+    @Test
+    public void 캐싱_테스트() {
+        Column column1 = Column.from("a");
+        Column column2 = Column.from("a");
+
+        assertThat(column1 == column2).isTrue();
+    }
+
+    @Test
+    public void 범위_유효성_테스트() {
+        String overThanMax = String.valueOf(Column.MAX + 1);
+        assertThrows(IllegalArgumentException.class, () -> Column.from(overThanMax));
+    }
+
+}
