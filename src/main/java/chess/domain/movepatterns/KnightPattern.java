738f10e8aba2f2e9460eaf176544580073b1c467
conatuseus <conatuseus@gmail.com>
2019-06-18 16:57:14 +0900

    feat: Knight move pattern 구현
    Knight가 지정된 위치로 움직일 수 있는지 확인하는 메서드를 구현하였다.
    KnightPattern Test 구현
    
    Squashed commit of the following:
    
    commit af114024b6e0e5ea070c1cd4d33e7efd1d504d89
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Tue Jun 18 16:56:22 2019 +0900
    
        Knight 이동 구현

--- src/main/java/chess/domain/movepatterns/KnightPattern.java
+++ src/main/java/chess/domain/movepatterns/KnightPattern.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class KingPattern implements MovePattern {
+public class KnightPattern implements MovePattern {
 
     private static final int FIRST_DIRECT_INDEX = 0;
     private static final int LAST_DIRECT_INDEX = 8;
 
-    private List<Integer> directX = Arrays.asList(-1, -1, -1, 0, 0, 1, 1, 1);
-    private List<Integer> directY = Arrays.asList(-1, 0, 1, -1, 1, -1, 0, 1);
+    private List<Integer> directX = Arrays.asList(-2, -2, -1, -1, 1, 1, 2, 2);
+    private List<Integer> directY = Arrays.asList(-1, 1, -2, 2, -2, 2, -1, 1);
 
     @Override
     public boolean canMove(Point source, Point target) {
