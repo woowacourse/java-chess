a7922f4f12166ed865588bcecbd2321c5f2f1419
pkch93 <star_final@naver.com>
2019-06-18 14:47:31 +0900

    feat: Position class implement

---
+++ src/main/java/chess/domain/Coordinate.java
@@ -0,0 +1,35 @@
+package chess.domain;
+
+import java.util.Objects;
+
+public class Coordinate {
+    private final int coordinate;
+
+    public Coordinate(int coordinate) {
+        if (coordinate < 1 || coordinate > 8) {
+            throw new IllegalArgumentException("세로 범위는 1~8입니다.");
+        }
+        this.coordinate = coordinate;
+    }
+
+    public Coordinate(char coordinate) {
+        int converted = coordinate - 96;
+        if (converted < 1 || converted > 8) {
+            throw new IllegalArgumentException("세로 범위는 1~8입니다.");
+        }
+        this.coordinate = converted;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Coordinate that = (Coordinate) o;
+        return coordinate == that.coordinate;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinate);
+    }
+}
---
+++ src/main/java/chess/domain/Position.java
@@ -0,0 +1,27 @@
+package chess.domain;
+
+import java.util.Objects;
+
+public class Position {
+    private final Coordinate x;
+    private final Coordinate y;
+
+    public Position(final Coordinate x, final Coordinate y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Position position = (Position) o;
+        return Objects.equals(x, position.x) &&
+                Objects.equals(y, position.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
