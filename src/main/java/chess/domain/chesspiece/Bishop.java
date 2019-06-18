5fcb64e6420092f2503bfbcd86de9d9bab412d5b
wbluke <pwb0127@naver.com>
2019-06-18 16:22:17 +0900

    [ADD] Bishop ChessPiece 로직 추가

--- src/main/java/chess/domain/chesspiece/Bishop.java
+++ src/main/java/chess/domain/chesspiece/Bishop.java
@@ -6,12 +6,10 @@ import chess.domain.RelativeChessPoint;
 import java.util.Arrays;
 import java.util.List;
 
-public class Queen implements ChessPiece {
+public class Bishop implements ChessPiece {
     private static final List<RelativeChessPoint> DIRECTIONS = Arrays.asList(
-            RelativeChessPoint.of(1, 0), RelativeChessPoint.of(1, 1)
-            , RelativeChessPoint.of(0, 1), RelativeChessPoint.of(-1, 1)
-            , RelativeChessPoint.of(-1, 0), RelativeChessPoint.of(-1, -1)
-            , RelativeChessPoint.of(0, -1), RelativeChessPoint.of(1, -1));
+            RelativeChessPoint.of(1, 1), RelativeChessPoint.of(-1, 1)
+            , RelativeChessPoint.of(-1, -1), RelativeChessPoint.of(1, -1));
 
     @Override
     public boolean checkRule(ChessPoint source, ChessPoint target) {
