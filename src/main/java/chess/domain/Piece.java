b03bd50e1efbc69396102efffbc1f29e9016e937
DaejunBae <dpudpu11@gmail.com>
2019-06-18 19:37:42 +0900

    feat: Piece.class Rule과 Color를 가지는 Piece 구현

--- src/main/java/chess/domain/Piece.java
+++ src/main/java/chess/domain/Piece.java
@@ -1,5 +1,40 @@
 package chess.domain;
 
-public interface Piece {
-    boolean isValidMove(final Position origin, final Position target);
+import chess.domain.RuleImpl.Empty;
+import chess.domain.RuleImpl.Rule;
+
+public class Piece {
+    private static final Piece EMPTY = new Piece(Color.WHITE, Empty.getInstance());
+
+    private Color color;
+    private Rule rule;
+
+
+    private Piece(final Color color, final Rule rule) {
+        this.color = color;
+        this.rule = rule;
+    }
+
+    public static Piece of(final Color color, final Rule rule) {
+        return new Piece(color, rule);
+    }
+
+    public boolean isValidMove(final Position origin, final Position target) {
+        return rule.isValidMove(origin, target);
+    }
+
+    public enum Color {
+        WHITE(1),
+        BLACK(2);
+
+        private int index;
+
+        Color(final int index) {
+            this.index = index;
+        }
+    }
+
+    public static Piece empty() {
+        return EMPTY;
+    }
 }
