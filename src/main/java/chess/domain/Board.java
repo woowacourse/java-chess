61efa833c2500e31be480d8e132db2f00689fd0b
stopsilver13 <stopsilver13>
2019-06-19 11:34:06 +0900

    [추가/수정] checkValidPosition 메서드 구현, 테스트코드 작성, README.md 수정

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -1,48 +1,55 @@
 package chess.domain;
 
-import chess.domain.Piece;
-import chess.domain.Position;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Board {
-    private static Map<Position, Piece> pieces = new HashMap<>();
+    private Map<Position, Piece> pieces = new HashMap<>();
 
-    static {
-        pieces.put(Position.valueOf("a1"),new Rook(Aliance.WHITE));
-        pieces.put(Position.valueOf("b1"),new Knight(Aliance.WHITE));
-        pieces.put(Position.valueOf("c1"),new Bishop(Aliance.WHITE));
-        pieces.put(Position.valueOf("d1"),new Queen(Aliance.WHITE));
-        pieces.put(Position.valueOf("e1"),new King(Aliance.WHITE));
-        pieces.put(Position.valueOf("f1"),new Bishop(Aliance.WHITE));
-        pieces.put(Position.valueOf("g1"),new Knight(Aliance.WHITE));
-        pieces.put(Position.valueOf("h1"),new Rook(Aliance.WHITE));
+    public Board() {
+        initBoard();
+    }
 
-        pieces.put(Position.valueOf("a8"),new Rook(Aliance.BLACK));
-        pieces.put(Position.valueOf("b8"),new Knight(Aliance.BLACK));
-        pieces.put(Position.valueOf("c8"),new Bishop(Aliance.BLACK));
-        pieces.put(Position.valueOf("d8"),new Queen(Aliance.BLACK));
-        pieces.put(Position.valueOf("e8"),new King(Aliance.BLACK));
-        pieces.put(Position.valueOf("f8"),new Bishop(Aliance.BLACK));
-        pieces.put(Position.valueOf("g8"),new Knight(Aliance.BLACK));
-        pieces.put(Position.valueOf("h8"),new Rook(Aliance.BLACK));
+    public void initBoard() {
+        pieces.put(Position.valueOf("a1"), new Rook(Aliance.WHITE));
+        pieces.put(Position.valueOf("b1"), new Knight(Aliance.WHITE));
+        pieces.put(Position.valueOf("c1"), new Bishop(Aliance.WHITE));
+        pieces.put(Position.valueOf("d1"), new Queen(Aliance.WHITE));
+        pieces.put(Position.valueOf("e1"), new King(Aliance.WHITE));
+        pieces.put(Position.valueOf("f1"), new Bishop(Aliance.WHITE));
+        pieces.put(Position.valueOf("g1"), new Knight(Aliance.WHITE));
+        pieces.put(Position.valueOf("h1"), new Rook(Aliance.WHITE));
+
+        pieces.put(Position.valueOf("a8"), new Rook(Aliance.BLACK));
+        pieces.put(Position.valueOf("b8"), new Knight(Aliance.BLACK));
+        pieces.put(Position.valueOf("c8"), new Bishop(Aliance.BLACK));
+        pieces.put(Position.valueOf("d8"), new Queen(Aliance.BLACK));
+        pieces.put(Position.valueOf("e8"), new King(Aliance.BLACK));
+        pieces.put(Position.valueOf("f8"), new Bishop(Aliance.BLACK));
+        pieces.put(Position.valueOf("g8"), new Knight(Aliance.BLACK));
+        pieces.put(Position.valueOf("h8"), new Rook(Aliance.BLACK));
 
         List<Position> whitePawnPositions = Position.getRow("2");
         List<Position> blackPawnPositions = Position.getRow("7");
 
-        for(Position whitePawnPosition : whitePawnPositions){
-            pieces.put(whitePawnPosition,new Pawn(Aliance.WHITE));
+        for (Position whitePawnPosition : whitePawnPositions) {
+            pieces.put(whitePawnPosition, new Pawn(Aliance.WHITE));
         }
 
-        for(Position blackPawnPosition : blackPawnPositions){
-            pieces.put(blackPawnPosition,new Pawn(Aliance.BLACK));
+        for (Position blackPawnPosition : blackPawnPositions) {
+            pieces.put(blackPawnPosition, new Pawn(Aliance.BLACK));
         }
 
     }
 
-    public static Piece pieceValueOf(String position) {
+    public Piece pieceValueOf(String position) {
         return pieces.get(Position.valueOf(position));
+    }
+
+    public void checkValidPosition(String position) {
+        if(Position.valueOf(position) == null){
+            throw new IllegalArgumentException("유효하지 않은 위치입니다.");
+        }
     }
 }
--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -1,19 +1,32 @@
 package domain;
 
 import chess.domain.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BoardTest {
+    Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
 
     @Test
     void 입력받은_위치에_말이_있는_경우_말_반환_확인() {
-        assertThat(Board.pieceValueOf("a1").toString()).isEqualTo("r");
+        assertThat(board.pieceValueOf("a1").toString()).isEqualTo("r");
     }
 
     @Test
     void 입력받은_위치에_말이_없는_경우() {
-        assertThat(Board.pieceValueOf("c3")).isEqualTo(null);
+        assertThat(board.pieceValueOf("c3")).isEqualTo(null);
+    }
+
+    @Test
+    void 입력받은_위치가_유효한위치가_아닌경우(){
+        assertThrows(IllegalArgumentException.class, () ->board.checkValidPosition("k3"));
     }
 }
