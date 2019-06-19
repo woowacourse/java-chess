99e1560c46ab35611d7742cb3576ea9b9f0d5966
pkch93 <star_final@naver.com>
2019-06-19 11:43:34 +0900

    feat: 퀸 이동 로직 구현

--- src/main/java/chess/domain/Queen.java
+++ src/main/java/chess/domain/Queen.java
@@ -1,10 +1,25 @@
 package chess.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static chess.domain.Direction.*;
+import static chess.domain.Direction.NW;
+
 public class Queen extends Piece {
     private static final String NAME = "q";
+    private static final List<Direction> MOVABLE_DIRECTION = Arrays.asList(N, NE, E, SE, S, SW, W, NW);
 
     public Queen(Team team) {
-        super(team, (a, b) -> false);
+        super(team, (source, target) -> {
+            Direction direction = source.direction(target);
+
+            if (MOVABLE_DIRECTION.stream().noneMatch(movable -> movable == direction)) {
+                throw new IllegalArgumentException("움직일 수 있는 방향이 아닙니다.");
+            }
+
+            return true;
+        });
     }
 
     public String getName() {
