3798083a84df6e0d7abe28e5c84583b81435ddd5
starkim06 <starkim06@naver.com>
2019-06-19 15:32:30 +0900

    feat: Spot 클래스 생성
    체스판에서 한 칸을 뜻하는 Spot 클래스 생성

---
+++ src/main/java/chess/domain/Spot.java
@@ -0,0 +1,61 @@
+package chess.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Spot {
+    private static final int MIN_COLUMN = 0;
+    private static final int MAX_COLUMN = 7;
+    private static final int MIN_ROW = 0;
+    private static final int MAX_ROW = 7;
+
+    private final int x;
+    private final int y;
+
+    private Spot(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Spot valueOf(final int x, final int y) {
+        validation(x, y);
+        int index = x * 8 + y;
+        return SpotCache.spotsCache.get(index);
+    }
+
+    private static void validation(int x, int y) {
+        if (validationRow(x) || validationColumn(y)) {
+            throw new IllegalArgumentException("범위 제대로 입력해 주세요");
+        }
+    }
+
+    private static boolean validationRow(int x) {
+        return x < MIN_COLUMN || x > MAX_COLUMN;
+    }
+
+    private static boolean validationColumn(int y) {
+        return y < MIN_ROW || y > MAX_ROW;
+    }
+
+    public int getX(Spot spot) {
+        return this.x - spot.x;
+    }
+
+    public int getY(Spot spot) {
+        return this.y - spot.y;
+    }
+
+    private static class SpotCache {
+        private static List<Spot> spotsCache = new ArrayList<>();
+
+        //TODO 이중 for 문 리팩토링하기
+        static {
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j <= 8; j++) {
+                    spotsCache.add(new Spot(i, j));
+                }
+            }
+        }
+
+    }
+}
---
+++ src/test/java/chess/domain/SpotTest.java
@@ -0,0 +1,29 @@
+package chess.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class SpotTest {
+    @Test
+    void 캐싱_확인() {
+        Spot spotA = Spot.valueOf(7, 1);
+        assertThat(spotA == Spot.valueOf(7, 1)).isTrue();
+    }
+
+    @Test
+    void 최저값_예외_검사() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Spot.valueOf(-1, -1);
+        });
+    }
+
+    @Test
+    void 최대값_예외_검사() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Spot.valueOf(8, 8);
+        });
+    }
+}
