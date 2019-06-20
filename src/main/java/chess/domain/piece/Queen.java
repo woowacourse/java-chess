8aa96fe16c4b7abda8a139c3b24b232c85b4fa1b
starkim06 <starkim06@naver.com>
2019-06-20 12:17:19 +0900

    feat: Queen 클래스 생성

---
+++ src/main/java/chess/domain/piece/Queen.java
@@ -0,0 +1,28 @@
+package chess.domain.piece;
+
+import chess.domain.MovementUnit;
+import chess.domain.Spot;
+import chess.domain.Team;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Queen extends Piece {
+    private final Set<MovementUnit> movementUnits;
+
+    public Queen(Team team) {
+        super(team);
+        movementUnits = new HashSet<>();
+        movementUnits.add(MovementUnit.RIGHT);
+        movementUnits.add(MovementUnit.UP);
+        movementUnits.add(MovementUnit.DIAGNOAL);
+    }
+
+    @Override
+    public boolean isMovable(Spot startSpot, Spot endSpot) {
+        int distanceX = startSpot.getX(endSpot);
+        int distanceY = startSpot.getY(endSpot);
+
+        return movementUnits.contains(MovementUnit.direction(distanceX, distanceY));
+    }
+}
