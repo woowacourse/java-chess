7fbdc20e925a025a73765bad7bfe06b350e96c15
pkch93 <star_final@naver.com>
2019-06-20 11:59:25 +0900

    refactor: ResultCounter Class 초기화 메서드 구현

--- src/main/java/chess/domain/ResultCounter.java
+++ src/main/java/chess/domain/ResultCounter.java
@@ -8,8 +8,9 @@ import java.util.List;
 import java.util.Map;
 
 public class ResultCounter {
-    private static final Map<Piece, Count> resultCounter;
-    private static List<Piece> pieces = Arrays.asList(
+    private final Map<Piece, Count> resultCounter;
+
+    private List<Piece> pieces = Arrays.asList(
             new Pawn(Team.BLACK),
             new Rook(Team.BLACK),
             new Bishop(Team.BLACK),
@@ -24,20 +25,24 @@ public class ResultCounter {
             new King(Team.WHITE)
     );
 
-    static {
+    private ResultCounter() {
         resultCounter = new HashMap<>();
         pieces.forEach(piece -> resultCounter.put(piece, new Count()));
     }
 
-    public static Count pieceCount(final Piece piece) {
+    public static ResultCounter init() {
+        return new ResultCounter();
+    }
+
+    public Count pieceCount(final Piece piece) {
         return resultCounter.get(piece);
     }
 
-    public static void addCount(final Piece piece) {
+    public void addCount(final Piece piece) {
         resultCounter.get(piece).add();
     }
 
-    public static double totalScore(final Team team) {
+    public double totalScore(final Team team) {
         return pieces.stream()
                 .filter(piece -> piece.isSameTeam(team))
                 .mapToDouble(piece -> resultCounter.get(piece).score(piece))
--- src/test/java/chess/domain/ResultCounterTest.java
+++ src/test/java/chess/domain/ResultCounterTest.java
@@ -1,16 +1,23 @@
 package chess.domain;
 
 import chess.domain.piece.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResultCounterTest {
+    ResultCounter resultCounter;
+
+    @BeforeEach
+    void setUp() {
+        resultCounter = ResultCounter.init();
+    }
 
     @Test
     void 초기_생성_테스트() {
-        Count pawnBlackCount = ResultCounter.pieceCount(new Pawn(Team.BLACK));
-        Count bishopWhiteCount = ResultCounter.pieceCount(new Pawn(Team.WHITE));
+        Count pawnBlackCount = resultCounter.pieceCount(new Pawn(Team.BLACK));
+        Count bishopWhiteCount = resultCounter.pieceCount(new Pawn(Team.WHITE));
 
         assertThat(pawnBlackCount).isEqualTo(new Count());
         assertThat(bishopWhiteCount).isEqualTo(new Count());
@@ -19,33 +26,32 @@ public class ResultCounterTest {
     @Test
     void 카운터_1_추가_테스트() {
         Rook rook = new Rook(Team.BLACK);
-        ResultCounter.addCount(rook);
-        ResultCounter.addCount(rook);
+        resultCounter.addCount(rook);
+        resultCounter.addCount(rook);
 
-        assertThat(ResultCounter.pieceCount(rook)).isEqualTo(new Count().add().add());
+        assertThat(resultCounter.pieceCount(rook)).isEqualTo(new Count().add().add());
     }
 
     @Test
     void 총_점수_블랙팀_계산_테스트() {
-        ResultCounter.addCount(new Rook(Team.BLACK)); // 5
-        ResultCounter.addCount(new Bishop(Team.WHITE)); // ignored
-        ResultCounter.addCount(new Queen(Team.BLACK)); // 9
-        ResultCounter.addCount(new Pawn(Team.WHITE)); // ignored
-        ResultCounter.addCount(new Knight(Team.BLACK)); // 2.5
+        resultCounter.addCount(new Rook(Team.BLACK)); // 5
+        resultCounter.addCount(new Bishop(Team.WHITE)); // ignored
+        resultCounter.addCount(new Queen(Team.BLACK)); // 9
+        resultCounter.addCount(new Pawn(Team.WHITE)); // ignored
+        resultCounter.addCount(new Knight(Team.BLACK)); // 2.5
 
-        assertThat(ResultCounter.totalScore(Team.BLACK)).isEqualTo(16.5);
-
+        assertThat(resultCounter.totalScore(Team.BLACK)).isEqualTo(16.5);
     }
 
     @Test
     void 총_점수_화이트팀_계산_테스트() {
-        ResultCounter.addCount(new Rook(Team.BLACK)); // ignored
-        ResultCounter.addCount(new Bishop(Team.WHITE)); // 3
-        ResultCounter.addCount(new Queen(Team.BLACK)); // ignored
-        ResultCounter.addCount(new Pawn(Team.WHITE)); // 1
-        ResultCounter.addCount(new Knight(Team.BLACK)); // ignored
+        resultCounter.addCount(new Rook(Team.BLACK)); // ignored
+        resultCounter.addCount(new Bishop(Team.WHITE)); // 3
+        resultCounter.addCount(new Queen(Team.BLACK)); // ignored
+        resultCounter.addCount(new Pawn(Team.WHITE)); // 1
+        resultCounter.addCount(new Knight(Team.BLACK)); // ignored
 
-        assertThat(ResultCounter.totalScore(Team.WHITE)).isEqualTo(4);
+        assertThat(resultCounter.totalScore(Team.WHITE)).isEqualTo(4);
 
     }
 }
