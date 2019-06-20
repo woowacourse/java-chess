82c483d529e1255a47aeeab501f42ce174c298bd
pkch93 <star_final@naver.com>
2019-06-20 17:37:18 +0900

    refactor: ResultCounter class

--- src/main/java/chess/domain/ResultCounter.java
+++ src/main/java/chess/domain/ResultCounter.java
@@ -10,26 +10,23 @@ import java.util.Map;
 public class ResultCounter {
     private final Map<AbstractPiece, Count> resultCounter;
 
-    private List<AbstractPiece> abstractPieces = Arrays.asList(
-            new Pawn(Team.BLACK),
-            new Rook(Team.BLACK),
-            new Bishop(Team.BLACK),
-            new Knight(Team.BLACK),
-            new Queen(Team.BLACK),
-            new King(Team.BLACK),
-            new Pawn(Team.WHITE),
-            new Rook(Team.WHITE),
-            new Bishop(Team.WHITE),
-            new Knight(Team.WHITE),
-            new Queen(Team.WHITE),
-            new King(Team.WHITE)
-    );
+    private List<AbstractPiece> pieces(Team team) {
+        return Arrays.asList(
+                new Pawn(team),
+                new Rook(team),
+                new Bishop(team),
+                new Knight(team),
+                new Queen(team),
+                new King(team)
+        );
+    }
 
     private static ResultCounter instance;
 
     private ResultCounter() {
         resultCounter = new HashMap<>();
-        abstractPieces.forEach(piece -> resultCounter.put(piece, new Count()));
+        pieces(Team.WHITE).forEach(piece -> resultCounter.put(piece, new Count()));
+        pieces(Team.BLACK).forEach(piece -> resultCounter.put(piece, new Count()));
     }
 
     public static ResultCounter getInstance() {
@@ -51,8 +48,7 @@ public class ResultCounter {
     }
 
     public double totalScore(final Team team) {
-        return abstractPieces.stream()
-                .filter(piece -> piece.isSameTeam(team))
+        return pieces(team).stream()
                 .mapToDouble(piece -> resultCounter.get(piece).score(piece))
                 .sum();
     }
