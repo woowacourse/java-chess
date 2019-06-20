bf962f4718a5f30ccdc624be94516db743f0452f
conatuseus <conatuseus@gmail.com>
2019-06-20 10:52:49 +0900

    feat: Queen 체스 말 구현
    * Queen.java: isValidMovePattern메서드는 도착 위치에 Queen 체스 말을
    옮길 수 있는지 확인하고 makePath메서드는 그 위치를 지나가는 좌표
    리스트를 반환한다.
    
    * QueenTest.java: makePath메서드가 정상적으로 작동하는지 테스트한다.
    
    Squashed commit of the following:
    
    commit d2ee2396ee16cd732e45929e92af1492b9a165b4
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Thu Jun 20 10:52:36 2019 +0900
    
        queen 구현

--- src/main/java/chess/domain/Queen.java
+++ src/main/java/chess/domain/Queen.java
@@ -6,11 +6,11 @@ import chess.domain.movepatterns.MovePattern;
 import java.util.ArrayList;
 import java.util.List;
 
-public class King {
+public class Queen {
     private final MovePattern movePattern = new KingPattern();
     private final Integer color;
 
-    public King(Integer color) {
+    public Queen(Integer color) {
         this.color = color;
     }
 
@@ -19,6 +19,15 @@ public class King {
     }
 
     public List<Point> makePath(Point source, Point target) {
-        return new ArrayList<>();
+        List<Point> path = new ArrayList<>();
+        Direction direction = Direction.of(source, target);
+
+        Point nextPoint = source.plusPoint(direction.getDirection());
+        while (!nextPoint.equals(target)) {
+            path.add(nextPoint);
+            nextPoint = nextPoint.plusPoint(direction.getDirection());
+        }
+
+        return path;
     }
 }
---
+++ src/test/java/domain/QueenTest.java
@@ -0,0 +1,36 @@
+package domain;
+
+import chess.domain.Point;
+import chess.domain.PointFactory;
+import chess.domain.Queen;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class QueenTest {
+
+    @Test
+    void 정상_생성_Test() {
+        assertDoesNotThrow(() -> new Queen(0));
+    }
+
+    @Test
+    void Queen_직선_경로_확인_Test() {
+        List<Point> path = Arrays.asList(PointFactory.of("a2"), PointFactory.of("a3"));
+        Point source = PointFactory.of("a1");
+        Point target = PointFactory.of("a4");
+        assertThat(new Queen(1).makePath(source, target)).isEqualTo(path);
+    }
+
+    @Test
+    void Queen_대각선_경로_확인_Test() {
+        List<Point> path = Arrays.asList(PointFactory.of("b2"), PointFactory.of("c3"));
+        Point source = PointFactory.of("a1");
+        Point target = PointFactory.of("d4");
+        assertThat(new Queen(1).makePath(source, target)).isEqualTo(path);
+    }
+}
