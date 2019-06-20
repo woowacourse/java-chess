b628c5674d36d00c5640f31d4500949eb7320419
hyojaekim <hj00686@naver.com>
2019-06-20 16:38:45 +0900

    feat: 폰 한칸 이동 추가

--- src/main/java/chess/domain/Pawn.java
+++ src/main/java/chess/domain/Pawn.java
@@ -1,18 +1,31 @@
 package chess.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import static chess.domain.Direction.*;
+
 public class Pawn implements Piece{
     private final boolean teamColor;
+    private final List<Direction> candidateDirection;
 
     public Pawn(boolean teamColor) {
         this.teamColor = teamColor;
+        this.candidateDirection = (teamColor) ? Arrays.asList(NORTH, NORTH_EAST, NORTH_WEST) :
+                Arrays.asList(SOUTH, SOUTH_EAST, SOUTH_WEST);
     }
 
     @Override
     public List<Point> getCandidatePoints(Point start, Point end) {
-        return null;
+        List<Point> points = new ArrayList<>();
+        Point vector = start.makeVector(end);
+        Direction foundDirection = Direction.findDirection(vector);
+        if(candidateDirection.contains(foundDirection)){
+            points.add(end);
+        }
+        return points;
     }
 
     @Override
--- src/test/java/chess/domain/PawnTest.java
+++ src/test/java/chess/domain/PawnTest.java
@@ -2,11 +2,71 @@ package chess.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class PawnTest {
     @Test
     void 생성() {
         assertDoesNotThrow(() -> new Pawn(true));
+    }
+
+    @Test
+    void 흰색팀_직선_한칸_이동() {
+        Piece pawn = new Pawn(true);
+        List<Point> points = new ArrayList<>();
+        points.add(new Point("a3"));
+        assertThat(pawn.getCandidatePoints(new Point("a2"), new Point("a3"))).isEqualTo(points);
+    }
+
+    @Test
+    void 흰색팀_대각선_한칸_이동() {
+        Piece pawn = new Pawn(true);
+        List<Point> points = new ArrayList<>();
+        points.add(new Point("b3"));
+        assertThat(pawn.getCandidatePoints(new Point("a2"), new Point("b3"))).isEqualTo(points);
+    }
+
+    @Test
+    void 검은색팀_직선_한칸_이동() {
+        Piece pawn = new Pawn(false);
+        List<Point> points = new ArrayList<>();
+        points.add(new Point("a6"));
+        assertThat(pawn.getCandidatePoints(new Point("a7"), new Point("a6"))).isEqualTo(points);
+    }
+
+    @Test
+    void 검은색팀_대각선_한칸_이동() {
+        Piece pawn = new Pawn(false);
+        List<Point> points = new ArrayList<>();
+        points.add(new Point("b6"));
+        assertThat(pawn.getCandidatePoints(new Point("a7"), new Point("b6"))).isEqualTo(points);
+    }
+
+    @Test
+    void 검은색팀_대각선_잘못된_이동() {
+        Piece pawn = new Pawn(false);
+        assertThat(pawn.getCandidatePoints(new Point("a7"), new Point("b5")).size()).isEqualTo(0);
+    }
+
+    @Test
+    void 흰색팀_대각선_잘못된_이동() {
+        Piece pawn = new Pawn(true);
+        assertThat(pawn.getCandidatePoints(new Point("a2"), new Point("b4")).size()).isEqualTo(0);
+    }
+
+    @Test
+    void 검은색팀_직선_잘못된_이동() {
+        Piece pawn = new Pawn(false);
+        assertThat(pawn.getCandidatePoints(new Point("a7"), new Point("a4")).size()).isEqualTo(0);
+    }
+
+    @Test
+    void 흰색팀_직선_잘못된_이동() {
+        Piece pawn = new Pawn(true);
+        assertThat(pawn.getCandidatePoints(new Point("a2"), new Point("a5")).size()).isEqualTo(0);
     }
 }
