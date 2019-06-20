d743558e24e84cd6c4d5ecbd0e359a254afefd9d
hyojaekim <hj00686@naver.com>
2019-06-20 15:29:55 +0900

    fix: 방향 결정 버그 수정

--- src/main/java/chess/domain/Navigator.java
+++ src/main/java/chess/domain/Navigator.java
@@ -1,16 +1,31 @@
 package chess.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Navigator {
-    private final Direction direction;
+    public static final int ROUNDING_DIGIT = 100;
+    private final Point start;
+    private final Point end;
 
     public Navigator(Point start, Point end) {
-        Point directionPoint = start.calDirection(end);
-        this.direction = Direction.findDirection(directionPoint);
+        this.start = start;
+        this.end = end;
     }
 
     public Direction getDirection(List<Direction> directions) {
-        return directions.contains(direction) ? direction : Direction.NOT_FIND;
+        List<Direction> filteredDirection = directions.stream()
+                .filter(d -> isParallel(d))
+                .collect(Collectors.toList());
+        return filteredDirection.size() == 0 ? Direction.NOT_FIND : filteredDirection.get(0);
+    }
+
+    private boolean isParallel(Direction direction) {
+        Point vector = start.makeVector(end);
+        double dotProduction = (double) vector.dotProduct(direction.getPosition());
+        double vectorScalar = vector.calScalar();
+        double directionScalar = direction.getPosition().calScalar();
+        double lengthProduction = (Math.floor((vectorScalar * directionScalar * ROUNDING_DIGIT))) / ROUNDING_DIGIT;
+        return dotProduction == lengthProduction;
     }
 }
