e208b1240ed540aea31e575e9ec72a5afffcd1c8
choihz <choixhz@gmail.com>
2019-06-20 12:07:02 +0900

    feat : Piece - determine if pawn is possible to move to given position

--- src/main/java/chess/domain/pieces/Pawn.java
+++ src/main/java/chess/domain/pieces/Pawn.java
@@ -5,8 +5,25 @@ import chess.domain.Team;
 
 public class Pawn extends Piece {
     private static final int SCORE = 0;
+    private static final int DISTANCE_FOR_ALL_DIRECTION = 1;
+    private static final int DISTANCE_FOR_STARTING_POSITION = 4;
+    private static final int STANDARD_DIRECTION_VALUE = 0;
 
     public Pawn(Position position, Team team) {
         super(position, team);
+    }
+
+    public boolean canMove(Position position) {
+        return isPossibleDistance(position) && isPossibleDirection(position);
+    }
+
+    public boolean isPossibleDistance(Position position) {
+        return (this.position.getDistanceSquare(position) == DISTANCE_FOR_ALL_DIRECTION)
+                || (this.position.isInStartingPosition() && this.position.getDistanceSquare(position) == DISTANCE_FOR_STARTING_POSITION);
+    }
+
+    public boolean isPossibleDirection(Position position) {
+        return team.equals(Team.WHITE) && this.position.subtractRow(position) < STANDARD_DIRECTION_VALUE
+                || team.equals(Team.BLACK) && this.position.subtractRow(position) > STANDARD_DIRECTION_VALUE;
     }
 }
