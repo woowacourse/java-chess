cda1f7730269297663c7da234331e4e75dcbe79a
gch01410 <gch01410@naver.com>
2019-06-20 13:24:43 +0900

    feat: Add Magnitude class

---
+++ src/main/java/chess/model/Magnitude.java
@@ -0,0 +1,62 @@
+package chess.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Magnitude {
+    private int magnitude;
+
+    public Magnitude(List<Coordinate> coordinates, Direction direction) {
+        validateMagnitudeParameters(coordinates, direction);
+
+        Coordinate sourceCoordinateX = coordinates.get(0);
+        Coordinate sourceCoordinateY = coordinates.get(1);
+        Coordinate targetCoordinateX = coordinates.get(2);
+        Coordinate targetCoordinateY = coordinates.get(3);
+
+        if ((direction == Direction.NORTH) || (direction == Direction.SOUTH)) {
+            this.magnitude = Math.abs(targetCoordinateY.calculateDistance(sourceCoordinateY));
+            return;
+        }
+
+        if ((direction == Direction.EAST) || (direction == Direction.WEST)) {
+            this.magnitude = Math.abs(targetCoordinateX.calculateDistance(sourceCoordinateX));
+            return;
+        }
+
+        if ((direction == Direction.SOUTHEAST) || (direction == Direction.SOUTHWEST)
+                || (direction == Direction.NORTHEAST) || (direction == Direction.NORTHWEST)) {
+            this.magnitude = Math.abs(targetCoordinateX.calculateDistance(sourceCoordinateX));
+            return;
+        }
+
+        this.magnitude = 0;
+    }
+
+    private void validateMagnitudeParameters(List<Coordinate> coordinates, Direction direction) {
+        if (Objects.isNull(coordinates) || coordinates.isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        if (Objects.isNull(direction)) {
+            throw new NullPointerException();
+        }
+    }
+
+    public int getMagnitude() {
+        return magnitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Magnitude magnitude1 = (Magnitude) o;
+        return magnitude == magnitude1.magnitude;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(magnitude);
+    }
+}
