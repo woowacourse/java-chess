a723e4b1b7f69332cd43a0b9897675cb08f5cca1
choihz <choixhz@gmail.com>
2019-06-20 11:45:16 +0900

    feat : Piece - determine if knight is possible to move to given position

--- src/main/java/chess/domain/pieces/Knight.java
+++ src/main/java/chess/domain/pieces/Knight.java
@@ -5,8 +5,13 @@ import chess.domain.Team;
 
 public class Knight extends Piece {
     private static final int SCORE = 0;
+    private static final int DISTANCE_FOR_ALL_DIRECTION = 5;
 
     public Knight(Position position, Team team) {
         super(position, team);
+    }
+
+    boolean canMove(Position position) {
+        return this.position.getDistanceSquare(position) == DISTANCE_FOR_ALL_DIRECTION;
     }
 }
