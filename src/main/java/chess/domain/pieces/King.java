b7bfb6c9c5c933e64bc991160fdf81bcfe4b9a7b
choihz <choixhz@gmail.com>
2019-06-20 11:43:11 +0900

    feat : Piece - determine if king is possible to move to given position

--- src/main/java/chess/domain/pieces/King.java
+++ src/main/java/chess/domain/pieces/King.java
@@ -5,8 +5,15 @@ import chess.domain.Team;
 
 public class King extends Piece {
     private static final int SCORE = 0;
+    private static final int DISTANCE_FOR_FOUR_DIRECTION = 1;
+    private static final int DISTANCE_FOR_DIAGONAL_DIRECTION = 2;
 
     public King(Position position, Team team) {
         super(position, team);
+    }
+
+    boolean canMove(Position position) {
+        return this.position.getDistanceSquare(position) == DISTANCE_FOR_FOUR_DIRECTION
+                || this.position.getDistanceSquare(position) == DISTANCE_FOR_DIAGONAL_DIRECTION;
     }
 }
---
+++ src/test/java/chess/domain/pieces/KingTest.java
@@ -0,0 +1,30 @@
+package chess.domain.pieces;
+
+import chess.domain.Position;
+import chess.domain.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KingTest {
+    private King king;
+
+    @BeforeEach
+    void setUp() {
+        king = new King(new Position(2, 3), Team.WHITE);
+    }
+
+    @Test
+    public void 움직여야_할_때_움직이는지_확인() {
+        assertThat(king.canMove(new Position(2, 4))).isTrue();
+        assertThat(king.canMove(new Position(3, 3))).isTrue();
+        assertThat(king.canMove(new Position(3, 4))).isTrue();
+    }
+
+    @Test
+    public void 움직이지_말아야_할_때_안_움직이는지_확인() {
+        assertThat(king.canMove(new Position(4, 3))).isFalse();
+        assertThat(king.canMove(new Position(4, 5))).isFalse();
+    }
+}
