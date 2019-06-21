953d43a8cc2d8d25e288b85d85da4493ba6b062d
ep1stas1s <masloz@konkuk.ac.kr>
2019-06-21 15:28:01 +0900

    feat: DirectionType 생성

--- src/main/java/chess/domain/board/DirectionType.java
+++ src/main/java/chess/domain/board/DirectionType.java
@@ -1,8 +1,35 @@
 package chess.domain.board;
 
-public enum DirectionType {
-    HORIZON
-    ,VERTICAL,DIAGONAL
+import java.util.Arrays;
 
+public enum DirectionType {
+    EAST(1, 0),
+    SOUTHEAST(1, 1),
+    WEST(-1, 0),
+    NORTHWEST(-1, -1),
+    SOUTH(0, 1),
+    SOUTHWEST(-1, 1),
+    NORTH(0, -1),
+    NORTHEAST(1, -1);
+
+    private int xDegree;
+    private int yDegree;
+
+    DirectionType(int xDegree, int yDegree) {
+        this.xDegree = xDegree;
+        this.yDegree = yDegree;
+    }
+
+    public static DirectionType valueOf(Point prev, Point next) {
+        int xDistance = next.xDistance(prev);
+        int yDistance = next.yDistance(prev);
+        int size = (xDistance != 0) ? Math.abs(xDistance) : Math.abs(yDistance);
+
+        return Arrays.stream(values())
+                .filter(value -> value.xDegree == xDistance / size)
+                .filter(value -> value.yDegree == yDistance / size)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 
 }
