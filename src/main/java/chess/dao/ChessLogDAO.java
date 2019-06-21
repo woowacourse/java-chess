d80816ea420b813e8d9f3b7a85d232e947c9f80b
SunHo Van <vsh123@naver.com>
2019-06-21 19:08:05 +0900

    feat: gameId에 따른 gameLog를 불러오는 기능 구현
    1. 사용자가 원하는 gameId를 입력하면 그 game의 move 경로를 리턴해준다

---
+++ src/main/java/chess/dao/ChessLogDAO.java
@@ -0,0 +1,44 @@
+package chess.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ChessLogDAO {
+    private static ChessLogDAO chessLogDAO;
+    private Connection con;
+
+    private ChessLogDAO() {
+        con = DataBaseConnector.getConnection();
+    }
+
+    public static ChessLogDAO getInstance() {
+        if (chessLogDAO == null) {
+            chessLogDAO = new ChessLogDAO();
+        }
+        return chessLogDAO;
+    }
+
+    public List<List<String>> findGameLogById(String gameId) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<List<String>> gameLog = new ArrayList<>(new ArrayList<>());
+        try {
+            String query = "select source,destination from game_log where game_id = ?";
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, gameId);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                gameLog.add(Arrays.asList(rs.getString(1), rs.getString(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return gameLog;
+    }
+}
