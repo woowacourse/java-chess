8f5d20d5c156d6f81a886a368c663c00290f0d29
backlo <hjun4512@naver.com>
2019-06-22 22:00:13 +0900

    feat(Way): update Way

--- src/main/java/chess/domain/direction/Way.java
+++ src/main/java/chess/domain/direction/Way.java
@@ -1,65 +1,49 @@
 package chess.domain.direction;
 
 import chess.domain.direction.core.Direction;
+import chess.domain.direction.core.MoveStrategy;
 import chess.domain.direction.core.Square;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Way {
     private static final int INFINITY = -1;
 
     private Direction direction;
+    private MoveStrategy moveStrategy;
     private int step;
-    private boolean isCatch;
 
-    public Way(Direction direction) {
-        this.direction = direction;
-        this.step = INFINITY;
+    public Way(Direction direction, MoveStrategy moveStrategy) {
+        this(direction, moveStrategy, INFINITY);
     }
 
-    public Way(Direction direction, int step) {
-        this(direction, step, true);
-    }
-
-    public Way(Direction direction, int step, boolean isCatch) {
+    public Way(Direction direction, MoveStrategy moveStrategy, int step) {
         this.direction = direction;
+        this.moveStrategy = moveStrategy;
         this.step = step;
-        this.isCatch = isCatch;
-    }
-
-    //    Square move(Square startSquare) {
-//        return move(startSquare, 0);
-//    }
-//
-//    private Square move(Square currentSquare, int count) {
-//        if (count == this.step) {
-//            return currentSquare;
-//        }
-//        return move(direction.move(currentSquare), count++);
-//    }
-
-    boolean isGo(Square source, Square target) {
-        return isInfinity() && direction.checkDirection(source, target) ||
-                direction.checkDirection(source, target) && direction.calculateStep(source, target) == step;
     }
 
     private boolean isInfinity() {
         return step == INFINITY;
     }
 
+    boolean canMove(Square source, Square target) {
+        return isInfinity() && direction.checkDirection(source, target) ||
+                direction.checkDirection(source, target) && direction.calculateStep(source, target) == step;
+    }
+
     Route generateRoute(Square source, Square target) {
-        if (!isGo(source, target)) {
+        if (!canMove(source, target)) {
             throw new IllegalArgumentException();
         }
-        List<Square> squares = new ArrayList<>();
-        squares.add(source);
-        for (int i = 1; i <= direction.calculateStep(source, target); i++) {
-            Square prevSquare = squares.get(i - 1);
-            squares.add(direction.move(prevSquare));
-        }
-        return new Route(squares, isCatch);
+
+        return new Route(
+                IntStream.rangeClosed(0, (int) direction.calculateStep(source, target))
+                        .mapToObj(step -> direction.calculateSquare(source, step))
+                        .collect(Collectors.toList())
+                , moveStrategy);
     }
 
     @Override
--- src/test/java/chess/domain/direction/WayTest.java
+++ src/test/java/chess/domain/direction/WayTest.java
@@ -1,6 +1,7 @@
 package chess.domain.direction;
 
 import chess.domain.direction.core.Direction;
+import chess.domain.direction.core.MoveStrategy;
 import chess.domain.direction.core.Square;
 import org.junit.jupiter.api.Test;
 
@@ -12,25 +13,25 @@ class WayTest {
 
     @Test
     void Way생성_테스트() {
-        Way way = new Way(Direction.UP, 1);
-        assertThat(way).isEqualTo(new Way(Direction.UP, 1));
+        Way way = new Way(Direction.UP, MoveStrategy.BOTH);
+        assertThat(way).isEqualTo(new Way(Direction.UP, MoveStrategy.BOTH));
     }
 
     @Test
     void Way를_통해_해당_좌표로_갈수_있는지_테스트() {
-        assertThat(new Way(Direction.DOWN, 1)
-                .isGo(Square.of(3, 2), Square.of(3, 3))).isTrue();
+        assertThat(new Way(Direction.DOWN, MoveStrategy.BOTH)
+                .canMove(Square.of(3, 2), Square.of(3, 3))).isTrue();
     }
 
     @Test
     void Way를_통해_갈수가_없는지_테스트() {
-        assertThat(new Way(Direction.DOWN, 1)
-                .isGo(Square.of(3, 2), Square.of(3, 4))).isFalse();
+        assertThat(new Way(Direction.DOWN, MoveStrategy.BOTH)
+                .canMove(Square.of(3, 2), Square.of(3, 0))).isFalse();
     }
 
     @Test
     void Way를_통해_경로_생성하기_테스트() {
-        assertThat(new Way(Direction.DOWN).generateRoute(Square.of(3, 2), Square.of(3, 8)))
+        assertThat(new Way(Direction.DOWN, MoveStrategy.BOTH).generateRoute(Square.of(3, 2), Square.of(3, 8)))
                 .isEqualTo(new Route(Arrays.asList(
                         Square.of(3, 2),
                         Square.of(3, 3),
@@ -39,6 +40,6 @@ class WayTest {
                         Square.of(3, 6),
                         Square.of(3, 7),
                         Square.of(3, 8)
-                )));
+                ), MoveStrategy.BOTH));
     }
 }
