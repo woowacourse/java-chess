01b518dd935f80a1e8a99e8511de8a80c9f5f7b9
starkim06 <starkim06@naver.com>
2019-06-22 17:24:52 +0900

    refactor: Pawn 움직이는 조건 수정

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -8,21 +8,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Pawn extends Piece {
+    private static final Set<Integer> PAWN_START_COLUMN;
+
+    static {
+        PAWN_START_COLUMN = new HashSet<>();
+        PAWN_START_COLUMN.add(1);
+        PAWN_START_COLUMN.add(6);
+    }
+
     private final Set<MovementUnit> movementUnits;
+    private final Set<MovementUnit> attackUnits;
     private final int movingDirection;
-    private boolean isFirst;
 
     Pawn(Team team) {
         super(team);
         movingDirection = initDirection(team);
-        isFirst = true;
         movementUnits = new HashSet<>();
-        movementUnits.add(MovementUnit.DIAGNOAL);
+        attackUnits = new HashSet<>();
         movementUnits.add(MovementUnit.UP);
+        attackUnits.add(MovementUnit.DIAGNOAL);
     }
 
     private int initDirection(Team team) {
-        if (team == Team.EMPTY) {
+        if (team == Team.BLACK) {
             return 1;
         }
         return -1;
@@ -30,11 +38,25 @@ public class Pawn extends Piece {
 
     @Override
     public boolean isMovable(Spot startSpot, Spot endSpot) {
-        return false;
+        int distanceX = startSpot.getX(endSpot);
+        int distanceY = startSpot.getY(endSpot);
+        boolean isFirst = PAWN_START_COLUMN.stream().anyMatch(y -> startSpot.isSameColumn(y));
+
+        //TODO 리팩토링
+        if (isFirst) {
+            if (movingDirection * distanceY > 0 && distanceY <= 2) {
+                return movementUnits.contains(MovementUnit.direction(distanceX, distanceY));
+            }
+            return false;
+        }
+        return movementUnits.contains(MovementUnit.direction(distanceX, distanceY));
     }
 
     @Override
     public boolean isAttackable(Spot startSpot, Spot endSpot) {
-        return false;
+        int distanceX = startSpot.getX(endSpot);
+        int distanceY = startSpot.getY(endSpot);
+
+        return attackUnits.contains(MovementUnit.direction(distanceX, distanceY));
     }
 }
