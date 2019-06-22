85346d64da13f200c7ef7ad9104587c28a1b8ab4
mrkwon <erased11@gmail.com>
2019-06-22 16:21:37 +0900

    refactor: WebUIChessApplication 리팩토링

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,24 +1,21 @@
 package chess;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.SQLException;
 
 import chess.dao.BoardDAO;
 import chess.dao.ResultCounterDAO;
 import chess.dao.TurnDAO;
 import chess.domain.ChessBoard;
 import chess.domain.Position;
-import chess.domain.ResultCounter;
 import chess.domain.Team;
-import chess.domain.Turn;
 import chess.dto.BoardDTO;
 import chess.dto.ResultCounterDTO;
+import chess.dto.TemplateEngine;
 import chess.dto.TurnDTO;
 import chess.utils.DataParser;
+import chess.utils.Model;
 import chess.utils.QueryParser;
 import chess.view.JsonTransformer;
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import static spark.Spark.exception;
 import static spark.Spark.get;
@@ -26,8 +23,6 @@ import static spark.Spark.post;
 import static spark.Spark.staticFiles;
 
 public class WebUIChessApplication {
-//    private static ChessBoard chessBoard;
-
     private static BoardDTO boardDTO;
     private static TurnDTO turnDTO;
     private static ResultCounterDTO resultCounterDTO;
@@ -35,146 +30,108 @@ public class WebUIChessApplication {
     public static void main(String[] args) {
         staticFiles.location("/assets");
 
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
-        });
+        get("/", (req, res) ->
+                TemplateEngine.render(Model.empty(), "index.html")
+        );
 
         // 게임을 처음 시작할 때 진입점
         get("/game_play", (req, res) -> {
-            BoardDAO.deleteAll();
-            TurnDAO.deleteAll();
-            ResultCounterDAO.deleteAll();
-
-            boardDTO = new BoardDTO();
-            turnDTO = new TurnDTO();
-            resultCounterDTO = new ResultCounterDTO();
+            daoDeleteHandler();
+            dtoInstantiation();
 
             ChessBoard chessBoard = new ChessBoard();
-            Map<String, Object> model = new HashMap<>();
 
-            boardDTO.setBoard(chessBoard.getBoard());
-            turnDTO.setTurn(chessBoard.getTurn());
-            resultCounterDTO.setResultCounter(chessBoard.getResultCounter());
+            dtoChessBoard(chessBoard);
+            daoAddHandler();
 
-            BoardDAO.add(boardDTO);
-            TurnDAO.add(turnDTO);
-            ResultCounterDAO.add(resultCounterDTO);
-
-            model.put("turn", chessBoard.getTurn());
-            return render(model, "game_play.html");
+            return TemplateEngine.render(Model.turn(chessBoard), "game_play.html");
         });
 
         // 계속할 때 진입점
-        get("/game_continue", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("turn", TurnDAO.selectLastTurn().getTurn());
-            return render(model, "game_play.html");
-        });
+        get("/game_continue", (req, res) ->
+                TemplateEngine.render(Model.turn(), "game_play.html")
+        );
 
         post("/game_play", (req, res) -> {
             Position source = QueryParser.position(req.queryParams("source"));
             Position target = QueryParser.position(req.queryParams("target"));
 
-            boardDTO = BoardDAO.selectAll();
-            turnDTO = TurnDAO.selectLastTurn();
-            resultCounterDTO = ResultCounterDAO.selectAll();
-
-            ChessBoard chessBoard = new ChessBoard(boardDTO.getBoard(), turnDTO.getTurn(), resultCounterDTO.getResultCounter());
-
+            ChessBoard chessBoard = dtoChessBoard();
             boolean isKingDead = chessBoard.move(source, target);
 
-            boardDTO.setBoard(chessBoard.getBoard());
-            turnDTO.setTurn(chessBoard.getTurn());
-            resultCounterDTO.setResultCounter(chessBoard.getResultCounter());
-
-            BoardDAO.afterMove(boardDTO);
-            TurnDAO.afterMove(turnDTO);
-            ResultCounterDAO.afterMove(resultCounterDTO);
+            dtoChessBoard(chessBoard);
+            daoMoveHandler(boardDTO, turnDTO, resultCounterDTO);
 
             if (isKingDead) {
-                Map<String, Object> model = new HashMap<>();
-
-                Double whiteScore = chessBoard.totalScore(Team.WHITE);
-                Double blackScore = chessBoard.totalScore(Team.BLACK);
-
-                Turn winner = chessBoard.getWinner();
-
-                model.put("message", "왕이 잡혔습니다.");
-                model.put("winner", winner);
-                model.put("whiteScore", whiteScore);
-                model.put("blackScore", blackScore);
-                return render(model, "result.html");
+                return TemplateEngine.render(Model.result(chessBoard), "result.html");
             }
-            Map<String, Object> model = new HashMap<>();
-            model.put("turn", chessBoard.getTurn());
-            return render(model, "game_play.html");
+            return TemplateEngine.render(Model.turn(chessBoard), "game_play.html");
         });
 
-        post("/status", (req, res) -> {
-            boardDTO = BoardDAO.selectAll();
-            turnDTO = TurnDAO.selectLastTurn();
-            resultCounterDTO = ResultCounterDAO.selectAll();
-
-            ChessBoard chessBoard = new ChessBoard(boardDTO.getBoard(), turnDTO.getTurn(), resultCounterDTO.getResultCounter());
-
-            return DataParser.board(chessBoard.getBoard());
-        }, new JsonTransformer());
+        post("/status", (req, res) ->
+                DataParser.board(dtoChessBoard().getBoard()), new JsonTransformer()
+        );
 
         get("/result", (req, res) -> {
-            boardDTO = BoardDAO.selectAll();
-            turnDTO = TurnDAO.selectLastTurn();
-            resultCounterDTO = ResultCounterDAO.selectAll();
-
-            ChessBoard chessBoard = new ChessBoard(boardDTO.getBoard(), turnDTO.getTurn(), resultCounterDTO.getResultCounter());
+            ChessBoard chessBoard = dtoChessBoard();
 
             Double whiteScore = chessBoard.totalScore(Team.WHITE);
             Double blackScore = chessBoard.totalScore(Team.BLACK);
 
-            String winner = getWinner(whiteScore, blackScore);
-
-            HashMap<String, Object> model = new HashMap<>();
-            model.put("whiteScore", whiteScore);
-            model.put("blackScore", blackScore);
-            model.put("winner", winner);
-            return render(model, "result.html");
+            return TemplateEngine.render(Model.result(whiteScore, blackScore), "result.html");
         });
 
 
         get("/end", (req, res) -> {
-            HashMap<String, Object> model = new HashMap<>();
-            BoardDAO.deleteAll();
-            TurnDAO.deleteAll();
-            ResultCounterDAO.deleteAll();
-            return render(model, "end.html");
+            daoDeleteHandler();
+            return TemplateEngine.render(Model.empty(), "end.html");
         });
 
         exception(RuntimeException.class, (e, req, res) -> {
-
             res.status(404);
             res.body(
-                 "<h1> 에러 발생 </h1>" +
-                 "<div>" + e.getMessage() + "</div>" +
-                 "<button onclick=\"window.history.back()\">되돌아가기</button>"
+                    "<h1> 에러 발생 </h1>" +
+                            "<div>" + e.getMessage() + "</div>" +
+                            "<button onclick=\"window.history.back()\">되돌아가기</button>"
             );
         });
     }
 
-    private static String getWinner(Double whiteScore, Double blackScore) {
-        String winner = "";
-        if (whiteScore > blackScore) {
-            winner = "WHITE";
-        }
-        if (whiteScore > blackScore) {
-            winner = "BLACK";
-        }
-        if (whiteScore == blackScore) {
-            winner = "NONE";
-        }
-        return winner;
+    private static void daoMoveHandler(BoardDTO boardDTO, TurnDTO turnDTO, ResultCounterDTO resultCounterDTO) throws SQLException {
+        BoardDAO.afterMove(boardDTO);
+        TurnDAO.afterMove(turnDTO);
+        ResultCounterDAO.afterMove(resultCounterDTO);
     }
 
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    private static void daoDeleteHandler() throws SQLException {
+        BoardDAO.deleteAll();
+        TurnDAO.deleteAll();
+        ResultCounterDAO.deleteAll();
+    }
+
+    private static ChessBoard dtoChessBoard() throws SQLException {
+        boardDTO = BoardDAO.selectAll();
+        turnDTO = TurnDAO.selectLastTurn();
+        resultCounterDTO = ResultCounterDAO.selectAll();
+
+        return new ChessBoard(boardDTO.getBoard(), turnDTO.getTurn(), resultCounterDTO.getResultCounter());
+    }
+
+    private static void daoAddHandler() throws SQLException {
+        BoardDAO.add(boardDTO);
+        TurnDAO.add(turnDTO);
+        ResultCounterDAO.add(resultCounterDTO);
+    }
+
+    private static void dtoInstantiation() {
+        boardDTO = new BoardDTO();
+        turnDTO = new TurnDTO();
+        resultCounterDTO = new ResultCounterDTO();
+    }
+
+    private static void dtoChessBoard(ChessBoard chessBoard) {
+        boardDTO.setBoard(chessBoard.getBoard());
+        turnDTO.setTurn(chessBoard.getTurn());
+        resultCounterDTO.setResultCounter(chessBoard.getResultCounter());
     }
 }
---
+++ src/main/java/chess/utils/Model.java
@@ -0,0 +1,55 @@
+package chess.utils;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import chess.dao.TurnDAO;
+import chess.domain.ChessBoard;
+import chess.domain.Team;
+
+public class Model {
+    public static Map<String, Object> empty() {
+        return new HashMap<>();
+    }
+
+    public static Map<String, Object> turn(ChessBoard chessBoard) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("turn", chessBoard.getTurn());
+        return model;
+    }
+
+    public static Map<String, Object> turn() throws SQLException {
+        Map<String, Object> model = new HashMap<>();
+        model.put("turn", TurnDAO.selectLastTurn().getTurn());
+        return model;
+    }
+
+    public static Map<String, Object> result(ChessBoard chessBoard) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("message", "왕이 잡혔습니다.");
+        model.put("winner", chessBoard.getWinner());
+        model.put("whiteScore", chessBoard.totalScore(Team.WHITE));
+        model.put("blackScore", chessBoard.totalScore(Team.BLACK));
+        return model;
+    }
+
+    public static HashMap<String, Object> result(Double whiteScore, Double blackScore) {
+        HashMap<String, Object> model = new HashMap<>();
+        model.put("whiteScore", whiteScore);
+        model.put("blackScore", blackScore);
+        model.put("winner", calculateWinner(whiteScore, blackScore));
+        return model;
+    }
+
+    private static String calculateWinner(Double whiteScore, Double blackScore) {
+        String winner = "NONE";
+        if (whiteScore > blackScore) {
+            return "WHITE";
+        }
+        if (whiteScore < blackScore) {
+            return "BLACK";
+        }
+        return winner;
+    }
+}
