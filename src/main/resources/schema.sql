9b730ab356d2e5a55a4f7b42f90dab9112e72f68
DaejunBae <dpudpu11@gmail.com>
2019-06-22 23:50:42 +0900

    docs: scema.sql 샘플 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,3 +22,10 @@ INSERT INTO command(origin, target, round, room_id) VALUES ('g8', 'f6', 4, 1);
 INSERT INTO command(origin, target, round, room_id) VALUES ('a1', 'a7', 5, 1);
 
 INSERT INTO command(origin, target, round, room_id) VALUES ('a2', 'a4', 1, 2);
+INSERT INTO command(origin, target, round, room_id) VALUES ('b7', 'b5', 2, 2);
+INSERT INTO command(origin, target, round, room_id) VALUES ('a1', 'a3', 3, 2);
+INSERT INTO command(origin, target, round, room_id) VALUES ('g8', 'f6', 4, 2);
+INSERT INTO command(origin, target, round, room_id) VALUES ('a3', 'e3', 5, 2);
+INSERT INTO command(origin, target, round, room_id) VALUES ('c7', 'c5', 6, 2);
+INSERT INTO command(origin, target, round, room_id) VALUES ('e3', 'e7', 7, 2);
+INSERT INTO command(origin, target, round, room_id) VALUES ('c5', 'c4', 8, 2);
