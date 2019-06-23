6911ede58385a1f857d5537b6671f161e52796e9
school0bhy <house1217@ajou.ac.kr>
2019-06-23 15:32:53 +0900

    Add : implement Queen moving

---
+++ src/main/java/chess/domain/piece/Queen.java
@@ -0,0 +1,15 @@
+package chess.domain.piece;
+
+import chess.domain.Range;
+import chess.domain.board.Tile;
+
+public class Queen extends Piece{
+    public Queen(PieceColor color) {
+        super(color);
+    }
+
+    @Override
+    protected Range getRange(boolean isTargetEmpty, Tile current) {
+        return Range.QUEEN_RANGE;
+    }
+}
