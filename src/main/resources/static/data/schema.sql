b4028e2e88a1aaf45a151018d2c58fc174a789db
pkch93 <star_final@naver.com>
2019-06-24 17:13:06 +0900

    refactor: schema.sql
    result 테이블 스키마 변경

--- src/main/resources/static/data/schema.sql
+++ src/main/resources/static/data/schema.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS result;
 CREATE TABLE board (
 	position VARCHAR(2) NOT NULL,
 	name VARCHAR(1) NOT NULL,
+    team VARCHAR(5) NOT NULL,
     primary key (position)
 );
 
@@ -15,8 +16,8 @@ CREATE TABLE turn (
 );
 
 CREATE TABLE result (
-	id INTEGER auto_increment NOT NULL,
-	white double NOT NULL,
-    black double NOT NULL,
-    PRIMARY KEY (id)
+	name VARCHAR(1) NOT NULL,
+    team VARCHAR(5) NOT NULL,
+    count INTEGER DEFAULT 0,
+    PRIMARY KEY (name, team)
 );
