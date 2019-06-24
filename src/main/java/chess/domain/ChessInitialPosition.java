e0f0034ebba3b526442c057997ac172891321c85
yejilee <yjll1019@naver.com>
2019-06-24 16:44:09 +0900

    feat: 초기 말들의 위치를 저장하는 ChessInitialPosition 구현

---
+++ src/main/java/chess/domain/ChessInitialPosition.java
@@ -0,0 +1,66 @@
+package chess.domain;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChessInitialPosition {
+	private static Map<ChessBoardGenerator, List<Position>> positions = new HashMap<>();
+
+	static {
+		positions.put(ChessBoardGenerator.BLACK_PAWN, Arrays.asList(
+				Position.getPosition(1, 7),
+				Position.getPosition(2, 7),
+				Position.getPosition(3, 7),
+				Position.getPosition(4, 7),
+				Position.getPosition(5, 7),
+				Position.getPosition(6, 7),
+				Position.getPosition(7, 7),
+				Position.getPosition(8, 7)));
+		positions.put(ChessBoardGenerator.WHITE_PAWN, Arrays.asList(
+				Position.getPosition(1, 2),
+				Position.getPosition(2, 2),
+				Position.getPosition(3, 2),
+				Position.getPosition(4, 2),
+				Position.getPosition(5, 2),
+				Position.getPosition(6, 2),
+				Position.getPosition(7, 2),
+				Position.getPosition(8, 2)));
+
+		positions.put(ChessBoardGenerator.BLACK_ROOK, Arrays.asList(
+				Position.getPosition(1, 8),
+				Position.getPosition(8, 8)));
+		positions.put(ChessBoardGenerator.WHITE_ROOK, Arrays.asList(
+				Position.getPosition(1, 1),
+				Position.getPosition(8, 1)));
+
+		positions.put(ChessBoardGenerator.BLACK_KNIGHT, Arrays.asList(
+				Position.getPosition(2, 8),
+				Position.getPosition(7, 8)));
+		positions.put(ChessBoardGenerator.WHITE_KNIGHT, Arrays.asList(
+				Position.getPosition(2, 1),
+				Position.getPosition(7, 1)));
+
+		positions.put(ChessBoardGenerator.BLACK_BISHOP, Arrays.asList(
+				Position.getPosition(3, 8),
+				Position.getPosition(6, 8)));
+		positions.put(ChessBoardGenerator.WHITE_BISHOP, Arrays.asList(
+				Position.getPosition(3, 1),
+				Position.getPosition(6, 1)));
+
+		positions.put(ChessBoardGenerator.BLACK_QUEEN, Arrays.asList(
+				Position.getPosition(4, 8)));
+		positions.put(ChessBoardGenerator.WHITE_QUEEN, Arrays.asList(
+				Position.getPosition(4, 1)));
+
+		positions.put(ChessBoardGenerator.BLACK_KING, Arrays.asList(
+				Position.getPosition(5, 8)));
+		positions.put(ChessBoardGenerator.WHITE_KING, Arrays.asList(
+				Position.getPosition(5, 1)));
+	}
+
+	public static Map<ChessBoardGenerator, List<Position>> getPositions() {
+		return positions;
+	}
+}
