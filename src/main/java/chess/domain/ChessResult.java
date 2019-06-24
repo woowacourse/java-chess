574222eac43616bb472924663d06c5fadafe7e67
SunHo Van <vsh123@naver.com>
2019-06-24 13:07:59 +0900

    feat : result 상태를 보여주는 status 구현

--- src/main/java/chess/domain/ChessResult.java
+++ src/main/java/chess/domain/ChessResult.java
@@ -1,15 +1,20 @@
 package chess.domain;
 
 import chess.domain.pieces.Piece;
+import chess.domain.pieces.PieceInfo;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ChessResult {
+    private static final double PAWN_BALANCE_POINT = 0.5;
     private final Map<Point, Piece> result;
+    private final ChessTeam team;
 
-    public ChessResult(Map<Point, Piece> result) {
+    ChessResult(Map<Point, Piece> result, ChessTeam team) {
         this.result = result;
+        this.team = team;
     }
 
     public double score() {
@@ -21,11 +26,18 @@ public class ChessResult {
 
     private double balanceInLinePawn(double sum) {
         return sum - result.entrySet().stream()
-                .filter(entry -> entry.getValue().score() == 1)
+                .filter(entry -> entry.getValue().score() == PieceInfo.Pawn.score())
                 .collect(Collectors.groupingBy(entry -> entry.getKey().getX(), Collectors.counting()))
                 .values().stream()
                 .filter(value -> value > 1)
-                .mapToDouble(value -> value * 0.5)
+                .mapToDouble(value -> value * PAWN_BALANCE_POINT)
                 .sum();
+    }
+
+    public Map<String, String> status() {
+        Map<String, String> status = new HashMap<>();
+        status.put("team", team.name());
+        status.put("score", String.valueOf(score()));
+        return status;
     }
 }
