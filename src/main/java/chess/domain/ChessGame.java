a8329a2fa21a62c8ada1f02c4602ad8075bd9c13
conatuseus <conatuseus@gmail.com>
2019-06-24 18:08:00 +0900

    feat: 게임 종료 조건 추가
    Squashed commit of the following:
    
    commit bcb6771aa326e834b8e38ac545a91df111f96299
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Mon Jun 24 18:06:27 2019 +0900
    
        게임 종료 조건 추가

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -1,16 +1,19 @@
 package chess.domain;
 
 import chess.domain.pieces.Color;
+import chess.domain.pieces.King;
 import chess.domain.pieces.Piece;
 
 import java.util.List;
 
 public class ChessGame {
 
+    private boolean isEnd;
     private Color colorOfTurn;
     private ChessBoard chessBoard;
 
     public ChessGame() {
+        this.isEnd = false;
         this.colorOfTurn = Color.WHITE;
         chessBoard = new ChessBoard();
     }
@@ -19,12 +22,21 @@ public class ChessGame {
         chessBoard.checkSourceAndTarget(source, target, colorOfTurn);
         List<Point> path = chessBoard.makePath(source, target);
         chessBoard.checkPath(path);
+
+        if (chessBoard.hasPiece(target, new King(changeColorOfTurn()))) {
+            isEnd = true;
+        }
+
         chessBoard.updatePieceLocation(source, target);
-        changeColorOfTurn();
+        this.colorOfTurn = changeColorOfTurn();
     }
 
-    private void changeColorOfTurn() {
-        this.colorOfTurn = (this.colorOfTurn == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    private Color changeColorOfTurn() {
+        return (this.colorOfTurn == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }
+
+    public boolean isEnd() {
+        return this.isEnd;
     }
 
     public boolean hasPiece(Point point, Piece piece) {
