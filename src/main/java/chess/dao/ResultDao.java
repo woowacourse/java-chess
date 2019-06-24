408b930945730343768781561de7da7a9d9c83bd
pkch93 <star_final@naver.com>
2019-06-24 09:35:23 +0900

    test: ResultDao class
    ResultDao 테스트 추가

--- src/main/java/chess/dao/ResultDao.java
+++ src/main/java/chess/dao/ResultDao.java
@@ -1,7 +1,6 @@
 package chess.dao;
 
 import chess.dto.ResultDto;
-import chess.dto.TurnDto;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,13 +8,17 @@ import java.sql.SQLException;
 
 public class ResultDao {
     public void add(ResultDto resultDto) throws SQLException {
-        String query = "INSERT INTO result (white, black) VALUES (?, ?)";
+        String query = "INSERT INTO result VALUES (1, ?, ?)";
         PreparedStatement pstmt = JDBCConnection.start().prepareStatement(query);
 
         pstmt.setDouble(1, resultDto.getWhiteScore());
         pstmt.setDouble(2, resultDto.getBlackScore());
 
-        pstmt.executeUpdate();
+        try {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            update(resultDto);
+        }
     }
 
     public ResultDto find() throws SQLException {
---
+++ src/test/java/chess/dao/ResultDaoTest.java
@@ -0,0 +1,32 @@
+package chess.dao;
+
+import chess.dto.ResultDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResultDaoTest {
+
+    ResultDao resultDao;
+
+    @BeforeEach
+    void setUp() {
+        resultDao = new ResultDao();
+    }
+
+    @Test
+    void result_데이터_추가_조회_테스트() throws SQLException {
+        ResultDto resultDto = new ResultDto();
+        resultDto.setBlackScore(10);
+        resultDto.setWhiteScore(20.5);
+
+        resultDao.add(resultDto);
+        ResultDto findResult = resultDao.find();
+
+        assertThat(findResult.getBlackScore()).isEqualTo(10);
+        assertThat(findResult.getWhiteScore()).isEqualTo(20.5);
+    }
+}
