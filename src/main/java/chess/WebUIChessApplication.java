3e5427e276a9d36896e268bb9a9ec9620557a7ea
Seongmo Jeon <oeeen3@gmail.com>
2019-06-24 16:28:56 +0900

    feat: gson 추가, 보드 초기화

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,5 +1,11 @@
 package chess;
 
+import chess.dao.GameDao;
+import chess.dao.PieceDao;
+import chess.domain.BoardFactory;
+import chess.domain.Game;
+import chess.domain.Point;
+import chess.domain.pieces.Piece;
 import chess.utils.DBUtil;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -8,14 +14,38 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
+        externalStaticFileLocation("src/main/resources/templates");
+        port(8080);
+
         DataSource dataSource = DBUtil.getDataSource();
+
+        PieceDao pieceDao = new PieceDao(dataSource);
+        GameDao gameDao = new GameDao(dataSource);
 
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
+            Game game = new Game(BoardFactory.init());
+            req.session().attribute("game", game);
+            return render(model, "index.html");
+        });
+
+        get("/move", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String source = req.queryParams("source");
+            String target = req.queryParams("target");
+            Point start = new Point(source);
+            Point end = new Point(target);
+
+            Game game = req.session().attribute("game");
+            game.move(start, end);
+            game.isKingAlive();
+            game.changeTurn();
+            Map<Point, Piece> board = game.getBoard();
+            model.put("board", board);
             return render(model, "index.html");
         });
     }
---
+++ src/main/resources/templates/js/chess.js
@@ -0,0 +1,20 @@
+$(function(){
+	var index = 0;
+	var source;
+
+    $('#boards tr td').click(function() {
+      	var position = $(this).attr('id');
+      	var value = index % 2;
+      	
+      	if ( value == 0 ) {
+      		source = position;
+      	} else {
+	    	var moveUrl = "/move?source=" + source + "&target=" + position;
+	    	console.log(moveUrl);
+	    	$(location).attr('href',moveUrl);
+      	}
+      	
+      	index++;
+      	return false;
+    });
+});
