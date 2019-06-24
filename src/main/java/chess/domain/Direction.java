4ad1c491829eb0d34f2394769c90f139a64bc51b
yejilee <yjll1019@naver.com>
2019-06-24 16:53:36 +0900

    feat: 말의 이동 방향 추가

--- src/main/java/chess/domain/Direction.java
+++ src/main/java/chess/domain/Direction.java
@@ -1,6 +1,6 @@
-package chess;
+package chess.domain;
 
-public enum  Direction {
+public enum Direction {
 	LEFT_TOP(-1, 1),
 	TOP(0, 1),
 	RIGHT_TOP(1, 1),
@@ -8,7 +8,16 @@ public enum  Direction {
 	RIGHT(1, 0),
 	LEFT_BOTTOM(-1, -1),
 	BOTTOM(0, -1),
-	RIGHT_BOTTOM(1, -1);
+	RIGHT_BOTTOM(1, -1),
+
+	LEFT_LEFT_TOP(-2, 1),
+	LEFT_LEFT_BOTTOM(-2, -1),
+	TOP_TOP_LEFT(-1, 2),
+	TOP_TOP_RIGHT(1, 2),
+	RIGHT_RIGHT_TOP(2, 1),
+	RIGHT_RIGHT_BOTTOM(2, -1),
+	BOTTOM_BOTTOM_RIGHT(1, -2),
+	BOTTOM_BOTTOM_LEFT(-1, -2);
 
 	private int directionX;
 	private int directionY;
