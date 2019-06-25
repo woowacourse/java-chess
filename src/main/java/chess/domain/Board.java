99190b553fce4ee7171ea6f6752f341aacc4a382
choihz <choixhz@gmail.com>
2019-06-25 17:30:37 +0900

    feat : Piece - create Blank

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -4,17 +4,16 @@ import chess.domain.pieces.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class Board {
-    private Map<Position, Optional<Piece>> boardState;
+    private Map<Position, Piece> boardState;
 
     public Board() {
         boardState = new HashMap<>();
         for (int i = 1; i <= 8; i++) {
             for (int j = 1; j <= 8; j++) {
                 Position position = new Position(i, j);
-                boardState.put(position, Optional.empty());
+                boardState.put(position, new Blank(position, Team.BLANK));
             }
         }
     }
@@ -31,62 +30,62 @@ public class Board {
     private void initializeRook() {
         Position position;
         position = new Position(1, 1);
-        boardState.put(position, Optional.of(new Rook(position, Team.WHITE)));
+        boardState.put(position, new Rook(position, Team.WHITE));
         position = new Position(8, 1);
-        boardState.put(position, Optional.of(new Rook(position, Team.WHITE)));
+        boardState.put(position, new Rook(position, Team.WHITE));
         position = new Position(1, 8);
-        boardState.put(position, Optional.of(new Rook(position, Team.BLACK)));
+        boardState.put(position, new Rook(position, Team.BLACK));
         position = new Position(8, 8);
-        boardState.put(position, Optional.of(new Rook(position, Team.BLACK)));
+        boardState.put(position, new Rook(position, Team.BLACK));
     }
 
     private void initializeKnight() {
         Position position;
         position = new Position(2, 1);
-        boardState.put(position, Optional.of(new Knight(position, Team.WHITE)));
+        boardState.put(position, new Knight(position, Team.WHITE));
         position = new Position(7, 1);
-        boardState.put(position, Optional.of(new Knight(position, Team.WHITE)));
+        boardState.put(position, new Knight(position, Team.WHITE));
         position = new Position(2, 8);
-        boardState.put(position, Optional.of(new Knight(position, Team.BLACK)));
+        boardState.put(position, new Knight(position, Team.BLACK));
         position = new Position(7, 8);
-        boardState.put(position, Optional.of(new Knight(position, Team.BLACK)));
+        boardState.put(position, new Knight(position, Team.BLACK));
     }
 
     private void initializeBishop() {
         Position position;
         position = new Position(3, 1);
-        boardState.put(position, Optional.of(new Bishop(position, Team.WHITE)));
+        boardState.put(position, new Bishop(position, Team.WHITE));
         position = new Position(6, 1);
-        boardState.put(position, Optional.of(new Bishop(position, Team.WHITE)));
+        boardState.put(position, new Bishop(position, Team.WHITE));
         position = new Position(3, 8);
-        boardState.put(position, Optional.of(new Bishop(position, Team.BLACK)));
+        boardState.put(position, new Bishop(position, Team.BLACK));
         position = new Position(6, 8);
-        boardState.put(position, Optional.of(new Bishop(position, Team.BLACK)));
+        boardState.put(position, new Bishop(position, Team.BLACK));
     }
 
     private void initializeQueen() {
         Position position;
         position = new Position(4, 1);
-        boardState.put(position, Optional.of(new Queen(position, Team.WHITE)));
+        boardState.put(position, new Queen(position, Team.WHITE));
         position = new Position(4, 8);
-        boardState.put(position, Optional.of(new Queen(position, Team.BLACK)));
+        boardState.put(position, new Queen(position, Team.BLACK));
     }
 
     private void initializeKing() {
         Position position;
         position = new Position(5, 1);
-        boardState.put(position, Optional.of(new King(position, Team.WHITE)));
+        boardState.put(position, new King(position, Team.WHITE));
         position = new Position(5, 8);
-        boardState.put(position, Optional.of(new King(position, Team.BLACK)));
+        boardState.put(position, new King(position, Team.BLACK));
     }
 
     private void initializePawn() {
         Position position;
         for (int i = 1; i <= 8; i++) {
             position = new Position(i, 2);
-            boardState.put(position, Optional.of(new Pawn(position, Team.WHITE)));
+            boardState.put(position, new Pawn(position, Team.WHITE));
             position = new Position(i, 7);
-            boardState.put(position, Optional.of(new Pawn(position, Team.BLACK)));
+            boardState.put(position, new Pawn(position, Team.BLACK));
         }
     }
 }
---
+++ src/main/java/chess/domain/pieces/Blank.java
@@ -0,0 +1,17 @@
+package chess.domain.pieces;
+
+import chess.domain.Position;
+import chess.domain.Team;
+
+public class Blank extends Piece {
+    private static final int SCORE = 0;
+
+    public Blank(Position position, Team team) {
+        super(position, team);
+    }
+
+    @Override
+    boolean canMove(Position position) {
+        return false;
+    }
+}
