312ac9191f9362b96c648156e0054ecdcf87e2bd
Seongmo Jeon <oeeen3@gmail.com>
2019-06-25 21:37:50 +0900

    refactor: Piece Factory refactoring

--- src/main/java/chess/domain/PieceFactory.java
+++ src/main/java/chess/domain/PieceFactory.java
@@ -18,12 +18,6 @@ public class PieceFactory {
     }
 
     public static Piece of(String name, Team team) {
-        if (name.equals("ROOK")) return new Rook(team);
-        if (name.equals("KNIGHT")) return new Knight(team);
-        if (name.equals("BISHOP")) return new Bishop(team);
-        if (name.equals("QUEEN")) return new Queen(team);
-        if (name.equals("KING")) return new King(team);
-        if (name.equals("PAWN")) return new Pawn(team);
-        return new Blank();
+        return Type.valueOf(name).create(team);
     }
 }
