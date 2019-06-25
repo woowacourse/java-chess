937becaa7bc53ac1719324be5d7491cd23633200
DaejunBae <dpudpu11@gmail.com>
2019-06-25 14:49:18 +0900

    refactor: BoardGenerator 폰 생성 분리

--- src/main/java/chess/domain/BoardGenerator.java
+++ src/main/java/chess/domain/BoardGenerator.java
@@ -14,15 +14,17 @@ public class BoardGenerator {
     public static Map<Position, Piece> generate() {
         Map<Position, Piece> map = new TreeMap<>();
 
-        initBlackPieces(map);
-        initEmptyPieces(map);
-        initWhitePieces(map);
+        initBlackPieces(map, 8);
+        initBlackPawns(map, 7);
+        initEmptyPieces(map, 6, 3);
+        initWhitePawns(map, 2);
+        initWhitePieces(map, 1);
 
         return map;
     }
 
-    private static void initBlackPieces(final Map<Position, Piece> map) {
-        List<Position> positions = getColumns(8);
+    private static void initBlackPieces(final Map<Position, Piece> map, final int rowIndex) {
+        List<Position> positions = getColumns(rowIndex);
         map.put(positions.get(0), Piece.of(positions.get(0), Piece.Color.BLACK, Rook.getInstance()));
         map.put(positions.get(1), Piece.of(positions.get(1), Piece.Color.BLACK, Knight.getInstance()));
         map.put(positions.get(2), Piece.of(positions.get(2), Piece.Color.BLACK, Bishop.getInstance()));
@@ -32,14 +34,17 @@ public class BoardGenerator {
         map.put(positions.get(6), Piece.of(positions.get(6), Piece.Color.BLACK, Knight.getInstance()));
         map.put(positions.get(7), Piece.of(positions.get(7), Piece.Color.BLACK, Rook.getInstance()));
 
-        positions = getColumns(7);
+    }
+
+    private static void initBlackPawns(final Map<Position, Piece> map, final int rowIndex) {
+        List<Position> positions = getColumns(rowIndex);
         for (Position position : positions) {
             map.put(position, Piece.of(position, Piece.Color.BLACK, Pawn.FIRST_TOP));
         }
     }
 
-    private static void initEmptyPieces(final Map<Position, Piece> map) {
-        for (int i = 6; i >= 3; i--) {
+    private static void initEmptyPieces(final Map<Position, Piece> map, final int starIndex, final int endIndex) {
+        for (int i = starIndex; i >= endIndex; i--) {
             final List<Position> positions = getColumns(i);
             for (Position position : positions) {
                 map.put(position, Piece.of(position, Piece.Color.EMPTY, Empty.getInstance()));
@@ -47,13 +52,15 @@ public class BoardGenerator {
         }
     }
 
-    private static void initWhitePieces(final Map<Position, Piece> map) {
-        List<Position> positions = getColumns(2);
+    private static void initWhitePawns(final Map<Position, Piece> map, final int rowIndex) {
+        List<Position> positions = getColumns(rowIndex);
         for (Position position : positions) {
             map.put(position, Piece.of(position, Piece.Color.WHITE, Pawn.FIRST_BOTTOM));
         }
+    }
 
-        positions = getColumns(1);
+    private static void initWhitePieces(final Map<Position, Piece> map, final int rowIndex) {
+        List<Position> positions = getColumns(rowIndex);
         map.put(positions.get(0), Piece.of(positions.get(0), Piece.Color.WHITE, Rook.getInstance()));
         map.put(positions.get(1), Piece.of(positions.get(1), Piece.Color.WHITE, Knight.getInstance()));
         map.put(positions.get(2), Piece.of(positions.get(2), Piece.Color.WHITE, Bishop.getInstance()));
