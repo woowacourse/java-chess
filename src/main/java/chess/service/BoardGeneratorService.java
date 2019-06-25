6c0121e539ab5a3481fe6cca65a2d82b61002308
hyperpace <cnk9109@gmail.com>
2019-06-26 03:34:33 +0900

    feat: create BoardGeneratorService

---
+++ src/main/java/chess/service/BoardGeneratorService.java
@@ -0,0 +1,30 @@
+package chess.service;
+
+import chess.domain.board.Board;
+import chess.persistence.dao.ChessBoardDAO;
+import chess.persistence.dto.ChessBoardDTO;
+import chess.persistence.dto.ChessGameDTO;
+
+public class BoardGeneratorService {
+    public static BoardGeneratorService getInstance() {
+        return BoardGeneratorHolder.INSTANCE;
+    }
+
+    public ChessBoardDTO request(ChessGameDTO chessGameDTO) {
+        ChessBoardDTO chessBoardDTO = new ChessBoardDTO();
+        try {
+            chessBoardDTO.setGameId(chessGameDTO.getGameId());
+            chessBoardDTO.setRoundNo(ChessBoardDAO.getInstance().findMaxRoundByGameId(chessGameDTO.getGameId()));
+            chessBoardDTO = ChessBoardDAO.getInstance().findByBoardStatus(chessBoardDTO);
+        } catch (IllegalArgumentException e) {
+            chessBoardDTO.setBoard(Board.drawBoard().getBoard());
+            ChessBoardDAO.getInstance().addBoardStatus(chessBoardDTO);
+        }
+
+        return chessBoardDTO;
+    }
+
+    private static class BoardGeneratorHolder {
+        static final BoardGeneratorService INSTANCE = new BoardGeneratorService();
+    }
+}
