c6e2d2526c3d5310add05eef480d6fa30e26f85c
backlo <hjun4512@naver.com>
2019-06-25 20:10:38 +0900

    feat: Update MoveService Class and create JsonTransformer

--- src/main/java/chess/service/MoveService.java
+++ src/main/java/chess/service/MoveService.java
@@ -4,18 +4,20 @@ import chess.domain.board.Board;
 import chess.domain.direction.Route;
 import chess.domain.direction.core.Square;
 import chess.domain.piece.core.Piece;
+import chess.domain.piece.core.Team;
+import chess.dto.ChessMoveDTO;
 
 public class MoveService {
     public static MoveService getInstance() {
         return MovieServiceHolder.INSTANCE;
     }
 
-    public Board request(Square source, Square target) {
-        Board board = Board.drawBoard();
-        Piece piece = board.getPiece(source);
-        Route route = piece.getRoute(source, target);
-        return board.changeBoard(route);
-    }
+//    public Board request(ChessMoveDTO moveDTO) {
+//        Board board = Board.drawBoard(moveDTO.getBoard(), Team.valueOf(moveDTO.getTeam()));
+//        Piece piece = board.getPiece();
+//        Route route = piece.getRoute(source, target);
+//        return board.changeBoard(route);
+//    }
 
     private static class MovieServiceHolder {
         static final MoveService INSTANCE = new MoveService();
