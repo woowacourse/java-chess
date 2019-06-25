7fff3f2e4c1d2bb698dc73044d996dfe9d1b69ab
hyperpace <cnk9109@gmail.com>
2019-06-26 03:34:06 +0900

    feat: create GameGeneratorService

---
+++ src/main/java/chess/service/GameGeneratorService.java
@@ -0,0 +1,26 @@
+package chess.service;
+
+import chess.domain.piece.core.Team;
+import chess.persistence.dao.ChessGameDAO;
+import chess.persistence.dto.ChessGameDTO;
+
+public class GameGeneratorService {
+    public static GameGeneratorService getInstance() {
+        return GameGeneratorHolder.INSTANCE;
+    }
+
+    public ChessGameDTO request() {
+        ChessGameDTO chessGameDTO = ChessGameDAO.getInstance().findByMaxGameId();
+        if (chessGameDTO.getGameId() == 0 || chessGameDTO.getGameStatus()) {
+            chessGameDTO.setGameStatus(false);
+            chessGameDTO.setLastUser(Team.WHITE.getTeam());
+            ChessGameDAO.getInstance().addGameStatus(chessGameDTO);
+            return ChessGameDAO.getInstance().findByMaxGameId();
+        }
+        return chessGameDTO;
+    }
+
+    private static class GameGeneratorHolder {
+        static final GameGeneratorService INSTANCE = new GameGeneratorService();
+    }
+}
