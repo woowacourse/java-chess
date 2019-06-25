2ab80174f75e41d2338e655329974fbc51bcb539
backlo <hjun4512@naver.com>
2019-06-25 19:34:00 +0900

    feat(DBConnector): Create DataSourceFactory Class with connect to JDBCTemplate

--- src/main/java/chess/persistence/dao/connector/AbstractDataSourceFactory.java
+++ src/main/java/chess/persistence/dao/connector/AbstractDataSourceFactory.java
@@ -1,6 +1,7 @@
-package chess.domain.dao;
+package chess.persistence.dao.connector;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 
 public interface AbstractDataSourceFactory {
     DataSource create();
--- src/main/java/chess/persistence/dao/connector/DataSourceFactory.java
+++ src/main/java/chess/persistence/dao/connector/DataSourceFactory.java
@@ -1,32 +1,52 @@
-package chess.domain.dao;
+package chess.persistence.dao.connector;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DataSourceFactory implements AbstractDataSourceFactory {
     private static final String URL_FORMAT = "jdbc:mysql://%s:%s/%s?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
 
+    private static DataSourceFactory instance;
+    private static MysqlDataSource ds;
+
+    public static DataSourceFactory getInstance(){
+        return instance == null ? instance = new DataSourceFactory() : instance;
+    }
+
     @Override
     public DataSource create() {
         try {
             Properties properties = loadProperty();
-            MysqlDataSource ds = new MysqlDataSource();
+            if(ds == null) {
+                ds = new MysqlDataSource();
+            }
             ds.setUser(properties.getProperty("user"));
             ds.setPassword(properties.getProperty("password"));
-            ds.setURL(properties.getProperty(
+            ds.setURL(
                     String.format(URL_FORMAT,
                             properties.getProperty("host"),
                             properties.getProperty("port"),
                             properties.getProperty("database"))
-            ));
+            );
             return ds;
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
+        }
+    }
+
+    public Connection getConnection(){
+        try {
+            return create().getConnection();
+        } catch (SQLException e) {
+            System.out.println("여기오륲");
+            throw new IllegalArgumentException(e.getMessage());
         }
     }
 
