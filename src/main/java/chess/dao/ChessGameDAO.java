c2c914c6130fd3ee2013efe18db78bfe3698bb53
andole87 <andole87@gmail.com>
2019-06-26 15:28:17 +0900

    refactor: Dao의 지역변수 변경
    1. 필요할 때 선언과 동시 할당

--- src/main/java/chess/dao/ChessGameDAO.java
+++ src/main/java/chess/dao/ChessGameDAO.java
@@ -9,10 +9,8 @@ import java.util.List;
 
 public class ChessGameDAO {
     private static ChessGameDAO chessGameDAO;
-    private Connection con;
 
     private ChessGameDAO() {
-        con = DataBaseConnector.getConnection();
     }
 
     public static ChessGameDAO getInstance() {
@@ -23,15 +21,14 @@ public class ChessGameDAO {
     }
 
     public List<Integer> findPreviousGamesById(String name) {
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        Connection con = DataBaseConnector.getConnection();
         List<Integer> previousGameId = new ArrayList<>();
         try {
             String query = "select game_id from chess_game where white_name = ? or black_name = ?";
-            pstmt = con.prepareStatement(query);
+            PreparedStatement pstmt = con.prepareStatement(query);
             pstmt.setString(1, name);
             pstmt.setString(2, name);
-            rs = pstmt.executeQuery();
+            ResultSet rs = pstmt.executeQuery();
 
             while (rs.next()) {
                 previousGameId.add(rs.getInt(1));
--- src/main/java/chess/dao/ChessLogDAO.java
+++ src/main/java/chess/dao/ChessLogDAO.java
@@ -10,10 +10,8 @@ import java.util.List;
 
 public class ChessLogDAO {
     private static ChessLogDAO chessLogDAO;
-    private Connection con;
 
     private ChessLogDAO() {
-        con = DataBaseConnector.getConnection();
     }
 
     public static ChessLogDAO getInstance() {
@@ -24,14 +22,13 @@ public class ChessLogDAO {
     }
 
     public List<List<String>> findGameLogById(String gameId) {
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        Connection con = DataBaseConnector.getConnection();
         List<List<String>> gameLog = new ArrayList<>(new ArrayList<>());
         try {
             String query = "select source,destination from game_log where game_id = ?";
-            pstmt = con.prepareStatement(query);
+            PreparedStatement pstmt = con.prepareStatement(query);
             pstmt.setString(1, gameId);
-            rs = pstmt.executeQuery();
+            ResultSet rs = pstmt.executeQuery();
 
             while (rs.next()) {
                 gameLog.add(Arrays.asList(rs.getString(1), rs.getString(2)));
@@ -43,10 +40,10 @@ public class ChessLogDAO {
     }
 
     public void insertLog(String from, String to, String gameId) {
-        PreparedStatement pstmt = null;
+        Connection con = DataBaseConnector.getConnection();
         try {
             String query = "insert into game_log(source, destination,game_id) values(?,?,?);";
-            pstmt = con.prepareStatement(query);
+            PreparedStatement pstmt = con.prepareStatement(query);
             pstmt.setString(1, from);
             pstmt.setString(2, to);
             pstmt.setString(3, gameId);
