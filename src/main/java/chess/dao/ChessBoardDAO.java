34023100210dcd1963da142e956c19e8b2265b1f
school0bhy <house1217@ajou.ac.kr>
2019-06-26 18:20:22 +0900

    Add : implement ChessBoardDAO

---
+++ src/main/java/chess/dao/ChessBoardDAO.java
@@ -0,0 +1,74 @@
+package chess.dao;
+
+import chess.domain.board.Tile;
+import chess.domain.piece.Piece;
+import chess.domain.piece.PieceColor;
+import chess.domain.piece.PieceType;
+import chess.dto.ChessBoardDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChessBoardDAO {
+    private static final String insertQuery = "INSERT INTO chess_board (game_id, tile, piece_type, piece_color) VALUES (?, ?, ?, ?)";
+    private static final String selectQuery = "SELECT tile, piece_type, piece_color FROM chess_board WHERE game_id=?";
+    private static final String deleteQuery = "DELETE FROM chess_board WHERE game_id=?";
+    private static ChessBoardDAO chessBoardDAO;
+
+    private ChessBoardDAO() {
+    }
+
+    public static ChessBoardDAO getInstance() {
+        if (chessBoardDAO == null) {
+            chessBoardDAO = new ChessBoardDAO();
+        }
+        return chessBoardDAO;
+    }
+
+    public ChessBoardDTO selectChessBoard(int id) throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+            PreparedStatement pstmt = connection.prepareStatement(selectQuery);
+            pstmt.setInt(1, id);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            Map<Tile, Piece> boardState = new HashMap<>();
+            while (rs.next()) {
+                Tile tile = Tile.of(rs.getString("tile"));
+                PieceType type = PieceType.valueOf(rs.getString("piece_type"));
+                PieceColor color = PieceColor.valueOf(rs.getString("piece_color"));
+                boardState.put(tile, type.generate(color));
+            }
+
+            return new ChessBoardDTO(boardState);
+        }
+    }
+
+    public void insertChessBoard(int id, ChessBoardDTO chessBoardDTO) throws SQLException {
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(insertQuery)) {
+
+            pstmt.setInt(1, id);
+
+            Map<Tile, Piece> board = chessBoardDTO.getBoard();
+            for (Tile tile : board.keySet()) {
+                pstmt.setString(2, tile.toString());
+                pstmt.setString(3, String.valueOf(board.get(tile).getType()));
+                pstmt.setString(4, String.valueOf(board.get(tile).getColor()));
+                pstmt.executeUpdate();
+            }
+        }
+    }
+
+    public void deleteChessBoard(int id) throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+            PreparedStatement pstmt = connection.prepareStatement(deleteQuery);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        }
+    }
+}
---
+++ src/test/java/chess/dao/ChessBoardDAOTest.java
@@ -0,0 +1,113 @@
+package chess.dao;
+
+import chess.domain.board.Tile;
+import chess.domain.piece.Piece;
+import chess.domain.piece.PieceColor;
+import chess.domain.piece.PieceType;
+import chess.dto.ChessBoardDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ChessBoardDAOTest {
+    ChessBoardDAO chessBoardDAO;
+    Connection connection;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        connection = DBUtil.getConnection();
+        connection.setAutoCommit(false);
+        chessBoardDAO = ChessBoardDAO.getInstance();
+    }
+
+    //에러 발생 (해당 게임ID 값이 chess_turn 테이블에 존재하지 않아 외래키가 없음)
+    @Test
+    void insertTest1() throws Exception {
+        ChessBoardDTO chessBoardDTO = new ChessBoardDTO(
+                new HashMap<Tile, Piece>() {{
+                    put(Tile.of("a1"), PieceType.KING.generate(PieceColor.WHITE));
+                }}
+        );
+        assertThrows(SQLException.class, () -> {
+            chessBoardDAO.insertChessBoard(1, chessBoardDTO);
+        });
+    }
+
+    //정상 실행 (외래키 존재)
+    @Test
+    void insertTest2() throws Exception {
+        ChessTurnDAO chessTurnDAO = ChessTurnDAO.getInstance();
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        int id = chessTurnDAO.selectMaxGameId();
+
+        ChessBoardDTO chessBoardDTO = new ChessBoardDTO(
+                new HashMap<Tile, Piece>() {{
+                    put(Tile.of("a1"), PieceType.KING.generate(PieceColor.WHITE));
+                }}
+        );
+
+        assertDoesNotThrow(() -> {
+            chessBoardDAO.insertChessBoard(id, chessBoardDTO);
+        });
+    }
+
+    //해당 체스보드 정보 존재하는 경우
+    @Test
+    public void selectTest1() throws Exception {
+        ChessTurnDAO chessTurnDAO = ChessTurnDAO.getInstance();
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        int id = chessTurnDAO.selectMaxGameId();
+
+        ChessBoardDTO chessBoardDTO = new ChessBoardDTO(
+                new HashMap<Tile, Piece>() {{
+                    put(Tile.of("a1"), PieceType.KING.generate(PieceColor.WHITE));
+                }}
+        );
+        chessBoardDAO.insertChessBoard(id, chessBoardDTO);
+
+        assertThat(chessBoardDAO.selectChessBoard(id).getBoard())
+                .isEqualTo(chessBoardDTO.getBoard());
+    }
+
+    //존재하지 않는 경우
+    @Test
+    public void selectTest2() throws SQLException {
+        ChessTurnDAO chessTurnDAO = ChessTurnDAO.getInstance();
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        int id = chessTurnDAO.selectMaxGameId();
+
+        assertThat(chessBoardDAO.selectChessBoard(id).getBoard()).isEmpty();
+    }
+
+    @Test
+    public void deleteTest() throws Exception {
+        ChessTurnDAO chessTurnDAO = ChessTurnDAO.getInstance();
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        int id = chessTurnDAO.selectMaxGameId();
+
+        ChessBoardDTO chessBoardDTO = new ChessBoardDTO(
+                new HashMap<Tile, Piece>() {{
+                    put(Tile.of("a1"), PieceType.KING.generate(PieceColor.WHITE));
+                }}
+        );
+        chessBoardDAO.insertChessBoard(id, chessBoardDTO);
+        assertThat(chessBoardDAO.selectChessBoard(id).getBoard()).isEqualTo(chessBoardDTO.getBoard());
+
+        chessBoardDAO.deleteChessBoard(id);
+        assertThat(chessBoardDAO.selectChessBoard(id).getBoard()).isEmpty();
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        connection.rollback();
+        connection.close();
+    }
+}
