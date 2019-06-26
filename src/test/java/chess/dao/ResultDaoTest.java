2cc80e8cc6c21a6e803033b2cffc3a5d942cfded
pkch93 <star_final@naver.com>
2019-06-26 16:43:18 +0900

    test: ResultDaoTest class
    resultDao 테스트 오류 수정

--- src/test/java/chess/dao/ResultDaoTest.java
+++ src/test/java/chess/dao/ResultDaoTest.java
@@ -1,6 +1,14 @@
 package chess.dao;
 
+import chess.dto.ResultDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResultDaoTest {
 
@@ -9,5 +17,22 @@ public class ResultDaoTest {
     @BeforeEach
     void setUp() {
         resultDao = ResultDao.getInstance();
+    }
+
+    @Test
+    void result_데이터_추가_조회_테스트() throws SQLException {
+        String name = "p";
+        String team = "BLACK";
+        int count = 1;
+        ResultDto resultDto = new ResultDto(name, team, count);
+
+        resultDao.add(resultDto);
+        ResultDto findResult = resultDao.find().get(0);
+
+        assertThat(findResult.getName()).isEqualTo(name);
+        assertThat(findResult.getTeam()).isEqualTo(team);
+        assertThat(findResult.getCount()).isEqualTo(count);
+
+        resultDao.delete();
     }
 }
