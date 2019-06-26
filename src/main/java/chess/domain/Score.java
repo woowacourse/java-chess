d762d367c4345125696c743fa030bf50c599f426
yk1028 <wongeunsong94@gmail.com>
2019-06-27 01:23:34 +0900

    refactor: Score class 정리

--- src/main/java/chess/domain/Score.java
+++ src/main/java/chess/domain/Score.java
@@ -3,55 +3,47 @@ package chess.domain;
 import java.util.Objects;
 
 public class Score {
-	private final int ROUNDING_CRITERION = 10;
-	private final double score;
+    private static final int ROUNDING_CRITERION = 10;
 
-	public Score(final double score) {
-		this.score = round(score);
-	}
+    private final double score;
 
-	private double round(double score) {
-		return Math.round(score * ROUNDING_CRITERION) / (double) ROUNDING_CRITERION;
-	}
+    public Score(final double score) {
+        this.score = round(score);
+    }
 
-	public double getScore() {
-		return score;
-	}
+    private double round(double score) {
+        return Math.round(score * ROUNDING_CRITERION) / (double) ROUNDING_CRITERION;
+    }
 
-	public Boolean isHigher(final Score score) {
-		if (this.score > score.score) {
-			return true;
-		}
-		return false;
-	}
+    public double getScore() {
+        return score;
+    }
 
-	public Score add(Score score) {
-		return new Score(this.score + score.score);
-	}
+    public Score add(Score score) {
+        return new Score(this.score + score.score);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof Score)) {
-			return false;
-		}
-		final Score score1 = (Score) o;
-		return ROUNDING_CRITERION == score1.ROUNDING_CRITERION &&
-				Double.compare(score1.score, score) == 0;
-	}
+    public Boolean isHigher(final Score score) {
+        return this.score > score.score;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(ROUNDING_CRITERION, score);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score1 = (Score) o;
+        return Double.compare(score1.score, score) == 0;
+    }
 
-	@Override
-	public String toString() {
-		return "Score{" +
-				"ROUNDING_CRITERION=" + ROUNDING_CRITERION +
-				", score=" + score +
-				'}';
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "score=" + score +
+                '}';
+    }
 }
