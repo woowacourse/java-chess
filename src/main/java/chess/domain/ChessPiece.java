25e760a6fd066657ed666c0925e35d6813c793c4
yejilee <yjll1019@naver.com>
2019-06-26 19:56:40 +0900

    feat: 체스 말의 종류에 따른 정보를 저장하는 ChessPiece 구현

---
+++ src/main/java/chess/domain/ChessPiece.java
@@ -0,0 +1,84 @@
+package chess.domain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+import chess.domain.piece.*;
+
+public enum ChessPiece {
+	BLACK_PAWN(Player.BLACK, Type.PAWN, Pawn::valueOf, "♟"),
+	WHITE_PAWN(Player.WHITE, Type.PAWN, Pawn::valueOf, "♙"),
+	BLACK_ROOK(Player.BLACK, Type.ROOK, Rook::valueOf, "♜"),
+	WHITE_ROOK(Player.WHITE, Type.ROOK, Rook::valueOf, "♖"),
+	BLACK_KNIGHT(Player.BLACK, Type.KNIGHT, Knight::valueOf, "♞"),
+	WHITE_KNIGHT(Player.WHITE, Type.KNIGHT, Knight::valueOf, "♘"),
+	BLACK_BISHOP(Player.BLACK, Type.BISHOP, Bishop::valueOf, "♝"),
+	WHITE_BISHOP(Player.WHITE, Type.BISHOP, Bishop::valueOf, "♗"),
+	BLACK_QUEEN(Player.BLACK, Type.QUEEN, Queen::valueOf, "♛"),
+	WHITE_QUEEN(Player.WHITE, Type.QUEEN, Queen::valueOf, "♕"),
+	BLACK_KING(Player.BLACK, Type.KING, King::valueOf, "♚"),
+	WHITE_KING(Player.WHITE, Type.KING, King::valueOf, "♔"),
+	EMPTY(Player.EMPTY, Type.EMPTY, EmptyPiece::valueOf, "");
+
+	private Player player;
+	private Type type;
+	private BiFunction<Player, Position, Piece> generator;
+	private String image;
+
+	ChessPiece(Player player, Type type, BiFunction<Player, Position, Piece> generator, String image) {
+		this.player = player;
+		this.type = type;
+		this.generator = generator;
+		this.image = image;
+	}
+
+	public static ChessBoard generateChessBoard(ChessInitialPosition chessPosition) {
+		Map<ChessPiece, List<Position>> positions = chessPosition.getPositions();
+		ChessBoard chessBoard = new ChessBoard();
+		for (ChessPiece chessPiece : ChessPiece.values()) {
+			if(chessPiece.equals(EMPTY)) {
+				continue;
+			}
+			for (Position position : positions.get(chessPiece)) {
+				chessBoard.addPiece(chessPiece.generator.apply(chessPiece.player, position));
+			}
+		}
+		return chessBoard;
+	}
+
+	public static Piece generatePiece(Player player, Type type, Position position) {
+		for(ChessPiece chessPiece : ChessPiece.values()) {
+			if(chessPiece.player.equals(player) && (chessPiece.type.equals(type))) {
+				return chessPiece.generator.apply(player, position);
+			}
+		}
+		throw new IllegalArgumentException("체스 말을 생성할 수 없습니다.");
+	}
+
+	public static String getPieceImage(Player player, Type type) {
+		for(ChessPiece chessPiece : ChessPiece.values()) {
+			if(chessPiece.player.equals(player) && (chessPiece.type.equals(type))) {
+				return chessPiece.image;
+			}
+		}
+		throw new IllegalArgumentException("해당 이미지를 찾을 수 없습니다.");
+	}
+
+	public static ChessPiece getChessPiece(Player player, Type type) {
+		for(ChessPiece chess : ChessPiece.values()) {
+			if(chess.player.equals(player) && (chess.type.equals(type))) {
+				return chess;
+			}
+		}
+		throw new IllegalArgumentException("해당 체스 말을 찾을 수 없습니다.");
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public Type getType() {
+		return type;
+	}
+}
