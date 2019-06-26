5f6330cbfecec021dc44d6f89df80a6b35e4bc16
deocksoo <dusol92@snu.ac.kr>
2019-06-27 02:24:25 +0900

    test: Add explanation to DirectionTest

--- src/test/java/model/DirectionTest.java
+++ src/test/java/model/DirectionTest.java
@@ -1,6 +1,6 @@
 package model;
 
-import model.Direction;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,11 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DirectionTest {
 
     @Test
+    @DisplayName("시계방향으로 돌면서 다음 방향을 되돌려준다.")
     void clockwiseNext() {
         assertThat(Direction.EAST.clockwiseNext()).isEqualTo(Direction.SOUTH_EAST);
     }
 
     @Test
+    @DisplayName("반시계방향으로 돌면서 다음 방향을 되돌려준다.")
     void counterclockwiseNext() {
         assertThat(Direction.EAST.counterclockwiseNext()).isEqualTo(Direction.NORTH_EAST);
     }
