68133e19c92b9dd0856b511da6f9f78900b43b3f
pkch93 <star_final@naver.com>
2019-06-26 14:19:58 +0900

    refactor: Direction enum
    MoveRules에 위치한 Direction 정보를 가지도록 구현

--- src/main/java/chess/domain/Direction.java
+++ src/main/java/chess/domain/Direction.java
@@ -1,5 +1,8 @@
 package chess.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Direction {
     N(0, 1, (x, y) -> x == 0 && y > 0),
     NE(1, 1, (x, y) -> x > 0 && y > 0 && isAbsEqual(x, y)),
@@ -11,6 +14,16 @@ public enum Direction {
     NW(-1, 1, (x, y) -> x < 0 && y > 0 && isAbsEqual(x, y)),
     KNIGHT(100, 100, (x, y) -> Math.abs(x) + Math.abs(y) == 3);
 
+    private static boolean isAbsEqual(final int x, final int y) {
+        return Math.abs(x) == Math.abs(y);
+    }
+
+    public static final List<Direction> WHITE_PAWN_DIRECTION = Arrays.asList(N, NE, NW);
+    public static final List<Direction> BLACK_PAWN_DIRECTION = Arrays.asList(S, SE, SW);
+    public static final List<Direction> DIAGONAL_DIRECTION = Arrays.asList(NE, SE, SW, NW);
+    public static final List<Direction> CROSS_DIRECTION = Arrays.asList(N, E, S, W);
+    public static final List<Direction> ALL_DIRECTION = Arrays.asList(N, NE, E, SE, S, SW, W, NW);
+
     private final int x;
     private final int y;
     private final DirectionCheck directionCheck;
@@ -19,10 +32,6 @@ public enum Direction {
         this.x = x;
         this.y = y;
         this.directionCheck = directionCheck;
-    }
-
-    private static boolean isAbsEqual(final int x, final int y) {
-        return Math.abs(x) == Math.abs(y);
     }
 
     public static Direction valueOf(int x, int y) {
