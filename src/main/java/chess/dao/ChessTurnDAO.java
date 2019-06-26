3fc673266aa0d956a6c0c37d5f4c52c6f5b307f7
school0bhy <house1217@ajou.ac.kr>
2019-06-26 18:20:55 +0900

    Add : implement ChessTurnDAO

---
+++ src/main/java/chess/dao/ChessTurnDAO.java
@@ -0,0 +1,95 @@
+package chess.dao;
+
+import chess.domain.piece.PieceColor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChessTurnDAO {
+    private static final String insertQuery = "INSERT INTO chess_turn (turn) VALUES (?)";
+    private static final String updateQuery = "UPDATE chess_turn SET turn=? WHERE game_id=?";
+    private static final String selectTurnQuery = "SELECT turn FROM chess_turn WHERE game_id=?";
+    private static final String selectMaxQuery = "SELECT max(game_id) FROM chess_turn";
+    private static final String deleteQuery = "DELETE FROM chess_turn WHERE game_id=?";
+    private static final String selectGameIdsQuery = "SELECT game_id FROM chess_turn";
+    private static ChessTurnDAO chessTurnDAO;
+
+    private ChessTurnDAO() {
+    }
+
+    public static ChessTurnDAO getInstance() {
+        if (chessTurnDAO == null) {
+            chessTurnDAO = new ChessTurnDAO();
+        }
+        return chessTurnDAO;
+    }
+
+    public PieceColor selectChessTurn(int id) throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+
+            PreparedStatement pstmt = connection.prepareStatement(selectTurnQuery);
+            pstmt.setInt(1, id);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next() ? PieceColor.valueOf(rs.getString("turn")) : null;
+        }
+    }
+
+    public void updateChessTurn(int id, PieceColor turn) throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+
+            PreparedStatement pstmt = connection.prepareStatement(updateQuery);
+            pstmt.setString(1, String.valueOf(turn));
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+        }
+    }
+
+    public void insertChessTurn(PieceColor turn) throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+
+            PreparedStatement pstmt = connection.prepareStatement(insertQuery);
+            pstmt.setString(1, String.valueOf(turn));
+            pstmt.executeUpdate();
+        }
+    }
+
+    public int selectMaxGameId() throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+
+            PreparedStatement pstmt = connection.prepareStatement(selectMaxQuery);
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next() ? rs.getInt("max(game_id)") : -1;
+        }
+    }
+
+    public void deleteChessTurn(int id) throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+
+            PreparedStatement pstmt = connection.prepareStatement(deleteQuery);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        }
+    }
+
+    public List<Integer> selectChessGames() throws SQLException {
+        try (Connection connection = DBUtil.getConnection()) {
+
+            PreparedStatement pstmt = connection.prepareStatement(selectGameIdsQuery);
+            ResultSet rs = pstmt.executeQuery();
+
+            List<Integer> gameIds = new ArrayList<>();
+            while (rs.next()) {
+                gameIds.add(rs.getInt("game_id"));
+            }
+
+            return gameIds;
+        }
+    }
+}
---
+++ src/test/java/chess/dao/ChessTurnDAOTest.java
@@ -0,0 +1,95 @@
+package chess.dao;
+
+import chess.domain.piece.PieceColor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class ChessTurnDAOTest {
+    ChessTurnDAO chessTurnDAO;
+    Connection connection;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        connection = DBUtil.getConnection();
+        connection.setAutoCommit(false);
+        chessTurnDAO = ChessTurnDAO.getInstance();
+    }
+
+    @Test
+    void insertTest1() {
+        assertDoesNotThrow(() ->
+                chessTurnDAO.insertChessTurn(PieceColor.BLACK));
+    }
+
+    @Test
+    void selectTest1() throws SQLException {
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        int gameId = chessTurnDAO.selectMaxGameId();
+        assertThat(chessTurnDAO.selectChessTurn(gameId)).isEqualTo(PieceColor.BLACK);
+
+    }
+
+    @Test
+    void selectTest2() throws Exception {
+        int gameId = chessTurnDAO.selectMaxGameId();
+        assertThat(chessTurnDAO.selectChessTurn(gameId + 100)).isNull();
+    }
+
+    @Test
+    void upDateTest() throws SQLException {
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        int gameId = chessTurnDAO.selectMaxGameId();
+        chessTurnDAO.updateChessTurn(gameId, PieceColor.WHITE);
+
+        assertThat(chessTurnDAO.selectChessTurn(gameId)).isEqualTo(PieceColor.WHITE);
+    }
+
+    @Test
+    void selectMaxTest() throws Exception {
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+
+        int before = chessTurnDAO.selectMaxGameId();
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+
+        assertThat(chessTurnDAO.selectMaxGameId()).isEqualTo(before + 3);
+    }
+
+    @Test
+    public void deleteTest() throws Exception {
+        ChessTurnDAO chessTurnDAO = ChessTurnDAO.getInstance();
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        int id = chessTurnDAO.selectMaxGameId();
+
+        assertThat(chessTurnDAO.selectChessTurn(id)).isEqualTo(PieceColor.BLACK);
+
+        chessTurnDAO.deleteChessTurn(id);
+
+        assertThat(chessTurnDAO.selectChessTurn(id)).isNull();
+    }
+
+    @Test
+    void selectGameIds() throws Exception {
+        int beforeGameSize = chessTurnDAO.selectChessGames().size();
+
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+        chessTurnDAO.insertChessTurn(PieceColor.BLACK);
+
+        assertThat(chessTurnDAO.selectChessGames().size()).isEqualTo(beforeGameSize + 3);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        connection.rollback();
+        connection.close();
+    }
+}
