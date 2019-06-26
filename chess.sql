4ef06f0d8321a322de4b8eebe2c07def0f8f0c86
jinwook <jin942002@naver.com>
2019-06-26 11:58:21 +0900

    chess sql 파일 추가

---
+++ chess.sql
@@ -0,0 +1,23 @@
+show databases;
+create database chess;
+use chess;
+
+
+create table team(
+	id int auto_increment primary key,
+    name varchar(10) not null
+);
+
+insert into team values (0, "BLACK");
+
+
+create table chess_board(
+	id int auto_increment primary key,
+    status varchar(80) not null,
+	team_id int not null,
+    foreign key (team_id) references team (id)
+    on delete no action
+    on update no action
+);
+
+
