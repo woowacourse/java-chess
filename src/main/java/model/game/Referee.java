ebb85bd87f0c0f0480890328a0d657ed28adc656
deocksoo <dusol92@snu.ac.kr>
2019-06-27 02:00:48 +0900

    refactor: remove domain to refactor all pieces

--- src/main/java/model/game/Referee.java
+++
@@ -1,8 +0,0 @@
-package model.game;
-
-public class Referee {
-    public static boolean isKingAlive(final Game game) {
-        return game.board().getPieces()
-                            .anyMatch(p -> p.isKing() && p.team() == game.turn().team());
-    }
-}
