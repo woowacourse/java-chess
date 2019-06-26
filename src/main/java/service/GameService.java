7f96521cd62bbc9f98e488be1b1793642a960e4c
deocksoo <dusol92@snu.ac.kr>
2019-06-27 02:03:19 +0900

    refactor: Disconnect controller and template temporarily

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -1,68 +1,56 @@
 package service;
 
-import model.board.Position;
-import model.game.FailedToRestartGameException;
-import model.game.FailedToRestoreGameException;
-import model.game.Game;
-import model.game.GameDAO;
-import model.game.Referee;
-import view.WebView;
-
-import java.sql.SQLException;
-import java.util.Optional;
-import java.util.function.Consumer;
-
 public class GameService {
-    private static Optional<Game> game = Optional.empty();
-    private static Optional<Position> from = Optional.empty();
-
-    public static Game getGame() {
-        return game.orElseGet(() -> {
-            try {
-                return new Game(GameDAO.retrieveLog());
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw new FailedToRestoreGameException();
-            }
-        });
-    }
-
-    public static Game restartGame() {
-        try {
-            GameDAO.eraseLog();
-            game = Optional.of(new Game());
-            return getGame();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new FailedToRestartGameException();
-        }
-    }
-
-    public static String selectSrc(final String position) {
-        from = Position.ofSafe(position);
-        if (from.map(pos -> game.get().isOwnPiece(pos)).orElse(false)) {
-            return WebView.printSelectPage(game.get(), from.get());
-        }
-        return WebView.printWrongChoicePage(game.get(), "잘못된 선택입니다.");
-    }
-
-    public static String selectDest(final String position, final Consumer<String> redirect, final Consumer<Integer> status) {
-        if (!from.isPresent()) {
-            return WebView.printWrongChoicePage(game.get(), "잘못된 접근입니다.");
-        }
-        final Optional<Position> to = Position.ofSafe(position);
-        if (to.map(pos -> game.get().tryToMoveFromTo(from.get(), to.get())).orElse(false)) {
-            return Referee.isKingAlive(game.get()) ? initState(redirect, status) : WebView.printEndPage(game.get());
-        }
-        return WebView.printWrongChoicePage(game.get(), from.get(), "잘못된 선택입니다.");
-    }
-
-    public static String initState(final Consumer<String> redirect, final Consumer<Integer> status) {
-        from = Optional.empty();
-        redirect.accept("/");
-        status.accept(200);
-        return null;
-    }
+//    private static Optional<Game> game = Optional.empty();
+//    private static Optional<Position> from = Optional.empty();
+//
+//    public static Game getGame() {
+//        return game.orElseGet(() -> {
+//            try {
+//                return new Game(GameDAO.retrieveLog());
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//                throw new FailedToRestoreGameException();
+//            }
+//        });
+//    }
+//
+//    public static Game restartGame() {
+//        try {
+//            GameDAO.eraseLog();
+//            game = Optional.of(new Game());
+//            return getGame();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            throw new FailedToRestartGameException();
+//        }
+//    }
+//
+//    public static String selectSrc(final String position) {
+//        from = Position.ofSafe(position);
+//        if (from.map(pos -> game.get().isOwnPiece(pos)).orElse(false)) {
+//            return WebView.printSelectPage(game.get(), from.get());
+//        }
+//        return WebView.printWrongChoicePage(game.get(), "잘못된 선택입니다.");
+//    }
+//
+//    public static String selectDest(final String position, final Consumer<String> redirect, final Consumer<Integer> status) {
+//        if (!from.isPresent()) {
+//            return WebView.printWrongChoicePage(game.get(), "잘못된 접근입니다.");
+//        }
+//        final Optional<Position> to = Position.ofSafe(position);
+//        if (to.map(pos -> game.get().tryToMoveFromTo(from.get(), to.get())).orElse(false)) {
+//            return Referee.isKingAlive(game.get()) ? initState(redirect, status) : WebView.printEndPage(game.get());
+//        }
+//        return WebView.printWrongChoicePage(game.get(), from.get(), "잘못된 선택입니다.");
+//    }
+//
+//    public static String initState(final Consumer<String> redirect, final Consumer<Integer> status) {
+//        from = Optional.empty();
+//        redirect.accept("/");
+//        status.accept(200);
+//        return null;
+//    }
 }
 
 //// TODO: 2019-06-26 refactor 
--- src/main/java/service/LogVO.java
+++ src/main/java/service/LogVO.java
@@ -1,44 +1,42 @@
 package service;
 
-import model.board.Position;
-import model.game.Turn;
-
-public class LogVO implements Comparable<LogVO> {
-    final Turn turn;
-    final Position from;
-    final Position to;
-
-    public LogVO(final Turn turn, final Position from, final Position to) {
-        this.turn = turn;
-        this.from = from;
-        this.to = to;
-    }
-
-    public LogVO(final int turnCount, final String from, final String to) {
-        this.turn = new Turn(turnCount);
-        this.from = Position.of(from);
-        this.to = Position.of(to);
-    }
-
-    public Turn turn() {
-        return this.turn;
-    }
-
-    public Position from() {
-        return this.from;
-    }
-
-    public Position to() {
-        return this.to;
-    }
-
-    @Override
-    public int compareTo(final LogVO rhs) {
-        return this.turn.count() - rhs.turn.count();
-    }
-
-    @Override
-    public String toString() {
-        return this.turn.count() + " : " + this.from.toString() + " -> " + this.to.toString();
-    }
+public class LogVO {
+//public class LogVO implements Comparable<LogVO> {
+//    final Turn turn;
+//    final Position from;
+//    final Position to;
+//
+//    public LogVO(final Turn turn, final Position from, final Position to) {
+//        this.turn = turn;
+//        this.from = from;
+//        this.to = to;
+//    }
+//
+//    public LogVO(final int turnCount, final String from, final String to) {
+//        this.turn = new Turn(turnCount);
+//        this.from = Position.of(from);
+//        this.to = Position.of(to);
+//    }
+//
+//    public Turn turn() {
+//        return this.turn;
+//    }
+//
+//    public Position from() {
+//        return this.from;
+//    }
+//
+//    public Position to() {
+//        return this.to;
+//    }
+//
+//    @Override
+//    public int compareTo(final LogVO rhs) {
+//        return this.turn.count() - rhs.turn.count();
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return this.turn.count() + " : " + this.from.toString() + " -> " + this.to.toString();
+//    }
 }
