c1dd27b4b4b68ca0b7c7e2cc32147e65d719195a
hyperpace <cnk9109@gmail.com>
2019-06-27 12:12:54 +0900

    feat: update BoardGeneratorService removed catch Exception

--- src/main/java/chess/service/BoardGeneratorService.java
+++ src/main/java/chess/service/BoardGeneratorService.java
@@ -6,21 +6,24 @@ import chess.persistence.dto.ChessBoardDTO;
 import chess.persistence.dto.ChessGameDTO;
 
 public class BoardGeneratorService {
+    private static final int PLUS_ROUND_NUMBER = 1;
+
     public static BoardGeneratorService getInstance() {
         return BoardGeneratorHolder.INSTANCE;
     }
 
     public ChessBoardDTO request(ChessGameDTO chessGameDTO) {
         ChessBoardDTO chessBoardDTO = new ChessBoardDTO();
-
-        try {
-            chessBoardDTO.setGameId(chessGameDTO.getGameId());
-            chessBoardDTO.setRoundNo(ChessBoardDAO.getInstance().findMaxRoundByGameId(chessGameDTO.getGameId()));
-            chessBoardDTO = ChessBoardDAO.getInstance().findByBoardStatus(chessBoardDTO);
-        } catch (IllegalArgumentException e) {
+        chessBoardDTO.setGameId(chessGameDTO.getGameId());
+        int maxRoundNo = ChessBoardDAO.getInstance().findMaxRoundByGameId(chessGameDTO.getGameId());
+        if (maxRoundNo == -1) {
+            chessBoardDTO.setRoundNo(maxRoundNo + PLUS_ROUND_NUMBER);
             chessBoardDTO.setBoard(Board.drawBoard().getBoard());
             ChessBoardDAO.getInstance().addBoardStatus(chessBoardDTO);
         }
+        chessBoardDTO.setRoundNo(ChessBoardDAO.getInstance().findMaxRoundByGameId(chessGameDTO.getGameId()));
+        chessBoardDTO = ChessBoardDAO.getInstance().findByBoardStatus(chessBoardDTO);
+        chessBoardDTO = ChessBoardDAO.getInstance().findByBoardStatus(chessBoardDTO);
 
         return chessBoardDTO;
     }
