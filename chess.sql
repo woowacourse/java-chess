febd0fcfc775e1ed34e783d50a2f0309c9105c1a
jinwook <jin942002@naver.com>
2019-06-27 11:32:52 +0900

    use space instead of tab

--- chess.sql
+++ chess.sql
@@ -4,20 +4,20 @@ use chess;
 
 
 create table team(
-	id int auto_increment primary key,
-    name varchar(10) not null
+ id int auto_increment primary key,
+ name varchar(10) not null
 );
 
 insert into team values (0, "BLACK");
 
 
 create table chess_board(
-	id int auto_increment primary key,
-    status varchar(80) not null,
-	team_id int not null,
-    foreign key (team_id) references team (id)
-    on delete no action
-    on update no action
+ id int auto_increment primary key,
+ status varchar(80) not null,
+ team_id int not null,
+ foreign key (team_id) references team (id)
+ on delete no action
+ on update no action
 );
 
 
