047bfb40f4c702356330cc59de2ff7ab76218ac5
starkim06 <starkim06@naver.com>
2019-06-27 16:12:56 +0900

    refactor: 익명클래스 람다함수로 수정

--- src/main/java/chess/dao/RoundDao.java
+++ src/main/java/chess/dao/RoundDao.java
@@ -2,21 +2,16 @@ package chess.dao;
 
 import chess.dto.RoundDto;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RoundDao {
     public void addRound(RoundDto roundDto) throws SQLException {
-        PreparedStatementSetter pss = new PreparedStatementSetter() {
-            @Override
-            public void setParameters(PreparedStatement preparedStatement) throws SQLException {
-                preparedStatement.setInt(1, roundDto.getRound());
-                preparedStatement.setInt(2, roundDto.getFrom());
-                preparedStatement.setInt(3, roundDto.getTo());
-            }
+        PreparedStatementSetter pss = preparedStatement -> {
+            preparedStatement.setInt(1, roundDto.getRound());
+            preparedStatement.setInt(2, roundDto.getFrom());
+            preparedStatement.setInt(3, roundDto.getTo());
         };
         JdbcTemplate template = new JdbcTemplate();
         String query = "INSERT INTO game (round, start, target) VALUES (?, ?, ?)";
@@ -24,19 +19,16 @@ public class RoundDao {
     }
 
     public List<RoundDto> selectRound() throws SQLException {
-        RowMapper rowMapper = new RowMapper() {
-            @Override
-            public Object mapRow(ResultSet resultSet) throws SQLException {
-                List<RoundDto> roundDtos = new ArrayList<>();
-                while (resultSet.next()) {
-                    RoundDto roundDto = new RoundDto();
-                    roundDto.setRound(resultSet.getInt("round"));
-                    roundDto.setFrom(resultSet.getInt("start"));
-                    roundDto.setTo(resultSet.getInt("target"));
-                    roundDtos.add(roundDto);
-                }
-                return roundDtos;
+        RowMapper rowMapper = resultSet -> {
+            List<RoundDto> roundDtos = new ArrayList<>();
+            while (resultSet.next()) {
+                RoundDto roundDto = new RoundDto();
+                roundDto.setRound(resultSet.getInt("round"));
+                roundDto.setFrom(resultSet.getInt("start"));
+                roundDto.setTo(resultSet.getInt("target"));
+                roundDtos.add(roundDto);
             }
+            return roundDtos;
         };
         JdbcTemplate template = new JdbcTemplate();
         String query = "SELECT round, start, target FROM game ORDER BY round ASC";
