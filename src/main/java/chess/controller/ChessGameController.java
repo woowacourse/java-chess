be13f69f79ba2521991223a3f9d422ddad2842ff
SUNGBUMPARK <psbum1609@gmail.com>
2019-06-27 17:43:56 +0900

    refactor: session 사용

--- src/main/java/chess/controller/ChessGameController.java
+++ src/main/java/chess/controller/ChessGameController.java
@@ -13,35 +13,39 @@ import spark.Route;
 
 public class ChessGameController {
 
-    private static ChessGame chessGame;
-    private static boolean isNewGame;
-
     public static Route newGame = (request, response) -> {
+        request.session(true);
+        request.session().attribute("isNewGame", true);
         response.redirect("/chessgame.html");
-        isNewGame = true;
         return null;
     };
 
     public static Route continueGame = (request, response) -> {
+        request.session(true);
+        request.session().attribute("isNewGame", false);
         response.redirect("/chessgame.html");
-        isNewGame = false;
         return null;
     };
 
     public static Route initialize = (request, response) -> {
         response.type("application/json");
+        boolean isNewGame = request.session().attribute("isNewGame");
+        ChessGame chessGame;
+        ChessBoardDto chessBoardDto;
+        //request.session(true);
         try {
-            ChessBoardDto chessBoardDto;
             if (isNewGame) {
                 chessGame = new ChessGame();
                 NewGameInitializer newGameInitializer = new NewGameInitializer();
                 chessBoardDto = newGameInitializer.initialize(chessGame);
+                request.session().attribute("chessGame", chessGame);
                 return new Gson().toJson(chessBoardDto.getInitWebBoard());
             }
 
             ContinueGameInitializer continueGameInitializer = new ContinueGameInitializer();
             chessBoardDto = continueGameInitializer.initialize();
             chessGame = new ChessGame(chessBoardDto.getCurrentOfTurn(), chessBoardDto.getGameBoard());
+            request.session().attribute("chessGame", chessGame);
             return new Gson().toJson(chessBoardDto.getInitWebBoard());
         } catch (Exception e) {
             response.status(500);
@@ -52,6 +56,7 @@ public class ChessGameController {
     public static Route move = (request, response) -> {
         response.type("application/json");
         MoveResultDto moveResultDto = new MoveResultDto();
+        ChessGame chessGame = request.session().attribute("chessGame");
         try {
             MoveDto moveDto = new MoveDto();
             moveDto.setSource(PointFactory.of(request.queryMap("source").value()));
