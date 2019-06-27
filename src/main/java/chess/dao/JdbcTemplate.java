648c315c0e02d12879f07426981be8e1c0ecba52
starkim06 <starkim06@naver.com>
2019-06-28 08:01:56 +0900

    refactor: JdbcTemplate 리팩토링

--- src/main/java/chess/dao/JdbcTemplate.java
+++ src/main/java/chess/dao/JdbcTemplate.java
@@ -10,59 +10,22 @@ import java.util.List;
 
 public class JdbcTemplate {
     public void executeUpdate(String sql, PreparedStatementSetter pss) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = ConnectionManager.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             pss.setParameters(preparedStatement);
-
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                throw new IllegalArgumentException(e);
-            }
         }
     }
 
     public <T> List<T> executeQuery(String sql, RowMapper rm) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = ConnectionManager.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery();
-
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
             return rm.mapRow(resultSet);
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                throw new IllegalArgumentException(e);
-            }
         }
     }
 }
