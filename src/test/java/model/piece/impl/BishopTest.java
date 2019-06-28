0cb658edccc1e2c5345d4f5caeda183a66477325
deocksoo <dusol92@snu.ac.kr>
2019-06-28 10:58:19 +0900

    refactor: Split Bishop test method

--- src/test/java/model/piece/impl/BishopTest.java
+++ src/test/java/model/piece/impl/BishopTest.java
@@ -7,6 +7,7 @@ import model.board.BoardView;
 import model.piece.Piece;
 import model.piece.PieceColor;
 import model.piece.PieceFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,34 +15,59 @@ import static model.Direction.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BishopTest {
+    Position position;
+    Piece bishop;
+
+    @BeforeEach
+    void setUp() {
+        position = Position.of(4, 4);
+        bishop = PieceFactory.create(Bishop.class, PieceColor.BLACK, position);
+    }
+
     @Test
-    @DisplayName("비숍의 움직일 수 있는 좌표를 모드 되돌려준다.")
-    void getMovablePositionsTest() {
-        Position position = Position.of(4, 4);
-        Piece bishop = PieceFactory.create(Bishop.class, PieceColor.BLACK, position);
+    @DisplayName("아무것도 없는 경우 끝까지 움직일 수 있다.")
+    void getMovablePositionsBoundTest() {
+        Board board = new BoardBuilder()
+                .piece(bishop)
+                .build();
+
+        assertThat(bishop.getMovablePositions(board.getBoardView())).contains(
+                Position.of(7,7));
+    }
+
+    @Test
+    @DisplayName("우리 팀이 있는 경우 해당 자리 포함하여 뒤로는 갈 수 없다.")
+    void getMovablePositionsWhenObstacleIsMyTeam() {
         Piece myPiece = PieceFactory.create(Pawn.class, PieceColor.BLACK,
-                position.of(SOUTH_WEST).of(SOUTH_WEST));
-        Piece enemyPiece = PieceFactory.create(Pawn.class, PieceColor.WHITE,
-                position.of(NORTH_WEST).of(NORTH_WEST));
+                position.of(6,6));
 
         Board board = new BoardBuilder()
                 .piece(bishop)
                 .piece(myPiece)
-                .piece(enemyPiece)
                 .build();
 
-        BoardView boardView = board.getBoardView();
+        assertThat(bishop.getMovablePositions(board.getBoardView())).contains(
+                Position.of(5,5));
+        assertThat(bishop.getMovablePositions(board.getBoardView())).doesNotContain(
+                Position.of(6,6));
+        assertThat(bishop.getMovablePositions(board.getBoardView())).doesNotContain(
+                Position.of(7,7));
+    }
 
-        assertThat(bishop.getMovablePositions(boardView)).contains(
-                position.of(NORTH_WEST),
-                position.of(NORTH_WEST).of(NORTH_WEST),
-                position.of(NORTH_EAST),
-                position.of(NORTH_EAST).of(NORTH_EAST),
-                position.of(NORTH_EAST).of(NORTH_EAST).of(NORTH_EAST),
-                position.of(SOUTH_EAST),
-                position.of(SOUTH_EAST).of(SOUTH_EAST),
-                position.of(SOUTH_EAST).of(SOUTH_EAST).of(SOUTH_EAST),
-                position.of(SOUTH_WEST)
-        );
+    @Test
+    @DisplayName("적팀이 있는 경우 해당 자리는 움직이고 그 뒤로는 갈 수 없다.")
+    void getMovablePositionsWhenObstacleIsEnemy() {
+        Piece enemy = PieceFactory.create(Pawn.class, PieceColor.WHITE,
+                position.of(6,6));
+
+        Board board = new BoardBuilder()
+                .piece(bishop)
+                .piece(enemy)
+                .build();
+
+        assertThat(bishop.getMovablePositions(board.getBoardView())).contains(
+                Position.of(6,6));
+        assertThat(bishop.getMovablePositions(board.getBoardView())).doesNotContain(
+                Position.of(7,7));
     }
 }
