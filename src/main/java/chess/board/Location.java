4a53f004f319de4e0067140be2a28a637e80fabc
hongbin23 <khbasd@gmail.com>
2020-03-24 17:40:34 +0900

    [refact] Location 테스트 추가

--- src/main/java/chess/board/Location.java
+++ src/main/java/chess/board/Location.java
@@ -1,23 +1,41 @@
 package chess.board;
 
+import java.util.Objects;
+
 public class Location {
-    private final int row;
-    private final char col;
+	private final int row;
+	private final char col;
 
-    public Location(int row, char col) {
-        this.row = row;
-        this.col = col;
-    }
+	public Location(int row, char col) {
+		this.row = row;
+		this.col = col;
+	}
 
-    public Location moveTo(final int row, final char col) {
-        return new Location(row, col);
-    }
+	public Location moveTo(final int row, final char col) {
+		return new Location(row, col);
+	}
 
-    public Location moveRowBy(final int rowValue) {
-        return moveTo(this.row + rowValue, col);
-    }
+	public Location moveRowBy(final int rowValue) {
+		return moveTo(this.row + rowValue, col);
+	}
 
-    public Location moveColBy(final int colValue) {
-        return moveTo(row, (char) (this.col + colValue));
-    }
+	public Location moveColBy(final int colValue) {
+		return moveTo(row, (char)(this.col + colValue));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Location location = (Location)o;
+		return row == location.row &&
+			col == location.col;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(row, col);
+	}
 }
