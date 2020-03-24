c8e2f9f575d193be3f2a2c321f438da092cfa6cd
hamliet <hamliet@hanyang.ac.kr>
2020-03-24 17:44:39 +0900

    [feat] InputView 생성

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -2,11 +2,13 @@ package chess;
 
 import chess.domain.ChessBoard;
 import chess.factory.ChessPieceFactory;
+import chess.view.InputView;
 import chess.view.OutputView;
 
 public class Application {
     public static void main(String []args) {
         ChessBoard chessBoard = new ChessBoard(ChessPieceFactory.blackTeamCreate(), ChessPieceFactory.whiteTeamCreate());
+        InputView.checkStart();
         OutputView.printBoard(chessBoard);
     }
 }
