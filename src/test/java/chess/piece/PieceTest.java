422c9be7dac77fef29f39964a6529abdfc02dd17
taejinseok <stj930@gmail.com>
2020-03-24 16:22:27 +0900

    test: 체스말 테스트 코드 추가

---
+++ src/test/java/chess/piece/PieceTest.java
@@ -0,0 +1,11 @@
+package chess.piece;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PieceTest {
+	@DisplayName("displayName")
+	@Test
+	void name() {
+	}
+}
