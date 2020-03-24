ba619bcc4311f6e0d75f588cf51679a32eeae4ab
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-24 17:35:44 +0900

    feat: Tile 클래스 생성

---
+++ src/main/java/chess/board/Tile.java
@@ -0,0 +1,19 @@
+package chess.board;
+
+import chess.board.piece.Piece;
+
+import java.util.Objects;
+
+public class Tile {
+    private final Coordinate coordinate;
+    private final Piece piece;
+
+    public Tile(final Coordinate coordinate, final Piece piece) {
+        this.coordinate = coordinate;
+        this.piece = piece;
+    }
+
+    public boolean hasPiece() {
+        return !Objects.isNull(piece);
+    }
+}
---
+++ src/test/java/chess/board/TileTest.java
@@ -0,0 +1,29 @@
+package chess.board;
+
+import chess.board.piece.King;
+import chess.board.piece.Piece;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TileTest {
+    private static Stream<Arguments> pieceProvider() {
+        return Stream.of(
+                Arguments.of(new King(), true),
+                Arguments.of(null, false)
+        );
+    }
+
+    @DisplayName("피스가 있는 경우 확인")
+    @ParameterizedTest
+    @MethodSource("pieceProvider")
+    void hasPiece(Piece piece, boolean expect) {
+        Tile tile = new Tile(new Coordinate(File.A, Rank.ONE), piece);
+        assertThat(tile.hasPiece()).isEqualTo(expect);
+    }
+}
