1c6c71abb5848f190aba4e6e073b2ec8c4971ac5
JunSeong <ebseud6135@naver.com>
2020-03-24 17:27:14 +0900

    [Refactor] ChessBoard 메소드 분리 및 리팩토링

--- src/main/java/chess/domain/ChessBoard.java
+++ src/main/java/chess/domain/ChessBoard.java
@@ -1,29 +1,41 @@
 package chess.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import chess.domain.chesspiece.ChessPiece;
 
-import java.util.List;
-
 public class ChessBoard {
-    private List<ChessPiece> blackChessPieces;
-    private List<ChessPiece> whiteChessPieces;
+	private List<ChessPiece> blackChessPieces;
+	private List<ChessPiece> whiteChessPieces;
 
-    public ChessBoard(List<ChessPiece> blackChessPieces, List<ChessPiece> whiteChessPieces) {
-        this.blackChessPieces = blackChessPieces;
-        this.whiteChessPieces = whiteChessPieces;
-    }
+	public ChessBoard(List<ChessPiece> blackChessPieces, List<ChessPiece> whiteChessPieces) {
+		this.blackChessPieces = blackChessPieces;
+		this.whiteChessPieces = whiteChessPieces;
+	}
 
-	public ChessPiece checkPosition(Position position){
-    	for(ChessPiece blackChessPiece : blackChessPieces){
-			if(blackChessPiece.equalPosition(position)){
+
+	public boolean checkPosition(Position position) {
+		List<ChessPiece> allPieces = new ArrayList<>(blackChessPieces);
+		allPieces.addAll(whiteChessPieces);
+
+		return allPieces.stream()
+			.anyMatch(chessPiece -> chessPiece.equalPosition(position));
+	}
+
+	public ChessPiece findByPosition(Position position) {
+		for (ChessPiece blackChessPiece : blackChessPieces) {
+			if (blackChessPiece.equalPosition(position)) {
 				return blackChessPiece;
 			}
 		}
-		for(ChessPiece whiteChessPiece : whiteChessPieces){
-			if(whiteChessPiece.equalPosition(position)){
+		for (ChessPiece whiteChessPiece : whiteChessPieces) {
+			if (whiteChessPiece.equalPosition(position)) {
 				return whiteChessPiece;
 			}
 		}
-		return null;
+		throw new NoSuchElementException();
 	}
+
 }
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -5,23 +5,21 @@ import chess.domain.Position;
 import chess.domain.chesspiece.ChessPiece;
 
 public class OutputView {
-    public static void printBoard(ChessBoard chessBoard){
-        for(int i=8;i>=1;i--){
-            for(char j='a' ;j<='h'; j++){
-                ChessPiece chessPiece = chessBoard.checkPosition(new Position(i, j));
-                if( chessPiece != null){
-                    if(chessPiece.isBlack()){
-                        System.out.print(chessPiece.getName().toUpperCase());
-                    }
-                    if(!chessPiece.isBlack()){
-                        System.out.print(chessPiece.getName());
-                    }
-                }
-                if(chessPiece == null){
-                    System.out.print(".");
-                }
-            }
-            System.out.println();
-        }
-    }
+	public static void printBoard(ChessBoard chessBoard) {
+		for (int i = 8; i >= 1; i--) {
+			for (char j = 'a'; j <= 'h'; j++) {
+				Position position = new Position(i, j);
+				String name = ".";
+				if (chessBoard.checkPosition(position)) {
+					ChessPiece chessPiece = chessBoard.findByPosition(position);
+					name = chessPiece.getName();
+					if (chessPiece.isBlack()) {
+						name = name.toUpperCase();
+					}
+				}
+				System.out.print(name);
+			}
+			System.out.println();
+		}
+	}
 }
