9793c93415bd45e5684d52141b3aaed4495afbaf
Yeji Ahn <yeppilisa@naver.com>
2020-03-24 20:36:25 +0900

    refactor: Position 객체의 범위 유효성 검증 추가와 테스트 코드 구현

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -7,13 +7,19 @@ public class Position {
 	private int y;
 
 	public Position(int x, int y) {
+		validatePositionRange(x, y);
 		this.x = x;
 		this.y = y;
 	}
 
 	public Position(String string) {
-		this.x = string.charAt(0) - 'a' + 1;
-		this.y = Character.getNumericValue(string.charAt(1));
+		this(string.charAt(0) - 'a' + 1, Character.getNumericValue(string.charAt(1)));
+	}
+
+	private void validatePositionRange(int x, int y) {
+		if (x < 1 || x > 8 || y < 1 || y > 8) {
+			throw new IllegalArgumentException("체스판 범위를 초과한 값입니다.");
+		}
 	}
 
 	@Override
--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -4,11 +4,24 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PositionTest {
 	@Test
 	@DisplayName("위치 정보를 String으로 받아 좌표값으로 변환")
 	void PositionReceive() {
 		assertThat(new Position("e1")).isEqualTo(new Position(5, 1));
+	}
+
+	@ParameterizedTest
+	@DisplayName("잘못된 위치 값을 받은 경우 예외를 잘 처리하는지 확인")
+	@CsvSource(value = {"0,4", "9,2", "1,0", "3,9"})
+	void invalidPositionTest(int x, int y) {
+		assertThatThrownBy(() ->
+			new Position(x, y)
+		).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("체스판 범위");
+
 	}
 }
