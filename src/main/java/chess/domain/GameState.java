ae6070fcd24027c1f7faea423f4ee6c82a2e6d5a
jamie9504 <jamie9504@kakao.com>
2020-03-24 19:58:08 +0900

    [feat] start, end 입력

---
+++ src/main/java/chess/domain/GameState.java
@@ -0,0 +1,33 @@
+package chess.domain;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public enum GameState {
+    START("start"),
+    END("end");
+
+    private final String name;
+
+    GameState(String name) {
+        this.name = name;
+    }
+
+    public static GameState of(String input) {
+        validateInput(input);
+        return Arrays.stream(GameState.values())
+                .filter(gameState -> gameState.getName().equals(input.toLowerCase()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 입력입니다"));
+    }
+
+    private static void validateInput(String input) {
+        if (Objects.isNull(input)) {
+            throw new IllegalArgumentException("잘못된 입력입니다");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/chess/domain/GameStateTest.java
@@ -0,0 +1,21 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GameStateTest {
+    @ParameterizedTest
+    @CsvSource(value = {"no", "pause"})
+    @NullAndEmptySource
+    @DisplayName("start, end 이외의 입력이 들어왔을 때 예외처리")
+    void otherInputTest(String input) {
+        assertThatThrownBy(() -> GameState.of(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못");
+    }
+
+}
