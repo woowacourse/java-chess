3278bed4bf0bd18593260a63169425bc2e1b20ed
lalize <printf@kakao.com>
2020-03-25 22:22:53 +0900

    feat: 체스 게임 클래스 추가

---
+++ src/main/java/chess/domain/game/ChessGame.java
@@ -0,0 +1,33 @@
+package chess.domain.game;
+
+import chess.domain.board.Board;
+import chess.domain.piece.Position;
+import chess.domain.state.State;
+
+public class ChessGame {
+	private State state;
+
+	public ChessGame(State state) {
+		this.state = state;
+	}
+
+	public void start() {
+		state = state.start();
+	}
+
+	public void move(Position source, Position target) {
+		state = state.move(source, target);
+	}
+
+	public void end() {
+		state = state.end();
+	}
+
+	public Board board() {
+		return state.board();
+	}
+
+	public boolean isFinished() {
+		return state.isFinished();
+	}
+}
