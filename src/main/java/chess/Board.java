885c10d8303b5eae3aab52c8fb509ea25f4cc6eb
taejinseok <stj930@gmail.com>
2020-03-25 14:19:58 +0900

    feat: 보드 위 체스말 이동 기능 추가

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -6,13 +6,18 @@ import chess.piece.Piece;
 import chess.position.Position;
 
 public class Board {
-    private final Map<Position, Piece> pieces;
+	private final Map<Position, Piece> pieces;
 
-    public Board(Map<Position, Piece> pieces) {
-        this.pieces = pieces;
-    }
+	public Board(Map<Position, Piece> pieces) {
+		this.pieces = pieces;
+	}
 
-    public Piece getPiece(Position position) {
-        return pieces.get(position);
+	public Piece getPiece(Position position) {
+		return pieces.get(position);
+	}
+
+	public void move(Position from, Position to) {
+		Piece target = pieces.remove(from);
+        pieces.put(to, target);
     }
 }
--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,44 +1,45 @@
 package chess;
 
+import static chess.position.File.*;
+import static chess.position.Rank.*;
 import static org.assertj.core.api.Assertions.*;
-
-import chess.piece.Pawn;
-import chess.piece.Piece;
-import chess.piece.Team;
-import chess.position.File;
-import chess.position.Position;
-import chess.position.Rank;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import chess.piece.Pawn;
+import chess.piece.Piece;
+import chess.piece.Team;
+import chess.position.Position;
+
 public class BoardTest {
-    private Board board;
-    private Pawn pawn;
+	private Board board;
+	private Pawn pawn;
 
-    @BeforeEach
-    void setup() {
-        Map<Position, Piece> pieces = new HashMap<>();
-        pawn = new Pawn(Team.BLACK);
-        pieces.put(Position.of(File.A, Rank.ONE), pawn);
-        board = new Board(pieces);
-    }
+	@BeforeEach
+	void setup() {
+		Map<Position, Piece> pieces = new HashMap<>();
+		pawn = new Pawn(Team.BLACK);
+		pieces.put(Position.of(A, ONE), pawn);
+		board = new Board(pieces);
+	}
 
-    @DisplayName("Board객체 생성 테스트")
-    @Test
-    void constructTest() {
-        assertThat(board).isInstanceOf(Board.class);
-    }
+	@DisplayName("Board객체 생성 테스트")
+	@Test
+	void constructTest() {
+		assertThat(board).isInstanceOf(Board.class);
+	}
 
-    @DisplayName("move메서드를 실행하면 Map이 수정되는지 테스트")
-    @Test
-    void moveTest() {
-        Position from = Position.of(File.A, Rank.ONE);
-        Position to = Position.of(File.A, Rank.TWO);
-        board.move(from, to);
-        assertThat(board.getPiece(to)).isEqualTo(pawn);
-    }
+	@DisplayName("move메서드를 실행하면 Map이 수정되는지 테스트")
+	@Test
+	void moveTest() {
+		Position from = Position.of(A, ONE);
+		Position to = Position.of(A, TWO);
+		board.move(from, to);
+		assertThat(board.getPiece(to)).isEqualTo(pawn);
+	}
 }
