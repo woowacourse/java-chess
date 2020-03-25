d03bd3edc3d73684b4b57333a9f26cddf4f22e2e
toneyparky <qkrtmddhks95@gmail.com>
2020-03-25 21:01:01 +0900

    feat: Piece에 Color 추가 및 빈 piece 구현
    Piece color 추가
    Color White, Black, Blank 색 추가
    Blank 빈 칸을 나타냄

---
+++ src/main/java/chess/domain/piece/Color.java
@@ -0,0 +1,30 @@
+package chess.domain.piece;
+
+public enum Color {
+	WHITE,
+	BLACK,
+	BLANK;
+
+	public Color changeColor() {
+		if (isWhite()) {
+			return BLACK;
+		}
+		return WHITE;
+	}
+
+	public boolean isWhite() {
+		return WHITE.equals(this);
+	}
+
+	public boolean isBlack() {
+		return BLACK.equals(this);
+	}
+
+	public boolean isBlank() {
+		return BLANK.equals(this);
+	}
+
+	public boolean isSame(Color color) {
+		return this.equals(color);
+	}
+}
--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -2,22 +2,25 @@ package chess.domain.piece;
 
 import chess.domain.piece.movable.Movable;
 import chess.domain.board.position.Position;
+import chess.domain.piece.remover.Remover;
 
 import java.util.Objects;
 import java.util.Set;
 
-public abstract class Piece {
+public abstract class Piece implements Remover {
 	private static final String INVALID_INPUT_EXCEPTION_MESSAGE = "말을 생성할 수 없습니다.";
 
 	private Position position;
 	private final String name;
 	private final Movable movable;
+	private final Color color;
 
-	public Piece(Position position, String name, Movable movable) {
+	public Piece(Position position, String name, Movable movable, Color color) {
 		validate(position, name, movable);
 		this.position = position;
 		this.name = name;
 		this.movable = movable;
+		this.color = color;
 	}
 
 	private void validate(Position position, String name, Movable movable) {
@@ -40,5 +43,13 @@ public abstract class Piece {
 
 	public String getName() {
 		return name;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void changePosition(Position position) {
+		this.position = position;
 	}
 }
