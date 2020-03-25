bf4370502a5f11e5f6d3f835e986a87cb0444aee
JunSeong <ebseud6135@naver.com>
2020-03-25 18:06:42 +0900

    [Feat] 나이트 canMove 기능 구현

--- src/main/java/chess/domain/chesspiece/Knight.java
+++ src/main/java/chess/domain/chesspiece/Knight.java
@@ -1,23 +1,25 @@
 package chess.domain.chesspiece;
 
-import chess.domain.Move;
+import java.util.List;
+
 import chess.domain.Position;
 import chess.domain.Team;
 
-import java.util.List;
-
 public class Knight extends ChessPiece {
-    public Knight(Position position, Team team) {
-        super("n", position, team);
-    }
 
-    @Override
-    public boolean canMove(Position position) {
-        return false;
-    }
+	public Knight(Position position, Team team) {
+		super("n", position, team);
+	}
 
-    @Override
-    public List<Position> makeCanMovePositions() {
-        return Move.makeKnightPath(this.position);
-    }
+	@Override
+	public boolean canMove(Position position) {
+		int y = Math.abs(position.getY() - this.position.getY());
+		int x = Math.abs(position.getX() - this.position.getX());
+		return Math.abs(x - y) == 1;
+	}
+
+	@Override
+	public List<Position> makeCanMovePositions() {
+		throw new UnsupportedOperationException();
+	}
 }
