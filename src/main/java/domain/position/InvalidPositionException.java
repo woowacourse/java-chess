3a7e672b6a4fd5d5d24bdb3b947c2510690418b0
soring <jnsorn@gmail.com>
2020-03-25 13:44:06 +0900

    feat : Position에 대한 예외 클래스 생성

---
+++ src/main/java/domain/position/InvalidPositionException.java
@@ -0,0 +1,9 @@
+package domain.position;
+
+public class InvalidPositionException extends IllegalArgumentException {
+	public static final String INVALID_COLUMN = "column을 잘못입력했습니다.";
+
+	public InvalidPositionException(String s) {
+		super(s);
+	}
+}
