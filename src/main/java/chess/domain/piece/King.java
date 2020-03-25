bbf689b7c34b815961b842dcb3f3295dbde4a72b
juyeongjung <juyeong.dev@gmail.com>
2020-03-25 20:22:45 +0900

    feat : king 이동 기능 구현

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -1,9 +1,11 @@
 package chess.domain.piece;
 
-import java.util.Collections;
+import static chess.domain.position.Direction.*;
+
+import java.util.Arrays;
 
 public class King extends FixedPiece {
 	public King(String name, Color color) {
-		super(name, color, Collections.emptyList());
+		super(name, color, Arrays.asList(UP, DOWN, LEFT, RIGHT, LEFT_UP, LEFT_DOWN, RIGHT_UP, RIGHT_DOWN));
 	}
 }
---
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -0,0 +1,44 @@
+package chess.domain.piece;
+
+import static chess.domain.position.PositionFixture.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import chess.domain.Board;
+import chess.domain.position.Position;
+
+class KingTest {
+	static Stream<Arguments> generatePositions() {
+		return Stream.of(
+			Arguments.of(E1, D2, true),
+			Arguments.of(E1, D1, false),
+			Arguments.of(E1, F1, true)
+		);
+	}
+
+	@Test
+	public void initTest() {
+		assertThat(new King("k", Color.WHITE)).isInstanceOf(King.class);
+	}
+
+	@ParameterizedTest
+	@MethodSource("generatePositions")
+	void findMovablePositionsTest(Position currentPosition, Position destination, boolean expect) {
+		Map<Position, Piece> pieces = new HashMap<>();
+		pieces.put(D2, new Bishop("b", Color.BLACK));
+		Board board = new Board(BoardFactory.initializeKingQueen(pieces));
+		Piece king = board.findPieceBy(currentPosition);
+
+		Set<Position> positions = king.findMovablePositions(currentPosition, board);
+		assertThat(positions.contains(destination)).isEqualTo(expect);
+	}
+}
