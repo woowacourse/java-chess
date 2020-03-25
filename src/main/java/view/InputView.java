6952ddb2016e438d361cdf3606edc08c02e64103
Tak Hyungmin <milkho11@naver.com>
2020-03-25 11:39:11 +0900

    [feat] 체스 시작 커맨드 입력

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public boolean inputStartCommand() {
+        System.out.println(
+                "체스 게임을 시작합니다. \n " +
+                "게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        String input = SCANNER.nextLine();
+        if(input.equals("start")) {
+            return true;
+        }
+        if(input.equals("end")) {
+            return false;
+        }
+        throw new InputMismatchException("잘못된 시작 커맨드를 입력했습니다.");
+    }
+}
