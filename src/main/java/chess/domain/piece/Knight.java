ed57c2656665116f82dfa604ca1d5f7e7df480a9
Yeji Ahn <yeppilisa@naver.com>
2020-03-25 16:19:31 +0900

    feat: 나이트가 움직일 수 있는지 검증하는 기능 및 테스트코드 추가

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,8 +1,16 @@
 package chess.domain.piece;
 
+import static chess.domain.Direction.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import chess.domain.Direction;
 import chess.domain.Position;
 
 public class Knight extends Piece {
+	private static List<Direction> possibleDirections = Arrays.asList(NNE, NEE, NNW, NWW, SSE, SEE, SSW, SWW);
+
 	public Knight(Position position, Team team) {
 		super(position, team);
 		this.representation = 'N';
@@ -10,7 +18,10 @@ public class Knight extends Piece {
 
 	@Override
 	public void validateMove(Position destination) {
-
+		Direction direction = position.compare(destination);
+		if (!possibleDirections.contains(direction)) {
+			throw new IllegalArgumentException("말이 움직일 수 없는 자리입니다.");
+		}
 	}
 
 }
