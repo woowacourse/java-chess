a40d4cda1e9027faccf29c7920b32fa7ad741a54
lalize <printf@kakao.com>
2020-03-25 20:58:38 +0900

    feat: Color 클래스 추가
    체스 말 컬러를 나타내는 클래스

--- src/main/java/chess/domain/piece/Blank.java
+++ src/main/java/chess/domain/piece/Blank.java
@@ -2,7 +2,7 @@ package chess.domain.piece;
 
 public class Blank extends Piece {
 	public Blank(Position position) {
-		super(position);
+		super(position, Color.NONE);
 	}
 
 	@Override
--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -2,9 +2,11 @@ package chess.domain.piece;
 
 public abstract class Piece {
 	private Position position;
+	private Color color;
 
-	public Piece(Position position) {
+	public Piece(Position position, Color color) {
 		this.position = position;
+		this.color = color;
 	}
 
 	public abstract void moveTo(Position position);
