6bf4d3f0714b087a607d11dd21303de04ea399a9
BEOMJUN KIM <skyblue300a@gmail.com>
2020-03-25 20:15:11 +0900

    feat: Rook의 MoveStrategy 구현

---
+++ src/test/java/chess/domain/piece/RookStrategyTest.java
@@ -0,0 +1,52 @@
+package chess.domain.piece;
+
+import chess.domain.board.Position;
+import chess.domain.exception.InvalidMovementException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RookStrategyTest {
+
+    @ParameterizedTest
+    @DisplayName("이동 경로 찾기")
+    @MethodSource("createSourceToTarget")
+    void findMovePath(Position source, Position target, List<Position> expected) {
+        MoveStrategy rookStrategy = new RookStrategy();
+        assertThat(rookStrategy.findMovePath(source, target)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> createSourceToTarget() {
+        return Stream.of(
+                Arguments.of(Position.from("a1"), Position.from("a4"), Arrays.asList(Position.from("a2"), Position.from("a3"))),
+                Arguments.of(Position.from("a4"), Position.from("a1"), Arrays.asList(Position.from("a3"), Position.from("a2"))),
+                Arguments.of(Position.from("b2"), Position.from("f2"), Arrays.asList(Position.from("c2"), Position.from("d2"), Position.from("e2"))),
+                Arguments.of(Position.from("f2"), Position.from("b2"), Arrays.asList(Position.from("e2"), Position.from("d2"), Position.from("c2"))),
+                Arguments.of(Position.from("g5"), Position.from("g4"), Collections.emptyList())
+        );
+    }
+
+    @Test
+    @DisplayName("이동할 수 없는 source, target")
+    void invalidMovementException() {
+        MoveStrategy rookStrategy = new RookStrategy();
+
+        Position source = Position.from("a1");
+        Position target = Position.from("b2");
+
+        assertThatThrownBy(() -> {
+            rookStrategy.findMovePath(source, target);
+        }).isInstanceOf(InvalidMovementException.class)
+                .hasMessage("이동할 수 없습니다.");
+    }
+}
