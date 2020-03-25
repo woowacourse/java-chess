07cf3b8bfb0cea9dd16c7ae6dde2e2d49d0350eb
moon <mjj920930@gmail.com>
2020-03-26 03:21:05 +0900

    feat: Pawn 의 기본 이동전략 구현

--- src/main/java/chess/domain/movingstrategy/PawnMovingStrategy.java
+++ src/main/java/chess/domain/movingstrategy/PawnMovingStrategy.java
@@ -1,11 +1,50 @@
 package chess.domain.movingstrategy;
 
+import chess.domain.Team;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
+
 import java.util.function.Predicate;
 
 public class PawnMovingStrategy implements Predicate<Variables> {
     @Override
     public boolean test(Variables variables) {
         // from 에서 to 로 갈 수 있으면 true return
+
+        Position fromPosition = variables.getFromPosition();
+        Position toPosition = variables.getToPosition();
+        Team team = variables.getTeam();
+
+        if (team == Team.BLACK) {
+            if (fromPosition.isAt(Rank.SEVEN)) {
+                if (fromPosition.increaseRank(-2) == toPosition
+                        || fromPosition.increaseRank(-1) == toPosition) {
+                    return true;
+                }
+                return false;
+            }
+
+            if (!fromPosition.isAt(Rank.SEVEN)) {
+                if (fromPosition.increaseRank(-1) == toPosition) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        // if team == Team.WHITE
+        if (fromPosition.isAt(Rank.TWO)) {
+            if (fromPosition.increaseRank(2) == toPosition
+                    || fromPosition.increaseRank(1) == toPosition) {
+                return true;
+            }
+            return false;
+        }
+
+        // if fromPosition.isAt(Rank.TWO)
+        if (fromPosition.increaseRank(1) == toPosition) {
+            return true;
+        }
         return false;
     }
 }
