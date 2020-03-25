56d4221c66b5f3e0cc06d4b0651bdd94ba5f308e
soring <jnsorn@gmail.com>
2020-03-25 21:35:26 +0900

    feat : turn을 변경하는 기능 추가

--- src/main/java/domain/piece/team/Team.java
+++ src/main/java/domain/piece/team/Team.java
@@ -1,6 +1,12 @@
 package domain.piece.team;
 
 public enum Team {
-	BLACK(),WHITE();
+	BLACK(), WHITE();
 
+	public static Team changeTurn(Team turn) {
+		if (WHITE == turn) {
+			return BLACK;
+		}
+		return WHITE;
+	}
 }
--- src/test/java/domain/piece/team/TeamTest.java
+++ src/test/java/domain/piece/team/TeamTest.java
@@ -2,11 +2,22 @@ package domain.piece.team;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TeamTest {
+	@DisplayName("팀 생성")
 	@Test
 	void constructor_CreateTeam_Success() {
 		assertThat(Team.BLACK).isInstanceOf(Team.class);
+	}
+
+	@DisplayName("현재 턴이 들어오면 다음 턴을 반환")
+	@ParameterizedTest
+	@CsvSource({"WHITE, BLACK", "BLACK, WHITE"})
+	void changeTurn_GivenNowTurn_ReturnExpectedTurn(Team nowTurn, Team expectedTurn) {
+		assertThat(Team.changeTurn(nowTurn)).isEqualTo(expectedTurn);
 	}
 }
