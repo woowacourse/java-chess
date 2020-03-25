c5d90c2d314df3eaf8fcdb6593e72138f199d4bb
Yeji Ahn <yeppilisa@naver.com>
2020-03-25 16:18:45 +0900

    refactor: King Test코드 추가

--- src/test/java/chess/domain/piece/KingTest.java
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -12,11 +12,20 @@ public class KingTest {
 
 	@ParameterizedTest
 	@DisplayName("킹이 움직일 수 없는 위치가 들어갔을 때 예외를 잘 처리하는지 확인")
-	@CsvSource(value = {"5, 3", "3, 1"})
-	void kingMove(int x, int y) {
+	@CsvSource(value = {"a3", "e5"})
+	void invalidKingMove(String destination) {
 		King king = new King(new Position("e1"), Team.WHITE);
 		assertThatThrownBy(() ->
-			king.validateMove(new Position(x, y))
+			king.validateMove(new Position(destination))
 		).isInstanceOf(IllegalArgumentException.class);
 	}
+
+	@ParameterizedTest
+	@DisplayName("킹이 제대로 움직이는지 확인")
+	@CsvSource(value = {"e1", "e3", "d1", "d2", "d3", "f1", "f2", "f3"})
+	void kingMove(String destination) {
+		King king = new King(new Position("e2"), Team.WHITE);
+		king.move(new Position(destination));
+	}
+
 }
