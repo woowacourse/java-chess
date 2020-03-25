34ed2bd30cc026a2fec800356dc57cb5e8a6390a
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-25 10:55:51 +0900

    feat: Coordinate singleton 패턴 Cache 생성

--- src/main/java/chess/board/Coordinate.java
+++ src/main/java/chess/board/Coordinate.java
@@ -1,15 +1,39 @@
 package chess.board;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public final class Coordinate {
     private final File file;
     private final Rank rank;
 
-    public Coordinate(final File file, final Rank rank) {
+    private Coordinate(final File file, final Rank rank) {
         this.file = file;
         this.rank = rank;
     }
 
+    public static Coordinate of(final File file, final Rank rank) {
+        return CoordinateCache.cache.get(file.name() + rank.name());
+    }
+
+    private String key() {
+        return this.file.name() + this.rank.name();
+    }
+
     public Variation calculateVariation(final Coordinate coordinate) {
         return new Variation(coordinate.file.subtract(this.file), coordinate.rank.subtract(this.rank));
+    }
+
+    private static class CoordinateCache {
+        private static final Map<String, Coordinate> cache;
+
+        static {
+            cache = Arrays.stream(File.values())
+                    .flatMap(file -> Arrays
+                            .stream(Rank.values())
+                            .map(rank -> new Coordinate(file, rank))
+                    ).collect(Collectors.toMap(Coordinate::key, coordinate -> coordinate));
+        }
     }
 }
--- src/test/java/chess/board/CoordinateTest.java
+++ src/test/java/chess/board/CoordinateTest.java
@@ -10,9 +10,9 @@ class CoordinateTest {
     @DisplayName("두 좌표 값의 차이를 구한다.")
     @Test
     void calculateVariation() {
-        Coordinate coordinate1 = new Coordinate(File.A, Rank.ONE);
-        Coordinate coordinate2 = new Coordinate(File.B, Rank.TWO);
+        Coordinate coordinate1 = Coordinate.of(File.A, Rank.ONE);
+        Coordinate coordinate2 = Coordinate.of(File.B, Rank.THREE);
         Assertions.assertThat(coordinate1.calculateVariation(coordinate2))
-                .isEqualTo(new Variation(1, 1));
+                .isEqualTo(new Variation(1, 2));
     }
 }
