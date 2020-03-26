a5ea1b0a6b32e7bffa4751e97bf74a6d0f3a3692
Sony <sonypark0204@gmail.com>
2020-03-26 15:32:01 +0900

    fix: 인덱스 수정

--- src/main/java/chess/domain/game/ChessGame.java
+++ src/main/java/chess/domain/game/ChessGame.java
@@ -5,42 +5,42 @@ import chess.domain.piece.Position;
 import chess.domain.state.State;
 
 public class ChessGame {
-	private State state;
+    private State state;
 
-	public ChessGame(State state) {
-		this.state = state;
-	}
+    public ChessGame(State state) {
+        this.state = state;
+    }
 
-	public void command(String input) {
-		String[] tokens = input.split(" ");
-		if (tokens[0].equals("start")) {
-			start();
-		}
-		if (tokens[0].equals("end")) {
-			end();
-		}
-		if (tokens[0].equals("move")) {
-			move(Position.from(tokens[1]), Position.from(tokens[1]));
-		}
-	}
+    public void command(String input) {
+        String[] tokens = input.split(" ");
+        if (tokens[0].equals("start")) {
+            start();
+        }
+        if (tokens[0].equals("end")) {
+            end();
+        }
+        if (tokens[0].equals("move")) {
+            move(Position.from(tokens[1]), Position.from(tokens[2]));
+        }
+    }
 
-	public void start() {
-		state = state.start();
-	}
+    public void start() {
+        state = state.start();
+    }
 
-	public void move(Position source, Position target) {
-		state = state.move(source, target);
-	}
+    public void move(Position source, Position target) {
+        state = state.move(source, target);
+    }
 
-	public void end() {
-		state = state.end();
-	}
+    public void end() {
+        state = state.end();
+    }
 
-	public Board board() {
-		return state.board();
-	}
+    public Board board() {
+        return state.board();
+    }
 
-	public boolean isFinished() {
-		return state.isFinished();
-	}
+    public boolean isFinished() {
+        return state.isFinished();
+    }
 }
