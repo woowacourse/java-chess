8e72b3ab5d6ca224dc953dadf7c09da72c362da4
soring <jnsorn@gmail.com>
2020-03-26 12:13:43 +0900

    feat : Rook -  목적지가 움직일 수 있는 방향인지 확인

--- src/main/java/domain/piece/Rook.java
+++ src/main/java/domain/piece/Rook.java
@@ -9,4 +9,12 @@ public class Rook extends Piece {
 	public Rook(Position position, Team team) {
 		super(position, team);
 	}
+
+	@Override
+	protected boolean validDirection(int rowGap, int columnGap) {
+		int absRowGap = Math.abs(rowGap);
+		int absColumnGap = Math.abs(columnGap);
+		return ((absRowGap > 0 && absRowGap < 8) && absColumnGap == 0) || ((absColumnGap > 0 && absColumnGap < 8)
+			&& absRowGap == 0);
+	}
 }
