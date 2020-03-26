3ca7d4cd751d3affc2ad633e82a11cbddf6d997f
Sony <sonypark0204@gmail.com>
2020-03-26 15:25:01 +0900

    feat: Rook 이동 기능 구현

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -3,17 +3,17 @@ package chess.domain.piece;
 import java.util.List;
 
 public class Rook extends Piece {
-	public Rook(Position position, Color color) {
-		super(position, color);
-	}
+    public Rook(Position position, Color color) {
+        super(position, color);
+    }
 
-	@Override
-	protected List<Direction> movableDirections(Piece piece, Direction direction) {
-		return null;
-	}
+    @Override
+    protected List<Direction> movableDirections(Piece piece, Direction direction) {
+        return Direction.linearDirection();
+    }
 
-	@Override
-	protected Direction findDirection(int x, int y) {
-		return null;
-	}
+    @Override
+    protected Direction findDirection(int x, int y) {
+        return Direction.ofLinear(x, y);
+    }
 }
