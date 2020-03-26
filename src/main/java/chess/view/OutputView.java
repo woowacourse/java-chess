af01c7f67abb915bbbf7453c85aae8789a519b95
ordinCode <ysh1346@naver.com>
2020-03-26 23:48:32 +0900

    feat : 승자 출력 추가

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -6,8 +6,11 @@ import chess.domain.chessboard.ChessBoard;
 
 public class OutputView {
 	private static final String EMPTY_MARK = ".";
-	private static final String STRING_FORMAT_PRINT_SCORE = "\n블랙팀 점수 : %.1f, 화이트팀 점수 : %.1f";
 	private static final String MESSAGE_END_GAME = "\n게임을 종료 합니다";
+	private static final String BLACK = "black";
+	private static final String WHITE = "white";
+	private static final String STRING_FORMAT_PRINT_SCORE = "\n블랙팀 점수 : %.1f, 화이트팀 점수 : %.1f";
+	private static final String STRING_FORMAT_PRINT_WINNER = "%s 팀이 이겼습니다.";
 
 	public static void printChessBoard(ChessBoard chessBoard) {
 		for (Position position : chessBoard.getChessBoard()) {
@@ -33,6 +36,11 @@ public class OutputView {
 
 	public static void printGameResult(double blackTeamScore, double whiteTeamScore) {
 		System.out.println(String.format(STRING_FORMAT_PRINT_SCORE, blackTeamScore, whiteTeamScore));
+		if (blackTeamScore > whiteTeamScore) {
+			System.out.println(String.format(STRING_FORMAT_PRINT_WINNER, BLACK));
+			return;
+		}
+		System.out.println(String.format(STRING_FORMAT_PRINT_WINNER, WHITE));
 	}
 
 	public static void printGameEndMessage() {
