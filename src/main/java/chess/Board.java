8ebd0925975c4ee6dd047733e37ad10af6f749ea
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-03-26 14:12:37 +0900

    feat: 상대 말을 잡는 로직 생성

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,84 +1,86 @@
 package chess;
 
-import static chess.piece.Team.*;
-import static chess.position.File.*;
-import static chess.position.Rank.*;
+import chess.piece.*;
+import chess.position.File;
+import chess.position.Position;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import chess.piece.Bishop;
-import chess.piece.King;
-import chess.piece.Knight;
-import chess.piece.Pawn;
-import chess.piece.Piece;
-import chess.piece.Queen;
-import chess.piece.Rook;
-import chess.position.File;
-import chess.position.Position;
+import static chess.piece.Team.BLACK;
+import static chess.piece.Team.WHITE;
+import static chess.position.File.*;
+import static chess.position.Rank.*;
 
 public class Board {
-	private final Map<Position, Piece> pieces;
+    private final Map<Position, Piece> pieces;
 
-	public Board(Map<Position, Piece> pieces) {
-		this.pieces = pieces;
+    public Board(Map<Position, Piece> pieces) {
+        this.pieces = pieces;
+    }
+
+    public static Board createInitialBoard() {
+        return new Board(getInitialPieces());
+    }
+
+    private static Map<Position, Piece> getInitialPieces() {
+        Map<Position, Piece> pieces = new HashMap<>();
+        pieces.put(Position.of(A, EIGHT), new Rook(BLACK));
+        pieces.put(Position.of(B, EIGHT), new Knight(BLACK));
+        pieces.put(Position.of(C, EIGHT), new Bishop(BLACK));
+        pieces.put(Position.of(D, EIGHT), new Queen(BLACK));
+        pieces.put(Position.of(E, EIGHT), new King(BLACK));
+        pieces.put(Position.of(F, EIGHT), new Bishop(BLACK));
+        pieces.put(Position.of(G, EIGHT), new Knight(BLACK));
+        pieces.put(Position.of(H, EIGHT), new Rook(BLACK));
+        for (File value : File.values()) {
+            pieces.put(Position.of(value, SEVEN), new Pawn(BLACK));
+        }
+
+        pieces.put(Position.of(A, ONE), new Rook(WHITE));
+        pieces.put(Position.of(B, ONE), new Knight(WHITE));
+        pieces.put(Position.of(C, ONE), new Bishop(WHITE));
+        pieces.put(Position.of(D, ONE), new Queen(WHITE));
+        pieces.put(Position.of(E, ONE), new King(WHITE));
+        pieces.put(Position.of(F, ONE), new Bishop(WHITE));
+        pieces.put(Position.of(G, ONE), new Knight(WHITE));
+        pieces.put(Position.of(H, ONE), new Rook(WHITE));
+        for (File value : File.values()) {
+            pieces.put(Position.of(value, TWO), new Pawn(WHITE));
+        }
+        return pieces;
+    }
+
+    public void move(Position from, Position to) {
+        Piece piece = pieces.get(from);
+        List<Position> trace = piece.findReachablePositions(from, to);
+        if (isExistAnyPieceAt(trace)) {
+            throw new IllegalArgumentException("해당 위치로 이동할 수 없습니다.");
+        }
+        if (isExistAt(to) && pieces.get(to).isSameTeam(pieces.get(from))) {
+            throw new IllegalArgumentException("본인의 말은 잡을 수 없습니다.");
+        }
+        Piece target = pieces.remove(from);
+        target.updateHasMoved();
+        pieces.put(to, target);
+    }
+
+	public boolean isExistAnyPieceAt(List<Position> traces) {
+        return traces.stream()
+                .anyMatch(pieces::containsKey);
+    }
+
+	private boolean isExistAt(Position position) {
+		return pieces.containsKey(position);
 	}
 
-	public static Board createInitialBoard() {
-		return new Board(getInitialPieces());
-	}
+    public Piece getPiece(Position position) {
+        return pieces.get(position);
+    }
 
-	private static Map<Position, Piece> getInitialPieces() {
-		Map<Position, Piece> pieces = new HashMap<>();
-		pieces.put(Position.of(A, EIGHT), new Rook(BLACK));
-		pieces.put(Position.of(B, EIGHT), new Knight(BLACK));
-		pieces.put(Position.of(C, EIGHT), new Bishop(BLACK));
-		pieces.put(Position.of(D, EIGHT), new Queen(BLACK));
-		pieces.put(Position.of(E, EIGHT), new King(BLACK));
-		pieces.put(Position.of(F, EIGHT), new Bishop(BLACK));
-		pieces.put(Position.of(G, EIGHT), new Knight(BLACK));
-		pieces.put(Position.of(H, EIGHT), new Rook(BLACK));
-		for (File value : File.values()) {
-			pieces.put(Position.of(value, SEVEN), new Pawn(BLACK));
-		}
-
-		pieces.put(Position.of(A, ONE), new Rook(WHITE));
-		pieces.put(Position.of(B, ONE), new Knight(WHITE));
-		pieces.put(Position.of(C, ONE), new Bishop(WHITE));
-		pieces.put(Position.of(D, ONE), new Queen(WHITE));
-		pieces.put(Position.of(E, ONE), new King(WHITE));
-		pieces.put(Position.of(F, ONE), new Bishop(WHITE));
-		pieces.put(Position.of(G, ONE), new Knight(WHITE));
-		pieces.put(Position.of(H, ONE), new Rook(WHITE));
-		for (File value : File.values()) {
-			pieces.put(Position.of(value, TWO), new Pawn(WHITE));
-		}
-		return pieces;
-	}
-
-	public void move(Position from, Position to) {
-		Piece piece = pieces.get(from);
-		List<Position> trace = piece.findReachablePositions(from, to);
-		if (isExistAt(trace)) {
-			throw new IllegalArgumentException("해당 위치로 이동할 수 없습니다.");
-		}
-		Piece target = pieces.remove(from);
-		target.updateHasMoved();
-		pieces.put(to, target);
-	}
-
-	public boolean isExistAt(List<Position> traces) {
-		return traces.stream()
-			.anyMatch(pieces::containsKey);
-	}
-
-	public Piece getPiece(Position position) {
-		return pieces.get(position);
-	}
-
-	public Map<Position, Piece> getPieces() {
-		return Collections.unmodifiableMap(this.pieces);
-	}
+    public Map<Position, Piece> getPieces() {
+        return Collections.unmodifiableMap(this.pieces);
+    }
 }
--- src/main/java/chess/piece/Piece.java
+++ src/main/java/chess/piece/Piece.java
@@ -1,27 +1,31 @@
 package chess.piece;
 
-import java.util.List;
-
 import chess.position.Position;
 
+import java.util.List;
+
 public abstract class Piece {
-	protected final Team team;
+    protected final Team team;
 
-	public Piece(Team team) {
-		this.team = team;
-	}
+    public Piece(Team team) {
+        this.team = team;
+    }
 
-	public String getSymbol() {
-		if (team.isBlack()) {
-			return getInitialCharacter();
-		}
-		return getInitialCharacter().toLowerCase();
-	}
-
-	protected abstract String getInitialCharacter();
-
-	public abstract List<Position> findReachablePositions(Position start, Position end);
+    public String getSymbol() {
+        if (team.isBlack()) {
+            return getInitialCharacter();
+        }
+        return getInitialCharacter().toLowerCase();
+    }
 
 	public void updateHasMoved() {
 	}
+
+	public boolean isSameTeam(Piece piece) {
+		return this.team == piece.team;
+	}
+
+    protected abstract String getInitialCharacter();
+
+    public abstract List<Position> findReachablePositions(Position start, Position end);
 }
