2b1f3b7cc390cbf433f0ae262e59d3e3f5596f6d
Sony <sonypark0204@gmail.com>
2020-03-26 15:21:41 +0900

    feat: 8방향을 리턴하는 기능 추가

--- src/main/java/chess/domain/piece/Direction.java
+++ src/main/java/chess/domain/piece/Direction.java
@@ -1,86 +1,94 @@
 package chess.domain.piece;
 
+import java.sql.Driver;
 import java.util.Arrays;
 import java.util.List;
 
 public enum Direction {
-	NORTH(0, 1),
-	SOUTH(0, -1),
-	WEST(-1, 0),
-	EAST(1, 0),
+    NORTH(0, 1),
+    SOUTH(0, -1),
+    WEST(-1, 0),
+    EAST(1, 0),
 
-	NW(-1, 1),
-	NE(1, 1),
-	SW(-1, -1),
-	SE(1, -1),
+    NW(-1, 1),
+    NE(1, 1),
+    SW(-1, -1),
+    SE(1, -1),
 
-	NNE(1, 2),
-	NNW(-1, 2),
-	SSE(1, -2),
-	SSW(-1, -2),
-	EEN(2, 1),
-	EES(2, -1),
-	WWN(-2, 1),
-	WWS(-2, -1);
+    NNE(1, 2),
+    NNW(-1, 2),
+    SSE(1, -2),
+    SSW(-1, -2),
+    EEN(2, 1),
+    EES(2, -1),
+    WWN(-2, 1),
+    WWS(-2, -1);
 
-	private final int x;
-	private final int y;
+    private final int x;
+    private final int y;
 
-	Direction(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public static Direction of(int x, int y) {
-		return Arrays.stream(values())
-				.filter(direction -> direction.x == x && direction.y == y)
-				.findFirst()
-				.orElseThrow(() -> new IllegalArgumentException("유효하지 않은 방향입니다."));
-	}
+    public static Direction of(int x, int y) {
+        return Arrays.stream(values())
+            .filter(direction -> direction.x == x && direction.y == y)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 방향입니다."));
+    }
 
-	public static Direction ofDiagonal(int x, int y) {
-		if (Math.abs(x) - Math.abs(y) != 0) {
-			throw new IllegalArgumentException("대각선 방향이 아닙니다.");
-		}
-		return of(Integer.compare(x, 0), Integer.compare(y, 0));
-	}
+    public static Direction ofDiagonal(int x, int y) {
+        if (Math.abs(x) - Math.abs(y) != 0) {
+            throw new IllegalArgumentException("대각선 방향이 아닙니다.");
+        }
+        return of(Integer.compare(x, 0), Integer.compare(y, 0));
+    }
 
-	public static Direction ofLinear(int x, int y) {
-		if (Math.abs(x - y) != Math.abs(x + y)) {
-			throw new IllegalArgumentException("직선 방향이 아닙니다.");
-		}
-		return of(Integer.compare(x, 0), Integer.compare(y, 0));
-	}
+    public static Direction ofLinear(int x, int y) {
+        if (Math.abs(x - y) != Math.abs(x + y)) {
+            throw new IllegalArgumentException("직선 방향이 아닙니다.");
+        }
+        return of(Integer.compare(x, 0), Integer.compare(y, 0));
+    }
 
-	public static List<Direction> linearDirection() {
-		return Arrays.asList(NORTH, EAST, SOUTH, WEST);
-	}
+    public static Direction ofEvery(int x, int y) {
+        if (Math.abs(x) - Math.abs(y) != 0 && Math.abs(x - y) != Math.abs(x + y)) {
+            throw new IllegalArgumentException("8방향이 아닙니다.");
+        }
+        return of(Integer.compare(x, 0), Integer.compare(y, 0));
+    }
 
-	public static List<Direction> diagonalDirection() {
-		return Arrays.asList(NE, NW, SE, SW);
-	}
+    public static List<Direction> linearDirection() {
+        return Arrays.asList(NORTH, EAST, SOUTH, WEST);
+    }
 
-	public static List<Direction> everyDirection() {
-		return Arrays.asList(NORTH, EAST, SOUTH, WEST, NE, NW, SE, SW);
-	}
+    public static List<Direction> diagonalDirection() {
+        return Arrays.asList(NE, NW, SE, SW);
+    }
 
-	public static List<Direction> knightDirection() {
-		return Arrays.asList(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS);
-	}
+    public static List<Direction> everyDirection() {
+        return Arrays.asList(NORTH, EAST, SOUTH, WEST, NE, NW, SE, SW);
+    }
 
-	public static List<Direction> whitePawnDirection() {
-		return Arrays.asList(NE, NW, NORTH);
-	}
+    public static List<Direction> knightDirection() {
+        return Arrays.asList(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS);
+    }
 
-	public static List<Direction> blackPawnDirection() {
-		return Arrays.asList(SE, SW, SOUTH);
-	}
+    public static List<Direction> whitePawnDirection() {
+        return Arrays.asList(NE, NW, NORTH);
+    }
 
-	public int getX() {
-		return x;
-	}
+    public static List<Direction> blackPawnDirection() {
+        return Arrays.asList(SE, SW, SOUTH);
+    }
 
-	public int getY() {
-		return y;
-	}
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
--- src/test/java/chess/domain/piece/DirectionTest.java
+++ src/test/java/chess/domain/piece/DirectionTest.java
@@ -11,67 +11,93 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class DirectionTest {
-	@ParameterizedTest
-	@DisplayName("방향을 입력한 경우 정상적으로 반환")
-	@MethodSource("provideAllDirections")
-	void of(Direction actual, Direction expected) {
-		assertThat(actual).isEqualTo(expected);
-	}
+    @ParameterizedTest
+    @DisplayName("방향을 입력한 경우 정상적으로 반환")
+    @MethodSource("provideAllDirections")
+    void of(Direction actual, Direction expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
 
-	@Test
-	@DisplayName("직선 방향이 아닌 경우 예외 발생")
-	void ofLinear_invalid() {
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(1, 1));
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(-1, -1));
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(1, -1));
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(-1, 1));
-	}
+    @Test
+    @DisplayName("직선 방향이 아닌 경우 예외 발생")
+    void ofLinear_invalid() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(1, 1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(-1, -1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(1, -1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofLinear(-1, 1));
+    }
 
-	@Test
-	@DisplayName("직선 방향을 입력한 경우 정상적으로 반환")
-	void ofLinear() {
-		assertThat(Direction.ofLinear(1, 0)).isEqualTo(Direction.EAST);
-		assertThat(Direction.ofLinear(0, 1)).isEqualTo(Direction.NORTH);
-		assertThat(Direction.ofLinear(-1, 0)).isEqualTo(Direction.WEST);
-		assertThat(Direction.ofLinear(0, -1)).isEqualTo(Direction.SOUTH);
-	}
+    @Test
+    @DisplayName("직선 방향을 입력한 경우 정상적으로 반환")
+    void ofLinear() {
+        assertThat(Direction.ofLinear(1, 0)).isEqualTo(Direction.EAST);
+        assertThat(Direction.ofLinear(0, 1)).isEqualTo(Direction.NORTH);
+        assertThat(Direction.ofLinear(-1, 0)).isEqualTo(Direction.WEST);
+        assertThat(Direction.ofLinear(0, -1)).isEqualTo(Direction.SOUTH);
+    }
 
-	@Test
-	@DisplayName("대각선 방향이 아닌 경우 예외 발생")
-	void ofDiagonal_invalid() {
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(1, 0));
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(0, 1));
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(-1, 0));
-		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(0, -1));
-	}
+    @Test
+    @DisplayName("대각선 방향이 아닌 경우 예외 발생")
+    void ofDiagonal_invalid() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(1, 0));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(0, 1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(-1, 0));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofDiagonal(0, -1));
+    }
 
-	@Test
-	@DisplayName("대각선 방향을 입력한 경우 정상적으로 반환")
-	void ofDiagonal() {
-		assertThat(Direction.ofDiagonal(1, 1)).isEqualTo(Direction.NE);
-		assertThat(Direction.ofDiagonal(-1, -1)).isEqualTo(Direction.SW);
-		assertThat(Direction.ofDiagonal(1, -1)).isEqualTo(Direction.SE);
-		assertThat(Direction.ofDiagonal(-1, 1)).isEqualTo(Direction.NW);
-	}
+    @Test
+    @DisplayName("대각선 방향을 입력한 경우 정상적으로 반환")
+    void ofDiagonal() {
+        assertThat(Direction.ofDiagonal(1, 1)).isEqualTo(Direction.NE);
+        assertThat(Direction.ofDiagonal(-1, -1)).isEqualTo(Direction.SW);
+        assertThat(Direction.ofDiagonal(1, -1)).isEqualTo(Direction.SE);
+        assertThat(Direction.ofDiagonal(-1, 1)).isEqualTo(Direction.NW);
+    }
 
-	private static Stream<Arguments> provideAllDirections() {
-		return Stream.of(
-				Arguments.of(Direction.of(0, 1), Direction.NORTH),
-				Arguments.of(Direction.of(0, -1), Direction.SOUTH),
-				Arguments.of(Direction.of(1, 0), Direction.EAST),
-				Arguments.of(Direction.of(-1, 0), Direction.WEST),
-				Arguments.of(Direction.of(1, 1), Direction.NE),
-				Arguments.of(Direction.of(-1, 1), Direction.NW),
-				Arguments.of(Direction.of(1, -1), Direction.SE),
-				Arguments.of(Direction.of(-1, -1), Direction.SW),
-				Arguments.of(Direction.of(1, 2), Direction.NNE),
-				Arguments.of(Direction.of(-1, 2), Direction.NNW),
-				Arguments.of(Direction.of(1, -2), Direction.SSE),
-				Arguments.of(Direction.of(-1, -2), Direction.SSW),
-				Arguments.of(Direction.of(2, 1), Direction.EEN),
-				Arguments.of(Direction.of(2, -1), Direction.EES),
-				Arguments.of(Direction.of(-2, 1), Direction.WWN),
-				Arguments.of(Direction.of(-2, -1), Direction.WWS)
-		);
-	}
+    @Test
+    @DisplayName("8방향을 입력한 경우 정상적으로 반환")
+    void ofEvery() {
+        assertThat(Direction.ofEvery(1, 1)).isEqualTo(Direction.NE);
+        assertThat(Direction.ofEvery(-1, -1)).isEqualTo(Direction.SW);
+        assertThat(Direction.ofEvery(1, -1)).isEqualTo(Direction.SE);
+        assertThat(Direction.ofEvery(-1, 1)).isEqualTo(Direction.NW);
+        assertThat(Direction.ofEvery(1, 0)).isEqualTo(Direction.EAST);
+        assertThat(Direction.ofEvery(0, 1)).isEqualTo(Direction.NORTH);
+        assertThat(Direction.ofEvery(-1, 0)).isEqualTo(Direction.WEST);
+        assertThat(Direction.ofEvery(0, -1)).isEqualTo(Direction.SOUTH);
+    }
+
+    @Test
+    @DisplayName("8방향아 아닌 경우 예외 발생")
+    void ofEvery_invalid() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(1, 2));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(1, -2));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(2, 1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(2, -1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(-1, -2));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(-1, 2));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(-2, -1));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> Direction.ofEvery(-2, 1));
+    }
+
+    private static Stream<Arguments> provideAllDirections() {
+        return Stream.of(
+            Arguments.of(Direction.of(0, 1), Direction.NORTH),
+            Arguments.of(Direction.of(0, -1), Direction.SOUTH),
+            Arguments.of(Direction.of(1, 0), Direction.EAST),
+            Arguments.of(Direction.of(-1, 0), Direction.WEST),
+            Arguments.of(Direction.of(1, 1), Direction.NE),
+            Arguments.of(Direction.of(-1, 1), Direction.NW),
+            Arguments.of(Direction.of(1, -1), Direction.SE),
+            Arguments.of(Direction.of(-1, -1), Direction.SW),
+            Arguments.of(Direction.of(1, 2), Direction.NNE),
+            Arguments.of(Direction.of(-1, 2), Direction.NNW),
+            Arguments.of(Direction.of(1, -2), Direction.SSE),
+            Arguments.of(Direction.of(-1, -2), Direction.SSW),
+            Arguments.of(Direction.of(2, 1), Direction.EEN),
+            Arguments.of(Direction.of(2, -1), Direction.EES),
+            Arguments.of(Direction.of(-2, 1), Direction.WWN),
+            Arguments.of(Direction.of(-2, -1), Direction.WWS)
+        );
+    }
 }
