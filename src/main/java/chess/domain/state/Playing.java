8f5f042347a2b82b17ad022ddf61bc3c3096604e
Sony <sonypark0204@gmail.com>
2020-03-26 15:36:47 +0900

    feat: Playing 상태에 move 기능 추가

--- src/main/java/chess/domain/state/Playing.java
+++ src/main/java/chess/domain/state/Playing.java
@@ -4,35 +4,36 @@ import chess.domain.board.Board;
 import chess.domain.piece.Position;
 
 public class Playing implements State {
-	private Board board;
+    private Board board;
 
-	public Playing(Board board) {
-		this.board = board;
-		board.initialize();
-	}
+    public Playing(Board board) {
+        this.board = board;
+        board.initialize();
+    }
 
-	@Override
-	public State start() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public State start() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public State end() {
-		return new Finished();
-	}
+    @Override
+    public State end() {
+        return new Finished();
+    }
 
-	@Override
-	public State move(Position source, Position target) {
-		return this;
-	}
+    @Override
+    public State move(Position source, Position target) {
+        board.move(source, target);
+        return this;
+    }
 
-	@Override
-	public Board board() {
-		return board;
-	}
+    @Override
+    public Board board() {
+        return board;
+    }
 
-	@Override
-	public boolean isFinished() {
-		return false;
-	}
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
