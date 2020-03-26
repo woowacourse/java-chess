aeb504640f056cc88f78a390b18e970dbe851de6
lalize <printf@kakao.com>
2020-03-26 13:22:34 +0900

    feat: Path 클래스 추가
    말의 이동 가능한 경로를 나타내는 클래스

---
+++ src/test/java/chess/domain/piece/PathTest.java
@@ -0,0 +1,19 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PathTest {
+	@Test
+	@DisplayName("이동 방향으로 정상적인 경로 생성")
+	void next() {
+		Path path = new Path(Position.from("a2"), Position.from("a5"), Direction.NORTH);
+		assertThat(path.hasNext()).isTrue();
+		assertThat(path.next()).isEqualTo(Position.from("a3"));
+		assertThat(path.hasNext()).isTrue();
+		assertThat(path.next()).isEqualTo(Position.from("a4"));
+		assertThat(path.hasNext()).isFalse();
+	}
+}
