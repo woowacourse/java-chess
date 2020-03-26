37c71ac17f15c6613e3d355b117492330afc84e2
soring <jnsorn@gmail.com>
2020-03-26 12:13:55 +0900

    feat : King-  목적지가 움직일 수 있는 방향인지 확인

--- src/main/java/domain/piece/King.java
+++ src/main/java/domain/piece/King.java
@@ -1,12 +1,19 @@
 package domain.piece;
 
+import domain.piece.position.Direction;
 import domain.piece.position.Position;
 import domain.piece.team.Team;
 
-public class King extends Piece{
+public class King extends Piece {
 	private static final String SYMBOL = "k";
 
 	public King(Position position, Team team) {
 		super(position, team);
+	}
+
+	@Override
+	protected boolean validDirection(int rowGap, int columnGap) {
+		return Direction.everyDirection().stream()
+			.anyMatch(direction -> direction.getColumnGap() == columnGap && direction.getRowGap() == rowGap);
 	}
 }
