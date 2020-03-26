8561f70ef17e1caa0869fd518ea765b55eb21f75
moon <mjj920930@gmail.com>
2020-03-27 04:31:38 +0900

    refactor: CommandReader의 메서드에서 cache와 함수형 인터페이스를 활용해서 if문 제거

--- src/main/java/chess/controller/command/CommandReader.java
+++ src/main/java/chess/controller/command/CommandReader.java
@@ -2,23 +2,29 @@ package chess.controller.command;
 
 import utils.StringUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class CommandReader {
+    private static final Map<String, Function<List<String>, Command>> COMMAND_CACHE = new HashMap<>();
+
+    static {
+        COMMAND_CACHE.put("start", splitCommand -> new Start());
+        COMMAND_CACHE.put("move", splitCommand -> new Move(splitCommand.get(1), splitCommand.get(2)));
+        COMMAND_CACHE.put("status", splitCommand -> new Status());
+    }
+    
     public static Command of(String command) {
         List<String> splitCommand = StringUtils.splitIntoList(command);
-        if ("start".equals(splitCommand.get(0))) {
-            return new Start();
+
+        Function<List<String>, Command> commandFunction = COMMAND_CACHE.get(splitCommand.get(0));
+
+        if (commandFunction == null) {
+            throw new IllegalArgumentException("존재하지 않는 명령어입니다.");
         }
 
-        if ("move".equals(splitCommand.get(0))) {
-            return new Move(splitCommand.get(1), splitCommand.get(2));
-        }
-
-        if ("status".equals(splitCommand.get(0))) {
-            return new Status();
-        }
-
-        throw new IllegalArgumentException("존재하지 않는 명령어입니다.");
+        return commandFunction.apply(splitCommand);
     }
 }
