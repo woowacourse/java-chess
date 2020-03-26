a0ceff7935ac05056a4f2bfeea1022621bf6c910
soring <jnsorn@gmail.com>
2020-03-26 12:52:54 +0900

    feat : Pawn - 목적지가 움직일 수 있는 방향인지 확인

--- src/main/java/domain/piece/Pawn.java
+++ src/main/java/domain/piece/Pawn.java
@@ -1,12 +1,28 @@
 package domain.piece;
 
+import java.util.List;
+
+import domain.piece.position.Direction;
 import domain.piece.position.Position;
 import domain.piece.team.Team;
 
-public class Pawn extends Piece{
+public class Pawn extends Piece {
 	private static final String SYMBOL = "p";
 
 	public Pawn(Position position, Team team) {
 		super(position, team);
+	}
+
+	private boolean validateDirectionByTeam(int rowGap, int columnGap, List<Direction> directions) {
+		return directions.stream()
+			.anyMatch(direction -> direction.getColumnGap() == columnGap && direction.getRowGap() == rowGap);
+	}
+
+	@Override
+	protected boolean validDirection(int rowGap, int columnGap) {
+		if (this.team == Team.WHITE) {
+			return validateDirectionByTeam(rowGap, columnGap, Direction.whitePawnDirection());
+		}
+		return validateDirectionByTeam(rowGap, columnGap, Direction.blackPawnDirection());
 	}
 }
--- src/test/java/domain/piece/PawnTest.java
+++ src/test/java/domain/piece/PawnTest.java
@@ -1,9 +1,14 @@
 package domain.piece;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import domain.piece.position.InvalidPositionException;
 import domain.piece.position.Position;
 import domain.piece.team.Team;
 
@@ -12,5 +17,21 @@ public class PawnTest {
 	@Test
 	void constructor_CreatePawn_Success() {
 		Assertions.assertThat(new Pawn(Position.of("b1"), Team.WHITE)).isInstanceOf(Pawn.class);
+	}
+
+	@DisplayName("유효한 목적지가 입력되면 true 반환")
+	@ParameterizedTest
+	@CsvSource({"d2, WHITE, d3", "h4, WHITE, h5", "d7, BLACK, d6", "f3, BLACK, f2", "d3, WHITE, e4", "e6, BLACK, f5"})
+	void canMove_ValidTargetPosition_ReturnTrue(Position sourcePosition, Team team, Position targetPosition) {
+		assertThat(new Pawn(sourcePosition, team).canMove(targetPosition, team)).isTrue();
+	}
+
+	@DisplayName("유효하지 않은 목적지가 입력되면 예외 발생")
+	@ParameterizedTest
+	@CsvSource({"d1, WHITE, b1", "f2, WHITE, g4", "h3, WHITE, g5", "d8, BLACK, c6"})
+	void canMove_InvalidTargetPosition_ExceptionThrown(Position sourcePosition, Team team, Position targetPosition) {
+		assertThatThrownBy(() -> new Pawn(sourcePosition, team).canMove(targetPosition, team))
+			.isInstanceOf(InvalidPositionException.class)
+			.hasMessage(InvalidPositionException.INVALID_TARGET_POSITION);
 	}
 }
