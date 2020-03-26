693f0f0270c77aa18c8b96322eae9083e54dfe22
Sony <sonypark0204@gmail.com>
2020-03-26 15:25:14 +0900

    feat: Queen 이동 기능 구현

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -3,17 +3,17 @@ package chess.domain.piece;
 import java.util.List;
 
 public class Queen extends Piece {
-	public Queen(Position position, Color color) {
-		super(position, color);
-	}
+    public Queen(Position position, Color color) {
+        super(position, color);
+    }
 
-	@Override
-	protected List<Direction> movableDirections(Piece piece, Direction direction) {
-		return null;
-	}
+    @Override
+    protected List<Direction> movableDirections(Piece piece, Direction direction) {
+        return Direction.everyDirection();
+    }
 
-	@Override
-	protected Direction findDirection(int x, int y) {
-		return null;
-	}
+    @Override
+    protected Direction findDirection(int x, int y) {
+        return Direction.ofEvery(x, y);
+    }
 }
--- src/test/java/chess/domain/board/BoardTest.java
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -7,30 +7,43 @@ import org.junit.jupiter.api.Test;
 
 import chess.domain.piece.Pawn;
 import chess.domain.piece.Position;
+import chess.domain.piece.exception.NotMovableException;
 
 public class BoardTest {
-	@Test
-	@DisplayName("체스판이 정상적으로 생성된 경우")
-	void constructor() {
-		assertThat(new Board()).isInstanceOf(Board.class);
-	}
 
-	@Test
-	@DisplayName("체스판 초기화")
-	void initialize() {
-		Board board = new Board();
-		board.initialize();
-		assertThat(board.getRanks().size()).isEqualTo(8);
-	}
+    @Test
+    @DisplayName("체스판이 정상적으로 생성된 경우")
+    void constructor() {
+        assertThat(new Board()).isInstanceOf(Board.class);
+    }
 
-	@Test
-	@DisplayName("체스판 말 위치 이동")
-	void move() {
-		Board board = new Board();
-		board.initialize();
-		Position source = Position.from("a2");
-		Position target = Position.from("a3");
-		board.move(source, target);
-		assertThat(board.getRanks().get(target.getY()).findPiece(target.getX())).isInstanceOf(Pawn.class);
-	}
+    @Test
+    @DisplayName("체스판 초기화")
+    void initialize() {
+        Board board = new Board();
+        board.initialize();
+        assertThat(board.getRanks().size()).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("체스판 말 위치 이동")
+    void move() {
+        Board board = new Board();
+        board.initialize();
+        Position source = Position.from("a2");
+        Position target = Position.from("a3");
+        board.move(source, target);
+        assertThat(board.getRanks().get(target.getY()).findPiece(target.getX())).isInstanceOf(Pawn.class);
+    }
+
+    @Test
+    @DisplayName("체스판 말 이동 경로에 장애물이 존재하는 경우 예외 발생")
+    void move_invalid_path_by_obstacle() {
+        Board board = new Board();
+        board.initialize();
+        Position source = Position.from("d1");
+        Position target = Position.from("d7");
+        assertThatExceptionOfType(NotMovableException.class).isThrownBy(
+            () -> board.move(source, target));
+    }
 }
