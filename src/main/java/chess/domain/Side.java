83d611a0744695eff44ad2c9111a8c394255e410
begaonnuri <turtlenam0928@gmail.com>
2020-03-26 17:55:26 +0900

    feat: Side 클래스 변경
    - Side에서 턴을 변경하는 기능 추가

--- src/main/java/chess/domain/Side.java
+++ src/main/java/chess/domain/Side.java
@@ -1,19 +1,45 @@
 package chess.domain;
 
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
 import chess.domain.position.Position;
 import chess.domain.position.Row;
 
 public enum Side {
-	BLACK(Row.TWO),
-	WHITE(Row.SEVEN);
+	BLACK(Row.SEVEN, Row.EIGHT, (source, target) -> source.compareToRow(target) > 0),
+	WHITE(Row.TWO, Row.ONE, (source, target) -> source.compareToRow(target) < 0);
 
 	private Row initPawnRow;
+	private Row initNobleRow;
+	private BiPredicate<Position, Position> attackForwardFilter;
 
-	Side(Row initPawnRow) {
+	Side(Row initPawnRow, Row initNobleRow, BiPredicate<Position, Position> attackForwardFilter) {
 		this.initPawnRow = initPawnRow;
+		this.initNobleRow = initNobleRow;
+		this.attackForwardFilter = attackForwardFilter;
 	}
 
-	public boolean isInitPawnRow(Position position){
+	public boolean isInitPawnRow(Position position) {
 		return position.isSameRow(initPawnRow);
+	}
+
+	public boolean isAttackForward(Position source, Position target) {
+		return attackForwardFilter.test(source, target);
+	}
+
+	public Side reverse() {
+		return Arrays.stream(values())
+				.filter(side -> side != this)
+				.findAny()
+				.orElseThrow(AssertionError::new);
+	}
+
+	public Row getInitPawnRow() {
+		return initPawnRow;
+	}
+
+	public Row getInitNobleRow() {
+		return initNobleRow;
 	}
 }
