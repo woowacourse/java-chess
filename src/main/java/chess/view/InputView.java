f27754f6e50c3fb3ee9081d2dffbe456a572ec2b
ordinCode <ysh1346@naver.com>
2020-03-26 20:46:56 +0900

    style

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class InputView {
 	private static final String INPUT_MESSAGE_GAME_START = "체스 게임을 시작합니다.\n게임 시작은 start, 종료 end 명령을 입력하세요";
 	private static final String INPUT_MESSAGE_MOVE_COMMAND = "\n게임 이동 : move source위치 tartget위치 - 예 : move b2 b3";
-	public static final String START_COMMAND = "start";
 	private static final String ERROR_MESSAGE_INPUT_GAME_START = "start 혹은 end 를 입력하세요";
+	public static final String START_COMMAND = "start";
 	private static final String END_COMMAND = "end";
 
 	private static final Scanner scanner = new Scanner(System.in);
