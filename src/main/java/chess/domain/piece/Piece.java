a9549cdc5f6bac9acd81db856520d1f5045abc00
Sony <sonypark0204@gmail.com>
2020-03-26 15:04:59 +0900

    fix: 거리를 포지션으로 계산해서 발생하는 오류 해결

--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -5,47 +5,48 @@ import java.util.List;
 import chess.domain.piece.exception.NotMovableException;
 
 public abstract class Piece {
-	private Position position;
-	private Color color;
+    private Position position;
+    private Color color;
 
-	public Piece(Position position, Color color) {
-		this.position = position;
-		this.color = color;
-	}
+    public Piece(Position position, Color color) {
+        this.position = position;
+        this.color = color;
+    }
 
-	public void move(Position position) {
-		this.position = position;
-	}
+    public void move(Position position) {
+        this.position = position;
+    }
 
-	public final boolean isBlack() {
-		return color.isBlack();
-	}
+    public final boolean isBlack() {
+        return color.isBlack();
+    }
 
-	public final Path pathTo(Piece piece) {
-		validateColor(piece);
-		Position distance = piece.position.subtract(position);
-		Direction direction = findDirection(distance.getX(), distance.getY());
-		validateDirections(direction, movableDirections(piece, direction));
-		return new Path(position, piece.position, direction);
-	}
+    public final Path pathTo(Piece piece) {
+        validateColor(piece);
+        int dx = piece.position.getX() - position.getX();
+        int dy = piece.position.getY() - position.getY();
+        Direction direction = findDirection(dx, dy);
+        validateDirections(direction, movableDirections(piece, direction));
+        return new Path(position, piece.position, direction);
+    }
 
-	private void validateColor(Piece piece) {
-		if (piece.isSameColor(this)) {
-			throw new NotMovableException("목표 위치에 같은 색상의 말이 존재하여 이동할 수 없습니다.");
-		}
-	}
+    private void validateColor(Piece piece) {
+        if (piece.isSameColor(this)) {
+            throw new NotMovableException("목표 위치에 같은 색상의 말이 존재하여 이동할 수 없습니다.");
+        }
+    }
 
-	private void validateDirections(Direction direction, List<Direction> directions) {
-		if (!directions.contains(direction)) {
-			throw new NotMovableException();
-		}
-	}
+    private void validateDirections(Direction direction, List<Direction> directions) {
+        if (!directions.contains(direction)) {
+            throw new NotMovableException();
+        }
+    }
 
-	public final boolean isSameColor(Piece piece) {
-		return color == piece.color;
-	}
+    public final boolean isSameColor(Piece piece) {
+        return color == piece.color;
+    }
 
-	protected abstract List<Direction> movableDirections(Piece piece, Direction direction);
+    protected abstract List<Direction> movableDirections(Piece piece, Direction direction);
 
-	protected abstract Direction findDirection(int x, int y);
+    protected abstract Direction findDirection(int x, int y);
 }
