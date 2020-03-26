6f922f799dfc88d34020ec0a3db39cb0649d55b2
begaonnuri <turtlenam0928@gmail.com>
2020-03-26 17:55:48 +0900

    refactor: ChessBoardFactory 클래스 변경
    - Side를 통해 초기화하도록 변경

--- src/main/java/chess/domain/ChessBoardFactory.java
+++ src/main/java/chess/domain/ChessBoardFactory.java
@@ -1,9 +1,9 @@
 package chess.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import chess.domain.piece.Bishop;
 import chess.domain.piece.King;
@@ -12,7 +12,9 @@ import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Queen;
 import chess.domain.piece.Rook;
+import chess.domain.position.Column;
 import chess.domain.position.Position;
+import chess.domain.position.Row;
 
 public class ChessBoardFactory {
 	private ChessBoardFactory() {
@@ -20,29 +22,30 @@ public class ChessBoardFactory {
 
 	public static ChessBoard create() {
 		List<Piece> chessBoard = new ArrayList<>();
-		chessBoard.addAll(createNoble(Side.BLACK, 1));
-		chessBoard.addAll(createPawn(Side.BLACK, 2));
-		chessBoard.addAll(createPawn(Side.WHITE, 7));
-		chessBoard.addAll(createNoble(Side.WHITE, 8));
+		chessBoard.addAll(createNoble(Side.BLACK));
+		chessBoard.addAll(createPawn(Side.BLACK));
+		chessBoard.addAll(createNoble(Side.WHITE));
+		chessBoard.addAll(createPawn(Side.WHITE));
 		return new ChessBoard(chessBoard);
 	}
 
-	public static List<Piece> createPawn(Side side, int row) {
-		return IntStream.rangeClosed(1, 8)
-				.mapToObj(i -> new Pawn(side, new Position(i, row)))
+	public static List<Piece> createPawn(Side side) {
+		return Arrays.stream(Column.values())
+				.map(col -> new Pawn(side, new Position(col, side.getInitPawnRow())))
 				.collect(Collectors.toList());
 	}
 
-	public static List<Piece> createNoble(Side side, int row) {
-		List<Piece> nobleLine = new ArrayList<>();
-		nobleLine.add(new Rook(side, new Position('a', row)));
-		nobleLine.add(new Knight(side, new Position('b', row)));
-		nobleLine.add(new Bishop(side, new Position('c', row)));
-		nobleLine.add(new Queen(side, new Position('d', row)));
-		nobleLine.add(new King(side, new Position('e', row)));
-		nobleLine.add(new Bishop(side, new Position('f', row)));
-		nobleLine.add(new Knight(side, new Position('g', row)));
-		nobleLine.add(new Rook(side, new Position('h', row)));
-		return nobleLine;
+	public static List<Piece> createNoble(Side side) {
+		Row row = side.getInitNobleRow();
+		return Arrays.asList(
+				new Rook(side, new Position("a", row)),
+				new Knight(side, new Position("b", row)),
+				new Bishop(side, new Position("c", row)),
+				new Queen(side, new Position("d", row)),
+				new King(side, new Position("e", row)),
+				new Bishop(side, new Position("f", row)),
+				new Knight(side, new Position("g", row)),
+				new Rook(side, new Position("h", row))
+		);
 	}
 }
