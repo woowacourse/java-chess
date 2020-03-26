92d803411993a9aee35b9ee4799f3bc3ecddd236
junk-sound <wom2277@naver.com>
2020-03-26 20:24:14 +0900

    feat: knight

---
+++ src/main/java/chess/domain/Piece/knight/Knight.java
@@ -0,0 +1,23 @@
+package chess.domain.Piece.knight;
+
+import chess.domain.Board;
+import chess.domain.Piece.Piece;
+import chess.domain.Piece.state.Running;
+import chess.domain.Piece.team.Team;
+import chess.domain.position.Position;
+
+public class Knight extends Running {
+    protected Knight(Position position, Team team) {
+        super(position, team);
+    }
+
+    @Override
+    protected boolean canNotMove(Position to, Board board) {
+        return false;
+    }
+
+    @Override
+    public Piece move(Position to, Board board) {
+        return new Knight(to, team);
+    }
+}
