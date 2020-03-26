609fd867488139fe5c8df659de0a9b795f49fe06
Sony <sonypark0204@gmail.com>
2020-03-26 15:24:29 +0900

    feat: King 이동 기능 구현

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -3,17 +3,17 @@ package chess.domain.piece;
 import java.util.List;
 
 public class King extends Piece {
-	public King(Position position, Color color) {
-		super(position, color);
-	}
+    public King(Position position, Color color) {
+        super(position, color);
+    }
 
-	@Override
-	protected List<Direction> movableDirections(Piece piece, Direction direction) {
-		return null;
-	}
+    @Override
+    protected List<Direction> movableDirections(Piece piece, Direction direction) {
+        return Direction.everyDirection();
+    }
 
-	@Override
-	protected Direction findDirection(int x, int y) {
-		return null;
-	}
+    @Override
+    protected Direction findDirection(int x, int y) {
+        return Direction.of(x, y);
+    }
 }
--- src/test/java/chess/domain/piece/KingTest.java
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -4,11 +4,30 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class KingTest {
-	@Test
-	@DisplayName("킹 생성")
-	void constructor() {
-		assertThat(new King(Position.from("e1"), Color.WHITE)).isInstanceOf(King.class);
-	}
+    @Test
+    @DisplayName("킹 생성")
+    void constructor() {
+        assertThat(new King(Position.from("e1"), Color.WHITE)).isInstanceOf(King.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("킹의 이동 가능한 경로가 존재")
+	@CsvSource(value = {"e1,d1", "e1,f1", "e1,f2", "e1,d2", "e8,e7","d7,d8"})
+    void pathTo(String source, String target) {
+        Piece piece = new King(Position.from(source), Color.WHITE);
+        assertThat(piece.pathTo(new Blank(Position.from(target)))).isInstanceOf(Path.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("킹의 이동 가능한 경로가 아닌 경우 예외 발생")
+    @CsvSource(value = {"e1,e3", "e1,b2", "e8,e6", "e8,c8"})
+    void pathTo_invalid_direction(String source, String target) {
+        Piece piece = new King(Position.from(source), Color.WHITE);
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(
+            () -> piece.pathTo(new Blank(Position.from(target))));
+    }
 }
