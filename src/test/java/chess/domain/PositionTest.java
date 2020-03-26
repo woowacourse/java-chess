0955cd665dc2bbf3bf01f71e609821764fdacd91
junk-sound <wom2277@naver.com>
2020-03-26 15:49:48 +0900

    feat: of() in Position

--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -3,10 +3,32 @@ package chess.domain;
 import chess.domain.Piece.Distance;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PositionTest {
+
+    @ParameterizedTest
+    @MethodSource({"getValidCasesForOf"})
+    @DisplayName("#of() : should return Position")
+    void ofSucceed(int x, int y) {
+        Position position = Position.of(x, y);
+        assertThat(position).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource({"getInvalidCasesForOf"})
+    @DisplayName("#of() : should throw IllegalArgumentException")
+    void ofThrowException(int x, int y) {
+        assertThatThrownBy(() -> Position.of(x,y))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     @DisplayName("#calculateDistance() : should return Distance between Positions")
@@ -22,5 +44,19 @@ class PositionTest {
         Distance distance = from.calculateDistance(to);
         //then
         assertThat(distance).isEqualTo(new Distance(toX - fromX, toY - fromY));
+    }
+
+    private static Stream<Arguments> getValidCasesForOf() {
+        return Stream.of(
+                Arguments.of(1,1),
+                Arguments.of(8,8)
+        );
+    }
+
+    private static Stream<Arguments> getInvalidCasesForOf() {
+        return Stream.of(
+                Arguments.of(0,0),
+                Arguments.of(9,9)
+        );
     }
 }
