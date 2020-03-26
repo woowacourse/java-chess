88114e81b6970f2c208cde2dd045c621705ba8e7
taejinseok <stj930@gmail.com>
2020-03-26 13:31:23 +0900

    test: Pawn 두칸 이동시 첫이동 여부 확인

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -7,17 +7,37 @@ import chess.position.Position;
 public class Pawn extends Piece {
 	private static final String INITIAL_CHARACTER = "P";
 
+	private boolean hasMoved;
+
 	public Pawn(Team team) {
 		super(team);
+		hasMoved = false;
 	}
 
 	@Override
 	public List<Position> findReachablePositions(Position start, Position end) {
+		if (isNotMovable(start, end) && isNotAbleToMoveDoubleSquare(start, end)) {
+			throw new IllegalArgumentException("해당 위치로 이동할 수 없습니다.");
+		}
+		// 		if(폰이 못움직이는 코스 || 무효한두칸체크())  { 예외  }
 		return null;
+	}
+
+	private boolean isNotMovable(Position start, Position end) {
+		return false;
+	}
+
+	private boolean isNotAbleToMoveDoubleSquare(Position start, Position end) {
+		return (end.getFileNumber() - start.getFileNumber() == 2) && hasMoved;
 	}
 
 	@Override
 	protected String getInitialCharacter() {
 		return INITIAL_CHARACTER;
+	}
+
+	@Override
+	public void updateHasMoved() {
+		this.hasMoved = true;
 	}
 }
--- src/main/java/chess/piece/Piece.java
+++ src/main/java/chess/piece/Piece.java
@@ -5,13 +5,13 @@ import java.util.List;
 import chess.position.Position;
 
 public abstract class Piece {
+	private static final String UNSUPPORTED_UPDATE_FEAT_EXCEPTION_MESSAGE = "지원하지 않는 연산입니다.";
+
 	protected final Team team;
 
 	public Piece(Team team) {
 		this.team = team;
 	}
-
-	public abstract List<Position> findReachablePositions(Position start, Position end);
 
 	public String getSymbol() {
 		if (team.isBlack()) {
@@ -21,4 +21,10 @@ public abstract class Piece {
 	}
 
 	protected abstract String getInitialCharacter();
+
+	public abstract List<Position> findReachablePositions(Position start, Position end);
+
+	public void updateHasMoved() {
+		throw new UnsupportedOperationException(UNSUPPORTED_UPDATE_FEAT_EXCEPTION_MESSAGE);
+	}
 }
