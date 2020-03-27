bf61f63031d9a644c9560a8480f3264a65797b19
giantim <giantim132@gmail.com>
2020-03-27 14:30:57 +0900

    refactor: 테스트 코드 디렉토리 추가

---
+++ src/test/java/chess/domain/strategy/move/BishopMoveStrategyTest.java
@@ -0,0 +1,12 @@
+package chess.domain.strategy.move;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BishopMoveStrategyTest {
+    @DisplayName("비숍의 우 상단 대각 이동")
+    @Test
+    void rightUpTest() {
+
+    }
+}
---
+++ src/test/java/chess/domain/strategy/move/direction/DirectionTest.java
@@ -0,0 +1,12 @@
+package chess.domain.strategy.move.direction;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DirectionTest {
+    @DisplayName("조건에 맞는 방향 반환")
+    @Test
+    void findTest() {
+
+    }
+}
