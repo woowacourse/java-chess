410470804aa8549445f568ed47e571b661da95d7
lalize <printf@kakao.com>
2020-03-27 22:48:44 +0900

    refactor: command 입력 리팩토링

--- src/main/java/chess/controller/ChessGameController.java
+++ src/main/java/chess/controller/ChessGameController.java
@@ -1,35 +1,35 @@
 package chess.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.function.Consumer;
+
 import chess.domain.game.ChessGame;
 import chess.domain.piece.Position;
 import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessGameController {
+	private static final String COMMAND_DELIMITER = " ";
+
 	private ChessGame chessGame;
+	private Map<String, Consumer<StringTokenizer>> commands;
 
 	public ChessGameController(ChessGame chessGame) {
 		this.chessGame = chessGame;
+		this.commands = new HashMap<String, Consumer<StringTokenizer>>() {{
+			put("start", tokenizer -> chessGame.start());
+			put("end", tokenizer -> chessGame.end());
+			put("move", tokenizer -> chessGame.move(Position.from(tokenizer.nextToken()),
+					Position.from(tokenizer.nextToken())));
+			put("status", tokenizer -> chessGame.status());
+		}};
 	}
 
 	public void command(String input) {
-		String[] tokens = input.split(" ");
-		switch (tokens[0]) {
-			case "start":
-				chessGame.start();
-				break;
-			case "end":
-				chessGame.end();
-				break;
-			case "move":
-				chessGame.move(Position.from(tokens[1]), Position.from(tokens[2]));
-				break;
-			case "status":
-				OutputView.printStatus(chessGame.status());
-				break;
-			default:
-				throw new IllegalArgumentException("존재하지 않는 커맨드 입니다.");
-		}
+		StringTokenizer tokenizer = new StringTokenizer(input, COMMAND_DELIMITER);
+		commands.get(tokenizer.nextToken()).accept(tokenizer);
 	}
 
 	public void run() {
