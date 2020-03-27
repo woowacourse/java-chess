f011a2144409d50a13dc47955879104eacf89d82
chws <rebeccachoosir@gmail.com>
2020-03-27 21:41:46 +0900

    refactor: output for문을 stream이용/메서드 분리로 depth 줄임

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -7,6 +7,8 @@ import chess.domain.Square;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class OutputView {
 
@@ -20,20 +22,22 @@ public class OutputView {
 
     public static void printChessBoard(ChessBoard chessBoard) {
         Map<Square, Piece> gameBoard = chessBoard.getChessBoard();
-        for (int rank = 8; rank >= 1; rank--) {
-            for (char file = 'a'; file <= 'h'; file++) {
-                if (gameBoard.containsKey(Square.of(String.valueOf(file) + rank))) {
-                    System.out.print(gameBoard.get(Square.of(String.valueOf(file) + rank)).getLetter());
-                    continue;
-                }
-                System.out.print(".");
+        IntStream.rangeClosed(8, 1).forEach(rank -> printOneRow(gameBoard, rank));
+    }
+
+    private static void printOneRow(Map<Square, Piece> gameBoard, int rank) {
+        for (char file = 'a'; file <= 'h'; file++) {
+            if (gameBoard.containsKey(Square.of(String.valueOf(file) + rank))) {
+                System.out.print(gameBoard.get(Square.of(String.valueOf(file) + rank)).getLetter());
+                continue;
             }
-            System.out.println();
+            System.out.print(".");
         }
+        System.out.println();
     }
 
     public static void printCanNotMove() {
-        System.out.println("못움직여요");
+        System.out.println("움직일 수 없습니다");
     }
 
     public static void printWinner(Color color) {
@@ -41,16 +45,15 @@ public class OutputView {
     }
 
     public static void printScore(Map<Color, Double> teamScore) {
-        for (Color color : teamScore.keySet()) {
-            System.out.println(color.getName() + "의 점수는 " + teamScore.get(color) + "입니다");
-        }
+        teamScore.keySet()
+                .forEach(color -> System.out.println(color.getName() + "의 점수는 " + teamScore.get(color) + "입니다"));
     }
 
     public static void printWinners(List<Color> winners) {
-        System.out.print("최고점은 ");
-        for (Color color : winners) {
-            System.out.print(color.getName() + " ");
-        }
+        System.out.print("우승자는 ");
+        System.out.print(winners.stream()
+                .map(Color::getName)
+                .collect(Collectors.joining(", ")));
         System.out.println("입니다.");
     }
 }
