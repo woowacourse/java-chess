81a9610b96566a897db8217592aa5abd74e859fc
icyMojito <dev.icymojito@gmail.com>
2020-03-28 01:46:07 +0900

    test : 체스말의 색깔에 따라 이름 형태를 변환해주는 메서드 정상작동 확인

---
+++ src/test/java/piece/PieceColorTest.java
@@ -0,0 +1,31 @@
+package piece;
+
+import chess.domain.piece.PieceColor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class PieceColorTest {
+
+    @ParameterizedTest
+    @DisplayName("하얀색 체스말의 이름을 소문자로 반환하는지 확인")
+    @CsvSource(value = {"b,b", "k,k", "n,n", "p,p", "q,q", "r,r"})
+    void ifWhitePieceThenReturnLowerCaseName(String input, String expected) {
+        Assertions.assertThat(PieceColor.WHITE.getPieceName(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("검은색 체스말의 이름을 대문자로 반환하는지 확인")
+    @CsvSource(value = {"b,B", "k,K", "n,N", "p,P", "q,Q", "r,R"})
+    void ifBlackPieceThenReturnUpperCaseName(String input, String expected) {
+        Assertions.assertThat(PieceColor.BLACK.getPieceName(input)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("색이 없는 체스말의 이름을 그대로 반환하는지 확인")
+    void ifNoneColorPieceThenReturnName() {
+        Assertions.assertThat(PieceColor.NONE.getPieceName(".")).isEqualTo(".");
+    }
+}
