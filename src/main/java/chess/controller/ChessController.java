578be48ccecceb8a917b8af1f04ca1ccd2347a9b
toneyparky <qkrtmddhks95@gmail.com>
2020-03-27 10:28:44 +0900

    refactor: 입력받은 OperationType이 if문을 지나간 후 true 반환에서 예외처리로 수정
    ChessController 매직넘버의 상수화, 예외처리문 상수화, 의미없는 반환값 예외처리

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -13,13 +13,17 @@ import static chess.view.InputView.inputOperation;
 public class ChessController {
 
 	private static final String DELIMITER = " ";
+	private static final String INVALID_OPERATION_EXCEPTION_MESSAGE = "잘못된 명령입니다.";
+	private static final int START_POSITION_INDEX = 1;
+	private static final int DESTINATION_POSITION_INDEX = 2;
+	private static final int OPERATIONTYPE_INDEX = 0;
 
 	public void run() {
 		ChessGame chessGame = new ChessGame();
 
 		OutputView.printOperationsFormat();
 		List<String> operations = Arrays.asList(inputOperation().split(DELIMITER));
-		OperationType operationType = OperationType.of(operations.get(0));
+		OperationType operationType = OperationType.of(operations.get(OPERATIONTYPE_INDEX));
 
 		validateFirstOperations(operationType);
 		if (operationType.isEnd()) {
@@ -30,13 +34,13 @@ public class ChessController {
 		do {
 			OutputView.printBoard(chessGame);
 			operations = Arrays.asList(inputOperation().split(DELIMITER));
-			operationType = OperationType.of(operations.get(0));
+			operationType = OperationType.of(operations.get(OPERATIONTYPE_INDEX));
 		} while (operateByType(chessGame, operations, operationType));
 	}
 
 	private void validateFirstOperations(OperationType operationType) {
 		if (!operationType.isStart() && !operationType.isEnd()) {
-			throw new UnsupportedOperationException("잘못된 명령입니다.");
+			throw new UnsupportedOperationException(INVALID_OPERATION_EXCEPTION_MESSAGE);
 		}
 	}
 
@@ -48,7 +52,7 @@ public class ChessController {
 		}
 
 		if (operationType.isMove()) {
-			chessGame.move(Board.of(operations.get(1)), Board.of(operations.get(2)));
+			chessGame.move(Board.of(operations.get(START_POSITION_INDEX)), Board.of(operations.get(DESTINATION_POSITION_INDEX)));
 			return isMoveOperating(chessGame);
 		}
 
@@ -58,9 +62,9 @@ public class ChessController {
 		}
 
 		if (operationType.isStart()) {
-			throw new UnsupportedOperationException("잘못된 명령입니다.");
+			throw new UnsupportedOperationException(INVALID_OPERATION_EXCEPTION_MESSAGE);
 		}
-		return true;
+		throw new UnsupportedOperationException(INVALID_OPERATION_EXCEPTION_MESSAGE);
 	}
 
 	private boolean isMoveOperating(ChessGame chessGame) {
