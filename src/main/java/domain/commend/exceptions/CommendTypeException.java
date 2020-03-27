a07a52952409a2a463a4913c2fa7fcc382e42a21
kuenhwi <kueniyam@naver.com>
2020-03-27 19:52:37 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/main/java/domain/commend/exceptions/CommendTypeException.java
+++
@@ -1,7 +0,0 @@
-package domain.commend.exceptions;
-
-public class CommendTypeException extends RuntimeException {
-    public CommendTypeException(String message) {
-        super(message);
-    }
-}
