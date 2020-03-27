8ba1ff84f50bd9b1bc61dbbae57cc37188394e2c
lalize <printf@kakao.com>
2020-03-27 23:00:17 +0900

    fix: 게임 시작 출력 내용 수정

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -11,8 +11,10 @@ public class OutputView {
 	private static final String STATUS_FORMAT = "WHITE: %.1f BLACK: %.1f - %s진영 승\n";
 
 	public static void printGameStart() {
-		System.out.println("체스 게임을 시작합니다.");
-		System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+		System.out.println("> 체스 게임을 시작합니다.");
+		System.out.println("> 게임 시작 : start");
+		System.out.println("> 게임 종료 : end");
+		System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
 	}
 
 	public static void printBoard(Board board) {
