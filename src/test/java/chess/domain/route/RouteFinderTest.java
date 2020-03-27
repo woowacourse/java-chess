d9e8efb2dbe344d624f6bc71d782378ccf784834
moon <mjj920930@gmail.com>
2020-03-27 23:42:03 +0900

    test: RouteFinder의 테스트 추가

--- src/test/java/chess/domain/route/RouteFinderTest.java
+++ src/test/java/chess/domain/route/RouteFinderTest.java
@@ -3,16 +3,34 @@ package chess.domain.route;
 import chess.domain.Team;
 import chess.domain.piece.PieceType;
 import chess.domain.position.Position;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RouteFinderTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("createPositionAndPieceType")
     void findRoute() {
         Route foundRoute = RouteFinder.findRoute(Position.of("a2"), Position.of("a3"), Team.WHITE, PieceType.PAWN);
 
         assertThat(foundRoute).isEqualTo(Route.emptyRoute());
+    }
+
+    private static Stream<Arguments> createPositionAndPieceType() {
+        return Stream.of(
+                Arguments.of(Position.of("a2"), Position.of("a3"), Team.WHITE, PieceType.PAWN,
+                        new RouteToProceed(new ArrayList<>())),
+                Arguments.of(Position.of("a2"), Position.of("a4"), Team.WHITE, PieceType.PAWN,
+                        new RouteToProceed(Arrays.asList(Position.of("a3")))),
+                Arguments.of(Position.of("a2"), Position.of("b3"), Team.WHITE, PieceType.PAWN,
+                        new RouteToAttack(new ArrayList<>()))
+        );
     }
 }
