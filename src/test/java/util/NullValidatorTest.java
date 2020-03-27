a6401a222e59caaa37ad5ae1b2368519342e6100
icyMojito <dev.icymojito@gmail.com>
2020-03-28 01:55:52 +0900

    test : null 체크 메서드 정상작동 확인

---
+++ src/test/java/util/NullValidatorTest.java
@@ -0,0 +1,20 @@
+package util;
+
+import chess.util.NullValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NullValidatorTest {
+
+    @ParameterizedTest
+    @DisplayName("null이 입력되면 예외가 발생해야 함")
+    @NullSource
+    void inputNullThenReturnException(String input) {
+        assertThatThrownBy(() -> NullValidator.validateNull(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("null이 들어왔습니다.");
+    }
+}
