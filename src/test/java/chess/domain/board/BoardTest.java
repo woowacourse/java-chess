75f51a721fc4b3d4ebd8a506536370e43d22e927
moon <mjj920930@gmail.com>
2020-03-27 17:24:26 +0900

    test: Board에 대한 테스트 추가

---
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -0,0 +1,70 @@
+package chess.domain.board;
+
+import chess.domain.Piece;
+import chess.domain.PieceType;
+import chess.domain.Pieces;
+import chess.domain.Team;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoardTest {
+
+    @ParameterizedTest
+    @MethodSource("createTeamAndPieces")
+    void findPiecesOf(Team team, Pieces expected) {
+        Board board = BoardFactory.createInitially();
+        Pieces pieces = board.findPiecesOf(team);
+
+        assertThat(pieces).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> createTeamAndPieces() {
+        return Stream.of(
+                Arguments.of(Team.BLACK, new Pieces(new HashSet<>(Arrays.asList(
+                        new Piece(Team.BLACK, PieceType.PAWN),
+                        new Piece(Team.BLACK, PieceType.PAWN),
+                        new Piece(Team.BLACK, PieceType.PAWN),
+                        new Piece(Team.BLACK, PieceType.PAWN),
+                        new Piece(Team.BLACK, PieceType.PAWN),
+                        new Piece(Team.BLACK, PieceType.PAWN),
+                        new Piece(Team.BLACK, PieceType.PAWN),
+                        new Piece(Team.BLACK, PieceType.PAWN),
+
+                        new Piece(Team.BLACK, PieceType.KING),
+                        new Piece(Team.BLACK, PieceType.QUEEN),
+                        new Piece(Team.BLACK, PieceType.BISHOP),
+                        new Piece(Team.BLACK, PieceType.BISHOP),
+                        new Piece(Team.BLACK, PieceType.KNIGHT),
+                        new Piece(Team.BLACK, PieceType.KNIGHT),
+                        new Piece(Team.BLACK, PieceType.ROOK),
+                        new Piece(Team.BLACK, PieceType.ROOK)
+                )))),
+                Arguments.of(Team.WHITE, new Pieces(new HashSet<>(Arrays.asList(
+                        new Piece(Team.WHITE, PieceType.PAWN),
+                        new Piece(Team.WHITE, PieceType.PAWN),
+                        new Piece(Team.WHITE, PieceType.PAWN),
+                        new Piece(Team.WHITE, PieceType.PAWN),
+                        new Piece(Team.WHITE, PieceType.PAWN),
+                        new Piece(Team.WHITE, PieceType.PAWN),
+                        new Piece(Team.WHITE, PieceType.PAWN),
+                        new Piece(Team.WHITE, PieceType.PAWN),
+
+                        new Piece(Team.WHITE, PieceType.KING),
+                        new Piece(Team.WHITE, PieceType.QUEEN),
+                        new Piece(Team.WHITE, PieceType.BISHOP),
+                        new Piece(Team.WHITE, PieceType.BISHOP),
+                        new Piece(Team.WHITE, PieceType.KNIGHT),
+                        new Piece(Team.WHITE, PieceType.KNIGHT),
+                        new Piece(Team.WHITE, PieceType.ROOK),
+                        new Piece(Team.WHITE, PieceType.ROOK)
+                ))))
+        );
+    }
+}
