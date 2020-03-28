657e3fd62fbddb115578016f3f915ce42cccc571
icyMojito <dev.icymojito@gmail.com>
2020-03-28 12:36:06 +0900

    refactor : OutputView에서 체스판을 찍는 메서드 내부의 매직 넘버를 상수화

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -7,19 +7,23 @@ import chess.domain.piece.Piece;
 import java.util.Map;
 
 public class OutputView {
+    private static final int MAX_Y_POINT = 8;
+    private static final int MIN_Y_POINT = 1;
+    private static final int MIN_X_POINT = 1;
+    private static final int MAX_X_POINT = 8;
 
     public static void printBoard(Board board) {
         Map<Position, Piece> chessBoard = board.getBoard();
 
-        for (int yPoint = 8; 1 <= yPoint; yPoint--) {
-            for (int xPoint = 1; xPoint <= 8; xPoint++) {
-                if (xPoint == 8) {
+        for (int yPoint = MAX_Y_POINT; yPoint >= MIN_Y_POINT; yPoint--) {
+            for (int xPoint = MIN_X_POINT; xPoint <= MAX_X_POINT; xPoint++) {
+                if (xPoint == MAX_X_POINT) {
                     System.out.println(chessBoard.get(new Position(xPoint, yPoint)).getName());
                 } else {
                     System.out.print(chessBoard.get(new Position(xPoint, yPoint)).getName());
                 }
             }
         }
-
     }
+
 }
