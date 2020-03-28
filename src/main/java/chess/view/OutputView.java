e334ef02c3b38c42f80408e2304ed58b474aa035
toneyparky <qkrtmddhks95@gmail.com>
2020-03-28 21:25:53 +0900

    refactor: String.join을 사용하여 출력하는 문자열 가독성 향상
    OutputView OPERATIONS_MESSAGE 상수 추가

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -10,8 +10,15 @@ import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
 
 public class OutputView {
+	private static final String OPERATIONS_MESSAGE = String.join("\n",
+			"> 체스 게임을 시작합니다. ",
+			"> 게임 시작 : start",
+			"> 게임 종료 : end",
+			"> 게임 이동 : move source위치 target위치 - 예. move b2 b3",
+			"> 점수 확인 : status");
+
 	public static void printOperationsFormat() {
-		System.out.println("> 체스 게임을 시작합니다. \n> 게임 시작 : start\n> 게임 종료 : end\n> 게임 이동 : move source위치 target위치 - 예. move b2 b3\n> 점수 확인 : status");
+		System.out.println(OPERATIONS_MESSAGE);
 	}
 
 	public static void printBoard(ChessGame chessGame) {
