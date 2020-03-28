44f74ea3b457b2ffcf4cdbee6f0ad1998dea5e46
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-03-28 18:31:20 +0900

    refactor : 상수 처리

--- src/main/java/chess/piece/AbstractPawn.java
+++ src/main/java/chess/piece/AbstractPawn.java
@@ -5,6 +5,7 @@ import chess.coordinate.Vector;
 public abstract class AbstractPawn extends Piece {
 
     private static final int PAWN_SCORE = 1;
+    private static final int PAWN_MOVE_RANGE = 1;
 
     public AbstractPawn(final Team team) {
         super(team, PAWN_SCORE);
@@ -18,17 +19,13 @@ public abstract class AbstractPawn extends Piece {
         if (canMoveTwoStep(vector, targetPiece)) {
             return true;
         }
-        if (!isSameDirection(vector, 1)) {
+        if (vector.isRangeUnderAbsolute(PAWN_MOVE_RANGE) && team.isSameDirection(vector.getRankVariation())) {
             return false;
         }
         if (targetPiece.isBlank()) {
             return vector.isStraight();
         }
         return vector.isDiagonal();
-    }
-
-    protected boolean isSameDirection(final Vector vector, int value) {
-        return vector.isRangeUnderAbsolute(value) && team.isSameDirection(vector.getRankVariation());
     }
 
     protected abstract boolean canMoveTwoStep(final Vector vector, final Piece targetPiece);
