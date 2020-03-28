dd1b13ee25b394836a98dd34356f9ce4e27cdedb
Andy H. Jung <commaniakr@gmail.com>
2020-03-28 21:16:41 +0900

    refactor: PieceFactory 중복 제거

--- src/main/java/chess/domain/piece/PieceFactory.java
+++ src/main/java/chess/domain/piece/PieceFactory.java
@@ -1,22 +1,24 @@
 package chess.domain.piece;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import chess.domain.Pieces;
 import chess.domain.Position;
 
 public class PieceFactory {
+	private static final int team1BackRank = 1;
+	private static final int team1PawnsRank = 2;
+	private static final int team2BackRank = 8;
+	private static final int team2PawnsRank = 7;
+	private static List<String> PawnPositions = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+
 	private static PieceFactory instance;
-	private static List<String> team1PawnPositions = Arrays.asList("a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2");
-	private static List<String> team2PawnPositions = Arrays.asList("a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7");
 
 	private List<Piece> pieces = new ArrayList<>();
 
 	private PieceFactory() {
-		initializeTeam1(Team.WHITE);
-		initializeTeam2(Team.BLACK);
+		initializeTeam(team1BackRank, team1PawnsRank, Team.WHITE);
+		initializeTeam(team2BackRank, team2PawnsRank, Team.BLACK);
 	}
 
 	public static PieceFactory getInstance() {
@@ -30,31 +32,17 @@ public class PieceFactory {
 		return new Pieces(getInstance().pieces);
 	}
 
-	private void initializeTeam1(Team team) {
-		pieces.add(new King(new Position("e1"), team));
-		pieces.add(new Queen(new Position("d1"), team));
-		pieces.add(new Bishop(new Position("c1"), team));
-		pieces.add(new Bishop(new Position("f1"), team));
-		pieces.add(new Knight(new Position("b1"), team));
-		pieces.add(new Knight(new Position("g1"), team));
-		pieces.add(new Rook(new Position("a1"), team));
-		pieces.add(new Rook(new Position("h1"), team));
-		for (String pawnPosition : team1PawnPositions) {
-			pieces.add(new Pawn(new Position(pawnPosition), team));
-		}
-	}
-
-	private void initializeTeam2(Team team) {
-		pieces.add(new King(new Position("e8"), team));
-		pieces.add(new Queen(new Position("d8"), team));
-		pieces.add(new Bishop(new Position("c8"), team));
-		pieces.add(new Bishop(new Position("f8"), team));
-		pieces.add(new Knight(new Position("b8"), team));
-		pieces.add(new Knight(new Position("g8"), team));
-		pieces.add(new Rook(new Position("a8"), team));
-		pieces.add(new Rook(new Position("h8"), team));
-		for (String pawnPosition : team2PawnPositions) {
-			pieces.add(new Pawn(new Position(pawnPosition), team));
+	private void initializeTeam(int backRank, int pawnRank, Team team) {
+		pieces.add(new King(new Position("e"+backRank), team));
+		pieces.add(new Queen(new Position("d"+backRank), team));
+		pieces.add(new Bishop(new Position("c"+backRank), team));
+		pieces.add(new Bishop(new Position("f"+backRank), team));
+		pieces.add(new Knight(new Position("b"+backRank), team));
+		pieces.add(new Knight(new Position("g"+backRank), team));
+		pieces.add(new Rook(new Position("a"+backRank), team));
+		pieces.add(new Rook(new Position("h"+backRank), team));
+		for (String pawnPosition : PawnPositions) {
+			pieces.add(new Pawn(new Position(pawnPosition+pawnRank), team));
 		}
 	}
 }
