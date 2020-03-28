d47a933279509a4b104d4f25c95471208951fea1
icyMojito <dev.icymojito@gmail.com>
2020-03-28 12:35:12 +0900

    feat : 출력 요구사항에 맞게 InputView 클래스의 메서드 수정과 추가

--- src/main/java/chess/domain/service/GameStartService.java
+++ src/main/java/chess/domain/service/GameStartService.java
@@ -1,6 +1,8 @@
 package chess.domain.service;
 
+import chess.domain.MoveCommand;
 import chess.domain.board.Board;
+import chess.view.InputView;
 import chess.view.OutputView;
 
 public class GameStartService implements GameService {
@@ -10,29 +12,8 @@ public class GameStartService implements GameService {
         Board board = new Board();
         OutputView.printBoard(board);
 
-//        while (true) {
-//            MoveCommand moveCommand = new MoveCommand(InputView.requestMoveCommand(), board);
-//            Position sourcePosition = moveCommand.getSourcePosition();
-//            Position targetPosition = moveCommand.getTargetPosition();
-//
-//            Piece selectedPiece = board.getPieceBy(moveCommand.getSourcePosition());
-//
-//            List<Position> pathCell = PieceType.of(selectedPiece.getName()).getPathCells(sourcePosition,
-//            targetPosition);
-//
-//            for (Position position : pathCell) {
-//                System.out.println(position.toString());
-//            }
-//
-//            if (board.isMovable(pathCell, selectedPiece)) {
-//                selectedPiece.moveTo(targetPosition);
-//                board.getCellBy(targetPosition).setPiece(selectedPiece);
-//                board.getCellBy(sourcePosition).clear();
-//            } else {
-//                throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
-//            }
-//
-//            OutputView.printBoard(board);
-//        }
+        while (true) {
+            MoveCommand moveCommand = new MoveCommand(InputView.requestMoveCommand());
+        }
     }
 }
--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -6,8 +6,14 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String requestCommand() {
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+        return scanner.nextLine();
+    }
+
+    public static String requestMoveCommand() {
         return scanner.nextLine();
     }
 }
