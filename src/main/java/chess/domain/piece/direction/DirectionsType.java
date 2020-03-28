b09323acef6ed2b7409861d9e865eb284008233c
icyMojito <dev.icymojito@gmail.com>
2020-03-29 02:06:48 +0900

    refactor : 사용하지 않는 DirectionsType 클래스 삭제

--- src/main/java/chess/domain/piece/direction/DirectionsType.java
+++ src/main/java/chess/domain/piece/direction/DirectionsType.java
@@ -8,8 +8,6 @@ import static chess.domain.piece.direction.Direction.*;
 public enum DirectionsType {
     LINEAR(Arrays.asList(NORTH, EAST, SOUTH, WEST)),
     DIAGONAL(Arrays.asList(NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST)),
-    EVERY(Arrays.asList(NORTH, EAST, SOUTH, WEST, NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST)),
-    KNIGHT(Arrays.asList(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS)),
     WHITE_PAWN(Arrays.asList(NORTH, NORTHEAST, NORTHWEST)),
     BLACK_PAWN(Arrays.asList(SOUTH, SOUTHEAST, SOUTHWEST));
 
