7459a1efd2ee4ea8d2d2a66d3f7cafd930b1a1c7
chws <rebeccachoosir@gmail.com>
2020-03-28 23:10:13 +0900

    feat: abstract class Piece를 상속하는 Rook 클래스 및 테스트 작성

---
+++ src/main/java/chess/domain/piece/Rook.java
@@ -0,0 +1,89 @@
+package chess.domain.piece;
+
+import chess.domain.Color;
+import chess.domain.Square;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Rook extends Piece {
+    private final static Map<Color, Rook> CACHE = new HashMap<>();
+    private final static String NAME_BLACK = "R";
+    private final static String NAME_WHITE = "r";
+
+    static {
+        Stream.of(Color.values())
+                .forEach(Rook::putIntoCache);
+    }
+
+    private static void putIntoCache(Color color) {
+        String name = NAME_BLACK;
+        if (color == Color.WHITE) {
+            name = NAME_WHITE;
+        }
+        CACHE.putIfAbsent(color, new Rook(color, name, 0));
+    }
+
+    public static Rook of(Color color) {
+        validateInput(color);
+        return CACHE.get(color);
+    }
+
+    private static void validateInput(Color color) {
+        if (Objects.isNull(color)) {
+            throw new IllegalArgumentException("잘못된 입력입니다");
+        }
+    }
+
+    private Rook(Color color, String name, double score) {
+        super(color, null, name, score);
+    }
+
+    @Override
+    public Set<Square> calculateScope(Square square) {
+        Set<Square> availableSquares = new HashSet<>();
+        for (int index = -7; index < 8; index++) {
+            availableSquares.add(addIfInBoundary(square, index, 0));
+            availableSquares.add(addIfInBoundary(square, 0, index));
+        }
+        availableSquares.remove(square);
+        return availableSquares;
+    }
+
+    @Override
+    public Set<Square> calculateMoveBoundary(Square square, Map<Square, Piece> board) {
+        validateNotNull(square, board);
+        Set<Square> squares = calculateScope(square);
+        Set<Square> squaresIter = calculateScope(square);
+        for (Square s : squaresIter) {
+            if (board.containsKey(s)) {
+                int fileDifference = s.getFile() - square.getFile();
+                int rankDifference = s.getRank() - square.getRank();
+                if (fileDifference == 0 && rankDifference > 0) {
+                    Set<Square> squaresToRemove = findSquaresToRemove(s, 0, 1);
+                    squares.removeAll(squaresToRemove);
+                }
+
+                if (fileDifference > 0 && rankDifference == 0) {
+                    Set<Square> squaresToRemove = findSquaresToRemove(s, 1, 0);
+                    squares.removeAll(squaresToRemove);
+                }
+
+                if (fileDifference < 0 && rankDifference == 0) {
+                    Set<Square> squaresToRemove = findSquaresToRemove(s, -1, 0);
+                    squares.removeAll(squaresToRemove);
+                }
+
+                if (fileDifference == 0 && rankDifference < 0) {
+                    Set<Square> squaresToRemove = findSquaresToRemove(s, 0, -1);
+                    squares.removeAll(squaresToRemove);
+                }
+
+                if (board.get(s).color == color) {
+                    squares.remove(s);
+                }
+            }
+        }
+        return squares;
+    }
+}
--- src/test/java/chess/domain/PieceTest.java
+++ src/test/java/chess/domain/PieceTest.java
@@ -4,12 +4,6 @@ import chess.domain.piece.Piece;
 import chess.domain.piece.PieceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -28,38 +22,6 @@ public class PieceTest {
         assertThatThrownBy(() -> PieceImpl.of(null, null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("잘못된 입력입니다");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"d1", "d3", "d4", "d5", "d6", "d7", "d8", "a2", "b2", "c2", "e2", "f2", "g2", "h2"})
-    @DisplayName("말의 위치(룩)를 받고 말의 종류에 따라 이동할 수 있는 칸 리스트 반환")
-    void calculateScopeRook(String input) {
-        Piece piece = PieceImpl.of(Color.WHITE, Type.ROOK);
-        Set<Square> availableSquares = piece.calculateScope(Square.of("d2"));
-        assertThat(availableSquares.contains(Square.of(input))).isTrue();
-        assertThat(availableSquares.size()).isEqualTo(14);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"a6", "b6", "d6", "e6", "c7"})
-    @DisplayName("판의 정보를 가져와서 rook이 갈 수 있는 칸에 장애물이 있는지 판단하여 이동할 수 있는 리스트 반환하는 테스트")
-    void movableRookSquareTest(String input) {
-        Map<Square, Piece> board = new HashMap<>();
-        board.put(Square.of("b7"), PieceImpl.of(Color.WHITE, Type.PAWN));
-        board.put(Square.of("c7"), PieceImpl.of(Color.WHITE, Type.PAWN));
-        board.put(Square.of("a6"), PieceImpl.of(Color.WHITE, Type.KING));
-        board.put(Square.of("c5"), PieceImpl.of(Color.BLACK, Type.PAWN));
-        board.put(Square.of("e8"), PieceImpl.of(Color.WHITE, Type.KNIGHT));
-        board.put(Square.of("f6"), PieceImpl.of(Color.BLACK, Type.QUEEN));
-        board.put(Square.of("f3"), PieceImpl.of(Color.BLACK, Type.PAWN));
-        board.put(Square.of("g6"), PieceImpl.of(Color.BLACK, Type.KING));
-        board.put(Square.of("g2"), PieceImpl.of(Color.WHITE, Type.PAWN));
-
-        Piece piece = PieceImpl.of(Color.BLACK, Type.ROOK);
-        Set<Square> availableSquares = piece.calculateMoveBoundary(Square.of("c6"), board);
-
-        assertThat(availableSquares.contains(Square.of(input))).isTrue();
-        assertThat(availableSquares.size()).isEqualTo(5);
     }
 
     @Test
