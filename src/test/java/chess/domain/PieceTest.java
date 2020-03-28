280cae462ed3a2e5a2476580f574a088eba5a784
chws <rebeccachoosir@gmail.com>
2020-03-28 22:57:09 +0900

    feat: abstract class Piece를 상속하는 Bishop 클래스 및 테스트 작성

--- src/test/java/chess/domain/PieceTest.java
+++ src/test/java/chess/domain/PieceTest.java
@@ -31,16 +31,6 @@ public class PieceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"c1", "e1", "c3", "b4", "a5", "e3", "f4", "g5", "h6"})
-    @DisplayName("말의 위치(비숍)를 받고 말의 종류에 따라 이동할 수 있는 칸 리스트 반환")
-    void calculateScopeBishop(String input) {
-        Piece piece = PieceImpl.of(Color.WHITE, Type.BISHOP);
-        Set<Square> availableSquares = piece.calculateScope(Square.of("d2"));
-        assertThat(availableSquares.contains(Square.of(input))).isTrue();
-        assertThat(availableSquares.size()).isEqualTo(9);
-    }
-
-    @ParameterizedTest
     @ValueSource(strings = {"d1", "d3", "d4", "d5", "d6", "d7", "d8", "a2", "b2", "c2", "e2", "f2", "g2", "h2"})
     @DisplayName("말의 위치(룩)를 받고 말의 종류에 따라 이동할 수 있는 칸 리스트 반환")
     void calculateScopeRook(String input) {
@@ -48,29 +38,6 @@ public class PieceTest {
         Set<Square> availableSquares = piece.calculateScope(Square.of("d2"));
         assertThat(availableSquares.contains(Square.of(input))).isTrue();
         assertThat(availableSquares.size()).isEqualTo(14);
-    }
-
-
-    @ParameterizedTest
-    @ValueSource(strings = {"b7", "d7", "b5", "d5", "a4", "e4", "e8"})
-    @DisplayName("판의 정보를 가져와서 bishop이 갈 수 있는 칸에 장애물이 있는지 판단하여 이동할 수 있는 리스트 반환하는 테스트")
-    void movableBishopSquareTest(String input) {
-        Map<Square, Piece> board = new HashMap<>();
-        board.put(Square.of("b7"), PieceImpl.of(Color.WHITE, Type.PAWN));
-        board.put(Square.of("c7"), PieceImpl.of(Color.WHITE, Type.PAWN));
-        board.put(Square.of("a6"), PieceImpl.of(Color.WHITE, Type.KING));
-        board.put(Square.of("c5"), PieceImpl.of(Color.BLACK, Type.PAWN));
-        board.put(Square.of("e8"), PieceImpl.of(Color.WHITE, Type.KNIGHT));
-        board.put(Square.of("f6"), PieceImpl.of(Color.BLACK, Type.QUEEN));
-        board.put(Square.of("f3"), PieceImpl.of(Color.BLACK, Type.PAWN));
-        board.put(Square.of("g6"), PieceImpl.of(Color.BLACK, Type.KING));
-        board.put(Square.of("g2"), PieceImpl.of(Color.WHITE, Type.PAWN));
-
-        Piece piece = PieceImpl.of(Color.BLACK, Type.BISHOP);
-        Set<Square> availableSquares = piece.calculateMoveBoundary(Square.of("c6"), board);
-
-        assertThat(availableSquares.contains(Square.of(input))).isTrue();
-        assertThat(availableSquares.size()).isEqualTo(7);
     }
 
     @ParameterizedTest
