6351969fabcdb597f50c789bce547a529c822fee
BEOMJUN KIM <skyblue300a@gmail.com>
2020-03-28 12:09:11 +0900

    refactor: PawnStrategy가 findPathTo 메서드를 사용하도록 수정

--- src/main/java/chess/domain/piece/PawnStrategy.java
+++ src/main/java/chess/domain/piece/PawnStrategy.java
@@ -3,31 +3,24 @@ package chess.domain.piece;
 import chess.domain.board.Position;
 import chess.domain.exception.InvalidMovementException;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class PawnStrategy implements MoveStrategy {
 
-    private final List<Direction> directions = Collections.singletonList(Direction.N);
+    private final Direction direction = Direction.N;
     private final List<Direction> killDirections = Arrays.asList(Direction.NE, Direction.NW);
 
     @Override
     public List<Position> findMovePath(Position source, Position target) {
-        for (Direction direction : directions) {
-            Position position = source.destinationOf(direction).orElse(null);
-            if (target.equals(position)) {
-                return Collections.emptyList();
-            }
-            List<Position> positions = new ArrayList<>();
-            if (ChessPiece.PAWN.getOriginalPositions().contains(source)) {
-                positions = Collections.singletonList(position);
-                position = position.destinationOf(direction).orElse(null);
-            }
-            if (target.equals(position)) {
-                return positions;
-            }
+        List<Position> path = source.pathTo(direction, 1);
+        if (path.contains(target)) {
+            return findPathTo(path, target);
+        }
+
+        path = source.pathTo(direction, 2);
+        if (path.contains(target)) {
+            return findPathTo(path, target);
         }
 
         throw new InvalidMovementException();
@@ -36,12 +29,16 @@ public class PawnStrategy implements MoveStrategy {
     @Override
     public List<Position> findKillPath(Position source, Position target) {
         for (Direction direction : killDirections) {
-            Position position = source.destinationOf(direction).orElse(null);
-            if (target.equals(position)) {
-                return Collections.emptyList();
+            List<Position> path = source.pathTo(direction, 1);
+            if (path.contains(target)) {
+                return findPathTo(path, target);
             }
         }
 
         throw new InvalidMovementException();
+    }
+
+    private List<Position> findPathTo(List<Position> path, Position target) {
+        return path.subList(0, path.indexOf(target));
     }
 }
