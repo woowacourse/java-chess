8fa75ec8ece4416249d796c155afb9b4a5c82786
yeonnseok <ljinsk3@gmail.com>
2020-03-28 23:11:17 +0900

    feat: King 이동

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -2,16 +2,49 @@ package chess.domain.piece;
 
 import chess.domain.board.Board;
 import chess.domain.position.Position;
+import chess.domain.util.Direction;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class King extends Piece {
+    private static final List<Direction> DIRECTIONS = new ArrayList<>(
+            Arrays.asList(
+                    Direction.NORTH,
+                    Direction.NE,
+                    Direction.EAST,
+                    Direction.SE,
+                    Direction.SOUTH,
+                    Direction.SW,
+                    Direction.WEST,
+                    Direction.NW
+            )
+    );
+
     public King(char representation, Team team, Position position) {
         super(representation, team, position);
     }
 
     @Override
     public List<Position> getPossiblePositions(Board board) {
-        return null;
+        List<Position> possiblePositions = new ArrayList<>();
+
+        for (Direction direction : DIRECTIONS) {
+            Position nextPosition = direction.move(position);
+            if (isInBoardRange(nextPosition) && board.isBlank(nextPosition)) {
+                possiblePositions.add(nextPosition);
+            }
+
+            if (board.isOtherTeam(position, nextPosition)) {
+                possiblePositions.add(nextPosition);
+            }
+        }
+        return possiblePositions;
+    }
+
+    private boolean isInBoardRange(Position nextPosition) {
+        return nextPosition.getX() <= 8 && nextPosition.getX() >= 1 &&
+                nextPosition.getY() <= 8 && nextPosition.getY() >= 1;
     }
 }
---
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -0,0 +1,56 @@
+package chess.domain.piece;
+
+import chess.domain.board.Board;
+import chess.domain.position.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KingTest {
+    private static final List<Piece> INITIALIZED_POSITIONS = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        for (int row = 1; row <= 8; row++) {
+            for (int col = 1; col <= 8; col++) {
+                INITIALIZED_POSITIONS.add(new Blank('.', Team.BLANK, new Position(col, row)));
+            }
+        }
+    }
+
+    @DisplayName("킹의 이동 가능 위치")
+    @ParameterizedTest
+    @MethodSource("getCasesForKingMoveByDirection")
+    void kingMove(Piece piece, List<Position> expectedToPositions) {
+        INITIALIZED_POSITIONS.set(36, new King('k', Team.WHITE, new Position(5, 5)));
+        INITIALIZED_POSITIONS.set(27, new Rook('r', Team.WHITE, new Position(4, 4)));
+        INITIALIZED_POSITIONS.set(37, new Bishop('B', Team.BLACK, new Position(6, 5)));
+        Board board = new Board(INITIALIZED_POSITIONS);
+        assertThat(piece.getPossiblePositions(board)).isEqualTo(expectedToPositions);
+    }
+
+    private static Stream<Arguments> getCasesForKingMoveByDirection() {
+        return Stream.of(
+                Arguments.of(new King('k', Team.WHITE, new Position(5, 5)),
+                        Arrays.asList(
+                                new Position(5, 6),
+                                new Position(6, 6),
+                                new Position(6, 5),
+                                new Position(6, 4),
+                                new Position(5, 4),
+                                new Position(4, 5),
+                                new Position(4, 6)
+                        )
+                )
+        );
+    }
+}
