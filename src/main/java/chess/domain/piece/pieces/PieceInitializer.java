224a943b4b2187a4430ed829712051a1d91a6b92
toneyparky <qkrtmddhks95@gmail.com>
2020-03-29 16:49:18 +0900

    refactor: PiecesFactory를 Enum으로 관리
    PiecesFactory 상수를 이용하여 초기 체스 말들 설정해주던 것을 Enum을 통해 생성
    PieceInitializer 초기에 배치될 체스 말들을 관리하는 Enum

---
+++ src/main/java/chess/domain/piece/pieces/PieceInitializer.java
@@ -0,0 +1,70 @@
+package chess.domain.piece.pieces;
+
+import chess.domain.piece.Color;
+import chess.domain.piece.PieceType;
+import chess.domain.piece.movable.*;
+import chess.domain.position.Position;
+import chess.domain.position.PositionFactory;
+
+public enum PieceInitializer {
+    A1(PieceType.ROOK, PositionFactory.of("a1"), Color.WHITE, new BlockedMovable(Directions.LINEAR)),
+    B1(PieceType.BISHOP, PositionFactory.of("b1"), Color.WHITE, new BlockedMovable(Directions.DIAGONAL)),
+    C1(PieceType.KNIGHT, PositionFactory.of("c1"), Color.WHITE, new UnblockedMovable(Directions.KNIGHT)),
+    D1(PieceType.QUEEN, PositionFactory.of("d1"), Color.WHITE, new BlockedMovable(Directions.EVERY)),
+    E1(PieceType.KING, PositionFactory.of("e1"), Color.WHITE, new UnblockedMovable(Directions.EVERY)),
+    F1(PieceType.KNIGHT, PositionFactory.of("f1"), Color.WHITE, new UnblockedMovable(Directions.KNIGHT)),
+    G1(PieceType.BISHOP, PositionFactory.of("g1"), Color.WHITE, new BlockedMovable(Directions.DIAGONAL)),
+    H1(PieceType.ROOK, PositionFactory.of("h1"), Color.WHITE, new BlockedMovable(Directions.LINEAR)),
+    A2(PieceType.PAWN, PositionFactory.of("a2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    B2(PieceType.PAWN, PositionFactory.of("b2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    C2(PieceType.PAWN, PositionFactory.of("c2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    D2(PieceType.PAWN, PositionFactory.of("d2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    E2(PieceType.PAWN, PositionFactory.of("e2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    F2(PieceType.PAWN, PositionFactory.of("f2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    G2(PieceType.PAWN, PositionFactory.of("g2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    H2(PieceType.PAWN, PositionFactory.of("h2"), Color.WHITE, new PawnMovable(Directions.WHITEPAWN)),
+    A7(PieceType.PAWN, PositionFactory.of("a7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    B7(PieceType.PAWN, PositionFactory.of("b7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    C7(PieceType.PAWN, PositionFactory.of("c7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    D7(PieceType.PAWN, PositionFactory.of("d7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    E7(PieceType.PAWN, PositionFactory.of("e7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    F7(PieceType.PAWN, PositionFactory.of("f7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    G7(PieceType.PAWN, PositionFactory.of("g7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    H7(PieceType.PAWN, PositionFactory.of("h7"), Color.BLACK, new PawnMovable(Directions.BLACKPAWN)),
+    A8(PieceType.ROOK, PositionFactory.of("a8"), Color.BLACK, new BlockedMovable(Directions.LINEAR)),
+    B8(PieceType.BISHOP, PositionFactory.of("b8"), Color.BLACK, new BlockedMovable(Directions.DIAGONAL)),
+    C8(PieceType.KNIGHT, PositionFactory.of("c8"), Color.BLACK, new UnblockedMovable(Directions.KNIGHT)),
+    D8(PieceType.QUEEN, PositionFactory.of("d8"), Color.BLACK, new BlockedMovable(Directions.EVERY)),
+    E8(PieceType.KING, PositionFactory.of("e8"), Color.BLACK, new UnblockedMovable(Directions.EVERY)),
+    F8(PieceType.KNIGHT, PositionFactory.of("f8"), Color.BLACK, new UnblockedMovable(Directions.KNIGHT)),
+    G8(PieceType.BISHOP, PositionFactory.of("g8"), Color.BLACK, new BlockedMovable(Directions.DIAGONAL)),
+    H8(PieceType.ROOK, PositionFactory.of("h8"), Color.BLACK, new BlockedMovable(Directions.LINEAR));
+
+    private final PieceType pieceType;
+    private final Position position;
+    private final Color color;
+    private final Movable movable;
+
+    PieceInitializer(PieceType pieceType, Position position, Color color, Movable movable) {
+        this.pieceType = pieceType;
+        this.position = position;
+        this.color = color;
+        this.movable = movable;
+    }
+
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Movable getMovable() {
+        return movable;
+    }
+}
--- src/main/java/chess/domain/piece/pieces/PiecesFactory.java
+++ src/main/java/chess/domain/piece/pieces/PiecesFactory.java
@@ -1,6 +1,5 @@
 package chess.domain.piece.pieces;
 
-import chess.domain.position.PositionFactory;
 import chess.domain.piece.*;
 
 import java.util.ArrayList;
@@ -9,39 +8,10 @@ import java.util.List;
 public class PiecesFactory {
 	public static Pieces of() {
 		List<Piece> pieces = new ArrayList<>();
-		pieces.add(new Rook(PositionFactory.of("a1"), "r", Color.WHITE));
-		pieces.add(new Rook(PositionFactory.of("h1"), "r", Color.WHITE));
-		pieces.add(new Knight(PositionFactory.of("b1"), "n", Color.WHITE));
-		pieces.add(new Knight(PositionFactory.of("g1"), "n", Color.WHITE));
-		pieces.add(new Bishop(PositionFactory.of("c1"), "b", Color.WHITE));
-		pieces.add(new Bishop(PositionFactory.of("f1"), "b", Color.WHITE));
-		pieces.add(new King(PositionFactory.of("d1"), "k", Color.WHITE));
-		pieces.add(new Queen(PositionFactory.of("e1"), "q", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("a2"), "p", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("b2"), "p", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("c2"), "p", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("d2"), "p", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("e2"), "p", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("f2"), "p", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("g2"), "p", Color.WHITE));
-		pieces.add(new Pawn(PositionFactory.of("h2"), "p", Color.WHITE));
 
-		pieces.add(new Rook(PositionFactory.of("a8"), "R", Color.BLACK));
-		pieces.add(new Rook(PositionFactory.of("h8"), "R", Color.BLACK));
-		pieces.add(new Knight(PositionFactory.of("b8"), "N", Color.BLACK));
-		pieces.add(new Knight(PositionFactory.of("g8"), "N", Color.BLACK));
-		pieces.add(new Bishop(PositionFactory.of("c8"), "B", Color.BLACK));
-		pieces.add(new Bishop(PositionFactory.of("f8"), "B", Color.BLACK));
-		pieces.add(new King(PositionFactory.of("d8"), "K", Color.BLACK));
-		pieces.add(new Queen(PositionFactory.of("e8"), "Q", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("a7"), "P", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("b7"), "P", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("c7"), "P", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("d7"), "P", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("e7"), "P", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("f7"), "P", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("g7"), "P", Color.BLACK));
-		pieces.add(new Pawn(PositionFactory.of("h7"), "P", Color.BLACK));
+		for(PieceInitializer pieceInitializer : PieceInitializer.values()) {
+			pieces.add(new Piece(pieceInitializer));
+		}
 
 		return new Pieces(pieces);
 	}
