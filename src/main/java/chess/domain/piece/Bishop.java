ecbe7d025030925d4c4ad4c418b2251a2d90f818
hj <a301dks@naver.com>
2020-03-29 22:57:25 +0900

    feat: 비숍이 움직이는 기능 추가

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -1,19 +1,60 @@
 package chess.domain.piece;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import chess.domain.board.Position;
+import chess.domain.piece.state.State;
 
 public class Bishop extends Piece {
-	public Bishop(String symbol) {
-		super(symbol);
+	public Bishop(State state, String symbol) {
+		super(state, symbol);
 	}
 
 	@Override
-	public void move(Position source, Position target) {
+	public List<Position> movingTrace(Position source, Position target) {
+		if (state.isCaptured()) {
+			throw new UnsupportedOperationException("죽은 말은 움직일 수 없습니다.");
+		}
 
+		return findRoute(source, target);
 	}
 
-	@Override
-	public String getSymbol() {
-		return super.getSymbol();
+	protected List<Position> findRoute(Position source, Position target) {
+		List<Position>[] possiblePositions = source.nextPositions(Direction.diagonalDirection());
+
+		return makeRoute(source, target, findTargetRoute(target, possiblePositions));
+	}
+
+	private List<Position> findTargetRoute(Position target, List<Position>[] possiblePositions) {
+		boolean isPossible = false;
+		int possibleIndex = 0;
+
+		for (int i = 0; i < possiblePositions.length; i++) {
+			if (possiblePositions[i].stream()
+				.anyMatch(position -> position.equals(target))) {
+				isPossible = true;
+				possibleIndex = i;
+				break;
+			}
+		}
+		if (!isPossible) {
+			throw new UnsupportedOperationException("갈 수 없는 target입니다.");
+		}
+		return possiblePositions[possibleIndex];
+	}
+
+	private List<Position> makeRoute(Position source, Position target, List<Position> targetRoute) {
+		List<Position> route = new LinkedList<>();
+
+		route.add(source);
+		for (Position position : targetRoute) {
+			if (position.equals(target)) {
+				break;
+			}
+			route.add(position);
+		}
+		route.add(target);
+		return route;
 	}
 }
---
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -0,0 +1,54 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import chess.domain.board.Position;
+import chess.domain.piece.state.Captured;
+import chess.domain.piece.state.Initial;
+
+/**
+ *    class description
+ *
+ *    @author AnHyungJu
+ */
+class BishopTest {
+	@DisplayName("말이 죽어있을 때 movingTrace를 호출하면 예외처리")
+	@Test
+	void movingTraceTest() {
+		Piece bishop = new Bishop(new Captured(), "b");
+
+		Position source = Position.of("a1");
+		Position target = Position.of("a1");
+
+		assertThatThrownBy(() -> bishop.movingTrace(source, target))
+			.isInstanceOf(UnsupportedOperationException.class)
+			.hasMessageContaining("죽은 말은");
+	}
+
+	@DisplayName("비숍이 source에서 target으로 갈 수 없는 경우 예외처리")
+	@Test
+	void movingTraceTest2() {
+		Piece bishop = new Bishop(new Initial(), "b");
+
+		Position source = Position.of("a1");
+		Position target = Position.of("c2");
+
+		assertThatThrownBy(() -> bishop.movingTrace(source, target))
+			.isInstanceOf(UnsupportedOperationException.class)
+			.hasMessageContaining("갈 수 없는");
+	}
+
+	@DisplayName("비숍이 source에서 target으로 정상적으로 갈 수 있는지 확인")
+	@Test
+	void movingTraceTest3() {
+		Piece bishop = new Bishop(new Initial(), "b");
+
+		Position source = Position.of("a1");
+		Position target = Position.of("h8");
+
+		assertThat(bishop.movingTrace(source, target).size()).isEqualTo(8);
+	}
+}
