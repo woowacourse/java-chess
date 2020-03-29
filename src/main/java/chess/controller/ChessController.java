9d9d5adbdc5159f3a2d040d52d698d2ac26937e4
gunhee lee <dlrjsgml42@naver.com>
2020-03-29 13:32:39 +0900

    feat: 컨트롤러에서 수행하는 명령을 OperationType enum으로 통합
    Operate 메서드 동작을 인자로 하기 위한 함수형 인터페이스
    OperationType 사용자 입력에 따른 명령 수행의 구현체를 저장하는 Enum

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,77 +1,51 @@
 package chess.controller;
 
-import chess.domain.position.PositionFactory;
+import chess.domain.game.Board;
 import chess.domain.game.ChessGame;
 import chess.domain.game.OperationType;
+import chess.domain.game.Operations;
 import chess.view.OutputView;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static chess.view.InputView.inputOperation;
 
 public class ChessController {
-
-	private static final String DELIMITER = " ";
-	private static final String INVALID_OPERATION_EXCEPTION_MESSAGE = "잘못된 명령입니다.";
-	private static final int START_POSITION_INDEX = 1;
-	private static final int DESTINATION_POSITION_INDEX = 2;
-	private static final int OPERATIONTYPE_INDEX = 0;
-
 	public void run() {
-		ChessGame chessGame = new ChessGame();
-
 		OutputView.printOperationsFormat();
-		List<String> operations = Arrays.asList(inputOperation().split(DELIMITER));
-		OperationType operationType = OperationType.of(operations.get(OPERATIONTYPE_INDEX));
-
-		validateFirstOperations(operationType);
-		if (operationType.isEnd()) {
-			OutputView.printFinish();
+		if (!beforeStart()) {
 			return;
 		}
-
-		do {
-			OutputView.printBoard(chessGame);
-			operations = Arrays.asList(inputOperation().split(DELIMITER));
-			operationType = OperationType.of(operations.get(OPERATIONTYPE_INDEX));
-		} while (operateByType(chessGame, operations, operationType));
+		execute();
+		OutputView.printFinish();
 	}
 
-	private void validateFirstOperations(OperationType operationType) {
-		if (!operationType.isStart() && !operationType.isEnd()) {
-			throw new UnsupportedOperationException(INVALID_OPERATION_EXCEPTION_MESSAGE);
-		}
-	}
+	private void execute() {
+		ChessGame chessGame = new ChessGame();
+		OutputView.printBoard(new Board(chessGame.getPieces()));
 
-	private boolean operateByType(ChessGame chessGame, List<String> operations, OperationType operationType) {
-		if (operationType.isEnd()) {
+		while (executeOperation(chessGame)) {
+			OutputView.printBoard(new Board(chessGame.getPieces()));
 			OutputView.printScore(chessGame.calculateScore());
-			OutputView.printFinish();
-			return false;
 		}
-
-		if (operationType.isMove()) {
-			chessGame.move(PositionFactory.of(operations.get(START_POSITION_INDEX)), PositionFactory.of(operations.get(DESTINATION_POSITION_INDEX)));
-			return isMoveOperating(chessGame);
-		}
-
-		if (operationType.isStatus()) {
-			OutputView.printScore(chessGame.calculateScore());
-			return true;
-		}
-
-		if (operationType.isStart()) {
-			throw new UnsupportedOperationException(INVALID_OPERATION_EXCEPTION_MESSAGE);
-		}
-		throw new UnsupportedOperationException(INVALID_OPERATION_EXCEPTION_MESSAGE);
-	}
-
-	private boolean isMoveOperating(ChessGame chessGame) {
-		if (chessGame.isKingDead()) {
+		if(chessGame.isKingDead()) {
 			OutputView.printFinishByKingDead(chessGame.getAliveKingColor());
+		}
+	}
+
+	private boolean beforeStart() {
+		OperationType operationType = new Operations(inputOperation()).getOperationType();
+
+		operationType.validateFirstOperations();
+		if (operationType.isEnd()) {
+			OutputView.printFinish();
 			return false;
 		}
 		return true;
+	}
+
+	private boolean executeOperation(ChessGame chessGame) {
+		Operations operations = new Operations(inputOperation());
+		OperationType operationType = operations.getOperationType();
+
+		return (operationType.runOperate(chessGame,operations) && !chessGame.isKingDead());
 	}
 }
--- src/main/java/chess/domain/game/OperationType.java
+++ src/main/java/chess/domain/game/OperationType.java
@@ -1,50 +1,69 @@
 package chess.domain.game;
 
+import chess.domain.position.PositionFactory;
+
 import java.util.Arrays;
 import java.util.Objects;
 
 public enum OperationType {
-	START("start"),
-	MOVE("move"),
-	END("end"),
-	STATUS("status");
+    START("start", (chessGame, operations) -> {
+        throw new UnsupportedOperationException("error");
+    }),
+    MOVE("move", (chessGame, operations) -> {// TODO: 2020/03/28 상수값 enum 내 메서드 적용 이슈
+        chessGame.move(PositionFactory.of(operations.getFirstArgument()), PositionFactory.of(operations.getSecondArgument()));
+        return true;
+    }),
+    END("end", (chessGame, operations) -> false),
+    STATUS("status", (chessGame, operations) -> true);
 
-	private static final String INVALID_INPUT_EXCEPTION_MESSAGE = "잘못된 입력입니다.";
+    private static final String INVALID_OPERATION_EXCEPTION_MESSAGE = "잘못된 명령입니다.";
 
-	private final String name;
+    private final String name;
+    private final Operate operate;
 
-	OperationType(String name) {
-		this.name = name;
-	}
+    OperationType(String name, Operate operate) {
+        this.name = name;
+        this.operate = operate;
+    }
 
-	public static OperationType of(String name) {
-		validate(name);
-		return Arrays.stream(OperationType.values())
-				.filter(operationType -> operationType.name.equals(name))
-				.findFirst()
-				.orElseThrow(() -> new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE));
-	}
+    public static OperationType of(String name) {
+        validate(name);
+        return Arrays.stream(OperationType.values())
+                .filter(operationType -> operationType.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_OPERATION_EXCEPTION_MESSAGE));
+    }
 
-	private static void validate(String name) {
-		Objects.requireNonNull(name, INVALID_INPUT_EXCEPTION_MESSAGE);
-		if (name.isEmpty()) {
-			throw new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE);
-		}
-	}
+    private static void validate(String name) {
+        Objects.requireNonNull(name, INVALID_OPERATION_EXCEPTION_MESSAGE);
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(INVALID_OPERATION_EXCEPTION_MESSAGE);
+        }
+    }
 
-	public boolean isStart() {
-		return START.equals(this);
-	}
+    public void validateFirstOperations() {// TODO: 2020/03/28 validate?check? 이름 이슈
+        if (!isStart() && !isEnd()) {
+            throw new UnsupportedOperationException(INVALID_OPERATION_EXCEPTION_MESSAGE);
+        }
+    }
 
-	public boolean isMove() {
-		return MOVE.equals(this);
-	}
+    public boolean isStart() {
+        return START.equals(this);
+    }
 
-	public boolean isEnd() {
-		return END.equals(this);
-	}
+    public boolean isMove() {
+        return MOVE.equals(this);
+    }
 
-	public boolean isStatus() {
-		return STATUS.equals(this);
-	}
+    public boolean isEnd() {
+        return END.equals(this);
+    }
+
+    public boolean isStatus() {
+        return STATUS.equals(this);
+    }
+
+    public boolean runOperate(ChessGame chessGame, Operations operations) {
+        return operate.apply(chessGame, operations);
+    }
 }
