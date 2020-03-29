9e79031cc0f1f6c0c6d43872b9bbecffaeff4f64
icyMojito <dev.icymojito@gmail.com>
2020-03-29 20:01:37 +0900

    test : Board 클래스에 턴(Turn)제 관련 구현 추가로 테스트 추가 수정

--- src/test/java/board/BoardTest.java
+++ src/test/java/board/BoardTest.java
@@ -25,29 +25,51 @@ public class BoardTest {
             "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
             "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"})
     void inputEmptyPiecePositionThenThrowException(String input) {
-        Assertions.assertThatThrownBy(() -> board.findPiece(Positions.of(input))).hasMessage("움직일 수 있는 체스말이 없습니다.");
+        Assertions.assertThatThrownBy(() -> board.findPiece(Positions.of(input), PieceColor.NONE)).hasMessage("움직일 수 있는 체스말이 없습니다.");
     }
 
     @Test
     @DisplayName("EmptyPiece가 아닌 체스말이 있는 위치값이 입력되면 해당 체스말을 반환해야 함")
     void inputPiecePositionThenReturnPiece() {
-        Piece piece = board.findPiece(Positions.of("a1"));
+        Piece piece = board.findPiece(Positions.of("a1"), PieceColor.WHITE);
         Assertions.assertThat(piece instanceof Rook).isTrue();
 
-        piece = board.findPiece(Positions.of("b8"));
+        piece = board.findPiece(Positions.of("b8"), PieceColor.BLACK);
         Assertions.assertThat(piece instanceof Knight).isTrue();
 
-        piece = board.findPiece(Positions.of("c1"));
+        piece = board.findPiece(Positions.of("c1"), PieceColor.WHITE);
         Assertions.assertThat(piece instanceof Bishop).isTrue();
 
-        piece = board.findPiece(Positions.of("d8"));
+        piece = board.findPiece(Positions.of("d8"), PieceColor.BLACK);
         Assertions.assertThat(piece instanceof Queen).isTrue();
 
-        piece = board.findPiece(Positions.of("e1"));
+        piece = board.findPiece(Positions.of("e1"), PieceColor.WHITE);
         Assertions.assertThat(piece instanceof King).isTrue();
 
-        piece = board.findPiece(Positions.of("c2"));
+        piece = board.findPiece(Positions.of("c2"), PieceColor.WHITE);
         Assertions.assertThat(piece instanceof Pawn).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("체스말이 없는 위치값으로 체스말을 찾을 때 예외가 발생해야 함")
+    @ValueSource(strings = {"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+            "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+            "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+            "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"})
+    void inputNoPiecePositionThenThrowException(String input) {
+        Assertions.assertThatThrownBy(() -> board.findPiece(Positions.of(input), PieceColor.NONE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("움직일 수 있는 체스말이 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("현재 팀의 색과 다른 색의 체스말을 찾을 때 예외가 발생해야 함")
+    @ValueSource(strings = {"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "a7", "b7", "c7", "d7", "e7", "f7", "g7",
+            "h7"})
+    void findDifferentPieceColorPositionThenThrowException(String input) {
+        Assertions.assertThatThrownBy(() -> board.findPiece(Positions.of(input), PieceColor.WHITE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("다른 색의 말을 움직일 수 없습니다.");
     }
 
     @Test
