1c6a51ca35f5d32423e87ac4a525ad471192803b
chanin <pci2676@gmail.com>
2020-03-30 00:56:24 +0900

    test : 폰 계산 로직 테스트 추가

--- src/test/java/chess/board/ScoreTest.java
+++ src/test/java/chess/board/ScoreTest.java
@@ -3,11 +3,14 @@ package chess.board;
 import chess.coordinate.Coordinate;
 import chess.coordinate.File;
 import chess.coordinate.Rank;
+import chess.piece.MovedPawn;
 import chess.piece.Pieces;
 import chess.piece.Queen;
 import chess.piece.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Map;
 
@@ -28,14 +31,15 @@ class ScoreTest {
         assertThat(sum).isEqualTo(0);
     }
 
+    @DisplayName("단일 팀에 대한 계산만 진행한다.")
     @Test
     void calculateScore() {
         //given
-        Score score = Score.zero();
-
         ChessBoard chessBoard = ChessBoard.empty();
-        Tile tile = new Tile(Coordinate.of(File.F, Rank.ONE), Pieces.findBy(Queen.class, Team.BLACK));
-        chessBoard.put(tile);
+        Tile tile1 = new Tile(Coordinate.of(File.F, Rank.ONE), Pieces.findBy(Queen.class, Team.BLACK));
+        Tile tile2 = new Tile(Coordinate.of(File.H, Rank.ONE), Pieces.findBy(Queen.class, Team.WHITE));
+        chessBoard.put(tile1);
+        chessBoard.put(tile2);
 
         Map<Coordinate, Tile> board = chessBoard.getChessBoard();
 
@@ -46,4 +50,23 @@ class ScoreTest {
         assertThat(sumOfBlack).isEqualTo(9);
     }
 
+    @DisplayName("폰이 세로로 2개 이상 있는 경우 폰의 점수를 0.5점으로 계산한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"F,1", "H,2"})
+    void name(File file, double expect) {
+        //given
+        ChessBoard chessBoard = ChessBoard.empty();
+        Tile tile1 = new Tile(Coordinate.of(File.F, Rank.ONE), Pieces.findBy(MovedPawn.class, Team.BLACK));
+        Tile tile2 = new Tile(Coordinate.of(file, Rank.TWO), Pieces.findBy(MovedPawn.class, Team.BLACK));
+        chessBoard.put(tile1);
+        chessBoard.put(tile2);
+
+        Map<Coordinate, Tile> board = chessBoard.getChessBoard();
+
+        //when
+        double sumOfBlack = Score.calculateScore(Team.BLACK, board::get).getSum();
+
+        //then
+        assertThat(sumOfBlack).isEqualTo(expect);
+    }
 }
