9e53a92afb3e31bb3b06be6cec51bd76ec16769d
icyMojito <dev.icymojito@gmail.com>
2020-03-29 17:46:06 +0900

    feat : Board 클래스에 게임 결과(승패와 점수) 생성과 관련된 메서드 추가

--- src/main/java/chess/domain/board/Board.java
+++ src/main/java/chess/domain/board/Board.java
@@ -1,5 +1,6 @@
 package chess.domain.board;
 
+import chess.domain.GameResult;
 import chess.domain.piece.*;
 
 import java.util.Collections;
@@ -7,9 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static chess.domain.piece.Pawn.PAWN_HALF_SCORE;
+import static chess.domain.piece.Pawn.PAWN_SCORE;
 import static chess.util.NullValidator.validateNull;
 
 public class Board {
+    public static final int ONLY_ONE_PAWN_IN_XPOINT = 1;
     private final Map<Position, Piece> board = createBoard();
 
     private Map<Position, Piece> createBoard() {
@@ -62,7 +66,7 @@ public class Board {
 
     public Piece findPiece(Position sourcePosition) {
         validateNull(sourcePosition);
-        if (board.get(sourcePosition).isNoneColor()) {
+        if (board.get(sourcePosition).isNone()) {
             throw new IllegalArgumentException("움직일 수 있는 체스말이 없습니다.");
         }
         return board.get(sourcePosition);
@@ -86,12 +90,12 @@ public class Board {
         path.remove(path.size() - 1);
         return !path.stream()
                 .map(board::get)
-                .allMatch(Piece::isNoneColor);
+                .allMatch(Piece::isNone);
     }
 
     private boolean cannotMoveToTargetPosition(Piece piece, Position targetPosition) {
         Piece targetPiece = board.get(targetPosition);
-        if (!targetPiece.isNoneColor()) {
+        if (!targetPiece.isNone()) {
             return piece.isSameColor(targetPiece);
         }
         return false;
@@ -110,24 +114,105 @@ public class Board {
     private boolean havePieceIn(List<Position> path) {
         return !path.stream()
                 .map(board::get)
-                .allMatch(Piece::isNoneColor);
+                .allMatch(Piece::isNone);
     }
 
     private boolean cannotMovePawnToTargetPosition(Pawn pawn, Position targetPosition) {
         Piece targetPiece = board.get(targetPosition);
-        if (!targetPiece.isNoneColor()) {
+        if (!targetPiece.isNone()) {
             return pawn.isSameColor(targetPiece);
         }
         return true;
-    }
-
-    public Map<Position, Piece> get() {
-        return Collections.unmodifiableMap(board);
     }
 
     public void move(Piece piece, Position targetPosition) {
         board.put(piece.getPosition(), new EmptyPiece(PieceColor.NONE, piece.getPosition()));
         board.put(targetPosition, piece);
         piece.changeTo(targetPosition);
+    }
+
+    public boolean isBlackKingKilled() {
+        return board.values().stream()
+                .map(Piece::getName)
+                .noneMatch("K"::equals);
+    }
+
+    public boolean isWhiteKingKilled() {
+        return board.values().stream()
+                .map(Piece::getName)
+                .noneMatch("k"::equals);
+    }
+
+    private double getAliveBlackPieceScoreSum() {
+        double scoreSum = board.values().stream()
+                .filter(piece -> !(piece instanceof Pawn) && piece.isBlack())
+                .mapToDouble(Piece::getScore)
+                .sum();
+
+        return scoreSum + getAliveBlackPawnScoreSum();
+    }
+
+    private double getAliveBlackPawnScoreSum() {
+        double scoreSum = 0;
+
+        for (char x = 'a'; x <= 'h'; x++) {
+            int pawnInXPointCount = 0;
+            for (char y = '1'; y <= '8'; y++) {
+                Piece piece = board.get(Positions.of(x, y));
+                if (piece instanceof Pawn && piece.isBlack()) {
+                    pawnInXPointCount++;
+                }
+            }
+            scoreSum += getPawnScoreSumInXPoint(pawnInXPointCount);
+        }
+
+        return scoreSum;
+    }
+
+    private double getAliveWhitePieceScoreSum() {
+        double scoreSum = board.values().stream()
+                .filter(piece -> !(piece instanceof Pawn) && piece.isWhite())
+                .mapToDouble(Piece::getScore)
+                .sum();
+
+        return scoreSum + getAliveWhitePawnScoreSum();
+    }
+
+    private double getAliveWhitePawnScoreSum() {
+        double scoreSum = 0;
+
+        for (char x = 'a'; x <= 'h'; x++) {
+            int pawnInXPointCount = 0;
+            for (char y = '1'; y <= '8'; y++) {
+                Piece piece = board.get(Positions.of(x, y));
+                if (piece instanceof Pawn && piece.isWhite()) {
+                    pawnInXPointCount++;
+                }
+            }
+            scoreSum += getPawnScoreSumInXPoint(pawnInXPointCount);
+        }
+
+        return scoreSum;
+    }
+
+    private double getPawnScoreSumInXPoint(int pawnInXPointCount) {
+        double scoreSum = 0;
+
+        if (ONLY_ONE_PAWN_IN_XPOINT < pawnInXPointCount) {
+            scoreSum += pawnInXPointCount * PAWN_HALF_SCORE;
+        } else {
+            scoreSum += pawnInXPointCount * PAWN_SCORE;
+        }
+
+        return scoreSum;
+    }
+
+    public GameResult createGameResult() {
+        return new GameResult(isBlackKingKilled(), isWhiteKingKilled(), getAliveBlackPieceScoreSum(),
+                              getAliveWhitePieceScoreSum());
+    }
+
+    public Map<Position, Piece> get() {
+        return Collections.unmodifiableMap(board);
     }
 }
