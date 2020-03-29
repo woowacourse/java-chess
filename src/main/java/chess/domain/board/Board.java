85b50fa67eabbdd61eeaa576959e920eb74f00e9
yeonnseok <ljinsk3@gmail.com>
2020-03-29 15:29:55 +0900

    refac: board package 상수추출

--- src/main/java/chess/domain/board/Board.java
+++ src/main/java/chess/domain/board/Board.java
@@ -9,19 +9,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+    private static final String BOARD_CREATION_FAIL_MESSAGE = "보드가 제대로 생성되지 못했습니다.";
+    private static final int BOARD_SIZE = 64;
+    private static final int COLUMN_VALUE_OF_STRING_COMMAND = 0;
+    private static final int ROW_VALUE_OF_STRING_COMMAND = 1;
+    private static final int FIRST_INDEX = 0;
+    private static final int ASCII_GAP = 96;
+    private static final String TAKE_TURN_EXCEPTION_MESSAGE = "체스 게임 순서를 지켜주세요.";
+    private static final char BLANK_REPRESENTATION = '.';
+    private static final int BOARD_ROW_SIZE = 8;
+
     private static boolean isFinished;
 
     private final List<Piece> board;
 
     public Board(final List<Piece> board) {
         if (isNotProperBoardSize(board)) {
-            throw new IllegalArgumentException("보드가 제대로 생성되지 못했습니다.");
+            throw new IllegalArgumentException(BOARD_CREATION_FAIL_MESSAGE);
         }
         this.board = board;
     }
 
     private boolean isNotProperBoardSize(final List<Piece> board) {
-        return board.size() != 64;
+        return board.size() != BOARD_SIZE;
     }
 
     public Piece findPieceBy(int index) {
@@ -29,54 +39,54 @@ public class Board {
     }
 
     public int getBoardIndexByStringPosition(String position) {
-        String x = String.valueOf(position.charAt(0));
-        String y = String.valueOf(position.charAt(1));
+        String x = String.valueOf(position.charAt(COLUMN_VALUE_OF_STRING_COMMAND));
+        String y = String.valueOf(position.charAt(ROW_VALUE_OF_STRING_COMMAND));
 
-        int col = x.charAt(0) - 96;
+        int col = x.charAt(FIRST_INDEX) - ASCII_GAP;
         int row = Integer.parseInt(y);
 
         return getBoardIndex(col, row);
     }
 
-    private int getBoardIndex(int col, int row) {
-        return (row - 1) * 8 + col - 1;
+    private int getBoardIndex(final int col, final int row) {
+        return (row - 1) * BOARD_ROW_SIZE + col - 1;
     }
 
-    public boolean isBlank(Position nextPosition) {
+    public boolean isBlank(final Position nextPosition) {
         return findPieceBy(getBoardIndex(nextPosition.getX(), nextPosition.getY()))
                 .getClass().equals(Blank.class);
     }
 
-    public boolean isOtherTeam(Position position, Position nextPosition) {
+    public boolean isOtherTeam(final Position position, final Position nextPosition) {
         return findPieceBy(getBoardIndex(position.getX(), position.getY()))
                 .isOtherTeam(findPieceBy(getBoardIndex(nextPosition.getX(), nextPosition.getY())));
     }
 
-    public Board movePiece(String from, String to, int turnFlag) throws IllegalAccessException {
+    public Board movePiece(final String from, final String to, final int turnFlag) throws IllegalAccessException {
         List<Piece> movedBoard = new ArrayList<>(board);
 
         int fromIndex = getBoardIndexByStringPosition(from);
         int toIndex = getBoardIndexByStringPosition(to);
         if (Team.isSameTeam(turnFlag, findPieceBy(fromIndex))) {
-            throw new IllegalAccessException("체스 게임 순서를 지켜주세요.");
+            throw new IllegalAccessException(TAKE_TURN_EXCEPTION_MESSAGE);
         }
 
         movePieceWithValidation(movedBoard, fromIndex, toIndex);
         return new Board(movedBoard);
     }
 
-    private void movePieceWithValidation(List<Piece> movedBoard, int fromIndex, int toIndex) {
+    private void movePieceWithValidation(final List<Piece> movedBoard, final int fromIndex, final int toIndex) {
         Piece fromPiece = findPieceBy(fromIndex);
         Piece toPiece = findPieceBy(toIndex);
 
         if (fromPiece.isMovable(this, toPiece.getPosition())) {
             movedBoard.set(toIndex, fromPiece.movePiecePosition(toPiece.getPosition()));
-            movedBoard.set(fromIndex, new Blank('.', Team.BLANK, fromPiece.getPosition()));
+            movedBoard.set(fromIndex, new Blank(BLANK_REPRESENTATION, Team.BLANK, fromPiece.getPosition()));
             changeFlagWhenKingCaptured(toPiece);
         }
     }
 
-    private void changeFlagWhenKingCaptured(Piece toPiece) {
+    private void changeFlagWhenKingCaptured(final Piece toPiece) {
         if (toPiece.isKing()) {
             isFinished = true;
         }
