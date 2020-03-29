c6ede286d6ee50d3c9c45e573fc5caa8a53d780e
icyMojito <dev.icymojito@gmail.com>
2020-03-29 17:49:04 +0900

    feat : InputView, OutputView 클래스에 게임 결과 출력에 관련된 메서드 추가

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String requestCommand() {
+    public static String requestRunCommand() {
         System.out.println("> 체스 게임을 시작합니다.");
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 종료 : end");
@@ -14,6 +14,12 @@ public class InputView {
     }
 
     public static String requestMoveCommand() {
+        return scanner.nextLine();
+    }
+
+    public static String requestStatusCommand() {
+        System.out.println("> 킹이 잡혔으므로 게임을 종료합니다.");
+        System.out.println("> 승패와 점수 확인 : status");
         return scanner.nextLine();
     }
 }
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,5 +1,6 @@
 package chess.view;
 
+import chess.domain.GameResult;
 import chess.domain.board.Board;
 import chess.domain.board.Position;
 import chess.domain.board.Positions;
@@ -19,15 +20,28 @@ public class OutputView {
         for (char yPoint = MAX_Y_POINT; yPoint >= MIN_Y_POINT; yPoint--) {
             for (char xPoint = MIN_X_POINT; xPoint <= MAX_X_POINT; xPoint++) {
                 if (xPoint == MAX_X_POINT) {
-                    System.out.println(chessBoard.get(Positions.of(xPoint, yPoint)).getName());
+                    System.out.println(chessBoard.get(Positions.of(xPoint, yPoint)).getName() + "   " + yPoint);
                 } else {
                     System.out.print(chessBoard.get(Positions.of(xPoint, yPoint)).getName());
                 }
             }
         }
+        System.out.print(System.lineSeparator());
+        System.out.println("abcdefgh");
     }
 
     public static void printErrorMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printResult(GameResult gameResult) {
+        if (gameResult.isBlackWin()) {
+            System.out.println("<승자> Black <패자> White");
+        }
+        if (gameResult.isWhiteWin()) {
+            System.out.println("<승자> White <패자> Black");
+        }
+        System.out.println(String.format("점수 : Black - %.1f점 / White - %.1f점", gameResult.getAliveBlackPieceScoreSum(),
+                                         gameResult.getAliveWhitePieceScoreSum()));
     }
 }
