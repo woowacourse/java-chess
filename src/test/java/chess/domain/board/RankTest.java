e8af4e625f16bfb988f599f4a818e609382816a8
hj <a301dks@naver.com>
2020-03-29 20:04:47 +0900

    test: rank의 of 테스트 추가

--- src/test/java/chess/domain/board/RankTest.java
+++ src/test/java/chess/domain/board/RankTest.java
@@ -1,8 +1,22 @@
 package chess.domain.board;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 /**
  *
  *    @author AnHyungJu, LeeHoBin
  */
 public class RankTest {
+	@DisplayName("of로 생성할 때 유효하지 않은 rank가 들어오면 예외처리")
+	@ParameterizedTest
+	@ValueSource(ints = {0, 9})
+	void of(int value) {
+		assertThatThrownBy(() -> Rank.of(value))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("유효하지");
+	}
 }
