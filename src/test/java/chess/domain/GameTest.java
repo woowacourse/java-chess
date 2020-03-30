4d935b8fd0522d122687c394a6d53af81d7bea31
hj <a301dks@naver.com>
2020-03-30 14:24:57 +0900

    test: GameTest 추가

---
+++ src/test/java/chess/domain/GameTest.java
@@ -0,0 +1,37 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import chess.domain.board.Board;
+import chess.domain.board.Position;
+import chess.domain.piece.BlackPiecesFactory;
+import chess.domain.piece.Pieces;
+import chess.domain.piece.WhitePiecesFactory;
+
+/**
+ *    class description
+ *
+ *    @author AnHyungJu
+ */
+class GameTest {
+	@DisplayName("첫 턴에 길이 막힌 기물을 움직이려고 할 때 예외처리 확인")
+	@Test
+	void movePieceFromTo() {
+		Game game = new Game(new Pieces(WhitePiecesFactory.create(), BlackPiecesFactory.create()), Board.getInstance());
+
+		assertThatThrownBy(() -> game.movePieceFromTo(Position.of("a1"), Position.of("a5")))
+			.isInstanceOf(UnsupportedOperationException.class)
+			.hasMessageContaining("해당 위치");
+	}
+
+	@DisplayName("왕이 살아있을 때 false를 반환하는지 확인")
+	@Test
+	void isKingDie() {
+		Game game = new Game(new Pieces(WhitePiecesFactory.create(), BlackPiecesFactory.create()), Board.getInstance());
+
+		assertThat(game.isKingDie()).isFalse();
+	}
+}
