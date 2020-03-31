c52573337d5aa992ff2647ab2365af56a07d3f73
soring <jnsorn@gmail.com>
2020-03-31 16:39:03 +0900

    Test : ChessCommand 관련 테스트 추가

--- src/test/java/domain/piece/command/CommandTest.java
+++ src/test/java/domain/piece/command/CommandTest.java
@@ -14,16 +14,32 @@ public class CommandTest {
 	@DisplayName("start 혹은 end가 입력됐을 경우 Command 반환")
 	@ParameterizedTest
 	@CsvSource({"start, START", "end, END"})
-	void of_ValidInputCommand_returnCommand(String inputCommand, Command command) {
+	void of_ValidInputGameCommand_ReturnCommand(String inputCommand, Command command) {
 		assertThat(Command.ofGameCommand(inputCommand)).isEqualTo(command);
 	}
 
 	@DisplayName("start 혹은 end 외의 명령어가 입력됐을 경우 InvalidCommandException 발생")
 	@ParameterizedTest
-	@ValueSource(strings = {"start", "movE", "run"})
-	void of_InvalidInputCommand_returnCommand(String inputCommand) {
-		assertThatThrownBy(() -> Command.ofChessCommand(inputCommand))
+	@ValueSource(strings = {"movE", "run"})
+	void of_InvalidInputGameCommand_ExceptionThrown(String inputCommand) {
+		assertThatThrownBy(() -> Command.ofGameCommand(inputCommand))
 			.isInstanceOf(InvalidCommandException.class)
 			.hasMessage(InvalidCommandException.INVALID_GAME_COMMAND);
 	}
+	@DisplayName("move, end, status가 입력됐을 경우 Command 반환")
+	@ParameterizedTest
+	@CsvSource({"move, MOVE", "end, END", "status, STATUS"})
+	void of_ValidInputChessCommand_ReturnCommand(String inputCommand, Command command) {
+		assertThat(Command.ofChessCommand(inputCommand)).isEqualTo(command);
+	}
+
+	@DisplayName("move, end, status외의 명령어가 입력됐을 경우 InvalidCommandException 발생")
+	@ParameterizedTest
+	@ValueSource(strings = {"start", "run"})
+	void of_InvalidInputChessCommand_ExceptionThrown(String inputCommand) {
+		assertThatThrownBy(() -> Command.ofChessCommand(inputCommand))
+			.isInstanceOf(InvalidCommandException.class)
+			.hasMessage(InvalidCommandException.INVALID_CHESS_COMMAND);
+	}
+
 }
