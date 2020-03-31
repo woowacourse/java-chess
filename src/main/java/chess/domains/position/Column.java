355605b84e644c3dd6b7f3c9668569a26513ef7c
hwangbo <taekwonod714@naver.com>
2020-03-31 09:19:30 +0900

    refactor: Column 클래스 내 상수명 변경

--- src/main/java/chess/domains/position/Column.java
+++ src/main/java/chess/domains/position/Column.java
@@ -3,14 +3,14 @@ package chess.domains.position;
 import java.util.Arrays;
 
 public enum Column {
-    a(1),
-    b(2),
-    c(3),
-    d(4),
-    e(5),
-    f(6),
-    g(7),
-    h(8);
+    A(1),
+    B(2),
+    C(3),
+    D(4),
+    E(5),
+    F(6),
+    G(7),
+    H(8);
 
     public static final String COLUMN_OUT_OF_RANGE_EXCEPTION_MESSAGE = "Column 범위를 벗어났습니다.";
     private int column;
