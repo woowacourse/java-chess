a1f14315d2d643ae97b497a6ca3fe8faf1d4f003
giantim <giantim132@gmail.com>
2020-03-31 16:18:59 +0900

    refactor: 컨트롤러 추상화(2)

--- src/main/java/chess/view/ConsoleInputView.java
+++ src/main/java/chess/view/ConsoleInputView.java
@@ -11,7 +11,7 @@ public class ConsoleInputView implements InputView {
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
-        return askGameCommand();
+        return SCANNER.nextLine();
     }
 
     @Override
