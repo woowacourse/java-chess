b616f640cffb5b52cac74f80dfb6f02abd6c99c9
hwangbo <taekwonod714@naver.com>
2020-03-31 09:24:58 +0900

    refactor: Board 클래스 내 하드코딩 제거 및 isBlocked 메소드 로직 일부 변경

--- src/main/java/chess/domains/board/Board.java
+++ src/main/java/chess/domains/board/Board.java
@@ -12,6 +12,11 @@ import java.util.stream.Stream;
 
 public class Board {
     public static final String INVALID_LOCATION_ERR_MSG = "위치를 잘못 입력하였습니다.";
+    public static final String INVALID_ROUTE_ERR_MSG = "말을 뛰어넘을 수 없습니다.";
+    public static final int NOT_BLOCKED = 0;
+    public static final int TWO_KINGS = 2;
+    public static final double SCORE_OF_PAWN_SAME_COLUMN = 0.5;
+    public static final double SCORE_OF_PAWN_IN_SAME_COLUMN1 = SCORE_OF_PAWN_SAME_COLUMN;
     private final Set<PlayingPiece> board = BoardFactory.getBoard();
     private PieceColor teamColor = PieceColor.WHITE;
 
@@ -54,22 +59,17 @@ public class Board {
 
     private void validRoute(List<Position> route) {
         if (isBlocked(route)) {
-            throw new IllegalArgumentException("말을 뛰어넘을 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_ROUTE_ERR_MSG);
         }
     }
 
     private boolean isBlocked(List<Position> route) {
-        int count = (int) route.stream()
+        return route.stream()
                 .map(this::findPiece)
-                .map(PlayingPiece::isBlank)
-                .filter(x -> !x)
-                .count();
-
-        return count != 0;
+                .noneMatch(PlayingPiece::isBlank);
     }
 
     private void exchange(PlayingPiece sourcePiece, PlayingPiece targetPiece) {
-
         this.board.remove(targetPiece);
         this.board.remove(sourcePiece);
         this.board.addAll(sourcePiece.moveTo(targetPiece));
@@ -79,7 +79,7 @@ public class Board {
         int count = (int) board.stream()
                 .filter(PlayingPiece::isKing)
                 .count();
-        return count != 2;
+        return count != TWO_KINGS;
     }
 
     public double calculateScore(PieceColor teamColor) {
@@ -90,7 +90,7 @@ public class Board {
 
         int pawnCount = countPawnInSameColumn(teamColor);
 
-        return score - pawnCount * 0.5;
+        return score - pawnCount * SCORE_OF_PAWN_IN_SAME_COLUMN1;
     }
 
     private int countPawnInSameColumn(PieceColor teamColor) {
