c410311e1cec9c41fb2ab43e0105a9394f9472f0
JunSeong <ebseud6135@naver.com>
2020-03-31 16:04:18 +0900

    [test] d

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,16 +1,26 @@
 package chess;
 
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class WebUIChessApplication {
-	//    public static void main(String[] args) {
-	//        get("/", (req, res) -> {
-	//            Map<String, Object> model = new HashMap<>();
-	//            return render(model, "index.html");
-	//        });
-	//    }
-	//
-	//    private static String render(Map<String, Object> model, String templatePath) {
-	//        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-	//    }
+	public static void main(String[] args) {
+		System.out.println("g학성");
+		staticFileLocation("/templates");
+		get("/", (req, res) -> {
+			Map<String, Object> model = new HashMap<>();
+			return render(model, "index.html");
+		});
+	}
+
+	private static String render(Map<String, Object> model, String templatePath) {
+		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+	}
 }
 
 
