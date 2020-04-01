6b3bcc8c819b3e9f269a4425fedb33c562536f61
Sony <sonypark0204@gmail.com>
2020-04-01 18:50:15 +0900

    refactor: 체스말 데이터 가공 로직을 BoardDto에서 처리

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -3,9 +3,7 @@ package chess;
 import static spark.Spark.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import chess.domain.game.ChessGame;
 import chess.domain.game.state.Ready;
@@ -22,52 +20,43 @@ import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessApplication {
-	public static void main(String[] args) {
-		Spark.staticFileLocation("templates");
-		Map<String, ChessGame> chessGames = new HashMap<>();
-		Gson gson = new GsonBuilder().create();
+    private static final Map<String, ChessGame> CHESS_GAMES = new HashMap<>();
+    private static final Gson GSON = new GsonBuilder().create();
 
-		get("/game/:id", (req, res) -> {
-			ChessGame chessGame = new ChessGame(new Ready());
-			chessGame.start();
-			chessGames.putIfAbsent(req.params(":id"), chessGame);
-			Map<String, Object> model = new HashMap<>();
-			model.put("id", req.params(":id"));
-			return render(model, "index.html");
-		});
+    public static void main(String[] args) {
+        Spark.staticFileLocation("templates");
 
-		get("/board/:id", (req, res) -> {
-			ChessGame chessGame = chessGames.get(req.params(":id"));
-			List<String> pieces = chessGame.board()
-					.getBoard()
-					.values()
-					.stream()
-					.map(piece -> piece.isBlack() ? piece.symbol().toUpperCase() : piece.symbol())
-					.collect(Collectors.toList());
-			return gson.toJson(new ChessGameDto(new BoardDto(pieces), new TurnDto(chessGame.turn()),
-					new StatusDto(new ScoreDto(chessGame.status().getWhiteScore()),
-							new ScoreDto(chessGame.status().getBlackScore())), chessGame.isFinished()));
-		});
+        get("/game/:id", (req, res) -> {
+            ChessGame chessGame = new ChessGame(new Ready());
+            chessGame.start();
+            CHESS_GAMES.putIfAbsent(req.params(":id"), chessGame);
+            Map<String, Object> model = new HashMap<>();
+            model.put("id", req.params(":id"));
+            return render(model, "index.html");
+        });
 
-		post("/move/:id", (req, res) -> {
-			ChessGame chessGame = chessGames.get(req.params(":id"));
-			Map<String, Double> data = gson.fromJson(req.body(), Map.class);
-			Position source = Position.of(data.get("sx").intValue(), data.get("sy").intValue());
-			Position target = Position.of(data.get("tx").intValue(), data.get("ty").intValue());
-			chessGame.move(source, target);
-			List<String> pieces = chessGame.board()
-					.getBoard()
-					.values()
-					.stream()
-					.map(piece -> piece.isBlack() ? piece.symbol().toUpperCase() : piece.symbol())
-					.collect(Collectors.toList());
-			return gson.toJson(new ChessGameDto(new BoardDto(pieces), new TurnDto(chessGame.turn()),
-					new StatusDto(new ScoreDto(chessGame.status().getWhiteScore()),
-							new ScoreDto(chessGame.status().getBlackScore())), chessGame.isFinished()));
-		});
-	}
+        get("/board/:id", (req, res) -> {
+            ChessGame chessGame = CHESS_GAMES.get(req.params(":id"));
+            return responseChessGame(chessGame);
+        });
 
-	private static String render(Map<String, Object> model, String templatePath) {
-		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-	}
+        post("/move/:id", (req, res) -> {
+            ChessGame chessGame = CHESS_GAMES.get(req.params(":id"));
+            Map<String, Double> data = GSON.fromJson(req.body(), Map.class);
+            Position source = Position.of(data.get("sx").intValue(), data.get("sy").intValue());
+            Position target = Position.of(data.get("tx").intValue(), data.get("ty").intValue());
+            chessGame.move(source, target);
+            return responseChessGame(chessGame);
+        });
+    }
+
+    private static String responseChessGame(ChessGame chessGame) {
+        return GSON.toJson(new ChessGameDto(new BoardDto(chessGame.board()), new TurnDto(chessGame.turn()),
+            new StatusDto(new ScoreDto(chessGame.status().getWhiteScore()),
+                new ScoreDto(chessGame.status().getBlackScore())), chessGame.isFinished()));
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
 }
--- src/main/java/chess/dto/BoardDto.java
+++ src/main/java/chess/dto/BoardDto.java
@@ -1,19 +1,26 @@
 package chess.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import chess.domain.game.Board;
 
 public class BoardDto {
-	private List<String> board;
+    private List<String> board;
 
-	public BoardDto(List<String> board) {
-		this.board = board;
-	}
+    public BoardDto(Board board) {
+        this.board = board.getBoard()
+            .values()
+            .stream()
+            .map(piece -> piece.isBlack() ? piece.symbol().toUpperCase() : piece.symbol())
+            .collect(Collectors.toList());
+    }
 
-	public List<String> getBoard() {
-		return board;
-	}
+    public List<String> getBoard() {
+        return board;
+    }
 
-	public void setBoard(List<String> board) {
-		this.board = board;
-	}
+    public void setBoard(List<String> board) {
+        this.board = board;
+    }
 }
