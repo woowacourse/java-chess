acde5d006b287e2f3bc03da06f6092d9d9ed5312
taejinseok <stj930@gmail.com>
2020-04-01 17:07:39 +0900

    feat: 웹 UI 추가, 게임 시작시 , 게임 판 초기화, 점수판 갱신

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -10,10 +10,21 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessApplication {
 	public static void main(String[] args) {
+		port(8080);
+		staticFiles.location("/templates");
+
 		get("/", (req, res) -> {
 			Map<String, Object> model = new HashMap<>();
 			return render(model, "index.html");
 		});
+
+		get("/chess/start", (req, res) ->
+			"[{\"position\":{\"row\": 4, \"col\": 3}, \"piece\": {\"symbol\": \"p\", \"team\": \"black\"}},{ \"position\": {\"row\": 3, \"col\": 3},\"piece\":{\"symbol\":\"\",\"team\":\"empty\"}}]"
+		);
+
+		get("/chess/record", (req, res) ->
+			"[{\"team\":\"black\",\"score\":35.5},{\"team\":\"white\",\"score\":25}]"
+		);
 	}
 
 	private static String render(Map<String, Object> model, String templatePath) {
