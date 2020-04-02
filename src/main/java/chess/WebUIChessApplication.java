1902a039399a13831f69497e4233f76c38d05f05
lalize <printf@kakao.com>
2020-04-02 17:51:51 +0900

    feat: 방 목록 UI 추가 및 서버 연결

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -3,6 +3,7 @@ package chess;
 import static spark.Spark.*;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import chess.domain.game.ChessGame;
@@ -13,7 +14,6 @@ import chess.domain.piece.exception.NotMovableException;
 import chess.dto.BoardDto;
 import chess.dto.ChessGameDto;
 import chess.dto.ResponseDto;
-import chess.dto.ScoreDto;
 import chess.dto.StatusDto;
 import chess.dto.TurnDto;
 import com.google.gson.Gson;
@@ -23,54 +23,70 @@ import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessApplication {
-    private static final Map<String, ChessGame> CHESS_GAMES = new HashMap<>();
-    private static final Gson GSON = new GsonBuilder().create();
+	private static final Map<String, ChessGame> CHESS_GAMES = new LinkedHashMap<>();
+	private static final Gson GSON = new GsonBuilder().create();
+	private static int nextId = 1;
 
-    public static void main(String[] args) {
-        Spark.staticFileLocation("templates");
+	public static void main(String[] args) {
+		Spark.staticFileLocation("assets");
 
-        get("/game/:id", (req, res) -> {
-            ChessGame chessGame = new ChessGame(new Ready());
-            chessGame.start();
-            CHESS_GAMES.putIfAbsent(req.params(":id"), chessGame);
-            Map<String, Object> model = new HashMap<>();
-            model.put("id", req.params(":id"));
-            return render(model, "index.html");
-        });
+		get("/games", (req, res) -> GSON.toJson(new ResponseDto(ResponseDto.SUCCESS, CHESS_GAMES.keySet())));
 
-        get("/board/:id", (req, res) -> {
-            ChessGame chessGame = CHESS_GAMES.get(req.params(":id"));
-            return responseChessGame(chessGame);
-        });
+		get("/", (req, res) -> render(new HashMap<>(), "index.html"));
 
-        post("/move/:id", (req, res) -> {
-            ChessGame chessGame = CHESS_GAMES.get(req.params(":id"));
-            Map<String, Double> data = GSON.fromJson(req.body(), Map.class);
-            Position source = Position.of(data.get("sx").intValue(), data.get("sy").intValue());
-            Position target = Position.of(data.get("tx").intValue(), data.get("ty").intValue());
-            try {
-                chessGame.move(source, target);
-            } catch (NotMovableException | InvalidTurnException e) {
-                return GSON.toJson(new ResponseDto(ResponseDto.FAIL, e.getMessage()));
-            }
-            return responseChessGame(chessGame);
-        });
+		get("/game/:id", (req, res) -> {
+			if (CHESS_GAMES.containsKey(req.params(":id"))) {
+				Map<String, Object> model = new HashMap<>();
+				model.put("id", req.params(":id"));
+				return render(model, "game.html");
+			}
+			return "<script>location.replace('/')</script>";
+		});
 
-        post("/restart/:id", (req, res) -> {
-            CHESS_GAMES.remove(req.params(":id"));
-            return GSON.toJson(new ResponseDto(ResponseDto.SUCCESS, null));
-        });
-    }
+		notFound("<script>location.replace('/')</script>");
 
-    private static String responseChessGame(ChessGame chessGame) {
-        return GSON.toJson(
-            new ResponseDto(ResponseDto.SUCCESS,
-                new ChessGameDto(new BoardDto(chessGame.board()), new TurnDto(chessGame.turn()),
-                    new StatusDto(chessGame.status().getWhiteScore(), chessGame.status().getBlackScore(),
-                        chessGame.status().getWinner()), chessGame.isFinished())));
-    }
+		get("/board/:id", (req, res) -> {
+			ChessGame chessGame = CHESS_GAMES.get(req.params(":id"));
+			return responseChessGame(chessGame);
+		});
 
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-    }
+		post("/move/:id", (req, res) -> {
+			ChessGame chessGame = CHESS_GAMES.get(req.params(":id"));
+			Map<String, Double> data = GSON.fromJson(req.body(), Map.class);
+			Position source = Position.of(data.get("sx").intValue(), data.get("sy").intValue());
+			Position target = Position.of(data.get("tx").intValue(), data.get("ty").intValue());
+			try {
+				chessGame.move(source, target);
+			} catch (NotMovableException | IllegalArgumentException e) {
+				return GSON.toJson(new ResponseDto(ResponseDto.FAIL, "이동할 수 없는 위치입니다."));
+			} catch (InvalidTurnException e) {
+				return GSON.toJson(new ResponseDto(ResponseDto.FAIL, chessGame.turn().getColor() + "의 턴입니다."));
+			}
+			return responseChessGame(chessGame);
+		});
+
+		post("/restart/:id", (req, res) -> {
+			CHESS_GAMES.remove(req.params(":id"));
+			return GSON.toJson(new ResponseDto(ResponseDto.SUCCESS, null));
+		});
+
+		post("/create", (req, res) -> {
+			ChessGame chessGame = new ChessGame(new Ready());
+			chessGame.start();
+			CHESS_GAMES.put(String.valueOf(nextId), chessGame);
+			return GSON.toJson(new ResponseDto(ResponseDto.SUCCESS, nextId++));
+		});
+	}
+
+	private static String responseChessGame(ChessGame chessGame) {
+		return GSON.toJson(
+				new ResponseDto(ResponseDto.SUCCESS,
+						new ChessGameDto(new BoardDto(chessGame.board()), new TurnDto(chessGame.turn()),
+								new StatusDto(chessGame.status().getWhiteScore(), chessGame.status().getBlackScore(),
+										chessGame.status().getWinner()), chessGame.isFinished())));
+	}
+
+	private static String render(Map<String, Object> model, String templatePath) {
+		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+	}
 }
