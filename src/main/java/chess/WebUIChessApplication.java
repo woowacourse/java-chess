00be0e2d3cf9afa32afc7b1df8bf060b8af813f1
kuenhwi <kueniyam@naver.com>
2020-04-02 16:57:23 +0900

    feat: SparkJava 테스트

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -5,11 +5,20 @@ import domain.state.Ended;
 import domain.state.State;
 import domain.pieces.Pieces;
 import domain.pieces.StartPieces;
+import spark.Spark;
 import view.InputView;
 import view.OutputView;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
+        Spark.port(8080);
+
+        Spark.get("/helloworld", (req, res) -> {
+            return "Hello world";
+        });
+    }
+
+    public static void main2(String[] args) {
         OutputView.printStart();
         Pieces startPieces = new Pieces(new StartPieces().getInstance());
         State state = new Ended(startPieces);
