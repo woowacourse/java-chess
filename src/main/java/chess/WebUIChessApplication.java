6191ab1f9cc7841df60d860010e535bef26a06f2
hwangbo <taekwonod714@naver.com>
2020-04-02 16:48:40 +0900

    feat: Web UI 초기화면, start 후 화면 구현

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,22 +1,83 @@
-//package chess;
-//
-//import spark.ModelAndView;
-//import spark.template.handlebars.HandlebarsTemplateEngine;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import static spark.Spark.get;
-//
-//public class WebUIChessApplication {
-//    public static void main(String[] args) {
-//        get("/", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return render(model, "index.html");
-//        });
-//    }
-//
-//    private static String render(Map<String, Object> model, String templatePath) {
-//        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-//    }
-//}
+package chess;
+
+import chess.domains.board.Board;
+import chess.domains.piece.Piece;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class WebUIChessApplication {
+    public static void main(String[] args) {
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "index.html");
+        });
+
+        get("/start", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            Board board = new Board();
+            List<Piece> pieces = board.showBoard();
+            List<String> pieceCodes = convertView(pieces);
+            model.put("pieces", pieceCodes);
+            return render(model, "index.html");
+        });
+    }
+
+    private static List<String> convertView(List<Piece> pieces) {
+        List<String> pieceCodes = new ArrayList<>();
+        for (Piece piece : pieces) {
+            switch (piece.name()) {
+                case "r":
+                    pieceCodes.add("♖");
+                    break;
+                case "n":
+                    pieceCodes.add("♘");
+                    break;
+                case "b":
+                    pieceCodes.add("♗");
+                    break;
+                case "k":
+                    pieceCodes.add("♔");
+                    break;
+                case "q":
+                    pieceCodes.add("♕");
+                    break;
+                case "p":
+                    pieceCodes.add("♙");
+                    break;
+                case "R":
+                    pieceCodes.add("♜");
+                    break;
+                case "N":
+                    pieceCodes.add("♞");
+                    break;
+                case "B":
+                    pieceCodes.add("♝");
+                    break;
+                case "K":
+                    pieceCodes.add("♚");
+                    break;
+                case "Q":
+                    pieceCodes.add("♛");
+                    break;
+                case "P":
+                    pieceCodes.add("♟");
+                    break;
+                case ".":
+                    pieceCodes.add("");
+                    break;
+            }
+        }
+        return pieceCodes;
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+}
