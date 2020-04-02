199a44bcad4aafb45c4a6055cf31c05c5c4dbf39
aegis <aegis1920@gmail.com>
2020-04-02 18:39:43 +0900

    refactor : ChessBoard 클래스에서 GameManager로 네이밍 변경사항을 PieceScore에 적용

--- src/main/java/chess/domain/PieceScore.java
+++ src/main/java/chess/domain/PieceScore.java
@@ -2,7 +2,6 @@ package chess.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -35,20 +34,20 @@ public enum PieceScore {
 			.orElseThrow(() -> new IllegalArgumentException("해당하는 Piece가 Null입니다!"));
 	}
 
-	public static double calculateByColor(Map<Position, Piece> chessBoard, Color color) {
+	public static double calculateByColor(GameManager gameManager, Color color) {
 		double teamScore = 0;
 
 		for (File file : File.values()) {
-			teamScore += sumPieceScoreExceptPawn(color, file, chessBoard);
-			teamScore += sumPawnScore(pawnsOfColumn(color, file, chessBoard));
+			teamScore += sumPieceScoreExceptPawn(color, file, gameManager);
+			teamScore += sumPawnScore(pawnsOfColumn(color, file, gameManager));
 		}
 
 		return teamScore;
 	}
 
-	private static List<Piece> pawnsOfColumn(Color color, File file, Map<Position, Piece> chessBoard) {
+	private static List<Piece> pawnsOfColumn(Color color, File file, GameManager gameManager) {
 		return Arrays.stream(Rank.values())
-			.map(rank -> chessBoard.get(Position.of(file, rank)))
+			.map(rank -> gameManager.getPiece(Position.of(file, rank)))
 			.filter(Objects::nonNull)
 			.filter(piece -> piece.isSameColor(color))
 			.filter(Piece::isPawn)
@@ -56,9 +55,9 @@ public enum PieceScore {
 
 	}
 
-	private static double sumPieceScoreExceptPawn(Color color, File file, Map<Position, Piece> chessBoard) {
+	private static double sumPieceScoreExceptPawn(Color color, File file, GameManager gameManager) {
 		return Arrays.stream(Rank.values())
-			.map(rank -> chessBoard.get(Position.of(file, rank)))
+			.map(rank -> gameManager.getPiece(Position.of(file, rank)))
 			.filter(Objects::nonNull)
 			.filter(piece -> piece.isSameColor(color))
 			.filter(piece -> !piece.isPawn())
