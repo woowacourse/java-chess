9f563bad428a49af50dc0adb29fb0096c63c28a8
JunSeong <ebseud6135@naver.com>
2020-04-02 23:06:36 +0900

    [Refactor] ChessGame 받아오기

--- src/main/java/chess/domain/chessgame/ChessGame.java
+++ src/main/java/chess/domain/chessgame/ChessGame.java
@@ -1,79 +1,51 @@
 package chess.domain.chessgame;
 
-import chess.domain.Result;
-import chess.domain.Status;
-import chess.domain.Team;
-import chess.domain.Turn;
 import chess.domain.chessboard.ChessBoard;
-import chess.domain.position.Position;
 import chess.factory.BoardFactory;
 import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessGame {
-	private static final String END_COMMAND = "end";
-
 	private final ChessBoard chessBoard;
-	private final Turn turn;
-	private ChessMenu chessMenu;
+	private Command command;
 
 	public ChessGame() {
 		this.chessBoard = BoardFactory.createBoard();
-		this.turn = new Turn();
 	}
 
 	public void play() {
 		OutputView.printRule();
-		decideMenu();
-		chessMenu.validateStart();
-		while (chessMenu.isNotEnd()) {
+		initMenu();
+		command.validateStart();
+		OutputView.printBoard(chessBoard);
+
+		while (command.isNotEnd() && chessBoard.isLiveBothKing()) {
+			initMenu();
 			playRound();
+		}
+		OutputView.printEndGame();
+	}
+
+	private void initMenu() {
+		while (isNotAllowedMenu());
+	}
+
+	private boolean isNotAllowedMenu() {
+		try {
+			command = new Command(InputView.inputMenu());
+			return false;
+		} catch (IllegalArgumentException | UnsupportedOperationException e) {
+			OutputView.printErrorMessage(e);
+			return true;
 		}
 	}
 
 	private void playRound() {
 		try {
-			proceed();
+			command.execute(chessBoard);
 			OutputView.printBoard(chessBoard);
-			decideMenu();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			OutputView.printErrorMessage(e);
-			decideMenu();
 		}
 	}
-
-	private void decideMenu() {
-		if (isDieKing()) {
-			OutputView.printEndGame();
-			chessMenu = new ChessMenu(END_COMMAND);
-			return;
-		}
-		try {
-			chessMenu = new ChessMenu(InputView.inputMenu());
-		} catch (IllegalArgumentException | UnsupportedOperationException e) {
-			OutputView.printErrorMessage(e);
-			decideMenu();
-		}
-	}
-
-	private void proceed() {
-		if (chessMenu.isMove()) {
-			Position startPosition = chessMenu.createStartPosition();
-			Position targetPosition = chessMenu.createTargetPosition();
-			turn.validateTurn(chessBoard.findByPosition(startPosition));
-			chessBoard.move(startPosition, targetPosition);
-			turn.changeTurn();
-		}
-
-		if (chessMenu.isStatus()) {
-			Status status = new Status(chessBoard.getRows());
-			Result result = status.getResult();
-			OutputView.printResult(result);
-		}
-	}
-
-	private boolean isDieKing() {
-		return chessBoard.isDieKing(Team.BLACK) || chessBoard.isDieKing(Team.WHITE);
-	}
-
 }
