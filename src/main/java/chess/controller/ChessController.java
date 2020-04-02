f0378617dc92b2d5701354b3def38747b6b790bd
kouz <lastpuzly@gmail.com>
2020-04-02 15:20:57 +0900

    feat: Command 구현

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,7 +1,5 @@
 package chess.controller;
 
-import java.util.List;
-
 import chess.domain.Status;
 import chess.domain.board.Board;
 import chess.domain.piece.Team;
@@ -11,15 +9,6 @@ import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessController {
-	private static final String START_COMMAND = "start";
-	private static final String END_COMMAND = "end";
-	private static final String MOVE_COMMAND = "move";
-	private static final String STATUS_COMMAND = "status";
-	private static final String DELIMITER = " ";
-	private static final List<String> FIRST_COMMANDS = List.of(START_COMMAND, END_COMMAND);
-	private static final List<String> PLAY_COMMANDS = List.of(MOVE_COMMAND, STATUS_COMMAND);
-	private static final int COMMAND_INDEX = 0;
-
 	private final ChessService service;
 	private final Board board;
 	private Team team;
@@ -32,12 +21,12 @@ public class ChessController {
 
 	public void start() {
 		OutputView.printInitialMessage();
-		String input = InputView.inputStartOrEnd();
+		Command command = Command.of(InputView.inputStartOrEnd());
 
-		if (!FIRST_COMMANDS.contains(input)) {
+		if (command.isNotFirst()) {
 			throw new IllegalArgumentException("잘못된 명령어 입력입니다.");
 		}
-		if (END_COMMAND.equals(input)) {
+		if (command.isEnd()) {
 			exit();
 		}
 
@@ -50,12 +39,12 @@ public class ChessController {
 
 	public void playTurn() {
 		String input = InputView.inputMoveOrStatus();
-		List<String> infos = List.of(input.split(DELIMITER));
+		Command command = Command.of(input);
 
-		if (!PLAY_COMMANDS.contains(infos.get(COMMAND_INDEX))) {
+		if (command.isNotPlay()) {
 			throw new IllegalArgumentException("잘못된 명령어 입력입니다.");
 		}
-		if (STATUS_COMMAND.equals(input)) {
+		if (command.isStatus()) {
 			status();
 		}
 
