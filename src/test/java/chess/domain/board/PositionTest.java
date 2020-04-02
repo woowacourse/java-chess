4aa0f5c470959b143ae90d4b5fec070f59a78ed0
aegis <aegis1920@gmail.com>
2020-04-02 19:28:55 +0900

    refactor : PositionTest 및 PawnTest 의 테스트 코드 추가

--- src/test/java/chess/domain/board/PositionTest.java
+++ src/test/java/chess/domain/board/PositionTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import chess.domain.Color;
+
 public class PositionTest {
 	@DisplayName("of 메서드에 대해서 File과 Rank가 같을 때 하나의 Position 인스턴스만 생성되는지 확인")
 	@Test
@@ -20,5 +22,11 @@ public class PositionTest {
 		assertThatThrownBy(() -> {
 			Position.of("a9");
 		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void isInitialPawnPosition() {
+		assertThat(Position.of("a2").isInitialPawnPosition(Color.WHITE)).isTrue();
+		assertThat(Position.of("b7").isInitialPawnPosition(Color.BLACK)).isTrue();
 	}
 }
--- src/test/java/chess/domain/piece/PawnTest.java
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -1,18 +1,76 @@
 package chess.domain.piece;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import chess.domain.Color;
+import chess.domain.GameManager;
+import chess.domain.board.Position;
+import chess.domain.command.Command;
+
 class PawnTest {
+	Map<Position, Piece> originPiece;
+	Pieces pieces;
+	GameManager gameManager;
 
-	@Test
-	void isSameName() {
+	@BeforeEach
+	void setUp() {
+		originPiece = new HashMap<>();
+		BlankPieceFactory.create(originPiece);
+		pieces = new Pieces(originPiece);
+		gameManager = new GameManager(pieces);
 	}
 
+	@DisplayName("폰이 처음으로 두 칸 갈 수 있는지 하는지 테스트")
 	@Test
-	void movablePositions() {
+	void goTwoBlockTest() {
+		Pawn pawn = new Pawn(Color.WHITE, "p");
+		pieces.addPiece(Position.of("b2"), pawn);
+
+		gameManager.moveFromTo(new Command("move b2 b4"));
+
+		assertThat(pieces.getPieceByPosition(Position.of("b4"))).isEqualTo(pawn);
 	}
 
+	@DisplayName("폰이 한 칸을 갈 수 있는지 테스트")
 	@Test
-	void isPawn() {
+	void goOneBlockTest() {
+		Pawn pawn = new Pawn(Color.WHITE, "p");
+		pieces.addPiece(Position.of("b2"), pawn);
+
+		gameManager.moveFromTo(new Command("move b2 b3"));
+
+		assertThat(pieces.getPieceByPosition(Position.of("b3"))).isEqualTo(pawn);
+	}
+
+	@DisplayName("폰이 대각선으로 적군이 있을 때 먹을 수 있는지 테스트")
+	@Test
+	void eatOneBlockTest() {
+		Pawn whitePawn = new Pawn(Color.WHITE, "p");
+		Pawn blackPawn = new Pawn(Color.BLACK, "p");
+
+		pieces.addPiece(Position.of("b2"), whitePawn);
+		pieces.addPiece(Position.of("c3"), blackPawn);
+		pieces.addPiece(Position.of("d2"), blackPawn);
+
+		gameManager.moveFromTo(new Command("move b2 c3"));
+		assertThat(pieces.getPieceByPosition(Position.of("c3"))).isEqualTo(whitePawn);
+	}
+
+	@DisplayName("이외 폰이 갈 수 없는 방향 테스트")
+	@Test
+	void goExceptionTest() {
+		Pawn pawn = new Pawn(Color.WHITE, "p");
+		pieces.addPiece(Position.of("b2"), pawn);
+
+		assertThatThrownBy(() -> {
+			gameManager.moveFromTo(new Command("move b2 b5"));
+		}).isInstanceOf(IllegalArgumentException.class);
 	}
 }
