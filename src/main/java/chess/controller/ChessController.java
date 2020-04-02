19df0bffbe4e3cdbf0cc76809370b96753db3b8b
yerin <forjustice9839>
2020-04-03 05:14:47 +0900

    [refactor] 메서드 분리 및 정리

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 public class ChessController {
 
+    private static final String BLANK = " ";
     ChessBoard chessBoard = new ChessBoard();
 
     public void run() {
@@ -20,59 +21,103 @@ public class ChessController {
         if (!initGame()) {
             return;
         }
-        boolean isEnd = false;
-        while (!isEnd) {
-            isEnd = proceed(chessBoard);
-        }
+        proceed();
         OutputView.printWinner(TeamScore.getWinners(chessBoard.getChessBoard()));
     }
 
-    private static boolean proceed(ChessBoard chessBoard) {
-        List<Square> moveSquares = new ArrayList<>();
-        List<String> stateInput = Arrays.asList(InputView.inputState().split(" "));
-        if (isNotEndState(chessBoard, moveSquares, stateInput)) {
+    private void proceed() {
+        while (!isEndState(stateInput())) {
+            OutputView.printChessBoard(chessBoard.getChessBoard());
+        }
+    }
+
+    private List<String> stateInput() {
+        return Arrays.asList(InputView.inputState().split(BLANK));
+    }
+
+    private boolean isEndState(List<String> stateInput) {
+        GameState gameState;
+        stateInput = getStateInput(stateInput);
+        gameState = GameState.of(stateInput.get(0));
+        if(isStartRepeat(gameState)){
+            return false;
+        }
+        if(isInputStateStatus(gameState)){
+            OutputView.printStatus(TeamScore.calculateTeamScore(chessBoard.getChessBoard()));
+            OutputView.printWinner(TeamScore.getWinners(chessBoard.getChessBoard()));
+            return false;
+        }
+        if(isInputStateEnd(gameState)){
+            OutputView.printStatus(TeamScore.calculateTeamScore(this.chessBoard.getChessBoard()));
             return true;
         }
-        OutputView.printChessBoard(chessBoard.getChessBoard());
+        if (isInputStateMove(gameState) && isRightMoveInput(stateInput)) {
+            move(getMoveSquare(stateInput));
+            return this.chessBoard.isKingCaptured();
+        }
         return false;
     }
 
-    private static boolean isNotEndState(ChessBoard chessBoard, List<Square> moveSquares, List<String> stateInput) {
-        if (isInputStateMove(stateInput)) {
-            moveSquares = getMoveSquare(stateInput);
+    private List<String> getStateInput(List<String> stateInput) {
+        while(!validateGameState(stateInput.get(0))) {
+            stateInput = stateInput();
         }
-        if (moveSquares == null) {
-            OutputView.printStatus(TeamScore.calculateTeamScore(chessBoard.getChessBoard()));
-            return true;
-        }
-        GameState gameState = GameState.of(stateInput.get(0));
-        checkStartRepeatOrError(gameState);
-        if (gameState.equals(GameState.END)) {
-            OutputView.printStatus(TeamScore.calculateTeamScore(chessBoard.getChessBoard()));
-            return true;
-        }
-        checkMoveOrStatus(chessBoard, moveSquares, gameState);
-        return chessBoard.isKingCaptured();
+        return stateInput;
     }
 
-    private static void checkMoveOrStatus(ChessBoard chessBoard, List<Square> moveSquares, GameState gameState) {
-        if (gameState.equals(GameState.MOVE)) {
-            move(chessBoard, moveSquares);
-        }
-        if (gameState.equals(GameState.STATUS)) {
-            OutputView.printStatus(TeamScore.calculateTeamScore(chessBoard.getChessBoard()));
-            OutputView.printWinner(TeamScore.getWinners(chessBoard.getChessBoard()));
-        }
+    private boolean isInputStateEnd(GameState gameState) {
+        return GameState.END.equals(gameState);
     }
 
-    private static void move(ChessBoard chessBoard, List<Square> moveSquares) {
+    private boolean isInputStateStatus(GameState gameState) {
+        return GameState.STATUS.equals(gameState);
+    }
+
+    private boolean validateGameState(String gameState) {
+        if(gameState == null || gameState.isEmpty()){
+            throw new IllegalArgumentException("입력이 존재하지 않습니다.");
+        }
+        return !GameState.of(gameState).equals(GameState.ERROR);
+    }
+
+    private boolean isRightMoveInput(List<String> stateInput) {
+        if (stateInput.size() != 3) {
+            return false;
+        }
+        try {
+            Square.of(stateInput.get(1));
+            Square.of(stateInput.get(2));
+        } catch (IllegalArgumentException e) {
+            OutputView.printInputError();
+            return false;
+        }
+        return true;
+    }
+
+    private void move(List<Square> moveSquares) {
         if (!chessBoard.movePiece(moveSquares)) {
             OutputView.printCanNotMoveMessage();
         }
     }
 
-    private static boolean isInputStateMove(List<String> stateInput) {
-        return stateInput.size() != 1;
+    private static boolean isInputStateMove(GameState gameState) {
+        return GameState.MOVE.equals(gameState);
+    }
+
+
+    private static List<Square> getMoveSquare(List<String> input) {
+        List<Square> moveSquares = new ArrayList<>();
+        moveSquares.add(Square.of(input.get(1)));
+        moveSquares.add(Square.of(input.get(2)));
+        return moveSquares;
+    }
+
+    private static boolean isStartRepeat(GameState gameState) {
+        if (gameState.equals(GameState.START)) {
+            OutputView.printAlreadyGameStartedMessage();
+            return true;
+        }
+        return false;
     }
 
     private boolean initGame() {
@@ -83,35 +128,10 @@ public class ChessController {
         return true;
     }
 
-    private static List<Square> getMoveSquare(List<String> input) {
-        List<Square> moveSquares = new ArrayList<>();
-        try {
-            moveSquares.add(Square.of(input.get(1)));
-            moveSquares.add(Square.of(input.get(2)));
-        } catch (Exception e) {
-            OutputView.printInputError();
-            String inputState = InputView.inputState();
-            if (GameState.of(inputState).equals(GameState.END)) {
-                return null;
-            }
-            moveSquares = getMoveSquare(Arrays.asList(inputState.split(" ")));
-        }
-        return moveSquares;
-    }
-
-    private static void checkStartRepeatOrError(GameState gameState) {
-        if (gameState.equals(GameState.START)) {
-            OutputView.printAlreadyGameStartedMessage();
-        }
-        if (gameState.equals(GameState.ERROR)) {
-            OutputView.printInputError();
-        }
-    }
-
     private static boolean startChessGame() {
         String input = InputView.inputState();
-        while (!GameState.of(input).equals(GameState.START)) {
-            if (GameState.of(input).equals(GameState.END)) {
+        while (!GameState.START.equals(GameState.of(input))) {
+            if (GameState.END.equals((GameState.of(input)))) {
                 return false;
             }
             OutputView.printStartGameFirstMessage();
