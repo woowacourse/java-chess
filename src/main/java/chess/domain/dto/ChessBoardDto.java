b4784b749e2f4b35a3afb3137fcfb5cdb9724cf7
begaonnuri <turtlenam0928@gmail.com>
2020-04-03 15:30:21 +0900

    feat: ChessBoardDto 추가
    - ChessBoardDto를 통해 뷰에 체스판을 전달하도록 변경

---
+++ src/main/java/chess/domain/dto/ChessBoardDto.java
@@ -0,0 +1,44 @@
+package chess.domain.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.domain.ChessBoard;
+import chess.domain.piece.Piece;
+
+public class ChessBoardDto {
+	private List<List<String>> boardDto;
+
+	private ChessBoardDto(List<List<String>> boardDto) {
+		this.boardDto = boardDto;
+	}
+
+	public static ChessBoardDto of(ChessBoard board) {
+		List<List<String>> initBoard = initBoard();
+		deployPiece(board.getPieces(), initBoard);
+		return new ChessBoardDto(initBoard);
+	}
+
+	private static void deployPiece(List<Piece> board, List<List<String>> initBoard) {
+		for (Piece piece : board) {
+			initBoard.get(8 - piece.getPosition().getRow().getSymbol())
+					.set(piece.getPosition().getCol().getValue() - 1, piece.getName());
+		}
+	}
+
+	private static List<List<String>> initBoard() {
+		List<List<String>> board = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			List<String> emptyRow = new ArrayList<>();
+			for (int j = 0; j < 8; j++) {
+				emptyRow.add(".");
+			}
+			board.add(emptyRow);
+		}
+		return board;
+	}
+
+	public List<List<String>> getBoardDto() {
+		return boardDto;
+	}
+}
