4351aaa3c9a14b466c315b3291abe3d590e3180a
Eun Seok <jeseok95@gmail.com>
2020-04-04 01:54:41 +0900

    [feat] UI form tag 로 수정 후 말이 이동하도록 script 구현
    추가한 webFont 에서 인식하지 못하는 url 이 생겨 ignore setting 해줘야 오류가 안생김

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.domain.chessBoard.ChessBoard;
+import chess.domain.chessBoard.ChessBoardFactory;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -11,11 +13,18 @@ import static spark.Spark.staticFileLocation;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
-        staticFileLocation("templates");
+        staticFileLocation("/templates");
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "index.html");
+        });
 
         get("/chess", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
+            ChessBoard chessBoard = new ChessBoard(ChessBoardFactory.create());
+            model.put("board", chessBoard.getChessBoard());
+            return render(model, "contents/chess.html");
         });
     }
 
