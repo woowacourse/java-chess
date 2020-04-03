b805557c4edb7bc8d1e02c3fac84ff7d3b28ecd6
icyMojito <dev.icymojito@gmail.com>
2020-04-03 22:08:26 +0900

    feat : GameResult 클래스가 승리한 색과 패배한 색의 정보를 가지도록 함
    GameResult가 미리 승리한 색 & 패배한 색 정보를 갖고 있게 하여 OutputView가 우승한 색에 대해 확인하는 과정 없이 바로 출력할 수 있도록 함

--- src/main/java/chess/domain/GameResult.java
+++ src/main/java/chess/domain/GameResult.java
@@ -1,25 +1,45 @@
 package chess.domain;
 
 public class GameResult {
-    private final boolean isBlackKingKilled;
-    private final boolean isWhiteKingKilled;
+    private final String winner;
+    private final String loser;
     private final double aliveBlackPieceScoreSum;
     private final double aliveWhitePieceScoreSum;
 
     public GameResult(boolean isBlackKingKilled, boolean isWhiteKingKilled, double aliveBlackPieceScoreSum,
                       double aliveWhitePieceScoreSum) {
-        this.isBlackKingKilled = isBlackKingKilled;
-        this.isWhiteKingKilled = isWhiteKingKilled;
+        this.winner = getWinner(isBlackKingKilled, isWhiteKingKilled);
+        this.loser = getLoser(isBlackKingKilled, isWhiteKingKilled);
         this.aliveBlackPieceScoreSum = aliveBlackPieceScoreSum;
         this.aliveWhitePieceScoreSum = aliveWhitePieceScoreSum;
     }
 
-    public boolean isBlackWin() {
-        return isWhiteKingKilled;
+    private String getWinner(boolean isBlackKingKilled, boolean isWhiteKingKilled) {
+        if (isBlackKingKilled) {
+            return "White";
+        }
+        if (isWhiteKingKilled) {
+            return "Black";
+        }
+        return "";
     }
 
-    public boolean isWhiteWin() {
-        return isBlackKingKilled;
+    private String getLoser(boolean isBlackKingKilled, boolean isWhiteKingKilled) {
+        if (isBlackKingKilled) {
+            return "Black";
+        }
+        if (isWhiteKingKilled) {
+            return "White";
+        }
+        return "";
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public String getLoser() {
+        return loser;
     }
 
     public double getAliveBlackPieceScoreSum() {
