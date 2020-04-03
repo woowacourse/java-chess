2807b3a9482ea085f4ae75131883faa9fb5f5152
taejinseok <stj930@gmail.com>
2020-04-04 01:41:25 +0900

    test: command 테스트 케이스 추가

---
+++ src/test/java/chess/controller/ReadyStateCommandTest.java
@@ -0,0 +1,43 @@
+package chess.controller;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import chess.domain.board.Board;
+import chess.domain.game.Game;
+import chess.domain.state.GameState;
+import chess.domain.state.Ready;
+
+class ReadyStateCommandTest {
+	private Board board;
+	private GameState gameState;
+	private Game game;
+
+	@BeforeEach
+	void setUp() {
+		board = new Board();
+		gameState = new Ready(board);
+		game = new Game(gameState);
+	}
+
+	@DisplayName("게임 준비 상태에서 start, end 명령어를 입력 받는 경우 예외 발생 하지 않는다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"start", "end"})
+	void readyState_EndCommandActionTest(String command) {
+		assertThatCode(() -> Command.action(game, command))
+			.doesNotThrowAnyException();
+	}
+
+	@DisplayName("게임 준비 상태에서 move, status 명령어를 입력 받는 경우 USO 예외 발생 한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"move a1 a3", "status"})
+	void readyStateMove_Status_ActionTest(String invalidCommand) {
+		assertThatThrownBy(() -> Command.action(game, invalidCommand))
+			.isInstanceOf(UnsupportedOperationException.class);
+	}
+}
---
+++ src/test/java/chess/controller/StartedStateCommandTest.java
@@ -0,0 +1,44 @@
+package chess.controller;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import chess.domain.board.Board;
+import chess.domain.game.Game;
+import chess.domain.state.GameState;
+import chess.domain.state.Ready;
+import chess.domain.state.Started;
+
+class StartedStateCommandTest {
+	private Board board;
+	private GameState gameState;
+	private Game game;
+
+	@BeforeEach
+	void setUp() {
+		board = new Board();
+		board.start();
+		gameState = new Started(board);
+		game = new Game(gameState);
+	}
+
+	@DisplayName("게임 중인 상태에서 모든 올바른 형식의 명령어를 입력 받는 경우 예외 발생 하지 않는다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"start", "move a2 a4", "status", "end"})
+	void readyState_EndCommandActionTest(String command) {
+		assertThatCode(() -> Command.action(game, command))
+			.doesNotThrowAnyException();
+	}
+
+	@DisplayName("게임 중인 상태에서 잘못된 형식의 명령어 입력 받는 경우 IllegalArgument 예외 발생 한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"move a7 a9", "statos", "star", "enD"})
+	void readyStateMove_Status_ActionTest(String invalidCommand) {
+		assertThatThrownBy(() -> Command.action(game, invalidCommand))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+}
