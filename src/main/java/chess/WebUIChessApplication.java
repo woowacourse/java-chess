134a45bc2cbf6466481d6844eb45cc7eef0d1e51
ParkDooWon <50234855+ParkDooWon@users.noreply.github.com>
2020-04-03 13:31:10 +0900

    feat : 체스판 UI 제작

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,22 +1,33 @@
 package chess;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import domain.board.Board;
+import domain.board.BoardFactory;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessApplication {
-/*    public static void main(String[] args) {
-        get("/", (req, res) -> {
+    public static void main(String[] args) {
+        staticFiles.location("/static");
+        // get("/", (req, res) -> {
+        //     Map<String, Object> model = new HashMap<>();
+        //     return render(model, "index.html");
+        // });
+
+        post("/users", (req, res) -> {
+            Board board = BoardFactory.create();
+
             Map<String, Object> model = new HashMap<>();
+            model.put("ranks", board.getRanks());
             return render(model, "index.html");
         });
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-    }*/
+    }
 }
