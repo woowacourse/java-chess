a0224be99c0d88493917ea30faa48ff1cdc3b20d
Yeji Ahn <yeppilisa@naver.com>
2020-04-03 11:09:30 +0900

    feature: WebUI 추가

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,18 +1,20 @@
 package chess;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
+            return render(model, "chess-running.html");
         });
     }
 
