0bb6ee506c0c1670b37d47112a03468780051cfb
icyMojito <dev.icymojito@gmail.com>
2020-04-05 02:03:36 +0900

    feat : 웹 체스 게임의 시작 화면 구현
    WebChessController 클래스의 run() 메서드 수정 및
    시작 화면을 보여주는 index.html과 시작 화면에 적용할 indexStyle.css 생성

--- src/main/java/chess/controller/WebChessController.java
+++ src/main/java/chess/controller/WebChessController.java
@@ -6,15 +6,19 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebChessController {
+
     private static String render(Map<String, Object> model, String templatePath) {
         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 
     public void run() {
-        get("/chessGame", (req, res) -> {
+        port(8080);
+        staticFiles.location("/templates");
+
+        get("/start", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
