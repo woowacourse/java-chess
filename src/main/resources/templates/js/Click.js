c3b48fc843c63d906e44f854f7796ef574de2be0
Eun Seok <jeseok95@gmail.com>
2020-04-05 01:17:43 +0900

    [feat] 체스판 움직이도록 js 구현

---
+++ src/main/resources/templates/js/Click.js
@@ -0,0 +1,20 @@
+function onClick(value) {
+    if (document.getElementsByClassName('sourcePosition').length) {
+        return selectDestinationPiece(value);
+    } else {
+        sourcePosition(value);
+    }
+}
+
+function sourcePosition(value) {
+    document.getElementById(value).classList.add('sourcePosition');
+}
+
+
+function selectDestinationPiece(value) {
+    document.getElementById(value).innerHTML = document.getElementsByClassName('sourcePosition')[0].innerHTML;
+    console.log(document.getElementsByClassName('sourcePosition')[0].id);
+
+    document.getElementsByClassName('sourcePosition')[0].innerHTML = "";
+    document.getElementsByClassName('sourcePosition')[0].classList.remove('sourcePosition');
+}
