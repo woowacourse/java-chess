bc227ebf3e3380adaa2e3afb9a754ab2b2c04da0
Eun Seok <jeseok95@gmail.com>
2020-04-05 02:48:15 +0900

    [feat] 체스보드 움직이는 명령 서버와 연결

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -2,6 +2,7 @@ package chess;
 
 import chess.domain.chessBoard.ChessBoard;
 import chess.domain.chessBoard.ChessBoardFactory;
+import chess.domain.position.Position;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -12,19 +13,30 @@ import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 
 public class WebUIChessApplication {
+
+    private static final String MOVE_SUCCESS = "";
+
     public static void main(String[] args) {
         staticFileLocation("/templates");
+        ChessBoard chessBoard = new ChessBoard(ChessBoardFactory.create());
 
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
 
+
         get("/chess", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            ChessBoard chessBoard = new ChessBoard(ChessBoardFactory.create());
-            model.put("board", chessBoard.getChessBoard());
-            return render(model, "contents/chess.html");
+            try {
+                chessBoard.move(Position.of(req.queryParams("source")), Position.of(req.queryParams("target")));
+                if (chessBoard.isCaughtKing()) {
+                    return chessBoard.getPlayerColor().getColor() + "이 승리했습니다!";
+                }
+                return MOVE_SUCCESS;
+            } catch (Exception e) {
+                res.status(403);
+                return e.getMessage();
+            }
         });
     }
 
--- src/main/resources/templates/js/Click.js
+++ src/main/resources/templates/js/Click.js
@@ -12,9 +12,32 @@ function sourcePosition(value) {
 
 
 function selectDestinationPiece(value) {
-    document.getElementById(value).innerHTML = document.getElementsByClassName('sourcePosition')[0].innerHTML;
-    console.log(document.getElementsByClassName('sourcePosition')[0].id);
+    $.ajax({
+        type: "get",
+        url: "/chess",
+        data: {
+            source: document.getElementsByClassName("sourcePosition")[0].id,
+            target: document.getElementById(value).id
+        },
+        dataType: "text",
+        success: function (data, status, XHR) {
+            console.log(status);
+            console.log(XHR.responseText);
+            if (data) {
+                alert(data);
+                return;
+            }
+            document.getElementById(value).innerHTML =
+                document.getElementsByClassName('sourcePosition')[0].innerHTML;
+            console.log(document.getElementsByClassName('sourcePosition')[0].id);
 
-    document.getElementsByClassName('sourcePosition')[0].innerHTML = "";
-    document.getElementsByClassName('sourcePosition')[0].classList.remove('sourcePosition');
+            document.getElementsByClassName('sourcePosition')[0].innerHTML = "";
+            document.getElementsByClassName('sourcePosition')[0].classList.remove('sourcePosition');
+        },
+        error: function (jqXHR) {
+            document.getElementsByClassName('sourcePosition')[0].classList.remove('sourcePosition');
+            console.log('error');
+            alert(jqXHR.responseText);
+        }
+    });
 }
