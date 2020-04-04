1ed68eb8771b8cf7b0d7a86527ff430d72cf0bdd
icyMojito <dev.icymojito@gmail.com>
2020-04-04 19:17:56 +0900

    feat : 웹 전용인 WebUIChessApplication, WebChessController 클래스 생성

---
+++ src/main/java/chess/controller/WebChessController.java
@@ -0,0 +1,22 @@
+package chess.controller;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class WebChessController {
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+
+    public void run() {
+        get("/chessGame", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "index.html");
+        });
+    }
+}
