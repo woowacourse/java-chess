e651b1ce05d50a44e49946a61e49ece108f7a2e3
gunhee lee <dlrjsgml42@naver.com>
2020-04-05 09:00:38 +0900

    feat: HTML/CSS/JS 및 이미지 리소스 생성

---
+++ src/main/resources/public/js/chess.js
@@ -0,0 +1,137 @@
+window.onload = function () {
+    const PIECES = {
+        BLACK_KING: `<img src="../image/king_black.png" class="piece king"/>`,
+        BLACK_QUEEN: `<img src="../image/queen_black.png" class="piece queen"/>`,
+        BLACK_ROOK: `<img src="../image/rook_black.png" class="piece rook"/>`,
+        BLACK_BISHOP: `<img src="../image/bishop_black.png" class="piece bishop"/>`,
+        BLACK_KNIGHT: `<img src="../image/knight_black.png" class="piece knight"/>`,
+        BLACK_PAWN: `<img src="../image/pawn_black.png" class="piece pawn"/>`,
+        WHITE_KING: `<img src="../image/king_white.png" class="piece king"/>`,
+        WHITE_QUEEN: `<img src="../image/queen_white.png" class="piece queen"/>`,
+        WHITE_ROOK: `<img src="../image/rook_white.png" class="piece rook"/>`,
+        WHITE_BISHOP: `<img src="../image/bishop_white.png" class="piece bishop"/>`,
+        WHITE_KNIGHT: `<img src="../image/knight_white.png" class="piece knight"/>`,
+        WHITE_PAWN: `<img src="../image/pawn_white.png" class="piece pawn"/>`,
+    };
+
+    let startPosition = null;
+
+    async function getChessGame() {
+        const response = await fetch("http://localhost:4567/board");
+        return await response.json();
+    }
+
+    function setBoard(board) {
+        document.querySelectorAll(".position").forEach(element => {
+            while (element.lastElementChild) {
+                element.removeChild(element.lastElementChild);
+            }
+        });
+        Object.keys(board)
+            .filter(position => board[position]["pieceType"] !== "BLANK")
+            .forEach(position => {
+                const color = board[position]["color"];
+                const pieceType = board[position]["pieceType"];
+                document.getElementById(position)
+                    .insertAdjacentHTML("beforeend", PIECES[`${color}_${pieceType}`]);
+            });
+    }
+
+    function setTurn(turn) {
+        document.getElementById("turn_box")
+            .innerHTML = '<p id="turn" style="font-size: 36px; padding-top: 20px;">' + turn["color"] + "의 턴입니다." + '</p>';
+    }
+    function setScore(score) {
+        document.getElementById("score")
+            .innerHTML = '<p id="turn" style="font-size: 18px; padding-top: 15px;">' +
+            "WHITE 팀의 점수는 " + score["WHITE"] + "점 입니다.\n" +
+            "BLACK 팀의 점수는 " + score["BLACK"] + "점 입니다." +
+            '</p>';
+    }
+
+    (async function () {
+        const chessGame = await getChessGame();
+        const board = chessGame["board"];
+        const turn = chessGame["turn"];
+        const score = chessGame["score"]["scores"];
+
+        setBoard(board);
+        setTurn(turn);
+        setScore(score);
+    })();
+
+    function chooseFirstPosition(position) {
+        fetch(`http://localhost:4567/start?start=${position}`, {method: "POST"})
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === false) {
+                    alert(data.exception);
+                    return;
+                }
+                startPosition = data.position;
+                const positions = data.movable;
+                positions.forEach(position => {
+                    document.getElementById(position)
+                        .style
+                        .backgroundColor = "rgba(255, 255, 0, 0.2)";
+                });
+                document.getElementById(startPosition)
+                    .style
+                    .backgroundColor = "rgba(255, 0, 0, 0.2)";
+            });
+    }
+
+    function chooseSecondPosition(position) {
+        fetch(`http://localhost:4567/end?end=${position}`, {method: "POST"})
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === false) {
+                    alert(data.exception);
+                    return;
+                }
+                const start = startPosition;
+                const end = data.position;
+                startPosition = null;
+
+                if(start === end) {
+                    alert("이동을 취소합니다.");
+                }
+
+                post_to_url("/board", {"start": start, "end": end});
+            });
+    }
+
+    document.querySelectorAll(".position").forEach(
+        element => {
+            element.addEventListener("click", (e) => {
+                let position = e.currentTarget.id;
+                e.preventDefault();
+                if (startPosition == null) {
+                    //움직일 유닛을 선택할 경우
+                    chooseFirstPosition(position);
+                } else {
+                    //유닛을 선택한 상태에서 움직일 경우
+                    chooseSecondPosition(position);
+                }
+            });
+        }
+    );
+
+    function post_to_url(path, params) {
+        let form = document.createElement("form");
+        form.setAttribute("method", "post");
+        form.setAttribute("action", path);
+
+        for (let key in params) {
+            let hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+
+        document.body.appendChild(form);
+        form.submit();
+    }
+};
