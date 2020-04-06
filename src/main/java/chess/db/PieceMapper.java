65e41e41c4ab07c12f178aa9993349cb9d1e835e
hj <a301dks@naver.com>
2020-04-06 16:54:21 +0900

    refactor: 맵핑을 간결하게 하기 위해 PieceMapper를 enum으로 변경

--- src/main/java/chess/db/PieceMapper.java
+++ src/main/java/chess/db/PieceMapper.java
@@ -1,5 +1,9 @@
 package chess.db;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import chess.domain.piece.Bishop;
 import chess.domain.piece.King;
 import chess.domain.piece.Knight;
@@ -13,50 +17,60 @@ import chess.domain.piece.state.Moved;
 import chess.domain.piece.state.State;
 
 /**
- *    디비의 정보와 체스 말을 매핑해주는 클래스입니다.
+ *    디비의 정보와 체스 말을 매핑해주는 enum입니다.
  *
  *    @author AnHyungJu
  */
-public class PieceMapper {
-	public static Piece mappingBy(String pieceSymbol, String pieceState) {
-		switch (pieceSymbol) {
-			case "♚":
-				return new King(mappingBy(pieceState), "♚");
-			case "♛":
-				return new Queen(mappingBy(pieceState), "♛");
-			case "♞":
-				return new Knight(mappingBy(pieceState), "♞");
-			case "♝":
-				return new Bishop(mappingBy(pieceState), "♝");
-			case "♜":
-				return new Rook(mappingBy(pieceState), "♜");
-			case "♟":
-				return new Pawn(mappingBy(pieceState), "♟");
-			case "♔":
-				return new King(mappingBy(pieceState), "♔");
-			case "♕":
-				return new Queen(mappingBy(pieceState), "♕");
-			case "♘":
-				return new Knight(mappingBy(pieceState), "♘");
-			case "♗":
-				return new Bishop(mappingBy(pieceState), "♗");
-			case "♖":
-				return new Rook(mappingBy(pieceState), "♖");
-			case "♙":
-				return new Pawn(mappingBy(pieceState), "♙");
-		}
-		return null;
+public enum PieceMapper {
+	BLACK_KING("♚", state -> new King(state, "♚")),
+	BLACK_QUEEN("♛", state -> new Queen(state, "♛")),
+	BLACK_KNIGHT("♞", state -> new Knight(state, "♞")),
+	BLACK_BISHOP("♝", state -> new Bishop(state, "♝")),
+	BLACK_ROOK("♜", state -> new Rook(state, "♜")),
+	BLACK_PAWN("♟", state -> new Pawn(state, "♟")),
+
+	WHITE_KING("♔", state -> new King(state, "♔")),
+	WHITE_QUEEN("♕", state -> new Queen(state, "♕")),
+	WHITE_KNIGHT("♘", state -> new Knight(state, "♘")),
+	WHITE_BISHOP("♗", state -> new Bishop(state, "♗")),
+	WHITE_ROOK("♖", state -> new Rook(state, "♖")),
+	WHITE_PAWN("♙", state -> new Pawn(state, "♙"));
+
+	private String symbol;
+	private Function<State, Piece> pieceMapper;
+
+	PieceMapper(String symbol, Function<State, Piece> pieceMapper) {
+		this.symbol = symbol;
+		this.pieceMapper = pieceMapper;
 	}
 
-	private static State mappingBy(String pieceState) {
-		switch (pieceState) {
-			case "Initial":
-				return new Initial();
-			case "Moved":
-				return new Moved();
-			case "Captured":
-				return new Captured();
+	public static Piece mappingBy(String pieceSymbol, String pieceState) {
+		return Arrays.stream(PieceMapper.values())
+			.filter(s -> s.symbol.equals(pieceSymbol))
+			.findFirst()
+			.map(mapper -> mapper.pieceMapper.apply(StateMapper.of(pieceState)))
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Symbol입니다."));
+	}
+
+	private enum StateMapper {
+		INITIAL("Initial", Initial::new),
+		MOVED("Moved", Moved::new),
+		CAPTURED("Captured", Captured::new);
+
+		private String state;
+		private Supplier<State> stateMapper;
+
+		StateMapper(String state, Supplier<State> stateMapper) {
+			this.state = state;
+			this.stateMapper = stateMapper;
 		}
-		return null;
+
+		private static State of(String pieceState) {
+			return Arrays.stream(StateMapper.values())
+				.filter(s -> s.state.equals(pieceState))
+				.findFirst()
+				.map(s -> s.stateMapper.get())
+				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상태입니다."));
+		}
 	}
 }
---
+++ src/test/java/chess/domain/db/PieceMapperTest.java
@@ -0,0 +1,30 @@
+package chess.domain.db;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import chess.db.PieceMapper;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Piece;
+
+/**
+ *    class description
+ *
+ *    @author AnHyungJu
+ */
+public class PieceMapperTest {
+	@DisplayName("화이트 폰 심볼과 이니셜 스테이트가 주어졌을 때 같은 상태를 가진 Pawn이 생성되는지 확인")
+	@Test
+	void mappingTest() {
+		String pieceSymbol = "♙";
+		String pieceState = "Initial";
+
+		Piece piece = PieceMapper.mappingBy(pieceSymbol, pieceState);
+
+		assertThat(piece).isInstanceOf(Pawn.class);
+		assertThat(piece.getState().toString()).isEqualTo(pieceState);
+		assertThat(piece.getSymbol()).isEqualTo(pieceSymbol);
+	}
+}
