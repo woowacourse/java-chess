2410fa67bc450cf384d9eea7e0b9082eff307419
Seyun <ksy90101@gmail.com>
2020-04-06 20:18:08 +0900

    refactor : dao 분리 작업

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -2,6 +2,7 @@ package chess;
 
 import chess.controller.ChessController;
 import chess.controller.dao.ChessBoardDao;
+import chess.controller.dao.GameDao;
 import chess.controller.dto.RequestDto;
 import chess.controller.dto.ResponseDto;
 import chess.domain.game.Command;
@@ -18,6 +19,7 @@ public class WebUIChessApplication {
     private static Gson gson = new Gson();
     private static final ChessController chessController = new ChessController();
     private static final ChessBoardDao chessBoardDao = new ChessBoardDao();
+    private static final GameDao gameDao = new GameDao();
 
     public static void main(String[] args) {
         port(8081);
@@ -31,22 +33,27 @@ public class WebUIChessApplication {
         get("/init", (req, res) -> {
             res.status(200);
             ResponseDto responseDto = null;
-            String state = chessBoardDao.loadState();
-            if (state.equals("playing")) {
-                responseDto = chessBoardDao.loadGamePlaying();
-            } else if (state.equals("end")) {
-                responseDto = chessController.start(new RequestDto(Command.START));
-                chessBoardDao.saveInitGame(responseDto);
-            }
+//            String state = gameDao.findState(roomNumber);
+//            if (state.equals("playing")) {
+//                Map<Position, Piece> positionPieceMap = chessBoardDao.loadGamePlaying(700);
+//            } else if (state.equals("end")) {
+            responseDto = chessController.start(new RequestDto(Command.START));
+            gameDao.saveInitGame(responseDto);
+            int roomNumber = gameDao.findMaxRoomNumber();
+            chessBoardDao.saveInitChessBoard(responseDto.getChessBoardDto(), roomNumber);
+//            }
             return gson.toJson(responseDto);
         });
 
         get("/move", (req, res) -> {
             try {
+                int roomNumber = gameDao.findMaxRoomNumber();
                 RequestDto requestDto = new RequestDto(Command.MOVE, req);
                 ResponseDto responseDto = chessController.move(requestDto);
                 res.status(200);
-                chessBoardDao.saveGameAfterPieceMove(responseDto);
+                gameDao.updateGame(responseDto);
+                chessBoardDao.deleteChessBoard(700);
+                chessBoardDao.saveInitChessBoard(responseDto.getChessBoardDto(), roomNumber);
                 return gson.toJson(responseDto);
             } catch (IllegalArgumentException e) {
                 res.status(400);
@@ -56,7 +63,7 @@ public class WebUIChessApplication {
 
         get("/end", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            chessBoardDao.updateEndState();
+            gameDao.updateEndState(700);
             return render(model, "end.html");
         });
     }
--- src/main/java/chess/domain/game/ChessGame.java
+++ src/main/java/chess/domain/game/ChessGame.java
@@ -1,5 +1,6 @@
 package chess.domain.game;
 
+import chess.controller.dto.ChessBoardDto;
 import chess.controller.dto.RequestDto;
 import chess.controller.dto.ResponseDto;
 
@@ -20,7 +21,7 @@ public class ChessGame {
 
         chessBoard = new ChessBoard(PieceFactory.create());
 
-        return new ResponseDto(chessBoard, chessBoard.createResult(), turn);
+        return new ResponseDto(new ChessBoardDto(chessBoard.getChessBoard()), chessBoard.createResult(), turn);
     }
 
     public ResponseDto move(RequestDto requestDto) {
@@ -29,15 +30,16 @@ public class ChessGame {
         }
 
         chessBoard.move(requestDto.getFrom(), requestDto.getTo());
+
         this.turn = Player.reversePlayer(turn);
-        return new ResponseDto(chessBoard, chessBoard.createResult(), turn);
+        return new ResponseDto(new ChessBoardDto(chessBoard.getChessBoard()), chessBoard.createResult(), turn);
     }
 
     public ResponseDto status(RequestDto requestDto) {
-        return new ResponseDto(chessBoard, chessBoard.createResult(), turn);
+        return new ResponseDto(new ChessBoardDto(chessBoard.getChessBoard()), chessBoard.createResult(), turn);
     }
 
     public ResponseDto end(RequestDto requestDto) {
-        return new ResponseDto(chessBoard, chessBoard.createResult(), turn);
+        return new ResponseDto(new ChessBoardDto(chessBoard.getChessBoard()), chessBoard.createResult(), turn);
     }
 }
