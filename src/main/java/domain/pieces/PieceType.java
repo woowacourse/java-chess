55b4b14776b7c8f638fce1106602110ba2464385
kuenhwi <kueniyam@naver.com>
2020-04-06 20:13:25 +0900

    feat: PieceType name 필드와 ofName 추가

--- src/main/java/domain/pieces/PieceType.java
+++ src/main/java/domain/pieces/PieceType.java
@@ -1,21 +1,32 @@
 package domain.pieces;
 
+import java.util.Arrays;
+
 public enum PieceType {
-	BISHOP(3, "B"),
-	KING(0, "K"),
-	KNIGHT(2.5, "N"),
-	PAWN(2, "P"),
-	QUEEN(9, "Q"),
-	ROOK(5, "R");
+	BISHOP(3, "B", "bishop"),
+	KING(0, "K", "king"),
+	KNIGHT(2.5, "N", "knight"),
+	PAWN(2, "P", "pawn"),
+	QUEEN(9, "Q", "queen"),
+	ROOK(5, "R", "rook");
 
 	public static final int HALF_DIVIDER = 2;
 
 	private final double score;
 	private final String initial;
+	private final String name;
 
-	PieceType(double score, String initial) {
+	PieceType(double score, String initial, String name) {
 		this.score = score;
 		this.initial = initial;
+		this.name = name;
+	}
+
+	public static PieceType ofName(String name) {
+		return Arrays.stream(values())
+				.filter(pieceType -> pieceType.name.equals(name))
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new);
 	}
 
 	public static double getPawnHalfScore() {
@@ -28,5 +39,9 @@ public enum PieceType {
 
 	public String getInitial() {
 		return initial;
+	}
+
+	public String getName() {
+		return name;
 	}
 }
