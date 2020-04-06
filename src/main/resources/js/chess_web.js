4ff59927af93d511d63b2c63d2bf6406f1f8c416
sujin <sujinemilycho@gmail.com>
2020-04-06 11:41:28 +0900

    fix: 게임 시작 전에는 move 버튼 비활성화

--- src/main/resources/js/chess_web.js
+++ src/main/resources/js/chess_web.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     if (window.location.pathname !== "/") {
         document.getElementById("startBtn").innerHTML = ("재시작");
     }
+    if (window.location.pathname === "/") {
+        document.getElementById("moveBtn").setAttribute("disabled", "disabled");
+    }
     document.getElementById("startBtn").onclick = function () {
         window.location.href = '/start';
     };
