5c802e65391742c8a6fba97d06c3147b69153605
kuenhwi <kueniyam@naver.com>
2020-04-07 14:58:19 +0900

    feat: PieceType에 Piece 생성자 추가

--- src/main/java/domain/pieces/PieceType.java
+++ src/main/java/domain/pieces/PieceType.java
@@ -1,25 +1,31 @@
 package domain.pieces;
 
+import domain.point.Coordinate;
+import domain.team.Team;
+
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum PieceType {
-	BISHOP(3, "B", "bishop"),
-	KING(0, "K", "king"),
-	KNIGHT(2.5, "N", "knight"),
-	PAWN(2, "P", "pawn"),
-	QUEEN(9, "Q", "queen"),
-	ROOK(5, "R", "rook");
+	BISHOP(3, "B", "bishop", Bishop::new),
+	KING(0, "K", "king", King::new),
+	KNIGHT(2.5, "N", "knight", Knight::new),
+	PAWN(2, "P", "pawn", Pawn::new),
+	QUEEN(9, "Q", "queen", Queen::new),
+	ROOK(5, "R", "rook", Rook::new);
 
 	public static final int HALF_DIVIDER = 2;
 
 	private final double score;
 	private final String initial;
 	private final String name;
+	private final BiFunction<Team, Coordinate, Piece> factory;
 
-	PieceType(double score, String initial, String name) {
+	PieceType(double score, String initial, String name, BiFunction<Team, Coordinate, Piece> factory) {
 		this.score = score;
 		this.initial = initial;
 		this.name = name;
+		this.factory = factory;
 	}
 
 	public static PieceType ofName(String name) {
@@ -27,6 +33,10 @@ public enum PieceType {
 				.filter(pieceType -> pieceType.name.equals(name))
 				.findFirst()
 				.orElseThrow(IllegalArgumentException::new);
+	}
+
+	public static BiFunction<Team, Coordinate, Piece> getFactoryOfName(String name) {
+		return ofName(name).factory;
 	}
 
 	public static double getPawnHalfScore() {
