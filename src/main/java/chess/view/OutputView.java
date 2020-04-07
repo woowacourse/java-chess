9be6986c315b3958baf54e7819882b074789b99f
junk-sound <wom2277@naver.com>
2020-04-07 15:43:32 +0900

    refac: feedback 2 - allow only 1 depth

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,20 +1,18 @@
 package chess.view;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class OutputView {
     private static final int LINE_START = 1;
     private static final int LINE_END = 8;
 
     public static void printBoard(Map<String, String> board) {
-        for (int y = LINE_END; LINE_START <= y; y--) {
-            for (int x = LINE_START; x <= LINE_END; x++) {
-                String position = String.valueOf(x) + String.valueOf(y);
-                String piece = board.get(position);
-                System.out.print(piece);
-            }
-            System.out.println();
-        }
+        IntStream.rangeClosed(LINE_START, LINE_END)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .forEach(y -> printRow(y, board));
         System.out.println();
     }
 
@@ -25,5 +23,17 @@ public class OutputView {
     public static void printResult(String winner, String loser, String winnerScore, String loserScore) {
         System.out.println(String.format("%s팀이 %s팀을 %s : %s 로 승리하였습니다.", winner, loser, winnerScore, loserScore));
 
+    }
+
+    private static void printRow(int y, Map<String, String> board) {
+        IntStream.rangeClosed(LINE_START, LINE_END)
+                .forEach(x -> printPosition(x, y, board));
+        System.out.println();
+    }
+
+    private static void printPosition(int x, int y, Map<String, String> board) {
+        String position = String.valueOf(x) + String.valueOf(y);
+        String piece = board.get(position);
+        System.out.print(piece);
     }
 }
