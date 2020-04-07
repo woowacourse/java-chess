553e053ec83225db715b24cca2a69cbac8a9220a
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-04-07 19:50:39 +0900

    refactor: 사용하지 않는 파일 제거

--- src/main/java/chess/piece/King.java
+++
@@ -1,9 +0,0 @@
-package chess.piece;
-
-import chess.validator.KingMoveValidator;
-
-public class King extends Piece {
-    public King(Team team) {
-        super(team, "K", new KingMoveValidator(), 0);
-    }
-}
