e61225bf3b3eb1b0e114127af2454bdcf4fc68d7
Tak Hyungmin <milkho11@naver.com>
2020-04-07 19:40:08 +0900

    [refact] ChessGame의 결과 생성 책임 이동
    ChessResult, ChessResultCreater로 이동시켰다.

--- src/main/java/chess/game/ChessGame.java
+++ src/main/java/chess/game/ChessGame.java
@@ -8,17 +8,11 @@ import chess.progress.Progress;
 import chess.player.Player;
 import chess.result.ChessResult;
 import chess.result.ChessScores;
-import chess.result.Result;
-import chess.score.Calculatable;
-import chess.score.Score;
-import chess.score.ScoreCalculator;
 import chess.team.Team;
 
-import static chess.progress.Progress.*;
 import static chess.team.Team.BLACK;
 import static chess.team.Team.WHITE;
 
-// TODO : 이제 갱신되는 Player들의 Set가 존재한다 -> 그러니 ChessGame들의 책임들을 옮길 수 있다.
 public class ChessGame {
     private final ChessBoard chessBoard;
     private final Player white;
@@ -59,6 +53,10 @@ public class ChessGame {
         }
     }
 
+    public ChessResult findWinner() {
+        return ChessResult.of(white, black);
+    }
+
     private Player getCounterTurnPlayer(Team turn) {
         Player counterplayer = white;
         if (black.isNotSame(turn)) {
@@ -67,46 +65,11 @@ public class ChessGame {
         return counterplayer;
     }
 
-    public Progress finishIfKingDie() {
-        if (isExistKingDiePlayer()) {
-            return END;
-        }
-        return Progress.CONTINUE;
-    }
-
-    private boolean isExistKingDiePlayer() {
-        return white.hasNotKing() || black.hasNotKing();
-    }
-
-    public ChessResult findWinner() {
-        if (isExistKingDiePlayer()) {
-            if (white.hasNotKing()) {
-                return new ChessResult(Result.WIN, black.getTeamName());
-            }
-            return new ChessResult(Result.WIN, white.getTeamName());
-        }
-        return compareScore();
-    }
-
     public ChessScores calculateScores() {
         return new ChessScores(
                 white.calculate(),
                 black.calculate()
         );
-    }
-
-    private ChessResult compareScore() {
-        Score whiteScore = white.calculate();
-        Score blackScore = black.calculate();
-
-        if (whiteScore.isHigherThan(blackScore)) {
-            return new ChessResult(Result.WIN, white.getTeamName());
-        }
-        if (blackScore.isHigherThan(whiteScore)) {
-            return new ChessResult(Result.WIN, black.getTeamName());
-        }
-
-        return new ChessResult(Result.DRAW);
     }
 
     public Team getTurn() {
--- src/main/java/chess/result/ChessResult.java
+++ src/main/java/chess/result/ChessResult.java
@@ -1,5 +1,7 @@
 package chess.result;
 
+import chess.player.Player;
+
 public class ChessResult {
     private static final String DRAW_RESULT_NAME = "승자 없음";
 
@@ -9,6 +11,10 @@ public class ChessResult {
     public ChessResult(Result result, String name) {
         this.result = result;
         this.name = name;
+    }
+
+    public static ChessResult of(Player white, Player black) {
+        return ChessResultCreater.findWinner(white, black);
     }
 
     public ChessResult(Result result) {
