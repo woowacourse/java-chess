476cd0a95bd8991a076a4a5ca4f50a1b9ae282c1
aegis <aegis1920@gmail.com>
2020-04-07 14:02:10 +0900

    feat : RoomController 클래스 작성

---
+++ src/main/java/chess/controller/RoomController.java
@@ -0,0 +1,53 @@
+package chess.controller;
+
+import static chess.util.HandlebarUtil.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import chess.domain.room.Room;
+import chess.service.RoomService;
+import spark.Route;
+
+public class RoomController {
+	public static final String BASIC_URL = "/rooms";
+	public static final String ENTER_ROOM_URL = BASIC_URL + "/enter";
+	public static final String CREATE_ROOM_URL = BASIC_URL + "/create";
+	public static final String REMOVE_ROOM_URL = BASIC_URL + "/remove";
+	public static Route enterRoom = (request, response) -> {
+		Map<String, Object> model = new HashMap<>();
+		model.put("roomId", request.queryParams("roomId"));
+
+		return render(model, "game.html");
+	};
+	public static Route getAllRoom = (request, response) -> {
+		Map<String, Object> model = new HashMap<>();
+
+		RoomService roomService = RoomService.getInstance();
+		List<Room> rooms = roomService.findAllRoom();
+		model.put("rooms", rooms);
+
+		return render(model, "index.html");
+	};
+	public static Route createRoom = (request, response) -> {
+		RoomService roomService = RoomService.getInstance();
+		String param = request.queryParams("roomName");
+		roomService.addRoom(param);
+
+		// TODO : 페이지 전체를 불러오기 때문에 비효율적이다. ajax로 하면 더 효율적이다.
+		response.redirect("/rooms");
+		return null;
+	};
+	// TODO : add랑 똑같은 로직인데 왜 새로고침해야 보일까?
+	public static Route removeRoom = (request, response) -> {
+		RoomService roomService = RoomService.getInstance();
+		roomService.removeRoom(request.queryParams("roomId"));
+
+		response.redirect("/rooms");
+		return null;
+	};
+
+	private RoomController() {
+	}
+}
