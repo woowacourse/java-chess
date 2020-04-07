626e6c26e02ea102aa633c52fe39bc04cf48227e
Andy H. Jung <commaniakr@gmail.com>
2020-04-07 15:13:27 +0900

    refactor: chessDAOTest 테스트 깨진 부분 복구

--- src/test/java/chess/domain/chessDAOTest.java
+++ src/test/java/chess/domain/chessDAOTest.java
@@ -9,12 +9,10 @@ import java.sql.Connection;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class chessDAOTest {
-    private static ChessDAO chessDAO;
-
     @Test
     @DisplayName("데이터베이스에 연결하는 것을 테스트")
     public void connection() {
-        chessDAO = new ChessDAO();
+        ChessDAO chessDAO = ChessDAO.getInstance();
         Connection con = chessDAO.getConnection();
         assertNotNull(con);
     }
