1ea41543d30047fb8c8a7c032642e820621ac287
aegis <aegis1920@gmail.com>
2020-04-07 14:08:41 +0900

    feat : 테스트만을 위한 AutoIncrementTest 클래스 작성

---
+++ src/test/java/chess/domain/dao/AutoIncrementTest.java
@@ -0,0 +1,15 @@
+package chess.domain.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import chess.util.JDBCConnector;
+
+public class AutoIncrementTest {
+
+	public static void applyAutoIncrementToZero() throws SQLException {
+		String query = "ALTER TABLE room AUTO_INCREMENT = 0";
+		PreparedStatement pstmt = JDBCConnector.getConnection().prepareStatement(query);
+		pstmt.executeUpdate();
+	}
+}
