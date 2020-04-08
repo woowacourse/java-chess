a6416dae6f51a7acc3d15b210e83522399084ea8
Sony <sonypark0204@gmail.com>
2020-04-08 13:19:10 +0900

    refactor: String 메서드를 이용해 문자열 병합

--- src/main/java/chess/dao/JdbcTemplateDao.java
+++ src/main/java/chess/dao/JdbcTemplateDao.java
@@ -5,13 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public interface JdbcTemplateDao {
+
     default Connection getConnection() {
         Connection connection = null;
-        String server = "localhost:13306"; // MySQL 서버 주소
+        String driverName = "jdbc:mysql://"; // jdbc 드라이버
+        String server = "localhost:13306/"; // MySQL 서버 주소
         String database = "chess_game"; // MySQL DATABASE 이름
         String option = "?useSSL=false&serverTimezone=UTC";
         String userName = "root"; //  MySQL 서버 아이디
         String password = "root"; // MySQL 서버 비밀번호
+        String DELIMITER = ""; // 구분자
 
         // 드라이버 로딩
         try {
@@ -23,8 +26,10 @@ public interface JdbcTemplateDao {
 
         // 드라이버 연결
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + option, userName,
+            connection = DriverManager.getConnection(String.join(DELIMITER, driverName, server, database, option),
+                userName,
                 password);
+            System.out.println();
             System.out.println("정상적으로 연결되었습니다.");
         } catch (SQLException e) {
             System.err.println("연결 오류:" + e.getMessage());
