c6a8eab80edaa9db6cc5a15640de8ca83f7a9371
hongbin23 <khbasd@gmail.com>
2020-04-08 16:48:53 +0900

    [refactor] 데이터베이스 정보 상수변경

--- src/main/java/dao/ConnectionManager.java
+++ src/main/java/dao/ConnectionManager.java
@@ -4,34 +4,37 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import view.OutputView;
+
 public class ConnectionManager {
+	private static final String SERVER = "localhost:13306";
+	private static final String DATABASE = "chess2";
+	private static final String OPTION = "?useSSL=false&serverTimezone=UTC";//  MySQL 서버 아이디
+	private static final String USER_NAME = "root";// MySQL 서버 비밀번호
+	private static final String PASSWORD = "root";
+
 	private ConnectionManager() {
 
 	}
 
 	public static Connection getConnection() {
 		Connection con = null;
-		String server = "localhost:13306";
-		String database = "chess2";
-		String option = "?useSSL=false&serverTimezone=UTC";
-		String userName = "root"; //  MySQL 서버 아이디
-		String password = "root"; // MySQL 서버 비밀번호
 
 		// 드라이버 로딩
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			System.err.println(" !! JDBC Driver load 오류: " + e.getMessage());
-			e.printStackTrace();
+			OutputView.printMessage(e);
 		}
 
 		// 드라이버 연결
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + option, userName, password);
+			con = DriverManager.getConnection(
+				String.format("jdbc:mysql://%s/%s%s", SERVER, DATABASE, OPTION), USER_NAME, PASSWORD);
 			System.out.println("정상적으로 연결되었습니다.");
 		} catch (SQLException e) {
-			System.err.println("연결 오류:" + e.getMessage());
-			e.printStackTrace();
+			OutputView.printMessage(e);
 		}
 
 		return con;
