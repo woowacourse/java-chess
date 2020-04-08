b973121eddf4e6b93bcc142a5e2415aca8500732
icyMojito <dev.icymojito@gmail.com>
2020-04-09 01:22:12 +0900

    feat : BoardDAO와 BoardDTO를 생성
    데이터 베이스의 테이블을 두 개(1.위치와 말의 정보를 담는 테이블 / 2.현재 차례인 팀 정보를 담는 테이블)로 분리함에 따라
    위치와 말의 정보를 담는 테이블에 접근하기 위한 BoardDAO와 BoardDTO를 생성함

--- src/main/java/chess/dto/BoardDTO.java
+++ src/main/java/chess/dto/BoardDTO.java
@@ -1,24 +1,51 @@
 package chess.dto;
 
-import java.util.List;
+import chess.domain.board.Board;
+import chess.domain.board.Position;
+import chess.domain.board.PositionFactory;
+import chess.domain.piece.Piece;
+import chess.domain.piece.PieceType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static chess.util.NullValidator.validateNull;
 
 public class BoardDTO {
-    private List<String> lines;
-    private String currentTeam;
+    private Map<String, String> board;
 
-    public List<String> getLines() {
-        return lines;
+    public BoardDTO(Map<String, String> board) {
+        this.board = board;
     }
 
-    public void setLines(List<String> lines) {
-        this.lines = lines;
+    public static BoardDTO from(Board board) {
+        validateNull(board);
+
+        Map<Position, Piece> boardData = board.get();
+        Map<String, String> boardDTOData = new HashMap<>();
+
+        for (Position position : boardData.keySet()) {
+            boardDTOData.put(position.getName(), boardData.get(position).getName());
+        }
+
+        return new BoardDTO(boardDTOData);
     }
 
-    public String getCurrentTeam() {
-        return currentTeam;
+    public Map<String, String> getBoard() {
+        return this.board;
     }
 
-    public void setCurrentTeam(String currentTeam) {
-        this.currentTeam = currentTeam;
+    public Map<Position, Piece> createBoard() {
+        Map<Position, Piece> boardData = new HashMap<>();
+
+        for (String positionName : this.board.keySet()) {
+            Position position = PositionFactory.of(positionName);
+            String pieceName = this.board.get(positionName);
+            Piece piece = PieceType.of(pieceName).createPiece(position);
+
+            boardData.put(position, piece);
+        }
+
+        return boardData;
     }
 }
