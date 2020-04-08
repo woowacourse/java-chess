7fc722b99ee87875a658fc1d02668d3c7008228c
hj <a301dks@naver.com>
2020-04-08 13:31:08 +0900

    refactor: 매직넘버를 상수로 치환하고 자원해제를 try resource 방식으로 수정

--- src/main/java/chess/db/dao/BoardDao.java
+++ src/main/java/chess/db/dao/BoardDao.java
@@ -19,25 +19,28 @@ import chess.domain.piece.Piece;
  *    @author AnHyungJu
  */
 public class BoardDao {
+	private static final String ADDRESS = "localhost:13306";
+	private static final String NAME = "chess";
+	private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
+	private static final String ID = "root";
+	private static final String PASSWORD = "root";
+	private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
 	public Connection getConnection() {
 		Connection con = null;
-		String server = "localhost:13306"; // MySQL 서버 주소
-		String database = "chess"; // MySQL DATABASE 이름
-		String option = "?useSSL=false&serverTimezone=UTC";
-		String boardName = "root"; // MySQL 서버 아이디
-		String password = "root"; // MySQL 서버 비밀번호
 
 		// 드라이버 로딩
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName(DRIVER_NAME);
 		} catch (ClassNotFoundException e) {
-			System.err.println(" !! JDBC Driver load 오류: " + e.getMessage());
+			System.err.println(String.format(" !! JDBC Driver load 오류: %s", e.getMessage()));
 			e.printStackTrace();
 		}
 
 		// 드라이버 연결
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + option, boardName, password);
+			con = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s%s", ADDRESS, NAME, OPTION)
+				, ID, PASSWORD);
 			System.out.println("정상적으로 연결되었습니다.");
 		} catch (SQLException e) {
 			System.err.println("연결 오류:" + e.getMessage());
@@ -45,16 +48,6 @@ public class BoardDao {
 		}
 
 		return con;
-	}
-
-	// 드라이버 연결해제
-	public void closeConnection(Connection con) {
-		try {
-			if (con != null)
-				con.close();
-		} catch (SQLException e) {
-			System.err.println("con 오류:" + e.getMessage());
-		}
 	}
 
 	public void createBoard(Board board) {
@@ -69,101 +62,99 @@ public class BoardDao {
 
 	private void addPieceTo(Position position, Piece piece) throws SQLException {
 		String query = "INSERT INTO board VALUES (?, ?, ?)";
-		Connection con = getConnection();
-		PreparedStatement pstmt = con.prepareStatement(query);
+		try (Connection con = getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)) {
 
-		pstmt.setString(1, position.getPosition());
-		if (Objects.isNull(piece)) {
-			pstmt.setString(2, null);
-			pstmt.setString(3, null);
-		} else {
-			pstmt.setString(2, piece.getSymbol());
-			pstmt.setString(3, piece.getState().toString());
+			pstmt.setString(1, position.getPosition());
+			if (Objects.isNull(piece)) {
+				pstmt.setString(2, null);
+				pstmt.setString(3, null);
+			} else {
+				pstmt.setString(2, piece.getSymbol());
+				pstmt.setString(3, piece.getState().toString());
+			}
+			pstmt.executeUpdate();
 		}
-		pstmt.executeUpdate();
-		pstmt.close();
-		closeConnection(con);
 	}
 
 	public Piece findPieceBy(String position) throws SQLException {
+		Piece piece;
 		String query = "SELECT * FROM board WHERE position = ?";
-		Connection con = getConnection();
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, position);
-		ResultSet rs = pstmt.executeQuery();
+		try (Connection con = getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)) {
+			pstmt.setString(1, position);
+			ResultSet rs = pstmt.executeQuery();
 
-		if (!rs.next())
-			return null;
+			if (!rs.next())
+				return null;
 
-		if (Objects.isNull(rs.getString("pieceSymbol"))) {
-			return null;
+			if (Objects.isNull(rs.getString("pieceSymbol"))) {
+				return null;
+			}
+
+			piece = WhitePieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
+			if (Objects.isNull(piece)) {
+				piece = BlackPieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
+			}
 		}
-
-		Piece piece = WhitePieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
-		if (Objects.isNull(piece)) {
-			piece = BlackPieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
-		}
-		pstmt.close();
-		closeConnection(con);
 		return piece;
 	}
 
 	public Piece findWhitePieceBy(String position) throws SQLException {
+		Piece piece;
 		String query = "SELECT * FROM board WHERE position = ?";
-		Connection con = getConnection();
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, position);
-		ResultSet rs = pstmt.executeQuery();
+		try (Connection con = getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)) {
+			pstmt.setString(1, position);
+			ResultSet rs = pstmt.executeQuery();
 
-		if (!rs.next())
-			return null;
+			if (!rs.next())
+				return null;
 
-		if (Objects.isNull(rs.getString("pieceSymbol"))) {
-			return null;
+			if (Objects.isNull(rs.getString("pieceSymbol"))) {
+				return null;
+			}
+
+			piece = WhitePieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
 		}
-
-		Piece piece = WhitePieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
-		pstmt.close();
-		closeConnection(con);
 		return piece;
 	}
 
 	public Piece findBlackPieceBy(String position) throws SQLException {
+		Piece piece;
 		String query = "SELECT * FROM board WHERE position = ?";
-		Connection con = getConnection();
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, position);
-		ResultSet rs = pstmt.executeQuery();
+		try (Connection con = getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)) {
+			pstmt.setString(1, position);
+			ResultSet rs = pstmt.executeQuery();
 
-		if (!rs.next())
-			return null;
+			if (!rs.next())
+				return null;
 
-		if (Objects.isNull(rs.getString("pieceSymbol"))) {
-			return null;
+			if (Objects.isNull(rs.getString("pieceSymbol"))) {
+				return null;
+			}
+
+			piece = BlackPieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
 		}
-
-		Piece piece = BlackPieceMapper.mappingBy(rs.getString("pieceSymbol"), rs.getString("pieceState"));
-		pstmt.close();
-		closeConnection(con);
 		return piece;
 	}
 
 	public void updateBoard(String position, Piece piece) throws SQLException {
 		String query = "UPDATE board SET pieceSymbol = ?, pieceState = ? WHERE position = ?";
-		Connection con = getConnection();
-		PreparedStatement pstmt = con.prepareStatement(query);
-
-		pstmt.setString(3, position);
-		if (Objects.isNull(piece)) {
-			pstmt.setString(1, null);
-			pstmt.setString(2, null);
+		try (
+			Connection con = getConnection();
+			PreparedStatement pstmt = con.prepareStatement(query)) {
+			pstmt.setString(3, position);
+			if (Objects.isNull(piece)) {
+				pstmt.setString(1, null);
+				pstmt.setString(2, null);
+				pstmt.executeUpdate();
+				return;
+			}
+			pstmt.setString(1, piece.getSymbol());
+			pstmt.setString(2, piece.getState().toString());
 			pstmt.executeUpdate();
-			return;
 		}
-		pstmt.setString(1, piece.getSymbol());
-		pstmt.setString(2, piece.getState().toString());
-		pstmt.executeUpdate();
-		pstmt.close();
-		closeConnection(con);
 	}
 }
