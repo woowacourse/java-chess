f2ba85e8339a950bcab5938821fb24e80dcb8377
kimjunseok <jnunseok@gmail.com>
2020-04-08 10:59:29 +0900

    refactor : Application 이동

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,7 +1,6 @@
+package chess;
+
 import controller.ChessGameController;
-import domain.pieces.Pieces;
-import domain.pieces.PiecesFactory;
-import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
