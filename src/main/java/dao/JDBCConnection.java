d47f145a9772a00d799125950bc0ecec550ebcfc
kimjunseok <jnunseok@gmail.com>
2020-04-09 10:29:22 +0900

    refactor : Connection 상수 처리

--- src/main/java/dao/JDBCConnection.java
+++ src/main/java/dao/JDBCConnection.java
@@ -5,15 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCConnection {
+
+    private static final String MYSQL_SERVER_ADDRESS = "localhost:3306";
+    private static final String DB_NAME = "chess";
+    private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
+    private static final String SERVER_ID = "root";
+    private static final String SERVER_PASSWORD = "root";
+
     public static Connection getConnection() {
         Connection con = null;
-        String server = "localhost:3306"; // MySQL 서버 주소
-        String database = "chess"; // MySQL DATABASE 이름
-        String option = "?useSSL=false&serverTimezone=UTC";
-        String userName = "root"; //  MySQL 서버 아이디
-        String password = "root"; // MySQL 서버 비밀번호
-
-        // 드라이버 로딩
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -21,9 +21,8 @@ public class JDBCConnection {
             e.printStackTrace();
         }
 
-        // 드라이버 연결
         try {
-            con = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + option, userName, password);
+            con = DriverManager.getConnection("jdbc:mysql://" + MYSQL_SERVER_ADDRESS + "/" + DB_NAME + OPTION, SERVER_ID, SERVER_PASSWORD);
             System.out.println("정상적으로 연결되었습니다.");
         } catch (SQLException e) {
             System.err.println("연결 오류:" + e.getMessage());
