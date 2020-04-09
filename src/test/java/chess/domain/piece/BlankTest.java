d86b0a1323e7dc5513f2c940e668056d93ea37e5
KIMSIYOUNG <tldud2404@gmail.com>
2020-04-09 11:14:00 +0900

    refactor : Blank의 move가 정상적으로 예외를 반환하는 테스트 추가

---
+++ src/test/java/chess/domain/piece/BlankTest.java
@@ -0,0 +1,21 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import chess.domain.Team;
+import chess.domain.position.Position;
+
+class BlankTest {
+	@Test
+	@DisplayName("Blank가 움직이는 경우 정상적으로 예외를 반환하는지")
+	void moveTest() {
+		Blank blank = new Blank(Position.of(3, 4));
+		assertThatThrownBy(() -> blank.move(Position.of(3, 4), Position.of(3, 5), new HashMap<Position, Team>()))
+			.isInstanceOf(UnsupportedOperationException.class);
+	}
+}
