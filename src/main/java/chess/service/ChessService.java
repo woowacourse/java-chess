c21faed40fed24ed249a8eb3e7dbe9574e54f963
giantim <giantim132@gmail.com>
2020-04-09 22:10:22 +0900

    refactor: service layer 추가

--- src/main/java/chess/service/ChessService.java
+++ src/main/java/chess/service/ChessService.java
@@ -1,4 +1,13 @@
 package chess.service;
 
+import chess.dao.ChessBoardDAO;
+import chess.dao.CurrentTeamDAO;
+import chess.dao.PieceDAO;
+import chess.dao.PlayerDAO;
+
 public class ChessService {
+    private final ChessBoardDAO chessBoardDAO = new ChessBoardDAO();
+    private final CurrentTeamDAO currentTeamDAO = new CurrentTeamDAO();
+    private final PieceDAO pieceDAO = new PieceDAO();
+    private final PlayerDAO playerDAO = new PlayerDAO();
 }
