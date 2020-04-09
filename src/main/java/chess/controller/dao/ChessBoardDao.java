875394f3ffe999031cf8f25045c51e7efdd7a869
Seyun <ksy90101@gmail.com>
2020-04-09 10:36:05 +0900

    refactor : try-resource으로 리팩토링

--- src/main/java/chess/controller/dao/ChessBoardDao.java
+++ src/main/java/chess/controller/dao/ChessBoardDao.java
@@ -15,44 +15,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ChessBoardDao {
-    private Connection connection = new ConnectionDao().getConnection();
+    private ConnectionDao connectionDao = new ConnectionDao();
 
     public void saveChessBoard(ChessBoardDto chessBoardDto, int roomNumber) throws SQLException {
         String query = "INSERT INTO chessboard VALUES (?,?,(SELECT piece_id FROM piece WHERE name=? AND player=?))";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        for (TileDto tile : chessBoardDto.getTiles()) {
-            pstmt.setInt(1, roomNumber);
-            pstmt.setString(2, tile.getPosition());
-            pstmt.setString(3, tile.getPieceName());
-            pstmt.setString(4, tile.getPiecePlayer());
-            pstmt.executeUpdate();
+
+        try (Connection connection = connectionDao.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+
+            for (TileDto tile : chessBoardDto.getTiles()) {
+                pstmt.setInt(1, roomNumber);
+                pstmt.setString(2, tile.getPosition());
+                pstmt.setString(3, tile.getPieceName());
+                pstmt.setString(4, tile.getPiecePlayer());
+                pstmt.executeUpdate();
+            }
         }
-        pstmt.close();
     }
 
     public void deleteChessBoard(int roomNumber) throws SQLException {
         String query = "DELETE FROM chessboard WHERE room = ?";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setInt(1, roomNumber);
-        pstmt.executeUpdate();
-        pstmt.close();
+        try (Connection connection = connectionDao.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, roomNumber);
+            pstmt.executeUpdate();
+        }
     }
 
     public Map<Position, Piece> findPlayingChessBoard(int roomNumber) throws SQLException {
         String query = "SELECT a.position, b.name, b.player FROM chessboard a " +
                 "INNER JOIN piece b ON a.piece_id = b.piece_id WHERE a.room = ?";
-
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setInt(1, roomNumber);
-
-        ResultSet rs = pstmt.executeQuery();
-
         Map<Position, Piece> chessBoard = new HashMap<>();
-        while (rs.next()) {
-            chessBoard.put(Positions.of(rs.getString("position")),
-                    PieceInfo.of(rs.getString("name"), rs.getString("player")));
+
+        try (Connection connection = connectionDao.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, roomNumber);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                chessBoard.put(Positions.of(rs.getString("position")),
+                        PieceInfo.of(rs.getString("name"), rs.getString("player")));
+            }
         }
-        pstmt.close();
 
         return chessBoard;
     }
