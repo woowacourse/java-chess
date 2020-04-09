9b37c198aee0960802bcf136c3036ea96beef059
aegis <aegis1920@gmail.com>
2020-04-09 21:43:57 +0900

    refactor : 각 DAO에 try with resource로 리펙토링

--- src/main/java/chess/dao/GameDAO.java
+++ src/main/java/chess/dao/GameDAO.java
@@ -22,113 +22,106 @@ public class GameDAO {
 	}
 
 	public void removeAllPiecesById(int roomId) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
 		String query = "DELETE FROM board WHERE room_id = ?";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setInt(1, roomId);
-		pstmt.executeUpdate();
 
-		JDBCConnector.closeConnection(con);
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query);
+		) {
+			pstmt.setInt(1, roomId);
+			pstmt.executeUpdate();
+		}
 	}
 
 	public void addAllPiecesById(int roomId, Pieces pieces) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
+		String query = "INSERT INTO board(room_id, piece_name, piece_position, piece_color) VALUES (?, ?, ?, ?)";
 
-		for (Position position : pieces.getPieces().keySet()) {
-			Piece piece = pieces.getPieceByPosition(position);
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query);
+		) {
+			for (Position position : pieces.getPieces().keySet()) {
+				Piece piece = pieces.getPieceByPosition(position);
 
-			String query = "INSERT INTO board(room_id, piece_name, piece_position, piece_color)"
-				+ " VALUES (?, ?, ?, ?)";
-			PreparedStatement pstmt = con.prepareStatement(query);
+				pstmt.setInt(1, roomId);
+				pstmt.setString(2, piece.getSymbol());
+				pstmt.setString(3, position.getPosition());
+				pstmt.setString(4, piece.getColor().name());
+				pstmt.executeUpdate();
+			}
+		}
+	}
+
+	public Map<Position, Piece> findAllPiecesById(int roomId) throws SQLException {
+		String query = "SELECT piece_name, piece_position, piece_color FROM board WHERE room_id = ?";
+
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query);
+		) {
+			Map<Position, Piece> pieces = new HashMap<>();
+
+			pstmt.setInt(1, roomId);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while (rs.next()) {
+					String name = rs.getString("piece_name");
+					String position = rs.getString("piece_position");
+					pieces.put(Position.of(position), PieceMapper.getInstance().findDBPiece(name));
+				}
+			}
+			return pieces;
+		}
+	}
+
+	public Piece findPieceByPosition(String position) throws SQLException {
+		String query = "SELECT piece_name FROM board WHERE piece_position = ?";
+
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query);
+		) {
+			pstmt.setString(1, position);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (!rs.next()) {
+					return Blank.getInstance();
+				}
+				return PieceMapper.getInstance().findDBPiece(rs.getString("piece_name"));
+			}
+		}
+	}
+
+	public void updatePieceByPosition(String currentPosition, String nextPosition) throws SQLException {
+		String query = "UPDATE board SET piece_position = ? WHERE piece_position = ?";
+
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
+			pstmt.setString(1, nextPosition);
+			pstmt.setString(2, currentPosition);
+			pstmt.executeUpdate();
+		}
+	}
+
+	public void deletePieceByPosition(String position) throws SQLException {
+		String query = "DELETE FROM board WHERE piece_position = ?";
+
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query);
+		) {
+			pstmt.setString(1, position);
+			pstmt.executeUpdate();
+		}
+	}
+
+	public void addPieceByPosition(int roomId, Position position, Piece piece) throws SQLException {
+		String query = "INSERT INTO board(room_id, piece_name, piece_position, piece_color) VALUES (?, ?, ?, ?)";
+
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
 			pstmt.setInt(1, roomId);
 			pstmt.setString(2, piece.getSymbol());
 			pstmt.setString(3, position.getPosition());
 			pstmt.setString(4, piece.getColor().name());
 			pstmt.executeUpdate();
 		}
-
-		JDBCConnector.closeConnection(con);
-	}
-
-	public Map<Position, Piece> findAllPiecesById(int roomId) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
-		Map<Position, Piece> pieces = new HashMap<>();
-
-		String query = "SELECT piece_name, piece_position, piece_color FROM board WHERE room_id = ?";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setInt(1, roomId);
-		ResultSet rs = pstmt.executeQuery();
-
-		while (rs.next()) {
-			String name = rs.getString("piece_name");
-			String position = rs.getString("piece_position");
-			pieces.put(Position.of(position), PieceMapper.getInstance().findDBPiece(name));
-		}
-
-		JDBCConnector.closeConnection(con);
-
-		return pieces;
-	}
-
-	public Piece findPieceByPosition(String position) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
-		String query = "SELECT piece_name FROM board WHERE piece_position = ?";
-
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, position);
-		ResultSet rs = pstmt.executeQuery();
-
-		if (!rs.next()) {
-			return Blank.getInstance();
-		}
-
-		Piece piece = PieceMapper.getInstance().findDBPiece(rs.getString("piece_name"));
-
-		JDBCConnector.closeConnection(con);
-
-		return piece;
-	}
-
-	public void updatePieceByPosition(String currentPosition, String nextPosition) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
-		String query = "UPDATE board SET piece_position = ? WHERE piece_position = ?";
-
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, nextPosition);
-		pstmt.setString(2, currentPosition);
-		pstmt.executeUpdate();
-
-		JDBCConnector.closeConnection(con);
-	}
-
-	public void deletePieceByPosition(String position) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
-		String query = "DELETE FROM board WHERE piece_position = ?";
-
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, position);
-		pstmt.executeUpdate();
-
-		JDBCConnector.closeConnection(con);
-	}
-
-	public void addPieceByPosition(int roomId, Position position, Piece piece) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
-		String query = "INSERT INTO board(room_id, piece_name, piece_position, piece_color) VALUES (?, ?, ?, ?)";
-
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setInt(1, roomId);
-		pstmt.setString(2, piece.getSymbol());
-		pstmt.setString(3, position.getPosition());
-		pstmt.setString(4, piece.getColor().name());
-		pstmt.executeUpdate();
-
-		JDBCConnector.closeConnection(con);
 	}
 }
--- src/main/java/chess/dao/RoomDAO.java
+++ src/main/java/chess/dao/RoomDAO.java
@@ -19,122 +19,110 @@ public class RoomDAO {
 	}
 
 	public Room findRoomById(int roomId) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
 		String query = "SELECT room_id, room_name, room_color FROM room WHERE room_id = ?";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setInt(1, roomId);
-		ResultSet rs = pstmt.executeQuery();
 
-		if (!rs.next()) {
-			return null;
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
+			pstmt.setInt(1, roomId);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (!rs.next()) {
+					return null;
+				}
+				return new Room(
+					rs.getInt("room_id"),
+					rs.getString("room_name"),
+					rs.getString("room_color")
+				);
+			}
 		}
-
-		Room room = new Room(
-			rs.getInt("room_id"),
-			rs.getString("room_name"),
-			rs.getString("room_color")
-		);
-
-		JDBCConnector.closeConnection(con);
-
-		return room;
 	}
 
 	public void addRoom(String roomName, String roomColor) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
 		String query = "INSERT INTO room(room_name, room_color) VALUES (?, ?)";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, roomName);
-		pstmt.setString(2, roomColor);
-		pstmt.executeUpdate();
 
-		JDBCConnector.closeConnection(con);
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
+			pstmt.setString(1, roomName);
+			pstmt.setString(2, roomColor);
+			pstmt.executeUpdate();
+		}
 	}
 
 	public void removeRoomById(int roomId) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
-
 		String query = "DELETE FROM room WHERE room_id = ?";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setInt(1, roomId);
-		pstmt.executeUpdate();
 
-		JDBCConnector.closeConnection(con);
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
+			pstmt.setInt(1, roomId);
+			pstmt.executeUpdate();
+		}
 	}
 
 	public int findRoomIdByRoomName(String roomName) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
 		String query = "SELECT room_id FROM room WHERE room_name = ?";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, roomName);
-		ResultSet rs = pstmt.executeQuery();
 
-		if (!rs.next()) {
-			return 0;
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
+			pstmt.setString(1, roomName);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (!rs.next()) {
+					return 0;
+				}
+
+				return rs.getInt("room_id");
+			}
 		}
-
-		int roomId = rs.getInt("room_id");
-
-		JDBCConnector.closeConnection(con);
-
-		return roomId;
 	}
 
 	public List<Room> findAllRoom() throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
+		String query = "SELECT room_id, room_name, room_color FROM room";
 		List<Room> rooms = new ArrayList<>();
 
-		String query = "SELECT room_id, room_name, room_color FROM room";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		ResultSet rs = pstmt.executeQuery();
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query);
+		) {
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while (rs.next()) {
+					int roomId = rs.getInt("room_id");
+					String roomName = rs.getString("room_name");
+					String roomColor = rs.getString("room_color");
 
-		while (rs.next()) {
-			int roomId = rs.getInt("room_id");
-			String roomName = rs.getString("room_name");
-			String roomColor = rs.getString("room_color");
-
-			rooms.add(new Room(roomId, roomName, roomColor));
+					rooms.add(new Room(roomId, roomName, roomColor));
+				}
+				return rooms;
+			}
 		}
-
-		JDBCConnector.closeConnection(con);
-
-		return rooms;
 	}
 
 	public void updateRoomColorById(int roomId, Color roomColor) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
 		String query = "UPDATE room SET room_color = ? WHERE room_id = ?";
 
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, roomColor.name());
-		pstmt.setInt(2, roomId);
-		pstmt.executeUpdate();
-
-		JDBCConnector.closeConnection(con);
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
+			pstmt.setString(1, roomColor.name());
+			pstmt.setInt(2, roomId);
+			pstmt.executeUpdate();
+		}
 	}
 
 	public Color findRoomColorById(int roomId) throws SQLException {
-		Connection con = JDBCConnector.getConnection();
-
 		String query = "SELECT room_color FROM room WHERE room_id = ?";
-		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setInt(1, roomId);
-		ResultSet rs = pstmt.executeQuery();
 
-		if (!rs.next()) {
-			return null;
+		try (Connection con = JDBCConnector.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(query)
+		) {
+			pstmt.setInt(1, roomId);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (!rs.next()) {
+					return null;
+				}
+				return Color.valueOf(rs.getString("room_color"));
+			}
 		}
-
-		Color color = Color.valueOf(rs.getString("room_color"));
-
-		JDBCConnector.closeConnection(con);
-
-		return color;
 	}
 }
