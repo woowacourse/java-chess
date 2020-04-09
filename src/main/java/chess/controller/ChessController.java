0234c8f2fdd6200281d0c5c10dcb0b93e205cc14
Ramen6315 <ramen6315@gmail.com>
2020-04-09 13:14:05 +0900

    refactor : ChessController class수정
     - 각 요청 별 메서드 분리

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,6 +1,6 @@
 package chess.controller;
 
-import chess.dao.ChessBoardDaoImpl;
+import chess.dao.ChessDataBase;
 import chess.domain.piece.position.Position;
 import chess.service.ChessService;
 import com.google.gson.Gson;
@@ -17,22 +17,35 @@ import static spark.Spark.post;
 public class ChessController {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final HandlebarsTemplateEngine handlebarsTemplateEngine = new HandlebarsTemplateEngine();
-    private static final ChessService chessService = new ChessService(new ChessBoardDaoImpl());
+    private static final ChessService chessService = new ChessService(new ChessDataBase());
 
     private static String render(Map<String, Object> model, String templatePath) {
         return handlebarsTemplateEngine.render(new ModelAndView(model, templatePath));
     }
 
     public void run() {
+        rendChessBoard();
+        initChessBoard();
+        restartChessBoard();
+        move();
+    }
+
+    private void rendChessBoard() {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
+    }
 
+    private void initChessBoard() {
         get("/init", (req, res) -> gson.toJson(chessService.initChessBoard()));
+    }
 
+    private void restartChessBoard() {
         get("/continue", (req, res) -> gson.toJson(chessService.board()));
+    }
 
+    private void move() {
         post("/move", (req, res) -> {
             String source = req.queryParams("sourcePosition");
             String target = req.queryParams("targetPosition");
