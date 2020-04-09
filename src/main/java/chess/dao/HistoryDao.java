b71a01919c86b44f911aa845277bf7791c67ec1c
toneyparky <qkrtmddhks95@gmail.com>
2020-04-09 20:30:43 +0900

    refactor: try with resource를 사용하여 JDBC connection 관리
    HistoryDao try with resource를 사용하여 JDBC connection 관리

--- src/main/java/chess/dao/HistoryDao.java
+++ src/main/java/chess/dao/HistoryDao.java
@@ -6,33 +6,38 @@ import chess.web.MovingPosition;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HistoryDao {
 	private final ConnectionManager connectionManager = new ConnectionManager();
 
 	public void insert(String start, String end) throws SQLException {
 		String query = "INSERT INTO history (start, end) VALUES (?, ?)";
-		PreparedStatement pstmt = connectionManager.getConnection().prepareStatement(query);
-		pstmt.setString(1, start);
-		pstmt.setString(2, end);
-		pstmt.executeUpdate();
+		try (PreparedStatement pstmt = connectionManager.getConnection().prepareStatement(query)) {
+			pstmt.setString(1, start);
+			pstmt.setString(2, end);
+			pstmt.executeUpdate();
+		}
 	}
 
 	public List<MovingPosition> selectAll() throws SQLException {
 		String query = "SELECT * FROM history";
-		PreparedStatement pstmt = connectionManager.getConnection().prepareStatement(query);
-		ResultSet rs = pstmt.executeQuery();
-		List<MovingPosition> result = new ArrayList<>();
-		while (rs.next()) {
-			result.add(new MovingPosition(rs.getString("start"), rs.getString("end")));
+		try (PreparedStatement pstmt = connectionManager.getConnection().prepareStatement(query); ResultSet rs = pstmt.executeQuery()) {
+
+			List<MovingPosition> result = new ArrayList<>();
+			while (rs.next()) {
+				result.add(new MovingPosition(rs.getString("start"), rs.getString("end")));
+			}
+			return Collections.unmodifiableList(result);
 		}
-		return Collections.unmodifiableList(result);
 	}
 
 	public void clear() throws SQLException {
 		String query = "DELETE FROM history";
-		PreparedStatement pstmt = connectionManager.getConnection().prepareStatement(query);
-		pstmt.executeUpdate();
+		try (PreparedStatement pstmt = connectionManager.getConnection().prepareStatement(query)) {
+			pstmt.executeUpdate();
+		}
 	}
 }
