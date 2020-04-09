2a17a6c308406f12ca89c25e09ae57a07640bff9
begaonnuri <turtlenam0928@gmail.com>
2020-04-09 16:49:45 +0900

    feat: BoardConverter 변경
    - String을 Board로 변환하는 기능 추가
    - String을 Side로 변환하는 기능 추가

--- src/main/java/chess/domain/BoardConverter.java
+++ src/main/java/chess/domain/BoardConverter.java
@@ -2,10 +2,40 @@ package chess.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import chess.domain.piece.Piece;
+import chess.domain.piece.PieceFactory;
+import chess.domain.position.Position;
 
 public class BoardConverter {
+	public static ChessBoard convertToBoard(String board) {
+		List<Piece> pieces = new ArrayList<>();
+		int col = 0;
+		int row = 8;
+
+		for (String boardName : board.split("")) {
+			Side side = convertToSide(boardName);
+			Optional<Piece> piece = PieceFactory.of(boardName.toLowerCase(), side, new Position((col % 8) + 1, row));
+			if (piece.isPresent()) {
+				pieces.add(piece.orElseThrow(NoSuchElementException::new));
+			}
+			col++;
+			if (col % 8 == 0) {
+				row--;
+			}
+		}
+		return new ChessBoard(pieces);
+	}
+
+	private static Side convertToSide(String boardName) {
+		if (Character.isUpperCase(boardName.charAt(0))) {
+			return Side.BLACK;
+		}
+		return Side.WHITE;
+	}
+
 	public static String convert(ChessBoard chessBoard, String marker) {
 		List<List<String>> boardInfo = makeStringBoard(chessBoard.getPieces(), marker);
 
