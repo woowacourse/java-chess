af0454b8d64239a3630bb2d89be09b092a4f9ef7
minuyim <minu_yim@naver.com>
2020-04-11 23:41:37 +0900

    style: 코드 포맷팅 수행

--- src/main/java/chess/contoller/WebErrorController.java
+++ src/main/java/chess/contoller/WebErrorController.java
@@ -9,18 +9,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WebErrorController {
-    public void renderStartError(Exception e, Request request, Response response) {
-        Map<String, Object> model = new HashMap<>();
-        model.put("error", e.getMessage());
-        response.status(400);
-        response.body(new HandlebarsTemplateEngine().render(new ModelAndView(model, "starterror.html")));
-    }
+	public void renderStartError(Exception e, Request request, Response response) {
+		Map<String, Object> model = new HashMap<>();
+		model.put("error", e.getMessage());
+		response.status(400);
+		response.body(new HandlebarsTemplateEngine().render(new ModelAndView(model, "starterror.html")));
+	}
 
-    public void renderError(Exception e, Request request, Response response) {
-        Map<String, Object> model = new HashMap<>();
-        model.put("error", e.getMessage());
-        model.put("game_id", request.queryParams("game_id"));
-        response.status(400);
-        response.body(new HandlebarsTemplateEngine().render(new ModelAndView(model, "error.html")));
-    }
+	public void renderError(Exception e, Request request, Response response) {
+		Map<String, Object> model = new HashMap<>();
+		model.put("error", e.getMessage());
+		model.put("game_id", request.queryParams("game_id"));
+		response.status(400);
+		response.body(new HandlebarsTemplateEngine().render(new ModelAndView(model, "error.html")));
+	}
 }
