be088c88105a859bb17643c195740ecebb046809
minuyim <minu_yim@naver.com>
2020-04-11 23:07:30 +0900

    refactor: chessGame을 필드로 갖고 있지 않도록 수정

--- src/main/java/chess/contoller/WebChessController.java
+++ src/main/java/chess/contoller/WebChessController.java
@@ -15,22 +15,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WebChessController {
-	private ChessGame chessGame;
+	private int gameId;
 	private ChessBoardDao chessBoardDao;
 	private ChessGameDao chessGameDao;
-	private int gameId;
 
 	public WebChessController() {
-		chessGame = new ChessGame(ChessBoardFactory.create());
 		chessBoardDao = new ChessBoardDao();
 		chessGameDao = new ChessGameDao();
 	}
 
 	public String startGame(Request request, Response response) {
-		chessGame = new ChessGame(ChessBoardFactory.create());
+		ChessGame chessGame = new ChessGame(ChessBoardFactory.create());
 		gameId = chessGameDao.add(chessGame.getTurn());
 		chessBoardDao.addPieces(chessGame.getChessBoard(), gameId);
-		return render();
+		return render(chessGame);
+	}
+
+	private ChessGame getChessGame() {
+		ChessBoard chessBoard = chessBoardDao.findByGameId(gameId);
+		return new ChessGame(chessBoard, chessGameDao.findTurnByGameId(gameId));
 	}
 
 	public String continueGame(Request request, Response response) throws RuntimeException {
@@ -39,34 +42,36 @@ public class WebChessController {
 		} catch (NumberFormatException e) {
 			throw new DataAccessException("게임 id(숫자)를 입력해주세요.");
 		}
-		ChessBoard chessBoard = chessBoardDao.findByGameId(gameId);
-		chessGame = new ChessGame(chessBoard, chessGameDao.findTurnByGameId(gameId));
-		return render();
+		ChessGame chessGame = getChessGame();
+		return render(chessGame);
 	}
 
 	public String runGame(Request request, Response response) {
+		ChessGame chessGame = getChessGame();
 		chessGame.move(Position.of(request.queryParams("source")), Position.of(request.queryParams("target")));
 		chessBoardDao.deleteByGameId(gameId);
 		chessBoardDao.addPieces(chessGame.getChessBoard(), gameId);
 		chessGameDao.updateTurn(gameId, chessGame.getTurn());
 		if (chessGame.isEnd()) {
 			chessGameDao.deleteByGameId(gameId);
-			return renderEnd();
+			return renderEnd(chessGame);
 		}
-		return render();
+		return render(chessGame);
 	}
 
 	public String showGame(Request request, Response response) {
-		return render();
+		ChessGame chessGame = getChessGame();
+		return render(chessGame);
 	}
 
 	public String endGame(Request request, Response response) {
+		ChessGame chessGame = getChessGame();
 		chessGame.end();
 		chessBoardDao.deleteByGameId(gameId);
-		return renderEnd();
+		return renderEnd(chessGame);
 	}
 
-	private String render() {
+	private String render(ChessGame chessGame) {
 		ChessStatus chessStatus = chessGame.createStatus();
 		Map<String, Object> model = new HashMap<>();
 		model.put("piece", ChessBoardAssembler.create(chessGame.getChessBoard()));
@@ -77,7 +82,7 @@ public class WebChessController {
 		return new HandlebarsTemplateEngine().render(new ModelAndView(model, "game.html"));
 	}
 
-	private String renderEnd() {
+	private String renderEnd(ChessGame chessGame) {
 		Map<String, Object> model = new HashMap<>();
 		model.put("turn", chessGame.getTurn());
 		return new HandlebarsTemplateEngine().render(new ModelAndView(model, "end.html"));
