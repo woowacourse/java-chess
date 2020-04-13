d690f75f0245b2ef5b90758dbed0e1250aee79e5
jaeju.jang <jjj0611@gmail.com>
2020-04-14 02:32:09 +0900

    [refactoring] web용 dto를 생성하여 구현

--- src/main/java/chess/controller/ChessWebController.java
+++ src/main/java/chess/controller/ChessWebController.java
@@ -1,9 +1,8 @@
 package chess.controller;
 
-import chess.controller.dto.PieceDto;
+import chess.controller.dto.ChessWebIndexDto;
+import chess.controller.dto.ChessWebResponseDto;
 import chess.controller.dto.ResponseDto;
-import chess.controller.dto.WebDto;
-import chess.domain.player.Team;
 import chess.domain.position.Position;
 import chess.service.ChessService;
 import spark.ModelAndView;
@@ -27,10 +26,8 @@ public class ChessWebController {
 
     public void run() {
         get("/", (req, res) -> {
-            List<WebDto> roomDto = getRoomDto(chessService.getRoomId());
-            Map<String, Object> model = new HashMap<>();
-            model.put("roomId", roomDto);
-            return render(model, "index.html");
+            ChessWebIndexDto chessWebIndexDto = ChessWebIndexDto.of(chessService.getRoomId());
+            return render(chessWebIndexDto, "index.html");
         });
 
         post("/createChessGame", (req, res) -> {
@@ -46,34 +43,22 @@ public class ChessWebController {
 
         post("/move", (req, res) -> {
             String message = null;
-            Map<String, Object> model = new HashMap<>();
             String number = req.queryParams("id").trim();
             Long id = Long.valueOf(number);
             List<String> parameters = new ArrayList<>(Arrays.asList(req.queryParams("parameter").split("_")));
             try {
                 chessService.move(id, parameters);
             } catch (IllegalArgumentException | UnsupportedOperationException e) {
-                model.put("error-message", e.getMessage());
+                message = e.getMessage();
             }
-            putGameDto(id, model);
-            return render(model, "chessGame.html");
+            ResponseDto responseDto = chessService.getResponseDto(id);
+            ChessWebResponseDto chessWebResponseDto = ChessWebResponseDto.of(id, responseDto, message);
+            return render(chessWebResponseDto, "chessGame.html");
         });
 
-        post("/save", (req, res) -> {
-            String number = req.queryParams("id").trim();
-            Long id = Long.valueOf(number);
-            chessService.save(id);
-            Map<String, Object> model = getIndexModel();
-            return render(model, "index.html");
-        });
+        post("/save", (req, res) -> endGame(req, chessService::save));
 
-        post("/end", (req, res) -> {
-            String number = req.queryParams("id").trim();
-            Long id = Long.valueOf(number);
-            chessService.remove(id);
-            Map<String, Object> model = getIndexModel();
-            return render(model, "index.html");
-        });
+        post("/end", (req, res) -> endGame(req, chessService::remove));
 
         get("/movable/:id/:position", (req, res) -> {
             Long id = Long.valueOf(req.params(":id"));
@@ -87,68 +72,24 @@ public class ChessWebController {
         String number = req.queryParams("id").trim();
         Long id = Long.valueOf(number);
         function.accept(id);
-        Map<String, Object> model = new HashMap<>();
-        putGameDto(id, model);
-        return render(model, "chessGame.html");
+        ChessWebResponseDto chessWebResponseDto = getGameResponseDto(id);
+        return render(chessWebResponseDto, "chessGame.html");
     }
 
-    private void putGameDto(Long id, Map<String, Object> model) {
+    private String endGame(Request req, LongConsumer function) {
+        String number = req.queryParams("id").trim();
+        Long id = Long.valueOf(number);
+        function.accept(id);
+        ChessWebIndexDto chessWebIndexDto = ChessWebIndexDto.of(chessService.getRoomId());
+        return render(chessWebIndexDto, "index.html");
+    }
+
+    private ChessWebResponseDto getGameResponseDto(Long id) {
         ResponseDto responseDto = chessService.getResponseDto(id);
-        List<WebDto> boardDto = getBoardDto(responseDto.getBoard());
-        List<WebDto> scoreDto = getScoreDto(responseDto.getScores());
-        WebDto turnDto = getTurnDto(responseDto.getTurn());
-        model.put("board", boardDto);
-        model.put("score", scoreDto);
-        model.put("turn", turnDto);
-        model.put("id", id);
+        return ChessWebResponseDto.of(id, responseDto);
     }
 
-    private Map<String, Object> getIndexModel() {
-        Map<String, Object> model = new HashMap<>();
-        List<WebDto> roomDto = getRoomDto(chessService.getRoomId());
-        model.put("roomId", roomDto);
-        return model;
-    }
-
-    private List<WebDto> getRoomDto(List<Long> roomIds) {
-        return roomIds.stream()
-                .map(room -> {
-                    String key = String.valueOf(room);
-                    String value = String.valueOf(room);
-                    return new WebDto(key, value);
-                })
-                .collect(Collectors.toList());
-    }
-
-    private static WebDto getTurnDto(final Team turn) {
-        return new WebDto(turn.toString(), turn.toString());
-    }
-
-    private static List<WebDto> getBoardDto(Map<Position, PieceDto> board) {
-        return board.entrySet()
-                .stream()
-                .map(entry -> {
-                    Position position = entry.getKey();
-                    PieceDto pieceDto = entry.getValue();
-                    String key = position.getFile().toString() + position.getRank().getRank();
-                    String value = pieceDto.getTeam() + pieceDto.getPieceType();
-                    return new WebDto(key, value);
-                })
-                .collect(Collectors.toList());
-    }
-
-    private static List<WebDto> getScoreDto(final Map<Team, Double> scores) {
-        return scores.entrySet()
-                .stream()
-                .map(entry -> {
-                    String key = entry.getKey().toString();
-                    String value = String.valueOf(entry.getValue());
-                    return new WebDto(key, value);
-                })
-                .collect(Collectors.toList());
-    }
-
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    private static String render(Object object, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(object, templatePath));
     }
 }
