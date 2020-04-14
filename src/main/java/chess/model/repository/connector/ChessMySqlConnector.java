f9247769e0f274398354bdafeb7a0a111befe889
jamie9504 <jamie9504@kakao.com>
2020-04-14 16:44:21 +0900

    [refactor] db charset 적용

--- src/main/java/chess/model/repository/connector/ChessMySqlConnector.java
+++ src/main/java/chess/model/repository/connector/ChessMySqlConnector.java
@@ -10,7 +10,7 @@ public class ChessMySqlConnector {
         Connection con = null;
         String server = "localhost:13306"; // MySQL 서버 주소
         String database = "Chess"; // MySQL DATABASE 이름
-        String option = "?useSSL=false&serverTimezone=UTC";
+        String option = "?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf8";
         String userName = "root"; //  MySQL 서버 아이디
         String password = "root"; // MySQL 서버 비밀번호
 
