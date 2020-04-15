8fa72618eeb6d8353d415a20b6aff4c8b58afbd0
sujin <sujinemilycho@gmail.com>
2020-04-15 14:57:58 +0900

    refactor: WebUIChessApplication 에서 라우팅 이외의 부분 Controller 로 분리

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,68 +1,24 @@
 package chess;
 
 import chess.controller.ChessWebController;
-import chess.db.ChessPieceDao;
-import chess.db.MoveHistoryDao;
-import chess.domains.board.Board;
-import chess.service.ChessWebService;
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static spark.Spark.*;
 
 public class WebUIChessApplication {
 
     public static void main(String[] args) {
-        final HandlebarsTemplateEngine templateEngine = new HandlebarsTemplateEngine();
-        final ChessWebService webService = new ChessWebService(new ChessPieceDao(), new MoveHistoryDao());
-        final ChessWebController controller = new ChessWebController(webService);
-        final Board board = new Board();
+        final ChessWebController controller = new ChessWebController();
 
         staticFiles.location("/");
 
-        get("/", (req, res) -> {
-            return render(templateEngine, new HashMap<>(), "index.html");
-        });
+        get("/", (req, res) -> controller.gameId());
 
-        post("/ready", (req, res) -> {
-            String gameId = req.queryParams("game_id");
+        post("/ready", (req, res) -> controller.canResume(req));
 
-            Map<String, Object> model = controller.canResume(gameId);
+        post("/play", (req, res) -> controller.startNewGame(req));
 
-            return render(templateEngine, model, "game_room.html");
-        });
+        post("/resume", (req, res) -> controller.resumeGame(req));
 
-        post("/play", (req, res) -> {
-            String gameId = req.queryParams("game_id");
-
-            Map<String, Object> model = controller.startNewGame(board, gameId);
-
-            return render(templateEngine, model, "game_room.html");
-        });
-
-        post("/resume", (req, res) -> {
-            String gameId = req.queryParams("game_id");
-
-            Map<String, Object> model = controller.resumeGame(board, gameId);
-
-            return render(templateEngine, model, "game_room.html");
-        });
-
-        post("/move", (req, res) -> {
-            String gameId = req.queryParams("game_id");
-            String source = req.queryParams("source");
-            String target = req.queryParams("target");
-
-            Map<String, Object> model = controller.move(board, gameId, source, target);
-
-            return render(templateEngine, model, "game_room.html");
-        });
-    }
-
-    private static String render(HandlebarsTemplateEngine templateEngine, Map<String, Object> model, String templatePath) {
-        return templateEngine.render(new ModelAndView(model, templatePath));
+        post("/move", (req, res) -> controller.move(req));
     }
 }
--- src/main/java/chess/controller/ChessWebController.java
+++ src/main/java/chess/controller/ChessWebController.java
@@ -1,7 +1,12 @@
 package chess.controller;
 
+import chess.db.ChessPieceDao;
+import chess.db.MoveHistoryDao;
 import chess.domains.board.Board;
 import chess.service.ChessWebService;
+import spark.ModelAndView;
+import spark.Request;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -9,48 +14,64 @@ import java.util.Map;
 
 public class ChessWebController {
     private final ChessWebService service;
+    private final HandlebarsTemplateEngine templateEngine = new HandlebarsTemplateEngine();
+    private final Board board = new Board();
 
-    public ChessWebController(ChessWebService service) {
-        this.service = service;
+    public ChessWebController() {
+        this.service = new ChessWebService(new ChessPieceDao(), new MoveHistoryDao());
     }
 
-    public Map<String, Object> canResume(String gameId) throws SQLException {
+    private static String render(HandlebarsTemplateEngine templateEngine, Map<String, Object> model, String templatePath) {
+        return templateEngine.render(new ModelAndView(model, templatePath));
+    }
+
+    public String gameId() {
+        return render(templateEngine, new HashMap<>(), "index.html");
+    }
+
+    public String canResume(Request req) throws SQLException {
         Map<String, Object> model = new HashMap<>();
+        String gameId = req.queryParams("game_id");
 
         service.canResume(model, gameId);
 
         model.put("game_id", gameId);
-        return model;
+        return render(templateEngine, model, "game_room.html");
     }
 
-    public Map<String, Object> startNewGame(Board board, String gameId) throws SQLException {
+    public String startNewGame(Request req) throws SQLException {
         Map<String, Object> model = new HashMap<>();
+        String gameId = req.queryParams("game_id");
 
         service.startNewGame(board, gameId);
 
         service.provideGameInfo(model, board);
         model.put("game_id", gameId);
-        return model;
+        return render(templateEngine, model, "game_room.html");
     }
 
-    public Map<String, Object> resumeGame(Board board, String gameId) throws SQLException {
+    public String resumeGame(Request req) throws SQLException {
         Map<String, Object> model = new HashMap<>();
+        String gameId = req.queryParams("game_id");
 
         service.resumeGame(board, gameId);
 
         service.provideGameInfo(model, board);
         model.put("game_id", gameId);
-        return model;
+        return render(templateEngine, model, "game_room.html");
     }
 
-    public Map<String, Object> move(Board board, String gameId, String source, String target) throws SQLException {
+    public String move(Request req) throws SQLException {
         Map<String, Object> model = new HashMap<>();
+        String gameId = req.queryParams("game_id");
+        String source = req.queryParams("source");
+        String target = req.queryParams("target");
 
         service.move(board, gameId, source, target);
         service.checkGameOver(model, board, gameId);
 
         service.provideGameInfo(model, board);
         model.put("game_id", gameId);
-        return model;
+        return render(templateEngine, model, "game_room.html");
     }
 }
