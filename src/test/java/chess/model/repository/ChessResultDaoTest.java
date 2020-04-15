78cc7ed7931f8f59a4fafd37f4b0a02d947bb5b4
jamie9504 <jamie9504@kakao.com>
2020-04-16 01:34:35 +0900

    [feat] ChessResultDao Test 보강

--- src/test/java/chess/model/repository/ChessResultDaoTest.java
+++ src/test/java/chess/model/repository/ChessResultDaoTest.java
@@ -5,43 +5,80 @@ import static org.assertj.core.api.Assertions.assertThat;
 import chess.model.domain.board.ChessGame;
 import chess.model.domain.board.TeamScore;
 import chess.model.domain.piece.Color;
+import chess.model.dto.GameResultDto;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ChessResultDaoTest {
 
-    private ChessResultDao chessResultDao;
+    private static final ChessResultDao CHESS_RESULT_DAO = ChessResultDao.getInstance();
+    private static final int GAME_ID = 1;
+    private static final Map<Color, Double> TEAM_SCORE;
 
-    @BeforeEach
-    public void setup() {
-        chessResultDao = ChessResultDao.getInstance();
+    static {
+        TeamScore defaultTeamScore = new ChessGame().getTeamScore();
+        TEAM_SCORE = Collections.unmodifiableMap(new HashMap<>(defaultTeamScore.getTeamScore()));
     }
 
-    @DisplayName("ChessGameResult CRUD")
+    @BeforeEach
+    void setUp() {
+        CHESS_RESULT_DAO.insert(GAME_ID, new TeamScore(TEAM_SCORE));
+    }
+
+    @AfterEach
+    void tearDown() {
+        CHESS_RESULT_DAO.delete(GAME_ID);
+    }
+
     @Test
-    public void crud() {
-        TeamScore defaultTeamScore = new ChessGame().getTeamScore();
-        Map<Color, Double> teamScore = defaultTeamScore.getTeamScore();
-        Map<Color, Double> teamScoreUpdate = new HashMap<>(teamScore);
-        int gameId = 1;
+    void getInstance() {
+        ChessResultDao chessResultDao1 = ChessResultDao.getInstance();
+        ChessResultDao chessResultDao2 = ChessResultDao.getInstance();
+        assertThat(chessResultDao1 == chessResultDao2).isTrue();
+    }
 
-        // Select
-        assertThat(chessResultDao.select(gameId)).isEmpty();
-
-        // Insert
-        chessResultDao.insert(gameId, new TeamScore(teamScore));
-        assertThat(chessResultDao.select(gameId)).isEqualTo(teamScore);
-
-        // Update
+    @Test
+    void update() {
+        Map<Color, Double> teamScoreUpdate = new HashMap<>(TEAM_SCORE);
         teamScoreUpdate.put(Color.BLACK, 1.0);
-        chessResultDao.update(gameId, new TeamScore(teamScoreUpdate));
-        assertThat(chessResultDao.select(gameId)).isEqualTo(teamScoreUpdate);
+        CHESS_RESULT_DAO.update(GAME_ID, new TeamScore(teamScoreUpdate));
+        assertThat(CHESS_RESULT_DAO.select(GAME_ID)).isEqualTo(teamScoreUpdate);
+    }
 
-        // Delete
-        chessResultDao.delete(gameId);
-        assertThat(chessResultDao.select(gameId)).isEmpty();
+    @Test
+    void insert() {
+        CHESS_RESULT_DAO.delete(GAME_ID);
+        assertThat(CHESS_RESULT_DAO.select(GAME_ID)).isEmpty();
+
+        CHESS_RESULT_DAO.insert(GAME_ID, new TeamScore(TEAM_SCORE));
+        assertThat(CHESS_RESULT_DAO.select(GAME_ID)).isEqualTo(TEAM_SCORE);
+    }
+
+    @Test
+    void delete() {
+        assertThat(CHESS_RESULT_DAO.select(GAME_ID)).isEqualTo(TEAM_SCORE);
+
+        CHESS_RESULT_DAO.delete(GAME_ID);
+        assertThat(CHESS_RESULT_DAO.select(GAME_ID)).isEmpty();
+    }
+
+    @Test
+    void select() {
+        assertThat(CHESS_RESULT_DAO.select(GAME_ID)).isEqualTo(TEAM_SCORE);
+    }
+
+    @Test
+    void getWinOrDraw() {
+        Optional<GameResultDto> gameResult = CHESS_RESULT_DAO.getWinOrDraw("BLACK");
+        assertThat(gameResult.isPresent()).isTrue();
+        GameResultDto gameResultDto = gameResult.orElseThrow(IllegalArgumentException::new);
+        assertThat(gameResultDto.getWinCount()).isGreaterThanOrEqualTo(0);
+        assertThat(gameResultDto.getDrawCount()).isGreaterThanOrEqualTo(0);
+        assertThat(gameResultDto.getLoseCount()).isGreaterThanOrEqualTo(0);
     }
 }
