253a9a67d7286a36d986a730db34b04baa8f4d8d
yeonnseok <ljinsk3@gmail.com>
2020-04-15 23:38:55 +0900

    refac: web controller 분리

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,80 +1,12 @@
 package chess;
 
-import chess.exception.InvalidPositionException;
-import chess.exception.PieceImpossibleMoveException;
-import chess.exception.TakeTurnException;
-import chess.service.BoardService;
-import chess.service.FinishService;
-import chess.service.TurnService;
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+import chess.controller.WebUIChessController;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-
-import static spark.Spark.*;
 
 public class WebUIChessApplication {
-    private static BoardService boardService = new BoardService();
-    private static TurnService turnService;
-    private static FinishService finishService = new FinishService();
-
-    static {
-        try {
-            turnService = new TurnService();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        staticFiles.location("/public");
-
-        get("/", (req, res) -> render(boardService.receiveEmptyBoard(), "index.html"));
-
-        post("/start", (req, res) -> {
-            turnService.initializeTurn();
-            return render(boardService.receiveInitializedBoard(), "index.html");
-        });
-
-        post("/end", (req, res) -> render(boardService.receiveEmptyBoard(), "index.html"));
-
-        post("/load", (req, res) -> {
-            Map<String, Object> model = boardService.receiveLoadedBoard(turnService);
-            return render(model, "index.html");
-        });
-
-        post("/move", (req, res) -> {
-            Map<String, Object> model = boardService.receiveLoadedBoard(turnService);
-            try {
-                model = boardService.receiveMovedBoard(
-                        req.queryParams("fromPiece"), req.queryParams("toPiece"), turnService, finishService);
-                if (finishService.isFinish()) {
-                    finishService.initialize();
-                    res.redirect("/finish");
-                }
-            } catch(InvalidPositionException | PieceImpossibleMoveException | TakeTurnException e) {
-                res.redirect("/exception");
-            }
-            return render(model, "index.html");
-        });
-
-        get("/finish", (req, res) -> render(turnService.receiveWinner(), "finish.html"));
-
-        get("/exception", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "exception.html");
-        });
-
-        post("/status", (req, res) -> {
-            Map<String, Object> model = boardService.receiveScoreStatus();
-            model.put("turn", turnService.getCurrentTurn() + "차례입니다.");
-            return render(model, "index.html");
-        });
-    }
-
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    public static void main(String[] args) throws SQLException {
+        WebUIChessController chessGame = new WebUIChessController();
+        chessGame.run();
     }
 }
---
+++ src/main/java/chess/controller/WebUIChessController.java
@@ -0,0 +1,72 @@
+package chess.controller;
+
+import chess.exception.InvalidPositionException;
+import chess.exception.PieceImpossibleMoveException;
+import chess.exception.TakeTurnException;
+import chess.service.ChessGameService;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class WebUIChessController {
+    private ChessGameService chessGameService;
+
+    public WebUIChessController() throws SQLException {
+        this.chessGameService = new ChessGameService();
+    }
+
+    public void run() {
+        staticFiles.location("/public");
+
+        get("/", (req, res) -> render(chessGameService.receiveEmptyBoard(), "index.html"));
+
+        post("/start", (req, res) -> {
+            chessGameService.initializeTurn();
+            return render(chessGameService.receiveInitializedBoard(), "index.html");
+        });
+
+        post("/end", (req, res) -> render(chessGameService.receiveEmptyBoard(), "index.html"));
+
+        post("/load", (req, res) -> {
+            Map<String, Object> model = chessGameService.receiveLoadedBoard();
+            return render(model, "index.html");
+        });
+
+        post("/move", (req, res) -> {
+            Map<String, Object> model = chessGameService.receiveLoadedBoard();
+            try {
+                model = chessGameService.receiveMovedBoard(
+                        req.queryParams("fromPiece"), req.queryParams("toPiece"));
+                if (chessGameService.isFinish()) {
+                    chessGameService.initialize();
+                    res.redirect("/finish");
+                }
+            } catch(InvalidPositionException | PieceImpossibleMoveException | TakeTurnException e) {
+                res.redirect("/exception");
+            }
+            return render(model, "index.html");
+        });
+
+        get("/finish", (req, res) -> render(chessGameService.receiveWinner(), "finish.html"));
+
+        get("/exception", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "exception.html");
+        });
+
+        post("/status", (req, res) -> {
+            Map<String, Object> model = chessGameService.receiveScoreStatus();
+            model.put("turn", chessGameService.getCurrentTurn() + "차례입니다.");
+            return render(model, "index.html");
+        });
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+}
