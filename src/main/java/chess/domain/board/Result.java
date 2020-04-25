0d2028cc3f68ba3e1d381d5a82a64b0a131de644
junk-sound <wom2277@naver.com>
2020-04-25 20:26:50 +0900

    refac: Result - constructor

--- src/main/java/chess/domain/board/Result.java
+++ src/main/java/chess/domain/board/Result.java
@@ -1,8 +1,12 @@
 package chess.domain.board;
 
+import chess.domain.piece.Piece;
+import chess.domain.piece.PiecesState;
+import chess.domain.piece.position.Position;
 import chess.domain.piece.score.Score;
 import chess.domain.piece.team.Team;
 
+import java.util.Map;
 import java.util.Objects;
 
 public class Result {
@@ -11,12 +15,12 @@ public class Result {
     private final Team loser;
     private final Score loserScore;
 
-
-    Result(Team winner, Score winnerScore, Team loser, Score loserScore) {
-        this.winner = winner;
-        this.winnerScore = winnerScore;
-        this.loser = loser;
-        this.loserScore = loserScore;
+    //todo: check if board is good
+    Result(Map<Position, Piece> pieces) {
+        this.winner = concludeWinner(pieces);
+        this.winnerScore = calculateScore(winner, pieces);
+        this.loser = winner.getOpposite();
+        this.loserScore = calculateScore(loser, pieces);
     }
 
     public String getWinner() {
@@ -33,6 +37,28 @@ public class Result {
 
     public String getLoserScore() {
         return loserScore.toString();
+    }
+
+    private Team concludeWinner(Map<Position, Piece> pieces) {
+        return pieces.values()
+                .stream()
+                .filter(Piece::isNotBlank)
+                .filter(Piece::isKing)
+                .findAny()
+                .map(Piece::getTeam)
+                .orElseThrow(() -> new IllegalStateException("어떤 팀도 King을 가지고 있지 않습니다."));
+    }
+
+    private Score calculateScore(Team team, Map<Position, Piece> pieces) {
+        PiecesState piecesState = new PiecesState(pieces);
+        double sum = pieces.values()
+                .stream()
+                .filter(Piece::isNotBlank)
+                .filter(piece -> piece.isSameTeam(team))
+                .map(piece -> piece.calculateScore(piecesState))
+                .mapToDouble(Score::getValue)
+                .sum();
+        return new Score(sum);
     }
 
     @Override
