4d7067a9155033286b79a8e2ec9c1f19350f2813
junk-sound <wom2277@naver.com>
2020-05-18 16:21:52 +0900

    refac: 예외처리 기능 추가

--- src/main/java/chess/dao/PieceDao.java
+++ src/main/java/chess/dao/PieceDao.java
@@ -15,37 +15,80 @@ public class PieceDao {
 
 
     public void add(Piece piece) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionGetter.get();
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "insert into pieces(id, team, name) values(?,?,?)");
-        preparedStatement.setString(1, piece.getId());
-        Team team = piece.getTeam();
-        preparedStatement.setString(2, team.toString());
-        preparedStatement.setString(3, piece.getName());
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = connectionGetter.get();
+            ps = c.prepareStatement(
+                    "insert into pieces(id, team, name) values(?,?,?)");
+            ps.setString(1, piece.getId());
+            Team team = piece.getTeam();
+            ps.setString(2, team.toString());
+            ps.setString(3, piece.getName());
 
-        preparedStatement.executeUpdate();
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
 
-        preparedStatement.close();
-        connection.close();
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+
+                }
+            }
+
+        }
     }
 
     public Piece get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionGetter.get();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c = connectionGetter.get();
+            ps = c.prepareStatement(
+                    "select * from pieces where id = ?");
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            rs.next();
+            Piece piece = new Piece(Team.valueOf(rs.getString("team")), rs.getString("name"), null, null);
+            piece.setId(id);
+            return piece;
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
 
-        PreparedStatement ps = c.prepareStatement(
-                "select * from pieces where id = ?");
-        ps.setString(1, id);
-        ResultSet rs = ps.executeQuery();
-        rs.next();
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
 
-        Piece piece = new Piece(Team.valueOf(rs.getString("team")), rs.getString("name"), null, null);
-        piece.setId(id);
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
 
-        rs.close();
-        ps.close();
-        c.close();
+                }
+            }
 
-        return piece;
+
+        }
     }
 
 
