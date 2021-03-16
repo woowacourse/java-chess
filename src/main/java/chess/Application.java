06bc33d112754dbb43243e0131eb96c47bdefbc1
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-16 14:47:31 +0900

    feat: 게임 시작 안내문 출력 기능 추가

---
+++ src/main/java/chess/Application.java
@@ -0,0 +1,9 @@
+package chess;
+
+import chess.view.InputView;
+
+public class Application {
+    public static void main(String[] args) {
+        InputView.printGameStartMessage();
+    }
+}
---
+++ src/main/java/chess/view/InputView.java
@@ -0,0 +1,8 @@
+package chess.view;
+
+public class InputView {
+    public static void printGameStartMessage() {
+        System.out.println("체스 게임을 시작합니다.");
+        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+    }
+}
