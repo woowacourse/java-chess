7fd3c8e136da5f4a504ee564dc5a563639bfcf44
TaewanKimmmm <ktww88@gmail.com>
2021-03-16 18:19:35 +0900

    feat: 좌표 객체 비정상적인 입력에 대한 예외 처리

--- src/main/java/chess/Point.java
+++ src/main/java/chess/Point.java
@@ -3,20 +3,38 @@ package chess;
 public class Point {
     private static final int ASCII_CODE_GAP = 49;
     private static final int RANK_GAP = 8;
+    private static final char MINIMUM_LETTER = 'a';
+    private static final char MAXIMUM_LETTER = 'h';
+    private static final int MINIMUM_RANK = 1;
+    private static final int MAXIMUM_RANK = 8;
 
     private final int x;
     private final int y;
 
-    public Point(char a, int i) {
-        x = convertLetterToIndex(a);
-        y = convertRankToIndex(i);
+    public Point(char letter, int rank) {
+        x = convertLetterToIndex(letter);
+        y = convertRankToIndex(rank);
     }
 
-    private int convertRankToIndex(int i) {
-        return Math.abs(i - RANK_GAP);
+    private int convertLetterToIndex(char letter) {
+        validateLetter(letter);
+        return (int) letter - ASCII_CODE_GAP;
     }
 
-    private int convertLetterToIndex(char a) {
-        return (int) a - ASCII_CODE_GAP;
+    private void validateLetter(char letter) {
+        if (letter < MINIMUM_LETTER || MAXIMUM_LETTER < letter){
+            throw new IllegalArgumentException("옳지 않은 열 입력입니다.");
+        }
+    }
+
+    private int convertRankToIndex(int rank) {
+        validateRank(rank);
+        return Math.abs(rank - RANK_GAP);
+    }
+
+    private void validateRank(int rank) {
+        if (rank < MINIMUM_RANK || MAXIMUM_RANK < rank){
+            throw new IllegalArgumentException("옳지 않은 행 입력입니다.");
+        }
     }
 }
--- src/test/java/chess/PointTest.java
+++ src/test/java/chess/PointTest.java
@@ -4,14 +4,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class PointTest {
     @DisplayName("좌표 생성")
     @Test
     void create() {
-        Assertions.assertDoesNotThrow(
-                () -> new Point('a', 1)
+        assertDoesNotThrow(
+                () -> new Point('a', 8)
         );
+        assertDoesNotThrow(
+                () -> new Point('h', 1)
+        );
+    }
+
+    @DisplayName("범위 밖 좌표 생성 시 예외 처리")
+    @Test
+    void checkIndexOutOfRange() {
+        assertThatThrownBy(()-> new Point('i', 0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> new Point('i', 8)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> new Point('h', 0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> new Point('h', 9)).isInstanceOf(IllegalArgumentException.class);
     }
 }
