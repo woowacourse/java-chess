8fdec640539666998aabfa3a42bd562d5ef07d1a
Wannte <T.wannte@gmail.com>
2021-03-16 20:59:12 +0900

    refactor(Position): cache 로 불필요해진 equals 삭제

--- src/main/java/domain/position/Position.java
+++ src/main/java/domain/position/Position.java
@@ -42,17 +42,4 @@ public class Position {
     public Position moveBy(int columnValue, int rowValue) {
         return Position.of(column.moveBy(columnValue), row.moveBy(rowValue));
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Position position = (Position) o;
-        return column == position.column && row == position.row;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(column, row);
-    }
 }
