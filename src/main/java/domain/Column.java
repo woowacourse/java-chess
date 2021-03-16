862d3517cd43fb169bd358a9a7a5c70b5d3a4c47
Wannte <T.wannte@gmail.com>
2021-03-16 16:49:04 +0900

    feat(Row, Column): moveBy 메서드 구현

--- src/main/java/domain/Column.java
+++ src/main/java/domain/Column.java
@@ -11,8 +11,16 @@ public enum Column {
     H("h");
 
     private final String value;
+    private static Column[] columns = values();
 
     Column(String value) {
         this.value = value;
+    }
+
+    public Column moveBy(int value) {
+        if ((this.ordinal() + value) < 0 || this.ordinal() + value >= columns.length) {
+            throw new IllegalArgumentException("범위를 넘어가는 move 입니다");
+        }
+        return columns[(this.ordinal() + value)];
     }
 }
--- src/main/java/domain/Row.java
+++ src/main/java/domain/Row.java
@@ -11,8 +11,16 @@ public enum Row {
     EIGHT("8");
 
     private final String value;
+    private static Row[] rows = values();
 
     Row(String value) {
         this.value = value;
+    }
+
+    public Row moveBy(int value) {
+        if ((this.ordinal() + value) < 0 || this.ordinal() + value >= rows.length) {
+            throw new IllegalArgumentException("범위를 넘어가는 move 입니다");
+        }
+        return rows[(this.ordinal() + value)];
     }
 }
