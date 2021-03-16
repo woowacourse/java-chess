0a1a62620c813f400ee193801e9a6faf5ad8360e
Wannte <T.wannte@gmail.com>
2021-03-16 16:57:37 +0900

    feat(Position): moveBy 메서드 구현

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Position {
     private final Column column;
     private final Row row;
@@ -7,5 +9,23 @@ public class Position {
     public Position(final Column column, final Row row) {
         this.column = column;
         this.row = row;
+    }
+
+
+    public Position moveBy(int columnValue, int rowValue) {
+        return new Position(column.moveBy(columnValue), row.moveBy(rowValue));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return column == position.column && row == position.row;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column, row);
     }
 }
--- src/test/java/domain/positionTest.java
+++ src/test/java/domain/positionTest.java
@@ -1,13 +1,36 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class positionTest {
+
+    private static Stream<Arguments> positions() {
+        return Stream.of(
+                Arguments.of(-3, -5, new Position(Column.A, Row.ONE)),
+                Arguments.of(-2, 2, new Position(Column.B, Row.EIGHT)),
+                Arguments.of(0, 2, new Position(Column.D, Row.EIGHT)),
+                Arguments.of(4, 0, new Position(Column.H, Row.SIX))
+        );
+    }
+
     @Test
     void createPosition() {
         assertThatCode(() -> new Position(Column.A, Row.ONE))
                 .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("positions")
+    void moveBy(int column, int row, Position expected) {
+        Position position = new Position(Column.D, Row.SIX);
+        assertThat(position.moveBy(column, row)).isEqualTo(expected);
     }
 }
