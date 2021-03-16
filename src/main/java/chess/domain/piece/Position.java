9ff03665e2c589437ed7ef62b1daaea6e673e56f
hoony <younghoon.kwon91@gmail.com>
2021-03-16 15:38:07 +0900

    feat(Position): 체스 말의 위치를 나타내는 Position 클래스에서 생성자 구현

---
+++ src/main/java/chess/domain/piece/Position.java
@@ -0,0 +1,11 @@
+package chess.domain.piece;
+
+public class Position {
+    private final char horizontal;
+    private final char vertical;
+
+    public Position(char horizontal, char vertical) {
+        this.horizontal = horizontal;
+        this.vertical = vertical;
+    }
+}
