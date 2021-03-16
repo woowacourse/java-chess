c20c42f24edf63d596ff091e4e9d02b5a993b401
Sol ji Choi <bssj9307@gmail.com>
2021-03-16 14:52:33 +0900

    feat: 체스판 생성 메소드 분리

--- src/main/java/chess/ChessBoard.java
+++ src/main/java/chess/ChessBoard.java
@@ -7,49 +7,45 @@ public class ChessBoard {
     private final String[][] chessBoard = new String[8][8];
 
     public ChessBoard() {
-        int[] temp = {0, 7};
-        for (int i : temp) {
-            for (int j = 0; j < 8; j++) {
-                if (j == 0 || j == 7) {
-                    chessBoard[i][j] = "R";
-                }
-                if (j == 1 || j == 6) {
-                    chessBoard[i][j] = "N";
-                }
-                if (j == 2 || j == 5) {
-                    chessBoard[i][j] = "B";
-                }
-                if (j == 3) {
-                    chessBoard[i][j] = "Q";
-                }
-                if (j == 4) {
-                    chessBoard[i][j] = "K";
-                }
-            }
-        }
-        int[] pawn = {1, 6};
-        for (int i : pawn) {
-            for (int j = 0; j < 8; j++) {
-                chessBoard[i][j] = "P";
-            }
-        }
-        for (int i = 2; i < 6; i++) {
-            for (int j = 0; j < 8; j++) {
-                chessBoard[i][j] = ".";
-            }
-        }
-        chessBoard[6] = Arrays.stream(chessBoard[6])
-            .map(String::toLowerCase)
-            .toArray(String[]::new);
-        chessBoard[7] = Arrays.stream(chessBoard[7])
-            .map(String::toLowerCase)
-            .toArray(String[]::new);
+        initPiece();
+        initPawn();
+        initBlank();
+        toLowerCase();
         for (String[] array : chessBoard) {
             for (String board : array) {
                 System.out.print(board);
             }
             System.out.println();
         }
+    }
+
+    private void initPiece() {
+        int[] row = {0, 7};
+        for (int i : row) {
+            chessBoard[i] = new String[]{"R", "N", "B", "Q", "K", "B", "N", "R"};
+        }
+    }
+
+    private void initPawn() {
+        int[] row = {1, 6};
+        for (int i : row) {
+            chessBoard[i] = new String[]{"P", "P", "P", "P", "P", "P", "P", "P"};
+        }
+    }
+
+    private void initBlank() {
+        for (int i = 2; i < 6; i++) {
+            chessBoard[i] = new String[]{".", ".", ".", ".", ".", ".", ".", "."};
+        }
+    }
+
+    private void toLowerCase() {
+        chessBoard[6] = Arrays.stream(chessBoard[6])
+            .map(String::toLowerCase)
+            .toArray(String[]::new);
+        chessBoard[7] = Arrays.stream(chessBoard[7])
+            .map(String::toLowerCase)
+            .toArray(String[]::new);
     }
 
     public String[][] getChessBoard() {
