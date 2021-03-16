53ac478d447e872aa7d970c9f0b2299e4cd3b81b
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-16 15:24:44 +0900

    feat: 체스판의 행에 해당하는 File 열거형 추가

---
+++ src/main/java/chess/domain/board/File.java
@@ -0,0 +1,22 @@
+package chess.domain.board;
+
+public enum File {
+    A("a"),
+    B("b"),
+    C("c"),
+    D("d"),
+    E("e"),
+    F("f"),
+    G("g"),
+    H("h");
+
+    private final String value;
+
+    File(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
