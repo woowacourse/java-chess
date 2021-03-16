87fd43162c58b5270e6ada721a9d9444aa1a794a
Wannte <T.wannte@gmail.com>
2021-03-16 17:44:51 +0900

    feat(Position): 체스 좌표 캐싱

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -1,19 +1,41 @@
 package domain;
 
-import java.util.Objects;
+import java.util.*;
 
 public class Position {
     private final Column column;
     private final Row row;
+    private static final Map<Column, Map<Row, Position>> cache = new HashMap<>();
 
-    public Position(final Column column, final Row row) {
+    static {
+        for (Column x : Column.values()) {
+            cache.put(x, getCacheByColumn(x));
+        }
+    }
+
+    private static Map<Row, Position> getCacheByColumn(Column x) {
+        Map<Row, Position> cacheByColumn = new HashMap<>();
+        for (Row y : Row.values()) {
+            cacheByColumn.put(y, new Position(x, y));
+        }
+        return cacheByColumn;
+    }
+
+    public static Position of(Column column, Row row) {
+        if (cache.containsKey(column) && cache.get(column).containsKey(row)) {
+            return cache.get(column).get(row);
+        }
+        return new Position(column, row);
+    }
+
+    private Position(final Column column, final Row row) {
         this.column = column;
         this.row = row;
     }
 
 
     public Position moveBy(int columnValue, int rowValue) {
-        return new Position(column.moveBy(columnValue), row.moveBy(rowValue));
+        return Position.of(column.moveBy(columnValue), row.moveBy(rowValue));
     }
 
     @Override
--- src/test/java/domain/positionTest.java
+++ src/test/java/domain/positionTest.java
@@ -14,23 +14,23 @@ public class positionTest {
 
     private static Stream<Arguments> positions() {
         return Stream.of(
-                Arguments.of(-3, -5, new Position(Column.A, Row.ONE)),
-                Arguments.of(-2, 2, new Position(Column.B, Row.EIGHT)),
-                Arguments.of(0, 2, new Position(Column.D, Row.EIGHT)),
-                Arguments.of(4, 0, new Position(Column.H, Row.SIX))
+                Arguments.of(-3, -5, Position.of(Column.A, Row.ONE)),
+                Arguments.of(-2, 2, Position.of(Column.B, Row.EIGHT)),
+                Arguments.of(0, 2, Position.of(Column.D, Row.EIGHT)),
+                Arguments.of(4, 0, Position.of(Column.H, Row.SIX))
         );
     }
 
     @Test
     void createPosition() {
-        assertThatCode(() -> new Position(Column.A, Row.ONE))
+        assertThatCode(() -> Position.of(Column.A, Row.ONE))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @MethodSource("positions")
     void moveBy(int column, int row, Position expected) {
-        Position position = new Position(Column.D, Row.SIX);
+        Position position = Position.of(Column.D, Row.SIX);
         assertThat(position.moveBy(column, row)).isEqualTo(expected);
     }
 }
