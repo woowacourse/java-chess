79ba3f02cec120527560e86e5b76c60e49b1f326
pika96 <52442949+pika96@users.noreply.github.com>
2021-03-16 21:28:02 +0900

    feat(Controller): 보드 게임 초기화 로직 구현

---
+++ src/main/java/chess/controller/ChessGame.java
@@ -0,0 +1,18 @@
+package chess.controller;
+
+import chess.domain.Board;
+import chess.view.InputView;
+import chess.view.OutputView;
+
+public class ChessGame {
+    public void start() {
+        OutputView.startGame();
+        String command = InputView.command();
+        if(command.equals("start")){
+            Board board = new Board();
+            OutputView.printBoard(board);
+        }
+
+
+    }
+}
