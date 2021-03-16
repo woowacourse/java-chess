8dca53eb2fefbf4281e29906970a041d357fd313
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-16 14:53:19 +0900

    feat: 플레이어의 선택을 입력받는 기능 구현

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -5,5 +5,11 @@ import chess.view.InputView;
 public class Application {
     public static void main(String[] args) {
         InputView.printGameStartMessage();
+
+        String playerCommand = InputView.inputPlayerCommand();
+        while ("start".equals(playerCommand)) {
+            //todo : 체스판 출력
+            playerCommand = InputView.inputPlayerCommand();
+        }
     }
 }
--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,8 +1,20 @@
 package chess.view;
 
+import java.util.Scanner;
+import jdk.internal.util.xml.impl.Input;
+
 public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {
+    }
+
     public static void printGameStartMessage() {
         System.out.println("체스 게임을 시작합니다.");
         System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+    }
+
+    public static String inputPlayerCommand() {
+        return SCANNER.nextLine();
     }
 }
