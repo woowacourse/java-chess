1e1e711eb08e6e329846582e016a57c08db6dcb3
bepoz <kangsy763@naver.com>
2021-03-17 17:45:01 +0900

    feature : Queen 이동 가능 좌표 반환

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -1,8 +1,15 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import chess.domain.board.Horizontal;
 import chess.domain.board.Position;
+import chess.domain.board.Vertical;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static chess.domain.board.Board.*;
 
 public class Queen extends Piece {
     private static final String QUEEN_NAME = "Q";
@@ -13,6 +20,54 @@ public class Queen extends Piece {
 
     @Override
     public List<Position> movablePositions(Position target) {
-        return null;
+        List<Position> movablePositions = new ArrayList<>();
+
+        movablePositions.addAll(rookPosition(target));
+        movablePositions.addAll(bishopPosition(target));
+
+        return movablePositions;
+    }
+
+    private List<Position> rookPosition(Position target) {
+        List<Position> movablePositions = new ArrayList<>();
+        Vertical targetVertical = target.getVertical();
+        Horizontal targetHorizontal = target.getHorizontal();
+
+        for (Horizontal horizontal : Horizontal.values()) {
+            movablePositions.add(Position.of(horizontal, targetVertical));
+        }
+        for (Vertical vertical : Vertical.values()) {
+            movablePositions.add(Position.of(targetHorizontal, vertical));
+        }
+
+        movablePositions.removeAll(Collections.singletonList(target));
+
+        return movablePositions;
+    }
+
+    private List<Position> bishopPosition(Position target) {
+        List<Position> movablePositions = new ArrayList<>();
+
+        movablePositions.addAll(calculateBishopMovablePositions(target, POSITIVE, POSITIVE));
+        movablePositions.addAll(calculateBishopMovablePositions(target, NEGATIVE, POSITIVE));
+        movablePositions.addAll(calculateBishopMovablePositions(target, NEGATIVE, NEGATIVE));
+        movablePositions.addAll(calculateBishopMovablePositions(target, POSITIVE, NEGATIVE));
+
+        return movablePositions;
+    }
+
+    private List<Position> calculateBishopMovablePositions(Position target, int horizontalDirection, int verticalDirection) {
+        List<Position> result = new ArrayList<>();
+        int horizontalWeight = target.getHorizontalWeight();
+        int verticalWeight = target.getVerticalWeight();
+        while (horizontalWeight + horizontalDirection >= MIN_BORDER && horizontalWeight + horizontalDirection <= MAX_BORDER
+                && verticalWeight + verticalDirection >= MIN_BORDER && verticalWeight + verticalDirection <= MAX_BORDER) {
+            horizontalWeight += horizontalDirection;
+            verticalWeight += verticalDirection;
+            result.add(
+                    Position.of(Horizontal.findFromWeight(horizontalWeight), Vertical.findFromWeight(verticalWeight))
+            );
+        }
+        return result;
     }
 }
---
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -0,0 +1,45 @@
+package chess.domain.piece;
+
+import chess.domain.board.Horizontal;
+import chess.domain.board.Position;
+import chess.domain.board.Vertical;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueenTest {
+    @Test
+    @DisplayName("퀸 움직일 수 있는 좌표 확인")
+    void movablePositionTest() {
+        Queen queen = new Queen(Team.BLACK);
+        List<Position> positions = queen.movablePositions(Position.of(Horizontal.B, Vertical.ONE));
+
+        assertThat(positions).containsExactly(
+                Position.of(Horizontal.A, Vertical.ONE),
+                Position.of(Horizontal.C, Vertical.ONE),
+                Position.of(Horizontal.D, Vertical.ONE),
+                Position.of(Horizontal.E, Vertical.ONE),
+                Position.of(Horizontal.F, Vertical.ONE),
+                Position.of(Horizontal.G, Vertical.ONE),
+                Position.of(Horizontal.H, Vertical.ONE),
+                Position.of(Horizontal.B, Vertical.EIGHT),
+                Position.of(Horizontal.B, Vertical.SEVEN),
+                Position.of(Horizontal.B, Vertical.SIX),
+                Position.of(Horizontal.B, Vertical.FIVE),
+                Position.of(Horizontal.B, Vertical.FOUR),
+                Position.of(Horizontal.B, Vertical.THREE),
+                Position.of(Horizontal.B, Vertical.TWO),
+                Position.of(Horizontal.C, Vertical.TWO),
+                Position.of(Horizontal.D, Vertical.THREE),
+                Position.of(Horizontal.E, Vertical.FOUR),
+                Position.of(Horizontal.F, Vertical.FIVE),
+                Position.of(Horizontal.G, Vertical.SIX),
+                Position.of(Horizontal.H, Vertical.SEVEN),
+                Position.of(Horizontal.A, Vertical.TWO)
+        );
+    }
+}
