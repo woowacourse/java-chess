04b4288f1e9fadb4c5523b44daa525a659d3be80
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-17 18:36:53 +0900

    feat(Piece): 체스 기물 움직임 체크 구현

--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -3,6 +3,8 @@ package chess.domain.piece;
 import chess.domain.ChessBoard;
 import chess.domain.Position;
 
+import java.util.List;
+
 public abstract class Piece {
     private final String team;
 
@@ -11,6 +13,41 @@ public abstract class Piece {
     }
 
     abstract boolean isMovable(final Position current, final Position destination, final ChessBoard chessBoard);
+
+    abstract boolean checkPositionRule(final Position current, final Position destination);
+
+    public final boolean checkDiagonalRule(final Position current, final Position destination) {
+        final int xDiff = Math.abs(current.getX() - destination.getX());
+        final int yDiff = Math.abs(current.getY() - destination.getY());
+        if (xDiff == yDiff) {
+            return true;
+        }
+        return false;
+    }
+
+    public final boolean checkStraightRule(final Position current, final Position destination) {
+        if (current.getX() == destination.getX() && current.getY() != destination.getY()) {
+            return true;
+        }
+        if (current.getY() == destination.getY() && current.getX() != destination.getX()) {
+            return true;
+        }
+        return false;
+    }
+
+    public final boolean checkEmptyPath(final List<Position> path, final ChessBoard chessBoard) {
+        System.out.println("여기로 와야해");
+        System.out.println("path = " + path);
+        for (Position position : path) {
+            if (chessBoard.havePiece(position)) {
+                return false;
+            }
+        }
+        return true;
+//
+//        return path.stream()
+//                .noneMatch(chessBoard::havePiece);
+    }
 
     public final String getTeam() {
         return team;
