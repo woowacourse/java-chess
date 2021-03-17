691eb3cdf8e559072ce5729e492720ad136c2afd
TaewanKimmmm <ktww88@gmail.com>
2021-03-17 14:26:53 +0900

    refactor: 64개의 Point 캐싱

--- src/main/java/chess/Point.java
+++ src/main/java/chess/Point.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Point {
     private static final int ASCII_CODE_GAP = 49;
     private static final int RANK_GAP = 8;
@@ -7,34 +10,38 @@ public class Point {
     private static final char MAXIMUM_LETTER = 'h';
     private static final int MINIMUM_RANK = 1;
     private static final int MAXIMUM_RANK = 8;
+    private static final String INVALID_INPUT_ERROR_MESSAGE = "옳지 않은 입력입니다.";
+
+    private static final List<Point> points = new ArrayList<>();
+
+    static {
+        for (int i = MINIMUM_LETTER; i <= MAXIMUM_LETTER; i++) {
+            for (int j = MINIMUM_RANK; j <= MAXIMUM_RANK; j++) {
+                points.add(new Point((char) i, j));
+            }
+        }
+    }
 
     private final int x;
     private final int y;
 
-    public Point(char letter, int rank) {
+    public static Point of(char letter, int rank) {
+        return points.stream()
+                .filter(p -> p.x == p.convertLetterToIndex(letter) && p.y == p.convertRankToIndex(rank))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE));
+    }
+
+    private Point(char letter, int rank) {
         x = convertLetterToIndex(letter);
         y = convertRankToIndex(rank);
     }
 
     private int convertLetterToIndex(char letter) {
-        validateLetter(letter);
         return (int) letter - ASCII_CODE_GAP;
     }
 
-    private void validateLetter(char letter) {
-        if (letter < MINIMUM_LETTER || MAXIMUM_LETTER < letter){
-            throw new IllegalArgumentException("옳지 않은 열 입력입니다.");
-        }
-    }
-
     private int convertRankToIndex(int rank) {
-        validateRank(rank);
-        return Math.abs(rank - RANK_GAP);
-    }
-
-    private void validateRank(int rank) {
-        if (rank < MINIMUM_RANK || MAXIMUM_RANK < rank){
-            throw new IllegalArgumentException("옳지 않은 행 입력입니다.");
-        }
+        return RANK_GAP - rank;
     }
 }
--- src/test/java/chess/PointTest.java
+++ src/test/java/chess/PointTest.java
@@ -1,6 +1,5 @@
 package chess;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,19 +11,19 @@ class PointTest {
     @Test
     void create() {
         assertDoesNotThrow(
-                () -> new Point('a', 8)
+                () -> Point.of('a', 8)
         );
         assertDoesNotThrow(
-                () -> new Point('h', 1)
+                () -> Point.of('h', 1)
         );
     }
 
     @DisplayName("범위 밖 좌표 생성 시 예외 처리")
     @Test
     void checkIndexOutOfRange() {
-        assertThatThrownBy(()-> new Point('i', 0)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()-> new Point('i', 8)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()-> new Point('h', 0)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()-> new Point('h', 9)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> Point.of('i', 0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> Point.of('i', 8)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> Point.of('h', 0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> Point.of('h', 9)).isInstanceOf(IllegalArgumentException.class);
     }
 }
