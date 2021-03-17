3f4f062c80ff2cd13f1a3939ed3c00fd6a9ecf8a
hoony <younghoon.kwon91@gmail.com>
2021-03-17 17:51:42 +0900

    feat(Rook): 룩 관련 로직 구현

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -1,16 +1,34 @@
 package chess.domain.piece;
 
+import chess.domain.position.Direction;
+import chess.domain.grid.Grid;
+import chess.domain.position.Position;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Rook extends Piece {
     private static final char NAME_WHEN_BLACK = 'R';
     private static final char NAME_WHEN_WHITE = 'r';
+    private static final int LINE_COUNT = 8;
 
-    public Rook(Boolean isBlack, char x, char y) {
+    public Rook(final Boolean isBlack, final char x, final char y) {
         super(isBlack, x, y);
     }
 
     @Override
-    void movable(char nextX, char nextY) {
-
+    List<Position> extractMovablePositions() {
+        return Direction.linearDirection()
+                .stream()
+                .flatMap(direction -> IntStream
+                        .rangeClosed(1, LINE_COUNT)
+                        .mapToObj(index -> getPosition().moved(
+                                direction.getXDegree() * index,
+                                direction.getYDegree() * index
+                        ))
+                        .filter(position -> !isOutOfRange(position) && Grid.isOccupied(position)))
+                .collect(Collectors.toList());
     }
 
     @Override
