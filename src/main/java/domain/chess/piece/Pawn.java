94931051c998028e1e18648d2a2a4c80dfa46ae9
sori <bosl95@naver.com>
2021-03-17 16:30:59 +0900

    refactor : pawn을 static list로 변경

--- src/main/java/domain/chess/piece/Pawn.java
+++ src/main/java/domain/chess/piece/Pawn.java
@@ -5,6 +5,22 @@ import java.util.List;
 
 public class Pawn extends Piece {
     private static final double SCORE = 3;
+    public static final List<Pawn> PAWNS = Arrays.asList(Pawn.Of("P", Position.Of(1, 0), true),
+            Pawn.Of("P", Position.Of(1, 1), true),
+            Pawn.Of("P", Position.Of(1, 2), true),
+            Pawn.Of("P", Position.Of(1, 3), true),
+            Pawn.Of("P", Position.Of(1, 4), true),
+            Pawn.Of("P", Position.Of(1, 5), true),
+            Pawn.Of("P", Position.Of(1, 6), true),
+            Pawn.Of("P", Position.Of(1, 7), true),
+            Pawn.Of("p", Position.Of(6, 0), false),
+            Pawn.Of("p", Position.Of(6, 1), false),
+            Pawn.Of("p", Position.Of(6, 2), false),
+            Pawn.Of("p", Position.Of(6, 3), false),
+            Pawn.Of("p", Position.Of(6, 4), false),
+            Pawn.Of("p", Position.Of(6, 5), false),
+            Pawn.Of("p", Position.Of(6, 6), false),
+            Pawn.Of("p", Position.Of(6, 7), false));
 
     private Pawn(String name, int x, int y, boolean isBlack) {
         super(name, SCORE, Position.Of(x, y), isBlack);
