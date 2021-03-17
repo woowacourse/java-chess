f380df0b48ad548838664417f1dabccaf020a63b
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-17 19:24:54 +0900

    feat(Pawn): 검은 팀인 경우 폰 이동 구현

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -4,12 +4,44 @@ import chess.domain.ChessBoard;
 import chess.domain.Position;
 
 public class Pawn extends Piece {
+    private boolean started;
+
     public Pawn(final String team) {
         super(team);
+        started = false;
     }
 
     @Override
     public boolean isMovable(Position current, Position destination, ChessBoard chessBoard) {
+        if ("Black".equals(getTeam())) {
+            //최초 시작인 경우
+            if (!started && (current.moveY(-1).equals(destination) || current.moveY(-2).equals(destination))) {
+                started = true;
+                return true;
+            }
+            //이미 시작한 경우
+            if (started && current.moveY(-1).equals(destination)) {
+                return true;
+            }
+            // 설마 대각선에 상대 기물이 있나?
+            if (current.moveLeftDown().equals(destination) || current.moveRightDown().equals(destination)) {
+                if (chessBoard.getChessBoard().get(destination).getTeam() == "White") {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    boolean checkPositionRule(Position current, Position destination) {
+        if (!started && (current.moveDown().equals(destination) || current.moveDown().moveDown().equals(destination))) {
+            started = true;
+            return true;
+        }
+        if (started && current.moveDown().equals(destination)) {
+            return true;
+        }
         return false;
     }
 }
---
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -0,0 +1,20 @@
+package chess.domain.piece;
+
+import chess.domain.ChessBoard;
+import chess.domain.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class PawnTest {
+    @Test
+    @DisplayName("이동할 수 있는 좌표가 주어졌을 때, 폰이 움직일 수 있는 칸인지 체크한다.")
+    void when_give_position_check_pawn_movable() {
+        ChessBoard chessBoard = new ChessBoard();
+        Pawn pawn = new Pawn("Black");
+        Position current = Position.of("a7");
+        Position destination = Position.of("a6");
+        assertThat(pawn.isMovable(current, destination, chessBoard)).isTrue();
+    }
+}
