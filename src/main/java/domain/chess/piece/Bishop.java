98df870122f7f2fb841f64cf8f407f9e72a42c86
sori <bosl95@naver.com>
2021-03-17 16:29:25 +0900

    refactor : Bishop 이동 로직 추가

--- src/main/java/domain/chess/piece/Bishop.java
+++ src/main/java/domain/chess/piece/Bishop.java
@@ -16,14 +16,56 @@ public class Bishop extends Piece {
 
     public static List<Bishop> initialBishopPieces() {
         return Arrays.asList(Bishop.Of("B", Position.Of(0, 2), true),
-                Bishop.Of("B",Position.Of(0, 5), true),
+                Bishop.Of("B", Position.Of(0, 5), true),
                 Bishop.Of("b", Position.Of(7, 2), false),
                 Bishop.Of("b", Position.Of(7, 5), false)
         );
     }
 
     @Override
-    public boolean canMove(Piece[][] board, Position end) {
-        return false;
+    public boolean canMove(Piece[][] board, Position endPosition) {
+        int dx[] = {-1, 1, -1, 1};
+        int dy[] = {1, 1, -1, -1};
+
+        if (!checkDiagonal(endPosition)) {
+            return false;
+        }
+
+        int index = findDirection(endPosition);
+        int nextRow = position.getRow() + dx[index];
+        int nextColumn = position.getColumn() + dy[index];
+
+        while (!(nextRow == endPosition.getRow() && nextColumn == endPosition.getColumn())
+                && board[nextRow][nextColumn] == null) {
+            nextRow += dx[index];
+            nextColumn += dy[index];
+        }
+
+        return Position.Of(nextRow, nextColumn).equals(endPosition);
+    }
+
+    private int findDirection(Position end) {
+        int row = end.getRow() - position.getRow();
+        int col = end.getColumn() - position.getColumn();
+
+        if (row > 0 && col > 0) {
+            return 1;
+        }
+
+        if (row > 0 && col < 0) {
+            return 3;
+        }
+
+        if (row < 0 && col < 0) {
+            return 2;
+        }
+        return 0;
+    }
+
+    public boolean checkDiagonal(Position endPosition) {
+        int rowDiff = Math.abs(position.getRow() - endPosition.getRow());
+        int colDiff = Math.abs(position.getColumn() - endPosition.getColumn());
+
+        return (rowDiff != 0 && colDiff != 0) && rowDiff == colDiff;
     }
 }
