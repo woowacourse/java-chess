ae2daf072e61333386b263ad50880b6bf22916c7
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-17 18:37:37 +0900

    feat(Queen): 퀸 움직임 체크 구현

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -3,13 +3,32 @@ package chess.domain.piece;
 import chess.domain.ChessBoard;
 import chess.domain.Position;
 
+import java.util.List;
+
 public class Queen extends Piece {
     public Queen(final String team) {
         super(team);
     }
 
     @Override
-    public boolean isMovable(Position current, Position destination, ChessBoard chessBoard) {
+    public boolean isMovable(final Position current, final Position destination, final ChessBoard chessBoard) {
+        if (!checkPositionRule(current, destination)) {
+            return false;
+        }
+        if (checkDiagonalRule(current, destination)) {
+            final List<Position> diagonalPath = current.generateDiagonalPath(destination);
+            return checkEmptyPath(diagonalPath, chessBoard);
+        }
+        if (checkStraightRule(current, destination)) {
+            System.out.println("여기로 와야해");
+            final List<Position> straightPath = current.generateStraightPath(destination);
+            return checkEmptyPath(straightPath, chessBoard);
+        }
         return false;
+    }
+
+    @Override
+    boolean checkPositionRule(Position current, Position destination) {
+        return checkDiagonalRule(current, destination) || checkStraightRule(current, destination);
     }
 }
---
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -0,0 +1,55 @@
+package chess.domain.piece;
+
+import chess.domain.ChessBoard;
+import chess.domain.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+public class QueenTest {
+    @Test
+    @DisplayName("퀸 정상 생성되는지 확인한다")
+    void init() {
+        assertThatCode(() -> new Queen("Black"))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("퀸이 직선으로 움직일 수 있는 경로가 주어지면, 참을 반환한다.")
+    void check_position_rule_straight_valid_test() {
+        final Queen queen = new Queen("Black");
+        final Position start = Position.of("c1");
+        final Position validDestination = Position.of("c3");
+        assertThat(queen.checkPositionRule(start, validDestination)).isTrue();
+    }
+
+    @Test
+    @DisplayName("퀸이 대각선으로 움직일 수 있는 경로가 주어지면, 참을 반환한다.")
+    void check_position_rule_diagonal_valid_test() {
+        final Queen queen = new Queen("Black");
+        final Position start = Position.of("c1");
+        final Position validDestination = Position.of("e3");
+        assertThat(queen.checkPositionRule(start, validDestination)).isTrue();
+    }
+
+    @Test
+    @DisplayName("퀸이 규칙상 정상적으로 움직일 수 없는 경로가 주어지면, 거짓을 반환한다.")
+    void check_position_rule_invalid_test() {
+        final Queen queen = new Queen("Black");
+        final Position start = Position.of("c1");
+        final Position invalidDestination = Position.of("d3");
+        assertThat(queen.checkPositionRule(start, invalidDestination)).isFalse();
+    }
+
+    @Test
+    @DisplayName("퀸이 이동가능한 불가능한 좌표가 주어지면 거짓을 반환 한다.")
+    void check_position_unmovable() {
+        final ChessBoard chessBoard = new ChessBoard();
+        final Queen queen = new Queen("Black");
+        final Position start = Position.of("e8");
+        final Position invalidDestination = Position.of("e6");
+        assertThat(queen.isMovable(start, invalidDestination, chessBoard)).isFalse();
+    }
+}
