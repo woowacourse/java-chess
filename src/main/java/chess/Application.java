4ad1ee5f38cfd74f364db70e0ae6dca5b6323510
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-17 12:23:11 +0900

    feat: 체스 판 전체 출력 기능 구현

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,14 +1,19 @@
 package chess;
 
+import chess.domain.board.Board;
 import chess.view.InputView;
+import chess.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView.printGameStartMessage();
-
         String playerCommand = InputView.inputPlayerCommand();
+
+        Board board = Board.getInstance();
+        board.initialize();
         while ("start".equals(playerCommand)) {
             //todo : 체스판 출력
+            OutputView.printBoard(board);
             playerCommand = InputView.inputPlayerCommand();
         }
     }
---
+++ src/main/java/chess/view/OutputView.java
@@ -0,0 +1,39 @@
+package chess.view;
+
+import chess.domain.board.Board;
+import chess.domain.board.Coordinate;
+import chess.domain.board.File;
+import chess.domain.board.Rank;
+import chess.domain.piece.Piece;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    public static void printBoard(Board board) {
+        List<Rank> ranks = Arrays.asList(Rank.values());
+        List<Rank> reversedRanks = ranks.stream()
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.toList());
+
+        reversedRanks.forEach(rank -> printFiles(rank, board));
+        System.out.println();
+    }
+
+    private static void printFiles(Rank rank, Board board) {
+        for (File file : File.values()) {
+            Piece piece = board.of(new Coordinate(file, rank));
+            printPiece(piece);
+        }
+        System.out.println();
+    }
+
+    private static void printPiece(Piece piece) {
+        if (piece != null) {
+            System.out.print(piece.getName());
+            return;
+        }
+        System.out.print(".");
+    }
+}
