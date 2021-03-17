26c5358bf32b5ffdc1e4c2139d6e043ff92ee69a
sori <bosl95@naver.com>
2021-03-17 18:08:54 +0900

    refactor : Queen 테스트 코드 추가 - Queen이 이동하려는 경로에 다른 말이 있으면, 실패를 반환한다.

--- src/test/java/domain/chess/piece/QueenTest.java
+++ src/test/java/domain/chess/piece/QueenTest.java
@@ -41,4 +41,18 @@ class QueenTest {
         assertThat(queen.canMove(board.getBoard(), Position.Of(3, 6))).isFalse();
         assertThat(queen.canMove(board.getBoard(), Position.Of(5, 6))).isFalse();
     }
+
+    @DisplayName("Queen이 이동하려는 경로에 다른 말이 있으면, 실패를 반환한다.")
+    @Test
+    void cant_move_queen_if_piece_exist() {
+        Board board = new Board(PieceFactory.createPieces());
+        Queen queen = Queen.Of("Q", Position.Of(4, 4), true);
+        Pawn pawn = Pawn.Of("P", Position.Of(3, 4), true);
+        board.put(pawn, Position.Of(3, 4));
+        assertThat(queen.canMove(board.getBoard(), Position.Of(2, 4))).isFalse();
+
+        Pawn pawn2 = Pawn.Of("P", Position.Of(5, 5), true);
+        board.put(pawn2, Position.Of(5, 5));
+        assertThat(queen.canMove(board.getBoard(), Position.Of(6, 6))).isFalse();
+    }
 }
