c077db89a18ce47271cfda8851683499d6b07c9f
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-17 18:37:22 +0900

    feat(Bishop): 비숍 움직임 체크 구현

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -3,6 +3,8 @@ package chess.domain.piece;
 import chess.domain.ChessBoard;
 import chess.domain.Position;
 
+import java.util.List;
+
 public class Bishop extends Piece {
     public Bishop(final String team) {
         super(team);
@@ -10,6 +12,15 @@ public class Bishop extends Piece {
 
     @Override
     public boolean isMovable(Position current, Position destination, ChessBoard chessBoard) {
-        return false;
+        if (!checkPositionRule(current, destination)) {
+            return false;
+        }
+        final List<Position> diagonalPath = current.generateDiagonalPath(destination);
+        return checkEmptyPath(diagonalPath, chessBoard);
+    }
+
+    @Override
+    public boolean checkPositionRule(Position current, Position destination) {
+        return checkDiagonalRule(current, destination);
     }
 }
---
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -0,0 +1,35 @@
+package chess.domain.piece;
+
+import chess.domain.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+public class BishopTest {
+    @Test
+    @DisplayName("비숍 정상 생성되는지 확인한다")
+    void init() {
+        assertThatCode(() -> new Bishop("Black"))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("비숍이 규칙상 정상적으로 움직일 수 있는 경로가 주어지면, 참을 반환한다.")
+    void check_position_rule_valid_test() {
+        final Bishop bishop = new Bishop("Black");
+        final Position start = Position.of("c1");
+        final Position validDestination = Position.of("e3");
+        assertThat(bishop.checkPositionRule(start, validDestination)).isTrue();
+    }
+
+    @Test
+    @DisplayName("비숍이 규칙상 정상적으로 움직일 수 없는 경로가 주어지면, 거짓을 반환한다.")
+    void check_position_rule_invalid_test() {
+        final Bishop bishop = new Bishop("Black");
+        final Position start = Position.of("c1");
+        final Position invalidDestination = Position.of("d3");
+        assertThat(bishop.checkPositionRule(start, invalidDestination)).isFalse();
+    }
+}
