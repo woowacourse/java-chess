f9ecd415f91673a6467db40d934b1334202aff04
hoony <younghoon.kwon91@gmail.com>
2021-03-17 17:49:14 +0900

    feat(Grid): 그리드 구현

--- src/main/java/chess/domain/grid/Grid.java
+++ src/main/java/chess/domain/grid/Grid.java
@@ -1,9 +1,20 @@
 package chess.domain.grid;
 
+import chess.domain.piece.Empty;
+import chess.domain.piece.Piece;
+import chess.domain.position.Position;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Grid {
+    private static final int FIRST_ROW = 1;
+    private static final int SECOND_ROW = 2;
+    private static final int THIRD_ROW = 3;
+    private static final int SIXTH_ROW = 6;
+    private static final int SEVENTH_ROW = 7;
+    private static final int EIGHTH_ROW = 8;
+    private static final int GAP_BETWEEN_INDEX_ACTUAL = 1;
     private static List<Line> lines;
 
     static {
@@ -11,13 +22,35 @@ public class Grid {
     }
 
     public static void initialize() {
-        List<Line> lines = new ArrayList<>();
-        lines.add(Line.createGeneralLine(1, false));
-        lines.add(Line.createPawnLine(2, false));
-        for (int i = 3; i <= 6 ; i++) {
+        lines = new ArrayList<>();
+        lines.add(Line.createGeneralLine(FIRST_ROW, false));
+        lines.add(Line.createPawnLine(SECOND_ROW, false));
+        for (int i = THIRD_ROW; i <= SIXTH_ROW; i++) {
             lines.add(Line.createEmptyLine(i));
         }
-        lines.add(Line.createPawnLine(7, true));
-        lines.add(Line.createGeneralLine(8, true));
+        lines.add(Line.createPawnLine(SEVENTH_ROW, true));
+        lines.add(Line.createGeneralLine(EIGHTH_ROW, true));
+    }
+
+    public static boolean isOccupied(final Position position) {
+        boolean b = findPiece(position) instanceof Empty;
+        return b;
+    }
+
+//    public void move(Position source, Position target) {
+//        // 해당 위치의 체스말 가져오기
+//        lines.get(source).move(target);
+//
+//        // 움직이고자 하는 곳의 체스말 바꾸기
+////        lines[source].set(target);
+////        lines[taget].set(source);
+//    }
+
+    private static Piece findPiece(final Position position) {
+        char x = position.getX();
+        char y = position.getY();
+        int yIndex = Character.getNumericValue(y) - GAP_BETWEEN_INDEX_ACTUAL;
+        Line line = lines.get(yIndex);
+        return line.findPiece(x);
     }
 }
