b9bdba778ea0b0ae5db2db871fcaca22abac91f1
yjksw <cairofaith@gmail.com>
2021-03-17 21:20:30 +0900

    feat(BishopMoveStrategy.java): 말 이동 전략 구현 - Bishop

---
+++ src/test/java/chess/domain/piece/strategy/BishopMoveStrategyTest.java
@@ -0,0 +1,82 @@
+package chess.domain.piece.strategy;
+
+import chess.domain.board.Board;
+import chess.domain.board.Position;
+import chess.domain.exceptions.InvalidMoveException;
+import chess.domain.piece.Piece;
+import chess.domain.piece.PieceColor;
+import chess.domain.piece.PieceKind;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BishopMoveStrategyTest {
+
+    Board board;
+    Position position;
+    Piece piece;
+    BishopMoveStrategy bishopMoveStrategy;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        position = Position.of('a', 3);
+        piece = new Piece(PieceKind.BISHOP, PieceColor.WHITE);
+        bishopMoveStrategy = new BishopMoveStrategy();
+        board.putPieceAtPosition(position, piece);
+    }
+
+    @DisplayName("Bishop 움직임 테스트 - 유효한 대각선 위치로 이동")
+    @Test
+    void queenValidMove_void() {
+        Position target = Position.of('c', 5);
+        bishopMoveStrategy.move(position, target, board);
+
+        Piece pieceOnTarget = board.checkPieceAtPosition(target);
+
+        assertEquals(pieceOnTarget, piece);
+    }
+
+    @DisplayName("Bishop 움직임 테스트 - 유효하지 않는 위치로 이동")
+    @Test
+    void queenInvalidMove_ExceptionThrown() {
+        Position target = Position.of('c', 3);
+
+        assertThatThrownBy(() -> bishopMoveStrategy.move(position, target, board))
+            .isInstanceOf(InvalidMoveException.class)
+            .hasMessageContaining(Piece.UNABLE_MOVE_TYPE_MESSAGE);
+    }
+
+    @DisplayName("같은 팀인 경우 Exception 발생")
+    @Test
+    void checkIsNotSameTeam_ExceptionThrown() {
+        Position target = Position.of('b', 2);
+
+        assertThatThrownBy(() -> bishopMoveStrategy.move(position, target, board))
+            .isInstanceOf(InvalidMoveException.class)
+            .hasMessageContaining(Piece.SAME_TEAM_MESSAGE);
+    }
+
+    @DisplayName("보드 밖 이동시 Exception 발생")
+    @Test
+    void checkUnableOutOfBoard_ExceptionThrown() {
+        Position target = Position.of('g', 9);
+
+        assertThatThrownBy(() -> bishopMoveStrategy.move(position, target, board))
+            .isInstanceOf(InvalidMoveException.class)
+            .hasMessageContaining(Piece.OUT_OF_BOUND_MESSAGE);
+    }
+
+    @DisplayName("경로상 CLEAR 상태가 아닐 시에 Exception 발생")
+    @Test
+    void checkUnableCROSSPIECE_ExceptionThrown() {
+        Position target = Position.of('f', 8);
+
+        assertThatThrownBy(() -> bishopMoveStrategy.move(position, target, board))
+            .isInstanceOf(InvalidMoveException.class)
+            .hasMessageContaining(Piece.UNABLE_CROSS_MESSAGE);
+    }
+}
