6b648fe91e59a6a0171ca21b16bc515979e3ae54
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-17 16:13:59 +0900

    feat(Position): 대각선 경로 구하는 로직 구현

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -1,18 +1,16 @@
 package chess.domain;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class Position {
     private static final Map<String, Position> positionMap = new HashMap<>();
 
     static {
-        String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8"};
         String[] alphabets = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8"};
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                positionMap.put(numbers[i] + alphabets[j], new Position(i, j));
+                positionMap.put(alphabets[i] + numbers[j], new Position(i, j));
             }
         }
     }
@@ -21,12 +19,111 @@ public class Position {
     private final int y;
 
     public Position(final int x, final int y) {
+        if ( x < 0 || x > 7 || y < 0 || y > 7 ) {
+            throw new IllegalArgumentException("판에서 벗어난 좌표입니다.");
+        }
         this.x = x;
         this.y = y;
     }
 
     public static Position of(final String input) {
         return positionMap.get(input);
+    }
+
+    public Position moveUp(int direction) {
+        return new Position(x, y + direction);
+    }
+
+    public Position moveDown() {
+        return new Position(x, y - 1);
+    }
+
+    public Position moveLeft() {
+        return new Position(x - 1, y);
+    }
+
+    public Position moveRight() {
+        return new Position(x + 1, y);
+    }
+
+    public Position moveLeftUp() {
+        return new Position(x - 1, y + 1);
+    }
+
+    public Position moveLeftDown() {
+        return new Position(x - 1, y - 1);
+    }
+
+    public Position moveRightUp() {
+        return new Position(x + 1, y + 1);
+    }
+
+    public Position moveRightDown() {
+        return new Position(x + 1, y - 1);
+    }
+
+    public List<Position> generateDiagonalPath(final Position destination) {
+        int xDiff = destination.x - this.x;
+        int yDiff = destination.y - this.y;
+        if (xDiff > 0 && yDiff > 0) {
+            return generateRightUpPath(destination);
+        } else if (xDiff > 0 && yDiff < 0) {
+            return generateRightDownPath(destination);
+        } else if (xDiff < 0 && yDiff > 0) {
+            return generateLeftUpPath(destination);
+        } else {
+            return generateLeftDownPath(destination);
+        }
+    }
+
+    private List<Position> generateRightUpPath(final Position destination) {
+        final List<Position> rightUpPath = new ArrayList<>();
+        Position path = this.moveRightUp();
+        while(!path.equals(destination)) {
+            rightUpPath.add(path);
+            path = path.moveRightUp();
+        }
+        return rightUpPath;
+    }
+
+    private List<Position> generateRightDownPath(final Position destination) {
+        final List<Position> rightDownPath = new ArrayList<>();
+        Position path = this.moveRightDown();
+        while(!path.equals(destination)) {
+            rightDownPath.add(path);
+            path = path.moveRightDown();
+        }
+        return rightDownPath;
+    }
+
+    private List<Position> generateLeftUpPath(final Position destination) {
+        final List<Position> leftUpPath = new ArrayList<>();
+        Position path = this.moveLeftUp();
+        while(!path.equals(destination)) {
+            leftUpPath.add(path);
+            path = path.moveLeftUp();
+        }
+        return leftUpPath;
+    }
+
+    private List<Position> generateLeftDownPath(final Position destination) {
+        final List<Position> leftDownPath = new ArrayList<>();
+        Position path = this.moveLeftDown();
+        while(!path.equals(destination)) {
+            leftDownPath.add(path);
+            path = path.moveLeftDown();
+        }
+        return leftDownPath;
+    }
+
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     @Override
--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -1,18 +1,35 @@
 package chess.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PositionTest {
     @ParameterizedTest
-    @CsvSource(value = {"1a,0,0", "1b,0,1", "2a,1,0", "1h,0,7", "2h,1,7", "8a,7,0", "8b,7,1", "8h,7,7"})
+    @CsvSource(value = {"a8,0,7", "a7,0,6", "a6,0,5", "c6,2,5", "h1,7,0"})
     @DisplayName("사용자가 체스 판에 있는 문자를 입력하면, 알맞은 Position 객체를 반환한다")
     void position_with_user_input(final String input, final int x, final int y) {
         Position userInputPosition = Position.of(input);
         Position boardPosition = new Position(x, y);
         assertThat(userInputPosition).isEqualTo(boardPosition);
+    }
+
+    @Test
+    @DisplayName("대각선으로 움직일 시작점과 종점을 입력하면, 그 사이 경로의 Position 객체를 반환한다.")
+    void position_diagonal_path_test() {
+        final Position start = new Position(0, 0);
+        final Position destination = new Position(3, 3);
+
+        List<Position> expectedPath = Arrays.asList(new Position(1, 1), new Position(2, 2));
+        List<Position> resultPath = start.generateDiagonalPath(destination);
+
+        assertEquals(expectedPath, resultPath);
     }
 }
