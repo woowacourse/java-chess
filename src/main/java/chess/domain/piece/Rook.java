55d8b6b326f55ad5b4d29dabf31ce08adab24877
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-17 18:37:49 +0900

    feat(Rook): 룩 움직임 체크 구현

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -3,6 +3,8 @@ package chess.domain.piece;
 import chess.domain.ChessBoard;
 import chess.domain.Position;
 
+import java.util.List;
+
 public class Rook extends Piece {
     public Rook(final String team) {
         super(team);
@@ -10,6 +12,15 @@ public class Rook extends Piece {
 
     @Override
     public boolean isMovable(Position current, Position destination, ChessBoard chessBoard) {
-        return false;
+        if (!checkPositionRule(current, destination)) {
+            return false;
+        }
+        final List<Position> straightPath = current.generateStraightPath(destination);
+        return checkEmptyPath(straightPath, chessBoard);
+    }
+
+    @Override
+    boolean checkPositionRule(Position current, Position destination) {
+        return checkStraightRule(current, destination);
     }
 }
---
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -0,0 +1,24 @@
+package chess.domain.piece;
+
+import chess.domain.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+public class RookTest {
+    @Test
+    @DisplayName("룩이 정상 생성되는지 확인한다")
+    void init() {
+        assertThatCode(() -> new Rook("Black")).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("룩이 규칙상 정상적으로 움직일 수 있는 경로가 주어지면, 참을 반환한다.")
+    void check_position_rule_valid_test() {
+        final Rook rook = new Rook("Black");
+        final Position start = Position.of("a1");
+        final Position validDestination = Position.of("a6");
+        assertThat(rook.checkPositionRule(start, validDestination)).isTrue();
+    }
+}
