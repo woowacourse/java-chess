ce3cf299ea6308f1da13f2e7a54d71934ac072a1
김주원 <kjw11077@naver.com>
2021-03-17 15:17:24 +0900

    test(Position): 생성자에서 행과 열 위치 변경

--- src/test/java/chess/domain/position/PositionTest.java
+++ src/test/java/chess/domain/position/PositionTest.java
@@ -1,22 +1,21 @@
 package chess.domain.position;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class PositionTest {
 
     @Test
     @DisplayName("Position 같은 좌표 비교")
     void equalsSucceed() {
-        Position position1 = new Position(Row.FIFTH, Column.A);
-        Position position2 = new Position(Row.FIFTH, Column.A);
+        Position position1 = new Position(Column.A, Row.FIFTH);
+        Position position2 = new Position(Column.A, Row.FIFTH);
 
         assertThat(position1).isEqualTo(position2);
     }
@@ -30,8 +29,8 @@ class PositionTest {
 
     private static Stream<Arguments> equalsFailTestcase() {
         return Stream.of(
-                Arguments.of(new Position(Row.FIRST, Column.A), new Position(Row.FIRST, Column.B)),
-                Arguments.of(new Position(Row.FIRST, Column.A), new Position(Row.SECOND, Column.A))
+                Arguments.of(new Position(Column.A, Row.FIRST), new Position(Column.B, Row.FIRST)),
+                Arguments.of(new Position(Column.A, Row.FIRST), new Position(Column.A, Row.SECOND))
         );
     }
 }
