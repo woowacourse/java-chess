245fb96005fb96f74deec58d8fb148862b07993a
Sehwan-Jang <whilter08@gmail.com>
2021-03-17 15:33:37 +0900

    test(KingTest) : 킹 이동 판단하는 기능 테스트 작성

---
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -0,0 +1,24 @@
+package chess.domain.piece;
+
+import chess.domain.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KingTest {
+    private final Piece white = new King(false);
+    private final Piece black = new King(true);
+    private final Position source = new Position("e", "4");
+
+    @ParameterizedTest
+    @DisplayName("이동 가능한지 판단하는 기능")
+    @ValueSource(strings = {"d,5", "e,5", "f,5", "d,4", "f,4", "d,3", "e,3", "f,3"})
+    void canMove(final String input) {
+        final String[] inputs = input.split(",");
+        assertThat(white.canMove(source, new Position(inputs[0], inputs[1]), new Queen(true))).isTrue();
+        assertThat(white.canMove(source, new Position(inputs[0], inputs[1]), new Blank())).isTrue();
+        assertThat(black.canMove(source, new Position(inputs[0], inputs[1]), new Queen(false))).isTrue();
+    }
+}
