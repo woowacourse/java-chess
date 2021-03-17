b7df3855e64787404ceb10c9492c77c19a1ed20e
Kimun Kim <kkm97351@gmail.com>
2021-03-17 17:17:31 +0900

    feat: 초기화된 보드 출력 기능 구현

--- src/main/java/chess/domain/position/Rank.java
+++ src/main/java/chess/domain/position/Rank.java
@@ -1,9 +1,6 @@
 package chess.domain.position;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -43,6 +40,16 @@ public enum Rank {
                 .skip(BLANK_LINE_ROW_PIVOT)
                 .limit(NUMBER_OF_BLANK_RANKS)
                 .collect(Collectors.toList());
+    }
+
+    public static List<Rank> asList() {
+        return Arrays.asList(values());
+    }
+
+    public static List<Rank> asListInReverseOrder() {
+        List<Rank> ranks = asList();
+        Collections.reverse(ranks);
+        return ranks;
     }
 
     public String getLetter() {
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,8 +1,28 @@
 package chess.view;
 
+import chess.domain.board.Board;
+import chess.domain.piece.Piece;
+import chess.domain.position.File;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class OutputView {
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printBoard(Board board) {
+        for (Rank rank: Rank.asListInReverseOrder()) {
+            String line = Arrays.stream(File.values())
+                    .map(file -> Position.of(file, rank))
+                    .map(board::findByPosition)
+                    .map(Piece::getNotation)
+                    .collect(Collectors.joining());
+            System.out.println(line);
+        }
     }
 }
