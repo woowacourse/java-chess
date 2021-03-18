fbf45240f76ccded7c2437eecf486628474f6aaa
Wannte <T.wannte@gmail.com>
2021-03-18 11:48:42 +0900

    feat(King): move, kill 구현

--- src/main/java/domain/piece/King.java
+++ src/main/java/domain/piece/King.java
@@ -2,6 +2,7 @@ package domain.piece;
 
 import domain.position.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Division {
@@ -11,10 +12,34 @@ public class King extends Division {
 
     @Override
     public void move(Position to, List<Piece> pieces) {
+        List<Position> possiblePositions = getKingPositions();
+        if (possiblePositions.contains(to)) {
+            position = to;
+        }
+    }
+
+    private List<Position> getKingPositions() {
+        List<Position> possiblePositions = new ArrayList<>();
+        add(possiblePositions, 0, 1);
+        add(possiblePositions, 0, -1);
+        add(possiblePositions, 1, 0);
+        add(possiblePositions, -1, 0);
+        add(possiblePositions, 1, 1);
+        add(possiblePositions, -1, 1);
+        add(possiblePositions, 1, -1);
+        add(possiblePositions, -1, -1);
+        return possiblePositions;
+    }
+
+    private void add(List<Position> possiblePositions, int columnValue, int rowValue) {
+        try {
+            possiblePositions.add(position.moveBy(columnValue, rowValue));
+        } catch (IllegalArgumentException ignored) {
+        }
     }
 
     @Override
     public void kill(Position to, List<Piece> pieces) {
-
+        move(to, pieces);
     }
 }
---
+++ src/test/java/domain/piece/KingTest.java
@@ -0,0 +1,44 @@
+package domain.piece;
+
+import domain.position.Position;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.*;
+
+class KingTest {
+    private static Stream<Arguments> destinations() {
+        return Stream.of(
+                Arguments.of(Position.from("b6")),
+                Arguments.of(Position.from("c6")),
+                Arguments.of(Position.from("d6")),
+                Arguments.of(Position.from("b5")),
+                Arguments.of(Position.from("d5")),
+                Arguments.of(Position.from("b4")),
+                Arguments.of(Position.from("c4")),
+                Arguments.of(Position.from("d4"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("destinations")
+    void move(Position position) {
+        King king = new King(Color.BLACK, Position.from("c5"));
+        king.move(position, Lists.emptyList());
+        assertTrue(king.hasPosition(position));
+    }
+
+    @Test
+    void cornerMove() {
+        King king = new King(Color.BLACK, Position.from("a5"));
+        assertThatCode(() -> king.move(Position.from("a4"), Lists.emptyList()))
+                .doesNotThrowAnyException();
+    }
+
+}
