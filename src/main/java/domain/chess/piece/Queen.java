f4bdf02c562fd9c23afdb1675b699f556ab19627
sori <bosl95@naver.com>
2021-03-18 21:08:28 +0900

    refactor : 코드 포맷팅

--- src/main/java/domain/chess/piece/Queen.java
+++ src/main/java/domain/chess/piece/Queen.java
@@ -41,7 +41,7 @@ public class Queen extends Piece {
             return false;
         }
 
-        int dx[] = {-1, 1, 0, 0}; // 상 하 좌 우
+        int dx[] = {-1, 1, 0, 0};
         int dy[] = {0, 0, -1, 1};
 
         int index = findStraightDirection(endPosition);
