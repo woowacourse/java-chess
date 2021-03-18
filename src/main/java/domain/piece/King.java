50f57fb31d745a79f4ce9d7fe04b640bb855442b
Wannte <T.wannte@gmail.com>
2021-03-18 17:11:03 +0900

    refactor(King): 킹 move 로직 수정

--- src/main/java/domain/piece/King.java
+++ src/main/java/domain/piece/King.java
@@ -2,7 +2,6 @@ package domain.piece;
 
 import domain.position.Position;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Division {
@@ -12,30 +11,14 @@ public class King extends Division {
 
     @Override
     public void move(Position to, List<Piece> pieces) {
-        List<Position> possiblePositions = getKingPositions();
-        if (possiblePositions.contains(to)) {
+        int diffRow = Math.abs(position.diffRow(to));
+        int diffColumn = Math.abs(position.diffColumn(to));
+
+        if ((diffRow | diffColumn) == 1) {
             position = to;
+            return;
         }
-    }
-
-    private List<Position> getKingPositions() {
-        List<Position> possiblePositions = new ArrayList<>();
-        add(possiblePositions, 0, 1);
-        add(possiblePositions, 0, -1);
-        add(possiblePositions, 1, 0);
-        add(possiblePositions, -1, 0);
-        add(possiblePositions, 1, 1);
-        add(possiblePositions, -1, 1);
-        add(possiblePositions, 1, -1);
-        add(possiblePositions, -1, -1);
-        return possiblePositions;
-    }
-
-    private void add(List<Position> possiblePositions, int columnValue, int rowValue) {
-        try {
-            possiblePositions.add(position.moveBy(columnValue, rowValue));
-        } catch (IllegalArgumentException ignored) {
-        }
+        throw new IllegalArgumentException();
     }
 
     @Override
