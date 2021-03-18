8f37d698c1206e1f82a39e15b44d8b2ea413c230
Wannte <T.wannte@gmail.com>
2021-03-18 11:20:56 +0900

    feat(Queen): move, kill 구현

--- src/main/java/domain/piece/Queen.java
+++ src/main/java/domain/piece/Queen.java
@@ -11,10 +11,32 @@ public class Queen extends Division{
 
     @Override
     public void move(Position to, List<Piece> pieces) {
+        if (position.isOrthogonal(to)) {
+            List<Position> positions = position.getBetween(to);
+            for (Piece piece : pieces) {
+                if (positions.contains(piece.getPosition())) {
+                    throw new IllegalArgumentException();
+                }
+            }
+            position = to;
+            return;
+        }
+
+        if (position.isDiagonal(to)) {
+            List<Position> positions = position.getBetween(to);
+            for (Piece piece : pieces) {
+                if (positions.contains(piece.getPosition())) {
+                    throw new IllegalArgumentException();
+                }
+            }
+            position = to;
+        }
+
+
     }
 
     @Override
     public void kill(Position to, List<Piece> pieces) {
-
+        move(to, pieces);
     }
 }
---
+++ src/test/java/domain/piece/QueenTest.java
@@ -0,0 +1,55 @@
+package domain.piece;
+
+import domain.position.Position;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueenTest {
+    private static Stream<Arguments> destinations() {
+        return Stream.of(
+                Arguments.of(Position.from("a5")),
+                Arguments.of(Position.from("h5")),
+                Arguments.of(Position.from("c1")),
+                Arguments.of(Position.from("c7")),
+                Arguments.of(Position.from("f8")),
+                Arguments.of(Position.from("a7")),
+                Arguments.of(Position.from("a3")),
+                Arguments.of(Position.from("e3"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("destinations")
+    void move(Position position) {
+        Queen queen = new Queen(Color.BLACK, Position.from("c5"));
+        queen.move(position, Lists.emptyList());
+        assertTrue(queen.hasPosition(position));
+    }
+
+    @ParameterizedTest
+    @MethodSource("destinations")
+    void interruptedMove(Position position) {
+        Queen queen = new Queen(Color.BLACK, Position.from("c5"));
+        List<Position> positions = Arrays.asList(Position.from("b5"), Position.from("e5"), Position.from("c2"), Position.from("c6"),
+                                                 Position.from("b6"), Position.from("b4"), Position.from("d4"), Position.from("d6"));
+
+        assertThatThrownBy(() -> queen.move(position, blackPieces(positions)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private List<Piece> blackPieces(List<Position> positions) {
+        return positions.stream()
+                        .map(position -> new Pawn(Color.BLACK, position))
+                        .collect(Collectors.toList());
+    }
+}
