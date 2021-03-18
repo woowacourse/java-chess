06eb3a8a2d852482417aa03fa7fc71b8bbdf78f8
Seed <hsik0225@gmail.com>
2021-03-18 18:24:55 +0900

    feat(pawn):전진 기능 구현

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -24,9 +24,6 @@ public class Pawn extends AbstractPiece {
         return SCORE;
     }
     
-    // TODO 대각선으로 이동할 대는 대각선에 상대방 기물이 있어야 함
-    // TODO 앞으로 전진할 때는 첫 이동일 때만 2칸 이동 가능
-    // TODO 앞에 상대방 기물이 있을 때 전진 불가
 //    @Override
 //    public Piece move(Position position) {
 //        final List<Direction> directions = findPawnDirection();
@@ -40,12 +37,21 @@ public class Pawn extends AbstractPiece {
     public Piece move(Position position, List<List<Piece>> board) {
         final List<Direction> directions = findPawnDirection();
         Direction direction = findDirection(position, directions, ABLE_LENGTH);
-        
+        if (isForward(direction) && !board.get(position.getX().getPoint()).get(position.getY().getPoint()).isSameColor(Color.BLANK)) {
+            throw new IllegalArgumentException("폰은 전진하는 위치에 기물이 있으면 안됩니다.");
+        }
+        if (!isForward(direction) && board.get(position.getX().getPoint()).get(position.getY().getPoint()).isSameColor(Color.BLANK)) {
+            throw new IllegalArgumentException("폰은 대각선으로 이동하기 위해서는 상대방의 기물이 있어야 합니다.");
+        }
         // TODO 장애물 체크
         if (isObstacleAtDirection(position, direction, board)) {
             throw new IllegalArgumentException("이동하는 경로 사이에 기물이 있습니다.");
         }
         return new Pawn(color, position);
+    }
+    
+    private boolean isForward(Direction direction) {
+        return direction == forward();
     }
     
     private List<Direction> findPawnDirection() {
@@ -54,5 +60,35 @@ public class Pawn extends AbstractPiece {
         }
         
         return Direction.whitePawnDirection();
+    }
+    
+    @Override
+    protected Direction findDirection(Position position, List<Direction> directions, int ableLength) {
+        return directions.stream()
+                         .filter(direction -> this.position.canMove(position, direction, ableLength))
+                         .findAny()
+                         .orElseGet(() -> {
+                             if (isFirst() && this.position.canMove(position, forward(),ableLength+1)) {
+                                 return forward();
+                             }
+                             throw new IllegalArgumentException(ERROR_CAN_NOT_MOVE);
+                         });
+    }
+    
+    private Direction forward() {
+        if (color.isBlack()) {
+            return Direction.SOUTH;
+        }
+        return Direction.NORTH;
+    }
+    
+    private boolean isFirst() {
+        if (position.getX().getPoint() == 1 && color.isWhite()) {
+            return true;
+        }
+        if (position.getX().getPoint() == 6 && color.isBlack()) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/chess/domain/piece/PawnTest.java
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -18,7 +18,7 @@ class PawnTest {
     @BeforeEach
     void setUp() {
         Position position = Position.of(1, 1);
-    
+        
         pawn = new Pawn(Color.WHITE, position);
         
         Board board = new Board();
@@ -28,7 +28,7 @@ class PawnTest {
     }
     
     @Test
-    @DisplayName("이동검사")
+    @DisplayName("1칸 전진 테스트")
     void move() {
         Position position = Position.of(2, 1);
         
@@ -51,5 +51,71 @@ class PawnTest {
     @DisplayName("점수 반환 테스트")
     void score() {
         assertThat(pawn.getScore()).isEqualTo(1);
+    }
+    
+    @Test
+    @DisplayName("2칸 전진 테스트")
+    void a() {
+        Position position = Position.of("b4");
+        
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(position, pieces);
+        
+        assertThatCode(callable).doesNotThrowAnyException();
+    }
+    
+    @Test
+    @DisplayName("1칸 대각선에 있는 상대방 기물 잡기 테스트")
+    void b() {
+        Position position = Position.of("b4");
+        
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(position, pieces);
+        
+        assertThatCode(callable).doesNotThrowAnyException();
+    }
+    
+    @Test
+    @DisplayName("전진 위치에 기물이 있을 경우 예외 발생")
+    void c() {
+        Position blackPawnPosition = Position.of("b3");
+        
+        pieces.get(2)
+              .set(1, new Pawn(Color.BLACK, blackPawnPosition));
+        
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(blackPawnPosition, pieces);
+        
+        assertThatIllegalArgumentException().isThrownBy(callable);
+    }
+    
+    @Test
+    @DisplayName("첫 이동이 아닌데 2칸을 전진할 경우 예외 발생")
+    void d() {
+        Position whitePawnPosition = Position.of("b3");
+        pieces.get(2)
+              .set(1, new Pawn(Color.WHITE, whitePawnPosition));
+        
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(Position.of("b5"), pieces);
+        
+        assertThatIllegalArgumentException().isThrownBy(callable);
+    }
+    
+    @Test
+    @DisplayName("1칸 대각선 이동시 타겟 위치가 블랭크일 경우 이동 불가")
+    void e() {
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(Position.of("c3"), pieces);
+        
+        assertThatIllegalArgumentException().isThrownBy(callable);
+    }
+    
+    @Test
+    @DisplayName("2칸 이동 시 경로 중간에 기물이 있을 경우 예외 발생")
+    void f() {
+        Position blackPawnPosition = Position.of("b3");
+        
+        pieces.get(2).set(1, new Pawn(Color.BLACK, blackPawnPosition));
+        
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(Position.of("b4"), pieces);
+        
+        assertThatIllegalArgumentException().isThrownBy(callable)
+                                            .withMessage("이동하는 경로 사이에 기물이 있습니다.");
     }
 }
