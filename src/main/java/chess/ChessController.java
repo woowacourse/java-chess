23dced9151052f06bdff3578ab99d1d0ec288dec
Seed <hsik0225@gmail.com>
2021-03-18 18:28:38 +0900

    feat(controller) : 컨트롤러 기능 구현

--- src/main/java/chess/ChessController.java
+++ src/main/java/chess/ChessController.java
@@ -8,27 +8,41 @@ import chess.view.OutputView;
 public class ChessController {
     public void run() {
         OutputView.printStart();
-        String command = InputView.askCommand();
-        
-        if (command.equals("start")) {
-            Board board = new Board();
-            board.init();
-            OutputView.printBoard(board);
-            
-            while (true) {
-                command = InputView.askCommand();
-                
-                if (command.equals("end")) {
-                    break;
-                }
-                String[] commands = command.split(" ");
-                if ("move".equals(commands[0])) {
-                    board.movePiece(commands[1], commands[2]);
-                }
+        Board board = new Board();
+        while (true) {
+            String command = InputView.askCommand();
+            // todo - 명령어 처리방식 변경
+            if (command.equals("start")) {
+                // todo 보드생성은 위로 빼기
+                board.init();
+                // todo - dto로 만들어서 뷰에 던지기
                 OutputView.printBoard(board);
-                
+                // todo - 보드의 상태에 따라서 반복문 설정하기
+                while (!board.isFinish()) {
+                    command = InputView.askCommand();
+                    // todo - 명령어 처리방식 변경
+                    if ("end".equals(command)) {
+                        break;
+                    }
+                    // todo 명령어 처리방식 변경
+                    String[] commands = command.split(" ");
+                    if ("move".equals(commands[0])) {
+                        board.movePiece(commands[1], commands[2]);
+                    }
+                    if ("status".equals(command)) {
+                        // todo - 결과 관련  dto 만들어서 던지기
+                        OutputView.printStatus(board.score(Color.BLACK), board.score(Color.WHITE), board.winner());
+                    }
+                    OutputView.printBoard(board);
+                }
             }
-            OutputView.printStatus(board.score(Color.BLACK), board.score(Color.WHITE), board.getWinner());
+            if ("status".equals(command)) {
+                // todo - 결과 관련  dto 만들어서 던지기
+                OutputView.printStatus(board.score(Color.BLACK), board.score(Color.WHITE), board.winner());
+            }
+            if ("exit".equals(command)) {
+                break;
+            }
         }
         
     }
