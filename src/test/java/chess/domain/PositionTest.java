0cb28287b2a68a3efce42c6a19de2b4fc1caeec7
Jinho <jh8579@gmail.com>
2021-03-18 11:18:11 +0900

    test : PositionTest 구현

--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -3,18 +3,42 @@ package chess.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PositionTest {
 
-    @DisplayName("좌표 방향 계산 테스트")
-    @Test
-    void calculateDirection() {
-        Position coordinateSource = Position.of("b2");
-        Position coordinateTarget = Position.of("c3");
-        Direction direction = coordinateSource.calculateDirection(coordinateTarget);
+    @DisplayName("좌표 방향 계산 테스트 - LinearDirection")
+    @ParameterizedTest
+    @CsvSource(value = {"b2, b5, NORTH", "f6, f2, SOUTH", "d4, a4, WEST", "b7, e7, EAST"})
+    void calculateLinearDirection(String source, String target, Direction direction) {
+        Position testSource = Position.of(source);
+        Position testTarget = Position.of(target);
+        Direction calculateDirection = testSource.calculateDirection(testTarget);
 
-        assertThat(direction).isEqualTo(Direction.NORTHEAST);
+        assertThat(calculateDirection).isEqualTo(direction);
     }
 
+    @DisplayName("좌표 방향 계산 테스트 - DiagonalDirection")
+    @ParameterizedTest
+    @CsvSource(value = {"b2, e5, NORTHEAST", "f2, b6, NORTHWEST", "b5, e2, SOUTHEAST", "h6, f4, SOUTHWEST"})
+    void calculateDiagonalDirection(String source, String target, Direction direction) {
+        Position testSource = Position.of(source);
+        Position testTarget = Position.of(target);
+        Direction calculateDirection = testSource.calculateDirection(testTarget);
+
+        assertThat(calculateDirection).isEqualTo(direction);
+    }
+
+    @DisplayName("좌표 방향 계산 테스트 - KnightDirection")
+    @ParameterizedTest
+    @CsvSource(value = {"b2, c4, NNE", "b2, a4, NNW", "c3, d1, SSE", "h6, g4, SSW", "c3, e4, EEN",
+    "c3, e2, EES", "c3, a4, WWN", "c3, a2, WWS"})
+    void calculateKnightDirection(String source, String target, Direction direction) {
+        Position testSource = Position.of(source);
+        Position testTarget = Position.of(target);
+        Direction calculateDirection = testSource.calculateDirection(testTarget);
+
+        assertThat(calculateDirection).isEqualTo(direction);
+    }
 }
