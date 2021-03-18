3c3861956056f529a4484c1ff2c40e02f68fdc28
Sehwan-Jang <whilter08@gmail.com>
2021-03-18 11:09:34 +0900

    test(QueenTest) : 퀸 이동판단 기능 테스트 작성

---
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -0,0 +1,24 @@
+package chess.domain.piece;
+
+import chess.domain.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class QueenTest {
+    private final Piece white = new Queen(false);
+    private final Piece black = new Queen(true);
+    private final Position source = new Position("e", "4");
+
+    @ParameterizedTest
+    @DisplayName("이동 가능한지 판단하는 기능")
+    @ValueSource(strings = {"d,4", "e,5", "f,4", "e,3", "d,5", "f,5", "d,3", "f,3"})
+    void canMove(final String input) {
+        final String[] inputs = input.split(",");
+        assertThat(white.canMove(source, new Position(inputs[0], inputs[1]), new Pawn(true))).isTrue();
+        assertThat(white.canMove(source, new Position(inputs[0], inputs[1]), new Blank())).isTrue();
+        assertThat(black.canMove(source, new Position(inputs[0], inputs[1]), new Pawn(false))).isTrue();
+    }
+}
