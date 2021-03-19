8c5e891adf0e54bc02e9e062cddcffbad617a670
Sehwan-Jang <whilter08@gmail.com>
2021-03-19 16:52:09 +0900

    test(TeamTest) : 상대팀 이름 확인 기능 테스트 작성

---
+++ src/test/java/chess/domain/piece/TeamTest.java
@@ -0,0 +1,17 @@
+package chess.domain.piece;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamTest {
+    @Test
+    @DisplayName("상대 팀 이름 확인 기능")
+    void oppositeName() {
+        assertThat(Team.BLACK.oppositeTeamName()).isEqualTo("white");
+        assertThat(Team.WHITE.oppositeTeamName()).isEqualTo("black");
+        assertThat(Team.NOTHING.oppositeTeamName()).isEqualTo("");
+    }
+}
