80a905f8804fbe9b1f67fe3c87213fcd5ef2dfe1
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-03-19 16:21:33 +0900

    feat: 퀸 이동 로직 구현

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -2,55 +2,73 @@ package chess.domain.piece;
 
 import chess.domain.ChessBoard;
 import chess.domain.Position;
-import chess.domain.State;
 import chess.domain.TeamColor;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Queen implements Piece {
+public class Queen extends PieceOnBoard {
 
-    private final TeamColor teamColor;
-
-    private String pieceType = "q";
+    private static final String NAME = "q";
+    private Position position;
 
     public Queen(TeamColor teamColor) {
-        this.teamColor = teamColor;
+        super(teamColor, NAME);
+    }
+
+
+    public Queen(TeamColor teamColor, Position position) {
+        super(teamColor, NAME);
+        this.position = position;
     }
 
 
     @Override
     public boolean isMoveAble(Position source, Position target, ChessBoard chessBoard) {
-        return false;
-    }
+        Set<Position> candidates = new HashSet<>();
 
-    @Override
-    public String getPieceName() {
-        if (teamColor == TeamColor.BLACK) {
-            return pieceType.toUpperCase();
+        Position position = source.moveUp();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveUp();
+
         }
-        return pieceType.toLowerCase();
-    }
+        position = source.moveDown();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveDown();
+        }
+        position = source.moveLeft();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveLeft();
+        }
+        position = source.moveRight();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveRight();
+        }
+        position = source.moveLeftDown();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveLeftDown();
+        }
 
-    @Override
-    public boolean isEnemyTeam(Piece comparePiece) {
-        return false;
-    }
+        position = source.moveLeftUp();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveLeftUp();
+        }
+        position = source.moveRightDown();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveRightDown();
+        }
+        position = source.moveRightUp();
+        while (movable(position, target, chessBoard)) {
+            candidates.add(position);
+            position = position.moveRightUp();
+        }
 
-    @Override
-    public TeamColor getColor() {
-        return teamColor;
-    }
-
-    @Override
-    public void dead() {
-
-    }
-
-    @Override
-    public State getState() {
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return teamColor.name() + " Queen\n";
+        return candidates.contains(target);
     }
 }
--- src/test/java/domain/piece/QueenTest.java
+++ src/test/java/domain/piece/QueenTest.java
@@ -2,15 +2,27 @@ package domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import chess.domain.ChessBoard;
+import chess.domain.Position;
 import chess.domain.TeamColor;
 import chess.domain.piece.Knight;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Queen;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class QueenTest {
+
+    private ChessBoard chessBoard;
+
+    @BeforeEach
+    void setUp() {
+        this.chessBoard = new ChessBoard();
+    }
 
     @Test
     @DisplayName("객체 생성")
@@ -30,5 +42,38 @@ public class QueenTest {
     void ValidWhiteQueenName() {
         Piece queen = new Queen(TeamColor.WHITE);
         assertThat(queen.getPieceName()).isEqualTo("q");
+    }
+
+    @Test
+    @DisplayName("퀸이 갈 수 있는 경우 1칸짜리")
+    void movable() {
+        Piece piece = new Queen(TeamColor.BLACK, Position.valueOf("b4"));
+
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("a4"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("c3"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("b3"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("b5"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("a3"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("c3"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("a5"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("c5"), chessBoard));
+    }
+
+    @Test
+    @DisplayName("퀸이 갈 수 있는 경우 2칸짜리")
+    void movable_2blocks() {
+        Piece piece = new Queen(TeamColor.BLACK, Position.valueOf("b4"));
+
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("b6"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("d6"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("d4"), chessBoard));
+    }
+
+    @Test
+    @DisplayName("퀸이 갈 수 있는 경우 2칸짜리 _ 실패")
+    void movable_2blocks_fail() {
+        Piece piece = new Queen(TeamColor.BLACK, Position.valueOf("b4"));
+
+        assertFalse(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("c6"), chessBoard));
     }
 }
