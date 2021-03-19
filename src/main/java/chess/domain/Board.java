54d40ff72be5e213af96865eca586741d2d1ceb5
TaewanKimmmm <ktww88@gmail.com>
2021-03-19 13:22:24 +0900

    refactor: 메서드 분리

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -18,72 +18,39 @@ public class Board {
 
     private void initialize() {
         for (int i = 0; i < BOARD_SIZE; i++) {
-            initializeRow(i);
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                board[i][j] = Pieces.findPiece(i, j);
+            }
         }
-    }
-
-    private void initializeRow(int i) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            board[i][j] = Pieces.findPiece(i, j);
-        }
-    }
-
-    public Piece[][] getBoard() {
-        return board;
     }
 
     public void movePiece(Point source, Point target, String currentColor) {
         Piece sourcePiece = selectPiece(source);
-        validateSourcePiece(sourcePiece);
-        System.out.println(sourcePiece.isSameColor(currentColor));
-        System.out.println(currentColor);
-
-        if(sourcePiece.isNotSameColor(currentColor)){
-            System.out.println(currentColor);
-
-            throw new IllegalArgumentException("기물 색이 일치하지 않습니다.");
-        }
+        validateSourcePieceNotEmpty(sourcePiece);
+        validateSourcePieceColor(currentColor, sourcePiece);
 
         Piece targetPiece = selectPiece(target);
+        validateTargetPieceColor(currentColor, targetPiece);
 
-        if(targetPiece.isSameColor(currentColor)) {
-            throw new IllegalArgumentException("도착지에 아군이 존재합니다.");
-        }
-
-        Point nowSource = source;
-        while(!nowSource.equals(target)) {
-            if(sourcePiece.isMovableRoute(targetPiece)) {
-                replacePieceWithEmpty(sourcePiece);
-                nowSource = sourcePiece.moveOneStep(target);
-                sourcePiece.movePoint(target);
-                board[target.getY()][target.getX()] = sourcePiece;
-            }
-        }
-    }
-
-    private void replacePieceWithEmpty(Piece sourcePiece) {
-        for(int i = 0; i < 8; i++) {
-            for (int j = 0; j< 8; j++) {
-                if(board[i][j].equals(sourcePiece)){
-                    board[i][j] = new Empty(".", null, Point.valueOf(i, j));
-                }
-            }
-        }
+        moveStepByStep(source, target, sourcePiece, targetPiece);
     }
 
     private Piece selectPiece(Point point) {
-        int x = point.getX();
-        int y = point.getY();
+        int x = point.getColumn();
+        int y = point.getRow();
         return this.board[y][x];
     }
 
-    private void validateSourcePiece(Piece piece) {
-        if (piece instanceof Empty) {
-            throw new IllegalArgumentException("기물이 존재하지 않습니다.");
+    private void moveStepByStep(Point source, Point target, Piece sourcePiece, Piece targetPiece) {
+        Point currentPoint = source;
+        while (!currentPoint.equals(target)) {
+            validatePossibleRoute(sourcePiece, targetPiece);
+            currentPoint = sourcePiece.moveOneStep(target);
+            validateNextPoint(currentPoint);
+            replacePiece(sourcePiece, new Empty(".", null, source));
+            sourcePiece.movePoint(target);
+            replacePiece(targetPiece, sourcePiece);
         }
-
-        // TODO: 색 검사
-//        if (this.board[x][y])
     }
 
     public boolean hasBothKings() {
@@ -93,5 +60,53 @@ public class Board {
                                 .filter(piece -> piece instanceof King)
                 )
                 .count() == 2;
+    }
+
+    private void validateSourcePieceNotEmpty(Piece piece) {
+        if (piece instanceof Empty) {
+            throw new IllegalArgumentException("기물이 존재하지 않습니다.");
+        }
+    }
+
+    private void validateSourcePieceColor(String currentColor, Piece sourcePiece) {
+        if (sourcePiece.isNotSameColor(currentColor)) {
+            throw new IllegalArgumentException("기물의 색이 일치하지 않아 움직일 수 없는 기물입니다.");
+        }
+    }
+
+    private void validateTargetPieceColor(String currentColor, Piece targetPiece) {
+        if (targetPiece.isSameColor(currentColor)) {
+            throw new IllegalArgumentException("도착지에 아군이 존재합니다.");
+        }
+    }
+
+    private void validatePossibleRoute(Piece sourcePiece, Piece targetPiece) {
+        if (!sourcePiece.isMovableRoute(targetPiece)) {
+            throw new IllegalArgumentException("이동할 수 없는 경로입니다.");
+        }
+    }
+
+    private void validateNextPoint(Point currentPoint) {
+        if (!(pickPiece(currentPoint) instanceof Empty)) {
+            throw new IllegalArgumentException("이동할 수 없습니다.");
+        }
+    }
+
+    private Piece pickPiece(Point point) {
+        return board[point.getRow()][point.getColumn()];
+    }
+
+    private void replacePiece(Piece currentPiece, Piece expectedPiece) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j].equals(currentPiece)) {
+                    board[i][j] = expectedPiece;
+                }
+            }
+        }
+    }
+
+    public Piece[][] getBoard() {
+        return board;
     }
 }
