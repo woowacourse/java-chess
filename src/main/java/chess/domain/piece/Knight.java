d893ebceccd2b8c17711627100876f47f5cec577
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-03-19 16:47:10 +0900

    feat: 나이트 이동 로직 구현

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -3,6 +3,8 @@ package chess.domain.piece;
 import chess.domain.ChessBoard;
 import chess.domain.Position;
 import chess.domain.TeamColor;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Knight extends PieceOnBoard {
 
@@ -21,7 +23,54 @@ public class Knight extends PieceOnBoard {
 
     @Override
     public boolean isMoveAble(Position source, Position target, ChessBoard chessBoard) {
-        return false;
+
+        Set<Position> candidates = new HashSet<>();
+
+        Position position = source.moveRightUp().moveUp();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+        position = source.moveRightUp().moveRight();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+
+        position = source.moveRightDown().moveRight();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+        position = source.moveRightDown().moveDown();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+
+        position = source.moveLeftDown().moveDown();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+        position = source.moveLeftDown().moveLeft();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+
+        position = source.moveLeftUp().moveLeft();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+        position = source.moveLeftUp().moveUp();
+        if (position != Position.ERROR && chessBoard.isBlank(position) ||
+            (position == target && isEnemyTeam(chessBoard.getPiece(position)))) {
+            candidates.add(position);
+        }
+
+        return candidates.contains(target);
     }
 
 }
--- src/test/java/domain/piece/KnightTest.java
+++ src/test/java/domain/piece/KnightTest.java
@@ -2,14 +2,26 @@ package domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import chess.domain.ChessBoard;
+import chess.domain.Position;
 import chess.domain.TeamColor;
 import chess.domain.piece.Knight;
 import chess.domain.piece.Piece;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class KnightTest {
+
+    private ChessBoard chessBoard;
+
+    @BeforeEach
+    void setUp() {
+        this.chessBoard = new ChessBoard();
+    }
 
     @Test
     @DisplayName("객체 생성")
@@ -29,5 +41,32 @@ public class KnightTest {
     void ValidWhiteKnightName() {
         Piece knight = new Knight(TeamColor.WHITE);
         assertThat(knight.getPieceName()).isEqualTo("n");
+    }
+
+    @Test
+    @DisplayName("이동이 가능한 경우 - 상대 말 잡기 포함")
+    void movable() {
+        Piece piece = new Knight(TeamColor.WHITE, Position.valueOf("d5"));
+
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("e7"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("f6"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("f4"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("e3"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("c3"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("b4"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("b6"), chessBoard));
+        assertTrue(piece.isMoveAble(Position.valueOf("d5"), Position.valueOf("c7"), chessBoard));
+    }
+
+    @Test
+    @DisplayName("이동 불가능한 경우 - 영역범위 초과 및 target에 같은팀 포함")
+    void fail_same_team() {
+        Piece piece = new Knight(TeamColor.BLACK, Position.valueOf("b4"));
+
+        assertFalse(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("d2"), chessBoard));
+        assertFalse(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("c3"), chessBoard));
+        assertFalse(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("b6"), chessBoard));
+        assertFalse(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("f6"), chessBoard));
+        assertFalse(piece.isMoveAble(Position.valueOf("b4"), Position.valueOf("e7"), chessBoard));
     }
 }
