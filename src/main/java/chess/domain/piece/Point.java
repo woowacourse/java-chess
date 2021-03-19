90006cc67e056b883659c13642f0922302381e63
멍토 <daum7766@naver.com>
2021-03-19 16:31:36 +0900

    refactor: 리팩토링
    캐싱적용
    유효성검사방식 수정

--- src/main/java/chess/domain/piece/Point.java
+++ src/main/java/chess/domain/piece/Point.java
@@ -3,34 +3,39 @@ package chess.domain.piece;
 import java.util.Objects;
 
 public class Point {
-    
+
     private static final int MIN_INDEX = 0;
-    private static final int MAX_INDEX = 7;
-    
-    private static final String ERROR_OUT_OF_RANGE = "인덱스는 0이상 7이하이어야 합니다.";
-    
+    private static final int MAX_INDEX = 8;
+    private static final Point[] POINTS = new Point[MAX_INDEX];
+
     private final int point;
-    
-    public Point(int point) {
+
+    static {
+        for (int i = 0; i < MAX_INDEX; i++) {
+            POINTS[i] = new Point(i);
+        }
+    }
+
+    private Point(int point) {
         this.point = point;
     }
-    
+
     public static Point from(int point) {
-        if (isOutOfBounds(point)) {
-            throw new IllegalArgumentException(ERROR_OUT_OF_RANGE);
+        validate(point);
+
+        return POINTS[point];
+    }
+
+    private static void validate(int position) {
+        if (position < MIN_INDEX || position >= MAX_INDEX) {
+            throw new IllegalArgumentException("인덱스는 0이상 7이하이어야 합니다.");
         }
-        
-        return new Point(point);
     }
-    
-    private static boolean isOutOfBounds(int position) {
-        return position < MIN_INDEX || position > MAX_INDEX;
-    }
-    
+
     public Point add(int distance) {
         return new Point(point + distance);
     }
-    
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -42,13 +47,13 @@ public class Point {
         Point point1 = (Point) o;
         return point == point1.point;
     }
-    
+
     @Override
     public int hashCode() {
         return Objects.hash(point);
     }
-    
-    public int getPoint() {
+
+    public int point() {
         return point;
     }
 }
