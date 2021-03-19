778f69565687b78bb4628707fe9068cc61e2e0d2
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-19 17:30:33 +0900

    refactor(ChessGame): 메서드를 역할에 따라 분리

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -12,32 +12,40 @@ public class ChessGame {
     public static final int BLACK_TEAM = 0;
     public static final int WHITE_TEAM = 1;
 
+    private final BlackTeam blackTeam;
+    private final WhiteTeam whiteTeam;
     private Team currentTurn;
-    private BlackTeam blackTeam;
-    private WhiteTeam whiteTeam;
     private boolean isEnd = false;
 
-    public ChessGame(BlackTeam blackTeam, WhiteTeam whiteTeam) {
+    public ChessGame(final BlackTeam blackTeam, final WhiteTeam whiteTeam) {
         this.blackTeam = blackTeam;
         this.whiteTeam = whiteTeam;
         this.currentTurn = this.whiteTeam;
     }
 
-    public void move(Position current, Position destination) {
+    public void move(final Position current, final Position destination) {
         final Piece chosenPiece = currentTurn.choosePiece(current);
-        if (currentTurn.havePiece(destination) || !chosenPiece.isMovable(current, destination, generateChessBoard())) {
-            throw new IllegalArgumentException("움직일 수 없는 경로입니다.");
-        }
+        validateMovable(current, destination, chosenPiece);
 
         Team enemy = getEnemy();
         if (enemy.havePiece(destination)) {
-            Piece piece = enemy.killPiece(destination);
-            currentTurn.catchPiece(piece);
-            checkMate(piece);
+            killEnemyPiece(destination, enemy);
         }
 
         currentTurn.move(current, destination);
         chosenPiece.isMoved();
+    }
+
+    private void validateMovable(final Position current, final Position destination, final Piece chosenPiece) {
+        if (currentTurn.havePiece(destination) || !chosenPiece.isMovable(current, destination, generateChessBoard())) {
+            throw new IllegalArgumentException("움직일 수 없는 경로입니다.");
+        }
+    }
+
+    private void killEnemyPiece(Position destination, Team enemy) {
+        Piece piece = enemy.killPiece(destination);
+        currentTurn.catchPiece(piece);
+        checkMate(piece);
     }
 
     private void checkMate(Piece piece) {
@@ -82,6 +90,7 @@ public class ChessGame {
 
         return 0;
     }
+
     public BlackTeam getBlackTeam() {
         return blackTeam;
     }
