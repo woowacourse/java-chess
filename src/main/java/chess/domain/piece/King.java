f358bb85a26fed1aaaac6297c32d8b15012d4bbe
TaewanKimmmm <ktww88@gmail.com>
2021-03-19 17:35:43 +0900

    feat: King 모든 기능 구현

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -2,6 +2,9 @@ package chess.domain.piece;
 
 import chess.domain.Point;
 
+import static chess.domain.ChessGame.BLACK;
+import static chess.domain.ChessGame.WHITE;
+
 public class King extends Piece {
     public King(String name, String color, Point point) {
         super(name, color, point);
@@ -9,11 +12,17 @@ public class King extends Piece {
 
     @Override
     public Direction direction(Piece target) {
-        return null;
+        Direction direction = Direction.findDirection(this.point, target.point);
+        int distance = this.point.calculateDistance(target.point);
+
+        if (distance == 1 || distance == 2) {
+            return direction;
+        }
+        throw new IllegalArgumentException(Piece.IMPOSSIBLE_ROUTE_ERROR_MESSAGE);
     }
 
     @Override
     public Point moveOneStep(Point target, Direction direction) {
-        return null;
+        return target;
     }
 }
--- src/test/java/chess/piece/KingTest.java
+++ src/test/java/chess/piece/KingTest.java
@@ -1,12 +1,14 @@
 package chess.piece;
 
 import chess.domain.Point;
-import chess.domain.piece.King;
-import chess.domain.piece.Pieces;
+import chess.domain.piece.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static chess.domain.ChessGame.BLACK;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KingTest {
     @DisplayName("King 생성")
@@ -16,5 +18,42 @@ public class KingTest {
         assertThat(Pieces.findPiece(0, 4)).isEqualTo(king1);
         King king2 = new King("k", "WHITE", Point.valueOf(7, 4));
         assertThat(Pieces.findPiece(7, 4)).isEqualTo(king2);
+    }
+
+    @DisplayName("King의 가능한 거리 확인")
+    @Test
+    void checkKingPossibleMove() {
+        King king = new King("K", BLACK, Point.valueOf(4, 4));
+        Empty empty = new Empty(".", null, Point.valueOf(5, 4));
+        Empty empty2 = new Empty(".", null, Point.valueOf(4, 5));
+        Empty empty3 = new Empty(".", null, Point.valueOf(3, 5));
+        Empty empty4 = new Empty(".", null, Point.valueOf(5, 3));
+
+        assertEquals(Direction.SOUTH, king.direction(empty));
+        assertEquals(Direction.EAST, king.direction(empty2));
+        assertEquals(Direction.NORTH_EAST, king.direction(empty3));
+        assertEquals(Direction.SOUTH_WEST, king.direction(empty4));
+    }
+
+    @DisplayName("King의 불가능한 거리 확인")
+    @Test
+    void checkKingImpossibleMove() {
+        King king = new King("K", BLACK, Point.valueOf(4, 4));
+        Empty empty = new Empty(".", null, Point.valueOf(2, 2));
+        Empty empty2 = new Empty(".", null, Point.valueOf(2, 6));
+        Empty empty3 = new Empty(".", null, Point.valueOf(4, 6));
+        Empty empty4 = new Empty(".", null, Point.valueOf(2, 4));
+
+        assertThatThrownBy(() -> king.direction(empty))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> king.direction(empty2))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> king.direction(empty3))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> king.direction(empty4))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
