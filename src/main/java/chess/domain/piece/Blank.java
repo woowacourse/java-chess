2328dee87a2978f1d7901efda52e1cb1b491e732
Kimun Kim <kkm97351@gmail.com>
2021-03-19 16:35:04 +0900

    refactor: CannotMoveStrategy 삭제

--- src/main/java/chess/domain/piece/Blank.java
+++ src/main/java/chess/domain/piece/Blank.java
@@ -1,12 +1,12 @@
 package chess.domain.piece;
 
-import chess.domain.piece.strategy.CannotMoveStrategy;
-
 public class Blank extends Piece {
     private static final String BLANK_NOTATION = ".";
 
     public Blank() {
-        super(BLANK_NOTATION, new CannotMoveStrategy());
+        super(BLANK_NOTATION, moveOrder -> {
+            throw new IllegalArgumentException("해당 칸에는 기물이 없습니다.");
+        });
     }
 
     @Override
