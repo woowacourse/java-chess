67e80a6c4dcd849bda77056ef3f68588b0e55002
hoony <younghoon.kwon91@gmail.com>
2021-03-19 18:53:15 +0900

    refactor: 상태패턴 리팩터링

--- src/main/java/chess/service/state/BlackTurn.java
+++ src/main/java/chess/service/state/BlackTurn.java
@@ -1,36 +1,25 @@
 package chess.service.state;
 
-import chess.domain.piece.King;
 import chess.domain.piece.Piece;
-import chess.domain.position.Position;
 import chess.service.ChessService;
-import chess.view.InputView;
 import chess.view.OutputView;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class BlackTurn extends Playing {
     @Override
-    public GameState run(ChessService chessService) {
-        GameState gameState = move(chessService);
-        OutputView.printGridStatus(chessService.grid());
-        return gameState;
-    }
-
-    private GameState move(ChessService chessService) {
-        String command = InputView.command();
-        List<String> moveInput = Arrays.asList(command.split(" "));
-
-        Piece sourcePiece = chessService.piece(new Position(moveInput.get(1).charAt(0), moveInput.get(1).charAt(1)));
-        Piece targetPiece = chessService.piece(new Position(moveInput.get(2).charAt(0), moveInput.get(2).charAt(1)));
-
+    GameState move(ChessService chessService, Piece sourcePiece, Piece targetPiece) {
+        validateIfBlack(sourcePiece);
         chessService.move(sourcePiece, targetPiece);
-        chessService.changeTurn();
-        if (targetPiece instanceof King) {
-            OutputView.printWinner(!targetPiece.isBlack());
+
+        if (catchedKing(targetPiece)) {
+            OutputView.printWinner(targetPiece.isBlack());
             return new End();
         }
         return new WhiteTurn();
+    }
+
+    void validateIfBlack(Piece sourcePiece){
+        if (!sourcePiece.isBlack()) {
+            throw new IllegalArgumentException("자신의 말만 옮길 수 있습니다.");
+        }
     }
 }
--- src/main/java/chess/service/state/WhiteTurn.java
+++ src/main/java/chess/service/state/WhiteTurn.java
@@ -1,37 +1,26 @@
 package chess.service.state;
 
-import chess.domain.piece.King;
 import chess.domain.piece.Piece;
-import chess.domain.position.Position;
 import chess.service.ChessService;
-import chess.view.InputView;
 import chess.view.OutputView;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class WhiteTurn extends Playing {
 
     @Override
-    public GameState run(ChessService chessService) {
-        GameState gameState = move(chessService);
-        OutputView.printGridStatus(chessService.grid());
-        return gameState;
-    }
-
-    private GameState move(ChessService chessService) {
-        String command = InputView.command();
-        List<String> moveInput = Arrays.asList(command.split(" "));
-
-        Piece sourcePiece = chessService.piece(new Position(moveInput.get(1).charAt(0), moveInput.get(1).charAt(1)));
-        Piece targetPiece = chessService.piece(new Position(moveInput.get(2).charAt(0), moveInput.get(2).charAt(1)));
-
+    GameState move(ChessService chessService, Piece sourcePiece, Piece targetPiece) {
+        validateIfWhite(sourcePiece);
         chessService.move(sourcePiece, targetPiece);
-        chessService.changeTurn();
-        if (targetPiece instanceof King) {
+
+        if (catchedKing(targetPiece)) {
             OutputView.printWinner(!targetPiece.isBlack());
             return new End();
         }
         return new BlackTurn();
+    }
+
+    void validateIfWhite(Piece sourcePiece){
+        if (sourcePiece.isBlack()) {
+            throw new IllegalArgumentException("자신의 말만 옮길 수 있습니다.");
+        }
     }
 }
