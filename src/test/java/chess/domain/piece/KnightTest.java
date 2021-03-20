a07ab82b4f1e6e3413cc83d4ee970dfdd2664bf0
pkeugine <pkeugine@gmail.com>
2021-03-20 13:19:43 +0900

    maintain: 테스트 이름 구체적으로 작성

--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -2,6 +2,7 @@ package chess.domain.piece;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import chess.domain.Board;
 import chess.domain.BoardFactory;
 import chess.domain.position.Position;
 import java.util.Arrays;
@@ -12,7 +13,7 @@ import org.junit.jupiter.api.Test;
 class KnightTest {
 
     // todo : 여기 테스트들은 path에 있어도 될 것 같고?
-    @DisplayName("흰나이트-e4 흰피스-없음 검은피스-없음")
+    @DisplayName("이동가능한 전체 위치를 구한다. 상황 : 흰나이트-e4 흰피스-없음 검은피스-없음")
     @Test
     void generatePath() {
         Position current = Position.ofName("e4");
@@ -22,13 +23,18 @@ class KnightTest {
         assertThat(paths.pathsToPosition()).isEqualTo(knightE4WithoutObstacles());
     }
 
-    @DisplayName("흰나이트-e4 ")
+    @DisplayName("장애물을 고려하여 위치를 구한다. 상황 : 흰나이트-e4 흰피스-d2,f2 검은피스-d6,f6")
     @Test
     void generateObstacleConsideredPath() {
         Position current = Position.ofName("e4");
         Piece bishop = new Bishop(PieceColor.WHITE);
         Paths paths = new Paths(bishop.findAllPath(current));
-        assertThat(paths.removeObstacles(BoardFactory.initializeBoard()).positions()).isEqualTo(
+        Board board = BoardFactory.initializeBoard();
+        Piece firstBlackPiece = board.findPieceBy(Position.ofName("d7"));
+        Piece secondBlackPiece = board.findPieceBy(Position.ofName("f7"));
+        board.move(firstBlackPiece, Position.ofName("d6"));
+        board.move(secondBlackPiece, Position.ofName("f6"));
+        assertThat(paths.removeObstacles(board).positions()).isEqualTo(
                 knightE4WithObstacles());
 
     }
