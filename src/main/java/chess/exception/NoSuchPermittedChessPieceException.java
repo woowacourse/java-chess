3f936bcef6b0e865049160b4770dcfd837595c0f
rok93 <goodboy302@naver.com>
2021-03-21 02:34:05 +0900

    style: 허용된 체스 말을 찾을 수 없을 경우 발생하는 오류 출력 메시지 정정

--- src/main/java/chess/exception/NoSuchPermittedChessPieceException.java
+++ src/main/java/chess/exception/NoSuchPermittedChessPieceException.java
@@ -1,7 +1,7 @@
 package chess.exception;
 
 public class NoSuchPermittedChessPieceException extends RuntimeException {
-    private static final String MESSAGE = "허용된 체스 말 을 찾을 수 없습니다.";
+    private static final String MESSAGE = "허용된 체스 말을 찾을 수 없습니다.";
 
     public NoSuchPermittedChessPieceException() {
         super(MESSAGE);
