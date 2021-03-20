b4364943dfc270db2aa6e615bde698dff28f4a5f
멍토 <daum7766@naver.com>
2021-03-20 23:42:33 +0900

    refactor: Position 인덴트 나누기

--- src/main/java/chess/domain/piece/Position.java
+++ src/main/java/chess/domain/piece/Position.java
@@ -8,19 +8,19 @@ public final class Position {
     private static final int DEFAULT_MOVE_LENGTH = 1;
     private static final int POSITION_MAX_SIZE = 8;
     private static final Position[] positions = new Position[POSITION_MAX_SIZE * POSITION_MAX_SIZE];
+    private static final int INDEX_X = 1;
+    private static final int INDEX_Y = 0;
+    private static final char CORRECTION_X = '1';
+    private static final char CORRECTION_Y = 'a';
+
+    static {
+        IntStream.range(0, POSITION_MAX_SIZE)
+            .forEach(x -> IntStream.range(0, POSITION_MAX_SIZE)
+                .forEach(y -> positions[asIndex(x, y)] = new Position(x, y)));
+    }
 
     private final Point x;
     private final Point y;
-
-    static {
-        for (int x = 0; x < POSITION_MAX_SIZE; x++) {
-            for (int y = 0; y < POSITION_MAX_SIZE; y++) {
-                Point pointX = Point.from(x);
-                Point pointY = Point.from(y);
-                positions[asIndex(pointX, pointY)] = new Position(x, y);
-            }
-        }
-    }
 
     private Position(int x, int y) {
         this(Point.from(x), Point.from(y));
@@ -31,31 +31,32 @@ public final class Position {
         this.y = y;
     }
 
-    public static Position of(String value) {
-        int x = value.charAt(1) - '1';
-        int y = value.charAt(0) - 'a';
-        return of(x, y);
-    }
-
     public static Position of(Point x, Point y) {
         return of(x.point(), y.point());
     }
 
-    public static Position of(int x, int y) {
-        return positions[asIndex(Point.from(x), Point.from(y))];
+    public static Position of(String value) {
+        int x = value.charAt(INDEX_X) - CORRECTION_X;
+        int y = value.charAt(INDEX_Y) - CORRECTION_Y;
+        return of(x, y);
     }
 
-    private static int asIndex(Point x, Point y) {
-        return x.point() * POSITION_MAX_SIZE + y.point();
+    public static Position of(int x, int y) {
+        return positions[asIndex(x, y)];
+    }
+
+    private static int asIndex(int x, int y) {
+        return x * POSITION_MAX_SIZE + y;
     }
 
     public boolean canMove(Position position, Direction direction, int ableLength) {
         return IntStream.rangeClosed(DEFAULT_MOVE_LENGTH, ableLength)
-            .filter(distance -> isAdd(direction.getXDegree() * distance, direction.getYDegree() * distance))
+            .filter(distance -> isAdd(direction.getXDegree() * distance,
+                direction.getYDegree() * distance))
             .mapToObj(distance -> {
-                    Point dx = x.add(direction.getXDegree() * distance);
-                    Point dy = y.add(direction.getYDegree() * distance);
-                    return new Position(dx, dy);
+                Point dx = x.add(direction.getXDegree() * distance);
+                Point dy = y.add(direction.getYDegree() * distance);
+                return new Position(dx, dy);
             })
             .anyMatch(position::equals);
     }
@@ -81,15 +82,15 @@ public final class Position {
         return Objects.hash(x, y);
     }
 
-    public Point getX() {
-        return x;
-    }
-
     public Position add(int x, int y) {
         return Position.of(this.x.add(x), this.y.add(y));
     }
 
     public boolean isSameColumn(Point point) {
         return y.equals(point);
+    }
+
+    public boolean isSameRow(Point point) {
+        return x.equals(point);
     }
 }
