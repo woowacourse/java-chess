df4e11e4cfc028747bf32f3df7b14ad29a3f0ed8
bomin <bomin_93@naver.com>
2021-03-20 21:55:40 +0900

    refactor(messagePositionConverter): 하드 코딩 상수 처리

--- src/main/java/chess/util/MessagePositionConverter.java
+++ src/main/java/chess/util/MessagePositionConverter.java
@@ -4,20 +4,23 @@ import chess.domain.Position;
 
 public final class MessagePositionConverter {
 
+    private static final String MESSAGE_SPLIT_REGEX = " ";
+    private static final int CURRENT_POSITION_INDEX = 1;
+    private static final int TARGET_POSITION_INDEX = 2;
+
     private final String[] positions;
     private final StringPositionConverter stringPositionConverter;
 
     public MessagePositionConverter(String message) {
-        positions = message.split(" ");
+        positions = message.split(MESSAGE_SPLIT_REGEX);
         stringPositionConverter = new StringPositionConverter();
     }
 
     public Position currentPosition() {
-        return stringPositionConverter.convert(positions[1]);
+        return stringPositionConverter.convert(positions[CURRENT_POSITION_INDEX]);
     }
 
     public Position targetPosition() {
-        return stringPositionConverter.convert(positions[2]);
-
+        return stringPositionConverter.convert(positions[TARGET_POSITION_INDEX]);
     }
 }
