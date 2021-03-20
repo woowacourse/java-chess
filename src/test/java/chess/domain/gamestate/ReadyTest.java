c572e06b85b2ef1b1425b943ce6043bc68b7b900
김주원 <kjw11077@naver.com>
2021-03-20 21:43:57 +0900

    test(State): 단위 테스트 추가

---
+++ src/test/java/chess/domain/gamestate/ReadyTest.java
@@ -0,0 +1,29 @@
+package chess.domain.gamestate;
+
+import chess.domain.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReadyTest {
+    private State ready;
+
+    @BeforeEach
+    void setUp() {
+        ready = new Ready(Board.getGamingBoard());
+    }
+
+    @Test
+    @DisplayName("start 메서드 테스트")
+    void startTest() {
+        assertThat(ready.start()).isInstanceOf(Running.class);
+    }
+
+    @Test
+    @DisplayName("finished 메서드 테스트")
+    void finishedTest() {
+        assertThat(ready.finished()).isInstanceOf(Finished.class);
+    }
+}
---
+++ src/test/java/chess/domain/gamestate/RunningTest.java
@@ -0,0 +1,38 @@
+package chess.domain.gamestate;
+
+import chess.domain.Board;
+import chess.domain.BoardTestInitializer;
+import chess.domain.Side;
+import chess.domain.position.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RunningTest {
+    private State running;
+
+    @BeforeEach
+    void setUp() {
+        running = new Running(new Board(BoardTestInitializer.init()), Side.WHITE);
+    }
+
+    @Test
+    @DisplayName("finished 메서드 테스트")
+    void finished() {
+        assertThat(running.finished()).isInstanceOf(Finished.class);
+    }
+
+    @Test
+    @DisplayName("기물을 움직여 왕을 잡을 경우 GameSet 상태로 변화")
+    void moveGameSet() {
+        assertThat(running.move(Position.from("d5"), Position.from("f6"))).isInstanceOf(GameSet.class);
+    }
+
+    @Test
+    @DisplayName("기물을 움직여 Running 상태로 변화")
+    void moveRunning() {
+        assertThat(running.move(Position.from("h2"), Position.from("h3"))).isInstanceOf(Running.class);
+    }
+}
