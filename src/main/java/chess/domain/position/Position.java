c055ea67df4d0d2908bd4ba664eb3140fa62acb0
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-20 21:46:15 +0900

    test(PositionTest): Position에 대한 테스트 추가

--- src/main/java/chess/domain/position/Position.java
+++ src/main/java/chess/domain/position/Position.java
@@ -29,7 +29,7 @@ public final class Position {
     }
 
     public final boolean isInValidRange() {
-        return x < MIN_X_RANGE || x > MAX_X_RANGE || y < MIN_Y_RANGE || y > MAX_Y_RANGE;
+        return x >= MIN_X_RANGE && x <= MAX_X_RANGE && y >= MIN_Y_RANGE && y <= MAX_Y_RANGE;
     }
 
     public final Position next(final int xDegree, final int yDegree) {
