1475b95a322c557b47695c62a46be0a4ad53ef5b
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-21 00:59:08 +0900

    refactor(Piece): Stream 사용하여 경로에 장애물이 없는지 판별

--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -8,9 +8,6 @@ import java.util.Map;
 public abstract class Piece {
     protected boolean isMoved = false;
 
-    public Piece() {
-    }
-
     public abstract boolean isMovable(final Position current, final Position destination, final Map<Position, Piece> chessBoard);
 
     public abstract boolean checkPositionRule(final Position current, final Position destination);
@@ -20,12 +17,8 @@ public abstract class Piece {
     public abstract boolean isPawn();
 
     public final boolean checkEmptyPath(final List<Position> path, final Map<Position, Piece> chessBoard) {
-        for (Position position : path) {
-            if (chessBoard.containsKey(position)) {
-                return false;
-            }
-        }
-        return true;
+        return path.stream()
+                .noneMatch(chessBoard::containsKey);
     }
 
     public void isMoved() {
