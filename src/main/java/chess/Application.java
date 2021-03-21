76896771aaa41deb1dd3df60c4e732d9ab2559ed
Wannte <T.wannte@gmail.com>
2021-03-21 16:08:32 +0900

    refactor(Controller): 컨트롤러 로직 분리

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,25 +1,10 @@
 package chess;
 
-import chess.domain.Game;
-import chess.domain.position.Position;
-import chess.view.InputView;
-import chess.view.OutputView;
+import chess.controller.ChessController;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game();
-
-        OutputView.printStartMessage();
-        game.display();
-        while (game.isNotEnd()) {
-            String[] inputString = InputView.requestInput();
-            if ("move".equals(inputString[0])) {
-                String from = inputString[1];
-                String to = inputString[2];
-                game.move(Position.from(from), Position.from(to));
-            }
-            game.display();
-        }
-        OutputView.printScore(game.score());
+        ChessController chessController = new ChessController();
+        chessController.run();
     }
 }
---
+++ src/main/java/chess/controller/ChessController.java
@@ -0,0 +1,25 @@
+package chess.controller;
+
+import chess.domain.Game;
+import chess.domain.position.Position;
+import chess.view.InputView;
+import chess.view.OutputView;
+
+public class ChessController {
+    public void run() {
+        Game game = new Game();
+
+        OutputView.printStartMessage();
+        game.display();
+        while (game.isNotEnd()) {
+            String[] inputString = InputView.requestInput();
+            if ("move".equals(inputString[0])) {
+                String from = inputString[1];
+                String to = inputString[2];
+                game.move(Position.from(from), Position.from(to));
+            }
+            game.display();
+        }
+        OutputView.printScore(game.score());
+    }
+}
