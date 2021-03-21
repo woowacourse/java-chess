bbec6d337a603ad837afc8cad5f0800d0af9e32e
bosl95 <bosl95@naver.com>
2021-03-21 23:44:18 +0900

    refactor: IndexMachine 테스트 코드 추가

--- src/test/java/domain/IndexMachineTest.java
+++ src/test/java/domain/IndexMachineTest.java
@@ -2,10 +2,14 @@ package domain;
 
 import domain.piece.Position;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class IndexMachineTest {
     @DisplayName("a3를 (5, 0)로 변환 가능하다.")
@@ -15,5 +19,12 @@ class IndexMachineTest {
         Position position = IndexMachine.convertPosition(input);
         assertThat(position.getRow()).isEqualTo(x);
         assertThat(position.getColumn()).isEqualTo(y);
+    }
+
+    @DisplayName("잘못된 위치가 들어오는 경우 예외처리한다.")
+    @Test
+    void invalid_input() {
+        assertThatThrownBy(() -> IndexMachine.convertPosition("x2"))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
