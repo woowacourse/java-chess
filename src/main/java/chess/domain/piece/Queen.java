db571cf5acad93c1303ff347417c34c1a303da36
Seed <hsik0225@gmail.com>
2021-03-21 14:00:05 +0900

    refactor(queen):메소드 분리 및 테스트 코드 추가

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -1,16 +1,19 @@
 package chess.domain.piece;
 
-import java.util.List;
+import chess.game.Board;
 
 public class Queen extends AbstractPiece {
     
     private static final String SYMBOL = "q";
     
     private static final double SCORE = 9;
-    public static final int ABLE_LENGTH = 7;
     
-    public Queen(Color color, Position position) {
-        super(color, position);
+    private static final int MAX_MOVABLE_LENGTH = 7;
+    private static final DirectionGroup DIRECTION_GROUP = new DirectionGroup(Direction.everyDirection(),
+            MAX_MOVABLE_LENGTH);
+    
+    public Queen(Color color) {
+        super(color, DIRECTION_GROUP);
     }
     
     @Override
@@ -18,25 +21,19 @@ public class Queen extends AbstractPiece {
         return changeColorSymbol(SYMBOL);
     }
     
-//    @Override
-//    public Queen move(Position position) {
-//        final List<Direction> directions = Direction.everyDirection();
-//        if (!isMovable(position, directions, ABLE_LENGTH)) {
-//            throw new IllegalArgumentException("퀸이 이동할 수 없는 위치입니다.");
-//        }
-//        return new Queen(color, position);
-//    }
-    
     @Override
-    public Piece move(Position position, List<List<Piece>> board) {
-        final List<Direction> directions = Direction.everyDirection();
-        Direction direction = findDirection(position, directions, ABLE_LENGTH);
+    public Piece move(Position sourcePosition, Position targetPosition, Board board) {
+        Direction direction = DIRECTION_GROUP.findDirection(sourcePosition, targetPosition);
         
-        // TODO 장애물 체크
-        if (isObstacleAtDirection(position, direction, board)) {
+        if (isObstacleAtDirection(sourcePosition, targetPosition, direction, board)) {
             throw new IllegalArgumentException("이동하는 경로 사이에 기물이 있습니다.");
         }
-        return new Queen(color, position);
+        
+        if (!board.isBlank(targetPosition)) {
+            throw new IllegalArgumentException("타겟 위치에 이미 기물이 있습니다.");
+        }
+        
+        return new Queen(color);
     }
     
     @Override
--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -1,59 +1,136 @@
 package chess.domain.piece;
 
-import chess.board.Board;
-import chess.view.OutputView;
+import chess.game.Board;
+import chess.game.InitializedChess;
 import org.assertj.core.api.ThrowableAssert;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class QueenTest {
     
-    private Queen queen;
-    
-    private List<List<Piece>> pieces;
-    
-    @BeforeEach
-    void setUp() {
-        Position position = Position.of("d1");
-    
-        queen = new Queen(Color.BLACK, position);
-        
-        Board board = new Board();
-        board.init();
-        
-        pieces = board.getBoard();
-    
-        pieces.get(1).set(2, new Blank(Color.BLANK, Position.of("c2")));
-    }
+    private final Queen queen = new Queen(Color.WHITE);
+    private final Position sourcePosition = Position.of("d1");
+    private final Board board = InitializedChess.create()
+                                                .getBoard();
     
     @Test
-    @DisplayName("이동검사")
-    void move() {
-        Position position = Position.of("a4");
+    @DisplayName("대각선 1칸 이동 검사")
+    void moveDiagonalOneStep() {
         
-        ThrowableAssert.ThrowingCallable callable = () -> queen.move(position, pieces);
-    
+        // given
+        final Position targetPosition = Position.of("c2");
+        final Board newBoard = BoardUtils.put(board, targetPosition, new Blank());
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> queen.move(sourcePosition, targetPosition, newBoard);
+        
+        // then
         assertThatCode(callable).doesNotThrowAnyException();
     }
     
     @Test
-    @DisplayName("이동 에러 검사")
-    void validate() {
-        Position position = Position.of(7, 2);
+    @DisplayName("대각선 여러 칸 이동 검사")
+    void moveDiagonalMultiStep() {
         
-        ThrowableAssert.ThrowingCallable callable = () -> queen.move(position, pieces);
+        // given
+        final Board newBoard = BoardUtils.put(board, Position.of("c2"), new Blank());
+        final Position targetPosition = Position.of("a4");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> queen.move(sourcePosition, targetPosition, newBoard);
+        
+        // then
+        assertThatCode(callable).doesNotThrowAnyException();
+    }
     
+    
+    @Test
+    @DisplayName("직선 1칸 이동 검사")
+    void moveLinearOneStep() {
+        
+        // given
+        final Position targetPosition = Position.of("d2");
+        final Board newBoard = BoardUtils.put(board, targetPosition, new Blank());
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> queen.move(sourcePosition, targetPosition, newBoard);
+        
+        // then
+        assertThatCode(callable).doesNotThrowAnyException();
+    }
+    
+    @Test
+    @DisplayName("직선 여러 칸 이동 검사")
+    void moveLinearMultiStep() {
+        
+        // given
+        final Board newBoard = BoardUtils.put(board, Position.of("d2"), new Blank());
+        final Position targetPosition = Position.of("d4");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> queen.move(sourcePosition, targetPosition, newBoard);
+        
+        // then
+        assertThatCode(callable).doesNotThrowAnyException();
+    }
+    
+    @Test
+    @DisplayName("현재 위치에서 갈 수 없는 칸으로 이동하려 할 경우 예외 발생")
+    void move_TryToMoveWhereCannotMove_ExceptionThrown() {
+        
+        // given
+        final Position targetPosition = Position.of("b2");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> queen.move(sourcePosition, targetPosition, board);
+        
+        
+        // then
         assertThatIllegalArgumentException().isThrownBy(callable);
     }
     
     @Test
+    @DisplayName("이동 경로 사이에 기물이 있을 경우 예외 발생")
+    void move_ObstacleIsInPath_ExceptionThrown() {
+        
+        // given
+        final Position targetPosition = Position.of("d3");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> queen.move(sourcePosition, targetPosition, board);
+        
+        
+        // then
+        assertThatIllegalArgumentException().isThrownBy(callable)
+                                            .withMessage("이동하는 경로 사이에 기물이 있습니다.");
+    }
+    
+    @Test
+    @DisplayName("타겟 위치에 이미 기물이 있을 경우 예외 발생")
+    void move_PieceAlreadyExistsAtTarget_ExceptionThrown() {
+        
+        // given
+        final Position targetPosition = Position.of("d2");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> queen.move(sourcePosition, targetPosition, board);
+        
+        
+        // then
+        assertThatIllegalArgumentException().isThrownBy(callable)
+                                            .withMessage("타겟 위치에 이미 기물이 있습니다.");
+    }
+    
+    @Test
     @DisplayName("점수 반환 테스트")
-    void score() {
-        assertThat(queen.getScore()).isEqualTo(9);
+    void scoreTest() {
+        
+        // when
+        final double score = queen.getScore();
+        
+        // then
+        assertThat(score).isEqualTo(9);
     }
 }
