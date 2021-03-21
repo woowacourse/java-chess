dc3925b42cebdbde551dbe8a820735877d99fa8e
Jinhong <xlffm3@gmail.com>
2021-03-21 16:48:23 +0900

    refactor: Rook 테스트 @nested 활용

--- src/test/java/chess/domain/piece/RookTest.java
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -4,107 +4,100 @@ import chess.domain.board.Cell;
 import chess.domain.board.ChessBoard;
 import chess.domain.board.ChessBoardGenerator;
 import chess.domain.board.Coordinate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 class RookTest {
-    @DisplayName("생성 테스트")
-    @Test
-    void makeRook() {
-        assertThatCode(() -> new Rook(TeamType.BLACK))
-                .doesNotThrowAnyException();
+
+    private final Piece rook = new Rook(TeamType.BLACK);
+    private final Coordinate current = Coordinate.from("d5");
+    private ChessBoard chessBoard;
+    private Map<Coordinate, Cell> cells;
+
+    @BeforeEach
+    void setup() {
+        cells = ChessBoardGenerator.generateEmptyBoard();
+        chessBoard = new ChessBoard(cells);
+        cells.get(current).put(rook);
     }
 
-    @DisplayName("빈 체스판 - 룩 이동 - 이동 가능")
-    @Test
-    void rookMoveOnEmptyBoard() {
-        ChessBoard chessBoard = new ChessBoard(ChessBoardGenerator.generateEmptyBoard());
-        Map<Coordinate, Cell> cells = chessBoard.getCells();
-        Rook rook = new Rook(TeamType.BLACK);
-        Coordinate currentCoordinate = Coordinate.from("d4");
-        Coordinate targetCoordinate = Coordinate.from("d5");
+    @DisplayName("Rook의 isMovable 메서드는")
+    @Nested
+    class Describe_isMovable {
 
-        cells.get(currentCoordinate).put(rook);
-        boolean isMovable = rook.isMovable(chessBoard, currentCoordinate, targetCoordinate);
+        @DisplayName("빈 체스판에서")
+        @Nested
+        class Context_EmptyChessBoard {
 
-        assertThat(isMovable).isTrue();
-    }
+            @DisplayName("동서남북 방향으로 최소 1칸 이상 이동 가능하다.")
+            @ParameterizedTest
+            @ValueSource(strings = {"d4", "d7", "c5", "f5"})
+            void moveSouthNorthEastWest(String destinationInput) {
+                Coordinate destination = Coordinate.from(destinationInput);
 
-    @DisplayName("목적지에 적 기물 - 룩 이동 - 이동 가능")
-    @Test
-    void rookMoveEnemyPieceOnTargetCoordinate() {
-        ChessBoard chessBoard = new ChessBoard(ChessBoardGenerator.generateEmptyBoard());
-        Map<Coordinate, Cell> cells = chessBoard.getCells();
-        Rook rook = new Rook(TeamType.BLACK);
+                boolean isMovable = rook.isMovable(chessBoard, current, destination);
 
-        Coordinate currentCoordinate = Coordinate.from("d4");
-        Coordinate routeCoordinate = Coordinate.from("d7");
-        Coordinate targetCoordinate = Coordinate.from("d7");
+                assertThat(isMovable).isTrue();
+            }
 
-        cells.get(currentCoordinate).put(rook);
-        Piece enemyPiece = new Queen(TeamType.WHITE);
-        cells.get(routeCoordinate).put(enemyPiece);
-        boolean isMovable = rook.isMovable(chessBoard, currentCoordinate, targetCoordinate);
+            @DisplayName("동서남북 방향을 제외한 방향으로는 이동할 수 없다.")
+            @ParameterizedTest
+            @ValueSource(strings = {"c4", "c6", "e4", "e6"})
+            void cannotMoveDiagonal(String destinationInput) {
+                Coordinate destination = Coordinate.from(destinationInput);
 
-        assertThat(isMovable).isTrue();
-    }
+                boolean isMovable = rook.isMovable(chessBoard, current, destination);
 
-    @DisplayName("빈 체스판 - 룩 이동 - 이동 불가 - 이동 경로 중간에 다른 기물 존재")
-    @Test
-    void rookCannotMoveOtherPieceOnRoute() {
-        ChessBoard chessBoard = new ChessBoard(ChessBoardGenerator.generateEmptyBoard());
-        Map<Coordinate, Cell> cells = chessBoard.getCells();
-        Rook rook = new Rook(TeamType.BLACK);
-        Coordinate currentCoordinate = Coordinate.from("d4");
-        Coordinate routeCoordinate = Coordinate.from("d6");
-        Coordinate targetCoordinate = Coordinate.from("d7");
+                assertThat(isMovable).isFalse();
+            }
+        }
 
-        cells.get(currentCoordinate).put(rook);
-        cells.get(routeCoordinate).put(new Rook(TeamType.WHITE));
+        @DisplayName("적이나 아군 기물이 졵재하는 체스판에서")
+        @Nested
+        class Context_InitializedChessBoard {
 
-        boolean isMovable = rook.isMovable(chessBoard, currentCoordinate, targetCoordinate);
-        assertThat(isMovable).isFalse();
-    }
+            private final Coordinate destination = Coordinate.from("g5");
 
-    @DisplayName("빈 체스판 - 룩 이동 - 이동 불가 - 도착 위치에 자신의 기물 존재")
-    @Test
-    void rookCannotMoveOwnPieceOnTargetCoordinate() {
-        ChessBoard chessBoard = new ChessBoard(ChessBoardGenerator.generateEmptyBoard());
-        Map<Coordinate, Cell> cells = chessBoard.getCells();
-        Rook rook = new Rook(TeamType.BLACK);
+            @DisplayName("현재 위치와 도착 위치 사이의 경로에 기물(팀에 상관없이)이 존재하는 경우 이동할 수 없다.")
+            @ParameterizedTest
+            @EnumSource(TeamType.class)
+            void cannotMoveWhenPieceOnRouteExists(TeamType teamType) {
+                Coordinate routeCoordinate = Coordinate.from("f5");
+                cells.get(routeCoordinate).put(new King(teamType));
 
-        Coordinate currentCoordinate = Coordinate.from("d4");
-        Coordinate routeCoordinate = Coordinate.from("d7");
-        Coordinate targetCoordinate = Coordinate.from("d7");
+                boolean isMovable = rook.isMovable(chessBoard, current, destination);
 
-        cells.get(currentCoordinate).put(rook);
-        Piece ownPiece = new Pawn(TeamType.BLACK);
-        cells.get(routeCoordinate).put(ownPiece);
+                assertThat(isMovable).isFalse();
+            }
 
-        boolean isMovable = rook.isMovable(chessBoard, currentCoordinate, targetCoordinate);
+            @DisplayName("도착 위치에 아군이 존재하는 경우 이동할 수 없다.")
+            @Test
+            void cannotMoveWhenSameTeamExistsOnDestination() {
+                cells.get(destination).put(new King(TeamType.BLACK));
 
-        assertThat(isMovable).isFalse();
-    }
+                boolean isMovable = rook.isMovable(chessBoard, current, destination);
 
-    @DisplayName("빈 체스판 - 룩 이동 - 이동 불가 - 이동할 수 없는 도착 위치")
-    @Test
-    void rookCannotInvalidTargetCoordinate() {
-        ChessBoard chessBoard = new ChessBoard(ChessBoardGenerator.generateEmptyBoard());
-        Map<Coordinate, Cell> cells = chessBoard.getCells();
-        Rook rook = new Rook(TeamType.BLACK);
+                assertThat(isMovable).isFalse();
+            }
 
-        Coordinate currentCoordinate = Coordinate.from("d4");
-        Coordinate targetCoordinate = Coordinate.from("c5");
+            @DisplayName("도착 위치에 적군이 존재하는 경우 이동할 수 있다.")
+            @Test
+            void canMoveWhenEnemyExistsOnDestination() {
+                cells.get(destination).put(new King(TeamType.WHITE));
 
-        cells.get(currentCoordinate).put(rook);
+                boolean isMovable = rook.isMovable(chessBoard, current, destination);
 
-        boolean isMovable = rook.isMovable(chessBoard, currentCoordinate, targetCoordinate);
-
-        assertThat(isMovable).isFalse();
+                assertThat(isMovable).isTrue();
+            }
+        }
     }
 }
