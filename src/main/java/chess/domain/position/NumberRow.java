5c16b86aff1a69586773c0ec40c1bc2bcae807a5
Naeun <knae11@naver.com>
2021-03-22 08:04:43 +0900

    refactor: 숫자열 원시값 포장

--- src/main/java/chess/domain/position/NumberRow.java
+++ src/main/java/chess/domain/position/NumberRow.java
@@ -1,5 +1,41 @@
 package chess.domain.position;
 
+import chess.domain.utils.RegexUtils;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberRow {
+    public static final int START_NUMBER = 1;
+    public static final int INITIAL_CAPACITY = 8;
+    private static final Map<Integer, NumberRow> cache = new HashMap<>(INITIAL_CAPACITY);
+
+    static{
+        for (int i = START_NUMBER; i < INITIAL_CAPACITY + START_NUMBER; i++) {
+            cache.put(i, new NumberRow(i));
+        }
+    }
+    private final int number;
+
+    private NumberRow(int i){
+        this.number = i;
+    }
+
+    public static NumberRow valueOf(String value) {
+        return valueOf(validNumber(value));
+     }
+
+    public static NumberRow valueOf(int value){
+        if(cache.containsKey(value)){
+            return cache.get(value);
+        }
+        throw new IllegalArgumentException("숫자 좌표가 유효하지 않습니다.");
+    }
+
+    private static int validNumber(String value){
+        if(RegexUtils.isValidRowNumber(value)){
+            return Integer.parseInt(value);
+        }
+        throw new IllegalArgumentException("유효하지 않은 입력입니다. 숫자이어야 합니다.");
+    }
 
 }
--- src/test/java/chess/domain/position/NumberRowTest.java
+++ src/test/java/chess/domain/position/NumberRowTest.java
@@ -1,7 +1,24 @@
 package chess.domain.position;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class NumberRowTest {
+    @Test
+    @DisplayName("문자로 생성")
+    void create(){
+        assertThatCode(() -> NumberRow.valueOf("1")).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("숫자로 생성")
+    void create1(){
+        assertThatCode(() -> NumberRow.valueOf(1)).doesNotThrowAnyException();
+    }
+
+
 
 }
