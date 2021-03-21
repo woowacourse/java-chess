a6282e810329d9451af8490d1729d8e78a6f3f44
Kimun Kim <kkm97351@gmail.com>
2021-03-21 14:37:28 +0900

    feat: ColoredPieces 구현

---
+++ src/main/java/chess/domain/piece/ColoredPieces.java
@@ -0,0 +1,52 @@
+package chess.domain.piece;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ColoredPieces {
+    private static final int PAWN_SIZE = 8;
+
+    private final List<RealPiece> pieces;
+    private final Color color;
+
+    public ColoredPieces(List<RealPiece> pieces, Color color) {
+        this.pieces = pieces;
+        this.color = color;
+    }
+
+    public static ColoredPieces createByColor(Color color) {
+        ArrayList<RealPiece> pieces = new ArrayList<>(Arrays.asList(
+                new King(color), new Queen(color),
+                new Bishop(color), new Bishop(color),
+                new Rook(color), new Rook(color),
+                new Knight(color), new Knight(color)));
+        pieces.addAll(createPawnsByColor(color));
+        return new ColoredPieces(pieces, color);
+    }
+
+    private static List<RealPiece> createPawnsByColor(Color color) {
+        return IntStream.range(0, PAWN_SIZE)
+                .mapToObj(i -> new Pawn(color))
+                .collect(Collectors.toList());
+    }
+
+    public void remove(RealPiece realPiece) {
+        if (realPiece.getColor() != color) {
+            throw new IllegalArgumentException("진영이 다른 피스를 제거할 수 없습니다");
+        }
+        pieces.remove(realPiece);
+    }
+
+    public boolean isKingAlive() {
+        return pieces.contains(new King(color));
+    }
+
+    public int size() {
+        return pieces.size();
+    }
+
+    public boolean contains(RealPiece realPiece){
+        return pieces.contains(realPiece);
+    }
+}
--- src/main/java/chess/domain/piece/RealPiece.java
+++ src/main/java/chess/domain/piece/RealPiece.java
@@ -2,6 +2,8 @@ package chess.domain.piece;
 
 import chess.domain.piece.strategy.MoveStrategy;
 
+import java.util.Objects;
+
 public class RealPiece extends Piece{
     private final Color color;
 
@@ -17,5 +19,18 @@ public class RealPiece extends Piece{
 
     public Color getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RealPiece realPiece = (RealPiece) o;
+        return color == realPiece.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color);
     }
 }
