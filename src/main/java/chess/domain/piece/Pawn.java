197e1a832d0b0e3a0b285204225ddbcbf10fa99f
Seed <hsik0225@gmail.com>
2021-03-21 13:59:59 +0900

    refactor(pawn):메소드 분리 및 테스트 코드 추가

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,16 +1,30 @@
 package chess.domain.piece;
 
-import java.util.List;
+import chess.game.Board;
 
 public class Pawn extends AbstractPiece {
     
+    public static final double SCORE = 1;
+    private static final int MAX_MOVABLE_LENGTH = 1;
     private static final String SYMBOL = "p";
     
-    public static final double SCORE = 1;
-    private static final int ABLE_LENGTH = 1;
+    public Pawn(Color color, DirectionGroup directionGroup) {
+        super(color, directionGroup);
+    }
     
-    public Pawn(Color color, Position position) {
-        super(color, position);
+    public static Pawn createWhitePawn() {
+        final DirectionGroup directionGroup = new DirectionGroup(Direction.whitePawnDirection(), MAX_MOVABLE_LENGTH);
+        return new Pawn(Color.WHITE, directionGroup);
+    }
+    
+    public static Pawn createBlackPawn() {
+        final DirectionGroup directionGroup = new DirectionGroup(Direction.blackPawnDirection(), MAX_MOVABLE_LENGTH);
+        return new Pawn(Color.WHITE, directionGroup);
+    }
+    
+    public static Pawn from(Color color) {
+        final DirectionGroup directionGroup = new DirectionGroup(color.getPawnDirections(), MAX_MOVABLE_LENGTH);
+        return new Pawn(color, directionGroup);
     }
     
     @Override
@@ -18,77 +32,32 @@ public class Pawn extends AbstractPiece {
         return changeColorSymbol(SYMBOL);
     }
     
-    // TODO 1 또는 0.5 가 가능하다
     @Override
     public double getScore() {
         return SCORE;
     }
     
-//    @Override
-//    public Piece move(Position position) {
-//        final List<Direction> directions = findPawnDirection();
-//        if (!isMovable(position, directions, ABLE_LENGTH)) {
-//            throw new IllegalArgumentException("폰이 이동할 수 없는 위치입니다.");
-//        }
-//        return new Pawn(color, position);
-//    }
-    
     @Override
-    public Piece move(Position position, List<List<Piece>> board) {
-        final List<Direction> directions = findPawnDirection();
-        Direction direction = findDirection(position, directions, ABLE_LENGTH);
-        if (isForward(direction) && !board.get(position.getX().getPoint()).get(position.getY().getPoint()).isSameColor(Color.BLANK)) {
-            throw new IllegalArgumentException("폰은 전진하는 위치에 기물이 있으면 안됩니다.");
-        }
-        if (!isForward(direction) && board.get(position.getX().getPoint()).get(position.getY().getPoint()).isSameColor(Color.BLANK)) {
-            throw new IllegalArgumentException("폰은 대각선으로 이동하기 위해서는 상대방의 기물이 있어야 합니다.");
-        }
-        // TODO 장애물 체크
-        if (isObstacleAtDirection(position, direction, board)) {
+    public Piece move(Position sourcePosition, Position targetPosition, Board board) {
+        Direction direction = directionGroup.findDirectionOfPawn(sourcePosition, targetPosition, color);
+        if (isObstacleAtDirection(sourcePosition, targetPosition, direction, board)) {
             throw new IllegalArgumentException("이동하는 경로 사이에 기물이 있습니다.");
         }
-        return new Pawn(color, position);
+        
+        final boolean isForwardDirection = isForward(direction);
+        final boolean isBlankAtTargetPosition = board.isBlank(targetPosition);
+        if (isForwardDirection && !isBlankAtTargetPosition) {
+            throw new IllegalArgumentException("폰이 전진하는 위치에 기물이 있으면 안됩니다.");
+        }
+        
+        if (!isForwardDirection && isBlankAtTargetPosition) {
+            throw new IllegalArgumentException("폰은 대각선으로 이동하기 위해서는 상대방의 기물이 있어야 합니다.");
+        }
+        
+        return Pawn.from(color);
     }
     
     private boolean isForward(Direction direction) {
-        return direction == forward();
-    }
-    
-    private List<Direction> findPawnDirection() {
-        if (color.isBlack()) {
-            return Direction.blackPawnDirection();
-        }
-        
-        return Direction.whitePawnDirection();
-    }
-    
-    @Override
-    protected Direction findDirection(Position position, List<Direction> directions, int ableLength) {
-        return directions.stream()
-                         .filter(direction -> this.position.canMove(position, direction, ableLength))
-                         .findAny()
-                         .orElseGet(() -> {
-                             if (isFirst() && this.position.canMove(position, forward(),ableLength+1)) {
-                                 return forward();
-                             }
-                             throw new IllegalArgumentException(ERROR_CAN_NOT_MOVE);
-                         });
-    }
-    
-    private Direction forward() {
-        if (color.isBlack()) {
-            return Direction.SOUTH;
-        }
-        return Direction.NORTH;
-    }
-    
-    private boolean isFirst() {
-        if (position.getX().getPoint() == 1 && color.isWhite()) {
-            return true;
-        }
-        if (position.getX().getPoint() == 6 && color.isBlack()) {
-            return true;
-        }
-        return false;
+        return (direction == Direction.SOUTH) || (direction == Direction.NORTH);
     }
 }
--- src/test/java/chess/domain/piece/PawnTest.java
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -1,121 +1,136 @@
 package chess.domain.piece;
 
-import chess.board.Board;
+import chess.game.Board;
+import chess.game.InitializedChess;
 import org.assertj.core.api.ThrowableAssert;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
 class PawnTest {
-    private Pawn pawn;
+    private final Pawn pawn = Pawn.createWhitePawn();
+    private final Position sourcePosition = Position.of("a2");
+    private final Board board = InitializedChess.create()
+                                                .getBoard();
     
-    private List<List<Piece>> pieces;
-    
-    @BeforeEach
-    void setUp() {
-        Position position = Position.of(1, 1);
-        
-        pawn = new Pawn(Color.WHITE, position);
-        
-        Board board = new Board();
-        board.init();
-        
-        pieces = board.getBoard();
-    }
     
     @Test
     @DisplayName("1칸 전진 테스트")
-    void move() {
-        Position position = Position.of(2, 1);
+    void moveLinearOneStep() {
+        Position targetPosition = Position.of("a3");
         
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(position, pieces);
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(sourcePosition, targetPosition, board);
         
         assertThatCode(callable).doesNotThrowAnyException();
     }
     
     @Test
-    @DisplayName("이동 에러 검사")
-    void validate() {
-        Position position = Position.of(2, 4);
+    @DisplayName("2칸 전진 테스트")
+    void moveLinearTwoStep() {
+        Position targetPosition = Position.of("a4");
         
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(position, pieces);
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(sourcePosition, targetPosition, board);
+        
+        assertThatCode(callable).doesNotThrowAnyException();
+    }
+    
+    @Test
+    @DisplayName("3칸 이상 전진 시 예외 발생")
+    void moveLinearMultiStep_OverTwoStep_ExceptionThrown() {
+        Position targetPosition = Position.of("a5");
+        
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(sourcePosition, targetPosition, board);
         
         assertThatIllegalArgumentException().isThrownBy(callable);
     }
     
     @Test
-    @DisplayName("점수 반환 테스트")
-    void score() {
-        assertThat(pawn.getScore()).isEqualTo(1);
-    }
-    
-    @Test
-    @DisplayName("2칸 전진 테스트")
-    void a() {
-        Position position = Position.of("b4");
-        
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(position, pieces);
-        
-        assertThatCode(callable).doesNotThrowAnyException();
-    }
-    
-    @Test
     @DisplayName("1칸 대각선에 있는 상대방 기물 잡기 테스트")
-    void b() {
-        Position position = Position.of("b4");
+    void moveDiagonalOneStep() {
         
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(position, pieces);
+        // given
+        final Map<Position, Piece> testBoard = this.board.getBoard();
+        Position targetPosition = Position.of("b3");
+        testBoard.put(targetPosition, Pawn.createBlackPawn());
         
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(sourcePosition, targetPosition,
+                new Board(testBoard));
+        
+        // then
         assertThatCode(callable).doesNotThrowAnyException();
     }
     
     @Test
-    @DisplayName("전진 위치에 기물이 있을 경우 예외 발생")
-    void c() {
-        Position blackPawnPosition = Position.of("b3");
+    @DisplayName("타겟 위치에 이미 기물이 있을 경우 예외 발생")
+    void move_PieceAlreadyExistsAtTarget_ExceptionThrown() {
         
-        pieces.get(2)
-              .set(1, new Pawn(Color.BLACK, blackPawnPosition));
+        // given
+        Position targetPosition = Position.of("a3");
+        Board newBoard = BoardUtils.put(board, targetPosition, Pawn.createBlackPawn());
         
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(blackPawnPosition, pieces);
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(sourcePosition, targetPosition, newBoard);
         
+        // then
         assertThatIllegalArgumentException().isThrownBy(callable);
     }
     
     @Test
     @DisplayName("첫 이동이 아닌데 2칸을 전진할 경우 예외 발생")
-    void d() {
-        Position whitePawnPosition = Position.of("b3");
-        pieces.get(2)
-              .set(1, new Pawn(Color.WHITE, whitePawnPosition));
+    void moveTwoStep_IfPawnIsNotFirstMovement_ExceptionThrown() {
         
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(Position.of("b5"), pieces);
+        // given
+        Position newSourcePosition = Position.of("c3");
+        Board newBoard = BoardUtils.put(board, newSourcePosition, Pawn.createWhitePawn());
         
+        Position targetPosition = Position.of("c5");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(newSourcePosition, targetPosition, newBoard);
+        
+        // then
         assertThatIllegalArgumentException().isThrownBy(callable);
     }
     
     @Test
     @DisplayName("1칸 대각선 이동시 타겟 위치가 블랭크일 경우 이동 불가")
-    void e() {
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(Position.of("c3"), pieces);
+    void moveDiagonal_IfTargetIsBlank_ExceptionThrown() {
         
+        // given
+        final Position targetPosition = Position.of("b3");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(sourcePosition, targetPosition, board);
+        
+        // then
         assertThatIllegalArgumentException().isThrownBy(callable);
     }
     
     @Test
     @DisplayName("2칸 이동 시 경로 중간에 기물이 있을 경우 예외 발생")
-    void f() {
-        Position blackPawnPosition = Position.of("b3");
+    void move_ObstacleIsInPath_ExceptionThrown() {
         
-        pieces.get(2).set(1, new Pawn(Color.BLACK, blackPawnPosition));
+        // given
+        Position blackPawnPosition = Position.of("c3");
+        Board newBoard = BoardUtils.put(board, blackPawnPosition, Pawn.createBlackPawn());
         
-        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(Position.of("b4"), pieces);
+        Position targetPosition = Position.of("c4");
         
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> pawn.move(sourcePosition, targetPosition, newBoard);
+        
+        // then
         assertThatIllegalArgumentException().isThrownBy(callable)
-                                            .withMessage("이동하는 경로 사이에 기물이 있습니다.");
+                                            .withMessage("기물이 이동할 수 없는 위치입니다.");
+    }
+    
+    @Test
+    @DisplayName("점수 반환 테스트")
+    void scoreTest() {
+        assertThat(pawn.getScore()).isEqualTo(1);
     }
 }
