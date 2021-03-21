db3d7e7205ab720c91a4cc730941204eaaf98255
Seed <hsik0225@gmail.com>
2021-03-22 01:11:55 +0900

    feat(empty-board):빈 칸만 들어있는 체스 보드 생성 기능 구현

---
+++ src/main/java/chess/domain/board/EmptyBoard.java
@@ -0,0 +1,33 @@
+package chess.domain.board;
+
+import chess.domain.Color;
+import chess.domain.piece.Blank;
+import chess.domain.piece.Piece;
+import chess.domain.position.Position;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmptyBoard {
+    
+    private static final int MIN_INDEX = 0;
+    private static final int MAX_INDEX = 7;
+    
+    public static Board create() {
+        final Map<Position, Piece> board = new HashMap<>();
+        createBlanks(board);
+        return new Board(board);
+    }
+    
+    private static void createBlanks(Map<Position, Piece> board) {
+        for (int yPoint = MIN_INDEX; yPoint <= MAX_INDEX; yPoint++) {
+            createPiecesAtRow(board, yPoint, new Blank(Color.BLANK));
+        }
+    }
+    
+    private static void createPiecesAtRow(Map<Position, Piece> board, int yPoint, Piece piece) {
+        for (int xPoint = MIN_INDEX; xPoint <= MAX_INDEX; xPoint++) {
+            board.put(Position.of(xPoint, yPoint), piece);
+        }
+    }
+}
