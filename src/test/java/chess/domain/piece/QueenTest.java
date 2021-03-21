62b6af990398270a443993d7f619ab59f6608729
Jinhong <xlffm3@gmail.com>
2021-03-21 16:56:12 +0900

    refactor: Queen 테스트 @nested 적용

--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -6,78 +6,98 @@ import chess.domain.board.ChessBoardGenerator;
 import chess.domain.board.Coordinate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 class QueenTest {
+
     private final Piece queen = new Queen(TeamType.BLACK);
-    private final Coordinate currentCoordinate = Coordinate.from("d5");
+    private final Coordinate current = Coordinate.from("d5");
     private ChessBoard chessBoard;
     private Map<Coordinate, Cell> cells;
 
     @BeforeEach
     void setup() {
-        chessBoard = new ChessBoard(ChessBoardGenerator.generateEmptyBoard());
-        cells = chessBoard.getCells();
-        cells.get(currentCoordinate).put(queen);
+        cells = ChessBoardGenerator.generateEmptyBoard();
+        chessBoard = new ChessBoard(cells);
+        cells.get(current).put(queen);
     }
 
-    @DisplayName("생성 테스트")
-    @Test
-    void makeQueen() {
-        assertThatCode(() -> new Queen(TeamType.BLACK))
-                .doesNotThrowAnyException();
-    }
+    @DisplayName("Queen의 isMovable 메서드는")
+    @Nested
+    class Describe_isMovable {
 
-    @DisplayName("Queen의 모든 방향으로 이동할 수 있다")
-    @ParameterizedTest
-    @ValueSource(strings = {"c6", "e6", "c4", "e4", "d4", "d6", "c5", "e5"})
-    void moveDiagonal(String targetCoordinateInput) {
-        Coordinate destination = Coordinate.from(targetCoordinateInput);
-        boolean isMovable = queen.isMovable(chessBoard, currentCoordinate, destination);
+        @DisplayName("빈 체스판에서")
+        @Nested
+        class Context_EmptyChessBoard {
 
-        assertThat(isMovable).isTrue();
-    }
+            @DisplayName("동서남북 대각선 방향으로 최소 1칸 이상 이동 가능하다.")
+            @ParameterizedTest
+            @ValueSource(strings = {"d4", "d7", "c5", "f5", "c4", "c6", "f3", "g2"})
+            void moveEveryDirection(String destinationInput) {
+                Coordinate destination = Coordinate.from(destinationInput);
 
-    @DisplayName("Queen의 도착위치까지 이동하는 경로 중간에 기물이 있으면 이동 불가")
-    @Test
-    void cannotMoveWhenPieceExistsOnRoute() {
-        Coordinate destination = Coordinate.from("b3");
+                boolean isMovable = queen.isMovable(chessBoard, current, destination);
 
-        Piece dummy = new Rook(TeamType.BLACK);
-        cells.get(Coordinate.from("c4")).put(dummy);
+                assertThat(isMovable).isTrue();
+            }
 
-        boolean isMovable = queen.isMovable(chessBoard, currentCoordinate, destination);
+            @DisplayName("동서남북대각선 방향을 제외한 방향으로 이동할 수 없다.")
+            @ParameterizedTest
+            @ValueSource(strings = {"c3", "c7", "e3", "e7"})
+            void cannotMoveUndefinedDirection(String destinationInput) {
+                Coordinate destination = Coordinate.from(destinationInput);
 
-        assertThat(isMovable).isFalse();
-    }
+                boolean isMovable = queen.isMovable(chessBoard, current, destination);
 
-    @DisplayName("Queen의 도착위치에 같은 팀 기물이 있으면 이동 불가")
-    @Test
-    void cannotMoveWhenSameTeamPieceExistsOnDestination() {
-        Coordinate destination = Coordinate.from("b3");
+                assertThat(isMovable).isFalse();
+            }
+        }
 
-        Piece dummy = new Rook(TeamType.BLACK);
-        cells.get(destination).put(dummy);
-        boolean isMovable = queen.isMovable(chessBoard, currentCoordinate, destination);
+        @DisplayName("적이나 아군 기물이 졵재하는 체스판에서")
+        @Nested
+        class Context_InitializedChessBoard {
 
-        assertThat(isMovable).isFalse();
-    }
+            private final Coordinate destination = Coordinate.from("b7");
 
-    @DisplayName("Queen의 도착위치에 적 팀 기물이 있으면 이동 가능")
-    @Test
-    void moveToDestinationWhenEnemyExistsOnDestination() {
-        Coordinate destination = Coordinate.from("b3");
-        Piece dummy = new Rook(TeamType.WHITE);
-        cells.get(destination).put(dummy);
-        boolean isMovable = queen.isMovable(chessBoard, currentCoordinate, destination);
+            @DisplayName("현재 위치와 도착 위치 사이의 경로에 기물(팀에 상관없이)이 존재하는 경우 이동할 수 없다.")
+            @ParameterizedTest
+            @EnumSource(TeamType.class)
+            void cannotMoveWhenPieceOnRouteExists(TeamType teamType) {
+                Coordinate routeCoordinate = Coordinate.from("c6");
+                cells.get(routeCoordinate).put(new King(teamType));
 
-        assertThat(isMovable).isTrue();
+                boolean isMovable = queen.isMovable(chessBoard, current, destination);
+
+                assertThat(isMovable).isFalse();
+            }
+
+            @DisplayName("도착 위치에 아군이 존재하는 경우 이동할 수 없다.")
+            @Test
+            void cannotMoveWhenSameTeamExistsOnDestination() {
+                cells.get(destination).put(new King(TeamType.BLACK));
+
+                boolean isMovable = queen.isMovable(chessBoard, current, destination);
+
+                assertThat(isMovable).isFalse();
+            }
+
+            @DisplayName("도착 위치에 적군이 존재하는 경우 이동할 수 있다.")
+            @Test
+            void canMoveWhenEnemyExistsOnDestination() {
+                cells.get(destination).put(new King(TeamType.WHITE));
+
+                boolean isMovable = queen.isMovable(chessBoard, current, destination);
+
+                assertThat(isMovable).isTrue();
+            }
+        }
     }
 }
