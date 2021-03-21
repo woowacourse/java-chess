72dc7293ad1f8c203b696c04a19c34f27560eefe
binghe819 <binghe819@naver.com>
2021-03-21 23:41:16 +0900

    refactor: 킹 움직임 예외처리 수정 및 테스트 케이스 추가

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -14,7 +14,9 @@ public class King extends Piece {
     }
 
     @Override
-    public boolean isMovable(final Location target) {
-        return location.isAdjacent(target);
+    public void validateMovingAbilityToTarget(Location target) {
+        if (!location.isAdjacent(target)) {
+            throw new IllegalArgumentException("[ERROR] 킹은 해당 위치로 이동할 능력이 없습니다.");
+        }
     }
 }
