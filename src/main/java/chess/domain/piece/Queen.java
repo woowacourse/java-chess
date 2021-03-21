9f8dd00cbe0a5d56f291a686603010ffe68cf7a7
binghe819 <binghe819@naver.com>
2021-03-21 23:21:30 +0900

    refactor: 퀸 움직임 예외처리 수정 및 테스트 케이스 추가

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -14,7 +14,9 @@ public class Queen extends Piece {
     }
 
     @Override
-    public boolean isMovable(final Location target) {
-        return location.isHorizontalOrVertical(target) || location.isDiagonal(target);
+    public void validateMovingAbilityToTarget(Location target) {
+        if (!(location.canMoveHorizontallyOrVerticallyTo(target) || location.canMoveDigonallyTo(target))) {
+            throw new IllegalArgumentException("[ERROR] 퀸은 해당 위치로 이동할 능력이 없습니다.");
+        }
     }
 }
