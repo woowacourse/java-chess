21adf7c8d19677608ba56d4c344c2877bd18213a
Seed <hsik0225@gmail.com>
2021-03-21 15:45:06 +0900

    feat(symbol-board):출력을 위한 보드 DTO 생성

---
+++ src/main/java/chess/domain/game/SymbolBoard.java
@@ -0,0 +1,38 @@
+package chess.domain.game;
+
+import chess.domain.piece.Piece;
+import chess.domain.piece.Position;
+
+import java.util.Map;
+
+public class SymbolBoard {
+    private final String[][] board;
+    
+    public SymbolBoard(String[][] board) {
+        this.board = board;
+    }
+    
+    public static SymbolBoard from(Chess chess) {
+        final Map<Position, Piece> pieceBoard = chess.getBoard()
+                                                     .getBoard();
+        final String[][] board = new String[8][8];
+        for (Map.Entry<Position, Piece> entry : pieceBoard.entrySet()) {
+            final int x = entry.getKey()
+                               .getX()
+                               .getPoint();
+            
+            final int y = entry.getKey()
+                               .getY()
+                               .getPoint();
+            
+            board[x][y] = entry.getValue()
+                               .getSymbol();
+        }
+        
+        return new SymbolBoard(board);
+    }
+    
+    public String[][] getBoard() {
+        return board;
+    }
+}
