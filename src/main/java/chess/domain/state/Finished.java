4f67bff792b4fe01a42235a852ba78ca9b5ace84
TaeJeong Kim <xowjd41@naver.com>
2021-03-21 19:04:02 +0900

    refactor: State move 방식 변경
    move 파라미터 및 구현 변경

--- src/main/java/chess/domain/state/Finished.java
+++ src/main/java/chess/domain/state/Finished.java
@@ -1,6 +1,8 @@
 package chess.domain.state;
 
 import chess.domain.piece.Pieces;
+import chess.domain.position.Source;
+import chess.domain.position.Target;
 
 public class Finished extends Turn {
     public Finished(final Pieces pieces) {
@@ -8,7 +10,20 @@ public class Finished extends Turn {
     }
 
     @Override
-    public boolean isFinish() {
+    public final State move(final Source source, final Target target, final State anotherState) {
+        throw new UnsupportedOperationException("끝난 상태에서는 기물을 움직일 수 없습니다.");
+    }
+
+    @Override
+    public final boolean isFinish() {
         return true;
+    }
+
+    @Override
+    public State toRunningState(final State anotherState) {
+        if (anotherState.isFinish()) {
+            return new Running(pieces());
+        }
+        throw new IllegalStateException("상태방 차례입니다.");
     }
 }
--- src/test/java/chess/domain/state/StateTest.java
+++ src/test/java/chess/domain/state/StateTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static chess.domain.piece.PositionTexture.A2;
-import static chess.domain.piece.PositionTexture.A3;
+import static chess.domain.piece.PositionTexture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StateTest {
     private static State white;
@@ -31,8 +31,58 @@ class StateTest {
         Source source = Source.valueOf(A2, white);
         Target target = Target.valueOf(source, A3, white);
 
-        State whiteAfterMove = white.move(source, target, black.pieces());
+        State whiteAfterMove = white.move(source, target, black);
 
         assertThat(whiteAfterMove).isInstanceOf(Finished.class);
+    }
+
+    @DisplayName("말이 움직여지면, 턴이 변경되고, 그상태에서 움직이려 하면 에러가 발생한다.")
+    @Test
+    void turnOverMove() {
+        Source source = Source.valueOf(A2, white);
+        Target target = Target.valueOf(source, A3, white);
+        State whiteAfterMove = white.move(source, target, black);
+        assertThat(whiteAfterMove).isInstanceOf(Finished.class);
+
+        Source secondSource = Source.valueOf(A3, white);
+        Target secondTarget = Target.valueOf(source, A4, white);
+
+        assertThatThrownBy(() ->
+                whiteAfterMove.move(
+                        secondSource, secondTarget, black
+                )
+        ).isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("끝난 상태에서는 기물을 움직일 수 없습니다.");
+    }
+
+    @DisplayName("말이 움직여지면, 턴이 변경되고, 상대가 움직인다.")
+    @Test
+    void turnChange() {
+        Source source = Source.valueOf(A2, white);
+        Target target = Target.valueOf(source, A3, white);
+        State whiteAfterMove = white.move(source, target, black);
+        assertThat(whiteAfterMove).isInstanceOf(Finished.class);
+
+        Source blackSource = Source.valueOf(A7, black);
+        Target blackTarget = Target.valueOf(source, A5, black);
+
+        black = black.toRunningState(whiteAfterMove);
+
+        State blackState = black.move(blackSource, blackTarget, whiteAfterMove);
+        assertThat(blackState.findPiece(A5)).isNotEmpty();
+    }
+
+    @DisplayName("자기 차례가 아닐 때 움직이려하면 예외가 발생한다.")
+    @Test
+    void turnNotOverException() {
+        Source source = Source.valueOf(A7, black);
+        Target target = Target.valueOf(source, A5, black);
+
+        assertThatThrownBy(() ->
+                black.move(
+                        source, target, white
+                )
+        ).isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("끝난 상태에서는 기물을 움직일 수 없습니다.");
     }
 }
