aa412b2799ec8e762f330bdd802d182bcdd419bb
Seed <hsik0225@gmail.com>
2021-03-21 13:59:51 +0900

    refactor(knight):메소드 분리 및 테스트 코드 추가

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,16 +1,17 @@
 package chess.domain.piece;
 
-import java.util.List;
+import chess.game.Board;
 
 public class Knight extends AbstractPiece {
     
-    private static final String SYMBOL = "n";
-    private static final int ABLE_LENGTH = 1;
-    
     public static final double SCORE = 2.5;
+    private static final int MAX_MOVABLE_LENGTH = 1;
+    private static final DirectionGroup DIRECTION_GROUP = new DirectionGroup(Direction.knightDirection(),
+            MAX_MOVABLE_LENGTH);
+    private static final String SYMBOL = "n";
     
-    public Knight(Color color, Position position) {
-        super(color, position);
+    public Knight(Color color) {
+        super(color, DIRECTION_GROUP);
     }
     
     @Override
@@ -23,24 +24,14 @@ public class Knight extends AbstractPiece {
         return SCORE;
     }
     
-//    @Override
-//    public Piece move(Position position) {
-//        final List<Direction> directions = Direction.knightDirection();
-//        if (!isMovable(position, directions, ABLE_LENGTH)) {
-//            throw new IllegalArgumentException("나이트가 이동할 수 없는 위치입니다.");
-//        }
-//        return new Knight(color, position);
-//    }
-    
     @Override
-    public Piece move(Position position, List<List<Piece>> board) {
-        final List<Direction> directions = Direction.knightDirection();
-        Direction direction = findDirection(position, directions, ABLE_LENGTH);
+    public Piece move(Position sourcePosition, Position targetPosition, Board board) {
+        Direction direction = DIRECTION_GROUP.findDirection(sourcePosition, targetPosition);
         
-        // TODO 장애물 체크
-        if (isObstacleAtDirection(position, direction, board)) {
-            throw new IllegalArgumentException("이동하는 경로 사이에 기물이 있습니다.");
+        if (!board.isBlank(targetPosition)) {
+            throw new IllegalArgumentException("타겟 위치에 이미 기물이 있습니다.");
         }
-        return new Knight(color, position);
+        
+        return new Knight(color);
     }
 }
--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -1,55 +1,73 @@
 package chess.domain.piece;
 
-import chess.board.Board;
+import chess.game.Board;
+import chess.game.InitializedChess;
 import org.assertj.core.api.ThrowableAssert;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class KnightTest {
-    private Knight knight;
-    
-    private List<List<Piece>> pieces;
-    
-    @BeforeEach
-    void setUp() {
-        Position position = Position.of(0, 1);
-    
-        knight = new Knight(Color.BLACK, position);
-        
-        Board board = new Board();
-        board.init();
-        
-        pieces = board.getBoard();
-    }
+    private final Knight knight = new Knight(Color.WHITE);
+    private final Position sourcePosition = Position.of("b1");
+    private final Board board = InitializedChess.create()
+                                                .getBoard();
     
     @Test
-    @DisplayName("이동검사")
-    void move() {
-        Position position = Position.of(2, 0);
+    @DisplayName("이동 검사")
+    void moveTest() {
         
-        ThrowableAssert.ThrowingCallable callable = () -> knight.move(position, pieces);
+        // given
+        final Position targetPosition = Position.of("a3");
         
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> knight.move(sourcePosition, targetPosition, board);
+        
+        // then
         assertThatCode(callable).doesNotThrowAnyException();
     }
     
     @Test
-    @DisplayName("이동 에러 검사")
-    void validate() {
-        Position position = Position.of(2, 1);
+    @DisplayName("타겟 위치에 이미 기물이 있을 경우 예외 발생")
+    void move_PieceAlreadyExistsAtTarget_ExceptionThrown() {
         
-        ThrowableAssert.ThrowingCallable callable = () -> knight.move(position, pieces);
+        // given
+        final Position targetPosition = Position.of("d2");
         
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> knight.move(sourcePosition, targetPosition, board);
+        
+        
+        // then
+        assertThatIllegalArgumentException().isThrownBy(callable)
+                                            .withMessage("타겟 위치에 이미 기물이 있습니다.");
+    }
+    
+    
+    @Test
+    @DisplayName("현재 위치에서 갈 수 없는 칸으로 이동하려 할 경우 예외 발생")
+    void move_TryToMoveWhereCannotMove_ExceptionThrown() {
+        
+        // given
+        final Position targetPosition = Position.of("a2");
+        
+        // when
+        ThrowableAssert.ThrowingCallable callable = () -> knight.move(sourcePosition, targetPosition, board);
+        
+        
+        // then
         assertThatIllegalArgumentException().isThrownBy(callable);
     }
     
     @Test
     @DisplayName("점수 반환 테스트")
-    void score() {
-        assertThat(knight.getScore()).isEqualTo(2.5);
+    void scoreTest() {
+        
+        // when
+        final double score = knight.getScore();
+        
+        // then
+        assertThat(score).isEqualTo(2.5);
     }
 }
