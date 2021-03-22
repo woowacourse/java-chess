f04a78a67fa26e33dbc61411d561b919eb4415fa
daeun <leede0418@likelion.org>
2021-03-22 19:06:06 +0900

    test(command): Status 테스트 추가

--- src/test/java/chess/domain/command/CommandTest.java
+++ src/test/java/chess/domain/command/CommandTest.java
@@ -7,8 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CommandTest {
-
-    @DisplayName("start 커맨드가 들어오면 Start상태를 반환한다.")
+    @DisplayName("start 커맨드가 들어오면 Start 상태를 반환한다.")
     @Test
     void start() {
         Command command = CommandFactory.initialCommand("start");
@@ -16,7 +15,7 @@ class CommandTest {
         assertThat(command).isInstanceOf(Start.class);
     }
 
-    @DisplayName("start가 들어오고, move 커맨드가 들어오면 move상태로 변경한다.")
+    @DisplayName("start가 들어오고, move 커맨드가 들어오면 Move 상태로 변경한다.")
     @Test
     void move() {
         Command command = CommandFactory.initialCommand("start");
@@ -25,7 +24,7 @@ class CommandTest {
         assertThat(move).isInstanceOf(Move.class);
     }
 
-    @DisplayName("move를 한 뒤, Ready상태로 변경한다.")
+    @DisplayName("move를 한 뒤, Ready 상태로 변경한다.")
     @Test
     void ready() {
         Command command = CommandFactory.initialCommand("start");
@@ -34,7 +33,7 @@ class CommandTest {
         assertThat(move.ready()).isInstanceOf(Ready.class);
     }
 
-    @DisplayName("start를 한 뒤, end 상태로 변경한다.")
+    @DisplayName("start를 한 뒤, end 커맨드가 들어오면 End 상태로 변경한다.")
     @Test
     void end() {
         Command command = CommandFactory.initialCommand("start");
@@ -43,7 +42,16 @@ class CommandTest {
         assertThat(end).isInstanceOf(End.class);
     }
 
-    @DisplayName("start 상태에서 옳지않은 커맨드가 들어오면 예가 발생한다.")
+    @DisplayName("start를 한 뒤, status 커맨드가 들어오면 Status 상태로 변경한다.")
+    @Test
+    void status() {
+        Command command = CommandFactory.initialCommand("start");
+        Command status = command.execute("status");
+
+        assertThat(status).isInstanceOf(Status.class);
+    }
+
+    @DisplayName("start 상태에서 옳지 않은 커맨드가 들어오면 예외가 발생한다.")
     @Test
     void startException() {
         Command command = CommandFactory.initialCommand("start");
@@ -52,7 +60,7 @@ class CommandTest {
                 .isInstanceOf(UnsupportedOperationException.class);
     }
 
-    @DisplayName("ready 상태에서 옳지않은 커맨드가 들어오면 예외가 발생한다.")
+    @DisplayName("ready 상태에서 옳지 않은 커맨드가 들어오면 예외가 발생한다.")
     @Test
     void readyException() {
         Command command = CommandFactory.initialCommand("start");
@@ -63,7 +71,7 @@ class CommandTest {
                 .isInstanceOf(UnsupportedOperationException.class);
     }
 
-    @DisplayName("move 상태에서 커맨드를 실행하려하면 예외가 발생한다.")
+    @DisplayName("move 상태에서 커맨드를 실행하려 하면 예외가 발생한다.")
     @Test
     void moveException() {
         Command command = CommandFactory.initialCommand("start");
@@ -73,13 +81,23 @@ class CommandTest {
                 .isInstanceOf(UnsupportedOperationException.class);
     }
 
-    @DisplayName("end 상태에서 커맨드를 실행하려하면 예외가 발생한다.")
+    @DisplayName("end 상태에서 커맨드를 실행하려 하면 예외가 발생한다.")
     @Test
     void endException() {
         Command command = CommandFactory.initialCommand("start");
         Command end = command.execute("end");
 
         assertThatThrownBy(() -> end.execute("end"))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @DisplayName("status 상태에서 커맨드를 실행하려 하면 예외가 발생한다.")
+    @Test
+    void statusException() {
+        Command command = CommandFactory.initialCommand("start");
+        Command status = command.execute("status");
+
+        assertThatThrownBy(() -> status.execute("status"))
                 .isInstanceOf(UnsupportedOperationException.class);
     }
 }
