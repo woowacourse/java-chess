1a86516738f873e18aee4d3f47054cb67d488ba9
y_woo :) <dusdn1702@naver.com>
2021-03-22 16:33:08 +0900

    refactor: Row, Column의 부모 Position 생성

---
+++ src/main/java/chess/domain/board/Position.java
@@ -0,0 +1,38 @@
+package chess.domain.board;
+
+import java.util.Objects;
+
+public class Position {
+	private final int position;
+
+	public Position(int position) {
+		this.position = position;
+	}
+
+	public int subtract(Position anotherPosition) {
+		return position - anotherPosition.position;
+	}
+
+	public int add(Position anotherPosition) {
+		return this.position + anotherPosition.position;
+	}
+
+	public boolean isSameAs(int position) {
+		return this.position == position;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Position position1 = (Position)o;
+		return position == position1.position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(position);
+	}
+}
--- src/main/java/chess/domain/board/Row.java
+++ src/main/java/chess/domain/board/Row.java
@@ -1,38 +1,7 @@
 package chess.domain.board;
 
-import java.util.Objects;
-
-public class Row {
-	private final int row;
-
+public class Row extends Position{
 	public Row(int row) {
-		this.row = row;
-	}
-
-	public int subtract(Row anotherRow) {
-		return row - anotherRow.row;
-	}
-
-	public int add(Row anotherRow) {
-		return this.row + anotherRow.row;
-	}
-
-	public boolean isSameAs(int row) {
-		return this.row == row;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Row row1 = (Row)o;
-		return row == row1.row;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(row);
+		super(row);
 	}
 }
