ccc31b0b3aae2ac401773d53b36f3e508b0864d4
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-22 20:08:00 +0900

    refactor: 컨트롤러 긴 메서드를 작은 메서드들로 분리

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -32,40 +32,55 @@ public class ChessController {
 
     private void inputCommandAndRun(Grid grid) {
         try {
-            String command = InputView.command();
-            if (command.equals(START_COMMAND)) {
-                OutputView.printGridStatus(grid.lines());
-                grid.start();
-                return;
-            }
-            if (command.equals(END_COMMAND)) {
-                grid.end();
-                return;
-            }
-            if (command.startsWith(MOVE_COMMAND)) {
-                List<String> moveInput = Arrays.asList(command.split(WHITE_SPACE_DELIMITER));
-                Position sourcePosition = new Position(moveInput.get(SOURCE_ARG_INDEX));
-                Position targetPosition = new Position(moveInput.get(TARGET_ARG_INDEX));
-                grid.move(grid.piece(sourcePosition), grid.piece(targetPosition));
-                if (grid.isKingCaught()) {
-                    OutputView.printWinner(grid.winnerColor());
-                    return;
-                }
-                OutputView.printGridStatus(grid.lines());
-                return;
-            }
-            if (command.equals(STATUS_COMMAND)) {
-                grid.status();
-                double blackScore = grid.score(Color.BLACK);
-                double whiteScore = grid.score(Color.WHITE);
-                OutputView.printScores(Color.BLACK, blackScore);
-                OutputView.printScores(Color.WHITE, whiteScore);
-                OutputView.printWinner(grid.winnerColor());
-                return;
-            }
-            throw new IllegalArgumentException("잘못된 입력값입니다.");
+            tryToInputCommandAndRun(grid);
+            return;
         } catch (IllegalArgumentException e) {
             System.out.println(e);
         }
+    }
+
+    private void tryToInputCommandAndRun(Grid grid) {
+        String command = InputView.command();
+        if (command.equals(START_COMMAND)) {
+            OutputView.printGridStatus(grid.lines());
+            grid.start();
+            return;
+        }
+        if (command.equals(END_COMMAND)) {
+            grid.end();
+            return;
+        }
+        if (command.startsWith(MOVE_COMMAND)) {
+            runMoveCommand(grid, command);
+            return;
+        }
+        if (command.equals(STATUS_COMMAND)) {
+            runStatusCommand(grid);
+            return;
+        }
+        throw new IllegalArgumentException("잘못된 입력값입니다.");
+    }
+
+    private void runMoveCommand(Grid grid, String command) {
+        List<String> moveInput = Arrays.asList(command.split(WHITE_SPACE_DELIMITER));
+        Position sourcePosition = new Position(moveInput.get(SOURCE_ARG_INDEX));
+        Position targetPosition = new Position(moveInput.get(TARGET_ARG_INDEX));
+        grid.move(grid.piece(sourcePosition), grid.piece(targetPosition));
+        if (grid.isKingCaught()) {
+            OutputView.printWinner(grid.winnerColor());
+            return;
+        }
+        OutputView.printGridStatus(grid.lines());
+        return;
+    }
+
+    private void runStatusCommand(Grid grid) {
+        grid.status();
+        double blackScore = grid.score(Color.BLACK);
+        double whiteScore = grid.score(Color.WHITE);
+        OutputView.printScores(Color.BLACK, blackScore);
+        OutputView.printScores(Color.WHITE, whiteScore);
+        OutputView.printWinner(grid.winnerColor());
+        return;
     }
 }
