c64f10fb43037a67e5bec84228386beb5852ab7f
김주원 <kjw11077@naver.com>
2021-03-22 14:40:44 +0900

    refactor(controller, ChessGame): 컨트롤러와 ChessGame(도메인) 분리

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,10 +1,57 @@
 package chess;
 
 import chess.controller.ChessGame;
+import chess.domain.board.Board;
+import chess.domain.command.Command;
+import chess.domain.gamestate.Ready;
+import chess.exception.ChessException;
+import chess.exception.InvalidCommandException;
+import chess.view.InputView;
+import chess.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        ChessGame chessGame = new ChessGame();
-        chessGame.start();
+        OutputView.startGame();
+        ChessGame chessGame = new ChessGame(new Ready(Board.createGamingBoard()));
+
+        if (command().isStart()) {
+            chessGame.start();
+            printBoard(chessGame);
+            runGame(chessGame);
+        }
+    }
+
+    private static void runGame(ChessGame chessGame) {
+        while (!chessGame.state().isFinished()) {
+            turn(chessGame, command());
+            printBoard(chessGame);
+        }
+    }
+
+    private static void turn(ChessGame chessGame, Command command) {
+        try {
+            chessGame.execute(command);
+        } catch (ChessException e) {
+            OutputView.printError(e);
+            turn(chessGame, command());
+        }
+    }
+
+    private static void printBoard(ChessGame chessGame) {
+        OutputView.printBoard(chessGame);
+
+        // TODO 디미터 .없애기
+        if (chessGame.state().isGameSet()) {
+            OutputView.printWinner(chessGame.winner());
+        }
+    }
+
+    private static Command command() {
+        try {
+            return Command.from(InputView.command());
+        } catch (InvalidCommandException e) {
+            OutputView.printError(e);
+            return command();
+        }
     }
 }
--- src/main/java/chess/controller/ChessGame.java
+++ src/main/java/chess/controller/ChessGame.java
@@ -1,76 +1,51 @@
 package chess.controller;
 
+import chess.domain.Side;
 import chess.domain.board.Board;
 import chess.domain.command.Command;
-import chess.domain.gamestate.Ready;
 import chess.domain.gamestate.State;
-import chess.exception.ChessException;
 import chess.exception.InvalidCommandException;
-import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessGame {
+    private State state;
+
+    public ChessGame(State state) {
+        this.state = state;
+    }
+
     public void start() {
-        OutputView.startGame();
-        State state = new Ready(Board.createGamingBoard());
-        Command command = command();
-        if (command.isStart()) {
-            startGame(state);
-        }
+        state = state.start();
     }
 
-    private void startGame(State initialState) {
-        State state = initialState.start();
-        printBoard(state);
-
-        while (!state.isFinished()) {
-            state = turn(state);
-        }
-    }
-
-    private void printBoard(State state) {
-        OutputView.print(state.board(), state.side());
-
-        if (state.isGameSet()) {
-            OutputView.printWinner(state.winner());
-        }
-    }
-
-    private State turn(State state) {
-        try {
-            state = execute(command(), state);
-            printBoard(state);
-        } catch (ChessException e) {
-            OutputView.printError(e);
-            return turn(state);
-        }
-        return state;
-    }
-
-    private State execute(Command command, State state) {
-
+    public void execute(Command command) {
         if (command.isMove()) {
             state = state.move(command.source(), command.target());
         }
-
         if (command.isStatus()) {
             state = state.status();
             OutputView.print(state.score());
-            return state;
         }
-
         if (command.isEnd()) {
             state = state.finished();
         }
+
+        throw new InvalidCommandException();
+    }
+
+    public State state() {
         return state;
     }
 
-    private Command command() {
-        try {
-            return Command.from(InputView.command());
-        } catch (InvalidCommandException e) {
-            OutputView.printError(e);
-            return command();
-        }
+    public Board board() {
+        return state.board();
+    }
+
+    public Side currentTurn() {
+        return state.currentTurn();
+    }
+
+    public Side winner() {
+        return state.winner();
     }
 }
