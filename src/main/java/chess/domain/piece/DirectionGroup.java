e7eabfe91f2b4adc7c8fa0c9186548e35b4e935c
Seed <hsik0225@gmail.com>
2021-03-22 22:48:02 +0900

    refactor(direction-group):findDirectionOfPawn을 findDirection 메소드와 결합

--- src/main/java/chess/domain/piece/DirectionGroup.java
+++ src/main/java/chess/domain/piece/DirectionGroup.java
@@ -1,15 +1,10 @@
 package chess.domain.piece;
 
-import chess.domain.Color;
-import chess.domain.position.MovePosition;
+import chess.domain.move.MovePosition;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DirectionGroup {
-    
-    private static final int CANNOT_MOVE_LENGTH = 0;
-    private static final int MAX_MOVABLE_LENGTH_OF_PAWN = 2;
     
     private static final String ERROR_CAN_NOT_MOVE = "기물이 이동할 수 없는 위치입니다.";
     
@@ -21,25 +16,10 @@ public class DirectionGroup {
         this.movableLength = movableLength;
     }
     
-    public static DirectionGroup createBlankDirectionGroup() {
-        return new DirectionGroup(new ArrayList<>(), CANNOT_MOVE_LENGTH);
-    }
-    
     public Direction findDirection(MovePosition movePosition) {
         return directionGroup.stream()
                              .filter(direction -> movePosition.canMove(direction, movableLength))
                              .findAny()
                              .orElseThrow(() -> new IllegalArgumentException(ERROR_CAN_NOT_MOVE));
-    }
-    
-    public Direction findDirectionOfPawn(MovePosition movePosition, Color color) {
-        final Direction forwardDirection = color.getPawnForwardDirection();
-        final boolean isAtDefaultPosition = movePosition.isAtDefaultPawnPosition(color);
-        final boolean canMoveTwoSteps = movePosition.canMove(forwardDirection, MAX_MOVABLE_LENGTH_OF_PAWN);
-        
-        if (isAtDefaultPosition && canMoveTwoSteps) {
-            return forwardDirection;
-        }
-        return findDirection(movePosition);
     }
 }
