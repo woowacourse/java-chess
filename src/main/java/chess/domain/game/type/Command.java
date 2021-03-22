aaeabd53d03b34b760ad0feb6e1ff04f256e0e72
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-22 11:34:59 +0900

    refactor: 불필요한 함수 분리 제거

--- src/main/java/chess/domain/game/type/Command.java
+++ src/main/java/chess/domain/game/type/Command.java
@@ -16,12 +16,8 @@ public enum Command {
 
     public static Command of(String commandInput) {
         return Arrays.stream(Command.values())
-            .filter(command -> command.hasEqualValue(commandInput))
+            .filter(command -> command.value.equals(commandInput))
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException("명령어를 잘못 입력했습니다."));
-    }
-
-    private boolean hasEqualValue(String value) {
-        return this.value.equals(value);
     }
 }
