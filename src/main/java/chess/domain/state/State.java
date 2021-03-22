b12a81c3b9d9ede0bc2464ce4a58d6ae5dc5858f
멍토 <daum7766@naver.com>
2021-03-22 16:24:04 +0900

    refactor: 상태인터페이스 수정

--- src/main/java/chess/domain/state/State.java
+++ src/main/java/chess/domain/state/State.java
@@ -2,18 +2,22 @@ package chess.domain.state;
 
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import chess.domain.piece.Position;
+import java.util.Map;
 
 public interface State {
 
     boolean isSameColor(final Piece piece);
 
-    State nextState();
-
     boolean isFinish();
-
-    void nextTurn();
 
     Color winner();
 
-    void validateMove();
+    State init();
+
+    double score(Color color);
+
+    Map<Position, Piece> pieces();
+
+    State movePiece(final Position sourcePosition, final Position targetPosition);
 }
