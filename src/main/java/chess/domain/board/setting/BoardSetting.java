3cc8d5151b95cab302dc690be19fbdf7f32a5ac3
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-22 23:14:05 +0900

    refactor: 생성자 접근제한자 수정

--- src/main/java/chess/domain/board/setting/BoardSetting.java
+++ src/main/java/chess/domain/board/setting/BoardSetting.java
@@ -9,7 +9,7 @@ public abstract class BoardSetting {
 
     private final List<PieceWithColorType> pieces = new ArrayList<>();
 
-    public BoardSetting(List<PieceWithColorType> piecesSetting) {
+    BoardSetting(List<PieceWithColorType> piecesSetting) {
         validate(piecesSetting);
         this.pieces.addAll(piecesSetting);
     }
