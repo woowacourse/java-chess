e7fbc0e70745e1889fc5b777cf7aa0442f1c3ceb
bepoz <kangsy763@naver.com>
2021-03-22 17:09:58 +0900

    feature: 점수계산 테스트코드 추가

--- src/test/java/chess/domain/board/BoardTest.java
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -1,32 +1,34 @@
 package chess.domain.board;
 
-import chess.domain.piece.Bishop;
-import chess.domain.piece.Pawn;
+import chess.domain.piece.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BoardTest {
+    Board board = BoardFactory.create();
+
     @DisplayName("보드가 초기화 되었을 때 체스말이 없는 곳은 null 값을 가진다.")
     @Test
     void nullPositionTest() {
-        Board board = BoardFactory.create();
         assertThat(board.findPieceFromPosition(Position.of(Horizontal.C, Vertical.SIX))).isNull();
     }
 
     @DisplayName("보드가 생성되고 체스말의 위치가 올바른지 확인한다.")
     @Test
     void boardCreateTest() {
-        Board board = BoardFactory.create();
         assertThat(board.findPieceFromPosition(Position.of(Horizontal.C, Vertical.EIGHT))).isInstanceOf(Bishop.class);
     }
 
     @Test
     @DisplayName("기물 이동 수행")
     void movePieceTest() {
-        Board board = BoardFactory.create();
         board.movePiece(Position.of(Horizontal.A, Vertical.TWO),
                 Position.of(Horizontal.A, Vertical.FOUR));
         assertThat(board.getBoard().get(Position.of(Horizontal.A, Vertical.TWO))).isNull();
@@ -36,10 +38,27 @@ class BoardTest {
     @Test
     @DisplayName("이동 범위 외 기물 이동 수행")
     void movePieceExceptionTest() {
-        Board board = BoardFactory.create();
         assertThatThrownBy(() -> {
             board.movePiece(Position.of(Horizontal.A, Vertical.TWO),
                     Position.of(Horizontal.A, Vertical.FIVE));
         }).isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("기본 체스판의 점수는 각 38점이다")
+    void calculateScore() {
+        assertThat(board.calculateScore(Team.BLACK)).isEqualTo(38);
+    }
+
+    @Test
+    @DisplayName("기물을 잃었을 때와 한 줄에 2개의 폰인 경우 점수 확인")
+    void calculateScoreWhenPieceIsDead() {
+        board.movePiece(Position.of(Horizontal.B, Vertical.TWO), Position.of(Horizontal.B, Vertical.FOUR));
+        board.movePiece(Position.of(Horizontal.B, Vertical.FOUR), Position.of(Horizontal.B, Vertical.FIVE));
+        board.movePiece(Position.of(Horizontal.B, Vertical.FIVE), Position.of(Horizontal.B, Vertical.SIX));
+        board.movePiece(Position.of(Horizontal.B, Vertical.SIX), Position.of(Horizontal.C, Vertical.SEVEN));
+
+        assertThat(board.calculateScore(Team.WHITE)).isEqualTo(37);
+        assertThat(board.calculateScore(Team.BLACK)).isEqualTo(37);
     }
 }
