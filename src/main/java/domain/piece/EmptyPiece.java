d735f721b3102b6113b79b3163eb71efdd58649c
jujubebat <bbwwpark@naver.com>
2021-03-22 22:12:46 +0900

    feat: 빈 공간을 나타내는 체스 기물 구현

---
+++ src/main/java/domain/piece/EmptyPiece.java
@@ -0,0 +1,28 @@
+package domain.piece;
+
+import domain.board.Board;
+import domain.position.Position;
+
+public class EmptyPiece extends Piece {
+
+    private static final String NAME = ".";
+
+    public EmptyPiece() {
+        super(NAME);
+    }
+
+    @Override
+    public boolean canMove(Board board, Position source, Position target) {
+        return false;
+    }
+
+    @Override
+    public boolean isNotEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return true;
+    }
+}
