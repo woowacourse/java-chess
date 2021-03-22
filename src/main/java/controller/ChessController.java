0e99fd7f17e4e3fa0134ea5338786460c52d44ab
jujubebat <bbwwpark@naver.com>
2021-03-22 22:13:13 +0900

    refactor: 체스 컨트롤러 구현

--- src/main/java/controller/ChessController.java
+++ src/main/java/controller/ChessController.java
@@ -1,12 +1,44 @@
 package controller;
 
-import domain.Board;
-import domain.PieceFactory;
+import domain.chessgame.ChessGame;
+import domain.position.Position;
+import java.util.List;
+import view.InputView;
 import view.OutputView;
 
 public class ChessController {
+
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
+    private static final String MOVE_COMMAND = "move";
+    private static final String STATUS_COMMAND = "status";
+
     public void run() {
-        Board board = new Board(PieceFactory.createPieces());
-        OutputView.showBoard(board);
+        try {
+            if (!InputView.command().equals(START_COMMAND)) {
+                throw new IllegalArgumentException();
+            }
+            startChessGame();
+        } catch (Exception e) {
+            run();
+        }
     }
+
+    private void startChessGame() {
+        ChessGame chessGame = new ChessGame();
+        while (chessGame.isNotEnd()) {
+            OutputView.printBoard(chessGame.getBoard());
+            List<String> command = InputView.moveCommand();
+            if (command.get(0).equals(END_COMMAND)) {
+                return;
+            }
+            if (command.get(0).equals(STATUS_COMMAND)) {
+                OutputView.printScore(chessGame.getBoard());
+            }
+            if (command.get(0).equals(MOVE_COMMAND)) {
+                chessGame.move(new Position(command.get(1)), new Position(command.get(2)));
+            }
+        }
+    }
+
 }
