309c226579e4b0873bac6c7ed5026ebe2fda2475
perenok <perenoke@gmail.com>
2021-03-22 14:59:48 +0900

    refactor(view) : 정적 클래스에 private 생성자 추가

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -6,6 +6,8 @@ public class InputView {
 
     private static final Scanner SCAN = new Scanner(System.in);
 
+    private InputView(){}
+
     public static String option() {
         return SCAN.nextLine();
     }
