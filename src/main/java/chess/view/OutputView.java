b683169347453ba626872baec203f0ba4cbdfd4f
DWL5 <ssop6403@gmail.com>
2021-03-23 14:24:29 +0900

    refactor: 불필요한 문자열 상수화 제거

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -5,12 +5,6 @@ import chess.domain.Position;
 import java.util.Map;
 
 public class OutputView {
-    private static final String CHESS_INIT_MESSAGE = "> 체스 게임을 시작합니다.";
-    private static final String CHESS_START_GUIDE_MESSAGE = "> 게임 시작 : start";
-    private static final String CHESS_END_GUIDE_MESSAGE = "> 게임 종료 : end";
-    private static final String CHESS_MOVE_GUIDE_MESSAGE = "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
-    private static final String BLACK_TEAM_SCORE = "블랙팀 점수: ";
-    private static final String WHITE_TEAM_SCORE = "화이트팀 점수: ";
     public static final int BOARD_START = 0;
     public static final int BOARD_END = 7;
 
@@ -18,10 +12,10 @@ public class OutputView {
     }
 
     public static void printChessStartMessage() {
-        System.out.println(CHESS_INIT_MESSAGE);
-        System.out.println(CHESS_START_GUIDE_MESSAGE);
-        System.out.println(CHESS_END_GUIDE_MESSAGE);
-        System.out.println(CHESS_MOVE_GUIDE_MESSAGE);
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
     public static void printChessBoard(final Map<Position, String> chessBoard) {
@@ -38,7 +32,7 @@ public class OutputView {
     }
 
     public static void printChessGameResult(final double blackTeamScore, final double whiteTeamScore) {
-        System.out.println(BLACK_TEAM_SCORE + blackTeamScore);
-        System.out.println(WHITE_TEAM_SCORE + whiteTeamScore);
+        System.out.println("블랙팀 점수: " + blackTeamScore);
+        System.out.println("화이트팀 점수: " + whiteTeamScore);
     }
 }
