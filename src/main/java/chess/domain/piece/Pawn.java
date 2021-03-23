c04ae83f29f6087765021b36c6eaa54834df61a1
Seed <hsik0225@gmail.com>
2021-03-23 16:59:13 +0900

    feat(pawn): 폰은 첫 이동시에만 2칸 움직이게 하는 기능 추가

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -6,14 +6,14 @@ import chess.domain.position.MovePosition;
 
 public class Pawn extends AbstractPiece {
     
-    private static final int MOVABLE_LENGTH = 1;
+    private static final int MOVABLE_LENGTH = 2;
     public static final Pawn BLACK_INSTANCE
             = new Pawn(Color.BLACK, new DirectionGroup(Direction.blackPawnDirection(), MOVABLE_LENGTH));
     public static final Pawn WHITE_INSTANCE
             = new Pawn(Color.WHITE, new DirectionGroup(Direction.whitePawnDirection(), MOVABLE_LENGTH));
     
     private static final String SYMBOL = "p";
-    public static final double SCORE = 1;
+    private static final double SCORE = 1;
     
     private static final String ERROR_PIECE_EXIST_AT_FORWARD_TARGET = "폰이 전진하는 위치에 기물이 있으면 안됩니다.";
     private static final String ERROR_PIECE_DOES_NOT_EXISTS_AT_DIAGONAL_TARGET = "폰은 대각선으로 이동하기 위해서는 상대방의 기물이 있어야 합니다.";
@@ -32,13 +32,17 @@ public class Pawn extends AbstractPiece {
     
     @Override
     public void checkToMoveToTargetPosition(MovePosition movePosition, Board board) {
-        Direction direction = directionGroup.findDirectionOfPawn(movePosition, color);
+        Direction direction = directionGroup.findDirection(movePosition);
         checkObstacleExistsAtDirection(movePosition, direction, board);
         
         final boolean isForwardDirection = isForward(direction);
         final boolean isBlankAtTargetPosition = board.isBlank(movePosition.getTargetPosition());
         if (isForwardDirection && !isBlankAtTargetPosition) {
             throw new IllegalArgumentException(ERROR_PIECE_EXIST_AT_FORWARD_TARGET);
+        }
+        
+        if (isForwardDirection && !movePosition.isAtDefaultPawnPosition(color)) {
+            throw new IllegalArgumentException("폰은 첫 이동 시에만 2칸 움직일 수 있습니다.");
         }
         
         if (!isForwardDirection && isBlankAtTargetPosition) {
