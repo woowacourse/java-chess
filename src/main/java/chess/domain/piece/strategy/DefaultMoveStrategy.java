6cf17593fd0ce2b21041f611ef748444b81a2957
Kimun Kim <kkm97351@gmail.com>
2021-03-24 19:39:23 +0900

    refactor: DefaultMoveStrategy 분기 메소드 명으로 분리 및 명시

--- src/main/java/chess/domain/piece/strategy/DefaultMoveStrategy.java
+++ src/main/java/chess/domain/piece/strategy/DefaultMoveStrategy.java
@@ -15,31 +15,34 @@ public abstract class DefaultMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean canMove(MoveOrder moveOrder) {
-        if (!isProperDirection(moveOrder.getDirection())) {
-            throw new IllegalArgumentException("움직일 수 없는 방향입니다.");
-        }
+        validateProperDirection(moveOrder);
+        validateRouteIsNotBlocked(moveOrder.getRoute());
+        validateToSquareHasSameColorOfPiece(moveOrder);
 
-        validateBlockedRoute(moveOrder.getRoute());
-
-        if (moveOrder.getToSquare().hasPiece()) {
-            validateSameColorPiece(moveOrder);
-        }
         return true;
     }
 
-    private boolean isProperDirection(Direction direction) {
-        return movableDirections.contains(direction);
+    private void validateProperDirection(MoveOrder moveOrder) {
+        if (!movableDirections.contains(moveOrder.getDirection())) {
+            throw new IllegalArgumentException("움직일 수 없는 방향입니다.");
+        }
     }
 
-    private void validateBlockedRoute(List<Square> route) {
+    private void validateRouteIsNotBlocked(List<Square> route) {
         boolean blocked = route.stream()
                 .anyMatch(Square::hasPiece);
-
         if (blocked) {
             throw new IllegalArgumentException("중간에 말이 있어 행마할 수 없습니다.");
         }
     }
 
+    private void validateToSquareHasSameColorOfPiece(MoveOrder moveOrder) {
+        if (moveOrder.getToSquare().hasPiece()) {
+            validateSameColorPiece(moveOrder);
+        }
+    }
+
+    // TODO 체이닝 간소화
     private void validateSameColorPiece(MoveOrder moveOrder) {
         if (moveOrder.getFromSquare().getPiece().isSameColor(moveOrder.getToSquare().getPiece())) {
             throw new IllegalArgumentException("동일한 진영의 말이 있어서 행마할 수 없습니다.");
