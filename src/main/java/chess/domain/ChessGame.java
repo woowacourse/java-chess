7126c4b6c31e62c8ad30229fab6a75bef70da610
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-03-24 19:50:54 +0900

    refactor: checkMate 메서드명 finishGameIfKing으로 변경

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -82,7 +82,7 @@ public final class ChessGame {
         if (enemyTeam.havePiece(destination)) {
             final Piece enemyPiece = enemyTeam.deletePiece(destination);
             currentTurnTeam.catchPiece(enemyPiece);
-            checkMate(enemyPiece);
+            finishGameIfKing(enemyPiece);
         }
     }
 
@@ -93,7 +93,7 @@ public final class ChessGame {
         return blackTeam;
     }
 
-    private void checkMate(final Piece piece) {
+    private void finishGameIfKing(final Piece piece) {
         if (piece.isKing()) {
             finish();
         }
