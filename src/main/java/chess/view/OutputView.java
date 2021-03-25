dfce2cf336ed849ae7193a73ac83248312769381
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-03-25 21:41:20 +0900

    refactor: 체스 보드 출력시 보드 번호가 보이도록 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,5 +1,6 @@
 package chess.view;
 
+import chess.domain.board.ChessBoard;
 import chess.domain.board.Position;
 import chess.domain.piece.Piece;
 import chess.domain.result.ResultDto;
@@ -16,6 +17,12 @@ public class OutputView {
     private static final String SKELETON_RESULT_WHITE_SCORE = "WHITE POINT: %s";
     private static final String SKELETON_RESULT_BLACK_SCORE = "BLACK POINT: %s";
     private static final String WINNER = "승자: %s";
+    public static final int CHESS_BOARD_ROW_UP = 7;
+    public static final int CHESS_BOARD_ROW_DOWN = 0;
+    public static final int CHESS_BOARD_COLUMN_LEFT = 0;
+    public static final int CHESS_BOARD_COLUMN_RIGHT = 8;
+    public static final int ONE_ROW_LINE_SIZE = 8;
+    public static final int PRINT_NUMBER_CORRECTION = 1;
 
     private OutputView() {
     }
@@ -25,12 +32,26 @@ public class OutputView {
             .map(Piece::getPieceName)
             .collect(Collectors.toList());
 
-        for (int i = 7; i >= 0; i--) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(chessBoardNames.get(i * 8 + j));
+        printAllChessBoard(chessBoardNames);
+        printRowLineNames();
+    }
+
+    private static void printAllChessBoard(List<String> chessBoardNames) {
+        for (int row = CHESS_BOARD_ROW_UP; row >= CHESS_BOARD_ROW_DOWN; row--) {
+            for (int column = CHESS_BOARD_COLUMN_LEFT; column < CHESS_BOARD_COLUMN_RIGHT;
+                column++) {
+                System.out.print(chessBoardNames.get(row * ONE_ROW_LINE_SIZE + column));
             }
-            System.out.println();
+            System.out.println("  " + (row + PRINT_NUMBER_CORRECTION));
         }
+        System.out.println();
+    }
+
+    private static void printRowLineNames() {
+        for (char row = ChessBoard.COLUMN_FIRST; row <= ChessBoard.COLUMN_LAST; row++) {
+            System.out.print(row);
+        }
+        System.out.println();
     }
 
     public static void printInitMessage() {
