6564a841ff3f916c36fde0502370f183b43cb8f8
Seed <hsik0225@gmail.com>
2021-03-25 17:39:09 +0900

    feat(input-view):입력 반환값을 String으로 수정

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,26 +1,12 @@
 package chess.view;
 
-import chess.controller.command.CommandController;
-import chess.controller.command.CommandControllerFactory;
-
 import java.util.Scanner;
 
 public class InputView {
     
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String BLANK = " ";
-    private static final int COMMAND_INDEX = 0;
     
-    private static final String ERROR_COMMAND_CANNOT_FIND = "메뉴에 없는 커맨드입니다. 다시 입력해주세요.";
-    
-    public static CommandController askCommand() {
-        String[] input = SCANNER.nextLine().split(BLANK);
-        while (!CommandControllerFactory.hasCommand(input[COMMAND_INDEX])) {
-            System.out.println(ERROR_COMMAND_CANNOT_FIND);
-            input = SCANNER.nextLine().split(BLANK);
-        }
-        
-        System.out.println();
-        return CommandControllerFactory.createFrom(input);
+    public static String askCommand() {
+        return SCANNER.nextLine();
     }
 }
