b697952d35404a260cec4f43603f6c9ce9e9cb8a
yoonsung.jung <ybook2012@gmail.com>
2021-03-25 15:12:22 +0900

    feat(OutputView) : 게임 이동 커멘드 메시지 출력

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -15,9 +15,10 @@ public class OutputView {
     }
 
     public static void printStartMessage() {
-        System.out.println("> 체스 게임을 시작합니다.");
-        System.out.println("> 게임 시작 : start");
-        System.out.println("> 게임 종료 : end");
+        System.out.println("> 체스 게임을 시작합니다." );
+        System.out.println("> 게임 시작 : start" );
+        System.out.println("> 게임 종료 : end" );
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3" );
     }
 
     public static void printBoard(final Board board) {
@@ -54,7 +55,7 @@ public class OutputView {
         String[][] arr = new String[8][8];
 
         for (int i = 0; i < 8; ++i) {
-            Arrays.fill(arr[i], ".");
+            Arrays.fill(arr[i], "." );
         }
         return arr;
     }
@@ -72,6 +73,6 @@ public class OutputView {
 
     public static void printWinner(final Team winner, final Double blackTeamScore, final Double whiteTeamScore) {
         printEachTeamScore(blackTeamScore, whiteTeamScore);
-        System.out.println("승자는 " + winner.name() + "팀 입니다.");
+        System.out.println("승자는 " + winner.name() + "팀 입니다." );
     }
 }
