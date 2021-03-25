8e57214db875de211bfd0a3f2b6889e814fdf7a0
김주원 <kjw11077@naver.com>
2021-03-25 20:07:10 +0900

    refactor(GamePiece): route메서드 수정

--- src/main/java/chess/domain/piece/GamePiece.java
+++ src/main/java/chess/domain/piece/GamePiece.java
@@ -19,24 +19,38 @@ public abstract class GamePiece implements Piece {
 
     @Override
     public final List<Position> route(Position from, Position to, Piece targetPiece, Side side) {
-        if (!isSideEqualTo(side)) {
-            throw new InvalidMovementException("자신의 기물만 움직일 수 있습니다.");
-        }
-        if (Objects.equals(from, to)) {
-            throw new InvalidMovementException("자기 자신의 위치로는 이동할 수 없습니다.");
-        }
-        if (targetPiece.isSideEqualTo(side)) {
-            throw new InvalidMovementException("이동하려는 위치에 자신의 기물이 존재합니다.");
-        }
+        checkMyPiece(side);
+        checkNotMyPosition(from, to);
+        checkMyPieceNotExistTargetPosition(targetPiece, side);
+        checkMovable(from, to, targetPiece);
+        return getRoute(from, to);
+    }
 
+    private void checkMovable(Position from, Position to, Piece targetPiece) {
         int rowDifference = Position.differenceOfRow(from, to);
         int columnDifference = Position.differenceOfColumn(from, to);
 
-        if (movable(rowDifference, columnDifference, targetPiece)) {
-            return getRoute(from, to);
+        if (!movable(rowDifference, columnDifference, targetPiece)) {
+            throw new InvalidMovementException("해당 기물의 이동 룰에 어긋납니다.");
         }
+    }
 
-        throw new InvalidMovementException("해당 기물의 이동 룰에 어긋납니다.");
+    private void checkMyPieceNotExistTargetPosition(Piece targetPiece, Side side) {
+        if (targetPiece.isSideEqualTo(side)) {
+            throw new InvalidMovementException("이동하려는 위치에 자신의 기물이 존재합니다.");
+        }
+    }
+
+    private void checkNotMyPosition(Position from, Position to) {
+        if (Objects.equals(from, to)) {
+            throw new InvalidMovementException("자기 자신의 위치로는 이동할 수 없습니다.");
+        }
+    }
+
+    private void checkMyPiece(Side side) {
+        if (!isSideEqualTo(side)) {
+            throw new InvalidMovementException("자신의 기물만 움직일 수 있습니다.");
+        }
     }
 
     protected abstract boolean movable(int rowDifference, int columnDifference, Piece targetPiece);
