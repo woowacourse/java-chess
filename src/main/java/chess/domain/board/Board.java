e70940b09435fc2512028b4cb5bd1bde43d2acb7
hyeon9mak <jinha3507@gmail.com>
2021-03-25 14:17:03 +0900

    refactor: fix check alive both kings method

--- src/main/java/chess/domain/board/Board.java
+++ src/main/java/chess/domain/board/Board.java
@@ -4,7 +4,6 @@ import chess.domain.board.position.Position;
 import chess.domain.movestrategy.MoveStrategy;
 import chess.domain.piece.Empty;
 import chess.domain.piece.Piece;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -12,25 +11,33 @@ import java.util.Set;
 public class Board {
 
     public static final int BOTH_KINGS_ALIVE = 2;
-    private final List<Map<Position, Piece>> squares;
     private final List<Rank> ranks;
 
     public Board(final List<Rank> ranks) {
-        this(ranks, new ArrayList<>());
-    }
-
-    public Board(final List<Rank> ranks, final List<Map<Position, Piece>> squares) {
         this.ranks = ranks;
-        this.squares = squares;
     }
 
     public List<Rank> ranks() {
         return this.ranks;
     }
 
-    public List<Map<Position, Piece>> squares() {
-        return squares;
+    public Piece pieceByPosition(Position position) {
+        return this.ranks.stream()
+            .filter(rank -> rank.hasPosition(position))
+            .map(map -> map.piece(position))
+            .findFirst()
+            .orElseThrow(IllegalArgumentException::new);
     }
+
+    public boolean isAliveBothKings() {
+        return this.ranks
+            .stream()
+            .flatMap(rank -> rank.squares().stream())
+            .filter(Piece::isKing)
+            .count() == BOTH_KINGS_ALIVE;
+    }
+
+    //--------------------- refactor line ------------------------------------------
 
     public void moveIfValidPosition(Position source, Position target) {
         if (isNotMovablePosition(source, target)) {
@@ -46,14 +53,6 @@ public class Board {
         return !movablePath.contains(target);
     }
 
-    public Piece pieceByPosition(Position position) {
-        return this.ranks.stream()
-            .filter(rank -> rank.hasPosition(position))
-            .map(map -> map.piece(position))
-            .findFirst()
-            .orElseThrow(IllegalArgumentException::new);
-    }
-
     private void swapPieces(Position source, Position target) {
         Piece sourcePiece = pieceByPosition(source);
         replacePiece(source, Empty.create());
@@ -61,21 +60,12 @@ public class Board {
     }
 
     private void replacePiece(Position position, Piece piece) {
-        for (Map<Position, Piece> map : squares) {
-            replaceIfValidKey(position, piece, map);
-        }
+
     }
 
     private void replaceIfValidKey(Position position, Piece piece, Map<Position, Piece> map) {
         if (map.containsKey(position)) {
             map.replace(position, piece);
         }
-    }
-
-    public boolean isAliveBothKings() {
-        return squares.stream()
-            .flatMap(map -> map.values().stream())
-            .filter(Piece::isKing)
-            .count() == BOTH_KINGS_ALIVE;
     }
 }
--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -5,14 +5,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import chess.domain.board.Board;
 import chess.domain.board.Rank;
 import chess.domain.board.position.Position;
+import chess.domain.piece.Bishop;
+import chess.domain.piece.King;
 import chess.domain.piece.Knight;
 import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
+import chess.domain.piece.Queen;
 import chess.domain.piece.Rook;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,11 +29,14 @@ public class BoardTest {
 
     @BeforeEach
     void setUp() {
-        Map<Position, Piece> squares1 = new HashMap<>();
+        Map<Position, Piece> squares1 = new LinkedHashMap<>();
         squares1.put(Position.of("a1"), Rook.createWhite());
         squares1.put(Position.of("b1"), Knight.createWhite());
+        squares1.put(Position.of("c1"), Bishop.createWhite());
+        squares1.put(Position.of("d1"), Queen.createWhite());
+        squares1.put(Position.of("e1"), King.createWhite());
 
-        Map<Position, Piece> squares2 = new HashMap<>();
+        Map<Position, Piece> squares2 = new LinkedHashMap<>();
         squares2.put(Position.of("a2"), Pawn.createWhite());
         squares2.put(Position.of("b2"), Pawn.createWhite());
 
@@ -62,10 +68,35 @@ public class BoardTest {
     @DisplayName("체스보드 출력을 위한 전체 보드 반환")
     void testGetRanks() {
         assertThat(this.board.ranks()).hasSize(2);
-        Rank rank1 = this.board.ranks().get(0);
-        Rank rank2 = this.board.ranks().get(1);
 
-        assertThat(rank1.squares()).containsExactly(Rook.createWhite(), Knight.createWhite());
+        Rank rank1 = this.board.ranks().get(0);
+        assertThat(rank1.squares()).containsExactly(
+            Rook.createWhite(), Knight.createWhite(), Bishop.createWhite(),
+            Queen.createWhite(), King.createWhite()
+        );
+
+        Rank rank2 = this.board.ranks().get(1);
         assertThat(rank2.squares()).containsExactly(Pawn.createWhite(), Pawn.createWhite());
+    }
+
+    @Test
+    @DisplayName("현재 체스보드에 양쪽 King이 살아있는지 확인")
+    void testIsAliveBothKings() {
+        assertThat(this.board.isAliveBothKings()).isFalse();
+
+        Map<Position, Piece> squares1 = new HashMap<>();
+        squares1.put(Position.of("e8"), King.createBlack());
+        Map<Position, Piece> squares2 = new HashMap<>();
+        squares2.put(Position.of("e1"), King.createWhite());
+
+        List<Rank> ranks = new ArrayList<>(Arrays.asList(
+            new Rank(squares1),
+            new Rank(squares2)
+        ));
+
+        this.board = new Board(ranks);
+
+        assertThat(this.board.isAliveBothKings()).isTrue();
+
     }
 }
