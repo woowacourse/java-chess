59bf8b8686819ca0ae5087eb85172da37fed5160
hyeon9mak <jinha3507@gmail.com>
2021-03-25 17:21:46 +0900

    refactor: delete unuseful try-catch keyword in Commands

--- src/main/java/chess/domain/command/Commands.java
+++ src/main/java/chess/domain/command/Commands.java
@@ -1,11 +1,11 @@
 package chess.domain.command;
 
 import chess.domain.game.ChessGame;
-
 import java.util.Arrays;
 import java.util.List;
 
 public class Commands {
+
     private final List<Command> commands;
 
     private Commands(List<Command> commands) {
@@ -14,21 +14,17 @@ public class Commands {
 
     public static Commands initCommands(ChessGame chessGame) {
         return new Commands(Arrays.asList(
-                new Start(chessGame),
-                new Move(chessGame),
-                new End(chessGame),
-                new Status(chessGame)
+            new Start(chessGame),
+            new Move(chessGame),
+            new End(chessGame),
+            new Status(chessGame)
         ));
     }
 
     public Command matchedCommand(String text) {
-        try {
-            return commands.stream()
-                    .filter(command -> command.isMatchedCommand(text))
-                    .findFirst()
-                    .orElseThrow(IllegalArgumentException::new);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalStateException("수행할 수 없는 명령어 입니다.");
-        }
+        return this.commands.stream()
+            .filter(command -> command.isMatchedCommand(text))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("수행할 수 없는 명령어 입니다."));
     }
 }
