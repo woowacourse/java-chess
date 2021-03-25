51cf33a7b127f63a6dbce8eb5446346e56c43263
hyeon9mak <jinha3507@gmail.com>
2021-03-25 13:00:59 +0900

    feat: create new object to horizental of chess board

---
+++ src/main/java/chess/domain/board/Rank.java
@@ -0,0 +1,28 @@
+package chess.domain.board;
+
+import chess.domain.board.position.Position;
+import chess.domain.piece.Piece;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Rank {
+
+    private final Map<Position, Piece> squares;
+
+    public Rank(final Map<Position, Piece> squares) {
+        this.squares = squares;
+    }
+
+    public boolean hasPosition(final Position position) {
+        return this.squares.containsKey(position);
+    }
+
+    public Piece piece(Position position) {
+        return this.squares.get(position);
+    }
+
+    public List<Piece> squares() {
+        return new ArrayList<>(this.squares.values());
+    }
+}
---
+++ src/test/java/chess/domain/board/RankTest.java
@@ -0,0 +1,55 @@
+package chess.domain.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.board.position.Position;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Piece;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RankTest {
+
+    private Rank rank;
+
+    @BeforeEach
+    void setUp() {
+        Map<Position, Piece> map = new HashMap<>();
+        map.put(Position.of("a7"), Pawn.createBlack());
+        map.put(Position.of("b7"), Pawn.createBlack());
+        map.put(Position.of("c7"), Pawn.createBlack());
+        this.rank = new Rank(map);
+    }
+
+    @Test
+    @DisplayName("체스판 가로줄 Rank 생성 테스트")
+    void testCreate() {
+        assertThat(new Rank(new HashMap<>())).isInstanceOf(Rank.class);
+    }
+
+    @Test
+    @DisplayName("체스판 가로줄 Rank에 원하는 좌표가 있는지 확인 테스트")
+    void testFindPosition() {
+        assertThat(this.rank.hasPosition(Position.of("a7"))).isTrue();
+        assertThat(this.rank.hasPosition(Position.of("a8"))).isFalse();
+    }
+
+    @Test
+    @DisplayName("체스판 가로줄 Rank에서 원하는 좌표에 존재하는 체스말 가져오기")
+    void testGetPieceOfPosition() {
+        assertThat(this.rank.piece(Position.of("a7"))).isEqualTo(Pawn.createBlack());
+    }
+
+    @Test
+    @DisplayName("체스판 출력을 위한 squares 반환")
+    void testSquares() {
+        assertThat(this.rank.squares()).containsExactly(
+            Pawn.createBlack(),
+            Pawn.createBlack(),
+            Pawn.createBlack()
+        );
+    }
+}
