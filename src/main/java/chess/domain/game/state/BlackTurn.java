df01b21dda107925bd26109a8321a341af8c640e
hyeon9mak <jinha3507@gmail.com>
2021-03-27 00:28:17 +0900

    refactor: fix BlackTurn state

--- src/main/java/chess/domain/game/state/BlackTurn.java
+++ src/main/java/chess/domain/game/state/BlackTurn.java
@@ -1,8 +1,11 @@
 package chess.domain.game.state;
 
 import chess.domain.board.Board;
+import chess.domain.board.Rank;
 import chess.domain.board.position.Position;
+import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import java.util.List;
 
 public class BlackTurn extends Running {
 
@@ -11,24 +14,63 @@ public class BlackTurn extends Running {
     }
 
     @Override
-    public State passTurn() {
-        return new WhiteTurn(board());
-    }
-
-    @Override
-    public State end() {
-        if (isSeizeOppositeKing()) {
-            return new BlackWin(board());
-        }
-        return new Draw(board());
+    public State start() {
+        throw new IllegalStateException("이미 체스게임이 진행 중 입니다.");
     }
 
     @Override
     public void moveIfValidColor(Position source, Position target) {
         Piece sourcePiece = board().pieceByPosition(source);
+
         if (sourcePiece.isWhite()) {
-            throw new IllegalStateException("검정색 차례엔 검정색 말만 이동 가능합니다.");
+            throw new IllegalStateException("흑색 차례엔 흑색 말만 이동 가능합니다.");
         }
+
         board().moveIfValidPosition(source, target);
+    }
+
+    @Override
+    public State passTurn() {
+        return new WhiteTurn(board());
+    }
+
+    @Override
+    public List<Rank> ranks() {
+        return board().ranks();
+    }
+
+    @Override
+    public String finishReason() {
+        throw new IllegalStateException("아직 체스게임이 진행중 입니다.");
+    }
+
+    @Override
+    public Color winner() {
+        throw new IllegalStateException("아직 체스게임이 진행중 입니다.");
+    }
+
+    @Override
+    public State end() {
+        return new End(board());
+    }
+
+    @Override
+    public boolean isInit() {
+        return false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return true;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public boolean isNotEnd() {
+        return true;
     }
 }
--- src/test/java/chess/domain/game/state/BlackTurnTest.java
+++ src/test/java/chess/domain/game/state/BlackTurnTest.java
@@ -1,7 +1,12 @@
 package chess.domain.game.state;
 
 import chess.domain.board.Board;
-import chess.domain.board.InitBoardGenerator;
+import chess.domain.board.Rank;
+import chess.domain.board.position.Position;
+import chess.domain.piece.Empty;
+import chess.domain.piece.Pawn;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,24 +14,101 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class BlackTurnTest {
-    @Test
-    @DisplayName("블랙 턴 상태에서 start 명령시 에러 반환")
-    void blackTurnStartException() {
-        BlackTurn blackTurn = new BlackTurn(new Board(InitBoardGenerator.initLines()));
-        assertThatThrownBy(blackTurn::start).isInstanceOf(IllegalStateException.class);
+    private BlackTurn blackTurn;
+
+    @BeforeEach
+    void setUp() {
+        this.blackTurn = new BlackTurn(new Board());
     }
 
     @Test
-    @DisplayName("블랙 턴 상태에서 passTurn 명령시 화이트 턴으로 전환")
-    void blackTurnMoveReturnWhiteTurn() {
-        BlackTurn blackTurn = new BlackTurn(new Board(InitBoardGenerator.initLines()));
-        assertThat(blackTurn.passTurn()).isInstanceOf(WhiteTurn.class);
+    @DisplayName("흑색 차례에서 start 명령시 에러 반환")
+    void testStartException() {
+        assertThatThrownBy(() -> this.blackTurn.start()).isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    @DisplayName("블랙 턴 상태에서 end 명령시 Draw 상태 반환")
-    void blackTurnEndReturnEndTurn() {
-        BlackTurn blackTurn = new BlackTurn(new Board(InitBoardGenerator.initLines()));
-        assertThat(blackTurn.end()).isInstanceOf(Draw.class);
+    @DisplayName("흑색 차례에서 흑색 말 move 명령 진행")
+    void testMove() {
+        Board board = this.blackTurn.board();
+
+        assertThat(board.pieceByPosition(Position.of("a7"))).isEqualTo(Pawn.createBlack());
+        assertThat(board.pieceByPosition(Position.of("a5"))).isEqualTo(Empty.create());
+
+        this.blackTurn.moveIfValidColor(Position.of("a7"), Position.of("a5"));
+
+        assertThat(board.pieceByPosition(Position.of("a7"))).isEqualTo(Empty.create());
+        assertThat(board.pieceByPosition(Position.of("a5"))).isEqualTo(Pawn.createBlack());
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 백색 말 move 명령시 예외반환")
+    void testMoveException() {
+        Board board = this.blackTurn.board();
+
+        assertThat(board.pieceByPosition(Position.of("a2"))).isEqualTo(Pawn.createWhite());
+        assertThat(board.pieceByPosition(Position.of("a4"))).isEqualTo(Empty.create());
+
+        assertThatThrownBy(
+            () -> this.blackTurn.moveIfValidColor(Position.of("a2"), Position.of("a4")))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 passTurn 명령시 백색 차례 반환")
+    void testPassTurn() {
+        assertThat(this.blackTurn.passTurn()).isInstanceOf(WhiteTurn.class);
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 ranks 명령시 ranks 반환")
+    void testRanks() {
+        List<Rank> ranks = this.blackTurn.ranks();
+        assertThat(ranks).hasSize(8);
+        for (Rank rank : ranks) {
+            assertThat(rank.squares()).hasSize(8);
+        }
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 finishReason 명령시 에러 반환")
+    void testFinishReasonException() {
+        assertThatThrownBy(() -> this.blackTurn.finishReason()).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 winner 명령시 에러 반환")
+    void testWinnerException() {
+        assertThatThrownBy(() -> this.blackTurn.winner()).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 end 명령시 End 상태 반환")
+    void testEnd() {
+        assertThat(this.blackTurn.end()).isInstanceOf(End.class);
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 isInit 명령시 false 반환")
+    void testIsInitTure() {
+        assertThat(this.blackTurn.isInit()).isFalse();
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 isRunning 명령시 true 반환")
+    void testIsRunningTrue() {
+        assertThat(this.blackTurn.isRunning()).isTrue();
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 isFinished 명령시 false 반환")
+    void testIsFinishedFalse() {
+        assertThat(this.blackTurn.isFinished()).isFalse();
+    }
+
+    @Test
+    @DisplayName("흑색 차례에서 isNotEnd 명령시 true 반환")
+    void testIsNotEndTrue() {
+        assertThat(this.blackTurn.isNotEnd()).isTrue();
     }
 }
