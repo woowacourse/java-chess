5c2c162f7fac2145a11a0bbc25a60e322ec5bebc
hyeon9mak <jinha3507@gmail.com>
2021-03-27 01:01:36 +0900

    refactor: fix WhiteWin state

--- src/main/java/chess/domain/game/state/WhiteWin.java
+++ src/main/java/chess/domain/game/state/WhiteWin.java
@@ -1,7 +1,10 @@
 package chess.domain.game.state;
 
 import chess.domain.board.Board;
+import chess.domain.board.Rank;
+import chess.domain.board.position.Position;
 import chess.domain.piece.Color;
+import java.util.List;
 
 public class WhiteWin extends Finished {
 
@@ -10,12 +13,57 @@ public class WhiteWin extends Finished {
     }
 
     @Override
+    public State start() {
+        return new Init(board());
+    }
+
+    @Override
+    public void moveIfValidColor(Position source, Position target) {
+        throw new IllegalStateException("이미 체스게임이 종료되었습니다.");
+    }
+
+    @Override
+    public State passTurn() {
+        throw new IllegalStateException("이미 체스게임이 종료되었습니다.");
+    }
+
+    @Override
+    public List<Rank> ranks() {
+        throw new IllegalStateException("이미 체스게임이 종료되었습니다.");
+    }
+
+    @Override
     public String finishReason() {
-        return "흰색의 승리로 종료되었습니다.";
+        return "백색의 승리로 게임이 종료되었습니다.";
     }
 
     @Override
     public Color winner() {
         return Color.WHITE;
+    }
+
+    @Override
+    public State end() {
+        return new End(board());
+    }
+
+    @Override
+    public boolean isInit() {
+        return false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return false;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public boolean isNotEnd() {
+        return true;
     }
 }
---
+++ src/test/java/chess/domain/game/state/WhiteWinTest.java
@@ -0,0 +1,90 @@
+package chess.domain.game.state;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import chess.domain.board.Board;
+import chess.domain.board.position.Position;
+import chess.domain.piece.Color;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WhiteWinTest {
+
+    private WhiteWin whiteWin;
+
+    @BeforeEach
+    void setUp() {
+        this.whiteWin = new WhiteWin(new Board());
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 start 명령시 Init 반환")
+    void testStart() {
+        assertThat(this.whiteWin.start()).isInstanceOf(Init.class);
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 move 명령시 예외 반")
+    void testMoveException() {
+        assertThatThrownBy(() ->
+            this.whiteWin.moveIfValidColor(Position.of("a7"), Position.of("a5"))
+        ).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 passTurn 명령시 예외 반환")
+    void testPassTurnException() {
+        assertThatThrownBy(() -> this.whiteWin.passTurn())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 ranks 명령시 예외 반환")
+    void testRanksException() {
+        assertThatThrownBy(() -> this.whiteWin.ranks()).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 finishReason 백색 승리 문구 반환")
+    void testFinishReason() {
+        assertThat(this.whiteWin.finishReason()).isEqualTo("백색의 승리로 게임이 종료되었습니다.");
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 winner 명령시 백색 반환")
+    void testWinner() {
+        assertThat(this.whiteWin.winner()).isEqualTo(Color.WHITE);
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 end 명령시 End 상태 반환")
+    void testEnd() {
+        assertThat(this.whiteWin.end()).isInstanceOf(End.class);
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 isInit 명령시 false 반환")
+    void testIsInitTure() {
+        assertThat(this.whiteWin.isInit()).isFalse();
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 isRunning 명령시 false 반환")
+    void testIsRunningFalse() {
+        assertThat(this.whiteWin.isRunning()).isFalse();
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 isFinished 명령시 true 반환")
+    void testIsFinishedTrue() {
+        assertThat(this.whiteWin.isFinished()).isTrue();
+    }
+
+    @Test
+    @DisplayName("백색 승리 상태에서 isNotEnd 명령시 true 반환")
+    void testIsNotEndTrue() {
+        assertThat(this.whiteWin.isNotEnd()).isTrue();
+    }
+}
