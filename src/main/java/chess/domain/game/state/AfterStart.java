6634accdc942cfb382f8bb6c4cfe87c9391773c9
hyeon9mak <jinha3507@gmail.com>
2021-03-27 01:16:45 +0900

    refactor: fix AfterStart abstract class

--- src/main/java/chess/domain/game/state/AfterStart.java
+++ src/main/java/chess/domain/game/state/AfterStart.java
@@ -6,49 +6,14 @@ import chess.domain.piece.Color;
 
 import java.util.List;
 
-public abstract class Started implements State {
+public abstract class AfterStart implements State {
     private final Board board;
 
-    public Started(Board board) {
+    public AfterStart(Board board) {
         this.board = board;
     }
 
     protected Board board() {
         return this.board;
-    }
-
-    @Override
-    public boolean isRunning() {
-        return false;
-    }
-
-    @Override
-    public boolean isNotEnd() {
-        return true;
-    }
-
-    @Override
-    public State passTurn() {
-        throw new IllegalStateException("현재 수행할 수 없는 명령입니다.");
-    }
-
-    @Override
-    public List<Rank> ranks() {
-        return board.ranks();
-    }
-
-    @Override
-    public Color winner() {
-        throw new IllegalStateException("현재 수행할 수 없는 명령입니다.");
-    }
-
-    @Override
-    public boolean isInit() {
-        return false;
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
     }
 }
