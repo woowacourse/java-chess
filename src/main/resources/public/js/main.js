175b8dd9c7d3fd752b30a3149e2df1cb173c312b
pjy1368 <basejin0810@naver.com>
2021-03-26 14:41:11 +0900

    refactor(main) : 체스판을 웹에 띄우는 메소드를 분리

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -11,29 +11,38 @@ function createChessBoard() {
     new Array(8).fill("WP"),
     ["WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"]
   ]
+  makeTable(pieces);
+}
 
+function makeTable(pieces) {
   const table = document.getElementById("chess-board");
   for (let i = 0; i < 8; i++) {
-    const newTr = document.createElement("tr");
-    for (let j = 0; j < 8; j++) {
-      const newTd = document.createElement("td");
-      const row = String(8 - i);
-      newTd.id = String.fromCharCode('a'.charCodeAt(0) + j) + row;
-      if (pieces[i][j]) {
-        const piece = document.createElement("img");
-        piece.src = "img/" + pieces[i][j] + ".png";
-        newTd.appendChild(piece);
-      }
-
-      decideCellStyle(newTd, i, j)
-      newTr.appendChild(newTd);
-    }
-    table.appendChild(newTr);
+    table.appendChild(makeTr(pieces, i));
   }
 }
 
-function decideCellStyle(td, i, j) {
-  if ((i % 2 === 0 && j % 2 === 0) || (i % 2 === 1 && j % 2 === 1)) {
+function makeTr(pieces, row) {
+  const newTr = document.createElement("tr");
+  for (let j = 0; j < 8; j++) {
+    newTr.appendChild(makeTd(pieces, row, j));
+  }
+  return newTr;
+}
+
+function makeTd(pieces, row, col) {
+  const newTd = document.createElement("td");
+  newTd.id = String.fromCharCode('a'.charCodeAt(0) + col) + String(8 - row);
+  if (pieces[row][col]) {
+    const piece = document.createElement("img");
+    piece.src = "img/" + pieces[row][col] + ".png";
+    newTd.appendChild(piece);
+  }
+  decideCellStyle(newTd, row, col)
+  return newTd;
+}
+
+function decideCellStyle(td, row, col) {
+  if ((row % 2 === 0 && col % 2 === 0) || (row % 2 === 1 && col % 2 === 1)) {
     td.setAttribute("style",
         "background-color: rgb(204, 204, 204); width: 60px; height: 60px;");
     return;
