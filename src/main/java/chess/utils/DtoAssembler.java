2708ea1e2fe577027e22709cc7cf027e3218a224
hyeon9mak <jinha3507@gmail.com>
2021-03-27 02:30:13 +0900

    refactor: fix DtoAssembler stream methods

--- src/main/java/chess/utils/DtoAssembler.java
+++ src/main/java/chess/utils/DtoAssembler.java
@@ -1,24 +1,21 @@
 package chess.utils;
 
-import chess.domain.board.LineDto;
-import chess.domain.board.position.Position;
+import chess.domain.board.Rank;
+import chess.domain.dto.RankDto;
 import chess.domain.piece.Piece;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DtoAssembler {
-    public static List<LineDto> assemble(List<Map<Position, Piece>> squares) {
-        List<LineDto> lineDtos = new ArrayList<>();
-        for (Map<Position, Piece> square : squares) {
-            List<String> pieces = square.values()
+
+    public static List<RankDto> board(final List<Rank> ranks) {
+        return ranks.stream()
+            .map(rank -> new RankDto(
+                rank.pieces()
                     .stream()
                     .map(Piece::getSymbol)
-                    .collect(Collectors.toList());
-            lineDtos.add(new LineDto(pieces));
-        }
-        return lineDtos;
+                    .collect(Collectors.toList()))
+            ).collect(Collectors.toList());
+
     }
 }
