f72a7b5fb085e4cc8142cc0f9d1057f6b0b8b05f
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-27 01:49:46 +0900

    feat: PositionDAO 추가

--- src/main/java/chess/dao/PositionDAO.java
+++ src/main/java/chess/dao/PositionDAO.java
@@ -1,61 +1,68 @@
 package chess.dao;
 
+import static chess.dao.DBConnection.getConnection;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+import chess.dao.entity.PieceEntity;
+import chess.dao.entity.PlayerEntity;
+import chess.dao.entity.PositionEntity;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PositionDAO {
-//
-//    public Position add(Position position) throws SQLException {
-//        String query = "INSERT INTO position (file_value, rank_value) VALUES (?, ?)";
-//        PreparedStatement pstmt = getConnection().prepareStatement(query);
-//        pstmt.setString(1, position.file().value());
-//        pstmt.setString(2, String.valueOf(position.rank().value()));
-//        pstmt.executeUpdate();
-//        return findByFileRank(position.file(), position.rank());
-//    }
-//
-//    public Position findById(Long id) throws SQLException {
-//        String query = "SELECT * FROM position WHERE id = ?";
-//        PreparedStatement pstmt = getConnection().prepareStatement(query);
-//        pstmt.setString(1, String.valueOf(id));
-//        ResultSet rs = pstmt.executeQuery();
-//        if (!rs.next()) return null;
-//
-//        return new Position(rs.getString("id"), rs.getString("file_value"),
-//            rs.getString("rank_value"));
-//    }
-//
-//    public Position findByFileRank(File file, Rank rank) throws SQLException {
-//        String query = "SELECT * FROM position WHERE file_value = ? AND rank_value = ?";
-//        PreparedStatement pstmt = getConnection().prepareStatement(query);
-//        pstmt.setString(1, file.value());
-//        pstmt.setString(2, String.valueOf(rank.value()));
-//        ResultSet rs = pstmt.executeQuery();
-//        if (!rs.next()) return null;
-//
-//        return new Position(
-//            rs.getString("id"),
-//            rs.getString("file_value"),
-//            rs.getString("rank_value"));
-//    }
-//
-//    public Position update(Position newPosition) throws SQLException {
-//        String query = "UPDATE position SET file_value = ?, rank_value = ? WHERE id = ?";
-//        PreparedStatement pstmt = getConnection().prepareStatement(query);
-//        pstmt.setString(1, newPosition.file().value());
-//        pstmt.setString(2, String.valueOf(newPosition.rank().value()));
-//        pstmt.setString(3, String.valueOf(newPosition.getId()));
-//        pstmt.executeUpdate();
-//        return findById(newPosition.getId());
-//    }
-//
-//    public void delete(Position position) throws SQLException {
-//        String query = "DELETE FROM position WHERE id = ?";
-//        PreparedStatement pstmt = getConnection().prepareStatement(query);
-//        pstmt.setString(1, String.valueOf(position.getId()));
-//        pstmt.executeUpdate();
-//    }
-//
-//    public void deleteAll() throws SQLException {
-//        String query = "DELETE FROM position";
-//        PreparedStatement pstmt = getConnection().prepareStatement(query);
-//        pstmt.executeUpdate();
-//    }
+    private final PieceDAO pieceDAO = new PieceDAO();
+
+    public PositionEntity add(PositionEntity positionEntity) throws SQLException {
+        String query = "INSERT INTO position (file_value, rank_value, piece_id) VALUES (?, ?, ?)";
+        PreparedStatement pstmt = getConnection().prepareStatement(query, RETURN_GENERATED_KEYS);
+        pstmt.setString(1, positionEntity.getFileValue());
+        pstmt.setString(2, positionEntity.getRankValue());
+        pstmt.setLong(3, positionEntity.getPieceEntity().getId());
+        pstmt.executeUpdate();
+        ResultSet generatedKeys = pstmt.getGeneratedKeys();
+        if (generatedKeys.next()) {
+            positionEntity.setId(generatedKeys.getLong(1));
+        }
+        return positionEntity;
+    }
+
+    public PositionEntity findById(Long id) throws SQLException {
+        String query = "SELECT * FROM position WHERE id = ?";
+        PreparedStatement pstmt = getConnection().prepareStatement(query);
+        pstmt.setLong(1, id);
+        ResultSet rs = pstmt.executeQuery();
+        if (!rs.next()) {
+            return null;
+        }
+        PieceEntity pieceEntity = pieceDAO.findById(rs.getLong("piece_id"));
+        return new PositionEntity(
+            rs.getLong("id"),
+            rs.getString("file_value"),
+            rs.getString("rank_value"),
+            pieceEntity);
+    }
+
+    public PositionEntity update(PositionEntity positionEntity) throws SQLException {
+        String query = "UPDATE position SET file_value = ?, rank_value = ? WHERE id = ?";
+        PreparedStatement pstmt = getConnection().prepareStatement(query);
+        pstmt.setLong(1, positionEntity.getId());
+        pstmt.setString(2, positionEntity.getFileValue());
+        pstmt.setString(3, positionEntity.getRankValue());
+        pstmt.executeUpdate();
+        return positionEntity;
+    }
+
+    public void delete(PositionEntity positionEntity) throws SQLException {
+        String query = "DELETE FROM position WHERE id = ?";
+        PreparedStatement pstmt = getConnection().prepareStatement(query);
+        pstmt.setLong(1, positionEntity.getId());
+        pstmt.executeUpdate();
+    }
+
+    public void deleteAll() throws SQLException {
+        String query = "DELETE FROM position";
+        PreparedStatement pstmt = getConnection().prepareStatement(query);
+        pstmt.executeUpdate();
+    }
 }
---
+++ src/main/java/chess/dao/entity/PositionEntity.java
@@ -0,0 +1,70 @@
+package chess.dao.entity;
+
+import chess.domain.position.type.File;
+import chess.domain.position.type.Rank;
+import java.util.Objects;
+
+public class PositionEntity {
+    private Long id;
+    private String fileValue;
+    private String rankValue;
+    private PieceEntity pieceEntity;
+
+    public PositionEntity(File file, Rank rank, PieceEntity pieceEntity) {
+        this.fileValue = file.value();
+        this.rankValue = String.valueOf(rank.value());
+        this.pieceEntity = pieceEntity;
+    }
+
+    public PositionEntity(Long id, String fileValue, String rankValue, PieceEntity pieceEntity) {
+        this.id = id;
+        this.fileValue = fileValue;
+        this.rankValue = rankValue;
+        this.pieceEntity = pieceEntity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileValue() {
+        return fileValue;
+    }
+
+    public void setFile(File file) {
+        fileValue = file.value();
+    }
+
+    public String getRankValue() {
+        return rankValue;
+    }
+
+    public void setRank(Rank rank) {
+        rankValue = String.valueOf(rank.value());
+    }
+
+    public PieceEntity getPieceEntity() {
+        return pieceEntity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PositionEntity)) {
+            return false;
+        }
+        PositionEntity that = (PositionEntity) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
