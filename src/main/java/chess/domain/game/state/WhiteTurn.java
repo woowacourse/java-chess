559c6bcbf3a2df4c034f3db19f9e52763b3beacd
hyeon9mak <jinha3507@gmail.com>
2021-03-27 00:14:56 +0900

    refactor: fix WhiteTurn state

--- src/main/java/chess/domain/game/state/WhiteTurn.java
+++ src/main/java/chess/domain/game/state/WhiteTurn.java
@@ -1,8 +1,11 @@
 package chess.domain.game.state;
 
 import chess.domain.board.Board;
+import chess.domain.board.Rank;
 import chess.domain.board.position.Position;
+import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import java.util.List;
 
 public class WhiteTurn extends Running {
 
@@ -11,24 +14,63 @@ public class WhiteTurn extends Running {
     }
 
     @Override
-    public State passTurn() {
-        return new BlackTurn(board());
-    }
-
-    @Override
-    public State end() {
-        if (isSeizeOppositeKing()) {
-            return new WhiteWin(board());
-        }
-        return new Draw(board());
+    public State start() {
+        throw new IllegalStateException("이미 체스게임이 진행 중 입니다.");
     }
 
     @Override
     public void moveIfValidColor(Position source, Position target) {
         Piece sourcePiece = board().pieceByPosition(source);
+
         if (sourcePiece.isBlack()) {
-            throw new IllegalStateException("흰색 차례엔 흰색 말만 이동 가능합니다.");
+            throw new IllegalStateException("백색 차례엔 백색 말만 이동 가능합니다.");
         }
+
         board().moveIfValidPosition(source, target);
+    }
+
+    @Override
+    public State passTurn() {
+        return new BlackTurn(board());
+    }
+
+    @Override
+    public List<Rank> ranks() {
+        return board().ranks();
+    }
+
+    @Override
+    public String finishReason() {
+        throw new IllegalStateException("아직 체스게임이 진행중 입니다.");
+    }
+
+    @Override
+    public Color winner() {
+        throw new IllegalStateException("아직 체스게임이 진행중 입니다.");
+    }
+
+    @Override
+    public State end() {
+        return new End(board());
+    }
+
+    @Override
+    public boolean isInit() {
+        return false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return true;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public boolean isNotEnd() {
+        return true;
     }
 }
