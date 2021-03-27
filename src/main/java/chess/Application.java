97e8c9de22871accd5a38739bbb9ff43aaeb9e82
Naeun <knae11@naver.com>
2021-03-27 18:22:37 +0900

    refactor: 예외처리 적용

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -9,13 +9,31 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        run();
+    }
+
+    private static void runUntilValid(ChessBoard chessBoard) {
+        try {
+            runByCommands(chessBoard);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            OutputView.printBoard(chessBoard.getChessBoard());
+            runUntilValid(chessBoard);
+        }
+    }
+
+    private static void run() {
         OutputView.printInitMessage();
         final ChessBoard chessBoard = new ChessBoard();
         while (chessBoard.isPlaying()) {
-            List<String> command = InputView.command();
-            Commands.run(chessBoard, command);
+            runUntilValid(chessBoard);
         }
         OutputView.printScore(chessBoard.result());
+    }
+
+    private static void runByCommands(ChessBoard chessBoard) {
+        List<String> command = InputView.command();
+        Commands.run(chessBoard, command);
     }
 
 
