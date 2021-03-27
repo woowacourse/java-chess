e52dda70f6309e1a1d07349c665da0cb5edaaad3
lns13301 <rkdrbgks22@ks.ac.kr>
2021-03-27 22:50:55 +0900

    refactor: 커맨드 동작 방식을 enum 으로 변경

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,53 +1,39 @@
 package chess.controller;
 
 import chess.domain.ChessGame;
-import chess.domain.command.CommandInput;
-import chess.domain.command.Commands;
-import chess.domain.piece.Team;
+import chess.domain.command.Command;
 import chess.exception.GameIsNotStartException;
+import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessController {
     private static final String STATUS = "status";
     private static final String END = "end";
 
-    public void run(ChessGame chessGame, Commands commands) {
+    public void run(ChessGame chessGame) {
         boolean isPlaying = true;
 
         OutputView.printManual();
         while (isPlaying) {
-            isPlaying = playGame(chessGame, commands);
+            isPlaying = playGame(chessGame);
         }
     }
 
-    public boolean playGame(ChessGame chessGame, Commands commands) {
-        boolean play = true;
+    public boolean playGame(ChessGame chessGame) {
         try {
-            CommandInput commandInput = CommandInput.create();
-            String command = commands.execute(chessGame, commandInput);
-            play = isPlay(command);
-            checkGameStart(chessGame, play);
+            String inputCommand = InputView.inputCommand();
+            Command.findCommand(inputCommand).execute(chessGame, inputCommand);
+            checkGameStart(chessGame);
             OutputView.printBoard(chessGame.getBoard());
-            showStatus(command, chessGame);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return play;
+        return chessGame.isBeforeEnd();
     }
 
-    private void checkGameStart(ChessGame chessGame, boolean isPlay) {
-        if (chessGame.isBeforeStart() && isPlay) {
+    private void checkGameStart(ChessGame chessGame) {
+        if (chessGame.isBeforeStart()) {
             throw new GameIsNotStartException();
         }
-    }
-
-    private void showStatus(String command, ChessGame chessGame) {
-        if (command.equals(STATUS)) {
-            OutputView.printStatus(chessGame.status(Team.BLACK), chessGame.status(Team.WHITE));
-        }
-    }
-
-    private boolean isPlay(String command) {
-        return !command.equals(END);
     }
 }
--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -2,49 +2,57 @@ package chess.domain;
 
 import chess.domain.board.*;
 import chess.domain.piece.Team;
+import chess.view.OutputView;
 
 public class ChessGame {
-    private static final String END = "end";
-    private static final int OPTION_TARGET_INDEX = 0;
-    private static final int OPTION_DESTINATION_INDEX = 1;
+    private static final int HORIZONTAL_INDEX = 0;
+    private static final int VERTICAL_INDEX = 1;
     private static final int OPTION_FINISH = 2;
+    private static final int TARGET_INDEX = 1;
+    private static final int DESTINATION_INDEX = 2;
+    private static final String DELIMITER = " ";
 
     private Board board;
-    private boolean isStart;
+    private boolean start;
+    private boolean end;
 
     public void settingBoard() {
         board = BoardFactory.create();
-        isStart = false;
+        start = true;
+        end = false;
     }
 
     public void start() {
+        settingBoard();
         board.applyStatus();
-        isStart = true;
     }
 
-    public boolean move(String target, String destination) {
-        if (board.movePiece(convertStringToPosition(target), convertStringToPosition(destination))) {
-            return true;
-        }
+    public void move(String input) {
+        String[] splitInput = input.split(DELIMITER);
+
+        board.movePiece(convertStringToPosition(splitInput[TARGET_INDEX])
+                , convertStringToPosition(splitInput[DESTINATION_INDEX]));
         board.applyStatus();
-        return false;
     }
 
-    public double status(Team team) {
-        return board.score(team);
+    public void status() {
+        OutputView.printStatus(board.score(Team.BLACK), board.score(Team.WHITE));
     }
 
-    public String end() {
-        return END;
+    public void end() {
+        end = true;
     }
 
     private Position convertStringToPosition(String input) {
-        return Position.of(Horizontal.find(input.substring(OPTION_TARGET_INDEX, OPTION_DESTINATION_INDEX)),
-                Vertical.find(input.substring(OPTION_DESTINATION_INDEX, OPTION_FINISH)));
+        return Position.of(Horizontal.find(input.substring(HORIZONTAL_INDEX, VERTICAL_INDEX)),
+                Vertical.find(input.substring(VERTICAL_INDEX, OPTION_FINISH)));
     }
 
+    public boolean isBeforeEnd() {
+        return !end;
+    }
     public boolean isBeforeStart() {
-        return !isStart;
+        return !start;
     }
 
     public Board getBoard() {
