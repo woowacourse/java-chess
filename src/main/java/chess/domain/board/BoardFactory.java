b4c21aeb01da97fd50633f2758a4701e57c41000
lns13301 <rkdrbgks22@ks.ac.kr>
2021-03-27 23:17:57 +0900

    refactor: 보드 기물 세팅 기능 분리

--- src/main/java/chess/domain/board/BoardFactory.java
+++ src/main/java/chess/domain/board/BoardFactory.java
@@ -16,23 +16,11 @@ public class BoardFactory {
     private static Map<Position, Piece> createBoard() {
         Map<Position, Piece> board = initialize();
 
-        board.put(Position.of(Horizontal.A, Vertical.EIGHT), new Rook(Team.BLACK));
-        board.put(Position.of(Horizontal.B, Vertical.EIGHT), new Knight(Team.BLACK));
-        board.put(Position.of(Horizontal.C, Vertical.EIGHT), new Bishop(Team.BLACK));
-        board.put(Position.of(Horizontal.D, Vertical.EIGHT), new Queen(Team.BLACK));
-        board.put(Position.of(Horizontal.E, Vertical.EIGHT), new King(Team.BLACK));
-        board.put(Position.of(Horizontal.F, Vertical.EIGHT), new Bishop(Team.BLACK));
-        board.put(Position.of(Horizontal.G, Vertical.EIGHT), new Knight(Team.BLACK));
-        board.put(Position.of(Horizontal.H, Vertical.EIGHT), new Rook(Team.BLACK));
-
-        board.put(Position.of(Horizontal.A, Vertical.ONE), new Rook(Team.WHITE));
-        board.put(Position.of(Horizontal.B, Vertical.ONE), new Knight(Team.WHITE));
-        board.put(Position.of(Horizontal.C, Vertical.ONE), new Bishop(Team.WHITE));
-        board.put(Position.of(Horizontal.D, Vertical.ONE), new Queen(Team.WHITE));
-        board.put(Position.of(Horizontal.E, Vertical.ONE), new King(Team.WHITE));
-        board.put(Position.of(Horizontal.F, Vertical.ONE), new Bishop(Team.WHITE));
-        board.put(Position.of(Horizontal.G, Vertical.ONE), new Knight(Team.WHITE));
-        board.put(Position.of(Horizontal.H, Vertical.ONE), new Rook(Team.WHITE));
+        setRook(board);
+        setKnight(board);
+        setBishop(board);
+        setQueen(board);
+        setKing(board);
         setPawn(board);
 
         return board;
@@ -46,6 +34,45 @@ public class BoardFactory {
         }
 
         return board;
+    }
+
+    private static void setRook(Map<Position, Piece> board) {
+        setPieceByBlack(board, new Rook(Team.BLACK), Horizontal.A);
+        setPieceByBlack(board, new Rook(Team.BLACK), Horizontal.H);
+        setPieceByWhite(board, new Rook(Team.WHITE), Horizontal.A);
+        setPieceByWhite(board, new Rook(Team.WHITE), Horizontal.H);
+    }
+
+    private static void setKnight(Map<Position, Piece> board) {
+        setPieceByBlack(board, new Knight(Team.BLACK), Horizontal.B);
+        setPieceByBlack(board, new Knight(Team.BLACK), Horizontal.G);
+        setPieceByWhite(board, new Knight(Team.WHITE), Horizontal.B);
+        setPieceByWhite(board, new Knight(Team.WHITE), Horizontal.G);
+    }
+
+    private static void setBishop(Map<Position, Piece> board) {
+        setPieceByBlack(board, new Bishop(Team.BLACK), Horizontal.C);
+        setPieceByBlack(board, new Bishop(Team.BLACK), Horizontal.F);
+        setPieceByWhite(board, new Bishop(Team.WHITE), Horizontal.C);
+        setPieceByWhite(board, new Bishop(Team.WHITE), Horizontal.F);
+    }
+
+    private static void setQueen(Map<Position, Piece> board) {
+        setPieceByBlack(board, new Queen(Team.BLACK), Horizontal.D);
+        setPieceByWhite(board, new Queen(Team.WHITE), Horizontal.D);
+    }
+
+    private static void setKing(Map<Position, Piece> board) {
+        setPieceByBlack(board, new King(Team.BLACK), Horizontal.E);
+        setPieceByWhite(board, new King(Team.WHITE), Horizontal.E);
+    }
+
+    private static void setPieceByBlack(Map<Position, Piece> board, Piece piece, Horizontal horizontal) {
+        board.put(Position.of(horizontal, Vertical.EIGHT), piece);
+    }
+
+    private static void setPieceByWhite(Map<Position, Piece> board, Piece piece, Horizontal horizontal) {
+        board.put(Position.of(horizontal, Vertical.ONE), piece);
     }
 
     private static void setPawn(Map<Position, Piece> board) {
