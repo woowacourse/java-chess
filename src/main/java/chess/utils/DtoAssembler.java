033cad90338cc7d25655cada5cc2fce3a25ea350
hyeon9mak <jinha3507@gmail.com>
2021-03-28 15:17:33 +0900

    refactor: fix create RankDto method in DtoAssembler

--- src/main/java/chess/utils/DtoAssembler.java
+++ src/main/java/chess/utils/DtoAssembler.java
@@ -1,21 +1,30 @@
 package chess.utils;
 
-import chess.domain.board.Rank;
+import chess.domain.board.Board;
+import chess.domain.board.position.Ypoint;
 import chess.domain.dto.RankDto;
 import chess.domain.piece.Piece;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DtoAssembler {
 
-    public static List<RankDto> board(final List<Rank> ranks) {
-        return ranks.stream()
-            .map(rank -> new RankDto(
-                rank.pieces()
-                    .stream()
-                    .map(Piece::getSymbol)
-                    .collect(Collectors.toList()))
-            ).collect(Collectors.toList());
+    public static List<RankDto> board(final Board board) {
+        List<RankDto> rankDtos = new ArrayList<>();
 
+        for (Ypoint ypoint : Ypoint.values()) {
+            List<String> symbols = ypointSymbols(board, ypoint);
+            rankDtos.add(new RankDto(symbols));
+        }
+
+        return rankDtos;
+    }
+
+    private static List<String> ypointSymbols(Board board, Ypoint ypoint) {
+        return board.piecesByYpoint(ypoint)
+            .stream()
+            .map(Piece::getSymbol)
+            .collect(Collectors.toList());
     }
 }
