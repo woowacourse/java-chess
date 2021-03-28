e9b12e9941f5eaab97ce89f231c6a04f63bb18af
멍토 <daum7766@naver.com>
2021-03-28 16:09:06 +0900

    feat: 웹보드 js 작성

---
+++ src/main/resources/static/function.js
@@ -0,0 +1,124 @@
+const BOARD = document.querySelector("#board");
+const API_URL = "http://localhost:4567/";
+const DEFAULT_PATH = "./image/";
+const SYMBOL_TO_IMAGE_PATH = {
+    "p": DEFAULT_PATH + "whitePawn.png",
+    "P": DEFAULT_PATH + "blackPawn.png",
+    "r": DEFAULT_PATH + "whiteRook.png",
+    "R": DEFAULT_PATH + "blackRook.png",
+    "n": DEFAULT_PATH + "whiteKnight.png",
+    "N": DEFAULT_PATH + "blackKnight.png",
+    "b": DEFAULT_PATH + "whiteBishop.png",
+    "B": DEFAULT_PATH + "blackBishop.png",
+    "q": DEFAULT_PATH + "whiteQueen.png",
+    "Q": DEFAULT_PATH + "blackQueen.png",
+    "k": DEFAULT_PATH + "whiteKing.png",
+    "K": DEFAULT_PATH + "blackKing.png"
+};
+let boardInfo = {};
+let isClicked = false;
+let source = null;
+
+window.onload = () => {
+    const divs = BOARD.querySelectorAll("div");
+    boardInit();
+    for (const div of divs) {
+        div.addEventListener("click", divClickEvent);
+    }
+}
+
+function boardInit() {
+    fetch(API_URL + "start")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(" ");
+        }
+        return response.json();
+    })
+    .then(imageSetting)
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
+function imageSetting(responseData) {
+    boardInfo = responseData;
+    const divs = BOARD.querySelectorAll("div");
+    for (const div of divs) {
+        const key = div.getAttribute("id");
+        if (SYMBOL_TO_IMAGE_PATH[responseData[key]] !== undefined) {
+            div.style.backgroundImage = "url(" + SYMBOL_TO_IMAGE_PATH[responseData[key]] + ")";;
+        } else {
+            div.style.backgroundImage = null;
+        }
+    }
+    // 끝났는지 체크 alter 띄우기
+    if (boardInfo["isFinish"] === "true") {
+        //게임이 끝났음
+        restartAsk();
+    }
+}
+
+function divClickEvent(event) {
+    if (boardInfo["isFinish"] === "true") {
+        restartAsk();
+        return;
+    }
+    const key = event.target.getAttribute("id");
+    if (isClicked === false && boardInfo[key] === ".") {
+        //선택할 수 없는 친구
+        console.log("빈 공간은 움직일 수 없습니다.")
+        return;
+    }
+    if (isClicked === false) {
+        isClicked = true;
+        source = event.target;
+        source.style.backgroundColor = "yellow";
+    } else if (source === event.target) {
+        isClicked = false;
+        source.style.backgroundColor = "";
+        source = null;
+    } else {
+        isClicked = false;
+        source.style.backgroundColor = "";
+        const sourcePosition = source.getAttribute("id");
+        const targetPosition = event.target.getAttribute("id");
+        movedPieces(sourcePosition, targetPosition);
+    }
+}
+
+function movedPieces(source, target) {
+    const newData = {
+        "source": source,
+        "target": target
+    }
+    const option = getOption("PUT", newData);
+
+    fetch(API_URL + "move", option)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(" ");
+        }
+        return response.json();
+    })
+    .then(imageSetting)
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
+function getOption(methodType, bodyData) {
+    return {
+        method: methodType,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bodyData)
+    };
+}
+
+function restartAsk() {
+    if (confirm("게임이 끝났습니다. 시작하겠습니까?") === true) {
+        boardInit();
+    }
+}
