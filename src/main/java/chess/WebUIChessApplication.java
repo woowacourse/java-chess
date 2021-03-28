9f8d3231fc32a6c5a8fc7acdebafcb616c188a40
멍토 <daum7766@naver.com>
2021-03-29 00:25:43 +0900

    feat: 클라이언트와 통신파트 추가

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,7 +1,11 @@
 package chess;
 
 import static spark.Spark.get;
+import static spark.Spark.put;
+import static spark.Spark.staticFileLocation;
 
+import chess.controller.ChessWebController;
+import com.google.gson.Gson;
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
@@ -10,9 +14,29 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 public class WebUIChessApplication {
 
     public static void main(String[] args) {
+//        Spark.port(8080);
+
+        staticFileLocation("/static");
+        final ChessWebController webController = new ChessWebController();
+        Gson gson = new Gson();
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
+        });
+
+        get("/start", (request, response) -> {
+            Map<String, String> boardInfo = webController.start();
+            return gson.toJson(boardInfo);
+        });
+        put("/move", (request, response) -> {
+            Map<String, String> requestBody = gson.fromJson(request.body(), HashMap.class) ;
+            System.out.println("이동");
+            System.out.println(requestBody);
+            System.out.println(requestBody.get("source"));
+            System.out.println(requestBody.get("target"));
+            Map<String, String> boardInfo = webController.movedPiece(requestBody.get("source"), requestBody.get("target"));
+            return gson.toJson(boardInfo);
         });
     }
 
---
+++ src/main/java/chess/controller/ChessWebController.java
@@ -0,0 +1,46 @@
+package chess.controller;
+
+import chess.domain.board.Board;
+import chess.domain.piece.Piece;
+import chess.domain.piece.Position;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ChessWebController {
+
+    final Board board = new Board();
+
+    public Map<String, String> start() {
+        board.init();
+        Map<Position, Piece> pieces = board.pieces();
+        return PositionToStringMap(pieces);
+    }
+
+    private Map<String, String> PositionToStringMap(Map<Position, Piece> pieces) {
+        Map<String, String> boardInfo = new HashMap<>();
+        for (Position position : pieces.keySet()) {
+            boardInfo.put(position.positionToString(), getSymbol(pieces.get(position)));
+        }
+        boardInfo.put("isFinish", String.valueOf(board.isFinish()));
+        boardInfo.put("turn", board.turn().color());
+        return boardInfo;
+    }
+
+    private String getSymbol(Piece piece) {
+        if (Objects.isNull(piece)) {
+            return ".";
+        }
+        return piece.symbol();
+    }
+
+    public Map<String, String> movedPiece(String source, String target) {
+        try {
+            board.movePiece(source, target);
+        } catch (Exception e) {
+            System.out.println("이동실패");
+        }
+        Map<Position, Piece> pieces = board.pieces();
+        return PositionToStringMap(pieces);
+    }
+}
