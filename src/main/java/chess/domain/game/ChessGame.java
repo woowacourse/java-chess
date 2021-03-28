d9345f0840305e7ea4f9fde740643101cba7f50d
seunghyun <ggyool@naver.com>
2021-03-29 03:31:57 +0900

    feat: state를 이용하여 게임을 진행하는 ChessGame 도메인을 생성

---
+++ src/main/java/chess/domain/game/ChessGame.java
@@ -0,0 +1,44 @@
+package chess.domain.game;
+
+import chess.domain.result.Result;
+import chess.domain.state.ResultType;
+import chess.domain.state.Start;
+import chess.domain.state.State;
+import java.util.Objects;
+
+public class ChessGame {
+
+    private State state;
+
+    public ChessGame() {
+        this.state = new Start();
+    }
+
+    public boolean isRunning() {
+        return state.isRunning();
+    }
+
+    public boolean needsCommand() {
+        return state.needsParam();
+    }
+
+    public void receiveCommand(String command) {
+        state.receive(command);
+    }
+
+    public boolean supports(ResultType board) {
+        return Objects.equals(board, state.bringResultType());
+    }
+
+    public Result bringResult() {
+        return state.bringResult();
+    }
+
+    public void progress(boolean isSuccessful) {
+        if (isSuccessful) {
+            state = state.next();
+            return;
+        }
+        state = state.before();
+    }
+}
