5bc1a38f8950507b54750d537d485918ab4fcd79
sakjung <ssangyu123@gmail.com>
2021-03-29 15:15:11 +0900

    feat: static file(html,css,images) 추가

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -6,18 +6,39 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebUIChessApplication {
+    public static void main(String[] args) {
+        staticFiles.location("");
 
-	public static void main(String[] args) {
-		get("/", (req, res) -> {
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "index.html");
+        });
+
+        get("/registration", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "registration.html");
+        });
+
+		get("/menu", (req, res) -> {
 			Map<String, Object> model = new HashMap<>();
-			return render(model, "index.html");
+			return render(model, "menu.html");
 		});
-	}
 
-	private static String render(Map<String, Object> model, String templatePath) {
-		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-	}
+        get("/game/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "game.html");
+        });
+
+		get("/game/saved", (req, res) -> {
+			Map<String, Object> model = new HashMap<>();
+			return render(model, "game.html");
+		});
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
 }
