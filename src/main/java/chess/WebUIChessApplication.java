90707d1919a486bb0845a501eaac6f968e25674e
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-29 17:00:01 +0900

    refactor : GSON을 조금 더 간결한 코드로 사용하기

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -19,7 +19,6 @@ import static spark.Spark.staticFiles;
 
 public class WebUIChessApplication {
     public static final Gson GSON = new Gson();
-    private static final JsonTransformer JSON_TRANSFORMER = new JsonTransformer();
 
     public static void main(String[] args) {
         staticFiles.location("/public");
@@ -33,31 +32,31 @@ public class WebUIChessApplication {
         post("/move", (req, res) -> {
             MoveRequestDto moveRequestDto = GSON.fromJson(req.body(), MoveRequestDto.class);
             return chessService.move(moveRequestDto);
-        }, JSON_TRANSFORMER);
+        }, GSON::toJson);
 
         get("/grid/:roomName", (req, res) -> {
             String roomName = req.params(":roomName");
             StartRequestDto startRequestDto = new StartRequestDto(roomName);
             GridAndPiecesResponseDto gridAndPiecesResponseDto = chessService.getGridAndPieces(startRequestDto);
             return new Response(ResponseCode.OK, gridAndPiecesResponseDto);
-        }, JSON_TRANSFORMER);
+        }, GSON::toJson);
 
         post("/grid/:gridId/start", (req, res) -> {
             String gridId = req.params("gridId");
             chessService.start(Long.parseLong(gridId));
             return new Response(ResponseCode.NO_CONTENT);
-        }, JSON_TRANSFORMER);
+        }, GSON::toJson);
 //
         post("/grid/:gridId/finish", (req, res) -> {
             String gridId = req.params("gridId");
             chessService.finish(Long.parseLong(gridId));
             return new Response(ResponseCode.NO_CONTENT);
-        }, JSON_TRANSFORMER);
+        }, GSON::toJson);
 
         get("/room/:roomId/restart", (req, res) -> {
             String roomId = req.params("roomId");
             return new Response(ResponseCode.OK, chessService.restart(Long.parseLong(roomId)));
-        }, JSON_TRANSFORMER);
+        }, GSON::toJson);
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
