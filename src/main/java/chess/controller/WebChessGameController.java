cf5c4700354053172d5d087389e9a96e3a631482
kimjunseo <air.junseo@gmail.com>
2021-03-29 15:18:55 +0900

    feat : 기본 레이아웃 설정

--- src/main/java/chess/controller/WebChessGameController.java
+++ src/main/java/chess/controller/WebChessGameController.java
@@ -12,15 +12,34 @@ import static spark.Spark.post;
 
 public class WebChessGameController {
     public void start() {
+        initialPage();
+        enterGameRoom();
+        gameStart();
+    }
+
+    private void initialPage() {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
+    }
 
+    private void enterGameRoom() {
+        post("/enter", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String button = "시작";
+            model.put("button", button);
+            return render(model, "chess.html");
+        });
+    }
+
+    private void gameStart() {
         post("/start", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             ChessGame chessGame = new ChessGame();
             model.put("chessGame", chessGame);
+            String button = "초기화";
+            model.put("button", button);
             return render(model, "chess.html");
         });
     }
