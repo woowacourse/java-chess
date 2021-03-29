1c76794c7eb38b710b9245de02a6684b9f2a03fc
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-30 00:24:07 +0900

    test: MoveRouteTest DB 적용 버전으로 변경

--- src/test/java/chess/beforedb/domain/position/MoveRouteTest.java
+++ src/test/java/chess/beforedb/domain/position/MoveRouteTest.java
@@ -7,6 +7,8 @@ import static chess.beforedb.domain.position.type.Rank.THREE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import chess.db.domain.position.MoveRouteForDB;
+import chess.db.domain.position.PositionEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,16 +16,16 @@ class MoveRouteTest {
     @DisplayName("유효한 출발위치와 도착위치")
     @Test
     void validMoveRoute() {
-        MoveRoute moveRoute = new MoveRoute("a3", "c5");
+        MoveRouteForDB moveRoute = new MoveRouteForDB("a3", "c5");
 
-        assertThat(moveRoute.startPosition()).isEqualTo(new Position(A, THREE));
-        assertThat(moveRoute.destination()).isEqualTo(new Position(C, FIVE));
+        assertThat(moveRoute.getStartPosition()).isEqualTo(new PositionEntity(A, THREE));
+        assertThat(moveRoute.getDestination()).isEqualTo(new PositionEntity(C, FIVE));
     }
 
     @DisplayName("유효하지 않은 출발위치와 도착위치")
     @Test
     void invalidMoveRoute() {
-        assertThatThrownBy(() -> new MoveRoute("i3", "c9"))
+        assertThatThrownBy(() -> new MoveRouteForDB("i3", "c9"))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
