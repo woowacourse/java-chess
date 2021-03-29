ae24d79f1d4fd70d9275f82b3f1d00b3c72bef26
pkeugine <pkeugine@gmail.com>
2021-03-29 14:45:56 +0900

    refactor: RookStrategy 변경

--- src/main/java/chess/domain/piece/strategy/PieceStrategy.java
+++ src/main/java/chess/domain/piece/strategy/PieceStrategy.java
@@ -1,7 +1,6 @@
 package chess.domain.piece.strategy;
 
 import chess.domain.board.Path;
-import chess.domain.piece.Piece;
 import chess.domain.position.Position;
 import java.util.List;
 
@@ -10,10 +9,6 @@ public interface PieceStrategy {
     List<Direction> directions();
 
     Path pathFrom(final Direction direction, final Position position);
-
-    boolean isDifferentColor(final Piece piece);
-
-    boolean blockedBy(final Piece piece);
 
     boolean isPawn();
 
