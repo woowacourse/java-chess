44e031862671757dc951183086afe6d330a07f86
NewWisdom <wlgp2500@gmail.com>
2021-03-29 15:45:40 +0900

    refactor(chess-controller): 상태를 가지지 않도록 변경

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -7,36 +7,33 @@ import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessController {
-    private final ChessGame chessGame;
-    private final Commands commands;
-
     public ChessController() {
-        this.chessGame = new ChessGame();
-        this.commands = Commands.validCommands();
     }
 
     public void run() {
+        ChessGame chessGame = new ChessGame();
         OutputView.printStartMessage();
         while (chessGame.runnable()) {
-            executeByCommand();
+            executeByCommand(chessGame);
         }
     }
 
-    private void executeByCommand() {
+    private void executeByCommand(ChessGame chessGame) {
         try {
             OutputView.printRequestCommandMessage();
             String[] splitCommand = InputView.command().split(" ");
             String firstCommand = splitCommand[0];
+            Commands commands = Commands.validCommands();
             Command command = commands.findCommandByText(firstCommand);
             command.execute(chessGame, splitCommand);
-            showResultByCommand(command);
+            showResultByCommand(command, chessGame);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            executeByCommand();
+            executeByCommand(chessGame);
         }
     }
 
-    private void showResultByCommand(Command command) {
+    private void showResultByCommand(Command command, ChessGame chessGame) {
         if (command.isMustShowBoard()) {
             OutputView.printChessBoard(chessGame.getPiecesByAllPosition());
         }
