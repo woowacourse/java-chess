0ce635f9f4d71218c9a34863937187e51223c972
thisisyoungbin <thisisyoungbin@gmail.com>
2021-03-31 01:31:20 +0900

    feat: 초기화면 구현

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,18 +1,38 @@
 package chess;
 
+import chess.domain.ChessGame;
+import chess.domain.board.Board;
+import chess.dto.PieceDto;
+import chess.dto.WebBoardDto;
+import com.google.gson.Gson;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
+        staticFiles.location("/templates");
+        ChessGame chessGame = new ChessGame(new Board());
+        chessGame.start();
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
+            return render(model, "init.html");
+        });
+
+        post("/main", (req, res) -> {
+            WebBoardDto webBoardDto = new WebBoardDto(chessGame.board());
+            Map<String, Object> model = webBoardDto.getBoardDto();
+            return render(model, "main.html");
+        });
+
+        post("/restart", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "init.html");
         });
     }
 
