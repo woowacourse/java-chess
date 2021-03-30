a8bd568faf70bfb23595e4bb9ed88ea09cffd88b
xrabcde <xrabcde@gmail.com>
2021-03-30 11:26:24 +0900

    feat: chessBoardColumn에 position 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -17,26 +17,27 @@ initChessBoard();
 
 function initChessBoard() {
     for (let i = 0; i < 8; i++) {
-        let chessBoardRow = document.createElement('div');
+        let chessBoardRow = document.createElement("div");
         chessBoardRow.setAttribute("class", "chessRow");
 
         for (let j = 0; j < 8; j++) {
-            let chessBoardColumn = document.createElement('div');
+            let chessBoardColumn = document.createElement("div");
             chessBoardColumn.setAttribute("class", "chessColumn");
 
             chessBoardColumn.style = initChessBoardColor(i, j);
 
-            let pieceImg = document.createElement('img');
+            let pieceImg = document.createElement("img");
             let positionPiece = initPieceImage(i, j);
 
             if (positionPiece === "") {
                 pieceImg.style = "display: none";
             }
-
             pieceImg.src = positionPiece;
 
+            chessBoardColumn.id = initPiecePosition(i, j);
             chessBoardColumn.appendChild(pieceImg);
             chessBoardRow.appendChild(chessBoardColumn);
+
         }
         $chessBoard.appendChild(chessBoardRow);
     }
@@ -46,6 +47,30 @@ function initChessBoardColor(row, column) {
     return (row + column) % 2
         ? "background-color: #7b9acc"
         : "background-color: #FCF6F5";
+}
+
+function initPiecePosition(row, column) {
+    const rowTable = {
+            0: "8",
+            1: "7",
+            2: "6",
+            3: "5",
+            4: "4",
+            5: "3",
+            6: "2",
+            7: "1",
+    };
+    const columnTable = {
+            0: "a",
+            1: "b",
+            2: "c",
+            3: "d",
+            4: "e",
+            5: "f",
+            6: "g",
+            7: "h",
+    };
+    return columnTable[column]+rowTable[row];
 }
 
 function initPieceImage(row, column) {
