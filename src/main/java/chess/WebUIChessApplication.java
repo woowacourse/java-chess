421e0a95af314cf2a7b587abdd0e3c8d22be5bfe
YeongBeom Heo <hybeom@gmail.com>
2021-03-30 20:30:11 +0900

    feat(WebUIChessAppication) : web 기능 기초 추

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,11 +1,9 @@
 package chess;
 
-import static spark.Spark.post;
+import static spark.Spark.get;
 import static spark.Spark.staticFiles;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -14,6 +12,10 @@ public class WebUIChessApplication {
     public static void main(String[] args) {
         staticFiles.location("/static");
 
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "Chess.html");
+        });
     }
 
     public static String render(Map<String, Object> model, String templatePath) {
