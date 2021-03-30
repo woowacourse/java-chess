235244fb3498edd8370046f71df54744aec76eb5
Jinhong <xlffm3@gmail.com>
2021-03-30 18:36:37 +0900

    refactor: MoveController 분리

---
+++ src/main/java/chess/controller/MoveController.java
@@ -0,0 +1,28 @@
+package chess.controller;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MoveController {
+
+    private MoveController() {
+    }
+
+    public static String moveToMainPage(Request request, Response response) {
+        return render("index.html");
+    }
+
+    public static String moveToResultPage(Request request, Response response) {
+        return render("result.html");
+    }
+
+    private static String render(String templatePath) {
+        Map<String, Object> model = new HashMap<>();
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+}
