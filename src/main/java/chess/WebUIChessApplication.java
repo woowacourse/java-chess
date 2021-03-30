4a4decced1707ed4899c4824b0cc8be58d678e0c
YeongBeom Heo <hybeom@gmail.com>
2021-03-30 18:29:52 +0900

    feat : WebApplication 연결

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,14 +1,22 @@
 package chess;
 
-import chess.controller.ChessController;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
-        try {
-            ChessController chessController = new ChessController();
-            chessController.run();
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
+        staticFiles.location("/static");
+
+    }
+
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
