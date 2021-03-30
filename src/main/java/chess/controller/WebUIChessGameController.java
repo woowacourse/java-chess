f340e79d4f04bce86ded5d365cac22767408fa46
hyeon9mak <jinha3507@gmail.com>
2021-03-30 21:30:53 +0900

    feat: add move piece in Web UI chess game controller

--- src/main/java/chess/controller/WebUIChessGameController.java
+++ src/main/java/chess/controller/WebUIChessGameController.java
@@ -3,8 +3,10 @@ package chess.controller;
 import chess.domain.board.Board;
 import chess.domain.board.position.Position;
 import chess.domain.game.ChessGame;
+import chess.dto.BoardDto;
 import chess.dto.MovableRequestDto;
 import chess.dto.MovableResponseDto;
+import chess.dto.MoveRequestDto;
 import chess.utils.DtoAssembler;
 import java.util.List;
 
@@ -20,6 +22,12 @@ public class WebUIChessGameController {
     public MovableResponseDto movablePath(final MovableRequestDto movableRequestDto) {
         List<Position> positions = this.chessGame.movablePath(movableRequestDto.position());
         return DtoAssembler.movableResponse(positions);
+    }
 
+    public BoardDto move(final MoveRequestDto moveRequestDto) {
+        Position source = Position.of(moveRequestDto.getSource());
+        Position target = Position.of(moveRequestDto.getTarget());
+        this.chessGame.move(source, target);
+        return DtoAssembler.board(this.chessGame.board());
     }
 }
