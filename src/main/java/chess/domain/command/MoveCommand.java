78428e7828f84a8d6ae904a3a6d8f2c6d49a182c
Gomding <backchida@gmail.com>
2021-03-31 02:32:48 +0900

    refactor(MoveCommand.java) : validateMoveCommand메서드 삭제
    이미 Command 에서 파라미터 사이즈에 대한 검사를 하기 때문에

--- src/main/java/chess/domain/command/MoveCommand.java
+++ src/main/java/chess/domain/command/MoveCommand.java
@@ -4,8 +4,6 @@ import chess.domain.board.position.Position;
 
 public class MoveCommand {
 
-    private static final int PARAMETER_SIZE = 2;
-
     private final Position source;
     private final Position target;
 
@@ -19,14 +17,7 @@ public class MoveCommand {
     }
 
     public static MoveCommand of(final String[] input) {
-        validateMoveCommand(input);
         return new MoveCommand(Position.of(input[1]), Position.of(input[2]));
-    }
-
-    private static void validateMoveCommand(final String[] input) {
-        if (input.length == PARAMETER_SIZE) {
-            throw new IllegalArgumentException("이동 명령으로 옳지않은 양식입니다.");
-        }
     }
 
     public Position source() {
