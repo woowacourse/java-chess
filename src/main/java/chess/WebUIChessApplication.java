deee26537cc0be942a5434e72ac4eb266e8be671
김주원 <kjw11077@naver.com>
2021-03-31 16:18:26 +0900

    feat: WebUI 컨트롤러 생성

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,22 +1,50 @@
 package chess;
 
+import static chess.web.view.RenderView.renderHtml;
 import static spark.Spark.get;
+import static spark.Spark.staticFiles;
 
-import java.util.HashMap;
+import chess.domain.board.Board;
+import chess.domain.command.Command;
+import chess.domain.game.ChessGame;
+import chess.domain.gamestate.Ready;
+import chess.exception.ChessException;
+import chess.web.dto.BoardDto;
 import java.util.Map;
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessApplication {
 
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
+        staticFiles.location("/static");
+        ChessGame chessGame = new ChessGame(new Ready(Board.createGamingBoard()));
+        chessGame.start();
+
+        get("/chess", (req, res) -> {
+            Map<String, Object> model = new BoardDto(chessGame.board()).getResult();
+            return renderHtml(model, "/index.html");
+        });
+
+        get("/chess/move", (req, res) -> {
+            String target = req.queryParams("target");
+            String source = req.queryParams("source");
+
+            Map<String, Object> model = movePiece(chessGame, target, source);
+
+            return renderHtml(model, "/index.html");
         });
     }
 
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    private static Map<String, Object> movePiece(ChessGame chessGame, String target,
+            String source) {
+        try {
+            chessGame.execute(Command.from("move " + target + " " + source));
+
+            // todo 네 마음속에 저장🌟
+            return new BoardDto(chessGame.board()).getResult();
+        } catch (ChessException e) {
+            Map<String, Object> model = new BoardDto(chessGame.board()).getResult();
+            model.put("error", String.format("<script>alert(\"%s\")</script>", e));
+            return model;
+        }
     }
 }
