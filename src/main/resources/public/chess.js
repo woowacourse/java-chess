f439cde1758835e06c377e694d4c2f0d44cda30a
pkeugine <pkeugine@gmail.com>
2021-03-31 14:44:21 +0900

    feat: 웹 애플리케이션 체스말 움직임 구현

--- src/main/resources/public/chess.js
+++ src/main/resources/public/chess.js
@@ -1,6 +1,6 @@
 const BOARD = document.querySelector("#board");
 const POSITIONS = BOARD.querySelectorAll("div");
-const API_URL = "https://localhost:4567/";
+const API_URL = "http://localhost:4567/";
 const PIECE_IMAGE_MAP = {
   "p": "./images/whitePawn.png",
   "P": "./images/blackPawn.png",
@@ -15,11 +15,14 @@ const PIECE_IMAGE_MAP = {
   "k": "./images/whiteKing.png",
   "K": "./images/blackKing.png",
 }
+let boardInfo = {};
+let positionSelected = false;
+let source = null;
 
 window.onload = () => {
   initializeBoard();
   for (const position of POSITIONS) {
-    position.addEventListener("click", something);
+    position.addEventListener("click", selectPosition);
   }
 };
 
@@ -47,6 +50,64 @@ function placePieces(responseData) {
   }
 }
 
-function something() {
+function selectPosition(event) {
+  const selectedPosition = event.target.getAttribute("id");
+  if (positionSelected === false) {
+    if (boardInfo[selectedPosition] === ".") {
+      console.log("빈 공간은 움직일 수 없습니다.")
+      return;
+    }
+    // getMovablePositions(targetPosition);
+    positionSelected = true;
+    source = event.target;
+    source.style.backgroundColor = "yellow";
+  } else {
+    console.log(selectedPosition);
+    // console.log(movablePosition);
+    // if (source !== event.target) {
+    //   alert("움직일 수 없는 위치입니다.");
+    //   return;
+    // }
+    // movablePositionSetting(false);
+    if (source === event.target) {
+      positionSelected = false;
+      source.style.backgroundColor = "";
+      source = null;
+      return;
+    }
+    const sourcePosition = source.getAttribute("id");
+    positionSelected = false;
+    source.style.backgroundColor = "";
+    movePieceByPosition(sourcePosition, selectedPosition);
+  }
+}
 
+function movePieceByPosition(source, target) {
+  const newData = {
+    "source": source,
+    "target": target
+  }
+  const option = getOption("PUT", newData);
+
+  fetch(API_URL + "move", option)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(" ");
+    }
+    return response.json();
+  })
+  .then(placePieces)
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+function getOption(methodType, bodyData) {
+  return {
+    method: methodType,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(bodyData)
+  };
 }
