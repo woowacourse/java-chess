1e8965799b4b52a1321df3d697d758853a3f5b94
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-31 21:16:29 +0900

    fix: DB connection 연결 설정 수정

--- src/main/java/chess/dao/setting/DBConnection.java
+++ src/main/java/chess/dao/setting/DBConnection.java
@@ -10,40 +10,33 @@ public class DBConnection {
     private static final String OPTION = "?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
     private static final String USER_NAME = "inbi"; //  MySQL 서버 아이디
     private static final String PASSWORD = "1234"; // MySQL 서버 비밀번호
+    private static Connection CONNECTION;
 
-    public static Connection getConnection() {
-        Connection con = null;
+    static {
+        loadJDBCDriver();
+        connectDriverManager();
+    }
 
-        // 드라이버 로딩
+    private static void loadJDBCDriver() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.err.println(" !! JDBC Driver load 오류: " + e.getMessage());
             e.printStackTrace();
         }
+    }
 
-        // 드라이버 연결
+    private static void connectDriverManager() {
         try {
-            con = DriverManager
-                .getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USER_NAME,
-                    PASSWORD);
-            // System.out.println("정상적으로 연결되었습니다.");
+            CONNECTION = DriverManager.getConnection(
+                "jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USER_NAME, PASSWORD);
         } catch (SQLException e) {
             System.err.println("연결 오류:" + e.getMessage());
             e.printStackTrace();
         }
-
-        return con;
     }
 
-    // 드라이버 연결해제
-    public static void closeConnection(Connection con) {
-        try {
-            if (con != null) {
-                con.close();
-            }
-        } catch (SQLException e) {
-            System.err.println("con 오류:" + e.getMessage());
-        }
+    public static Connection getConnection() {
+        return CONNECTION;
     }
 }
