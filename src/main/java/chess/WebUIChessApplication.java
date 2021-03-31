95cda5f984f2a1f22cbbecad1634bdfba099426d
Kimun Kim <kkm97351@gmail.com>
2021-03-31 20:33:32 +0900

    feat: 홈페이지 배경과 내부 레이아웃 설정

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -6,17 +6,20 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebUIChessApplication {
-//    public static void main(String[] args) {
-//        get("/", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return render(model, "index.html");
-//        });
-//    }
-//
-//    private static String render(Map<String, Object> model, String templatePath) {
-//        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-//    }
+    public static void main(String[] args) {
+        staticFileLocation("public");
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("text", "hello, world!");
+            return render(model, "index.html");
+        });
+    }
+
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
 }
