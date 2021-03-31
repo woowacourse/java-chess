5c5a2ec5410481ff3fd37f20676dcc5405cc61bc
pkeugine <pkeugine@gmail.com>
2021-03-31 14:44:21 +0900

    feat: 웹 애플리케이션 체스말 움직임 구현

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,6 +1,7 @@
 package chess;
 
 import static spark.Spark.get;
+import static spark.Spark.put;
 import static spark.Spark.staticFileLocation;
 
 import chess.controller.WebController;
@@ -36,6 +37,12 @@ public class WebUIChessApplication {
         get("/new", (req, res) -> {
             Map<String, String> model = webController.startGame();
 //            return render(model, "board.html");
+            return gson.toJson(model);
+        });
+
+        put("/move", (req, res) -> {
+            Map<String, String> requestBody = gson.fromJson(req.body(), HashMap.class);
+            Map<String, String> model = webController.move(requestBody.get("source"), requestBody.get("target"));
             return gson.toJson(model);
         });
     }
--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -6,6 +6,7 @@ import chess.domain.game.state.InitialState;
 import chess.domain.piece.Piece;
 import chess.domain.position.Position;
 import chess.domain.result.Result;
+import chess.view.OutputView;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -21,6 +22,17 @@ public class WebController {
     public Map<String, String> startGame() {
         CommandAsString command = new CommandAsString("start");
         game = game.execute(command);
+        Result result = game.result(command);
+        return convertToStringMap(result.infoAsMap());
+    }
+
+    public Map<String, String> move(String source, String target) {
+        CommandAsString command = new CommandAsString("move " + source + " " + target);
+        try {
+            game = game.execute(command);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e);
+        }
         Result result = game.result(command);
         return convertToStringMap(result.infoAsMap());
     }
