332cfe4f028505154a7ac46577d0fabc7cb6369b
jaeseongDev <qkrwotjd1445@naver.com>
2021-03-31 18:23:32 +0900

    test : PieceDAO에 대한 테스트 코드 추가

--- src/main/java/chess/dto/PieceDto.java
+++ src/main/java/chess/dto/PieceDto.java
@@ -1,8 +1,14 @@
 package chess.dto;
 
+import chess.domain.piece.Color;
+import chess.domain.piece.Piece;
+import chess.domain.piece.PieceFactory;
 import chess.dto.responsedto.ResponseDto;
 
 public class PieceDto implements ResponseDto {
+    private static final int PIECE_NAME_INDEX = 0;
+    private static final int Y_POSITION_INDEX = 1;
+    private static final int X_POSITION_INDEX = 0;
     private final long pieceId;
     private final boolean isBlack;
     private final String position;
@@ -35,6 +41,22 @@ public class PieceDto implements ResponseDto {
 
     public String getName() {
         return name;
+    }
+
+    public Piece toEntity() {
+        Color color = null;
+        if (isBlack()) {
+            color = Color.BLACK;
+        }
+        if (!isBlack()) {
+            color = Color.WHITE;
+        }
+        return PieceFactory.from(
+                name.charAt(PIECE_NAME_INDEX),
+                color,
+                position.charAt(X_POSITION_INDEX),
+                position.charAt(Y_POSITION_INDEX)
+        );
     }
 
     @Override
