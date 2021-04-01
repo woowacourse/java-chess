a02efc208799f6e5be38a5895d8ccd79908ebcf8
Seed <hsik0225@gmail.com>
2021-04-02 03:07:10 +0900

    feat(user-dao):이미 존재하는 이름일 경우 DB에 추가안하는 기능 구현

--- src/main/java/chess/domain/user/UserDAO.java
+++ src/main/java/chess/domain/user/UserDAO.java
@@ -2,17 +2,23 @@ package chess.domain.user;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import chess.domain.ConnectionUtils;
 
 public class UserDAO {
     public void addUser(User user) throws SQLException {
-        String query = "INSERT INTO user (nickname) VALUES (?)";
+        String findQuery = "SELECT nickname FROM user WHERE nickname = (?)";
         try (Connection connection = ConnectionUtils.getConnection();
-             PreparedStatement pstmt = connection.prepareStatement(query)) {
+             PreparedStatement pstmt = connection.prepareStatement(findQuery)) {
             pstmt.setString(1, user.getNickname());
-            pstmt.executeUpdate();
+            if (!pstmt.executeQuery().first()) {
+                String query = "INSERT INTO user (nickname) VALUES (?)";
+                PreparedStatement pstmtTwo = connection.prepareStatement(query);
+                pstmtTwo.setString(1, user.getNickname());
+                pstmtTwo.executeUpdate();
+            }
         }
     }
 }
