feb5e7f396d3aee16125c34962ab6d7f998a54c4
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-04-02 00:45:58 +0900

    refactor(ChessGameDTO): DTO 내의 로직 제거

---
+++ src/main/java/chess/webdto/ChessGameDTO.java
@@ -0,0 +1,40 @@
+package chess.webdto;
+
+import java.util.Map;
+
+public class ChessGameDTO {
+    private final Map<String, Map<String, String>> piecePositionByTeam;
+    private final String currentTurnTeam;
+    private final double whiteTeamScore;
+    private final double blackTeamScore;
+    private final boolean isPlaying;
+
+    public ChessGameDTO(final Map<String, Map<String, String>> piecePositionByTeam, final String currentTurnTeam,
+                         final double whiteTeamScore, final double blackTeamScore, final boolean isPlaying) {
+        this.piecePositionByTeam = piecePositionByTeam;
+        this.whiteTeamScore = whiteTeamScore;
+        this.blackTeamScore = blackTeamScore;
+        this.currentTurnTeam = currentTurnTeam;
+        this.isPlaying = isPlaying;
+    }
+
+    public Map<String, Map<String, String>> getPiecePositionByTeam() {
+        return piecePositionByTeam;
+    }
+
+    public String getCurrentTurnTeam() {
+        return currentTurnTeam;
+    }
+
+    public double getWhiteTeamScore() {
+        return whiteTeamScore;
+    }
+
+    public double getBlackTeamScore() {
+        return blackTeamScore;
+    }
+
+    public boolean getIsPlaying() {
+        return isPlaying;
+    }
+}
