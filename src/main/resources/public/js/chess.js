bcd50457f256aa6de0ac4e08efe2f2fa78aabcce
hoony <younghoon.kwon91@gmail.com>
2021-04-01 14:23:46 +0900

    refactor: js .then() 함수들 async await으로 리팩토링

--- src/main/resources/public/js/chess.js
+++ src/main/resources/public/js/chess.js
@@ -16,9 +16,10 @@ unicodeMap = {
     ".": ""
 }
 
-function addClickListener(){
+function addClickListener() {
     document.getElementById("chessboard").addEventListener("click", selectPieces)
     document.getElementById("startButton").addEventListener("click", start)
+    document.getElementById("homeButton").addEventListener("click", refresh)
     document.getElementById("saveButton").addEventListener("click", save)
     document.getElementById("loadButton").addEventListener("click", load)
 }
@@ -26,127 +27,115 @@ function addClickListener(){
 function showAndHideButtons() {
     document.getElementById("saveButton").style.display = null;
     document.getElementById("loadButton").style.display = "none";
+    document.getElementById("homeButton").style.display = null;
     document.getElementById("startButton").innerText = "RESET";
 }
 
-function init(){
-    fetch('http://localhost:4567/pieces')
-        .then(response => response.json())
-        .then(function (pieces){
-            for(let position in pieces){
-                document.getElementById(position).innerText = unicodeMap[pieces[position]]
-            }
-        });
+function refresh() {
+    location.reload();
 }
 
-async function start(){
+async function loadGrid() {
+    let response = await fetch('http://localhost:4567/pieces')
+    response = await response.json()
+    for(let position in response){
+        document.getElementById(position).innerText = unicodeMap[response[position]]
+    }
+}
+
+async function start() {
     await postData("/reset")
     await postData("/start")
-    init()
+    loadGrid()
     showTurn()
     showScores()
     showAndHideButtons()
 }
 
-function save(){
-    let response = postData("/save")
-    response.then(function (result){
-        if(result == 200){
-            alert("게임을 저장합니다.")
-        }
-    })
+async function save() {
+    let response = await postData("/save")
+    if (response === 200) {
+        alert("게임을 저장합니다.")
+    }
 }
 
-function load(){
-    let response = postData("/load")
-    response.then(function (result){
-        if(result == 200){
-            alert("게임을 불러옵니다.")
-        }
-        start()
-    })
+async function load() {
+    let response = await postData("/load")
+    if (response === 200) {
+        alert("게임을 불러옵니다.")
+    }
+    await postData("/start")
+    loadGrid()
+    showTurn()
+    showScores()
+    showAndHideButtons()
 }
 
 async function move(source, target) {
-    let response = postData("/move", {source:source.id, target:target.id})
-    await response.then(function (result){
-        if(result == 400){
-            alert("잘못된 움직임 입니다.")
-        }
-        if(result == 200){
-            let sourceText = document.getElementById(source.id).textContent;
-            document.getElementById(target.id).innerText = sourceText;
-            document.getElementById(source.id).innerText = "";
-        }
-    })
+    let response = await postData("/move", {source: source.id, target: target.id})
+    if (response === 400) {
+        alert("잘못된 움직임 입니다.")
+    }
+    if (response === 200) {
+        let sourceText = document.getElementById(source.id).textContent;
+        document.getElementById(target.id).innerText = sourceText;
+        document.getElementById(source.id).innerText = "";
+    }
     showTurnReverse()
     showScores()
     checkGameOver()
 }
 
-function showTurn(){
-    fetch('http://localhost:4567/turn')
-        .then(response => response.json())
-        .then(function (turn){
-            document.getElementById("status").innerText = "TURN: " + turn;
-        });
+async function showTurn() {
+    let turn = await fetch('http://localhost:4567/turn')
+    turn = await turn.json()
+    document.getElementById("status").innerText = "TURN: " + turn;
 }
 
-function showTurnReverse(){
-    fetch('http://localhost:4567/turn')
-        .then(response => response.json())
-        .then(function (turn){
-            if(turn == "BLACK"){
-                turn = "WHITE"
-            }
-            else{
-                turn = "BLACK"
-            }
-            document.getElementById("status").innerText = "TURN: " + turn;
-        });
+async function showTurnReverse() {
+    let turn = fetch('http://localhost:4567/turn')
+    if (turn == "BLACK") {
+        turn = "WHITE"
+    } else {
+        turn = "BLACK"
+    }
+    document.getElementById("status").innerText = "TURN: " + turn;
 }
 
-function showScores(){
-    fetch('http://localhost:4567/score/black')
-        .then(response => response.json())
-        .then(function (score){
-            document.getElementById("blackScore").innerText = "BLACK SCORE: " + score;
-        });
-    fetch('http://localhost:4567/score/white')
-        .then(response => response.json())
-        .then(function (score){
-            document.getElementById("whiteScore").innerText = "WHITE SCORE: " + score;
-        });
+async function showScores() {
+    let blackScore = await fetch('http://localhost:4567/score/black')
+    blackScore = await blackScore.json()
+    document.getElementById("blackScore").innerText = "BLACK SCORE: " + blackScore;
+
+    let whiteScore = await fetch('http://localhost:4567/score/white')
+    whiteScore = await whiteScore.json()
+    document.getElementById("whiteScore").innerText = "WHITE SCORE: " + whiteScore;
 }
 
-function checkGameOver(){
-    fetch('http://localhost:4567/gameover')
-        .then(response => response.json())
-        .then(function (gameOver){
-            if(gameOver[0] == true){
-                alert("게임 종료!" + gameOver[1]+ " 승리!")
-                document.getElementById("status").innerText = gameOver[1]+ " 승리!";
-            }
-        });
+async function checkGameOver() {
+    let response = await fetch('http://localhost:4567/gameover')
+    response = await response.json()
+    if (response[0] === true) {
+        alert("게임 종료!" + response[1] + " 승리!")
+        document.getElementById("status").innerText = response[1] + " 승리!";
+    }
 }
 
-function selectPieces(event){
+async function selectPieces(event) {
     let source = clickedPieces();
     let target = event.target;
-    if(source){
-        if(source != target){
-            move(source, target)
-            source.classList.toggle("clicked")
-            target.classList.toggle("clicked")
-        }
+    if (source && source !== target) {
+        await move(source, target)
+        source.classList.toggle("clicked")
+        target.classList.toggle("clicked")
     }
     event.target.classList.toggle("clicked")
 }
 
-function clickedPieces(){
+function clickedPieces() {
     let pieces = document.querySelector("#chessboard").querySelectorAll("div")
-    for(let i = 0; i < pieces.length; i++){
-        if(pieces[i].classList.contains("clicked")){
+    for (let i = 0; i < pieces.length; i++) {
+        if (pieces[i].classList.contains("clicked")) {
             return pieces[i];
         }
     }
