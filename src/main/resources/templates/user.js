edd84b32eb83896a65f0691ae72023cd74d16144
y_woo :) <dusdn1702@naver.com>
2021-04-01 17:35:00 +0900

    refactor: 체스판 수정

--- src/main/resources/templates/user.js
+++ src/main/resources/templates/user.js
@@ -9,37 +9,3 @@ function addUser() {
 function alertSuccess() {
     alert('회원 추가 성공');
 }
-
-function alertUser() {
-    // console.log(document.getElementById("name").onsubmit);
-    // if(document.getElementById("name").onsubmit == false)
-    // alert('로그인 성공');
-}
-
-// async function startGame(event) {
-//     console.log(event.target);
-//     let data = {
-//         name: document.getElementById("login"),
-//         password: document.getElementById("signup")
-//     }
-//     await fetch('/findUser', {
-//         method: 'POST',
-//         body: JSON.stringify(data),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }).then(function (res) {
-//         console.log(res);
-//         if (res == null) {
-//             alert('등록되지 않은 사용자입니다.');
-//         } else {
-//             fetch('/user', {
-//                 method: 'POST',
-//                 body: res,
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 }
-//             }).then(res => res.json());
-//         }
-//     });
-// }
