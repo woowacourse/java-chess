c58fde0eca9d2139c1cbd74c656143707eeb2cdf
RinSabbit <celena1004@naver.com>
2021-04-03 01:38:58 +0900

    refactor: 게임 시작시 비동기적으로 체스판과 점수가 초기화됨

--- src/main/java/chess/controller/WebUIChessController.java
+++ src/main/java/chess/controller/WebUIChessController.java
@@ -1,43 +1,76 @@
 package chess.controller;
 
+import static chess.util.JsonParser.json;
 import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.path;
+import static spark.Spark.post;
+import static spark.route.HttpMethod.post;
 
 import chess.DTO.BoardDto;
+import chess.DTO.ScoreDto;
+import chess.domain.board.Board;
 import chess.domain.board.BoardFactory;
-import com.google.gson.Gson;
+import chess.domain.piece.PieceColor;
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebUIChessController {
+
+    private final Board board;
+
+    public WebUIChessController() {
+        board = BoardFactory.initializeBoard();
+        run();
+    }
+
     public void run() {
-        staticFileLocation("/templates");
-        Gson gson = new Gson();
+        get("/", this::renderInitBoard);
+        get("/board", this::getBoard, json());
 
-        get("/", (req, res) -> {
-            Map<String, String> model = new HashMap<>();
-            return render(model, "index.html");
-        });
+        path("/board", () ->{
+            get("/status", this::isEnd, json());
+            get("/turn", this::isWhiteTurn, json());
+            get("/score", this::getScore, json());
 
-        get("/start", (req, res) -> {
-            BoardDto boardDto = new BoardDto(BoardFactory.initializeBoard());
-            return render(boardDto.getBoard(), "game.html");
-        });
-
-//        get("/path", (req, res) -> {
-//            BoardDto boardDto = new BoardDto();
-//            return render(boardDto.getBoard(), "game.html");
-//        });
-
-        get("/move", (req, res) -> {
-            Map<String, String> model = new HashMap<>();
-            return render(model, "game.html");
+            post("/move", this::move, json());
+            post("/move", this::movablePath, json());
         });
     }
 
-    private static String render(Map<String, String> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    private boolean movablePath(final Request request, final Response response) {
+        return false;
+    }
+
+    private boolean move(final Request request, final Response response){
+        return false;
+    }
+
+    private boolean isEnd(final Request request, final Response response) {
+        return false;
+    }
+
+    private Map<PieceColor, Double> getScore(final Request request, final Response response) {
+        return new ScoreDto(board).getScores();
+    }
+
+    private Object isWhiteTurn(final Request request, final Response response) {
+        return null;
+    }
+
+    private BoardDto getBoard(final Request request, final Response response) {
+        return new BoardDto(board);
+    }
+
+    private String renderInitBoard(Request request, Response response) {
+        response.type("text/html");
+        return render(new HashMap<>());
+    }
+
+    private static String render(Map<String, String> model) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, "index.html"));
     }
 }
