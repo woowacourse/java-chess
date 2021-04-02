430598d97070dc7bec50fd051c8bdf9df8448932
hoony <younghoon.kwon91@gmail.com>
2021-04-02 20:20:43 +0900

    refactor: Enum PieceMapper 리팩토링

--- src/main/java/chess/domain/grid/PieceMapper.java
+++ src/main/java/chess/domain/grid/PieceMapper.java
@@ -7,35 +7,27 @@ import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public enum PieceMapper {
-    WHITE_BISHOP('b', Color.WHITE, Bishop::new),
-    BLACK_BISHOP('B', Color.BLACK, Bishop::new),
-    WHITE_KING('k', Color.WHITE, King::new),
-    BLACK_KING('K', Color.BLACK, King::new),
-    WHITE_PAWN('p', Color.WHITE, Pawn::new),
-    BLACK_PAWN('P', Color.BLACK, Pawn::new),
-    WHITE_QUEEN('q', Color.WHITE, Queen::new),
-    BLACK_QUEEN('Q', Color.BLACK, Queen::new),
-    WHITE_ROOK('r', Color.WHITE, Rook::new),
-    BLACK_ROOK('R', Color.BLACK, Rook::new),
-    WHITE_KNIGHT('n', Color.WHITE, Knight::new),
-    BLACK_KNIGHT('N', Color.BLACK, Knight::new),
-    EMPTY('.', Color.BLACK, Empty::new);
+    BISHOP('b', Bishop::new),
+    KING('k', King::new),
+    PAWN('p', Pawn::new),
+    QUEEN('q', Queen::new),
+    ROOK('r', Rook::new),
+    KNIGHT('n', Knight::new),
+    EMPTY('.', Empty::new);
 
     private final char name;
-    private final Color color;
-    private final BiFunction<Color, Position, Piece> pieceBuilder;
+    private final BiFunction<Color, Position, Piece> builder;
 
-    PieceMapper(final char name, final Color color, final BiFunction<Color, Position, Piece> pieceBuilder) {
+    PieceMapper(final char name, final BiFunction<Color, Position, Piece> builder) {
         this.name = name;
-        this.color = color;
-        this.pieceBuilder = pieceBuilder;
+        this.builder = builder;
     }
 
     public static Piece of(final char name, final Position position) {
         return Arrays.stream(values())
-                .filter(mapper -> mapper.name == name)
+                .filter(mapper -> mapper.name == Character.toLowerCase(name))
                 .findAny()
-                .map(pieceMapper -> pieceMapper.pieceBuilder.apply(pieceMapper.color, position))
+                .map(pieceMapper -> pieceMapper.builder.apply(Color.of(name), position))
                 .orElseThrow(() -> new IllegalArgumentException("매칭 되는 피스가 존재하지 않습니다."));
     }
 }
--- src/main/java/chess/domain/piece/Color.java
+++ src/main/java/chess/domain/piece/Color.java
@@ -1,5 +1,12 @@
 package chess.domain.piece;
 
 public enum Color {
-    WHITE, BLACK
+    WHITE, BLACK;
+
+    public static Color of(char name){
+        if(Character.isLowerCase(name)){
+            return Color.WHITE;
+        }
+        return Color.BLACK;
+    }
 }
