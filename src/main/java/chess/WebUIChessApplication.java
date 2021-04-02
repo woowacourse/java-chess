0dbdc0f3752d86cd2598dd735c30fe9ddee81f8a
RinSabbit <celena1004@naver.com>
2021-04-03 01:37:38 +0900

    chore: 파일 이동

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,11 +1,20 @@
 package chess;
 
+import static spark.Spark.before;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
 import chess.controller.WebUIChessController;
 
 public class WebUIChessApplication {
 
     public static void main(String[] args) {
+        port(8080);
+        staticFileLocation("/public");
+        before(((request, response) -> {
+            response.type("application/json");
+        }));
+
         WebUIChessController webController = new WebUIChessController();
-        webController.run();
     }
 }
