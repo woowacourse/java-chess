ec3184d8920748a3b3cf3c3c9d0131c18ec06d52
Kimun Kim <kkm97351@gmail.com>
2021-04-04 01:36:36 +0900

    feat: 웹 클릭하여 움직이는 기능과 서버의 /move API 구현

--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -1,12 +1,14 @@
 package chess.controller;
 
 import chess.domain.ChessGameManager;
+import chess.domain.position.Position;
 import chess.dto.CommonDto;
 import chess.dto.GameStatusDto;
 import chess.dto.StatusCode;
 import com.google.gson.Gson;
+import org.json.JSONObject;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebController {
     private final Gson gson = new Gson();
@@ -21,6 +23,18 @@ public class WebController {
             chessGameManager.start();
             return gson.toJson(new CommonDto<GameStatusDto>(StatusCode.OK,
                     "New game has been created successfully",
+                    GameStatusDto.from(chessGameManager.getBoard(), chessGameManager.getCurrentTurnColor())));
+        });
+
+
+        post("/move", "application/json", (req, res) -> {
+            JSONObject jsonObject = new JSONObject(req.body());
+            String from = jsonObject.getString("from");
+            String to = jsonObject.getString("to");
+
+            chessGameManager.move(Position.of(from), Position.of(to));
+            return gson.toJson(new CommonDto<GameStatusDto>(StatusCode.OK,
+                    "A piece has been moved successfully",
                     GameStatusDto.from(chessGameManager.getBoard(), chessGameManager.getCurrentTurnColor())));
         });
     }
--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -1,15 +1,65 @@
 const BLOCK_SIZE_PIXEL = 80;
 
+const $chessBoard = document.getElementById('chess-board');
 const $controlPanel = document.getElementById('control-panel');
 const $startButton = document.getElementById('start-button');
+
+const squareBuffer = new SquareBuffer();
+
+addSelectionEventOnChessBoard();
 addEventOnStartButton();
+
+function processResponse(responseJsonBody, successScenarioFunction) {
+    if (Math.floor(responseJsonBody.statusCode / 100) === 2) {
+        successScenarioFunction();
+    }
+}
+
+function SquareBuffer() {
+    this.buffer = [];
+    this.add = addAndRequestMove;
+}
+
+function addAndRequestMove(square) {
+    this.buffer.push(square);
+    if (this.buffer.length === 2) {
+        const toSquare = this.buffer.pop();
+        const fromSquare = this.buffer.pop();
+        console.log(`request [POST]/move, body: from: ${fromSquare.id}, \nto: ${toSquare.id}\n`);
+
+        try {
+            fetch("/move", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    from: fromSquare.id,
+                    to: toSquare.id,
+                }),
+            })
+                .then(res => res.json())
+                .then(res => processResponse(res, () => updateBoard(res.item.chessBoard)));
+        } catch
+            (error) {
+            console.error(error.messages);
+        }
+    }
+}
+
+async function addSelectionEventOnChessBoard() {
+    $chessBoard.addEventListener('click', event => {
+        const selectedSquare = event.target.closest('div');
+        squareBuffer.add(selectedSquare);
+    })
+}
 
 async function addEventOnStartButton() {
     await $startButton.addEventListener('click', event => {
         try {
             fetch('/newgame')
                 .then(res => res.json())
-                .then(res => updateBoard(res.item.chessBoard));
+                .then(res => processResponse(res, () => updateBoard(res.item.chessBoard)));
             $startButton.style.display = 'none';
         } catch (error) {
             console.error(error.messages);
@@ -17,14 +67,10 @@ async function addEventOnStartButton() {
     });
 }
 
-function makeImage(imageName) {
-    const img = document.createElement('img');
-    img.setAttribute('src', '/images/' + imageName + '.png');
-    img.height = BLOCK_SIZE_PIXEL;
-    return img;
-}
-
 function updateBoard(piecesMap) {
+    for (const square of $chessBoard.querySelectorAll('div')) {
+        square.innerHTML = '';
+    }
     for (const [position, piece] of Object.entries(piecesMap)) {
         updateSqaure(position, piece);
     }
@@ -33,4 +79,11 @@ function updateBoard(piecesMap) {
 async function updateSqaure(position, piece) {
     const square = document.getElementById(position);
     square.appendChild(makeImage(piece.color + '-' + piece.notation));
+}
+
+function makeImage(imageName) {
+    const img = document.createElement('img');
+    img.setAttribute('src', '/images/' + imageName + '.png');
+    img.height = BLOCK_SIZE_PIXEL;
+    return img;
 }
