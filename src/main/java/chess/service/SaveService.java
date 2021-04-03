903516b009be124f93f884cccfe662ac10b53917
Minsung <sns2831@gmail.com>
2021-04-03 15:49:04 +0900

    fix: 게임 저장시 버그 수정

--- src/main/java/chess/service/SaveService.java
+++ src/main/java/chess/service/SaveService.java
@@ -21,13 +21,22 @@ public class SaveService {
 
         try {
             ChessGame chessGame = GameRepository.findByGameIdFromCache(gameId);
-            GameRepository.updateToDB(gameId, chessGame);
+            saveGameToDB(chessGame);
         } catch (RuntimeException | SQLException e) {
             response.status(400);
             return new MessageDto(e.getMessage());
         }
 
         return new MessageDto("저장 완료");
+    }
+
+    private void saveGameToDB(ChessGame chessGame) throws SQLException {
+        if(GameRepository.isGameIdExistingInDB(gameId)) {
+            GameRepository.updateToDB(gameId, chessGame);
+            return;
+        }
+
+        GameRepository.saveToDB(gameId, chessGame);
     }
 
 }
