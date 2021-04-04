ff73e80506cf1dde77244ec072ed9dcbb1e78990
naeunkim <knae11@naver.com>
2021-04-04 14:44:59 +0900

    refactor: BlankTest 수정

--- src/test/java/chess/domain/piece/BlankTest.java
+++ src/test/java/chess/domain/piece/BlankTest.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BlankTest {
-
     @Test
-    @DisplayName("빈공간 객체 생성")
+    @DisplayName("캐싱된 빈 객체 생성")
     void create() {
-        assertThat(Blank.INSTANCE.getPieceName()).isEqualTo(".");
+        assertThat(Blank.INSTANCE).isSameAs(Blank.INSTANCE);
     }
 
     @Test
-    @DisplayName("공백은 움직일 수 없다.")
-    void fail_move_position() {
-        //todo: 수정
+    @DisplayName("빈공간 객체 이름 가져오")
+    void name() {
+        assertThat(Blank.INSTANCE.getPieceName()).isEqualTo(".");
     }
 
 }
