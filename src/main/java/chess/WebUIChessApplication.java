72da9a5fb4c722721401da9806eab9ac03b6cdca
yoonsung.jung <ybook2012@gmail.com>
2021-04-05 01:14:57 +0900

    refactor : compile을 implementation으로 교체, 주석제거

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -6,7 +6,6 @@ import static spark.Spark.staticFiles;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
-        //Spark.port(3693);
         staticFiles.location("/static");
 
         WebUIChessController webUIChessController = new WebUIChessController();
