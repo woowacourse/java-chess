083fcaf396cd0adb321d7f3c90029805603e4ef7
xrabcde <xrabcde@gmail.com>
2021-04-05 00:41:38 +0900

    test: 테스트코드에 접근제어자 추가

--- src/test/java/chess/dao/PieceDaoTest.java
+++ src/test/java/chess/dao/PieceDaoTest.java
@@ -17,7 +17,7 @@ public class PieceDaoTest {
     @Test
     @DisplayName("db 초기세팅이 잘 되는지 확인")
     void clear() throws Exception {
-        Pieces pieces = new Pieces();
+        final Pieces pieces = new Pieces();
         pieces.init();
         pieceDao.clear(pieces);
     }
@@ -25,7 +25,7 @@ public class PieceDaoTest {
     @Test
     @DisplayName("바뀐 턴이 db에 잘 저장되는지 확인")
     void updateTurn() throws Exception {
-        Turn turn = new Turn();
+        final Turn turn = new Turn();
         pieceDao.initTurn();
         pieceDao.updateTurn(turn);
     }
