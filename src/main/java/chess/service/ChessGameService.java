baf0af3d99167966306428c8ad5712b9b88a63a9
dwl5 <ssop6403@gmail.com>
2021-04-05 04:13:04 +0900

    refactor: ChessGameService 코드 정리

--- src/main/java/chess/service/ChessGameService.java
+++ src/main/java/chess/service/ChessGameService.java
@@ -21,7 +21,7 @@ public class ChessGameService {
     private Gson gson;
 
 
-    public ChessGameService(ChessRepository chessRepository) {
+    public ChessGameService(final ChessRepository chessRepository) {
         this.chessRepository = chessRepository;
         this.gson = new Gson();
     }
@@ -32,51 +32,43 @@ public class ChessGameService {
         return new ResponseDTO(true, gson.toJson(chessGameDTO), "게임이 생성 되었습니다.");
     }
 
-    public ResponseDTO refreshChessGame(String roomId) {
+    public ResponseDTO refreshChessGame(final String roomId) {
         RoomDTO roomDTO = chessRepository.findRoomFromId(roomId);
         ChessGameDTO chessGameDTO = createChessGameDTO(new ChessGame(new BlackTeam(), new WhiteTeam()));
         chessRepository.saveChessGame(roomDTO.getGameId(), gson.toJson(chessGameDTO));
         return new ResponseDTO(true, gson.toJson(chessGameDTO), "게임이 재시작 되었습니다.");
     }
 
-    public ResponseDTO loadChessGame(String roomId) {
+    public ResponseDTO loadChessGame(final String roomId) {
         RoomDTO roomDTO = chessRepository.findRoomFromId(roomId);
         String chessData = chessRepository.loadChessGame(roomDTO.getGameId());
         return new ResponseDTO(true, chessData, "게임을 로드 하였습니다.");
     }
 
-    public Object endChessGame(final String gameId) {
-        ChessGameDTO chessGameDTO = gson.fromJson(chessRepository.loadChessGame(gameId), ChessGameDTO.class);
-        ChessGame chessGame = createChessGame(chessGameDTO);
-        chessGame.finish();
-        ChessGameDTO movedChessGameDTO = createChessGameDTO(chessGame);
-        chessRepository.saveChessGame(gameId, gson.toJson(movedChessGameDTO));
-        return new ResponseDTO(true, gson.toJson(movedChessGameDTO), "게임을 종료 했습니다.");
-    }
-
-    public ResponseDTO selectPiece(String roomId, String selected) {
+    public ResponseDTO selectPiece(final String roomId, final String selected) {
         Position selectedPosition = Position.of(selected);
+
         RoomDTO roomDTO = chessRepository.findRoomFromId(roomId);
         String chessGameData = chessRepository.loadChessGame(roomDTO.getGameId());
         ChessGameDTO chessGameDTO = gson.fromJson(chessGameData, ChessGameDTO.class);
         ChessGame chessGame = createChessGame(chessGameDTO);
-        boolean havePiece = chessGame.havePieceInCurrentTurn(selectedPosition);
 
+        boolean havePiece = chessGame.havePieceInCurrentTurn(selectedPosition);
         if (havePiece) {
             return new ResponseDTO(true, "", "기물을 선택 하셨습니다.");
         }
         return new ResponseDTO(false, "", "잘못 선택 하셨습니다.");
     }
 
-    public ResponseDTO moveChessGame(String roomId, String selected, String target) {
+    public ResponseDTO moveChessGame(final String roomId, final String selected, final String target) {
         Position selectedPosition = Position.of(selected);
         Position targetPosition = Position.of(target);
 
         RoomDTO roomDTO = chessRepository.findRoomFromId(roomId);
-        System.out.println(roomDTO);
         String chessGameData = chessRepository.loadChessGame(roomDTO.getGameId());
         ChessGameDTO chessGameDTO = gson.fromJson(chessGameData, ChessGameDTO.class);
         ChessGame chessGame = createChessGame(chessGameDTO);
+
         boolean isSuccess = chessGame.move(selectedPosition, targetPosition);
         if (isSuccess) {
             ChessGameDTO movedChessGameDTO = createChessGameDTO(chessGame);
@@ -86,7 +78,7 @@ public class ChessGameService {
         return new ResponseDTO(false, "", "움직일 수 없습니다.");
     }
 
-    public ResponseDTO createRoom(String data) {
+    public ResponseDTO createRoom(final String data) {
         createChessGame();
         RoomDTO roomDTO = gson.fromJson(data, RoomDTO.class);
         RoomsDTO roomsDTO = chessRepository.createRoom(roomDTO.getName(), roomDTO.getPw());
@@ -98,10 +90,26 @@ public class ChessGameService {
         return new ResponseDTO(true, gson.toJson(roomsDTO), "방을 가져왔습니다.");
     }
 
-    private ChessGame createChessGame(ChessGameDTO chessGameDTO) {
-        System.out.println("createChessGame() called.");
-        System.out.println(gson.toJson(chessGameDTO));
+    private ChessGame createChessGame(final ChessGameDTO chessGameDTO) {
         TeamDTO blackTeamDTO = chessGameDTO.getBlackTeam();
+        BlackTeam blackTeam = createBlackTeam(blackTeamDTO);
+
+        TeamDTO whiteTeamDTO = chessGameDTO.getWhiteTeam();
+        WhiteTeam whiteTeam = createWhiteTeam(whiteTeamDTO);
+
+        Team currentTurn = whiteTeam;
+        if (blackTeam.isCurrentTurn()) {
+            currentTurn = blackTeam;
+        }
+
+        blackTeam.setEnemy(whiteTeam);
+        whiteTeam.setEnemy(blackTeam);
+
+        return new ChessGame(blackTeam, whiteTeam, currentTurn, !chessGameDTO.isRunning());
+    }
+
+
+    private BlackTeam createBlackTeam(final TeamDTO blackTeamDTO) {
         PiecesDTO blackPiecesDTO = blackTeamDTO.getPiecesDto();
         List<PieceDTO> blackPieceDTOs = blackPiecesDTO.getPieceDtoList();
         Map<Position, Piece> blackPiecePositions = new HashMap<>();
@@ -111,9 +119,12 @@ public class ChessGameService {
             Piece piece = PieceConverter.convertToPiece(pieceDTO.getPiece());
             blackPiecePositions.put(position, piece);
         }
-        BlackTeam blackTeam = new BlackTeam(blackTeamDTO.getName(), blackTeamDTO.isTurn(), blackPiecePositions);
 
-        TeamDTO whiteTeamDTO = chessGameDTO.getWhiteTeam();
+        BlackTeam blackTeam = new BlackTeam(blackTeamDTO.getName(), blackTeamDTO.isTurn(), blackPiecePositions);
+        return blackTeam;
+    }
+
+    private WhiteTeam createWhiteTeam(final TeamDTO whiteTeamDTO) {
         PiecesDTO whitePiecesDTO = whiteTeamDTO.getPiecesDto();
         List<PieceDTO> whitePieceDTOs = whitePiecesDTO.getPieceDtoList();
         Map<Position, Piece> whitePiecePositions = new HashMap<>();
@@ -125,57 +136,40 @@ public class ChessGameService {
         }
 
         WhiteTeam whiteTeam = new WhiteTeam(whiteTeamDTO.getName(), whiteTeamDTO.isTurn(), whitePiecePositions);
-
-        Team currentTurn = whiteTeam;
-        if (blackTeam.isCurrentTurn()) {
-            currentTurn = blackTeam;
-        }
-
-        blackTeam.setEnemy(whiteTeam);
-        whiteTeam.setEnemy(blackTeam);
-
-        ChessGame chessGame = new ChessGame(blackTeam, whiteTeam, currentTurn, !chessGameDTO.isRunning());
-        return chessGame;
+        return whiteTeam;
     }
 
-
     private ChessGameDTO createChessGameDTO(final ChessGame chessGame) {
-        final Map<Position, String> blackPieces = convertToBlackPrintName(chessGame.getBlackTeam().getPiecePosition());
-        final Map<Position, String> whitePieces = convertToWhitePrintName(chessGame.getWhiteTeam().getPiecePosition());
+        Map<Position, String> blackPieces = convertToBlackPrintName(chessGame.getBlackTeam().getPiecePosition());
+        Map<Position, String> whitePieces = convertToWhitePrintName(chessGame.getWhiteTeam().getPiecePosition());
 
-
-        List<PieceDTO> blackPieceDtos = new ArrayList<>();
-
-        for (Map.Entry<Position, String> entry : blackPieces.entrySet()) {
-            blackPieceDtos.add(new PieceDTO(entry.getKey().getKey(), entry.getValue()));
-        }
-
-        PiecesDTO blackPiecesDto = new PiecesDTO(blackPieceDtos);
-
-        List<PieceDTO> whitePieceDtos = new ArrayList<>();
-
-        for (Map.Entry<Position, String> entry : whitePieces.entrySet()) {
-            whitePieceDtos.add(new PieceDTO(entry.getKey().getKey(), entry.getValue()));
-        }
-
-        PiecesDTO whitePiecesDto = new PiecesDTO(whitePieceDtos);
+        PiecesDTO blackPiecesDto = createPiecesDTO(blackPieces);
+        PiecesDTO whitePiecesDto = createPiecesDTO(whitePieces);
 
         TeamDTO whiteTeamDTO = new TeamDTO(whitePiecesDto, chessGame.getWhiteTeam().getName(),
                 String.valueOf(chessGame.getWhiteTeam().calculateTotalScore()),
                 chessGame.getWhiteTeam().isCurrentTurn());
 
-
         TeamDTO blackTeamDTO = new TeamDTO(blackPiecesDto, chessGame.getBlackTeam().getName(),
                 String.valueOf(chessGame.getBlackTeam().calculateTotalScore()),
                 chessGame.getBlackTeam().isCurrentTurn());
 
+        return new ChessGameDTO(blackTeamDTO, whiteTeamDTO, !chessGame.isEnd());
+    }
 
-        ChessGameDTO chessGameDto = new ChessGameDTO(blackTeamDTO, whiteTeamDTO, !chessGame.isEnd());
-        return chessGameDto;
+    private PiecesDTO createPiecesDTO(final Map<Position, String> pieces) {
+        List<PieceDTO> pieceDTOs = new ArrayList<>();
+
+        for (Map.Entry<Position, String> entry : pieces.entrySet()) {
+            pieceDTOs.add(new PieceDTO(entry.getKey().getKey(), entry.getValue()));
+        }
+
+        PiecesDTO piecesDTO = new PiecesDTO(pieceDTOs);
+        return piecesDTO;
     }
 
     private Map<Position, String> convertToBlackPrintName(final Map<Position, Piece> pieces) {
-        final Map<Position, String> blackPrintFormat = new HashMap<>();
+        Map<Position, String> blackPrintFormat = new HashMap<>();
         for (Position position : pieces.keySet()) {
             final Piece piece = pieces.get(position);
             blackPrintFormat.put(position, PieceConverter.convertToPieceName(piece).toUpperCase());
@@ -184,7 +178,7 @@ public class ChessGameService {
     }
 
     private Map<Position, String> convertToWhitePrintName(final Map<Position, Piece> pieces) {
-        final Map<Position, String> whitePrintFormat = new HashMap<>();
+        Map<Position, String> whitePrintFormat = new HashMap<>();
         for (Position position : pieces.keySet()) {
             final Piece piece = pieces.get(position);
             if (piece == null) continue;
