a517fbe02942df3f401a067f1bd33a109e2a0f14
bosl95 <bosl95@naver.com>
2021-04-04 22:02:51 +0900

    feat: DDL 파일 추가

---
+++ src/main/resources/db_name_game.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: db_name
+-- ------------------------------------------------------
+-- Server version	5.7.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `game` (
+  `gameid` int(11) NOT NULL AUTO_INCREMENT,
+  `jsondata` text,
+  PRIMARY KEY (`gameid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+INSERT INTO `game` VALUES (2,'{\"success\":true,\"response\":\"{\\\"pieces\\\":[{\\\"position\\\":\\\"g1\\\",\\\"pieceName\\\":\\\"n\\\"},{\\\"position\\\":\\\"f2\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"b1\\\",\\\"pieceName\\\":\\\"n\\\"},{\\\"position\\\":\\\"g2\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"e7\\\",\\\"pieceName\\\":\\\"P\\\"},{\\\"position\\\":\\\"b2\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"c8\\\",\\\"pieceName\\\":\\\"B\\\"},{\\\"position\\\":\\\"h1\\\",\\\"pieceName\\\":\\\"r\\\"},{\\\"position\\\":\\\"d8\\\",\\\"pieceName\\\":\\\"Q\\\"},{\\\"position\\\":\\\"a2\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"f1\\\",\\\"pieceName\\\":\\\"b\\\"},{\\\"position\\\":\\\"b7\\\",\\\"pieceName\\\":\\\"P\\\"},{\\\"position\\\":\\\"h2\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"g8\\\",\\\"pieceName\\\":\\\"N\\\"},{\\\"position\\\":\\\"f5\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"c2\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"d2\\\",\\\"pieceName\\\":\\\"p\\\"},{\\\"position\\\":\\\"c1\\\",\\\"pieceName\\\":\\\"b\\\"},{\\\"position\\\":\\\"e8\\\",\\\"pieceName\\\":\\\"K\\\"},{\\\"position\\\":\\\"a8\\\",\\\"pieceName\\\":\\\"R\\\"},{\\\"position\\\":\\\"d1\\\",\\\"pieceName\\\":\\\"q\\\"},{\\\"position\\\":\\\"a7\\\",\\\"pieceName\\\":\\\"P\\\"},{\\\"position\\\":\\\"h7\\\",\\\"pieceName\\\":\\\"P\\\"},{\\\"position\\\":\\\"f8\\\",\\\"pieceName\\\":\\\"B\\\"},{\\\"position\\\":\\\"b8\\\",\\\"pieceName\\\":\\\"N\\\"},{\\\"position\\\":\\\"g7\\\",\\\"pieceName\\\":\\\"P\\\"},{\\\"position\\\":\\\"a1\\\",\\\"pieceName\\\":\\\"r\\\"},{\\\"position\\\":\\\"c7\\\",\\\"pieceName\\\":\\\"P\\\"},{\\\"position\\\":\\\"d7\\\",\\\"pieceName\\\":\\\"P\\\"},{\\\"position\\\":\\\"h8\\\",\\\"pieceName\\\":\\\"R\\\"},{\\\"position\\\":\\\"e1\\\",\\\"pieceName\\\":\\\"k\\\"}],\\\"status\\\":{\\\"WHITE\\\":38.0,\\\"BLACK\\\":37.0},\\\"turn\\\":true,\\\"end\\\":true}\",\"errorMessage\":\"\"}');
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-04 22:00:13
