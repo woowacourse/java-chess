7605b54a09965e702abfd04a0f31ce5011b0f3c9
pika96 <52442949+pika96@users.noreply.github.com>
2021-04-05 00:49:04 +0900

    feat: 이름에 따라 Side를 반환하는 메서드 추가 및 테스트

--- src/test/java/chess/domain/SideTest.java
+++ src/test/java/chess/domain/SideTest.java
@@ -11,4 +11,11 @@ class SideTest {
         assertThat(Side.BLACK.changeTurn()).isEqualTo(Side.WHITE);
         assertThat(Side.NONE.changeTurn()).isEqualTo(Side.NONE);
     }
+
+    @Test
+    void getTurnByName() {
+        assertThat(Side.getTurnByName("WHITE")).isEqualTo(Side.WHITE);
+        assertThat(Side.getTurnByName("BLACK")).isEqualTo(Side.BLACK);
+        assertThat(Side.getTurnByName("NONE")).isEqualTo(Side.NONE);
+    }
 }
