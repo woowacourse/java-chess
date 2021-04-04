42b250664d47b7686ea8fff47a232df26b705e6b
Seed <hsik0225@gmail.com>
2021-04-05 02:22:00 +0900

    feat(main.js):진행 중인 게임이 없을 경우 이어하기를 못하도록 설정

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,8 +1,10 @@
 document.getElementById("new-game").addEventListener("click", onNewGame);
+document.getElementById("continue").addEventListener("click", onContinue);
 
 const OPTION = {
     "method": 'POST',
 }
+
 function onNewGame() {
     fetch('/chess/new', OPTION)
         .then(res => {
@@ -13,4 +15,16 @@ function onNewGame() {
             return res;
         })
         .catch(() => alert("서버가 죄송합니다..ㅠㅜ"));
+}
+
+function onContinue() {
+    fetch('/chess')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+            window.location.href = '/chess/view';
+            return res;
+        })
+        .catch(() => alert("진행 중인 게임이 없습니다."));
 }
