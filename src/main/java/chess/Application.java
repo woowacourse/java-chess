f280deb76c1a37295fb00acb6282a364efa2ee3d
Minsung <sns2831@gmail.com>
2021-04-04 12:20:29 +0900

    fix: 콘솔게임과 web게임이 반대로 실행되는 버그 수정

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -7,8 +7,6 @@ import static spark.Spark.staticFiles;
 public class Application {
 
     public static void main(String[] args) {
-        staticFiles.location("/templates");
-
         ChessController chessController = new ChessController();
         chessController.run();
     }
--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,10 +1,14 @@
 package chess;
 
-import chess.console.ChessController;
+import chess.web.ChessController;
+
+import static spark.Spark.staticFiles;
 
 public class WebUIChessApplication {
 
     public static void main(String[] args) {
+        staticFiles.location("/templates");
+
         ChessController chessController = new ChessController();
         chessController.run();
     }
