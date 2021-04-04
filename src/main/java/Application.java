2f8f1294f8c2f9677966160a450dbd7660217a6a
jujubebat <bbwwpark@naver.com>
2021-04-04 23:05:25 +0900

    refactor: 불필요한 클래스 삭제

--- src/main/java/Application.java
+++
@@ -1,9 +0,0 @@
-import controller.ChessController;
-
-public class Application {
-
-    public static void main(String[] args) {
-        ChessController chessController = new ChessController();
-        chessController.run();
-    }
-}
