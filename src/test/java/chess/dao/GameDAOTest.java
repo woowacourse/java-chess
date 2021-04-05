40aa91451d86496c393e4ff51c526a71f157d9a2
Kimun Kim <kkm97351@gmail.com>
2021-04-06 01:09:51 +0900

    feat: GameDAO 생성

---
+++ src/test/java/chess/dao/GameDAOTest.java
@@ -0,0 +1,24 @@
+package chess.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class GameDAOTest {
+    private GameDAO gameDao;
+
+    @BeforeEach
+    public void setup() {
+        gameDao = new GameDAO();
+    }
+
+    @Test
+    public void getConnectionTest() {
+        Connection con = gameDao.getConnection();
+        assertNotNull(con);
+    }
+
+}
