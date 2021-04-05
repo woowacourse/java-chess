38529a7b2f2d9e4af200b64504416c4e3f1ab872
Seed <hsik0225@gmail.com>
2021-04-05 21:53:05 +0900

    feat(main.js):게임 새로하기 api 통신 기능 구현

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,30 +1,65 @@
 document.getElementById("new-game").addEventListener("click", onNewGame);
 document.getElementById("continue").addEventListener("click", onContinue);
 
-const OPTION = {
+const POST = {
     "method": 'POST',
+    "headers" : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
 }
 
-function onNewGame() {
-    fetch('/chess/new', OPTION)
-        .then(res => {
-            if (!res.ok) {
-                throw new Error(res.status);
-            }
-            window.location.replace(res.url);
-            return res;
-        })
-        .catch(() => alert("서버가 죄송합니다..ㅠㅜ"));
+const PATCH = {
+    "method": 'PATCH',
+    "headers" : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
 }
 
-function onContinue() {
-    fetch('/chess')
-        .then(res => {
-            if (!res.ok) {
-                throw new Error(res.status);
-            }
-            window.location.href = '/chess/view';
-            return res;
-        })
-        .catch(() => alert("진행 중인 게임이 없습니다."));
+const DELETE = {
+    "method": 'DELETE',
+    "headers": {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+}
+
+async function insertNewGame() {
+    await fetch('/chess', POST);
+
+    const chessId = getCookie("chessId");
+    await fetch('/chess/' + chessId + '/pieces', POST);
+}
+
+function getCookie(name) {
+    return document.cookie.split("; ").find(row => row.startsWith(name)).split("=")[1];
+}
+
+async function removePreviousGame(id) {
+    await fetch('/chess/' + id + '/pieces', DELETE);
+    await fetch('/chess', DELETE);
+}
+
+async function onNewGame() {
+    const idResponse = await fetch('/chess/ids');
+    const idJson = await idResponse.json();
+    const id = idJson["body"];
+    if (id !== 'EMPTY') {
+        await removePreviousGame(id);
+    }
+    await insertNewGame();
+    window.location.href = '/chess';
+}
+
+async function onContinue() {
+    const idResponse = await fetch('/chess/ids');
+    const idJson = await idResponse.json();
+    const id = idJson["body"];
+    if (id === 'EMPTY') {
+        alert("진행 중인 게임이 없습니다.");
+        return;
+    }
+
+    window.location.href = '/chess';
 }
