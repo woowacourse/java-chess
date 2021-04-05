83d43f945c6391d8afc67f33e116acd04eae3902
Seed <hsik0225@gmail.com>
2021-04-06 01:29:17 +0900

    feat(chess-controller):id와 현재 턴을 검색하는 기능 추가

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -4,13 +4,19 @@ import java.sql.SQLException;
 
 import com.google.gson.Gson;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import chess.ChessResponse;
 import chess.domain.service.ChessService;
 import spark.Request;
+import spark.Response;
 
 public class ChessController {
 
-    public static final Gson GSON = new Gson();
+    private static final Logger log = LoggerFactory.getLogger(ChessController.class);
+
+    private static final Gson GSON = new Gson();
 
     private final ChessService chessService;
 
@@ -18,24 +24,41 @@ public class ChessController {
         this.chessService = new ChessService();
     }
 
-    public String insert(Request req, spark.Response res) throws SQLException {
+    public String getIds(Request req, Response res) throws SQLException {
+        log.warn("chess ids");
+        String chessId = chessService.findChessIdAsString();
+        ChessResponse chessResponse = new ChessResponse.Ok(chessId);
+        return GSON.toJson(chessResponse);
+    }
+
+    public String getTurn(Request req, Response res) throws SQLException {
+        log.warn("chess turn");
+        Long chessId = Long.valueOf(req.cookie("chessId"));
+        String turn = chessService.findTurnById(chessId);
+        ChessResponse chessResponse = new ChessResponse.Ok(turn);
+        return GSON.toJson(chessResponse);
+    }
+
+    public String insert(Request req, Response res) throws SQLException {
+        log.warn("chess insert");
         chessService.insert();
 
         Long chessId = chessService.findChessId();
         res.cookie("chessId", String.valueOf(chessId));
-        res.redirect("/chess");
 
         ChessResponse chessResponse = new ChessResponse.Created("새로운 체스가 추가되었습니다.");
         return GSON.toJson(chessResponse);
     }
 
-    public String update(Request req, spark.Response res) throws SQLException {
+    public String update(Request req, Response res) throws SQLException {
+        log.warn("chess update");
         chessService.updateTurn();
         ChessResponse chessResponse = new ChessResponse.Ok("턴이 변경되었습니다.");
         return GSON.toJson(chessResponse);
     }
 
-    public String delete(Request req, spark.Response res) throws SQLException {
+    public String delete(Request req, Response res) throws SQLException {
+        log.warn("chess delete");
         chessService.delete();
         ChessResponse chessResponse = new ChessResponse.Ok( "저장된 체스를 삭제했습니다.");
         return GSON.toJson(chessResponse);
--- src/main/java/chess/domain/service/ChessService.java
+++ src/main/java/chess/domain/service/ChessService.java
@@ -1,6 +1,7 @@
 package chess.domain.service;
 
 import java.sql.SQLException;
+import java.util.Optional;
 
 import chess.domain.chess.ChessDAO;
 
@@ -26,5 +27,17 @@ public class ChessService {
     public Long findChessId() throws SQLException {
         return chessDAO.findChessId()
                        .orElseThrow(() -> new IllegalStateException("진행 중인 게임이 없습니다."));
+    }
+
+    public String findChessIdAsString() throws SQLException {
+        final Optional<Long> chessId = chessDAO.findChessId();
+        if (chessId.isPresent()) {
+            return String.valueOf(chessId.get());
+        }
+        return "EMPTY";
+    }
+
+    public String findTurnById(Long chessId) throws SQLException {
+        return chessDAO.findTurnById(chessId).orElseThrow(() -> new IllegalStateException("진행 중인 게임이 없습니다."));
     }
 }
