f569841c925234947f26599644a3ce4e86ee67f8
jaeseongDev <qkrwotjd1445@naver.com>
2021-04-05 16:32:17 +0900

    feat : HTTP 통신 시 상태 코드 추가

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -5,7 +5,7 @@ import chess.dto.requestdto.StartRequestDto;
 import chess.dto.response.Response;
 import chess.dto.response.ResponseCode;
 import chess.dto.responsedto.GridAndPiecesResponseDto;
-import chess.exception.NotExistsPieceException;
+import chess.exception.ChessException;
 import chess.service.ChessService;
 import com.google.gson.Gson;
 import spark.ModelAndView;
@@ -21,6 +21,9 @@ import static spark.Spark.staticFiles;
 
 public class WebUIChessApplication {
     public static final Gson GSON = new Gson();
+    private static final int BAD_REQUEST = 400;
+    private static final int OK = 200;
+    private static final int NO_CONTENT = 204;
 
     public static void main(String[] args) {
         staticFiles.location("/public");
@@ -28,44 +31,47 @@ public class WebUIChessApplication {
 
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
+            res.status(OK);
             return render(model, "index.html");
         });
 
         post("/move", (req, res) -> {
-            try {
-                MoveRequestDto moveRequestDto = GSON.fromJson(req.body(), MoveRequestDto.class);
-                return chessService.move(moveRequestDto);
-            } catch (NotExistsPieceException e) {
-                return new Response(ResponseCode.WRONG_ARGUMENTS.getCode(), e.getMessage());
-            }
+            MoveRequestDto moveRequestDto = GSON.fromJson(req.body(), MoveRequestDto.class);
+            res.status(NO_CONTENT);
+            return chessService.move(moveRequestDto);
         }, GSON::toJson);
 
         get("/grid/:roomName", (req, res) -> {
             String roomName = req.params(":roomName");
             StartRequestDto startRequestDto = new StartRequestDto(roomName);
             GridAndPiecesResponseDto gridAndPiecesResponseDto = chessService.getGridAndPieces(startRequestDto);
+            res.status(OK);
             return new Response(ResponseCode.OK, gridAndPiecesResponseDto);
         }, GSON::toJson);
 
         post("/grid/:gridId/start", (req, res) -> {
             String gridId = req.params("gridId");
             chessService.start(Long.parseLong(gridId));
+            res.status(NO_CONTENT);
             return new Response(ResponseCode.NO_CONTENT);
         }, GSON::toJson);
 
         post("/grid/:gridId/finish", (req, res) -> {
             String gridId = req.params("gridId");
             chessService.finish(Long.parseLong(gridId));
+            res.status(NO_CONTENT);
             return new Response(ResponseCode.NO_CONTENT);
         }, GSON::toJson);
 
         get("/room/:roomId/restart", (req, res) -> {
             String roomId = req.params("roomId");
+            res.status(OK);
             return new Response(ResponseCode.OK, chessService.restart(Long.parseLong(roomId)));
         }, GSON::toJson);
 
-        exception(Exception.class, (exception, request, response) -> {
-            Response res = new Response(ResponseCode.SERVER_ERROR.getCode(), exception.getMessage());
+        exception(ChessException.class, (exception, request, response) -> {
+            Response res = new Response(exception.getCode(), exception.getMessage());
+            response.status(BAD_REQUEST);
             response.body(GSON.toJson(res));
         });
     }
