77cb2cf2643aa3808b779790a0625453d1539578
seunghyun <ggyool@naver.com>
2021-04-06 23:15:49 +0900

    feat(js): 체스판의 타일 관련 코드 추가

---
+++ src/main/resources/static/js/tile/Tiles.js
@@ -0,0 +1,35 @@
+import {Tile} from "./Tile.js"
+
+export class Tiles {
+  #tiles = []
+  #components
+
+  constructor() {
+    this.#components = document.querySelectorAll(".tile");
+    this.#components.forEach(component => this.#tiles.push(new Tile(component)))
+  }
+
+  findByComponent(component) {
+    for (const tile of this.#tiles) {
+      if (Object.is(tile.component, component)) {
+        return tile;
+      }
+    }
+  }
+
+  findByPosition(x, y) {
+    for (const tile of this.#tiles) {
+      if (tile.x === x && tile.y === y) {
+        return tile;
+      }
+    }
+  }
+
+  get tiles() {
+    return this.#tiles;
+  }
+
+  get components() {
+    return this.#components;
+  }
+}
