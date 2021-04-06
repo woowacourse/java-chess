107e6a24e1ec2b409c78025457f574910e0d9cc2
Solji Choi <bssj9307@gmail.com>
2021-04-06 21:07:37 +0900

    feat: spark에서 index.html 렌더링

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,6 +1,8 @@
 package chess;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFiles;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +12,8 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 public class WebUIChessApplication {
 
     public static void main(String[] args) {
+        port(8080);
+        staticFiles.location("/public/assets");
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
