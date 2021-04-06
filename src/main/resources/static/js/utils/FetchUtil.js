e6863f003ef6f4c892c28bfd45ac0122bb47fc4d
seunghyun <ggyool@naver.com>
2021-04-07 01:45:17 +0900

    feat(js): AJAX를 위한 util 추가

---
+++ src/main/resources/static/js/utils/FetchUtil.js
@@ -0,0 +1,24 @@
+export async function getData(url = '', params = {}) {
+  let urlSearchParams = new URLSearchParams();
+  for (let key in params) {
+    urlSearchParams.append(key, params[key])
+  }
+  return fetch(`${url}?${urlSearchParams.toString()}`)
+  .then(response => response.json())
+  .catch(error => console.error('Error:', error));
+}
+
+export async function postData(url = '', data = {}) {
+  return fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .catch(error => console.error('Error:', error));
+}
