ea60a514d1fc1fb3d7b568c87fe83e00b6b607de
kimjunseo <air.junseo@gmail.com>
2021-04-06 11:27:07 +0900

    refactor : DB connection 설정 정보 상수 분리

--- src/main/java/chess/dao/ConnectDB.java
+++ src/main/java/chess/dao/ConnectDB.java
@@ -4,16 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public final class ConnectDB {
+    private static final String SERVER = "localhost:13306";
+    private static final String DATABASE = "chess";
+    private static final String OPTION = "?useSSL=false&serverTimezone=UTC&characterEncoding=utf8";
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "root";
 
     public static Connection getConnection() throws Exception {
-        String server = "localhost:13306";
-        String database = "chess";
-        String option = "?useSSL=false&serverTimezone=UTC&characterEncoding=utf8";
-        String userName = "root";
-        String password = "root";
-
         Class.forName("com.mysql.cj.jdbc.Driver");
-
-        return DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + option, userName, password);
+        return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USER_NAME, USER_PASSWORD);
     }
 }
