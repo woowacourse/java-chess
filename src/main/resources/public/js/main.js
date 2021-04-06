5d9e74768dfef71f3df71ef8315a99ebed114e44
RinSabbit <celena1004@naver.com>
2021-04-06 23:46:31 +0900

    fix: 게임을 불러올 시 점수 갱신이 안 되는 오류 수정

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -29,12 +29,6 @@ window.onload = function () {
         event.target.classList.contains(`${turn.toLowerCase()}`)
   }
 
-  function startGame() {
-    document.querySelectorAll(".tile").forEach(tile => {
-      tile.classList.remove("end");
-    })
-  }
-
   async function endGame() {
     document.querySelectorAll(".tile").forEach(tile => {
       tile.classList.add("end");
@@ -158,7 +152,6 @@ window.onload = function () {
 
     $start_bt.addEventListener("click", async () => {
       $start_bt.classList.remove("start");
-      $start_bt.innerHTML = "NEW GAME";
       $load_bt.classList.add("hidden");
       setMessage(await checkTurn());
       const data = await getBoard();
@@ -168,10 +161,10 @@ window.onload = function () {
     $load_bt.addEventListener("click", async () => {
       if (confirm("게임을 이어 갑니까?")) {
         $start_bt.classList.remove("start");
-        $start_bt.innerHTML = "NEW GAME";
         $load_bt.innerHTML = "LOAD";
         $load_bt.classList.add("hidden");
         const data = await loadBoard();
+        await fillScore();
         setMessage(await checkTurn());
         return setBoard(data);
       }
