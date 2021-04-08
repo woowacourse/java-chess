336e69ae52e73b0e729ed64eb1b030d65b78ad88
perenok <perenoke@gmail.com>
2021-04-08 23:10:58 +0900

    docs(chess.sql) : move_log 추가 밑 board에 id 대신 name으로 변경

--- src/main/resources/chess.sql
+++ src/main/resources/chess.sql
@@ -10,24 +10,24 @@ USE chessdb;
 
 CREATE TABLE board
 (
-    id         INT AUTO_INCREMENT NOT NULL,
-    board_size INT                NOT NULL,
-    turn       VARCHAR(10)        NOT NULL,
+    name       VARCHAR(30) NOT NULL,
+    board_size INT         NOT NULL,
+    turn       VARCHAR(10) NOT NULL,
     checked    BOOLEAN,
     king_dead  BOOLEAN,
-    PRIMARY KEY (id)
+    PRIMARY KEY (name)
 );
 
 CREATE TABLE piece
 (
-    id       INT AUTO_INCREMENT NOT NULL,
-    name     VARCHAR(10)        NOT NULL,
-    team     VARCHAR(10)        NOT NULL,
-    score    INT                NOT NULL,
-    position VARCHAR(10)        NOT NULL,
-    board_id INT                NOT NULL,
+    id         INT AUTO_INCREMENT NOT NULL,
+    name       VARCHAR(10)        NOT NULL,
+    team       VARCHAR(10)        NOT NULL,
+    score      INT                NOT NULL,
+    position   VARCHAR(10)        NOT NULL,
+    board_name VARCHAR(30)        NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (board_id) references board (id)
+    FOREIGN KEY (board_name) references board (name)
 );
 
 
