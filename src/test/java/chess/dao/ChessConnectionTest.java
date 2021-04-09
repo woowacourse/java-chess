6efff1190e41720a86bc42010a849996d5d80656
hoony <younghoon.kwon91@gmail.com>
2021-04-09 17:10:13 +0900

    refactor: 주석 삭제

--- src/test/java/chess/dao/ChessConnectionTest.java
+++ src/test/java/chess/dao/ChessConnectionTest.java
@@ -11,61 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ChessConnectionTest {
 
-//    @DisplayName("DB 잘못된 서버로 연결시 throws Exception")
-//    @Test
-//    void connectionWrongServerThrowsException() {
-//        DBManager chessConnection = DBManager.getInstance(new ConnectionProperty("localhost:13307", "db_name",
-//                "?useSSL=false&serverTimezone=UTC", "root", "root"));
-//
-//        assertThatThrownBy(() -> chessConnection.getConnection())
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessageContaining("Communications link failure");
-//    }
-//
-//    @DisplayName("DB 잘못된 DB로 연결시 throws Exception")
-//    @Test
-//    void connectionWrongDBThrowsException() {
-//        DBManager chessConnection = DBManager.getInstance(new ConnectionProperty("localhost:13306", "db_nam",
-//                "?useSSL=false&serverTimezone=UTC", "root", "root"));
-//
-//        assertThatThrownBy(() -> chessConnection.getConnection())
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessageContaining("Unknown database");
-//    }
-//
-//    @DisplayName("DB 잘못된 option timezone으로 연결시 throws Exception")
-//    @Test
-//    void connectionWrongOptionTimezoneThrowsException() {
-//        DBManager chessConnection = DBManager.getInstance(new ConnectionProperty("localhost:13306", "db_name",
-//                "?useSSL=false&serverTimezone=UT", "root", "root"));
-//
-//        assertThatThrownBy(() -> chessConnection.getConnection())
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessageContaining("timezone");
-//    }
-//
-//    @DisplayName("DB 잘못된 username으로 연결시 throws Exception")
-//    @Test
-//    void connectionWrongUserNameThrowsException() {
-//        DBManager chessConnection = DBManager.getInstance(new ConnectionProperty("localhost:13306", "db_name",
-//                "?useSSL=false&serverTimezone=UTC", "roo", "root"));
-//
-//        assertThatThrownBy(() -> chessConnection.getConnection())
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessageContaining("Access denied for user");
-//    }
-//
-//    @DisplayName("DB 잘못된 password으로 연결시 throws Exception")
-//    @Test
-//    void connectionWrongPasswordThrowsException() {
-//        DBManager chessConnection = DBManager.getInstance(new ConnectionProperty("localhost:13306", "db_name",
-//                "?useSSL=false&serverTimezone=UTC", "root", "roo"));
-//
-//        assertThatThrownBy(() -> chessConnection.getConnection())
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessageContaining("Access denied for user");
-//    }
-
     @DisplayName("DB 연결 확인")
     @Test
     void getConnection() throws SQLException {
