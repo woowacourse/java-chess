43c22b77922cd9eb9cacd12ca494e653020e3f6d
daeun <leede0418@likelion.org>
2021-04-09 23:04:35 +0900

    refactor(dao): try-with-resources 적용
    - try 문이 종료될 때 자동으로 자원 해제

--- src/main/java/chess/dao/PieceDao.java
+++ src/main/java/chess/dao/PieceDao.java
@@ -14,49 +14,50 @@ import java.util.List;
 public class PieceDao extends DBConnection {
     public void initializePieceStatus(final PieceRequestDto pieceRequestDto) throws SQLException {
         String query = "INSERT INTO piece (piece_name, piece_position) VALUE (?, ?)";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        psmt.setString(1, pieceRequestDto.getPieceName());
-        psmt.setString(2, pieceRequestDto.getPiecePosition());
-        psmt.executeUpdate();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query)) {
+            psmt.setString(1, pieceRequestDto.getPieceName());
+            psmt.setString(2, pieceRequestDto.getPiecePosition());
+            psmt.executeUpdate();
+        }
     }
 
     public List<ChessRequestDto> showAllPieces() throws SQLException {
         List<ChessRequestDto> pieces = new ArrayList<>();
-        ResultSet rs = readPieceStatus();
-        while (rs.next()) {
-            pieces.add(new ChessRequestDto(
-                    rs.getLong("id"),
-                    rs.getString("piece_name"),
-                    rs.getString("piece_position")
-            ));
-        }
-        return pieces;
-    }
-
-    private ResultSet readPieceStatus() throws SQLException {
         String query = "SELECT * FROM piece";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        return psmt.executeQuery();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query);
+             ResultSet rs = psmt.executeQuery()) {
+            while (rs.next()) {
+                pieces.add(new ChessRequestDto(
+                        rs.getLong("id"),
+                        rs.getString("piece_name"),
+                        rs.getString("piece_position")
+                ));
+            }
+            return pieces;
+        }
     }
 
     public void movePiece(final MoveRequestDto moveRequestDto) throws SQLException {
         String query = "UPDATE piece SET piece_position=? WHERE piece_position=?";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        psmt.setString(1, moveRequestDto.getTarget());
-        psmt.setString(2, moveRequestDto.getSource());
-        psmt.executeUpdate();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query)) {
+            psmt.setString(1, moveRequestDto.getTarget());
+            psmt.setString(2, moveRequestDto.getSource());
+            psmt.executeUpdate();
+        }
     }
 
     public void removeAllPieces() throws SQLException {
         String query = "DELETE FROM piece";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        psmt.executeUpdate();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query)) {
+            psmt.executeUpdate();
+        }
     }
 
     public void removePiece(final MoveRequestDto moveRequestDto) throws SQLException {
         String query = "DELETE FROM piece WHERE piece_position=?";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        psmt.setString(1, moveRequestDto.getTarget());
-        psmt.executeUpdate();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query)) {
+            psmt.setString(1, moveRequestDto.getTarget());
+            psmt.executeUpdate();
+        }
     }
 }
--- src/main/java/chess/dao/TurnDao.java
+++ src/main/java/chess/dao/TurnDao.java
@@ -13,40 +13,40 @@ import java.util.List;
 public class TurnDao extends DBConnection {
     public void initializeTurn() throws SQLException {
         String query = "INSERT INTO turn (current_turn) VALUE (?)";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        psmt.setString(1, "white");
-        psmt.executeUpdate();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query)) {
+            psmt.setString(1, "white");
+            psmt.executeUpdate();
+        }
     }
 
     public List<TurnRequestDto> showCurrentTurn() throws SQLException {
         List<TurnRequestDto> turn = new ArrayList<>();
-        ResultSet rs = readCurrentTurn();
-        while (rs.next()) {
-            turn.add(new TurnRequestDto(
-                    rs.getLong("id"),
-                    rs.getString("current_turn")
-            ));
-        }
-        return turn;
-    }
-
-    private ResultSet readCurrentTurn() throws SQLException {
         String query = "SELECT * FROM turn";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        return psmt.executeQuery();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query);
+             ResultSet rs = psmt.executeQuery()) {
+            while (rs.next()) {
+                turn.add(new TurnRequestDto(
+                        rs.getLong("id"),
+                        rs.getString("current_turn")
+                ));
+            }
+            return turn;
+        }
     }
 
     public void changeTurn(final TurnChangeRequestDto turnChangeRequestDto) throws SQLException {
         String query = "UPDATE turn SET current_turn=? WHERE current_turn=?";
-        PreparedStatement psmt = getConnection().prepareStatement(query);
-        psmt.setString(1, turnChangeRequestDto.getNextTurn());
-        psmt.setString(2, turnChangeRequestDto.getCurrentTurn());
-        psmt.executeUpdate();
+        try (PreparedStatement psmt = getConnection().prepareStatement(query)) {
+            psmt.setString(1, turnChangeRequestDto.getNextTurn());
+            psmt.setString(2, turnChangeRequestDto.getCurrentTurn());
+            psmt.executeUpdate();
+        }
     }
 
     public void removeTurn() throws SQLException {
         String query = "DELETE FROM turn";
-        PreparedStatement pstm = getConnection().prepareStatement(query);
-        pstm.executeUpdate();
+        try (PreparedStatement pstm = getConnection().prepareStatement(query)) {
+            pstm.executeUpdate();
+        }
     }
 }
