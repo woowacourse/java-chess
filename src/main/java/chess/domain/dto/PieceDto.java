e6c3ee056724b85b8ce73fc836e50973d81de697
binghe819 <binghe819@naver.com>
2021-04-09 17:04:14 +0900

    refactor: 서비스 레이어 리팩토링된 도메인 객체 사용하도록 수정

--- src/main/java/chess/domain/dto/PieceDto.java
+++ src/main/java/chess/domain/dto/PieceDto.java
@@ -1,27 +1,24 @@
 package chess.domain.dto;
 
+import chess.domain.piece.Piece;
+
 public class PieceDto {
 
-    private final long id;
-    private final long roomId;
     private final char signature;
     private final String team;
     private final String location;
 
-    public PieceDto(long id, long roomId, char signature, String team, String location) {
-        this.id = id;
-        this.roomId = roomId;
+    public PieceDto(char signature, String team, String location) {
         this.signature = signature;
         this.team = team;
         this.location = location;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public long getRoomId() {
-        return roomId;
+    public static PieceDto from(Piece piece) {
+        return new PieceDto(
+            piece.getSignature(),
+            piece.getTeam().getValue(),
+            String.valueOf(piece.getX()) + String.valueOf(piece.getY()));
     }
 
     public char getSignature() {
--- src/main/java/chess/serivce/chess/ChessService.java
+++ src/main/java/chess/serivce/chess/ChessService.java
@@ -2,10 +2,8 @@ package chess.serivce.chess;
 
 import chess.domain.board.Board;
 import chess.domain.dto.PieceDto;
-import chess.domain.dto.RoomDto;
 import chess.domain.dto.move.MoveResponseDto;
 import chess.domain.game.Room;
-import chess.domain.gamestate.State;
 import chess.domain.gamestate.running.Ready;
 import chess.domain.location.Location;
 import chess.domain.piece.Piece;
@@ -15,11 +13,9 @@ import chess.repository.piece.PieceRepository;
 import chess.repository.room.JdbcRoomRepository;
 import chess.repository.room.RoomRepository;
 import chess.utils.BoardUtil;
-import chess.utils.PieceUtil;
-import chess.utils.StateUtil;
 import java.sql.SQLException;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChessService {
 
@@ -27,101 +23,92 @@ public class ChessService {
     private final PieceRepository pieceRepository = new JdbcPieceRepository();
 
     public MoveResponseDto start(String roomName) throws SQLException {
-//        RoomDto roomDto = roomRepository.findRoomByRoomName(roomName);
-//
-//        Room room = generateRoomDomainFromDB(roomDto, Collections.emptyList());
-//        room.play("start");
-//        roomRepository.update(roomDto.getId(), roomDto.getName(), room);
-//
-//        Board board = room.state().getBoard();
-//        for (Piece piece : board.getPieces()) {
-//            pieceRepository.insert(roomDto.getId(), piece);
-//        }
-//
-//        return new MoveResponseDto(
-//            pieceRepository.findPiecesByRoomId(roomDto.getId()),
-//            room.currentTeam().getValue(),
-//            room.judgeResult()
-//        );
-        return null;
+        Room room = roomRepository.findRoomByRoomName(roomName);
+
+        room.play("start");
+        roomRepository.update(room);
+
+        Board board = room.getState().getBoard();
+        for (Piece piece : board.getPieces()) {
+            pieceRepository.insert(room.getId(), piece);
+        }
+
+        List<PieceDto> pieceDtos = room.getState().getBoard().getPieces()
+            .stream()
+            .map(piece -> PieceDto.from(piece))
+            .collect(Collectors.toList());
+        return new MoveResponseDto(
+            pieceDtos,
+            room.getCurrentTeam().getValue(),
+            room.judgeResult()
+        );
     }
 
     public MoveResponseDto end(String roomName) throws SQLException {
-//        RoomDto roomDto = roomRepository.findRoomByRoomName(roomName);
-//        List<PieceDto> pieces = pieceRepository.findPiecesByRoomId(roomDto.getId());
-//
-//        Room room = generateRoomDomainFromDB(roomDto, pieces);
-//        room.play("end");
-//        roomRepository.update(roomDto.getId(), roomDto.getName(), room);
-//
-//        return new MoveResponseDto(
-//            pieceRepository.findPiecesByRoomId(roomDto.getId()),
-//            room.currentTeam().getValue(),
-//            room.judgeResult()
-//        );
-        return null;
+        Room room = roomRepository.findRoomByRoomName(roomName);
+
+        room.play("end");
+        roomRepository.update(room);
+
+        List<PieceDto> pieceDtos = room.getState().getBoard().getPieces()
+            .stream()
+            .map(piece -> PieceDto.from(piece))
+            .collect(Collectors.toList());
+        return new MoveResponseDto(
+            pieceDtos,
+            room.getCurrentTeam().getValue(),
+            room.judgeResult()
+        );
     }
 
     public MoveResponseDto move(String roomName, String source, String target) throws SQLException {
-//        RoomDto roomDto = roomRepository.findRoomByRoomName(roomName);
-//        List<PieceDto> piecesInDb = pieceRepository.findPiecesByRoomId(roomDto.getId());
-//
-//        Room room = generateRoomDomainFromDB(roomDto, piecesInDb);
-//        room.play("move " + source + " " + target);
-//        roomRepository.update(roomDto.getId(), roomDto.getName(), room);
-//
-//        Board board = room.state().getBoard();
-//        List<Piece> pieces = room.state().getBoard().getPieces();
-//
-//        if (pieces.size() != piecesInDb.size()) {
-//            PieceDto pieceInDBAtSource = piecesInDb.stream()
-//                .filter(pieceDto -> pieceDto.getLocation().equals(source))
-//                .findFirst().get();
-//            PieceDto pieceInDBAtTarget = piecesInDb.stream()
-//                .filter(pieceDto -> pieceDto.getLocation().equals(target))
-//                .findFirst().get();
-//
-//            Piece pieceInTarget = board.find(Location.of(target));
-//            pieceRepository.update(pieceInDBAtSource.getId(), pieceInTarget);
-//            pieceRepository.deletePieceById(pieceInDBAtTarget.getId());
-//            return new MoveResponseDto(
-//                pieceRepository.findPiecesByRoomId(roomDto.getId()),
-//                room.currentTeam().getValue(),
-//                room.judgeResult()
-//            );
-//        }
-//
-//        for (int i = 0; i < pieces.size(); i++) {
-//            pieceRepository.update(piecesInDb.get(i).getId(), pieces.get(i));
-//        }
-//
-//        return new MoveResponseDto(
-//            pieceRepository.findPiecesByRoomId(roomDto.getId()),
-//            room.currentTeam().getValue(),
-//            room.judgeResult()
-//        );
-        return null;
-    }
+        Room room = roomRepository.findRoomByRoomName(roomName);
+        Board board = room.getState().getBoard();
+        Piece sourcePiece = board.find(Location.of(source));
+        List<Piece> beforeMovePieces = board.getPieces();
 
-    private Room generateRoomDomainFromDB(RoomDto roomDto, List<PieceDto> pieceDtos) {
-        Board board = Board.of(PieceUtil.generatePiecesByPieceDtos(pieceDtos));
-        State state = StateUtil.generateState(roomDto.getState(), board);
-        Room room = new Room(state, Team.of(roomDto.getCurrentTeam()));
-        return room;
+        room.play("move " + source + " " + target);
+        roomRepository.update(room);
+        List<Piece> afterMovePieces = board.getPieces();
+
+        if (beforeMovePieces.size() != afterMovePieces.size()) {
+            Piece removedPiece = beforeMovePieces
+                .stream()
+                .filter(piece -> piece.getLocation().equals(Location.of(target)))
+                .filter(piece -> !piece.equals(sourcePiece))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 기물입니다."));
+            pieceRepository.deletePieceById(removedPiece.getId());
+        }
+
+        for (Piece piece : board.getPieces()) {
+            pieceRepository.update(piece);
+        }
+
+        List<PieceDto> pieceDtos = board.getPieces()
+            .stream()
+            .map(piece -> PieceDto.from(piece))
+            .collect(Collectors.toList());
+        return new MoveResponseDto(
+            pieceDtos,
+            room.getCurrentTeam().getValue(),
+            room.judgeResult()
+        );
     }
 
     public MoveResponseDto findPiecesByRoomName(String roomName) throws SQLException {
-//        RoomDto roomDto = roomRepository.findRoomByRoomName(roomName);
-//        List<PieceDto> piecesInDb = pieceRepository.findPiecesByRoomId(roomDto.getId());
-//
-//        Room room = generateRoomDomainFromDB(roomDto, piecesInDb);
-//
-//        return new MoveResponseDto(
-//            piecesInDb,
-//            room.currentTeam().getValue(),
-//            room.judgeResult()
-//        );
-        return null;
+        Room room = roomRepository.findRoomByRoomName(roomName);
+
+        List<PieceDto> pieceDtos = pieceRepository.findPiecesByRoomId(room.getId())
+            .stream()
+            .map(piece -> PieceDto.from(piece))
+            .collect(Collectors.toList());
+
+        return new MoveResponseDto(
+            pieceDtos,
+            room.getCurrentTeam().getValue(),
+            room.judgeResult()
+        );
     }
 
     public void createRoom(String roomName) throws SQLException {
@@ -129,9 +116,6 @@ public class ChessService {
             throw new IllegalArgumentException("[ERROR] 이미 존재하는 방입니다. 다른 이름을 사용해주세요.");
         }
 
-        int userId = 0; // TODO: 임시 아이디 (유저 기능 구현시 활용)
-        Room newRoom = new Room(new Ready(BoardUtil.generateInitialBoard()), Team.WHITE);
-
-        roomRepository.insert(userId, roomName, newRoom);
+        roomRepository.insert(new Room(0, roomName, new Ready(BoardUtil.generateInitialBoard()), Team.WHITE));
     }
 }
