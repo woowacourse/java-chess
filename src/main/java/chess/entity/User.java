b68b56afb6c7c26ba97f1edd9e23d8fd30d78796
seunghyun <ggyool@naver.com>
2021-04-10 17:44:00 +0900

    feat: User 엔티티 생성

---
+++ src/main/java/chess/entity/User.java
@@ -0,0 +1,42 @@
+package chess.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private final long id;
+    private final String name;
+    private final long winCount;
+    private final long loseCount;
+    private final LocalDateTime createdTime;
+
+    public User(final long id, final String name, final long winCount, final long loseCount,
+        final LocalDateTime createdTime) {
+        this.id = id;
+        this.name = name;
+        this.winCount = winCount;
+        this.loseCount = loseCount;
+        this.createdTime = createdTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getWinCount() {
+        return winCount;
+    }
+
+    public long getLoseCount() {
+        return loseCount;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+}
+
