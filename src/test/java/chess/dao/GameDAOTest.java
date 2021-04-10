7365a3ae0c288529338586e6270c23bfc13df57c
Kimun Kim <kkm97351@gmail.com>
2021-04-10 19:40:46 +0900

    refactor: GameDAOTest.getConnectionTest() 삭제

--- src/test/java/chess/dao/GameDAOTest.java
+++ src/test/java/chess/dao/GameDAOTest.java
@@ -1,11 +1,6 @@
 package chess.dao;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.sql.Connection;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GameDAOTest {
     private GameDAO gameDao;
@@ -13,11 +8,5 @@ class GameDAOTest {
     @BeforeEach
     public void setup() {
         gameDao = new GameDAO();
-    }
-
-    @Test
-    public void getConnectionTest() {
-        Connection con = gameDao.getConnection();
-        assertNotNull(con);
     }
 }
