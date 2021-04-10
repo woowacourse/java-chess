8d65fdde7055afb58e2a632a418b59102f2c5fa5
seunghyun <ggyool@naver.com>
2021-04-10 23:29:09 +0900

    feat: 요청 처리 중 예외 발생시 동작 구현

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,9 +1,12 @@
 package chess;
 
+import static spark.Spark.exception;
 import static spark.Spark.staticFiles;
 
 import chess.controller.web.GameController;
 import chess.controller.web.HomeController;
+import chess.controller.web.UserController;
+import chess.dao.exception.UncheckedSQLException;
 
 public class WebUIChessApplication {
 
@@ -11,5 +14,12 @@ public class WebUIChessApplication {
         staticFiles.location("/static");
         final HomeController homeController = new HomeController();
         final GameController gameController = new GameController();
+        final UserController userController = new UserController();
+
+        exception(RuntimeException.class, ((exception, request, response) -> {
+            response.status(500);
+            response.type("text/html");
+            response.body(exception.getMessage());
+        }));
     }
 }
