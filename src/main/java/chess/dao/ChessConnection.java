daf8c40de7493d02929d361820421a1a6896aad1
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-04-10 15:44:07 +0900

    refactor : ChessConnection 예외처리 및 콘솔 출력 로직 제거

--- src/main/java/chess/dao/ChessConnection.java
+++ src/main/java/chess/dao/ChessConnection.java
@@ -16,16 +16,13 @@ public class ChessConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            System.err.println(" !! JDBC Driver load 오류: " + e.getMessage());
             e.printStackTrace();
         }
 
         try {
             con = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + option, userName, password);
-            System.out.println("정상적으로 연결되었습니다.");
         } catch (SQLException e) {
-            System.err.println("연결 오류:" + e.getMessage());
-            e.printStackTrace();
+            throw new IllegalStateException(e.getMessage());
         }
 
         return con;
