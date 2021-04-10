35c5f5204841e791d8b96db6669d0e014122d61d
Seed <hsik0225@gmail.com>
2021-04-10 16:50:19 +0900

    feat(ddl.sql):컬럼 크기 수정

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS piece
 (
     piece_id BIGINT AUTO_INCREMENT,
     chess_id BIGINT,
-    position varchar(100),
-    color    varchar(100),
-    name     varchar(100),
+    position varchar(10),
+    color    varchar(10),
+    name     varchar(10),
     PRIMARY KEY (piece_id),
     FOREIGN KEY (chess_id) REFERENCES chess (chess_id)
 );
