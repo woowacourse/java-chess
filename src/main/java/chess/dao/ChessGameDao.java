56040ee92cfb507aca10422836c3f87036ff7990
TaewanKimmmm <ktww88@gmail.com>
2021-04-10 16:32:09 +0900

    refactor: try with resources 적용 및 데이터 가져오는 역할만 하도록 수정

--- src/main/java/chess/dao/ChessGameDao.java
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -3,13 +3,11 @@ package chess.dao;
 import chess.domain.ChessGame;
 import chess.domain.Point;
 import chess.domain.piece.Color;
-import chess.domain.piece.PieceType;
 import chess.domain.piece.kind.Piece;
 import chess.dto.ChessGameDto;
 import chess.dto.UserDto;
 
 import java.sql.*;
-import java.util.HashMap;
 import java.util.Map;
 
 public class ChessGameDao {
@@ -37,18 +35,7 @@ public class ChessGameDao {
             System.err.println("연결 오류:" + e.getMessage());
             e.printStackTrace();
         }
-
         return con;
-    }
-
-    // 드라이버 연결해제
-    public void closeConnection(Connection con) {
-        try {
-            if (con != null)
-                con.close();
-        } catch (SQLException e) {
-            System.err.println("con 오류:" + e.getMessage());
-        }
     }
 
     public void addUser(UserDto userDto) throws SQLException {
@@ -64,16 +51,20 @@ public class ChessGameDao {
     }
 
     public UserDto findUser(UserDto userDto) throws SQLException {
+        Connection con = getConnection();
         String query = "SELECT user_id FROM user WHERE user_id = ?";
-        PreparedStatement pstmt = getConnection().prepareStatement(query);
+        PreparedStatement pstmt = con.prepareStatement(query);
         pstmt.setString(1, userDto.getUserId());
         ResultSet rs = pstmt.executeQuery();
 
-        if (!rs.next()) {
-            return null;
+        try (con; pstmt; rs) {
+            if (rs.next()) {
+                return new UserDto(rs.getString("user_id"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        closeConnection(getConnection());
-        return new UserDto(rs.getString("user_id"));
+        return null;
     }
 
     public ChessGameDto findGameByUserId(UserDto userDto) throws SQLException {
@@ -83,32 +74,20 @@ public class ChessGameDao {
         pstmt.setString(1, userDto.getUserId());
         ResultSet rs = pstmt.executeQuery();
 
-        if (!rs.next()) {
-            return null;
+        try (con; pstmt; rs) {
+            if (rs.next()) {
+                String currentColor = rs.getString("current_color");
+                String[] pieceNames = rs.getString("pieces").split("");
+                return new ChessGameDto(currentColor, pieceNames);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        String currentColor = rs.getString("current_color");
-        String[] pieceNames = rs.getString("pieces").split("");
-        Map<Point, Piece> board = new HashMap<>();
-
-        for (int column = 0; column <= 7; column++) {
-            makeRow(pieceNames, board, column);
-        }
-
-        ChessGameDto chessGameDto = new ChessGameDto();
-        chessGameDto.setBoard(board);
-        chessGameDto.setCurrentColor(Color.parseColor(currentColor));
-        closeConnection(getConnection());
-        return chessGameDto;
+        return null;
     }
 
-    private void makeRow(String[] pieceNames, Map<Point, Piece> board, int column) {
-        for (int row = 7; row >= 0; row--) {
-            board.put(Point.of(row, column), PieceType.createByPieceName(pieceNames[(7 - row) + column * 8].charAt(0)));
-        }
-    }
-
-    public void updateGame(String userId, ChessGame chessGame) throws SQLException {
+    public void updateGame(UserDto userDto, ChessGame chessGame) throws SQLException {
+        Connection con = getConnection();
         Map<Point, Piece> board = chessGame.getBoard();
         Color currentColor = chessGame.getCurrentColor();
 
@@ -117,30 +96,40 @@ public class ChessGameDao {
             makeBoard(board, sb, letter);
         }
         String query = "UPDATE game SET pieces = ?, current_color = ? WHERE user_id = ?";
-        PreparedStatement pstmt = getConnection().prepareStatement(query);
-        pstmt.setString(1, sb.toString());
-        pstmt.setString(2, currentColor.getName());
-        pstmt.setString(3, userId);
-        pstmt.executeUpdate();
+        PreparedStatement pstmt = con.prepareStatement(query);
+
+        try (con; pstmt;) {
+            pstmt.setString(1, sb.toString());
+            pstmt.setString(2, currentColor.getName());
+            pstmt.setString(3, userDto.getUserId());
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void makeBoard(Map<Point, Piece> board, StringBuilder sb, char letter) {
         for (int i = 1; i <= 8; i++) {
-            String s = Character.toString(letter) + Integer.toString(i);
+            String s = letter + Integer.toString(i);
             sb.append(board.get(Point.of(s)).getName());
         }
     }
 
     public void createNewGame(UserDto userDto, Map<Point, Piece> board) throws SQLException {
+        Connection con = getConnection();
         StringBuilder sb2 = new StringBuilder();
         for (char letter = 'a'; letter <= 'h'; letter++) {
             makeBoard(board, sb2, letter);
         }
         String query = "INSERT INTO game(user_id, current_color, pieces) VALUES(?, ?, ?)";
-        PreparedStatement pstmt = getConnection().prepareStatement(query);
-        pstmt.setString(1, userDto.getUserId());
-        pstmt.setString(2, "white");
-        pstmt.setString(3, sb2.toString());
-        pstmt.executeUpdate();
+        PreparedStatement pstmt = con.prepareStatement(query);
+        try (con; pstmt;) {
+            pstmt.setString(1, userDto.getUserId());
+            pstmt.setString(2, "white");
+            pstmt.setString(3, sb2.toString());
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
