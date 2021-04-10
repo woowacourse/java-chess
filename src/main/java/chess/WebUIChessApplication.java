e23f126b155ca771d6e1d7001d8c79eee5618f49
dwl5 <ssop6403@gmail.com>
2021-04-10 20:10:17 +0900

    refactor: RoomDAO와 GameDAO를 가진 레포지토리 생성

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -1,14 +1,16 @@
 package chess;
 
 import chess.controller.WebChessController;
-import chess.repository.MySqlChessRepository;
+import chess.dao.GameDAOImpl;
+import chess.dao.RoomDAOImpl;
+import chess.repository.ChessRepositoryImpl;
 import chess.service.ChessGameService;
 import spark.Spark;
 
 public class WebUIChessApplication {
     public static void main(String[] args) {
         Spark.staticFiles.location("/static");
-        ChessGameService chessGameService = new ChessGameService(new MySqlChessRepository());
+        ChessGameService chessGameService = new ChessGameService(new ChessRepositoryImpl(new GameDAOImpl(), new RoomDAOImpl()));
         WebChessController webChessController = new WebChessController(chessGameService);
         webChessController.run();
     }
