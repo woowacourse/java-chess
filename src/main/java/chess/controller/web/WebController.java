11fe45521df8b5d4703b2a298a8228ebc79a23a9
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-04-10 15:55:30 +0900

    refactor : 라우팅 로직 메소드로 분리

--- src/main/java/chess/controller/web/WebController.java
+++ src/main/java/chess/controller/web/WebController.java
@@ -12,6 +12,7 @@ import chess.service.ChessService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import spark.ModelAndView;
+import spark.Route;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
@@ -21,7 +22,6 @@ import static spark.Spark.*;
 
 public class WebController {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final int HTTP_STATUS_OK = 200;
     private static final int HTTP_STATUS_ERROR = 400;
 
     private final ChessService chessService;
@@ -41,55 +41,70 @@ public class WebController {
         exceptionHandling();
     }
 
-    private void gameRouting() {
+    private String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
 
-        get("/user", (request, response) -> {
+    private void gameRouting() {
+        get("/user", userRoute(), GSON::toJson);
+
+        path("/game", () -> {
+            get("/start", startRoute(), GSON::toJson);
+            get("/score/:id", scoreRoute(), GSON::toJson);
+            get("/load/:id", loadRoute(), GSON::toJson);
+            post("/move", moveRoute(), GSON::toJson);
+        });
+    }
+
+    private Route userRoute() {
+        return (request, response) -> {
             response.type("application/json; charset=utf-8");
             ChessGameManagerBundle runningGames = chessService.findRunningGames();
             return new RunningGameResponseDto(runningGames.getIdAndNextTurn());
-        }, GSON::toJson);
+        };
+    }
 
-        path("/game", () -> {
-            get("/start", (request, response) -> {
-                response.type("application/json; charset=utf-8");
-                return new ChessGameResponseDto(chessService.start());
-            }, GSON::toJson);
+    private Route startRoute() {
+        return (request, response) -> {
+            response.type("application/json; charset=utf-8");
+            return new ChessGameResponseDto(chessService.start());
+        };
+    }
 
-            get("/score/:id", (request, response) -> {
+    private Route scoreRoute() {
+        return (request, response) -> {
+            long id = Long.parseLong(request.params("id"));
+            response.type("application/json; charset=utf-8");
+            return new ScoreResponseDto(chessService.getStatistics(id));
+        };
+    }
+
+    private Route loadRoute() {
+        return (request, response) -> {
+            try {
                 long id = Long.parseLong(request.params("id"));
-                response.type("application/json; charset=utf-8");
-                return new ScoreResponseDto(chessService.getStatistics(id));
-            }, GSON::toJson);
+                ChessGameManager load = chessService.load(id);
+                return new ChessGameResponseDto(load);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("게임 id는 숫자값이어야 합니다.");
+            }
+        };
+    }
 
-            get("/load/:id", (request, response) -> {
-                try {
-                    long id = Long.parseLong(request.params("id"));
-                    ChessGameManager load = chessService.load(id);
-                    return new ChessGameResponseDto(load);
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("게임 id는 숫자값이어야 합니다.");
-                }
-            }, GSON::toJson);
-
-            post("/move", (request, response) -> {
-                MoveRequestDto moveRequestDto = GSON.fromJson(request.body(), MoveRequestDto.class);
-                chessService.move(moveRequestDto);
-                response.type("application/json; charset=utf-8");
-                return new MoveResponseDto(chessService.isEnd(moveRequestDto.getId()), chessService.nextColor(moveRequestDto.getId()));
-            }, GSON::toJson);
-        });
+    private Route moveRoute() {
+        return (request, response) -> {
+            MoveRequestDto moveRequestDto = GSON.fromJson(request.body(), MoveRequestDto.class);
+            chessService.move(moveRequestDto);
+            response.type("application/json; charset=utf-8");
+            return new MoveResponseDto(chessService.isEnd(moveRequestDto.getId()), chessService.nextColor(moveRequestDto.getId()));
+        };
     }
 
     private void exceptionHandling() {
         exception(RuntimeException.class, (e, request, response) -> {
             response.type("application/json; charset=utf-8");
             response.status(HTTP_STATUS_ERROR);
-            e.printStackTrace();
             response.body(GSON.toJson(new MessageResponseDto(e.getMessage())));
         });
-    }
-
-    private String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
