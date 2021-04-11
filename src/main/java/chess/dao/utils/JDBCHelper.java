b62258455c555bf8a4ee0fe865c6150fb14717b0
seunghyun <ggyool@naver.com>
2021-04-11 20:30:40 +0900

    feat: insert한 id를 얻는 기능 추가

--- src/main/java/chess/dao/utils/JDBCHelper.java
+++ src/main/java/chess/dao/utils/JDBCHelper.java
@@ -2,19 +2,31 @@ package chess.dao.utils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class JDBCHelper {
 
     public static PreparedStatement createPreparedStatement(
-        final Connection connection, final String query, final List<String> params)
+        final Connection connection, final String query, final List<Object> params)
         throws SQLException {
 
-        final PreparedStatement pstmt = connection.prepareStatement(query);
+        final PreparedStatement pstmt = connection
+            .prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
         for (int i = 0; i < params.size(); i++) {
-            pstmt.setString(i + 1, params.get(i));
+            pstmt.setObject(i + 1, params.get(i));
         }
         return pstmt;
+    }
+
+    public static Long getGeneratedKey(final PreparedStatement pstmt) throws SQLException {
+        try (
+            final ResultSet resultSet = pstmt.getGeneratedKeys()
+        ) {
+            resultSet.next();
+            return resultSet.getLong(1);
+        }
     }
 }
