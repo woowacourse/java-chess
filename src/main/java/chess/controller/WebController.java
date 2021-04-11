51ce612ee855fc7b986a0352ad8570ddbe4c3825
Kimun Kim <kkm97351@gmail.com>
2021-04-11 14:35:54 +0900

    refactor: 예상된 예외에 대한 일괄적 응답을 하는 메소드를 통한 중복 코드 제거

--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -4,7 +4,6 @@ import chess.dao.GameDAO;
 import chess.domain.ChessGameManager;
 import chess.domain.position.Position;
 import chess.dto.*;
-import chess.exception.DomainException;
 import chess.exception.HandledException;
 import com.google.gson.Gson;
 import org.json.JSONObject;
@@ -16,6 +15,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
@@ -46,70 +46,74 @@ public class WebController {
         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 
-    private CommonDto<?> createNewGame(Request request, Response response) {
+    private CommonDto<?> handleExpectedException(Supplier<CommonDto<?>> supplier, int statusCodeOnError) {
         try {
-            chessGameManager.start();
+            return supplier.get();
+        } catch (HandledException e) {
             return new CommonDto<>(
-                    StatusCode.OK,
-                    "새로운 게임을 시작합니다.",
-                    NewGameResponse.from(chessGameManager));
-        } catch (DomainException e) {
-            return new CommonDto<>(
-                    StatusCode.BAD_REQUEST,
+                    statusCodeOnError,
                     e.getMessage(),
                     new NoneItem());
         }
     }
 
+    private CommonDto<?> createNewGame(Request request, Response response) {
+        return handleExpectedException(() -> {
+            chessGameManager.start();
+            return new CommonDto<>(
+                    StatusCode.OK,
+                    "새로운 게임을 시작합니다.",
+                    NewGameResponse.from(chessGameManager));
+        }, StatusCode.BAD_REQUEST);
+    }
+
     private CommonDto<?> move(Request request, Response response) {
-        try {
+        return handleExpectedException(() -> {
             JSONObject jsonObject = new JSONObject(request.body());
             String from = jsonObject.getString("from");
             String to = jsonObject.getString("to");
 
             chessGameManager.move(Position.of(from), Position.of(to));
 
-            return new CommonDto<RunningGameResponse>(
+            return new CommonDto<>(
                     StatusCode.OK,
                     "기물을 이동했습니다.",
                     RunningGameResponse.from(chessGameManager));
-        } catch (DomainException e) {
-            return new CommonDto<>(
-                    StatusCode.BAD_REQUEST,
-                    e.getMessage(),
-                    new NoneItem());
-        }
+        }, StatusCode.BAD_REQUEST);
     }
 
     private CommonDto<?> saveGame(Request request, Response response) {
-        try {
-            gameDAO.saveGame(ChessBoardDto.from(chessGameManager.getBoard()), chessGameManager.getCurrentTurnColor());
-            return new CommonDto<>(
-                    StatusCode.CREATE,
-                    "게임을 저장했습니다.",
-                    new NoneItem());
-        } catch (SQLException e) {
-            return new CommonDto<>(
-                    StatusCode.INTERNAL_SERVER_ERROR,
-                    "게임을 데이터베이스에 저장하는 데에 오류가 발생했습니다.\n에러 메세지: " + e.getMessage(),
-                    new NoneItem());
-        }
+        return handleExpectedException(() -> {
+            try {
+                gameDAO.saveGame(ChessBoardDto.from(chessGameManager.getBoard()), chessGameManager.getCurrentTurnColor());
+                return new CommonDto<>(
+                        StatusCode.CREATE,
+                        "게임을 저장했습니다.",
+                        new NoneItem());
+            } catch (SQLException e) {
+                return new CommonDto<>(
+                        StatusCode.INTERNAL_SERVER_ERROR,
+                        "알 수 없는 서버 내부 오류입니다. 에러 메세지: " + e.getMessage(),
+                        new NoneItem());
+            }
+        }, StatusCode.BAD_REQUEST);
     }
 
-    private CommonDto<?> loadGame(Request request, Response response) throws SQLException {
-        try {
-            SavedGameData savedGameData = gameDAO.loadGame();
-            chessGameManager.load(savedGameData.getChessBoardDto().toChessBoard(), savedGameData.getCurrentTurnColor());
-            return new CommonDto<>(
-                    StatusCode.OK,
-                    "게임을 불러왔습니다.",
-                    RunningGameResponse.from(chessGameManager));
-        } catch (HandledException e) {
-            return new CommonDto<>(
-                    StatusCode.BAD_REQUEST,
-                    "게임을 불러오는 데에 오류가 발생했습니다.\n에러 메세지: " + e.getMessage(),
-                    new NoneItem());
-        }
-
+    private CommonDto<?> loadGame(Request request, Response response) {
+        return handleExpectedException(()-> {
+            try {
+                SavedGameData savedGameData = gameDAO.loadGame();
+                chessGameManager.load(savedGameData.getChessBoardDto().toChessBoard(), savedGameData.getCurrentTurnColor());
+                return new CommonDto<>(
+                        StatusCode.OK,
+                        "게임을 불러왔습니다.",
+                        RunningGameResponse.from(chessGameManager));
+            } catch (SQLException e) {
+                return new CommonDto<>(
+                        StatusCode.INTERNAL_SERVER_ERROR,
+                        "알 수 없는 서버 내부 오류입니다. 에러 메세지: " + e.getMessage(),
+                        new NoneItem());
+            }
+        }, StatusCode.BAD_REQUEST);
     }
 }
