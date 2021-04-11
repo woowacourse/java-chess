15a71f9167b6f4b5db198a9bb6cf73d2ae6707bc
Seed <hsik0225@gmail.com>
2021-04-11 20:42:49 +0900

    test(chess-dao):코드 변경에 따른 테스트 코드 변경

--- src/test/java/chess/domain/chess/ChessDAOTest.java
+++ src/test/java/chess/domain/chess/ChessDAOTest.java
@@ -1,96 +1,53 @@
 package chess.domain.chess;
 
-import java.sql.SQLException;
-import java.util.Optional;
-
-import org.assertj.core.api.ThrowableAssert;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
-import chess.domain.piece.PieceDAO;
+import chess.service.ChessService;
 
 public class ChessDAOTest {
 
+    private final ChessService chessService = new ChessService();
     private final ChessDAO chessDAO = new ChessDAO();
-    private final PieceDAO pieceDAO = new PieceDAO();
-    private Long chessId;
+    private final Long chessId = chessService.insert();
 
-    @BeforeEach
-    void setUp() throws SQLException {
-        Optional<Long> optionalPreviousChessId = chessDAO.findChessId();
-        if (optionalPreviousChessId.isPresent()) {
-            final Long previousChessId = optionalPreviousChessId.get();
-            pieceDAO.delete(previousChessId);
-            chessDAO.delete(previousChessId);
-        }
-
-        chessDAO.insert();
-        chessId = chessDAO.findChessId()
-                          .get();
-    }
-
-
-    @DisplayName("체스의 아이디 가져오기 테스트")
+    @DisplayName("체스 아이디로 체스 게임 정보 가져오기 테스트")
     @Test
-    void findChessId() throws SQLException {
+    void findChessByIdTest() {
 
         // when
-        ThrowableAssert.ThrowingCallable callable = () -> chessDAO.findChessId()
-                                                                  .get();
+        final ChessDTO chessDTO = chessDAO.findChessById(chessId);
 
         // then
-        assertThatCode(callable).doesNotThrowAnyException();
-    }
-
-    @DisplayName("체스 아이디로 체스의 현재 턴 가져오기 테스트")
-    @Test
-    void findTurnById() throws SQLException {
-
-        // when
-        final String turn = chessDAO.findTurnById(chessId)
-                                    .get();
-
-        // then
-        assertThat(turn).isEqualTo("WHITE");
+        assertThat(chessDTO.getStatus()).isEqualTo("RUNNING");
+        assertThat(chessDTO.getTurn()).isEqualTo("WHITE");
+        assertThat(chessDTO.getPieceDTOS()).size()
+                                           .isEqualTo(64);
     }
 
     @DisplayName("초기 체스판 삽입 테스트")
     @Test
-    void insert() throws SQLException {
-
-        // then
-        final boolean inserted = chessDAO.findTurnById(chessId)
-                                         .isPresent();
-        assertThat(inserted).isTrue();
-    }
-
-    @DisplayName("턴 변경 테스트")
-    @Test
-    void updateTurn() throws SQLException {
+    void insertTest() {
 
         // when
-        chessDAO.updateTurn(chessId);
+        Long newChessId = chessDAO.insert();
 
         // then
-        final String turn = chessDAO.findTurnById(chessId)
-                                    .get();
-        assertThat(turn).isEqualTo("BLACK");
+        assertThat(newChessId).isNotEqualTo(chessId);
     }
 
-    @DisplayName("체스 아이디에 해당 하는 체스 삭제 테스트")
+    @DisplayName("체스 상태 및 현재 턴 변경 테스트")
     @Test
-    void delete() throws SQLException {
+    void updateChessTest() {
 
         // when
-        chessDAO.delete(chessId);
+        chessDAO.updateChess(chessId, Status.KING_DEAD.name(), Color.BLACK.name());
 
         // then
-        final boolean isChessRemaining = chessDAO.findTurnById(chessId)
-                                                 .isPresent();
-        assertThat(isChessRemaining).isFalse();
+        final ChessDTO chessDTO = chessDAO.findChessById(chessId);
+        assertThat(chessDTO.getStatus()).isEqualTo("KING_DEAD");
+        assertThat(chessDTO.getTurn()).isEqualTo("BLACK");
     }
 }
