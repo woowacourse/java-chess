397d3e06be48bead427b934406b7cbf19710c030
Seed <hsik0225@gmail.com>
2021-04-11 19:02:42 +0900

    feat(chess-controller):컨트롤러가 기존 PieceDao의 역할도 같이 하도록 수정 및 새로운 게임 시작 기능 구현

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -16,31 +16,10 @@ public class ChessController {
         this.chessService = new ChessService();
     }
 
-    public String getIds(Request req, Response res) {
-        String chessId = chessService.findChessIdAsString();
-        return GSON.toJson(chessId);
-    }
-
-    public String getTurn(Request req, Response res) {
-        Long chessId = Long.valueOf(req.params(":chessId"));
-        String turn = chessService.findTurnById(chessId);
-        return GSON.toJson(turn);
-    }
-
-    public String insert(Request req, Response res) {
+    public String newGame(Request req, Response res) {
         Long chessId = chessService.insert();
-
         res.cookie("chessId", String.valueOf(chessId));
         res.status(201);
-
         return GSON.toJson(chessId);
-    }
-
-    public Response delete(Request req, Response res) {
-        Long chessId = Long.valueOf(req.params(":chessId"));
-        chessService.delete(chessId);
-
-        res.status(204);
-        return res;
     }
 }
--- src/main/java/chess/service/ChessService.java
+++ src/main/java/chess/service/ChessService.java
@@ -1,44 +1,32 @@
 package chess.service;
 
-import java.util.Optional;
+import java.util.List;
 
+import chess.domain.board.BoardDTO;
+import chess.domain.chess.Chess;
 import chess.domain.chess.ChessDAO;
+import chess.domain.piece.PieceDAO;
 
 public class ChessService {
     private final ChessDAO chessDAO;
+    private final PieceDAO pieceDAO;
 
     public ChessService() {
         this.chessDAO = new ChessDAO();
+        this.pieceDAO = new PieceDAO();
     }
 
     public Long insert() {
-        chessDAO.insert();
-        return findChessId();
-    }
+        final long newChessId = chessDAO.insert();
 
-    public void updateTurn(Long chessId) {
-        chessDAO.updateTurn(chessId);
+        final Chess chess = Chess.createWithEmptyBoard()
+                                 .start();
+        final BoardDTO boardDTO = BoardDTO.from(chess);
+        pieceDAO.insert(newChessId, boardDTO);
+        return newChessId;
     }
 
     public void delete(Long chessId) {
         chessDAO.delete(chessId);
-    }
-
-    public Long findChessId() {
-        return chessDAO.findChessId()
-                       .orElseThrow(() -> new IllegalStateException("진행 중인 게임이 없습니다."));
-    }
-
-    public String findChessIdAsString() {
-        final Optional<Long> chessId = chessDAO.findChessId();
-        if (chessId.isPresent()) {
-            return String.valueOf(chessId.get());
-        }
-        return "EMPTY";
-    }
-
-    public String findTurnById(Long chessId) {
-        return chessDAO.findTurnById(chessId)
-                       .orElseThrow(() -> new IllegalStateException("진행 중인 게임이 없습니다."));
     }
 }
