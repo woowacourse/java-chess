2eb469b80efc44da1f012767600659aa6ea21f32
seunghyun <ggyool@naver.com>
2021-04-12 00:13:46 +0900

    refactor: TestUserDao 분리

--- src/test/java/chess/service/UserServiceTest.java
+++ src/test/java/chess/service/UserServiceTest.java
@@ -3,18 +3,12 @@ package chess.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-import chess.dao.UserDaoInterface;
 import chess.dto.user.UserRequestDto;
-import chess.entity.User;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import chess.service.testdao.TestUserDao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UserServiceTest {
+public class UserServiceTest {
 
     private UserService userService;
 
@@ -33,29 +27,5 @@ class UserServiceTest {
 
         assertThat(userService.find(name).getName())
             .isEqualTo(name);
-    }
-}
-
-class TestUserDao implements UserDaoInterface {
-
-    private final List<User> users = new ArrayList<>();
-
-    @Override
-    public void insert(final String name) {
-        users.add(
-            new User(users.size(), name, 0, 0, LocalDateTime.now())
-        );
-    }
-
-    @Override
-    public Optional<User> selectByName(final String name) {
-        return users.stream()
-            .filter(user -> Objects.equals(user.getName(), name))
-            .findAny();
-    }
-
-    @Override
-    public void deleteByName(final String name) {
-
     }
 }
---
+++ src/test/java/chess/service/testdao/TestUserDao.java
@@ -0,0 +1,40 @@
+package chess.service.testdao;
+
+import chess.dao.UserDaoInterface;
+import chess.entity.User;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class TestUserDao implements UserDaoInterface {
+
+    private long generatedKey;
+    private final List<User> users;
+
+    public TestUserDao() {
+        this.generatedKey = 1;
+        this.users = new ArrayList<>();
+    }
+
+    @Override
+    public void insert(final String name) {
+        users.add(
+            new User(generatedKey++, name, 0, 0, LocalDateTime.now())
+        );
+    }
+
+    @Override
+    public Optional<User> selectByName(final String name) {
+        return users.stream()
+            .filter(user -> Objects.equals(user.getName(), name))
+            .findAny();
+    }
+
+    @Override
+    public void deleteByName(final String name) {
+
+    }
+}
+
