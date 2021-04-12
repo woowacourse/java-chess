bacbf26e5d576fbc678ae5ca89afdff461dad09b
seunghyun <ggyool@naver.com>
2021-04-12 16:14:58 +0900

    faet: 게임 초기 기물 생성 (PieceService)

--- src/main/java/chess/dto/piece/PieceDto.java
+++ src/main/java/chess/dto/piece/PieceDto.java
@@ -1,7 +1,9 @@
 package chess.dto.piece;
 
 import chess.domain.piece.Piece;
+import chess.domain.piece.PieceType;
 import chess.domain.team.Team;
+import chess.entity.PieceEntity;
 
 public class PieceDto {
 
@@ -18,8 +20,17 @@ public class PieceDto {
     }
 
     public static PieceDto from(final Piece piece) {
-        return new PieceDto(piece.getTeam(), piece.getPieceType().getValue(), piece.getX(),
-            piece.getY());
+        return new PieceDto(piece.getTeam(), piece.getPieceType().getValue(),
+            piece.getX(), piece.getY());
+    }
+
+    public static PieceDto from(final PieceEntity pieceEntity) {
+        return new PieceDto(pieceEntity.getTeam(), pieceEntity.getPieceType().getValue(),
+            pieceEntity.getX(), pieceEntity.getY());
+    }
+
+    public PieceEntity toEntity(final long gameId) {
+        return new PieceEntity(0L, gameId, PieceType.from(pieceLetter), team, x, y);
     }
 
     public Team getTeam() {
---
+++ src/main/java/chess/service/PieceService.java
@@ -0,0 +1,33 @@
+package chess.service;
+
+import chess.dao.PieceDaoInterface;
+import chess.domain.board.Board;
+import chess.domain.piece.Piece;
+import chess.dto.piece.PieceDto;
+import chess.entity.PieceEntity;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PieceService {
+
+    private final PieceDaoInterface pieceDao;
+
+    public PieceService(final PieceDaoInterface pieceDao) {
+        this.pieceDao = pieceDao;
+    }
+
+    public void initPieces(final long gameId) {
+        final List<Piece> pieces = Board.createWithInitialLocation().toList();
+        final List<PieceDto> pieceDtos = pieces.stream()
+            .map(PieceDto::from)
+            .collect(Collectors.toList());
+        addAll(gameId, pieceDtos);
+    }
+
+    private void addAll(final long gameId, final List<PieceDto> pieceDtos) {
+        final List<PieceEntity> pieceEntities = pieceDtos.stream()
+            .map(pieceDto -> pieceDto.toEntity(gameId))
+            .collect(Collectors.toList());
+        pieceDao.insertBatch(gameId, pieceEntities);
+    }
+}
