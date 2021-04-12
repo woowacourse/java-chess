690cc09b91de1feef5dc046c0c0a07abfb2e0087
Wannte <T.wannte@gmail.com>
2021-04-12 23:18:24 +0900

    refactor(Board): 메소드 분리

--- src/main/java/chess/domain/game/Board.java
+++ src/main/java/chess/domain/game/Board.java
@@ -19,34 +19,49 @@ public class Board {
     }
 
     public void move(Color color, Position from, Position to) {
-        Piece fromPiece = pieceByPosition.get(from);
-        if (!fromPiece.isSame(color)) {
-            throw new IllegalArgumentException("from 잘못 입력");
-        }
-        Piece toPiece = pieceByPosition.get(to);
-        if (toPiece.isSame(color)) {
-            throw new IllegalArgumentException("같은 색깔의 말을 잡을 수 없습니다.");
-        }
-        move(from, to);
+        validateFromPiece(color, from);
+        validateToPiece(color, to);
+        validateCanMove(from, to);
+        updateIsKingDead(to);
+        movePiece(from, to);
     }
 
-    public void move(Position from, Position to) {
-        Piece toPiece = pieceByPosition.get(to);
+    private void validateFromPiece(Color color, Position from) {
+        if (!pieceByPosition.get(from)
+                            .isSame(color)) {
+            throw new IllegalArgumentException("같은 색깔의 말을 골라야 합니다.");
+        }
+    }
+
+    private void validateToPiece(Color color, Position to) {
+        if (pieceByPosition.get(to)
+                           .isSame(color)) {
+            throw new IllegalArgumentException("같은 색깔의 말을 잡을 수 없습니다.");
+        }
+    }
+
+    private void validateCanMove(Position from, Position to) {
         if (!movablePositions(from).contains(to)) {
             throw new IllegalArgumentException("이동할 수 없는 위치로의 이동입니다.");
-        }
-        pieceByPosition.put(to, pieceByPosition.get(from));
-        pieceByPosition.put(from, new Empty());
-        if (toPiece.isKing()) {
-            isKingDead = true;
         }
     }
 
     private List<Position> movablePositions(Position from) {
-        Piece fromPiece = pieceByPosition.get(from);
-        return fromPiece.movablePositions(from, allPieces());
+        return pieceByPosition.get(from)
+                              .movablePositions(from, allPieces());
     }
 
+    private void updateIsKingDead(Position to) {
+        if (pieceByPosition.get(to)
+                           .isKing()) {
+            isKingDead = true;
+        }
+    }
+
+    private void movePiece(Position from, Position to) {
+        pieceByPosition.put(to, pieceByPosition.get(from));
+        pieceByPosition.put(from, new Empty());
+    }
 
     public boolean isKingDead() {
         return isKingDead;
