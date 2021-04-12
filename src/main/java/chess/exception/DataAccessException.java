58f55c33d767707925ff161536bbb9da81a1932b
Gomding <backchida@gmail.com>
2021-04-12 20:38:16 +0900

    refactor() : 커스텀 예외의 사용하지않는 생성자 삭제

--- src/main/java/chess/exception/DataAccessException.java
+++ src/main/java/chess/exception/DataAccessException.java
@@ -2,13 +2,6 @@ package chess.exception;
 
 public class DataAccessException extends RuntimeException{
 
-    public DataAccessException() {
-    }
-
-    public DataAccessException(String message) {
-        super(message);
-    }
-
     public DataAccessException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/chess/exception/DriverNotFoundException.java
+++ src/main/java/chess/exception/DriverNotFoundException.java
@@ -2,13 +2,6 @@ package chess.exception;
 
 public class DriverNotFoundException extends RuntimeException{
 
-    public DriverNotFoundException() {
-    }
-
-    public DriverNotFoundException(String message) {
-        super(message);
-    }
-
     public DriverNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
