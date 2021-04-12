d2084ee7732e91dd6a25a5a831894d908d1145a1
lns13301 <rkdrbgks22@ks.ac.kr>
2021-04-12 11:45:24 +0900

    refactor: try-with-resource 를 통해 예외 발생 시 connection 이 종료 되도록 변경

--- src/main/java/chess/dao/ChessLogDao.java
+++ src/main/java/chess/dao/ChessLogDao.java
@@ -51,39 +51,59 @@ public class ChessLogDao {
         }
     }
 
-    public void addLog(String roomId, String target, String destination) throws SQLException {
+    public void addLog(String roomId, String target, String destination) {
         Connection connection = getConnection();
         String query = "INSERT INTO chessgame (room_id, target, destination) VALUES (?, ?, ?)";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setString(1, roomId);
-        pstmt.setString(2, target);
-        pstmt.setString(3, destination);
-        pstmt.executeUpdate();
-        closeConnection(connection);
+        try (PreparedStatement pstmt = connection.prepareStatement(query);) {
+            pstmt.setString(1, roomId);
+            pstmt.setString(2, target);
+            pstmt.setString(3, destination);
+            pstmt.executeUpdate();
+            closeConnection(connection);
+        }
+        catch (SQLException e) {
+            System.err.println("로그 추가 오류:" + e.getMessage());
+            e.printStackTrace();
+            closeConnection(connection);
+        }
     }
 
-    public List<String> applyCommand(String userId) throws SQLException {
+    public List<String> applyCommand(String userId) {
         Connection connection = getConnection();
         String query = "SELECT target, destination FROM chess.chessgame WHERE room_id = ? ORDER BY command_date ASC;";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setString(1, userId);
-        ResultSet rs = pstmt.executeQuery();
-        List<String> commands = new ArrayList<>();
+        try (PreparedStatement pstmt = connection.prepareStatement(query);) {
+            pstmt.setString(1, userId);
+            ResultSet rs = pstmt.executeQuery();
+            List<String> commands = new ArrayList<>();
 
-        while (rs.next()) {
-            commands.add(MOVE + rs.getString(1) + DELIMITER + rs.getString(2));
+            while (rs.next()) {
+                commands.add(MOVE + rs.getString(1) + DELIMITER + rs.getString(2));
+            }
+            closeConnection(connection);
+
+            return commands;
         }
-        closeConnection(connection);
-
-        return commands;
+        catch (SQLException e) {
+            System.err.println("커맨드 적용 오류:" + e.getMessage());
+            e.printStackTrace();
+            closeConnection(connection);
+            return null;
+        }
     }
 
-    public void deleteLog(String roomId) throws SQLException {
+    public void deleteLog(String roomId) {
         Connection connection = getConnection();
         String query = "DELETE FROM chess.chessgame WHERE room_id = ?";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setString(1, roomId);
-        pstmt.executeUpdate();
-        closeConnection(connection);
+
+        try (PreparedStatement pstmt = connection.prepareStatement(query);) {
+            pstmt.setString(1, roomId);
+            pstmt.executeUpdate();
+            closeConnection(connection);
+        }
+        catch (SQLException e) {
+            System.err.println("로그 제거 오류:" + e.getMessage());
+            e.printStackTrace();
+            closeConnection(connection);
+        }
     }
 }
