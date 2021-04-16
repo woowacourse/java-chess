cef60728e9f61f81506789c222b32104eb222d70
Wannte <T.wannte@gmail.com>
2021-04-16 16:22:02 +0900

    refactor(ChessService): 서비스 병합

--- src/main/java/chess/WebUIChessApplication.java
+++ src/main/java/chess/WebUIChessApplication.java
@@ -5,12 +5,10 @@ import chess.dao.CommandDao;
 import chess.service.ChessService;
 
 public class WebUIChessApplication {
-
     public static void main(String[] args) {
         CommandDao commandDao = new CommandDao();
 
         WebChessController chessController = new WebChessController(new ChessService(commandDao));
         chessController.run();
     }
-
 }
