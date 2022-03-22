2f14b763b670381322e88fbaa15c652094f24333
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-03-22 23:49:28 +0900

    feat(Piece): Piece에 타입 상태 추가 및 비교 구현 및 테스트 작성

--- src/main/java/chess/PieceType.java
+++ src/main/java/chess/PieceType.java
@@ -1,5 +1,20 @@
 package chess;
 
 public enum PieceType {
-    PAWN;
+    PAWN("P"),
+    KNIGHT("N"),
+    ROOK("R"),
+    BISHOP("B"),
+    QUEEN("Q"),
+    KING("K");
+
+    private final String name;
+
+    PieceType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
