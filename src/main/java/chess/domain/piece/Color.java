62a57f9d655f2e8669b0ed1ec44cdab0e0e33b34
sinb57 <sinb57@naver.com>
2022-03-22 23:35:34 +0900

    feat: Color에 따른 문자케이스 변경 및 테스트

--- src/main/java/chess/domain/piece/Color.java
+++ src/main/java/chess/domain/piece/Color.java
@@ -1,8 +1,20 @@
 package chess.domain.piece;
 
+import java.util.function.Function;
+
 public enum Color {
 
-    WHITE,
-    BLACK,
+    WHITE(String::toLowerCase),
+    BLACK(String::toUpperCase),
     ;
+
+    public final Function<String, String> caseConverter;
+
+    Color(Function<String, String> caseConvertor) {
+        this.caseConverter = caseConvertor;
+    }
+
+    public String convertToCase(String value) {
+        return caseConverter.apply(value);
+    }
 }
