d5ff3178f5d5fe24104e4fd1defd55f4ad29099b
sinb57 <sinb57@naver.com>
2022-03-22 15:51:16 +0900

    refactor: 위치 가로 제한 범위 상수화

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -2,6 +2,9 @@ package chess.domain;
 
 public class Position {
 
+    private static final char MIN_ROW = 'a';
+    private static final char MAX_ROW = 'h';
+
     private final char row;
 
     public Position(char row) {
@@ -10,7 +13,7 @@ public class Position {
     }
 
     private void validateRowInRange(char row) {
-        if (row < 'a' || row > 'h') {
+        if (row < MIN_ROW || row > MAX_ROW) {
             throw new IllegalArgumentException("가로 위치는 a~h 범위에 포함되어야 합니다.");
         }
     }
