6429b433b4f744d129a51608e98959af8090416b
이한울 <dkfghg3@naver.com>
2022-03-22 22:55:28 +0900

    feat: 위치를 계산하는 기능 구현

---
+++ src/main/java/chess/domain/Position.java
@@ -0,0 +1,80 @@
+package chess.domain;
+
+import java.util.Objects;
+
+public class Position {
+
+    private static final int MIN_RANK_RANGE = 1;
+    private static final int MAX_RANK_RANGE = 8;
+    private static final char MIN_FILE_RANGE = 'a';
+    private static final char MAX_FILE_RANGE = 'h';
+    private static final int WHITE_PAWN_FIRST_RANK = 2;
+    private static final int BLACK_PAWN_FIRST_RANK = 7;
+
+    private final int rank;
+    private final char file;
+
+    public Position(final int rank, final char file) {
+        char lowerCaseFile = Character.toLowerCase(file);
+        validatePositionRange(rank, lowerCaseFile);
+        this.rank = rank;
+        this.file = lowerCaseFile;
+    }
+
+    private void validatePositionRange(final int rank, final char file) {
+        if (rank < MIN_RANK_RANGE || rank > MAX_RANK_RANGE) {
+            throw new IllegalArgumentException("잘못된 범위입니다.");
+        }
+        if (file < MIN_FILE_RANGE || file > MAX_FILE_RANGE) {
+            throw new IllegalArgumentException("잘못된 범위입니다.");
+        }
+    }
+
+    public boolean isFirstTurnOfPawn() {
+        return rank == WHITE_PAWN_FIRST_RANK || rank == BLACK_PAWN_FIRST_RANK;
+    }
+
+    public int countMoveForward(final Position destination) {
+        if (file != destination.getFile()) {
+            throw new IllegalArgumentException("전진이 아닌 다른 방향으로 이동했습니다.");
+        }
+        if (destination.getRank() - rank < 0) {
+            throw new IllegalArgumentException("후진하였습니다.");
+        }
+        return destination.getRank() - rank;
+    }
+
+    public int countMoveDiagonal(final Position destination) {
+        final int fileDistance = Math.abs(file - destination.getFile());
+        final int rankDistance = Math.abs(rank - destination.getRank());
+        if (fileDistance != rankDistance) {
+            throw new IllegalArgumentException("대각선이 아닌 방향으로 이동했습니다.");
+        }
+        return destination.getRank() - rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return rank == position.rank && file == position.file;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rank, file);
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public char getFile() {
+        return file;
+    }
+}
---
+++ src/test/java/chess/domain/PositionTest.java
@@ -0,0 +1,63 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class PositionTest {
+
+    @Test
+    @DisplayName("범위가 정상인 경우, Position을 생성한다.")
+    void createPosition() {
+        assertThatCode(() -> new Position(2, 'B'))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest(name = "범위 벗어난 경우, 예외를 발생시킨다.")
+    @MethodSource("provideInvalidPosition")
+    void createPositionException(final int rank, final char file) {
+        assertThatThrownBy(() -> new Position(rank, file))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 범위입니다.");
+    }
+
+    private static Stream<Arguments> provideInvalidPosition() {
+        return Stream.of(
+                Arguments.of(0, 'a'),
+                Arguments.of(4, 'i'),
+                Arguments.of(9, 'a'),
+                Arguments.of(8, 'I')
+        );
+    }
+
+    @Test
+    @DisplayName("전진으로 몇 칸 이동하는지 확인한다.")
+    void checkMoveForward() {
+        final Position currentPosition = new Position(2, 'b');
+        final Position destinationPosition = new Position(3, 'b');
+        final int expected = 1;
+
+        final int actual = currentPosition.countMoveForward(destinationPosition);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("대각선으로 몇 칸 이동하는지 확인한다.")
+    void checkMoveDiagonal() {
+        final Position currentPosition = new Position(2, 'a');
+        final Position destinationPosition = new Position(4, 'c');
+        final int expected = 2;
+
+        final int actual = currentPosition.countMoveDiagonal(destinationPosition);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
