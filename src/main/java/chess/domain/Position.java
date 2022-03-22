8068becc330d57a7df1c6116bc254db796242a80
sinb57 <sinb57@naver.com>
2022-03-22 16:01:36 +0900

    feat: 위치 세로 범위 예외처리 및 테스트

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -4,17 +4,28 @@ public class Position {
 
     private static final char MIN_ROW = 'a';
     private static final char MAX_ROW = 'h';
+    private static final char MIN_COLUMN = '1';
+    private static final char MAX_COLUMN = '8';
 
     private final char row;
+    private final char column;
 
-    public Position(char row) {
+    public Position(char row, char column) {
         validateRowInRange(row);
+        validateColumnInRange(column);
         this.row = row;
+        this.column = column;
     }
 
     private void validateRowInRange(char row) {
         if (row < MIN_ROW || row > MAX_ROW) {
             throw new IllegalArgumentException("가로 위치는 a~h 범위에 포함되어야 합니다.");
+        }
+    }
+
+    private void validateColumnInRange(char column) {
+        if (column < MIN_COLUMN || column > MAX_COLUMN) {
+            throw new IllegalArgumentException("세로 위치는 1~8 범위에 포함되어야 합니다.");
         }
     }
 }
--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -12,8 +12,17 @@ public class PositionTest {
     @ValueSource(chars = {'`', 'A', 'i'})
     @DisplayName("가로 위치가 범위를 벗어나면 예외발생")
     void outOfRawRangeException(char row) {
-        assertThatThrownBy(() -> new Position(row))
+        assertThatThrownBy(() -> new Position(row, '1'))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("가로 위치는 a~h 범위에 포함되어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(chars = {'a', '0', '9'})
+    @DisplayName("세로 위치가 범위를 벗어나면 예외발생")
+    void outOfColumnRangeException(char column) {
+        assertThatThrownBy(() -> new Position('a', column))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("세로 위치는 1~8 범위에 포함되어야 합니다.");
     }
 }
