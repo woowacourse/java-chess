9d38e71f29b655a07ec65240b27808a97d090008
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-22 17:47:14 +0900

    feat(Board): implement Board domain
    - initialize with 8 rows with chess pieces

---
+++ src/main/java/chess2/domain/board/Board.java
@@ -0,0 +1,29 @@
+package chess2.domain.board;
+
+import chess2.domain.square.Color;
+import java.util.List;
+
+public class Board {
+
+    private final List<Row> rows;
+
+    public Board() {
+        this.rows = List.of(Row.ofEmpty(),
+                Row.ofPawn(Color.BLACK),
+                Row.ofEmpty(),
+                Row.ofEmpty(),
+                Row.ofEmpty(),
+                Row.ofEmpty(),
+                Row.ofPawn(Color.WHITE),
+                Row.ofEmpty());
+    }
+
+    public List<Row> getRows() {
+        return rows;
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" + "rows=" + rows + '}';
+    }
+}
