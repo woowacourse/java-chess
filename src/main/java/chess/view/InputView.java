afe7475eb5122d1cf19ad6ac756877659c360384
Juhyung990122 <harukawa99@naver.com>
2022-03-22 16:46:52 +0900

    feat: 게임 시작 기능 구현

---
+++ src/main/java/chess/view/InputView.java
@@ -0,0 +1,35 @@
+package chess.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static void main(String[] args) {
+        requestCommand();
+    }
+
+    private static String input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static String requestCommand() {
+        String command = input().toLowerCase();
+        validateNull(command);
+        validateCommand(command);
+
+        return command;
+    }
+
+    private static void validateNull(String text) {
+        if (text == null) {
+            throw new IllegalArgumentException("null은 허용되지 않습니다.");
+        }
+    }
+
+    private static void validateCommand(String text) {
+        if (!text.equals("start") && !text.equals("end")) {
+            throw new IllegalArgumentException("잘못된 명령입니다.");
+        }
+    }
+}
