64e9e887e48634cb820000e3791b42a2ef496b6d
Juhyung990122 <harukawa99@naver.com>
2022-03-22 20:57:49 +0900

    test: 기물 탐색 기능 테스트 작성

--- src/test/java/chess/domain/ChessBoardTest.java
+++ src/test/java/chess/domain/ChessBoardTest.java
@@ -1,16 +1,41 @@
 package chess.domain;
 
+import chess.domain.chessPiece.ChessPiece;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChessBoardTest {
 
+    ChessBoard chessBoard = new ChessBoard();
+
     @Test
+    @DisplayName("체스판을 초기화하고 기물을 배치한다.")
     void init() {
-        ChessBoard chessBoard = new ChessBoard();
         int actual = chessBoard.countPiece();
         int expected = 32;
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"a1:r", "a8:R"}, delimiter = ':')
+    @DisplayName("위치를 기반으로 기물을 찾는다.")
+    void findPiece(String position, String expected) {
+        Optional<ChessPiece> possiblePiece = chessBoard.findPiece(position);
+        String actual = possiblePiece.get().getName();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("위치에 기물이 있는지 확인한다.")
+    void findPiece_Null(){
+        Optional<ChessPiece> actual = chessBoard.findPiece("a3");
+        assertThat(actual.isEmpty()).isEqualTo(true);
     }
 }
