2e0e8773efb1df06628cc364c2ebe8fd9f0b75a0
pup-paw <wldusdl0310@gmail.com>
2022-03-23 01:48:15 +0900

    feat: enum 값을 가져오는 기능 추가

--- src/main/java/chess/File.java
+++ src/main/java/chess/File.java
@@ -15,4 +15,8 @@ public enum File {
     File(String value){
         this.value = value;
     }
+
+    public String getValue() {
+        return value;
+    }
 }
--- src/main/java/chess/Rank.java
+++ src/main/java/chess/Rank.java
@@ -15,4 +15,8 @@ public enum Rank {
     Rank(String value) {
         this.value = value;
     }
+
+    public String getValue() {
+        return value;
+    }
 }
