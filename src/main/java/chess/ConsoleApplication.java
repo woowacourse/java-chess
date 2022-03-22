ad7825fbb282038f00bd1d51fd778865fb9acc2a
yhh1056 <yhh1056@naver.com>
2022-03-22 20:34:07 +0900

    chore: 체스판 출력 확인

--- src/main/java/chess/ConsoleApplication.java
+++ src/main/java/chess/ConsoleApplication.java
@@ -1,9 +1,32 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ConsoleApplication {
 
     public static void main(final String[] args) {
-        final Board board = Board.init();
+        final BoardInitializer initializer = new BoardInitializer();
+        final Board board = new Board(initializer.initialize());
+        final List<Piece> pieces = board.getPieces();
+
+        String[][] boards = new String[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                boards[i][j] = ".";
+            }
+        }
+
+        for (Piece piece : pieces) {
+            final Position position = piece.getPosition();
+            final PieceType pieceType = piece.getPieceType();
+            boards[position.getRow()][position.getColumn()] = pieceType.getName().name();
+        }
+
+        for (String[] strings : boards) {
+            System.out.println(Arrays.toString(strings));
+        }
     }
 
 }
