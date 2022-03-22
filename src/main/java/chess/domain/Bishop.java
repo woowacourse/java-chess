0666c5f5c4dfbc431381d6b07d70e6276ee66af5
meatsby <meatsby@gmail.com>
2022-03-22 17:51:23 +0900

    feat: 비숍 기물 객체 초기화 및 테스트 작성

---
+++ src/main/java/chess/domain/Bishop.java
@@ -0,0 +1,19 @@
+package chess.domain;
+
+public class Bishop extends Piece {
+
+    private static final char WHITE_SIGNATURE = 'b';
+    private static final char BLACK_SIGNATURE = 'B';
+
+    private Bishop(Position position, char signature) {
+        super(position, signature);
+    }
+
+    public static Bishop createWhite(Position position) {
+        return new Bishop(position, WHITE_SIGNATURE);
+    }
+
+    public static Bishop createBlack(Position position) {
+        return new Bishop(position, BLACK_SIGNATURE);
+    }
+}
---
+++ src/test/java/chess/domain/BishopTest.java
@@ -0,0 +1,25 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BishopTest {
+
+    @DisplayName("백팀 비숍 기물이 생성되었을 때 소문자 시그니처로 표시된다.")
+    @Test
+    void createWhite_Bishop_k() {
+        Bishop bishop = Bishop.createWhite(new Position("a1"));
+
+        assertThat(bishop.getSignature()).isEqualTo('b');
+    }
+
+    @DisplayName("흑팀 비숍 기물이 생성되었을 때 대문자 시그니처로 표시된다.")
+    @Test
+    void createBlack_Bishop_K() {
+        Bishop bishop = Bishop.createBlack(new Position("a1"));
+
+        assertThat(bishop.getSignature()).isEqualTo('B');
+    }
+}
