3ac86fa5f75ff13e6398a45290cbf32927468ba7
sinb57 <sinb57@naver.com>
2022-03-22 21:54:56 +0900

    test: 커맨드 생성 테스트 추가

--- src/test/java/chess/view/CommandTest.java
+++ src/test/java/chess/view/CommandTest.java
@@ -1,9 +1,11 @@
 package chess.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CommandTest {
@@ -15,5 +17,13 @@ public class CommandTest {
         assertThatThrownBy(() -> Command.from(command))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("존재하지 않는 커맨드입니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"start, START", "end, END"})
+    @DisplayName("커맨드 생성")
+    void createCommand(String input, Command expected) {
+        Command actual = Command.from(input);
+        assertThat(actual).isEqualTo(expected);
     }
 }
