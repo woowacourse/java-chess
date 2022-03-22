a4768b54ec2ecd60714c7802ae1e209ea3c20234
jayjaehunchoi <wogns0108@naver.com>
2022-03-22 17:40:26 +0900

    refactor : 입력에서 enum을 반환하도록 수정

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,13 +1,14 @@
 package chess.view;
 
+import chess.GameSwitch;
 import java.util.Scanner;
 
 public class InputView {
 
     private final static Scanner scanner = new Scanner(System.in);
 
-    public static String inputStartOrEnd() {
+    public static GameSwitch inputStartOrEnd() {
         System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
-        return scanner.nextLine();
+        return GameSwitch.of(scanner.nextLine());
     }
 }
