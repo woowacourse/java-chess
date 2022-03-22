41bc24008dca58ead545b5bff921e7eb6362e766
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-22 23:05:20 +0900

    feat(Knight): implement new Knight domain
    - manage position info

---
+++ src/main/java/chess/domain/piece/Knight.java
@@ -0,0 +1,48 @@
+package chess.domain.piece;
+
+import chess.domain.piece.position.Position;
+import java.util.Objects;
+
+public class Knight extends Piece {
+
+    private static final String BLACK_DISPLAY = "♘";
+    private static final String WHITE_DISPLAY = "♞";
+
+    public Knight(Color color, Position position) {
+        super(color, position);
+    }
+
+    @Override
+    public String display() {
+        if (color == Color.BLACK) {
+            return BLACK_DISPLAY;
+        }
+        return WHITE_DISPLAY;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Knight knight = (Knight) o;
+        return color == knight.color
+                && position == knight.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, position);
+    }
+
+    @Override
+    public String toString() {
+        return "Knight{" +
+                "color=" + color +
+                ", position=" + position +
+                '}';
+    }
+}
---
+++ src/test/java/chess/piece/KnightTest.java
@@ -0,0 +1,39 @@
+package chess.piece;
+
+import static chess.domain.piece.Color.BLACK;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import chess.domain.piece.Knight;
+import chess.domain.piece.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class KnightTest {
+
+    @DisplayName("색과 위치가 동일한 Knight 인스턴스는 서로 동일하다고 간주된다.")
+    @Test
+    void equals_sameOnSameColorAndPosition() {
+        Knight actual = new Knight(BLACK, Position.of("a2"));
+        Knight expected = new Knight(BLACK, Position.of("a2"));
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("다른 위치에 있는 Knight 인스턴스는 색이 같더라도 서로 다른 것으로 간주된다.")
+    @Test
+    void equals_differentOnPositionDifference() {
+        Knight knight1 = new Knight(BLACK, Position.of("b1"));
+        Knight knight2 = new Knight(BLACK, Position.of("g1"));
+
+        assertThat(knight1).isNotEqualTo(knight2);
+    }
+
+    @DisplayName("같은 색과 위치의 Knight 인스턴스의 해쉬코드 값은 서로 같다.")
+    @Test
+    void hashCode_sameOnSameColor() {
+        int actual = new Knight(BLACK, Position.of("a2")).hashCode();
+        int expected = new Knight(BLACK, Position.of("a2")).hashCode();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
