17e61a88570ee2c5bbe2696951ea30e9538bcb0d
jayjaehunchoi <wogns0108@naver.com>
2022-03-22 17:10:24 +0900

    feat : Board 초기화 기능 추가

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,7 +1,16 @@
 package chess;
 
+import chess.piece.Bishop;
+import chess.piece.Blank;
+import chess.piece.Color;
+import chess.piece.King;
+import chess.piece.Knight;
+import chess.piece.Pawn;
 import chess.piece.Piece;
+import chess.piece.Queen;
+import chess.piece.Rock;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
@@ -13,6 +22,45 @@ public class Board {
     }
 
     public static Board create() {
-        return new Board(new HashMap<>());
+        List<Position> positions = Position.init();
+        Map<Position, Piece> value = initEmptyBoard(positions);
+
+        initFirstLine(Color.BLACK, Row.EIGHT, value);
+        initPawn(Color.BLACK, Row.SEVEN, value);
+        initFirstLine(Color.WHITE, Row.ONE, value);
+        initPawn(Color.WHITE, Row.TWO, value);
+        return new Board(value);
+    }
+
+    private static Map<Position, Piece> initEmptyBoard(List<Position> positions) {
+        Map<Position, Piece> value = new HashMap<>();
+        for (Position position : positions) {
+            value.put(position, new Blank(Color.NONE));
+        }
+        return value;
+    }
+
+    private static void initFirstLine(final Color color, Row row, final Map<Position, Piece> value) {
+        List<Piece> pieces = List.of(
+                new Rock(color),
+                new Knight(color),
+                new Bishop(color),
+                new King(color),
+                new Queen(color),
+                new Bishop(color),
+                new Knight(color),
+                new Rock(color)
+                );
+        Column[] columns = Column.values();
+        for (int i = 0; i < 8; i++) {
+            value.replace(new Position(columns[i], row), pieces.get(i));
+        }
+    }
+
+    private static void initPawn(final Color color, Row row, final Map<Position, Piece> value) {
+        Column[] columns = Column.values();
+        for (int i = 0; i < 8; i++) {
+            value.replace(new Position(columns[i], row), new Pawn(color));
+        }
     }
 }
--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Position  {
 
@@ -22,5 +23,22 @@ public class Position  {
             }
         }
         return positions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return column == position.column && row == position.row;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column, row);
     }
 }
