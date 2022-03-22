1c57f24d18ccef79f9695e6be1a5da457d980fb3
Donggyu <a29661498@gmail.com>
2022-03-22 18:18:38 +0900

    feat(Type): 체스말의 타입을 나타내는 Type 정의

---
+++ src/main/java/chess/Type.java
@@ -0,0 +1,16 @@
+package chess;
+
+public enum Type {
+    PAWN("p"),
+    BISHOP("b"),
+    KNIGHT("n"),
+    ROOK("r"),
+    QUEEN("q"),
+    KING("k");
+
+    private final String symbol;
+
+    Type(String symbol) {
+        this.symbol = symbol;
+    }
+}
---
+++ src/test/java/chess/TypeTest.java
@@ -0,0 +1,21 @@
+package chess;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TypeTest {
+    @DisplayName("타입의 개수를 테스트")
+    @Test
+    public void size () {
+        //given
+        Type[] types = Type.values();
+
+        //when
+        int size = types.length;
+        //then
+        assertThat(size).isEqualTo(6);
+
+    }
+}
