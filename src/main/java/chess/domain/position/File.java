719861f3613fa2f9bc7164b063a4bdc3f4f015a6
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-23 17:30:55 +0900

    feat : File에 a~h이외의 값이 오는 경우 예외처리 기능 구현

--- src/main/java/chess/domain/position/File.java
+++ src/main/java/chess/domain/position/File.java
@@ -1,6 +1,32 @@
 package chess.domain.position;
 
+import java.util.Arrays;
+
 public enum File {
 
-    A, B, C, D, E, F, G, H
+    A("a"),
+    B("b"),
+    C("c"),
+    D("d"),
+    E("e"),
+    F("f"),
+    G("g"),
+    H("h");
+
+    private final String file;
+
+    File(String file) {
+        this.file = file;
+    }
+
+    public static File of(final String file) {
+        return Arrays.stream(File.values())
+            .filter(value -> value.getFile().equals(file))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 File 값 입니다."));
+    }
+
+    public String getFile() {
+        return file;
+    }
 }
--- src/test/java/chess/domain/position/FileTest.java
+++ src/test/java/chess/domain/position/FileTest.java
@@ -14,4 +14,16 @@ public class FileTest {
             .containsOnly(File.A, File.B, File.C, File.D, File.E, File.F, File.G, File.H);
     }
 
+    @Test
+    @DisplayName("FIle에 a~h값이 있는지 확인한다.")
+    void containValue() {
+        assertThat(File.A.getFile()).isEqualTo("a");
+    }
+
+    @Test
+    @DisplayName("값을 이용해 File을 찾는다.")
+    void findFile() {
+        assertThat(File.of("c")).isEqualTo(File.C);
+    }
+
 }
