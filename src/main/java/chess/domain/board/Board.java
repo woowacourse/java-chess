83ebb15e7f0f8ab57ec326f0e34e8b33b48fda71
= <rerub0831@gmail.com>
2022-03-23 14:41:27 +0900

    feat : 체스 보드 초기화 기능 구현

--- src/main/java/chess/domain/board/Board.java
+++ src/main/java/chess/domain/board/Board.java
@@ -1,7 +1,14 @@
 package chess.domain.board;
 
+import chess.domain.piece.Bishop;
+import chess.domain.piece.Color;
 import chess.domain.piece.EmptyPiece;
+import chess.domain.piece.King;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
+import chess.domain.piece.Queen;
+import chess.domain.piece.Rook;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,12 +21,39 @@ public class Board {
 
     private Map<Position, Piece> initialize() {
         Map<Position, Piece> squares = new HashMap<>();
+
+        initEmptyPieces(squares);
+        initNotPawnSqaures(squares, Rank.ONE, Color.WHITE);
+        initPawnPieces(squares, Rank.TWO, Color.WHITE);
+        initNotPawnSqaures(squares, Rank.SEVEN, Color.BLACK);
+        initPawnPieces(squares, Rank.EIGHT, Color.BLACK);
+
+        return squares;
+    }
+
+    private void initEmptyPieces(Map<Position, Piece> squares) {
         for (File file : File.values()) {
             for (Rank rank : Rank.values()) {
                 squares.put(new Position(file, rank), new EmptyPiece());
             }
         }
-        return squares;
+    }
+
+    private void initPawnPieces(Map<Position, Piece> squares, Rank rank, Color color) {
+        for (File file : File.values()) {
+            squares.replace(new Position(file, rank), new Pawn(color));
+        }
+    }
+
+    private void initNotPawnSqaures(Map<Position, Piece> squares, Rank rank, Color color) {
+        squares.replace(new Position(File.A, rank), new Rook(color));
+        squares.replace(new Position(File.B, rank), new Knight(color));
+        squares.replace(new Position(File.C, rank), new Bishop(color));
+        squares.replace(new Position(File.D, rank), new Queen(color));
+        squares.replace(new Position(File.E, rank), new King(color));
+        squares.replace(new Position(File.F, rank), new Bishop(color));
+        squares.replace(new Position(File.G, rank), new Knight(color));
+        squares.replace(new Position(File.H, rank), new Rook(color));
     }
 
     public Piece findByPosition(Position position) {
--- src/test/java/chess/domain/board/BoardTest.java
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -1,8 +1,13 @@
 package chess.domain.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import chess.domain.piece.EmptyPiece;
+import chess.domain.piece.Bishop;
+import chess.domain.piece.King;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Queen;
 import chess.domain.piece.Rook;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +15,27 @@ import org.junit.jupiter.api.Test;
 class BoardTest {
 
     @Test
-    @DisplayName("보드를 생성할 수 있다.")
-    void createBoard() {
+    @DisplayName("체스판 중 Pawn을 제외한 기물을 생성할 수 있다.")
+    void createBoardOfExcludePawn() {
         Board board = new Board();
-        assertThat(board.findByPosition(new Position(File.A,Rank.ONE)))
-                .isInstanceOf(EmptyPiece.class);
+
+        assertAll(
+                () -> assertThat(board.findByPosition(new Position(File.A, Rank.ONE))).isInstanceOf(Rook.class),
+                () -> assertThat(board.findByPosition(new Position(File.B, Rank.ONE))).isInstanceOf(Knight.class),
+                () -> assertThat(board.findByPosition(new Position(File.C, Rank.ONE))).isInstanceOf(Bishop.class),
+                () -> assertThat(board.findByPosition(new Position(File.D, Rank.ONE))).isInstanceOf(Queen.class),
+                () -> assertThat(board.findByPosition(new Position(File.E, Rank.ONE))).isInstanceOf(King.class),
+                () -> assertThat(board.findByPosition(new Position(File.F, Rank.ONE))).isInstanceOf(Bishop.class),
+                () -> assertThat(board.findByPosition(new Position(File.G, Rank.ONE))).isInstanceOf(Knight.class),
+                () -> assertThat(board.findByPosition(new Position(File.H, Rank.ONE))).isInstanceOf(Rook.class)
+        );
+    }
+
+    @Test
+    @DisplayName("체스판 중 Pawn 기물을 생성할 수 있다.")
+    void createBoardOfPawn() {
+        Board board = new Board();
+
+        assertThat(board.findByPosition(new Position(File.A, Rank.TWO))).isInstanceOf(Pawn.class);
     }
 }
