20ae56af97044dc613952efe8e3d761d5b5c51da
yeon-06 <rnjstldus2@hs.ac.kr>
2022-03-23 16:09:42 +0900

    refactor: 생성자 추가

--- src/main/java/chess/domain/position/Position.java
+++ src/main/java/chess/domain/position/Position.java
@@ -3,26 +3,32 @@ package chess.domain.position;
 import java.util.Objects;
 
 public class Position {
-	private final Row row;
-	private final Column column;
+    private final Row row;
+    private final Column column;
 
-	public Position(Row row, Column column) {
-		this.row = row;
-		this.column = column;
-	}
+    public Position(Row row, Column column) {
+        this.row = row;
+        this.column = column;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Position position = (Position)o;
-		return row == position.row && column == position.column;
-	}
+    public Position(String value) {
+        this(Row.of(value.substring(0, 1)), Column.of(value.substring(1, 2)));
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(row, column);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return row == position.row && column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
 }
