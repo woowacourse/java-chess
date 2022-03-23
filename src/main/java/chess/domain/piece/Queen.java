4e3f84ea9ab499e5ef24fa7313f5cff9d609a571
skullKim <kyk990328@gmail.com>
2022-03-23 18:39:13 +0900

    feat(Queen.java): 퀸의 방향 검증 로직 구현
    사용자가 이동 방향을 입력했을 때 해당 방향으로 이동할 수 있는지를
    검증하는 로직 구현

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -1,8 +1,31 @@
 package chess.domain.piece;
 
+import static chess.domain.piece.Direction.*;
+
+import java.util.List;
+
+import chess.domain.board.Position;
+
 public class Queen extends Piece {
+
+	private static final List<Direction> POSSIBLE_DIRECTIONS = List.of(N, S, W, E, NE, SE, SW, NW);
 
 	public Queen(Color color) {
 		super("queen", color);
+	}
+
+	@Override
+	public Direction findValidDirection(Position current, Position target) {
+		int rowDifference = target.calculateRowDifference(current);
+		int columnDifference = target.calculateColumnDifference(current);
+		Direction direction = Direction.calculate(rowDifference, columnDifference);
+		validateDirection(direction);
+		return direction;
+	}
+
+	private void validateDirection(Direction direction) {
+		if (!POSSIBLE_DIRECTIONS.contains(direction)) {
+			throw new IllegalArgumentException("진행할 수 없는 방향입니다.");
+		}
 	}
 }
---
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -0,0 +1,41 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import chess.domain.board.Column;
+import chess.domain.board.Position;
+import chess.domain.board.Row;
+
+class QueenTest {
+
+	@DisplayName("입력된 방향에 대해")
+	@Nested
+	class DirectionTest {
+
+		@DisplayName("유효하지 않으면 예외를 반환한다.")
+		@Test
+		void invalid_Direction() {
+			Position current = new Position(Row.FIRST, Column.a);
+			Position invalidTarget = new Position(Row.THIRD, Column.b);
+			Queen queen = new Queen(Color.BLACK);
+
+			assertThatThrownBy(() -> queen.findValidDirection(current, invalidTarget));
+		}
+
+		@DisplayName("유효하면 방향 객체를 반환한다.")
+		@Test
+		void valid_Direction() {
+			Position current = new Position(Row.FIRST, Column.a);
+			Position target = new Position(Row.THIRD, Column.c);
+			Queen queen = new Queen(Color.BLACK);
+
+			Direction actual = queen.findValidDirection(current, target);
+
+			assertThat(actual).isEqualTo(Direction.NE);
+		}
+	}
+}
