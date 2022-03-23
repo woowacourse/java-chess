3b1d2a6d1c151900d279bf6e5afbc41b54463cf4
Chris <bjuuuu98@gmail.com>
2022-03-23 15:43:28 +0900

    feat(TeamColor): Rank 값을 받아 팀 컬러를 반환하는 기능 구현

--- src/main/java/chess/domain/piece/vo/TeamColor.java
+++ src/main/java/chess/domain/piece/vo/TeamColor.java
@@ -1,9 +1,26 @@
 package chess.domain.piece.vo;
 
+import chess.domain.board.Rank;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 public enum TeamColor {
-    BLACK,
-    WHITE,
+    WHITE(rank -> rank == Rank.ONE || rank == Rank.TWO),
+    BLACK(rank -> rank == Rank.SEVEN || rank == Rank.EIGHT),
     ;
+
+    private final Predicate<Rank> rankPredicate;
+
+    TeamColor(final Predicate<Rank> rankPredicate) {
+        this.rankPredicate = rankPredicate;
+    }
+
+    public static TeamColor findByRank(final Rank rank) {
+        return Arrays.stream(values())
+                .filter(teamColor -> teamColor.rankPredicate.test(rank))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("팀 컬러를 식별할 수 있는 Rank는 1,2,7,8 입니다."));
+    }
 
     public String convertSymbolByTeamColor(final String symbol) {
         if (this == BLACK) {
--- src/test/java/chess/domain/piece/vo/TeamColorTest.java
+++ src/test/java/chess/domain/piece/vo/TeamColorTest.java
@@ -1,8 +1,11 @@
 package chess.domain.piece.vo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import chess.domain.board.Rank;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -18,6 +21,27 @@ class TeamColorTest {
         final String actual = teamColor.convertSymbolByTeamColor(symbol);
         //then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Rank 값을 받아 팀 컬러를 반환한다.")
+    @CsvSource(value = {"ONE, WHITE", "TWO, WHITE", "SEVEN, BLACK", "EIGHT, BLACK"})
+    void findByRank(final Rank rank, final TeamColor expected) {
+        //when
+        TeamColor actual = TeamColor.findByRank(rank);
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("1,2,7,8 이 아닌 Rank 값으로 팀 컬러를 찾으려 하면 예외를 발생시킨다.")
+    void findByRankException() {
+        //given
+        Rank rank = Rank.FIVE;
+        //when, then
+        assertThatThrownBy(() -> TeamColor.findByRank(rank))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("팀 컬러를 식별할 수 있는 Rank는 1,2,7,8 입니다.");
     }
 
 }
