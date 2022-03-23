2053c53ae2ad747ebbf09b548c1cb693d2fea794
이한울 <dkfghg3@naver.com>
2022-03-23 19:04:03 +0900

    feat: 퀸 기능 구현

---
+++ src/main/java/chess/domain/Queen.java
@@ -0,0 +1,20 @@
+package chess.domain;
+
+public class Queen {
+
+    private Position position;
+
+    public Queen(Position position) {
+        this.position = position;
+    }
+
+    public Position move(final Position currentPosition, final Position destinationPosition) {
+        final boolean isMoveLinear = currentPosition.isMoveLinear(destinationPosition);
+        final boolean isMoveDiagonal = currentPosition.isMoveDiagonal(destinationPosition);
+
+        if (!isMoveLinear && !isMoveDiagonal) {
+            throw new IllegalArgumentException("퀸은 상하좌우, 대각선 중 한 방향으로 이동해야 합니다.");
+        }
+        return position = destinationPosition;
+    }
+}
