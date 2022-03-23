7b9197d854b4c1ab64143d71c41bfd630ac13203
pup-paw <wldusdl0310@gmail.com>
2022-03-23 14:35:14 +0900

    feat: 체스판 초기화 및 랭크별로 구분해주는 기능 추가

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,7 +1,12 @@
 package chess;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.google.common.collect.Lists;
 
 import chess.piece.Piece;
 
@@ -9,7 +14,7 @@ public class Board {
     private final Map<Square, Piece> board;
 
     public Board() {
-        Map<Square, Piece> board = new HashMap<>();
+        Map<Square, Piece> board = new LinkedHashMap<>();
         for (Rank rank : Rank.values()) {
             createRow(board, rank);
         }
@@ -18,7 +23,14 @@ public class Board {
 
     private void createRow(Map<Square, Piece> board, Rank rank) {
         for (File file : File.values()) {
-            board.put(new Square(file, rank), Piece.from(file,rank));
+            board.put(new Square(file, rank), Piece.from(file, rank));
         }
+    }
+
+    public List<List<Piece>> splitByRank() {
+        List<Piece> pieces = new ArrayList<>(board.values());
+        List<List<Piece>> splitPieces = Lists.partition(pieces, Rank.values().length);
+        Collections.reverse(splitPieces);
+        return splitPieces;
     }
 }
