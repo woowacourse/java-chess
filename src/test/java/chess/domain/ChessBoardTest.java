444367ea15c5a9336a8d625a253812bde03efbfe
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-23 14:06:53 +0900

    feat: ChessBoard 생성자 null 예외처리 및 테스트

--- src/test/java/chess/domain/ChessBoardTest.java
+++ src/test/java/chess/domain/ChessBoardTest.java
@@ -1,11 +1,23 @@
 package chess.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ChessBoardTest {
+
+    @Test
+    @DisplayName("생성 시 null 예외발생")
+    void constructorNullException() {
+        assertThatThrownBy(() -> new ChessBoard(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("pieces는 null이 들어올 수 없습니다.");
+    }
 
     @Test
     @DisplayName("체스판 생성")
