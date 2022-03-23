7a98304956023dad2af9e00303c8d6a85abbfb68
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-23 12:15:21 +0900

    refactor: Position 가로세로 네이밍 수정

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -4,32 +4,32 @@ import java.util.Objects;
 
 public class Position {
 
-    private static final char MIN_ROW = 'a';
-    private static final char MAX_ROW = 'h';
-    private static final char MIN_COLUMN = '1';
-    private static final char MAX_COLUMN = '8';
+    private static final char MIN_COLUMN = 'a';
+    private static final char MAX_COLUMN = 'h';
+    private static final char MIN_ROW = '1';
+    private static final char MAX_ROW = '8';
 
-    private final char row;
     private final char column;
+    private final char row;
 
-    public Position(char row, char column) {
-        validateRowInRange(row);
+    public Position(char column, char row) {
         validateColumnInRange(column);
-        this.row = row;
+        validateRowInRange(row);
         this.column = column;
-    }
-
-    private void validateRowInRange(char row) {
-        if (row < MIN_ROW || row > MAX_ROW) {
-            throw new IllegalArgumentException(
-                    String.format("가로 위치는 %s~%s 범위에 포함되어야 합니다.", MIN_ROW, MAX_ROW));
-        }
+        this.row = row;
     }
 
     private void validateColumnInRange(char column) {
         if (column < MIN_COLUMN || column > MAX_COLUMN) {
             throw new IllegalArgumentException(
-                    String.format("세로 위치는 %s~%s 범위에 포함되어야 합니다.", MIN_COLUMN, MAX_COLUMN));
+                    String.format("열 위치는 %s~%s 범위에 포함되어야 합니다.", MIN_COLUMN, MAX_COLUMN));
+        }
+    }
+
+    private void validateRowInRange(char row) {
+        if (row < MIN_ROW || row > MAX_ROW) {
+            throw new IllegalArgumentException(
+                    String.format("행 위치는 %s~%s 범위에 포함되어야 합니다.", MIN_ROW, MAX_ROW));
         }
     }
 
