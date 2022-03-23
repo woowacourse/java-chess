39e2a10821518bd39166d7c9810f3272f25e6d7d
summerlunaa <summerlunaa9@gmail.com>
2022-03-23 15:01:48 +0900

    feat: 랭크 열거형 추가

---
+++ src/main/java/chess/domain/position/Rank.java
@@ -0,0 +1,19 @@
+package chess.domain.position;
+
+public enum Rank {
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8")
+    ;
+
+    private final String rank;
+
+    Rank(String rank) {
+        this.rank = rank;
+    }
+}
