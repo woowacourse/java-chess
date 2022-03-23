9ac7dfb96c90d5fef8c501580ad1a043b7b40291
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-23 14:30:55 +0900

    feat: 위치에 존재하는 기물 반환 기능 추가 및 테스트

--- src/test/java/chess/domain/ChessBoardTest.java
+++ src/test/java/chess/domain/ChessBoardTest.java
@@ -6,7 +6,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import chess.domain.piece.Knight;
 import chess.domain.piece.Piece;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -38,5 +40,26 @@ class ChessBoardTest {
         ChessBoard chessBoard = new ChessBoard(Map.of(position, piece));
 
         assertThat(chessBoard.isPositionEmpty(new Position(col, row))).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("입력 위치에 기물이 없을 때 예외발생")
+    void exceptionPieceByPosition() {
+        ChessBoard chessBoard = new ChessBoard(new HashMap<>());
+        Position position = new Position('a', '1');
+
+        assertThatThrownBy(() -> chessBoard.pieceByPosition(position))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("해당 위치에 존재하는 기물이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("입력 위치에 기물 반환")
+    void pieceByPosition() {
+        Position position = new Position('a', '1');
+        Piece piece = new Knight(WHITE, position);
+        ChessBoard chessBoard = new ChessBoard(Map.of(position, piece));
+
+        assertThat(chessBoard.pieceByPosition(position)).isEqualTo(piece);
     }
 }
