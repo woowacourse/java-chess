5dac669b49b69f18fbe376cd01232ecadf41407c
Donggyu <a29661498@gmail.com>
2022-03-23 12:32:45 +0900

    refactor(Position): 불필요한 initFiles 와 isInitPosition 메소드 제거

--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -1,18 +1,11 @@
 package chess;
 
-import java.util.List;
-
 public class Position {
     private final Rank rank;
     private final File file;
-    private static final List<File> initFiles = List.of(File.ONE, File.TWO, File.SEVEN, File.EIGHT);
 
     public Position(Rank rank, File file) {
         this.rank = rank;
         this.file = file;
-    }
-
-    public boolean isInitPosition() {
-        return initFiles.contains(file);
     }
 }
