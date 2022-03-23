c0cf1d6308bef9084bd392f477bb0dd98d376582
Chris <bjuuuu98@gmail.com>
2022-03-23 17:06:03 +0900

    refactor(Position): Position 생성 로직 수정

--- src/main/java/chess/domain/board/Position.java
+++ src/main/java/chess/domain/board/Position.java
@@ -1,16 +1,29 @@
 package chess.domain.board;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Position {
-
-    private static final Map<PositionKey, Position> cache = new HashMap<>();
 
     private static final int RANK_INDEX = 0;
     private static final int FILE_INDEX = 1;
     private static final String RANK_FILE_DELIMITER = "";
+
+    private static final List<Position> allPositions;
+
+    static {
+        allPositions = Arrays.stream(File.values())
+                .map(Position::generatePositionOf)
+                .flatMap(List::stream)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static List<Position> generatePositionOf(final File file) {
+        return Arrays.stream(Rank.values())
+                .map(rank -> new Position(file, rank))
+                .collect(Collectors.toList());
+    }
 
     private final File file;
     private final Rank rank;
@@ -20,13 +33,12 @@ public class Position {
         this.rank = rank;
     }
 
-    private Position(final String file, final String rank) {
-        this(File.from(file), Rank.from(rank));
-    }
-
     public static Position of(final File file, final Rank rank) {
-        final PositionKey positionKey = new PositionKey(file, rank);
-        return cache.computeIfAbsent(positionKey, ignored -> new Position(file, rank));
+        return allPositions.stream()
+                .filter(position -> position.file == file)
+                .filter(position -> position.rank == rank)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("위치 정보가 유효하지 않습니다."));
     }
 
     public static Position from(final String positionValue) {
@@ -42,37 +54,6 @@ public class Position {
     private static void validatePositionValue(final String positionValue) {
         if (positionValue.length() != 2) {
             throw new IllegalArgumentException("위치 정보가 유효하지 않습니다.");
-        }
-    }
-
-    static class PositionKey {
-        private final File file;
-        private final Rank rank;
-
-        private PositionKey(final File file, final Rank rank) {
-            this.file = file;
-            this.rank = rank;
-        }
-
-        private PositionKey(final String file, final String rank) {
-            this(File.from(file), Rank.from(rank));
-        }
-
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            final PositionKey that = (PositionKey) o;
-            return file == that.file && rank == that.rank;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(file, rank);
         }
     }
 }
