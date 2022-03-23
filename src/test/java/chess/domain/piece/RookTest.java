203bace88eef92a22d7e2049fdfe6a37a76d2df2
skullKim <kyk990328@gmail.com>
2022-03-23 18:34:08 +0900

    feat(Rook.java): 룩의 방향 검증 구현
    사용자가 이동 방향을 입력했을 때 해당 방향으로 이동할 수 있는지를
    검증하는 로직 구현

---
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -0,0 +1,41 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import chess.domain.board.Column;
+import chess.domain.board.Position;
+import chess.domain.board.Row;
+
+class RookTest {
+
+	@DisplayName("입력된 방향에 대해")
+	@Nested
+	class DirectionTest {
+
+		@DisplayName("유효하지 않으면 예외를 반환한다.")
+		@Test
+		void invalid_Direction() {
+			Position current = new Position(Row.FIRST, Column.a);
+			Position invalidTarget = new Position(Row.THIRD, Column.b);
+			Rook rook = new Rook(Color.BLACK);
+
+			assertThatThrownBy(() -> rook.findValidDirection(current, invalidTarget));
+		}
+
+		@DisplayName("유효하면 방향 객체를 반환한다.")
+		@Test
+		void valid_Direction() {
+			Position current = new Position(Row.FIRST, Column.a);
+			Position target = new Position(Row.THIRD, Column.a);
+			Rook rook = new Rook(Color.BLACK);
+
+			Direction actual = rook.findValidDirection(current, target);
+
+			assertThat(actual).isEqualTo(Direction.N);
+		}
+	}
+}
