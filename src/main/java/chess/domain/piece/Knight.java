52e7ba4737a6ba99cb00985fae137c9f811e3bb5
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-23 12:53:05 +0900

    feat: 나이트 위치 이동 기능 구현 및 테스트

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -11,13 +11,19 @@ public final class Knight extends Piece {
         super(color, KNIGHT_NAME, position);
     }
 
-    public void move(Position position) {
+    public Knight(Knight knight, Position position) {
+        super(knight, position);
+    }
+
+    public Knight move(Position position) {
         if (!isPossibleToMove(position)) {
             throw new IllegalStateException("나이트는 L자 형태로만 움직일 수 있습니다.");
         }
+        return new Knight(this, position);
     }
 
     private boolean isPossibleToMove(Position position) {
-        return position.equalsColumnOrRow(position) && position.calculateDistance(position) == KNIGHT_MOVABLE_DISTANCE;
+        return !position.equalsColumnOrRow(getPosition())
+                && position.calculateDistance(getPosition()) == KNIGHT_MOVABLE_DISTANCE;
     }
 }
