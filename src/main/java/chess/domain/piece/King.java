44f1d2ad414d24aa5f30c4c12af7728bb50b6ffc
summerlunaa <summerlunaa9@gmail.com>
2022-03-23 14:21:36 +0900

    feat: 진영에 맞는 표기 정보를 반환하는 기능 구현

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -4,9 +4,18 @@ import chess.domain.Color;
 
 public class King {
 
+    private static final String NOTATION = "K";
+
     private final Color color;
 
     public King(Color color) {
         this.color = color;
+    }
+
+    public String getNotation() {
+        if (color == Color.WHITE) {
+            return NOTATION.toLowerCase();
+        }
+        return NOTATION;
     }
 }
--- src/test/java/chess/domain/piece/KingTest.java
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -1,10 +1,13 @@
 package chess.domain.piece;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import chess.domain.Color;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class KingTest {
 
@@ -12,5 +15,14 @@ public class KingTest {
     @Test
     void King을_생성한다() {
         assertDoesNotThrow(() -> new King(Color.BLACK));
+    }
+
+    @DisplayName("진영에 맞는 표기 정보를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"BLACK,K", "WHITE,k"})
+    void 진영에_맞는_표기_정보를_반환한다(Color color, String notation){
+        King king = new King(color);
+
+        assertThat(king.getNotation()).isEqualTo(notation);
     }
 }
