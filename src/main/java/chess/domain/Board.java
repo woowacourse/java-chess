5b0b91a5cb36907a87468171e54441d343591fa5
beomWhale <cndqjacndqja@gmail.com>
2022-03-23 22:13:42 +0900

    refactor: Board의 Rank 리스트 생성 기능 Rank로 이동

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -5,64 +5,26 @@ import java.util.List;
 
 public class Board {
 
-    private List<Rank> board;
+    private final List<Rank> board;
 
     public Board() {
         board = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            board.add(createRank(i));
+        board.add(Rank.createFirstRank(Team.BLACK));
+        board.add(Rank.createPawn(Team.BLACK));
+        for (int i = 0; i < 4; i++) {
+            board.add(Rank.createBlank());
         }
+        board.add(Rank.createPawn(Team.WHITE));
+        board.add(Rank.createFirstRank(Team.WHITE));
     }
 
-    private Rank createRank(int i) {
-        if (i == 0) {
-            return createFirstRank(Team.BLACK);
-        }
-        if (i == 1) {
-            return createPawn(Team.BLACK);
-        }
-        if (i == 6) {
-            return createPawn(Team.WHITE);
-        }
-        if (i == 7) {
-            return createFirstRank(Team.WHITE);
-        }
-        return createBlank();
+    public Piece getPiece(String input) {
+        Position position = Position.from(input);
+        return getRank(position.getRow()).getPiece(position.getColumn());
     }
 
-    private Rank createBlank() {
-        List<Piece> pieces = new ArrayList<>();
-        for (int j = 0; j < 8; j++) {
-            pieces.add(new Blank(Team.NONE));
-        }
-        return new Rank(pieces);
-    }
-
-    private Rank createPawn(Team team) {
-        List<Piece> pieces = new ArrayList<>();
-        for (int j = 0; j < 8; j++) {
-            pieces.add(new Pawn(team));
-        }
-        return new Rank(pieces);
-    }
-
-    private Rank createFirstRank(Team team) {
-        List<Piece> pieces = new ArrayList<>();
-        pieces.add(new Rook(team));
-        pieces.add(new Knight(team));
-        pieces.add(new Bishop(team));
-        pieces.add(new Queen(team));
-        pieces.add(new King(team));
-        pieces.add(new Bishop(team));
-        pieces.add(new Knight(team));
-        pieces.add(new Rook(team));
-        return new Rank(pieces);
-    }
-
-    public Piece getPiece(String position) {
-        int x = position.charAt(0) - 'a';
-        int y = Character.getNumericValue(position.charAt(1)) - 1;
-        return board.get(y).getPieces().get(x);
+    private Rank getRank(int row) {
+        return board.get(row);
     }
 
     public List<Rank> getBoard() {
--- src/main/java/chess/domain/Rank.java
+++ src/main/java/chess/domain/Rank.java
@@ -1,15 +1,49 @@
 package chess.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rank {
     List<Piece> pieces;
 
-    public Rank(List<Piece> pieces) {
+    private Rank(List<Piece> pieces) {
         this.pieces = pieces;
     }
 
     public List<Piece> getPieces() {
         return pieces;
+    }
+
+    public static Rank createBlank() {
+        List<Piece> pieces = new ArrayList<>();
+        for (int j = 0; j < 8; j++) {
+            pieces.add(new Blank(Team.NONE));
+        }
+        return new Rank(pieces);
+    }
+
+    public static Rank createPawn(Team team) {
+        List<Piece> pieces = new ArrayList<>();
+        for (int j = 0; j < 8; j++) {
+            pieces.add(new Pawn(team));
+        }
+        return new Rank(pieces);
+    }
+
+    public static Rank createFirstRank(Team team) {
+        List<Piece> pieces = new ArrayList<>();
+        pieces.add(new Rook(team));
+        pieces.add(new Knight(team));
+        pieces.add(new Bishop(team));
+        pieces.add(new Queen(team));
+        pieces.add(new King(team));
+        pieces.add(new Bishop(team));
+        pieces.add(new Knight(team));
+        pieces.add(new Rook(team));
+        return new Rank(pieces);
+    }
+
+    public Piece getPiece(int position) {
+        return pieces.get(position);
     }
 }
