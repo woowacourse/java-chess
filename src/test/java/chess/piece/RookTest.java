4a094b20b5b341dccafcdeb17d31c81f64857fd7
giron <ggyucheol0@gmail.com>
2022-03-23 14:41:48 +0900

    test: 룩의 진행방향이 다르면 예외가 발생하는 테스트 추가

--- src/test/java/chess/piece/RookTest.java
+++ src/test/java/chess/piece/RookTest.java
@@ -7,17 +7,29 @@ import chess.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RookTest {
 
     @Test
     @DisplayName("룩의 진행 방향이 맞는지 확인하는 테스트")
-    void move() {
+    void correctMove() {
         Rook rook = new Rook(new Position(File.A, Rank.ONE), Team.WHITE);
 
         assertDoesNotThrow(
                 () -> rook.move(new Position(File.F, Rank.ONE))
         );
     }
+
+    @Test
+    @DisplayName("룩의 진행 방향이 다르면 예외가 발생하는 테스트")
+    void unCorrectMove() {
+        Rook rook = new Rook(new Position(File.A, Rank.ONE), Team.WHITE);
+
+        assertThatThrownBy(
+                () -> rook.move(new Position(File.F, Rank.TWO))
+        ).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
 }
