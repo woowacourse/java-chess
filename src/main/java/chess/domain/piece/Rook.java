bb5a70cbc9758a2d43f0d3b7ae56617986108616
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-23 15:41:58 +0900

    feat: 룩 위치 이동불가 예외처리 및 테스트

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -9,4 +9,14 @@ public final class Rook extends Piece {
     public Rook(Color color, Position position) {
         super(color, ROOK_NAME, position);
     }
+
+    public void move(Position position) {
+        if (!isPossibleToMove(position)) {
+            throw new IllegalStateException("룩은 상하좌우 방향으로만 움직일 수 있습니다.");
+        }
+    }
+
+    private boolean isPossibleToMove(Position position) {
+        return position.equalsColumnOrRow(getPosition());
+    }
 }
