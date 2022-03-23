e06a9eda20c835bb1ba3533f040c6476dcfcbd2e
meatsby <meatsby@gmail.com>
2022-03-23 21:03:27 +0900

    feat: 퀸 기물 이동 기능 구현 및 테스트 작성

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -1,5 +1,11 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Queen extends Piece {
 
     private static final String WHITE_SIGNATURE = "q";
@@ -17,7 +23,28 @@ public class Queen extends Piece {
         return new Queen(position, BLACK_SIGNATURE);
     }
 
+    @Override
     public boolean isMovable(Piece piece) {
-        return false;
+        return isInRange(piece.getPosition()) && isValidPosition(piece);
+    }
+
+    private boolean isInRange(Position targetPosition) {
+        List<Position> inRangePosition = new ArrayList<>();
+
+        for (Direction direction : Direction.getEightStraightDirections()) {
+            List<Position> directionPositions = IntStream.rangeClosed(1, Board.RANK_CAPACITY)
+                    .mapToObj(product -> new Position(
+                            position.getX() + direction.getXDegree() * product,
+                            position.getY() + direction.getYDegree() * product))
+                    .filter(position -> Position.isValidPosition(position.getX(), position.getY()))
+                    .collect(Collectors.toList());
+            inRangePosition.addAll(directionPositions);
+        }
+
+        return inRangePosition.contains(targetPosition);
+    }
+
+    private boolean isValidPosition(Piece piece) {
+        return piece.isBlank() || piece.isEnemy(getSignature());
     }
 }
--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -22,4 +22,35 @@ public class QueenTest {
 
         assertThat(queen.getSignature()).isEqualTo("Q");
     }
+
+    @DisplayName("타겟 위치가 빈칸일 경우 이동에 성공한다.")
+    @Test
+    void move_Blank() {
+        Queen queen = Queen.createBlack(new Position("c8"));
+
+        assertThat(queen.isMovable(new Blank(new Position("d7"))))
+                .isTrue();
+        assertThat(queen.isMovable(new Blank(new Position("c7"))))
+                .isTrue();
+        assertThat(queen.isMovable(new Blank(new Position("d8"))))
+                .isTrue();
+    }
+
+    @DisplayName("타겟 위치가 적 기물일 경우 공격에 성공한다.")
+    @Test
+    void move_Enemy() {
+        Queen queen = Queen.createBlack(new Position("c8"));
+
+        assertThat(queen.isMovable(Queen.createWhite(new Position("d7"))))
+                .isTrue();
+    }
+
+    @DisplayName("타겟 위치가 같은 팀 기물일 경우 이동에 실패한다.")
+    @Test
+    void move_Ally() {
+        Queen queen = Queen.createBlack(new Position("c8"));
+
+        assertThat(queen.isMovable(Queen.createBlack(new Position("d7"))))
+                .isFalse();
+    }
 }
