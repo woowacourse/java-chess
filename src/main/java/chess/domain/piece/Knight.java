832c78400a4bde037ca4bc1dd896c9b4e325f29f
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-23 12:41:18 +0900

    feat: 나이트 위치 이동 불가 예외처리 및 테스트

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -5,8 +5,19 @@ import chess.domain.Position;
 public final class Knight extends Piece {
 
     private static final String KNIGHT_NAME = "K";
+    private static final int KNIGHT_MOVABLE_DISTANCE = 3;
 
     public Knight(Color color, Position position) {
         super(color, KNIGHT_NAME, position);
+    }
+
+    public void move(Position position) {
+        if (!isPossibleToMove(position)) {
+            throw new IllegalStateException("나이트는 L자 형태로만 움직일 수 있습니다.");
+        }
+    }
+
+    private boolean isPossibleToMove(Position position) {
+        return position.equalsColumnOrRow(position) && position.calculateDistance(position) == KNIGHT_MOVABLE_DISTANCE;
     }
 }
--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -1,12 +1,13 @@
 package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import chess.domain.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class KnightTest {
 
@@ -16,5 +17,17 @@ class KnightTest {
     void createPawn(Color color) {
         Piece knight = new Knight(color, new Position('a', '1'));
         assertThat(knight).isInstanceOf(Knight.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"a,2", "a,3", "a,4", "b,1", "b,2", "c,3"})
+    @DisplayName("나이트 위치 이동 불가 예외발생")
+    void moveException(char row, char col) {
+        Knight knight = new Knight(Color.WHITE, new Position('a', '1'));
+        Position movePosition = new Position(row, col);
+
+        assertThatThrownBy(() -> knight.move(movePosition))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("나이트는 L자 형태로만 움직일 수 있습니다.");
     }
 }
