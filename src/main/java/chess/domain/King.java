24e1c2568309e444a17d777f2d423b1badc3f201
이한울 <dkfghg3@naver.com>
2022-03-23 19:24:34 +0900

    feat: 킹 기능 구현

---
+++ src/main/java/chess/domain/King.java
@@ -0,0 +1,33 @@
+package chess.domain;
+
+public class King {
+
+    private static final int KING_LINEAR_MOVE_DISTANCE = 1;
+    private static final int KING_DIAGONAL_MOVE_DISTANCE = 2;
+
+    private Position position;
+
+    public King(Position position) {
+        this.position = position;
+    }
+
+    public Position move(Position currentPosition, Position destinationPosition) {
+        boolean isMoveLinear = currentPosition.isMoveLinear(destinationPosition);
+        boolean isMoveDiagonal = currentPosition.isMoveDiagonal(destinationPosition);
+
+        if (!isMoveLinear && !isMoveDiagonal) {
+            throw new IllegalArgumentException("킹은 상하좌우, 대각선 중 한 방향으로 이동해야 합니다.");
+        }
+
+        validateMoveDistance(currentPosition.calculateDistance(destinationPosition), isMoveLinear, isMoveDiagonal);
+
+        return position = destinationPosition;
+    }
+
+    private void validateMoveDistance(final int distance, final boolean isMoveLinear, final boolean isMoveDiagonal) {
+        if (isMoveLinear && distance != KING_LINEAR_MOVE_DISTANCE
+                || isMoveDiagonal && distance != KING_DIAGONAL_MOVE_DISTANCE) {
+            throw new IllegalArgumentException("킹은 1칸만 이동할 수 있습니다.");
+        }
+    }
+}
