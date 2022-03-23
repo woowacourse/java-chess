9baf9e845daf19ed7f18e027d166ff53f0b430cb
Seongwon97 <osw970325@naver.com>
2022-03-23 13:52:51 +0900

    feat(Piece): 비어있는 경우에 해당하는 말 추가

--- src/main/java/chess/piece/Color.java
+++ src/main/java/chess/piece/Color.java
@@ -1,5 +1,5 @@
 package chess.piece;
 
 public enum Color {
-    WHITE, BLACK
+    WHITE, BLACK, NONE
 }
---
+++ src/main/java/chess/piece/Empty.java
@@ -0,0 +1,8 @@
+package chess.piece;
+
+public class Empty extends Piece {
+
+    public Empty(Color color) {
+        super(color);
+    }
+}
