3971f1de4fc8e32f502b38ec176d52ab1c4c93b4
swcho <progress0407@gmail.com>
2022-03-23 22:07:20 +0900

    test(CommandTest): 입력 요청에 알맞은 Command를 반환하는지 검증하는 테스트 추가

--- src/test/java/chess/CommandTest.java
+++ src/test/java/chess/CommandTest.java
@@ -1,27 +1,41 @@
 package chess;
 
-import java.util.regex.Pattern;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CommandTest {
 
-    private static final String regex = "(start)|(end)|(move [a-h][1-8] [a-h][1-8])";
-    public static final Pattern PATTERN = Pattern.compile(regex);
-
     @ParameterizedTest
-    @ValueSource(strings = {"start", "end", "move a1 a2", "move a7 b6"})
-    void regexp_pass(String input) {
-        boolean matches = PATTERN.matcher(input).matches();
-        Assertions.assertThat(matches).isTrue();
+    @CsvSource(value = {"start:START", "end:END"}, delimiter = ':')
+    void startEnd_pass(String input, Command expected) {
+        Command actual = Command.startEnd(input);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"MOVE a7 b6", "startendstart", "sttart"})
-    void regexp_exception(String input) {
-        boolean matches = PATTERN.matcher(input).matches();
-        Assertions.assertThat(matches).isFalse();
+    @ValueSource(strings = {"starttt", "", " "})
+    void startEnd_exception(String input) {
+        assertThatThrownBy(() -> Command.startEnd(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Command.NOT_FOUND_COMMAND_EXCEPTION);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"move a1 a2:MOVE", "end:END"}, delimiter = ':')
+    void endMove_pass(String input, Command expected) {
+        Command actual = Command.endMove(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"move a1 a9", "move a9 b1", "move B1 b2", "move a1 a2 a3"})
+    void endMove_exception(String input) {
+        assertThatThrownBy(() -> Command.endMove(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Command.NOT_FOUND_COMMAND_EXCEPTION);
+    }
 }
