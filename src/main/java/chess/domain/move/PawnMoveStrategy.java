1a8886c832f73cc1992adbcc8f8372d83deea506
jayjaehunchoi <wogns0108@naver.com>
2022-03-24 00:08:51 +0900

    refactor : PawnMoveStrategy 이동 전략 리팩터링

--- src/main/java/chess/domain/move/PawnMoveStrategy.java
+++ src/main/java/chess/domain/move/PawnMoveStrategy.java
@@ -8,51 +8,88 @@ import java.util.List;
 
 public class PawnMoveStrategy {
 
-    private final static List<MovePattern> BLACK_PAWN_MOVE_PATTERN = List.of(
+    private final static List<MovePattern> BLACK_MOVE_PATTERN = List.of(
             MovePattern.SOUTH,
             MovePattern.SOUTHEAST,
             MovePattern.SOUTHWEST,
             MovePattern.PAWN_START_MOVE_BLACK
     );
-    private final static List<MovePattern> WHITE_PAWN_MOVE_PATTERN = List.of(
+    private final static List<MovePattern> WHITE_MOVE_PATTERN = List.of(
             MovePattern.NORTH,
             MovePattern.NORTHEAST,
             MovePattern.NORTHWEST,
             MovePattern.PAWN_START_MOVE_WHITE
     );
 
-
     public boolean isMovable(final Board board, final Position source, final Position target) {
-        final Piece sourcePiece = board.getPiece(source);
-        final Piece targetPiece = board.getPiece(target);
-
         final Distance distance = new Distance(source.subtractRow(target), source.subtractColumn(target));
         final int horizon = distance.getHorizon();
         final int vertical = distance.getVertical();
 
-        Color color = sourcePiece.getColor();
-        MovePattern movePattern = MovePattern.of(horizon, vertical);
-        if (color == Color.BLACK) {
-            if (!BLACK_PAWN_MOVE_PATTERN.contains(movePattern)) {
-                return false;
-            }
-            // 2칸이동
-            if (movePattern == MovePattern.PAWN_START_MOVE_BLACK) {
-                if (!source.isPawnStartPosition(color)) {
-                    return false;
-                }
-                return board.getPiece(source.move(0, -1)).isBlank() && targetPiece.isBlank();
-            }
-            // 1칸 전진
-            if (movePattern == MovePattern.SOUTH) {
-                return targetPiece.isBlank();
-            }
-            // 대각이동
-            if (movePattern == MovePattern.SOUTHEAST || movePattern == MovePattern.SOUTHWEST) {
-                return !targetPiece.isBlank() && targetPiece.getColor() == Color.WHITE;
-            }
-        }
+        final Color color = board.getPiece(source).getColor();
+        final MovePattern movePattern = MovePattern.of(horizon, vertical);
+        final Piece targetPiece = board.getPiece(target);
 
-        return true;
+        if (color == Color.BLACK) {
+            return isBlackMovePattern(movePattern, board, source, targetPiece, color);
+        }
+        return isWhiteMovePattern(movePattern, board, source, targetPiece, color);
+    }
+
+    private boolean isBlackMovePattern(final MovePattern movePattern, final Board board, final Position source, final Piece targetPiece, final Color color) {
+        if (!BLACK_MOVE_PATTERN.contains(movePattern)) {
+            return false;
+        }
+        if (movePattern == MovePattern.PAWN_START_MOVE_BLACK) {
+            return isStartMove(board, source, targetPiece, color);
+        }
+        if (movePattern == MovePattern.SOUTH) {
+            return targetPiece.isBlank();
+        }
+        if (movePattern == MovePattern.SOUTHEAST || movePattern == MovePattern.SOUTHWEST) {
+            return isCatchable(targetPiece, color);
+        }
+        return false;
+    }
+
+    private boolean isWhiteMovePattern(final MovePattern movePattern, final Board board, final Position source, final Piece targetPiece, final Color color) {
+        if (!WHITE_MOVE_PATTERN.contains(movePattern)) {
+            return false;
+        }
+        if (movePattern == MovePattern.PAWN_START_MOVE_WHITE) {
+            return isStartMove(board, source, targetPiece, color);
+        }
+        if (movePattern == MovePattern.NORTH) {
+            return targetPiece.isBlank();
+        }
+        if (movePattern == MovePattern.NORTHEAST || movePattern == MovePattern.NORTHWEST) {
+            return isCatchable(targetPiece, color);
+        }
+        return false;
+    }
+
+    private boolean isStartMove(final Board board, final Position source, final Piece targetPiece, final Color color) {
+        if (!source.isPawnStartPosition(color)) {
+            return false;
+        }
+        return board.getPiece(source.move(0, findForward(color))).isBlank() && targetPiece.isBlank();
+    }
+
+    private int findForward(final Color color) {
+        if (color == Color.BLACK) {
+            return -1;
+        }
+        return 1;
+    }
+
+    private boolean isCatchable(final Piece targetPiece, final Color color) {
+        return !targetPiece.isBlank() && targetPiece.getColor() == oppositeColor(color);
+    }
+
+    private Color oppositeColor(final Color color) {
+        if (color == Color.BLACK) {
+            return Color.WHITE;
+        }
+        return Color.BLACK;
     }
 }
