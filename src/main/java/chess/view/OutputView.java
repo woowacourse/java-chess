027c45b33eb6c10d3f957f6ba0d484d2377d1f36
beomWhale <cndqjacndqja@gmail.com>
2022-03-23 21:35:18 +0900

    refactor: printRank 메서드로 분리

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -2,17 +2,22 @@ package chess.view;
 
 import chess.domain.Board;
 import chess.domain.Piece;
+import chess.domain.Rank;
 
 import java.util.List;
 
 public class OutputView {
     public static void printChessBoard(Board board) {
-        List<List<Piece>> boards = board.getBoard();
-        for (List<Piece> rank : boards) {
-            for (Piece piece : rank) {
-                System.out.print(PieceSymbolMapper.getSymbol(piece));
-            }
-            System.out.println();
+        List<Rank> boards = board.getBoard();
+        for (Rank rank : boards) {
+            printRank(rank);
         }
+    }
+
+    private static void printRank(Rank rank) {
+        for (Piece piece : rank.getPieces()) {
+            System.out.print(PieceSymbolMapper.getSymbol(piece));
+        }
+        System.out.println();
     }
 }
