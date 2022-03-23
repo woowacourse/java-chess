d8cb1747849a39d0ae139add4b7347a1a0558704
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-24 00:04:40 +0900

    refactor(controller): move all main logic to controller

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,14 +1,7 @@
 package chess;
 
-import static chess.view.InputView.requestValidMoveInput;
-import static chess.view.InputView.requestValidStatusOrEndInput;
-import static chess.view.OutputView.printBoard;
-import static chess.view.OutputView.printGameOverInstructions;
-import static chess.view.OutputView.printStatus;
-
 import chess.controller.GameController;
 import chess.domain.ChessGame;
-import chess.dto.BoardDto;
 
 public class Application {
 
@@ -16,14 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         ChessGame game = controller.startGame();
-        printBoard(new BoardDto(game));
-        while (!game.isEnd()) {
-            game.moveChessmen(requestValidMoveInput());
-            printBoard(new BoardDto(game));
-        }
-        printGameOverInstructions();
-        while (requestValidStatusOrEndInput()) {
-            printStatus(game.getGameResult());
-        }
+        controller.playGame(game);
+        controller.endGame(game);
     }
 }
--- src/main/java/chess/controller/GameController.java
+++ src/main/java/chess/controller/GameController.java
@@ -1,9 +1,15 @@
 package chess.controller;
 
+import static chess.view.InputView.requestValidMoveInput;
 import static chess.view.InputView.requestValidStartOrEndInput;
+import static chess.view.InputView.requestValidStatusOrEndInput;
+import static chess.view.OutputView.printBoard;
 import static chess.view.OutputView.printGameInstructions;
+import static chess.view.OutputView.printGameOverInstructions;
+import static chess.view.OutputView.printStatus;
 
 import chess.domain.ChessGame;
+import chess.dto.BoardDto;
 
 public class GameController {
 
@@ -15,5 +21,20 @@ public class GameController {
             System.exit(EXIT_STATUS_CODE);
         }
         return new ChessGame();
+    }
+
+    public void playGame(ChessGame game) {
+        printBoard(new BoardDto(game));
+        while (!game.isEnd()) {
+            game.moveChessmen(requestValidMoveInput());
+            printBoard(new BoardDto(game));
+        }
+    }
+
+    public void endGame(ChessGame game) {
+        printGameOverInstructions();
+        while (requestValidStatusOrEndInput()) {
+            printStatus(game.getGameResult());
+        }
     }
 }
