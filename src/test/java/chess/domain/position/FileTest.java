cb8605470f6601a42fcf6e752900bf70a1e6fee7
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-23 17:45:33 +0900

    test : Rank, File 예외 테스트 추가

--- src/test/java/chess/domain/position/FileTest.java
+++ src/test/java/chess/domain/position/FileTest.java
@@ -1,6 +1,7 @@
 package chess.domain.position;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,14 @@ public class FileTest {
     @DisplayName("값을 이용해 File을 찾는다.")
     void findFile() {
         assertThat(File.of("c")).isEqualTo(File.C);
+    }
+
+    @Test
+    @DisplayName("a~h 이외의 값이 들어오는 경우 예외를 발생시킨다.")
+    void exception() {
+        assertThatThrownBy(() -> File.of("i"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 존재하지 않는 File 값 입니다.");
     }
 
 }
--- src/test/java/chess/domain/position/RankTest.java
+++ src/test/java/chess/domain/position/RankTest.java
@@ -1,6 +1,7 @@
 package chess.domain.position;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,5 +25,13 @@ public class RankTest {
     @DisplayName("값을 이용해 Rank를 찾는다.")
     void findRank() {
         assertThat(Rank.of("7")).isEqualTo(Rank.SEVEN);
+    }
+
+    @Test
+    @DisplayName("1~8 이외의 값이 들어오는 경우 예외를 발생시킨다.")
+    void exception() {
+        assertThatThrownBy(() -> Rank.of("9"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 존재하지 않는 Rank 값 입니다.");
     }
 }
