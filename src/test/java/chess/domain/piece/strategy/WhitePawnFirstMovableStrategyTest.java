4784d0a7684f2ccd1e7b5b4cd21c8693a879e4c8
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 03:48:05 +0900

    feat: 기물 존재시 전진 불가능 기능 구현 및 테스트

--- src/test/java/chess/domain/piece/strategy/WhitePawnFirstMovableStrategyTest.java
+++ src/test/java/chess/domain/piece/strategy/WhitePawnFirstMovableStrategyTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.ChessBoard;
 import chess.domain.Position;
+import chess.domain.piece.Color;
 import chess.domain.piece.Pawn;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,11 +26,23 @@ class WhitePawnFirstMovableStrategyTest {
 
     @ParameterizedTest
     @CsvSource(value = {"b,2,true", "b,3,true", "b,1,false", "b,4,false", "a,1,false"})
-    @DisplayName("폰의 빈곳 이동 가능 여부 확인")
+    @DisplayName("폰의 빈곳 전진 가능 여부 확인")
     void isMovableToEmptyPosition(char col, char row, boolean expected) {
         Position target = new Position(col, row);
         ChessBoard chessBoard = new ChessBoard(Map.of(start, new Pawn(WHITE)));
 
         assertThat(whitePawnFirstMovableStrategy.isMovable(start, target, chessBoard)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"b,2,WHITE", "b,3,WHITE", "b,2,BLACK", "b,3,BLACK"})
+    @DisplayName("기물이 가로막을 경우의 전진 불가능")
+    void cannotMoveToPiecePosition(char col, char row, Color color) {
+        Position target = new Position(col, row);
+        ChessBoard chessBoard = new ChessBoard(Map.of(
+                start, new Pawn(WHITE),
+                target, new Pawn(color)));
+
+        assertThat(whitePawnFirstMovableStrategy.isMovable(start, target, chessBoard)).isFalse();
     }
 }
