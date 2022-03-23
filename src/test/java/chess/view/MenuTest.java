c042866d98f2a9bf3a274d3564c1674342e60244
yeon-06 <rnjstldus2@hs.ac.kr>
2022-03-23 17:26:55 +0900

    test: 테스트 케이스 추가

--- src/test/java/chess/view/MenuTest.java
+++ src/test/java/chess/view/MenuTest.java
@@ -1,19 +1,18 @@
 package chess.view;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 class MenuTest {
-    @DisplayName("생성 확인")
-    @Test
-    void create() {
-        // given
-        String input = "start";
 
-        // then
-        assertDoesNotThrow(() -> Menu.of(input));
+    @DisplayName("생성 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"start:START", "Start:START", "START:START", "move:MOVE", "end:END"}, delimiter = ':')
+    void create(String input, Menu expect) {
+        assertThat(Menu.of(input)).isEqualTo(expect);
     }
 }
