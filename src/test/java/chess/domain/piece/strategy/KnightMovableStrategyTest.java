78ee755a6c4aeb43ff2e81605be13363a0752ec5
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 03:08:28 +0900

    feat: 나이트 기물 있는 곳으로의 이동 전략 구현 및 테스트

--- src/test/java/chess/domain/piece/strategy/KnightMovableStrategyTest.java
+++ src/test/java/chess/domain/piece/strategy/KnightMovableStrategyTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.ChessBoard;
 import chess.domain.Position;
+import chess.domain.piece.Color;
 import chess.domain.piece.Knight;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,9 +27,22 @@ class KnightMovableStrategyTest {
     @ParameterizedTest
     @CsvSource(value = {"c,2,true", "b,3,true", "b,2,false", "b,4,false", "c,5,false"})
     @DisplayName("나이트의 빈곳 이동 가능 여부 확인")
-    void exceptionFindRoute(char col, char row, boolean expected) {
+    void isMovableToEmptyPosition(char col, char row, boolean expected) {
         Position target = new Position(col, row);
         ChessBoard chessBoard = new ChessBoard(Map.of(start, new Knight(WHITE)));
+
+        assertThat(knightMovableStrategy.isMovable(start, target, chessBoard)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"c,2,WHITE,false", "b,3,BLACK,true"})
+    @DisplayName("기물이 존재할 경우의 이동 가능 여부 확인")
+    void isMovableToDifferentPiecePosition(char col, char row, Color color, boolean expected) {
+        Position target = new Position(col, row);
+        ChessBoard chessBoard = new ChessBoard(Map.of(
+                start, new Knight(WHITE),
+                target, new Knight(color)
+        ));
 
         assertThat(knightMovableStrategy.isMovable(start, target, chessBoard)).isEqualTo(expected);
     }
