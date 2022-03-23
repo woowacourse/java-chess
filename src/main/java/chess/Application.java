eb30c6fa51a8a4c34a2ef9ef2ee746b9b5176116
Seongwon97 <osw970325@naver.com>
2022-03-23 18:18:28 +0900

    feat: 게임 실행 로직 작성

---
+++ src/main/java/chess/Application.java
@@ -0,0 +1,12 @@
+package chess;
+
+import chess.controller.ChessController;
+import chess.view.InputView;
+import chess.view.OutputView;
+
+public class Application {
+    public static void main(String[] args) {
+        ChessController controller = new ChessController(new InputView(), new OutputView());
+        controller.run();
+    }
+}
---
+++ src/main/java/chess/controller/ChessController.java
@@ -0,0 +1,28 @@
+package chess.controller;
+
+import chess.controller.api.Request;
+import chess.game.Game;
+import chess.view.InputView;
+import chess.view.OutputView;
+
+public class ChessController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ChessController(InputView inputView, OutputView outputVIew) {
+        this.inputView = inputView;
+        this.outputView = outputVIew;
+    }
+
+    public void run() {
+        Game game = new Game();
+        outputView.printIntroduction();
+
+        game.execute(Request.of(inputView.inputCommand()));
+        while (game.isRunnable()) {
+            outputView.printBoard(game.getPointPieces());
+            game.execute(Request.of(inputView.inputCommand()));
+        }
+    }
+}
