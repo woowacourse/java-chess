6a8fb839471129c4572cee66bef72ea7a90c6172
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-23 15:15:29 +0900

    test: 나이트 테스트 chessboard 셋업

--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -1,6 +1,6 @@
 package chess.domain.piece;
 
-import static chess.domain.piece.Color.WHITE;
+import static chess.domain.piece.Color.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
 
 class KnightTest {
 
@@ -21,10 +20,13 @@ class KnightTest {
 
     @BeforeEach
     void setUp() {
-        knight = new Knight(Color.WHITE, new Position('a', '1'));
-        Position position = new Position('b', '3');
-        Piece piece = new Knight(WHITE, position);
-        chessBoard = new ChessBoard(Map.of(position, piece));
+        knight = new Knight(Color.WHITE, new Position('b', '1'));
+
+        Piece whietePiece = new Knight(WHITE, new Position('a', '3'));
+        Piece darkPiece = new Knight(BLACK, new Position('c', '3'));
+        chessBoard = new ChessBoard(Map.of(
+                whietePiece.getPosition(), whietePiece,
+                darkPiece.getPosition(), darkPiece));
     }
 
     @Test
@@ -34,7 +36,7 @@ class KnightTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"a,2", "a,3", "a,4", "b,1", "b,2", "c,3"})
+    @CsvSource(value = {"b,2", "b,3", "b,4", "c,1", "c,2", "d,3"})
     @DisplayName("나이트 위치 이동 불가 예외발생")
     void moveException(char row, char col) {
         Position movePosition = new Position(row, col);
@@ -46,8 +48,8 @@ class KnightTest {
 
     @Test
     @DisplayName("해당 위치에 같은 색상의 기물 존재시 예외발생")
-    void test() {
-        Position position = new Position('b', '3');
+    void moveExceptionByExistEqualsColorPiece() {
+        Position position = new Position('a', '3');
 
         assertThatThrownBy(() -> knight.move(position, chessBoard))
                 .isInstanceOf(IllegalStateException.class)
@@ -55,7 +57,7 @@ class KnightTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"b,3", "c,2"})
+    @CsvSource(value = {"c,3", "d,2"})
     @DisplayName("나이트 위치 이동")
     void move(char row, char col) {
         Position expected = new Position(row, col);
