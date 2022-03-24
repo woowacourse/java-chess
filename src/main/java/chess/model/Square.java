1c115dda49a2c5eed5179f3a32c9cf54ee2a8e1d
yangdongjue5510 <djwhy5510@gmail.com>
2022-03-24 18:56:56 +0900

    feat: Square 문자열로 생성하는 정적 팩토리 메서드 추가

--- src/main/java/chess/model/Square.java
+++ src/main/java/chess/model/Square.java
@@ -11,6 +11,41 @@ public final class Square {
         this.rank = rank;
     }
 
+    public static Square of(String squareName) {
+        if (squareName.length() != 2) {
+            throw new IllegalArgumentException("잘못된 위치를 입력하였습니다.");
+        }
+        File file = File.valueOf(String.valueOf(Character.toUpperCase(squareName.charAt(0))));
+        Rank rank = Rank.of(Character.getNumericValue(squareName.charAt(1)));
+        return new Square(file, rank);
+    }
+
+    public boolean isPawnFirstSquare(Color color) {
+        if (color.isBlack()) {
+            return rank.equals(Rank.SEVEN);
+        }
+        return rank.equals(Rank.TWO);
+    }
+
+    public Square tryToMove(Direction direction) {
+        if (canMove(direction)) {
+            return move(direction);
+        }
+        return this;
+    }
+    private Square move(Direction direction) {
+        return new Square(file.add(direction.getRow()), rank.add(direction.getCol()));
+    }
+
+    private boolean canMove(Direction direction) {
+        try {
+            move(direction);
+            return true;
+        } catch (IllegalArgumentException exception) {
+            return false;
+        }
+    }
+
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -26,31 +61,5 @@ public final class Square {
     @Override
     public int hashCode() {
         return Objects.hash(file, rank);
-    }
-
-    public boolean isPawnFirstSquare(Color color) {
-        if (color.isBlack()) {
-            return rank.equals(Rank.SEVEN);
-        }
-        return rank.equals(Rank.TWO);
-    }
-    public Square tryToMove(Direction direction) {
-        if (canMove(direction)) {
-            return move(direction);
-        }
-        return this;
-    }
-
-    private Square move(Direction direction) {
-        return new Square(file.add(direction.getRow()), rank.add(direction.getCol()));
-    }
-
-    private boolean canMove(Direction direction) {
-        try {
-            move(direction);
-            return true;
-        } catch (IllegalArgumentException exception) {
-            return false;
-        }
     }
 }
