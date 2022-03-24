c25da55029acc4092065a16fcf147cff91827e51
Richard JEON <ztzy1907@gmail.com>
2022-03-24 14:47:28 +0900

    test(bishop): 목적지에 아군 기물 존재로 인한 비숍 이동 실패 케이스 테스트 추가

--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -8,6 +8,7 @@ import chess.domain.board.BoardFactory;
 import chess.domain.board.Position;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -27,5 +28,24 @@ class BishopTest {
 
         // then
         assertThat(move).isTrue();
+    }
+
+    @ParameterizedTest(name = "출발지 : E4, 도착지 : {0}")
+    @ValueSource(strings = {"A8", "B1", "H1", "H7"})
+    @DisplayName("아군 기물로 인한 비숍 이동 실패 테스트")
+    void bishopMoveFailCausedBySameColoredPiece(String to) {
+        // given
+        Map<Position, Piece> testBoard = new LinkedHashMap<>();
+        Stream.of("A8", "B1", "H1", "H7")
+                .forEach(position -> testBoard.put(Position.of(position), new Pawn(Color.WHITE)));
+
+        testBoard.put(Position.of("E4"), new Bishop(Color.WHITE));
+        Board board = BoardFactory.newInstance(testBoard);
+
+        // when
+        boolean move = board.move("E4", to);
+
+        // then
+        assertThat(move).isFalse();
     }
 }
