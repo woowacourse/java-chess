fba346c6c26872ff833f61fe8b19dd66f38e9669
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 15:07:32 +0900

    feat: 룩 움직임 전략 구현 및 테스트

--- src/main/java/chess/domain/piece/strategy/RookMovableStrategy.java
+++ src/main/java/chess/domain/piece/strategy/RookMovableStrategy.java
@@ -1,11 +1,16 @@
 package chess.domain.piece.strategy;
 
-import static chess.domain.direction.Direction.*;
+import static chess.domain.direction.Direction.DOWN;
+import static chess.domain.direction.Direction.LEFT;
+import static chess.domain.direction.Direction.RIGHT;
+import static chess.domain.direction.Direction.UP;
 
 import chess.domain.ChessBoard;
 import chess.domain.Position;
 import chess.domain.direction.Direction;
+import chess.domain.piece.Piece;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class RookMovableStrategy implements PieceMovableStrategy {
@@ -14,8 +19,30 @@ public class RookMovableStrategy implements PieceMovableStrategy {
 
     @Override
     public boolean isMovable(Position start, Position target, ChessBoard chessBoard) {
+        List<Position> route = calculateRoute(start, target);
+        if (route.isEmpty()) {
+            return false;
+        }
+        return isEmptyRouteWithoutTargetPosition(target, chessBoard, route)
+                && !isTargetSameColor(start, target, chessBoard);
+    }
+
+    private List<Position> calculateRoute(final Position start, final Position target) {
         return MOVE_DIRECTIONS.stream()
                 .map(direction -> direction.route(start, target))
-                .anyMatch(route -> !route.isEmpty());
+                .filter(route -> !route.isEmpty())
+                .findAny()
+                .orElse(Collections.emptyList());
+    }
+
+    private boolean isEmptyRouteWithoutTargetPosition(Position target, ChessBoard chessBoard, List<Position> route) {
+        return route.stream()
+                .filter(position -> !position.equals(target))
+                .allMatch(chessBoard::isPositionEmpty);
+    }
+
+    private boolean isTargetSameColor(Position start, Position target, ChessBoard chessBoard) {
+        Piece piece = chessBoard.pieceByPosition(start);
+        return !chessBoard.isPositionEmpty(target) && chessBoard.pieceByPosition(target).isSameColor(piece);
     }
 }
