2b2e1327293c1531a2817991c8d21d71a51ce67b
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-24 15:55:21 +0900

    feat: Knight Piece의 행마법이 맞는지 확인하는 기능 구현

--- src/main/java/chess/domain/piece/KnightPiece.java
+++ src/main/java/chess/domain/piece/KnightPiece.java
@@ -1,15 +1,37 @@
 package chess.domain.piece;
 
-public class KnightPiece extends FullPiece{
+import chess.domain.position.Direction;
+import chess.domain.position.Position;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KnightPiece extends FullPiece {
 
     private static final String WHITE_NAME = "n";
     private static final String BLACK_NAME = "N";
 
     private final String name;
+    private final List<Direction> movableDirections;
 
     public KnightPiece(final Color color) {
         super(color);
         this.name = decideName(color);
+        this.movableDirections = new ArrayList<>(
+            List.of(Direction.KNIGHT_EAST_LEFT, Direction.KNIGHT_EAST_RIGHT,
+                Direction.KNIGHT_WEST_LEFT, Direction.KNIGHT_WEST_RIGHT,
+                Direction.KNIGHT_NORTH_LEFT, Direction.KNIGHT_NORTH_RIGHT,
+                Direction.KNIGHT_SOUTH_LEFT,
+                Direction.KNIGHT_SOUTH_RIGHT));
+    }
+
+    @Override
+    public boolean isMovable(Position from, Position to) {
+        final int fileDistance = to.calculateFileDistance(from);
+        final int rankDistance = to.calculateRankDistance(from);
+
+        final Direction direction = Direction.of(fileDistance, rankDistance);
+
+        return movableDirections.contains(direction);
     }
 
     private String decideName(final Color color) {
---
+++ src/test/java/chess/domain/piece/KnightPieceTest.java
@@ -0,0 +1,34 @@
+package chess.domain.piece;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import chess.domain.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class KnightPieceTest {
+
+    @ParameterizedTest
+    @CsvSource({"d4, f5", "d4, f3", "d4, b3", "d4, b5", "d4, c6", "d4, e6", "d4, e2", "d4, c2"})
+    @DisplayName("move 할 수 있는지 확인한다.")
+    void canMove(String source, String target) {
+        Piece knight = new KnightPiece(Color.BLACK);
+        Position from = Position.create(source);
+        Position to = Position.create(target);
+
+        assertTrue(knight.isMovable(from, to));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"a1, a3", "a1, c3"})
+    @DisplayName("move 할 수 없는지 확인한다.")
+    void cantMove(String source, String target) {
+        Piece knight = new KnightPiece(Color.BLACK);
+        Position from = Position.create(source);
+        Position to = Position.create(target);
+
+        assertFalse(knight.isMovable(from, to));
+    }
+}
