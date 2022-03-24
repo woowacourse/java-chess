65901bc936d6060a542843a623c6a729ec85761f
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 22:46:24 +0900

    feat: ChessGame 생성 및 게임 실행 기능 구현

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -3,5 +3,7 @@ package chess;
 public class Application {
 
     public static void main(String[] args) {
+        ChessGame chessGame = new ChessGame();
+        chessGame.run();
     }
 }
---
+++ src/main/java/chess/ChessGame.java
@@ -0,0 +1,26 @@
+package chess;
+
+import chess.view.InputView;
+import chess.view.OutputView;
+import chess.view.command.Command;
+import chess.view.command.Ready;
+
+public class ChessGame {
+
+    private Command command;
+
+    public ChessGame() {
+        this.command = new Ready();
+    }
+
+    public void run() {
+        OutputView.printGameGuide();
+        runGameUntilEnd();
+    }
+
+    private void runGameUntilEnd() {
+        while(!command.isEnd()) {
+            command = command.run(InputView.requestGameCommand());
+        }
+    }
+}
