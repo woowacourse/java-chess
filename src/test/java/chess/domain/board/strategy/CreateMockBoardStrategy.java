8bff489a3375635ad7bc1770f2fbe6f149bc9433
skullKim <kyk990328@gmail.com>
2022-03-24 16:54:55 +0900

    feat(Board.java): 폰이랑 나이트를 제외한 나머지 말 이동 기능 완료
    폰이랑 나이트를 제외한 나머지 말을 이동시키는 로직 구현 완료

---
+++ src/test/java/chess/domain/board/strategy/CreateMockBoardStrategy.java
@@ -0,0 +1,19 @@
+package chess.domain.board.strategy;
+
+import java.util.Map;
+
+import chess.domain.board.Position;
+import chess.domain.piece.Piece;
+
+public class CreateMockBoardStrategy implements BoardInitializeStrategy {
+	private final Map<Position, Piece> pieces;
+
+	public CreateMockBoardStrategy(final Map<Position, Piece> pieces) {
+		this.pieces = pieces;
+	}
+
+	@Override
+	public Map<Position, Piece> createPieces() {
+		return pieces;
+	}
+}
