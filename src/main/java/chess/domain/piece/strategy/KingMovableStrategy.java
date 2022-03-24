9e55bc939eef3f3beb3f35034d3ff9386f467c8a
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 15:31:03 +0900

    feat: 킹 움직임 전략 구현 및 테스트

---
+++ src/main/java/chess/domain/piece/strategy/KingMovableStrategy.java
@@ -0,0 +1,40 @@
+package chess.domain.piece.strategy;
+
+import static chess.domain.direction.Direction.DOWN;
+import static chess.domain.direction.Direction.DOWN_LEFT;
+import static chess.domain.direction.Direction.DOWN_RIGHT;
+import static chess.domain.direction.Direction.LEFT;
+import static chess.domain.direction.Direction.RIGHT;
+import static chess.domain.direction.Direction.UP;
+import static chess.domain.direction.Direction.UP_LEFT;
+import static chess.domain.direction.Direction.UP_RIGHT;
+
+import chess.domain.ChessBoard;
+import chess.domain.Position;
+import chess.domain.direction.Direction;
+import chess.domain.piece.Piece;
+import java.util.Arrays;
+import java.util.List;
+
+public class KingMovableStrategy implements PieceMovableStrategy {
+
+    private static final int MOVABLE_COUNT = 1;
+    private static final List<Direction> MOVE_DIRECTIONS = Arrays.asList(
+            UP, DOWN, RIGHT, LEFT, UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT);
+
+    @Override
+    public boolean isMovable(Position start, Position target, ChessBoard chessBoard) {
+        return !existSameColorPiece(start, target, chessBoard) && isMovableByDirection(start, target);
+    }
+
+    private boolean existSameColorPiece(Position start, Position target, ChessBoard chessBoard) {
+        Piece piece = chessBoard.pieceByPosition(start);
+        return !chessBoard.isPositionEmpty(target) && piece.isSameColor(chessBoard.pieceByPosition(target));
+    }
+
+    private boolean isMovableByDirection(final Position start, final Position target) {
+        return MOVE_DIRECTIONS.stream()
+                .map(direction -> direction.route(start, target))
+                .anyMatch(route -> route.size() == MOVABLE_COUNT);
+    }
+}
---
+++ src/test/java/chess/domain/piece/strategy/KingMovableStrategyTest.java
@@ -0,0 +1,49 @@
+package chess.domain.piece.strategy;
+
+import static chess.domain.piece.Color.WHITE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.ChessBoard;
+import chess.domain.Position;
+import chess.domain.piece.Color;
+import chess.domain.piece.Knight;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class KingMovableStrategyTest {
+
+    private PieceMovableStrategy kingMovableStrategy;
+    private Position start;
+
+    @BeforeEach
+    void setUp() {
+        kingMovableStrategy = new KingMovableStrategy();
+        start = new Position('a', '1');
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"a,2,true", "b,1,true", "b,2,true", "b,3,false", "c,3,false"})
+    @DisplayName("킹의 빈곳 이동 가능 여부 확인")
+    void isMovableToEmptyPosition(char col, char row, boolean expected) {
+        Position target = new Position(col, row);
+        ChessBoard chessBoard = new ChessBoard(Map.of(start, new Knight(WHITE)));
+
+        assertThat(kingMovableStrategy.isMovable(start, target, chessBoard)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"WHITE,false", "BLACK,true"})
+    @DisplayName("기물이 존재할 경우의 이동 가능 여부 확인")
+    void isMovableToDifferentPiecePosition(Color color, boolean expected) {
+        Position target = new Position('a', '2');
+        ChessBoard chessBoard = new ChessBoard(Map.of(
+                start, new Knight(WHITE),
+                target, new Knight(color)
+        ));
+
+        assertThat(kingMovableStrategy.isMovable(start, target, chessBoard)).isEqualTo(expected);
+    }
+}
