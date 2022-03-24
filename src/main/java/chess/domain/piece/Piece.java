f49cacde6f762869589a24df503d82706fca29b3
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-24 15:54:38 +0900

    feat: Queen Piece의 행마법이 맞는지 확인하는 기능 구현

--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -1,6 +1,10 @@
 package chess.domain.piece;
 
+import chess.domain.position.Position;
+
 public interface Piece {
 
     String getName();
+
+    boolean isMovable(final Position from, final Position to);
 }
