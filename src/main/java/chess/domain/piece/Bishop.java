4dadc6f5e14eea0d1d602cd7bf8b8c5a6e679aea
Chris <bjuuuu98@gmail.com>
2022-03-24 17:38:53 +0900

    feat(Bishop): 비숍 이동 및 예외 처리 기능 구현

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -2,10 +2,39 @@ package chess.domain.piece;
 
 import chess.domain.board.Position;
 import chess.domain.piece.vo.TeamColor;
+import java.util.List;
 
 public class Bishop extends Piece {
 
     public Bishop(final TeamColor teamColor, final Position position) {
         super(teamColor, position);
+    }
+
+    @Override
+    public Piece move(final List<Piece> pieces, final Position targetPosition) {
+        final int rankDifference = position.calculateDifferenceOfRank(targetPosition);
+        final int fileDifference = position.calculateDifferenceOfFile(targetPosition);
+
+        validateTargetPositionToMove(fileDifference, rankDifference);
+        checkOtherPiecesInPathToTarget(position, targetPosition, pieces);
+
+        return new Rook(teamColor, targetPosition);
+    }
+
+    private void checkOtherPiecesInPathToTarget(final Position currentPosition, final Position targetPosition,
+                                                final List<Piece> pieces) {
+        if (currentPosition == targetPosition) {
+            return;
+        }
+        if (pieces.stream().anyMatch(piece -> piece.hasPosition(currentPosition))) {
+            throw new IllegalArgumentException("이동 경로에 다른 기물이 존재합니다.");
+        }
+        checkOtherPiecesInPathToTarget(currentPosition.nextPosition(targetPosition), targetPosition, pieces);
+    }
+
+    private void validateTargetPositionToMove(final int fileDifference, final int rankDifference) {
+        if (fileDifference != rankDifference) {
+            throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+        }
     }
 }
---
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -0,0 +1,53 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import chess.domain.board.Position;
+import chess.domain.piece.vo.TeamColor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BishopTest {
+
+    @Test
+    @DisplayName("이동경로에 기물이 존재하지 않으면 목표 지점에 이동한다.")
+    void move() {
+        //given
+        final Bishop Bishop = new Bishop(TeamColor.BLACK, Position.from("1a"));
+        final Position targetPosition = Position.from("3c");
+        final Piece moved = Bishop.move(new ArrayList<>(), targetPosition);
+        //when
+        final boolean actual = moved.hasPosition(targetPosition);
+        //then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("이동경로에 기물이 존재하면 예외를 발생시킨다.")
+    void moveException() {
+        //given
+        final Bishop Bishop = new Bishop(TeamColor.BLACK, Position.from("1a"));
+        final List<Piece> pieces = Collections.singletonList(new Knight(TeamColor.BLACK, Position.from("2b")));
+        final Position targetPosition = Position.from("3c");
+        //when, then
+        assertThatThrownBy(() -> Bishop.move(pieces, targetPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이동 경로에 다른 기물이 존재합니다.");
+    }
+
+    @Test
+    @DisplayName("목표 지점이 갈 수 없는 곳이면 예외를 발생시킨다.")
+    void moveExceptionTarget() {
+        //given
+        final Bishop Bishop = new Bishop(TeamColor.BLACK, Position.from("1a"));
+        final Position targetPosition = Position.from("2a");
+        //when, then
+        assertThatThrownBy(() -> Bishop.move(new ArrayList<>(), targetPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이동할 수 없는 위치입니다.");
+    }
+}
