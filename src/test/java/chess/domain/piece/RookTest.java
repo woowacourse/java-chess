e281daa17b32fd0888e133a36922a3b447752a21
Richard JEON <ztzy1907@gmail.com>
2022-03-24 14:53:46 +0900

    test(rook): 목적지에 아군 기물 존재로 인한 이동 실패 케이스 테스트 추가

--- src/test/java/chess/domain/piece/RookTest.java
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -8,6 +8,7 @@ import chess.domain.board.BoardFactory;
 import chess.domain.board.Position;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -27,5 +28,24 @@ class RookTest {
 
         // then
         assertThat(move).isTrue();
+    }
+
+    @ParameterizedTest(name = "출발지 : F5, 도착지 : {0}")
+    @ValueSource(strings = {"F1", "F8", "A5", "H5"})
+    @DisplayName("아군 기물로 인한 룩 이동 실패 테스트")
+    void rookMoveFailCausedBySameColoredPiece(String to) {
+        // given
+        Map<Position, Piece> testBoard = new LinkedHashMap<>();
+        Stream.of("F1", "F8", "A5", "H5")
+                .forEach(position -> testBoard.put(Position.of(position), new Pawn(Color.WHITE)));
+
+        testBoard.put(Position.of("F5"), new Rook(Color.WHITE));
+        Board board = BoardFactory.newInstance(testBoard);
+
+        // when
+        boolean move = board.move("F5", to);
+
+        // then
+        assertThat(move).isFalse();
     }
 }
