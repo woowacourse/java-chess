9239cfaea2c8f127c415045dab28adddfa40e957
yhh1056 <yhh1056@naver.com>
2022-03-24 17:36:13 +0900

    feat: 열 좌표 추가

--- src/main/java/chess/Column.java
+++ src/main/java/chess/Column.java
@@ -4,7 +4,20 @@ import java.util.Arrays;
 
 public enum Column {
 
-    A, B, C, D, E, F, G, H;
+    A(1),
+    B(2),
+    C(3),
+    D(4),
+    E(5),
+    F(6),
+    G(7),
+    H(8);
+
+    private final int value;
+
+    Column(final int value) {
+        this.value = value;
+    }
 
     public static Column of(final String value) {
         return Arrays.stream(values())
