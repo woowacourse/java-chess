3b8ce04b1d9fdc530f64106928d36897c24c37a4
Richard JEON <ztzy1907@gmail.com>
2022-03-24 14:53:23 +0900

    test(pawn): 목적지에 아군 기물 존재로 인한 이동 실패 케이스 테스트 추가

--- src/test/java/chess/domain/piece/PawnTest.java
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -2,8 +2,14 @@ package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import chess.domain.Color;
 import chess.domain.board.Board;
+import chess.domain.board.BoardFactory;
+import chess.domain.board.Position;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -13,12 +19,28 @@ class PawnTest {
     @DisplayName("Pawn 앞으로 전진 테스트")
     void pawnMoveForward(String from, String to) {
         // given
-        Board board = new Board();
+        Board board = BoardFactory.newInstance();
 
         // when
         boolean move = board.move(from, to);
 
         // then
         assertThat(move).isTrue();
+    }
+
+    @Test
+    @DisplayName("아군 기물 존재로 인한 폰 이동 실패 테스트")
+    void pawnMoveShouldFailCausedBySameColoredPiece() {
+        // given
+        Map<Position, Piece> testBoard = new LinkedHashMap<>();
+        testBoard.put(Position.of("F6"), new Rook(Color.WHITE));
+        testBoard.put(Position.of("F5"), new Pawn(Color.WHITE));
+        Board board = BoardFactory.newInstance(testBoard);
+
+        // when
+        boolean move = board.move("F5", "F6");
+
+        // then
+        assertThat(move).isFalse();
     }
 }
