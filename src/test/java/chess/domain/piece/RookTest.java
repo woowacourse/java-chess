0ccc72f204042cf9ae07f02a94f93a6e340d75df
hyeonic <dev.hyeonic@gmail.com>
2022-03-24 21:05:17 +0900

    test: Rook 이동 불가능 테스트 추가

--- src/test/java/chess/domain/piece/RookTest.java
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import chess.domain.BoardFixtures;
 import chess.domain.ChessBoard;
 import chess.domain.Color;
+import chess.domain.generator.EmptyBoardGenerator;
 import chess.domain.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,20 @@ class RookTest {
         boolean result = rook.canMove(chessBoard.getBoard(), sourcePosition, targetPosition);
 
         assertThat(result).isTrue();
+    }
+
+    @DisplayName("위치 정보를 기반으로 중간에 기물이 있는 경우 이동이 불가능하다.")
+    @Test
+    void 중간에_기물이_있는_경우_이동_불가능하다() {
+        ChessBoard chessBoard = new ChessBoard(new EmptyBoardGenerator());
+        chessBoard.init();
+
+        Position sourcePosition = new Position("a1");
+        Position targetPosition = new Position("a8");
+        Rook rook = new Rook(Color.WHITE);
+
+        boolean result = rook.canMove(chessBoard.getBoard(), sourcePosition, targetPosition);
+
+        assertThat(result).isFalse();
     }
 }
