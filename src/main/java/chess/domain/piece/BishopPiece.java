4cf9102b9cac9fb682cdb93263d0c5a890418f88
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-24 15:55:01 +0900

    feat: Bishop Piece의 행마법이 맞는지 확인하는 기능 구현

--- src/main/java/chess/domain/piece/BishopPiece.java
+++ src/main/java/chess/domain/piece/BishopPiece.java
@@ -1,15 +1,34 @@
 package chess.domain.piece;
 
+import chess.domain.position.Direction;
+import chess.domain.position.Position;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BishopPiece extends FullPiece {
 
     private static final String WHITE_NAME = "b";
     private static final String BLACK_NAME = "B";
 
     private final String name;
+    private final List<Direction> movableDirections;
 
     public BishopPiece(final Color color) {
         super(color);
         this.name = decideName(color);
+        this.movableDirections = new ArrayList<>(
+            List.of(Direction.NORTH_EAST, Direction.NORTH_WEST, Direction.SOUTH_EAST,
+                Direction.SOUTH_WEST));
+    }
+
+    @Override
+    public boolean isMovable(Position from, Position to) {
+        final int fileDistance = to.calculateFileDistance(from);
+        final int rankDistance = to.calculateRankDistance(from);
+
+        final Direction direction = Direction.of(fileDistance, rankDistance);
+
+        return movableDirections.contains(direction);
     }
 
     private String decideName(final Color color) {
