e1e0dbc40e3eb74d2b6aaf5cf6ac32ab5458e5b3
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 21:02:19 +0900

    feat: Ready null입력시 예외 추가 및 테스트

--- src/main/java/chess/view/command/Ready.java
+++ src/main/java/chess/view/command/Ready.java
@@ -1,9 +1,12 @@
 package chess.view.command;
 
+import java.util.Objects;
+
 public class Ready implements Command {
 
     @Override
     public Command run(String command) {
+        Objects.requireNonNull(command, "command는 null이 들어올 수 없습니다.");
         if (!command.equals("start")) {
             throw new IllegalArgumentException("게임이 시작되지 않아 다른 명령을 실행할 수 없습니다.");
         }
