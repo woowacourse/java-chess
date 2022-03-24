df4e9735244464892bdebd017afb033ce6864a97
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 15:07:32 +0900

    feat: 룩 움직임 전략 구현 및 테스트

--- src/test/java/chess/domain/piece/strategy/RookMovableStrategyTest.java
+++ src/test/java/chess/domain/piece/strategy/RookMovableStrategyTest.java
@@ -1,16 +1,21 @@
 package chess.domain.piece.strategy;
 
+import static chess.domain.piece.Color.BLACK;
 import static chess.domain.piece.Color.WHITE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.ChessBoard;
 import chess.domain.Position;
+import chess.domain.piece.Color;
+import chess.domain.piece.Pawn;
 import chess.domain.piece.Rook;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class RookMovableStrategyTest {
 
@@ -25,12 +30,56 @@ class RookMovableStrategyTest {
 
     @ParameterizedTest
     @CsvSource(value = {"d,8,true", "d,7,true", "c,4,true", "c,5,false", "c,6,false"})
-    @DisplayName("룩의 빈곳 전진 가능 여부 확인")
-    void isMovableToEmptyPosition(char col, char row, boolean expected) {
+    @DisplayName("목표 지점이 이동 가능 경로를 벗어났는지에 따른 이동 가능여부")
+    void emptyDirection(char col, char row, boolean expected) {
         Position target = new Position(col, row);
         ChessBoard chessBoard = new ChessBoard(Map.of(start, new Rook(WHITE)));
 
         assertThat(rookMovableStrategy.isMovable(start, target, chessBoard)).isEqualTo(expected);
     }
 
+    @Test
+    @DisplayName("가로막히지 않고 목표지점이 비어 있으면 이동 가능")
+    void isMovableToClearEmptyPosition() {
+        Position target = new Position('d', '8');
+        ChessBoard chessBoard = new ChessBoard(Map.of(start, new Pawn(WHITE)));
+
+        assertThat(rookMovableStrategy.isMovable(start, target, chessBoard)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"WHITE,false", "BLACK,true"})
+    @DisplayName("가로막히지 않고 목표지점에 기물이 존재시 이동 가능 여부")
+    void isMovableToClearPiecePosition(Color color, boolean expected) {
+        Position target = new Position('d', '8');
+        ChessBoard chessBoard = new ChessBoard(Map.of(
+                start, new Pawn(WHITE),
+                target, new Pawn(color)));
+
+        assertThat(rookMovableStrategy.isMovable(start, target, chessBoard)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("가로막히고 목표지점이 비어 있으면 이동 불가능")
+    void isMovableToNotClearEmptyPosition() {
+        Position target = new Position('d', '8');
+        ChessBoard chessBoard = new ChessBoard(Map.of(
+                start, new Pawn(WHITE),
+                new Position('d', '7'), new Pawn(BLACK)));
+
+        assertThat(rookMovableStrategy.isMovable(start, target, chessBoard)).isFalse();
+    }
+
+    @ParameterizedTest
+    @EnumSource(Color.class)
+    @DisplayName("가로막히고 목표지점에 기물이 존재시 이동 불가능")
+    void isMovableToNotClearPiecePosition(Color color) {
+        Position target = new Position('d', '8');
+        ChessBoard chessBoard = new ChessBoard(Map.of(
+                start, new Pawn(WHITE),
+                new Position('d', '7'), new Pawn(BLACK),
+                target, new Pawn(color)));
+
+        assertThat(rookMovableStrategy.isMovable(start, target, chessBoard)).isFalse();
+    }
 }
