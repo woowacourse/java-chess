e300deb3e1f2da1c406869f1738106e16f761054
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-25 02:32:09 +0900

    test: class public 제거

--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class PositionTest {
+class PositionTest {
 
     @ParameterizedTest
     @ValueSource(chars = {'`', 'A', 'i'})
--- src/test/java/chess/domain/piece/strategy/BlackPawnMovableStrategyTest.java
+++ src/test/java/chess/domain/piece/strategy/BlackPawnMovableStrategyTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class BlackPawnMovableStrategyTest {
+class BlackPawnMovableStrategyTest {
 
     private PieceMovableStrategy blackPawnMovableStrategy;
     private Position source;
