3abdc2499faf68a4cba69f8d96e16bfdd1361df1
Richard JEON <ztzy1907@gmail.com>
2022-03-24 14:44:33 +0900

    test(queen): 퀸 목적지에 아군이 있어서 이동 실패하는 케이스 테스트 추가

--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -8,13 +8,14 @@ import chess.domain.board.BoardFactory;
 import chess.domain.board.Position;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class QueenTest {
     @ParameterizedTest(name = "출발지 : D5, 도착지 : {0}")
-    @ValueSource(strings = {"A8", "A5", "A2", "D1", "H1", "H5", "G8", "D8"})
+    @ValueSource(strings = {"A8", "A5", "A2", "D1", "H1", "H5", "G8", "D"})
     @DisplayName("퀸 이동 테스트")
     void rookMove(String to) {
         // given
@@ -27,5 +28,24 @@ class QueenTest {
 
         // then
         assertThat(move).isTrue();
+    }
+
+    @ParameterizedTest(name = "출발지 : D5, 도착지 : {0}")
+    @ValueSource(strings = {"D8", "F7", "G5", "G2", "D4", "C4", "C5", "C6"})
+    @DisplayName("아군 기물로 인한 퀸 이동 실패 테스트")
+    void rookMoveFailCausedBySameColoredPiece(String to) {
+        // given
+        Map<Position, Piece> testBoard = new LinkedHashMap<>();
+        Stream.of("D8", "F7", "G5", "G2", "D4", "C4", "C5", "C6")
+                .forEach(position -> testBoard.put(Position.of(position), new Pawn(Color.WHITE)));
+
+        testBoard.put(Position.of("D5"), new Queen(Color.WHITE));
+        Board board = BoardFactory.newInstance(testBoard);
+
+        // when
+        boolean move = board.move("D5", to);
+
+        // then
+        assertThat(move).isFalse();
     }
 }
