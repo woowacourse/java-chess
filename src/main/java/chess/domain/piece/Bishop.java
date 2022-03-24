cd4f204d7bce0da9e629088d7d82877230ea319a
Richard JEON <ztzy1907@gmail.com>
2022-03-24 14:20:50 +0900

    feat(bishop): 비숍 행마법 구현

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -4,14 +4,18 @@ import chess.domain.Color;
 import java.util.List;
 
 public final class Bishop extends Piece {
+    private static final int FILE_INDEX = 0;
+    private static final int RANK_INDEX = 1;
+
     public Bishop(Color color) {
         super(color);
     }
 
     @Override
     public boolean movable(List<Integer> distances, Piece target) {
-        return false;
+        boolean isMovableDistance = distances.get(FILE_INDEX).equals(distances.get(RANK_INDEX));
+        boolean isOpponent = isOpponent(target);
+
+        return isMovableDistance && isOpponent;
     }
-
-
 }
--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -1,9 +1,31 @@
 package chess.domain.piece;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import chess.domain.Color;
+import chess.domain.board.Board;
+import chess.domain.board.BoardFactory;
+import chess.domain.board.Position;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BishopTest {
+    @ParameterizedTest(name = "출발지 : E4, 도착지 : {0}")
+    @ValueSource(strings = {"A8", "B1", "H1", "H7"})
+    @DisplayName("비숍 이동 테스트")
+    void rookMove(String to) {
+        // given
+        Map<Position, Piece> testBoard = new LinkedHashMap<>();
+        testBoard.put(Position.of("E4"), new Bishop(Color.WHITE));
+        Board board = BoardFactory.newInstance(testBoard);
 
+        // when
+        boolean move = board.move("E4", to);
+
+        // then
+        assertThat(move).isTrue();
+    }
 }
