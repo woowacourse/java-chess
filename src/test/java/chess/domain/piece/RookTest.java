8efe420378c42a380a956debd9833247b09d2ee8
dongho <dongho108@naver.com>
2022-03-24 19:57:23 +0900

    test: RookTest ParameterizedTest 로 리팩터링

--- src/test/java/chess/domain/piece/RookTest.java
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -2,9 +2,13 @@ package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import chess.domain.ChessBoard;
 import chess.domain.piece.position.File;
@@ -15,76 +19,50 @@ import chess.domain.piece.property.Color;
 public class RookTest {
 
     private ChessBoard board;
+    private Piece sourcePiece;
 
     @BeforeEach
     void initBoard() {
         board = new ChessBoard();
-        board.putPiece(Position.of(File.a, Rank.Seven), new Rook(Color.Black));
-        board.putPiece(Position.of(File.b, Rank.Seven), new Rook(Color.White));
+        sourcePiece = new Rook(Color.Black);
+
+        board.putPiece(Position.of(File.d, Rank.Four), sourcePiece);
+        board.putPiece(Position.of(File.d, Rank.Three), new Rook(Color.Black));
+        board.putPiece(Position.of(File.d, Rank.Five), new Rook(Color.White));
     }
 
-    @Test
-    @DisplayName("룩이 가려는 포지션에 같은 색의 기물이 있다면 에러를 출력한다.")
-    void pieceTest() {
-        Position source = Position.of(File.a, Rank.Eight);
-        Position target = Position.of(File.a, Rank.Seven);
-
-        Piece piece = new Rook(Color.Black);
-        board.putPiece(source, piece);
+    @ParameterizedTest(name = "{index}: {1}")
+    @MethodSource("invalidParameters")
+    @DisplayName("룩이 이동할 수 없는 곳으로 이동")
+    void rookInvalidTest(Position target, String testName) {
+        Position source = Position.of(File.d, Rank.Four);
 
         assertThatThrownBy(() -> board.move(source, target))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    @DisplayName("룩이 가려는 포지션에 다른 색의 기물이 있다면 기물을 잡고 그 위치로 이동한다.")
-    void pieceTest2() {
-        Position source = Position.of(File.b, Rank.Eight);
-        Position target = Position.of(File.b, Rank.Seven);
+    private static Stream<Arguments> invalidParameters() {
+        return Stream.of(
+            Arguments.of(Position.of(File.d, Rank.Three), "룩이 가려는 포지션에 같은 색의 기물이 있다면 이동할 수 없다."),
+            Arguments.of(Position.of(File.e, Rank.Five), "룩은 대각선으로 이동할 수 없다."),
+            Arguments.of(Position.of(File.d, Rank.One), "룩은 가려는 방향에서 기물이 있는 곳 너머로 이동할 수 없다.")
+        );
+    }
 
-        Piece piece = new Rook(Color.Black);
-        board.putPiece(source, piece);
+    @ParameterizedTest(name = "{index}: {1}")
+    @MethodSource("validParameters")
+    @DisplayName("룩이 이동할 수 있는 곳으로 이동")
+    void rookValidTest(Position target, String testName) {
+        Position source = Position.of(File.d, Rank.Four);
         board.move(source, target);
 
-        assertThat(board.getPiece(target)).isSameAs(piece);
+        assertThat(board.getPiece(target)).isSameAs(sourcePiece);
     }
 
-    @Test
-    @DisplayName("룩이 대각선으로 이동할 경우 예외를 출력한다.")
-    void pieceTest3() {
-        Position source = Position.of(File.a, Rank.Eight);
-        Position target = Position.of(File.b, Rank.Seven);
-
-        Piece piece = new Rook(Color.Black);
-        board.putPiece(source, piece);
-
-        assertThatThrownBy(() -> board.move(source, target))
-            .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("가려는 방향에서 기물이 있기 전까지의 위치는 이동이 가능하다.")
-    void pieceTest4() {
-        Position source = Position.of(File.a, Rank.Eight);
-        Position target = Position.of(File.h, Rank.Eight);
-
-        Piece piece = new Rook(Color.Black);
-        board.putPiece(source, piece);
-        board.move(source, target);
-
-        assertThat(board.getPiece(target)).isSameAs(piece);
-    }
-
-    @Test
-    @DisplayName("가려는 방향에서 기물이 있는 곳 너머로 이동하는 경우 에러를 출력한다.")
-    void pieceTest5() {
-        Position source = Position.of(File.a, Rank.Eight);
-        Position target = Position.of(File.a, Rank.Six);
-
-        Piece piece = new Rook(Color.Black);
-        board.putPiece(source, piece);
-
-        assertThatThrownBy(() -> board.move(source, target))
-            .isInstanceOf(IllegalArgumentException.class);
+    private static Stream<Arguments> validParameters() {
+        return Stream.of(
+            Arguments.of(Position.of(File.d, Rank.Five), "룩이 가려는 포지션에 다른 색의 기물이 있다면 기물을 잡고 그 위치로 이동한다."),
+            Arguments.of(Position.of(File.a, Rank.Four), "가려는 방향에서 기물이 있기 전까지의 위치는 이동이 가능하다.")
+        );
     }
 }
