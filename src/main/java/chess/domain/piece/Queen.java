b4e001657fbcc9f9c2487f47024a825b99d45d97
YJGwon <yj970125@gmail.com>
2022-03-24 18:49:14 +0900

    feat(domain/piece/Queen): 퀸의 이동거리 구현

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -1,10 +1,23 @@
 package chess.domain.piece;
 
 import chess.domain.Camp;
+import chess.domain.board.Position;
 
 public final class Queen extends Piece{
     public Queen(Camp camp) {
         super(camp);
+    }
+
+    public boolean canMove(Position beforePosition, Position afterPosition) {
+        int columnDistance = beforePosition.columnDistance(afterPosition);
+        int rowDistance = beforePosition.rowDistance(afterPosition);
+        if (columnDistance == 0) {
+            return true;
+        }
+        if (rowDistance == 0) {
+            return true;
+        }
+        return columnDistance == rowDistance;
     }
 
     @Override
