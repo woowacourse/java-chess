5f8cafe6d212a229e23969acc4f48dec4e6d7928
jayjaehunchoi <wogns0108@naver.com>
2022-03-24 18:58:12 +0900

    feat : 게임 진행 기능 구현

--- src/main/java/chess/controller/ChessGameController.java
+++ src/main/java/chess/controller/ChessGameController.java
@@ -3,6 +3,7 @@ package chess.controller;
 import chess.domain.game.ChessGameProgressor;
 import chess.view.InputView;
 import chess.view.OutputView;
+import java.util.List;
 
 public class ChessGameController {
 
@@ -10,6 +11,21 @@ public class ChessGameController {
         OutputView.printStartGame();
         if (InputView.inputInitialCommand()) {
             ChessGameProgressor chessGameProgressor = new ChessGameProgressor();
+            printCurrentBoard(chessGameProgressor);
+            progressChessGame(chessGameProgressor);
+        }
+    }
+
+    private void progressChessGame(final ChessGameProgressor chessGameProgressor) {
+        while (chessGameProgressor.isOn()) {
+            List<String> inputs = InputView.inputProgressCommand();
+            chessGameProgressor.progress(inputs);
+            printCurrentBoard(chessGameProgressor);
+        }
+    }
+
+    private void printCurrentBoard(final ChessGameProgressor chessGameProgressor) {
+        if (chessGameProgressor.isOn()) {
             OutputView.printBoard(chessGameProgressor.getCurrentBoard());
         }
     }
--- src/main/java/chess/domain/game/ChessGameProgressor.java
+++ src/main/java/chess/domain/game/ChessGameProgressor.java
@@ -3,9 +3,15 @@ package chess.domain.game;
 import chess.domain.board.Board;
 import chess.domain.board.Position;
 import chess.domain.piece.Piece;
+import java.util.List;
 import java.util.Map;
 
 public class ChessGameProgressor {
+
+    private static final int COMMAND_INDEX = 0;
+    private static final int SOURCE_INDEX = 1;
+    private static final int TARGET_INDEX = 2;
+    private static final String END_COMMAND = "end";
 
     private final ChessGame chessGame;
     private final GameSwitch gameSwitch;
@@ -17,5 +23,17 @@ public class ChessGameProgressor {
 
     public Map<Position, Piece> getCurrentBoard() {
         return chessGame.getBoard();
+    }
+
+    public boolean isOn() {
+        return gameSwitch.isOn();
+    }
+
+    public void progress(final List<String> inputs) {
+        if (inputs.get(COMMAND_INDEX).equals(END_COMMAND)) {
+            gameSwitch.turnOff();
+            return;
+        }
+        chessGame.move(Position.valueOf(inputs.get(SOURCE_INDEX)), Position.valueOf(inputs.get(TARGET_INDEX)));
     }
 }
