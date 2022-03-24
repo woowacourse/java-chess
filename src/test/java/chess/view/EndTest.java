70457603690a1752e7183ac60b5ce9101dac5e95
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 21:34:27 +0900

    feat: End 상태 run 실행 예외처리 및 테스트

---
+++ src/test/java/chess/view/EndTest.java
@@ -0,0 +1,22 @@
+package chess.view;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import chess.view.command.Command;
+import chess.view.command.End;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class EndTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"start", "move", "end", "input"})
+    @DisplayName("종료 상태에서 run 실행시 예외발생")
+    void runException(String inputLine) {
+        Command command = new End();
+        assertThatThrownBy(() -> command.run(inputLine))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("게임이 종료된 상태에서는 게임을 실행할 수 없습니다.");
+    }
+}
