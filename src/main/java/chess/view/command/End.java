fc84aaaf62c1e979cc1972488501c07356011aab
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 22:37:28 +0900

    feat: Move 커맨드 기능 구현 및 테스트

--- src/main/java/chess/view/command/End.java
+++ src/main/java/chess/view/command/End.java
@@ -3,7 +3,7 @@ package chess.view.command;
 public class End implements Command {
 
     @Override
-    public Command run(final String command) {
+    public Command run(String command) {
         throw new IllegalStateException("게임이 종료된 상태에서는 게임을 실행할 수 없습니다.");
     }
 }
