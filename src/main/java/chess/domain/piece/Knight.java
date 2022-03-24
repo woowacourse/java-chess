7d76366cab332bd8ec62023c3955d663eafe8a27
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-25 01:25:29 +0900

    feat: 체스판 게임 종료 여부 확인 기능 및 테스트

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -21,4 +21,9 @@ public final class Knight extends Piece {
     public final boolean isPawn() {
         return false;
     }
+
+    @Override
+    public final boolean isKing() {
+        return false;
+    }
 }
