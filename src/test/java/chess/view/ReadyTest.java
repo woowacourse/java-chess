0e9029516e2e529961d858816834e5c24191c09d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-24 21:02:19 +0900

    feat: Ready null입력시 예외 추가 및 테스트

--- src/test/java/chess/view/ReadyTest.java
+++ src/test/java/chess/view/ReadyTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import chess.view.command.Command;
 import chess.view.command.Ready;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -18,5 +19,14 @@ class ReadyTest {
         assertThatThrownBy(() -> command.run(inputLine))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("게임이 시작되지 않아 다른 명령을 실행할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("커맨드가 null일 경우 예외발생")
+    void commandNullException() {
+        Command command = new Ready();
+        assertThatThrownBy(() -> command.run(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("command는 null이 들어올 수 없습니다.");
     }
 }
