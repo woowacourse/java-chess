c0570f14b2b8788e131e7b611a8cea84ce463984
Richard JEON <ztzy1907@gmail.com>
2022-03-24 17:03:42 +0900

    feat(controller): 체스 게임 시작, 종료, 말 이동 기능 조합

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,16 +1,30 @@
 package chess.controller;
 
+import chess.domain.ChessExecution;
 import chess.domain.board.Board;
+import chess.domain.board.BoardFactory;
 import chess.dto.BoardDto;
+import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessController {
     public void start() {
         OutputView outputView = OutputView.getInstance();
-        Board board = new Board();
-
+        InputView inputView = InputView.getInstance();
+        Board board = BoardFactory.newInstance();
         outputView.printBoard(BoardDto.from(board));
 
-    }
+        String[] command = inputView.scanCommand().split(" ");
+        ChessExecution execution = ChessExecution.from(command[0]);
 
+        while (execution != ChessExecution.END) {
+            if (execution == ChessExecution.MOVE) {
+                board.move(command[1], command[2]);
+                outputView.printBoard(BoardDto.from(board));
+            }
+
+            command = inputView.scanCommand().split(" ");
+            execution = ChessExecution.from(command[0]);
+        }
+    }
 }
