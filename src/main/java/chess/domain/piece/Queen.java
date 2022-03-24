eebd40dc4f08ee655a498972f50c792fc9237974
Hongdonggeon <ghd700@naver.com>
2022-03-24 11:38:31 +0900

    feat : Queen 움직임 여부 확이 기능 추가

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -2,7 +2,7 @@ package chess.domain.piece;
 
 import chess.domain.Position;
 
-public class Queen extends Piece{
+public class Queen extends Piece {
 
     public Queen(Color color) {
         super(color);
@@ -10,6 +10,16 @@ public class Queen extends Piece{
 
     @Override
     public boolean isMovable(Position fromPosition, Position toPosition) {
-        return false;
+        return isSameRowOrCol(fromPosition, toPosition) || isDiagonal(fromPosition, toPosition);
+    }
+
+    private boolean isSameRowOrCol(Position fromPosition, Position toPosition) {
+        return fromPosition.isSameAbscissa(toPosition) || fromPosition.isSameOrdinate(toPosition);
+    }
+
+    private boolean isDiagonal(Position fromPosition, Position toPosition) {
+        int height = fromPosition.getOrdinateDifference(toPosition);
+        int width = fromPosition.getAbscissaDifference(toPosition);
+        return Math.pow(height, 2) == Math.pow(width, 2);
     }
 }
---
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -0,0 +1,83 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.Abscissa;
+import chess.domain.Ordinate;
+import chess.domain.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class QueenTest {
+
+    private Queen whiteQueen;
+
+    @BeforeEach
+    void setUp() {
+        whiteQueen = new Queen(Color.WHITE);
+    }
+
+    @Test
+    @DisplayName("Queen이 오른쪽 위 대각선으로 움직일 수 있는 경우 True를 반환한다")
+    void isMovableRightUpTest() {
+        Position from = Position.valueOf(Abscissa.c, Ordinate.ONE);
+        Position to = Position.valueOf(Abscissa.d, Ordinate.TWO);
+
+        assertThat(whiteQueen.isMovable(from, to)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Queen이 왼쪽 위 대각선으로 움직일 수 있는 경우 True를 반환한다")
+    void isMovableLeftUpTest() {
+        Position from = Position.valueOf(Abscissa.c, Ordinate.ONE);
+        Position to = Position.valueOf(Abscissa.b, Ordinate.TWO);
+
+        assertThat(whiteQueen.isMovable(from, to)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Queen이 왼쪽 아래 대각선으로 움직일 수 있는 경우 True를 반환한다")
+    void isMovableLeftDownTest() {
+        Position from = Position.valueOf(Abscissa.b, Ordinate.TWO);
+        Position to = Position.valueOf(Abscissa.a, Ordinate.ONE);
+
+        assertThat(whiteQueen.isMovable(from, to)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Queen이 오른쪽 아래 대각선으로 움직일 수 있는 경우 True를 반환한다")
+    void isMovableRightDownTest() {
+        Position from = Position.valueOf(Abscissa.b, Ordinate.TWO);
+        Position to = Position.valueOf(Abscissa.c, Ordinate.ONE);
+
+        assertThat(whiteQueen.isMovable(from, to)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Queen이 세로로 움직일 수 있는 경우 True를 반환한다")
+    void isMovableColumnTest() {
+        Position from = Position.valueOf(Abscissa.a, Ordinate.ONE);
+        Position to = Position.valueOf(Abscissa.a, Ordinate.FIVE);
+
+        assertThat(whiteQueen.isMovable(from, to)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Queen이 가로로 움직일 수 있는 경우 True를 반환한다")
+    void isMovableRowTest() {
+        Position from = Position.valueOf(Abscissa.a, Ordinate.ONE);
+        Position to = Position.valueOf(Abscissa.h, Ordinate.ONE);
+
+        assertThat(whiteQueen.isMovable(from, to)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Queen이 움직일 수 없는 경우 False를 반환한다.")
+    void isNotMovableTest() {
+        Position from = Position.valueOf(Abscissa.d, Ordinate.FOUR);
+        Position to = Position.valueOf(Abscissa.b, Ordinate.THREE);
+
+        assertThat(whiteQueen.isMovable(from, to)).isFalse();
+    }
+}
