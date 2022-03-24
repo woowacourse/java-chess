413badff5ca77a7701015eeb3c931122df9e69d6
Richard JEON <ztzy1907@gmail.com>
2022-03-24 12:05:55 +0900

    feat(knight): 나이트 여부 검증 메서드 추가

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -19,4 +19,9 @@ public final class Knight extends Piece {
 
         return isMovableDistance && isOpponent;
     }
+
+    @Override
+    public boolean isKnight() {
+        return true;
+    }
 }
