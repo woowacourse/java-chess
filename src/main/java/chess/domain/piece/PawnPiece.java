87471a73ea5464bd5ec3f3a6299863e1099b183c
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-24 15:55:47 +0900

    feat: Pawn Piece의 행마법이 맞는지 확인하는 기능 구현

--- src/main/java/chess/domain/piece/PawnPiece.java
+++ src/main/java/chess/domain/piece/PawnPiece.java
@@ -1,15 +1,54 @@
 package chess.domain.piece;
 
-public class PawnPiece extends FullPiece{
+import chess.domain.position.Direction;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PawnPiece extends FullPiece {
 
     private static final String WHITE_NAME = "p";
     private static final String BLACK_NAME = "P";
 
     private final String name;
+    private final List<Direction> movableDirections;
 
     public PawnPiece(final Color color) {
         super(color);
         this.name = decideName(color);
+        this.movableDirections = decideMovableDirections(color);
+    }
+
+    private List<Direction> decideMovableDirections(final Color color) {
+        if (color == Color.WHITE) {
+            return new ArrayList<>(
+                List.of(Direction.NORTH, Direction.NORTH_EAST, Direction.NORTH_WEST));
+        }
+        return new ArrayList<>(
+            List.of(Direction.SOUTH, Direction.SOUTH_EAST, Direction.SOUTH_WEST));
+    }
+
+    @Override
+    public boolean isMovable(final Position from, final Position to) {
+        final int fileDistance = to.calculateFileDistance(from);
+        final int rankDistance = to.calculateRankDistance(from);
+
+        final Direction direction = Direction.of(fileDistance, rankDistance);
+
+        if (isInitialPositionAndMoveTwice(from, fileDistance, rankDistance)) {
+            return true;
+        }
+
+        return movableDirections.contains(direction) &&
+            Math.abs(fileDistance) < 2 && Math.abs(rankDistance) < 2;
+    }
+
+    private boolean isInitialPositionAndMoveTwice(final Position from, final int fileDistance, final int rankDistance) {
+        if (super.getColor() == Color.BLACK && from.isSameRank(Rank.SEVEN) && fileDistance == 0 && rankDistance == -2) {
+            return true;
+        }
+        return super.getColor() == Color.WHITE && from.isSameRank(Rank.TWO) && fileDistance == 0 && rankDistance == 2;
     }
 
     private String decideName(final Color color) {
---
+++ src/test/java/chess/domain/piece/PawnPieceTest.java
@@ -0,0 +1,83 @@
+package chess.domain.piece;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import chess.domain.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PawnPieceTest {
+
+    // 전진 ->  블랙: south, 화이트: north
+    // 대각선 -> 블랙: south east, west
+    //
+    // rank 2에서 출발할때는 1칸 또는 2칸 가능
+
+    @ParameterizedTest
+    @CsvSource({"d2, d3", "d2, e3", "d2, c3"})
+    @DisplayName("move 할 수 있는지 확인한다.")
+    void canMoveWhite(String source, String target) {
+        Piece pawn = new PawnPiece(Color.WHITE);
+        Position from = Position.create(source);
+        Position to = Position.create(target);
+
+        assertTrue(pawn.isMovable(from, to));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"d7, d6", "d7, e6", "d7, c6"})
+    @DisplayName("move 할 수 있는지 확인한다.")
+    void canMoveBlack(String source, String target) {
+        Piece pawn = new PawnPiece(Color.BLACK);
+        Position from = Position.create(source);
+        Position to = Position.create(target);
+
+        assertTrue(pawn.isMovable(from, to));
+    }
+
+    @Test
+    @DisplayName("초기 위치에서 앞으로 두칸 move할 수 있는지 확인한다.")
+    void canMoveTwiceWhite() {
+        Piece pawn = new PawnPiece(Color.WHITE);
+        Position from = Position.create("a2");
+        Position to = Position.create("a4");
+
+        assertTrue(pawn.isMovable(from, to));
+    }
+
+    @Test
+    @DisplayName("초기 위치에서 앞으로 두칸 move할 수 있는지 확인한다.")
+    void canMoveTwiceBlack() {
+        Piece pawn = new PawnPiece(Color.BLACK);
+        Position from = Position.create("a7");
+        Position to = Position.create("a5");
+
+        assertTrue(pawn.isMovable(from, to));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"d2, d5", "d2, d1", "d2, f4", "d2, c1"})
+    @DisplayName("move 할 수 없는지 확인한다.")
+    void cantMoveWhite(String source, String target) {
+        Piece pawn = new PawnPiece(Color.WHITE);
+        Position from = Position.create(source);
+        Position to = Position.create(target);
+
+        assertFalse(pawn.isMovable(from, to));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"d7, d4", "d7, d8", "d7, f5", "d7, c8"})
+    @DisplayName("move 할 수 없는지 확인한다.")
+    void cantMoveBlack(String source, String target) {
+        Piece pawn = new PawnPiece(Color.BLACK);
+        Position from = Position.create(source);
+        Position to = Position.create(target);
+
+        assertFalse(pawn.isMovable(from, to));
+    }
+
+}
