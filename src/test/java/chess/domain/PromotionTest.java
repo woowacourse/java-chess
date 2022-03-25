59c3a94035e2c2c4d079d8172d0e99109e0e2bc9
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-25 13:02:33 +0900

    feat: 프로모션 기물 생성 기능 구현 및 테스트

---
+++ src/test/java/chess/domain/PromotionTest.java
@@ -0,0 +1,30 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import chess.domain.piece.Piece;
+import chess.domain.piece.single.Knight;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PromotionTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"q", "r", "p", "k"})
+    @DisplayName("Promotion 기물 생성 시 잘못된 입력값이 들어올 경우 예외발생")
+    void cretePromotionPieceException(String input) {
+        assertThatThrownBy(() -> Promotion.createPromotionPiece(input, Color.WHITE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("불가능한 프로모션 기물 이름입니다.");
+    }
+
+    @Test
+    @DisplayName("입력값에 따른 기물 생성")
+    void createPromotionPiece() {
+        Piece piece = Promotion.createPromotionPiece("N", Color.WHITE);
+        assertThat(piece).isInstanceOf(Knight.class);
+    }
+}
