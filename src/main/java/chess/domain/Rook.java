e9f9335f28355f8ba01e306999a3dc296b72bbe9
beomWhale <cndqjacndqja@gmail.com>
2022-03-25 16:59:20 +0900

    feat: Rook 이동 예외 검증 기능 구현

--- src/main/java/chess/domain/Rook.java
+++ src/main/java/chess/domain/Rook.java
@@ -5,4 +5,12 @@ public class Rook extends Piece {
     public Rook(Team team, Position position) {
         super(team, Rook.class.getSimpleName(), position);
     }
+
+    public void validateIsPossible(Position destination) {
+        if (position.getRow().getDifference(destination.getRow()) == 0
+                || position.getCol().getDifference(destination.getCol()) == 0) {
+            return;
+        }
+        throw new IllegalArgumentException("해당 위치로 말이 움직일 수 없습니다.");
+    }
 }
