5c25a4544cc31759fdd13ca99df6896c549efa6d
Seongwon97 <osw970325@naver.com>
2022-03-25 16:57:42 +0900

    feat(King): 킹의 이동 로직 구현
    - 누락된 README 체크리스트 정리

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -1,5 +1,9 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import chess.domain.board.Point;
+import chess.domain.piece.move.StraightDirection;
+
 public class King extends Piece {
 
     public King(Color color) {
@@ -9,5 +13,19 @@ public class King extends Piece {
     @Override
     public PieceType getType() {
         return PieceType.KING;
+    }
+
+    @Override
+    public void move(Board ignored, Point from, Point to) {
+        StraightDirection direction = StraightDirection.find(from, to);
+        Point next = from.next(direction);
+        if (!next.equals(to)) {
+            throw new IllegalArgumentException("[ERROR] 킹은 한 칸만 이동할 수 있습니다.");
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 }
--- src/test/java/chess/domain/piece/KingTest.java
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -1,9 +1,12 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import chess.domain.board.BoardFixtures;
+import chess.domain.board.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class KingTest {
 
@@ -13,5 +16,57 @@ public class KingTest {
         Piece piece = new King(Color.WHITE);
 
         assertThat(piece).isNotNull();
+    }
+
+    @Test
+    @DisplayName("앞으로 한 칸 움직인다.")
+    void moveForwardOneStep() {
+        Piece piece = new King(Color.BLACK);
+
+        Point from = Point.of("e1");
+        Point to = Point.of("e2");
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatCode(() -> piece.move(board, from, to))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("대각선으로 한 칸 움직인다.")
+    void moveDiagonalOneStep() {
+        Piece piece = new King(Color.BLACK);
+
+        Point from = Point.of("e1");
+        Point to = Point.of("d2");
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatCode(() -> piece.move(board, from, to))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("옆으로 한 칸 움직인다.")
+    void moveSideOneStep() {
+        Piece piece = new King(Color.BLACK);
+
+        Point from = Point.of("e1");
+        Point to = Point.of("d1");
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatCode(() -> piece.move(board, from, to))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("킹은 두 칸 이상 움직이지 못한다.")
+    void notMovable() {
+        Piece piece = new King(Color.BLACK);
+
+        Point from = Point.of("e1");
+        Point to = Point.of("d4");
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> piece.move(board, from, to));
     }
 }
