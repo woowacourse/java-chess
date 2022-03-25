ff8ddced2752fdd1b3ce86173b03bcfc62260190
Donggyu <a29661498@gmail.com>
2022-03-26 00:57:53 +0900

    feat: 비숍 움직임 전략 구현
    비숍의 움직임에 대한 전략 클래스를 구현하였습니다.

--- src/main/java/chess/domain/strategy/BishopMoveStrategy.java
+++ src/main/java/chess/domain/strategy/BishopMoveStrategy.java
@@ -3,13 +3,11 @@ package chess.domain.strategy;
 import chess.domain.ChessBoard;
 import chess.domain.position.Position;
 
-public class BishopMoveStrategy implements MoveStrategy {
+public final class BishopMoveStrategy extends CommonMovingStrategy {
 
     @Override
     public void isMovable(Position source, Position target, ChessBoard chessBoard) {
-        if (source.isSameFile(target) && source.isSameRank(target)) {
-            throw new IllegalStateException("제자리에 머무를 수 없습니다.");
-        }
+        checkCommonCondition(source, target, chessBoard);
 
         if (!source.isDiagonal(target)) {
             throw new IllegalStateException("대각선이 아닙니다.");
