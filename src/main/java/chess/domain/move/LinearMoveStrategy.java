2ded74618ab9bcbcd8aa2bae4d4f968b5dd58699
jayjaehunchoi <wogns0108@naver.com>
2022-03-25 16:01:00 +0900

    style : 긴 매개변수 스타일 변경

--- src/main/java/chess/domain/move/LinearMoveStrategy.java
+++ src/main/java/chess/domain/move/LinearMoveStrategy.java
@@ -9,26 +9,32 @@ public abstract class LinearMoveStrategy extends FirstRowMoveStrategy {
     private static final int ADD_START_UNIT = 1;
     private static final int REVERSE_DIRECTION = -1;
 
-    protected boolean isPieceExistWhenHorizon(final Board board, final Position smallerPosition, final Distance distance) {
+    protected boolean isPieceExistWhenHorizon(final Board board,
+                                              final Position smallerPosition,
+                                              final Distance distance) {
         return IntStream.range(ADD_START_UNIT, Math.abs(distance.getHorizon()))
                 .mapToObj(moveUnit -> smallerPosition.move(moveUnit, NOT_MOVE))
                 .anyMatch(position -> isPieceExist(board, position));
     }
 
-    protected boolean isPieceExistWhenVertical(final Board board, final Position smallerPosition, final Distance distance) {
+    protected boolean isPieceExistWhenVertical(final Board board,
+                                               final Position smallerPosition,
+                                               final Distance distance) {
         return IntStream.range(ADD_START_UNIT, Math.abs(distance.getVertical()))
                 .mapToObj(moveUnit -> smallerPosition.move(NOT_MOVE, moveUnit * REVERSE_DIRECTION))
                 .anyMatch(position -> isPieceExist(board, position));
     }
 
-    protected boolean isPieceExistWhenPositiveDiagonal(final Board board, final Position smallerPosition,
+    protected boolean isPieceExistWhenPositiveDiagonal(final Board board,
+                                                       final Position smallerPosition,
                                                        final Distance distance) {
         return IntStream.range(ADD_START_UNIT, Math.abs(distance.getVertical()))
                 .mapToObj(moveUnit -> smallerPosition.move(moveUnit * REVERSE_DIRECTION, moveUnit * REVERSE_DIRECTION))
                 .anyMatch(position -> isPieceExist(board, position));
     }
 
-    protected boolean isPieceExistWhenNegativeDiagonal(final Board board, final Position smallerPosition,
+    protected boolean isPieceExistWhenNegativeDiagonal(final Board board,
+                                                       final Position smallerPosition,
                                                        final Distance distance) {
         return IntStream.range(ADD_START_UNIT, Math.abs(distance.getVertical()))
                 .mapToObj(moveUnit -> smallerPosition.move(moveUnit, moveUnit * REVERSE_DIRECTION))
