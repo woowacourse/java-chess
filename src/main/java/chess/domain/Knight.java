e1e6a7c00efcb4ad08060ec566297d598d430cf8
beomWhale <cndqjacndqja@gmail.com>
2022-03-25 16:58:33 +0900

    feat: Knight 이동 예외 검증 기능 구현

--- src/main/java/chess/domain/Knight.java
+++ src/main/java/chess/domain/Knight.java
@@ -5,4 +5,14 @@ public class Knight extends Piece {
     public Knight(Team team, Position position) {
         super(team, Knight.class.getSimpleName(), position);
     }
+
+    public void validateIsPossible(Position destination) {
+        for (Direction direction : Direction.knightDirection()) {
+            if (position.getRow().getDifference(destination.getRow()) == direction.getYDegree()
+            && position.getCol().getDifference(destination.getCol()) == direction.getXDegree()) {
+                return;
+            }
+        }
+        throw new IllegalArgumentException("해당 위치로 말이 움직일 수 없습니다.");
+    }
 }
