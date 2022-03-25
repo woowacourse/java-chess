fbed170a26815871acc0743b108be4e26f8fc870
hyeonic <dev.hyeonic@gmail.com>
2022-03-25 19:59:32 +0900

    test: BoardFixtures 데이터 추가

--- src/test/java/chess/domain/BoardFixtures.java
+++ src/test/java/chess/domain/BoardFixtures.java
@@ -1,28 +1,17 @@
 package chess.domain;
 
-import static java.util.stream.Collectors.toList;
-
-import chess.domain.piece.EmptyPiece;
-import chess.domain.piece.Piece;
-import chess.domain.piece.Rook;
-import java.util.List;
-import java.util.stream.IntStream;
+import chess.domain.generator.EmptyBoardGenerator;
 
 public class BoardFixtures {
 
-    public static List<List<Piece>> generateOnlyRookBoard() {
-        List<List<Piece>> board = IntStream.rangeClosed(0, 7)
-                .mapToObj(ignored -> generatePieces())
-                .collect(toList());
-
-        board.get(7).set(0, new Rook(Color.WHITE));
-
-        return board;
+    public static ChessBoard generateEmptyChessBoard() {
+        return new ChessBoard(new EmptyBoardGenerator());
     }
 
-    private static List<Piece> generatePieces() {
-        return IntStream.rangeClosed(0, 7)
-                .mapToObj(ignored -> new EmptyPiece())
-                .collect(toList());
+    public static ChessBoard generateInitChessBoard() {
+        ChessBoard chessBoard = new ChessBoard(new EmptyBoardGenerator());
+        chessBoard.init();
+
+        return chessBoard;
     }
 }
