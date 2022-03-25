f0271cbba971befe438a7ab9869c6edf40009e59
Seongwon97 <osw970325@naver.com>
2022-03-25 16:35:03 +0900

    feat(Bishop): 비숍 이동로직 작성

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -1,5 +1,9 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import chess.domain.board.Point;
+import chess.domain.piece.move.StraightDirection;
+
 public class Bishop extends Piece {
 
     public Bishop(Color color) {
@@ -7,7 +11,48 @@ public class Bishop extends Piece {
     }
 
     @Override
+    public void move(Board board, Point from, Point to) {
+        StraightDirection direction = findDirection(from, to);
+
+        Point next = from.next(direction);
+        while (!next.equals(to)) {
+            validateEmpty(board, next);
+            next = next.next(direction);
+        }
+    }
+
+    private StraightDirection findDirection(Point from, Point to) {
+        StraightDirection direction = StraightDirection.find(from, to);
+        validateDirection(direction);
+        validateDiagonal(from, to);
+        return direction;
+    }
+
+    private void validateDirection(StraightDirection direction) {
+        if (direction.isCross()) {
+            throw new IllegalArgumentException("[ERROR] 비숌은 대각선으로만 이동할 수 있습니다.");
+        }
+    }
+
+    private void validateDiagonal(Point from, Point to) {
+        if (Math.abs(from.subtractVertical(to)) != Math.abs(from.subtractHorizontal(to))) {
+            throw new IllegalArgumentException("[ERROR] 비숌은 대각선으로만 이동할 수 있습니다.");
+        }
+    }
+
+    private void validateEmpty(Board board, Point point) {
+        if (!board.isEmpty(point)) {
+            throw new IllegalArgumentException("[ERROR] 이동 과정 중에 장애물이 있습니다.");
+        }
+    }
+
+    @Override
     public PieceType getType() {
         return PieceType.BISHOP;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 }
--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -1,9 +1,14 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import chess.domain.board.BoardFixtures;
+import chess.domain.board.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class BishopTest {
 
@@ -13,5 +18,43 @@ public class BishopTest {
         Piece piece = new Bishop(Color.WHITE);
 
         assertThat(piece).isNotNull();
+    }
+
+    @Test
+    @DisplayName("대각선으로 이동할 수 있다.")
+    void moveWithDiagonal() {
+        Point from = Point.of("c1");
+        Point to = Point.of("g5");
+        Piece piece = new Bishop(Color.WHITE);
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatCode(() -> piece.move(board, from, to))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("장애물이 있을 경우 이동할 수 없다.")
+    void notMovableWithObstacle() {
+        Point from = Point.of("c1");
+        Point to = Point.of("g5");
+        Piece piece = new Bishop(Color.WHITE);
+        Board board = BoardFixtures.create(Map.of(
+                Point.of("d2"), new Pawn(Color.WHITE)
+        ));
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> piece.move(board, from, to));
+    }
+
+    @Test
+    @DisplayName("상하 좌우로 이동할 수 없다.")
+    void notMovableWithCross() {
+        Point from = Point.of("c1");
+        Point to = Point.of("c4");
+        Piece piece = new Bishop(Color.WHITE);
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> piece.move(board, from, to));
     }
 }
