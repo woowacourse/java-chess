973471ebad51fa7de74cec6aa9f1cbb8920aa4c9
Seongwon97 <osw970325@naver.com>
2022-03-25 16:25:01 +0900

    feat(Knight): 나이트 이동로직 작성

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,5 +1,11 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import chess.domain.board.Point;
+import chess.domain.piece.move.knight.KnightDirection;
+
+import java.util.List;
+
 public class Knight extends Piece {
 
     public Knight(Color color) {
@@ -9,5 +15,18 @@ public class Knight extends Piece {
     @Override
     public PieceType getType() {
         return PieceType.KNIGHT;
+    }
+
+    @Override
+    public void move(Board ignored, Point from, Point to) {
+        List<Point> candidates = KnightDirection.createNextPointCandidates(from);
+        if (!candidates.contains(to)) {
+            throw new IllegalArgumentException("[ERROR] 이동할 수 없는 경로입니다.");
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 }
--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -1,9 +1,14 @@
 package chess.domain.piece;
 
+import chess.domain.board.Board;
+import chess.domain.board.BoardFixtures;
+import chess.domain.board.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class KnightTest {
 
@@ -14,4 +19,55 @@ public class KnightTest {
 
         assertThat(piece).isNotNull();
     }
+
+    @Test
+    @DisplayName("나이트를 움직인다.")
+    void move() {
+        Piece piece = new Knight(Color.WHITE);
+        Point from = Point.of("b1");
+        Point to = Point.of("c3");
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatCode(() -> piece.move(board, from, to))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("나이트가 잘못된 위치로 이동할 수 없다.")
+    void moveWrongPoint() {
+        Piece piece = new Knight(Color.WHITE);
+        Point from = Point.of("b1");
+        Point to = Point.of("c4");
+        Board board = BoardFixtures.EMPTY_BOARD;
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> piece.move(board, from, to));
+    }
+
+    @Test
+    @DisplayName("도착지에 적이 있어도 이동할 수 있다.")
+    void moveWithEnemy() {
+        Piece piece = new Knight(Color.WHITE);
+        Point from = Point.of("b1");
+        Point to = Point.of("c3");
+
+        Board board = BoardFixtures.create(Map.of(Point.of("c3"), new Pawn(Color.BLACK)));
+
+        assertThatCode(() -> piece.move(board, from, to))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("나이트는 장애물을 뛰어넘을 수 있다.")
+    void moveWithObstacle() {
+        Piece piece = new Knight(Color.WHITE);
+        Point from = Point.of("b1");
+        Point to = Point.of("c3");
+
+        Board board = BoardFixtures.create(Map.of(Point.of("b2"), new Pawn(Color.WHITE)));
+
+        assertThatCode(() -> piece.move(board, from, to))
+                .doesNotThrowAnyException();
+    }
+
 }
