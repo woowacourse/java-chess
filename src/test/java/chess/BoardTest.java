e42da2d3dcdacce7f341b709f09f48f122876a07
swcho <progress0407@gmail.com>
2022-03-25 16:46:05 +0900

    test(BoardTest): 테스트 코드 작성 (기물이 다른 기물의 이동경로를 막고 있다면 이동이 불가)

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,22 +1,25 @@
 package chess;
 
-import static chess.Board.*;
-import static org.assertj.core.api.Assertions.*;
+import static chess.Board.SOURCE_POSITION_SHOULD_HAVE_PIECE_MESSAGE;
+import static chess.File.B;
+import static chess.File.C;
+import static chess.Rank.FOUR;
+import static chess.Rank.ONE;
+import static chess.Rank.THREE;
+import static chess.Rank.TWO;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import chess.piece.Queen;
+import chess.piece.Pawn;
+import chess.piece.Piece;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import chess.piece.Pawn;
-import chess.piece.Piece;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -82,7 +85,7 @@ public class BoardTest {
     @Test
     @DisplayName("체스 말이 없는 곳에서 이동 시키면 예외를 던진다.")
     void move_exception() {
-        assertThatThrownBy(() -> board.move(new Position(Rank.THREE, File.A), new Position(Rank.THREE, File.B)))
+        assertThatThrownBy(() -> board.move(new Position(Rank.THREE, File.A), new Position(Rank.THREE, B)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(SOURCE_POSITION_SHOULD_HAVE_PIECE_MESSAGE);
     }
@@ -103,17 +106,26 @@ public class BoardTest {
     @DisplayName("퀸은 경로에 다른 기물 있으면 이동할 수 없다")
     void isBlocked(Rank rank, File file) {
         assertThatThrownBy(() ->
-                board.move(new Position(Rank.ONE, File.C), new Position(rank, file))
+                board.move(new Position(ONE, File.C), new Position(rank, file))
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
 
     @ParameterizedTest
-    @CsvSource(value = {"TWO:D", "THREE:C"}, delimiter = ':')
+    @CsvSource(value = {"THREE:C", "THREE:A"}, delimiter = ':')
     @DisplayName("나이트는 경로에 다른 기물 있으면 이동할 수 있다")
     void isNonBlocked(Rank rank, File file) {
         assertDoesNotThrow(() ->
-                board.move(new Position(Rank.ONE, File.B), new Position(rank, file))
+                board.move(new Position(ONE, B), new Position(rank, file))
         );
+    }
+
+    @DisplayName("기물이 다른 기물의 이동경로를 막고 있다면 이동이 불가하다")
+    @Test
+    void isBlockedAfterNightMoved() {
+        board.move(new Position(ONE, B), new Position(THREE, C));
+        assertThatThrownBy(() ->
+                board.move(new Position(TWO, C), new Position(FOUR, C))
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
