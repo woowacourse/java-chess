0a7fb957388d5964ce7631863745b62d4e8be778
beomWhale <cndqjacndqja@gmail.com>
2022-03-25 16:58:48 +0900

    feat: King 이동 예외 검증 기능 구현

--- src/main/java/chess/domain/King.java
+++ src/main/java/chess/domain/King.java
@@ -5,4 +5,12 @@ public class King extends Piece {
     public King(Team team, Position position) {
         super(team, King.class.getSimpleName(), position);
     }
+
+    public void validateIsPossible(Position destination) {
+        if (Math.abs(position.getCol().getDifference(destination.getCol())) <= 1
+                && Math.abs(position.getRow().getDifference(destination.getRow())) <= 1) {
+            return;
+        }
+        throw new IllegalArgumentException("해당 위치로 말이 움직일 수 없습니다.");
+    }
 }
