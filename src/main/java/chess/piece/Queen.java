f6a448fcc9e6480e936b42e1c11046d015c20588
yhh1056 <yhh1056@naver.com>
2022-03-27 02:04:23 +0900

    feat: 퀸 이동 구현

--- src/main/java/chess/piece/Queen.java
+++ src/main/java/chess/piece/Queen.java
@@ -1,9 +1,32 @@
 package chess.piece;
 
+import chess.Direction;
+import chess.MoveCommand;
+import chess.Position;
+
 public class Queen extends AbstractPiece {
 
     public Queen(final Color color) {
         super(Name.QUEEN, color);
     }
 
+    @Override
+    public boolean canMove(final MoveCommand command) {
+        final Position from = command.getFrom();
+        final Position to = command.getTo();
+
+        return Direction.getRookDirections().stream()
+                .anyMatch(direction -> canRookMove(from, to));
+    }
+
+    private boolean canRookMove(final Position from, final Position to) {
+        final int columnDistance = to.getColumnDistance(from);
+        final int rowDistance = to.getRowDistance(from);
+
+        if (columnDistance == 0 || rowDistance == 0) {
+            return true;
+        }
+
+        return Math.abs(columnDistance) == Math.abs(rowDistance);
+    }
 }
