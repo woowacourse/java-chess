f0695f6208dea1492849fb6c514708477dbf2c68
yhh1056 <yhh1056@naver.com>
2022-03-27 01:59:10 +0900

    feat: 비숍 이동 구현

--- src/main/java/chess/piece/Bishop.java
+++ src/main/java/chess/piece/Bishop.java
@@ -1,9 +1,29 @@
 package chess.piece;
 
+import chess.Direction;
+import chess.MoveCommand;
+import chess.Position;
+
 public class Bishop extends AbstractPiece {
 
     public Bishop(final Color color) {
         super(Name.BISHOP, color);
+    }
+
+    @Override
+    public boolean canMove(final MoveCommand command) {
+        final Position from = command.getFrom();
+        final Position to = command.getTo();
+
+        return Direction.getRookDirections().stream()
+                .anyMatch(direction -> canRookMove(from, to));
+    }
+
+    private boolean canRookMove(final Position from, final Position to) {
+        final int columnDistance = to.getColumnDistance(from);
+        final int rowDistance = to.getRowDistance(from);
+
+        return Math.abs(columnDistance) == Math.abs(rowDistance);
     }
 
 }
