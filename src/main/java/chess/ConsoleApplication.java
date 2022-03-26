b19697bf55244632af89bd5aae562c88ae8f6908
hyeonic <dev.hyeonic@gmail.com>
2022-03-26 20:06:02 +0900

    feat: ChessController 추가

--- src/main/java/chess/ConsoleApplication.java
+++ src/main/java/chess/ConsoleApplication.java
@@ -1,75 +1,11 @@
 package chess;
 
-import chess.domain.ChessBoard;
-import chess.domain.Color;
-import chess.domain.generator.InitBoardGenerator;
-import chess.dto.CommandRequest;
-import chess.view.Command;
-import chess.view.InputView;
-import chess.view.OutputView;
+import chess.controller.ChessController;
 
 public class ConsoleApplication {
 
     public static void main(String[] args) {
-        ChessBoard chessBoard = new ChessBoard(new InitBoardGenerator());
-
-        OutputView.printStartMessage();
-        startChess(chessBoard);
-
-        repeatPlayChess(chessBoard);
-    }
-
-    private static void startChess(ChessBoard chessBoard) {
-        CommandRequest commandRequest = InputView.inputCommand();
-        Command command = commandRequest.getCommand();
-
-        if (command.isStart()) {
-            OutputView.printChessBoard(chessBoard.getBoard());
-            return;
-        }
-
-        OutputView.printErrorMessage("게임이 시작되지 않았습니다.");
-        startChess(chessBoard);
-    }
-
-    private static void repeatPlayChess(ChessBoard chessBoard) {
-        try {
-            playChess(chessBoard);
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            repeatPlayChess(chessBoard);
-        }
-    }
-
-    private static void playChess(ChessBoard chessBoard) {
-        CommandRequest commandRequest = InputView.inputCommand();
-        Command command = commandRequest.getCommand();
-
-        if (command.isStart()) {
-            OutputView.printErrorMessage("게임이 이미 시작되었습니다.");
-            playChess(chessBoard);
-        }
-
-        if (command.isEnd()) {
-            return;
-        }
-
-        if (command.isStatus()) {
-            double whiteScore = chessBoard.calculateScore(Color.WHITE);
-            double blackScore = chessBoard.calculateScore(Color.BLACK);
-            OutputView.printStatusMessage(whiteScore, blackScore);
-            playChess(chessBoard);
-        }
-
-        chessBoard.move(commandRequest.getSource(), commandRequest.getTarget());
-        OutputView.printChessBoard(chessBoard.getBoard());
-
-        if (chessBoard.isFinished()) {
-            Color winner = chessBoard.getTurn();
-            System.out.println(winner + "가 승리하였습니다.");
-            return;
-        }
-
-        playChess(chessBoard);
+        ChessController chessController = new ChessController();
+        chessController.run();
     }
 }
---
+++ src/main/java/chess/controller/ChessController.java
@@ -0,0 +1,7 @@
+package chess.controller;
+
+public class ChessController {
+
+    public void run() {
+    }
+}
