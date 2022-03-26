fde4dc89fa32dba489a858e16655ee336979383c
SuyeonChoi <tndus970120@gmail.com>
2022-03-26 20:07:22 +0900

    refactor: 폰 isMovable 재정의

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -11,64 +11,17 @@ public class Pawn extends ChessPiece {
     private static final int WHITE_INITIAL_ROW_POSITION = 7;
     private static final int BLACK_INITIAL_ROW_POSITION = 2;
     private static final String NAME = "PAWN";
-    private static final String UNEXPECTED_MOVEMENT_EXCEPTION = "[ERROR] 폰이 이동할 수 없는 위치입니다.";
 
     public Pawn(Team team, ChessBoardPosition position) {
         super(NAME, team, position);
-    }
-
-    private void moveWhite(ChessBoardPosition targetPosition) {
-        if (isWhiteInitialPosition()) {
-            whiteInitialMove(targetPosition);
-            return;
-        }
-        forwardWhite(targetPosition);
     }
 
     private boolean isWhiteInitialPosition() {
         return position.isSameRow(WHITE_INITIAL_ROW_POSITION);
     }
 
-    private void whiteInitialMove(ChessBoardPosition targetPosition) {
-        int rowDistance = calculateRowDistance(position.getRow(), targetPosition.getRow());
-        if (rowDistance != DEFAULT_DISTANCE && rowDistance != DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
-            throw new IllegalArgumentException(UNEXPECTED_MOVEMENT_EXCEPTION);
-        }
-        position = targetPosition;
-    }
-
-    private void forwardWhite(ChessBoardPosition targetPosition) {
-        if (calculateRowDistance(position.getRow(), targetPosition.getRow()) != DEFAULT_DISTANCE) {
-            throw new IllegalArgumentException(UNEXPECTED_MOVEMENT_EXCEPTION);
-        }
-        position = targetPosition;
-    }
-
-    private void moveBlack(ChessBoardPosition targetPosition) {
-        if (isBlackInitialPosition()) {
-            blackInitialMove(targetPosition);
-            return;
-        }
-        forwardBlack(targetPosition);
-    }
-
     private boolean isBlackInitialPosition() {
         return position.isSameRow(BLACK_INITIAL_ROW_POSITION);
-    }
-
-    private void blackInitialMove(ChessBoardPosition targetPosition) {
-        int rowDistance = calculateRowDistance(targetPosition.getRow(), position.getRow());
-        if (rowDistance != DEFAULT_DISTANCE && rowDistance != DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
-            throw new IllegalArgumentException(UNEXPECTED_MOVEMENT_EXCEPTION);
-        }
-        position = targetPosition;
-    }
-
-    private void forwardBlack(ChessBoardPosition targetPosition) {
-        if (calculateRowDistance(targetPosition.getRow(), position.getRow()) != DEFAULT_DISTANCE) {
-            throw new IllegalArgumentException(UNEXPECTED_MOVEMENT_EXCEPTION);
-        }
-        position = targetPosition;
     }
 
     private int calculateRowDistance(int highRow, int lowRow) {
@@ -79,70 +32,84 @@ public class Pawn extends ChessPiece {
         return position.equals(anotherPosition);
     }
 
-    public void moveDiagonal(ChessBoardPosition targetPosition) {
-        if (team.isWhite()) {
-            moveDiagonalWhite(targetPosition);
-            return;
-        }
-        moveDiagonalBlack(targetPosition);
-    }
-
-    private void moveDiagonalWhite(ChessBoardPosition targetPosition) {
-        int rowDistance = calculateRowDistance(position.getRow(), targetPosition.getRow());
-        int columnDistance = calculateColumnDistance(position.getColumn(), targetPosition.getColumn());
-        if (rowDistance != DEFAULT_DISTANCE || columnDistance != DEFAULT_DISTANCE) {
-            throw new IllegalArgumentException(UNEXPECTED_MOVEMENT_EXCEPTION);
-        }
-        position = targetPosition;
-    }
-
-    private void moveDiagonalBlack(ChessBoardPosition targetPosition) {
-        int rowDistance = calculateRowDistance(targetPosition.getRow(), position.getRow());
-        int columnDistance = calculateColumnDistance(targetPosition.getColumn(), position.getColumn());
-        if (rowDistance != DEFAULT_DISTANCE || columnDistance != DEFAULT_DISTANCE) {
-            throw new IllegalArgumentException(UNEXPECTED_MOVEMENT_EXCEPTION);
-        }
-        position = targetPosition;
-    }
-
     private int calculateColumnDistance(char highColumn, char lowColumn) {
         return Math.abs(highColumn - lowColumn);
     }
 
     @Override
     public void move(ChessBoardPosition targetPosition) {
-        if (team.isWhite()) {
-            moveWhite(targetPosition);
-            return;
-        }
-        moveBlack(targetPosition);
+        position = targetPosition;
     }
 
     @Override
     public boolean isMovable(ChessBoardPosition targetPosition, ChessMen whiteChessMen, ChessMen blackChessMen) {
-        if (!isBlackInitialPosition() && !isWhiteInitialPosition()) {
-            return true;
+        if (isDiagonalMove(targetPosition)) {
+            return enemyExistsInTargetPosition(targetPosition, getEnemy(whiteChessMen, blackChessMen));
         }
+
         if (team.isWhite()) {
-            return isWhiteMovable(targetPosition, whiteChessMen, blackChessMen);
+            return isMovableWhite(targetPosition, whiteChessMen, blackChessMen);
         }
-        return isBlackMovable(targetPosition, whiteChessMen, blackChessMen);
+
+        return isMovableBlack(targetPosition, whiteChessMen, blackChessMen);
     }
 
-    private boolean isWhiteMovable(ChessBoardPosition targetPosition, ChessMen whiteChessMen, ChessMen blackChessMen) {
-        int rowDistance = calculateRowDistance(position.getRow(), targetPosition.getRow());
-        if (rowDistance != DEFAULT_DISTANCE && rowDistance != DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
-            return false;
+    private boolean isDiagonalMove(ChessBoardPosition targetPosition) {
+        if (team.isWhite()) {
+            return isDiagonalMoveWhite(targetPosition);
         }
-        return rowDistance != 2 || isUnobstructed(whiteChessMen, blackChessMen, -DEFAULT_DISTANCE);
+        return isDiagonalMoveBlack(targetPosition);
     }
 
-    private boolean isBlackMovable(ChessBoardPosition targetPosition, ChessMen whiteChessMen, ChessMen blackChessMen) {
+    private boolean isDiagonalMoveBlack(ChessBoardPosition targetPosition) {
         int rowDistance = calculateRowDistance(targetPosition.getRow(), position.getRow());
-        if (rowDistance != DEFAULT_DISTANCE && rowDistance != DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
-            return false;
+        int columnDistance = calculateColumnDistance(targetPosition.getColumn(), position.getColumn());
+        return rowDistance == DEFAULT_DISTANCE && columnDistance == DEFAULT_DISTANCE;
+    }
+
+    private boolean isDiagonalMoveWhite(ChessBoardPosition targetPosition) {
+        int rowDistance = calculateRowDistance(position.getRow(), targetPosition.getRow());
+        int columnDistance = calculateColumnDistance(position.getColumn(), targetPosition.getColumn());
+        return rowDistance == DEFAULT_DISTANCE && columnDistance == DEFAULT_DISTANCE;
+    }
+
+    private ChessMen getEnemy(ChessMen whiteChessMen, ChessMen blackChessMen) {
+        if (team.isWhite()) {
+            return blackChessMen;
         }
-        return rowDistance != 2 || isUnobstructed(whiteChessMen, blackChessMen, DEFAULT_DISTANCE);
+        return whiteChessMen;
+    }
+
+    private boolean isMovableBlack(ChessBoardPosition targetPosition, ChessMen whiteChessMen, ChessMen blackChessMen) {
+        if (isBlackInitialPosition()) {
+            return isMovableBlackInitialPosition(targetPosition, whiteChessMen, blackChessMen);
+        }
+        return calculateRowDistance(targetPosition.getRow(), position.getRow()) == DEFAULT_DISTANCE;
+    }
+
+    private boolean isMovableBlackInitialPosition(ChessBoardPosition targetPosition, ChessMen whiteChessMen,
+                                                  ChessMen blackChessMen) {
+        int rowDistance = calculateRowDistance(targetPosition.getRow(), position.getRow());
+        if (rowDistance == DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
+            return isUnobstructed(whiteChessMen, blackChessMen, DEFAULT_DISTANCE);
+        }
+        return rowDistance == DEFAULT_DISTANCE;
+    }
+
+    private boolean isMovableWhite(ChessBoardPosition targetPosition, ChessMen whiteChessMen, ChessMen blackChessMen) {
+        if (isWhiteInitialPosition()) {
+            return isMovableWhiteInitialPosition(targetPosition, whiteChessMen, blackChessMen);
+        }
+        return calculateRowDistance(position.getRow(), targetPosition.getRow()) == DEFAULT_DISTANCE;
+    }
+
+    private boolean isMovableWhiteInitialPosition(ChessBoardPosition targetPosition, ChessMen whiteChessMen,
+                                                  ChessMen blackChessMen) {
+        int rowDistance = calculateRowDistance(position.getRow(), targetPosition.getRow());
+        if (rowDistance == DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
+            return isUnobstructed(whiteChessMen, blackChessMen, -DEFAULT_DISTANCE);
+        }
+        return rowDistance == DEFAULT_DISTANCE;
     }
 
     private boolean isUnobstructed(ChessMen whiteChessMen, ChessMen blackChessMen, int forwardDirection) {
--- src/test/java/chess/domain/piece/PawnTest.java
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -1,10 +1,13 @@
 package chess.domain.piece;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import chess.domain.ChessBoardPosition;
+import chess.domain.ChessMen;
 import chess.domain.Team;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,10 +20,11 @@ class PawnTest {
     @ParameterizedTest(name = "{index}: {3}")
     @MethodSource("moveParameters")
     @DisplayName("폰은 한 칸 또는 두 칸 전진한다.")
-    void initialMoveTest(Team team, ChessBoardPosition initialPosition, ChessBoardPosition nextPosition) {
+    void movableTest(Team team, ChessBoardPosition initialPosition, ChessBoardPosition nextPosition) {
         Pawn pawn = new Pawn(team, initialPosition);
-        pawn.move(nextPosition);
-        assertThat(pawn.isSamePosition(nextPosition)).isTrue();
+        List<ChessPiece> empty = new ArrayList<>();
+        boolean result = pawn.isMovable(nextPosition, new ChessMen(empty), new ChessMen(empty));
+        assertTrue(result);
     }
 
     private static Stream<Arguments> moveParameters() {
@@ -44,8 +48,9 @@ class PawnTest {
         ChessBoardPosition initialPosition = new ChessBoardPosition('a', 2);
         Pawn pawn = new Pawn(Team.BLACK, initialPosition);
         ChessBoardPosition nextPosition = new ChessBoardPosition('a', 5);
-
-        assertThatThrownBy(() -> pawn.move(nextPosition)).isInstanceOf(IllegalArgumentException.class);
+        List<ChessPiece> empty = new ArrayList<>();
+        boolean result = pawn.isMovable(nextPosition, new ChessMen(empty), new ChessMen(empty));
+        assertFalse(result);
     }
 
 
@@ -55,17 +60,34 @@ class PawnTest {
         ChessBoardPosition initialPosition = new ChessBoardPosition('a', 3);
         Pawn pawn = new Pawn(Team.BLACK, initialPosition);
         ChessBoardPosition nextPosition = new ChessBoardPosition('a', 5);
-
-        assertThatThrownBy(() -> pawn.move(nextPosition)).isInstanceOf(IllegalArgumentException.class);
+        List<ChessPiece> empty = new ArrayList<>();
+        boolean result = pawn.isMovable(nextPosition, new ChessMen(empty), new ChessMen(empty));
+        assertFalse(result);
     }
 
     @ParameterizedTest(name = "{index}: {3}")
     @MethodSource("diagonalParameters")
-    @DisplayName("폰은 한 칸 또는 두 칸 전진한다.")
+    @DisplayName("폰은 적이 대각선 방향에 존재하는 경우 이동 가능하다.")
     void diagonalMoveTest(Team team, ChessBoardPosition initialPosition, ChessBoardPosition nextPosition) {
         Pawn pawn = new Pawn(team, initialPosition);
-        pawn.moveDiagonal(nextPosition);
-        assertThat(pawn.isSamePosition(nextPosition)).isTrue();
+        ChessMen whiteChessMen = getWhiteChessMen();
+        ChessMen blackChessMen = getBlackChessMen();
+        pawn.isMovable(nextPosition, whiteChessMen, blackChessMen);
+        assertTrue(pawn.isMovable(nextPosition, whiteChessMen, blackChessMen));
+    }
+
+    private ChessMen getWhiteChessMen() {
+        List<ChessPiece> chessPieces = new ArrayList<>();
+        chessPieces.add(new Pawn(Team.WHITE, ChessBoardPosition.of("a3")));
+        chessPieces.add(new Pawn(Team.WHITE, ChessBoardPosition.of("c3")));
+        return new ChessMen(chessPieces);
+    }
+
+    private ChessMen getBlackChessMen() {
+        List<ChessPiece> chessPieces = new ArrayList<>();
+        chessPieces.add(new Pawn(Team.BLACK, ChessBoardPosition.of("a6")));
+        chessPieces.add(new Pawn(Team.BLACK, ChessBoardPosition.of("c6")));
+        return new ChessMen(chessPieces);
     }
 
     private static Stream<Arguments> diagonalParameters() {
