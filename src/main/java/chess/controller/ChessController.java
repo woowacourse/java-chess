93f36f01216c084e373970b44181c46f450b3377
= <rerub0831@gmail.com>
2022-03-26 17:03:41 +0900

    feat: move 커맨드로 체스 말을 이동하는 기능 구현

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -2,6 +2,9 @@ package chess.controller;
 
 import chess.domain.ChessGame;
 import chess.domain.board.Board;
+import chess.domain.board.File;
+import chess.domain.board.Position;
+import chess.domain.board.Rank;
 import chess.view.InputView;
 import chess.view.OutputView;
 
@@ -13,12 +16,16 @@ public class ChessController {
     }
 
     private void runByCommend() {
+        ChessGame chessGame = new ChessGame();
         while (true) {
             String commend = InputView.inputCommend();
             if (commend.equals("end")) {
                 break;
             }
-            ChessGame chessGame = new ChessGame();
+            if (commend.contains("move")) {
+                String[] positions = commend.substring(5).split(" ");
+                chessGame.play(Position.from(positions[0]), Position.from(positions[1]));
+            }
             OutputView.printChessBoard(chessGame.getBoard());
         }
     }
