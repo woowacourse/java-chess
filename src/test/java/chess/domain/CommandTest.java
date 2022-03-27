362429ec83566401438c16e4f91184e22c965ee3
RIANAEH <ririhan217@gmail.com>
2022-03-27 15:46:39 +0900

    test : Command status 명령어 테스트 추가

--- src/test/java/chess/domain/CommandTest.java
+++ src/test/java/chess/domain/CommandTest.java
@@ -27,6 +27,12 @@ public class CommandTest {
     }
 
     @Test
+    @DisplayName("status Command가 존재하는지 확인한다.")
+    void status() {
+        assertThat(Command.of("status")).isEqualTo(Command.STATUS);
+    }
+
+    @Test
     @DisplayName("존재하지 않는 Command를 입력했을경우 예외를 발생시킨다.")
     void checkNonCommandException() {
         assertThatThrownBy(() -> Command.of("go"))
