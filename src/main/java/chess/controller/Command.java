b5a75586287c9ae3bdd5cbebb39cee95baffee32
= <rerub0831@gmail.com>
2022-03-27 20:41:18 +0900

    refactor(Command): move 식별 로직 개선

--- src/main/java/chess/controller/Command.java
+++ src/main/java/chess/controller/Command.java
@@ -6,32 +6,20 @@ import java.util.regex.Pattern;
 public enum Command {
     START("start"),
     END("end"),
-    MOVE("move"),
+    MOVE("move [a-h]\\d [a-h]\\d"),
     STATUS("status");
 
     private final String value;
-    private static final Pattern MOVE_PATTERN = Pattern.compile("move [a-h]\\d [a-h]\\d");
-
 
     Command(String value) {
         this.value = value;
     }
 
     public static Command from(String commandText) {
-        final String[] splitCommand = commandText.split(" ");
-        Command result =  Arrays.stream(values())
-                .filter(command -> command.value.equals(splitCommand[0]))
+        return Arrays.stream(values())
+                .filter(command -> Pattern.matches(command.value, commandText))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 명령입니다."));
-        if (result == MOVE) {
-            checkMove(commandText);
-        }
-        return result;
-    }
-
-    private static void checkMove(String commendText) {
-        if (!MOVE_PATTERN.matcher(commendText).matches()) {
-            throw new IllegalArgumentException("move 포맷에 맞게 작성해주세요.");
-        }
     }
 }
+
