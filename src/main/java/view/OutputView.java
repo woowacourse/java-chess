f63e2013526459da0188a1ba43ef333cb1cfe16b
HyeonbinSa <shb1833@gmail.com>
2022-03-27 13:37:47 +0900

    refactor(OutputView): 문자열 상수화

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,6 +11,10 @@ import java.util.Collections;
 
 public class OutputView {
 
+    private static final String STATUS_MESSAGE = "%s 현재 점수 : %.1f (%s)" + System.lineSeparator();
+    private static final String KING_ATTACK_MESSAGE = "%s의 King이 공격당하였습니다." + System.lineSeparator();
+    private static final String GAME_EXIT_MESSAGE = "게임이 종료되었습니다.";
+
     public static void printBoard(final ChessBoard chessBoard) {
         Rank[] ranks = Rank.values();
         Arrays.sort(ranks, Collections.reverseOrder());
@@ -28,18 +32,17 @@ public class OutputView {
     }
 
     public static void printAttackKingMessage(final Player player) {
-        System.out.println(player + "의 King이 공격당하였습니다.");
+        System.out.printf(KING_ATTACK_MESSAGE, player);
         printExitMessage();
     }
 
     public static void printExitMessage() {
-        System.out.println("게임이 종료되었습니다.");
+        System.out.println(GAME_EXIT_MESSAGE);
     }
 
     public static void printStatus(final StatusDTO statusDTO) {
-        System.out.println(
-            statusDTO.getPlayer() + " 현재 점수 : " + statusDTO.getScore() + "(" + statusDTO.getResult()
-                + ")");
+        System.out.printf(STATUS_MESSAGE, statusDTO.getPlayer(), statusDTO.getScore(),
+            statusDTO.getResult());
     }
 
     public static void printErrorMessage(final String errorMessage) {
