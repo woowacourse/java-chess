485de2a1ffb9ccf9914cf9d43dbf861ab932f2ce
Ohzzi <clover7even@naver.com>
2022-03-27 17:48:16 +0900

    test: CachedPosition이 적용되지 않은 테스트에 적용
    BoardTest의 일부 테스트에 CachedPosition으로 대체 가능한 부분이 있어 해당 부분에 CachedPosition 사용하도록 변경

--- src/test/java/chess/domain/CachedPosition.java
+++ src/test/java/chess/domain/CachedPosition.java
@@ -4,6 +4,7 @@ import static chess.domain.board.Column.a;
 import static chess.domain.board.Column.b;
 import static chess.domain.board.Column.c;
 import static chess.domain.board.Row.FIRST;
+import static chess.domain.board.Row.FOURTH;
 import static chess.domain.board.Row.SECOND;
 import static chess.domain.board.Row.THIRD;
 
@@ -14,9 +15,9 @@ public class CachedPosition {
     public static final Position a1 = new Position(a, FIRST);
     public static final Position a2 = new Position(a, SECOND);
     public static final Position a3 = new Position(a, THIRD);
+    public static final Position a4 = new Position(a, FOURTH);
     public static final Position b1 = new Position(b, FIRST);
     public static final Position b2 = new Position(b, SECOND);
     public static final Position b3 = new Position(b, THIRD);
     public static final Position c3 = new Position(c, THIRD);
-
 }
