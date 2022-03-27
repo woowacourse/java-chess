c772d4caacf11a44fd02a587268103f0a1a45242
youngwooyoo <fbduddn97@naver.com>
2022-03-27 19:50:25 +0900

    test(BoardFixture): 초기화된 체스판 setting하도록 변경

--- src/test/java/chess/domain/BoardFixture.java
+++ src/test/java/chess/domain/BoardFixture.java
@@ -1,15 +1,9 @@
 package chess.domain;
 
-import chess.domain.piece.Piece;
-import chess.domain.postion.Position;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class BoardFixture {
 
     static Board setup() {
-        Map<Position, Piece> cells = new HashMap<>();
-       return new Board(cells);
+        Board board = new BoardInitializer().init();
+        return board;
     }
 }
