1eaddbeb2805111422a5e391c7ee9b0698ca591e
김찬 <gmelddl514@gmail.com>
2022-03-27 14:16:23 +0900

    feat : 이동 기능을 입력

---
+++ src/main/java/chess/function/Function.java
@@ -0,0 +1,23 @@
+package chess.function;
+
+import java.util.Arrays;
+
+public enum Function {
+
+	START("start"),
+	END("end"),
+	MOVE("move");
+
+	private final String value;
+
+	Function(final String value) {
+		this.value = value;
+	}
+
+	public static Function from(final String value) {
+		return Arrays.stream(Function.values())
+			.filter(function -> function.value.equals(value))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 기능입니다."));
+	}
+}
