1051d41bda6d52d139939c4ddf888713d760f46e
pup-paw <wldusdl0310@gmail.com>
2022-03-27 16:27:46 +0900

    fix: 폰이 대각선 외의 공격을 못하도록 수정

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -11,6 +11,8 @@ public final class Pawn extends Piece {
     private static final String WHITE_PAWN = "♙";
     private static final List<Direction> MOVABLE_DIRECTIONS = new ArrayList<>();
     private static final List<Direction> ATTACK_DIRECTIONS = List.of(new Direction(1, 1), new Direction(-1, 1));
+    private static final List<Direction> START_DIRECTIONS = List.of(new Direction(0, 2));
+    private static final String ERROR_MESSAGE_ATTACK_DIRECTION = "[ERROR] 5252! 폰은 대각선으로만 공격할 수 있다능~";
 
     static {
         MOVABLE_DIRECTIONS.add(new Direction(0, 1));
@@ -38,32 +40,43 @@ public final class Pawn extends Piece {
     }
 
     @Override
-    public boolean canMove(Direction direction, Piece target) {
-        if (color == Color.BLACK) {
-            direction = direction.flipAboutX();
-        }
-        checkSameTeam(target);
-        List<Direction> directions = new ArrayList<>(MOVABLE_DIRECTIONS);
-        checkCanAttack(direction, target, directions);
-        checkIsStart(directions);
-        return direction.hasSame(directions);
-    }
-
-    @Override
     public boolean isPawn() {
         return true;
     }
 
-    private void checkCanAttack(Direction direction, Piece target, List<Direction> directions) {
-        if (direction.hasSame(ATTACK_DIRECTIONS) && !target.isNone()) {
-            directions.addAll(ATTACK_DIRECTIONS);
+    @Override
+    public boolean canMove(Direction direction, Piece target) {
+        if (color == Color.BLACK) {
+            direction = direction.flipAboutX();
         }
+
+        if (checkCanAttack(direction, target) || checkIsStart(direction, target)
+                || checkCanMove(direction, target)) {
+            this.start = false;
+            return true;
+        }
+        return false;
     }
 
-    private void checkIsStart(List<Direction> directions) {
-        if (start) {
-            this.start = false;
-            directions.add(new Direction(0, 2));
+    private boolean checkCanAttack(Direction direction, Piece target) {
+        checkSameTeam(target);
+        return direction.hasSame(ATTACK_DIRECTIONS) && !target.isNone();
+    }
+
+    private boolean checkIsStart(Direction direction, Piece target) {
+        checkTarget(target);
+        return start && direction.hasSame(START_DIRECTIONS);
+    }
+
+    private boolean checkCanMove(Direction direction, Piece target) {
+        checkTarget(target);
+        return direction.hasSame(MOVABLE_DIRECTIONS);
+    }
+
+    private void checkTarget(Piece target) {
+        checkSameTeam(target);
+        if (!target.isNone()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_ATTACK_DIRECTION);
         }
     }
 }
