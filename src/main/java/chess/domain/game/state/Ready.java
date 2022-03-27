f77d0f4a3082f914ca822560aa5866ecb615b8a6
Hongdonggeon <ghd700@naver.com>
2022-03-27 18:26:45 +0900

    feat : 보드가 초기화 되지 않았을 때의 Ready 상태 추가

---
+++ src/main/java/chess/domain/game/state/Ready.java
@@ -0,0 +1,43 @@
+package chess.domain.game.state;
+
+import chess.domain.Board;
+import chess.domain.BoardFactory;
+import chess.domain.Position;
+
+public class Ready implements GameState{
+
+    private static final String NOT_STARTED_GAME = "[ERROR] 게임이 시작되지 않았습니다.";
+
+    private final Board board;
+
+    public Ready() {
+        board = BoardFactory.getEmptyBoard();
+    }
+
+    @Override
+    public GameState initBoard() {
+        return new Running(BoardFactory.getInitialPieces());
+    }
+
+    @Override
+    public GameState movePiece(Position fromPosition, Position toPosition) {
+        throw new IllegalStateException(NOT_STARTED_GAME);
+    }
+
+    @Override
+    public GameState end() {
+        throw new IllegalStateException(NOT_STARTED_GAME);
+    }
+
+    @Override
+    public boolean isFinish() {
+        return false;
+    }
+
+    @Override
+    public Board getBoard() {
+        throw new IllegalStateException(NOT_STARTED_GAME);
+    }
+
+
+}
---
+++ src/test/java/chess/game/state/ReadyTest.java
@@ -0,0 +1,55 @@
+package chess.game.state;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import chess.domain.Abscissa;
+import chess.domain.Ordinate;
+import chess.domain.Position;
+import chess.domain.game.state.GameState;
+import chess.domain.game.state.Ready;
+import chess.domain.game.state.Running;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ReadyTest {
+    private GameState state;
+
+    @BeforeEach
+    void setUp() {
+        state = new Ready();
+    }
+
+    @Test
+    @DisplayName("Ready 상태에서 보드를 초기화하면 Running 상태가 된다")
+    void initBoard() {
+        GameState newState = state.initBoard();
+        assertThat(newState).isInstanceOf(Running.class);
+    }
+
+    @Test
+    @DisplayName("Ready 상태에서 말을 움직이려고 하면 예외가 발생한다")
+    void movePieceWhenReadyState() {
+        assertThatThrownBy(() -> state.movePiece(Position.valueOf(Abscissa.a, Ordinate.ONE),
+            Position.valueOf(Abscissa.b, Ordinate.ONE)))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("[ERROR] 게임이 시작되지 않았습니다.");
+    }
+
+    @Test
+    @DisplayName("Ready 상태에서 게임을 종료하려고 하면 예외가 발생한다")
+    void endWhenReadyState() {
+        assertThatThrownBy(() -> state.end())
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("[ERROR] 게임이 시작되지 않았습니다.");
+    }
+
+    @Test
+    @DisplayName("Ready 보드의 상태를 꺼내려고 하면 예외가 발생한다")
+    void getBoardWhenReadyState() {
+        assertThatThrownBy(() -> state.getBoard())
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("[ERROR] 게임이 시작되지 않았습니다.");
+    }
+}
