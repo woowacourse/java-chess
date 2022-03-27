8ac70e82449130230150ddb6a6ff760d010b0212
youngwooyoo <fbduddn97@naver.com>
2022-03-28 03:22:53 +0900

    feat(AbstractCommonMoveStrategy): 중복되는 코드 없애기 위해AbstractCommonMoveStrategy 생성

--- src/main/java/chess/domain/piece/strategy/BishopMoveStrategy.java
+++ src/main/java/chess/domain/piece/strategy/BishopMoveStrategy.java
@@ -1,17 +1,11 @@
 package chess.domain.piece.strategy;
 
-import chess.domain.Board;
 import chess.domain.postion.Position;
 
-public class BishopMoveStrategy implements MoveStrategy {
+public class BishopMoveStrategy extends AbstractCommonMoveStrategy {
     @Override
     public void isMovable(Position source, Position target) {
-        if (source.equals(target)) {
-            throw new IllegalArgumentException("source와 target은 같을 수 없습니다.");
-        }
-
-        if (!source.isDiagonal(target)) {
-            throw new IllegalArgumentException("source와 target은 대각선이여야 합니다.");
-        }
+        validateSamePosition(source, target);
+        validateDiagonal(source, target);
     }
 }
--- src/main/java/chess/domain/piece/strategy/RookMoveStrategy.java
+++ src/main/java/chess/domain/piece/strategy/RookMoveStrategy.java
@@ -2,16 +2,10 @@ package chess.domain.piece.strategy;
 
 import chess.domain.postion.Position;
 
-public class RookMoveStrategy implements MoveStrategy{
+public class RookMoveStrategy extends AbstractCommonMoveStrategy {
     @Override
     public void isMovable(Position source, Position target) {
-
-        if(source.equals(target)) {
-            throw new IllegalArgumentException("source와 target은 같을 수 없습니다.");
-        }
-
-        if(!source.isSameRank(target) || !source.isSameFile(target)) {
-            throw new IllegalArgumentException("target은 source의 상하좌우에 있어야 합니다.");
-        }
+        validateSamePosition(source, target);
+        validateTopBottomRightLeft(source, target);
     }
 }
