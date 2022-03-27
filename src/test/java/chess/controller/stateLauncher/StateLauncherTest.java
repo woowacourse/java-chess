37e127fd6b6961a5f578921fb55840df70630b57
= <rerub0831@gmail.com>
2022-03-27 23:43:56 +0900

    test: 테스트용 픽스쳐 생성

---
+++ src/test/java/chess/controller/stateLauncher/StateLauncherTest.java
@@ -0,0 +1,44 @@
+package chess.controller.stateLauncher;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import chess.domain.Fixtures;
+import chess.domain.board.Board;
+import chess.domain.board.position.Position;
+import chess.domain.game.ChessGame;
+import chess.domain.piece.Piece;
+import chess.domain.piece.attribute.Team;
+import chess.view.OutputView;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class StateLauncherTest {
+    private final ChessGame chessGame = new ChessGame();
+
+    @Test
+    void goEnd() {
+        String boardString =
+                  "........ "
+                + "........ "
+                + "........ "
+                + "........ "
+                + ".......K "
+                + "........ "
+                + "........ "
+                + ".......q";
+
+        Map<Position, Piece> stringPieceMap = Fixtures.stringToBoard(boardString);
+        OutputView.printChessBoard(new Board(stringPieceMap));
+        StateLauncher stateLauncher = new Play(new ChessGame(new Board(stringPieceMap)));
+        stateLauncher = stateLauncher.go("move h1 h4");
+
+        assertThat(stateLauncher)
+                .isInstanceOf(KingGo.class);
+        stateLauncher = stateLauncher.go("status");
+        assertThat(stateLauncher)
+                .isInstanceOf(Status.class);
+
+        assertThat(chessGame.getStatus().getWinner())
+                .isEqualTo(Team.WHITE);
+    }
+}
---
+++ src/test/java/chess/domain/Fixtures.java
@@ -0,0 +1,56 @@
+package chess.domain;
+
+import chess.domain.board.position.File;
+import chess.domain.board.position.Position;
+import chess.domain.board.position.Rank;
+import chess.domain.piece.Bishop;
+import chess.domain.piece.EmptyPiece;
+import chess.domain.piece.King;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Piece;
+import chess.domain.piece.Queen;
+import chess.domain.piece.Rook;
+import chess.domain.piece.attribute.Team;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fixtures {
+
+    private static final Map<String, Piece> stringPieceMap = new HashMap<>();
+
+    static {
+        stringPieceMap.put(".", new EmptyPiece());
+        stringPieceMap.put("R", new Rook(Team.BLACK));
+        stringPieceMap.put("N", new Knight(Team.BLACK));
+        stringPieceMap.put("B", new Bishop(Team.BLACK));
+        stringPieceMap.put("Q", new Queen(Team.BLACK));
+        stringPieceMap.put("K", new King(Team.BLACK));
+        stringPieceMap.put("P", new Pawn(Team.BLACK));
+        stringPieceMap.put("r", new Rook(Team.WHITE));
+        stringPieceMap.put("n", new Knight(Team.WHITE));
+        stringPieceMap.put("b", new Bishop(Team.WHITE));
+        stringPieceMap.put("q", new Queen(Team.WHITE));
+        stringPieceMap.put("k", new King(Team.WHITE));
+        stringPieceMap.put("p", new Pawn(Team.WHITE));
+
+    }
+
+    public static Map<Position, Piece> stringToBoard(String text) {
+        Map<Position, Piece> board = new HashMap<>();
+        int rankY = 8;
+        for (String rank : text.split(" ")) {
+            int fileX = 1;
+            for (String pieceString : rank.split("")) {
+                board.put(new Position(
+                                File.numberOf(fileX), Rank.numberOf(rankY)
+                        ),
+                        stringPieceMap.get(pieceString)
+                );
+                fileX++;
+            }
+            rankY--;
+        }
+        return board;
+    }
+}
