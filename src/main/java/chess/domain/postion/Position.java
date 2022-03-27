0371caf547ceacb12264546ba5a8ffd5f7478efb
youngwooyoo <fbduddn97@naver.com>
2022-03-27 20:50:35 +0900

    feat(Position): equals(), hashcode() override

--- src/main/java/chess/domain/postion/Position.java
+++ src/main/java/chess/domain/postion/Position.java
@@ -1,5 +1,7 @@
 package chess.domain.postion;
 
+import java.util.Objects;
+
 public class Position {
     private File file;
     private Rank rank;
@@ -7,5 +9,18 @@ public class Position {
     public Position(final File file, final Rank rank) {
         this.file = file;
         this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+        Position position = (Position) o;
+        return file == position.file && rank == position.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(file, rank);
     }
 }
