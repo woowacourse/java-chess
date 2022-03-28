af121a89eaa5a11266e4e84d272d796086e28918
summerlunaa <summerlunaa9@gmail.com>
2022-03-28 10:10:20 +0900

    refactor: 폰 이동 전략 로직 수정

--- src/main/java/chess/domain/piece/strategy/BlackPawnMovingStrategy.java
+++ src/main/java/chess/domain/piece/strategy/BlackPawnMovingStrategy.java
@@ -13,82 +13,51 @@ public class BlackPawnMovingStrategy implements MovingStrategy {
 
     @Override
     public void validateMove(List<List<Piece>> board, Position sourcePosition, Position targetPosition) {
+        int rankLength = Math.abs(sourcePosition.getRankIndex() - targetPosition.getRankIndex());
+        int fileLength = Math.abs(sourcePosition.getFileIndex() - targetPosition.getFileIndex());
         Direction direction = Direction.of(sourcePosition, targetPosition);
-        validateDirection(direction);
-        validateMove(board, sourcePosition, targetPosition, direction);
-        validateCapture(board, sourcePosition, targetPosition, direction);
+
+        if (direction == MOVABLE_DIRECTION && isMovableLengthAtMove(sourcePosition, rankLength)) {
+            validateMoveTop(sourcePosition, rankLength, findPiece(board, sourcePosition.add(direction)));
+            validateExistPiece(findPiece(board, targetPosition));
+            return;
+        }
+        if (CAPTURABLE_DIRECTIONS.contains(direction) && isMovableLengthAtCapture(rankLength, fileLength)) {
+            validateCapture(findPiece(board, targetPosition));
+            return;
+        }
+
+        throw new IllegalArgumentException("해당 기물이 갈 수 없는 경로입니다.");
     }
 
-    private void validateDirection(Direction direction) {
-        if (!(direction == MOVABLE_DIRECTION || CAPTURABLE_DIRECTIONS.contains(direction))) {
-            throw new IllegalArgumentException("해당 기물이 갈 수 없는 경로입니다.");
+    private boolean isMovableLengthAtMove(Position sourcePosition, int rankLength) {
+        return rankLength == 1
+                || (sourcePosition.getRankIndex() == RANK_INDEX_STARTING_POINT && rankLength == 2);
+    }
+
+    private void validateMoveTop(Position source, int rankLength, Piece piece) {
+        if (rankLength == 2 && source.getRankIndex() == RANK_INDEX_STARTING_POINT) {
+            validateExistPiece(piece);
         }
     }
 
-    private void validateMove(List<List<Piece>> board, Position source, Position target, Direction direction) {
-        int rankLength = Math.abs(source.getRankIndex() - target.getRankIndex());
-
-        if (direction == MOVABLE_DIRECTION) {
-            validateStartingPoint(board, source, direction, rankLength);
-            validateNotStartingPoint(source, rankLength);
-            validateExistPiece(findPiece(board, target));
-        }
-    }
-
-    private void validateStartingPoint(List<List<Piece>> board, Position source, Direction direction, int rankLength) {
-        if (source.getRankIndex() == RANK_INDEX_STARTING_POINT && rankLength == 2) {
-            validateExistPieceInPath(board, source, direction);
-        }
-        if (source.getRankIndex() == RANK_INDEX_STARTING_POINT && rankLength > 2) {
-            throw new IllegalArgumentException("해당 기물이 갈 수 없는 경로입니다.");
-        }
-    }
-
-    private void validateExistPieceInPath(List<List<Piece>> board, Position source, Direction direction) {
-        Piece currentPiece = findPiece(board, source.add(direction));
-        validateExistPiece(currentPiece);
-    }
-
-    private void validateExistPiece(Piece target) {
-        if (!target.isEmpty()) {
+    private void validateExistPiece(Piece piece) {
+        if (!piece.isEmpty()) {
             throw new IllegalArgumentException("경로에 기물이 존재하여 이동할 수 없습니다.");
         }
     }
 
-    private void validateNotStartingPoint(Position source, int rankLength) {
-        if (source.getRankIndex() != RANK_INDEX_STARTING_POINT && rankLength != 1) {
-            throw new IllegalArgumentException("해당 기물이 갈 수 없는 경로입니다.");
-        }
+    private boolean isMovableLengthAtCapture(int rankLength, int fileLength) {
+        return rankLength + fileLength == 2;
     }
 
-    private void validateCapture(List<List<Piece>> board, Position source, Position target, Direction direction) {
-        int rankLength = Math.abs(source.getRankIndex() - target.getRankIndex());
-        int fileLength = Math.abs(source.getFileIndex() - target.getFileIndex());
-
-        if (CAPTURABLE_DIRECTIONS.contains(direction)) {
-            validateCaptureMove(rankLength, fileLength);
-
-            Piece targetPiece = findPiece(board, target);
-            validateEmptyPiece(targetPiece);
-            validateSameColor(targetPiece);
-        }
+    private void validateCapture(Piece targetPiece) {
+        validateEmptyPiece(targetPiece);
+        validateSameColor(targetPiece);
     }
 
-    private Piece findPiece(List<List<Piece>> board, Position position) {
-        int rankIndex = position.getRankIndex();
-        int fileIndex = position.getFileIndex();
-
-        return board.get(rankIndex).get(fileIndex);
-    }
-
-    private void validateCaptureMove(int rankLength, int fileLength) {
-        if (rankLength + fileLength != 2) {
-            throw new IllegalArgumentException("해당 기물이 갈 수 없는 경로입니다.");
-        }
-    }
-
-    private void validateEmptyPiece(Piece targetPiece) {
-        if (targetPiece.isEmpty()) {
+    private void validateEmptyPiece(Piece piece) {
+        if (piece.isEmpty()) {
             throw new IllegalArgumentException("target 위치에 기물이 존재하지 않아 공격할 수 없습니다.");
         }
     }
@@ -97,5 +66,12 @@ public class BlackPawnMovingStrategy implements MovingStrategy {
         if (targetPiece.isBlack()) {
             throw new IllegalArgumentException("공격은 다른 진영만 가능합니다.");
         }
+    }
+
+    private Piece findPiece(List<List<Piece>> board, Position position) {
+        int rankIndex = position.getRankIndex();
+        int fileIndex = position.getFileIndex();
+
+        return board.get(rankIndex).get(fileIndex);
     }
 }
