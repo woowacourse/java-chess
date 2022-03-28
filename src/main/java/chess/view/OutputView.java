596dc931535dfc2d466491edca5dff48fd5a1884
Seongwon97 <osw970325@naver.com>
2022-03-28 15:30:37 +0900

    refactor(OutputView): 변경하지 못하였던 초기 명령 출력문 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -5,8 +5,11 @@ import chess.dto.Response;
 public class OutputView {
 
     public void printIntroduction() {
-        System.out.println("체스 게임을 시작합니다.\n" +
-                "게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.println("> 체스 게임을 시작합니다." + System.lineSeparator() +
+                "> 게임 시작 : start" + System.lineSeparator() +
+                "> 게임 종료 : end" + System.lineSeparator() +
+                "> 게임 이동 : move source위치 target위치 - 예. move b2 b3" + System.lineSeparator() +
+                "> 중간 점수 : status");
     }
 
     public void printException(RuntimeException e) {
