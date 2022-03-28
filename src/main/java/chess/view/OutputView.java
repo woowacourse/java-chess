d146f46e2affe2abb57c710464eee695c1f1e08e
cjlee38 <cjlee38@github.com>
2022-03-28 16:37:09 +0900

    refactor(view): 초기 안내 문구 수정

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -5,8 +5,11 @@ import chess.dto.Response;
 public class OutputView {
 
     public void printIntroduction() {
-        System.out.println("체스 게임을 시작합니다.\n" +
-            "게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.println("> 체스 게임을 시작합니다.\n"
+            + "> 게임 시작 : start\n"
+            + "> 게임 종료 : end\n"
+            + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3"
+            + "> 점수 확인 : status");
     }
 
     public void printException(RuntimeException e) {
