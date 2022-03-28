36cc9f6d146aae0527571b2aa72bfec806e5e246
shindong96 <2006peter@naver.com>
2022-03-28 17:01:47 +0900

    fix: Mone 피스도 생성자 수정

--- src/main/java/chess/domain/piece/None.java
+++ src/main/java/chess/domain/piece/None.java
@@ -3,25 +3,24 @@ package chess.domain.piece;
 import chess.domain.position.Direction;
 
 public final class None extends Piece {
-    private static final String NONE = "·";
+	private static final String NONE = "·";
 
-    public None(Color color) {
-        super(color);
-        this.score = 0;
-    }
+	public None(Color color) {
+		super(color, 0);
+	}
 
-    @Override
-    public String getEmoji() {
-        return NONE;
-    }
+	@Override
+	public String getEmoji() {
+		return NONE;
+	}
 
-    @Override
-    public boolean canMove(Direction direction, Piece target) {
-        return false;
-    }
+	@Override
+	public boolean canMove(Direction direction, Piece target) {
+		return false;
+	}
 
-    @Override
-    public boolean isNone() {
-        return true;
-    }
+	@Override
+	public boolean isNone() {
+		return true;
+	}
 }
