9a5d40561b182d3322ba3ae3fb38604497ed9d7b
Wishoon <heonga2@gmail.com>
2022-03-28 13:57:24 +0900

    fix : 초기 출력문 수정

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -22,6 +22,7 @@ public class OutputView {
     private static final String INIT_MESSAGE =
             ARROW + "체스 게임을 시작합니다." + SEPARATOR
                     + ARROW + "게임 시작 : start" + SEPARATOR
+                    + ARROW + "게임 상태 : status" + SEPARATOR
                     + ARROW + "게임 종료 : end" + SEPARATOR
                     + ARROW + "게임 이동 : move source위치 target위치 - 예. move b2 b3";
 
