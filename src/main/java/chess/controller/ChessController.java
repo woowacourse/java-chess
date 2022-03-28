8a0ddbe46c7b7df1d23da9192e37fce97584f540
jaejae-yoo <wotj102@gmail.com>
2022-03-28 16:50:02 +0900

    feat: 잘못된 명령어 입력시 오류

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -4,11 +4,15 @@ import chess.domain.ChessGame;
 import chess.domain.piece.Color;
 import chess.view.OutputView;
 
+import java.util.Arrays;
+
 import static chess.Command.*;
 import static chess.view.InputView.inputCommand;
 import static chess.view.OutputView.printStartMessage;
 
 public class ChessController {
+
+    private static final String ERROR_WRONG_COMMAND = "잘못된 명령어 입력입니다.";
 
     public void run() {
         ChessGame chessGame = new ChessGame();
@@ -20,21 +24,29 @@ public class ChessController {
     }
 
     private void playTurn(ChessGame chessGame) {
-        String command = inputCommand();
-        if (START.isValue(command)) {
+        String input = inputCommand();
+        if (START.isValue(input)) {
             chessGame.start();
             OutputView.printBoard(chessGame.getChessBoard());
         }
-        if (MOVE.isValue(command)) {
-            chessGame.move(command);
+        if (MOVE.isValue(input)) {
+            chessGame.move(input);
             OutputView.printBoard(chessGame.getChessBoard());
         }
-        if (STATUS.isValue(command)) {
+        if (STATUS.isValue(input)) {
             OutputView.printProgressScore(chessGame.computeScore(Color.BLACK), chessGame.computeScore(Color.WHITE));
         }
-        if (END.isValue(command)) {
+        if (END.isValue(input)) {
             chessGame.end();
             OutputView.printBoard(chessGame.getChessBoard());
         }
+        if (!isExistCommand(input)) {
+            throw new IllegalArgumentException(ERROR_WRONG_COMMAND);
+        }
+    }
+
+    private boolean isExistCommand(String input) {
+        return Arrays.stream(values())
+                .anyMatch(command -> command.isValue(input));
     }
 }
