d138abd6337463f74a3d2ca10cbf4e77928e7043
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-29 18:27:36 +0900

    fix(PieceType): fix wrong local variable usage

--- src/main/java/chess2/domain2/board2/piece2/PieceType.java
+++ src/main/java/chess2/domain2/board2/piece2/PieceType.java
@@ -17,6 +17,8 @@ public enum PieceType {
     private static final int KNIGHT_MAIN_MOVE_DIFF = 2;
     private static final int KING_MAX_MOVE_DIFF = 1;
 
+    private static final String NON_PAWN_ONLY_EXCEPTION_MESSAGE = "폰의 이동 로직은 별도로 구현해야 합니다.";
+
     private final RouteChecker routeChecker;
 
     PieceType(RouteChecker routeChecker) {
@@ -27,21 +29,21 @@ public enum PieceType {
         return routeChecker.checkMovable(from, to);
     }
 
-    private static boolean isPawnMovable(Position fromPosition, Position toPosition) {
-        throw new UnsupportedOperationException("폰의 이동 로직은 별도로 구현해야 합니다.");
+    private static boolean isPawnMovable(Position from, Position to) {
+        throw new UnsupportedOperationException(NON_PAWN_ONLY_EXCEPTION_MESSAGE);
     }
 
-    private static boolean isKnightMovable(Position fromPosition, Position toPosition) {
-        int fileDiff = fromPosition.fileDifference(toPosition);
-        int rankDiff = toPosition.rankDifference(toPosition);
+    private static boolean isKnightMovable(Position from, Position to) {
+        int fileDiff = from.fileDifference(to);
+        int rankDiff = from.rankDifference(to);
 
         return (fileDiff == KNIGHT_SUB_MOVE_DIFF && rankDiff == KNIGHT_MAIN_MOVE_DIFF)
                 || (fileDiff == KNIGHT_MAIN_MOVE_DIFF && rankDiff == KNIGHT_SUB_MOVE_DIFF);
     }
 
-    private static boolean isKingMovable(Position fromPosition, Position toPosition) {
-        int fileDiff = fromPosition.fileDifference(toPosition);
-        int rankDiff = toPosition.rankDifference(toPosition);
+    private static boolean isKingMovable(Position from, Position to) {
+        int fileDiff = from.fileDifference(to);
+        int rankDiff = from.rankDifference(to);
 
         return fileDiff <= KING_MAX_MOVE_DIFF && rankDiff <= KING_MAX_MOVE_DIFF;
     }
