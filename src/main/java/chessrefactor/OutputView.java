1414fea8066e571ffb423180b5922abb6d932806
leo0842 <kunga077@gmail.com>
2022-03-30 02:30:32 +0900

    feat: output view 클래스 생성

---
+++ src/main/java/chessrefactor/OutputView.java
@@ -0,0 +1,13 @@
+package chessrefactor;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static void announce(BoardDto boardDto) {
+        List<List<String>> dto = boardDto.getDto();
+        for (List<String> line : dto) {
+            System.out.println(String.join(" ", line));
+        }
+    }
+}
