0dddddb39e1b89f3cd2db59e9978f269c20ec307
asebn1 <asebn1@khu.ac.kr>
2022-03-29 16:57:39 +0900

    refactor: team 검증 추가

--- src/main/java/domain/ChessBoardGenerator.java
+++ src/main/java/domain/ChessBoardGenerator.java
@@ -39,68 +39,75 @@ public final class ChessBoardGenerator implements BoardGenerator {
         }
     }
 
-    private void createTeamBoard(final Map<Position, Piece> board, final Team Team) {
-        createInitPawn(board, Team);
-        createInitRook(board, Team);
-        createInitKnight(board, Team);
-        createInitBishop(board, Team);
-        createInitQueen(board, Team);
-        createInitKing(board, Team);
+    private void createTeamBoard(final Map<Position, Piece> board, final Team team) {
+        checkTeamNull(team);
+        createInitPawn(board, team);
+        createInitRook(board, team);
+        createInitKnight(board, team);
+        createInitBishop(board, team);
+        createInitQueen(board, team);
+        createInitKing(board, team);
     }
 
-    private void createInitPawn(final Map<Position, Piece> board, final Team Team) {
-        if (Team == Team.BLACK) {
+    private void checkTeamNull(Team team) {
+        if (team == null) {
+            throw new IllegalArgumentException("[ERROR] Team 값은 지정되어야 합니다.");
+        }
+    }
+
+    private void createInitPawn(final Map<Position, Piece> board, final Team team) {
+        if (team == team.BLACK) {
             Arrays.stream(XPosition.values())
-                    .forEach(x -> board.put(Position.of(x, YPosition.SEVEN), new Pawn(Team.BLACK)));
+                    .forEach(x -> board.put(Position.of(x, YPosition.SEVEN), new Pawn(team.BLACK)));
             return;
         }
         Arrays.stream(XPosition.values())
-                .forEach(x -> board.put(Position.of(x, YPosition.TWO), new Pawn(Team.WHITE)));
+                .forEach(x -> board.put(Position.of(x, YPosition.TWO), new Pawn(team.WHITE)));
     }
 
-    private void createInitRook(final Map<Position, Piece> board, final Team Team) {
-        if (Team == Team.BLACK) {
-            board.put(Position.of(XPosition.A, YPosition.EIGHT), new Rook(Team.BLACK));
-            board.put(Position.of(XPosition.H, YPosition.EIGHT), new Rook(Team.BLACK));
+    private void createInitRook(final Map<Position, Piece> board, final Team team) {
+        if (team == team.BLACK) {
+            board.put(Position.of(XPosition.A, YPosition.EIGHT), new Rook(team.BLACK));
+            board.put(Position.of(XPosition.H, YPosition.EIGHT), new Rook(team.BLACK));
             return;
         }
-        board.put(Position.of(XPosition.A, YPosition.ONE), new Rook(Team.WHITE));
-        board.put(Position.of(XPosition.H, YPosition.ONE), new Rook(Team.WHITE));
+        board.put(Position.of(XPosition.A, YPosition.ONE), new Rook(team.WHITE));
+        board.put(Position.of(XPosition.H, YPosition.ONE), new Rook(team.WHITE));
     }
 
-    private void createInitKnight(final Map<Position, Piece> board, final Team Team) {
-        if (Team == Team.BLACK) {
-            board.put(Position.of(XPosition.B, YPosition.EIGHT), new Knight(Team.BLACK));
-            board.put(Position.of(XPosition.G, YPosition.EIGHT), new Knight(Team.BLACK));
+    private void createInitKnight(final Map<Position, Piece> board, final Team team) {
+        if (team == team.BLACK) {
+            board.put(Position.of(XPosition.B, YPosition.EIGHT), new Knight(team.BLACK));
+            board.put(Position.of(XPosition.G, YPosition.EIGHT), new Knight(team.BLACK));
             return;
         }
-        board.put(Position.of(XPosition.B, YPosition.ONE), new Knight(Team.WHITE));
-        board.put(Position.of(XPosition.G, YPosition.ONE), new Knight(Team.WHITE));
+        board.put(Position.of(XPosition.B, YPosition.ONE), new Knight(team.WHITE));
+        board.put(Position.of(XPosition.G, YPosition.ONE), new Knight(team.WHITE));
     }
 
-    private void createInitBishop(final Map<Position, Piece> board, final Team Team) {
-        if (Team == Team.BLACK) {
-            board.put(Position.of(XPosition.C, YPosition.EIGHT), new Bishop(Team.BLACK));
-            board.put(Position.of(XPosition.F, YPosition.EIGHT), new Bishop(Team.BLACK));
+    private void createInitBishop(final Map<Position, Piece> board, final Team team) {
+        if (team == team.BLACK) {
+            board.put(Position.of(XPosition.C, YPosition.EIGHT), new Bishop(team.BLACK));
+            board.put(Position.of(XPosition.F, YPosition.EIGHT), new Bishop(team.BLACK));
             return;
         }
-        board.put(Position.of(XPosition.C, YPosition.ONE), new Bishop(Team.WHITE));
-        board.put(Position.of(XPosition.F, YPosition.ONE), new Bishop(Team.WHITE));
+        board.put(Position.of(XPosition.C, YPosition.ONE), new Bishop(team.WHITE));
+        board.put(Position.of(XPosition.F, YPosition.ONE), new Bishop(team.WHITE));
     }
 
-    private void createInitQueen(final Map<Position, Piece> board, final Team Team) {
-        if (Team == Team.BLACK) {
-            board.put(Position.of(XPosition.D, YPosition.EIGHT), new Queen(Team.BLACK));
+    private void createInitQueen(final Map<Position, Piece> board, final Team team) {
+        if (team == team.BLACK) {
+            board.put(Position.of(XPosition.D, YPosition.EIGHT), new Queen(team.BLACK));
             return;
         }
-        board.put(Position.of(XPosition.D, YPosition.ONE), new Queen(Team.WHITE));
+        board.put(Position.of(XPosition.D, YPosition.ONE), new Queen(team.WHITE));
     }
 
-    private void createInitKing(final Map<Position, Piece> board, final Team Team) {
-        if (Team == Team.BLACK) {
-            board.put(Position.of(XPosition.E, YPosition.EIGHT), new King(Team.BLACK));
+    private void createInitKing(final Map<Position, Piece> board, final Team team) {
+        if (team == team.BLACK) {
+            board.put(Position.of(XPosition.E, YPosition.EIGHT), new King(team.BLACK));
             return;
         }
-        board.put(Position.of(XPosition.E, YPosition.ONE), new King(Team.WHITE));
+        board.put(Position.of(XPosition.E, YPosition.ONE), new King(team.WHITE));
     }
 }
