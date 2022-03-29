0e8d0a2c6c1c2e97d50a4ed8bb1b25ae2f881b6e
Ohzzi <clover7even@naver.com>
2022-03-30 00:08:59 +0900

    fix: 잘못된 오버라이딩 제거

--- src/main/java/chess/domain/state/BlackRunning.java
+++ src/main/java/chess/domain/state/BlackRunning.java
@@ -1,24 +1,12 @@
 package chess.domain.state;
 
 import chess.domain.board.Board;
-import chess.domain.board.Position;
 import chess.domain.piece.Color;
-import chess.domain.piece.Piece;
-import chess.domain.piece.PieceType;
 
 public class BlackRunning extends Running {
 
     public BlackRunning(Board board) {
         super(board, Color.BLACK);
-    }
-
-    @Override
-    public ChessState move(Position start, Position target) {
-        Piece caughtPiece = board.move(start, target, color);
-        if (caughtPiece.isSamePiece(PieceType.KING)) {
-            return new Finished(board);
-        }
-        return new WhiteRunning(board);
     }
 
     @Override
