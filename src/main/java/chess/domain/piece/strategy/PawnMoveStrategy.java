d5aa701cdc85187fc201e8316e65d22c4eeabce8
= <rerub0831@gmail.com>
2022-03-30 01:34:11 +0900

    style: 코드 리포맷

--- src/main/java/chess/domain/piece/strategy/PawnMoveStrategy.java
+++ src/main/java/chess/domain/piece/strategy/PawnMoveStrategy.java
@@ -3,7 +3,6 @@ package chess.domain.piece.strategy;
 import chess.domain.board.position.Direction;
 import chess.domain.board.position.Position;
 import chess.domain.board.position.Rank;
-import chess.domain.piece.AbstractPiece;
 import chess.domain.piece.Piece;
 import chess.domain.piece.attribute.Team;
 import java.util.List;
