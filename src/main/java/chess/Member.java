ef8f8246f129acede99a8cd8e707c2231e7c01e7
meatsby <meatsby@gmail.com>
2022-03-29 13:29:51 +0900

    feat: docker

---
+++ src/main/java/chess/Member.java
@@ -0,0 +1,39 @@
+package chess;
+
+public class Member {
+
+    private final String id;
+    private final String name;
+    private final Role role;
+
+    public Member(String id, String name) {
+        this(id, name, null);
+    }
+
+    public Member(String id, String name, Role role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
