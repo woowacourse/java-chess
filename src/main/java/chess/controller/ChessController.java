4cefd8cffa46d2c1f2c0da0627222467bd9313dc
hyunrrr <whgusrms96@gmail.com>
2022-03-29 15:06:44 +0900

    refactor: 컨트롤러 메서드 분리

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -12,6 +12,9 @@ public class ChessController {
 
     private static final int SOURCE_POSITION_INDEX = 1;
     private static final int TARGET_POSITION_INDEX = 2;
+    private static final int COMMAND_INDEX = 0;
+    private static final String STATUS_COMMAND = "status";
+    private static final String MOVE_COMMAND = "move";
 
     public void run() {
         OutputView.printChessGameStart();
@@ -23,26 +26,40 @@ public class ChessController {
 
     private void playChessGame() {
         ChessGame chessGame = ChessGame.create();
-        OutputView.printCurrentChessBoard(ChessMenDto.of(chessGame.getBlackChessMen()),
-                ChessMenDto.of(chessGame.getWhiteChessMen()));
+        sendDataForPrintCurrentChessBoard(chessGame);
 
         while (!chessGame.isGameEnd()) {
-            List<String> command = InputView.requestMoveOrStatus();
-            inGameCommand(chessGame, command);
+            doTurn(chessGame);
         }
     }
 
-    private void inGameCommand(ChessGame chessGame, List<String> command) {
-        if (command.get(0).equals("status")) {
-            OutputView.printStatus(ChessStatusDto.of(chessGame));
-        }
-        if (command.get(0).equals("move")) {
-            ChessBoardPosition sourcePosition = ChessBoardPosition.of(command.get(SOURCE_POSITION_INDEX));
-            ChessBoardPosition targetPosition = ChessBoardPosition.of(command.get(TARGET_POSITION_INDEX));
+    private void sendDataForPrintCurrentChessBoard(ChessGame chessGame) {
+        OutputView.printCurrentChessBoard(ChessMenDto.of(chessGame.getBlackChessMen()),
+                ChessMenDto.of(chessGame.getWhiteChessMen()));
+    }
 
-            chessGame.move(sourcePosition, targetPosition);
-            OutputView.printCurrentChessBoard(ChessMenDto.of(chessGame.getBlackChessMen()),
-                    ChessMenDto.of(chessGame.getWhiteChessMen()));
+    private void doTurn(ChessGame chessGame) {
+        List<String> command = InputView.requestMoveOrStatus();
+        inGameCommand(chessGame, command);
+    }
+
+    private void inGameCommand(ChessGame chessGame, List<String> command) {
+        if (command.get(COMMAND_INDEX).equals(STATUS_COMMAND)) {
+            statusCommand(chessGame);
         }
+        if (command.get(COMMAND_INDEX).equals(MOVE_COMMAND)) {
+            moveCommand(chessGame, command);
+        }
+    }
+
+    private void statusCommand(ChessGame chessGame) {
+        OutputView.printStatus(ChessStatusDto.of(chessGame));
+    }
+
+    private void moveCommand(ChessGame chessGame, List<String> command) {
+        ChessBoardPosition sourcePosition = ChessBoardPosition.of(command.get(SOURCE_POSITION_INDEX));
+        ChessBoardPosition targetPosition = ChessBoardPosition.of(command.get(TARGET_POSITION_INDEX));
+        chessGame.move(sourcePosition, targetPosition);
+        sendDataForPrintCurrentChessBoard(chessGame);
     }
 }
