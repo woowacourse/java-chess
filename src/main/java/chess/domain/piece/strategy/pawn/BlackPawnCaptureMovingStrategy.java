d14f4d18de61015cfbc4c5ed0f953fb1738cd931
hyeonic <dev.hyeonic@gmail.com>
2022-03-29 17:07:02 +0900

    feat: Black Pawn 캡처 전략 추가

---
+++ src/main/java/chess/domain/piece/strategy/pawn/BlackPawnCaptureMovingStrategy.java
@@ -0,0 +1,44 @@
+package chess.domain.piece.strategy.pawn;
+
+import chess.domain.piece.Piece;
+import chess.domain.position.Direction;
+import chess.domain.position.Position;
+import java.util.List;
+
+public class BlackPawnCaptureMovingStrategy {
+
+    private static final List<Direction> CAPTURABLE_DIRECTIONS = List.of(Direction.BOTTOM_LEFT, Direction.BOTTOM_RIGHT);
+
+    public boolean canMove(List<List<Piece>> board, Position source, Position target) {
+        Direction direction = Direction.of(source, target);
+
+        return CAPTURABLE_DIRECTIONS.contains(direction)
+                && calculateDistance(source, target) == 2
+                && isCapture(board, source, target);
+    }
+
+    private int calculateDistance(Position source, Position target) {
+        int rankLength = Math.abs(source.getRankIndex() - target.getRankIndex());
+        int fileLength = Math.abs(source.getFileIndex() - target.getFileIndex());
+
+        return square(rankLength) + square(fileLength);
+    }
+
+    private int square(int value) {
+        return value * value;
+    }
+
+    private boolean isCapture(List<List<Piece>> board, Position source, Position target) {
+        Piece sourcePiece = findPiece(board, source);
+        Piece targetPiece = findPiece(board, target);
+
+        return !sourcePiece.isSameColor(targetPiece);
+    }
+
+    private Piece findPiece(List<List<Piece>> board, Position position) {
+        int rankIndex = position.getRankIndex();
+        int fileIndex = position.getFileIndex();
+
+        return board.get(rankIndex).get(fileIndex);
+    }
+}
--- src/test/java/chess/domain/piece/strategy/pawn/BlackPawnCaptureMovingStrategyTest.java
+++ src/test/java/chess/domain/piece/strategy/pawn/BlackPawnCaptureMovingStrategyTest.java
@@ -14,17 +14,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class WhitePawnCaptureMovingStrategyTest {
+class BlackPawnCaptureMovingStrategyTest {
 
-    @DisplayName("White Pawn은 위 오른쪽과 왼쪽으로 capture 가능하다.")
+    @DisplayName("Black Pawn은 아래 오른쪽과 왼쪽으로 capture 가능하다.")
     @ParameterizedTest
-    @CsvSource({"b2,a3,true", "b2,c3,true", "b2,b3,false", "c2,a3,false"})
-    void white_pawn은_위_오른쪽_왼쪽_capture_가능하다(String source, String target, boolean expected) {
-        WhitePawnCaptureMovingStrategy movingStrategy = new WhitePawnCaptureMovingStrategy();
+    @CsvSource({"b7,a6,true", "b7,c6,true", "b7,b6,false", "c7,a6,false"})
+    void black_pawn은_아래_오른쪽_왼쪽_capture_가능하다(String source, String target, boolean expected) {
+        BlackPawnCaptureMovingStrategy movingStrategy = new BlackPawnCaptureMovingStrategy();
         ChessBoard chessBoard = generateEmptyChessBoard();
         List<List<Piece>> board = chessBoard.getBoard();
-        setPiece(board, source, new Pawn(Color.WHITE));
-        setPiece(board, target, new Pawn(Color.BLACK));
+        setPiece(board, source, new Pawn(Color.BLACK));
+        setPiece(board, target, new Pawn(Color.WHITE));
 
         boolean result = movingStrategy.canMove(board, new Position(source), new Position(target));
 
