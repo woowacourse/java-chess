e9869ac1accd386a2f57a60deee6c6ac7565c2e7
leo0842 <kunga077@gmail.com>
2022-03-30 02:23:40 +0900

    feat: 체스 컨트롤러 생성

---
+++ src/main/java/chessrefactor/ChessController.java
@@ -0,0 +1,41 @@
+package chessrefactor;
+
+import chessrefactor.piece.Piece;
+import chessrefactor.square.File;
+import chessrefactor.square.Rank;
+import chessrefactor.square.Square;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class ChessController {
+
+    public BoardDto startGame() {
+        Board board = new Board();
+        return new BoardDto(toBoardDto(board));
+    }
+
+    private List<List<String>> toBoardDto(Board board) {
+        List<List<String>> boardDto = new ArrayList<>();
+        List<Rank> ranks = Arrays.asList(Rank.values());
+        Collections.reverse(ranks);
+        for (Rank rank : ranks) {
+            List<String> tempLine = new ArrayList<>();
+            for (File file : File.values()) {
+                Piece piece = board.get(Square.of(rank, file));
+                tempLine.add(toPieceDto(piece));
+            }
+            boardDto.add(tempLine);
+        }
+        return boardDto;
+    }
+
+    private String toPieceDto(Piece piece) {
+        if (piece.isBlack()) {
+            return piece.name().toUpperCase(Locale.ROOT);
+        }
+        return piece.name();
+    }
+}
