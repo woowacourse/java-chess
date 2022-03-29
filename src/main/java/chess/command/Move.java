3297d13220ade3e9f9cc8b63c9f1488713cbeda6
Ohzzi <clover7even@naver.com>
2022-03-29 22:32:50 +0900

    feat: Move 명령 객체 구현

---
+++ src/main/java/chess/command/Move.java
@@ -0,0 +1,23 @@
+package chess.command;
+
+import chess.domain.board.Position;
+import chess.domain.state.ChessState;
+import chess.view.OutputView;
+
+public class Move implements Command {
+
+    private final Position start;
+    private final Position target;
+
+    public Move(Position start, Position target) {
+        this.start = start;
+        this.target = target;
+    }
+
+    @Override
+    public ChessState execute(ChessState chessState) {
+        ChessState movedState = chessState.move(start, target);
+        OutputView.printBoard(movedState.getPieces());
+        return movedState;
+    }
+}
