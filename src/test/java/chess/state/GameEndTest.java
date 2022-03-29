56f5ad5d046e9baf8537d769e2d96f3ce3b265ea
Kim Seunglae <obey1342@gmail.com>
2022-03-29 20:53:59 +0900

    test: 게임상태 테스트 코드 케이스 추가

--- src/test/java/chess/state/GameEndTest.java
+++ src/test/java/chess/state/GameEndTest.java
@@ -4,16 +4,67 @@ import static org.assertj.core.api.Assertions.*;
 
 import chess.ChessBoard;
 import chess.piece.Color;
+import chess.piece.King;
+import chess.position.Position;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameEndTest {
 
+    private GameEnd gameEnd;
+
+    @BeforeEach
+    void setUp() {
+        gameEnd = new GameEnd(ChessBoard.createChessBoard(), Color.WHITE);
+    }
+
     @Test
     @DisplayName("finished 호출 후 finished 상태로 변한다.")
     void callFinishedAfterFinishedState() {
-        State gameEnd = new GameEnd(ChessBoard.createChessBoard(), Color.WHITE);
-
         assertThat(gameEnd.finished()).isInstanceOf(Finished.class);
+    }
+
+    @Test
+    @DisplayName("move 메서드 호출 시 예외 발생")
+    void throwExceptionCallMove() {
+        assertThatThrownBy(() -> gameEnd.move(Position.from("e5"), Position.from("e6")))
+            .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("isFinished 메서드 호출 시 false를 반환")
+    void isFinished() {
+        assertThat(gameEnd.isFinished()).isFalse();
+    }
+
+    @Test
+    @DisplayName("isGameEnd 메서드 호출 시 true를 반환")
+    void isGameEnd() {
+        assertThat(gameEnd.isGameEnd()).isTrue();
+    }
+
+    @Test
+    @DisplayName("score 메서드를 호출하면 예외 발생")
+    void throwExceptionCallScore() {
+        assertThatThrownBy(() -> gameEnd.score())
+            .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("start 메서드 호출 시 예외 발생")
+    void throwExceptionCallStart() {
+        assertThatThrownBy(() -> gameEnd.start())
+            .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("winner 호출 시 정상적으로 우승자 반환")
+    void winner() {
+        GameEnd gameEnd = new GameEnd(new ChessBoard(
+            List.of(new King(Color.WHITE, Position.from("a1")))), Color.WHITE);
+
+        assertThat(gameEnd.winner()).isEqualTo(Color.WHITE);
     }
 }
--- src/test/java/chess/state/RunningTest.java
+++ src/test/java/chess/state/RunningTest.java
@@ -9,30 +9,38 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import chess.ChessBoard;
+import chess.game.Score;
+import chess.piece.Bishop;
 import chess.piece.Color;
 import chess.piece.King;
+import chess.piece.Knight;
 import chess.piece.Rook;
 import chess.position.Position;
+import java.math.BigDecimal;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RunningTest {
 
+    private Running running;
+
+    @BeforeEach
+    void setUp() {
+        running = new Running(ChessBoard.createChessBoard(), Color.WHITE);
+    }
+
     @Test
     @DisplayName("finished 메서드 호출 후 Finished로 변경")
     void callFinishedAfterFinishedState() {
-        State running = new Running(ChessBoard.createChessBoard(), Color.WHITE);
-
         assertThat(running.finished()).isInstanceOf(Finished.class);
     }
 
     @Test
     @DisplayName("기물을 움직여도 Running 상태인지 확인")
     void movePieceAfterRunningState() {
-        State running = new Running(ChessBoard.createChessBoard(), Color.WHITE);
-
         assertThat(running.move(Position.from("d2"), Position.from("d4")))
             .isInstanceOf(Running.class);
     }
@@ -40,7 +48,7 @@ class RunningTest {
     @Test
     @DisplayName("왕을 잡을 경우 GameEnd상태로 변화")
     void killOfKingAfterGameEndState() {
-        State running = new Running(
+        Running running = new Running(
             new ChessBoard(List.of(new King(Color.BLACK, Position.from("e8")),
                 new Rook(Color.WHITE, Position.from("e1")),
                 new King(Color.WHITE, Position.from("f1")))), Color.WHITE);
@@ -52,7 +60,7 @@ class RunningTest {
     @Test
     @DisplayName("같은 플레이어가 연속해서 기물을 움직일 경우 예외 발생")
     void throwExceptionWhenMoveSameColorPieceSubsequently() {
-        State running = new Running(new ChessBoard(
+        Running running = new Running(new ChessBoard(
             List.of(new King(Color.WHITE, new Position(E, FIVE)),
                 new King(Color.BLACK, new Position(B, FIVE)))), Color.WHITE);
 
@@ -65,5 +73,47 @@ class RunningTest {
                 new King(Color.WHITE, new Position(E, SIX)),
                 new King(Color.BLACK, new Position(B, FIVE)));
         });
+    }
+
+    @Test
+    @DisplayName("점수 계산이 정확한지 확인")
+    void score() {
+        Running running = new Running(new ChessBoard(
+            List.of(new Rook(Color.WHITE, Position.from("e5")),
+                new Knight(Color.WHITE, Position.from("e6")),
+                new Bishop(Color.BLACK, Position.from("a7")))), Color.WHITE);
+
+        Score score = running.score();
+
+        assertAll(()-> {
+            assertThat(score.getBlackScore()).isEqualTo(new BigDecimal("3.0"));
+            assertThat(score.getWhiteScore()).isEqualTo(new BigDecimal("7.5"));
+        });
+    }
+
+    @Test
+    @DisplayName("start메서드를 실행하면 예외 발생")
+    void throwExceptionCallStart() {
+        assertThatThrownBy(() -> running.start())
+            .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("isFinished 호출 시 항상 false를 반환")
+    void isFinished() {
+        assertThat(running.isFinished()).isFalse();
+    }
+
+    @Test
+    @DisplayName("isGameEnd 호출 시 항상 false를 반환")
+    void isGameEnd() {
+        assertThat(running.isGameEnd()).isFalse();
+    }
+
+    @Test
+    @DisplayName("winner 메서드를 호출 시 예외 발생")
+    void throwExceptionCallWinner() {
+        assertThatThrownBy(() -> running.winner())
+            .isInstanceOf(UnsupportedOperationException.class);
     }
 }
