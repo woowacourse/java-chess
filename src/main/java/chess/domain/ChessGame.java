c8cfbcbc229cc2866310c7fc8741fab6f899e5ac
nailseong <nnis9857@gmail.com>
2022-03-29 15:46:01 +0900

    refactor(ChessGame): 현재 턴 확인 기능 리팩토링

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -5,6 +5,7 @@ import chess.domain.chesspiece.ChessPiece;
 import chess.domain.chesspiece.Color;
 import chess.domain.position.Position;
 import java.util.Map;
+import java.util.Optional;
 
 public class ChessGame {
 
@@ -19,10 +20,7 @@ public class ChessGame {
     }
 
     public void move(final Position from, final Position to) {
-        final ChessPiece movablePiece = chessBoard.findPiece(from)
-                .orElseThrow(() -> new IllegalArgumentException("해당 위치에 기물이 존재하지 않습니다."));
-
-        checkCurrentTurn(movablePiece);
+        checkCurrentTurn(from);
         chessBoard.move(from, to);
         if (chessBoard.isKingDie(to)) {
             gameStatus = GameStatus.END;
@@ -30,8 +28,13 @@ public class ChessGame {
         currentTurnColor = currentTurnColor.toOpposite();
     }
 
-    private void checkCurrentTurn(final ChessPiece movablePiece) {
-        if (!movablePiece.isSameColor(currentTurnColor)) {
+    private void checkCurrentTurn(final Position from) {
+        final Optional<ChessPiece> possiblePiece = chessBoard.findPiece(from);
+        if (possiblePiece.isEmpty()) {
+            return;
+        }
+        final ChessPiece chessPiece = possiblePiece.get();
+        if (!chessPiece.isSameColor(currentTurnColor)) {
             throw new IllegalArgumentException(currentTurnColor.name() + "의 차례입니다.");
         }
     }
