97e931bfaa8028d3293c16abb8848265bdb0322e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-03-30 03:46:50 +0900

    feat: AbstractPiece 제거 및 조합 사용 변경

---
+++ src/main/java/chess/domain/piece/Piece.java
@@ -0,0 +1,48 @@
+package chess.domain.piece;
+
+import chess.domain.ChessBoard;
+import chess.domain.Color;
+import chess.domain.Position;
+
+public final class Piece {
+
+    private final Color color;
+    private final PieceRule pieceRule;
+
+    public Piece(final Color color, final PieceRule pieceRule) {
+        this.color = color;
+        this.pieceRule = pieceRule;
+    }
+
+    public Piece move(Position source, Position target, ChessBoard chessBoard) {
+        return new Piece(color, pieceRule.move(source, target, chessBoard));
+    }
+
+    public String convertedName() {
+        return pieceRule.convertedName(color);
+    }
+
+    public boolean isSameColor(Color color) {
+        return this.color == color;
+    }
+
+    public boolean isSameTeamPiece(Piece piece) {
+        return isSameColor(piece.color);
+    }
+
+    public double score() {
+        return pieceRule.score();
+    }
+
+    public boolean isPawn() {
+        return pieceRule.isPawn();
+    }
+
+    public boolean isKing() {
+        return pieceRule.isKing();
+    }
+
+    public Color color() {
+        return color;
+    }
+}
