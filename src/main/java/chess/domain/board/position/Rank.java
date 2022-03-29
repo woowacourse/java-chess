2454d0cedfa20a15babedd65f2386018e087d403
= <rerub0831@gmail.com>
2022-03-29 17:27:39 +0900

    refactor: Rank에서 letter 제거

--- src/main/java/chess/domain/board/position/Rank.java
+++ src/main/java/chess/domain/board/position/Rank.java
@@ -4,23 +4,21 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    ONE(1, "1"),
-    TWO(2, "2"),
-    THREE(3, "3"),
-    FOUR(4, "4"),
-    FIVE(5, "5"),
-    SIX(6, "6"),
-    SEVEN(7, "7"),
-    EIGHT(8, "8");
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8);
 
     private static final String NO_NUMBER_ERROR_MESSAGE = "잘못된 위치 값 입니다.";
 
     private final int number;
-    private final String condition;
 
-    Rank(int number, String condition) {
+    Rank(int number) {
         this.number = number;
-        this.condition = condition;
     }
 
     public static Rank numberOf(int value) {
@@ -28,17 +26,6 @@ public enum Rank {
                 .filter(rank -> rank.number == value)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(NO_NUMBER_ERROR_MESSAGE));
-    }
-
-    public static Rank conditionOf(String value) {
-        return Arrays.stream(values())
-                .filter(rank -> rank.condition.equals(value))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(NO_NUMBER_ERROR_MESSAGE));
-    }
-
-    public String getCondition() {
-        return condition;
     }
 
     public int getNumber() {
