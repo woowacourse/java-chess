ca7d7600a9edf2bee64dd458fc90ca4d8c7235c6
yoon <duddbs774@gmail.com>
2022-03-30 01:16:31 +0900

    test: 사용하지 않는 테스트 삭제

--- src/test/java/chess/domain/board/BoardTest.java
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -24,29 +24,6 @@ public class BoardTest {
                 .hasMessage("해당 위치에 기물이 존재하지 않습니다.");
     }
 
-//    @Test
-//    @DisplayName("보드판에 king 이 없는 경우 예외 발생")
-//    void moveWithOutKing() {
-//        final Board hasNoKingBoard = new Board(() -> new HashMap<>(Map.of(Position.from("e5"), new Pawn(Color.BLACK))));
-//
-//        assertThatThrownBy(() -> hasNoKingBoard.move(Position.from("e5"), Position.from("e4"), Color.BLACK))
-//                .isInstanceOf(IllegalStateException.class)
-//                .hasMessage("해당 보드판에 king 이 존재하지 않습니다.");
-//    }
-
-//    @Test
-//    @DisplayName("보드판에 king 이 한 개만 존재하는 경우 예외 발생")
-//    void moveOnlyOneKing() {
-//        final Board hasOneKingBoard = new Board(() -> new HashMap<>(Map.of(
-//                Position.from("e5"), new Pawn(Color.BLACK),
-//                Position.from("a5"), new King(Color.BLACK)
-//        )));
-//
-//        assertThatThrownBy(() -> hasOneKingBoard.move(Position.from("e5"), Position.from("e4"), Color.BLACK))
-//                .isInstanceOf(IllegalStateException.class)
-//                .hasMessage("해당 보드판에 king 이 존재하지 않습니다.");
-//    }
-
     @Test
     @DisplayName("보드판에 king 이 두 개 존재하는 경우")
     void moveHasTwoKing() {
