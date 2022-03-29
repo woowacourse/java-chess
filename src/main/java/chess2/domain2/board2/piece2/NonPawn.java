90cd648809038a783b96822687b015578aefdfaa
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-29 18:28:19 +0900

    test(NonPawn): add missing tests on move

--- src/main/java/chess2/domain2/board2/piece2/NonPawn.java
+++ src/main/java/chess2/domain2/board2/piece2/NonPawn.java
@@ -4,8 +4,17 @@ import chess2.domain2.board2.position.Position;
 
 public final class NonPawn extends Piece {
 
+    private static final String INVALID_TYPE_EXCEPTION_MESSAGE = "해당 타입은 폰이 될 수 없습니다.";
+
     public NonPawn(Color color, PieceType type) {
         super(color, type);
+        validateNonPawn();
+    }
+
+    private void validateNonPawn() {
+        if (hasTypeOf(PieceType.PAWN)) {
+            throw new IllegalArgumentException(INVALID_TYPE_EXCEPTION_MESSAGE);
+        }
     }
 
     @Override
@@ -15,6 +24,6 @@ public final class NonPawn extends Piece {
 
     @Override
     protected boolean isAttackableRoute(Position from, Position to) {
-        return type.isMovable(from, to);
+        return canMove(from, to);
     }
 }
