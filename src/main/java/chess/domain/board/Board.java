cee025ba28aadc4c8a077cf8db54057cb64bfbf1
EastHShin <xldk78@gmail.com>
2022-03-29 21:36:00 +0900

    refactor(domain/board/Board): Board 를 초기화하는 책임을 BoardInitializer 로 이전

--- src/main/java/chess/domain/board/Board.java
+++ src/main/java/chess/domain/board/Board.java
@@ -1,35 +1,17 @@
 package chess.domain.board;
 
-import static chess.domain.Color.*;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.TreeMap;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import chess.domain.Color;
-import chess.domain.piece.Bishop;
-import chess.domain.piece.King;
-import chess.domain.piece.Knight;
-import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
-import chess.domain.piece.Queen;
-import chess.domain.piece.Rook;
 
 public final class Board {
-	private static final Position ROOK_INITIAL_POSITION = new Position(Column.A, Row.ONE);
-	private static final Position KNIGHT_INITIAL_POSITION = new Position(Column.B, Row.ONE);
-	private static final Position BISHOP_INITIAL_POSITION = new Position(Column.C, Row.ONE);
-	private static final Position QUEEN_INITIAL_POSITION = new Position(Column.D, Row.ONE);
-	private static final Position KING_INITIAL_POSITION = new Position(Column.E, Row.ONE);
-	private static final Row PAWN_INITIAL_ROW = Row.TWO;
-	private static final int BLANK_INITIAL_START_ROW_INDEX = 2;
-	private static final int BLANK_INITIAL_END_ROW_INDEX = 5;
+
 	private static final String SAME_CAMP_MOVE_EXCEPTION = "같은 팀 기물이 있는 위치로는 이동할 수 없습니다.";
 	private static final String EMPTY_SPACE_EXCEPTION = "이동할 수 있는 기물이 없습니다.";
 	private static final String INVALID_TURN_EXCEPTION = "상대 진영의 차례입니다.";
@@ -41,48 +23,8 @@ public final class Board {
 	private boolean whiteTurn;
 
 	public Board() {
-		this.value = new TreeMap<>();
+		this.value = BoardInitializer.createBoard();
 		this.whiteTurn = true;
-		initializeFourPiecesOf(ROOK_INITIAL_POSITION, Rook::new);
-		initializeFourPiecesOf(KNIGHT_INITIAL_POSITION, Knight::new);
-		initializeFourPiecesOf(BISHOP_INITIAL_POSITION, Bishop::new);
-
-		initializeTwoPiecesOf(QUEEN_INITIAL_POSITION, Queen::new);
-		initializeTwoPiecesOf(KING_INITIAL_POSITION, King::new);
-
-		initializePawn();
-		initializeBlanks();
-	}
-
-	private void initializeFourPiecesOf(Position pieceInitialPosition,
-		Function<Color, Piece> pieceConstructor) {
-		value.put(pieceInitialPosition, pieceConstructor.apply(WHITE));
-		value.put(pieceInitialPosition.flipHorizontally(), pieceConstructor.apply(WHITE));
-		value.put(pieceInitialPosition.flipVertically(), pieceConstructor.apply(BLACK));
-		value.put(pieceInitialPosition.flipDiagonally(), pieceConstructor.apply(BLACK));
-	}
-
-	private void initializeTwoPiecesOf(Position pieceInitialPosition, Function<Color, Piece> pieceConstructor) {
-		value.put(pieceInitialPosition, pieceConstructor.apply(WHITE));
-		value.put(pieceInitialPosition.flipVertically(), pieceConstructor.apply(BLACK));
-	}
-
-	private void initializePawn() {
-		for (Column column : Column.values()) {
-			initializeTwoPiecesOf(new Position(column, PAWN_INITIAL_ROW), Pawn::new);
-		}
-	}
-
-	private void initializeBlanks() {
-		for (Column column : Column.values()) {
-			initializeBlankColumn(column);
-		}
-	}
-
-	private void initializeBlankColumn(Column column) {
-		for (int i = BLANK_INITIAL_START_ROW_INDEX; i <= BLANK_INITIAL_END_ROW_INDEX; i++) {
-			value.put(new Position(column, Row.values()[i]), null);
-		}
 	}
 
 	public void move(Position beforePosition, Position afterPosition) {
