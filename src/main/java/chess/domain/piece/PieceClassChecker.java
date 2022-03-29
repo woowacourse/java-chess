56ef149fda897a6ae2f08a2fcd1e9078c0363e86
RunaNam <nmj5012@naver.com>
2022-03-29 22:53:05 +0900

    refactor(PieceClassChecker): 클래스 검증 로직 수정

--- src/main/java/chess/domain/piece/PieceClassChecker.java
+++ src/main/java/chess/domain/piece/PieceClassChecker.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum PieceClassChecker {
-    KING((piece) -> piece.getClass().getSimpleName().equals("King")),
-    QUEEN((piece) -> piece.getClass().getSimpleName().equals("Queen")),
-    BISHOP((piece) -> piece.getClass().getSimpleName().equals("Bishop")),
-    KNIGHT((piece -> piece.getClass().getSimpleName().equals("Knight"))),
-    ROOK((piece) -> piece.getClass().getSimpleName().equals("Rook")),
-    PAWN((piece) -> piece.getClass().getSimpleName().equals("Pawn")),
-    INVALID((piece) -> piece.getClass().getSimpleName().equals("InvalidPiece")),
+    KING((piece) -> checkPieceClass(piece, King.class.getName())),
+    QUEEN((piece) -> checkPieceClass(piece, Queen.class.getName())),
+    BISHOP((piece) -> checkPieceClass(piece, Bishop.class.getName())),
+    KNIGHT((piece) -> checkPieceClass(piece, Knight.class.getName())),
+    ROOK((piece) -> checkPieceClass(piece, Rook.class.getName())),
+    PAWN((piece) -> checkPieceClass(piece, Pawn.class.getName())),
+    INVALID((piece) -> checkPieceClass(piece, InvalidPiece.class.getName())),
     ;
+
+    private static boolean checkPieceClass(Piece piece, String targetPiece) {
+        return piece.getClass().getName().equals(targetPiece);
+    }
 
     private final Predicate<Piece> piecePredicate;
 
@@ -21,8 +25,8 @@ public enum PieceClassChecker {
 
     public static PieceClassChecker from(Piece piece) {
         return Arrays.stream(values())
-            .filter(score -> score.piecePredicate.test(piece))
-            .findAny()
-            .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 기물입니다."));
+                .filter(score -> score.piecePredicate.test(piece))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 기물입니다."));
     }
 }
