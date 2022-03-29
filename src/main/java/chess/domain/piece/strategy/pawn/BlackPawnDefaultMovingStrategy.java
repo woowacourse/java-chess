bbef48513c1673a508073fe998f729e0a36d671d
hyeonic <dev.hyeonic@gmail.com>
2022-03-29 17:03:01 +0900

    feat: Black Pawn 기본 이동 전략 추가

---
+++ src/main/java/chess/domain/piece/strategy/pawn/BlackPawnDefaultMovingStrategy.java
@@ -0,0 +1,41 @@
+package chess.domain.piece.strategy.pawn;
+
+import chess.domain.piece.Piece;
+import chess.domain.position.Direction;
+import chess.domain.position.Position;
+import java.util.List;
+
+public class BlackPawnDefaultMovingStrategy {
+
+    private static final Direction MOVABLE_DIRECTION = Direction.BOTTOM;
+
+    public boolean canMove(List<List<Piece>> board, Position source, Position target) {
+        Direction direction = Direction.of(source, target);
+        double distance = calculateDistance(source, target);
+
+        return direction == MOVABLE_DIRECTION && canMoveOnePosition(board, distance, source);
+    }
+
+    private double calculateDistance(Position source, Position target) {
+        int rankLength = Math.abs(source.getRankIndex() - target.getRankIndex());
+        int fileLength = Math.abs(source.getFileIndex() - target.getFileIndex());
+
+        return Math.sqrt(square(rankLength) + square(fileLength));
+    }
+
+    private int square(int value) {
+        return value * value;
+    }
+
+    private boolean canMoveOnePosition(List<List<Piece>> board, double distance, Position source) {
+        Piece targetPosition = findPiece(board, source.add(MOVABLE_DIRECTION));
+        return distance == 1 && targetPosition.isEmpty();
+    }
+
+    private Piece findPiece(List<List<Piece>> board, Position position) {
+        int rankIndex = position.getRankIndex();
+        int fileIndex = position.getFileIndex();
+
+        return board.get(rankIndex).get(fileIndex);
+    }
+}
--- src/test/java/chess/domain/piece/strategy/pawn/BlackPawnDefaultMovingStrategyTest.java
+++ src/test/java/chess/domain/piece/strategy/pawn/BlackPawnDefaultMovingStrategyTest.java
@@ -15,13 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class BlackPawnStartingPointMovingStrategyTest {
+class BlackPawnDefaultMovingStrategyTest {
 
-    @DisplayName("Black Pawn은 시작점에서 1칸 혹은 2칸 이동 가능하다.")
+    @DisplayName("Pawn은 보통의 경우 앞으로 1칸 이동 가능하다.")
     @ParameterizedTest
-    @CsvSource({"a7,a6,true", "a7,a5,true", "a7,a4,false", "a7,a8,false", "a6,a5,false"})
-    void pawn_시작점에서_1칸_2칸_이동_가능하다(String source, String target, boolean expected) {
-        BlackPawnStartingPointMovingStrategy movingStrategy = new BlackPawnStartingPointMovingStrategy();
+    @CsvSource({"a6,a5,true", "a5,a4,true", "a6,a4,false", "a6,b5,false", "a6,b4,false"})
+    void pawn_1칸_이동_가능하다(String source, String target, boolean expected) {
+        BlackPawnDefaultMovingStrategy movingStrategy = new BlackPawnDefaultMovingStrategy();
         ChessBoard chessBoard = generateEmptyChessBoard();
         List<List<Piece>> board = chessBoard.getBoard();
         setPiece(board, source, new Pawn(Color.BLACK));
@@ -31,16 +31,16 @@ class BlackPawnStartingPointMovingStrategyTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @DisplayName("Black Pawn은 시작점에서 2칸 이동할 때 앞에 기물이 존재하면 이동할 수 없다.")
+    @DisplayName("Pawn은 앞에 기물이 존재하는 경우 이동할 수 없다.")
     @Test
-    void pawn_시작점에서_2칸_이동할_때_기물이_존재하면_이동_불가능하다() {
-        WhitePawnStartingPointMovingStrategy movingStrategy = new WhitePawnStartingPointMovingStrategy();
+    void pawn_이동할_때_기물이_존재하면_이동_불가능하다() {
+        BlackPawnDefaultMovingStrategy movingStrategy = new BlackPawnDefaultMovingStrategy();
         ChessBoard chessBoard = generateEmptyChessBoard();
         List<List<Piece>> board = chessBoard.getBoard();
-        setPiece(board, "a7", new Pawn(Color.BLACK));
         setPiece(board, "a6", new Pawn(Color.BLACK));
+        setPiece(board, "a5", new Pawn(Color.BLACK));
 
-        boolean result = movingStrategy.canMove(board, new Position("a7"), new Position("a6"));
+        boolean result = movingStrategy.canMove(board, new Position("a6"), new Position("a5"));
 
         assertThat(result).isFalse();
     }
