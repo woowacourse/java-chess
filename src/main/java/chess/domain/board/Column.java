dfb25ec7295ad8733aea5a2445ce348309aad56e
skullKim <kyk990328@gmail.com>
2022-03-29 17:53:06 +0900

    refactor(Column.java): 불필요한 필드 삭제
    기존 기능으로 대체 가능한 부분 삭제
    
    BREAKING CHANGE: 필드 삭제
    as-is:
    public enum Column {
    
        a(1),
        b(2),
        c(3),
        d(4),
        e(5),
        f(6),
        g(7),
        h(8);
    
        private final int value;
    
        Column(final int value) {
            this.value = value;
        }
    
        public static Column from(final int value) {
            return Arrays.stream(values())
                    .filter(column -> column.value == value)
                    .findFirst()
                    .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 위치입니다."));
        }
    
        public int calculateDifference(final Column target) {
            return this.value - target.value;
        }
    
        public Column move(final int columnDifference) {
            return from(value + columnDifference);
        }
    }
    
    to-be:
    public enum Column {
    
        a,
        b,
        c,
        d,
        e,
        f,
        g,
        h;
    
        public static Column from(final int value) {
    
            return Arrays.stream(values())
                    .filter(column -> column.ordinal() == value)
                    .findFirst()
                    .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 위치입니다."));
        }
    
        public int calculateDifference(final Column target) {
            return this.ordinal() - target.ordinal();
        }
    
        public Column move(final int columnDifference) {
            return from(ordinal() + columnDifference);
        }
    }

--- src/main/java/chess/domain/board/Column.java
+++ src/main/java/chess/domain/board/Column.java
@@ -4,33 +4,28 @@ import java.util.Arrays;
 
 public enum Column {
 
-    a(1),
-    b(2),
-    c(3),
-    d(4),
-    e(5),
-    f(6),
-    g(7),
-    h(8);
-
-    private final int value;
-
-    Column(final int value) {
-        this.value = value;
-    }
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h;
 
     public static Column from(final int value) {
+
         return Arrays.stream(values())
-                .filter(column -> column.value == value)
+                .filter(column -> column.ordinal() == value)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 위치입니다."));
     }
 
     public int calculateDifference(final Column target) {
-        return this.value - target.value;
+        return this.ordinal() - target.ordinal();
     }
 
     public Column move(final int columnDifference) {
-        return from(value + columnDifference);
+        return from(ordinal() + columnDifference);
     }
 }
