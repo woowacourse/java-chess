68bcfb0f47d62b8a5f650605fd77cf65189c6ba5
youngwooyoo <fbduddn97@naver.com>
2022-03-29 14:42:19 +0900

    feat(PieceFixture): 폰 피스, 블랙 피스들 추가 및 피스들 상수처리

--- src/test/java/chess/domain/PieceFixture.java
+++ src/test/java/chess/domain/PieceFixture.java
@@ -4,12 +4,20 @@ import chess.domain.piece.*;
 
 public class PieceFixture {
 
-    private static final Team whiteTeam = Team.WHITE;
+    private static final Team WHITE_TEAM = Team.WHITE;
+    private static final Team BLACK_TEAM = Team.BLACK;
 
-    public static Rook whiteRook = new Rook(whiteTeam, "r");
-    public static Knight whiteKnight = new Knight(whiteTeam, "n");
-    public static Bishop whiteBishop = new Bishop(whiteTeam, "b");
-    public static Queen whiteQueen = new Queen(whiteTeam, "q");
-    public static King whiteKing = new King(whiteTeam, "k");
+    public static final Pawn WHITE_PAWN = new Pawn(WHITE_TEAM, "p");
+    public static final Rook WHITE_ROOK = new Rook(WHITE_TEAM, "r");
+    public static final Knight WHITE_KNIGHT = new Knight(WHITE_TEAM, "n");
+    public static final Bishop WHITE_BISHOP = new Bishop(WHITE_TEAM, "b");
+    public static final Queen WHITE_QUEEN = new Queen(WHITE_TEAM, "q");
+    public static final King WHITE_KING = new King(WHITE_TEAM, "k");
 
+    public static final Pawn BLACK_PAWN = new Pawn(BLACK_TEAM, "P");
+    public static final Rook BLACK_ROOK = new Rook(BLACK_TEAM, "R");
+    public static final Knight BLACK_KNIGHT = new Knight(BLACK_TEAM, "N");
+    public static final Bishop BLACK_BISHOP = new Bishop(BLACK_TEAM, "B");
+    public static final Queen BLACK_QUEEN = new Queen(BLACK_TEAM, "Q");
+    public static final King BLACK_KING = new King(BLACK_TEAM, "K");
 }
