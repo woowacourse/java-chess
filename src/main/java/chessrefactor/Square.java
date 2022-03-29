47b34430a42b0182e768db6762b85f30d8502c43
leo0842 <kunga077@gmail.com>
2022-03-29 23:37:01 +0900

    feat: 각각의 칸을 의미하는 Square 객체 생성

---
+++ src/main/java/chessrefactor/Square.java
@@ -0,0 +1,36 @@
+package chessrefactor;
+
+public class Square {
+
+    private final Rank rank;
+    private final File file;
+
+    public Square(Rank rank, File file) {
+        this.rank = rank;
+        this.file = file;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Square square = (Square) o;
+
+        if (rank != square.rank) {
+            return false;
+        }
+        return file == square.file;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = rank != null ? rank.hashCode() : 0;
+        result = 31 * result + (file != null ? file.hashCode() : 0);
+        return result;
+    }
+}
