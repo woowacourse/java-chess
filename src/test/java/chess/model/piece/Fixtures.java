f93590c27a0f318c4e42ea9299ab7889081dc023
leo0842 <kunga077@gmail.com>
2022-03-30 16:41:29 +0900

    feat: 테스트시 자주 사용되는 변수 Fixtures 클래스에서 상수화

---
+++ src/test/java/chess/model/piece/Fixtures.java
@@ -0,0 +1,17 @@
+package chess.model.piece;
+
+import chess.square.File;
+import chess.square.Rank;
+import chess.square.Square;
+
+public class Fixtures {
+
+    public static final Square D4 = Square.of(Rank.FOUR, File.D);
+    public static final Square D5 = Square.of(Rank.FIVE, File.D);
+    public static final Square B6 = Square.of(Rank.SIX, File.B);
+    public static final Square B4 = Square.of(Rank.FOUR, File.B);
+    public static final Square F6 = Square.of(Rank.SIX, File.F);
+    public static final Square F4 = Square.of(Rank.FOUR, File.F);
+    public static final Square C5 = Square.of(Rank.FIVE, File.C);
+    public static final Square A7 = Square.of(Rank.SEVEN, File.A);
+}
