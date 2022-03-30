7f657ffd31c5b1bf89a15bd45d9b1c43cf8eed6f
sinb57 <sinb57@naver.com>
2022-03-31 05:56:45 +0900

    feat: Player 생성 및 이동경로와 공격경로 계산 로직 구현

---
+++ src/main/java/chess/domain/player/Player.java
@@ -0,0 +1,42 @@
+package chess.domain.player;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import chess.domain.Position;
+import chess.domain.piece.Piece;
+
+public class Player {
+
+    private final Map<Position, Piece> pieces;
+
+    public Player(final Map<Position, Piece> pieces) {
+        this.pieces = pieces;
+    }
+
+    public List<Position> calculateRouteToMove(final Position source, final Position target) {
+        final Piece piece = findPieceByPosition(source);
+        return piece.calculateRouteToMove(source, target);
+    }
+
+    public List<Position> calculateRouteToAttack(final Position source, final Position target) {
+        final Piece piece = findPieceByPosition(source);
+        return piece.calculateRouteToAttack(source, target);
+    }
+
+    private Piece findPieceByPosition(final Position position) {
+        validatePieceExist(position);
+        return pieces.get(position);
+    }
+
+    private void validatePieceExist(final Position position) {
+        if (!contains(position)) {
+            throw new NoSuchElementException("위치에 해당하는 기물을 찾을 수 없습니다.");
+        }
+    }
+
+    private boolean contains(final Position position) {
+        return pieces.containsKey(position);
+    }
+}
---
+++ src/test/java/chess/domain/player/PlayerTest.java
@@ -0,0 +1,90 @@
+package chess.domain.player;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import chess.domain.Position;
+import chess.domain.piece.movable.Pawn;
+import chess.domain.piece.movable.multiple.Queen;
+
+class PlayerTest {
+
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player(Map.of(
+                Position.from("a2"), Pawn.getWhitePawn(),
+                Position.from("d1"), Queen.getInstance()
+        ));
+    }
+
+    @DisplayName("보유한 기물 중 하나는 출발지에 위치해야만 한다.")
+    @Test
+    void noSuchPiecePlacedAtPosition() {
+        assertThatThrownBy(() -> player.calculateRouteToMove(Position.from("a8"), Position.from("a7")))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessageContaining("위치에 해당하는 기물을 찾을 수 없습니다.");
+    }
+
+    @DisplayName("보유한 기물로부터 출발지에서 목적지까지의 이동경로를 계산할 수 있어야 한다.")
+    @ParameterizedTest(name = "[{index}] 출발지 : {0}, 도착지 : {1}, 이동경로 : {2}")
+    @MethodSource("provideForCalculateRouteToMove")
+    void calculateRouteToMove(final Position source, final Position target, final List<Position> expectedRoute) {
+        final List<Position> actualRoute = player.calculateRouteToMove(source, target);
+        assertThat(actualRoute).isEqualTo(expectedRoute);
+    }
+
+    private static Stream<Arguments> provideForCalculateRouteToMove() {
+        return Stream.of(
+                Arguments.of(
+                        Position.from("a2"),
+                        Position.from("a4"),
+                        List.of(Position.from("a3"), Position.from("a4"))
+                ),
+                Arguments.of(
+                        Position.from("d1"),
+                        Position.from("d4"),
+                        List.of(Position.from("d2"), Position.from("d3"), Position.from("d4"))
+                )
+        );
+    }
+
+    @DisplayName("보유한 기물로부터 출발지에서 목적지까지의 공격경로를 계산할 수 있어야 한다.")
+    @ParameterizedTest(name = "[{index}] 출발지 : {0}, 도착지 : {1}, 공격경로 : {2}")
+    @MethodSource("provideForCalculateRouteToAttack")
+    void calculateRouteToAttack(final Position source, final Position target, final List<Position> expectedRoute) {
+        final List<Position> actualRoute = player.calculateRouteToAttack(source, target);
+        assertThat(actualRoute).isEqualTo(expectedRoute);
+    }
+
+
+    private static Stream<Arguments> provideForCalculateRouteToAttack() {
+        return Stream.of(
+                Arguments.of(
+                        Position.from("a2"),
+                        Position.from("b3"),
+                        List.of(Position.from("b3"))
+                ),
+                Arguments.of(
+                        Position.from("d1"),
+                        Position.from("d4"),
+                        List.of(Position.from("d2"), Position.from("d3"), Position.from("d4"))
+                )
+        );
+    }
+
+
+}
