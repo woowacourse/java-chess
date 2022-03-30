7722995a739927729ac41bae7b3ecbe4dabc1342
tonic523 <diddmlvkf@gmail.com>
2022-03-30 17:56:14 +0900

    refactor(Knight): 행마법 조건 수정

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -2,15 +2,16 @@ package chess.domain.piece;
 
 import chess.domain.board.Position;
 import chess.domain.piece.vo.TeamColor;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
 
 public class Knight extends Piece {
 
     private static final double SCORE = 2.5;
+    private static final List<Double> DIRECTION_VALUES = Arrays.asList(Math.atan2(1, 2), Math.atan2(2, 1));
     private static final BiPredicate<Integer, Integer> movingCondition =
-            (rankDifference, fileDifference) ->
-                    (fileDifference == 2 && rankDifference == 1) || (fileDifference == 1 && rankDifference == 2);
+            (rankDifference, fileDifference) -> DIRECTION_VALUES.contains(Math.atan2(rankDifference, fileDifference));
 
     public Knight(final TeamColor teamColor, final Position position) {
         super(teamColor, position);
