0c7c990e16aebfe189212c592de63370d5b4beca
김찬 <gmelddl514@gmail.com>
2022-03-30 23:26:31 +0900

    refactor : 2중 for문 제거

--- src/main/java/chess/domain/board/InitialBoard.java
+++ src/main/java/chess/domain/board/InitialBoard.java
@@ -1,10 +1,5 @@
 package chess.domain.board;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import chess.domain.board.coordinate.Column;
 import chess.domain.board.coordinate.Coordinate;
 import chess.domain.board.coordinate.Row;
@@ -18,49 +13,56 @@ import chess.domain.piece.Queen;
 import chess.domain.piece.Rook;
 import chess.domain.piece.Symbol;
 import chess.domain.piece.Team;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public enum InitialBoard {
-	BLACK_BISHOP(Arrays.asList(Column.C, Column.F), Arrays.asList(Row.EIGHT), new Bishop(Symbol.BISHOP, Team.BLACK)),
-	WHITE_BISHOP(Arrays.asList(Column.C, Column.F), Arrays.asList(Row.ONE), new Bishop(Symbol.BISHOP, Team.WHITE)),
-	BLACK_KING(Arrays.asList(Column.E), Arrays.asList(Row.EIGHT), new King(Symbol.KING, Team.BLACK)),
-	WHITE_KING(Arrays.asList(Column.E), Arrays.asList(Row.ONE), new King(Symbol.KING, Team.WHITE)),
-	BLACK_KNIGHT(Arrays.asList(Column.B, Column.G), Arrays.asList(Row.EIGHT), new Knight(Symbol.KNIGHT, Team.BLACK)),
-	WHITE_KNIGHT(Arrays.asList(Column.B, Column.G), Arrays.asList(Row.ONE), new Knight(Symbol.KNIGHT, Team.WHITE)),
-	BLACK_PAWN(Arrays.asList(Column.values()), Arrays.asList(Row.SEVEN), new Pawn(Symbol.PAWN, Team.BLACK)),
-	WHITE_PAWN(Arrays.asList(Column.values()), Arrays.asList(Row.TWO), new Pawn(Symbol.PAWN, Team.WHITE)),
-	BLACK_QUEEN(Arrays.asList(Column.D), Arrays.asList(Row.EIGHT), new Queen(Symbol.QUEEN, Team.BLACK)),
-	WHITE_QUEEN(Arrays.asList(Column.D), Arrays.asList(Row.ONE), new Queen(Symbol.QUEEN, Team.WHITE)),
-	BLACK_ROOK(Arrays.asList(Column.A, Column.H), Arrays.asList(Row.EIGHT), new Rook(Symbol.ROOK, Team.BLACK)),
-	WHITE_ROOK(Arrays.asList(Column.A, Column.H), Arrays.asList(Row.ONE), new Rook(Symbol.ROOK, Team.WHITE)),
-	EMPTY(Arrays.asList(Column.values()),
-		Arrays.asList(Row.THREE, Row.FOUR, Row.FIVE, Row.SIX), new Empty(Symbol.EMPTY, Team.NONE));
+    BLACK_BISHOP(Arrays.asList(Column.C, Column.F), Arrays.asList(Row.EIGHT), new Bishop(Symbol.BISHOP, Team.BLACK)),
+    WHITE_BISHOP(Arrays.asList(Column.C, Column.F), Arrays.asList(Row.ONE), new Bishop(Symbol.BISHOP, Team.WHITE)),
+    BLACK_KING(Arrays.asList(Column.E), Arrays.asList(Row.EIGHT), new King(Symbol.KING, Team.BLACK)),
+    WHITE_KING(Arrays.asList(Column.E), Arrays.asList(Row.ONE), new King(Symbol.KING, Team.WHITE)),
+    BLACK_KNIGHT(Arrays.asList(Column.B, Column.G), Arrays.asList(Row.EIGHT), new Knight(Symbol.KNIGHT, Team.BLACK)),
+    WHITE_KNIGHT(Arrays.asList(Column.B, Column.G), Arrays.asList(Row.ONE), new Knight(Symbol.KNIGHT, Team.WHITE)),
+    BLACK_PAWN(Arrays.asList(Column.values()), Arrays.asList(Row.SEVEN), new Pawn(Symbol.PAWN, Team.BLACK)),
+    WHITE_PAWN(Arrays.asList(Column.values()), Arrays.asList(Row.TWO), new Pawn(Symbol.PAWN, Team.WHITE)),
+    BLACK_QUEEN(Arrays.asList(Column.D), Arrays.asList(Row.EIGHT), new Queen(Symbol.QUEEN, Team.BLACK)),
+    WHITE_QUEEN(Arrays.asList(Column.D), Arrays.asList(Row.ONE), new Queen(Symbol.QUEEN, Team.WHITE)),
+    BLACK_ROOK(Arrays.asList(Column.A, Column.H), Arrays.asList(Row.EIGHT), new Rook(Symbol.ROOK, Team.BLACK)),
+    WHITE_ROOK(Arrays.asList(Column.A, Column.H), Arrays.asList(Row.ONE), new Rook(Symbol.ROOK, Team.WHITE)),
+    EMPTY(Arrays.asList(Column.values()),
+            Arrays.asList(Row.THREE, Row.FOUR, Row.FIVE, Row.SIX), new Empty(Symbol.EMPTY, Team.NONE));
 
-	private final List<Column> columns;
-	private final List<Row> rows;
-	private final Piece piece;
+    private final List<Column> columns;
+    private final List<Row> rows;
+    private final Piece piece;
 
-	InitialBoard(List<Column> columns, List<Row> rows, Piece piece) {
-		this.columns = columns;
-		this.rows = rows;
-		this.piece = piece;
-	}
+    InitialBoard(List<Column> columns, List<Row> rows, Piece piece) {
+        this.columns = columns;
+        this.rows = rows;
+        this.piece = piece;
+    }
 
-	public static Map<Coordinate, Piece> init() {
-		Map<Coordinate, Piece> map = new HashMap<>();
-		for (Row row : Row.values()) {
-			for (Column column : Column.values()) {
-				map.put(Coordinate.of(column, row), findPiece(column, row));
-			}
-		}
-		return map;
-	}
+    public static Map<Coordinate, Piece> initialize() {
+        Map<Coordinate, Piece> map = new HashMap<>();
+        for (Row row : Row.values()) {
+            initializeInSameRow(map, row);
+        }
+        return map;
+    }
 
-	private static Piece findPiece(Column column, Row row) {
-		return Arrays.stream(InitialBoard.values())
-			.filter(board -> board.columns.contains(column))
-			.filter(board -> board.rows.contains(row))
-			.findFirst()
-			.map(board -> board.piece)
-			.orElseThrow(() -> new IllegalArgumentException("찾을 수 없습니다."));
-	}
+    private static void initializeInSameRow(Map<Coordinate, Piece> map, Row row) {
+        for (Column column : Column.values()) {
+            map.put(Coordinate.of(column, row), findPiece(column, row));
+        }
+    }
+
+    private static Piece findPiece(Column column, Row row) {
+        return Arrays.stream(InitialBoard.values())
+                .filter(board -> board.columns.contains(column) && board.rows.contains(row))
+                .findFirst()
+                .map(board -> board.piece)
+                .orElseThrow(() -> new IllegalArgumentException("찾을 수 없습니다."));
+    }
 }
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,35 +1,38 @@
 package chess.view;
 
-import java.util.Map;
-
 import chess.domain.board.coordinate.Column;
 import chess.domain.board.coordinate.Coordinate;
 import chess.domain.board.coordinate.Row;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Team;
+import java.util.Map;
 
 public class OutputView {
 
-	private OutputView() {
-	}
+    private OutputView() {
+    }
 
-	public static void printGameStart() {
-		System.out.println("체스 게임을 시작합니다.\n"
-			+ "게임 시작 : start\n"
-			+ "게임 종료 : end\n"
-			+ "게임 이동 : move source위치 target위치 - 예. move b2 b3");
-	}
+    public static void printGameStart() {
+        System.out.println("체스 게임을 시작합니다.\n"
+                + "게임 시작 : start\n"
+                + "게임 종료 : end\n"
+                + "게임 이동 : move source위치 target위치 - 예. move b2 b3");
+    }
 
-	public static void printBoard(Map<Coordinate, Piece> init) {
-		for (Row row : Row.values()) {
-			for (Column column : Column.values()) {
-				System.out.print(init.get(Coordinate.of(column, row)).getSymbol());
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void printStatus(Map<Team, Double> status) {
-		status.forEach((team, count) -> System.out.println(team + " : " + count + "점"));
-	}
+    public static void printBoard(Map<Coordinate, Piece> value) {
+        for (Row row : Row.values()) {
+            printBoardInSameRow(value, row);
+            System.out.println();
+        }
+    }
+
+    private static void printBoardInSameRow(Map<Coordinate, Piece> value, Row row) {
+        for (Column column : Column.values()) {
+            System.out.print(value.get(Coordinate.of(column, row)).getSymbol());
+        }
+    }
+
+    public static void printStatus(Map<Team, Double> status) {
+        status.forEach((team, count) -> System.out.println(team + " : " + count + "점"));
+    }
 }
