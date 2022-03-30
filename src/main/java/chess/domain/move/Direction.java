34e3b8e559df5bf48c9be6cefa44b3bae799b41b
jurlring <78091011+jurlring@users.noreply.github.com>
2022-03-30 18:36:21 +0900

    refactor : direction 찾는 로직 Math이용하여 수정

--- src/main/java/chess/domain/move/Direction.java
+++ src/main/java/chess/domain/move/Direction.java
@@ -3,45 +3,45 @@ package chess.domain.move;
 import chess.domain.position.File;
 import chess.domain.position.Rank;
 import java.util.Arrays;
-import java.util.function.BiPredicate;
 
 public enum Direction {
 
-    EAST(((file, rank) -> file > 0 && rank == 0), 1, 0),
-    WEST(((file, rank) -> file < 0 && rank == 0), -1, 0),
-    NORTH(((file, rank) -> file == 0 && rank > 0), 0, 1),
-    SOUTH(((file, rank) -> file == 0 && rank < 0), 0, -1),
-    NORTH_EAST(((file, rank) -> file > 0 && rank > 0 && file == rank), 1, 1),
-    NORTH_WEST(((file, rank) -> file < 0 && rank > 0 && -file == rank), -1, 1),
-    SOUTH_EAST(((file, rank) -> file > 0 && rank < 0 && -file == rank), 1, -1),
-    SOUTH_WEST(((file, rank) -> file < 0 && rank < 0 && file == rank), -1, -1),
-    KNIGHT_EAST_LEFT(((file, rank) -> file == 2 && rank == 1), 2, 1),
-    KNIGHT_EAST_RIGHT(((file, rank) -> file == 2 && rank == -1), 2, -1),
-    KNIGHT_WEST_LEFT(((file, rank) -> file == -2 && rank == -1), -2, 1),
-    KNIGHT_WEST_RIGHT(((file, rank) -> file == -2 && rank == 1), -2, -1),
-    KNIGHT_NORTH_LEFT(((file, rank) -> file == -1 && rank == 2), -1, 2),
-    KNIGHT_NORTH_RIGHT(((file, rank) -> file == 1 && rank == 2), 1, 2),
-    KNIGHT_SOUTH_LEFT(((file, rank) -> file == 1 && rank == -2), 1, -2),
-    KNIGHT_SOUTH_RIGHT(((file, rank) -> file == -1 && rank == -2), -1, -2),
-    NONE(((file, rank) -> false), 0, 0),
+    EAST(1, 0),
+    WEST(-1, 0),
+    NORTH(0, 1),
+    SOUTH(0, -1),
+    NORTH_EAST(1, 1),
+    NORTH_WEST(-1, 1),
+    SOUTH_EAST(1, -1),
+    SOUTH_WEST(-1, -1),
+    KNIGHT_EAST_LEFT(2, 1),
+    KNIGHT_EAST_RIGHT(2, -1),
+    KNIGHT_WEST_LEFT(-2, 1),
+    KNIGHT_WEST_RIGHT(-2, -1),
+    KNIGHT_NORTH_LEFT(-1, 2),
+    KNIGHT_NORTH_RIGHT(1, 2),
+    KNIGHT_SOUTH_LEFT(1, -2),
+    KNIGHT_SOUTH_RIGHT(-1, -2),
+    NONE(0, 0),
     ;
 
-    private final BiPredicate<Integer, Integer> condition;
     private final int fileDistance;
     private final int rankDistance;
 
-    Direction(final BiPredicate<Integer, Integer> condition,
-              final int fileDistance, final int rankDistance) {
-        this.condition = condition;
+    Direction(final int fileDistance, final int rankDistance) {
         this.fileDistance = fileDistance;
         this.rankDistance = rankDistance;
     }
 
     public static Direction of(final int fileDistance, final int rankDistance) {
         return Arrays.stream(values())
-                .filter(value -> value.condition.test(fileDistance, rankDistance))
+                .filter(value -> isSameDirection(value, fileDistance, rankDistance))
                 .findFirst()
                 .orElse(NONE);
+    }
+
+    private static boolean isSameDirection(final Direction direction, final int fileDistance, final int rankDistance) {
+        return Math.atan2(direction.fileDistance, direction.rankDistance) == Math.atan2(fileDistance, rankDistance);
     }
 
     public File moveFile(final File file) {
