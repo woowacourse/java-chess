0975cf071f134679f19216201c0b9001a837b4d9
her0807 <her0807@naver.com>
2022-03-30 13:31:56 +0900

    feat: member table 생성, 단건 조회, 조인 기능 구현

---
+++ src/test/java/chess/dao/MemberDaoTest.java
@@ -0,0 +1,48 @@
+package chess.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.Member;
+import java.sql.Connection;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class MemberDaoTest {
+
+    @Test
+    void connection() {
+        final MemberDao memberDao = new MemberDao();
+        final Connection connection = memberDao.getConnection();
+        assertThat(connection).isNotNull();
+    }
+
+    @Test
+    void save() {
+        final MemberDao memberDao = new MemberDao();
+        memberDao.save(new Member("sudal", "수달"));
+    }
+
+    @Test
+    void findById() {
+        final MemberDao memberDao = new MemberDao();
+        final Member member = new Member("sudal", "수달");
+        memberDao.findById(member.getId());
+        System.out.println(member);
+    }
+
+    @Test
+    void findWithRoleById() {
+        final MemberDao memberDao = new MemberDao();
+        final Member member = new Member("sudal", "수달");
+        Member find = memberDao.findWithRoleById(member.getId());
+        System.out.println(find);
+    }
+
+
+    @Test
+    void findAll() {
+        final MemberDao memberDao = new MemberDao();
+        final List<Member> members = memberDao.findAll();
+        assertThat(members).isNotEmpty();
+    }
+}
