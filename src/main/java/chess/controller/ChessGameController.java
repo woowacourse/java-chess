874755e279ecb69781f52e9622bf1af2c42ba27a
jayjaehunchoi <wogns0108@naver.com>
2022-03-30 12:07:02 +0900

    refactor : 컨트롤러에서 command 사용

--- src/main/java/chess/controller/ChessGameController.java
+++ src/main/java/chess/controller/ChessGameController.java
@@ -5,6 +5,7 @@ import chess.domain.board.BoardInitializer;
 import chess.domain.game.ChessGame;
 import chess.domain.game.Score;
 import chess.domain.piece.Team;
+import chess.view.Command;
 import chess.view.InputView;
 import chess.view.OutputView;
 import java.util.List;
@@ -14,9 +15,6 @@ public class ChessGameController {
     private static final int COMMAND_INDEX = 0;
     private static final int SOURCE_INDEX = 1;
     private static final int TARGET_INDEX = 2;
-    private static final String END_COMMAND_MESSAGE = "end";
-    private static final String MOVE_COMMAND_MESSAGE = "move";
-    private static final String STATUS_COMMAND_MESSAGE = "status";
 
     public void run() {
         OutputView.printStartGame();
@@ -45,20 +43,20 @@ public class ChessGameController {
     }
 
     private void move(final ChessGame chessGame, final List<String> inputs,final String commandMessage) {
-        if (commandMessage.equals(MOVE_COMMAND_MESSAGE)) {
+        if (Command.of(commandMessage) == Command.MOVE) {
             chessGame.move(inputs.get(SOURCE_INDEX), inputs.get(TARGET_INDEX));
             printCurrentBoard(chessGame);
         }
     }
 
     private void showStatus(final ChessGame chessGame, final String commandMessage) {
-        if (commandMessage.equals(STATUS_COMMAND_MESSAGE)) {
+        if (Command.of(commandMessage) == Command.STATUS) {
             printStatus(chessGame);
         }
     }
 
     private void endGame(final ChessGame chessGame, final String commandMessage) {
-        if (commandMessage.equals(END_COMMAND_MESSAGE)) {
+        if (Command.of(commandMessage) == Command.END) {
             chessGame.turnOff();
             printStatus(chessGame);
         }
