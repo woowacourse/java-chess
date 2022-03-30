947932023ae3effe9f28a7c3cec3b52cb99b7f1d
leo0842 <kunga077@gmail.com>
2022-03-30 17:49:30 +0900

    refactor: Square 생성 signature 변경

--- src/test/java/chess/model/piece/Fixtures.java
+++ src/test/java/chess/model/piece/Fixtures.java
@@ -6,12 +6,21 @@ import chess.square.Square;
 
 public class Fixtures {
 
-    public static final Square D4 = Square.of(Rank.FOUR, File.D);
-    public static final Square D5 = Square.of(Rank.FIVE, File.D);
-    public static final Square B6 = Square.of(Rank.SIX, File.B);
-    public static final Square B4 = Square.of(Rank.FOUR, File.B);
-    public static final Square F6 = Square.of(Rank.SIX, File.F);
-    public static final Square F4 = Square.of(Rank.FOUR, File.F);
-    public static final Square C5 = Square.of(Rank.FIVE, File.C);
-    public static final Square A7 = Square.of(Rank.SEVEN, File.A);
+    public static final Square D4 = Square.of(File.D, Rank.FOUR);
+    public static final Square D5 = Square.of(File.D, Rank.FIVE);
+    public static final Square B6 = Square.of(File.B, Rank.SIX);
+    public static final Square B4 = Square.of(File.B, Rank.FOUR);
+    public static final Square F6 = Square.of(File.F, Rank.SIX);
+    public static final Square F4 = Square.of(File.F, Rank.FOUR);
+    public static final Square C5 = Square.of(File.C, Rank.FIVE);
+    public static final Square A7 = Square.of(File.A, Rank.SEVEN);
+
+    public static final Square E6 = Square.of(File.E, Rank.SIX);
+    public static final Square F5 = Square.of(File.C, Rank.SIX);
+    public static final Square F3 = Square.of(File.F, Rank.FIVE);
+    public static final Square E2 = Square.of(File.B, Rank.FIVE);
+    public static final Square C2 = Square.of(File.F, Rank.THREE);
+    public static final Square B3 = Square.of(File.B, Rank.THREE);
+    public static final Square B5 = Square.of(File.E, Rank.TWO);
+    public static final Square C6 = Square.of(File.C, Rank.TWO);
 }
