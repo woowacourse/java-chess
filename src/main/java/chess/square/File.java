1de88c88e4dec326c493ba81730cefb8a5d72377
leo0842 <kunga077@gmail.com>
2022-03-30 16:38:09 +0900

    feat: 존재하는 좌표인지와 거리 만큼의 좌표 반환 로직 구현

--- src/main/java/chess/square/File.java
+++ src/main/java/chess/square/File.java
@@ -1,14 +1,34 @@
 package chess.square;
 
+import java.util.Arrays;
+
 public enum File {
 
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-    H,
+    A(1),
+    B(2),
+    C(3),
+    D(4),
+    E(5),
+    F(6),
+    G(7),
+    H(8),
     ;
+
+    private final int value;
+
+    File(int value) {
+        this.value = value;
+    }
+
+    public boolean availableLocation(int distance) {
+        return Arrays.stream(File.values())
+                .anyMatch(file -> file.value == (this.value + distance));
+    }
+
+    public File nextTo(Integer value) {
+        return Arrays.stream(File.values())
+                .filter(file -> file.value == (this.value + value))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
--- src/main/java/chess/square/Rank.java
+++ src/main/java/chess/square/Rank.java
@@ -1,14 +1,34 @@
 package chess.square;
 
+import java.util.Arrays;
+
 public enum Rank {
 
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
     ;
+
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public boolean availableLocation(int distance) {
+        return Arrays.stream(Rank.values())
+                .anyMatch(rank -> rank.value == (this.value + distance));
+    }
+
+    public Rank nextTo(Integer value) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.value == this.value + value)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
