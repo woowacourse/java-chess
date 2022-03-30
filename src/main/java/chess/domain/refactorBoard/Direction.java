8554c651200fb079bd0119c713511ab06de48cb7
Wishoon <heonga2@gmail.com>
2022-03-31 01:04:38 +0900

    fix : 상수 필드 이름 수정

--- src/main/java/chess/domain/refactorBoard/Direction.java
+++ src/main/java/chess/domain/refactorBoard/Direction.java
@@ -6,21 +6,21 @@ import java.util.List;
 
 public enum Direction {
 
-    UP(0, 1),
+    TOP(0, 1),
     DOWN(0, -1),
     RIGHT(1, 0),
     LEFT(-1, 0),
 
-    UP_RIGHT(1, 1),
-    UP_LEFT(-1, 1),
+    TOP_RIGHT(1, 1),
+    TOP_LEFT(-1, 1),
     DOWN_RIGHT(1, -1),
     DOWN_LEFT(-1, -1),
 
-    UP_UP_RIGHT(1, 2),
-    UP_UP_LEFT(-1, 2),
+    TOP_TOP_RIGHT(1, 2),
+    TOP_TOP_LEFT(-1, 2),
 
-    UP_RIGHT_RIGHT(2, 1),
-    UP_LEFT_LEFT(-2, 1),
+    TOP_RIGHT_RIGHT(2, 1),
+    TOP_LEFT_LEFT(-2, 1),
 
     DOWN_DOWN_RIGHT(1, -2),
     DOWN_DOWN_LEFT(-1, -2),
