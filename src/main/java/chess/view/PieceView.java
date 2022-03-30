87fb997f9129a54f8aec320f64b80ed8d70b988d
bcc0830 <bcc0830@naver.com>
2022-03-31 01:01:43 +0900

    refactor : enum의 인스턴스 필드를 Predicate로 변경

--- src/main/java/chess/view/PieceView.java
+++ src/main/java/chess/view/PieceView.java
@@ -1,42 +1,37 @@
 package chess.view;
 
-import chess.domain.piece.Bishop;
-import chess.domain.piece.King;
-import chess.domain.piece.Knight;
-import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
-import chess.domain.piece.Queen;
-import chess.domain.piece.Rook;
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 public enum PieceView {
 
-    BLACK_KING(King.createBlack(), "♔"),
-    WHITE_KING(King.createWhite(), "♚"),
-    BLACK_QUEEN(Queen.createBlack(), "♕"),
-    WHITE_QUEEN(Queen.createWhite(), "♛"),
-    BLACK_BISHOP(Bishop.createBlack(), "♗"),
-    WHITE_BISHOP(Bishop.createWhite(), "♝"),
-    BLACK_KNIGHT(Knight.createBlack(), "♘"),
-    WHITE_KNIGHT(Knight.createWhite(), "♞"),
-    BLACK_ROOK(Rook.createBlack(), "♖"),
-    WHITE_ROOK(Rook.createWhite(), "♜"),
-    BLACK_PAWN(Pawn.createBlack(), "♙"),
-    WHITE_PAWN(Pawn.createWhite(), "♟");
+    BLACK_KING((piece -> piece.isKing() && !piece.isWhite()), "♔"),
+    WHITE_KING((piece -> piece.isKing() && piece.isWhite()), "♚"),
+    BLACK_QUEEN((piece -> piece.isQueen() && !piece.isWhite()), "♕"),
+    WHITE_QUEEN((piece -> piece.isQueen() && piece.isWhite()), "♛"),
+    BLACK_BISHOP((piece -> piece.isBishop() && !piece.isWhite()), "♗"),
+    WHITE_BISHOP((piece -> piece.isBishop() && piece.isWhite()), "♝"),
+    BLACK_KNIGHT((piece -> piece.isKnight() && !piece.isWhite()), "♘"),
+    WHITE_KNIGHT((piece -> piece.isKnight() && piece.isWhite()), "♞"),
+    BLACK_ROOK((piece -> piece.isRook() && !piece.isWhite()), "♖"),
+    WHITE_ROOK((piece -> piece.isRook() && piece.isWhite()), "♜"),
+    BLACK_PAWN((piece -> piece.isPawn() && !piece.isWhite()), "♙"),
+    WHITE_PAWN((piece -> piece.isPawn() && piece.isWhite()), "♟");
 
     private static final String NOT_HAVE_SYMBOL = "심볼이 없는 피스입니다.";
 
-    private final Piece piece;
+    private final Predicate<Piece> piecePredicate;
     private final String symbol;
 
-    PieceView(Piece piece, String symbol) {
-        this.piece = piece;
+    PieceView(Predicate<Piece> piecePredicate, String symbol) {
+        this.piecePredicate = piecePredicate;
         this.symbol = symbol;
     }
 
     public static String from(Piece piece) {
         return Arrays.stream(values())
-                .filter(pieceView -> pieceView.piece.equals(piece))
+                .filter(pieceView -> pieceView.piecePredicate.test(piece))
                 .map(pieceView -> pieceView.symbol)
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(NOT_HAVE_SYMBOL));
