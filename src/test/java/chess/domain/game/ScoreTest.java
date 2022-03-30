4070a6ae2b1dae5686e507a7188cd677d22a824d
jojogreen91 <iawbg13@gmail.com>
2022-03-30 23:15:14 +0900

    test : ScoreTest 수정

--- src/test/java/chess/domain/game/ScoreTest.java
+++ src/test/java/chess/domain/game/ScoreTest.java
@@ -6,51 +6,50 @@ import chess.domain.board.Board;
 import chess.domain.board.CatchPieces;
 import chess.domain.board.Position;
 import chess.domain.piece.Color;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ScoreTest {
 
     CatchPieces catchPieces = new CatchPieces();
+    Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = Board.createChessBoard();
+    }
 
     @Test
     @DisplayName("폰이 수직으로 존재할 경우 총점을 계산한다.")
-    void getWhiteScoreWhenPawnVertical() {
-        Board board = Board.createChessBoard();
+    void getWhiteScore_WhenPawnVertical() {
         board.movePiece(Position.valueOf("a2"), Position.valueOf("b3"), catchPieces);
         board.movePiece(Position.valueOf("c2"), Position.valueOf("b4"), catchPieces);
 
-        Score score = new Score(board.getBoard());
-        assertThat(score.getWhiteScore()).isEqualTo(36.5);
+        assertThat(new Score(board.getBoard()).getWhiteScore()).isEqualTo(36.5);
     }
 
     @Test
-    @DisplayName("queen이 없고 폰이 수직으로 존재하는 경우 총점을 계산한다.")
-    void getWhiteScoreWhenNoQueenPawnVertical() {
-        Board board = Board.createChessBoard();
+    @DisplayName("Queen 이 없고 폰이 수직으로 존재하는 경우 총점을 계산한다.")
+    void getWhiteScore_WhenNoQueenAndPawnExistVertically() {
         board.movePiece(Position.valueOf("a2"), Position.valueOf("d1"), catchPieces);
 
-        Score score = new Score(board.getBoard());
-        assertThat(score.getWhiteScore()).isEqualTo(28);
+        assertThat(new Score(board.getBoard()).getWhiteScore()).isEqualTo(28);
     }
 
     @Test
-    @DisplayName("게임에서 이긴 플레이어의 Color 를 반환")
-    void getWinColorWhenBlackWin() {
-        Board board = Board.createChessBoard();
+    @DisplayName("Black 팀이 이겼을 때 게임에서 이긴 플레이어의 Color 를 반환")
+    void getWinColor_WhenBlackWin() {
         board.movePiece(Position.valueOf("a2"), Position.valueOf("d1"), catchPieces);
 
-        Score score = new Score(board.getBoard());
-        assertThat(score.getWinColor()).isEqualTo(Color.BLACK);
+        assertThat(new Score(board.getBoard()).getWinColor()).isEqualTo(Color.BLACK);
     }
 
     @Test
-    @DisplayName("게임에서 이긴 플레이어의 Color 를 반환")
-    void getWinColorWhenWhiteWin() {
-        Board board = Board.createChessBoard();
+    @DisplayName("White 팀이 이겼을 때 게임에서 이긴 플레이어의 Color 를 반환")
+    void getWinColor_WhenWhiteWin() {
         board.movePiece(Position.valueOf("a7"), Position.valueOf("b6"), catchPieces);
 
-        Score score = new Score(board.getBoard());
-        assertThat(score.getWinColor()).isEqualTo(Color.WHITE);
+        assertThat(new Score(board.getBoard()).getWinColor()).isEqualTo(Color.WHITE);
     }
 }
