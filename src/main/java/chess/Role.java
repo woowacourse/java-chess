c07486a0ef5dd2e26bafc77bab0db649f8004d85
seong-wooo <seongwoo0513@uos.ac.kr>
2022-03-31 00:19:47 +0900

    feat: memberDao 추가

---
+++ src/main/java/chess/Role.java
@@ -0,0 +1,14 @@
+package chess;
+
+public class Role {
+    private final String role;
+
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
