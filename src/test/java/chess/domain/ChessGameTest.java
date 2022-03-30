5811e6ebc45a584ade6733a64f65677fd64321e5
nailseong <nnis9857@gmail.com>
2022-03-30 17:22:58 +0900

    test(ChessGame): 킹을 잡는 이동 테스트

--- src/test/java/chess/domain/ChessGameTest.java
+++ src/test/java/chess/domain/ChessGameTest.java
@@ -1,10 +1,17 @@
 package chess.domain;
 
 import static chess.domain.position.Position.from;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import chess.domain.chessboard.ChessBoard;
 import chess.domain.chessboard.ChessBoardFactory;
+import chess.domain.chesspiece.ChessPiece;
+import chess.domain.chesspiece.Color;
+import chess.domain.chesspiece.King;
 import chess.domain.position.Position;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -33,5 +40,26 @@ class ChessGameTest {
         assertThatThrownBy(() -> chessGame.move(Position.from("a1"), Position.from("a2")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("게임이 시작되지 않았습니다.");
+    }
+
+    @Test
+    @DisplayName("킹을 잡으면 게임이 끝난다.")
+    void game_end_when_king_die() {
+        // given
+        final Position from = from("d5");
+        final Position to = from("d6");
+        final Map<Position, ChessPiece> pieceByPosition = new HashMap<>();
+        pieceByPosition.put(from, King.from(Color.WHITE));
+        pieceByPosition.put(to, King.from(Color.BLACK));
+        final ChessBoard chessBoard = new ChessBoard(pieceByPosition);
+        final ChessGame chessGame = new ChessGame(chessBoard);
+        chessGame.start();
+
+        // when
+        chessGame.move(from, to);
+        final boolean actual = chessGame.isFinish();
+
+        // then
+        assertThat(actual).isEqualTo(true);
     }
 }
