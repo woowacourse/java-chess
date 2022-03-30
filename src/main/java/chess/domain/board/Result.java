7b7451fabfcab29d3817aeb93de3989dbe580e15
yoon <duddbs774@gmail.com>
2022-03-31 01:07:54 +0900

    refactor: 빈값 추가

--- src/main/java/chess/domain/board/Result.java
+++ src/main/java/chess/domain/board/Result.java
@@ -1,16 +1,20 @@
-package chess.domain;
+package chess.domain.board;
 
-import chess.domain.piece.Color;
+import chess.domain.piece.notation.Color;
 
 public enum Result {
     BLACK_WIN,
     WHITE_WIN,
+    EMPTY,
     ;
 
     public static Result from(final Color color) {
         if (color == Color.BLACK) {
             return BLACK_WIN;
         }
-        return WHITE_WIN;
+        if (color == Color.WHITE) {
+            return WHITE_WIN;
+        }
+        return EMPTY;
     }
 }
