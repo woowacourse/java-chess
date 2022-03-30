a3401f093815fbdf52e64cc2d6f42158631fa772
devHudi <devhudi@gmail.com>
2022-03-30 13:21:40 +0900

    feat(Controller): 잘못된 명령을 입력받았을 경우 재입력받는 기능 구현

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -13,30 +13,37 @@ public class ChessController {
 
         OutputView.printCommandGuide();
 
+        // TODO: depth 줄이기
+        // TODO: while 대신 재귀 고려하기
+        // TODO: 메소드 길이를 줄이기 위해 메소드 추출하기
         while (true) {
-            List<String> commands = InputView.inputCommand();
-            String command = commands.get(0);
+            try {
+                List<String> commands = InputView.inputCommand();
+                String command = commands.get(0);
 
-            if (command.equals("start")) {
-                chessGame.startGame();
-                OutputView.printBoard(chessGame.getBoard());
-            }
+                if (command.equals("start")) {
+                    chessGame.startGame();
+                    OutputView.printBoard(chessGame.getBoard());
+                }
 
-            if (command.equals("move")) {
-                Position fromPosition = Position.from(commands.get(1));
-                Position toPosition = Position.from(commands.get(2));
+                if (command.equals("move")) {
+                    Position fromPosition = Position.from(commands.get(1));
+                    Position toPosition = Position.from(commands.get(2));
 
-                chessGame.movePiece(fromPosition, toPosition);
-                OutputView.printBoard(chessGame.getBoard());
-            }
+                    chessGame.movePiece(fromPosition, toPosition);
+                    OutputView.printBoard(chessGame.getBoard());
+                }
 
-            if (command.equals("status")) {
-                chessGame.showStatus();
-                OutputView.printScore(chessGame.getBoard());
-            }
+                if (command.equals("status")) {
+                    chessGame.showStatus();
+                    OutputView.printScore(chessGame.getBoard());
+                }
 
-            if (command.equals("end")) {
-                break;
+                if (command.equals("end")) {
+                    break;
+                }
+            } catch (Exception e) {
+                OutputView.printException(e);
             }
         }
     }
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -6,6 +6,7 @@ import chess.view.dto.BoardDto;
 
 public class OutputView {
 
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
     private static final String COMMAND_GUIDE_MESSAGE = "> 체스 게임을 시작합니다.%n"
             + "> 게임 시작 : start%n"
             + "> 게임 종료 : end%n"
@@ -23,5 +24,9 @@ public class OutputView {
     public static void printScore(Board board) {
         System.out.println("흰색팀 점수 : " + board.calculateScore(PieceColor.WHITE));
         System.out.println("검정색팀 점수 : " + board.calculateScore(PieceColor.BLACK));
+    }
+
+    public static void printException(Exception exception) {
+        System.out.println(ERROR_MESSAGE_PREFIX + exception.getMessage());
     }
 }
