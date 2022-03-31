c75f0f8a615ee7c59ee14796fc15c75e83dd2f3c
yeongunheo <gjduddns@gmail.com>
2022-03-31 21:45:32 +0900

    refactor: Command 클래스를 Enum 타입으로 변경

--- src/main/java/chess/controller/Command.java
+++ src/main/java/chess/controller/Command.java
@@ -1,9 +1,7 @@
 package chess.controller;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.function.BiFunction;
 import java.util.regex.Pattern;
@@ -14,25 +12,28 @@ import chess.domain.piece.position.Position;
 import chess.domain.piece.position.Rank;
 import chess.dto.GameResult;
 
-public class Command {
-    private static final Map<Pattern, BiFunction<ChessGame, List<String>, GameResult>> commands = new HashMap<>();
-
-    static {
-        commands.put(Pattern.compile("^(status)$"), (chessGame, ignored) -> GameResult.ofScore(chessGame.status()));
-
-        commands.put(Pattern.compile("^(start)$"), (chessGame, ignored) -> GameResult.ofBoard(chessGame.start()));
-        commands.put(Pattern.compile("^(end)$"), (chessGame, ignored) -> GameResult.ofBoard(chessGame.end()));
-        commands.put(Pattern.compile("^((move) ([a-h][1-8]) ([a-h][1-8]))$"),
+public enum Command {
+    STATUS(Pattern.compile("^(status)$"), (chessGame, ignored) -> GameResult.ofScore(chessGame.status())),
+    START(Pattern.compile("^(start)$"), (chessGame, ignored) -> GameResult.ofBoard(chessGame.start())),
+    END(Pattern.compile("^(end)$"), (chessGame, ignored) -> GameResult.ofBoard(chessGame.end())),
+    MOVE(Pattern.compile("^((move) ([a-h][1-8]) ([a-h][1-8]))$"),
             (chessGame, inputs) -> GameResult.ofBoard(chessGame.move(toPosition(inputs.get(1)), toPosition(inputs.get(2)))));
+
+    private Pattern pattern;
+    private BiFunction<ChessGame, List<String>, GameResult> operator;
+
+    Command(Pattern pattern, BiFunction<ChessGame, List<String>, GameResult> operator) {
+        this.pattern = pattern;
+        this.operator = operator;
     }
 
     public static GameResult act(String input, ChessGame chessGame) {
-        Pattern command = commands.keySet().stream()
-            .filter(pattern -> pattern.matcher(input).find())
-            .findFirst()
-            .orElseThrow(() -> new NoSuchElementException("명령어를 올바르게 입력해 주세요."));
-
-        return commands.get(command).apply(chessGame, Arrays.asList(input.split(" ")));
+        return Arrays.stream(Command.values())
+                .filter(pattern -> pattern.pattern.matcher(input).find())
+                .findFirst()
+                .map(command -> command.operator)
+                .map(operator -> operator.apply(chessGame, Arrays.asList(input.split(" "))))
+                .orElseThrow(() -> new NoSuchElementException("명령어를 올바르게 입력해 주세요."));
     }
 
     private static Position toPosition(String input) {
