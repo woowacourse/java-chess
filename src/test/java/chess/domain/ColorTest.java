751613e1c030fe17e125199ee4854901f3536f2c
sure-why-not <pkse1213@gmail.com>
2022-04-01 05:52:54 +0900

    test: Color 테스트 추가

--- src/test/java/chess/domain/ColorTest.java
+++ src/test/java/chess/domain/ColorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ColorTest {
 
@@ -11,6 +13,7 @@ public class ColorTest {
     @DisplayName("검은색 기물이면 대문자를 출력하는지")
     void upperCaseOnBlack() {
         Color color = Color.BLACK;
+
         assertThat(color.correctSignature("a")).isEqualTo("A");
     }
 
@@ -18,6 +21,29 @@ public class ColorTest {
     @DisplayName("흰색 기물이면 소문자를 출력하는지")
     void lowerCaseOnWhite() {
         Color color = Color.WHITE;
+
         assertThat(color.correctSignature("a")).isEqualTo("a");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"WHITE:BLACK", "BLACK:WHITE"}, delimiter = ':')
+    @DisplayName("상대편 색을 가져오는지")
+    void finEnemyColor(Color color, Color enemyColor) {
+        assertThat(color.enemyColor()).isEqualTo(enemyColor);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"WHITE:false", "BLACK:true"}, delimiter = ':')
+    @DisplayName("검정색인지")
+    void isBlack(Color color, boolean isBlack) {
+        assertThat(color.isBlack()).isEqualTo(isBlack);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"WHITE:true", "BLACK:false"}, delimiter = ':')
+    @DisplayName("흰색인지")
+    void isWhite(Color color, boolean isBlack) {
+        assertThat(color.isWhite()).isEqualTo(isBlack);
     }
 }
