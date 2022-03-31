a95e550880c1c3998fa94b7aabda3f37a22b834c
HyeonbinSa <shb1833@gmail.com>
2022-03-31 21:49:10 +0900

    refactor(PieceScore): PieceScore 내 symbol 미사용으로 삭제

--- src/main/java/domain/piece/PieceScore.java
+++ src/main/java/domain/piece/PieceScore.java
@@ -1,30 +1,19 @@
 package domain.piece;
 
-import java.util.Arrays;
-
 public enum PieceScore {
-    QUEEN(9, "Q"),
-    BISHOP(3, "B"),
-    KNIGHT(2.5, "N"),
-    ROOK(5, "R"),
-    PAWN(1, "P"),
-    KING(0, "K");
+    QUEEN(9),
+    BISHOP(3),
+    KNIGHT(2.5),
+    ROOK(5),
+    PAWN(1),
+    KING(0);
 
-    public static final double SEVERAL_PAWN_POINT = 0.5;
+    private static final double SEVERAL_PAWN_POINT = 0.5;
 
     private final double score;
-    private final String symbol;
 
-    PieceScore(final double score, final String symbol) {
+    PieceScore(final double score) {
         this.score = score;
-        this.symbol = symbol;
-    }
-
-    public static PieceScore of(String symbol) {
-        return Arrays.stream(PieceScore.values())
-            .filter(pieceScore -> pieceScore.symbol.equals(symbol))
-            .findFirst()
-            .orElse(null);
     }
 
     public double score(boolean isSeveralPawn) {
