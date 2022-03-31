7f125b7d81a54a4b1d17ab6de48c0ea51e66c698
asebn1 <asebn1@khu.ac.kr>
2022-03-31 11:26:28 +0900

    refactor: ChessBoardGenerator의 잘못된 team.Black 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,47 @@
-import controller.ChessController;
+import static domain.classification.InputCase.*;
+
+import domain.board.BoardGenerator;
+import domain.board.ChessBoard;
+import domain.board.ChessBoardGenerator;
+import domain.classification.InputCase;
+import domain.dto.StatusDto;
+import domain.position.Position;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        ChessController chessController = new ChessController();
-        chessController.start();
+        final BoardGenerator boardGenerator = new ChessBoardGenerator();
+        final ChessBoard chessBoard = new ChessBoard(boardGenerator);
+
+        InputCase input = ELSE;
+        while (!input.equals(START)) {
+            try {
+                input = InputView.responseUserStartCommand();
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+
+        while (!input.equals(END) || !chessBoard.checkKingExist()) {
+            try {
+                OutputView.printBoard(chessBoard);
+                input = InputView.responseUserCommand();
+
+                if (input.equals(STATUS)){
+                    OutputView.printStatus(new StatusDto(chessBoard));
+                }
+                if (input.equals(MOVE)) {
+                    Position source = InputView.responseSource();
+                    Position target = InputView.responseTarget();
+                    chessBoard.move(source, target);
+                }
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+
+        OutputView.printWinner(chessBoard.calculateWhoWinner());
     }
 }
