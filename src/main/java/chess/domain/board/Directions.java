e9f4c580c035299edd3447944b7d0fedc52ba728
Richard JEON <ztzy1907@gmail.com>
2022-03-31 16:41:48 +0900

    test(directions): 폰 이동 각도 테스트 추가

--- src/main/java/chess/domain/board/Directions.java
+++ src/main/java/chess/domain/board/Directions.java
@@ -4,17 +4,18 @@ import chess.domain.piece.Piece;
 import java.util.List;
 
 public class Directions {
-    public static final List<Double> PAWN_DIRECTIONS = List.of(90.0, -45.0, 45.0);
+    public static final List<Double> PAWN_DIRECTIONS = List.of(90.0, 135.0, 45.0);
 
     private Directions() {
     }
 
     public static boolean isMovableDirection(Piece piece, Position from, Position to) {
+        final double angle = calculateAngleOfTwoPositions(from, to);
+
         if (piece.isPawn()) {
-            final List<Integer> differential = from.calculateDistance(to);
-            double angle = Math.atan2(differential.get(1), differential.get(0)) * (180.0 / Math.PI);
             return PAWN_DIRECTIONS.contains(angle);
         }
+
         return true;
     }
 
--- src/test/java/chess/domain/board/DirectionsTest.java
+++ src/test/java/chess/domain/board/DirectionsTest.java
@@ -1,7 +1,5 @@
 package chess.domain.board;
 
-import static chess.domain.PositionFixture.B3;
-import static chess.domain.PositionFixture.B4;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.Color;
@@ -12,9 +10,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class DirectionsTest {
     @ParameterizedTest(name = "출발지 : {0}, 도착지 : {1}, 기대값 : {2}")
-    @CsvSource(value = {
-            "E5,E7,90.0", "E5,G7,45.0", "E5,G5,0.0", "E5,C3,-135.0",
-            "E5,E3,-90.0", "E5,C3,-135.0", "E5,B5,180.0", "E5,C3,-135.0"})
+    @CsvSource(value = {"E5,E7,90.0", "E5,G7,45.0", "E5,G5,0.0", "E5,G3,-45.0",
+            "E5,E3,-90.0", "E5,C3,-135.0", "E5,C5,180.0"})
     @DisplayName("두 지점 간 각도 계산 테스트")
     void calculateAngleOfTwoPositions(String from, String to, double expected) {
         final double actual = Directions.calculateAngleOfTwoPositions(Position.from(from), Position.from(to));
@@ -22,9 +19,20 @@ class DirectionsTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"A2,A4,true"})
-    @DisplayName("폰은 기본적으로 앞으로만 전진 가능하다")
+    @CsvSource(value = {"A2,A3,true", "A2,A4,true", "B2,A3,true", "B2,C3,true",
+            "A2,B2,false", "A2,A1,false", "A2,A1,false", "B2,A1,false", "B2,C1,false"})
+    @DisplayName("폰은 앞으로 한 칸 전진하며, 첫 수 에만 두 칸 전진 가능하다. "
+            + "적 기물이 있을 시 대각 한 칸 전진하며 잡을 수 있다."
+            + "좌우 또는 뒤로는 이동 불가하다.")
     void isMovableDirection(String from, String to, boolean expected) {
-        assertThat(Directions.isMovableDirection(new Pawn(Color.WHITE), B3, B4)).isEqualTo(expected);
+        // given
+        final Position fromPosition = Position.from(from);
+        final Position toPosition = Position.from(to);
+
+        // when
+        final boolean isMovable = Directions.isMovableDirection(new Pawn(Color.WHITE), fromPosition, toPosition);
+
+        // then
+        assertThat(isMovable).isEqualTo(expected);
     }
 }
