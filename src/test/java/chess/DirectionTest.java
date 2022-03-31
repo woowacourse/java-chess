ad6c40ff8ec9358db58179b6bce0072160ce87e7
bomi <bonita9931@gmail.com>
2022-03-31 17:30:46 +0900

    test: direction test 추가

---
+++ src/test/java/chess/DirectionTest.java
@@ -0,0 +1,39 @@
+package chess;
+
+import chess.model.Direction;
+import chess.model.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DirectionTest {
+
+    @ParameterizedTest
+    @MethodSource("getDirections")
+    @DisplayName("source와 target의 방향이 올바른지 확인한다.")
+    void getDirectionTest(String targetPos, Direction direction) {
+        Position source = Position.from("d3");
+        Position target = Position.from(targetPos);
+        Direction expected = Direction.of(source, target);
+
+        assertThat(expected).isEqualTo(direction);
+    }
+
+    static Stream<Arguments> getDirections() {
+        return Stream.of(
+                Arguments.of("d6", Direction.N),
+                Arguments.of("g3", Direction.E),
+                Arguments.of("a3", Direction.W),
+                Arguments.of("d2", Direction.S),
+                Arguments.of("b5", Direction.NW),
+                Arguments.of("g6", Direction.NE),
+                Arguments.of("c2", Direction.SW),
+                Arguments.of("f1", Direction.SE)
+        );
+    }
+}
