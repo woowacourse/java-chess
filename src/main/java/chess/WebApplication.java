f7bf2267d3d19a5c803057f193638e600cb7fd36
TaeHyeon Kim <kth990303@naver.com>
2022-03-31 18:20:55 +0900

    feat: 체스판 이미지 구현

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -6,10 +6,12 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class WebApplication {
     public static void main(String[] args) {
+        port(8081);
+        staticFileLocation("/public");
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
