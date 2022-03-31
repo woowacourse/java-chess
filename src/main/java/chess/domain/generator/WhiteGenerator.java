fb7a6640c88ee36104a78039d046de9fdddfce70
이한울 <dkfghg3@naver.com>
2022-03-31 22:57:27 +0900

    refactor: 체스말 generator 중복 제거

--- src/main/java/chess/domain/generator/WhiteGenerator.java
+++ src/main/java/chess/domain/generator/WhiteGenerator.java
@@ -1,55 +1,11 @@
 package chess.domain.generator;
 
-import chess.domain.Position;
-import chess.domain.piece.Bishop;
-import chess.domain.piece.King;
-import chess.domain.piece.Knight;
-import chess.domain.piece.Pawn;
-import chess.domain.piece.Piece;
-import chess.domain.piece.Queen;
-import chess.domain.piece.Rook;
-import java.util.ArrayList;
-import java.util.List;
+public class WhiteGenerator extends Generator {
 
-public class WhiteGenerator implements Generator {
+    private static final int PAWN_RANK = 2;
+    private static final int DEFAULT_RANK = 1;
 
-    @Override
-    public List<Piece> generate() {
-        final List<Piece> pieces = new ArrayList<>();
-        pieces.addAll(createPawn());
-        pieces.addAll(createRook());
-        pieces.addAll(createKnight());
-        pieces.addAll(createBishop());
-        pieces.add(createQueen());
-        pieces.add(createKing());
-        return pieces;
-    }
-
-    private List<Piece> createPawn() {
-        final List<Piece> pawns = new ArrayList<>();
-        for (int i = 0; i < PAWN_COUNT_PER_PLAYER; i++) {
-            pawns.add(new Pawn(new Position(2, (char) ('a' + i))));
-        }
-        return pawns;
-    }
-
-    private List<Piece> createRook() {
-        return List.of(new Rook(new Position(1, 'a')), new Rook(new Position(1, 'h')));
-    }
-
-    private List<Piece> createKnight() {
-        return List.of(new Knight(new Position(1, 'b')), new Knight(new Position(1, 'g')));
-    }
-
-    private List<Piece> createBishop() {
-        return List.of(new Bishop(new Position(1, 'c')), new Bishop(new Position(1, 'f')));
-    }
-
-    private Piece createQueen() {
-        return new Queen(new Position(1, 'd'));
-    }
-
-    private Piece createKing() {
-        return new King(new Position(1, 'e'));
+    public WhiteGenerator() {
+        super(PAWN_RANK, DEFAULT_RANK);
     }
 }
--- src/test/java/chess/domain/generator/CustomGenerator.java
+++ src/test/java/chess/domain/generator/CustomGenerator.java
@@ -1,45 +1,18 @@
 package chess.domain.generator;
 
-import chess.domain.Position;
-import chess.domain.piece.Bishop;
-import chess.domain.piece.King;
-import chess.domain.piece.Knight;
 import chess.domain.piece.Piece;
-import chess.domain.piece.Queen;
-import chess.domain.piece.Rook;
-import java.util.ArrayList;
 import java.util.List;
 
-public class CustomGenerator implements Generator {
+public class CustomGenerator extends Generator {
+
+    private static final int PAWN_RANK = 2;
+    private static final int DEFAULT_RANK = 1;
+
+    public CustomGenerator() {
+        super(PAWN_RANK, DEFAULT_RANK);
+    }
 
     @Override
-    public List<Piece> generate() {
-        final List<Piece> pieces = new ArrayList<>();
-        pieces.addAll(createRook());
-        pieces.addAll(createKnight());
-        pieces.addAll(createBishop());
-        pieces.add(createQueen());
-        pieces.add(createKing());
-        return pieces;
-    }
-
-    private List<Piece> createRook() {
-        return List.of(new Rook(new Position(1, 'a')), new Rook(new Position(1, 'h')));
-    }
-
-    private List<Piece> createKnight() {
-        return List.of(new Knight(new Position(1, 'b')), new Knight(new Position(1, 'g')));
-    }
-
-    private List<Piece> createBishop() {
-        return List.of(new Bishop(new Position(1, 'c')), new Bishop(new Position(1, 'f')));
-    }
-
-    private Piece createQueen() {
-        return new Queen(new Position(1, 'd'));
-    }
-
-    private Piece createKing() {
-        return new King(new Position(1, 'e'));
+    void createPawn(final List<Piece> pieces) {
     }
 }
