d227e0d68f46f0a03ea66e77108292e594f27839
jayjaehunchoi <wogns0108@naver.com>
2022-03-31 11:06:29 +0900

    refactor : 빈칸을 제외한 모든 기물은 팀 없이 생성 불가하게 변경

--- src/main/java/chess/domain/piece/ValidPiece.java
+++ src/main/java/chess/domain/piece/ValidPiece.java
@@ -4,6 +4,9 @@ public abstract class ValidPiece extends Piece {
 
     protected ValidPiece(final Team team, final double point) {
         super(team, point);
+        if (team == Team.NONE) {
+            throw new IllegalArgumentException("[ERROR] 팀을 입력해주세요.");
+        }
     }
 
     @Override
