f3896cca747d675555c77cc579df684efc1ec30b
swcho <progress0407@gmail.com>
2022-04-01 18:38:26 +0900

    test(ChessBoardTest): `StringBoardFactory` 테스트 픽스쳐 적용

--- src/test/java/chess/domain/board/ChessBoardTest.java
+++ src/test/java/chess/domain/board/ChessBoardTest.java
@@ -4,14 +4,10 @@ import static chess.domain.board.ChessBoard.SOURCE_POSITION_SHOULD_HAVE_PIECE_ME
 import static chess.domain.board.position.File.A;
 import static chess.domain.board.position.File.B;
 import static chess.domain.board.position.File.C;
-import static chess.domain.board.position.File.E;
-import static chess.domain.board.position.File.H;
 import static chess.domain.board.position.Rank.EIGHT;
-import static chess.domain.board.position.Rank.FIVE;
 import static chess.domain.board.position.Rank.FOUR;
 import static chess.domain.board.position.Rank.ONE;
 import static chess.domain.board.position.Rank.SEVEN;
-import static chess.domain.board.position.Rank.SIX;
 import static chess.domain.board.position.Rank.THREE;
 import static chess.domain.board.position.Rank.TWO;
 import static chess.domain.piece.PieceTeam.WHITE;
@@ -19,11 +15,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import chess.domain.board.factory.BoardFactory;
 import chess.domain.board.factory.RegularBoardFactory;
+import chess.domain.board.factory.StringBoardFactory;
 import chess.domain.board.position.File;
 import chess.domain.board.position.Position;
 import chess.domain.board.position.Positions;
-import chess.domain.board.factory.BoardFactory;
 import chess.domain.board.position.Rank;
 import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
@@ -166,22 +163,24 @@ public class ChessBoardTest {
     @DisplayName("킹이 잡힐 경우 게임은 종료된다")
     @Test
     void when_king_captured_then_game_end() {
+        List<String> stringChessBoard = List.of(
+                "K.......",
+                ".p......",
+                "........",
+                "........",
+                "........",
+                "........",
+                "........",
+                "........"
+        );
+
+        BoardFactory boardFactory = StringBoardFactory.getInstance(stringChessBoard);
         AlternatingGameFlow gameFlow = new AlternatingGameFlow();
         chessBoard = new ChessBoard(boardFactory.create(), gameFlow);
 
-        chessBoard.movePiece(Positions.findPositionBy(H, TWO), Positions.findPositionBy(H, FOUR)); // 흰 폰
-        chessBoard.movePiece(Positions.findPositionBy(E, SEVEN), Positions.findPositionBy(E, FIVE)); // 검은 폰
+        assertThat(chessBoard.isGamePlaying()).isTrue();
 
-        chessBoard.movePiece(Positions.findPositionBy(H, ONE), Positions.findPositionBy(H, THREE)); // 흰 룩
-        chessBoard.movePiece(Positions.findPositionBy(E, FIVE), Positions.findPositionBy(E, FOUR)); // 검은 폰
-
-        chessBoard.movePiece(Positions.findPositionBy(H, THREE), Positions.findPositionBy(E, THREE)); // 흰 룩
-        chessBoard.movePiece(Positions.findPositionBy(E, EIGHT), Positions.findPositionBy(E, SEVEN)); // 검은 킹
-
-        chessBoard.movePiece(Positions.findPositionBy(E, THREE), Positions.findPositionBy(E, FOUR)); // 흰 룩 : 게임 룰 상 이때 체크메이트 !
-        chessBoard.movePiece(Positions.findPositionBy(E, SEVEN), Positions.findPositionBy(E, SIX)); // 검은 킹
-
-        chessBoard.movePiece(Positions.findPositionBy(E, FOUR), Positions.findPositionBy(E, SIX)); // 흰 룩
+        chessBoard.movePiece(Positions.findPositionBy(B, SEVEN), Positions.findPositionBy(A, EIGHT)); // 흰 폰 이동
 
         assertThat(chessBoard.isGamePlaying()).isFalse();
     }
@@ -195,16 +194,33 @@ public class ChessBoardTest {
     }
 
     @Test
-    @DisplayName("폰이 같은 File에 두 개 이상 있을 경우 각 0.5점으로 계산한다.")
-    void when_pawns_in_same_file() {
+    @DisplayName("점수 계산 - 초기 한 쪽 팀의 점수는 38점이다")
+    void score_is_38_when_first_time() {
         ChessBoard chessBoard = new ChessBoard(boardFactory.create(), new AlternatingGameFlow());
-
-        chessBoard.movePiece(Positions.findPositionBy(A, TWO), Positions.findPositionBy(A, FOUR));
-        chessBoard.movePiece(Positions.findPositionBy(B, SEVEN), Positions.findPositionBy(B, FIVE));
-        chessBoard.movePiece(Positions.findPositionBy(A, FOUR), Positions.findPositionBy(B, FIVE));
 
         //then
         double actual = chessBoard.calculateScore();
-        assertThat(actual).isEqualTo(37.0);
+        assertThat(actual).isEqualTo(38.0);
+    }
+
+    @Test
+    @DisplayName("폰이 같은 File(열) 에 두 개 이상 있을 경우 각 0.5점으로 계산한다.")
+    void when_pawns_in_same_file() {
+        List<String> stringChessBoard = List.of(
+                "........",
+                "........",
+                "........",
+                "........",
+                "........",
+                "..p.....",
+                ".pp.....",
+                "ppp....."
+        );
+        BoardFactory boardFactory = StringBoardFactory.getInstance(stringChessBoard);
+        ChessBoard chessBoard = new ChessBoard(boardFactory.create(), new AlternatingGameFlow());
+
+        //then
+        double actual = chessBoard.calculateScore();
+        assertThat(actual).isEqualTo(3.5);
     }
 }
