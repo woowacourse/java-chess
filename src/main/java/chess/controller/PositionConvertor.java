060dc28ccdbccde23331bdaeda270c73b39aa7cc
yh20studio(tiki) <yh20studio@gmail.com>
2022-04-01 15:10:38 +0900

    refactor(): 하드코딩 제거

--- src/main/java/chess/controller/PositionConvertor.java
+++ src/main/java/chess/controller/PositionConvertor.java
@@ -9,13 +9,14 @@ import java.util.List;
 public class PositionConvertor {
 
 	private static final String WRONG_LENGTH_ERROR = "움직일 기물의 위치는 2글자로 입력해야 합니다.";
+	private static final String DELIMITER = "";
 	private static final int FORMAT_LENGTH = 2;
 	private static final int FILE_INDEX = 0;
 	private static final int RANK_INDEX = 1;
 
 	public static Position to(final String rawPosition) {
 		validateLength(rawPosition);
-		List<String> fileAndRank = Arrays.asList(rawPosition.split(""));
+		List<String> fileAndRank = Arrays.asList(rawPosition.split(DELIMITER));
 
 		Rank rank = Rank.of(fileAndRank.get(RANK_INDEX));
 		File file = File.of(fileAndRank.get(FILE_INDEX));
