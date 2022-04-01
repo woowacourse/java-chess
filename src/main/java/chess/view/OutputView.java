85ee3a6688ea96d9147cf1171bcc9ba0a0d6be0d
devHudi <devhudi@gmail.com>
2022-04-01 12:39:23 +0900

    refactor(OutputView): 개행 문자를 상수로 분리

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -9,15 +9,16 @@ import java.util.Optional;
 
 public class OutputView {
 
+    private static final String NEW_LINE = System.lineSeparator();
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
-    private static final String COMMAND_GUIDE_MESSAGE = "> 체스 게임을 시작합니다.%n"
-            + "> 게임 시작 : start%n"
-            + "> 게임 종료 : end%n"
-            + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3%n";
+    private static final String COMMAND_GUIDE_MESSAGE = "> 체스 게임을 시작합니다." + NEW_LINE
+            + "> 게임 시작 : start" + NEW_LINE
+            + "> 게임 종료 : end" + NEW_LINE
+            + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
     private static final String EMPTY_PIECE_NOTATION = ".";
 
     public static void printCommandGuide() {
-        System.out.printf(COMMAND_GUIDE_MESSAGE);
+        System.out.println(COMMAND_GUIDE_MESSAGE);
     }
 
     public static void printBoard(BoardDto boardDto) {
