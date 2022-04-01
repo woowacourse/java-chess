d3e6087f4b8f6a225bd1b8783896e9dc7ecf2523
BeomsooKim <kbsat@naver.com>
2022-04-01 14:46:57 +0900

    refactor: GameCommand의 상수를 좀 더 구체적으로 변경

--- src/main/java/chess/domain/GameCommand.java
+++ src/main/java/chess/domain/GameCommand.java
@@ -11,8 +11,8 @@ public class GameCommand {
 
     private static final int FROM_PARAMETER_INDEX = 1;
     private static final int TO_PARAMETER_INDEX = 2;
-    private static final int FROM_INDEX = 0;
-    private static final int TO_INDEX = 1;
+    private static final int FROM_POSITION_INDEX = 0;
+    private static final int TO_POSITION_INDEX = 1;
     private static final int MIN_ARGUMENT_NUM = 1;
 
     private final CommandType command;
@@ -42,14 +42,14 @@ public class GameCommand {
 
     public Position getFromPosition() {
         if (command == CommandType.MOVE) {
-            return movePositions.get(FROM_INDEX);
+            return movePositions.get(FROM_POSITION_INDEX);
         }
         throw new IllegalStateException("해당 커맨드는 이 작업을 할 수 없습니다.");
     }
 
     public Position getToPosition() {
         if (command == CommandType.MOVE) {
-            return movePositions.get(TO_INDEX);
+            return movePositions.get(TO_POSITION_INDEX);
         }
         throw new IllegalStateException("해당 커맨드는 이 작업을 할 수 없습니다.");
     }
