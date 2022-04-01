b3f061aed5cbb74b36aa8b60f2bf10a045320dcc
hyunrrr <whgusrms96@gmail.com>
2022-04-01 18:49:25 +0900

    test: 테스트 돌아가도록 수정

--- src/test/java/chess/domain/ChessBoardPositionTest.java
+++ src/test/java/chess/domain/ChessBoardPositionTest.java
@@ -10,10 +10,10 @@ import static org.assertj.core.api.Assertions.*;
 class ChessBoardPositionTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"a:0", "a:9", "j:3", "j:10"}, delimiter = ':')
+    @CsvSource(value = {"1:0", "1:9", "10:3", "3:10"}, delimiter = ':')
     @DisplayName("유효하지 않은 체스판 내부 위치를 생성하면 에외를 발생한다.")
-    void validateChess(char column, int row) {
-        assertThatThrownBy(() -> new ChessBoardPosition(column, row))
+    void validateChess(int column, int row) {
+        assertThatThrownBy(() -> ChessBoardPosition.of(column, row))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 체스판 범위를 벗어나는 위치가 입력되었습니다.");
     }
@@ -21,16 +21,16 @@ class ChessBoardPositionTest {
     @Test
     @DisplayName("인자로 들어온 위치가 현재 내 위치랑 같은지 확인하는 테스트한다.")
     void equalsTest() {
-        ChessBoardPosition chessBoardPosition = new ChessBoardPosition('a', 3);
-        assertThat(chessBoardPosition.equals(new ChessBoardPosition('a', 3))).isTrue();
+        ChessBoardPosition chessBoardPosition = ChessBoardPosition.of(1, 3);
+        assertThat(chessBoardPosition.equals(ChessBoardPosition.of(1, 3))).isTrue();
     }
 
 
     @Test
     @DisplayName("인자로 들어온 위치가 현재 내 위치랑 다른지 확인하는 테스트한다.")
     void notEqualsTest() {
-        ChessBoardPosition chessBoardPosition = new ChessBoardPosition('a', 3);
-        assertThat(chessBoardPosition.equals(new ChessBoardPosition('a', 4))).isFalse();
+        ChessBoardPosition chessBoardPosition = ChessBoardPosition.of(1, 3);
+        assertThat(chessBoardPosition.equals(ChessBoardPosition.of(1, 4))).isFalse();
     }
 
     @Test
