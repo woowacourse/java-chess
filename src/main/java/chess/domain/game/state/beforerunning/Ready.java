67e615146c4b7c84e25ddaf77d86c5c1be170a82
injoon2019 <ison@sfu.ca>
2022-04-01 16:37:38 +0900

    refactor: end시 FinishedEnd 상태 반환

--- src/main/java/chess/domain/game/state/beforerunning/Ready.java
+++ src/main/java/chess/domain/game/state/beforerunning/Ready.java
@@ -2,6 +2,7 @@ package chess.domain.game.state.beforerunning;
 
 import chess.domain.board.BoardFactory;
 import chess.domain.game.state.State;
+import chess.domain.game.state.afterrunning.FinishedEnd;
 import chess.domain.game.state.afterrunning.FinishedKing;
 import chess.domain.position.Position;
 
@@ -13,12 +14,11 @@ public class Ready extends BeforeRunning {
 
     @Override
     public State endGame() {
-        return new FinishedKing(board);
+        return new FinishedEnd(board);
     }
 
     @Override
     public State move(Position from, Position to) {
         throw new IllegalStateException("게임 시작 전 말을 움직일 수 없습니다.");
     }
-
 }
