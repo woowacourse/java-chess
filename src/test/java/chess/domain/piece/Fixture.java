fee4a0b626e2880963ff62918bfcbc387adbf05d
Richard JEON <ztzy1907@gmail.com>
2022-04-02 08:42:52 +0900

    test(queen): 퀸 행마법 테스트 코드 추가

--- src/test/java/chess/domain/piece/Fixture.java
+++ src/test/java/chess/domain/piece/Fixture.java
@@ -6,4 +6,5 @@ public class Fixture {
     public static final Position E4 = Position.from("E4");
     public static final Bishop BISHOP_WHITE = new Bishop(Color.WHITE);
     public static final Rook ROOK_WHITE = new Rook(Color.WHITE);
+    public static final Queen QUEEN_WHITE = new Queen(Color.WHITE);
 }
