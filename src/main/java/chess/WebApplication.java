68cfac3d304e8a13278424cf374c02ce178560e6
hyeonic <dev.hyeonic@gmail.com>
2022-04-01 22:22:58 +0900

    feat: web ui를 위한 index 및 이미지 추가

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,15 +1,20 @@
 package chess;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static spark.Spark.get;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebApplication {
+
     public static void main(String[] args) {
+        port(8080);
+        staticFileLocation("/static");
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
