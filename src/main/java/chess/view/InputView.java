27d32de52612fe3d5f6b41c4f5efa88707460d44
EastHShin <xldk78@gmail.com>
2022-04-01 16:32:21 +0900

    refactor(view/InputView): 메서드 순서 정렬

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
 	private static final String EMPTY_INPUT_EXCEPTION = "명령을 입력하세요.";
 	private static final String COMMAND_DELIMITER = " ";
 	private static final int MOVE_COMMAND_SIZE = 3;
+	private static final String MOVE_COMMAND = "move";
 	private static final Scanner SCANNER = new Scanner(System.in);
 
 	public List<String> inputCommand() {
@@ -30,14 +31,14 @@ public class InputView {
 		return command;
 	}
 
-	private boolean isMoveCommand(final List<String> command) {
-		return "move".equals(command.get(0));
-	}
-
 	private void validateEmpty(final String input) {
 		if (input.isEmpty()) {
 			throw new IllegalArgumentException(EMPTY_INPUT_EXCEPTION);
 		}
+	}
+
+	private boolean isMoveCommand(final List<String> command) {
+		return MOVE_COMMAND.equals(command.get(0));
 	}
 
 	private void validateMoveCommandSize(final List<String> command) {
