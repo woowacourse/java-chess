7eada7b81b3401e3f482dfc28330cef13b63e768
is2js <tingstyle1@gmail.com>
2022-04-01 22:17:34 +0900

    refactor: 불필요한 메서드 삭제 및 StatusScore 필드 원시값으로 변경

--- src/main/java/chess/domain/GameResult.java
+++ src/main/java/chess/domain/GameResult.java
@@ -1,23 +1,14 @@
 package chess.domain;
 
-import java.util.Arrays;
-
 public enum GameResult {
     BLACK_WIN("흑색 진영의 승리"),
-    BLACK_LOSE("백색 진영의 승리"),
+    WHITE_WIN("백색 진영의 승리"),
     DRAW("무승부");
 
     private final String message;
 
     GameResult(final String message) {
         this.message = message;
-    }
-
-    public static GameResult from(final GameResult hasBlackWon) {
-        return Arrays.stream(GameResult.values())
-            .filter(it -> it == hasBlackWon)
-            .findFirst()
-            .orElseThrow(() -> new IllegalStateException("승패를 알 수 없는 상태입니다."));
     }
 
     public String getMessage() {
--- src/main/java/chess/domain/StatusScore.java
+++ src/main/java/chess/domain/StatusScore.java
@@ -9,11 +9,11 @@ public class StatusScore {
 
     private final double white;
     private final double black;
-    private final GameResult gameResult;
+    private final String gameResult;
 
     private StatusScore(final double statusOfWhite,
                         final double statusOfBlack,
-                        final GameResult gameResult) {
+                        final String gameResult) {
         this.white = statusOfWhite;
         this.black = statusOfBlack;
         this.gameResult = gameResult;
@@ -24,14 +24,14 @@ public class StatusScore {
         final double whiteStatus = statusCalculator.calculate(piece -> !piece.isBlack());
         final double blackStatus = statusCalculator.calculate(Piece::isBlack);
         final GameResult gameResult = findWinner(board, whiteStatus, blackStatus);
-        return new StatusScore(whiteStatus, blackStatus, gameResult);
+        return new StatusScore(whiteStatus, blackStatus, gameResult.getMessage());
     }
 
     private static GameResult findWinner(final Board board,
                                          final double statusOfWhite,
                                          final double statusOfBlack) {
         if (board.hasBlackKingCaptured()) {
-            return GameResult.BLACK_LOSE;
+            return GameResult.WHITE_WIN;
         }
         if (board.hasWhiteKingCaptured()) {
             return GameResult.BLACK_WIN;
@@ -45,7 +45,7 @@ public class StatusScore {
             return GameResult.BLACK_WIN;
         }
         if (resultNumber > RESULT_CRITERIA) {
-            return GameResult.BLACK_LOSE;
+            return GameResult.WHITE_WIN;
         }
         return GameResult.DRAW;
     }
@@ -58,7 +58,7 @@ public class StatusScore {
         return black;
     }
 
-    public GameResult getGameResult() {
+    public String getGameResult() {
         return gameResult;
     }
 }
