2367a5c724d0435a1f81bb7dcfe9f58c5d3f3199
leo0842 <kunga077@gmail.com>
2022-04-01 13:13:30 +0900

    feat: 커맨드를 enum 으로 만들고 명령에 따라 다르게 실행되는 로직 구현

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -1,10 +1,7 @@
 package chess;
 
 import chess.controller.ChessController;
-import chess.dto.BoardDto;
-import chess.dto.ScoreDto;
 import chess.view.InputView;
-import chess.view.OutputView;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,23 +11,8 @@ public class Main {
         ChessController chessController = new ChessController();
         while (!chessController.isEnd()) {
             List<String> inputCommand = Arrays.asList(InputView.inputStartOrEndGame());
-            String command = inputCommand.get(0);
-            if (command.equals("start")) {
-                BoardDto boardDto = chessController.startGame();
-                OutputView.announce(boardDto);
-                continue;
-            }
-            if (command.equals("move")) {
-                BoardDto boardDto = chessController.move(inputCommand.get(1), inputCommand.get(2));
-                OutputView.announce(boardDto);
-            }
-            if (command.equals("status")) {
-                ScoreDto scoreDto = chessController.score();
-                OutputView.announceScoreDto(scoreDto);
-            }
-            if (command.equals("end")) {
-                chessController.finishGame();
-            }
+            ChessCommand command = ChessCommand.findCommand(inputCommand.get(0));
+            command.accept(chessController, inputCommand);
         }
     }
 }
