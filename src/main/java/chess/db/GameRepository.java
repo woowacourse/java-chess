7299fbd9c91397e0a2546a1071cf01ea9274e2ca
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-01 21:03:23 +0900

    feat(GameRepository): implement repository for maintaining games
    - implement CRU functionality
    - TODO: replace with MySQL db after finishing web

---
+++ src/main/java/chess/db/GameRepository.java
@@ -0,0 +1,33 @@
+package chess.db;
+
+import chess.domain.game.Game;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameRepository {
+
+    private static final String GAME_NOT_FOUND_EXCEPTION_MESSAGE = "존재하지 않는 게임입니다.";
+
+    private final Map<Integer, Game> database = new HashMap<>();
+    private int id = 0;
+
+    public int add(Game game) {
+        id++;
+        database.put(id, game);
+        return id;
+    }
+
+    public Game findById(int id) {
+        if (!database.containsKey(id)) {
+            throw new IllegalArgumentException(GAME_NOT_FOUND_EXCEPTION_MESSAGE);
+        }
+        return database.get(id);
+    }
+
+    public void update(int id, Game newGame) {
+        if (!database.containsKey(id)) {
+            throw new IllegalArgumentException(GAME_NOT_FOUND_EXCEPTION_MESSAGE);
+        }
+        database.put(id, newGame);
+    }
+}
