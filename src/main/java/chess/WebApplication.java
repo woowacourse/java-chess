2c7a54e457148f587f05cc0e7283b69117f0e65a
injoon2019 <ison@sfu.ca>
2022-04-02 22:32:13 +0900

    feat: 정적 데이터들 추가
    - html
    - css
    - 이미지들

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -7,9 +7,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+import static spark.Spark.staticFiles;
 
 public class WebApplication {
     public static void main(String[] args) {
+        staticFileLocation("/static");
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
