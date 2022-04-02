8c76e437360aad55d8b3582f280a57687a9951d8
YJGwon <yj970125@gmail.com>
2022-04-02 17:27:38 +0900

    feat(resources): 웹 체스 게임 ui 구현

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -7,9 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class WebApplication {
     public static void main(String[] args) {
+        staticFileLocation("/static");
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
