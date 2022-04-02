fc685d48be109b6454b386102e8f17ce4396b09c
SuyeonChoi <tndus970120@gmail.com>
2022-04-02 19:11:18 +0900

    refactor(Pawn): 중복되는 메서드 제거 - 폰이 한칸 또는 두칸 전진할 때 적 체스피스를 잡는 오류 해결

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -5,6 +5,7 @@ import chess.domain.ChessBoardPosition;
 import chess.domain.Team;
 
 public class Pawn extends ChessPiece {
+    private static final int NO_MOVEMENT = 0;
     private static final int DEFAULT_DISTANCE = 1;
     private static final int OPTIONAL_DISTANCE = 1;
     private static final int WHITE_INITIAL_ROW_POSITION = 2;
@@ -15,89 +16,44 @@ public class Pawn extends ChessPiece {
         super(SCORE, team);
     }
 
-    private boolean isWhiteInitialPosition(ChessBoardPosition position) {
-        return position.isSameRow(WHITE_INITIAL_ROW_POSITION);
+    private int calculateRowDistance(int sourceRow, int targetRow) {
+        if (team.isWhite()) {
+            return targetRow - sourceRow;
+        }
+        return sourceRow - targetRow;
     }
 
-    private boolean isBlackInitialPosition(ChessBoardPosition position) {
-        return position.isSameRow(BLACK_INITIAL_ROW_POSITION);
-    }
-
-    private int calculateRowDistance(int highRow, int lowRow) {
-        return highRow - lowRow;
-    }
-
-    private int calculateColumnDistance(char highColumn, char lowColumn) {
-        return Math.abs(highColumn - lowColumn);
+    private int calculateColumnDistance(char sourceColumn, char targetColumn) {
+        return Math.abs(sourceColumn - targetColumn);
     }
 
     @Override
     public boolean isMovable(ChessBoardPosition sourcePosition, ChessBoardPosition targetPosition,
                              ChessBoard chessBoard) {
-        if (isDiagonalMove(sourcePosition, targetPosition)) {
-            return chessBoard.existChessPieceOf(targetPosition, team.reverse());
-        }
-
-        if (team.isWhite()) {
-            return isMovableWhite(sourcePosition, targetPosition, chessBoard);
-        }
-
-        return isMovableBlack(sourcePosition, targetPosition, chessBoard);
-    }
-
-    private boolean isDiagonalMove(ChessBoardPosition sourcePosition, ChessBoardPosition targetPosition) {
-        if (team.isWhite()) {
-            return isDiagonalMoveWhite(sourcePosition, targetPosition);
-        }
-        return isDiagonalMoveBlack(sourcePosition, targetPosition);
-    }
-
-    private boolean isDiagonalMoveBlack(ChessBoardPosition sourcePosition,
-                                        ChessBoardPosition targetPosition) {
         int rowDistance = calculateRowDistance(sourcePosition.getRow(), targetPosition.getRow());
         int columnDistance = calculateColumnDistance(sourcePosition.getColumn(), targetPosition.getColumn());
+
+        if (chessBoard.existChessPieceOf(targetPosition, team.reverse())) {
+            return isDiagonalMove(rowDistance, columnDistance);
+        }
+        if (rowDistance == DEFAULT_DISTANCE + OPTIONAL_DISTANCE && columnDistance == NO_MOVEMENT) {
+            return isOptionalMovable(sourcePosition, chessBoard);
+        }
+        return rowDistance == DEFAULT_DISTANCE && columnDistance == NO_MOVEMENT;
+    }
+
+    private boolean isDiagonalMove(int rowDistance, int columnDistance) {
         return rowDistance == DEFAULT_DISTANCE && columnDistance == DEFAULT_DISTANCE;
     }
 
-    private boolean isDiagonalMoveWhite(ChessBoardPosition sourcePosition,
-                                        ChessBoardPosition targetPosition) {
-        int rowDistance = calculateRowDistance(targetPosition.getRow(), sourcePosition.getRow());
-        int columnDistance = calculateColumnDistance(targetPosition.getColumn(), sourcePosition.getColumn());
-        return rowDistance == DEFAULT_DISTANCE && columnDistance == DEFAULT_DISTANCE;
-    }
-
-    private boolean isMovableBlack(ChessBoardPosition sourcePosition, ChessBoardPosition targetPosition,
-                                   ChessBoard chessBoard) {
-        if (isBlackInitialPosition(sourcePosition)) {
-            return isMovableBlackInitialPosition(sourcePosition, targetPosition, chessBoard);
-        }
-        return calculateRowDistance(sourcePosition.getRow(), targetPosition.getRow()) == DEFAULT_DISTANCE;
-    }
-
-    private boolean isMovableBlackInitialPosition(ChessBoardPosition sourcePosition, ChessBoardPosition targetPosition,
-                                                  ChessBoard chessBoard) {
-        int rowDistance = calculateRowDistance(sourcePosition.getRow(), targetPosition.getRow());
-        if (rowDistance == DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
-            return isUnobstructed(sourcePosition, chessBoard, -DEFAULT_DISTANCE);
-        }
-        return rowDistance == DEFAULT_DISTANCE;
-    }
-
-    private boolean isMovableWhite(ChessBoardPosition sourcePosition, ChessBoardPosition targetPosition,
-                                   ChessBoard chessBoard) {
-        if (isWhiteInitialPosition(sourcePosition)) {
-            return isMovableWhiteInitialPosition(sourcePosition, targetPosition, chessBoard);
-        }
-        return calculateRowDistance(targetPosition.getRow(), sourcePosition.getRow()) == DEFAULT_DISTANCE;
-    }
-
-    private boolean isMovableWhiteInitialPosition(ChessBoardPosition sourcePosition, ChessBoardPosition targetPosition,
-                                                  ChessBoard chessBoard) {
-        int rowDistance = calculateRowDistance(targetPosition.getRow(), sourcePosition.getRow());
-        if (rowDistance == DEFAULT_DISTANCE + OPTIONAL_DISTANCE) {
+    private boolean isOptionalMovable(ChessBoardPosition sourcePosition, ChessBoard chessBoard) {
+        if (team.isWhite() && sourcePosition.isSameRow(WHITE_INITIAL_ROW_POSITION)) {
             return isUnobstructed(sourcePosition, chessBoard, DEFAULT_DISTANCE);
         }
-        return rowDistance == DEFAULT_DISTANCE;
+        if (team.isBlack() && sourcePosition.isSameRow(BLACK_INITIAL_ROW_POSITION)) {
+            return isUnobstructed(sourcePosition, chessBoard, -DEFAULT_DISTANCE);
+        }
+        return false;
     }
 
     private boolean isUnobstructed(ChessBoardPosition sourcePosition, ChessBoard chessBoard, int forwardDirection) {
