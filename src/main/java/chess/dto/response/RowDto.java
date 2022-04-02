da843f02a8adc43d408ae2646b7732a5e60f90dd
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-02 20:27:45 +0900

    refactor(dto): squareDto to inner class

--- src/main/java/chess/dto/response/RowDto.java
+++ src/main/java/chess/dto/response/RowDto.java
@@ -1,9 +1,11 @@
 package chess.dto.response;
 
+import chess.domain.board.piece.Color;
 import chess.domain.board.piece.Piece;
 import chess.domain.board.position.File;
 import chess.domain.board.position.Position;
 import chess.domain.board.position.Rank;
+import chess.util.PieceDisplayUtil;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -30,5 +32,44 @@ public class RowDto {
 
     public List<SquareDto> getSquares() {
         return squares;
+    }
+
+    private static class SquareDto {
+
+        static final String WHITE_PIECE_COLOR_CLASSNAME = "white";
+        static final String EMPTY_PIECE_DISPLAY = "";
+        static final String EMPTY_CLASSNAME = "";
+
+        final String pieceDisplay;
+        final String textColor;
+
+        SquareDto(String pieceDisplay, String textColor) {
+            this.pieceDisplay = pieceDisplay;
+            this.textColor = textColor;
+        }
+
+        static SquareDto ofOccupied(Piece piece) {
+            String pieceDisplay = PieceDisplayUtil.toWebDisplay(piece);
+            return new SquareDto(pieceDisplay, toPieceColorIfWhite(piece));
+        }
+
+        static SquareDto ofEmpty() {
+            return new SquareDto(EMPTY_PIECE_DISPLAY, EMPTY_CLASSNAME);
+        }
+
+        static String toPieceColorIfWhite(Piece piece) {
+            if (piece.hasColorOf(Color.WHITE)) {
+                return WHITE_PIECE_COLOR_CLASSNAME;
+            }
+            return EMPTY_CLASSNAME;
+        }
+
+        public String getValue() {
+            return pieceDisplay;
+        }
+
+        public String getColor() {
+            return textColor;
+        }
     }
 }
