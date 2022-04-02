aa9c6bd7c5ee088dc5f05bf4af548f681fffce04
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-02 14:40:21 +0900

    feat(PlayGameRequestDto): implement dto
    - parse json content for move command
    - add minor study tests

---
+++ src/main/java/chess/dto/PlayGameRequestDto.java
@@ -0,0 +1,48 @@
+package chess.dto;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlayGameRequestDto {
+
+    private static final String JSON_CONTENT_REGEX = "\\{"
+            + "\"source\":\"(.*)\","
+            + "\"target\":\"(.*)\"}";
+    private static final Pattern PATTERN = Pattern.compile(JSON_CONTENT_REGEX);
+    private static final String INVALID_JSON_FORMAT = "JSON 형식에 부합하지 않습니다.";
+
+    private final int gameId;
+    private final MoveCommandDto moveCommand;
+
+    public PlayGameRequestDto(String param, String body) {
+        this.gameId = Integer.parseInt(param);
+        this.moveCommand = toMoveCommand(body);
+    }
+
+    private MoveCommandDto toMoveCommand(String body) {
+        Matcher matcher = toValidMatcher(body);
+        String source = matcher.group(1);
+        String target = matcher.group(2);
+        return new MoveCommandDto(source, target);
+    }
+
+    private static Matcher toValidMatcher(String json) {
+        Matcher matcher = PATTERN.matcher(json);
+
+        System.out.println("!!!!!!");
+        System.out.println(json);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INVALID_JSON_FORMAT);
+        }
+        return matcher;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public MoveCommandDto getMoveCommand() {
+        return moveCommand;
+    }
+}
--- src/test/java/study/RegexTest.java
+++ src/test/java/study/RegexTest.java
@@ -4,55 +4,88 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class RegexTest {
 
-    private static final String POSITION_INPUT_FORMAT_REGEX = "([abcdefgh][12345678])";
-    private static final Pattern PATTERN = Pattern.compile(POSITION_INPUT_FORMAT_REGEX);
+    @Nested
+    class MatchesTest {
 
-    @Test
-    void a1_형식이면_참() {
-        Matcher matcher = PATTERN.matcher("a1");
+        private final String POSITION_INPUT_FORMAT_REGEX = "([abcdefgh][12345678])";
+        private final Pattern PATTERN = Pattern.compile(POSITION_INPUT_FORMAT_REGEX);
 
-        boolean actual = matcher.matches();
+        @Test
+        void a1_형식이면_참() {
+            Matcher matcher = PATTERN.matcher("a1");
 
-        assertThat(actual).isTrue();
+            boolean actual = matcher.matches();
+
+            assertThat(actual).isTrue();
+        }
+
+        @Test
+        void 알파벳_범위를_벗어나면_거짓() {
+            Matcher matcher = PATTERN.matcher("i1");
+
+            boolean actual = matcher.matches();
+
+            assertThat(actual).isFalse();
+        }
+
+        @Test
+        void 숫자_범위를_벗어나면_거짓() {
+            Matcher matcher = PATTERN.matcher("a0");
+
+            boolean actual = matcher.matches();
+
+            assertThat(actual).isFalse();
+        }
+
+        @Test
+        void 한글자면_거짓() {
+            Matcher matcher = PATTERN.matcher("a");
+
+            boolean actual = matcher.matches();
+
+            assertThat(actual).isFalse();
+        }
+
+        @Test
+        void 두글자를_넘어가면_거짓() {
+            Matcher matcher = PATTERN.matcher("a12");
+
+            boolean actual = matcher.matches();
+
+            assertThat(actual).isFalse();
+        }
     }
 
-    @Test
-    void 알파벳_범위를_벗어나면_거짓() {
-        Matcher matcher = PATTERN.matcher("i1");
+    @Nested
+    class GroupTest {
 
-        boolean actual = matcher.matches();
+        private final String JSON_CONTENT_REGEX = "\\{\"source\":\"(.*)\",\"target\":\"(.*)\"}";
+        private final Pattern JSON_PATTERN = Pattern.compile(JSON_CONTENT_REGEX);
 
-        assertThat(actual).isFalse();
-    }
+        @Test
+        void 첫번째() {
+            Matcher matcher = JSON_PATTERN.matcher("{\"source\":\"e7\",\"target\":\"e5\"}");
 
-    @Test
-    void 숫자_범위를_벗어나면_거짓() {
-        Matcher matcher = PATTERN.matcher("a0");
+            boolean matched = matcher.matches();
+            String actual = matcher.group(1);
 
-        boolean actual = matcher.matches();
+            assertThat(actual).isEqualTo("e7");
+        }
 
-        assertThat(actual).isFalse();
-    }
+        @Test
+        void 두번째() {
+            Matcher matcher = JSON_PATTERN.matcher("{\"source\":\"e7\",\"target\":\"e5\"}");
+            boolean matched = matcher.matches();
 
-    @Test
-    void 한글자면_거짓() {
-        Matcher matcher = PATTERN.matcher("a");
+            String actual = matcher.group(2);
 
-        boolean actual = matcher.matches();
-
-        assertThat(actual).isFalse();
-    }
-
-    @Test
-    void 두글자를_넘어가면_거짓() {
-        Matcher matcher = PATTERN.matcher("a12");
-
-        boolean actual = matcher.matches();
-
-        assertThat(actual).isFalse();
+            assertThat(actual).isEqualTo("e5");
+        }
     }
 }
