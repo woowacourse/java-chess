a09b6fb3d1bceb948876aba110d7f483e1c35275
Ohzzi <clover7even@naver.com>
2022-04-02 23:30:15 +0900

    feat: 웹에서 체스 보드를 그리는 기능 구현

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,17 +1,28 @@
 package chess;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class WebApplication {
 
-    /*
     public static void main(String[] args) {
-         get("/", (req, res) -> {
-             Map<String, Object> model = new HashMap<>();
-             return render(model, "index.html");
-         });
+        staticFileLocation("/static");
+
+        port(8080);
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, "index.html");
+        });
     }
 
-     private static String render(Map<String, Object> model, String templatePath) {
-         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-     }
-     */
+    private static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
 }
