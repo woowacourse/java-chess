2b541ce6874b72d422a5d677edbaee9ca21a4bc0
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-02 15:08:33 +0900

    feat(web): implement moving chess piece with click event

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -5,6 +5,7 @@ import static spark.Spark.post;
 import static spark.Spark.staticFiles;
 
 import chess.controller.WebController;
+import chess.dto.PlayGameRequestDto;
 import chess.model.NewGameModel;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -15,16 +16,23 @@ public class WebApplication {
 
     public static void main(String[] args) {
         staticFiles.location("/static");
+
         get("/", (req, res) -> render(null, "home.html"));
         post("/new-game", (req, res) -> {
             res.type("application/json");
             NewGameModel newGameModel = controller.initGame();
             return newGameModel.toJson();
         });
+
         get("/game/:id", (req, res) -> {
-            int gameId = Integer.parseInt(req.params("id"));
+            int gameId = Integer.parseInt(req.params("id")); // TODO: handle exception on non int input
             return render(controller.findGame(gameId), "game.html");
         });
+        post("/game/:id", (req, res) -> {
+            PlayGameRequestDto request = new PlayGameRequestDto(req.params("id"), req.body());
+            return render(controller.playGame(request), "game.html");
+        });
+
         get("/search", (req, res) -> "TBA");
     }
 
