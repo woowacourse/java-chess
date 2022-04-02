94e806cad91798336f54518d739f29126885b819
bcc0830 <bcc0830@naver.com>
2022-04-02 13:41:52 +0900

    feat : 웹 UI 초기화면 구현

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -7,11 +7,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.staticFiles;
 
 public class WebApplication {
     public static void main(String[] args) {
+        staticFiles.location("/templates");
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
+            System.out.println("리퀘스트 바디 : "  + req.body());
             return render(model, "index.html");
         });
     }
