0ea4b1bea97e07cba5086507ea296e88204d747a
Ohzzi <clover7even@naver.com>
2022-04-03 16:49:06 +0900

    feat: 보드 정보를 가져올 때 기물에 맞는 이미지를 출력하는 기능 추가

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -16,16 +16,16 @@ window.onload = function () {
 
   fetch("/api/start")
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => data.pieces.forEach(piece => createPieceImage(piece.position, piece.pieceType)));
 }
 
 function makeRow(rowDiv, rowIndex) {
   for (let colIndex = 0; colIndex < 8; colIndex++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.id = createSquareId(colIndex, rowIndex);
-    square.style.background = decideCellColor(colIndex, rowIndex);
-    rowDiv.appendChild(square);
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.id = createSquareId(colIndex, rowIndex);
+    cell.style.background = decideCellColor(colIndex, rowIndex);
+    rowDiv.appendChild(cell);
   }
 }
 
@@ -38,4 +38,12 @@ function decideCellColor(column, row) {
     return lightCellColor;
   }
   return darkCellColor;
+}
+
+function createPieceImage(position, pieceType) {
+  const cell = document.getElementById(position);
+  const piece = document.createElement("img");
+  piece.classList.add("piece-image");
+  piece.src = `/images/${pieceType}.png`
+  cell.appendChild(piece);
 }
