2264dac425a6f142e39813a6f089260cf36f9a6e
nailseong <nnis9857@gmail.com>
2022-04-03 22:20:16 +0900

    feat(init.sql): Room 테이블 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,16 +1,27 @@
 USE chess;
 
-CREATE TABLE ChessBoard
+CREATE TABLE Room
 (
+    Name  VARCHAR(10) NOT NULL UNIQUE,
+    White VARCHAR(10) UNIQUE,
+    Black VARCHAR(10) UNIQUE,
+    PRIMARY KEY (Name)
+);
+
+CREATE TABLE ChessPiece
+(
+    Room_Id    VARCHAR(10) NOT NULL,
     Position   VARCHAR(10) NOT NULL,
     ChessPiece VARCHAR(10) NOT NULL,
     Color      VARCHAR(10) NOT NULL,
-    PRIMARY KEY (Position)
+    FOREIGN KEY (Room_Id) REFERENCES Room (Name)
 );
 
 CREATE TABLE Status
 (
+    Room_Id     VARCHAR(10) NOT NULL,
     GameStatus  VARCHAR(10) NOT NULL,
-    CurrentTurn VARCHAR(10) NOT NULL
+    CurrentTurn VARCHAR(10) NOT NULL,
+    FOREIGN KEY (Room_Id) REFERENCES Room (Name)
 );
 
