a51d07a4e25c14f3f3e4a4510f3d490feb8d91cb
Ohzzi <clover7even@naver.com>
2022-04-03 14:57:59 +0900

    refactor: 체스 보드 그리는 방식 수정
    canvas 사용 대신 div에 배경색을 넣는 형태로 수정

---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,37 @@
+const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const rows = ['8', '7', '6', '5', '4', '3', '2', '1'];
+
+const section = document.getElementById("chess-section");
+
+const lightCellColor = '#ddb180';
+const darkCellColor = '#7c330c';
+
+window.onload = function () {
+  for (let i = 0; i < 8; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    makeRow(row, i);
+    section.appendChild(row);
+  }
+}
+
+function makeRow(rowDiv, rowIndex) {
+  for (let colIndex = 0; colIndex < 8; colIndex++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.id = createSquareId(colIndex, rowIndex);
+    square.style.background = decideCellColor(colIndex, rowIndex);
+    rowDiv.appendChild(square);
+  }
+}
+
+function createSquareId(column, row) {
+  return columns[column] + rows[row];
+}
+
+function decideCellColor(column, row) {
+  if ((column + row) % 2 === 0) {
+    return lightCellColor;
+  }
+  return darkCellColor;
+}
