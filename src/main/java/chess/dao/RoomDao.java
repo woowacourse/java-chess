a1360bd19e75f3ba56ff95be0a671b217f720f80
nailseong <nnis9857@gmail.com>
2022-04-03 23:33:45 +0900

    feat(RoomDao): 이름으로 방 찾는 기능 추가

--- src/main/java/chess/dao/RoomDao.java
+++ src/main/java/chess/dao/RoomDao.java
@@ -3,6 +3,7 @@ package chess.dao;
 import chess.dto.RoomDto;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RoomDao {
@@ -17,6 +18,26 @@ public class RoomDao {
             final int result = statement.executeUpdate();
             if (result == 1) {
                 return RoomDto.from(roomName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public RoomDto findByName(final String roomName) {
+        final String sql = "SELECT * FROM Room WHERE Name = ?";
+
+        try (final Connection connection = ConnectionGenerator.getConnection();
+        final PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, roomName);
+
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                if (!resultSet.next()) {
+                    return null;
+                }
+                return RoomDto.from(resultSet.getString("Name"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
