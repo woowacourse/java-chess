def7f8eb0a620079052f2000f5bde658fc65823c
nailseong <nnis9857@gmail.com>
2022-04-03 17:54:58 +0900

    feat(init.sql): Status 테이블

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -7,3 +7,10 @@ CREATE TABLE ChessBoard
     Color      VARCHAR(10) NOT NULL,
     PRIMARY KEY (Position)
 );
+
+CREATE TABLE Status
+(
+    GameStatus  VARCHAR(10) NOT NULL,
+    CurrentTurn VARCHAR(10) NOT NULL
+);
+
