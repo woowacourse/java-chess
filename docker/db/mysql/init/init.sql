8a72c8ed0b7e473edc1caf0efd016510d3e95a98
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-03 14:47:04 +0900

    feat(docker): config mysql db and create tables

---
+++ docker/db/mysql/init/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE game
+(
+    id    bigint not null,
+    state ENUM('WHITE_TURN', 'BLACK_TURN', 'OVER') not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE piece
+(
+    game_id  bigint     not null,
+    position varchar(2) not null,
+    type     ENUM('PAWN', 'KNIGHT', 'BISHOP', 'ROOK', 'QUEEN', 'KING') not null,
+    color    ENUM('WHITE', 'BLACK') not null,
+    primary key (game_id, position),
+    foreign key (game_id) references game (id)
+);
+
+CREATE TABLE result
+(
+    game_id     bigint not null,
+    winner      ENUM('WHITE', 'BLACK') not null,
+    white_score float  not null,
+    black_score float  not null,
+    primary key (game_id),
+    foreign key (game_id) references game (id)
+);
