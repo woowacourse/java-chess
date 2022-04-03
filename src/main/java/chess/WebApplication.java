42bb50c545041263714a3c4b22412815585dfe70
injoon2019 <ison@sfu.ca>
2022-04-03 11:06:11 +0900

    feat: WebController 생성

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,25 +1,13 @@
 package chess;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
-import static spark.Spark.staticFiles;
+
+import chess.controller.ChessWebController;
 
 public class WebApplication {
     public static void main(String[] args) {
         staticFileLocation("/static");
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
-        });
-    }
-
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+        ChessWebController controller = new ChessWebController();
+        controller.run();
     }
 }
