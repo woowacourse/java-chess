574f5696d8b85aee3c3d99d583f2b14f2f07f7a4
nailseong <nnis9857@gmail.com>
2022-04-03 13:17:31 +0900

    feat(Color): 문자열에 해당하는 색깔을 찾는 기능

--- src/main/java/chess/domain/chesspiece/Color.java
+++ src/main/java/chess/domain/chesspiece/Color.java
@@ -1,5 +1,7 @@
 package chess.domain.chesspiece;
 
+import java.util.Arrays;
+
 public enum Color {
 
     WHITE("white"),
@@ -9,6 +11,13 @@ public enum Color {
 
     Color(final String value) {
         this.value = value;
+    }
+
+    public static Color from(String value) {
+        return Arrays.stream(values())
+                .filter(it -> it.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("색깔을 찾을 수 없습니다."));
     }
 
     public boolean isBlack() {
--- src/test/java/chess/domain/chesspiece/ColorTest.java
+++ src/test/java/chess/domain/chesspiece/ColorTest.java
@@ -1,8 +1,11 @@
 package chess.domain.chesspiece;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,5 +20,29 @@ class ColorTest {
 
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+    
+    @ParameterizedTest
+    @DisplayName("문자열로 색깔을 찾는다.")
+    @CsvSource(value = {"black:BLACK", "white:WHITE"}, delimiter = ':')
+    void from(String value, Color expected) {
+        // when
+        final Color actual = Color.from(value);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("문자열에 해당하는 색깔이 없으면 예외를 던진다.")
+    void from_exception() {
+        // given
+        final String value = "green";
+
+        // then
+        assertThatThrownBy(() -> Color.from(value))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("색깔을 찾을 수 없습니다."
+                        );
     }
 }
