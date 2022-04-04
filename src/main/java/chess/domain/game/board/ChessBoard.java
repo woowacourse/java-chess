f9ebc88d08790ccb3eca47d663ba0977e20174b2
Juhyung990122 <harukawa99@naver.com>
2022-04-04 10:42:22 +0900

    refactor: 매개변수, 메소드 명 변경

--- src/main/java/chess/domain/game/board/ChessBoard.java
+++ src/main/java/chess/domain/game/board/ChessBoard.java
@@ -21,39 +21,39 @@ public class ChessBoard {
         this.chessBoard = board;
     }
 
-    public void move(Position from, Position to) {
-        ChessPiece me = checkPiece(from, to);
+    public void move(Position source, Position target) {
+        ChessPiece piece = checkPieceMove(source, target);
 
-        if (isEmptyDestination(to)) {
-            moveEmptyPosition(from, to, me);
+        if (isEmptyBlock(target)) {
+            moveEmptyPosition(source, target, piece);
             return;
         }
 
-        if (enemyExist(me, to)) {
-            catchAndMove(from, to, me);
+        if (enemyExist(piece, target)) {
+            catchAndMove(source, target, piece);
         }
     }
 
-    private ChessPiece checkPiece(Position from, Position to) {
-        ChessPiece me = findPiece(from)
+    private ChessPiece checkPieceMove(Position source, Position target) {
+        ChessPiece piece = findPiece(source)
                 .orElseThrow(() -> new IllegalArgumentException("해당 위치에 기물이 존재하지 않습니다."));
 
-        checkMove(from, to, me);
-        return me;
+        checkMove(source, target, piece);
+        return piece;
     }
 
-    private void checkMove(Position from, Position to, ChessPiece me) {
-        validateTurn(me);
-        me.checkMovable(from, to);
-        Stack<Position> routes = me.findRoute(from, to);
+    private void checkMove(Position source, Position target, ChessPiece piece) {
+        validateTurn(piece);
+        piece.checkMovable(source, target);
+        Stack<Position> routes = piece.findRoute(source, target);
 
         while (!routes.isEmpty()) {
             checkHurdle(routes.pop());
         }
     }
 
-    private void validateTurn(ChessPiece me) {
-        if (currentTurn != me.getColor()) {
+    private void validateTurn(ChessPiece piece) {
+        if (currentTurn != piece.getColor()) {
             throw new IllegalArgumentException(currentTurn.name() + "의 차례입니다.");
         }
     }
@@ -64,49 +64,49 @@ public class ChessBoard {
         }
     }
 
-    private boolean isEmptyDestination(Position to) {
-        return findPiece(to).isEmpty();
+    private boolean isEmptyBlock(Position target) {
+        return findPiece(target).isEmpty();
     }
 
-    private void moveEmptyPosition(Position from, Position to, ChessPiece me) {
-        checkPawnStraightMove(from, to, me);
-        movePiece(from, to, me);
+    private void moveEmptyPosition(Position source, Position target, ChessPiece piece) {
+        checkPawnStraightMove(source, target, piece);
+        movePiece(source, target, piece);
     }
 
-    private void catchAndMove(Position from, Position to, ChessPiece me) {
-        checkPawnCrossMove(from, to, me);
-        movePiece(from, to, me);
+    private void catchAndMove(Position source, Position target, ChessPiece piece) {
+        checkPawnCrossMove(source, target, piece);
+        movePiece(source, target, piece);
     }
 
-    private void checkPawnStraightMove(Position from, Position to, ChessPiece me) {
-        if (me.isPawn() && isCross(from, to)) {
+    private void checkPawnStraightMove(Position source, Position target, ChessPiece piece) {
+        if (piece.isPawn() && isCross(source, target)) {
             throw new IllegalArgumentException("폰은 대각선에 상대 기물이 존재해야합니다");
         }
     }
 
-    private boolean isCross(Position from, Position to) {
-        return to.findDirection(from) != Direction.N && to.findDirection(from) != Direction.S;
+    private boolean isCross(Position source, Position target) {
+        return target.findDirection(source) != Direction.N && target.findDirection(source) != Direction.S;
     }
 
-    private void checkPawnCrossMove(Position from, Position to, ChessPiece me) {
-        if (me.isPawn() && isStraight(from, to)) {
+    private void checkPawnCrossMove(Position source, Position target, ChessPiece piece) {
+        if (piece.isPawn() && isStraight(source, target)) {
             throw new IllegalArgumentException("폰은 대각선 이동으로 적을 잡을 수 있습니다.");
         }
     }
 
-    private boolean isStraight(Position from, Position to) {
-        return to.findDirection(from) == Direction.N || to.findDirection(from) == Direction.S;
+    private boolean isStraight(Position source, Position target) {
+        return target.findDirection(source) == Direction.N || target.findDirection(source) == Direction.S;
     }
 
-    private void movePiece(Position from, Position to, ChessPiece me) {
-        checkMate(to);
-        chessBoard.put(to, me);
-        chessBoard.remove(from);
+    private void movePiece(Position source, Position target, ChessPiece piece) {
+        checkMate(target);
+        chessBoard.put(target, piece);
+        chessBoard.remove(source);
         currentTurn = currentTurn.toOpposite();
     }
 
-    private void checkMate(Position to) {
-        ChessPiece pieceOfTo = chessBoard.get(to);
+    private void checkMate(Position target) {
+        ChessPiece pieceOfTo = chessBoard.get(target);
         if (pieceOfTo != null && pieceOfTo.isKing()) {
             gameStatus = GameStatus.END;
         }
@@ -121,14 +121,14 @@ public class ChessBoard {
         return Optional.ofNullable(chessBoard.get(position));
     }
 
-    public boolean enemyExist(ChessPiece me, Position to) {
-        Optional<ChessPiece> possiblePiece = findPiece(to);
+    public boolean enemyExist(ChessPiece piece, Position target) {
+        Optional<ChessPiece> possiblePiece = findPiece(target);
         if (possiblePiece.isEmpty()) {
             throw new IllegalArgumentException("폰은 대각선에 상대 기물이 존재해야합니다");
         }
 
-        ChessPiece piece = possiblePiece.get();
-        if (piece.isSameColorPiece(me)) {
+        ChessPiece targetPiece = possiblePiece.get();
+        if (targetPiece.isSameColorPiece(piece)) {
             throw new IllegalArgumentException("같은색 기물입니다.");
         }
 
