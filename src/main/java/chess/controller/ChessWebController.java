f46e12ed7a013f429c63d34632529413ba65a380
chawani <chawani@naver.com>
2022-04-05 01:09:10 +0900

    feat: 웹에서 말을 움직이도록 구현

--- src/main/java/chess/controller/ChessWebController.java
+++ src/main/java/chess/controller/ChessWebController.java
@@ -2,8 +2,7 @@ package chess.controller;
 
 import static spark.Spark.*;
 
-import chess.domain.board.Board;
-import chess.domain.board.BoardFactory;
+import chess.domain.game.ChessGame;
 import chess.dto.BoardDto;
 import spark.ModelAndView;
 import spark.Request;
@@ -12,15 +11,33 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class ChessWebController {
 
+    private ChessGame chessGame = new ChessGame();
+
     public void run() {
         staticFileLocation("/static");
 
         get("/", this::start);
+        post("/move", this::move);
+        get("/update", this::update);
     }
 
     private String start(Request request, Response response) {
-        final var board = new Board(BoardFactory.getInitialPieces());
-        return render(new BoardDto(board.getValue()), "index.html");
+        chessGame.start();
+        return render(new BoardDto(chessGame.getBoard().getValue()), "index.html");
+    }
+
+    private String move(Request request, Response response) {
+        String source = request.queryMap().get("source").value();
+        String target = request.queryMap().get("target").value();
+
+        chessGame.movePiece(source, target);
+
+        response.redirect("/update");
+        return null;
+    }
+
+    private String update(Request request, Response response) {
+        return render(new BoardDto(chessGame.getBoard().getValue()), "index.html");
     }
 
     private String render(Object model, String templatePath) {
