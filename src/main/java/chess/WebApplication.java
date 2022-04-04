a9b5ee608d4eced2910395d6fe90114e6ca985cb
her0807 <her0807@naver.com>
2022-04-04 17:04:01 +0900

    feat: 체스판 출력

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,22 +1,33 @@
 package chess;
 
+import chess.domain.board.BasicBoardStrategy;
+import chess.domain.board.Board;
+import chess.domain.piece.Piece;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
 
 public class WebApplication {
-    public static void main(String[] args) {
+
+    public static void main(final String... args) {
+        port(8081);
+        staticFileLocation("/static");
         get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
+            Board board = new Board();
+            board.initBoard(new BasicBoardStrategy());
+            Map<String, Piece> model = board.toMap();
             return render(model, "index.html");
         });
     }
 
-    private static String render(Map<String, Object> model, String templatePath) {
+    private static String render(Map<String, Piece> model, String templatePath) {
         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
+
