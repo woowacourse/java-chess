0d4c3856f6aab696b744363de98bf37195c1913f
yeon-06 <rnjstldus2@gmail.com>
2022-04-04 12:07:29 +0900

    refactor: 메서드 분리

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,11 +1,15 @@
 package chess;
 
+import static spark.Spark.staticFileLocation;
+
 import chess.controller.ChessController;
 import chess.domain.board.generator.BasicBoardGenerator;
 import chess.domain.board.generator.BoardGenerator;
 
 public class WebApplication {
     public static void main(String[] args) {
+        staticFileLocation("/static");
+
         BoardGenerator boardGenerator = new BasicBoardGenerator();
         ChessController chessController = new ChessController();
         chessController.run(boardGenerator);
--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -2,7 +2,6 @@ package chess.controller;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
 
 import chess.domain.board.Board;
 import chess.domain.board.Score;
@@ -18,78 +17,55 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class ChessController {
     public void run(BoardGenerator boardGenerator) {
-        staticFileLocation("/static");
-
         ChessGame chessGame = new ChessGame();
 
-        // 초기 페이지
         get("/", (request, response) -> render(new HashMap<>(), "index.html"));
-
-        // 보드 출력
-        get("/board", (request, response) -> {
-            Map<String, Object> model = generateBoardModel(chessGame);
-            return render(model, "board.html");
-        });
-
-        // 새로운 보드 생성
-        post("/new", (request, response) -> {
-            chessGame.start(boardGenerator);
-            response.redirect("/board");
-            return null;
-        });
-
-        // 기물 이동
-        get("/move", (request, response) -> {
-            List<String> inputs = new ArrayList<>();
-            inputs.add("move");
-            inputs.add(request.queryParams("from"));
-            inputs.add(request.queryParams("to"));
-
-            Map<String, Object> model = new HashMap<>();
-            if (!chessGame.move(inputs)) {
-                model.put("message", "잘못된 이동입니다.");
-            }
-            model.putAll(generateBoardModel(chessGame));
-
-            return render(model, "board.html");
-        });
-
-        // 점수 보기
-        get("/status", (request, response) -> {
-            Score score = chessGame.status();
-
-            Map<String, Object> model = generateBoardModel(chessGame);
-            model.put("message", drawScoreSentence(score));
-
-            return render(model, "board.html");
-        });
-
-        // 게임 종료
-        get("/end", (request, response) -> {
-            chessGame.end();
-            response.redirect("/");
-            return null;
-        });
+        get("/board", (request, response) -> renderBoard(chessGame));
+        post("/new", (request, response) -> createNewBoard(boardGenerator, chessGame, response));
+        get("/move", (request, response) -> move(chessGame, request));
+        get("/status", (request, response) -> renderStatus(chessGame));
+        get("/end", (request, response) -> end(chessGame, response));
     }
 
-    private Map<String, Object> generateBoardModel(ChessGame chessGame) {
-        Board board = chessGame.getBoard();
+    private String renderBoard(ChessGame chessGame) {
+        Map<String, Object> model = generateBoardModel(chessGame);
+        return render(model, "board.html");
+    }
+
+    private Object createNewBoard(BoardGenerator boardGenerator, ChessGame chessGame, Response response) {
+        chessGame.start(boardGenerator);
+        response.redirect("/board");
+        return null;
+    }
+
+    private String move(ChessGame chessGame, Request request) {
+        List<String> inputs = new ArrayList<>();
+        inputs.add("move");
+        inputs.add(request.queryParams("from"));
+        inputs.add(request.queryParams("to"));
 
         Map<String, Object> model = new HashMap<>();
-        for (Entry<Position, Piece> entry : board.getValue().entrySet()) {
-            model.put(entry.getKey().getName()
-                    , new PieceDto(entry.getValue()));
+        if (!chessGame.move(inputs)) {
+            model.put("message", "잘못된 이동입니다.");
         }
-        model.put("turn", chessGame.getTurn());
-        return model;
+        model.putAll(generateBoardModel(chessGame));
+
+        return render(model, "board.html");
     }
 
-    private String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    private String renderStatus(ChessGame chessGame) {
+        Score score = chessGame.status();
+
+        Map<String, Object> model = generateBoardModel(chessGame);
+        model.put("message", drawScoreSentence(score));
+
+        return render(model, "board.html");
     }
 
     private String drawScoreSentence(Score score) {
@@ -110,5 +86,27 @@ public class ChessController {
             return "무승부";
         }
         return String.format("승리 팀: %s", team);
+    }
+
+    private Object end(ChessGame chessGame, Response response) {
+        chessGame.end();
+        response.redirect("/");
+        return null;
+    }
+
+    private Map<String, Object> generateBoardModel(ChessGame chessGame) {
+        Board board = chessGame.getBoard();
+
+        Map<String, Object> model = new HashMap<>();
+        for (Entry<Position, Piece> entry : board.getValue().entrySet()) {
+            model.put(entry.getKey().getName()
+                    , new PieceDto(entry.getValue()));
+        }
+        model.put("turn", chessGame.getTurn());
+        return model;
+    }
+
+    private String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
