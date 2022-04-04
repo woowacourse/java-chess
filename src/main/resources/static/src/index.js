345b7621948ab9f5b900b6fed229ef4dbd4706c1
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-05 02:18:23 +0900

    feat(front): start function 추가

--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -1,8 +1,16 @@
+let gameStatus = "";
+
+async function setUpIndex() {
+    await displayBoard();
+    const tweetDiv = document.createElement('div');
+    document.body.append(tweetDiv);
+}
+
 async function displayBoard() {
     Array.from(await getBoard()).forEach(
         function(element) {
             const imgTeg = document.createElement("img");
-            const imgPath = "images/" + element.color + "_" + element.name + ".png";
+            const imgPath = `images/${element.color}_${element.name}.png`;
 
             imgTeg.setAttribute("src", imgPath);
             document.getElementById(element.position).appendChild(imgTeg);
@@ -13,4 +21,13 @@ async function displayBoard() {
 async function getBoard() {
     return await fetch("/board")
         .then((response) => response.json());
+}
+
+async function startChessGame() {
+    await fetch("/start", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    });
 }
