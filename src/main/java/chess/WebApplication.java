62a830844f88a2bb7afa06ef67dfc253df1811f6
bomi <bonita9931@gmail.com>
2022-04-04 14:54:46 +0900

    docs - 4단계 기능 요구사항 정리

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -7,9 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class WebApplication {
     public static void main(String[] args) {
+        port(8081);
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
