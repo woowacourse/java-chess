ea0c32b1ff500db298b9fd0a058ec5bb8b6e7b15
skullKim <kyk990328@gmail.com>
2022-04-04 17:26:31 +0900

    feat(init.sql): 말들 정보 추가
    말들에 대한 정보 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,16 +1,18 @@
 CREATE DATABASE IF NOT EXISTS chess;
 
+-- game status
 CREATE TABLE game_status(
     id INT NOT NULL AUTO_INCREMENT,
     game_status VARCHAR(15) NOT NULL,
     PRIMARY KEY (id),
     INDEX game_status_index (game_status ASC));
 
-INSERT INTO game_status(game_status) VALUES("READY");
-INSERT INTO game_status(game_status) VALUES("WHITE_ROOM");
-INSERT INTO game_status(game_status) VALUES("BLACK_ROOM");
-INSERT INTO game_status(game_status) VALUES("END");
+INSERT INTO game_status(game_status) VALUES('READY');
+INSERT INTO game_status(game_status) VALUES('WHITE_ROOM');
+INSERT INTO game_status(game_status) VALUES('BLACK_ROOM');
+INSERT INTO game_status(game_status) VALUES('END');
 
+-- user
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
@@ -21,15 +23,32 @@ CREATE TABLE user (
     FOREIGN KEY (game_status_id)
     REFERENCES game_status (id));
 
+-- piece
 CREATE TABLE piece (
    id INT NOT NULL AUTO_INCREMENT,
    symbol VARCHAR(1) NOT NULL,
-   score INT NOT NULL,
-   color VARCHAR(1) NOT NULL,
+   score DOUBLE NOT NULL,
+   color VARCHAR(10) NOT NULL,
    image_url VARCHAR(100) NOT NULL,
-   PRIMARY KEY (id)
-);
+   PRIMARY KEY (id));
 
+INSERT INTO piece(symbol, score, color, image_url) VALUES('q', 9, 'BLACK', '/images/pieces/black/black-queen.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('r', 5, 'BLACK', '/images/pieces/black/black-rook.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('b', 3, 'BLACK', '/images/pieces/black/black-bishop.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('n', 2.5, 'BLACK', '/images/pieces/black-knight.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('p', 1, 'BLACK', '/images/pieces/black/black-pawn.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('k', 0, 'BLACK', '/images/pieces/black/black-king.svg');
+
+INSERT INTO piece(symbol, score, color, image_url) VALUES('q', 9, 'WHITE', '/images/pieces/white/white-queen.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('r', 5, 'WHITE', '/images/pieces/white/white-rook.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('b', 3, 'WHITE', '/images/pieces/white/white-bishop.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('n', 2.5, 'WHITE', '/images/pieces/white/white-knight.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('p', 1, 'WHITE', '/images/pieces/white/white-pawn.svg');
+INSERT INTO piece(symbol, score, color, image_url) VALUES('k', 0, 'WHITE', '/images/pieces/white/white-king.svg');
+
+INSERT INTO piece(symbol, score, color, image_url) VALUE('.', 0, 'NONE', '');
+
+-- board
 CREATE TABLE board(
     id INT NOT NULL AUTO_INCREMENT,
     piece_id INT NOT NULL,
