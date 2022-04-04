965038d0577e6f228b166c1af588c05224a247cf
hyunrrr <whgusrms96@gmail.com>
2022-04-04 20:14:21 +0900

    feat: 말 움직이면 ui도 움직이도록 하는 기능 추가

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,8 +1,10 @@
 package chess;
 
 import chess.controller.Command;
+import chess.domain.ChessBoardPosition;
 import chess.domain.ChessGame;
 import chess.webview.WebOutputView;
+import java.util.List;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -12,6 +14,25 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class WebApplication {
+    private static final int COMMAND_INDEX = 0;
+    private static final int SOURCE_INDEX = 1;
+    private static final int TARGET_INDEX = 2;
+    private static final Map<Character, Integer> convertColumn = new HashMap<>();
+    private static final int COLUMN_INDEX = 0;
+    private static final int ROW_INDEX = 1;
+    private static final String IN_GAME_COMMAND_DELIMITER = " ";
+
+    static {
+        convertColumn.put('a', 1);
+        convertColumn.put('b', 2);
+        convertColumn.put('c', 3);
+        convertColumn.put('d', 4);
+        convertColumn.put('e', 5);
+        convertColumn.put('f', 6);
+        convertColumn.put('g', 7);
+        convertColumn.put('h', 8);
+    }
+
     public static void main(String[] args) {
         port(8082);
         staticFiles.location("/public");
@@ -21,7 +42,6 @@ public class WebApplication {
             Command command = Command.of(req.queryParams("command"));
             if (Command.START.equals(command)) {
                 chessGame.initialze();
-                System.out.println("@@@@@@@@@@@@startstart@@@@@@@@@@@@@@@@@");
                 res.redirect("/board");
                 return null;
             }
@@ -32,9 +52,41 @@ public class WebApplication {
         });
 
         get("/board", (req, res) -> {
-            System.out.println("@@@@@@@@@@@@@@boardboardboard@@@@@@@@@@@@@@@@@@@@@@");
             return render(WebOutputView.makeBoardModel(chessGame.getChessBoardInformation()), "board.html");
         });
+
+        post("/inGameCommand", (req, res) -> {
+            List<String> inputs = divideInGameCommandInput(req.queryParams("command"));
+            Command command = Command.of(inputs.get(COMMAND_INDEX));
+            if (Command.MOVE.equals(command)) {
+                ChessBoardPosition source = coordinateToChessBoardPosition(inputs.get(SOURCE_INDEX));
+                ChessBoardPosition target = coordinateToChessBoardPosition(inputs.get(TARGET_INDEX));
+                chessGame.move(source, target);
+                res.redirect("/board");
+                return null;
+            }
+            if (Command.STATUS.equals(command)) {
+                res.redirect("/status");
+            }
+            return null;
+        });
+    }
+
+    private static List<String> divideInGameCommandInput(String command) {
+        return List.of(command.split(IN_GAME_COMMAND_DELIMITER));
+    }
+
+    private static ChessBoardPosition coordinateToChessBoardPosition(String coordinate) {
+        System.out.println(coordinate);
+        return ChessBoardPosition.of(extractColumn(coordinate), extractRow(coordinate));
+    }
+
+    private static int extractColumn(String input) {
+        return convertColumn.get(input.charAt(COLUMN_INDEX));
+    }
+
+    private static int extractRow(String input) {
+        return Character.getNumericValue(input.charAt(ROW_INDEX));
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
