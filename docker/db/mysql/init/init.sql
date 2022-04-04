c366e3754e595d0805080f8631e0a885ab11a395
skullKim <kyk990328@gmail.com>
2022-04-04 17:37:48 +0900

    fix(init.sql): user 테이블 외래키 생성
    유저와 보드의 관계를 나타내기 위해 유저 테이블에 보드에 대한 외래키 생성

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,28 +1,5 @@
 CREATE DATABASE IF NOT EXISTS chess;
 
--- game status
-CREATE TABLE game_status(
-    id INT NOT NULL AUTO_INCREMENT,
-    game_status VARCHAR(15) NOT NULL,
-    PRIMARY KEY (id),
-    INDEX game_status_index (game_status ASC));
-
-INSERT INTO game_status(game_status) VALUES('READY');
-INSERT INTO game_status(game_status) VALUES('WHITE_ROOM');
-INSERT INTO game_status(game_status) VALUES('BLACK_ROOM');
-INSERT INTO game_status(game_status) VALUES('END');
-
--- user
-CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL,
-    game_status_id INT NOT NULL DEFAULT 1,
-    PRIMARY KEY (id),
-    INDEX name_idx (name ASC),
-    UNIQUE INDEX name_UNIQUE (name ASC),
-    FOREIGN KEY (game_status_id)
-    REFERENCES game_status (id));
-
 -- piece
 CREATE TABLE piece (
    id INT NOT NULL AUTO_INCREMENT,
@@ -50,12 +27,38 @@ INSERT INTO piece(symbol, score, color, image_url) VALUE('.', 0, 'NONE', '');
 
 -- board
 CREATE TABLE board(
+   id INT NOT NULL AUTO_INCREMENT,
+   piece_id INT NOT NULL,
+   position VARCHAR(2) NOT NULL,
+   PRIMARY KEY (id),
+   UNIQUE INDEX positino_UNIQUE (position ASC),
+   FOREIGN KEY (piece_id) REFERENCES piece(id));
+
+-- game status
+CREATE TABLE game_status(
     id INT NOT NULL AUTO_INCREMENT,
-    piece_id INT NOT NULL,
-    position VARCHAR(2) NOT NULL,
+    game_status VARCHAR(15) NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE INDEX positino_UNIQUE (position ASC),
-    FOREIGN KEY (piece_id)
-    REFERENCES piece(id));
+    INDEX game_status_index (game_status ASC));
+
+INSERT INTO game_status(game_status) VALUES('READY');
+INSERT INTO game_status(game_status) VALUES('WHITE_ROOM');
+INSERT INTO game_status(game_status) VALUES('BLACK_ROOM');
+INSERT INTO game_status(game_status) VALUES('END');
+
+-- user
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    game_status_id INT NOT NULL DEFAULT 1,
+    board_id INT NOT NULL,
+    PRIMARY KEY (id),
+    INDEX name_idx (name ASC),
+    UNIQUE INDEX name_UNIQUE (name ASC),
+    FOREIGN KEY (game_status_id) REFERENCES game_status (id),
+    FOREIGN KEY (board_id) REFERENCES board (id));
+
+
+
 
 
