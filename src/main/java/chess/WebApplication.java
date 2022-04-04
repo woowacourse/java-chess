ad1c245470d19b519ff330c014addf16e42a03b9
awesomeo184 <awesomeo184@gmail.com>
2022-04-04 16:06:15 +0900

    feat: 체스판 마크업

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,6 +1,8 @@
 package chess;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +11,10 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebApplication {
     public static void main(String[] args) {
+        port(8081);
+
+        staticFileLocation("/static");
+
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
