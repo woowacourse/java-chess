e4d2e3df3f8c33bdade1840b0f4260729552e37a
injoon2019 <ison@sfu.ca>
2022-04-05 17:03:05 +0900

    refactor: PieceColor 수정

--- src/main/java/chess/domain/piece/PieceColor.java
+++ src/main/java/chess/domain/piece/PieceColor.java
@@ -1,6 +1,25 @@
 package chess.domain.piece;
 
+import java.util.stream.Stream;
+
 public enum PieceColor {
-    BLACK,
-    WHITE
+    BLACK("BLACK"),
+    WHITE("WHITE");
+
+    private final String value;
+
+    PieceColor(String name) {
+        this.value = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static PieceColor getPieceColor(String pieceColor) {
+        return Stream.of(PieceColor.values())
+                .filter(value -> value.getValue().equals(pieceColor))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 색상입니다."));
+    }
 }
