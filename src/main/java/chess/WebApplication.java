8d3839763d5477cb5023c02b59c77a97d669ae5d
HyeonbinSa <shb1833@gmail.com>
2022-04-05 20:03:56 +0900

    feat: Web UI 적용 - 컨트롤러 분리

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -3,73 +3,55 @@ package chess;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
-import domain.Status;
-import domain.chessgame.ChessBoard;
-import domain.chessgame.ChessGame;
-import domain.piece.Piece;
-import domain.position.Position;
+import controller.WebChessController;
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-import utils.ChessBoardGenerator;
 
 public class WebApplication {
 
     public static void main(String[] args) {
-        ChessBoard chessBoard = ChessBoardGenerator.generate();
-        ChessGame chessGame = new ChessGame(chessBoard);
+        WebChessController webChessController = new WebChessController();
 
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
+
         get("/play", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            Map<Position, Piece> board = chessBoard.getBoard();
-            for (Position position : board.keySet()) {
-                model.put(position.getPosition(), board.get(position).symbolByPlayer());
-            }
+            Map<String, Object> model = webChessController.modelBoard();
             return render(model, "index.html");
         });
+
         get("/start", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            Map<Position, Piece> board = chessBoard.getBoard();
-            for (Position position : board.keySet()) {
-                model.put(position.getPosition(), board.get(position).symbolByPlayer());
-            }
-            return render(model, "index.html");
+            webChessController.start();
+            res.redirect("/play");
+            return null;
         });
+
         get("/end", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return render(model, "index.html");
         });
+
         get("/status", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            Status status = chessGame.status();
-            model.put("winner", status.winner());
-            model.put("whiteScore", status.getWhiteScore());
-            model.put("blackScore", status.getBlackScore());
+            Map<String, Object> model = webChessController.status();
+            model.put("state", "status");
             return render(model, "index.html");
         });
+
         post("/move", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            String[] source = req.queryParams("source").split("");
-            String[] target = req.queryParams("target").split("");
-            Position sourcePosition = Position.of(source[0], source[1]);
-            Position targetPosition = Position.of(target[0], target[1]);
-            System.out.println(sourcePosition);
-            System.out.println(targetPosition);
-            chessGame.move(sourcePosition, targetPosition);
-            if (chessGame.isFinished()) {
+            boolean isFinished = webChessController.move(req.queryParams("source"),
+                req.queryParams("target"));
+            if (isFinished) {
                 res.redirect("/end");
                 return null;
             }
-            Map<Position, Piece> board = chessBoard.getBoard();
-            for (Position position : board.keySet()) {
-                model.put(position.getPosition(), board.get(position).symbolByPlayer());
-            }
-            return render(model, "index.html");
+            res.redirect("/play");
+            return null;
         });
     }
 
---
+++ src/main/java/controller/WebChessController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import domain.Status;
+import domain.chessgame.ChessBoard;
+import domain.chessgame.ChessGame;
+import domain.piece.Piece;
+import domain.position.Position;
+import java.util.HashMap;
+import java.util.Map;
+import utils.ChessBoardGenerator;
+
+public class WebChessController {
+
+    private ChessGame chessGame;
+
+    public WebChessController() {
+    }
+
+    public void start() {
+        ChessBoard chessBoard = ChessBoardGenerator.generate();
+        chessGame = new ChessGame(chessBoard);
+    }
+
+    public Map<String, Object> modelBoard() {
+        Map<String, Object> model = new HashMap<>();
+        Map<Position, Piece> board = chessGame.getChessBoard().getBoard();
+        for (Position position : board.keySet()) {
+            model.put(position.getPosition(), board.get(position).symbolByPlayer());
+        }
+        return model;
+    }
+
+    public Map<String, Object> status() {
+        Map<String, Object> model = modelBoard();
+        Status status = chessGame.status();
+        model.put("winner", status.winner());
+        model.put("whiteScore", status.getWhiteScore());
+        model.put("blackScore", status.getBlackScore());
+        return model;
+    }
+
+    public boolean move(String source, String target) {
+        String[] sources = source.split("");
+        String[] targets = target.split("");
+        Position sourcePosition = Position.of(sources[0], sources[1]);
+        Position targetPosition = Position.of(targets[0], targets[1]);
+        chessGame.move(sourcePosition, targetPosition);
+        return chessGame.isFinished();
+    }
+}
