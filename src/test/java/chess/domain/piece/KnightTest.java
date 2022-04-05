825f045e79b2cfde7bf9100aeb55bcf615c54532
beomWhale <cndqjacndqja@gmail.com>
2022-04-06 06:13:23 +0900

    test(KnightTest): 테스트 케이스 추가

--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -6,28 +6,41 @@ import chess.domain.position.Position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class KnightTest {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KnightTest {
     @ParameterizedTest
-    @ValueSource(strings = {"d4", "b3", "f5", "f7", "h1"})
-    @DisplayName("출발 지점과 도착 지점을 입력 후, 경로 리스트를 조회한다.")
-    void validateIsPossible(String input) {
-        Position position = Position.from("c4");
+    @ValueSource(strings = {"c5", "d6", "g5"})
+    @DisplayName("출발 지점과 도착 지점을 입력 후, 이동 가능하지 않은 위치라면 예외가 발생한다.")
+    void validateIsPossible(String destination) {
+        Position source = Position.from("d5");
         Knight knight = new Knight(Team.BLACK);
         Assertions.assertThatThrownBy(() -> {
-                    knight.findPath(position, Position.from(input));
+                    knight.findPath(source, Position.from(destination));
                 }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("해당 위치로 말이 움직일 수 없습니다.");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"f6", "d6", "f2", "d2", "c3", "g3", "g5", "c5"})
+    @ValueSource(strings = {"b6", "c7", "e7", "f6", "f4", "e3", "c3", "b4"})
     @DisplayName("출발 지점과 도착 지점을 입력 후, 경로 리스트를 조회한다.")
-    void isPossible(String input) {
-        Position position = Position.from("e4");
+    void findPath(String destination) {
+        // given
+        Position sourcePosition = Position.from("d5");
+        Position destinationPosition = Position.from(destination);
         Knight knight = new Knight(Team.BLACK);
-        knight.findPath(position, Position.from(input));
+
+        // when
+        List<Position> path = knight.findPath(sourcePosition, destinationPosition);
+
+        // then
+        assertThat(path.size()).isEqualTo(0);
     }
 }
