87308de6b8442605244e0bec5c29c9051d392724
tonic523 <diddmlvkf@gmail.com>
2022-04-05 18:26:08 +0900

    refactor(TeamColor): 사용하지 않는 메서드 삭제 및 테스트 추가 구현

--- src/test/java/chess/domain/piece/vo/TeamColorTest.java
+++ src/test/java/chess/domain/piece/vo/TeamColorTest.java
@@ -1,34 +1,20 @@
 package chess.domain.piece.vo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import chess.domain.board.Rank;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class TeamColorTest {
 
     @ParameterizedTest
-    @DisplayName("Rank 값을 받아 팀 컬러를 반환한다.")
-    @CsvSource(value = {"ONE, WHITE", "TWO, WHITE", "SEVEN, BLACK", "EIGHT, BLACK"})
-    void findByRank(final Rank rank, final TeamColor expected) {
+    @DisplayName("턴을 바꾼다.")
+    @CsvSource({"WHITE,BLACK", "BLACK,WHITE"})
+    void findByRankException(TeamColor teamColor, TeamColor expected) {
         //when
-        TeamColor actual = TeamColor.findByRank(rank);
+        TeamColor actual = teamColor.nextTurn();
         //then
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("1,2,7,8 이 아닌 Rank 값으로 팀 컬러를 찾으려 하면 예외를 발생시킨다.")
-    void findByRankException() {
-        //given
-        Rank rank = Rank.FIVE;
-        //when, then
-        assertThatThrownBy(() -> TeamColor.findByRank(rank))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("팀 컬러를 식별할 수 있는 Rank는 1,2,7,8 입니다.");
+        Assertions.assertThat(actual).isEqualTo(expected);
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
