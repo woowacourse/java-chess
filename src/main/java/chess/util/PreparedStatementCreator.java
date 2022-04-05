28705543834a0171ee096aabb980d6053ef70d49
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-05 22:40:00 +0900

    refactor: FunctionalInterface 어노테이션 추가

--- src/main/java/chess/util/PreparedStatementCreator.java
+++ src/main/java/chess/util/PreparedStatementCreator.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementCreator {
 
     PreparedStatement excute(Connection connection) throws SQLException;
--- src/main/java/chess/util/ResultSetMapper.java
+++ src/main/java/chess/util/ResultSetMapper.java
@@ -3,6 +3,7 @@ package chess.util;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface ResultSetMapper<T> {
 
     T map(ResultSet resultSet) throws SQLException;
