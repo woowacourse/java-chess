2da4f7e503c22c32f3f8f3003b156edc95076d55
hyeonic <dev.hyeonic@gmail.com>
2022-04-05 16:31:13 +0900

    docs: DB 테이블 초기 세팅 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -2,6 +2,11 @@ create table piece
 (
     id         varchar(2)  not null,
     piece_type varchar(10) not null,
-    color      varchar(10) not null,
     primary key (id)
 );
+
+create table turn
+(
+    turn_type varchar(10) not null,
+    primary key (turn_type)
+)
