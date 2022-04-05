b9de84c3901096a1ea7a2779e2d2337d1bbca463
bomi <bonita9931@gmail.com>
2022-04-05 15:53:02 +0900

    feat - 누구의 차례인지 표시하는 display 추가

--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -1,3 +1,4 @@
+
 const startGame = () => {
     const response = fetch(`/start`, {
         method: "GET",
@@ -6,13 +7,16 @@ const startGame = () => {
     response.then(data => data.json())
         .then(body => {
             Object.entries(body).forEach((entry) => {
-                const piece = entry[1];
                 if (entry[1].includes('.')) {
                     return;
                 }
-                const key = document.getElementById(entry[0]);
+                const block = document.getElementById(entry[0]);
                 const imgSrc ="/images/" + entry[1] + ".svg";
-                key.innerHTML = '<img id = "piece-image" class="piece-image" src=' + imgSrc + '/>'
+                block.innerHTML = '<img id = "piece-image" class="piece-image" src=' + imgSrc + '/>'
             })
+            const button = document.getElementById("start-button")
+            button.innerText = "end!";
+            const turnBox = document.getElementById("turn-box")
+            turnBox.innerText = "white 팀 차례!";
         })
 }
