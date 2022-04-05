236bab48aaec902015f91f68c0a0000c74f8cde3
is2js <tingstyle1@gmail.com>
2022-04-06 00:27:22 +0900

    feat(chess/dao): CampDao 기능 구현

---
+++ src/main/java/chess/dao/CampDao.java
@@ -0,0 +1,52 @@
+package chess.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CampDao {
+
+    private static final String URL = "jdbc:mysql://localhost:13306/chess";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static final String TABLE = "camp";
+    private static final String CURRENT_CAMP = "currentCamp";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public String findCurrentCamp() {
+        final String sql = "" + "SELECT currentCamp" + "  FROM " + TABLE;
+        try (final PreparedStatement statement = getConnection().prepareStatement(
+            sql); final ResultSet resultSet = statement.executeQuery()) {
+            if (!resultSet.next()) {
+                return null;
+            }
+            return resultSet.getString(CURRENT_CAMP);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void update(final String camp) {
+        final String sql = ""
+            + "UPDATE " + TABLE
+            + " SET currentCamp = ?";
+        try (final PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, camp);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/chess/domain/CampTest.java
@@ -0,0 +1,12 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CampTest {
+    @DisplayName("")
+    @Test
+    void name() {
+        System.out.println(Camp.WHITE.name());
+    }
+}
