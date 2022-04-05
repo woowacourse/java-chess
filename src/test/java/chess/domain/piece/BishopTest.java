919d25630a0509f1e863c5ad6b59d7c478b30852
beomWhale <cndqjacndqja@gmail.com>
2022-04-06 06:12:04 +0900

    test(Bishop): 테스트 케이스 추가

--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -1,22 +1,23 @@
 package chess.domain.piece;
 
 import chess.domain.Team;
-import chess.domain.piece.Bishop;
 import chess.domain.position.Position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BishopTest {
     @ParameterizedTest
     @ValueSource(strings = {"d4", "c5", "d6", "e5"})
-    @DisplayName("출발 지점과 도착 지점을 입력 후, 경로 리스트를 조회한다.")
+    @DisplayName("출발 지점과 도착 지점을 입력 후, 이동 가능하지 않은 위치라면 예외가 발생한다.")
     void validateIsPossible(String input) {
         Position source = Position.from("d5");
         Bishop bishop = new Bishop(Team.BLACK);
@@ -27,20 +28,19 @@ class BishopTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"h7", "a8", "b1", "h1"})
-    @DisplayName("출발 지점과 도착 지점을 입력 후, 이동 가능한 경로인지 검증한다.")
-    void isPossible(String input) {
-        Position source = Position.from("e4");
-        Bishop bishop = new Bishop(Team.BLACK);
-        bishop.findPath(source, Position.from(input));
-    }
-
-    @Test
+    @CsvSource(value = {"c1:g5:d2,e3,f4", "f1:b5:e2,d3,c4", "c8:f5:d7,e6"}, delimiter = ':')
     @DisplayName("출발 지점과 도착 지점을 입력 후, 경로 리스트를 조회한다.")
-    void findPath() {
-        Position source = Position.from("e4");
-        Bishop bishop = new Bishop(Team.BLACK);
-        List<Position> path = bishop.findPath(source, Position.from("h7"));
-        assertThat(path).containsExactly(Position.from("f5"), Position.from("g6"));
+    void findPath(String source, String destination, String paths) {
+        // given
+        Bishop bishop = new Bishop(Team.WHITE);
+        List<Position> expected = Arrays.stream(paths.split(","))
+                .map(Position::from)
+                .collect(Collectors.toList());
+
+        // when
+        List<Position> path = bishop.findPath(Position.from(source), Position.from(destination));
+
+        // then
+        assertThat(path).isEqualTo(expected);
     }
 }
