664e2aae6e83adde9be3f871f34e607a66545afe
pup-paw <wldusdl0310@gmail.com>
2022-04-05 22:56:50 +0900

    feat: 시작 페이지와 인게임 페이지 연결
    게임 중 불가능한 move 에 대한 에러 메세지 출력

--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -2,15 +2,16 @@ package chess.controller;
 
 import static spark.Spark.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import chess.domain.board.Board;
+import chess.domain.ChessGame;
 import chess.domain.board.InitialBoardGenerator;
+import chess.domain.position.Square;
 import chess.dto.BoardDto;
-import chess.dto.PieceDto;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -19,22 +20,37 @@ public class WebController {
         staticFiles.location("/static");
         port(8080);
 
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
+        get("/", (request, response) -> render(new HashMap<>(), "/ready.html"));
 
-            Board board = new Board(new InitialBoardGenerator());
-            BoardDto boardDto = new BoardDto(board);
-
-            List<String> emojis = boardDto.getPieces().stream()
-                    .map(PieceDto::getEmoji)
-                    .collect(Collectors.toList());
+        Map<String, Object> model = new HashMap<>();
+        ChessGame chessGame = new ChessGame(new InitialBoardGenerator());
+        get("/ingame", (request, response) -> {
+            chessGame.startGame();
+            BoardDto boardDto = new BoardDto(chessGame.getBoard());
+            List<String> emojis = boardDto.getEmojis();
 
             model.put("pieces", emojis);
+            model.put("error", "누가 이기나 보자구~!");
 
             return render(model, "/ingame.html");
         });
 
-
+        post("/ingame", (request, response) -> {
+            String input = request.queryParams("squares");
+            List<Square> squares = Arrays.stream(input.split(", "))
+                    .map(Square::new)
+                    .collect(Collectors.toList());
+            try {
+                chessGame.move(squares.get(0), squares.get(1));
+                BoardDto boardDto = new BoardDto(chessGame.getBoard());
+                List<String> emojis = boardDto.getEmojis();
+                model.put("pieces", emojis);
+                model.put("error", "누가 이기나 보자구~!");
+            } catch (IllegalArgumentException e) {
+                model.put("error", e.getMessage());
+            }
+            return render(model, "/ingame.html");
+        });
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
