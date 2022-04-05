9469e56666f0250315d07be62830741e3860f906
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-05 20:43:15 +0900

    refactor: piecesMapper 메소드 분리

--- src/main/java/chess/dao/PieceDaoImpl.java
+++ src/main/java/chess/dao/PieceDaoImpl.java
@@ -8,6 +8,7 @@ import chess.util.DBConnection;
 import chess.util.JdbcTemplate;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,21 +28,22 @@ public class PieceDaoImpl implements PieceDao {
     public Map<Position, Piece> findAllPieces() {
         final String query = "select type, color, position_col, position_row from piece";
 
-        return jdbcTemplate.executeSelect(connection -> connection.prepareStatement(query),
-                resultSet -> {
-                    Map<Position, Piece> pieces = new HashMap<>();
-                    while (resultSet.next()) {
-                        String type = resultSet.getString("type");
-                        String colorName = resultSet.getString("color");
-                        char column = resultSet.getString("position_col").charAt(0);
-                        char row = resultSet.getString("position_row").charAt(0);
+        return jdbcTemplate.executeSelect(connection -> connection.prepareStatement(query), this::piecesMapper);
+    }
 
-                        Position position = Position.of(column, row);
-                        Color color = Color.valueOf(colorName);
-                        pieces.put(position, PieceConverter.parseToPiece(type, color));
-                    }
-                    return pieces;
-                });
+    private Map<Position, Piece> piecesMapper(final java.sql.ResultSet resultSet) throws SQLException {
+        Map<Position, Piece> pieces = new HashMap<>();
+        while (resultSet.next()) {
+            String type = resultSet.getString("type");
+            String colorName = resultSet.getString("color");
+            char column = resultSet.getString("position_col").charAt(0);
+            char row = resultSet.getString("position_row").charAt(0);
+
+            Position position = Position.of(column, row);
+            Color color = Color.valueOf(colorName);
+            pieces.put(position, PieceConverter.parseToPiece(type, color));
+        }
+        return pieces;
     }
 
     @Override
