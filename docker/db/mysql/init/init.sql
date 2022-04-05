70fd5c2fce7305231900acb74c04dcacd8f827ee
injoon2019 <ison@sfu.ca>
2022-04-05 17:02:39 +0900

    refactor: 테이블 구조 수정
    - color 컬럼 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -2,52 +2,52 @@ create table game
 (
     id   varchar(10) not null,
     turn varchar(10) not null,
-    status varchar(20) not null,
     primary key (id)
 );
 
-create table status
+create table board
 (
     game_id varchar(10) not null,
     position varchar(10) not null,
     piece varchar(20),
+    color varchar(20),
     primary key (game_id, position)
 );
 
-insert into game (id, turn) values ('1', 'WHITE', 'READY');
+insert into game (id, turn) values ('1', 'WHITE');
 
-insert into status(game_id, position, piece) values ('1', 'a1', 'WHITE_ROOK');
-insert into status(game_id, position, piece) values ('1', 'b1', 'WHITE_BISHOP');
-insert into status(game_id, position, piece) values ('1', 'c1', 'WHITE_KNIGHT');
-insert into status(game_id, position, piece) values ('1', 'd1', 'WHITE_KING');
-insert into status(game_id, position, piece) values ('1', 'e1', 'WHITE_QUEEN');
-insert into status(game_id, position, piece) values ('1', 'f1', 'WHITE_KNIGHT');
-insert into status(game_id, position, piece) values ('1', 'g1', 'WHITE_BISHOP');
-insert into status(game_id, position, piece) values ('1', 'h1', 'WHITE_ROOK');
+insert into board(game_id, position, piece, color) values ('1', 'a1', 'ROOK', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'b1', 'BISHOP', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'c1', 'KNIGHT', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'd1', 'KING', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'e1', 'QUEEN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'f1', 'KNIGHT', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'g1', 'BISHOP', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'h1', 'ROOK', 'WHITE');
 
-insert into status(game_id, position, piece) values ('1', 'a2', 'WHITE_PAWN');
-insert into status(game_id, position, piece) values ('1', 'b2', 'WHITE_PAWN');
-insert into status(game_id, position, piece) values ('1', 'c2', 'WHITE_PAWN');
-insert into status(game_id, position, piece) values ('1', 'd2', 'WHITE_PAWN');
-insert into status(game_id, position, piece) values ('1', 'e2', 'WHITE_PAWN');
-insert into status(game_id, position, piece) values ('1', 'f2', 'WHITE_PAWN');
-insert into status(game_id, position, piece) values ('1', 'g2', 'WHITE_PAWN');
-insert into status(game_id, position, piece) values ('1', 'h2', 'WHITE_PAWN');
+insert into board(game_id, position, piece, color) values ('1', 'a2', 'PAWN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'b2', 'PAWN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'c2', 'PAWN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'd2', 'PAWN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'e2', 'PAWN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'f2', 'PAWN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'g2', 'PAWN', 'WHITE');
+insert into board(game_id, position, piece, color) values ('1', 'h2', 'PAWN', 'WHITE');
 
-insert into status(game_id, position, piece) values ('1', 'a8', 'BLACK_ROOK');
-insert into status(game_id, position, piece) values ('1', 'b8', 'BLACK_BISHOP');
-insert into status(game_id, position, piece) values ('1', 'c8', 'BLACK_KNIGHT');
-insert into status(game_id, position, piece) values ('1', 'd8', 'BLACK_KING');
-insert into status(game_id, position, piece) values ('1', 'e8', 'BLACK_QUEEN');
-insert into status(game_id, position, piece) values ('1', 'f8', 'BLACK_KNIGHT');
-insert into status(game_id, position, piece) values ('1', 'g8', 'BLACK_BISHOP');
-insert into status(game_id, position, piece) values ('1', 'h8', 'BLACK_ROOK');
+insert into board(game_id, position, piece, color) values ('1', 'a8', 'ROOK', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'b8', 'BISHOP', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'c8', 'KNIGHT', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'd8', 'KING', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'e8', 'QUEEN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'f8', 'KNIGHT', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'g8', 'BISHOP', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'h8', 'ROOK', 'BLACK');
 
-insert into status(game_id, position, piece) values ('1', 'a7', 'BLACK_PAWN');
-insert into status(game_id, position, piece) values ('1', 'b7', 'BLACK_PAWN');
-insert into status(game_id, position, piece) values ('1', 'c7', 'BLACK_PAWN');
-insert into status(game_id, position, piece) values ('1', 'd7', 'BLACK_PAWN');
-insert into status(game_id, position, piece) values ('1', 'e7', 'BLACK_PAWN');
-insert into status(game_id, position, piece) values ('1', 'f7', 'BLACK_PAWN');
-insert into status(game_id, position, piece) values ('1', 'g7', 'BLACK_PAWN');
-insert into status(game_id, position, piece) values ('1', 'h7', 'BLACK_PAWN');
+insert into board(game_id, position, piece, color) values ('1', 'a7', 'PAWN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'b7', 'PAWN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'c7', 'PAWN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'd7', 'PAWN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'e7', 'PAWN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'f7', 'PAWN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'g7', 'PAWN', 'BLACK');
+insert into board(game_id, position, piece, color) values ('1', 'h7', 'PAWN', 'BLACK');
