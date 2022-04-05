d8fcc72d70913c452566a2f441211d78f52b9211
tonic523 <diddmlvkf@gmail.com>
2022-04-05 23:19:05 +0900

    test(Piece): Piece 테스트 구현

--- src/test/java/chess/domain/piece/PieceTest.java
+++ src/test/java/chess/domain/piece/PieceTest.java
@@ -1,29 +1,80 @@
 package chess.domain.piece;
 
+import static chess.domain.piece.vo.TeamColor.WHITE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.board.Position;
+import chess.domain.piece.vo.TeamColor;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class PieceTest {
-// To Do: Piece 테스트 구현
-//    @ParameterizedTest
-//    @DisplayName("Rank, File을 받아 자신의 위치와 동일한지 반환한다.")
-//    @CsvSource({"A, ONE, true", "A, TWO, false"})
-//    void hasPosition(final File file, final Rank rank, final boolean expected) {
-//        // given
-//        final Piece piece = Piece.create(A, ONE);
-//        // when
-//        final boolean actual = piece.hasPosition(Position.of(file, rank));
-//        // then
-//        assertThat(actual).isEqualTo(expected);
-//    }
-//
-//    @ParameterizedTest
-//    @DisplayName("다른 기물을 받아 자신과 같은 팀인지 반환한다.")
-//    @CsvSource({"H, ONE, true", "H, EIGHT, false"})
-//    void isSameTeam(final File anotherFile, final Rank anotherRank, final boolean expected) {
-//        // given
-//        final Piece piece = Piece.create(A, ONE);
-//        final Piece anotherPiece = Piece.create(anotherFile, anotherRank);
-//        // when
-//        final boolean actual = piece.isSameTeam(anotherPiece);
-//        // then
-//        assertThat(actual).isEqualTo(expected);
-//    }
+
+    private Piece piece;
+
+    @BeforeEach
+    void setUp() {
+        piece = new Piece(WHITE, Position.from("a1")) {
+            @Override
+            public Piece move(List<Piece> otherPieces, Position targetPosition) {
+                return null;
+            }
+
+            @Override
+            public double getScore() {
+                return 0;
+            }
+        };
+    }
+
+    @ParameterizedTest
+    @DisplayName("기물이 BLACK 팀인지 반환")
+    @CsvSource({"BLACK, true", "WHITE, false"})
+    void isBlackTeam(TeamColor teamColor, boolean expected) {
+        // given
+        Piece piece = new Piece(teamColor, Position.from("a1")) {
+            @Override
+            public Piece move(List<Piece> otherPieces, Position targetPosition) {
+                return null;
+            }
+
+            @Override
+            public double getScore() {
+                return 0;
+            }
+        };
+        //when, then
+        assertThat(piece.isBlackTeam()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("기물이 주어진 위치에 있는지 반환")
+    void hasPosition() {
+        assertThat(piece.hasPosition(Position.from("a1"))).isTrue();
+    }
+
+    @Test
+    @DisplayName("기물과 같은 팀인지 반환")
+    void isSameTeam() {
+        //given
+        Piece otherPiece = new Piece(WHITE, Position.from("a2")) {
+            @Override
+            public Piece move(List<Piece> otherPieces, Position targetPosition) {
+                return null;
+            }
+
+            @Override
+            public double getScore() {
+                return 0;
+            }
+        };
+        // when
+        boolean actual = piece.isSameTeam(otherPiece);
+        // then
+        assertThat(actual).isTrue();
+    }
 }
