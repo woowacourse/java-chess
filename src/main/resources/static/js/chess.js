07597601a8fd85d236230401dfc383f9f666f4c1
Hongdonggeon <ghd700@naver.com>
2022-04-05 23:33:01 +0900

    refactor : 렌더링이 계속되서 hidden이 계속되는 현상 발생, js 파일 미적용

--- src/main/resources/static/js/chess.js
+++ src/main/resources/static/js/chess.js
@@ -1,12 +1,12 @@
-const score = document.querySelector(".score");
-const button = document.querySelector("#status")
-
-const clickHandler = () => {
-    deleteHidden();
-}
-
-const deleteHidden = () => {
-    score.classList.remove("hidden");
-}
-
-button.addEventListener("click", clickHandler);
+// const score = document.querySelector(".score");
+// const button = document.querySelector("#status")
+//
+// const clickHandler = () => {
+//     deleteHidden();
+// }
+//
+// const deleteHidden = () => {
+//     score.classList.remove("hidden");
+// }
+//
+// button.addEventListener("click", clickHandler);
