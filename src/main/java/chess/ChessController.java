4938bd23808701ec203a8e6279ddbb6cfa856867
BeomsooKim <kbsat@naver.com>
2022-04-07 01:09:42 +0900

    refactor: Gson을 정적필드로 사용

--- src/main/java/chess/ChessController.java
+++ src/main/java/chess/ChessController.java
@@ -17,47 +17,49 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 public class ChessController {
 
     public static final int STATUS_BAD_REQUEST = 400;
+    public static final int FIXED_ROOM_ID = 1;
+
     private final ChessService chessService;
+    private final Gson gson;
 
     public ChessController(ChessService chessService) {
         this.chessService = chessService;
+        this.gson = new Gson();
     }
 
     public void run() {
-        Gson gson = new Gson();
-
         get("/", (req, res) -> {
             return render(new HashMap<>(), "index.html");
         });
 
         get("/start", (req, res) -> {
-            return gson.toJson(chessService.startNewGame(1));
+            return gson.toJson(chessService.startNewGame(FIXED_ROOM_ID));
         });
 
         get("/load", (req, res) -> {
-            return gson.toJson(chessService.load(1));
+            return gson.toJson(chessService.load(FIXED_ROOM_ID));
         });
 
         post("/move", (req, res) -> {
             MoveDto moveDto = gson.fromJson(req.body(), MoveDto.class);
-            return gson.toJson(chessService.move(1, moveDto));
+            return gson.toJson(chessService.move(FIXED_ROOM_ID, moveDto));
         });
 
         get("/status", (req, res) -> {
-            return gson.toJson(chessService.status(1));
+            return gson.toJson(chessService.status(FIXED_ROOM_ID));
         });
 
         exception(IllegalStateException.class, (e, req, res) -> {
-            handleError(res, gson, e.getMessage());
+            handleError(res, e.getMessage());
         });
 
         exception(IllegalArgumentException.class, (e, req, res) -> {
-            handleError(res, gson, e.getMessage());
+            handleError(res, e.getMessage());
         });
     }
 
-    private void handleError(Response res, Gson gson, String exceptionMessage) {
-        res.status(400);
+    private void handleError(Response res, String exceptionMessage) {
+        res.status(STATUS_BAD_REQUEST);
         res.body(gson.toJson(new ExceptionResponseDto(exceptionMessage)));
     }
 
