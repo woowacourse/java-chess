981ba915407dd84254e052fba338ba274baab4f4
chawani <chawani@naver.com>
2022-04-06 17:21:17 +0900

    feat: Piece를 생성하는 PieceFactory 구현

--- src/main/java/chess/domain/piece/Color.java
+++ src/main/java/chess/domain/piece/Color.java
@@ -1,5 +1,17 @@
 package chess.domain.piece;
 
+import java.util.Arrays;
+
 public enum Color {
     BLACK, WHITE, NONE;
+
+    private static final String NOT_EXIST_COLOR = "[ERROR] 없는 색 입니다.";
+
+    public static Color get(String colorName) {
+        Color color = Arrays.stream(values())
+            .filter(value -> value.name().equals(colorName))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_COLOR));
+        return color;
+    }
 }
---
+++ src/main/java/chess/domain/piece/PieceFactory.java
@@ -0,0 +1,37 @@
+package chess.domain.piece;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+public enum PieceFactory {
+
+    BISHOP("Bishop", Bishop::new),
+    KING("King", King::new),
+    KNIGHT("Knight", Knight::new),
+    PAWN("Pawn", Pawn::new),
+    QUEEN("Queen", Queen::new),
+    ROOK("Rook", Rook::new);
+
+    private static final String NOT_EXIST_PIECE_NAME = "[ERROR] 잘못된 말 이름입니다.";
+
+    private String name;
+    private Function<Color, Piece> function;
+
+    PieceFactory(String name, Function<Color, Piece> function) {
+        this.name = name;
+        this.function = function;
+    }
+
+    public static Piece create(String name, String color) {
+        PieceFactory pieceFactory = get(name);
+        return pieceFactory.function.apply(Color.get(color));
+    }
+
+    private static PieceFactory get(String name) {
+        PieceFactory pieceFactory = Arrays.stream(values())
+            .filter(piece -> piece.name.equals(name))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_PIECE_NAME));
+        return pieceFactory;
+    }
+}
