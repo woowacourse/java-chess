6906733a66f2578adfcb2030e643372ef84306cd
sinb57 <sinb57@naver.com>
2022-04-06 22:16:34 +0900

    feat: GameService 생성

---
+++ src/main/java/application/web/service/GameService.java
@@ -0,0 +1,53 @@
+package application.web.service;
+
+import java.util.Map;
+
+import chess.domain.Color;
+import chess.domain.Position;
+import chess.domain.game.ChessGame;
+import chess.domain.game.repository.GameRepository;
+
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public GameService(final GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public Map<Long, Boolean> listGames() {
+        return gameRepository.findIdAndFinished();
+    }
+
+    public ChessGame createNewGame() {
+        final ChessGame chessGame = ChessGame.initializeChessGame();
+        return gameRepository.save(chessGame);
+    }
+
+    public ChessGame loadGame(final Long gameId) {
+        return gameRepository.findById(gameId);
+    }
+
+    public ChessGame movePiece(final Long gameId, final String source, final String target) {
+        final ChessGame chessGame = gameRepository.findById(gameId);
+        chessGame.movePiece(Position.from(source), Position.from(target));
+        return gameRepository.update(chessGame);
+    }
+
+    public ChessGame promotion(final Long gameId, final String pieceName) {
+        final ChessGame chessGame = gameRepository.findById(gameId);
+        chessGame.promotePiece(pieceName);
+        return gameRepository.update(chessGame);
+    }
+
+    public Map<Color, Double> calculatePlayerScores(final Long gameId) {
+        final ChessGame chessGame = gameRepository.findById(gameId);
+        return chessGame.getPlayerScores();
+    }
+
+    public ChessGame endGame(final Long gameId) {
+        final ChessGame chessGame = gameRepository.findById(gameId);
+        chessGame.end();
+        return gameRepository.update(chessGame);
+    }
+}
