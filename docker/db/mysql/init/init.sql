2d2f844d4c525d8baedbbb8377280dc1692f1e72
sinb57 <sinb57@naver.com>
2022-04-06 16:33:16 +0900

    chore: sql ddl 수정

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -5,33 +5,13 @@ create table Player (
     primary key (id)
 );
 
-create table GameState (
-    id integer not null auto_increment,
-    state varchar(20) not null,
-    primary key (id)
-);
-
 create table Game (
     id integer not null auto_increment,
-    state_id integer not null,
+    player_id1 integer not null,
+    player_id2 integer not null,
+    finished boolean not null,
+    turn_color varchar(5) not null,
     primary key (id),
-    foreign key (state_id) references GameState (id)
-);
-
-create table GameMatch (
-    id integer not null auto_increment,
-    game_id integer not null,
-    player_id integer not null,
-    primary key (id),
-    foreign key (game_id) references Game (id),
-    foreign key (player_id) references Player (id)
-);
-
-create table GameTurn (
-    id integer not null auto_increment,
-    match_id integer not null,
-    current_player_id integer not null,
-    primary key (id),
-    foreign key (match_id) references GameMatch (id),
-    foreign key (current_player_id) references GameMatch (player_id)
+    foreign key (player_id1) references Player (id),
+    foreign key (player_id2) references Player (id)
 );
