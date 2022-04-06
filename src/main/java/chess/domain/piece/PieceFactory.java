f8b79fc86f9296c86d14ac1565e316a75c03f97b
injoon2019 <ison@sfu.ca>
2022-04-07 00:22:23 +0900

    feat: PieceFactory 구현

--- src/main/java/chess/domain/piece/PieceFactory.java
+++ src/main/java/chess/domain/piece/PieceFactory.java
@@ -1,28 +1,25 @@
 package chess.domain.piece;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PieceFactory {
+    public static final Map<String, AbstractPiece> pieceFactory = new HashMap<>();
 
-    public static Pawn createPawn(PieceColor pieceColor) {
-        return new Pawn(pieceColor);
+    static {
+        pieceFactory.put("BLACKPAWN", new Pawn(PieceColor.BLACK));
+        pieceFactory.put("WHITEPAWN", new Pawn(PieceColor.WHITE));
+        pieceFactory.put("BLACKROOK", new Rook(PieceColor.BLACK));
+        pieceFactory.put("WHITEROOK", new Rook(PieceColor.WHITE));
+        pieceFactory.put("BLACKKNIGHT", new Knight(PieceColor.BLACK));
+        pieceFactory.put("WHITEKNIGHT", new Knight(PieceColor.WHITE));
+        pieceFactory.put("BLACKKING", new King(PieceColor.BLACK));
+        pieceFactory.put("WHITEKING", new King(PieceColor.WHITE));
+        pieceFactory.put("BLACKQUEEN", new Queen(PieceColor.BLACK));
+        pieceFactory.put("WHITEQUEEN", new Queen(PieceColor.WHITE));
     }
 
-    public static Rook createRook(PieceColor pieceColor) {
-        return new Rook(pieceColor);
-    }
-
-    public static Knight createNight(PieceColor pieceColor) {
-        return new Knight(pieceColor);
-    }
-
-    public static Bishop createBishop(PieceColor pieceColor) {
-        return new Bishop(pieceColor);
-    }
-
-    public static Queen createQueen(PieceColor pieceColor) {
-        return new Queen(pieceColor);
-    }
-
-    public static King createKing(PieceColor pieceColor) {
-        return new King(pieceColor);
+    public static AbstractPiece getPiece(String pieceName) {
+        return pieceFactory.get(pieceName);
     }
 }
