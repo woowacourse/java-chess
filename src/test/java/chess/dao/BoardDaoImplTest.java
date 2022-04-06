f52cc76132a5fda73fe4ed9d0634b4e9a3896c10
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-04-06 13:35:54 +0900

    feat: BoardDaoImpl save 기능 추가

--- src/test/java/chess/dao/BoardDaoImplTest.java
+++ src/test/java/chess/dao/BoardDaoImplTest.java
@@ -1,23 +1,45 @@
 package chess.dao;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.sql.Connection;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import chess.EmblemMapper;
+import chess.model.Board;
+import chess.model.PieceArrangement.DefaultArrangement;
+
 public class BoardDaoImplTest {
+
+    private static BoardDao boardDao;
+
+    @BeforeEach
+    void setUp() {
+        boardDao = new BoardDaoImpl();
+    }
 
     @Test
     @DisplayName("Connection 동작을 검증한다.")
     void connection() {
-        //given
-        BoardDao boardDaoImpl = new BoardDaoImpl();
 
         //when
-        Connection connection = boardDaoImpl.getConnection();
+        Connection connection = boardDao.getConnection();
 
         //then
-        Assertions.assertThat(connection).isNotNull();
+        assertThat(connection).isNotNull();
     }
+
+    @Test
+    @DisplayName("DB에 현재 기물 위치, 피스 정보를 저장한다.")
+    void save() {
+        Board board = new Board(new DefaultArrangement());
+        assertThatCode(() -> boardDao.save(1,
+            EmblemMapper.StringPieceMapByPiecesByPositions(board.getValues())))
+            .doesNotThrowAnyException();
+    }
+
 }
