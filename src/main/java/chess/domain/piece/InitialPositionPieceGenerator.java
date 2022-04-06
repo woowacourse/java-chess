71313c0e19c9460c974e9d39b2a628539e8a87f2
pup-paw <wldusdl0310@gmail.com>
2022-04-07 03:51:36 +0900

    refactor: PieceGenerator 클래스명 수정, type 과 color 에 맞는 piece 생성 기능 추가

---
+++ src/main/java/chess/domain/piece/InitialPositionPieceGenerator.java
@@ -0,0 +1,51 @@
+package chess.domain.piece;
+
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
+import chess.domain.position.Column;
+import chess.domain.position.Row;
+
+public enum InitialPositionPieceGenerator {
+    KING((column, row) -> new InitialPiecePositionChecker(column, row).isKing(), row -> new King(getColor(row))),
+    QUEEN((column, row) -> new InitialPiecePositionChecker(column, row).isQueen(), row -> new Queen(getColor(row))),
+    ROOK((column, row) -> new InitialPiecePositionChecker(column, row).isRook(), row -> new Rook(getColor(row))),
+    BISHOP((column, row) -> new InitialPiecePositionChecker(column, row).isBishop(), row -> new Bishop(getColor(row))),
+    KNIGHT((column, row) -> new InitialPiecePositionChecker(column, row).isKnight(), row -> new Knight(getColor(row))),
+    PAWN((column, row) -> new InitialPiecePositionChecker(column, row).isPawn(), row -> new Pawn(getColor(row))),
+    NONE((column, row) -> new InitialPiecePositionChecker(column, row).isNone(), row -> new None(getColor(row)));
+
+    private final BiPredicate<Column, Row> condition;
+    private final Function<Row, Piece> of;
+
+    InitialPositionPieceGenerator(BiPredicate<Column, Row> condition, Function<Row, Piece> of) {
+        this.condition = condition;
+        this.of = of;
+    }
+
+    public static Piece generatePiece(Column column, Row row) {
+        return Arrays.stream(InitialPositionPieceGenerator.values())
+                .filter(piece -> piece.condition.test(column, row))
+                .map(piece -> piece.of.apply(row))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public static InitialPositionPieceGenerator getType(Column column, Row row) {
+        return Arrays.stream(InitialPositionPieceGenerator.values())
+                .filter(piece -> piece.condition.test(column, row))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public static Color getColor(Row row) {
+        if (row == Row.SEVEN || row == Row.EIGHT) {
+            return Color.BLACK;
+        }
+        if (row == Row.ONE || row == Row.TWO) {
+            return Color.WHITE;
+        }
+        return Color.NONE;
+    }
+}
