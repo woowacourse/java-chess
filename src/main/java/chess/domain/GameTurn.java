455a807f0e1acd457da9461167333fb8db9dd415
pup-paw <wldusdl0310@gmail.com>
2022-04-07 03:52:06 +0900

    feat: String 을 입력받아 해당 turn 을 반환

--- src/main/java/chess/domain/GameTurn.java
+++ src/main/java/chess/domain/GameTurn.java
@@ -1,5 +1,7 @@
 package chess.domain;
 
+import java.util.Arrays;
+
 import chess.domain.piece.Color;
 
 public enum GameTurn {
@@ -12,6 +14,13 @@ public enum GameTurn {
 
     GameTurn(Color color) {
         this.color = color;
+    }
+
+    public static GameTurn find(String name) {
+        return Arrays.stream(values())
+                .filter(gameTurn -> gameTurn.name().equals(name))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     public GameTurn switchColor() {
