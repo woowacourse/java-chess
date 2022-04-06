f8cb45188d29cb5d8715a521d1d67794735c40e4
jurlring <78091011+jurlring@users.noreply.github.com>
2022-04-07 03:59:11 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -5,9 +5,6 @@ import static spark.Spark.staticFiles;
 
 public class WebApplication {
 
-    ChessController chessController = new ChessController();
-
-
     public static void main(String[] args) {
         staticFiles.location("/static");
         port(8080);
