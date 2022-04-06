88201e5c8f1739aecf092cc765582a560b08dccc
skullKim <kyk990328@gmail.com>
2022-04-06 18:26:24 +0900

    fix(init.sql): 필요없는 컬럼 삭제
    디비 테이블을 생성할 때 필요없는 컬럼을 생성하지 않기 위해 해당 쿼리
    삭제.
    
    BREAKING CHANGE: piece 테이블 생성 쿼리문에서 score 컬럼 생성 부분 삭제

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -4,26 +4,25 @@ CREATE DATABASE IF NOT EXISTS chess;
 CREATE TABLE piece (
    id INT NOT NULL AUTO_INCREMENT,
    symbol VARCHAR(1) NOT NULL,
-   score DOUBLE NOT NULL,
    color VARCHAR(10) NOT NULL,
    image_url VARCHAR(100) NOT NULL,
    PRIMARY KEY (id));
 
-INSERT INTO piece(symbol, score, color, image_url) VALUES('q', 9, 'BLACK', '/images/pieces/black/black-queen.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('r', 5, 'BLACK', '/images/pieces/black/black-rook.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('b', 3, 'BLACK', '/images/pieces/black/black-bishop.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('n', 2.5, 'BLACK', '/images/pieces/black-knight.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('p', 1, 'BLACK', '/images/pieces/black/black-pawn.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('k', 0, 'BLACK', '/images/pieces/black/black-king.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('q', 'BLACK', '/images/pieces/black/black-queen.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('r', 'BLACK', '/images/pieces/black/black-rook.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('b', 'BLACK', '/images/pieces/black/black-bishop.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('n', 'BLACK', '/images/pieces/black-knight.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('p', 'BLACK', '/images/pieces/black/black-pawn.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('k', 'BLACK', '/images/pieces/black/black-king.svg');
 
-INSERT INTO piece(symbol, score, color, image_url) VALUES('q', 9, 'WHITE', '/images/pieces/white/white-queen.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('r', 5, 'WHITE', '/images/pieces/white/white-rook.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('b', 3, 'WHITE', '/images/pieces/white/white-bishop.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('n', 2.5, 'WHITE', '/images/pieces/white/white-knight.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('p', 1, 'WHITE', '/images/pieces/white/white-pawn.svg');
-INSERT INTO piece(symbol, score, color, image_url) VALUES('k', 0, 'WHITE', '/images/pieces/white/white-king.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('q', 'WHITE', '/images/pieces/white/white-queen.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('r', 'WHITE', '/images/pieces/white/white-rook.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('b', 'WHITE', '/images/pieces/white/white-bishop.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('n', 'WHITE', '/images/pieces/white/white-knight.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('p', 'WHITE', '/images/pieces/white/white-pawn.svg');
+INSERT INTO piece(symbol, color, image_url) VALUES('k', 'WHITE', '/images/pieces/white/white-king.svg');
 
-INSERT INTO piece(symbol, score, color, image_url) VALUE('.', 0, 'NONE', '');
+INSERT INTO piece(symbol, color, image_url) VALUE('.', 'NONE', '');
 
 -- board
 CREATE TABLE board(
