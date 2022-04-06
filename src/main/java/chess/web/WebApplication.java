ae73665e1beb1b1e180e033e795cfc8396e14a44
summerlunaa <summerlunaa9@gmail.com>
2022-04-06 14:25:51 +0900

    feat: 기물을 움직이는 기능 구현

--- src/main/java/chess/web/WebApplication.java
+++ src/main/java/chess/web/WebApplication.java
@@ -1,29 +1,39 @@
 package chess.web;
 
-import chess.domain.ChessGame;
-import chess.domain.piece.Piece;
-import chess.web.dto.BoardDto;
-import java.util.List;
-import java.util.stream.Collectors;
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.exception;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
 
+import chess.domain.ChessGame;
+import chess.web.dto.BoardDto;
 import java.util.HashMap;
 import java.util.Map;
-
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebApplication {
     public static void main(String[] args) {
         staticFileLocation("/static");
 
         ChessGame chessGame = new ChessGame();
-        Map<String, Object> model = new HashMap<>();
+        chessGame.start();
 
         get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
             model.put("pieces", BoardDto.newInstance(chessGame.board()).getPieces());
             return render(model, "index.html");
+        });
+
+        get("/move", (req, res) -> {
+            chessGame.move(req.queryParams("source"), req.queryParams("target"));
+            res.redirect("/");
+            return null;
+        });
+
+        exception(Exception.class, (exception, request, response) -> {
+            response.status(400);
+            response.body(exception.getMessage());
         });
     }
 
