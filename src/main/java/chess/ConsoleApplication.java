19163152667e8612321f63007d89df066c714871
progress0407 <progress0407@gmail.com>
2022-04-06 23:42:36 +0900

    refactor(ChessController): DI 주입 방식으로 변경

--- src/main/java/chess/ConsoleApplication.java
+++ src/main/java/chess/ConsoleApplication.java
@@ -1,11 +1,20 @@
 package chess;
 
 import chess.controller.ChessController;
+import chess.domain.board.ChessBoard;
+import chess.domain.board.factory.BoardFactory;
+import chess.domain.board.factory.RegularBoardFactory;
+import chess.turndecider.AlternatingGameFlow;
+import chess.turndecider.GameFlow;
 
 public class ConsoleApplication {
 
     public static void main(String[] args) {
-        ChessController chessController = new ChessController();
+        BoardFactory boardFactory = RegularBoardFactory.getInstance();
+        GameFlow gameFlow = new AlternatingGameFlow();
+        ChessBoard chessBoard = new ChessBoard(boardFactory.create(), gameFlow);
+
+        ChessController chessController = new ChessController(chessBoard);
         chessController.run();
     }
 }
