b4a0db60a80449cdb3e9dc61361384506852cd18
beomWhale <cndqjacndqja@gmail.com>
2022-04-07 04:49:34 +0900

    refactor: DBConnection getConnection() 수정

--- src/main/java/chess/database/DBConnection.java
+++ src/main/java/chess/database/DBConnection.java
@@ -7,25 +7,24 @@ public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/chess";
     private static final String USER = "user";
     private static final String PASSWORD = "password";
+    private static Connection CONNECTION;
 
     public static Connection getConnection() {
         loadDriver();
-        Connection connection = null;
-        try{
-             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            return connection;
-        }catch (Exception exception) {
+        try {
+            CONNECTION = DriverManager.getConnection(URL, USER, PASSWORD);
+            return CONNECTION;
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
-        return connection;
+        return CONNECTION;
     }
 
 
     private static void loadDriver() {
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (Exception exception) {
+        } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
     }
