6be1738346323410481ca2fb890ce84a469d1198
woong7 <driedseas@gmail.com>
2022-04-07 03:57:46 +0900

    refactor: 패키지 구조 정리

---
+++ src/main/java/chess/console/Application.java
@@ -0,0 +1,8 @@
+package chess.console;
+
+public class Application {
+    public static void main(String[] args) {
+        ChessController chessController = new ChessController();
+        chessController.run();
+    }
+}
