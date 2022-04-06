2a9ff7d4a71fb860f1d2b3d0ad31c8d1d20f4956
soominsohn <soominsohn@gmail.com>
2022-04-07 00:12:29 +0900

    feat(DatabaseConnector): implement DatabaseConnector

---
+++ src/main/java/chess/dao/util/DatabaseConnector.java
@@ -0,0 +1,53 @@
+package chess.dao.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/chess";
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
+
+    public Connection getConnection() {
+        loadDriver();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private void loadDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close(PreparedStatement statement, Connection connection) {
+        try {
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close(PreparedStatement statement, ResultSet resultSet, Connection connection) {
+        try {
+            statement.close();
+            resultSet.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
