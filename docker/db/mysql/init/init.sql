8103bb3b090e567c18be26d3c82cd61220e68b93
beomWhale <cndqjacndqja@gmail.com>
2022-04-06 20:03:01 +0900

    feat: init.sql 파일 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,7 +1,19 @@
-CREATE TABLE `piece` (
-                         `id` int unsigned NOT NULL AUTO_INCREMENT,
-                         `position` varchar(3),
-                         `symbol` varchar(3),
-                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+CREATE TABLE room
+(
+    id     int        NOT NULL,
+    status varchar(5) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY `room_id_UNIQUE` (`id`)
+);
+
+CREATE TABLE board
+(
+    id       int        NOT NULL AUTO_INCREMENT,
+    position varchar(5) NOT NULL,
+    symbol   varchar가(5) NOT NULL,
+    room_id  int        NOT NULL,
+    PRIMARY KEY (id, room_id),
+    KEY      room_id (room_id),
+    CONSTRAINT id FOREIGN KEY (room_id) REFERENCES room (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
--- src/main/java/WebApplication.java
+++ src/main/java/WebApplication.java
@@ -4,18 +4,8 @@ import org.apache.log4j.BasicConfigurator;
 import static spark.Spark.*;
 
 public class WebApplication {
-    public static String STATUS = "dev";
-
     public static void main(String[] args) {
         port(8080);
-
-        if (STATUS.equals("dev")) {
-            String projectDirectory = System.getProperty("user.dir");
-            String staticDirectory = "/src/main/resources/templates";
-            externalStaticFileLocation(projectDirectory + staticDirectory);
-        } else {
-            staticFileLocation("/templates");
-        }
         staticFileLocation("templates");
         BasicConfigurator.configure();
         ChessController chessController = new ChessController();
