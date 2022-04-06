7b87dfd98c5f89b646da164a1cab094064fb78ef
jurlring <78091011+jurlring@users.noreply.github.com>
2022-04-07 03:48:20 +0900

    feat : name으로 color 찾는 로직 구현

--- src/main/java/chess/domain/piece/Color.java
+++ src/main/java/chess/domain/piece/Color.java
@@ -1,5 +1,7 @@
 package chess.domain.piece;
 
+import java.util.Arrays;
+
 public enum Color {
 
     BLACK("black"),
@@ -11,6 +13,13 @@ public enum Color {
 
     Color(String name) {
         this.name = name;
+    }
+
+    public static Color from(final String name) {
+        return Arrays.stream(Color.values())
+                .filter(color -> color.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 Color 입니다."));
     }
 
     public Color getOpposite() {
