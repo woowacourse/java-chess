e743b3fc16b45671a88f638e2310d5c7fe77a08d
YuJeong <yjfflq4872@gmail.com>
2022-04-07 01:14:06 +0900

    feat: Piece 팩토리 클래스 구현

---
+++ src/test/java/chess/dao/PieceFactoryTest.java
@@ -0,0 +1,16 @@
+package chess.dao;
+
+import chess.domain.Team;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Piece;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PieceFactoryTest {
+
+    @Test
+    void createPieceTest() {
+        Piece piece = PieceFactory.of("Knight", "b", 1, Team.WHITE.name());
+        Assertions.assertThat(piece).isInstanceOf(Knight.class);
+    }
+}
