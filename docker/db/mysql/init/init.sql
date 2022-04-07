e4cbd6b6194f1fb1df38ca8e1ee9893c446475be
SuyeonChoi <tndus970120@gmail.com>
2022-04-07 17:41:51 +0900

    chore: sql 스크립트 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -11,3 +11,43 @@ create table role(
     primary key (user_id),
     foreign key (user_id) references member (id)
 );
+
+create table chessgame(
+    id int not null auto_increment,
+    state varchar(9) not null,
+    primary key (id)
+);
+
+create table chesspiece(
+    id int not null auto_increment,
+    name varchar(6) not null,
+    team varchar(5) not null,
+    primary key (id)
+);
+
+create table chessboard(
+    id int not null auto_increment,
+    board_row int not null,
+    board_column varchar(1) not null,
+    chesspiece_id int not null,
+    chessgame_id int not null,
+    primary key (id),
+    foreign key (chesspiece_id) references chesspiece (id),
+    foreign key (chessgame_id) references chessgame (id)
+);
+
+insert into chesspiece (name, team)
+values
+    ("pawn", "black"),
+    ("pawn", "white"),
+    ("rook", "black"),
+    ("rook", "white"),
+    ("bishop", "black"),
+    ("bishop", "white"),
+    ("knight", "black"),
+    ("knight", "white"),
+    ("queen", "black"),
+    ("queen", "white"),
+    ("king", "black"),
+    ("king", "white")
+    ;
