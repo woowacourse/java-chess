495870a5620e90d4de450af9ee981b4e4fda09e9
Wishoon <heonga2@gmail.com>
2022-04-07 12:37:26 +0900

    feat: 체스판을 요청하고 그리는 기능 구현

---
+++ src/main/resources/static/src/index.js
@@ -0,0 +1,152 @@
+let source = "";
+let target = "";
+
+window.onload = function () {
+    fetch("/load")
+        .then(response => response.json())
+        .then(data => {
+            initSquarePosition(data.board);
+            loadData(data);
+        });
+}
+
+async function requestStart() {
+    await fetch("/start")
+        .then(response => response.json())
+        .then(data => {
+            if (data.code === "success") {
+                initSquarePosition(data.board);
+                loadData(data);
+            } else {
+                alert(data.message);
+            }
+        });
+}
+
+async function requestEnd() {
+    await fetch("/end")
+        .then(response => response.json())
+        .then(data => {
+            if (data.code === "success") {
+                loadData(data);
+            } else {
+                alert(data.message);
+            }
+        });
+}
+
+async function sendStatus() {
+    await fetch("/status")
+        .then(response => response.json())
+        .then(data => {
+            if (data.code === "success") {
+                if (data.result == "NONE") {
+                    alert("현재 동점입니다.");
+                }
+                if (data.result != "NONE") {
+                    alert("WHITE 팀 : " + data.whiteScore + " BLACK 팀 : " + data.blackScore
+                        + "으로 현재 " + data.result + "팀이 유리합니다.");
+                }
+            } else {
+                alert(data.message);
+            }
+        });
+}
+
+async function move(event) {
+    const id = event.currentTarget.id;
+    const divClassName = document.getElementById(id).classList;
+    if (source === "") {
+        source = id;
+        divClassName.add('selected');
+        return;
+    }
+    if (source !== "" && target === "") {
+        target = id;
+        divClassName.add('selected');
+        await requestMove();
+    }
+}
+
+async function requestMove() {
+    await fetch("/move", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            source: source,
+            target: target
+        })})
+        .then(response => response.json())
+        .then(data => {
+            if (data.code === "success") {
+                loadData(data);
+            } else {
+                alert(data.message);
+            }
+            document.getElementById(source).classList.remove('selected');
+            document.getElementById(target).classList.remove('selected');
+        });
+
+    source = "";
+    target = "";
+}
+
+function initSquarePosition(board) {
+    const section = document.getElementsByClassName('chessBox')[0];
+    for (key in board) {
+        addPosition(section, key);
+    }
+}
+
+function addPosition(section, key) {
+    const div = document.createElement('div');
+    div.id = key;
+    div.className = 'square';
+    div.addEventListener('click', move);
+    section.appendChild(div);
+}
+
+function loadData(data) {
+    loadBoard(data.board);
+    loadState(data.state, data.turn);
+}
+
+function loadBoard(board) {
+    for (key in board) {
+        const div = document.getElementById(key);
+        resetPiece(div);
+        setPiece(div, board[key]);
+    }
+}
+
+function resetPiece(div) {
+    console.log(div);
+    if (div.hasChildNodes()) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+function setPiece(div, piece) {
+    if (piece !== "._NONE") {
+        const img = document.createElement("img");
+        img.src = "/images/" + piece + ".png";
+        div.appendChild(img);
+    }
+}
+
+
+function loadState(state, turn) {
+    document.getElementsByClassName('state')[0].textContent = state;
+    document.getElementsByClassName('chessBox')[0].style.visibility = 'visible';
+
+    if (state === "ready") {
+        document.getElementsByClassName('chessBox')[0].style.visibility = 'visible';
+        document.getElementsByClassName('state')[0].textContent += " " + turn + " ";
+    }
+    if (state === "finished") {
+        document.getElementsByClassName('chessBox')[0].style.visibility = 'visible';
+        alert("게임이 종료되었습니다.");
+    }
+}
