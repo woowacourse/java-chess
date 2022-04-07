cd93adba99132ea80a4dce2555f8d255a16ff18b
yxxnghwan <younghwan960@gmail.com>
2022-04-07 17:08:13 +0900

    refacor: PieceFactory 불필요한 메서드, 클래스변수 삭제

--- src/main/java/chess/domain/piece/PieceFactory.java
+++ src/main/java/chess/domain/piece/PieceFactory.java
@@ -8,22 +8,6 @@ import java.util.function.Function;
 
 public class PieceFactory {
 
-    private final static Map<String, Function<Position, Piece>> blackPieceSupplier = new HashMap<>() {{
-        put(PieceType.ROOK.getName(), Rook::createBlack);
-        put(PieceType.KNIGHT.getName(), Knight::createBlack);
-        put(PieceType.BISHOP.getName(), Bishop::createBlack);
-        put(PieceType.QUEEN.getName(), Queen::createBlack);
-        put(PieceType.KING.getName(), King::createBlack);
-    }};
-
-    private final static Map<String, Function<Position, Piece>> whitePieceSupplier = new HashMap<>() {{
-        put(PieceType.ROOK.getName(), Rook::createWhite);
-        put(PieceType.KNIGHT.getName(), Knight::createWhite);
-        put(PieceType.BISHOP.getName(), Bishop::createWhite);
-        put(PieceType.QUEEN.getName(), Queen::createWhite);
-        put(PieceType.KING.getName(), King::createWhite);
-    }};
-
     private final static Map<String, BiFunction<Team, Position, Piece>> pieceSupplier = new HashMap<>() {{
         put(PieceType.ROOK.getName(), Rook::create);
         put(PieceType.KNIGHT.getName(), Knight::create);
@@ -39,9 +23,6 @@ public class PieceFactory {
         if (pieceType.equals(PieceType.PAWN.getName())) {
             return findPawn(team, position, isFirstTurn);
         }
-        if (team.equals(Team.BLACK.name())) {
-            return findBlackPiece(pieceType, position);
-        }
         return pieceSupplier.getOrDefault(pieceType, (t, p) -> new Blank(p))
                 .apply(Team.valueOf(team), position);
     }
@@ -51,15 +32,5 @@ public class PieceFactory {
             return Pawn.createBlack(position, isFirstTurn);
         }
         return Pawn.createWhite(position, isFirstTurn);
-    }
-
-    private static Piece findBlackPiece(String pieceType, Position position) {
-        return blackPieceSupplier.getOrDefault(pieceType, Blank::new)
-                .apply(position);
-    }
-
-    private static Piece findWhitePiece(String pieceType, Position position) {
-        return whitePieceSupplier.getOrDefault(pieceType, Blank::new)
-                .apply(position);
     }
 }
