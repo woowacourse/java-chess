ed3f81f4308de5141a2da7e8e8f947a52f8ab195
yh20studio(tiki) <yh20studio@gmail.com>
2022-04-07 17:16:54 +0900

    feat(): 체스 게임 이름에 대한 길이 조건 추가

--- src/main/java/chess/domain/game/Game.java
+++ src/main/java/chess/domain/game/Game.java
@@ -3,6 +3,8 @@ package chess.domain.game;
 public class Game {
 
 	private static final String EMPTY_NAME_ERROR = "게임 이름은 빈칸일 수 없습니다.";
+	private static final String TOO_LONG_NAME_ERROR = "게임 이름은 10글자를 넘어갈 수 없습니다.";
+	private static final int MAX_NAME_LENGTH = 10;
 
 	private int id;
 	private final String name;
@@ -21,8 +23,19 @@ public class Game {
 	}
 
 	private void validateName(final String name) {
+		validateEmpty(name);
+		validateLength(name);
+	}
+
+	private void validateEmpty(final String name) {
 		if (name.isEmpty()) {
 			throw new IllegalArgumentException(EMPTY_NAME_ERROR);
+		}
+	}
+
+	private void validateLength(final String name) {
+		if (name.length() > MAX_NAME_LENGTH) {
+			throw new IllegalArgumentException(TOO_LONG_NAME_ERROR);
 		}
 	}
 
