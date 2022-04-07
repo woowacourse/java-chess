d0edba91995c6f6272035f7a3e461c69c2698c05
injoon2019 <ison@sfu.ca>
2022-04-07 13:27:27 +0900

    feat: GameDao 테스트 코드 작성

--- src/main/java/chess/dao/GameDao.java
+++ src/main/java/chess/dao/GameDao.java
@@ -2,7 +2,6 @@ package chess.dao;
 
 import chess.domain.piece.PieceColor;
 import chess.dto.TurnDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,22 +13,25 @@ public class GameDao {
     private static final String USER = "user";
     private static final String PASSWORD = "password";
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
     public TurnDto getTurn() {
-        final String sql = "SELECT game.turn "
-                + "FROM game WHERE id = '1'";
+        String sql = "SELECT game.turn FROM game WHERE id = '1'";
         String turn = "";
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                turn = resultSet.getString("turn");
-            }
+
+            validateTurnExist(resultSet);
+            turn = resultSet.getString("turn");
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return new TurnDto(turn);
+    }
+
+    private void validateTurnExist(ResultSet resultSet) throws SQLException {
+        if (!resultSet.next()) {
+            throw new IllegalStateException();
+        }
     }
 
     public Connection getConnection() {
@@ -45,12 +47,12 @@ public class GameDao {
     public void changeTurn() {
         String changeTurnSql = "UPDATE game SET turn = ? WHERE id = ?";
 
-        try (   Connection connection = getConnection();
-                PreparedStatement statement = connection.prepareStatement(changeTurnSql)) {
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(changeTurnSql)) {
             TurnDto currentTurn = getTurn();
             statement.setString(1, PieceColor.getOpposite(currentTurn.getTurn()));
             statement.setString(2, "1");
-            int resultSet = statement.executeUpdate();
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
