59e7257aaccd8d4a0633faf94fcee13ecbda1d2c
soominsohn <soominsohn@gmail.com>
2022-04-07 21:21:23 +0900

    refactor(PieceFactory): reimplement PieceFactory
    - use BiFunction

--- src/main/java/chess/domain/piece/PieceFactory.java
+++ src/main/java/chess/domain/piece/PieceFactory.java
@@ -1,29 +1,15 @@
 package chess.domain.piece;
 
 import chess.domain.position.Position;
+import java.util.function.BiFunction;
 
 public class PieceFactory {
 
     public static Piece of(String type, String color, String position) {
-        if (type.equals("pawn")) {
-            return new Pawn(Color.of(color), Position.of(position));
-        }
-        if (type.equals("rook")) {
-            return new Rook(Color.of(color), Position.of(position));
-        }
-        if (type.equals("bishop")) {
-            return new Bishop(Color.of(color), Position.of(position));
-        }
-        if (type.equals("knight")) {
-            return new Knight(Color.of(color), Position.of(position));
-        }
-        if (type.equals("queen")) {
-            return new Queen(Color.of(color), Position.of(position));
-        }
-        if (type.equals("king")) {
-            return new King(Color.of(color), Position.of(position));
-        }
-        throw new IllegalArgumentException("오류입니다.");
+        PieceType pieceType = PieceType.of(type);
+        BiFunction<Color, Position, Piece> biFunction  = pieceType.getConstructor();
+
+        return biFunction.apply(Color.of(color), Position.of(position));
     }
 
 }
