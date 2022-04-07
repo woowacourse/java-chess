18182144fb1cee11f56c982fce81cc4c8546de99
summerlunaa <summerlunaa9@gmail.com>
2022-04-07 13:45:20 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/chess/web/dto/BoardDto.java
+++ src/main/java/chess/web/dto/BoardDto.java
@@ -1,11 +1,6 @@
 package chess.web.dto;
 
-import chess.domain.Board;
-import chess.domain.piece.Piece;
-import chess.domain.position.Position;
 import chess.domain.state.StateType;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BoardDto {
 
@@ -13,21 +8,6 @@ public class BoardDto {
 
     public BoardDto(StateType stateType) {
         this.stateType = stateType;
-    }
-
-    public static List<PieceDto> newInstance(Board board) {
-        List<PieceDto> pieces = new ArrayList<>();
-
-        for (int rankIndex = 0; rankIndex < 8; rankIndex++) {
-            for (int fileIndex = 0; fileIndex < 8; fileIndex++) {
-                Position position = new Position(fileIndex, rankIndex);
-                Piece piece = board.findPiece(position);
-                PieceDto pieceDto = new PieceDto(piece, position);
-                pieces.add(pieceDto);
-            }
-        }
-
-        return pieces;
     }
 
     public StateType getStateType() {
