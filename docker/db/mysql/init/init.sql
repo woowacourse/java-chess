c285c60690a02c3dbd308e51eb0897ea3b8277ae
yangdongjue5510 <djwhy5510@gmail.com>
2022-04-07 16:58:44 +0900

    refactor: sql 코드 포맷팅

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,17 +1,20 @@
-CREATE TABLE game (
-                      name varchar(20) not null,
-                      status varchar(10) not null,
-                      turn varchar(10) not null,
-                      primary key (name)
+CREATE TABLE game
+(
+    name   varchar(20) not null,
+    status varchar(10) not null,
+    turn   varchar(10) not null,
+    primary key (name)
 );
 
-CREATE TABLE board (
-    piece_type varchar(10) not null,
+CREATE TABLE board
+(
+    piece_type  varchar(10) not null,
     piece_color varchar(10) not null,
-    square varchar(10) not null,
-    game_name varchar(20) not null,
+    square      varchar(10) not null,
+    game_name   varchar(20) not null,
     primary key (game_name, square),
     foreign key (game_name) references game (name)
 );
 
-insert into game (name, status, turn) VALUES ('game', 'empty', 'white')
+insert into game (name, status, turn)
+VALUES ('game', 'empty', 'white')
