33956c32e549e9f93c0e4f88286fad31794f71c4
yoon <duddbs774@gmail.com>
2022-04-07 17:42:46 +0900

    feat: 기존 로직에 dao 로직 추가

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -1,29 +1,40 @@
 package chess.domain;
 
+import chess.dao.GameDao;
+import chess.dao.PieceDao;
+import chess.domain.board.Board;
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import chess.domain.piece.PieceFactory;
 import chess.domain.position.Position;
 import chess.domain.state.Ready;
+import chess.domain.state.Running;
 import chess.domain.state.State;
 import chess.domain.state.Status;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public final class ChessGame {
 
     private static final int FROM = 0;
     private static final int TO = 1;
+    private static final int PIECE_NAME_INDEX = 0;
+    private static final int PIECE_COLOR_INDEX = 1;
 
+    private final PieceDao pieceDao = new PieceDao();
+    private final GameDao gameDao = new GameDao();
     private State state = new Ready();
 
     public void start() {
         state = state.start();
-        // initBoard db에 저장하기
+        pieceDao.initBoard(state.getBoard());
+        gameDao.initTurn();
     }
 
     public void end() {
         state = state.end();
-        // db 보드 내역 삭제
     }
 
     public Map<Position, Piece> getBoard() {
@@ -35,7 +46,12 @@ public final class ChessGame {
         final Position to = Position.from(positions[TO]);
 
         state = state.move(from, to);
-        // db에 이동내역 저장하기
+        pieceDao.move(from, to);
+        gameDao.updateTurn(state.getCurrentColor());
+        if (isEnd()) {
+            pieceDao.deleteAllPiece();
+            gameDao.initTurn();
+        }
     }
 
     public Status status() {
@@ -52,5 +68,18 @@ public final class ChessGame {
 
     public boolean isRemovedKing() {
         return state.isRemovedKing();
+    }
+
+    public void restart() {
+        final var boardData = pieceDao.findAllPiece();
+        state = new Running(Color.from(gameDao.findCurrentColor()), convertToBoard(boardData));
+    }
+
+    private Board convertToBoard (final Map<String, List<String>> boardData) {
+        final Map<Position, Piece> board = new HashMap<>();
+        for (final Map.Entry<String, List<String>> entry : boardData.entrySet()) {
+            board.put(Position.from(entry.getKey()), PieceFactory.of(entry.getValue().get(PIECE_NAME_INDEX),entry.getValue().get(PIECE_COLOR_INDEX)));
+        }
+        return new Board(() -> board);
     }
 }
