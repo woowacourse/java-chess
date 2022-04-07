8f62fe94600f5a78b1ba2390a01827bbdf78ec7b
RIANAEH <ririhan217@gmail.com>
2022-04-07 19:42:59 +0900

    feat : 게임 ID를 varchar로 변경하고 데이터 초기화 추가

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,13 +1,12 @@
 CREATE TABLE game (
-    `id` INT NOT NULL AUTO_INCREMENT,
+    `id` varchar(100) NOT NULL,
     `state` VARCHAR(10) NOT NULL,
     `turn` VARCHAR(5),
-    `latest_date` DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE board (
-    `game_id`INT NOT NULL,
+    `game_id` varchar(100) NOT NULL,
     `position` VARCHAR(2) NOT NULL,
     `type` VARCHAR(6) NOT NULL,
     `color` VARCHAR(5) NOT NULL,
@@ -15,3 +14,8 @@ CREATE TABLE board (
     FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
 );
 
+INSERT INTO game VALUES ("First Game", "Started", "white");
+
+INSERT INTO board VALUES ("First Game", "a1", "pawn", "white");
+INSERT INTO board VALUES ("First Game", "a2", "rook", "black");
+INSERT INTO board VALUES ("First Game", "c2", "pawn", "white");
