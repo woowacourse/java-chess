44859cf9c66e5511d0f42cf19a2fe7d9a5c71b80
hyewoncc <hyewoncc@gmail.com>
2022-04-07 11:56:20 +0900

    refactor: db connection 생성용 객체 추가

--- src/main/java/chess/web/dao/BoardDao.java
+++ src/main/java/chess/web/dao/BoardDao.java
@@ -1,76 +1,71 @@
 package chess.web.dao;
 
 import chess.domain.Color;
+import chess.web.DBConnector;
 import chess.web.dto.GameStateDto;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BoardDao {
 
-    private static final String URL = "jdbc:mysql://localhost:3310/chess";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
-
-    public Connection getConnection() {
-        loadDriver();
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
-    private void loadDriver() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (final Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public void save(GameStateDto gameStateDto) {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "insert into board (id, turn) values (?, ?)";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+
+        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, 1);
             statement.setString(2, gameStateDto.getTurn());
             statement.execute();
+            closeResources(statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public Color find() {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "select * from board where id = ?";
+        Color color = Color.WHITE;
+
         try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, 1);
             final ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                return Color.findByValue(resultSet.getString("turn"));
+                color = Color.findByValue(resultSet.getString("turn"));
             }
+            closeResources(resultSet, statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return Color.findByValue("");
+        return color;
     }
 
     public void update(GameStateDto gameStateDto) {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "update board set turn = ? where id = 1";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+
+        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, gameStateDto.getTurn());
             statement.execute();
+            closeResources(statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private void closeResources(Statement statement, Connection connection) throws SQLException {
+        statement.close();
+        connection.close();
+    }
+
+    private void closeResources(ResultSet resultSet, Statement statement, Connection connection)
+            throws SQLException {
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 }
--- src/main/java/chess/web/dao/PieceDao.java
+++ src/main/java/chess/web/dao/PieceDao.java
@@ -2,9 +2,9 @@ package chess.web.dao;
 
 import chess.domain.piece.Piece;
 import chess.domain.position.Position;
+import chess.web.DBConnector;
 import chess.web.dto.PieceDto;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,47 +16,24 @@ import java.util.Optional;
 
 public class PieceDao {
 
-    private static final String URL = "jdbc:mysql://localhost:3310/chess";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
-
-    public Connection getConnection() {
-        loadDriver();
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
-    private void loadDriver() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (final Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public void saveOne(PieceDto pieceDto) {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "insert into piece (position , color, role) values (?, ?, ?)";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, pieceDto.getPosition());
             statement.setString(2, pieceDto.getColor());
             statement.setString(3, pieceDto.getRole());
             statement.execute();
+            closeResources(statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveAll(Map<Position, Piece> pieces) {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "insert into piece (position, color, role) values (?, ?, ?)";
-        try (final PreparedStatement statement = connection.prepareStatement(sql);) {
+        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             for (Position position : pieces.keySet()) {
                 PieceDto piece = PieceDto.from(position, pieces.get(position));
                 statement.setString(1, piece.getPosition());
@@ -66,13 +43,14 @@ public class PieceDao {
                 statement.clearParameters();
             }
             statement.executeBatch();
+            closeResources(statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public Optional<PieceDto> findOne(String position) {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "select * from piece where position = ?";
         Optional<PieceDto> pieceDto = Optional.ofNullable(null);
         try (final PreparedStatement statement = connection.prepareStatement(sql);) {
@@ -80,9 +58,10 @@ public class PieceDao {
             final ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 pieceDto = Optional.of(new PieceDto(resultSet.getString("position"),
-                                resultSet.getString("color"),
-                                resultSet.getString("role")));
+                        resultSet.getString("color"),
+                        resultSet.getString("role")));
             }
+            closeResources(resultSet, statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -90,7 +69,7 @@ public class PieceDao {
     }
 
     public List<PieceDto> findAll() {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "select * from piece";
         List<PieceDto> pieceDtos = new ArrayList<>();
         try (final PreparedStatement statement = connection.prepareStatement(sql);) {
@@ -102,6 +81,7 @@ public class PieceDao {
                         resultSet.getString("role")
                 ));
             }
+            closeResources(resultSet, statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -109,36 +89,51 @@ public class PieceDao {
     }
 
     public void updateOne(String position, PieceDto pieceDto) {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "update piece set color = ?, role = ? where position = ?";
         try (final PreparedStatement statement = connection.prepareStatement(sql);) {
             statement.setString(1, pieceDto.getColor());
             statement.setString(2, pieceDto.getRole());
             statement.setString(3, position);
             statement.execute();
+            closeResources(statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void deleteOne(String position) {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "delete from piece where position = ?";
         try (final PreparedStatement statement = connection.prepareStatement(sql);) {
             statement.setString(1, position);
             statement.execute();
+            closeResources(statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void deleteAll() {
-        final Connection connection = getConnection();
+        final Connection connection = DBConnector.getConnection();
         final String sql = "delete from piece";
         try (final Statement statement = connection.createStatement();) {
             statement.execute(sql);
+            closeResources(statement, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private void closeResources(Statement statement, Connection connection) throws SQLException {
+        statement.close();
+        connection.close();
+    }
+
+    private void closeResources(ResultSet resultSet, Statement statement, Connection connection)
+            throws SQLException {
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 }
