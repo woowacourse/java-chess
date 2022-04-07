fa458ac8ff0282f5f027f2ef76e2b848ff8bf6d5
pup-paw <wldusdl0310@gmail.com>
2022-04-07 17:55:17 +0900

    refactor: List<Piece> 가 아닌 Map<String,String> 을 필드로 갖도록 변경

--- src/main/java/chess/dto/BoardDto.java
+++ src/main/java/chess/dto/BoardDto.java
@@ -1,47 +1,20 @@
 package chess.dto;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.google.common.collect.Lists;
 
 import chess.domain.board.Board;
-import chess.domain.piece.Piece;
-import chess.domain.position.Row;
-import chess.domain.position.Square;
 
 public class BoardDto {
-    private final List<Piece> pieces;
+    private final Map<String, String> board;
 
     public BoardDto(Board board) {
-        this.pieces = splitByRank(board.getBoard());
+        this.board = board.getBoard().entrySet()
+                .stream()
+                .collect(Collectors.toMap(entry -> entry.getKey().getName(), entry -> entry.getValue().getEmoji()));
     }
 
-    private List<Piece> splitByRank(Map<Square, Piece> board) {
-        List<Piece> pieces = new ArrayList<>(board.values());
-        List<List<Piece>> splitPieces = Lists.partition(pieces, Row.values().length);
-        return reverse(splitPieces);
-    }
-
-    private List<Piece> reverse(List<List<Piece>> splitPieces) {
-        List<Piece> pieces = new ArrayList<>();
-        for (List<Piece> splitPiece : splitPieces) {
-            pieces = Stream.concat(splitPiece.stream(), pieces.stream())
-                    .collect(Collectors.toList());
-        }
-        return pieces;
-    }
-
-    public List<String> getEmojis() {
-        return pieces.stream()
-                .map(Piece::getEmoji)
-                .collect(Collectors.toList());
-    }
-
-    public List<Piece> getPieces() {
-        return pieces;
+    public Map<String, String> getBoard() {
+        return board;
     }
 }
