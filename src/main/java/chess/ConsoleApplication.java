93c80fb3e27731161c0ab0ce4143e55160ba1ed8
이한울 <dkfghg3@naver.com>
2022-04-07 17:51:43 +0900

    refactor: 콘솔 컨트롤러 클래스명 변경

--- src/main/java/chess/ConsoleApplication.java
+++ src/main/java/chess/ConsoleApplication.java
@@ -1,11 +1,11 @@
 package chess;
 
-import chess.controller.ChessGameController;
+import chess.controller.ConsoleChessGameController;
 
 public class ConsoleApplication {
 
     public static void main(String[] args) {
-        final ChessGameController chessGameController = new ChessGameController();
-        chessGameController.run();
+        final ConsoleChessGameController consoleChessGameController = new ConsoleChessGameController();
+        consoleChessGameController.run();
     }
 }
