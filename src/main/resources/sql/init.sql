83b6bbfaa68603c02134c14552b6b674a77974c0
Donggyu <a29661498@gmail.com>
2022-04-07 12:45:52 +0900

    refactor: DDL 변경
    불필요한 CHESSBOARD 테이블을 제거하였습니다.

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,8 +1,3 @@
-create table CHESSBOARD
-(
-    id     bigint auto_increment primary key
-);
-
 create table PIECE
 (
     id     bigint auto_increment primary key,
@@ -16,18 +11,14 @@ create table CHESSGAME
 (
     game_name varchar(20) not null,
     turn      varchar(5)  not null,
-    chess_board_id bigint,
     constraint CHESSGAME_pk
-        primary key (game_name),
-    constraint CHESSGAME_CHESSBOARD__fk
-        foreign key (chess_board_id) references CHESSBOARD (id)
-            on update cascade on delete cascade
+        primary key (game_name)
 );
 
 alter table PIECE
-    add chessboard_id bigint null;
+    add game_name varchar(20) null;
 
 alter table PIECE
     add constraint PIECE_CHESSBOARD__fk
-        foreign key (chessboard_id) references CHESSBOARD (id)
+        foreign key (game_name) references CHESSGAME (game_name)
             on update cascade on delete cascade;
