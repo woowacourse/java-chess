4098bbb811dd24419194882f0903b527f24eb038
summerlunaa <summerlunaa9@gmail.com>
2022-04-07 11:13:20 +0900

    feat: Color에 from 메서드 추가

--- src/main/java/chess/domain/Color.java
+++ src/main/java/chess/domain/Color.java
@@ -1,5 +1,7 @@
 package chess.domain;
 
+import java.util.Arrays;
+
 public enum Color {
 
     BLACK("black"),
@@ -11,6 +13,13 @@ public enum Color {
 
     Color(String name) {
         this.name = name;
+    }
+
+    public static Color from(String colorName) {
+        return Arrays.stream(values())
+                .filter(color -> color.name.equals(colorName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상태입니다."));
     }
 
     public boolean isBlack() {
