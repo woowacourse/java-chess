2fdcbef7fbb73954ba2c5d2bab5165bf542b6b02
pup-paw <wldusdl0310@gmail.com>
2022-04-07 18:40:34 +0900

    refactor: Map<>board 를 입력받아 board 를 생성해주는 전략 사용

--- src/test/java/chess/domain/ChessGameTest.java
+++ src/test/java/chess/domain/ChessGameTest.java
@@ -1,11 +1,14 @@
 package chess.domain;
 
+import static chess.domain.TestPieces.*;
 import static org.assertj.core.api.Assertions.*;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import chess.domain.board.TestOtherTeamBoard;
+import chess.domain.board.SavedBoardGenerator;
 import chess.domain.position.Square;
 
 public class ChessGameTest {
@@ -14,20 +17,24 @@ public class ChessGameTest {
     @DisplayName("흰 말로 시작하지 않으면 에러를 반환한다")
     void errorTurn_Start() {
         ChessGame chessGame = new ChessGame();
-        chessGame.startGame(new TestOtherTeamBoard(), GameTurn.WHITE);
+        chessGame.startGame(
+                new SavedBoardGenerator(Map.of(new Square("c3"), WHITE_QUEEN, new Square("d4"), BLACK_QUEEN)),
+                GameTurn.WHITE);
         assertThatThrownBy(() -> chessGame.move(new Square("d4"), new Square("e5")))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 순서 지키시지?!");
+                .hasMessage("순서 지키시지?!");
     }
 
     @Test
     @DisplayName("흰 말 다음 검은 말 순서로 진행하지 않으면 에러를 반환한다")
     void errorTurn() {
         ChessGame chessGame = new ChessGame();
-        chessGame.startGame(new TestOtherTeamBoard(), GameTurn.WHITE);
+        chessGame.startGame(
+                new SavedBoardGenerator(Map.of(new Square("c3"), WHITE_QUEEN, new Square("d4"), BLACK_QUEEN)),
+                GameTurn.WHITE);
         chessGame.move(new Square("c3"), new Square("d4"));
         assertThatThrownBy(() -> chessGame.move(new Square("d4"), new Square("e5")))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 순서 지키시지?!");
+                .hasMessage("순서 지키시지?!");
     }
 }
