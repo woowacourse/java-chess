664ed537d30f0075a15a7b60f5947c9fa042d5c6
Ohzzi <clover7even@naver.com>
2022-04-07 19:07:37 +0900

    fix: piece 테이블의 외래키 제약조건 변경

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -12,5 +12,5 @@ CREATE TABLE piece
     piece_type VARCHAR(10) NOT NULL,
     color      VARCHAR(10) NOT NULL,
     FOREIGN KEY (game_id)
-        REFERENCES game (game_id) ON UPDATE CASCADE
+        REFERENCES game (game_id) ON DELETE CASCADE
 );
