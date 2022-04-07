7a3fff1a395004b5cd084a629e2eeb50b441dfe3
yh20studio(tiki) <yh20studio@gmail.com>
2022-04-07 17:02:35 +0900

    refactor(): sql 자료형 변경

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -2,6 +2,6 @@ create table game
 (
     id    int         not null AUTO_INCREMENT,
     name varchar (10) not null unique,
-    command_log varchar(10000) not null,
+    command_log text not null,
     primary key (id)
 );
