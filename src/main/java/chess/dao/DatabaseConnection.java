8b581e774792d1807ad029069b83ff5fe014312f
yh20studio(tiki) <yh20studio@gmail.com>
2022-04-07 17:16:15 +0900

    refactor(): 데이터베이스 커넥션 에러 메세지 추가

--- src/main/java/chess/dao/DatabaseConnection.java
+++ src/main/java/chess/dao/DatabaseConnection.java
@@ -9,14 +9,13 @@ public class DatabaseConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/chess";
 	private static final String USER = "user";
 	private static final String PASSWORD = "password";
+	private static final String CONNECTION_ERROR = "현재 데이터 베이스를 연결할 수 없습니다.";
 
 	public static Connection getConnection() {
-		Connection connection = null;
 		try {
-			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			return DriverManager.getConnection(URL, USER, PASSWORD);
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new IllegalStateException(CONNECTION_ERROR);
 		}
-		return connection;
 	}
 }
