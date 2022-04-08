da2f2a49a920a2f4cac6f32e54c5c7822b965752
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-08 17:54:57 +0900

    refactor(web): init all config, routers, exception handler with one method

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,15 +1,12 @@
 package chess;
 
-import chess.web.WebConfig;
 import chess.web.WebRouter;
 
 public class WebApplication {
 
-    private static final WebConfig config = new WebConfig();
     private static final WebRouter router = new WebRouter();
 
     public static void main(String[] args) {
-        config.init();
         router.init();
     }
 }
