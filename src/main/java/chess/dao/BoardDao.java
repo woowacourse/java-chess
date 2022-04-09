9974d5cfbba9d5dc37b38e83472d33a2f9bcceb8
brorae <jhr3239@gmail.com>
2022-04-09 16:24:59 +0900

    refactor: 메소드 분리하여 인덴트 정리

--- src/main/java/chess/dao/BoardDao.java
+++ src/main/java/chess/dao/BoardDao.java
@@ -15,41 +15,55 @@ public class BoardDao {
         final String sql = "select position, symbol, color from board";
         final List<BoardDto> boardDtos = new ArrayList<>();
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    boardDtos.add(new BoardDto(
-                            resultSet.getString("position"),
-                            resultSet.getString("symbol"),
-                            resultSet.getString("color")));
-                }
-            }
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            loadBoardFromDB(boardDtos, statement);
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
         return boardDtos;
     }
 
+    private void loadBoardFromDB(final List<BoardDto> boardDtos, final PreparedStatement statement)
+            throws SQLException {
+        try (final ResultSet resultSet = statement.executeQuery()) {
+            loadPieceFromDB(boardDtos, resultSet);
+        }
+    }
+
+    private void loadPieceFromDB(final List<BoardDto> boardDtos, final ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            boardDtos.add(new BoardDto(
+                    resultSet.getString("position"),
+                    resultSet.getString("symbol"),
+                    resultSet.getString("color")));
+        }
+    }
+
     public void save(final List<BoardDto> boardDtos, final int gameId) {
         final String sql = "insert into board (position, symbol, color, game_id) values (?, ?, ?, ?)";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
-            for (BoardDto boardDto : boardDtos) {
-                statement.setString(1, boardDto.getPosition());
-                statement.setString(2, boardDto.getSymbol());
-                statement.setString(3, boardDto.getColor());
-                statement.setInt(4, gameId);
-                statement.executeUpdate();
-            }
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            convertPieceToBoard(boardDtos, gameId, statement);
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private void convertPieceToBoard(final List<BoardDto> boardDtos, final int gameId,
+                                     final PreparedStatement statement) throws SQLException {
+        for (final BoardDto boardDto : boardDtos) {
+            statement.setString(1, boardDto.getPosition());
+            statement.setString(2, boardDto.getSymbol());
+            statement.setString(3, boardDto.getColor());
+            statement.setInt(4, gameId);
+            statement.executeUpdate();
         }
     }
 
     public void update(final BoardDto boardDto) {
         final String sql = "update board set symbol = (?), color = (?) where position = (?)";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
             statement.setString(1, boardDto.getSymbol());
             statement.setString(2, boardDto.getColor());
             statement.setString(3, boardDto.getPosition());
--- src/main/java/chess/dao/GameDao.java
+++ src/main/java/chess/dao/GameDao.java
@@ -11,37 +11,53 @@ public class GameDao {
     public int findGameId() {
         final String sql = "select id from game order by id DESC LIMIT 1";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (!resultSet.next()) {
-                    throw new RuntimeException();
-                }
-                return resultSet.getInt("id");
-            }
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            return loadGameIdFromDB(statement);
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    private int loadGameIdFromDB(final PreparedStatement statement) throws SQLException {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            return checkExistId(resultSet);
+        }
+    }
+
+    private int checkExistId(ResultSet resultSet) throws SQLException {
+        if (!resultSet.next()) {
+            throw new RuntimeException();
+        }
+        return resultSet.getInt("id");
     }
 
     public String findGameState() {
         final String sql = "select * from game order by id DESC LIMIT 1";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (!resultSet.next()) {
-                    throw new RuntimeException();
-                }
-                return resultSet.getString("state");
-            }
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            return loadStateFromDB(statement);
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
+    private String loadStateFromDB(final PreparedStatement statement) throws SQLException {
+        try (final ResultSet resultSet = statement.executeQuery()) {
+            return checkExistState(resultSet);
+        }
+    }
+
+    private String checkExistState(ResultSet resultSet) throws SQLException {
+        if (!resultSet.next()) {
+            throw new RuntimeException();
+        }
+        return resultSet.getString("state");
+    }
+
     public void save(final String state) {
         final String sql = "insert into game (state) values (?)";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
             statement.setString(1, state);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -52,7 +68,7 @@ public class GameDao {
     public void update(final String state, int gameId) {
         final String sql = "update game set state = (?) where id = (?)";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
             statement.setString(1, state);
             statement.setInt(2, gameId);
             statement.executeUpdate();
@@ -64,7 +80,7 @@ public class GameDao {
     public void delete() {
         final String sql = "delete from game";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
             statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
@@ -74,7 +90,7 @@ public class GameDao {
     public void delete(int id) {
         final String sql = "delete from game where id = (?)";
 
-        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+        try (final PreparedStatement statement = getConnection().prepareStatement(sql)) {
             statement.setInt(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
