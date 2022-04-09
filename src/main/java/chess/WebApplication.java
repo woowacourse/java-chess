d3b581fc30c5c59972db6c385737cfc701e69a25
pup-paw <wldusdl0310@gmail.com>
2022-04-09 23:27:40 +0900

    refactor: application에서 분기해 controller의 메서드를 호출하는 방식으로 수정

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -1,10 +1,25 @@
 package chess;
 
+import static spark.Spark.*;
+
 import chess.controller.WebController;
+import chess.dao.ChessGameDao;
+import chess.dao.PieceDao;
 
 public class WebApplication {
     public static void main(String[] args) {
+        staticFiles.location("/static");
+        port(8080);
         WebController webController = new WebController();
-        webController.run();
+
+        ChessGameDao chessGameDao = new ChessGameDao();
+        PieceDao pieceDao = new PieceDao();
+
+        get("/", webController.ready());
+        get("/findgame", webController.askGameID());
+        post("/findgame", webController.findGame(chessGameDao, pieceDao));
+        get("/ingame", webController.runGame(chessGameDao, pieceDao));
+        post("/ingame", webController.movePiece(chessGameDao, pieceDao));
+        get("/status", webController.showResult(chessGameDao, pieceDao));
     }
 }
--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -16,49 +16,12 @@ import chess.domain.piece.Color;
 import chess.domain.position.Square;
 import chess.dto.BoardDto;
 import spark.ModelAndView;
+import spark.Route;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebController {
-    public void run() {
-        staticFiles.location("/static");
-        port(8080);
-
-        ChessGameDao chessGameDao = new ChessGameDao();
-        PieceDao pieceDao = new PieceDao();
-
-        get("/", (request, response) -> render(new HashMap<>(), "/ready.html"));
-
-        get("/findgame", ((request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("gameRoom", "어디로 들어가려구??");
-            return render(model, "/findgame.html");
-        }));
-
-        post("/findgame", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            String gameID = request.queryParams("gameID");
-            GameTurn gameTurn = GameTurn.find(chessGameDao.findTurnByID(gameID));
-            ChessGame chessGame = new ChessGame(new SavedBoardGenerator(pieceDao.findByGameID(gameID)), gameTurn);
-            chessGame.startGame();
-            try {
-                BoardDto boardDto = new BoardDto(chessGame.getBoard());
-                model.putAll(boardDto.getBoard());
-                model.put("gameID", gameID);
-                model.put("message", "누가 이기나 보자구~!");
-
-                if (chessGame.isKingDie()) {
-                    model.put("message", "킹 잡았다!! 게임 끝~!~!");
-                    return render(model, "/finished.html");
-                }
-                return render(model, "/ingame.html");
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-                model.put("gameRoom", e.getMessage());
-                return render(model, "/findgame.html");
-            }
-        });
-
-        get("/ingame", (request, response) -> {
+    public Route runGame(ChessGameDao chessGameDao, PieceDao pieceDao) {
+        return (request, response) -> {
             String gameID = request.queryParams("gameID");
             ChessGame chessGame;
             try {
@@ -85,9 +48,29 @@ public class WebController {
             model.put("message", "누가 이기나 보자구~!");
 
             return render(model, "/ingame.html");
-        });
+        };
+    }
 
-        post("/ingame", (request, response) -> {
+    public Route showResult(ChessGameDao chessGameDao, PieceDao pieceDao) {
+        return (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String gameID = request.queryParams("gameID");
+
+            GameTurn gameTurn = GameTurn.find(chessGameDao.findTurnByID(gameID));
+            ChessGame chessGame = new ChessGame(new SavedBoardGenerator(pieceDao.findByGameID(gameID)), gameTurn);
+            chessGame.startGame();
+
+            GameResult gameResult = new GameResult(chessGame.getBoard());
+            model.put("gameID", gameID);
+            model.put("whiteScore", gameResult.calculateScore(Color.WHITE));
+            model.put("blackScore", gameResult.calculateScore(Color.BLACK));
+            model.put("message", "수고하셨습니다 ^0^");
+            return render(model, "/status.html");
+        };
+    }
+
+    public Route movePiece(ChessGameDao chessGameDao, PieceDao pieceDao) {
+        return (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             String gameID = request.queryParams("gameID");
             String source = request.queryParams("source");
@@ -119,23 +102,45 @@ public class WebController {
                 return render(model, "/finished.html");
             }
             return render(model, "/ingame.html");
-        });
+        };
+    }
 
-        get("/status", (request, response) -> {
+    public Route findGame(ChessGameDao chessGameDao, PieceDao pieceDao) {
+        return (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             String gameID = request.queryParams("gameID");
-
             GameTurn gameTurn = GameTurn.find(chessGameDao.findTurnByID(gameID));
             ChessGame chessGame = new ChessGame(new SavedBoardGenerator(pieceDao.findByGameID(gameID)), gameTurn);
             chessGame.startGame();
+            try {
+                BoardDto boardDto = new BoardDto(chessGame.getBoard());
+                model.putAll(boardDto.getBoard());
+                model.put("gameID", gameID);
+                model.put("message", "누가 이기나 보자구~!");
 
-            GameResult gameResult = new GameResult(chessGame.getBoard());
-            model.put("gameID", gameID);
-            model.put("whiteScore", gameResult.calculateScore(Color.WHITE));
-            model.put("blackScore", gameResult.calculateScore(Color.BLACK));
-            model.put("message", "수고하셨습니다 ^0^");
-            return render(model, "/status.html");
-        });
+                if (chessGame.isKingDie()) {
+                    model.put("message", "킹 잡았다!! 게임 끝~!~!");
+                    return render(model, "/finished.html");
+                }
+                return render(model, "/ingame.html");
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                model.put("gameRoom", e.getMessage());
+                return render(model, "/findgame.html");
+            }
+        };
+    }
+
+    public Route askGameID() {
+        return (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("gameRoom", "어디로 들어가려구??");
+            return render(model, "/findgame.html");
+        };
+    }
+
+    public Route ready() {
+        return (request, response) -> render(new HashMap<>(), "/ready.html");
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
