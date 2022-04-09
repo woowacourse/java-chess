e5663dfdd03d3363e1c50570a4fa38b163ae0ccf
jurlring <78091011+jurlring@users.noreply.github.com>
2022-04-09 20:03:37 +0900

    refactor: DB connection 중복 코드 제거

--- src/main/java/chess/dao/BoardDao.java
+++ src/main/java/chess/dao/BoardDao.java
@@ -5,7 +5,6 @@ import chess.domain.piece.Piece;
 import chess.domain.piece.Pieces;
 import chess.domain.position.Position;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,30 +14,14 @@ import java.util.Map.Entry;
 
 public class BoardDao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/chess";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-
-    public Connection getConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
     public void save(final String name, final String position, final Long gameId) {
-        final Connection connection = getConnection();
         final String sql = "insert into board (name, position, gameId) values (?,?,?)";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, name);
             statement.setString(2, position);
             statement.setLong(3, gameId);
             statement.executeUpdate();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -54,10 +37,9 @@ public class BoardDao {
 
     public Board findById(final Long gameId) {
         Map<Position, Piece> board = new LinkedHashMap<>();
-        final Connection connection = getConnection();
         final String sql = "select * from board where gameId = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, gameId);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
@@ -65,7 +47,6 @@ public class BoardDao {
                 String name = resultSet.getString("name");
                 board.put(Position.create(position), Pieces.find(name));
             }
-            connection.close();
             return new Board(board);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -74,28 +55,24 @@ public class BoardDao {
     }
 
     public void deleteByGameId(final Long gameId) {
-        final Connection connection = getConnection();
         final String sql = "delete from board where gameId = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, gameId);
             statement.executeUpdate();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void updateNameByGameIdAndPosition(final Long gameId, final String position, final String name) {
-        final Connection connection = getConnection();
         final String sql = "update board set name = ? where gameId = ? and position = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, name);
             statement.setLong(2, gameId);
             statement.setString(3, position);
             statement.executeUpdate();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/chess/dao/ChessGameDao.java
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -2,7 +2,6 @@ package chess.dao;
 
 import chess.domain.piece.Color;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,53 +9,33 @@ import java.sql.Statement;
 
 public class ChessGameDao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/chess";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-
-    public Connection getConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
     public Long save(final Color turn) {
-        final Connection connection = getConnection();
         final String sql = "insert into chessGame (turn) values (?)";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, turn.getName());
             statement.executeUpdate();
             ResultSet generatedKeys = statement.getGeneratedKeys();
             if (!generatedKeys.next()) {
                 return null;
             }
-            long gameId = generatedKeys.getLong(1);
-            connection.close();
-            return gameId;
+            return generatedKeys.getLong(1);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return 0L;
+        return null;
     }
 
     public Color findTurnById(final Long id) {
-        final Connection connection = getConnection();
         final String sql = "select turn from chessGame where id = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (!resultSet.next()) {
                 return null;
             }
-            Color turn = Color.from(resultSet.getString("turn"));
-            connection.close();
-            return turn;
+            return Color.from(resultSet.getString("turn"));
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -64,27 +43,23 @@ public class ChessGameDao {
     }
 
     public void deleteById(final Long id) {
-        final Connection connection = getConnection();
         final String sql = "delete from chessGame where id = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
             statement.executeUpdate();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void updateTurnById(final Long id, final String turn) {
-        final Connection connection = getConnection();
         final String sql = "update chessGame set turn = ? where id = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (final Connection connection = JdbcConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, turn);
             statement.setLong(2, id);
             statement.executeUpdate();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
