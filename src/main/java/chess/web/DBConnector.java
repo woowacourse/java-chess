1a3d6a5ecd27e13e32a252fc29383c65710359df
hyewoncc <hyewoncc@gmail.com>
2022-04-10 00:37:38 +0900

    refactor: db 연결 오류 발생 시 RuntimeException 처리

--- src/main/java/chess/web/DBConnector.java
+++ src/main/java/chess/web/DBConnector.java
@@ -8,7 +8,9 @@ public class DBConnector {
 
     private static final String URL = "jdbc:mysql://localhost:3310/chess";
     private static final String USER = "user";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "1234";
+    public static final String ERROR_DB_CONNECTION_FAILED = "[ERROR] DB 연결에 실패했습니다.";
+    public static final String ERROR_CLASS_NOT_FOUND = "[ERROR] 해당 클래스를 찾을 수 없습니다.";
 
     public static Connection getConnection() {
         loadDriver();
@@ -16,16 +18,23 @@ public class DBConnector {
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_CONNECTION_FAILED);
         }
+        checkConnectionNotNull(connection);
         return connection;
     }
 
     private static void loadDriver() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (final Exception e) {
-            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(ERROR_CLASS_NOT_FOUND);
+        }
+    }
+
+    private static void checkConnectionNotNull(Connection connection) {
+        if (connection == null) {
+            throw new RuntimeException(ERROR_DB_CONNECTION_FAILED);
         }
     }
 
