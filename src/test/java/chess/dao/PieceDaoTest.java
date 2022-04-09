01969c3187fbe2b93a26ad15f20e1c654ce56a82
pup-paw <wldusdl0310@gmail.com>
2022-04-10 02:27:26 +0900

    refactor: 테스트 내용이 다른 테스트에 영향을 주지 않도록 beforeEach 를 사용해 테이블 초기화

--- src/test/java/chess/dao/PieceDaoTest.java
+++ src/test/java/chess/dao/PieceDaoTest.java
@@ -2,10 +2,45 @@ package chess.dao;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import chess.DBConnector;
+
 public class PieceDaoTest {
+
+    @BeforeEach
+    void initTable() {
+        DBConnector dbConnector = new DBConnector();
+        String dropSql = "drop table piece";
+        String createSql = "create table piece(\n"
+                + "    position varchar(10) not null,\n"
+                + "    type varchar(10) not null,\n"
+                + "    color varchar(10) not null,\n"
+                + "    gameID varchar(10) not null,\n"
+                + "    primary key(position),\n"
+                + "    foreign key(gameID) references chessGame(gameID)\n"
+                + ");";
+
+        try (Connection connection = dbConnector.getConnection();
+        PreparedStatement statement = connection.prepareStatement(dropSql)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try (Connection connection = dbConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(createSql)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @DisplayName("존재하는 게임에 대한 검색은 예외를 반환하지 않는다")
     @Test
