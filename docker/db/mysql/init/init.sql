4ba279dc0edc00e32e5e32680ec7b3c47d4bbbaf
seong-wooo <seongwoo0513@uos.ac.kr>
2022-04-09 16:10:26 +0900

    fix: 사용하지 않는 테이블 삭제

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,18 +1,3 @@
-create table member
-(
-    id   varchar(10) not null,
-    name varchar(20) not null,
-    primary key (id)
-);
-
-create table role
-(
-    user_id varchar(10) not null,
-    type    varchar(10) not null,
-    primary key (user_id),
-    foreign key (user_id) references member (id)
-);
-
 create table turn
 (
     team char(5) not null,
