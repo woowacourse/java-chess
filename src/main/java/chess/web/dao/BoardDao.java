31eaf3933c92c85793ce5f964788810f32ccd7b5
hyewoncc <hyewoncc@gmail.com>
2022-04-10 19:34:18 +0900

    refactor: SQLException 발생 시, RuntimeException throw 처리

--- src/main/java/chess/web/dao/BoardDao.java
+++ src/main/java/chess/web/dao/BoardDao.java
@@ -7,84 +7,71 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class BoardDao {
 
+    private static final String ERROR_DB_FAILED = "[ERROR] DB 연결에 문제가 발생했습니다.";
+
     public void save(int userId, GameStateDto gameStateDto) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "insert into board (player_id, turn) values (?, ?)";
 
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, userId);
             statement.setString(2, gameStateDto.getTurn());
             statement.execute();
-            closeResources(statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
     }
 
     public Color getTurn(int boardId) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "select turn from board where id = ?";
         Color color = Color.WHITE;
 
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, boardId);
-            final ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                color = Color.findByValue(resultSet.getString("turn"));
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    color = Color.findByValue(resultSet.getString("turn"));
+                }
             }
-            closeResources(resultSet, statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
-
         return color;
     }
 
     public int getBoardIdByPlayer(int plyerId) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "select id from board where player_id = ?";
         int id = 0;
 
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, plyerId);
-            final ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                id = resultSet.getInt("id");
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    id = resultSet.getInt("id");
+                }
             }
-            closeResources(resultSet, statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
         return id;
     }
 
     public void update(int boardId, GameStateDto gameStateDto) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "update board set turn = ? where id = ?";
 
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, gameStateDto.getTurn());
             statement.setInt(2, boardId);
             statement.execute();
-            closeResources(statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
     }
 
-    private void closeResources(Statement statement, Connection connection) throws SQLException {
-        statement.close();
-        connection.close();
-    }
-
-    private void closeResources(ResultSet resultSet, Statement statement, Connection connection)
-            throws SQLException {
-        resultSet.close();
-        statement.close();
-        connection.close();
-    }
 }
--- src/main/java/chess/web/dao/PieceDao.java
+++ src/main/java/chess/web/dao/PieceDao.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,25 +15,28 @@ import java.util.Optional;
 
 public class PieceDao {
 
+    private static final String ERROR_DB_FAILED = "[ERROR] DB 연결에 문제가 발생했습니다.";
+
     public void save(int boardId, PieceDto pieceDto) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "insert into piece (board_id, position , color, role) values (?, ?, ?, ?)";
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, boardId);
             statement.setString(2, pieceDto.getPosition());
             statement.setString(3, pieceDto.getColor());
             statement.setString(4, pieceDto.getRole());
             statement.execute();
-            closeResources(statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
     }
 
     public void saveAll(int boardId, Map<Position, Piece> pieces) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "insert into piece (board_id, position, color, role) values (?, ?, ?, ?)";
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             for (Position position : pieces.keySet()) {
                 PieceDto piece = PieceDto.from(position, pieces.get(position));
                 statement.setInt(1, boardId);
@@ -45,102 +47,92 @@ public class PieceDao {
                 statement.clearParameters();
             }
             statement.executeBatch();
-            closeResources(statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
     }
 
     public Optional<PieceDto> findOne(int boardId, String position) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "select * from piece where board_id = ? and position = ?";
         Optional<PieceDto> pieceDto = Optional.ofNullable(null);
-        try (final PreparedStatement statement = connection.prepareStatement(sql);) {
+
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, boardId);
             statement.setString(2, position);
-            final ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                pieceDto = Optional.of(new PieceDto(resultSet.getString("position"),
-                        resultSet.getString("color"),
-                        resultSet.getString("role")));
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    pieceDto = Optional.of(new PieceDto(resultSet.getString("position"),
+                            resultSet.getString("color"),
+                            resultSet.getString("role")));
+                }
             }
-            closeResources(resultSet, statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
         return pieceDto;
     }
 
     public List<PieceDto> findAll(int boardId) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "select * from piece where board_id = ?";
         List<PieceDto> pieceDtos = new ArrayList<>();
-        try (final PreparedStatement statement = connection.prepareStatement(sql);) {
+
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, boardId);
-            final ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                pieceDtos.add(new PieceDto(
-                        resultSet.getString("position"),
-                        resultSet.getString("color"),
-                        resultSet.getString("role")
-                ));
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    pieceDtos.add(new PieceDto(
+                            resultSet.getString("position"),
+                            resultSet.getString("color"),
+                            resultSet.getString("role")
+                    ));
+                }
             }
-            closeResources(resultSet, statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
         return pieceDtos;
     }
 
     public void updateOne(int boardId, String position, PieceDto pieceDto) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "update piece set color = ?, role = ? where board_id = ? and position = ?";
-        try (final PreparedStatement statement = connection.prepareStatement(sql);) {
+
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, pieceDto.getColor());
             statement.setString(2, pieceDto.getRole());
             statement.setInt(3, boardId);
             statement.setString(4, position);
             statement.execute();
-            closeResources(statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
     }
 
     public void deleteOne(int boardId, String position) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "delete from piece where board_id = ? and position = ?";
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, boardId);
             statement.setString(2, position);
             statement.execute();
-            closeResources(statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
     }
 
     public void deleteAll(int boardId) {
-        final Connection connection = DBConnector.getConnection();
         final String sql = "delete from piece where board_id = ?";
-        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+
+        try (final Connection connection = DBConnector.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, boardId);
             statement.executeUpdate();
-            closeResources(statement, connection);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ERROR_DB_FAILED);
         }
     }
 
-    private void closeResources(Statement statement, Connection connection) throws SQLException {
-        statement.close();
-        connection.close();
-    }
-
-    private void closeResources(ResultSet resultSet, Statement statement, Connection connection)
-            throws SQLException {
-        resultSet.close();
-        statement.close();
-        connection.close();
-    }
 }
