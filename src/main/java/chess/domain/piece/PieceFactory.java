77e4d63a31cbaa95d3b7a32b384e514956652a23
yeongunheo <gjduddns@gmail.com>
2022-04-10 22:11:24 +0900

    refactor: PieceFactory 인스턴스 이름을 규칙에 맞게 재정의

--- src/main/java/chess/domain/piece/PieceFactory.java
+++ src/main/java/chess/domain/piece/PieceFactory.java
@@ -5,22 +5,23 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum PieceFactory {
-    B_BLACK("B_BLACK", new Bishop(Color.BLACK)),
-    b_WHITE("b_WHITE", new Bishop(Color.WHITE)),
-    K_BLACK("K_BLACK", new King(Color.BLACK)),
-    k_WHITE("k_WHITE", new King(Color.WHITE)),
-    N_BLACK("N_BLACK", new Knight(Color.BLACK)),
-    n_WHITE("n_WHITE", new Knight(Color.WHITE)),
-    P_BLACK("P_BLACK", new StartedPawn(Color.BLACK)),
-    p_WHITE("p_WHITE", new StartedPawn(Color.WHITE)),
-    Q_BLACK("Q_BLACK", new Queen(Color.BLACK)),
-    q_WHITE("q_WHITE", new Queen(Color.WHITE)),
-    R_BLACK("R_BLACK", new Rook(Color.BLACK)),
-    r_WHITE("r_WHITE", new Rook(Color.WHITE))
+    BISHOP_BLACK("B_BLACK", new Bishop(Color.BLACK)),
+    BISHOP_WHITE("b_WHITE", new Bishop(Color.WHITE)),
+    KING_BLACK("K_BLACK", new King(Color.BLACK)),
+    KING_WHITE("k_WHITE", new King(Color.WHITE)),
+    KNIGHT_BLACK("N_BLACK", new Knight(Color.BLACK)),
+    KNIGHT_WHITE("n_WHITE", new Knight(Color.WHITE)),
+    PAWN_BLACK("P_BLACK", new StartedPawn(Color.BLACK)),
+    PAWN_WHITE("p_WHITE", new StartedPawn(Color.WHITE)),
+    QUEEN_BLACK("Q_BLACK", new Queen(Color.BLACK)),
+    QUEEN_WHITE("q_WHITE", new Queen(Color.WHITE)),
+    ROOK_BLACK("R_BLACK", new Rook(Color.BLACK)),
+    ROOK_WHITE("r_WHITE", new Rook(Color.WHITE))
     ;
 
     public static final String BLACK_PAWN_START_RANK = "7";
     public static final String WHITE_PAWN_START_RANK = "2";
+
     private String name;
     private Piece piece;
 
