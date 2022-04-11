af544bcc9259b5bd1d9024d336d0a89c66f336ef
Hongdonggeon <ghd700@naver.com>
2022-04-12 03:04:57 +0900

    chore: sql문 변경

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,8 +1,10 @@
 CREATE TABLE `game`
 (
-    `id`    int         NOT NULL AUTO_INCREMENT,
-    `state` varchar(10) NOT NULL,
-    PRIMARY KEY (`id`)
+    `id`      int         NOT NULL AUTO_INCREMENT,
+    `state`   varchar(20) NOT NULL,
+    `room_id` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `game_room_id_uindex` (`room_id`)
 );
 
 CREATE TABLE `board`
@@ -12,7 +14,7 @@ CREATE TABLE `board`
     `piece`    varchar(10) NOT NULL,
     `game_id`  int         NOT NULL,
     PRIMARY KEY (`position`, `game_id`),
-    KEY        `board_game_id_fk` (`game_id`),
-    CONSTRAINT `board_game_id_fk` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    KEY `board_game_id_fk` (`game_id`),
+    CONSTRAINT `board_game_id_fk` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
 );
 
