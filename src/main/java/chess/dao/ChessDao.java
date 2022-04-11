bcb953e6e85894f76fe733c7ed6f1b0e0490ef19
yoon <duddbs774@gmail.com>
2022-04-11 11:17:06 +0900

    refactor: connection 사용뒤 close 로직 추가

--- src/main/java/chess/dao/ChessDao.java
+++ src/main/java/chess/dao/ChessDao.java
@@ -3,9 +3,6 @@ package chess.dao;
 import chess.domain.piece.Piece;
 import chess.domain.position.Position;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
@@ -13,47 +10,47 @@ import java.util.Map;
 
 public class ChessDao {
 
-    static final int GAME_ID = 0;
+    private static final int GAME_ID = 0;
 
     static {
-        try {
-            if (!hasAlreadyGame()) {
-                insertGame();
+        if (!hasAlreadyGame()) {
+            insertGame();
+        }
+    }
+
+    private static boolean hasAlreadyGame() {
+        final var sql = "SELECT COUNT(*) from game WHERE game_id = ?";
+        var count = 0;
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setInt(1, GAME_ID);
+            try (final var resultSet = prepareStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    count = Integer.parseInt(resultSet.getString(1));
+                }
+                return count != 0;
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
     }
 
-    private static boolean hasAlreadyGame() throws SQLException {
-        final var sql = "SELECT COUNT(*) from game WHERE game_id = ?";
-        final var prepareStatement = ChessConnection.getConnection().prepareStatement(sql);
-        prepareStatement.setInt(1, GAME_ID);
-        final ResultSet resultSet = prepareStatement.executeQuery();
-        if (resultSet.next()) {
-            final var count = Integer.parseInt(resultSet.getString(1));
-            return count != 0;
-        }
-        throw new SQLException("game_id: " + GAME_ID + "를 조회할수 없습니다.");
-    }
-
-    private static void insertGame() throws SQLException {
+    private static void insertGame() {
         final var sql = "INSERT INTO game (game_id) VALUES(?)";
-        final var prepareStatement = ChessConnection.getConnection().prepareStatement(sql);
-        prepareStatement.setInt(1, GAME_ID);
-        prepareStatement.execute();
-    }
-
-    private final Connection connection = ChessConnection.getConnection();
-
-    public void initBoard(final Map<Position, Piece> board) {
-        try {
-            initTurn();
-            deleteAllPiece();
-            savePiece(board);
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setInt(1, GAME_ID);
+            prepareStatement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void initBoard(final Map<Position, Piece> board) {
+        initTurn();
+        deleteAllPiece();
+        savePiece(board);
     }
 
     public void initTurn() {
@@ -62,9 +59,8 @@ public class ChessDao {
 
     public void updateTurn(final String color) {
         final var sql = "UPDATE game SET current_turn=? WHERE game_id=?";
-        final PreparedStatement prepareStatement;
-        try {
-            prepareStatement = ChessConnection.getConnection().prepareStatement(sql);
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
             prepareStatement.setString(1, color);
             prepareStatement.setInt(2, GAME_ID);
             prepareStatement.execute();
@@ -75,46 +71,48 @@ public class ChessDao {
 
     public void deleteAllPiece() {
         final var sql = "DELETE FROM piece WHERE game_id = ?";
-        final PreparedStatement statement;
-        try {
-            statement = connection.prepareStatement(sql);
-            statement.setInt(1, GAME_ID);
-            statement.execute();
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql);) {
+            prepareStatement.setInt(1, GAME_ID);
+            prepareStatement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
-    private void savePiece(final Map<Position, Piece> board) throws SQLException {
+    private void savePiece(final Map<Position, Piece> board) {
         final var sql = "INSERT INTO piece (game_id, piece_name, piece_color, position) VALUES(?,?,?,?)";
-        final PreparedStatement statement = connection.prepareStatement(sql);
-        for (final Map.Entry<Position, Piece> entry : board.entrySet()) {
-            statement.setInt(1, GAME_ID);
-            statement.setString(2, entry.getValue().getNotation().name());
-            statement.setString(3, entry.getValue().getColor().name());
-            statement.setString(4, entry.getKey().getFile().name() + entry.getKey().getRankNumber());
-            statement.addBatch();
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
+            for (final Map.Entry<Position, Piece> entry : board.entrySet()) {
+                prepareStatement.setInt(1, GAME_ID);
+                prepareStatement.setString(2, entry.getValue().getNotation().name());
+                prepareStatement.setString(3, entry.getValue().getColor().name());
+                prepareStatement.setString(4, entry.getKey().getFile().name() + entry.getKey().getRankNumber());
+                prepareStatement.addBatch();
+            }
+            prepareStatement.executeBatch();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        statement.executeBatch();
     }
 
     public Map<String, List<String>> findBoard() {
         final var sql = "SELECT * FROM piece WHERE game_id=?";
-        final PreparedStatement prepareStatement;
-        try {
-            prepareStatement = ChessConnection.getConnection().prepareStatement(sql);
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
             prepareStatement.setInt(1, GAME_ID);
-            final var resultSet = prepareStatement.executeQuery();
-            final Map<String, List<String>> boardData = new HashMap<>();
-            while (resultSet.next()) {
-                boardData.put(resultSet.getString("position"),
-                        List.of(
-                                resultSet.getString("piece_name"),
-                                resultSet.getString("piece_color")
-                        ));
+            try (final var resultSet = prepareStatement.executeQuery()) {
+                final Map<String, List<String>> boardData = new HashMap<>();
+                while (resultSet.next()) {
+                    boardData.put(resultSet.getString("position"),
+                            List.of(
+                                    resultSet.getString("piece_name"),
+                                    resultSet.getString("piece_color")
+                            ));
+                }
+                return boardData;
             }
-            return boardData;
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -123,15 +121,15 @@ public class ChessDao {
 
     public String findCurrentColor() {
         final var sql = "SELECT current_turn FROM game WHERE game_id=?";
-        final PreparedStatement prepareStatement;
-        try {
-            prepareStatement = ChessConnection.getConnection().prepareStatement(sql);
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
             prepareStatement.setInt(1, GAME_ID);
-            final ResultSet resultSet = prepareStatement.executeQuery();
-            if (resultSet.next()) {
-                return resultSet.getString(1);
+            try (final var resultSet = prepareStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getString(1);
+                }
+                throw new SQLException("현재 순서의 색상을 조회할 수 없습니다.");
             }
-            throw new SQLException("현재 순서의 색상을 조회할 수 없습니다.");
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -139,28 +137,32 @@ public class ChessDao {
     }
 
     public void updateBoard(final String from, final String to, final String color) {
-        try {
-            deletePiece(to);
-            updatePiece(from, to);
-            updateTurn(color);
+        deletePiece(to);
+        updatePiece(from, to);
+        updateTurn(color);
+    }
+
+    private void deletePiece(final String to) {
+        final var sql = "DELETE FROM piece WHERE position = ?";
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, to);
+            prepareStatement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private void deletePiece(final String to) throws SQLException {
-        final var sql = "DELETE FROM piece WHERE position = ?";
-        final var statement = connection.prepareStatement(sql);
-        statement.setString(1, to);
-        statement.execute();
-    }
-
-    private void updatePiece(final String from, final String to) throws SQLException {
+    private void updatePiece(final String from, final String to) {
         final var sql = "UPDATE piece SET position = ? WHERE position = ? AND game_id = ?";
-        final var statement = connection.prepareStatement(sql);
-        statement.setString(1, to);
-        statement.setString(2, from);
-        statement.setInt(3, GAME_ID);
-        statement.execute();
+        try (final var connection = ChessConnection.getConnection();
+             final var prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, to);
+            prepareStatement.setString(2, from);
+            prepareStatement.setInt(3, GAME_ID);
+            prepareStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
