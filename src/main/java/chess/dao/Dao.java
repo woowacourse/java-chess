6db4e589c2815aa135bb3d43270e2aa6c5f6c08e
devHudi <devhudi@gmail.com>
2022-04-11 14:26:05 +0900

    refactor(Dao): 메소드 및 상수 추출

--- src/main/java/chess/dao/Dao.java
+++ src/main/java/chess/dao/Dao.java
@@ -5,29 +5,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class Dao {
-    protected Connection getConnection() {
-        Connection con = null;
-        String server = "localhost:3306"; // MySQL 서버 주소
-        String database = "chess"; // MySQL DATABASE 이름
-        String userName = "user"; //  MySQL 서버 아이디
-        String password = "password"; // MySQL 서버 비밀번호
 
-        // 드라이버 로딩
+    private static final String MYSQL_HOST = "localhost:3306";
+    private static final String MYSQL_DATABASE = "chess";
+    private static final String MYSQL_USERNAME = "user";
+    private static final String MYSQL_PASSWORD = "password";
+
+    protected Connection getConnection() {
+        loadDriver();
+        return loadConnection();
+    }
+
+    private Connection loadConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://" + MYSQL_HOST + "/" + MYSQL_DATABASE,
+                    MYSQL_USERNAME,
+                    MYSQL_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private void loadDriver() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            System.err.println(" !! JDBC Driver load 오류: " + e.getMessage());
             e.printStackTrace();
         }
-
-        // 드라이버 연결
-        try {
-            con = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database, userName, password);
-        } catch (SQLException e) {
-            System.err.println("연결 오류:" + e.getMessage());
-            e.printStackTrace();
-        }
-
-        return con;
     }
 }
