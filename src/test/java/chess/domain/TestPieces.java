fd78d66fdd80e72a2ff1bd2ab8084e4fc1edd25a
pup-paw <wldusdl0310@gmail.com>
2022-04-11 16:05:31 +0900

    refactor: test 내에서 사용되는 piece 상수 접근 제어자 통일

--- src/test/java/chess/domain/TestPieces.java
+++ src/test/java/chess/domain/TestPieces.java
@@ -7,7 +7,7 @@ import chess.domain.position.Row;
 public class TestPieces {
     public static final Piece WHITE_QUEEN = Piece.createByPosition(Column.D, Row.ONE);
     public static final Piece BLACK_QUEEN = Piece.createByPosition(Column.D, Row.EIGHT);
-    static final Piece WHITE_BISHOP = Piece.createByPosition(Column.C, Row.ONE);
-    static final Piece WHITE_PAWN = Piece.createByPosition(Column.A, Row.TWO);
-    static final Piece WHITE_ROOK = Piece.createByPosition(Column.A, Row.ONE);
+    public static final Piece WHITE_BISHOP = Piece.createByPosition(Column.C, Row.ONE);
+    public static final Piece WHITE_PAWN = Piece.createByPosition(Column.A, Row.TWO);
+    public static final Piece WHITE_ROOK = Piece.createByPosition(Column.A, Row.ONE);
 }
