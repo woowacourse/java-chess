888b85a54c2e2b381253e4c5f50b5417b1093aa2
bcc0830 <bcc0830@naver.com>
2022-04-12 12:15:12 +0900

    refactor : RequestParser의 parse 로직 메소드 분리

--- src/main/java/chess/web/RequestParser.java
+++ src/main/java/chess/web/RequestParser.java
@@ -1,13 +1,14 @@
 package chess.web;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RequestParser {
 
     private static final String INPUT_EMPTY = "입력은 한 글자 이상이어야 합니다.";
     private static final String INPUT_BLANK = "입력 전체가 공백문자여서는 안됩니다.";
+    private static final String REGEX = "(command=)|(\r\n)";
+    private static final String DELETE_STRING = "";
+    private static final String DELIMITER = " ";
 
     private final List<String> command;
 
@@ -18,10 +19,12 @@ public class RequestParser {
     }
 
     public static RequestParser from(String body) {
-        return new RequestParser(
-                Arrays.stream(body.replace("command=", "")
-                        .replace("\r\n", "")
-                        .split(" ")).collect(Collectors.toList()));
+        return new RequestParser(parse(body));
+    }
+
+    private static List<String> parse(String body) {
+        return List.of(body.replaceAll(REGEX, DELETE_STRING)
+                .split(DELIMITER));
     }
 
     public List<String> getCommand() {
@@ -29,7 +32,7 @@ public class RequestParser {
     }
 
     public String getRoomID() {
-        return String.join("", command);
+        return String.join(DELETE_STRING, command);
     }
 
     private void validateNotEmpty(List<String> command) {
@@ -39,7 +42,7 @@ public class RequestParser {
     }
 
     private void validateNotBlank(List<String> command) {
-        if (String.join("", command).isBlank()) {
+        if (String.join(DELETE_STRING, command).isBlank()) {
             throw new IllegalArgumentException(INPUT_BLANK);
         }
     }
