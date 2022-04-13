b6e6bbb26de64cbc0301cf132accc4e7daf3abf6
awesomeo184 <awesomeo184@gmail.com>
2022-04-13 14:10:32 +0900

    chore

--- src/main/java/chess/ConsoleApplication.java
+++ src/main/java/chess/ConsoleApplication.java
@@ -1,10 +1,10 @@
 package chess;
 
-import chess.controller.ConsoleGameController;
+import chess.controller.ConsoleGameRunner;
 
 public class ConsoleApplication {
 
     public static void main(String[] args) {
-        new ConsoleGameController().run();
+        new ConsoleGameRunner().run();
     }
 }
