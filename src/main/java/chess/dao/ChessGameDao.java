b934e6d3f75e3485551b38904f5b62cde364edbc
SuyeonChoi <tndus970120@gmail.com>
2022-04-14 17:21:49 +0900

    refactor: try-with-resources로 변경

--- src/main/java/chess/dao/ChessGameDao.java
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -2,35 +2,20 @@ package chess.dao;
 
 import chess.domain.state.GameState;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ChessGameDao {
-    private static final String URL = "jdbc:mysql://localhost:3306/chess";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
     private static final String TABLE_ID = "id";
     private static final String CHESSGAME_STATE = "state";
     private static final String SQL_STATEMENT_EXCEPTION = "[ERROR] SQL 문을 실행할 수 없습니다.";
     private static final String DATA_NOT_EXISTS_EXCEPTION = "[ERROR] 요청된 데이터가 존재하지 않습니다.";
 
-    public Connection getConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
     public void save(GameState gameState) {
-        final Connection connection = getConnection();
         final String sql = "insert into chessgame (state) values (?)";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, gameState.getClass().getSimpleName());
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -39,10 +24,9 @@ public class ChessGameDao {
     }
 
     public Integer findId() {
-        final Connection connection = getConnection();
         final String sql = "select id from chessgame";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             final ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 return resultSet.getInt(TABLE_ID);
@@ -54,10 +38,9 @@ public class ChessGameDao {
     }
 
     public String findStateName() {
-        final Connection connection = getConnection();
         final String sql = "select state from chessgame";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             final ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 return resultSet.getString(CHESSGAME_STATE);
@@ -69,10 +52,9 @@ public class ChessGameDao {
     }
 
     public void update(GameState gameState) {
-        final Connection connection = getConnection();
         final String sql = "update chessgame set state = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, gameState.getClass().getSimpleName());
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -81,10 +63,9 @@ public class ChessGameDao {
     }
 
     public void delete() {
-        final Connection connection = getConnection();
         final String sql = "delete from chessgame";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
--- src/main/java/chess/dao/ChessPieceDao.java
+++ src/main/java/chess/dao/ChessPieceDao.java
@@ -3,7 +3,6 @@ package chess.dao;
 import chess.domain.ChessBoardPosition;
 import chess.domain.piece.ChessPiece;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,9 +13,6 @@ import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 
 public class ChessPieceDao {
-    private static final String URL = "jdbc:mysql://localhost:3306/chess";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
     private static final String TABLE_ID = "id";
     private static final String CHESSBOARD_BOARD_ROW = "board_row";
     private static final String CHESSBOARD_BOARD_COLUMN = "board_column";
@@ -24,16 +20,6 @@ public class ChessPieceDao {
     private static final String CHESSPIECE_TEAM = "team";
     private static final String SQL_STATEMENT_EXCEPTION = "[ERROR] SQL 문을 실행할 수 없습니다.";
     private static final String DATA_NOT_EXISTS_EXCEPTION = "[ERROR] 요청된 데이터가 존재하지 않습니다.";
-
-    public Connection getConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
 
     public void saveAll(int chessGameId, Map<ChessBoardPosition, ChessPiece> chessPieces) {
         for (Entry<ChessBoardPosition, ChessPiece> boardBlock : chessPieces.entrySet()) {
@@ -43,10 +29,9 @@ public class ChessPieceDao {
     }
 
     private int findId(ChessPiece chessPiece) {
-        final Connection connection = getConnection();
         final String sql = "select id from chesspiece where name = ? and team = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, chessPiece.getClass().getSimpleName());
             statement.setString(2, chessPiece.getTeam().name());
             final ResultSet resultSet = statement.executeQuery();
@@ -60,11 +45,10 @@ public class ChessPieceDao {
     }
 
     private void save(ChessBoardPosition chessBoardPosition, int chessPieceId, int chessGameId) {
-        final Connection connection = getConnection();
         final String sql = "insert into chessboard (board_row, board_column, chesspiece_id, chessgame_id) "
                 + "values (?, ?, ?, ?)";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, chessBoardPosition.getRow());
             statement.setString(2, String.valueOf(chessBoardPosition.getColumn()));
             statement.setInt(3, chessPieceId);
@@ -76,11 +60,10 @@ public class ChessPieceDao {
     }
 
     public Map<String, List<String>> findAll() {
-        final Connection connection = getConnection();
         final String sql = "select board_row, board_column, name, team "
                 + "from chessboard join chesspiece on chessboard.chesspiece_id = chesspiece.id";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             ResultSet resultSet = statement.executeQuery();
             Map<String, List<String>> board = new HashMap<>();
             while (resultSet.next()) {
@@ -98,11 +81,10 @@ public class ChessPieceDao {
 
     public void update(int sourcePositionRow, char sourcePositionColumn,
                        int targetPositionRow, char targetPositionColumn) {
-        final Connection connection = getConnection();
         final String sql = "update chessboard set board_row = ?, board_column = ? "
                 + "where board_row=? and board_column = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, targetPositionRow);
             statement.setString(2, String.valueOf(targetPositionColumn));
             statement.setInt(3, sourcePositionRow);
@@ -114,10 +96,9 @@ public class ChessPieceDao {
     }
 
     public void deleteAll() {
-        final Connection connection = getConnection();
         final String sql = "delete from chessboard";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -125,10 +106,9 @@ public class ChessPieceDao {
     }
 
     public void deleteByPosition(int row, char column) {
-        final Connection connection = getConnection();
         final String sql = "delete from chessboard where board_row= ? and board_column = ?";
-        try {
-            final PreparedStatement statement = connection.prepareStatement(sql);
+        try(Connection connection = DatabaseConnector.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, row);
             statement.setString(2, String.valueOf(column));
             statement.executeUpdate();
