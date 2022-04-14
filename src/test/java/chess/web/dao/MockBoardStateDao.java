a3c4d3da82c3004c57a30f5e48a7992af5548b33
summerlunaa <summerlunaa9@gmail.com>
2022-04-14 20:42:52 +0900

    refactor: 사용하지 않는 Dto 삭제

--- src/test/java/chess/web/dao/MockBoardStateDao.java
+++ src/test/java/chess/web/dao/MockBoardStateDao.java
@@ -1,31 +1,27 @@
 package chess.web.dao;
 
 import chess.domain.state.StateType;
-import chess.web.dto.BoardStateDto;
 
 public class MockBoardStateDao implements BoardStateDao {
 
-    private BoardStateDto boardStateDto = null;
+    private StateType stateType = null;
 
     @Override
     public void save(StateType stateType) {
-        this.boardStateDto = new BoardStateDto(stateType);
+        this.stateType = stateType;
     }
 
     @Override
     public void update(StateType stateType) {
-        this.boardStateDto = new BoardStateDto(stateType);
+        this.stateType = stateType;
     }
 
     @Override
     public void deleteAll() {
-        boardStateDto = null;
+        stateType = null;
     }
 
     public StateType selectState() {
-        if (boardStateDto == null) {
-            return null;
-        }
-        return boardStateDto.getStateType();
+        return stateType;
     }
 }
