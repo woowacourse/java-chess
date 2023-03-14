b5d87a1d413f6e5b3464ef485a5b61a45af3f165
Cyma-s <ssol0319@naver.com>
2023-03-14 16:45:25 +0900

    feat(Square): 체스판의 한 칸 생성 기능 추가
    Co-authored-by: dooboocookie <bkh3030@gmail.com>

---
+++ src/main/java/chess/domain/Square.java
@@ -0,0 +1,41 @@
+package chess.domain;
+
+import java.util.*;
+
+public class Square {
+    private static final Map<File, Map<Rank, Square>> CACHE;
+
+    private final File file;
+    private final Rank rank;
+
+    static {
+        CACHE = new HashMap<>();
+        for (File file : File.values()) {
+            CACHE.put(file, createFile(file));
+        }
+    }
+
+    private static Map<Rank, Square> createFile(final File file) {
+        Map<Rank, Square> ranks = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            ranks.put(rank, new Square(file, rank));
+        }
+        return ranks;
+    }
+
+    private Square(final File file, final Rank rank) {
+        this.file = file;
+        this.rank = rank;
+    }
+
+    public static Square of(final File file, final Rank rank) {
+        Map<Rank, Square> rankSquareMap = CACHE.get(file);
+        return rankSquareMap.get(rank);
+    }
+
+    public static List<Square> getAllSquares() {
+        List<Square> squares = new ArrayList<>();
+        CACHE.values().forEach(file -> squares.addAll(file.values()));
+        return squares;
+    }
+}
