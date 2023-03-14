0e39f4dba9b3f8794c76b4db50fecd1bda244978
dhkim <akdlzmf1123@gmail.com>
2023-03-14 16:43:40 +0900

    feat: 파일간의 거리를 구하는 기능 추가

---
+++ src/main/java/chess/domain/File.java
@@ -0,0 +1,24 @@
+package chess.domain;
+
+public enum File {
+    A(1, "a"),
+    B(2, "b"),
+    C(3, "c"),
+    D(4, "d"),
+    E(5, "e"),
+    F(6, "f"),
+    G(7, "g"),
+    H(8, "h");
+
+    private final int position;
+    private final String symbol;
+
+    File(final int position, final String symbol) {
+        this.position = position;
+        this.symbol = symbol;
+    }
+
+    public int distanceTo(final File other) {
+        return Math.abs(position - other.position);
+    }
+}
---
+++ src/test/java/chess/domain/FileTest.java
@@ -0,0 +1,28 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("File 클래스")
+class FileTest {
+
+    @Nested
+    @DisplayName("distanceTo 메서드는")
+    class distanceTo {
+        @Nested
+        @DisplayName("다른 File 객체가 주어지면")
+        class withAnotherFile {
+            final File from = File.A;
+            final File other = File.D;
+
+            @Test
+            @DisplayName("두 파일간의 거리를 계산한다")
+            void it_returns_distance() {
+                assertThat(from.distanceTo(other)).isEqualTo(3);
+            }
+        }
+    }
+}
