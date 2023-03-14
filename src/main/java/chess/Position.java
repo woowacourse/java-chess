a2230377f102232f34915b3b7d34a67a089d1111
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-14 21:55:34 +0900

    feat : 위치 생성 기능 추가
    Co-authored-by: thdwoqor <celebarte01@gmail.com>

---
+++ src/main/java/chess/Position.java
@@ -0,0 +1,21 @@
+package chess;
+
+public class Position {
+    private final int row;
+    private final int column;
+
+    public Position(final int row, final int column) {
+        validate(row, column);
+        this.row = row;
+        this.column = column;
+    }
+
+    private void validate(final int row, final int column) {
+        if (row < 0) {
+            throw new IllegalArgumentException("가로 위치는 0 미만일 수 없습니다.");
+        }
+        if (column < 0) {
+            throw new IllegalArgumentException("세로 위치는 0 미만일 수 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/chess/PositionTest.java
@@ -0,0 +1,25 @@
+package chess;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PositionTest {
+    @DisplayName("가로는 0미만일 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-5, -4, -3, -2, -1})
+    void throwExceptionWhenRowIsLessThenZero(final int row) {
+        assertThatThrownBy(() -> new Position(row, 0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("세로는 0미만일 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-5, -4, -3, -2, -1})
+    void throwExceptionWhenColumnIsLessThenZero(final int column) {
+        assertThatThrownBy(() -> new Position(0, column))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
