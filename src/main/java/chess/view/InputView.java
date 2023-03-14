6a4eccd61e118e981978420ea895fc193f8ef17f
dhkim <akdlzmf1123@gmail.com>
2023-03-14 21:11:02 +0900

    feat: 명령어 변환 기능

---
+++ src/main/java/chess/view/InputView.java
@@ -0,0 +1,12 @@
+package chess.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+}
