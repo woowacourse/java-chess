a1e03c607165f422f4ddb4761952ab98d9141dda
dev_kong <ahdjd5@gmail.com>
2023-03-14 22:26:11 +0900

    test: knight가 이동할 수 있는 square를 반환하는 기능에 대한 테스트코드 작성

---
+++ src/test/java/domain/piece/type/KnightTest.java
@@ -0,0 +1,22 @@
+package domain.piece.type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import domain.board.Square;
+import domain.piece.Camp;
+
+class KnightTest {
+
+    @Test
+    @DisplayName("knight가 이동할 수 있는 square를 반환한다.")
+    void fetchMovableCoordinate() {
+        Knight knight = new Knight(Camp.WHITE);
+        assertThat(knight.fetchMovableCoordinate(new Square(1,3), new Square(3,4))).contains(
+                new Square(3,4)
+        );
+    }
+}
