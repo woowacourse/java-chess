dbb65c6d65e0ee3a129e381a8141e45769eac519
Cyma-s <ssol0319@naver.com>
2023-03-14 15:20:36 +0900

    feat(Piece): 초기 기물 생성 기능 추가
    Co-authored-by: dooboocookie <bkh3030@gmail.com>

--- src/main/java/chess/domain/Role.java
+++ src/main/java/chess/domain/Role.java
@@ -1,10 +1,20 @@
 package chess.domain;
 
 public enum Role {
-    PAWN,
-    ROOK,
-    BISHOP,
-    KNIGHT,
-    QUEEN,
-    KING
+    PAWN(8),
+    ROOK(2),
+    BISHOP(2),
+    KNIGHT(2),
+    QUEEN(1),
+    KING(1);
+
+    private final int count;
+
+    Role(final int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
