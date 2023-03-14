dda4b0bb5d55f8e63da603eead975c76869f7ded
JunH <tjdtls690@gmail.com>
2023-03-14 17:26:13 +0900

    :sparkles: feat : x좌표 객체 구현 및 예외 처리 기능 구현

---
+++ src/main/java/coordinate/Row.java
@@ -0,0 +1,19 @@
+package coordinate;
+
+public class Row {
+    private static final int MIN_ROW = 1;
+    private static final int MAX_ROW = 8;
+    
+    private final int row;
+    
+    public Row(int row) {
+        validateOutOfRange(row);
+        this.row = row;
+    }
+    
+    private void validateOutOfRange(int row) {
+        if (row < MIN_ROW || row > MAX_ROW) {
+            throw new IllegalArgumentException("row는 1~8까지의 숫자만 가능합니다.");
+        }
+    }
+}
