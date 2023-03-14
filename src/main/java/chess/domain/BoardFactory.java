19813940e065b3e90e61cb1dd428a3605ea4ae25
drunkenhw <drunkenhw@gmail.com>
2023-03-14 17:09:49 +0900

    feat: BoardFactory 생성

---
+++ src/main/java/chess/domain/BoardFactory.java
@@ -0,0 +1,18 @@
+package chess.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardFactory {
+
+    public static Board createBoard() {
+        List<Rank> ranks = new ArrayList<>();
+        for (RankCoordinate rankCoordinate : RankCoordinate.values()) {
+            Color color = Color.of(rankCoordinate);
+            RankType rankType = RankType.of(rankCoordinate);
+            Rank rank = RankFactory.createRank(rankType, rankCoordinate, color);
+            ranks.add(rank);
+        }
+        return new Board(ranks);
+    }
+}
---
+++ src/test/java/chess/domain/BoardFactoryTest.java
@@ -0,0 +1,18 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class BoardFactoryTest {
+
+    @Test
+    void 체스판을_생성한다() {
+        Board board = BoardFactory.createBoard();
+        assertThat(board.getRanks()).hasSize(8);
+    }
+}
