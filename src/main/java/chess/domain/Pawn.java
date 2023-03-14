67f41c57743ca62b98ae38740e07d4d83895d7be
dhkim <akdlzmf1123@gmail.com>
2023-03-14 17:17:50 +0900

    feat: Pawn 클래스 추가
    - 각 진영별로 8개만 생성 제한

---
+++ src/main/java/chess/domain/Pawn.java
@@ -0,0 +1,33 @@
+package chess.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Pawn {
+    private static final List<Pawn> blackPawns = new ArrayList<>();
+    private static final List<Pawn> whitePawns = new ArrayList<>();
+
+    static {
+        addPawns(blackPawns, Team.BLACK);
+        addPawns(whitePawns, Team.WHITE);
+    }
+
+    private final Team team;
+
+    private Pawn(Team team) {
+        this.team = team;
+    }
+
+    private static void addPawns(final List<Pawn> pawns, final Team team) {
+        for (int i = 0; i < 8; i++) {
+            pawns.add(new Pawn(team));
+        }
+    }
+
+    public static List<Pawn> of(final Team team) {
+        if (team == Team.BLACK) {
+            return List.copyOf(blackPawns);
+        }
+        return List.copyOf(whitePawns);
+    }
+}
---
+++ src/test/java/chess/domain/PawnTest.java
@@ -0,0 +1,25 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Pawn 클래스")
+class PawnTest {
+
+    @Nested
+    @DisplayName("of 메서드는")
+    class of {
+        @Nested
+        @DisplayName("진영이 주어지면")
+        class givenColor {
+            @Test
+            @DisplayName("해당 진영의 Pawn을 8개 생성한다")
+            void it_returns_pawns() {
+                assertThat(Pawn.of(Team.BLACK)).hasSize(8);
+            }
+        }
+    }
+}
