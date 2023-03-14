d170f4ebd8be4f84603618328c126140f649c7dc
dev_kong <ahdjd5@gmail.com>
2023-03-14 20:53:47 +0900

    feat: king이 움직일 수 있는 좌표를 반환하는 기능 구현

--- src/main/java/domain/piece/type/King.java
+++ src/main/java/domain/piece/type/King.java
@@ -1,9 +1,16 @@
 package domain.piece.type;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import domain.piece.Camp;
+import domain.piece.Direction;
 import domain.piece.Piece;
 
 public class King extends Piece {
+    private static final List<Direction> movableDirection = Arrays.asList(Direction.values());
+
     public King(Camp camp) {
         super(camp);
     }
@@ -11,5 +18,25 @@ public class King extends Piece {
     @Override
     public boolean isKing() {
         return true;
+    }
+
+    @Override
+    public List<List<Integer>> fetchMovableCoordinate(List<Integer> currentCoordinate) {
+        Integer file = currentCoordinate.get(FILE);
+        Integer rank = currentCoordinate.get(RANK);
+
+        List<List<Integer>> movableCoordinate = new ArrayList<>();
+
+        for (Direction direction : movableDirection) {
+            int fileCoordinate = file + direction.getFile();
+            int rankCoordinate = rank + direction.getRank();
+
+            if (fileCoordinate < 0 || fileCoordinate > 7 || rankCoordinate < 0 || rankCoordinate > 7) {
+                continue;
+            }
+
+            movableCoordinate.add(List.of(fileCoordinate, rankCoordinate));
+        }
+        return movableCoordinate;
     }
 }
