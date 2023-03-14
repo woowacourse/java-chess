97b2bf8b601a4c213ef62cdce8a48e1fa41a2bcf
yoondgu <doyou2ing@naver.com>
2023-03-14 20:48:54 +0900

    feat: Pawn 의 이동 가능 목록 반환

--- src/main/java/chess/domain/Pawn.java
+++ src/main/java/chess/domain/Pawn.java
@@ -1,11 +1,74 @@
 package chess.domain;
 
-public class Pawn {
+import java.util.ArrayList;
+import java.util.List;
 
-    private final Position position;
+public class Pawn implements Movable {
 
-    public Pawn(final Position position) {
-        this.position = position;
+    private static final int FIRST_RANK = 2;
+
+    @Override
+    public List<Position> findMovablePositions(final Position position) {
+        int sourceX = position.getX();
+        int sourceY = position.getY();
+
+        if (isFirstMove(sourceX)) {
+            return findFirstMoveDestination(sourceX, sourceY);
+        }
+        return findMovePosition(sourceX, sourceY);
     }
 
+    private boolean isFirstMove(int sourceX) {
+        return sourceX == FIRST_RANK;
+    }
+
+    private List<Position> findFirstMoveDestination(final int sourceX, final int sourceY) {
+        List<Position> movablePositions = new ArrayList<>();
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                if (isFirstMovableDestination(sourceX, sourceY, i, j)) {
+                    movablePositions.add(new Position(i, j));
+                }
+            }
+        }
+        return movablePositions;
+    }
+
+    private boolean isFirstMovableDestination(final int sourceX, final int sourceY, final int destX, final int destY) {
+        return (destY == sourceY) && (destX - sourceX == 2 || destX - sourceX == 1);
+    }
+
+    private List<Position> findMovePosition(final int sourceX, final int sourceY) {
+        List<Position> movablePositions = new ArrayList<>();
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                if (isMovableDestination(sourceX, sourceY, i, j)) {
+                    movablePositions.add(new Position(i, j));
+                }
+            }
+        }
+        return movablePositions;
+    }
+
+
+    private boolean isMovableDestination(final int sourceX, final int sourceY, final int destX, final int destY) {
+        return isDestinationInRange(sourceX, sourceY, destX, destY);
+    }
+
+    private boolean isDestinationInRange(final int sourceX, final int sourceY, final int destX, final int destY) {
+        return isInForwardRange(sourceX, sourceY, destX, destY) || isInForwardDiagonalRange(sourceX, sourceY, destX,
+                destY);
+    }
+
+    private boolean isInForwardRange(final int sourceX, final int sourceY, final int destX, final int destY) {
+        return sourceY == destY && destX - sourceX == 1;
+    }
+
+    private boolean isInForwardDiagonalRange(final int sourceX, final int sourceY, final int destX, final int destY) {
+        return destX - sourceX == 1 && Math.abs(destY - sourceY) == 1;
+    }
+
+    private boolean isNotSource(final int sourceX, final int sourceY, final int destX, final int destY) {
+        return sourceX != destX || sourceY != destY;
+    }
 }
--- src/test/java/chess/domain/PawnTest.java
+++ src/test/java/chess/domain/PawnTest.java
@@ -1,16 +1,33 @@
 package chess.domain;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PawnTest {
+
+    @Test
+    @DisplayName("Pawn 은 처음에는 앞으로 1칸, 또는 2칸 이동 가능하다.")
+    void 초기_이동() {
+        Pawn pawn = new Pawn();
+
+        List<Position> movablePositions = pawn.findMovablePositions(new Position(2, 2));
+
+        assertThat(movablePositions).contains(new Position(3, 2), new Position(4, 2));
+        assertThat(movablePositions.size()).isEqualTo(2);
+    }
     
     @Test
-    @DisplayName("Pawn 은 위치를 가진다.")
-    void createPawnSuccess() {
-        assertDoesNotThrow(() -> new Pawn(new Position(1, 2)));
+    @DisplayName("Pawn 은 첫 이동이 아닌 경우, 앞으로 1칸 또는 공격의 경우만 이동 가능하다.")
+    void 이동_또는_공격() {
+        Pawn pawn = new Pawn();
+
+        List<Position> movablePositions = pawn.findMovablePositions(new Position(3, 2));
+
+        assertThat(movablePositions).contains(new Position(4, 2), new Position(4, 1), new Position(4, 3));
+        assertThat(movablePositions.size()).isEqualTo(3);
     }
 
 }
