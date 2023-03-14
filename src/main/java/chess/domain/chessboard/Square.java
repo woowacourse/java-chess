d038d2396885c3a83f26362f15b440f1789a8405
songusika <sws981202@gmail.com>
2023-03-14 17:31:14 +0900

    feat: 스퀘어가 말을 가지는 기능 구현
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/main/java/chess/domain/chessboard/Square.java
+++ src/main/java/chess/domain/chessboard/Square.java
@@ -1,19 +1,22 @@
 package chess.domain.chessboard;
 
 import chess.domain.chessboard.state.Empty;
+import chess.domain.chessboard.state.Piece;
 import chess.domain.chessboard.state.PieceState;
 
 public class Square {
     private final PieceState pieceState;
 
-    public Square(){
+    public Square() {
         pieceState = new Empty();
     }
 
-    public boolean isEmpty(){
-        return pieceState.isEmpty();
+    public Square(final Piece piece) {
+        pieceState = piece;
     }
 
-
+    public boolean isEmpty() {
+        return pieceState.isEmpty();
+    }
 
 }
