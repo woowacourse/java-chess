d34e66b94ba3e77a6e6ebfd2163281eb8ae8c311
dev_kong <ahdjd5@gmail.com>
2023-03-14 22:26:31 +0900

    feat: knight가 이동할 수 있는 square를 반환하는 기능 구현

--- src/main/java/domain/piece/type/Knight.java
+++ src/main/java/domain/piece/type/Knight.java
@@ -8,6 +8,17 @@ import domain.piece.Camp;
 import domain.piece.Piece;
 
 public class Knight extends Piece {
+    private static final List<List<Integer>> movableDirection = List.of(
+            List.of(1, 2),
+            List.of(1, -2),
+            List.of(-1, 2),
+            List.of(-1, -2),
+            List.of(2, 1),
+            List.of(2, -1),
+            List.of(-2, 1),
+            List.of(-2, -1)
+    );
+
     public Knight(Camp camp) {
         super(camp);
     }
@@ -19,6 +30,12 @@ public class Knight extends Piece {
 
     @Override
     public List<Square> fetchMovableCoordinate(Square currentSquare, Square targetSquare) {
+        List<Integer> coordinateGap = List.of(
+                currentSquare.toCoordinate().get(FILE) - targetSquare.toCoordinate().get(FILE),
+                currentSquare.toCoordinate().get(RANK) - targetSquare.toCoordinate().get(RANK));
+        if (movableDirection.contains(coordinateGap)) {
+            return List.of(targetSquare);
+        }
         return null;
     }
 }
