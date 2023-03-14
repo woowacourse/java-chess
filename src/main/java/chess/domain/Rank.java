a9907a39614581f65d1b04b7110d2e1ab882781f
dhkim <akdlzmf1123@gmail.com>
2023-03-14 16:49:29 +0900

    feat: 랭크간의 거리를 구하는 기능 추가

---
+++ src/main/java/chess/domain/Rank.java
@@ -0,0 +1,25 @@
+package chess.domain;
+
+public enum Rank {
+
+    ONE(1, "1"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8");
+
+    private final int position;
+    private final String symbol;
+
+    Rank(final int position, final String symbol) {
+        this.position = position;
+        this.symbol = symbol;
+    }
+
+    public int distanceTo(final Rank other) {
+        return Math.abs(position - other.position);
+    }
+}
---
+++ src/test/java/chess/domain/RankTest.java
@@ -0,0 +1,28 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Rank 클래스")
+class RankTest {
+
+    @Nested
+    @DisplayName("distanceTo 메서드는")
+    class distanceTo {
+        @Nested
+        @DisplayName("다른 Rank 객체가 주어지면")
+        class withAnotherRank {
+            final Rank from = Rank.FOUR;
+            final Rank other = Rank.EIGHT;
+
+            @Test
+            @DisplayName("두 랭크간의 거리를 계산한다")
+            void it_returns_distance() {
+                assertThat(from.distanceTo(other)).isEqualTo(4);
+            }
+        }
+    }
+}
