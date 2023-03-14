6c848916aac778d21cc80bc7292bfe6a31687ace
kevstevie <jianna442@gmail.com>
2023-03-14 18:04:51 +0900

    feat: File 좌우 이동 구현

--- src/main/java/chess/domain/board/File.java
+++ src/main/java/chess/domain/board/File.java
@@ -1,19 +1,36 @@
 package chess.domain.board;
 
-public enum File {
+import java.util.Arrays;
 
-    ONE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8);
+public enum File {
+    A(1),
+    B(2),
+    C(3),
+    D(4),
+    E(5),
+    F(6),
+    G(7),
+    H(8);
 
     private final int value;
 
     File(final int value) {
         this.value = value;
+    }
+
+    public File plus() {
+        int filePlused = this.value + 1;
+        return Arrays.stream(File.values())
+                .filter(file -> file.value == filePlused)
+                .findAny()
+                .orElseThrow(IllegalStateException::new);
+    }
+
+    public File minus() {
+        int fileMinused = this.value - 1;
+        return Arrays.stream(File.values())
+                .filter(file -> file.value == fileMinused)
+                .findAny()
+                .orElseThrow(IllegalStateException::new);
     }
 }
