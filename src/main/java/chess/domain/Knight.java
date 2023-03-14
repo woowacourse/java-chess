2c0b98f7d9f228c45390826f48daab9b4caa2902
dhkim <akdlzmf1123@gmail.com>
2023-03-14 17:34:31 +0900

    feat: Knight 클래스 추가
    - 각 진영별로 2개 생성 제한

---
+++ src/main/java/chess/domain/Knight.java
@@ -0,0 +1,33 @@
+package chess.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Knight {
+    private static final List<Knight> blackKnights = new ArrayList<>();
+    private static final List<Knight> whiteKnights = new ArrayList<>();
+
+    static {
+        addKnights(blackKnights, Team.BLACK);
+        addKnights(whiteKnights, Team.WHITE);
+    }
+
+    private final Team team;
+
+    private Knight(final Team team) {
+        this.team = team;
+    }
+
+    private static void addKnights(final List<Knight> Knights, final Team team) {
+        for (int i = 0; i < 2; i++) {
+            Knights.add(new Knight(team));
+        }
+    }
+
+    public static List<Knight> of(final Team team) {
+        if (team == Team.BLACK) {
+            return List.copyOf(blackKnights);
+        }
+        return List.copyOf(whiteKnights);
+    }
+}
---
+++ src/test/java/chess/domain/KnightTest.java
@@ -0,0 +1,25 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Knight 클래스")
+public class KnightTest {
+
+    @Nested
+    @DisplayName("of 메서드는")
+    class of {
+        @Nested
+        @DisplayName("진영이 주어지면")
+        class givenTeam {
+            @Test
+            @DisplayName("해당 진영의 Knight를 8개 생성한다")
+            void it_returns_knights() {
+                assertThat(Knight.of(Team.BLACK)).hasSize(2);
+            }
+        }
+    }
+}
