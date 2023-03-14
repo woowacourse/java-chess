30dfe4166ce39d832a026d98b841b76564110986
woosung1223 <dntjd991223@naver.com>
2023-03-14 17:25:15 +0900

    feat: 체스판은 랭크들을 초기화할 수 있다

---
+++ src/main/java/domain/Board.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    public static final int RANK_ROW_COUNT = 8;
+    public static final int TOTAL_COL_COUNT = 8;
+    private final List<Rank> ranks;
+
+    public Board() {
+        ranks = new ArrayList<>();
+        for (int row = 0; row < RANK_ROW_COUNT; row++) {
+            ranks.add(new Rank(row, TOTAL_COL_COUNT));
+        }
+    }
+}
