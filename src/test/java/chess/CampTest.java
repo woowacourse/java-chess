dc8144824d550b3770a12eb12a03d2d33d94ce93
jiwon <ljwon77@daum.net>
2023-03-14 19:10:40 +0900

    feat: 입력받은 위치 정보를 검증하고, 진영을 나눈다
    Co-authored-by: yenawee <yaena0319@naver.com>

---
+++ src/test/java/chess/CampTest.java
@@ -0,0 +1,33 @@
+package chess;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class CampTest {
+
+    @ParameterizedTest(name = "잘못된 길이의 위치 정보가 주어지면, 예외가 발생한다.")
+    @ValueSource(strings = {"", "a", "a23", "a234"})
+    void createLengthFail(final String input) {
+        assertThatThrownBy(() -> Camp.create(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 입력입니다.");
+    }
+
+    @ParameterizedTest(name = "잘못된 열의 위치 정보가 주어지면, 예외가 발생한다.")
+    @ValueSource(strings = {"i1", "i2", "I1", "I2", "00"})
+    void createColumnRangeFail(final String input) {
+        assertThatThrownBy(() -> Camp.create(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이동 위치 열은 a~h 사이여야 합니다.");
+    }
+
+    @ParameterizedTest(name = "잘못된 행의 위치 정보가 주어지면, 예외가 발생한다.")
+    @ValueSource(strings = {"a0", "a9"})
+    void createRowRangeFail(final String input) {
+        assertThatThrownBy(() -> Camp.create(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이동 위치 행은 1~8 사이여야 합니다.");
+    }
+}
