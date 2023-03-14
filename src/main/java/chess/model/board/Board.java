9e843138b2dbdcfa3704006909b1b932fca9b060
apptie <appti@naver.com>
2023-03-14 20:17:21 +0900

    feat: 체스판 초기화 기능 구현

--- src/main/java/chess/model/board/Board.java
+++ src/main/java/chess/model/board/Board.java
@@ -1,12 +1,31 @@
 package chess.model.board;
 
-import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.toUnmodifiableList;
+import static chess.model.piece.Color.BLACK;
+import static chess.model.piece.Color.WHITE;
+import static chess.model.piece.PieceType.BISHOP;
+import static chess.model.piece.PieceType.KING;
+import static chess.model.piece.PieceType.KNIGHT;
+import static chess.model.piece.PieceType.PAWN;
+import static chess.model.piece.PieceType.QUEEN;
+import static chess.model.piece.PieceType.ROOK;
 
-import java.util.ArrayList;
+import chess.model.piece.Color;
+import chess.model.piece.Piece;
+import chess.model.piece.PieceType;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Board {
+
+
+    private static final List<PieceType> INITIAL_BACK_PIECES = List.of(ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP,
+            KNIGHT, ROOK);
+    private static final List<PieceType> INITIAL_FRONT_PIECES = List.of(PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN,
+            PAWN);
+    private static final int WHITE_BACK_START_POSITION = 0;
+    private static final int WHITE_FRONT_START_POSITION = 8;
+    private static final int BLACK_FRONT_START_POSITION = 48;
+    private static final int BLACK_BACK_START_POSITION = 56;
 
     private final List<Square> squares;
 
@@ -17,11 +36,13 @@ public class Board {
     public static Board create() {
         final List<Square> squares = createSquares();
 
+        initializePieces(squares);
+
         return new Board(squares);
     }
 
     private static List<Square> createSquares() {
-        final List<Square> squares = new ArrayList<>();
+        final List<Square> squares = new LinkedList<>();
 
         for (File file : File.values()) {
             createSquare(squares, file);
@@ -34,6 +55,33 @@ public class Board {
         for (Rank rank : Rank.values()) {
             final Position position = new Position(file, rank);
             squares.add(new EmptySquare(position));
+        }
+    }
+
+    private static void initializePieces(final List<Square> squares) {
+        initializeBackPieces(squares, WHITE, WHITE_BACK_START_POSITION);
+        initializeFrontPieces(squares, WHITE, WHITE_FRONT_START_POSITION);
+        initializeFrontPieces(squares, BLACK, BLACK_FRONT_START_POSITION);
+        initializeBackPieces(squares, BLACK, BLACK_BACK_START_POSITION);
+    }
+
+    private static void initializeBackPieces(final List<Square> squares, final Color color, int position) {
+        for (PieceType pieceType : INITIAL_BACK_PIECES) {
+            final Square square = squares.get(position);
+            final Piece piece = new Piece(color, pieceType);
+
+            squares.remove(position);
+            squares.add(position++, square.receivePiece(piece));
+        }
+    }
+
+    private static void initializeFrontPieces(final List<Square> squares, final Color color, int position) {
+        for (PieceType pieceType : INITIAL_FRONT_PIECES) {
+            final Square square = squares.get(position);
+            final Piece piece = new Piece(color, pieceType);
+
+            squares.remove(position);
+            squares.add(position++, square.receivePiece(piece));
         }
     }
 
--- src/test/java/chess/model/board/BoardTest.java
+++ src/test/java/chess/model/board/BoardTest.java
@@ -1,9 +1,18 @@
 package chess.model.board;
 
+import static chess.model.piece.PieceType.BISHOP;
+import static chess.model.piece.PieceType.KING;
+import static chess.model.piece.PieceType.KNIGHT;
+import static chess.model.piece.PieceType.PAWN;
+import static chess.model.piece.PieceType.QUEEN;
+import static chess.model.piece.PieceType.ROOK;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import chess.model.Type;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,5 +30,43 @@ class BoardTest {
                 .extracting("squares", InstanceOfAssertFactories.list(Square.class))
                 .hasSize(64)
         );
+    }
+
+    /**
+     * rank = 8 RNBQKBNR
+     * rank = 7 PPPPPPPP
+     * rank = 2 pppppppp
+     * rank = 1 rnbqkbnr
+     */
+    @Test
+    @DisplayName("체스판에 정상적으로 기물이 생성 되었는지 확인한다.")
+    void constructor_whenCall_thenReturnPieces() {
+        //given
+        final Board board = Board.create();
+        final List<Square> squares = board.getSquares();
+
+        //when
+        final List<Type> rankOne = getRank(squares, 0, 8);
+        final List<Type> rankTwo = getRank(squares, 8, 16);
+        final List<Type> rankSeven = getRank(squares, 48, 56);
+        final List<Type> rankEight = getRank(squares, 56, 64);
+
+        //then
+        assertAll(
+            () -> assertThat(rankOne).containsExactly(ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP,
+                KNIGHT, ROOK),
+            () -> assertThat(rankTwo).containsExactly(PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN,
+                PAWN),
+            () -> assertThat(rankSeven).containsExactly(PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN,
+                PAWN),
+            () -> assertThat(rankEight).containsExactly(ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP,
+                KNIGHT, ROOK)
+        );
+    }
+
+    private List<Type> getRank(final List<Square> squares, final int from, final int to) {
+        return squares.subList(from, to).stream()
+            .map(Square::getType)
+            .collect(Collectors.toList());
     }
 }
