3199c89488ccb3b9cc187cb1b17c33a3fbc6cfbb
woosung1223 <dntjd991223@naver.com>
2023-03-14 17:11:17 +0900

    feat: 말을 초기화할 수 있다

--- src/main/java/domain/Square.java
+++ src/main/java/domain/Square.java
@@ -4,12 +4,8 @@ public class Square {
 
     private final Piece piece;
 
-    public Square() {
-        this.piece = null;
-    }
-
-    public Square(Piece piece) {
-        this.piece = piece;
+    public Square(int row, int col) {
+        this.piece = BoardInitialImage.getPieceByCoordinate(row, col);
     }
 
     public Piece getPiece() {
