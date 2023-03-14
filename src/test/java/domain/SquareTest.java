e0495359b7cc0c90da301b953c47897b70dd8f71
ChoiYoungHoon <dudgns2865@naver.com>
2023-03-14 17:54:26 +0900

    test: Square 테스트 작성

---
+++ src/test/java/domain/SquareTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SquareTest {
+
+    @Test
+    @DisplayName("빈 칸 생성을 테스트")
+    public void testEmpty() {
+        //given
+        //when
+        final Square square = Square.empty();
+
+        //then
+        assertThat(square).extracting("chessPiece").isEqualTo(null);
+    }
+}
