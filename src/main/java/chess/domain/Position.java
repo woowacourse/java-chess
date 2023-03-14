a0836752132074f54e28d3f18c3f20784822244d
seokjin8678 <seokjin8678@gmail.com>
2023-03-14 20:24:31 +0900

    feat: Position 검증 메서드 추가
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Position {
     private static final Map<Integer, Position> CACHE = new ConcurrentHashMap<>(64);
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 7;
 
     private final int x;
     private final int y;
@@ -16,7 +18,14 @@ public class Position {
     }
 
     public static Position of(int x, int y) {
+        if (validateRange(x) || validateRange(y)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 위치 값 입니다.");
+        }
         return CACHE.computeIfAbsent(Objects.hash(x, y), ignore -> new Position(x, y));
+    }
+
+    private static boolean validateRange(int range) {
+        return range < MIN_RANGE || range > MAX_RANGE;
     }
 
     public int getX() {
--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -2,8 +2,11 @@ package chess.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PositionTest {
 
@@ -18,5 +21,15 @@ class PositionTest {
                 .isEqualTo(4);
         assertThat(position.getY())
                 .isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-1,-1", "8,8", "-1,3", "3,-1"})
+    @DisplayName("잘못된 위치가 주어지면 예외가 발생해야 한다.")
+    void create_Fail(int x, int y) {
+        // expect
+        Assertions.assertThatThrownBy(() -> Position.of(x, y))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 잘못된 위치 값 입니다.");
     }
 }
