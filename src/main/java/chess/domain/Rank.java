8e2c763922a08c664618aae6dd5e61ae24361267
jeonghoon <shb03323@naver.com>
2023-03-14 17:18:31 +0900

    feat: Rank Enum 생성

---
+++ src/main/java/chess/domain/Rank.java
@@ -0,0 +1,28 @@
+package chess.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    ONE('1'),
+    TWO('2'),
+    THREE('3'),
+    FOUR('4'),
+    FIVE('5'),
+    SIX('6'),
+    SEVEN('7'),
+    EIGHT('8');
+
+    private final char value;
+
+    Rank(char value) {
+        this.value = value;
+    }
+
+    public static Rank from(char value) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.value == value)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 Rank입니다."));
+    }
+}
---
+++ src/test/java/chess/domain/RankTest.java
@@ -0,0 +1,24 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RankTest {
+
+    @Test
+    @DisplayName("1~8이 아닌 Rank를 검색하면 예외가 발생한다")
+    void search_rank_exception() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Rank.from('9'));
+    }
+
+    @Test
+    @DisplayName("1~8인 Rank를 검색하면 Enum 필드를 반환한다.")
+    void search_rank() {
+        assertThat(Rank.from('1')).isEqualTo(Rank.ONE);
+        assertThat(Rank.from('8')).isEqualTo(Rank.EIGHT);
+    }
+}
