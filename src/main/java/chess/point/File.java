77907b5f2ead9ae51d65e512614ed3967af8685b
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-14 17:33:49 +0900

    feat: 보드의 File, Rank 추가

---
+++ src/main/java/chess/point/File.java
@@ -0,0 +1,13 @@
+package chess.point;
+
+public enum File {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    ;
+}
---
+++ src/main/java/chess/point/Rank.java
@@ -0,0 +1,13 @@
+package chess.point;
+
+public enum Rank {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    ;
+}
