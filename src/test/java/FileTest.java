d545ada0f3f518e6b18be401f3ef23d687e320e4
Youngho <cyh6099@gmail.com>
2023-03-14 19:48:38 +0900

    refactor: Rank와 File 이름 서로 변경

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class FileTest {
+public class FileTest {
 
     @DisplayName("text에 맞는 File 인스턴스를 반환한다.")
     @Test
     void shouldReturnAppropriateFileWhenIncomeText() {
-        File file = File.from("1");
-        assertThat(file).isEqualTo(File.ONE);
+        File file = File.from("a");
+        assertThat(file).isEqualTo(File.A);
     }
 
     @DisplayName("유효하지 않은 File 텍스트 입력 시 예외가 발생한다.")
     @Test
     void shouldThrowExceptionWhenInvalidFileText() {
-        assertThatThrownBy(() -> File.from("0"))
+        assertThatThrownBy(() -> File.from("q"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("존재하지 않는 file입니다.");
     }
--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,12 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.text.Position;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class PawnTest {
 
     @DisplayName("위로 한 칸 이동하면, 이동 경로는 위치를 0개 가진다.")
@@ -14,8 +8,8 @@ public class PawnTest {
     void shouldHasNoPositionWhenGetPath() {
         Pawn pawn = new Pawn();
 
-        List<Position> path = pawn.getPath(new Position("b", "2"), new Position("b", "3"));
+//        List<Position> path = pawn.getPath(Position.of("b", "2"), Position.of("b", "3"));
 
-        assertThat(path).hasSize(0);
+//        assertThat(path).hasSize(0);
     }
 }
