0ee7958cb1447b2d0cfcf3e4ee719d65bcc96ea2
yujamint <woawoa0513@gmail.com>
2023-03-14 15:40:03 +0900

    test(PositionTest): Position 이동 기능 테스트

--- src/test/java/chess/piece/board/PositionTest.java
+++ src/test/java/chess/piece/board/PositionTest.java
@@ -1,5 +1,7 @@
 package chess.piece.board;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import chess.board.File;
 import chess.board.Position;
 import chess.board.Rank;
@@ -12,6 +14,21 @@ public class PositionTest {
     @Test
     @DisplayName("File과 Rank를 받아서 Position을 생성한다.")
     void generatePosition() {
+        // when, then
         Assertions.assertDoesNotThrow(() -> new Position(File.A, Rank.ONE));
+    }
+
+    @Test
+    @DisplayName("받은 File과 Rank로 이동한다.")
+    void move() {
+        // given
+        final Position position = new Position(File.A, Rank.ONE);
+
+        // when
+        position.move(File.F, Rank.EIGHT);
+
+        // then
+        assertThat(position.getFile()).isEqualTo(File.F);
+        assertThat(position.getRank()).isEqualTo(Rank.EIGHT);
     }
 }
