1b892e252c8d7386588e998e557ab7fcccacf492
kokodak <kokodakadokok@gmail.com>
2023-03-14 16:33:49 +0900

    feat: JDBC 기능 구현

---
+++ src/test/java/database/user/UserDaoTest.java
@@ -0,0 +1,38 @@
+package database.user;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class UserDaoTest {
+
+    private final UserDao userDao = new UserDao();
+
+    @Test
+    public void connection() throws SQLException {
+        try (final var connection = userDao.getConnection()) {
+            assertThat(connection).isNotNull();
+        }
+    }
+
+    @Test
+    public void addUser() {
+        final var user = new User("testUserId", "testUser");
+        userDao.addUser(user);
+    }
+
+    @Test
+    public void findByUserId() {
+        final var user = userDao.findByUserId("testUserId");
+
+        assertThat(user).isEqualTo(new User("testUserId", "testUser"));
+    }
+
+    @Test
+    public void update() throws SQLException {
+        userDao.update("pobiconan", "hello");
+
+    }
+}
