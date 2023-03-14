f84b2706031723c4409c276a5f81237e4afc3352
dev_kong <ahdjd5@gmail.com>
2023-03-14 18:03:17 +0900

    feat: square 구현

---
+++ src/main/java/domain/board/File.java
@@ -0,0 +1,36 @@
+package domain.board;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum File {
+    A('a'),
+    B('b'),
+    C('c'),
+    D('d'),
+    E('e'),
+    F('f'),
+    G('g'),
+    H('h');
+
+    private final char value;
+
+    File(char value) {
+        this.value = value;
+    }
+
+    public File convertToOpposite() {
+        List<File> values = List.of(File.values());
+
+        int index = values.indexOf(this);
+
+        return values.get(values.size() - index);
+    }
+
+    public static File findFile(char value) {
+        return Arrays.stream(File.values())
+                .filter(file -> file.value == value)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("af"));
+    }
+}
---
+++ src/main/java/domain/board/Rank.java
@@ -0,0 +1,27 @@
+package domain.board;
+
+import java.util.Arrays;
+
+public enum Rank {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8);
+
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public static Rank findRank(int value) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.value == value)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("af"));
+    }
+}
