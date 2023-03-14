70d180611a1d0479c9dc7584aaf62ac4e3e4a5a8
jiwon <ljwon77@daum.net>
2023-03-14 18:07:23 +0900

    feat: 체스판 목표 위치 반환 기능을 구현한다
    Co-authored-by: yenawee <yaena0319@naver.com>

---
+++ src/main/java/chess/Position.java
@@ -0,0 +1,30 @@
+package chess;
+
+import java.util.Objects;
+
+public class Position {
+    private final int column;
+    private final int row;
+
+    public Position(final int column, final int row) {
+        this.column = column;
+        this.row = row;
+    }
+
+    public Position calculate(final int columnMove, final int rowMove) {
+        return new Position(this.column + columnMove, this.row + rowMove);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Position position = (Position) o;
+        return column == position.column && row == position.row;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column, row);
+    }
+}
---
+++ src/test/java/chess/PositionTest.java
@@ -0,0 +1,23 @@
+package chess;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("이동 거리와 방향을 주었을 때 목표 지점의 위치를 반환한다.")
+    void calculate() {
+        // given
+        Position position = new Position(0, 0);
+
+        // when
+        Position targetPosition = position.calculate(3, -3);
+
+        // then
+        assertThat(targetPosition)
+                .isEqualTo(new Position(3, -3));
+    }
+}
