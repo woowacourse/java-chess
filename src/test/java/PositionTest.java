540c5816ef942e00f6a32a1be38174934d5d8130
Youngho <cyh6099@gmail.com>
2023-03-14 20:03:48 +0900

    test(Position): Source와 Target 간 Increment 계산하는 로직 테스트

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,16 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    @DisplayName("source position과 target position간 거리차이를 반환한다.")
+    @Test
+    void shouldReturnIncrementBetweenSourceAndTargetWhenInputTargetToSource() {
+        Position sourcePosition = Position.of("a", "1");
+        Position targetPosition = Position.of("d", "6");
+
+        Assertions.assertThat(sourcePosition.calculateIncrement(targetPosition)).isEqualTo(new Increment(3, 5));
+    }
+
+}
