090d02703ee43d75d2785234e026fa930f864c39
hum02 <ehdgma02@gmail.com>
2023-03-14 17:00:40 +0900

    test: 위치 대각선상 비교 로직 테스트

--- src/test/java/domain/PositionTest.java
+++ src/test/java/domain/PositionTest.java
@@ -12,6 +12,7 @@ public class PositionTest {
 
     public static final Position POSITION_A1 = Position.of(File.FILE_A, Rank.RANK1);
     public static final Position POSITION_B2 = Position.of(File.FILE_B, Rank.RANK2);
+    public static final Position POSITION_B1 = Position.of(File.FILE_B, Rank.RANK1);
 
     @Test
     void 위치_생성_성공() {
@@ -34,5 +35,17 @@ public class PositionTest {
 
         //then
         assertThat(isStraightSame).isFalse();
+    }
+
+    @Test
+    void 두_위치가_대각선상에서_동일한지_검사한다() {
+        Position position1 = POSITION_A1;
+        Position position2 = POSITION_B1;
+
+        //when
+        boolean isDiagonalSame = position1.isDiagonalEqual(position2);
+
+        //then
+        assertThat(isDiagonalSame).isFalse();
     }
 }
