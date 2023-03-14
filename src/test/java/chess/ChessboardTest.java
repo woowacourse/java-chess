e273017fea181342656d4d3a422a2714ad74ef0a
waterricecake <fruturum@nate.com>
2023-03-14 20:14:35 +0900

    test(ChessboardTest): 체스보드 생성 테스트 구현

---
+++ src/test/java/chess/ChessboardTest.java
@@ -0,0 +1,18 @@
+package chess;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class ChessboardTest {
+    @DisplayName("체스판은 64개의 Square로 이루어진다.")
+    @Test
+    void createChessboardSuccessTest(){
+        Assertions.assertThat(new Chessboard())
+                .extracting("board")
+                .asInstanceOf(InstanceOfAssertFactories.map(Square.class, Piece.class))
+                .hasSize(64);
+    }
+}
