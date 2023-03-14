53feab2329264aa15ec634c45f7ebc2a2c37b2f4
kokodak <kokodakadokok@gmail.com>
2023-03-14 17:24:40 +0900

    feat: 폰 도메인 생성
    Co-authored-by: parkmuhyeun <pjhg410@gmail.com>

--- src/main/java/chess/domain/Side.java
+++ src/main/java/chess/domain/Side.java
@@ -4,4 +4,12 @@ public enum Side {
 
     BLACK,
     WHITE;
+
+    public boolean isWhite() {
+        return this == WHITE;
+    }
+
+    public boolean isBlack() {
+        return this == BLACK;
+    }
 }
