29375d5816dcb82735c8554e9c398623b45cca69
seokjin8678 <seokjin8678@gmail.com>
2023-03-15 20:33:47 +0900

    feat: Rook 이동 메서드 구현
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -1,5 +1,6 @@
 package chess.domain.piece;
 
+import chess.domain.Position;
 import chess.domain.Role;
 import chess.domain.Team;
 
@@ -7,5 +8,10 @@ public class Rook extends Piece {
 
     public Rook(Team team) {
         super(Role.ROOK, team);
+    }
+
+    @Override
+    public boolean canMove(Position source, Position target) {
+        return canMoveCross(source, target);
     }
 }
