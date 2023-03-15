0449ff95849cf06da630dbf043aaf80d7f295a09
drunkenhw <drunkenhw@gmail.com>
2023-03-15 20:00:11 +0900

    feat: Knigth가 이동할 수 있는지 확인하는 기능 추가

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -10,6 +10,17 @@ public class Knight extends Piece {
 
     @Override
     boolean canMove(Position sourcePosition, Position targetPosition) {
+        int sourceColumnNumber = sourcePosition.getFileCoordinate().getColumnNumber();
+        int targetColumnNumber = targetPosition.getFileCoordinate().getColumnNumber();
+        int sourceRankNumber = sourcePosition.getRankCoordinate().getRowNumber();
+        int targetRankNumber = targetPosition.getRankCoordinate().getRowNumber();
+
+        if (Math.abs(sourceColumnNumber - targetColumnNumber) == 2) {
+            return Math.abs(sourceRankNumber - targetRankNumber) == 1;
+        }
+        if (Math.abs(sourceColumnNumber - targetColumnNumber) == 1) {
+            return Math.abs(sourceRankNumber - targetRankNumber) == 2;
+        }
         return false;
     }
 
