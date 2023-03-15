6c8ba171e0562d3a30f1efc0939bf540d9e95cfc
hyunseo <ohseo8813@gmail.com>
2023-03-15 15:34:58 +0900

    refactor: 퀸이 이동가능한 경로를 반환하도록 변경

--- src/main/java/techcourse/fp/chess/domain/Queen.java
+++ src/main/java/techcourse/fp/chess/domain/Queen.java
@@ -1,42 +1,55 @@
 package techcourse.fp.chess.domain;
-//
-//public class Queen extends Piece {
-//
-//    public Queen(final Side side) {
-//        super(side);
-//    }
-//
-//    @Override
-//    public boolean isMovable(final Position sourcePosition, final Position targetPosition) {
-//        if (sourcePosition.equals(targetPosition)) {
-//            return false;
-//        }
-//
-//        return isMovableHorizontalAndVertical(sourcePosition, targetPosition) || isMovableDiagonal(sourcePosition, targetPosition);
-//    }
-//
-//    private boolean isMovableHorizontalAndVertical(final Position sourcePosition, final Position targetPosition) {
-//        return isSameFileOrder(sourcePosition, targetPosition) || isSameRankOrder(sourcePosition, targetPosition);
-//    }
-//
-//    private boolean isSameRankOrder(final Position sourcePosition, final Position targetPosition) {
-//        return sourcePosition.getRankOrder() == targetPosition.getRankOrder();
-//    }
-//
-//    private boolean isSameFileOrder(final Position sourcePosition, final Position targetPosition) {
-//        return sourcePosition.getFileOrder() == targetPosition.getFileOrder();
-//    }
-//
-//    private boolean isMovableDiagonal(final Position sourcePosition, final Position targetPosition) {
-//        return calculateFileDifference(sourcePosition, targetPosition) ==
-//                calculateRankDifference(sourcePosition, targetPosition);
-//    }
-//
-//    private int calculateFileDifference(final Position sourcePosition, final Position targetPosition) {
-//        return Math.abs(sourcePosition.getFileOrder() - targetPosition.getFileOrder());
-//    }
-//
-//    private int calculateRankDifference(final Position sourcePosition, final Position targetPosition) {
-//        return Math.abs(sourcePosition.getRankOrder() - targetPosition.getRankOrder());
-//    }
-//}
+
+import java.util.ArrayList;
+import java.util.List;
+import techcourse.fp.movingStrategy.DownStrategy;
+import techcourse.fp.movingStrategy.LeftDownStrategy;
+import techcourse.fp.movingStrategy.LeftStrategy;
+import techcourse.fp.movingStrategy.LeftUpStrategy;
+import techcourse.fp.movingStrategy.MovingStrategies;
+import techcourse.fp.movingStrategy.MovingStrategy;
+import techcourse.fp.movingStrategy.RightDownStrategy;
+import techcourse.fp.movingStrategy.RightStrategy;
+import techcourse.fp.movingStrategy.RightUpStrategy;
+import techcourse.fp.movingStrategy.UpStrategy;
+
+public class Queen extends Piece {
+
+    {
+        final List<MovingStrategy> rawStrategies = List.of(
+                new RightUpStrategy(), new RightDownStrategy(),
+                new LeftDownStrategy(), new LeftUpStrategy(),
+                new UpStrategy(), new DownStrategy(),
+                new LeftStrategy(), new RightStrategy());
+        strategies = new MovingStrategies(rawStrategies);
+    }
+
+    public Queen(final Color color) {
+        super(color);
+    }
+
+    @Override
+    public List<Position> findPath(final Position sourcePosition, final Position targetPosition,
+                                   final Color targetColor) {
+        final MovingStrategy movingStrategy = strategies.findStrategy(sourcePosition, targetPosition);
+
+        if (targetColor.isSameColor(this.color)) {
+            throw new IllegalStateException("아군의 기물이 존재하는 곳으로는 이동할 수 없습니다.");
+        }
+
+        return createPath(sourcePosition, targetPosition, movingStrategy);
+    }
+
+    private List<Position> createPath(final Position sourcePosition, final Position targetPosition,
+                                      final MovingStrategy movingStrategy) {
+        List<Position> path = new ArrayList<>();
+        Position currentPosition = sourcePosition;
+
+        do {
+            currentPosition = movingStrategy.move(currentPosition);
+            path.add(currentPosition);
+        } while (!currentPosition.equals(targetPosition));
+
+        return path;
+    }
+}
