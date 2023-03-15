09db9ccffa9e7174a602d81388f375b22345d132
woo-chang <clllickme@naver.com>
2023-03-15 21:43:55 +0900

    feat: 비숍이 움직이는 경로 구하는 기능 구현
    Co-authored-by: swonny <wonny921@gmail.com>

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -1,8 +1,44 @@
 package chess.domain.piece;
 
+import static chess.domain.piece.DirectionVector.NORTHEAST;
+import static chess.domain.piece.DirectionVector.NORTHWEST;
+import static chess.domain.piece.DirectionVector.SOUTHEAST;
+import static chess.domain.piece.DirectionVector.SOUTHWEST;
+
+import chess.domain.board.Square;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bishop extends Piece {
+
+    private static final List<DirectionVector> directions = List.of(NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST);
 
     public Bishop(final Color color) {
         super(color);
+    }
+
+    @Override
+    public List<Square> findRoute(final Square source, final Square destination) {
+        final DirectionVector directionVector = findDirectionVector(source, destination);
+        return generateRoute(directionVector, source, destination);
+    }
+
+    private DirectionVector findDirectionVector(final Square source, final Square destination) {
+        final int distanceX = destination.calculateDistanceX(source);
+        final int distanceY = destination.calculateDistanceY(source);
+        return directions.stream()
+                .filter(directions -> directions.isOnMyWay(distanceX, distanceY))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 위치로 이동할 수 없습니다."));
+    }
+
+    private List<Square> generateRoute(final DirectionVector direction, final Square source, final Square destination) {
+        final List<Square> route = new ArrayList<>();
+        Square currentSquare = source;
+        while (!currentSquare.equals(destination)) {
+            currentSquare = currentSquare.next(direction);
+            route.add(currentSquare);
+        }
+        return route;
     }
 }
