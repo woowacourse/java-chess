4ea7f72b6bb4cac9060c40775dfbcf259a74dcb1
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-03-15 19:35:06 +0900

    Feat(View): 게임 시작 종료 메세지 및 게임 이동 출력 구현
    Co-authored-by: wugawuga <dnr3655@naver.com>

--- src/main/java/chess/ui/OutputView.java
+++ src/main/java/chess/ui/OutputView.java
@@ -10,13 +10,17 @@ import static chess.domain.Pieces.LAST_RANK;
 
 public final class OutputView {
 
-    private static final String START_CHESS_GAME = "체스 게임을 시작합니다.";
-    private static final String INPUT_GAME_START_COMMAND = "게임 시작은 start, 종료는 end 명령을 입력하세요.";
+    private static final String START_CHESS_GAME = "> 체스 게임을 시작합니다.";
+    private static final String INPUT_GAME_START_COMMAND = "> 게임 시작 : start";
+    private static final String INPUT_GAME_END_COMMAND = "> 게임 종료 : end";
+    private static final String GAME_MOVE_MESSAGE = "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
     private static final String EMPTY_POSITION = ".";
 
     public static void printStartGame() {
         System.out.println(START_CHESS_GAME);
         System.out.println(INPUT_GAME_START_COMMAND);
+        System.out.println(INPUT_GAME_END_COMMAND);
+        System.out.println(GAME_MOVE_MESSAGE);
     }
 
     public static void printInitializedChessBoard(PiecesResponse piecesResponse) {
