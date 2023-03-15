44335df890c53233e768b29721cb5624cd772bfd
shin-mallang <huipulci1@naver.com>
2023-03-15 21:11:21 +0900

    feat: Rook 의 이동 조건 탐색 기능 구현

--- src/main/java/chess/domain/piece/type/Rook.java
+++ src/main/java/chess/domain/piece/type/Rook.java
@@ -2,11 +2,22 @@ package chess.domain.piece.type;
 
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import chess.domain.piece.position.WayPointsWithCondition;
+import chess.domain.piece.position.Path;
 import chess.domain.piece.position.PiecePosition;
 
 public class Rook extends Piece {
 
     public Rook(final Color color, final PiecePosition piecePosition) {
         super(color, piecePosition);
+    }
+
+    @Override
+    protected WayPointsWithCondition wayPointsWithCondition(final Path path) {
+        if (path.isStraight()) {
+            return WayPointsWithCondition.possible(path.wayPoints());
+        }
+        return WayPointsWithCondition.impossible();
+
     }
 }
---
+++ src/test/java/chess/domain/piece/type/RookTest.java
@@ -0,0 +1,82 @@
+package chess.domain.piece.type;
+
+import chess.domain.piece.Color;
+import chess.domain.piece.position.Condition;
+import chess.domain.piece.position.WayPointsWithCondition;
+import chess.domain.piece.position.PiecePosition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayName("Rook 은")
+class RookTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            // 기준 e, 4
+            "f,4",  // 동
+            "d,4",  // 서
+            "e,5",  // 북
+            "e,3",  // 남
+    })
+    void 동서남북_방향으로_한칸은_무조건_이동_가능하다(final char file, final int rank) {
+        // given
+        final PiecePosition currentPosition = PiecePosition.of(4, 'e');
+        final PiecePosition destination = PiecePosition.of(rank, file);
+        final Rook rook = new Rook(Color.WHITE, currentPosition);
+
+        // when & then
+        final WayPointsWithCondition condition = rook.wayPointsWithCondition(destination);
+        assertThat(condition.condition()).isEqualTo(Condition.POSSIBLE);
+        assertThat(condition.wayPoints()).isEmpty();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            // 기준 e, 4
+            "g,4",  // 동
+            "c,4",  // 서
+            "e,6",  // 북
+            "e,2",  // 남
+            "h,4",  // 동
+            "a,4",  // 서
+            "e,8",  // 북
+            "e,1",  // 남
+    })
+    void 동서남북_방향으로_두칸_이상_움직이려면_경로에_피스가_없어야한다(final char file, final int rank) {
+        // given
+        final PiecePosition currentPosition = PiecePosition.of(4, 'e');
+        final PiecePosition destination = PiecePosition.of(rank, file);
+        final Rook rook = new Rook(Color.WHITE, currentPosition);
+
+        // when & then
+        final WayPointsWithCondition condition = rook.wayPointsWithCondition(destination);
+        assertThat(condition.condition()).isEqualTo(Condition.POSSIBLE);
+        assertThat(condition.wayPoints()).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            // 기준 e, 4
+            "f,5",  // 북동
+            "f,3",  // 남동
+            "d,5",  // 북서
+            "d,3"   // 남서
+    })
+    void 대각선_방향으로_이동할_수_없다(final char file, final int rank) {
+        // given
+        final PiecePosition currentPosition = PiecePosition.of(4, 'e');
+        final PiecePosition destination = PiecePosition.of(rank, file);
+        final Rook rook = new Rook(Color.WHITE, currentPosition);
+
+        // when & then
+        final WayPointsWithCondition condition = rook.wayPointsWithCondition(destination);
+        assertThat(condition.condition()).isEqualTo(Condition.IMPOSSIBLE);
+    }
+}
