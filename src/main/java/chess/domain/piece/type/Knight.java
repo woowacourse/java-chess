585e2092f30c7ffc9ead79f034445b135e75201a
shin-mallang <huipulci1@naver.com>
2023-03-15 21:11:49 +0900

    feat: Knight 의 이동 조건 탐색 기능 구현

--- src/main/java/chess/domain/piece/type/Knight.java
+++ src/main/java/chess/domain/piece/type/Knight.java
@@ -2,11 +2,26 @@ package chess.domain.piece.type;
 
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import chess.domain.piece.position.WayPointsWithCondition;
+import chess.domain.piece.position.Path;
 import chess.domain.piece.position.PiecePosition;
+
+import java.util.Collections;
 
 public class Knight extends Piece {
 
     public Knight(final Color color, final PiecePosition piecePosition) {
         super(color, piecePosition);
+    }
+
+    @Override
+    protected WayPointsWithCondition wayPointsWithCondition(final Path path) {
+        if (Math.abs(path.fileDistance()) == 1 && Math.abs(path.rankDistance()) == 2) {
+            return WayPointsWithCondition.possible(Collections.emptyList());
+        }
+        if (Math.abs(path.fileDistance()) == 2 && Math.abs(path.rankDistance()) == 1) {
+            return WayPointsWithCondition.possible(Collections.emptyList());
+        }
+        return WayPointsWithCondition.impossible();
     }
 }
