36522af6a15a81038a7a1654178a7d4e9184fc64
Youngho <cyh6099@gmail.com>
2023-03-15 15:31:45 +0900

    test(Movement): 수직으로 상, 하, 좌, 우 방향의 움직임인지 확인하는 테스트 구현

--- src/test/java/MovementTest.java
+++ src/test/java/MovementTest.java
@@ -1,9 +1,9 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MovementTest {
@@ -21,7 +21,7 @@ class MovementTest {
     @CsvSource(value = {"0,1,true", "0,-1,false"})
     void shouldReturnTrueWhenMovementDirectionIsUpwardOrFalse(int fileIncrement, int rankIncrement, boolean result) {
         Movement movement = new Movement(fileIncrement, rankIncrement);
-        Assertions.assertThat(movement.isUpward()).isEqualTo(result);
+        assertThat(movement.isUpward()).isEqualTo(result);
     }
 
     @DisplayName("아래 방향의 움직임이면 true, 아니면 false를 반환한다.")
@@ -29,7 +29,7 @@ class MovementTest {
     @CsvSource(value = {"0,-1,true", "0,1,false"})
     void shouldReturnTrueWhenMovementDirectionIsDownwardOrFalse(int fileIncrement, int rankIncrement, boolean result) {
         Movement movement = new Movement(fileIncrement, rankIncrement);
-        Assertions.assertThat(movement.isDownward()).isEqualTo(result);
+        assertThat(movement.isDownward()).isEqualTo(result);
     }
 
     @DisplayName("2칸 이내의 움직임이면 true, 아니면 false를 반환한다.")
@@ -37,6 +37,15 @@ class MovementTest {
     @CsvSource(value = {"2,0,true", "0,1,true", "2,2,true", "-2,2,true", "-3,1,false"})
     void shouldReturnTrueWhenMovementDirectionIsUnderTwoStepsOrFalse(int fileIncrement, int rankIncrement, boolean result) {
         Movement movement = new Movement(fileIncrement, rankIncrement);
-        Assertions.assertThat(movement.isUnderTwoSteps()).isEqualTo(result);
+        assertThat(movement.isUnderTwoSteps()).isEqualTo(result);
     }
+
+    @DisplayName("수직으로 상, 하, 좌, 우 방향의 움직임이면 true, 아니면 false를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"2,0,true", "0,1,true", "-2,0,true", "0,-3,true", "1,1,false", "-1,3,false"})
+    void shouldReturnTrueWhenMovementDirectionIsPerpendicularOrFalse(int fileIncrement, int rankIncrement, boolean result) {
+        Movement movement = new Movement(fileIncrement, rankIncrement);
+        assertThat(movement.isPerpendicular()).isEqualTo(result);
+    }
+
 }
