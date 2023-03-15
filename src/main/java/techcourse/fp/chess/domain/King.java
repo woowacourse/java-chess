3ece90f4de68205cfb2f5209b7aeeb7ca54df1a2
hyunseo <ohseo8813@gmail.com>
2023-03-15 15:44:09 +0900

    feat: 킹 객체 구현

---
+++ src/main/java/techcourse/fp/chess/domain/King.java
@@ -0,0 +1,52 @@
+package techcourse.fp.chess.domain;
+
+import java.util.List;
+import techcourse.fp.movingStrategy.DownStrategy;
+import techcourse.fp.movingStrategy.LeftDownStrategy;
+import techcourse.fp.movingStrategy.LeftStrategy;
+import techcourse.fp.movingStrategy.LeftUpStrategy;
+import techcourse.fp.movingStrategy.MovingStrategies;
+import techcourse.fp.movingStrategy.MovingStrategy;
+import techcourse.fp.movingStrategy.RightDownStrategy;
+import techcourse.fp.movingStrategy.RightStrategy;
+import techcourse.fp.movingStrategy.RightUpStrategy;
+import techcourse.fp.movingStrategy.UpStrategy;
+
+public class King extends Piece {
+
+    {
+        final List<MovingStrategy> rawStrategies = List.of(
+                new RightUpStrategy(), new RightDownStrategy(),
+                new LeftDownStrategy(), new LeftUpStrategy(),
+                new UpStrategy(), new DownStrategy(),
+                new LeftStrategy(), new RightStrategy());
+        strategies = new MovingStrategies(rawStrategies);
+    }
+
+    public King(final Color color) {
+        super(color);
+    }
+
+    @Override
+    public List<Position> findPath(final Position sourcePosition, final Position targetPosition,
+                                   final Color targetColor) {
+        final MovingStrategy movingStrategy = strategies.findStrategy(sourcePosition, targetPosition);
+
+        if (targetColor.isSameColor(this.color)) {
+            throw new IllegalStateException("아군의 기물이 존재하는 곳으로는 이동할 수 없습니다.");
+        }
+
+        return createPath(sourcePosition, targetPosition, movingStrategy);
+    }
+
+    private List<Position> createPath(final Position sourcePosition, final Position targetPosition,
+                                      final MovingStrategy movingStrategy) {
+        final Position moveResult = movingStrategy.move(sourcePosition);
+
+        if (!moveResult.equals(targetPosition)) {
+            throw new IllegalArgumentException("킹은 한 칸만 이동 가능합니다.");
+        }
+
+        return List.of(moveResult);
+    }
+}
