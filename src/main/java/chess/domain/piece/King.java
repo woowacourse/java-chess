e778e414e383e643f49083bc11a6752ce6cf203a
seokjin8678 <seokjin8678@gmail.com>
2023-03-15 20:34:38 +0900

    feat: King 이동 메서드 구현
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -1,5 +1,6 @@
 package chess.domain.piece;
 
+import chess.domain.Position;
 import chess.domain.Role;
 import chess.domain.Team;
 
@@ -7,5 +8,10 @@ public class King extends Piece {
 
     public King(Team team) {
         super(Role.KING, team);
+    }
+
+    @Override
+    public boolean canMove(Position source, Position target) {
+        return source.getDistanceTo(target) == 1;
     }
 }
