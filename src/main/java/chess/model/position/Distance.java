512bd50c46ad6311cdb8176bbebcc2609e48f04d
apptie <appti@naver.com>
2023-03-15 14:43:55 +0900

    feat: Position 간의 거리를 index로 변환하는 메서드 구현

--- src/main/java/chess/model/position/Distance.java
+++ src/main/java/chess/model/position/Distance.java
@@ -2,11 +2,17 @@ package chess.model.position;
 
 public class Distance {
 
+    private static final int FILE_MAX_SIZE = 8;
+
     private final int rank;
     private final int file;
 
     public Distance(final int rank, final int file) {
         this.rank = rank;
         this.file = file;
+    }
+
+    public int convertToIndex() {
+        return (file * FILE_MAX_SIZE) + rank;
     }
 }
--- src/test/java/chess/model/board/DistanceTest.java
+++ src/test/java/chess/model/board/DistanceTest.java
@@ -6,14 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import chess.model.position.Distance;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class DistanceTest {
 
     @Test
-    @DisplayName("생성자는 rank와 file을 전달하면 Distance를 생성한다")
+    @DisplayName("생성자는 rank와 file을 전달하면 Distance를 생성한다.")
     void constructor_givenRankAndFile_ThenSuccess() {
         final Distance distance = assertDoesNotThrow(() -> new Distance(1, 1));
 
         assertThat(distance).isExactlyInstanceOf(Distance.class);
+    }
+
+    @ParameterizedTest(name = "converToIndex()는 호출하면 거리를 인덱스로 변환한다.")
+    @CsvSource({"1,1,9", "3,5,43", "-2,-1,-10"})
+    void convertToIndex_whenCall_thenReturnIndex(final int rank, final int file, final int result) {
+        final Distance distance = new Distance(rank, file);
+
+        assertThat(distance.convertToIndex()).isEqualTo(result);
     }
 }
