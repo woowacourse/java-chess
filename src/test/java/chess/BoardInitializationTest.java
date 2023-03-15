bd0c0318af803d889c643da1c4375e4e648fdc95
waterricecake <fruturum@nate.com>
2023-03-15 10:49:01 +0900

    test(BoardInitializationTest): 보드 초기 세팅 테스트 구현

--- src/test/java/chess/BoardInitializationTest.java
+++ src/test/java/chess/BoardInitializationTest.java
@@ -22,6 +22,7 @@ public class BoardInitializationTest {
 
     static Stream<Arguments> squareProvider() {
         return Stream.of(
+                //White rank1 초기화
                 Arguments.arguments(new Square(File.A, Rank.ONE), new Rook(Camp.NONE)),
                 Arguments.arguments(new Square(File.B, Rank.ONE), new Knight(Camp.NONE)),
                 Arguments.arguments(new Square(File.C, Rank.ONE), new Bishop(Camp.NONE)),
@@ -29,7 +30,36 @@ public class BoardInitializationTest {
                 Arguments.arguments(new Square(File.E, Rank.ONE), new King(Camp.NONE)),
                 Arguments.arguments(new Square(File.F, Rank.ONE), new Bishop(Camp.NONE)),
                 Arguments.arguments(new Square(File.G, Rank.ONE), new Knight(Camp.NONE)),
-                Arguments.arguments(new Square(File.H, Rank.ONE), new Rook(Camp.NONE))
+                Arguments.arguments(new Square(File.H, Rank.ONE), new Rook(Camp.NONE)),
+                //White rank2 초기화
+                Arguments.arguments(new Square(File.A, Rank.TWO), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.B, Rank.TWO), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.C, Rank.TWO), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.D, Rank.TWO), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.E, Rank.TWO), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.F, Rank.TWO), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.G, Rank.TWO), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.H, Rank.TWO), new Pawn(Camp.NONE)),
+
+                //Black rank7 초기화
+                Arguments.arguments(new Square(File.A, Rank.SEVEN), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.B, Rank.SEVEN), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.C, Rank.SEVEN), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.D, Rank.SEVEN), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.E, Rank.SEVEN), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.F, Rank.SEVEN), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.G, Rank.SEVEN), new Pawn(Camp.NONE)),
+                Arguments.arguments(new Square(File.H, Rank.SEVEN), new Pawn(Camp.NONE)),
+
+                //Black rank8 초기화
+                Arguments.arguments(new Square(File.A, Rank.EIGHT), new Rook(Camp.NONE)),
+                Arguments.arguments(new Square(File.B, Rank.EIGHT), new Knight(Camp.NONE)),
+                Arguments.arguments(new Square(File.C, Rank.EIGHT), new Bishop(Camp.NONE)),
+                Arguments.arguments(new Square(File.D, Rank.EIGHT), new Queen(Camp.NONE)),
+                Arguments.arguments(new Square(File.E, Rank.EIGHT), new King(Camp.NONE)),
+                Arguments.arguments(new Square(File.F, Rank.EIGHT), new Bishop(Camp.NONE)),
+                Arguments.arguments(new Square(File.G, Rank.EIGHT), new Knight(Camp.NONE)),
+                Arguments.arguments(new Square(File.H, Rank.EIGHT), new Rook(Camp.NONE))
         );
     }
 }
