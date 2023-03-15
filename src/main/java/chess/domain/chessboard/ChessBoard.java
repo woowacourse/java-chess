244a4a28d64244007dc05184a2b2cefe76b16968
songusika <sws981202@gmail.com>
2023-03-15 16:27:02 +0900

    feat: 체스판이 규칙에 맞게 기물을 생성하는 기능 구현
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/main/java/chess/domain/chessboard/ChessBoard.java
+++ src/main/java/chess/domain/chessboard/ChessBoard.java
@@ -1,21 +1,56 @@
 package chess.domain.chessboard;
 
+import chess.domain.chessboard.state.Team;
+import chess.domain.chessboard.state.piece.Bishop;
+import chess.domain.chessboard.state.piece.King;
+import chess.domain.chessboard.state.piece.Knight;
+import chess.domain.chessboard.state.piece.Pawn;
+import chess.domain.chessboard.state.piece.Queen;
+import chess.domain.chessboard.state.piece.Rook;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public final class ChessBoard {
 
     private static final int FIXED_RANK_SIZE = 8;
+    public static final int BLANK_RANK_SIZE = 4;
 
-    private final List<Rank> ranks = new ArrayList<>();
+    private final List<Rank> ranks = new ArrayList<>(FIXED_RANK_SIZE);
 
-    public ChessBoard(){
+    public ChessBoard() {
         initRanks();
     }
 
     private void initRanks() {
-        for (int rankIndex = 0; rankIndex < FIXED_RANK_SIZE; rankIndex++) {
+        ranks.add(createSideRankByTeam(Team.WHITE));
+        ranks.add(createPawnRankByTeam(Team.WHITE));
+
+        for (int rankIndex = 0; rankIndex < BLANK_RANK_SIZE; rankIndex++) {
             ranks.add(new Rank());
         }
+
+        ranks.add(createPawnRankByTeam(Team.BLACK));
+        ranks.add(createSideRankByTeam(Team.BLACK));
+    }
+
+    private static Rank createPawnRankByTeam(final Team team) {
+        return new Rank(new Square(new Pawn(team)));
+    }
+
+    private Rank createSideRankByTeam(final Team team) {
+        return new Rank(List.of(
+                new Square(new Rook(team)),
+                new Square(new Knight(team)),
+                new Square(new Bishop(team)),
+                new Square(new Queen(team)),
+                new Square(new King(team)),
+                new Square(new Bishop(team)),
+                new Square(new Knight(team)),
+                new Square(new Rook(team))));
+    }
+
+    public List<Rank> getRanks() {
+        return Collections.unmodifiableList(ranks);
     }
 }
--- src/main/java/chess/domain/chessboard/Rank.java
+++ src/main/java/chess/domain/chessboard/Rank.java
@@ -1,20 +1,42 @@
 package chess.domain.chessboard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public final class Rank {
 
     private static final int FIXED_ONE_RANK_SIZE = 8;
-    private final List<Square> squares = new ArrayList<>();
+    private final List<Square> squares;
 
     public Rank() {
-        initSquares();
+        squares = new ArrayList<>();
+        initSquares(new Square());
     }
 
-    private void initSquares() {
+    public Rank(final Square square) {
+        squares = new ArrayList<>();
+        initSquares(square);
+    }
+
+    public Rank(final List<Square> squares) {
+        this.squares = new ArrayList<>(squares);
+        validateSize();
+    }
+
+    private void initSquares(final Square square) {
         for (int squareIndex = 0; squareIndex < FIXED_ONE_RANK_SIZE; squareIndex++) {
-            squares.add(new Square());
+            squares.add(square);
         }
+    }
+
+    private void validateSize() {
+        if (squares.size() != FIXED_ONE_RANK_SIZE) {
+            throw new IllegalArgumentException("랭크는 8칸이어야 합니다.");
+        }
+    }
+
+    public List<Square> getSquares() {
+        return Collections.unmodifiableList(squares);
     }
 }
