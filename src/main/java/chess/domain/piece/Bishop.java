40204a442efbd829c575875b2d1e9c939854be54
seokjin8678 <seokjin8678@gmail.com>
2023-03-15 20:34:11 +0900

    feat: Bishop 이동 메서드 구현
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -1,5 +1,6 @@
 package chess.domain.piece;
 
+import chess.domain.Position;
 import chess.domain.Role;
 import chess.domain.Team;
 
@@ -7,5 +8,10 @@ public class Bishop extends Piece {
 
     public Bishop(Team team) {
         super(Role.BISHOP, team);
+    }
+
+    @Override
+    public boolean canMove(Position source, Position target) {
+        return canMoveDiagonal(source, target);
     }
 }
