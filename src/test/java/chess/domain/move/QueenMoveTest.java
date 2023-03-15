a7d2f0884aca472baa08f2c47e22db6ab10ad035
jiwon <ljwon77@daum.net>
2023-03-15 20:01:34 +0900

    test: 퀸, 룩의 테스트 조건을 수정한다
    Co-authored-by: yenawee <yaena0319@naver.com>

--- src/test/java/chess/domain/move/QueenMoveTest.java
+++ src/test/java/chess/domain/move/QueenMoveTest.java
@@ -10,30 +10,30 @@ class QueenMoveTest {
 
     @ParameterizedTest(name = "source에서 target으로 퀸이 이동할 수 있으면 true를 반환한다.")
     @CsvSource(value = {"0:1", "0:7", "1:0", "7:0", "1:1", "7:7"}, delimiter = ':')
-    void canMoveSuccess(final int rank, final int file) {
+    void canMoveSuccess(final int targetRank, final int targetFile) {
         // given
         final QueenMove queenMove = new QueenMove();
         final int sourceRank = 0, sourceFile = 0;
         final Position source = new Position(sourceRank, sourceFile);
 
         // when
-        boolean actual = queenMove.canMove(source, new Position(sourceRank + rank, sourceFile + file));
+        boolean actual = queenMove.canMove(source, new Position(targetRank, targetFile));
 
         // then
         assertThat(actual)
                 .isTrue();
     }
 
-    @ParameterizedTest(name = "source에서 target으로 이동하는 방향이 퀸이 갈 수 없는 위치면 false를 반환한다.")
+    @ParameterizedTest(name = "source에서 target으로 퀸이 갈 수 없는 위치면 false를 반환한다.")
     @CsvSource(value = {"1:2", "0:8", "-8:0", "8:0", "0:-8", "2:1", "-1:2", "1:-2", "8:8"}, delimiter = ':')
-    void canMoveFailWhenWrongTarget(final int rank, final int file) {
+    void canMoveFailWhenWrongTarget(final int targetRank, final int targetFile) {
         // given
         final QueenMove queenMove = new QueenMove();
         final int sourceRank = 0, sourceFile = 0;
         final Position source = new Position(sourceRank, sourceFile);
 
         // when
-        boolean actual = queenMove.canMove(source, new Position(sourceRank + rank, sourceFile + file));
+        boolean actual = queenMove.canMove(source, new Position(targetRank, targetFile));
 
         // then
         assertThat(actual)
