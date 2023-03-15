0fda99310948203e582c1f1161fb19c91f515980
waterricecake <fruturum@nate.com>
2023-03-15 16:19:00 +0900

    feat(Knight): 이동 가능 여부 기능 구현

--- src/main/java/chess/piece/Knight.java
+++ src/main/java/chess/piece/Knight.java
@@ -1,6 +1,12 @@
 package chess.piece;
 
+import chess.Square;
+
 public class Knight extends Piece {
+
+    private static final int MOVABLE_STRAIGHT_DISTANCE = 2;
+    private static final int MOBABLE_SIDE_DISTANCE = 1;
+
     public Knight(Camp camp) {
         super(camp);
     }
@@ -8,5 +14,19 @@ public class Knight extends Piece {
     @Override
     public PieceType getPieceType() {
         return PieceType.KNIGHT;
+    }
+
+    @Override
+    public boolean canMove(Square source, Square target) {
+        if(source.calculateRankDistance(target) == MOVABLE_STRAIGHT_DISTANCE
+                && source.calculateFileDistance(target) == MOBABLE_SIDE_DISTANCE){
+            return true;
+        }
+
+        if(source.calculateRankDistance(target) == MOBABLE_SIDE_DISTANCE
+                && source.calculateFileDistance(target) == MOVABLE_STRAIGHT_DISTANCE){
+            return true;
+        }
+        return false;
     }
 }
