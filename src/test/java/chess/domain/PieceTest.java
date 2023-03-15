6aa1523c203d0762a23f107937e54127e9117453
Cyma-s <ssol0319@naver.com>
2023-03-15 13:34:59 +0900

    test(Piece): 생성 테스트 추가
    Co-authored-by: dooboocookie <bkh3030@gmail.com>

--- src/test/java/chess/domain/PieceTest.java
+++ src/test/java/chess/domain/PieceTest.java
@@ -1,31 +1,39 @@
 package chess.domain;
 
-import org.assertj.core.api.Assertions;
+import chess.domain.piece.*;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PieceTest {
-    @Test
-    @DisplayName("기물들을 생성한다.")
-    void createInitialPieces() {
-        // given
+
+    @ParameterizedTest
+    @MethodSource("pieceDummy")
+    @DisplayName("역할에 따른 기물이 생성된다.")
+    void create(Role role, Class<Piece> expectedPieceType) {
+        //given
         Side side = new Side(Color.BLACK);
+        //when
+        Piece piece = role.create(side);
+        //expected
+        assertThat(piece).isInstanceOf(expectedPieceType);
+    }
 
-        // when
-        List<Piece> pieces = Piece.createInitialPieces(side);
-
-        // expected
-        Role[] values = Role.values();
-        Arrays.stream(values).forEach(
-                role -> {
-                    long count = pieces.stream()
-                            .filter(piece -> piece.getRole().equals(role))
-                            .count();
-                    Assertions.assertThat(count).isEqualTo(role.getCount());
-                }
+    static Stream<Arguments> pieceDummy() {
+        return Stream.of(
+                Arguments.arguments(Role.EMPTY, Empty.class),
+                Arguments.arguments(Role.PAWN, Pawn.class),
+                Arguments.arguments(Role.ROOK, Rook.class),
+                Arguments.arguments(Role.KNIGHT, Knight.class),
+                Arguments.arguments(Role.BISHOP, Bishop.class),
+                Arguments.arguments(Role.QUEEN, Queen.class),
+                Arguments.arguments(Role.KING, King.class)
         );
     }
+
 }
