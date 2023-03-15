3cbc9f6eabd4a9b387db7576d38f31ab57313cc7
yujamint <woawoa0513@gmail.com>
2023-03-15 13:43:51 +0900

    test(PiecesTest): Pieces 생성 기능 테스트 ChessGameTest -> PiecesTest로 위임

--- src/test/java/chess/PiecesTest.java
+++ src/test/java/chess/PiecesTest.java
@@ -3,22 +3,23 @@ package chess;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.piece.Piece;
+import chess.piece.Pieces;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class ChessGameTest {
+class PiecesTest {
 
     @Test
     @DisplayName("기물 32개를 생성한다.")
     void generateInitialPieces() {
         // given
-        final ChessGame chessGame = new ChessGame();
+        final Pieces pieces = new Pieces();
 
         // when
-        List<Piece> pieces = chessGame.generateInitialPieces();
+        final List<Piece> generatedPieces = pieces.generateInitialPieces();
 
         // then
-        assertThat(pieces.size()).isEqualTo(32);
+        assertThat(generatedPieces.size()).isEqualTo(32);
     }
 }
