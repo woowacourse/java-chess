85cd37d2fe7a8eb94e4ba81b9e92c07f46047f13
woo-chang <clllickme@naver.com>
2023-03-15 14:58:23 +0900

    feat: 게임 시작 문구 출력
    Co-authored-by: swonny <wonny921@gmail.com>

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,12 +1,15 @@
 package chess.controller;
 
 import chess.view.InputView;
+import chess.view.OutputView;
 
 public class ChessController {
 
     private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     public void run() {
+        outputView.printStartMessage();
         final ChessExecuteCommand chessExecuteCommand = ChessExecuteCommand.from(inputView.readChessExecuteCommand());
     }
 }
