17851fb38eca82f2ccdfc4d751fac655b6eee484
yoondgu <doyou2ing@naver.com>
2023-03-15 15:57:06 +0900

    refactor: King 의 이동 가능 목록 반환 방식 수정
    - 변경 방식이 미반영 된 다른 클래스들에 대한 테스트는 주석처리 함

--- src/main/java/chess/domain/King.java
+++ src/main/java/chess/domain/King.java
@@ -5,36 +5,20 @@ import java.util.List;
 
 public class King implements Movable {
 
+    private static final List<Direction> directions;
+
+    static {
+        directions = List.of(Direction.EAST, Direction.WEST, Direction.SOUTH, Direction.NORTH, Direction.NORTH_EAST,
+                Direction.NORTH_WEST, Direction.SOUTH_EAST, Direction.SOUTH_WEST);
+    }
+
     @Override
-    public List<Path> findMovablePaths(final Position position) {
-        int sourceX = position.getX();
-        int sourceY = position.getY();
-
-        List<Position> movablePositions = new ArrayList<>();
-        for (int i = 1; i < 9; i++) {
-            for (int j = 1; j < 9; j++) {
-                if (isMovableDestination(sourceX, sourceY, i, j)) {
-                    movablePositions.add(new Position(i, j));
-                }
-            }
+    public List<Path> findMovablePaths(final Position current) {
+        List<Path> paths = new ArrayList<>();
+        for (Direction direction : directions) {
+            paths.add(Path.ofSinglePath(current, direction));
         }
-        return null;
-    }
-
-    private boolean isMovableDestination(final int sourceX, final int sourceY, final int destX, final int destY) {
-        return isDestinationInRange(sourceX, sourceY, destX, destY) && isNotSource(sourceX, sourceY, destX, destY);
-    }
-
-    private boolean isDestinationInRange(final int sourceX, final int sourceY, final int destX, final int destY) {
-        return isInRange(destX, sourceX) && isInRange(destY, sourceY);
-    }
-
-    private boolean isInRange(final int targetValue, final int sourceValue) {
-        return sourceValue - 1 <= targetValue && targetValue <= sourceValue + 1;
-    }
-
-    private boolean isNotSource(final int sourceX, final int sourceY, final int destX, final int destY) {
-        return sourceX != destX || sourceY != destY;
+        return paths;
     }
 
 }
--- src/test/java/chess/domain/KingTest.java
+++ src/test/java/chess/domain/KingTest.java
@@ -1,20 +1,31 @@
 package chess.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class KingTest {
 
-//    @Test
-//    @DisplayName("King은 앞뒤양옆, 대각선 한 칸 씩 이동 가능하다.")
-//    void 이동_범위_확인() {
-//        King king = new King();
-//
-//        List<Position> movablePositions = king.findMovablePaths(new Position(1, 1));
-//
-//        assertThat(movablePositions).containsExactlyInAnyOrder(new Position(1, 2), new Position(2, 1),
-//                new Position(2, 2));
-//    }
+
+    @Test
+    @DisplayName("Knight 은 앞뒤양옆 1칸 이동 후 대각선으로 1칸 이동 가능하다.")
+    void 이동_범위_확인() {
+        King king = new King();
+
+        List<Path> movablePaths = king.findMovablePaths(new Position(1, 1));
+
+        int totalPositionCount = 0;
+
+        for (Path path : movablePaths) {
+            totalPositionCount += path.positions().size();
+        }
+
+        assertThat(totalPositionCount).isEqualTo(3);
+    }
 
 }
