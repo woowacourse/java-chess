4c1bdf65f42012ed66933f28133b172b0ad8a019
seokjin8678 <seokjin8678@gmail.com>
2023-03-15 14:52:08 +0900

    chore: 사용하지 않는 테스트 코드 삭제
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/test/java/chess/domain/PieceTest.java
+++ src/test/java/chess/domain/PieceTest.java
@@ -1,22 +1,4 @@
 package chess.domain;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-
 class PieceTest {
-
-    @ParameterizedTest
-    @DisplayName("Piece가 정상적으로 생성되어야 한다.")
-    @EnumSource(Role.class)
-    void create_success(Role input) {
-        // given
-        Piece piece = new Piece(input, Team.BLACK);
-
-        // expect
-        assertThat(piece.getRole())
-                .isEqualTo(input);
-    }
 }
