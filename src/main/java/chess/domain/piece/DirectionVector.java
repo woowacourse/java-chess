ff8bc456e7bc714cfefb25bb59b0173378846356
woo-chang <clllickme@naver.com>
2023-03-15 20:41:44 +0900

    feat: 방향 벡터(다음 파일, 다음 랭크, 방향 확인) 기능 구현
    Co-authored-by: swonny <wonny921@gmail.com>

---
+++ src/main/java/chess/domain/piece/DirectionVector.java
@@ -0,0 +1,51 @@
+package chess.domain.piece;
+
+import chess.domain.board.File;
+import chess.domain.board.Rank;
+import java.util.function.BiFunction;
+
+public enum DirectionVector {
+
+    NORTH(0, 1, (x, y) -> x == 0 && y > 0),
+    SOUTH(0, -1, (x, y) -> x == 0 && y < 0),
+    WEST(-1, 0, (x, y) -> x < 0 && y == 0),
+    EAST(1, 0, (x, y) -> x > 0 && y == 0),
+    SOUTHEAST(1, -1, (x, y) -> x > 0 && y < 0 && Math.abs(x) == Math.abs(y)),
+    SOUTHWEST(-1, -1, (x, y) -> x < 0 && y < 0 && Math.abs(x) == Math.abs(y)),
+    NORTHEAST(1, 1, (x, y) -> x > 0 && y > 0 && Math.abs(x) == Math.abs(y)),
+    NORTHWEST(-1, 1, (x, y) -> x < 0 && y > 0 && Math.abs(x) == Math.abs(y));
+
+    private final int x;
+    private final int y;
+    private final BiFunction<Integer, Integer, Boolean> biFunction;
+
+    DirectionVector(final int x, final int y, final BiFunction<Integer, Integer, Boolean> biFunction) {
+        this.x = x;
+        this.y = y;
+        this.biFunction = biFunction;
+    }
+
+    public boolean isOnMyWay(final int distanceX, final int distanceY) {
+        return biFunction.apply(distanceX, distanceY);
+    }
+
+    public File next(final File file) {
+        if (x == 1) {
+            return file.next();
+        }
+        if (x == -1) {
+            return file.prev();
+        }
+        return file;
+    }
+
+    public Rank next(final Rank rank) {
+        if (y == 1) {
+            return rank.next();
+        }
+        if (y == -1) {
+            return rank.prev();
+        }
+        return rank;
+    }
+}
