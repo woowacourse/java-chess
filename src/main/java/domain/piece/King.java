c0ceefa279f6a7b8c034675ec579d38af612795f
MoonJeWoong <munjin0201@naver.com>
2023-03-15 21:35:27 +0900

    feat: king 이동 경로 판단 기능 구현

--- src/main/java/domain/piece/King.java
+++ src/main/java/domain/piece/King.java
@@ -12,16 +12,37 @@ public final class King extends Piece {
 
     @Override
     public boolean isMovablePath(Position start, List<Position> path) {
-        return false;
+        return isMovableDirection(start, path.get(0)) &&
+                isMovableDistance(path.size());
     }
 
     @Override
     protected boolean isMovableDirection(Position start, Position nextPosition) {
+        return isMovableCrossDirection(start, nextPosition) || isMovableDiagonalDirection(start, nextPosition);
+    }
+
+    private boolean isMovableCrossDirection(Position start, Position nextPosition) {
+        if (start.moveDown().equals(nextPosition) ||
+                start.moveUp().equals(nextPosition) ||
+                start.moveRight().equals(nextPosition) ||
+                start.moveLeft().equals(nextPosition)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isMovableDiagonalDirection(Position start, Position nextPosition) {
+        if (start.moveUpRight().equals(nextPosition) ||
+                start.moveUpLeft().equals(nextPosition) ||
+                start.moveDownRight().equals(nextPosition) ||
+                start.moveDownLeft().equals(nextPosition)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     protected boolean isMovableDistance(int distance) {
-        return false;
+        return distance == 1;
     }
 }
--- src/test/java/domain/piece/KingTest.java
+++ src/test/java/domain/piece/KingTest.java
@@ -3,8 +3,15 @@ package domain.piece;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import domain.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @DisplayName("King은 ")
 class KingTest {
@@ -35,4 +42,57 @@ class KingTest {
         assertThat(name).isEqualTo("k");
     }
 
+    @ParameterizedTest
+    @MethodSource("isMovablePathTest_SuccessCase")
+    @DisplayName("모든 방향으로 1칸 이동할 수 있다.")
+    void isMovablePathTest_Success(List<Position> path) {
+        // given
+        King king = new King(Color.WHITE);
+
+        // when
+        boolean result = king.isMovablePath(Position.of(3,3), path);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("isMovablePathTest_FailCase")
+    @DisplayName("2칸 이상 이동할 수 없다.")
+    void isMovablePathTest_Fail(List<Position> path) {
+        // given
+        King king = new King(Color.WHITE);
+
+        // when
+        boolean result = king.isMovablePath(Position.of(3,3), path);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    static Stream<Arguments> isMovablePathTest_SuccessCase() {
+        return Stream.of(
+                Arguments.of(List.of(Position.of(4,3))),
+                Arguments.of(List.of(Position.of(3,4))),
+                Arguments.of(List.of(Position.of(2,3))),
+                Arguments.of(List.of(Position.of(3,2))),
+                Arguments.of(List.of(Position.of(4,4))),
+                Arguments.of(List.of(Position.of(2,4))),
+                Arguments.of(List.of(Position.of(2,2))),
+                Arguments.of(List.of(Position.of(4,2)))
+        );
+    }
+
+    static Stream<Arguments> isMovablePathTest_FailCase() {
+        return Stream.of(
+                Arguments.of(List.of(Position.of(4,3), Position.of(5,3))),
+                Arguments.of(List.of(Position.of(3,4), Position.of(3,5))),
+                Arguments.of(List.of(Position.of(2,3), Position.of(1,3))),
+                Arguments.of(List.of(Position.of(3,2), Position.of(3,1))),
+                Arguments.of(List.of(Position.of(4,4), Position.of(5,5))),
+                Arguments.of(List.of(Position.of(2,4), Position.of(1,5))),
+                Arguments.of(List.of(Position.of(2,2), Position.of(1,1))),
+                Arguments.of(List.of(Position.of(4,2), Position.of(5,1)))
+        );
+    }
 }
