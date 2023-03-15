1057ba25dfcc652afc3e0d6d1d203b336e981cc2
Youngho <cyh6099@gmail.com>
2023-03-15 18:18:39 +0900

    feat(Piece): source piece와 target piece가 서로 다른 진영인지 확인

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -10,4 +10,11 @@ public abstract class Piece {
     public boolean isSameSide(Piece targetPiece) {
         return this.side == targetPiece.side;
     }
+
+    public boolean isOpponentSide(Piece targetPiece) {
+        if (this.side.equals(Side.WHITE)) {
+            return targetPiece.side.equals(Side.BLACK);
+        }
+        return targetPiece.side.equals(Side.WHITE);
+    }
 }
