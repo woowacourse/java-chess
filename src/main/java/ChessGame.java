54e7d4d116cc88fc4fc0a9a6ce6b7fc110d5e264
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-15 17:09:51 +0900

    feat: ChessGame 생성 기능 추가
    Co-authored-by: jjongwa <troas96@naver.com>

---
+++ src/main/java/ChessGame.java
@@ -0,0 +1,11 @@
+import chess.ChessBoard;
+
+public class ChessGame {
+
+    private final ChessBoard chessBoard;
+
+    public ChessGame(ChessBoard chessBoard) {
+        this.chessBoard = chessBoard;
+    }
+
+}
