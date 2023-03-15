518d441f636fafd9ebe97cc052f7758805c0390d
yoondgu <doyou2ing@naver.com>
2023-03-15 15:47:20 +0900

    refactor: Bishop 의 이동 가능 목록 반환 방식 수정
    - 변경 방식이 미반영 된 다른 클래스들에 대한 테스트는 주석처리 함

--- src/main/java/chess/domain/Bishop.java
+++ src/main/java/chess/domain/Bishop.java
@@ -5,37 +5,19 @@ import java.util.List;
 
 public class Bishop implements Movable {
 
+    private static final List<Direction> directions;
+
+    static {
+        directions = List.of(Direction.NORTH_EAST, Direction.NORTH_WEST, Direction.SOUTH_EAST, Direction.SOUTH_WEST);
+    }
 
     @Override
-    public List<Path> findMovablePaths(final Position position) {
-        int sourceX = position.getX();
-        int sourceY = position.getY();
-
-        List<Position> movablePositions = new ArrayList<>();
-        for (int i = 1; i < 9; i++) {
-            for (int j = 1; j < 9; j++) {
-                if (isMovableDestination(sourceX, sourceY, i, j)) {
-                    movablePositions.add(new Position(i, j));
-                }
-            }
+    public List<Path> findMovablePaths(final Position current) {
+        List<Path> paths = new ArrayList<>();
+        for (Direction direction : directions) {
+            paths.add(Path.ofMultiPath(current, direction));
         }
-        return null;
-    }
-
-    private boolean isMovableDestination(final int sourceX, final int sourceY, final int destX, final int destY) {
-        return isDestinationInRange(sourceX, sourceY, destX, destY) && isNotSource(sourceX, sourceY, destX, destY);
-    }
-
-    private boolean isDestinationInRange(final int sourceX, final int sourceY, final int destX, final int destY) {
-        return isDiagonal(sourceX, sourceY, destX, destY);
-    }
-
-    private boolean isDiagonal(final int sourceX, final int sourceY, final int destX, final int destY) {
-        return Math.abs(sourceX - destX) == Math.abs(sourceY - destY);
-    }
-
-    private boolean isNotSource(final int sourceX, final int sourceY, final int destX, final int destY) {
-        return sourceX != destX || sourceY != destY;
+        return paths;
     }
 
 }
