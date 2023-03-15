0b3d4375d3cb3d48d9c10c77999c1dd37a860733
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-15 14:37:42 +0900

    feat : compareTo, equals, hashcode 오버라이딩
    Co-authored-by: thdwoqor <celebarte01@gmail.com>

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -1,6 +1,8 @@
 package chess.domain;
 
-public class Position {
+import java.util.Objects;
+
+public class Position implements Comparable<Position> {
     private final int row;
     private final int column;
 
@@ -17,5 +19,32 @@ public class Position {
         if (column < 0) {
             throw new IllegalArgumentException("세로 위치는 0 미만일 수 없습니다.");
         }
+    }
+
+    @Override
+    public int compareTo(final Position position) {
+        final int columnCompare = Integer.compare(this.row, position.row);
+        if (columnCompare == 0) {
+            return Integer.compare(this.column, position.column);
+        }
+        return columnCompare;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + row + "," + column + ")";
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Position position = (Position) o;
+        return row == position.row && column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
     }
 }
