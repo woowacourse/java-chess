a94dbb4249e5602039c3be7705b72e6b462319b5
woo-chang <clllickme@naver.com>
2023-03-15 20:08:29 +0900

    feat: 파일 위치(이전, 다음, 거리) 계산 기능 구현
    Co-authored-by: swonny <wonny921@gmail.com>

--- src/main/java/chess/domain/board/File.java
+++ src/main/java/chess/domain/board/File.java
@@ -4,26 +4,38 @@ import java.util.Arrays;
 
 public enum File {
 
-    A("a"),
-    B("b"),
-    C("c"),
-    D("d"),
-    E("e"),
-    F("f"),
-    G("g"),
-    H("h");
+    A('a'),
+    B('b'),
+    C('c'),
+    D('d'),
+    E('e'),
+    F('f'),
+    G('g'),
+    H('h');
 
-    private final String index;
+    private final Character index;
 
-    File(final String index) {
+    File(final Character index) {
         this.index = index;
     }
 
-    public static File from(final String fileIndex) {
+    public static File from(final Character fileIndex) {
         return Arrays.stream(File.values())
                 .filter(file -> file.index.equals(fileIndex))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 파일 인덱스입니다."));
+    }
+
+    public int calculateDistance(final File file) {
+        return this.index - file.index;
+    }
+
+    public File next() {
+        return File.from((char) (index + 1));
+    }
+
+    public File prev() {
+        return File.from(((char) (index - 1)));
     }
 }
 
