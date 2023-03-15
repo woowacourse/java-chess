452f4c96e3920ef67660ebb7482d0f75d5fb304c
Choi-JJunho <junho5336@gmail.com>
2023-03-15 21:39:20 +0900

    feat: 나이트 이동 구현
    Co-authored-by: jeomxon <jeomxon@gmail.com>

--- src/main/java/chess/piece/Knight.java
+++ src/main/java/chess/piece/Knight.java
@@ -10,7 +10,19 @@ public class Knight extends Piece {
 
     @Override
     public boolean isMovable(final Position from, final Position to) {
-        return false;
+        if (from.equals(to)) {
+            return false;
+        }
+
+        int fileInterval = Math.abs(from.getFile().getIndex() - to.getFile().getIndex());
+        int rankInterval = Math.abs(from.getRank().getIndex() - to.getRank().getIndex());
+        boolean isFileUnderTwo = fileInterval <= 2;
+        boolean isRankUnderTwo = rankInterval <= 2;
+
+        if (isFileUnderTwo && isRankUnderTwo && fileInterval + rankInterval == 3) {
+            return true;
+        }
+        throw new IllegalArgumentException("Knight가 이동할 수 없는 경로입니다.");
     }
 
     @Override
---
+++ src/test/java/chess/piece/KnightTest.java
@@ -0,0 +1,45 @@
+package chess.piece;
+
+import chess.board.File;
+import chess.board.Position;
+import chess.board.Rank;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class KnightTest {
+
+
+    @Nested
+    class 나이트가_움직일_때_이동방향은_ {
+        @Test
+        void 두_칸_전진한_상태에서_좌우로_한_칸_움직일_수_있다() {
+            //given
+            Knight knight = new Knight(Team.WHITE);
+
+            //when
+            Position to = new Position(File.C, Rank.TWO);
+            Position from = new Position(File.A, Rank.ONE);
+
+            //then
+            assertThat(knight.isMovable(from, to)).isTrue();
+        }
+
+        @Test
+        void 대각선이_아니면_예외() {
+            //given
+            Knight knight = new Knight(Team.WHITE);
+
+            //when
+            Position to = new Position(File.B, Rank.EIGHT);
+            Position from = new Position(File.A, Rank.ONE);
+
+            //then
+            assertThatThrownBy(() -> knight.isMovable(from, to))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("Knight가 이동할 수 없는 경로입니다.");
+        }
+    }
+}
