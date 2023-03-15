fa6ccb0b5f92f38bdd95e926944ddd27d61f2b1a
MoonJeWoong <munjin0201@naver.com>
2023-03-15 17:29:37 +0900

    feat: 명령어 안내 문구 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,8 +8,13 @@ import java.util.Map;
 
 public final class OutputView {
 
+    private static final String START_MESSAGE = "> 체스 게임을 시작합니다.\n" +
+            "> 게임 시작 : start\n" +
+            "> 게임 종료 : end \n" +
+            "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
+
     public static void printStartMessage() {
-        System.out.println("체스 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
     }
 
     public static void printChessBoard(List<Position> allPosition, Map<Position, Piece> chessBoard) {
