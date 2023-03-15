5afd41f04b7f466c038600de72eb42d021ddbe4a
drunkenhw <drunkenhw@gmail.com>
2023-03-15 21:37:24 +0900

    feat: Rook의 이동 경로를 구하는 기능 추가

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -1,6 +1,9 @@
 package chess.domain.piece;
 
+import chess.domain.board.FileCoordinate;
 import chess.domain.board.Position;
+import chess.domain.board.RankCoordinate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
@@ -24,7 +27,41 @@ public class Rook extends Piece {
 
     @Override
     List<Position> findPath(Position sourcePosition, Position targetPosition) {
-        return null;
+        if (!canMove(sourcePosition, targetPosition)) {
+            throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+        }
+        List<Position> paths = new ArrayList<>();
+        int nowFileCoordinate = sourcePosition.getColumn();
+        int nowRankCoordinate = sourcePosition.getRow();
+        int targetFileCoordinate = targetPosition.getColumn();
+        int targetRankCoordinate = targetPosition.getRow();
+        if (nowFileCoordinate - targetFileCoordinate == 0) {
+            int rowStep = getStep(nowRankCoordinate, targetRankCoordinate);
+
+            while (nowRankCoordinate != targetRankCoordinate) {
+                Position position = new Position(sourcePosition.getFileCoordinate(),
+                        RankCoordinate.findBy(nowRankCoordinate));
+                nowRankCoordinate += rowStep;
+                paths.add(position);
+            }
+            return paths;
+        }
+        int columnStep = getStep(nowFileCoordinate, targetFileCoordinate);
+
+        while (nowFileCoordinate != targetFileCoordinate) {
+            Position position = new Position(FileCoordinate.findBy(nowFileCoordinate),
+                    sourcePosition.getRankCoordinate());
+            nowFileCoordinate += columnStep;
+            paths.add(position);
+        }
+        return paths;
+    }
+
+    private int getStep(int nowFileCoordinate, int targetFileCoordinate) {
+        if (nowFileCoordinate - targetFileCoordinate > 0) {
+            return -1;
+        }
+        return 1;
     }
 
     @Override
--- src/test/java/chess/domain/piece/RookTest.java
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -1,14 +1,22 @@
 package chess.domain.piece;
 
 import static chess.domain.board.FileCoordinate.C;
+import static chess.domain.board.FileCoordinate.D;
+import static chess.domain.board.FileCoordinate.E;
+import static chess.domain.board.FileCoordinate.F;
+import static chess.domain.board.RankCoordinate.FIVE;
 import static chess.domain.board.RankCoordinate.FOUR;
+import static chess.domain.board.RankCoordinate.SEVEN;
+import static chess.domain.board.RankCoordinate.SIX;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.board.FileCoordinate;
 import chess.domain.board.Position;
 import chess.domain.board.RankCoordinate;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,6 +24,12 @@ import org.junit.jupiter.params.provider.CsvSource;
 @SuppressWarnings("NonAsciiCharacters")
 class RookTest {
     private static final Position C_4 = new Position(C, FOUR);
+    private static final Position D_4 = new Position(D, FOUR);
+    private static final Position E_4 = new Position(E, FOUR);
+    private static final Position F_4 = new Position(F, FOUR);
+    private static final Position C_5 = new Position(C, FIVE);
+    private static final Position C_6 = new Position(C, SIX);
+    private static final Position C_7 = new Position(C, SEVEN);
 
     @ParameterizedTest
     @CsvSource(value = {"B:SIX:false", "A:FOUR:true", "C:SIX:true", "C:FOUR:false"}, delimiter = ':')
@@ -25,4 +39,23 @@ class RookTest {
         assertThat(rook.canMove(C_4, new Position(fileCoordinate, rankCoordinate))).isEqualTo(expect);
     }
 
+    @Test
+    void 룩이_랭크로_움직일_수_있는_경로를_알_수_있다() {
+        Rook rook = new Rook(Color.WHITE);
+        List<Position> path = rook.findPath(C_4, F_4);
+        List<Position> reversePath = rook.findPath(F_4, C_4);
+
+        assertThat(path).contains(D_4, E_4);
+        assertThat(reversePath).contains(D_4, E_4);
+    }
+
+    @Test
+    void 룩이_파일로_움직일_수_있는_경로를_알_수_있다() {
+        Rook rook = new Rook(Color.WHITE);
+        List<Position> path = rook.findPath(C_4, C_7);
+        List<Position> reversePath = rook.findPath(C_7, C_4);
+
+        assertThat(path).contains(C_5, C_6);
+        assertThat(reversePath).contains(C_5, C_6);
+    }
 }
