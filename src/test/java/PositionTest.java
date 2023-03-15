52ab7fc12c0d0c814cff165a70270d94d88d4ea5
Youngho <cyh6099@gmail.com>
2023-03-15 14:57:27 +0900

    test(Position): 현재 rank와 parameter rank동등 비교 테스트 작성

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -1,6 +1,9 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PositionTest {
 
@@ -11,7 +14,14 @@ class PositionTest {
         Position targetPosition = Position.of("d", "6");
 
         Movement increment = sourcePosition.calculateIncrement(targetPosition);
-        Assertions.assertThat(increment).isEqualTo(new Movement(3, 5));
+        assertThat(increment).isEqualTo(new Movement(3, 5));
     }
 
+    @DisplayName("현재 position rank와 parameter로 들어온 rank의 동등여부를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"2, TWO ,true", "3, TWO, false"})
+    void shouldReturnIsEqualRankBetweenSourcePositionAndParameterRankWhenInput(String sourceRank, Rank targetRank, boolean result) {
+        Position position = Position.of("a", sourceRank);
+        assertThat(position.hasRankOf(targetRank)).isEqualTo(result);
+    }
 }
