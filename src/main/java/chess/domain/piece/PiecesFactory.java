3690f099b109de6cffbdeb62cc7d39271f1db1b1
yeonkkk <tjddus0302@gmail.com>
2023-03-15 14:55:53 +0900

    refactor: PiecesFactory 메서드 수정

--- src/main/java/chess/domain/piece/PiecesFactory.java
+++ src/main/java/chess/domain/piece/PiecesFactory.java
@@ -6,11 +6,26 @@ import java.util.List;
 public class PiecesFactory {
     public static Pieces create(Color color) {
         List<Piece> pieces = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            pieces.add(new King(color));
-            pieces.add(new Pawn(color));
-        }
+        pieces.add(new King(color));
+        pieces.add(new Queen(color));
+        pieces = addPiece(pieces, new Bishop(color));
+        pieces = addPiece(pieces, new Rook(color));
+        pieces = addPiece(pieces, new Knight(color));
+        pieces = addPawn(pieces, color);
         return new Pieces(pieces);
     }
 
+    private static List<Piece> addPawn(final List<Piece> pieces, final Color color) {
+        for (int i = 0; i < 8; i++) {
+            pieces.add(new Pawn(color));
+        }
+        return pieces;
+    }
+
+    private static List<Piece> addPiece(final List<Piece> pieces, final Piece piece) {
+        for (int i = 0; i < 2; i++) {
+            pieces.add(piece);
+        }
+        return pieces;
+    }
 }
