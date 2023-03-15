76c5a21dfa6cba19cf5b6cd8a77f02f092443aa8
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-15 15:53:52 +0900

    test: 체스판 구성 및 기물 초기 세팅 확인 테스트
    Co-authored-by: jjongwa <troas96@naver.com>

---
+++ src/test/java/chess/ChessBoardTest.java
@@ -0,0 +1,89 @@
+package chess;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ChessBoardTest {
+
+    ChessBoard chessBoard = ChessBoard.generateChessBoard();
+    
+    @Test
+    @DisplayName("체스판 생성 후 룩은 올바른 위치에 만들어진다.")
+    void shouldSuccessGenerateRook() {
+
+        Assertions.assertAll(
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(1, 1))).isInstanceOf(Rook.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(1, 8))).isInstanceOf(Rook.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(8, 1))).isInstanceOf(Rook.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(8, 8))).isInstanceOf(Rook.class)
+        );
+    }
+
+    @Test
+    @DisplayName("체스판 생성 후 킹은 올바른 위치에 만들어진다.")
+    void shouldSuccessGenerateKing() {
+
+        Assertions.assertAll(
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(5, 1))).isInstanceOf(King.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(5, 8))).isInstanceOf(King.class)
+        );
+    }
+
+    @Test
+    @DisplayName("체스판 생성 후 퀸은 올바른 위치에 만들어진다.")
+    void shouldSuccessGenerateQueen() {
+
+        Assertions.assertAll(
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(4, 1))).isInstanceOf(Queen.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(4, 8))).isInstanceOf(Queen.class)
+        );
+    }
+
+    @Test
+    @DisplayName("체스판 생성 후 나이트는 올바른 위치에 만들어진다.")
+    void shouldSuccessGenerateKnight() {
+
+        Assertions.assertAll(
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(2, 1))).isInstanceOf(Knight.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(7, 1))).isInstanceOf(Knight.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(2, 8))).isInstanceOf(Knight.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(7, 8))).isInstanceOf(Knight.class)
+        );
+    }
+
+    @Test
+    @DisplayName("체스판 생성 후 비숍은 올바른 위치에 만들어진다.")
+    void shouldSuccessGenerateBishop() {
+
+        Assertions.assertAll(
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(3, 1))).isInstanceOf(Bishop.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(6, 1))).isInstanceOf(Bishop.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(3, 8))).isInstanceOf(Bishop.class),
+                () -> assertThat(chessBoard.getChessBoard().get(List.of(6, 8))).isInstanceOf(Bishop.class)
+        );
+    }
+
+    @Test
+    @DisplayName("체스판 생성 후 폰은 올바른 위치에 만들어진다.")
+    void shouldSuccessGeneratePawn() {
+        for (int i = 1; i <= 8; i++) {
+            assertThat(chessBoard.getChessBoard().get(List.of(i, 2))).isInstanceOf(Pawn.class);
+            assertThat(chessBoard.getChessBoard().get(List.of(i, 7))).isInstanceOf(Pawn.class);
+        }
+    }
+
+    @Test
+    @DisplayName("체스판 생성 후 기물이 없는 곳은 공백 클래스가 위치한다.")
+    void shouldSuccessSpaceIsEmpty() {
+        for (int i = 3; i <= 8; i++) {
+            for (int j = 3; j <= 6; j++) {
+                assertThat(chessBoard.getChessBoard().get(List.of(i, j))).isInstanceOf(Empty.class);
+            }
+        }
+    }
+}
