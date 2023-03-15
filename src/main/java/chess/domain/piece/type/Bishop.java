4cc1eb62ed039ffd50752e9ff88f53500f1c0bd6
shin-mallang <huipulci1@naver.com>
2023-03-15 21:11:08 +0900

    feat: Bishop 의 이동 조건 탐색 기능 구현

--- src/main/java/chess/domain/piece/type/Bishop.java
+++ src/main/java/chess/domain/piece/type/Bishop.java
@@ -2,11 +2,21 @@ package chess.domain.piece.type;
 
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import chess.domain.piece.position.WayPointsWithCondition;
+import chess.domain.piece.position.Path;
 import chess.domain.piece.position.PiecePosition;
 
 public class Bishop extends Piece {
 
     public Bishop(final Color color, final PiecePosition piecePosition) {
         super(color, piecePosition);
+    }
+
+    @Override
+    protected WayPointsWithCondition wayPointsWithCondition(final Path path) {
+        if (path.isDiagonal()) {
+            return WayPointsWithCondition.possible(path.wayPoints());
+        }
+        return WayPointsWithCondition.impossible();
     }
 }
