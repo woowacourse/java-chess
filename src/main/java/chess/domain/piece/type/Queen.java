c9e40519197f211766a9e911f4ebcdff36ac78b5
shin-mallang <huipulci1@naver.com>
2023-03-15 21:11:30 +0900

    feat: Queen 의 이동 조건 탐색 기능 구현

--- src/main/java/chess/domain/piece/type/Queen.java
+++ src/main/java/chess/domain/piece/type/Queen.java
@@ -2,11 +2,21 @@ package chess.domain.piece.type;
 
 import chess.domain.piece.Color;
 import chess.domain.piece.Piece;
+import chess.domain.piece.position.WayPointsWithCondition;
+import chess.domain.piece.position.Path;
 import chess.domain.piece.position.PiecePosition;
 
 public class Queen extends Piece {
 
     public Queen(final Color color, final PiecePosition piecePosition) {
         super(color, piecePosition);
+    }
+
+    @Override
+    protected WayPointsWithCondition wayPointsWithCondition(final Path path) {
+        if (path.isStraight() || path.isDiagonal()) {
+            return WayPointsWithCondition.possible(path.wayPoints());
+        }
+        return WayPointsWithCondition.impossible();
     }
 }
