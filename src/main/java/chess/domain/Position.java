1981857f1185b94add1dc23d236e060c768975b7
greeng00se <thegreengoose7@gmail.com>
2023-03-15 09:56:18 +0900

    feat: Position 생성 기능 추가
    Co-authored-by: iamjooon2 <iamjooon2@gmail.com>

---
+++ src/main/java/chess/domain/Position.java
@@ -0,0 +1,16 @@
+package chess.domain;
+
+public class Position {
+
+    private final Rank rank;
+    private final File file;
+
+    private Position(final Rank rank, final File file) {
+        this.rank = rank;
+        this.file = file;
+    }
+
+    public static Position of(final Rank rank, final File file) {
+        return new Position(rank, file);
+    }
+}
---
+++ src/test/java/chess/domain/PositionTest.java
@@ -0,0 +1,18 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class PositionTest {
+
+    @Test
+    void Rank_와_File_을_받아_정상적으로_생성된다() {
+        // expect
+        assertThatNoException().isThrownBy(() -> Position.of(Rank.A, File.ONE));
+    }
+}
