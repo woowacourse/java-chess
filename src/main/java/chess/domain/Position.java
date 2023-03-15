e2004e06c242a367806317153d82e6825181c888
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-15 22:35:28 +0900

    feat : 음수 검증 기능 삭제
    Co-authored-by: thdwoqor <celebarte01@gmail.com>

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -7,18 +7,16 @@ public class Position implements Comparable<Position> {
     private final int column;
 
     public Position(final int row, final int column) {
-        validate(row, column);
         this.row = row;
         this.column = column;
     }
 
-    private void validate(final int row, final int column) {
-        if (row < 0) {
-            throw new IllegalArgumentException("가로 위치는 0 미만일 수 없습니다.");
-        }
-        if (column < 0) {
-            throw new IllegalArgumentException("세로 위치는 0 미만일 수 없습니다.");
-        }
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
     }
 
     @Override
