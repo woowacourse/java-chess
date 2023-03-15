e65a856542eff70ef3920af983660a7c6fdb5ea7
drunkenhw <drunkenhw@gmail.com>
2023-03-15 13:54:21 +0900

    refactor: 출력 형식 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -9,15 +9,13 @@ import chess.domain.Square;
 public class OutputView {
 
     public void printBoard(Board board) {
-        for (int i = board.getRanks().size() - 1; i > -1; i--) {
-            Rank rank = board.getRanks().get(i);
+        for (Rank rank : board.getRanks()) {
             printRank(rank);
             System.out.println();
         }
-
     }
 
-    private static void printRank(Rank rank) {
+    private void printRank(Rank rank) {
         for (Square square : rank.getSquares()) {
             Piece piece = square.getPiece();
             PieceType pieceType = piece.getPieceType();
