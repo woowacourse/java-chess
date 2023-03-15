6bfb0eb18de37677e4811cd7326c8607b98bf7eb
songusika <sws981202@gmail.com>
2023-03-15 16:26:20 +0900

    test: 체스판이 규칙에 맞게 기물을 생성하는 기능 테스트
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/test/java/chess/domain/chessboard/ChessBoardTest.java
+++ src/test/java/chess/domain/chessboard/ChessBoardTest.java
@@ -1,9 +1,8 @@
 package chess.domain.chessboard;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
+import chess.domain.chessboard.state.piece.Rook;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -18,5 +17,18 @@ class ChessBoardTest {
                 .extracting("ranks")
                 .asInstanceOf(InstanceOfAssertFactories.list(Rank.class))
                 .hasSize(8);
+    }
+
+    @Test
+    void 체스판은_각_기물을_규칙에_맞게_배치한다() {
+        assertThat(new ChessBoard())
+                .extracting("ranks")
+                .asInstanceOf(InstanceOfAssertFactories.list(Rank.class))
+                .first()
+                .extracting("squares")
+                .asInstanceOf(InstanceOfAssertFactories.list(Square.class))
+                .first()
+                .extracting("pieceState")
+                .isInstanceOf(Rook.class);
     }
 }
