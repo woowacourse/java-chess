9390a7ff95844f54a0df27240e0916714a1056c7
woosung1223 <dntjd991223@naver.com>
2023-03-15 17:56:44 +0900

    feat: knight 생성

---
+++ src/main/java/domain/Knight.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.List;
+
+public class Knight implements NewPieceType {
+
+    private static final List<Double> availableInclinations = List.of(
+            0.5, -0.5, 2.0, -2.0
+    );
+    private static final List<Coordinate> availableCoordinateDifferences = List.of(
+            new Coordinate(1, 2),
+            new Coordinate(2, 1)
+    );
+
+    @Override
+    public boolean isReachableByRule(final Coordinate startCoordinate, final Coordinate endCoordinate) {
+        if (startCoordinate.equals(endCoordinate)) {
+            return false;
+        }
+        return availableInclinations.contains(startCoordinate.getInclination(endCoordinate)) &&
+                availableCoordinateDifferences.contains(startCoordinate.minusWithAbsoluteValue(endCoordinate));
+    }
+}
