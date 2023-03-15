edb371f564328e64da8e98867a21e3d8371f35d5
waterricecake <fruturum@nate.com>
2023-03-15 11:28:53 +0900

    feat(PieceType): 기물 생성 로직 enum화

--- src/main/java/chess/BoardInitialization.java
+++ src/main/java/chess/BoardInitialization.java
@@ -4,8 +4,18 @@ import chess.piece.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static chess.piece.PieceType.*;
 
 public class BoardInitialization {
+    private static final List<PieceType> ARRANGEMENT_WITHOUT_PAWN;
+
+    static {
+        ARRANGEMENT_WITHOUT_PAWN = List.of(
+              ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK
+        );
+    }
 
     public void initializeBoard(Chessboard chessboard) {
         setWhitePieces(chessboard);
@@ -14,28 +24,26 @@ public class BoardInitialization {
 
     private void setBlackPieces(Chessboard chessboard) {
         chessboard.putPiece(Rank.EIGHT, initializeWithoutPawn(Camp.BLACK));
-        chessboard.putPiece(Rank.SEVEN,initializePawn(Camp.BLACK));
+        chessboard.putPiece(Rank.SEVEN, initializePawn(Camp.BLACK));
     }
 
     private void setWhitePieces(Chessboard chessboard) {
-        chessboard.putPiece(Rank.TWO,initializePawn(Camp.WHITE));
+        chessboard.putPiece(Rank.TWO, initializePawn(Camp.WHITE));
         chessboard.putPiece(Rank.ONE, initializeWithoutPawn(Camp.WHITE));
     }
 
-    private List<Piece> initializeWithoutPawn(Camp camp){
-        return List.of(
-                new Rook(camp), new Knight(camp),new Bishop(camp),new Queen(camp),
-                new King(camp),new Bishop(camp),new Knight(camp),new Rook(camp)
-        );
+    private List<Piece> initializeWithoutPawn(Camp camp) {
+        return ARRANGEMENT_WITHOUT_PAWN.stream()
+                .map(pieceType -> pieceType.createPiece(camp))
+                .collect(Collectors.toList());
     }
 
-    private List<Piece> initializePawn(Camp camp){
+    private List<Piece> initializePawn(Camp camp) {
         List<Piece> pieces = new ArrayList<>();
-
         for (int i = 0; i < 8; i++) {
-            pieces.add(new Pawn(camp));
+            Piece pawn = PieceType.PAWN.createPiece(camp);
+            pieces.add(pawn);
         }
-
         return pieces;
     }
 }
