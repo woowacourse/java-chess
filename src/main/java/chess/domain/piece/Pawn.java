b9f6fd0fb7344cd6b809cc162d3995cf647c5400
seokjin8678 <seokjin8678@gmail.com>
2023-03-15 20:33:02 +0900

    feat: Pawn 이동 메서드 구현
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,5 +1,6 @@
 package chess.domain.piece;
 
+import chess.domain.Position;
 import chess.domain.Role;
 import chess.domain.Team;
 
@@ -7,5 +8,31 @@ public class Pawn extends Piece {
 
     public Pawn(Team team) {
         super(Role.PAWN, team);
+    }
+
+    @Override
+    public boolean canMove(Position source, Position target) {
+        if (source.getY() == target.getY()) {
+            return false;
+        }
+        int distance = source.getDistanceTo(target);
+        if (this.team == Team.WHITE) {
+            if (distance == 2 && source.getY() == 1) {
+                return true;
+            }
+            if(source.getY() > target.getY()) {
+                return false;
+            }
+        }
+        if (this.team == Team.BLACK) {
+            if (distance == 2 && source.getY() == 6) {
+                return true;
+            }
+
+            if(source.getY() < target.getY()) {
+                return false;
+            }
+        }
+        return distance == 1;
     }
 }
