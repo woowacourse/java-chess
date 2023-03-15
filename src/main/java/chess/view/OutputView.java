6536e2ad5b742ae203dab928d6ea04570ea1dfe2
yujamint <woawoa0513@gmail.com>
2023-03-15 14:09:23 +0900

    feat(OutputView): 게임 커맨드 입력 메시지 출력 기능 구현

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -5,6 +5,10 @@ public class OutputView {
         printMessage("체스 게임을 시작합니다.");
     }
 
+    public static void printGameCommandInputMessage() {
+        printMessage("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+    }
+
     private static void printMessage(String message) {
         System.out.println(message);
     }
