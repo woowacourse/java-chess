d1c8aeeba8db01a5d458f881c65f14925c7759e5
dev_kong <ahdjd5@gmail.com>
2023-03-15 21:12:29 +0900

    feat: 명령을 입력받는 기능 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,10 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static String requestCommand() {
+        return scanner.nextLine();
+    }
+}
