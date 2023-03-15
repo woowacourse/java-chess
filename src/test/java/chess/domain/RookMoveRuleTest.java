f0e85962f90a6e34f347983e9e622f30661e35fc
hum02 <ehdgma02@gmail.com>
2023-03-15 16:22:14 +0900

    test: 룩의 도착 지점까지의 중간경로 구하는 테스트

--- src/test/java/chess/domain/RookMoveRuleTest.java
+++ src/test/java/chess/domain/RookMoveRuleTest.java
@@ -1,6 +1,13 @@
 package chess.domain;
 
+import chess.domain.piece.moveRule.MoveRule;
+import chess.domain.piece.moveRule.RookMoveRule;
+import chess.domain.position.File;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,17 +20,19 @@ class RookMoveRuleTest {
         Position currentPosition = Position.of(File.FILE_A, Rank.RANK1);
         Position nextPosition = Position.of(File.FILE_B, Rank.RANK2);
 
-        assertThatThrownBy(() -> moveRule.move(currentPosition, nextPosition))
+        assertThatThrownBy(() -> moveRule.possibleRoute(currentPosition, nextPosition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("룩은 직선상으로만 움직일 수 있습니다.");
     }
 
     @Test
-    void 룩_무브_성공() {
+    void 룩의_중간경로를_반환한다() {
         MoveRule moveRule = new RookMoveRule();
         Position currentPosition = Position.of(File.FILE_A, Rank.RANK1);
-        Position nextPosition = Position.of(File.FILE_A, Rank.RANK2);
+        Position nextPosition = Position.of(File.FILE_D, Rank.RANK1);
 
-        assertThat(moveRule.move(currentPosition, nextPosition)).isEqualTo(nextPosition);
+        List<Position> routePositions = moveRule.possibleRoute(currentPosition, nextPosition);
+        List<Position> expected = List.of(Position.of(File.FILE_B, Rank.RANK1), Position.of(File.FILE_C, Rank.RANK1));
+        assertThat(routePositions.containsAll(expected)).isTrue();
     }
 }
