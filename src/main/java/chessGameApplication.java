185d5942aed26769c55dce8aea1a7b4e808a42f5
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-15 20:03:34 +0900

    feat: 게임 진행 중 커멘드 입력 기능 추가
    Co-authored-by: jjongwa <troas96@naver.com>

--- src/main/java/chessGameApplication.java
+++ src/main/java/chessGameApplication.java
@@ -3,10 +3,13 @@ import chess.ChessGame;
 import view.InputView;
 import view.OutputView;
 
+import java.util.List;
+
 public class chessGameApplication {
 
     public static void main(String[] args) {
         startPhase();
+        executePhase();
     }
 
     private static void startPhase() {
@@ -18,6 +21,21 @@ public class chessGameApplication {
         } catch (IllegalArgumentException e) {
             OutputView.printMessage(e.getMessage());
             startPhase();
+        }
+    }
+
+    private static void executePhase() {
+        try {
+            List<String> command = InputView.readPlayGameCommand();
+            if (command.size() == 1) {
+                System.out.println("end");
+            } else if (command.size() == 3) {
+                System.out.println("move");
+                // 무브 나머지 커맨드들이 들어왔는지 확인
+            }
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            executePhase();
         }
     }
 }
