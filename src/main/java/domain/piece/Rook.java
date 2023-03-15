2fe7be26f064ddd1b93a22930b386fa0d3af0426
MoonJeWoong <munjin0201@naver.com>
2023-03-15 21:26:26 +0900

    feat: rook 이동 경로 판단 기능 구현

--- src/main/java/domain/piece/Rook.java
+++ src/main/java/domain/piece/Rook.java
@@ -11,17 +11,24 @@ public final class Rook extends Piece {
     }
 
     @Override
-    public boolean isMovablePath(Position start, List<Position> path) {
-        return false;
+    public boolean isMovablePath (Position start, List<Position> path) {
+        return isMovableDirection(start, path.get(0)) &&
+                isMovableDistance(path.size());
     }
 
     @Override
     protected boolean isMovableDirection(Position start, Position nextPosition) {
+        if (start.moveDown().equals(nextPosition) ||
+                start.moveUp().equals(nextPosition) ||
+                start.moveRight().equals(nextPosition) ||
+                start.moveLeft().equals(nextPosition)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     protected boolean isMovableDistance(int distance) {
-        return false;
+        return true;
     }
 }
--- src/test/java/domain/piece/RookTest.java
+++ src/test/java/domain/piece/RookTest.java
@@ -3,8 +3,15 @@ package domain.piece;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import domain.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @DisplayName("Rook은 ")
 class RookTest {
@@ -33,5 +40,51 @@ class RookTest {
 
         // then
         assertThat(name).isEqualTo("r");
+    }
+
+    @ParameterizedTest
+    @MethodSource("isMovablePathTest_SuccessCase")
+    @DisplayName("상하좌우에 위치한 모든 칸으로 이동할 수 있다.")
+    void isMovablePathTest_Success(List<Position> path) {
+        // given
+        Rook rook = new Rook(Color.WHITE);
+
+        // when
+        boolean result = rook.isMovablePath(Position.of(3,3), path);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("isMovablePathTest_FailCase")
+    @DisplayName("대각선에 위치한 모든 칸으로 이동할 수 없다.")
+    void isMovablePathTest_Fail(List<Position> path) {
+        // given
+        Rook rook = new Rook(Color.WHITE);
+
+        // when
+        boolean result = rook.isMovablePath(Position.of(3,3), path);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    static Stream<Arguments> isMovablePathTest_SuccessCase() {
+        return Stream.of(
+                Arguments.of(List.of(Position.of(4,3), Position.of(5,3))),
+                Arguments.of(List.of(Position.of(3,4), Position.of(3,5))),
+                Arguments.of(List.of(Position.of(2,3), Position.of(1,3))),
+                Arguments.of(List.of(Position.of(3,2), Position.of(3,1)))
+        );
+    }
+
+    static Stream<Arguments> isMovablePathTest_FailCase() {
+        return Stream.of(
+                Arguments.of(List.of(Position.of(4,4), Position.of(5,5))),
+                Arguments.of(List.of(Position.of(2,4), Position.of(1,5))),
+                Arguments.of(List.of(Position.of(2,2), Position.of(1,1))),
+                Arguments.of(List.of(Position.of(4,2), Position.of(5,1)))
+        );
     }
 }
