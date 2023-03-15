76c269fd42b5207ee4b318f24f05ba1727741107
Youngho <cyh6099@gmail.com>
2023-03-15 21:50:34 +0900

    feat(Rook): 움직일 수 있는지 확인

---
+++ src/main/java/Rook.java
@@ -0,0 +1,22 @@
+public class Rook extends Piece {
+    protected Rook(Side side) {
+        super(side);
+    }
+
+    public static Rook createOfWhite() {
+        return new Rook(Side.WHITE);
+    }
+
+    public static Rook createOfBlack() {
+        return new Rook(Side.BLACK);
+    }
+
+    @Override
+    public boolean isMovable(Piece targetPiece, Position sourcePosition, Position targetPosition) {
+        Movement movement = sourcePosition.calculateMovement(targetPosition);
+        if (this.isSameSideWith(targetPiece)) {
+            return false;
+        }
+        return movement.isPerpendicular();
+    }
+}
--- src/test/java/RookTest.java
+++ src/test/java/RookTest.java
@@ -7,9 +7,36 @@ class RookTest {
 
     @DisplayName("source position이 target position까지 수직 위 방향이면 true를 반환한다. ")
     @Test
-    void shouldReturnTrueWhenRookDirectionIsDiagonal() {
+    void shouldReturnTrueWhenRookDirectionIsPerpendicular() {
         Rook whiteRook = Rook.createOfWhite();
         boolean movable = whiteRook.isMovable(new EmptyPiece(), Position.of("c", "2"), Position.of("c", "4"));
+
+        assertThat(movable).isTrue();
+    }
+
+    @DisplayName("source position이 target position까지 수직 방향이 아니면 false를 반환한다. ")
+    @Test
+    void shouldReturnFalseWhenRookDirectionIsNotPerpendicular() {
+        Rook whiteRook = Rook.createOfWhite();
+        boolean movable = whiteRook.isMovable(new EmptyPiece(), Position.of("c", "2"), Position.of("d", "4"));
+
+        assertThat(movable).isFalse();
+    }
+
+    @DisplayName("source position이 target position까지 수직 방향이고, target piece가 같은 진영이면 false를 반환한다. ")
+    @Test
+    void shouldReturnFalseWhenRookDirectionIsPerpendicularButTargetPieceIsSameSide() {
+        Rook whiteRook = Rook.createOfWhite();
+        boolean movable = whiteRook.isMovable(Bishop.createOfWhite(), Position.of("c", "2"), Position.of("c", "4"));
+
+        assertThat(movable).isFalse();
+    }
+
+    @DisplayName("source position이 target position까지 수직 방향이고, target piece가 다른 진영이면 true를 반환한다. ")
+    @Test
+    void shouldReturnTrueWhenRookDirectionIsPerpendicularAndTargetPieceIsOpponentSide() {
+        Rook blackRook = Rook.createOfBlack();
+        boolean movable = blackRook.isMovable(Bishop.createOfWhite(), Position.of("c", "2"), Position.of("c", "4"));
 
         assertThat(movable).isTrue();
     }
