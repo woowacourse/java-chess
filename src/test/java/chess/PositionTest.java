cbfcda9bb9567da9d20594c0aa99927417279576
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-15 21:44:15 +0900

    test: 포지션 형식 검증 테스트 추가
    Co-authored-by: jjongwa <troas96@naver.com>

--- src/test/java/chess/PositionTest.java
+++ src/test/java/chess/PositionTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,13 +14,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class PositionTest {
 
     @ParameterizedTest()
-    @ValueSource(strings = {"i1", "a0", "b9", "b0"})
+    @ValueSource(strings = {"i1", "a0", "b9", "b0"," ", "asd", "123", "", "ac1", "b78"})
     @DisplayName("move command의 source위치와 target위치가 체스판 좌표 형식에 맞지 않으면 예외처리한다.")
     void shouldFailMismatchPositionRegex(String input) {
         assertThatThrownBy(() -> Position.validatePositionRegex(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 이동 방향이 체스판 형식에 맞지 않습니다.");
     }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"h1", "a8", "b5", "c4", "a1", "h8", "a8"})
+    @DisplayName("move command의 source위치와 target위치가 체스판 좌표 형식에 맞으면 성공한다.")
+    void shouldSuccessMatchPositionRegex(String input) {
+        Assertions.assertDoesNotThrow(() -> Position.validatePositionRegex(input));
+    }
+
 
     @ParameterizedTest
     @CsvSource(value = {"b1, 2, 1", "c8, 3, 8", "a8, 1, 8", "h1, 8, 1", "h8, 8, 8", "c4, 3, 4", "a1, 1, 1"})
