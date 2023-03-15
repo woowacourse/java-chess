fbe410b63053b6fbeba49555926e9ed005c9e862
Youngho <cyh6099@gmail.com>
2023-03-15 20:38:19 +0900

    feat(Movement): 왼쪽 움직임인지 확인

--- src/main/java/Movement.java
+++ src/main/java/Movement.java
@@ -22,6 +22,10 @@ public class Movement {
         return this.fileIncrement > 0;
     }
 
+    public boolean isLeft() {
+        return this.fileIncrement < 0;
+    }
+
     public boolean isPerpendicular() {
         return this.rankIncrement == NON_INCREMENT || this.fileIncrement == NON_INCREMENT;
     }
