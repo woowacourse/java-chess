15710893a6744ddc6192db6a23727437adbf8203
yujamint <woawoa0513@gmail.com>
2023-03-15 14:33:41 +0900

    feat: 보드판 출력 기능 구현

--- src/main/java/chess/board/dto/BoardDto.java
+++ src/main/java/chess/board/dto/BoardDto.java
@@ -14,22 +14,26 @@ public class BoardDto {
         fillPieceNames(board);
     }
 
-    private void fillPieceNames(final Board board) {
-        List<Piece> pieces = board.getPieces();
-        for (Piece piece : pieces) {
-            final NameDto nameDto = new NameDto(piece);
-            final String pieceName = nameDto.getName();
-            final List<String> names = nameBoard.get(piece.getRank());
-            names.set(piece.getFile(), pieceName);
-        }
-    }
-
     private void initBoardSpaces() {
         final List<String> emptyNames = List.of(".", ".", ".", ".", ".", ".", ".", ".");
         for (int rowIndex = 0; rowIndex < 8; rowIndex++) {
             List<String> row = new ArrayList<>(emptyNames);
             nameBoard.add(row);
         }
+    }
+
+    private void fillPieceNames(final Board board) {
+        List<Piece> pieces = board.getPieces();
+        for (Piece piece : pieces) {
+            final NameDto nameDto = new NameDto(piece);
+            final String pieceName = nameDto.getName();
+            final List<String> names = nameBoard.get(getFlippedIndex(piece.getRank()));
+            names.set(getFlippedIndex(piece.getFile()), pieceName);
+        }
+    }
+
+    private int getFlippedIndex(int originalIndex) {
+        return Math.abs(originalIndex - nameBoard.size());
     }
 
     public List<List<String>> getNameBoard() {
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,5 +1,8 @@
 package chess.view;
 
+import chess.board.dto.BoardDto;
+import java.util.List;
+
 public class OutputView {
     public static void printGameStartMessage() {
         printMessage("체스 게임을 시작합니다.");
@@ -7,6 +10,15 @@ public class OutputView {
 
     public static void printGameCommandInputMessage() {
         printMessage("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+    }
+
+    public static void printBoard(BoardDto boardDto) {
+        final List<List<String>> nameBoard = boardDto.getNameBoard();
+
+        for (List<String> names : nameBoard) {
+            final String rowNames = String.join("", names);
+            System.out.println(rowNames);
+        }
     }
 
     private static void printMessage(String message) {
