f07027d1dfe98a7310de6ec8918b2cbc15d74c0b
drunkenhw <drunkenhw@gmail.com>
2023-03-15 20:29:07 +0900

    feat: Queen이 이동할 수 있는지 확인하는 기능 추가

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -10,7 +10,26 @@ public class Queen extends Piece {
 
     @Override
     boolean canMove(Position sourcePosition, Position targetPosition) {
-        return false;
+        int sourceColumnNumber = sourcePosition.getFileCoordinate().getColumnNumber();
+        int targetColumnNumber = targetPosition.getFileCoordinate().getColumnNumber();
+        int sourceRankNumber = sourcePosition.getRankCoordinate().getRowNumber();
+        int targetRankNumber = targetPosition.getRankCoordinate().getRowNumber();
+
+        boolean isSameFileCoordinate = sourcePosition.getFileCoordinate() == targetPosition.getFileCoordinate();
+        boolean isSameRankCoordinate = sourcePosition.getRankCoordinate() == targetPosition.getRankCoordinate();
+
+        return (isStraight(isSameFileCoordinate, isSameRankCoordinate) || isDiagonal(sourceColumnNumber,
+                targetColumnNumber, sourceRankNumber, targetRankNumber))
+                && !sourcePosition.equals(targetPosition);
+    }
+
+    private boolean isDiagonal(int sourceColumnNumber, int targetColumnNumber, int sourceRankNumber,
+                               int targetRankNumber) {
+        return Math.abs(sourceColumnNumber - targetColumnNumber) == Math.abs(sourceRankNumber - targetRankNumber);
+    }
+
+    private boolean isStraight(boolean isSameFileCoordinate, boolean isSameRankCoordinate) {
+        return (isSameFileCoordinate || isSameRankCoordinate);
     }
 
     @Override
