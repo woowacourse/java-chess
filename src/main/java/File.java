37153c4a80c1105d7702089b6683ccdf7bcb7d17
Youngho <cyh6099@gmail.com>
2023-03-15 19:18:57 +0900

    feat(File): 다음 값 반환

--- src/main/java/File.java
+++ src/main/java/File.java
@@ -25,6 +25,13 @@ public enum File {
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 file입니다."));
     }
 
+    public File getNext() {
+        return Arrays.stream(File.values())
+                .filter(file -> file.order == this.order + 1)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("인덱스를 벗어난 움직임입니다."));
+    }
+
     public String getText() {
         return text;
     }
