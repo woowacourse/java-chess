deee802bddbe9a748b6982f67123eab3d2d85473
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-15 22:29:22 +0900

    refactor : 시작 메시지 위치 변경
    Co-authored-by: thdwoqor <celebarte01@gmail.com>

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -9,7 +9,6 @@ public class InputView {
     }
 
     public static GameState inputGameState() {
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
         return GameState.from(scanner.nextLine());
     }
 
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -11,10 +11,11 @@ public class OutputView {
 
     public static void printStartMessage() {
         System.out.println("체스 게임을 시작합니다.");
+        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
     }
 
     public static void printChessBoard(final Map<Position, Piece> board) {
         final String chessBoard = BoardViewForm.createChessBoard(board);
-        System.out.println(chessBoard);
+        System.out.println(chessBoard + System.lineSeparator());
     }
 }
