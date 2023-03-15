4edb0f10c325bb959e843fbaec5f6afcd5fd7937
Miseong Kim <miseongk@naver.com>
2023-03-15 15:45:02 +0900

    feat(Queen): Queen 기능 구현

---
+++ src/main/java/chess/domain/piece/Queen.java
@@ -0,0 +1,52 @@
+package chess.domain.piece;
+
+import chess.domain.Position;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queen extends Piece {
+
+    @Override
+    boolean isMovable(Position source, Position target) {
+        int fileDiff = source.fileDiff(target);
+        int rankDiff = source.rankDiff(target);
+
+        return (Math.abs(fileDiff) == Math.abs(rankDiff))
+                || (fileDiff == 0 || rankDiff == 0);
+    }
+
+    @Override
+    List<Position> findPath(Position source, Position target) {
+        int rankDiff = source.rankDiff(target);
+        int fileDiff = source.fileDiff(target);
+
+        int count = initCount(rankDiff, fileDiff);
+        int rankUnit = rankDiff / count;
+        int fileUnit = fileDiff / count;
+
+        return makePath(count, rankUnit, fileUnit, source);
+    }
+
+    private int initCount(int rankDiff, int fileDiff) {
+        int count = 1;
+        if (Math.abs(fileDiff) == Math.abs(rankDiff)) {
+            count = Math.abs(fileDiff);
+        }
+        if (fileDiff == 0 || rankDiff == 0) {
+            count = Math.abs(fileDiff + rankDiff);
+        }
+
+        return count;
+    }
+
+    private List<Position> makePath(int count, int rankUnit, int fileUnit, Position current) {
+        List<Position> path = new ArrayList<>();
+
+        for(int i = 0; i < count; i++) {
+            current = current.moveBy(rankUnit, fileUnit);
+            path.add(current);
+        }
+
+        return path;
+    }
+}
---
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -0,0 +1,82 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.Position;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class QueenTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:3", "5:4", "3:5"}, delimiter = ':')
+    @DisplayName("퀸은 상하좌우, 대각선으로 움직일 수 있다.")
+    void isMovable(int rank, int file) {
+        Piece queen = new Queen();
+        Position source = new Position(4, 4);
+        Position target = new Position(rank, file);
+        assertThat(queen.isMovable(source, target)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2:3", "7:5", "3:0"}, delimiter = ':')
+    @DisplayName("퀸은 상하좌우, 대각선을 제외한 곳으로 움직일 수 없다.")
+    void isUnmovable(int rank, int file) {
+        Piece queen = new Queen();
+        Position source = new Position(4, 4);
+        Position target = new Position(rank, file);
+        assertThat(queen.isMovable(source, target)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("findPathProvider")
+    void findPath(Position source, Position target, List<Position> expectedPath) {
+        Piece queen = new Queen();
+        assertThat(queen.findPath(source, target)).containsAll(expectedPath);
+    }
+
+    static Stream<Arguments> findPathProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new Position(4, 4),
+                        new Position(6, 4),
+                        List.of(
+                                new Position(5, 4),
+                                new Position(6, 4)
+                        )
+                ),
+                Arguments.of(
+                        new Position(2, 1),
+                        new Position(2, 6),
+                        List.of(
+                                new Position(2, 2),
+                                new Position(2, 3),
+                                new Position(2, 4),
+                                new Position(2, 5),
+                                new Position(2, 6)
+                        )
+                ),
+                Arguments.of(
+                        new Position(4, 4),
+                        new Position(6, 6),
+                        List.of(
+                                new Position(5, 5),
+                                new Position(6, 6)
+                        )
+                ),
+                Arguments.of(
+                        new Position(4, 4),
+                        new Position(2, 6),
+                        List.of(
+                                new Position(3, 5),
+                                new Position(2, 6)
+                        )
+                )
+        );
+    }
+}
