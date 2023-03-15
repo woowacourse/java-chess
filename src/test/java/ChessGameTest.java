6ef4b94df048e7ef1711f44f62f36f93017fceaa
the9kim <96895686+the9kim@users.noreply.github.com>
2023-03-15 17:03:31 +0900

    test: 체스 게임 생성 테스트
    Co-authored-by: jjongwa <troas96@naver.com>

---
+++ src/test/java/ChessGameTest.java
@@ -0,0 +1,15 @@
+import chess.ChessBoard;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessGameTest {
+
+    @Test
+    @DisplayName("체스 게임을 올바르게 생성한다.")
+    void shouldSuccessGenerateChessGame() {
+        assertDoesNotThrow(() -> new ChessGame());
+    }
+}
