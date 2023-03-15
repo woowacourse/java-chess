6cabcf3df0b5854e55f69ce268cf66fefbae7263
greeng00se <thegreengoose7@gmail.com>
2023-03-15 20:03:09 +0900

    refactor: 퀸 이동 확인 기능 구현
    Co-authored-by: iamjooon2 <iamjooon2@gmail.com>

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -8,6 +8,7 @@ public class Queen extends Piece {
 
     private static final Queen WHITE = new Queen(Color.WHITE);
     private static final Queen BLACK = new Queen(Color.BLACK);
+    private static final int VALID_GAP = 0;
 
     private Queen(final Color color) {
         super(color, QUEEN);
@@ -22,11 +23,22 @@ public class Queen extends Piece {
 
     @Override
     protected boolean isValidMove(final Position start, final Position end) {
-        return false;
+        final int fileGap = Math.abs(start.calculateFileGap(end));
+        final int rankGap = Math.abs(start.calculateRankGap(end));
+
+        return canMoveStraight(fileGap, rankGap) || canMoveDiagonal(fileGap, rankGap);
+    }
+
+    private boolean canMoveStraight(final int fileGap, final int rankGap) {
+        return fileGap == VALID_GAP || rankGap == VALID_GAP;
+    }
+
+    private boolean canMoveDiagonal(final int fileGap, final int rankGap) {
+        return fileGap == rankGap;
     }
 
     @Override
     protected boolean isValidTarget(final Piece target) {
-        return false;
+        return color() != target.color();
     }
 }
