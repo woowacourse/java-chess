030288178eef1a2c614e86deb5766c0845c68fdf
MoonJeWoong <munjin0201@naver.com>
2023-03-15 21:27:22 +0900

    feat: queen 이동 경로 판단 기능 구현

--- src/test/java/domain/piece/QueenTest.java
+++ src/test/java/domain/piece/QueenTest.java
@@ -3,8 +3,15 @@ package domain.piece;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import domain.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @DisplayName("Queen은 ")
 class QueenTest {
@@ -35,4 +42,30 @@ class QueenTest {
         assertThat(name).isEqualTo("q");
     }
 
+    @ParameterizedTest
+    @MethodSource("isMovablePathTest_SuccessCase")
+    @DisplayName("모든 방향에 위치한 모든 칸으로 이동할 수 있다.")
+    void isMovablePathTest_Success(List<Position> path) {
+        // given
+        Queen queen = new Queen(Color.WHITE);
+
+        // when
+        boolean result = queen.isMovablePath(Position.of(3,3), path);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    static Stream<Arguments> isMovablePathTest_SuccessCase() {
+        return Stream.of(
+                Arguments.of(List.of(Position.of(4,3), Position.of(5,3))),
+                Arguments.of(List.of(Position.of(3,4), Position.of(3,5))),
+                Arguments.of(List.of(Position.of(2,3), Position.of(1,3))),
+                Arguments.of(List.of(Position.of(3,2), Position.of(3,1))),
+                Arguments.of(List.of(Position.of(4,4), Position.of(5,5))),
+                Arguments.of(List.of(Position.of(2,4), Position.of(1,5))),
+                Arguments.of(List.of(Position.of(2,2), Position.of(1,1))),
+                Arguments.of(List.of(Position.of(4,2), Position.of(5,1)))
+        );
+    }
 }
