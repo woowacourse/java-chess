56aa52854a05100f669d7e577818898fdc148b4a
Miseong Kim <miseongk@naver.com>
2023-03-16 17:58:02 +0900

    feat(OutputView): 출력 기능 구현

---
+++ src/main/java/chess/constant/GameCommand.java
@@ -0,0 +1,8 @@
+package chess.constant;
+
+public class GameCommand {
+
+    public static final String START = "start";
+    public static final String MOVE = "move";
+    public static final String END = "end";
+}
---
+++ src/main/java/chess/view/OutputView.java
@@ -0,0 +1,80 @@
+package chess.view;
+
+import static chess.view.PieceView.*;
+
+import chess.domain.Board;
+import chess.domain.Position;
+import chess.domain.piece.Bishop;
+import chess.domain.piece.King;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Piece;
+import chess.domain.piece.Queen;
+import chess.domain.piece.Rook;
+import java.util.Map;
+
+public class OutputView {
+
+    private static final int MIN_RANK = 1;
+    private static final int MAX_RANK = 8;
+    private static final int MIN_FILE = 1;
+    private static final int MAX_FILE = 8;
+
+    public void printStartMessage() {
+        System.out.println("> 체스 게임을 시작합니다.\n"
+                + "> 게임 시작 : start\n"
+                + "> 게임 종료 : end\n"
+                + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+    }
+
+    public void printBoard(Board board) {
+        Map<Position, Piece> chessBoard = board.getBoard();
+
+        for (int rank = MAX_RANK; rank >= MIN_RANK; rank--) {
+            iterateFile(chessBoard, rank);
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private void iterateFile(Map<Position, Piece> chessBoard, int rank) {
+        for (int file = MIN_FILE; file <= MAX_FILE; file++) {
+            printBoardUnit(chessBoard, rank, file);
+        }
+    }
+
+    private void printBoardUnit(Map<Position, Piece> chessBoard, int rank, int file) {
+        if (!chessBoard.containsKey(new Position(rank, file))) {
+            System.out.print(".");
+        }
+        if (chessBoard.containsKey(new Position(rank, file))) {
+            Piece piece = chessBoard.get(new Position(rank, file));
+            printPiece(piece);
+        }
+    }
+
+    private void printPiece(Piece piece) {
+        if (piece.getClass() == King.class) {
+            System.out.print(KING.getPieceView(piece.getTeam()));
+        }
+        if (piece.getClass() == Queen.class) {
+            System.out.print(QUEEN.getPieceView(piece.getTeam()));
+        }
+        if (piece.getClass() == Knight.class) {
+            System.out.print(KNIGHT.getPieceView(piece.getTeam()));
+        }
+        if (piece.getClass() == Bishop.class) {
+            System.out.print(BISHOP.getPieceView(piece.getTeam()));
+        }
+        if (piece.getClass() == Rook.class) {
+            System.out.print(ROOK.getPieceView(piece.getTeam()));
+        }
+        if (piece.getClass() == Pawn.class) {
+            System.out.print(PAWN.getPieceView(piece.getTeam()));
+        }
+    }
+
+    public void printExceptionMessage(Exception exception) {
+        System.out.println(exception.getMessage());
+    }
+}
