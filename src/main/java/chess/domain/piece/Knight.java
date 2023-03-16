e2d4ef09cbf0988d96a8cb85cc74b5800fd5bd7e
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-16 11:48:51 +0900

    feat: 나이트의 움직임 검증

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,8 +1,19 @@
 package chess.domain.piece;
 
+import chess.domain.position.Position;
+
 public final class Knight extends Piece {
 
     public Knight(Camp camp) {
         super(camp, PieceSymbol.KNIGHT);
+    }
+
+    @Override
+    public boolean isMovable(Position from, Position to) {
+        int rankGap = from.calculateFileGap(to);
+        int fileGap = from.calculateRankGap(to);
+
+        return (Math.abs(rankGap) == 1 && Math.abs(fileGap) == 2)
+                || (Math.abs(rankGap) == 2 && Math.abs(fileGap) == 1);
     }
 }
---
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -0,0 +1,25 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import chess.domain.position.File;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KnightTest {
+
+    @Test
+    @DisplayName("나이트는 L자 모양으로 움직일 수 있다.")
+    void isMovable() {
+        Piece knight = new Knight(Camp.WHITE);
+
+        Position from = new Position(File.A, Rank.ONE);
+        Position to = new Position(File.C, Rank.TWO);
+
+        boolean result = knight.isMovable(from, to);
+
+        assertThat(result).isTrue();
+    }
+}
