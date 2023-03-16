c7bb635e7f578ecd8878ba03ec1be799113f77ce
songusika <sws981202@gmail.com>
2023-03-16 16:40:36 +0900

    feat: 킹이 도착지로 갈 수 있는지 검증하는 기능 구현
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/main/java/chess/domain/chessboard/state/piece/King.java
+++ src/main/java/chess/domain/chessboard/state/piece/King.java
@@ -1,10 +1,39 @@
 package chess.domain.chessboard.state.piece;
 
+import chess.domain.chessboard.Coordinate;
 import chess.domain.chessboard.state.Team;
+import java.util.List;
 
 public final class King extends Piece {
 
     public King(final Team team) {
         super(team);
+    }
+
+    @Override
+    public List<Coordinate> findRoute (final Coordinate from, final Coordinate to){
+        validatePossibleDestination(from, to);
+
+        if (from.isPositiveDiagonal(to)) {
+            return positiveDiagonalRoute(from, to);
+        }
+
+        if (from.isSameFile(to)) {
+            return verticalRoute(from, to);
+        }
+
+        if (from.isNegativeDiagonal(to)) {
+            return negativeDiagonalRoute(from, to);
+        }
+        return horizontalRoute(from, to);
+    }
+
+    private void validatePossibleDestination(final Coordinate from, final Coordinate to) {
+        final int fileDistance = from.calculateFileDistance(to);
+        final int rankDistance = from.calculateRankDistance(to);
+
+        if (fileDistance >1 || rankDistance > 1) {
+            throwCanNotMoveException();
+        }
     }
 }
