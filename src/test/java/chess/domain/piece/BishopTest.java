27ca6510aa3a0045e2e0eb50c7b4a57384798c9a
jiwon <ljwon77@daum.net>
2023-03-16 18:13:07 +0900

    test: 각 말이 이동 가능한지 테스트한다
    Co-authored-by: yenawee <yaena0319@naver.com>

---
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -0,0 +1,41 @@
+package chess.domain.piece;
+
+import chess.domain.camp.CampType;
+import chess.domain.move.BishopMove;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BishopTest {
+
+    @ParameterizedTest(name = "비숍이 시작 위치에서 타겟 위치로 이동 가능하면 true를 반환한다")
+    @CsvSource(value = {"1:1", "7:7", "1:3", "3:1", "3:3"}, delimiter = ':')
+    void canMoveSuccess(final int targetRank, final int targetFile) {
+        // given
+        final Piece piece = new Bishop(PieceType.BISHOP, CampType.WHITE, new BishopMove());
+
+        // when
+        boolean actual = piece.canMove(new Position(2, 2),
+                new Position(targetRank, targetFile));
+
+        // then
+        assertThat(actual)
+                .isTrue();
+    }
+
+    @ParameterizedTest(name = "비숍이 시작 위치에서 타겟 위치로 이동 불가능하면 false를 반환한다")
+    @CsvSource(value = {"1:2", "0:8", "-8:0", "8:0", "0:-8", "0:1", "1:0", "8:8"}, delimiter = ':')
+    void canMoveFail(final int targetRank, final int targetFile) {
+        // given
+        final Piece piece = new Bishop(PieceType.BISHOP, CampType.WHITE, new BishopMove());
+
+        // when
+        boolean actual = piece.canMove(new Position(0, 0),
+                new Position(targetRank, targetFile));
+
+        // then
+        assertThat(actual)
+                .isFalse();
+    }
+}
---
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -0,0 +1,41 @@
+package chess.domain.piece;
+
+import chess.domain.camp.CampType;
+import chess.domain.move.KingMove;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KingTest {
+
+    @ParameterizedTest(name = "킹이 시작 위치에서 타겟 위치로 이동 가능하면 true를 반환한다")
+    @CsvSource(value = {"0:1", "1:0", "1:2", "2:1", "0:2", "0:0", "2:0", "2:2"}, delimiter = ':')
+    void canMoveSuccess(final int targetRank, final int targetFile) {
+        // given
+        final Piece piece = new King(PieceType.KING, CampType.WHITE, new KingMove());
+
+        // when
+        boolean actual = piece.canMove(new Position(1, 1),
+                new Position(targetRank, targetFile));
+
+        // then
+        assertThat(actual)
+                .isTrue();
+    }
+
+    @ParameterizedTest(name = "킹이 시작 위치에서 타겟 위치로 이동 불가능하면 false를 반환한다")
+    @CsvSource(value = {"1:3", "0:8", "-8:0", "8:0", "0:-8", "3:1", "-1:1", "1:-2", "8:8"}, delimiter = ':')
+    void canMoveFail(final int targetRank, final int targetFile) {
+        // given
+        final Piece piece = new King(PieceType.KING, CampType.WHITE, new KingMove());
+
+        // when
+        boolean actual = piece.canMove(new Position(1, 1),
+                new Position(targetRank, targetFile));
+
+        // then
+        assertThat(actual)
+                .isFalse();
+    }
+}
