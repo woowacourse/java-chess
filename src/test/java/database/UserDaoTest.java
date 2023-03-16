1ae6c28beacc80c89fef8486aa50c219117480da
nuyh <hjaehyun25@gmail.com>
2023-03-16 16:55:37 +0900

    feat: JDBC CRUD 구현

---
+++ src/test/java/database/UserDaoTest.java
@@ -0,0 +1,71 @@
+package database;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class UserDaoTest {
+
+    private final UserDao userDao = new UserDao();
+
+    @BeforeEach
+    void init() {
+        userDao.deleteAll();
+    }
+
+    @Test
+    void connection() {
+        try (final var connection = userDao.getConnection()) {
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void addUser() {
+        final var user = new User("testUserId", "testUser");
+        userDao.addUser(user);
+    }
+
+    @Test
+    void findByUserId() {
+        //given
+        final User user = new User("연어", "연어");
+        userDao.addUser(user);
+
+        //when
+        final User foundUser = userDao.findByUserId("연어");
+
+        //then
+        assertThat(foundUser).isEqualTo(new User("연어", "연어"));
+    }
+
+    @Test
+    void updateUserName() {
+        //given
+        final User user = new User("연어", "연어");
+        userDao.addUser(user);
+
+        //when
+        userDao.updateUserName("연어", "먹고 싶다");
+
+        //then
+        assertThat(userDao.findByUserId("연어")).isEqualTo(new User("연어", "먹고 싶다"));
+    }
+
+    @Test
+    void deleteByUserId() {
+        final User user = new User("연어", "연어");
+        userDao.addUser(user);
+
+        //when
+        userDao.deleteByUserId("연어");
+
+        //then
+        assertThat(userDao.findByUserId("연어")).isNull();
+    }
+}
