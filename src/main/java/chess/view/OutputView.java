cc13f950e246e142e75735b8370ba3c2a5046836
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-16 17:47:38 +0900

    refactor : 게임 시작 메시지 수정
    Co-authored-by: thdwoqor <celebarte01@gmail.com>

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,7 +1,7 @@
 package chess.view;
 
-import chess.domain.Piece;
 import chess.domain.Position;
+import chess.domain.piece.Piece;
 
 import java.util.Map;
 
@@ -10,8 +10,12 @@ public class OutputView {
     }
 
     public static void printStartMessage() {
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.print(
+                "> 체스 게임을 시작합니다." + System.lineSeparator() +
+                        "> 게임 시작 : start" + System.lineSeparator() +
+                        "> 게임 종료 : end" + System.lineSeparator() +
+                        "> 게임 이동 : move source위치 target위치 - 예. move b2 b3" + System.lineSeparator()
+        );
     }
 
     public static void printChessBoard(final Map<Position, Piece> board) {
