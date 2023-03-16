72725e0d94831e46f9db41cfc85a1c19cf2a2cd8
yujamint <woawoa0513@gmail.com>
2023-03-16 14:02:38 +0900

    feat(Bishop): 이동 가능 여부 반환 기능 구현

--- src/main/java/chess/piece/Bishop.java
+++ src/main/java/chess/piece/Bishop.java
@@ -1,10 +1,30 @@
 package chess.piece;
 
 import chess.board.Position;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bishop extends Piece {
 
+    private final List<Direction> directions;
+
     public Bishop(final Position position, final Side side) {
         super(position, side);
+        this.directions = initDirections();
+    }
+
+    private List<Direction> initDirections() {
+        final List<Direction> directions = new ArrayList<>();
+        directions.add(Direction.UP_RIGHT);
+        directions.add(Direction.UP_LEFT);
+        directions.add(Direction.DOWN_RIGHT);
+        directions.add(Direction.DOWN_LEFT);
+        return directions;
+    }
+
+    @Override
+    public boolean isMovable(Position targetPosition) {
+        final Direction direction = position.getDirectionTo(targetPosition);
+        return directions.contains(direction);
     }
 }
