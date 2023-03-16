9fe7c9a85284d0dd404e9605a1585d7f3bc01d59
junpakPark <junpak.park@gmail.com>
2023-03-16 15:29:44 +0900

    refactor : initPawn으로 변경

--- src/main/java/domain/chessboard/InitRank.java
+++ src/main/java/domain/chessboard/InitRank.java
@@ -8,9 +8,9 @@ import java.util.List;
 public enum InitRank {
 
     OTHERS_BLACK(List.of(new Rook(Color.BLACK), new Knight(Color.BLACK), new Bishop(Color.BLACK), new Queen(Color.BLACK), new King(Color.BLACK), new Bishop(Color.BLACK), new Knight(Color.BLACK), new Rook(Color.BLACK))),
-    PAWN_BLACK(List.of(new Pawn(Color.BLACK), new Pawn(Color.BLACK), new Pawn(Color.BLACK), new Pawn(Color.BLACK), new Pawn(Color.BLACK), new Pawn(Color.BLACK), new Pawn(Color.BLACK), new Pawn(Color.BLACK))),
+    PAWN_BLACK(List.of(new InitPawn(Color.BLACK), new InitPawn(Color.BLACK), new InitPawn(Color.BLACK), new InitPawn(Color.BLACK), new InitPawn(Color.BLACK), new InitPawn(Color.BLACK), new InitPawn(Color.BLACK), new InitPawn(Color.BLACK))),
     EMPTY(List.of(new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty())),
-    PAWN_WHITE(List.of(new Pawn(Color.WHITE), new Pawn(Color.WHITE), new Pawn(Color.WHITE), new Pawn(Color.WHITE), new Pawn(Color.WHITE), new Pawn(Color.WHITE), new Pawn(Color.WHITE), new Pawn(Color.WHITE))),
+    PAWN_WHITE(List.of(new InitPawn(Color.WHITE), new InitPawn(Color.WHITE), new InitPawn(Color.WHITE), new InitPawn(Color.WHITE), new InitPawn(Color.WHITE), new InitPawn(Color.WHITE), new InitPawn(Color.WHITE), new InitPawn(Color.WHITE))),
     OTHERS_WHITE(List.of(new Rook(Color.WHITE), new Knight(Color.WHITE), new Bishop(Color.WHITE), new Queen(Color.WHITE), new King(Color.WHITE), new Bishop(Color.WHITE), new Knight(Color.WHITE), new Rook(Color.WHITE)));
 
     private final List<SquareStatus> squareStatus;
