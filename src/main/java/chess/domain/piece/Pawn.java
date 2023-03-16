04e45e93410b40fed9439149920e27ef8475dbe7
kokodak <kokodakadokok@gmail.com>
2023-03-16 12:25:40 +0900

    feat: 폰이 갈 수 있는 위치 반환 기능 오버라이딩
    Co-authored-by: parkmuhyeun <pjhg410@gmail.com>

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,26 +1,97 @@
 package chess.domain.piece;
 
+import chess.domain.Board;
+import chess.domain.Position;
 import chess.domain.Side;
 import chess.domain.Type;
-import chess.domain.movepattern.KnightMovePattern;
 import chess.domain.movepattern.MovePattern;
 import chess.domain.movepattern.PawnMovePattern;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece {
 
     private final List<MovePattern> movePatterns;
+    private final boolean moved;
 
     public Pawn(final Type type, final Side side) {
         super(type, side);
-        movePatterns = Arrays.asList(PawnMovePattern.values());
+        //TODO this 추가
+        movePatterns = initMovePatterns(side);
+        moved = false;
+    }
+
+    private List<MovePattern> initMovePatterns(final Side side) {
+        List<MovePattern> movePatterns = new ArrayList<>();
+        if (side.isWhite()) {
+            movePatterns.add(PawnMovePattern.UP);
+            movePatterns.add(PawnMovePattern.LEFT_TOP);
+            movePatterns.add(PawnMovePattern.RIGHT_TOP);
+        }
+        if (side.isBlack()) {
+            movePatterns.add(PawnMovePattern.DOWN);
+            movePatterns.add(PawnMovePattern.LEFT_BOTTOM);
+            movePatterns.add(PawnMovePattern.RIGHT_BOTTOM);
+        }
+        return movePatterns;
     }
 
     @Override
     protected void validate(final Type type, final Side side) {
         validateType(type);
         validateSide(side);
+    }
+
+    @Override
+    public List<Position> findMovablePositions(final Position source, final Board board) {
+        final List<Position> movablePositions = new ArrayList<>();
+        final List<MovePattern> movePatterns = getMovePatterns();
+        for (MovePattern movePattern : movePatterns) {
+            Position nextPosition = source;
+            if (isRangeValid(nextPosition, movePattern)) {
+                nextPosition = nextPosition.move(movePattern);
+                checkSide(movablePositions, source, nextPosition, board);
+            }
+            if (!isDiagonal(source, source.move(movePattern)) && isRangeValid(source, movePattern) && isRangeValid(
+                    source.move(movePattern), movePattern) && !moved) {
+                movablePositions.add(source.move(movePattern).move(movePattern));
+            }
+        }
+        return movablePositions;
+    }
+
+    private void checkSide(final List<Position> movablePositions, final Position source, final Position nextPosition,
+                           final Board board) {
+        final Side nextSide = board.findSideByPosition(nextPosition);
+        if (isDiagonal(source, nextPosition)) {
+            checkDiagonalSide(movablePositions, nextPosition, nextSide);
+            return;
+        }
+        checkFrontSide(movablePositions, nextPosition, nextSide);
+    }
+
+    private boolean isDiagonal(Position source, Position nextPosition) {
+        return Math.abs((source.getRank() - nextPosition.getRank())) == Math.abs((source.getFile() - nextPosition.getFile()));
+    }
+
+    private void checkFrontSide(final List<Position> movablePositions, final Position nextPosition,
+                                final Side nextSide) {
+        if (nextSide == Side.NEUTRALITY) {
+            movablePositions.add(nextPosition);
+        }
+    }
+
+    private void checkDiagonalSide(final List<Position> movablePositions, final Position nextPosition,
+                                   final Side nextSide) {
+        if (nextSide != this.side && nextSide != Side.NEUTRALITY) {
+            movablePositions.add(nextPosition);
+        }
+    }
+
+    private boolean isRangeValid(final Position position, final MovePattern movePattern) {
+        final int nextRank = position.getRank() + movePattern.getRankVector();
+        final int nextFile = position.getFile() + movePattern.getFileVector();
+        return nextRank >= 1 && nextRank <= 8 && nextFile >= 1 && nextFile <= 8;
     }
 
     @Override
