a0b80beee5f86cfe5f015ddb5716030f4119521c
hum02 <ehdgma02@gmail.com>
2023-03-16 15:32:19 +0900

    feat: 나이트의 보드 위 이동 로직 구현

--- src/main/java/chess/domain/piece/moveRule/KnightMoveRule.java
+++ src/main/java/chess/domain/piece/moveRule/KnightMoveRule.java
@@ -1,19 +1,37 @@
 package chess.domain.piece.moveRule;
 
+import chess.domain.piece.Piece;
 import chess.domain.piece.PieceType;
 import chess.domain.position.Position;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Map;
 
 public class KnightMoveRule implements MoveRule {
 
+    private static KnightMoveRule instance;
+    private KnightMoveRule() {}
+
+    public static KnightMoveRule getInstance() {
+        if (instance == null) {
+            instance = new KnightMoveRule();
+        }
+        return instance;
+    }
+
     @Override
-    public List<Position> possibleRoute(Position currentPosition, Position nextPosition) {
+    public void move(Position currentPosition, Position nextPosition,  Map<Position, Piece> board) {
         if (!currentPosition.isKnightPosition(nextPosition)) {
             throw new IllegalArgumentException("나이트는 L 모양으로만 움직일 수 있습니다.");
         }
-        return Collections.emptyList();
+
+        Piece pieceOfCurrentPosition = board.get(currentPosition);
+        Piece pieceOfNextPosition = board.get(nextPosition);
+
+        if(board.containsKey(nextPosition) && !pieceOfCurrentPosition.isOpponent(pieceOfNextPosition)){
+            throw new IllegalArgumentException("도착 지점에 아군 기물이 있어 움직일 수 없습니다.");
+        }
+        Piece movingPiece = board.remove(currentPosition);
+        board.put(nextPosition, movingPiece);
     }
 
     @Override
--- src/test/java/chess/domain/piece/moveRule/KnightMoveRuleTest.java
+++ src/test/java/chess/domain/piece/moveRule/KnightMoveRuleTest.java
@@ -1,23 +1,33 @@
 package chess.domain.piece.moveRule;
 
+import chess.domain.piece.Color;
+import chess.domain.piece.Piece;
 import chess.domain.position.File;
 import chess.domain.position.Position;
 import chess.domain.position.Rank;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class KnightMoveRuleTest {
-
     @Test
     void 나이트_움직임_실패() {
-        MoveRule moveRule = new KnightMoveRule();
-        Position currentPosition = Position.of(File.FILE_B, Rank.RANK1);
-        Position nextPosition = Position.of(File.FILE_B, Rank.RANK2);
+        MoveRule moveRule = KnightMoveRule.getInstance();
+        Map<Position, Piece> board = new HashMap<>();
 
-        assertThatThrownBy(() -> moveRule.possibleRoute(currentPosition, nextPosition))
+        Position currentPosition = Position.of(File.FILE_A, Rank.RANK1);
+        Piece startPiece = new Piece(moveRule, Color.BLACK);
+        board.put(currentPosition, startPiece);
+
+        Position nextPosition = Position.of(File.FILE_B, Rank.RANK2);
+        Piece endPiece = new Piece(moveRule, Color.WHITE);
+        board.put(nextPosition, endPiece);
+
+        assertThatThrownBy(() -> moveRule.move(currentPosition, nextPosition, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("나이트는 L 모양으로만 움직일 수 있습니다.");
     }
-
 }
