6804f90b62c2bb1a1e7d6d6951ff85d32c12f63d
yujamint <woawoa0513@gmail.com>
2023-03-16 21:46:56 +0900

    feat(Board): 기물 이동 기능 구현

--- src/main/java/chess/board/Board.java
+++ src/main/java/chess/board/Board.java
@@ -12,11 +12,48 @@ public class Board {
         this.pieces = pieces;
     }
 
-    public List<Piece> getPieces() {
-        return pieces.getPieces();
+    public void movePiece(Position sourcePosition, Position targetPosition) {
+        final Piece sourcePiece = pieces.findPieceByPosition(sourcePosition);
+        checkPieceMovable(targetPosition, sourcePiece);
+        checkPath(targetPosition, sourcePiece);
+        checkTargetPosition(targetPosition, sourcePiece);
+        final Piece movedPiece = sourcePiece.move(targetPosition);
+        pieces.synchronizeMovedPiece(sourcePiece, movedPiece);
     }
 
-    public Piece findPieceByPosition(final Position position) {
-        return pieces.findPieceByPosition(position);
+    private void checkPieceMovable(final Position targetPosition, final Piece sourcePiece) {
+        if (!sourcePiece.isMovable(targetPosition)) {
+            throw new IllegalArgumentException("[ERROR] 해당 기물은 대상 위치로 이동할 수 없습니다.");
+        }
+    }
+
+    private void checkPath(final Position targetPosition, final Piece sourcePiece) {
+        final List<Position> paths = sourcePiece.getPaths(targetPosition);
+        for (Position pathPosition : paths) {
+            checkPieceExistOnPath(pathPosition);
+        }
+    }
+
+    private void checkPieceExistOnPath(final Position pathPosition) {
+        if (pieces.isPieceExistOnPosition(pathPosition)) {
+            throw new IllegalArgumentException("[ERROR] 타겟 위치까지의 경로에 말이 존재합니다.");
+        }
+    }
+
+    private void checkTargetPosition(final Position targetPosition, final Piece sourcePiece) {
+        if (pieces.isPieceExistOnPosition(targetPosition)) {
+            final Piece targetPiece = pieces.findPieceByPosition(targetPosition);
+            checkSameSidePiece(sourcePiece, targetPiece);
+        }
+    }
+
+    private void checkSameSidePiece(final Piece sourcePiece, final Piece targetPiece) {
+        if (targetPiece.isSameSide(sourcePiece)) {
+            throw new IllegalArgumentException("[ERROR] 타겟 위치에 아군 말이 존재합니다.");
+        }
+    }
+
+    public List<Piece> getPieces() {
+        return pieces.getPieces();
     }
 }
