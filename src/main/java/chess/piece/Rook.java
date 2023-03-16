5b3dafeb2bdd0370015f6631e8267aa0e3d7fc8e
yujamint <woawoa0513@gmail.com>
2023-03-16 13:24:11 +0900

    feat(Rook): 이동 가능 여부 반환 기능 구현

--- src/main/java/chess/piece/Rook.java
+++ src/main/java/chess/piece/Rook.java
@@ -1,10 +1,30 @@
 package chess.piece;
 
 import chess.board.Position;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Rook extends Piece {
 
+    private final List<Direction> directions;
+
     public Rook(final Position position, final Side side) {
         super(position, side);
+        this.directions = initDirections();
+    }
+
+    private List<Direction> initDirections() {
+        final List<Direction> directions = new ArrayList<>();
+        directions.add(Direction.UP);
+        directions.add(Direction.DOWN);
+        directions.add(Direction.LEFT);
+        directions.add(Direction.RIGHT);
+        return directions;
+    }
+
+    @Override
+    public boolean isMovable(Position targetPosition) {
+        final Direction direction = position.getDirectionTo(targetPosition);
+        return directions.contains(direction);
     }
 }
