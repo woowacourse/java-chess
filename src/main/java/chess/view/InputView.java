3592c4f883aef0eb68e2924630a6c76256d3c793
yoondgu <doyou2ing@naver.com>
2023-03-16 15:59:13 +0900

    feat: 입력값 검증 구현

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -9,12 +9,15 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String REQUEST_DELIMITER = " ";
     private static final int SINGLE_COMMAND_LENGTH = 1;
+    private static final int MOVE_COMMAND_LENGTH = 3;
     private static final int COMMAND_INDEX = 0;
     private static final int SOURCE_POSITION_INDEX = 1;
     private static final int DESTINATION_POSITION_INDEX = 2;
     private static final int POSITION_RANK_INDEX = 0;
     private static final int POSITION_FILE_INDEX = 1;
+    private static final String WRONG_MOVE_COMMAND_REQUEST_ERROR_MESSAGE = "이동 요청 정보가 잘못 되었습니다.";
     private final Scanner scanner;
 
     public InputView() {
@@ -22,13 +25,24 @@ public class InputView {
     }
 
     public CommandRequest requestGameCommand() {
-        String line = scanner.nextLine();
-        String[] request = line.split(" ");
+        String[] request = readRequest();
         if (request.length == SINGLE_COMMAND_LENGTH) {
             return CommandRequest.fromControlCommand(Command.findByAnswer(request[COMMAND_INDEX]));
         }
+        validateMoveCommandRequest(request);
         return CommandRequest.fromMoveCommand(parsePosition(request[SOURCE_POSITION_INDEX]),
                 parsePosition(request[DESTINATION_POSITION_INDEX]));
+    }
+
+    private String[] readRequest() {
+        String line = scanner.nextLine();
+        return line.split(REQUEST_DELIMITER);
+    }
+
+    private void validateMoveCommandRequest(final String[] request) {
+        if (request.length != MOVE_COMMAND_LENGTH) {
+            throw new IllegalArgumentException(WRONG_MOVE_COMMAND_REQUEST_ERROR_MESSAGE);
+        }
     }
 
     private List<Integer> parsePosition(String word) {
