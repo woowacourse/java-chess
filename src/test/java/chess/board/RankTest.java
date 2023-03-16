3bfecb48325a82adf63a5a99a3b8a30ceb434cea
yujamint <woawoa0513@gmail.com>
2023-03-16 09:51:32 +0900

    test(RankTest): 대상 Rank와의 값 차이 반환 기능 테스트

--- src/test/java/chess/board/RankTest.java
+++ src/test/java/chess/board/RankTest.java
@@ -32,4 +32,19 @@ class RankTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 더한 랭크 값이 존재하지 않습니다.");
     }
+
+    @Test
+    @DisplayName("대상 Rank과의 값 차이를 반환한다.")
+        // TODO: 2023/03/15 테스트 케이스 추가(Parameterized Test)
+    void getValueDiff() {
+        // given
+        Rank rank = Rank.ONE;
+        Rank targetRank = Rank.FIVE;
+
+        // when
+        final int valueDiff = rank.getValueDiff(targetRank);
+
+        // then
+        assertThat(valueDiff).isEqualTo(4);
+    }
 }
