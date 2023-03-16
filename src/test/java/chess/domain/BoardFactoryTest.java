8819526fecd156206c214a1a171c99907e5eb291
seokjin8678 <seokjin8678@gmail.com>
2023-03-16 15:23:37 +0900

    test: 테스트 메서드 변경
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/test/java/chess/domain/BoardFactoryTest.java
+++ src/test/java/chess/domain/BoardFactoryTest.java
@@ -1,7 +1,6 @@
 package chess.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import chess.domain.piece.Bishop;
 import chess.domain.piece.King;
@@ -33,42 +32,41 @@ class BoardFactoryTest {
         // given
         Map<Position, Piece> squares = BoardFactory.create();
 
-        assertAll(
-                () -> assertThat(squares).containsEntry(Position.of(0, 0), new Rook(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(1, 0), new Knight(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(2, 0), new Bishop(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(3, 0), new Queen(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(4, 0), new King(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(5, 0), new Bishop(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(6, 0), new Knight(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(7, 0), new Rook(Team.WHITE)),
-
-                () -> assertThat(squares).containsEntry(Position.of(0, 1), new Pawn(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(1, 1), new Pawn(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(2, 1), new Pawn(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(3, 1), new Pawn(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(4, 1), new Pawn(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(5, 1), new Pawn(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(6, 1), new Pawn(Team.WHITE)),
-                () -> assertThat(squares).containsEntry(Position.of(7, 1), new Pawn(Team.WHITE)),
-
-                () -> assertThat(squares).containsEntry(Position.of(0, 6), new Pawn(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(1, 6), new Pawn(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(2, 6), new Pawn(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(3, 6), new Pawn(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(4, 6), new Pawn(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(5, 6), new Pawn(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(6, 6), new Pawn(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(7, 6), new Pawn(Team.BLACK)),
-
-                () -> assertThat(squares).containsEntry(Position.of(0, 7), new Rook(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(1, 7), new Knight(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(2, 7), new Bishop(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(3, 7), new Queen(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(4, 7), new King(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(5, 7), new Bishop(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(6, 7), new Knight(Team.BLACK)),
-                () -> assertThat(squares).containsEntry(Position.of(7, 7), new Rook(Team.BLACK))
-        );
+        // expect
+        assertThat(squares)
+                .containsAllEntriesOf(Map.ofEntries(
+                        Map.entry(Position.of(0, 0), new Rook(Team.WHITE)),
+                        Map.entry(Position.of(1, 0), new Knight(Team.WHITE)),
+                        Map.entry(Position.of(2, 0), new Bishop(Team.WHITE)),
+                        Map.entry(Position.of(3, 0), new Queen(Team.WHITE)),
+                        Map.entry(Position.of(4, 0), new King(Team.WHITE)),
+                        Map.entry(Position.of(5, 0), new Bishop(Team.WHITE)),
+                        Map.entry(Position.of(6, 0), new Knight(Team.WHITE)),
+                        Map.entry(Position.of(7, 0), new Rook(Team.WHITE)),
+                        Map.entry(Position.of(0, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(1, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(2, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(3, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(4, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(5, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(6, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(7, 1), new Pawn(Team.WHITE)),
+                        Map.entry(Position.of(0, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(1, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(2, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(3, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(4, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(5, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(6, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(7, 6), new Pawn(Team.BLACK)),
+                        Map.entry(Position.of(0, 7), new Rook(Team.BLACK)),
+                        Map.entry(Position.of(1, 7), new Knight(Team.BLACK)),
+                        Map.entry(Position.of(2, 7), new Bishop(Team.BLACK)),
+                        Map.entry(Position.of(3, 7), new Queen(Team.BLACK)),
+                        Map.entry(Position.of(4, 7), new King(Team.BLACK)),
+                        Map.entry(Position.of(5, 7), new Bishop(Team.BLACK)),
+                        Map.entry(Position.of(6, 7), new Knight(Team.BLACK)),
+                        Map.entry(Position.of(7, 7), new Rook(Team.BLACK))
+                ));
     }
 }
