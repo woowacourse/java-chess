506be9bea82fffdf73a3430d2ba56fa07f9d9be2
junpakPark <junpak.park@gmail.com>
2023-03-16 13:57:47 +0900

    feat : Rook 클래스 추상메서드 구현

--- src/main/java/domain/piece/Rook.java
+++ src/main/java/domain/piece/Rook.java
@@ -3,6 +3,9 @@ package domain.piece;
 import domain.coordinate.Position;
 import domain.coordinate.Route;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rook extends Piece {
 
     public Rook(final Color color) {
@@ -11,11 +14,36 @@ public class Rook extends Piece {
 
     @Override
     public Route findRoute(final Position source, final Position target) {
-        return null;
+        validateMovable(source, target);
+
+        int moveX = getMoveCoordinate(target.diffX(source));
+        int moveY = getMoveCoordinate(target.diffY(source));
+
+        return new Route(findPositions(source, target, moveX, moveY));
     }
 
     @Override
     protected boolean isMovable(final Position source, final Position target) {
-        return false;
+        int diffX = Math.abs(target.diffX(source));
+        int diffY = Math.abs(target.diffY(source));
+
+        return (diffX != 0 || diffY != 0) && (diffX == 0 || diffY == 0);
     }
+
+
+    public int getMoveCoordinate(final int diff) {
+        return Integer.compare(diff, 0);
+    }
+
+    private List<Position> findPositions(final Position source, final Position target, final int moveX, final int moveY) {
+        List<Position> positions = new ArrayList<>();
+        Position position = source.move(moveX, moveY);
+
+        while (position != target) {
+            positions.add(position);
+            position = position.move(moveX, moveY);
+        }
+        return positions;
+    }
+
 }
