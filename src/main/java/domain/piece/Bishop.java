7cfa6a20716b67fa01975d18d41772127ccf7306
junpakPark <junpak.park@gmail.com>
2023-03-16 13:23:53 +0900

    feat : Bishop 객체 추상메서드 구현

--- src/main/java/domain/piece/Bishop.java
+++ src/main/java/domain/piece/Bishop.java
@@ -3,6 +3,9 @@ package domain.piece;
 import domain.coordinate.Position;
 import domain.coordinate.Route;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bishop extends Piece {
 
     public Bishop(final Color color) {
@@ -11,11 +14,42 @@ public class Bishop extends Piece {
 
     @Override
     public Route findRoute(final Position source, final Position target) {
-        return null;
+        validateMovable(source, target);
+
+        return new Route(getPositions(source, target));
     }
 
     @Override
     protected boolean isMovable(final Position source, final Position target) {
-        return false;
+        final int diffX = Math.abs(source.diffX(target));
+        final int diffY = Math.abs(source.diffY(target));
+
+        return (diffX != 0 || diffY != 0) && (diffX == diffY);
+    }
+
+    private List<Position> getPositions(final Position source, final Position target) {
+        int moveX = getMoveCoordinate(target.diffX(source));
+        int moveY = getMoveCoordinate(target.diffY(source));
+
+        return findPositions(source, target, moveX, moveY);
+    }
+
+    private int getMoveCoordinate(final int diff) {
+        if (diff < 0) {
+            return -1;
+        }
+
+        return 1;
+    }
+
+    private List<Position> findPositions(final Position source, final Position target, final int moveX, final int moveY) {
+        List<Position> positions = new ArrayList<>();
+        Position position = source.move(moveX, moveY);
+
+        while (position != target) {
+            positions.add(position);
+            position = position.move(moveX, moveY);
+        }
+        return positions;
     }
 }
