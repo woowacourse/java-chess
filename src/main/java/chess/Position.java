e6f29eba6fda7de6c59bc68b662aa62d1b13a6e2
Eunkeee <eunkeeeee@gmail.com>
2023-03-16 16:22:09 +0900

    feat: movement 반환
    Co-Authored-By: HyeonWoo Kim <62413589+java-saeng@users.noreply.github.com>
    Co-Authored-By: hong-sile <gurwns9325@gmail.com>

--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -20,6 +20,25 @@ public class Position {
         return file == position.file && rank == position.rank;
     }
 
+    public Movement convertMovement(Position from) {
+        int rankGap = rank.value() - from.rank.value();
+        int fileGap = file.value() - from.file.value();
+
+        int greatestCommonDivisor = findGreatestCommonDivisor(Math.max(rankGap, fileGap), Math.min(rankGap, fileGap));
+
+        return Movement.of(fileGap / greatestCommonDivisor,
+                rankGap / greatestCommonDivisor);
+    }
+
+    private int findGreatestCommonDivisor(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        } else {
+            return findGreatestCommonDivisor(num2, num1 % num2);
+        }
+    }
+
+
     @Override
     public int hashCode() {
         return Objects.hash(file, rank);
---
+++ src/test/java/chess/PositionTest.java
@@ -0,0 +1,31 @@
+package chess;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    @Test
+    void test_convertMovement() {
+
+        Position from = new Position(1, 1);
+        Position destination = new Position(2, 3);
+
+        Movement movement = destination.convertMovement(from);
+
+        assertThat(movement).isEqualTo(Movement.UUR);
+    }
+
+    @Test
+    void test_convertMovement2() {
+
+        Position from = new Position(1, 1);
+        Position destination = new Position(7, 1);
+
+        Movement movement = destination.convertMovement(from);
+
+        assertThat(movement).isEqualTo(Movement.R);
+    }
+}
