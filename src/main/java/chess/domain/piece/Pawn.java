9f4ecdd9651c382ba9afd4858e2e8bd5136a06bf
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-03-16 13:48:00 +0900

    feat: 폰 이동 기능 구현
    Co-authored-by: ezzanzzan <eunzzann@gmail.com>

---
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -0,0 +1,45 @@
+package chess.domain.piece;
+
+import chess.domain.Camp;
+import chess.domain.Move;
+import chess.domain.Square;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pawn extends Piece {
+    private final List<Move> possibleMoves;
+    private final boolean isMoved;
+
+    public Pawn(final Camp camp) {
+        super(camp);
+        this.possibleMoves = makePossibleMove();
+        this.isMoved = false;
+    }
+
+    public Pawn(final Camp camp, final boolean isMoved) {
+        super(camp);
+        this.possibleMoves = makePossibleMove();
+        this.isMoved = isMoved;
+    }
+
+    private List<Move> makePossibleMove() {
+        if (camp.equals(Camp.WHITE)) {
+            return new ArrayList<>(List.of(Move.UP, Move.RIGHT_UP, Move.LEFT_UP));
+        }
+        return new ArrayList<>(List.of(Move.DOWN, Move.RIGHT_DOWN, Move.LEFT_DOWN));
+    }
+
+    @Override
+    public boolean isMovable(final Square source, final Square target, final Move move) {
+        if (possibleMoves.contains(move)) {
+            if (move.equals(Move.UP) || move.equals(Move.DOWN)) {
+                if (!isMoved) {
+                    return source.isAble(target, move.getFile(), move.getRank()) || source.isAble(target,
+                            move.getFile(), move.getRank() + camp.calculateDirection(1));
+                }
+            }
+            return source.isAble(target, move.getFile(), move.getRank());
+        }
+        return false;
+    }
+}
---
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -0,0 +1,132 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import chess.domain.Camp;
+import chess.domain.File;
+import chess.domain.Move;
+import chess.domain.Rank;
+import chess.domain.Square;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class PawnTest {
+    @DisplayName("폰이 흰색 진영이다.")
+    @Nested
+    class WhitePawnTest {
+        @DisplayName("폰은 앞으로 한 칸 움직일 수 있다.")
+        @Test
+        void Pawn1() {
+            final Square source = new Square(File.A, Rank.TWO);
+            final Square target = new Square(File.A, Rank.THREE);
+            final Pawn pawn = new Pawn(Camp.WHITE);
+            final Move move = Move.UP;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+
+        @DisplayName("폰은 움직인 적이 없을 경우 두 칸 움직일 수 있다.")
+        @Test
+        void Pawn2() {
+            final Square source = new Square(File.A, Rank.TWO);
+            final Square target = new Square(File.A, Rank.FOUR);
+            final Pawn pawn = new Pawn(Camp.WHITE);
+            final Move move = Move.UP;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+
+        @DisplayName("폰은 움직인 적이 있을 경우 두 칸 이상 움직일 수 없다.")
+        @Test
+        void Pawn2_1() {
+            final Square source = new Square(File.A, Rank.TWO);
+            final Square target = new Square(File.A, Rank.FOUR);
+            final Pawn pawn = new Pawn(Camp.WHITE, true);
+            final Move move = Move.UP;
+
+            assertThat(pawn.isMovable(source, target, move)).isFalse();
+        }
+
+        @DisplayName("폰은 오른쪽 대각선으로 움직일 수 있다.")
+        @Test
+        void Pawn3() {
+            final Square source = new Square(File.A, Rank.TWO);
+            final Square target = new Square(File.B, Rank.THREE);
+            final Pawn pawn = new Pawn(Camp.WHITE);
+            final Move move = Move.RIGHT_UP;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+
+        @DisplayName("폰은 왼쪽 대각선으로 움직일 수 있다.")
+        @Test
+        void Pawn4() {
+            final Square source = new Square(File.B, Rank.TWO);
+            final Square target = new Square(File.A, Rank.THREE);
+            final Pawn pawn = new Pawn(Camp.WHITE);
+            final Move move = Move.LEFT_UP;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+    }
+
+    @DisplayName("폰이 검은색 진영이다.")
+    @Nested
+    class BlackPawnTest {
+        @DisplayName("폰은 앞으로 한 칸 움직일 수 있다.")
+        @Test
+        void Pawn1() {
+            final Square source = new Square(File.A, Rank.THREE);
+            final Square target = new Square(File.A, Rank.TWO);
+            final Pawn pawn = new Pawn(Camp.BLACK);
+            final Move move = Move.DOWN;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+
+        @DisplayName("폰은 움직인 적이 없을 경우 두 칸 움직일 수 있다.")
+        @Test
+        void Pawn2() {
+            final Square source = new Square(File.A, Rank.SEVEN);
+            final Square target = new Square(File.A, Rank.FIVE);
+            final Pawn pawn = new Pawn(Camp.BLACK);
+            final Move move = Move.DOWN;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+
+        @DisplayName("폰은 움직인 적이 있을 경우 두 칸 이상 움직일 수 없다.")
+        @Test
+        void Pawn2_1() {
+            final Square source = new Square(File.A, Rank.SIX);
+            final Square target = new Square(File.A, Rank.FOUR);
+            final Pawn pawn = new Pawn(Camp.BLACK, true);
+            final Move move = Move.DOWN;
+
+            assertThat(pawn.isMovable(source, target, move)).isFalse();
+        }
+
+        @DisplayName("폰은 오른쪽 대각선으로 움직일 수 있다.")
+        @Test
+        void Pawn3() {
+            final Square source = new Square(File.B, Rank.SEVEN);
+            final Square target = new Square(File.A, Rank.SIX);
+            final Pawn pawn = new Pawn(Camp.BLACK);
+            final Move move = Move.LEFT_DOWN;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+
+        @DisplayName("폰은 왼쪽 대각선으로 움직일 수 있다.")
+        @Test
+        void Pawn4() {
+            final Square source = new Square(File.A, Rank.SEVEN);
+            final Square target = new Square(File.B, Rank.SIX);
+            final Pawn pawn = new Pawn(Camp.BLACK);
+            final Move move = Move.RIGHT_DOWN;
+
+            assertThat(pawn.isMovable(source, target, move)).isTrue();
+        }
+    }
+}
