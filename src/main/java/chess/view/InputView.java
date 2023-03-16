c66fc2a3df8295d5b99871b4c813a87ea3a70ff5
Miseong Kim <miseongk@naver.com>
2023-03-16 17:48:53 +0900

    feat(InputView): 입력 및 검증 기능 구현

---
+++ src/main/java/chess/view/InputView.java
@@ -0,0 +1,42 @@
+package chess.view;
+
+import static chess.constant.GameCommand.END;
+import static chess.view.InputValidator.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void readStart() {
+        String input = scanner.nextLine();
+        validateStartCommand(input);
+    }
+
+    public String readCommand() {
+        String input = scanner.nextLine();
+
+        if (END.equals(input)) {
+            return input;
+        }
+
+        List<String> commands = Arrays.asList(input.split(" "));
+        validateCommands(commands);
+
+        return commands.get(1) + commands.get(2);
+    }
+
+    private static void validateCommands(List<String> commands) {
+        validateBlank(commands);
+        validateMoveCommand(commands.get(0));
+        validateCommandSize(commands);
+        validateCommandLength(commands.get(1), commands.get(2));
+        validateFile(commands.get(1).charAt(0));
+        validateFile(commands.get(2).charAt(0));
+        validateRank(commands.get(1).charAt(1));
+        validateRank(commands.get(2).charAt(1));
+    }
+}
