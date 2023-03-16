29339dcc78a03255adf235e5f88b1b45a85782da
yujamint <woawoa0513@gmail.com>
2023-03-16 09:53:07 +0900

    test(RankTest): 대상 Rank와의 값 차이에 따른 수직 좌표 값 반환 기능 테스트

--- src/test/java/chess/board/RankTest.java
+++ src/test/java/chess/board/RankTest.java
@@ -47,4 +47,18 @@ class RankTest {
         // then
         assertThat(valueDiff).isEqualTo(4);
     }
+
+    @Test
+    @DisplayName("대상 Rank과의 값 차이에 따른 수직 좌표 값을 반환한다.")
+    void getValuePoint() {
+        // given
+        Rank rank = Rank.ONE;
+        Rank targetRank = Rank.FIVE;
+
+        // when
+        final int valuePoint = rank.getValuePoint(targetRank);
+
+        // then
+        assertThat(valuePoint).isEqualTo(1);
+    }
 }
