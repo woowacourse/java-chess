9a1bdd4039b93c642275ff8c05475efac9a646dd
junpakPark <junpak.park@gmail.com>
2023-03-16 16:39:51 +0900

    feat : Color 객체 리버스 기능 추가 및 None 제거

--- src/main/java/domain/piece/Color.java
+++ src/main/java/domain/piece/Color.java
@@ -1,6 +1,15 @@
 package domain.piece;
 
+import java.util.Arrays;
+
 public enum Color {
-    BLACK, WHITE, NONE
+    BLACK, WHITE;
+
+    public Color reverse() {
+        return Arrays.stream(values())
+                .filter(value -> value != this)
+                .findFirst()
+                .orElseThrow(IllegalAccessError::new);
+    }
 
 }
