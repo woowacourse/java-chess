317fe3ea53985dafd14d55eb3d2c021b09dc9955
yujamint <woawoa0513@gmail.com>
2023-03-16 14:38:25 +0900

    refactor(Directio): 나이트의 이동 방향 삭제

--- src/main/java/chess/piece/Direction.java
+++ src/main/java/chess/piece/Direction.java
@@ -10,16 +10,7 @@ public enum Direction {
     DOWN(0, -1),
     DOWN_LEFT(-1, -1),
     LEFT(-1, 0),
-    UP_LEFT(-1, 1),
-
-    UP_UP_RIGHT(1, 2),
-    UP_UP_LEFT(-1, 2),
-    RIGHT_RIGHT_UP(2, 1),
-    RIGHT_RIGHT_DOWN(2, -1),
-    DOWN_DOWN_LEFT(-1, -2),
-    DOWN_DOWN_RIGHT(1, -2),
-    LEFT_LEFT_UP(-2, 1),
-    LEFT_LEFT_DOWN(-2, -1);
+    UP_LEFT(-1, 1);
 
     private final int x;
     private final int y;
