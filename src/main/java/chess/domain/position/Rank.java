1e2a9852038b32041069db096fd59b2076930a53
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-16 10:59:27 +0900

    feat: 랭크간 크기 차이 계산 추가

--- src/main/java/chess/domain/position/Rank.java
+++ src/main/java/chess/domain/position/Rank.java
@@ -1,13 +1,23 @@
 package chess.domain.position;
 
 public enum Rank {
-    EIGHT,
-    SEVEN,
-    SIX,
-    FIVE,
-    FOUR,
-    THREE,
-    TWO,
-    ONE,
+    EIGHT(8),
+    SEVEN(7),
+    SIX(6),
+    FIVE(5),
+    FOUR(4),
+    THREE(3),
+    TWO(2),
+    ONE(1),
     ;
+
+    private final int order;
+
+    Rank(int order) {
+        this.order = order;
+    }
+
+    public int subtractOrder(Rank other) {
+        return this.order - other.order;
+    }
 }
