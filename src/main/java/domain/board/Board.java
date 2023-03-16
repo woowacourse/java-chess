c74cef2f628e4080dc2257e4522116f9a3680414
nuyh <hjaehyun25@gmail.com>
2023-03-16 17:07:53 +0900

    refactor: 에러 메세지 상수화

--- src/main/java/domain/board/Board.java
+++ src/main/java/domain/board/Board.java
@@ -9,6 +9,9 @@ import java.util.Map;
 
 public final class Board {
 
+    private static final String NOT_EXIST_SOURCE = "해당 위치에 말이 존재하지 않습니다.";
+    private static final String INVALID_MOVEMENT = "해당 위치로 말을 이동할 수 없습니다.";
+
     private final Map<Position, Piece> board;
 
     private Board(final Map<Position, Piece> board) {
@@ -46,31 +49,31 @@ public final class Board {
 
     private void validateSourceExistence(final Position source) {
         if (!board.containsKey(source)) {
-            throw new IllegalArgumentException("해당 위치에 말이 존재하지 않습니다.");
+            throw new IllegalArgumentException(NOT_EXIST_SOURCE);
         }
     }
 
     private void validatePieceMovable(final Position source, final Position destination, final Piece piece) {
         if (!piece.isMovable(source, destination)) {
-            throw new IllegalArgumentException("해당 위치로 말을 이동할 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_MOVEMENT);
         }
     }
 
     private void validatePieceEatable(final Position source, final Position destination, final Piece piece) {
         if (!piece.isEatable(source, destination)) {
-            throw new IllegalArgumentException("해당 위치로 말을 이동할 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_MOVEMENT);
         }
     }
 
     private void validatePieceExistenceOnDestination(final Position destination, final Piece piece) {
         if (board.containsKey(destination) && piece.isBlack() == board.get(destination).isBlack()) {
-            throw new IllegalArgumentException("해당 위치로 말을 이동할 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_MOVEMENT);
         }
     }
 
     private void validatePieceExistenceInRoute(final Position source, final Position destination) {
         if (Positions.between(source, destination).stream().anyMatch(board::containsKey)) {
-            throw new IllegalArgumentException("해당 위치로 말을 이동할 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_MOVEMENT);
         }
     }
 
