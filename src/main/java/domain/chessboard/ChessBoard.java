cbca7dd72643557193e6d31b150f09177d672227
junpakPark <junpak.park@gmail.com>
2023-03-16 17:25:12 +0900

    refactor : validateTurn 메서드 삭제

--- src/main/java/domain/chessboard/ChessBoard.java
+++ src/main/java/domain/chessboard/ChessBoard.java
@@ -1,7 +1,6 @@
 package domain.chessboard;
 
 import domain.coordinate.Position;
-import domain.piece.Color;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,11 +22,4 @@ public class ChessBoard {
                 .getRank()
                 .get(position.getX());
     }
-
-    public void validateTurn(final Color color, final Square square) {
-        if (square.getColor() != color) {
-            throw new IllegalStateException(String.format("%s의 턴이 아닙니다.", square.getColor()));
-        }
-    }
-
 }
