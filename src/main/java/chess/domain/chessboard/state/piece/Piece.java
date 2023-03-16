c9f18c34924d2be47894c03c14de45deae47f38c
songusika <sws981202@gmail.com>
2023-03-16 16:31:03 +0900

    refactor: 공통 메서드 상위 클래스로 이동
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/main/java/chess/domain/chessboard/state/piece/Piece.java
+++ src/main/java/chess/domain/chessboard/state/piece/Piece.java
@@ -1,7 +1,11 @@
 package chess.domain.chessboard.state.piece;
 
+import chess.domain.chessboard.Coordinate;
+import chess.domain.chessboard.Square;
 import chess.domain.chessboard.state.PieceState;
 import chess.domain.chessboard.state.Team;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Piece implements PieceState {
 
@@ -26,8 +30,100 @@ public abstract class Piece implements PieceState {
         return this.team.equals(piece.team);
     }
 
+
+    protected List<Coordinate> horizontalRoute(final Coordinate from, final Coordinate to) {
+        final int distance = from.calculateFileDistance(to);
+
+        if (distance < 0) {
+            return horizontalRouteBySign(from, distance, -1);
+        }
+
+        return horizontalRouteBySign(from, distance, 1);
+    }
+
+    protected List<Coordinate> horizontalRouteBySign(final Coordinate from, final int distance, int sign) {
+        final List<Coordinate> route = new ArrayList<>();
+        for (int i = 1; i <= sign * distance; i++) {
+            route.add(from.horizontalMove(sign * i));
+        }
+        return route;
+    }
+
+
+    protected List<Coordinate> negativeDiagonalRoute(final Coordinate from, final Coordinate to) {
+        final int distance = from.calculateRankDistance(to);
+
+        if (distance < 0) {
+            return negativeDiagonalRouteBySign(from, distance, -1);
+        }
+
+        return negativeDiagonalRouteBySign(from, distance, 1);
+    }
+
+    protected List<Coordinate> negativeDiagonalRouteBySign(final Coordinate from, final int distance, final int sign) {
+        final List<Coordinate> route = new ArrayList<>();
+        for (int i = 1; i <= sign * distance; i++) {
+            route.add(from.negativeDiagonalMove(sign * i));
+        }
+        return route;
+    }
+
+    protected List<Coordinate> verticalRoute(final Coordinate from, final Coordinate to) {
+        final int distance = from.calculateRankDistance(to);
+
+        if (distance < 0) {
+            return verticalRouteBySign(from, distance, -1);
+        }
+
+        return verticalRouteBySign(from, distance, 1);
+    }
+
+    protected List<Coordinate> verticalRouteBySign(final Coordinate from, final int distance, int sign) {
+        final List<Coordinate> route = new ArrayList<>();
+        for (int i = 1; i <= sign * distance; i++) {
+            route.add(from.verticalMove(sign * i));
+        }
+        return route;
+    }
+
+    protected List<Coordinate> positiveDiagonalRoute(final Coordinate from, final Coordinate to) {
+        final int distance = from.calculateFileDistance(to);
+
+        if (distance < 0) {
+            return positiveDiagonalRouteBySign(from, distance, -1);
+        }
+
+        return positiveDiagonalRouteBySign(from, distance, 1);
+    }
+
+    protected List<Coordinate> positiveDiagonalRouteBySign(final Coordinate from, final int distance, final int sign) {
+        final List<Coordinate> route = new ArrayList<>();
+        for (int i = 1; i <= sign * distance; i++) {
+            route.add(from.positiveDiagonalMove(sign * i));
+        }
+        return route;
+    }
+
     protected void throwCanNotMoveException() {
         throw new IllegalArgumentException(this.getClass().getSimpleName() + "(은)는 해당 좌표로 이동할 수 없습니다.");
     }
 
+    @Override
+    public void canMove(final List<Square> routeSquares) {
+        final int lastIndex = routeSquares.size() - 1;
+        final Square lastSquare = routeSquares.get(lastIndex);
+        if (lastSquare.isSameTeam(this)) {
+            throwCanNotMoveException();
+        }
+
+        for (Square curSquare : routeSquares.subList(0, lastIndex)) {
+            checkSquareEmpty(curSquare);
+        }
+    }
+
+    protected void checkSquareEmpty(final Square curSquare) {
+        if (!curSquare.isEmpty()) {
+            throwCanNotMoveException();
+        }
+    }
 }
--- src/main/java/chess/domain/chessboard/state/piece/Rook.java
+++ src/main/java/chess/domain/chessboard/state/piece/Rook.java
@@ -1,9 +1,7 @@
 package chess.domain.chessboard.state.piece;
 
 import chess.domain.chessboard.Coordinate;
-import chess.domain.chessboard.Square;
 import chess.domain.chessboard.state.Team;
-import java.util.ArrayList;
 import java.util.List;
 
 public final class Rook extends Piece {
@@ -22,64 +20,9 @@ public final class Rook extends Piece {
         return horizontalRoute(from, to);
     }
 
-    private List<Coordinate> horizontalRoute(final Coordinate from, final Coordinate to) {
-        final int distance = from.calculateFileDistance(to);
-
-        if (distance < 0) {
-            return horizontalRouteBySign(from, distance, -1);
-        }
-
-        return horizontalRouteBySign(from, distance, 1);
-    }
-
-    private List<Coordinate> horizontalRouteBySign(final Coordinate from, final int distance, int sign) {
-        final List<Coordinate> route = new ArrayList<>();
-        for (int i = 1; i <= sign * distance; i++) {
-            route.add(from.horizontalMove(sign * i));
-        }
-        return route;
-    }
-
-    private List<Coordinate> verticalRoute(final Coordinate from, final Coordinate to) {
-        final int distance = from.calculateRankDistance(to);
-
-        if (distance < 0) {
-            return verticalRouteBySign(from, distance, -1);
-        }
-
-        return verticalRouteBySign(from, distance, 1);
-    }
-
-    private List<Coordinate> verticalRouteBySign(final Coordinate from, final int distance, int sign) {
-        final List<Coordinate> route = new ArrayList<>();
-        for (int i = 1; i <= sign * distance; i++) {
-            route.add(from.verticalMove(sign * i));
-        }
-        return route;
-    }
 
     private void validatePossibleDestination(final Coordinate from, final Coordinate to) {
         if (!(from.isSameFile(to) || from.isSameRank(to))) {
-            throwCanNotMoveException();
-        }
-    }
-
-    @Override
-    public void canMove(final List<Square> routeSquares) {
-
-        final int lastIndex = routeSquares.size() - 1;
-        final Square lastSquare = routeSquares.get(lastIndex);
-        if (lastSquare.isSameTeam(this)) {
-            throwCanNotMoveException();
-        }
-
-        for (Square curSquare : routeSquares.subList(0, lastIndex)) {
-            checkSquareEmpty(curSquare);
-        }
-    }
-
-    private void checkSquareEmpty(final Square curSquare) {
-        if (!curSquare.isEmpty()) {
             throwCanNotMoveException();
         }
     }
