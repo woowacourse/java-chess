796e7765972a5988ef7697a27e419a13248a9e20
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-16 17:55:56 +0900

    feat : 체스 컨트롤러 추가
    Co-authored-by: thdwoqor <celebarte01@gmail.com>

--- src/main/java/chess/controller/Controller.java
+++ src/main/java/chess/controller/Controller.java
@@ -1,6 +1,9 @@
 package chess.controller;
 
 import chess.domain.Board;
+import chess.domain.state.Ready;
+import chess.domain.state.State;
+import chess.dto.ChessInputDto;
 import chess.view.GameState;
 import chess.view.InputView;
 import chess.view.OutputView;
@@ -8,8 +11,37 @@ import chess.view.OutputView;
 public class Controller {
     public void run() {
         OutputView.printStartMessage();
-        while (InputView.inputGameState() != GameState.END) {
-            OutputView.printChessBoard(Board.create().getBoard());
+
+        State state = new Ready(Board.create());
+        while (!state.isEnd()) {
+            state = processGame(state);
         }
+    }
+
+    private State processGame(final State state) {
+        try {
+            final ChessInputDto chessInputDto = InputView.inputGameState();
+            final GameState gameState = chessInputDto.getGameState();
+
+            return processState(state, chessInputDto, gameState);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return state.end();
+    }
+
+    private State processState(final State state, final ChessInputDto chessInputDto, final GameState gameState) {
+        State newState = state;
+        if (gameState == GameState.START) {
+            newState = state.start();
+        }
+        if (gameState == GameState.END) {
+            newState = state.end();
+        }
+        if (gameState == GameState.MOVE) {
+            newState = state.move(chessInputDto.getSource(), chessInputDto.getTarget());
+        }
+        OutputView.printChessBoard(state.getBoard().getBoard());
+        return newState;
     }
 }
