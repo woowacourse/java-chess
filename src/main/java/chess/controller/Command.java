45b6bf48adfb0f58e94fca05ebff339059159f10
jiwon <ljwon77@daum.net>
2023-03-17 01:06:19 +0900

    feat: 사용자가 입력한 명령어에 대한 관리 방식을 변경한다
    Co-authored-by: yenawee <yaena0319@naver.com>

--- src/main/java/chess/controller/Command.java
+++ src/main/java/chess/controller/Command.java
@@ -1,18 +1,42 @@
 package chess.controller;
 
+import chess.controller.status.CommandType;
+
 import java.util.Arrays;
+import java.util.List;
 
-public enum Command {
-    START,
-    END,
-    MOVE;
+public class Command {
+    private final CommandType type;
+    private final List<String> commands;
 
-    private static final String COMMAND_ERROR_MESSAGE = "잘못된 명령어 입력입니다. (start, end, move)";
+    public Command(final CommandType type, final List<String> commands) {
+        this.type = type;
+        this.commands = commands;
+    }
 
-    public static Command findCommand(final String command) {
-        return Arrays.stream(values())
-                .filter(e -> e.name().equalsIgnoreCase(command))
+    private static final String COMMAND_ERROR_MESSAGE = "잘못된 명령어 입력입니다.";
+
+    public static Command findCommand(final List<String> commands) {
+        final CommandType type = Arrays.stream(CommandType.values())
+                .filter(e -> e.name().equalsIgnoreCase(commands.get(0)))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(COMMAND_ERROR_MESSAGE));
+        return new Command(type, commands);
+    }
+
+    public boolean isStart() {
+        return type == CommandType.START;
+    }
+
+    public boolean isEnd() {
+        return type == CommandType.END;
+    }
+
+    public boolean isCorrectWhenMove() {
+        return commands.size() == 3;
+    }
+
+    public List<String> getCommands() {
+        return List.copyOf(commands);
     }
 }
