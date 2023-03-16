62990d182a8ae4d31be86dad38e6bb0b0d40d095
seokjin8678 <seokjin8678@gmail.com>
2023-03-16 15:23:11 +0900

    feat: 체스 말의 움직임을 제어하는 메서드 추가
    Co-authored-by: kim0914 <kosa0914@naver.com>

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -18,21 +18,66 @@ public class Board {
     }
 
     public void move(Position source, Position target) {
+        validate(source, target);
+        squares.put(target, squares.get(source));
+        squares.put(source, Empty.INSTANCE);
+    }
+
+    private void validate(Position source, Position target) {
         Piece sourcePiece = squares.get(source);
         Piece targetPiece = squares.get(target);
-        if (sourcePiece.getTeam() == targetPiece.getTeam()) {
-            throw new IllegalArgumentException();
+        validateEmptySquare(sourcePiece);
+        validateSameTeam(sourcePiece, targetPiece);
+        validateMovement(source, target);
+        validatePieceRole(source, target);
+    }
+
+    private void validateEmptySquare(Piece sourcePiece) {
+        if (sourcePiece.isRoleOf(Role.EMPTY)) {
+            throw new IllegalArgumentException("[ERROR] 빈 칸은 움직일 수 없습니다.");
         }
+    }
+
+    private void validateSameTeam(Piece sourcePiece, Piece targetPiece) {
+        if (sourcePiece.isSameTeamWith(targetPiece)) {
+            throw new IllegalArgumentException("[ERROR] 목적지에 아군 말이 존재합니다.");
+        }
+    }
+
+    private void validateMovement(Position source, Position target) {
+        Piece sourcePiece = squares.get(source);
         if (!sourcePiece.canMove(source, target)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 해당 목적지로 이동할 수 없습니다.");
         }
+    }
+
+    private void validatePieceRole(Position source, Position target) {
+        Piece sourcePiece = squares.get(source);
+        if (!sourcePiece.isRoleOf(Role.KNIGHT)) {
+            validateCollision(source, target);
+        }
+        if (sourcePiece.isRoleOf(Role.PAWN)) {
+            validatePawnMove(source, target);
+        }
+    }
+
+    private void validateCollision(Position source, Position target) {
         List<Position> routes = source.getBetweenPositions(target);
-        for (Position route : routes) {
-            if (squares.get(route) != Empty.INSTANCE) {
-                throw new IllegalArgumentException();
-            }
+        boolean isEmptyRoute = routes.stream()
+                .map(squares::get)
+                .allMatch(piece -> piece.isRoleOf(Role.EMPTY));
+        if (!isEmptyRoute) {
+            throw new IllegalArgumentException("[ERROR] 해당 경로에 말이 있습니다.");
         }
-        squares.put(source, Empty.INSTANCE);
-        squares.put(target, sourcePiece);
+    }
+
+    private void validatePawnMove(Position source, Position target) {
+        Piece targetPiece = squares.get(target);
+        if (source.isSameXTo(target) && !targetPiece.isRoleOf(Role.EMPTY)) {
+            throw new IllegalArgumentException("[ERROR] 해당 목적지로 이동할 수 없습니다.");
+        }
+        if (!source.isSameXTo(target) && targetPiece.isRoleOf(Role.EMPTY)) {
+            throw new IllegalArgumentException("[ERROR] 해당 목적지로 이동할 수 없습니다.");
+        }
     }
 }
