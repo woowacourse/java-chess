b3717860708bf296b50382afab3fea63cc0a24b2
Go-Jaecheol <gojaech@naver.com>
2023-03-16 16:12:51 +0900

    refactor: 사용하지 않는 import문 삭제

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -1,9 +1,6 @@
 package chess;
 
 import chess.controller.ChessController;
-import chess.domain.board.ChessBoard;
-import chess.view.InputView;
-import chess.view.OutputView;
 
 public class ChessApplication {
     public static void main(String[] args) {
