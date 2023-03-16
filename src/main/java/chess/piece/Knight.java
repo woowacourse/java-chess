1207b1f563a69ae729b042b2f071d2ae938cce17
yujamint <woawoa0513@gmail.com>
2023-03-16 16:16:30 +0900

    feat(Knight): 이동 가능 여부 반환 기능 구현

--- src/main/java/chess/piece/Knight.java
+++ src/main/java/chess/piece/Knight.java
@@ -1,10 +1,24 @@
 package chess.piece;
 
 import chess.board.Position;
+import java.util.Collections;
+import java.util.List;
 
 public class Knight extends Piece {
 
     public Knight(final Position position, final Side side) {
         super(position, side);
+    }
+
+    @Override
+    public boolean isMovable(Position targetPosition) {
+        final double slope = position.getSlope(targetPosition);
+
+        return slope == 0.5 || slope == 2.0;
+    }
+
+    @Override
+    public List<Position> getPaths(final Position targetPosition) {
+        return Collections.emptyList();
     }
 }
