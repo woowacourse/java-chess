d7b10bf72d8b8d93fdb3c2949dd3dcea02dbc9fd
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-16 11:52:26 +0900

    feat: 룩의 움직임 검증

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -1,8 +1,19 @@
 package chess.domain.piece;
 
+import chess.domain.position.Position;
+
 public final class Rook extends Piece {
 
     public Rook(Camp camp) {
         super(camp, PieceSymbol.ROOK);
+    }
+
+    @Override
+    public boolean isMovable(Position from, Position to) {
+        int rankGap = from.calculateFileGap(to);
+        int fileGap = from.calculateRankGap(to);
+
+        return Math.abs(rankGap) == 0
+                || Math.abs(fileGap) == 0;
     }
 }
---
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -0,0 +1,37 @@
+package chess.domain.piece;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import chess.domain.position.File;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RookTest {
+
+    @Test
+    @DisplayName("룩은 직선으로 움직일 수 있다.")
+    void movableTest() {
+        Piece rook = new Rook(Camp.WHITE);
+        Position from = new Position(File.A, Rank.ONE);
+        Position to = new Position(File.A, Rank.THREE);
+
+        boolean result = rook.isMovable(from, to);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("룩은 대각선으로 움직일 수 없다.")
+    void movableFailDiagonalTest() {
+        Piece rook = new Rook(Camp.WHITE);
+
+        Position from = new Position(File.A, Rank.ONE);
+        Position to = new Position(File.C, Rank.THREE);
+
+        boolean result = rook.isMovable(from, to);
+
+        assertThat(result).isFalse();
+    }
+}
