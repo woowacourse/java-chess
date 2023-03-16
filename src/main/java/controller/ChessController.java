20b311e176aa6fd9439c12a66e8e64873c170350
woosung1223 <dntjd991223@naver.com>
2023-03-16 16:53:18 +0900

    chore: 리터럴 상수화

--- src/main/java/controller/ChessController.java
+++ src/main/java/controller/ChessController.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 public class ChessController {
 
+    public static final char ASCII_ALPHABET_A = 'a';
+    public static final int START_COORDINATE_INDEX = 1;
+    public static final int END_COORDINATE_INDEX = 2;
+
     private final InputView inputView;
     private final OutputView outputView;
 
@@ -41,14 +45,14 @@ public class ChessController {
         if (command.isEnd()) {
             return;
         }
-        Coordinate startCoordinate = convertCoordinate(commands.get(1));
-        Coordinate endCoordinate = convertCoordinate(commands.get(2));
+        Coordinate startCoordinate = convertCoordinate(commands.get(START_COORDINATE_INDEX));
+        Coordinate endCoordinate = convertCoordinate(commands.get(END_COORDINATE_INDEX));
         board.move(startCoordinate, endCoordinate);
     }
 
     private Coordinate convertCoordinate(final String frontCoordinate) {
         int row = Character.getNumericValue(frontCoordinate.charAt(1)) - 1;
-        int col = (int) frontCoordinate.charAt(0) - 97;
+        int col = (int) frontCoordinate.charAt(0) - ASCII_ALPHABET_A;
         return new Coordinate(row, col);
     }
 }
