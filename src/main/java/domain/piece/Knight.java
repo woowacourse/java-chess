c9e208c0b6201dd9d2316e0fb901030f42394fbc
junpakPark <junpak.park@gmail.com>
2023-03-16 11:20:48 +0900

    feat : Knight 객체 추상 메서드 구현

--- src/main/java/domain/piece/Knight.java
+++ src/main/java/domain/piece/Knight.java
@@ -1,5 +1,12 @@
 package domain.piece;
 
+import domain.coordinate.Position;
+import domain.coordinate.Route;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Knight extends Piece {
 
     public Knight(final Color color) {
@@ -7,7 +14,27 @@ public class Knight extends Piece {
     }
 
     @Override
-    public boolean findRoute() {
-        return false;
+    public Route findRoute(final Position source, final Position target) {
+        validateMovable(source, target);
+        return new Route(Collections.emptyList());
     }
+
+    private void validateMovable(final Position source, final Position target) {
+        if (isMovable(source, target)) {
+            return;
+        }
+        throw new IllegalStateException("잘못된 도착 지점입니다.");
+    }
+
+    private boolean isMovable(final Position startPoint, final Position endPoint) {
+        int diffY = Math.abs(endPoint.diffY(startPoint));
+        int diffX = Math.abs(endPoint.diffX(startPoint));
+
+        Set<Integer> set = new HashSet<>();
+        set.add(diffX);
+        set.add(diffY);
+
+        return set.contains(1) && set.contains(2);
+    }
+
 }
