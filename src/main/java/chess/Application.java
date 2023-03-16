d660ad06514491efa05cd6aafe7b3a2c01798107
bjk1649 <qhwns1649@naver.com>
2023-03-16 11:11:36 +0900

    feat: 게임 가이드 출력 기능 추가

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -8,6 +8,7 @@ import chess.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
+        outputView.printGameGuide();
         ChessBoard chessBoard = ChessBoardFactory.create();
         outputView.printChessBoard(new ChessBoardDto(chessBoard));
     }
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -4,6 +4,14 @@ import chess.controller.ChessBoardDto;
 
 public class OutputView {
 
+    public void printGameGuide() { // TODO: 2023/03/16 명령어 관리하는 곳에서 상수 or enum으로 정의해서 새로 포매팅
+        System.out.println(""
+                + "> 체스 게임을 시작합니다.\n"
+                + "> 게임 시작 : start\n"
+                + "> 게임 종료 : end\n"
+                + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+    }
+
     public void printChessBoard(ChessBoardDto chessBoardDto){
         for (String oneFile : chessBoardDto.getBoard()) {
             System.out.println(oneFile);
