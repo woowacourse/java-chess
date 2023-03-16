e248d15277c42bf97aa5d328589d68a2c3bf7034
This2sho <zx00018@naver.com>
2023-03-16 11:11:40 +0900

    feat(Queen): 퀸은 8방향으로 어디든 움직일 수 있다.
    - 시작지와 목적지 입력시 방향을 구하는 기능 구현
    - 시작지에서 목적지까지의 루트 구하는 기능 구현

--- src/main/java/domain/piece/Queen.java
+++ src/main/java/domain/piece/Queen.java
@@ -1,17 +1,44 @@
 package domain.piece;
 
 import domain.Square;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class Queen extends Piece {
 
     private static final List<DirectionVector> directions = List.of(DirectionVector.values());
+
     public Queen(TeamColor teamColor) {
         super(teamColor);
     }
 
     @Override
     public List<Square> findRoutes(Square src, Square dest) {
-        return null;
+        Vectorr vector = dest.calculateVector(src);
+        Optional<DirectionVector> direction = findDirection(vector);
+
+        if (direction.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return getSquaresToDestination(src, vector, direction.get());
+    }
+
+    private Optional<DirectionVector> findDirection(Vectorr vector) {
+        return directions.stream()
+            .filter(direction -> direction.isSameDirection(vector))
+            .findAny();
+    }
+
+    private List<Square> getSquaresToDestination(Square src, Vectorr vector,
+        DirectionVector direction) {
+        int maxStep = vector.getMaxLength();
+        List<Square> result = new ArrayList<>();
+        for (int step = 1; step <= maxStep; step++) {
+            Square next = src.add(direction.multiply(step));
+            result.add(next);
+        }
+        return Collections.unmodifiableList(result);
     }
 }
