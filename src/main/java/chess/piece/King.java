fc3da94b2a971003a034a9aa800e80e7f6e45c69
yujamint <woawoa0513@gmail.com>
2023-03-16 14:40:44 +0900

    feat(King): 이동 가능 여부 반환 기능 구현

--- src/main/java/chess/piece/King.java
+++ src/main/java/chess/piece/King.java
@@ -1,10 +1,34 @@
 package chess.piece;
 
 import chess.board.Position;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class King extends Piece {
 
+    private final List<Direction> directions;
+
     public King(final Position position, final Side side) {
         super(position, side);
+        this.directions = initDirections();
+    }
+
+    private List<Direction> initDirections() {
+        final List<Direction> directions = new ArrayList<>(Arrays.asList(Direction.values()));
+        return directions;
+    }
+
+    @Override
+    public boolean isMovable(Position targetPosition) {
+        final Direction direction = position.getDirectionTo(targetPosition);
+        final boolean isPossibleDirection = directions.contains(direction);
+        final boolean isPossibleDistance = position.getMoveCount(targetPosition, direction) == 1;
+        return isPossibleDistance && isPossibleDirection;
+    }
+
+    @Override
+    public List<Position> getPaths(Position targetPosition) {
+        return null;
     }
 }
