1a4facc8689a9df9dfe9b51af5764b99b25a4654
This2sho <zx00018@naver.com>
2023-03-16 17:44:53 +0900

    refactor(ChessColumn): 알파벳으로 조회하는 기능 추가

--- src/main/java/domain/ChessColumn.java
+++ src/main/java/domain/ChessColumn.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum ChessColumn {
     A(1), B(2), C(3), D(4),
     E(5), F(6), G(7), H(8);
@@ -21,6 +23,13 @@ public enum ChessColumn {
             }
         }
         throw new IllegalArgumentException("체스판을 벗어날 수 없습니다.");
+    }
+
+    public static ChessColumn find(char column) {
+        return Arrays.stream(values())
+            .filter(chessColumn -> chessColumn.name().equalsIgnoreCase(column + ""))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("잘못된 위치입니다."));
     }
 
     public int minus(ChessColumn other) {
