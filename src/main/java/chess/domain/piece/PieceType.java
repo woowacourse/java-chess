0f01ec7b05ffbaaaaf66ad228b80a5e6944ae253
jiwon <ljwon77@daum.net>
2023-03-16 14:36:05 +0900

    feat: 특정 체스말이 시작 위치에서 종료 위치까지 이동 가능한지 판단한다
    Co-authored-by: yenawee <yaena0319@naver.com>

--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -1,10 +1,28 @@
 package chess.domain.piece;
 
+import chess.domain.move.BishopMove;
+import chess.domain.move.KingMove;
+import chess.domain.move.KnightMove;
+import chess.domain.move.Movable;
+import chess.domain.move.PawnMove;
+import chess.domain.move.QueenMove;
+import chess.domain.move.RookMove;
+
 public enum PieceType {
-    QUEEN,
-    ROOK,
-    KNIGHT,
-    PAWN,
-    BISHOP,
-    KING;
+    QUEEN(new QueenMove()),
+    ROOK(new RookMove()),
+    KNIGHT(new KnightMove()),
+    PAWN(new PawnMove()),
+    BISHOP(new BishopMove()),
+    KING(new KingMove());
+
+    private final Movable movable;
+
+    PieceType(final Movable movable) {
+        this.movable = movable;
+    }
+
+    public boolean canMove(final Position source, final Position target) {
+        return movable.canMove(source, target);
+    }
 }
