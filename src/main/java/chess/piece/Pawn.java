09508c27a05a52155fe5adc8001ac606c57e4927
yujamint <woawoa0513@gmail.com>
2023-03-16 15:52:25 +0900

    feat(Pawn): 이동 가능 여부 반환 기능 구현

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -1,17 +1,65 @@
 package chess.piece;
 
-import chess.board.File;
 import chess.board.Position;
-import chess.board.Rank;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pawn extends Piece {
 
+    private final List<Direction> directions;
+
     public Pawn(final Position position, final Side side) {
         super(position, side);
+        this.directions = initDirections();
     }
 
-    public void move(final int fileMovingCount, final int rankMovingCount) {
-        position.move(fileMovingCount, rankMovingCount);
+    private List<Direction> initDirections() {
+        final List<Direction> directions = new ArrayList<>();
+        if (this.side == Side.WHITE) {
+            initWhiteSideDirections(directions);
+        }
+        if (this.side == Side.BLACK) {
+            initBlackSideDirections(directions);
+        }
+        return directions;
+    }
+
+    private void initWhiteSideDirections(final List<Direction> directions) {
+        directions.add(Direction.UP);
+        directions.add(Direction.UP_RIGHT);
+        directions.add(Direction.UP_LEFT);
+    }
+
+    private void initBlackSideDirections(final List<Direction> directions) {
+        directions.add(Direction.DOWN);
+        directions.add(Direction.DOWN_RIGHT);
+        directions.add(Direction.DOWN_LEFT);
+    }
+
+    @Override
+    public boolean isMovable(Position targetPosition) {
+        final Direction direction = position.getDirectionTo(targetPosition);
+        final int moveCount = position.getMoveCount(targetPosition, direction);
+
+        if (isStartPosition() && moveCount == 2) {
+            return direction == Direction.UP || direction == Direction.DOWN;
+        }
+
+        final boolean isPossibleDirection = directions.contains(direction);
+        final boolean isPossibleDistance = moveCount == 1;
+
+        return isPossibleDirection && isPossibleDistance;
+    }
+
+    private boolean isStartPosition() {
+        final boolean isWhiteStartPosition = position.getRank() == 2 && side == Side.WHITE;
+        final boolean isBlackStartPosition = position.getRank() == 7 && side == Side.BLACK;
+        return isWhiteStartPosition || isBlackStartPosition;
+    }
+
+    @Override
+    public List<Position> getPaths(final Position targetPosition) {
+        return null;
     }
 
     // TODO: 2023/03/14 시작 위치 체크
