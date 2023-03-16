04180ce7ab392ea19732ccbc42698f9fc6f1f8fb
songusika <sws981202@gmail.com>
2023-03-16 16:54:41 +0900

    feat: 나이트가 도착지로 갈 수 있는지 검증하는 기능 구현
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/main/java/chess/domain/chessboard/state/piece/Knight.java
+++ src/main/java/chess/domain/chessboard/state/piece/Knight.java
@@ -1,10 +1,38 @@
 package chess.domain.chessboard.state.piece;
 
+import chess.domain.chessboard.Coordinate;
+import chess.domain.chessboard.Square;
 import chess.domain.chessboard.state.Team;
+import java.util.List;
 
 public final class Knight extends Piece {
 
     public Knight(final Team team) {
         super(team);
+    }
+
+    @Override
+    public List<Coordinate> findRoute (final Coordinate from, final Coordinate to){
+        validatePossibleDestination(from, to);
+        return List.of(to);
+
+    }
+
+    private void validatePossibleDestination(final Coordinate from, final Coordinate to) {
+        final int fileDistance = Math.abs(from.calculateFileDistance(to));
+        final int rankDistance = Math.abs(from.calculateRankDistance(to));
+
+        if (!((fileDistance == 2 && rankDistance == 1) || (fileDistance == 1 && rankDistance == 2))) {
+            throwCanNotMoveException();
+        }
+    }
+
+    @Override
+    public void canMove(final List<Square> routeSquares) {
+        final int lastIndex = routeSquares.size() - 1;
+        final Square lastSquare = routeSquares.get(lastIndex);
+        if (lastSquare.isSameTeam(this)) {
+            throwCanNotMoveException();
+        }
     }
 }
