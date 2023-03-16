4ac3e0ab23e3c765832f88a0ba1c2ca202b0d022
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-16 17:22:55 +0900

    refactor : Piece를 추상클래스로 수정
    Co-authored-by: thdwoqor <celebarte01@gmail.com>

--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -1,11 +1,14 @@
 package chess.domain.piece;
 
 import chess.domain.Color;
+import chess.domain.PieceType;
 
 public abstract class Piece implements Movable {
+    private final PieceType pieceType;
     protected final Color color;
 
-    Piece(final Color color) {
+    Piece(final PieceType pieceType, final Color color) {
+        this.pieceType = pieceType;
         this.color = color;
     }
 }
