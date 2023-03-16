421a29eeb188fbebf1551bb5d60d55a050cf8fb7
This2sho <zx00018@naver.com>
2023-03-16 12:10:03 +0900

    feat(Knight): 나이트는 상, 하, 좌, 우 방향의 왼쪽, 오른쪽 대각선으로 점프할 수 있다.

--- src/main/java/domain/piece/Knight.java
+++ src/main/java/domain/piece/Knight.java
@@ -1,10 +1,16 @@
 package domain.piece;
 
 import domain.Square;
+import java.util.Collections;
 import java.util.List;
 
 public class Knight extends Piece {
 
+    private static final List<Vectorr> directions = List.of(
+        Vectorr.of(1, 2), Vectorr.of(2, 1),
+        Vectorr.of(2, -1), Vectorr.of(1, -2),
+        Vectorr.of(-1, -2), Vectorr.of(-2, -1),
+        Vectorr.of(-2, 1), Vectorr.of(-1, 2));
 
     public Knight(TeamColor teamColor) {
         super(teamColor);
@@ -12,6 +18,14 @@ public class Knight extends Piece {
 
     @Override
     public List<Square> findRoutes(Square src, Square dest) {
-        return null;
+
+        Vectorr vector = dest.calculateVector(src);
+
+        for (Vectorr direction : directions) {
+            if (direction.equals(vector)) {
+                return List.of(dest);
+            }
+        }
+        return Collections.emptyList();
     }
 }
