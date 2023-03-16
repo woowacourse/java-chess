33afef6513a08931a829884b58802e1cc3a09bc9
jaehee329 <jjhs9803@gmail.com>
2023-03-16 10:48:19 +0900

    test: pawn, king 테스트 케이스 추가

---
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -0,0 +1,38 @@
+package chess.domain.piece;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import chess.domain.RelativePosition;
+import chess.domain.Team;
+
+class KingTest {
+
+	@ParameterizedTest
+	@DisplayName("킹은 팀과 관계 없이 모든 방향으로 1칸 이동할 수 있다.")
+	@CsvSource({"0,1","1,1","1,0","1,-1","0,-1","-1,-1","-1,0","-1,1"})
+	void kingCanMoveEveryDirectionTest(int x, int y) {
+		King whiteKing = new King(Team.WHITE);
+		King blackKing = new King(Team.BLACK);
+		RelativePosition relativePosition = new RelativePosition(x, y);
+
+		assertTrue(whiteKing.isMobile(relativePosition));
+		assertTrue(blackKing.isMobile(relativePosition));
+	}
+
+	@ParameterizedTest
+	@DisplayName("킹은 팀과 관계 없이 어떤 방향으로든 1칸 이상 이동할 수 있다.")
+	@CsvSource({"0,1","1,1","1,0","1,-1","0,-1","-1,-1","-1,0","-1,1"})
+	void kingCannotMoveMoreThan1Test(int x, int y) {
+		int multiplier = 2;
+		King whiteKing = new King(Team.WHITE);
+		King blackKing = new King(Team.BLACK);
+		RelativePosition relativePosition = new RelativePosition(multiplier * x, multiplier * y);
+
+		assertFalse(whiteKing.isMobile(relativePosition));
+		assertFalse(blackKing.isMobile(relativePosition));
+	}
+}
--- src/test/java/chess/domain/piece/PawnTest.java
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -11,8 +11,8 @@ import chess.domain.Team;
 class PawnTest {
 
 	@Test
-	@DisplayName("폰은 처음에 앞으로 2칸 갈 수 있다.")
-	void pawnIsMobileTest() {
+	@DisplayName("흑팀 폰은 처음 남쪽으로 2칸 전진할 수 있다.")
+	void blackPawnCanMoveSouth2FirstTest() {
 		Pawn pawn = new Pawn(Team.BLACK);
 		RelativePosition relativePosition = new RelativePosition(0, -2);
 
@@ -20,8 +20,26 @@ class PawnTest {
 	}
 
 	@Test
-	@DisplayName("폰은 처음에 앞으로 1칸만 갈 수도 있다.")
-	void pawnIsMobileTest2() {
+	@DisplayName("흑팀 폰은 처음에 아래로 1칸만 전진할 수도 있다.")
+	void blackPawnCanMoveSouth1FirstTest() {
+		Pawn pawn = new Pawn(Team.BLACK);
+		RelativePosition relativePosition = new RelativePosition(0, -1);
+
+		assertTrue(pawn.isMobile(relativePosition));
+	}
+
+	@Test
+	@DisplayName("백팀 폰은 처음 위로로 2칸 전진할 수 있다.")
+	void blackPawnCanMoveNorth2FirstTest() {
+		Pawn pawn = new Pawn(Team.WHITE);
+		RelativePosition relativePosition = new RelativePosition(0, 2);
+
+		assertTrue(pawn.isMobile(relativePosition));
+	}
+
+	@Test
+	@DisplayName("흑팀 폰은 처음에 아래로 1칸만 전진할 수도 있다.")
+	void blackPawnCanMoveNort1FirstTest() {
 		Pawn pawn = new Pawn(Team.WHITE);
 		RelativePosition relativePosition = new RelativePosition(0, 1);
 
@@ -30,11 +48,47 @@ class PawnTest {
 
 	@Test
 	@DisplayName("폰은 이미 움직인 적이 있다면 앞으로 2칸 갈 수 없다.")
-	void pawnIsMobileTest3() {
+	void whitePawnCannotMoveSouth2AgainTest() {
 		Pawn pawn = new Pawn(Team.BLACK);
 		RelativePosition relativePosition = new RelativePosition(0, -2);
 
 		pawn.isMobile(relativePosition);
 		assertFalse(pawn.isMobile(relativePosition));
+	}
+
+	@Test
+	@DisplayName("흑팀 폰은 남동쪽으로 1칸 이동할 수 있다.")
+	void blackPawnCanMoveSouthEastTest() {
+		Pawn pawn = new Pawn(Team.BLACK);
+		RelativePosition relativePosition = new RelativePosition(1, -1);
+
+		assertTrue(pawn.isMobile(relativePosition));
+	}
+
+	@Test
+	@DisplayName("흑팀 폰은 남서쪽으로 1칸 이동할 수 있다.")
+	void blackPawnCanMoveSouthWestTest() {
+		Pawn pawn = new Pawn(Team.BLACK);
+		RelativePosition relativePosition = new RelativePosition(-1, -1);
+
+		assertTrue(pawn.isMobile(relativePosition));
+	}
+
+	@Test
+	@DisplayName("백팀 폰은 북동쪽으로 1칸 이동할 수 있다.")
+	void whitePawnCanMoveNorthEastTest() {
+		Pawn pawn = new Pawn(Team.WHITE);
+		RelativePosition relativePosition = new RelativePosition(1, 1);
+
+		assertTrue(pawn.isMobile(relativePosition));
+	}
+
+	@Test
+	@DisplayName("백팀 폰은 북서쪽으로 1칸 이동할 수 있다.")
+	void whitePawnCanMoveNorthWestTest() {
+		Pawn pawn = new Pawn(Team.WHITE);
+		RelativePosition relativePosition = new RelativePosition(-1, 1);
+
+		assertTrue(pawn.isMobile(relativePosition));
 	}
 }
