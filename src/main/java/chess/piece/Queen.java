7187fa83cfab51acadd18823c96fb35bb78b6516
yujamint <woawoa0513@gmail.com>
2023-03-16 16:04:20 +0900

    feat(Queen): 이동 가능 여부 반환 기능 구현

--- src/main/java/chess/piece/Queen.java
+++ src/main/java/chess/piece/Queen.java
@@ -1,10 +1,39 @@
 package chess.piece;
 
 import chess.board.Position;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Queen extends Piece {
 
+    private final List<Direction> directions;
+
     public Queen(final Position position, final Side side) {
         super(position, side);
+        this.directions = initDirections();
+    }
+
+    private List<Direction> initDirections() {
+        return new ArrayList<>(Arrays.asList(Direction.values()));
+    }
+
+    @Override
+    public boolean isMovable(Position targetPosition) {
+        final Direction direction = position.getDirectionTo(targetPosition);
+        return directions.contains(direction);
+    }
+
+    @Override
+    public List<Position> getPaths(Position targetPosition) {
+        List<Position> paths = new ArrayList<>();
+        final Direction direction = position.getDirectionTo(targetPosition);
+        final int moveCountBeforeArrivalPosition = position.getMoveCount(targetPosition, direction) - 1;
+        Position nextPosition = this.position;
+        for (int next = 0; next < moveCountBeforeArrivalPosition; next++) {
+            nextPosition = nextPosition.getNextPosition(direction);
+            paths.add(nextPosition);
+        }
+        return paths;
     }
 }
