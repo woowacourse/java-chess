e230d2890eb064636ffdee306708efdb8ed83150
bjk1649 <qhwns1649@naver.com>
2023-03-16 17:48:19 +0900

    refactor: 매직넘버 상수화

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -5,6 +5,9 @@ import chess.domain.piece.info.Team;
 
 public class Pawn extends Piece {
 
+    private static final int DEFAULT_MOVE_RANGE = 1;
+    private static final int INITIAL_MOVE_RANGE = 2;
+
     public Pawn(final Team team) {
         super(team);
     }
@@ -17,9 +20,9 @@ public class Pawn extends Piece {
             return false;
         }
         if (team == Team.BLACK) {
-            return diffFile == -1 || (!trace.hasLog() && diffFile == -2);
+            return diffFile == -DEFAULT_MOVE_RANGE || (!trace.hasLog() && diffFile == -INITIAL_MOVE_RANGE);
         }
-        return diffFile == 1 || (!trace.hasLog() && diffFile == 2);
+        return diffFile == DEFAULT_MOVE_RANGE || (!trace.hasLog() && diffFile == INITIAL_MOVE_RANGE);
     }
 
     @Override
@@ -28,9 +31,9 @@ public class Pawn extends Piece {
         int diffRank = endPosition.calculateRankDistance(startPosition);
 
         if (team == Team.WHITE) {
-            return Math.abs(diffRank) == 1 && diffFile == 1;
+            return Math.abs(diffRank) == DEFAULT_MOVE_RANGE && diffFile == DEFAULT_MOVE_RANGE;
         }
-        return Math.abs(diffRank) == 1 && diffFile == -1;
+        return Math.abs(diffRank) == DEFAULT_MOVE_RANGE && diffFile == -DEFAULT_MOVE_RANGE;
     }
 }
 
