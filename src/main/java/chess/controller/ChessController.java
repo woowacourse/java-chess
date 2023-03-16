092a44bd792625c91b8d4d5769b3b101bc3b9690
BGuga <tmdwns9913@gmail.com>
2023-03-16 12:43:18 +0900

    feat: controller 작성 및 2단계 요구사항 완료

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,9 +1,13 @@
 package chess.controller;
 
 import chess.domain.Board;
+import chess.domain.Position;
+import chess.domain.exception.IllegalPieceMoveException;
 import chess.view.InputView;
 import chess.view.OutputView;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ChessController {
@@ -17,29 +21,49 @@ public class ChessController {
         this.inputView = inputView;
     }
 
-//    public boolean realInit() {
-////        GameCommand gameCommand = readCommand();
-////        if (gameCommand == GameCommand.END) {
-////            return;
-////        }
-////        start();
-//    }
-
-    public void init() {
-        final GameCommand gameCommand = GameCommand.START;
+    public void realInit() {
         outputView.printInitialMessage();
-//        while (GameCommand.END.isCommandOf(readCommand())) {
-//            gameCommand = repeat(() -> GameCommand.from(inputView.inputGameCommand()));
-//            if (gameCommand != GameCommand.END) {
-//                Board board = new Board();
-//                outputView.printBoard(board.getPiecePosition());
-//            }
-//        }
+        while (true) {
+            CommandData gameCommand = repeat(() -> new CommandData(inputView.inputGameCommand()));
+            if (gameCommand.gameCommand == GameCommand.END) {
+                return;
+            }
+            if (gameCommand.gameCommand == GameCommand.START) {
+                gameStart();
+                return;
+            }
+            outputView.printUnsuitableCommand();
+        }
     }
 
-//    private String readCommand() {
-//        repeat(() -> GameCommand.from(inputView.inputGameCommand()));
-//    }
+    private void gameStart() {
+        printBoard();
+        while (true) {
+            CommandData gameCommand = repeat(() -> new CommandData(inputView.inputGameCommand()));
+            if (gameCommand.gameCommand == GameCommand.END) {
+                return;
+            }
+            if (gameCommand.gameCommand == GameCommand.MOVE) {
+                move(gameCommand.input);
+                continue;
+            }
+            outputView.printUnsuitableCommand();
+        }
+    }
+
+    private void move(String input) {
+        List<String> command = Arrays.asList(input.split(" "));
+        try {
+            board.movePiece(Position.of(command.get(1).substring(0, 1), command.get(1).substring(1, 2)), Position.of(command.get(2).substring(0, 1), command.get(2).substring(1, 2)));
+        } catch (IllegalPieceMoveException e) {
+            outputView.printError(e);
+        }
+        printBoard();
+    }
+
+    private void printBoard() {
+        outputView.printBoard(board.getPiecePosition());
+    }
 
     private <T> T repeat(Supplier<T> supplier) {
         while (true) {
@@ -48,6 +72,16 @@ public class ChessController {
             } catch (Exception e) {
                 outputView.printError(e);
             }
+        }
+    }
+
+    private static class CommandData {
+        private final GameCommand gameCommand;
+        private final String input;
+
+        private CommandData(String input) {
+            gameCommand = GameCommand.from(input);
+            this.input = input;
         }
     }
 }
