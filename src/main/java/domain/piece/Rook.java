554e967409e3c942609bbbc84ca5c440476ff734
This2sho <zx00018@naver.com>
2023-03-16 11:34:04 +0900

    feat(Rook): 룩은 상, 하, 좌, 우 어디든 움직일 수있다

--- src/main/java/domain/piece/Rook.java
+++ src/main/java/domain/piece/Rook.java
@@ -1,9 +1,14 @@
 package domain.piece;
 
 import domain.Square;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class Rook extends Piece {
+
+    private static final List<DirectionVector> directions = List.of(DirectionVector.TOP, DirectionVector.RIGHT, DirectionVector.BOTTOM, DirectionVector.LEFT);
 
     public Rook(TeamColor teamColor) {
         super(teamColor);
@@ -11,6 +16,29 @@ public class Rook extends Piece {
 
     @Override
     public List<Square> findRoutes(Square src, Square dest) {
-        return null;
+        Vectorr vector = dest.calculateVector(src);
+        Optional<DirectionVector> direction = findDirection(vector);
+
+        if (direction.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return getSquaresToDestination(src, vector, direction.get());
+    }
+
+    private Optional<DirectionVector> findDirection(Vectorr vector) {
+        return directions.stream()
+            .filter(direction -> direction.isSameDirection(vector))
+            .findAny();
+    }
+
+    private List<Square> getSquaresToDestination(Square src, Vectorr vector,
+        DirectionVector direction) {
+        int maxStep = vector.getMaxLength();
+        List<Square> result = new ArrayList<>();
+        for (int step = 1; step <= maxStep; step++) {
+            Square next = src.add(direction.multiply(step));
+            result.add(next);
+        }
+        return Collections.unmodifiableList(result);
     }
 }
