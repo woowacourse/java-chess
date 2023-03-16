a1e501362426c4d4a8798ef7c7e532016a0e0b23
xxeol2 <jhj01177@naver.com>
2023-03-16 14:00:47 +0900

    feat: 기물을 움직인다

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -1,5 +1,6 @@
 package chess.domain;
 
+import chess.domain.move.Move;
 import chess.domain.piece.Bishop;
 import chess.domain.piece.King;
 import chess.domain.piece.Knight;
@@ -18,7 +19,7 @@ public class Board {
     private final Map<Position, Piece> pieces;
 
     public Board() {
-        this.pieces = Map.ofEntries(
+        Map<Position, Piece> initialArrangement = Map.ofEntries(
                 Map.entry(new Position(File.A, Rank.ONE), Rook.from(true)),
                 Map.entry(new Position(File.B, Rank.ONE), Knight.from(true)),
                 Map.entry(new Position(File.C, Rank.ONE), Bishop.from(true)),
@@ -53,6 +54,77 @@ public class Board {
                 Map.entry(new Position(File.G, Rank.SEVEN), Pawn.from(false)),
                 Map.entry(new Position(File.H, Rank.SEVEN), Pawn.from(false))
         );
+        this.pieces = new HashMap<>(initialArrangement);
+    }
+
+    public void move(Position source, Position target) {
+        Piece sourcePiece = findSourcePiece(source);
+        boolean isAttack = checkIsAttack(sourcePiece, target);
+        Move move = Move.of(source, target);
+
+        checkPieceReachable(sourcePiece, isAttack, move);
+        checkNotCrossOtherPiece(source, target, move);
+
+        if (isAttack) {
+            pieces.remove(target);
+        }
+        pieces.remove(source);
+        pieces.put(target, sourcePiece.touch());
+    }
+
+    private Piece findSourcePiece(Position source) {
+        Piece sourcePiece = pieces.get(source);
+        if (sourcePiece == null) {
+            throw new IllegalArgumentException("움직일 기물이 없습니다");
+        }
+        return sourcePiece;
+    }
+
+    private boolean checkIsAttack(Piece sourcePiece, Position target) {
+        Piece targetPiece = pieces.get(target);
+        if (isEmpty(target)) {
+            return false;
+        }
+        if (sourcePiece.hasSameColor(targetPiece)) {
+            throw new IllegalArgumentException("목표 위치에 같은 색 말이 있습니다");
+        }
+        return true;
+    }
+
+    private void checkPieceReachable(Piece sourcePiece, boolean isAttack, Move move) {
+        if (!canMove(sourcePiece, move, isAttack)) {
+            throw new IllegalArgumentException("해당 기물이 이동할 수 없는 수입니다");
+        }
+    }
+
+    private boolean canMove(Piece piece, Move move, boolean isAttack) {
+        if (isAttack) {
+            return piece.hasAttackMove(move);
+        }
+        return piece.hasMove(move);
+    }
+
+    private void checkNotCrossOtherPiece(Position source, Position target, Move move) {
+        Move unitMove = move.getUnitMove();
+        Position currentPosition = unitMove.findDestination(source);
+        while (!currentPosition.equals(target)) {
+            checkEmpty(currentPosition);
+            currentPosition = unitMove.findDestination(source);
+        }
+    }
+
+    private void checkEmpty(Position position) {
+        if (isNotEmpty(position)) {
+            throw new IllegalArgumentException("다른 기물을 지나칠 수 없습니다");
+        }
+    }
+
+    private boolean isEmpty(Position position) {
+        return !isNotEmpty(position);
+    }
+
+    private boolean isNotEmpty(Position position) {
+        return pieces.get(position) != null;
     }
 
     public Map<Position, Piece> getPieces() {
---
+++ src/test/java/chess/domain/BoardTest.java
@@ -0,0 +1,101 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Piece;
+import chess.domain.position.Position;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest extends AbstractTestFixture {
+
+    @DisplayName("출발 위치에 말이 존재하지 않으면 예외를 던진다")
+    @Test
+    void invalidSourcePosition_throws() {
+        var board = new Board();
+        var source = createPosition("C,THREE");
+        var target = createPosition("D,TWO");
+
+        assertThatThrownBy(() -> board.move(source, target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("움직일 기물이 없습니다");
+    }
+
+    @DisplayName("목표 위치에 같은 색상의 말이 있으면 예외를 던진다")
+    @Test
+    void reachSameColor_throws() {
+        var board = new Board();
+        var source = createPosition("C,ONE");
+        var target = createPosition("D,TWO");
+
+        assertThatThrownBy(() -> board.move(source, target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("목표 위치에 같은 색 말이 있습니다");
+    }
+
+    @DisplayName("기물의 이동 수가 아니면 예외를 던진다")
+    @Test
+    void pieceNotHasMove_throws() {
+        var board = new Board();
+        var source = createPosition("C,TWO");
+        var target = createPosition("B,THREE");
+
+        assertThatThrownBy(() -> board.move(source, target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 기물이 이동할 수 없는 수입니다");
+    }
+
+    @DisplayName("목표에 도달하는중 다른 기물이 있으면 예외를 던진다")
+    @Test
+    void canNotCrossOtherPiece_throws() {
+        var board = new Board();
+        var source = createPosition("C,ONE");
+        var target = createPosition("E,THREE");
+
+        assertThatThrownBy(() -> board.move(source, target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("다른 기물을 지나칠 수 없습니다");
+    }
+
+    @DisplayName("이동이면, 해당 기물을 위치시킨다")
+    @Test
+    void putPiece_onMove() {
+        var board = new Board();
+        var source = createPosition("D,TWO");
+        var target = createPosition("D,THREE");
+
+        board.move(source, target);
+
+        Map<Position, Piece> pieces = board.getPieces();
+        assertThat(pieces.get(source))
+                .isNull();
+        assertThat(pieces.get(target))
+                .isNotNull()
+                .isInstanceOf(Pawn.class);
+    }
+
+    @DisplayName("공격이면, 기존 기물을 대체한다")
+    @Test
+    void replacePiece_onAttack() {
+        var board = new Board();
+        var source = createPosition("G,ONE");
+        var target = createPosition("F,THREE");
+        var target2 = createPosition("G,FIVE");
+        var target3 = createPosition("F,SEVEN");
+
+        board.move(source, target);
+        board.move(target, target2);
+        board.move(target2, target3);
+
+        Map<Position, Piece> pieces = board.getPieces();
+        assertThat(pieces.get(source))
+                .isNull();
+        assertThat(pieces.get(target3))
+                .isNotNull()
+                .isInstanceOf(Knight.class);
+    }
+}
