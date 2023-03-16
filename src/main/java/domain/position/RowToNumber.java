d16e708933a5ebc2f920de84010940f9d0e728cf
MoonJeWoong <munjin0201@naver.com>
2023-03-16 18:08:31 +0900

    refactor: 행, 열 좌표값을 관리하는 enum 수정
    세부사항
    - 내부 상태값 타입 변경

--- src/main/java/domain/position/RowToNumber.java
+++ src/main/java/domain/position/RowToNumber.java
@@ -4,32 +4,32 @@ import java.util.Arrays;
 
 public enum RowToNumber {
 
-    A("1", 1),
-    B("2", 2),
-    C("3", 3),
-    D("4", 4),
-    E("5", 5),
-    F("6", 6),
-    G("7", 7),
-    H("8", 8);
+    A('1', 1),
+    B('2', 2),
+    C('3', 3),
+    D('4', 4),
+    E('5', 5),
+    F('6', 6),
+    G('7', 7),
+    H('8', 8);
 
-    private final String row;
+    private final char row;
     private final int number;
 
-    RowToNumber(String row, int number) {
+    RowToNumber(char row, int number) {
         this.row = row;
         this.number = number;
     }
 
-    public static int of(String alphabet) {
+    public static int of(char alphabet) {
         return Arrays.stream(values())
-                .filter(rowToNumber -> rowToNumber.getRow().equals(alphabet))
+                .filter(rowToNumber -> rowToNumber.getRow() == alphabet)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 행 좌표값입니다."))
                 .getNumber();
     }
 
-    private String getRow() {
+    private char getRow() {
         return row;
     }
 
