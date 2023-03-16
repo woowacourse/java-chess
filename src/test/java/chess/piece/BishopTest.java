39c7212aa600dbc5ca7b9e25eeeb740259a11e45
yujamint <woawoa0513@gmail.com>
2023-03-16 14:12:44 +0900

    test(BishopTest): Target Position까지 가는 경로 반환 테스트

--- src/test/java/chess/piece/BishopTest.java
+++ src/test/java/chess/piece/BishopTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import chess.board.File;
 import chess.board.Position;
 import chess.board.Rank;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,12 +26,31 @@ class BishopTest {
     @DisplayName("대상 Position을 받아서 이동 가능 여부를 반환한다. - 수평 & 수직")
     void isMovable_false() {
         // given
-        final Bishop bishop = new Bishop(new Position(File.A, Rank.ONE), Side.WHITE);
-        final Position verticalPosition = new Position(File.A, Rank.THREE);
-        final Position horizontalPosition = new Position(File.E, Rank.ONE);
+        final Bishop bishop = new Bishop(new Position(File.A, Rank.THREE), Side.WHITE);
+        final Position verticalPosition = new Position(File.A, Rank.FIVE);
+        final Position horizontalPosition = new Position(File.E, Rank.THREE);
 
         // when, then
         assertThat(bishop.isMovable(verticalPosition)).isFalse();
         assertThat(bishop.isMovable(horizontalPosition)).isFalse();
+    }
+
+    @Test
+    @DisplayName("대상 Position까지 가는 경로들을 반환한다.")
+    void getPaths() {
+        // given
+        final Bishop bishop = new Bishop(new Position(File.A, Rank.THREE), Side.WHITE);
+        final Position targetPosition = new Position(File.E, Rank.SEVEN);
+        List<Position> expectedPaths = List.of(
+                new Position(File.B, Rank.FOUR),
+                new Position(File.C, Rank.FIVE),
+                new Position(File.D, Rank.SIX)
+        );
+
+        // when
+        List<Position> paths = bishop.getPaths(targetPosition);
+
+        // then
+        assertThat(paths).isEqualTo(expectedPaths);
     }
 }
