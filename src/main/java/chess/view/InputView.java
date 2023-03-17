beb244203668d157d4ea74017a791f0025bd268a
hong-sile <gurwns9325@naver.com>
2023-03-17 19:41:33 +0900

    refactor: positions의 길이에 대한 유효성 검사 로직을 메서드 추출

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
-    public static final String POSITION_FORMAT_VALIDATE_MESSAGE = "이동할 위치를 형식에 맞게 입력해주십시오. ex : b2 b4";
+    private static final String POSITION_FORMAT_VALIDATE_MESSAGE = "이동할 위치를 형식에 맞게 입력해주십시오. ex : b2 b4";
 
     public static String readCommand() {
         return scanner.next().trim();
@@ -17,11 +17,13 @@ public class InputView {
     public static List<String> readPositions() {
         final List<String> positions = Arrays.stream(scanner.nextLine().trim()
                 .split(" ", -1)).collect(Collectors.toList());
+        validatePositionsSize(positions);
+        return positions;
+    }
 
+    private static void validatePositionsSize(final List<String> positions) {
         if (positions.size() != 2) {
             throw new IllegalArgumentException(POSITION_FORMAT_VALIDATE_MESSAGE);
         }
-
-        return positions;
     }
 }
