7151b9298a4440e2f750e3c478519d48dcba50a2
waterricecake <fruturum@nate.com>
2023-03-17 11:55:42 +0900

    feat(PawnMove): 적용안된 validator 추가

--- src/main/java/chess/domain/validateMove/PawnMove.java
+++ src/main/java/chess/domain/validateMove/PawnMove.java
@@ -3,15 +3,18 @@ package chess.domain.validateMove;
 import chess.domain.piece.PieceType;
 
 public class PawnMove implements ValidateMove {
+    private ValidateMove next;
+
     @Override
     public void setNext(ValidateMove validateMove) {
-
+        this.next = validateMove;
     }
 
     @Override
     public boolean validate(ValidateDto validateDto) {
         if (validateDto.getSourcePiece().getPieceType() != PieceType.PAWN) {
-
+            setNext(new EmptyRoute());
+            return next.validate(validateDto);
         }
         if (validateDto.getSource().isSameFile(validateDto.getTarget())) {
             return validateDto.getChessboard().isEmptyInRoute(validateDto.getSource(), validateDto.getTarget()) &&
