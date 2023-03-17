1d2891b342d8389ee31e256460fa0b68bb260b63
kim0914 <kosa0914@naver.com>
2023-03-17 13:35:46 +0900

    refactor: 게임 시작 문구 수정

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -7,8 +7,10 @@ public class OutputView {
     private static final int CRITERIA_Y_POS = 7;
     private static final int BOARD_SIZE = 8;
     private static final String ROW_DELIMITER = "";
-    private static final String GAME_START_MESSAGE = "체스 게임을 시작합니다.";
-    private static final String COMMAND_INPUT_MESSAGE = "게임 시작은 start, 종료는 end 명령을 입력하세요.";
+    private static final String GAME_START_MESSAGE = "> 체스 게임을 시작합니다.";
+    private static final String COMMAND_INPUT_MESSAGE = "> 게임 시작 : start\n" +
+            "> 게임 종료 : end\n" +
+            "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
 
     private OutputView() {
     }
