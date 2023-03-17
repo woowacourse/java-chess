e07db81664ae37db209808167c13f9ed6ad2eccf
java-saeng <gusdn85554@naver.com>
2023-03-17 10:34:57 +0900

    test : Rank test

--- src/main/java/chess/Rank.java
+++ src/main/java/chess/Rank.java
@@ -22,9 +22,9 @@ public enum Rank {
 
     static Rank from(int value) {
         return Arrays.stream(values())
-                .filter(it -> it.value == value)
-                .findAny()
-                .orElseThrow(NoSuchElementException::new);
+                     .filter(it -> it.value == value)
+                     .findAny()
+                     .orElseThrow(() -> new NoSuchElementException("이동할 수 없는 Rank 방향입니다."));
     }
 
     public int value() {
---
+++ src/test/java/chess/MovementTest.java
@@ -0,0 +1,19 @@
+package chess;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovementTest {
+
+//    @Test
+//    @DisplayName("")
+//    void () throws Exception {
+//        //given
+//
+//        //when
+//
+//        //then
+//    }
+}
