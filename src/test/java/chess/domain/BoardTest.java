3b9b9736c1369155b6a337f5f1693891589b8b12
be-student <presentsong@naver.com>
2023-03-17 11:50:55 +0900

    test: board 테스트 케이스 추가

--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -1,16 +1,22 @@
 package chess.domain;
 
+import static chess.domain.PositionFixture.A3;
+import static chess.domain.PositionFixture.A5;
+import static chess.domain.PositionFixture.A7;
+import static chess.domain.PositionFixture.B2;
+import static chess.domain.PositionFixture.B4;
+import static chess.domain.PositionFixture.C1;
+import static chess.domain.PositionFixture.D1;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import chess.domain.dto.PieceResponse;
 import chess.domain.exception.IllegalPieceMoveException;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-
-import java.util.stream.Collectors;
-
-import static chess.domain.PositionFixture.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SuppressWarnings({"NonAsciiCharacters", "SpellCheckingInspection"})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -60,8 +66,23 @@ public class BoardTest {
     void 같은편_말이_없는_곳으로_이동할_수_있다() {
         //given
         Board board = new Board();
-        board.movePiece(B2, B4);
-        board.movePiece(A7, A5);
-        board.movePiece(C1, A3);
+
+        assertDoesNotThrow(() -> {
+            board.movePiece(B2, B4);
+            board.movePiece(A7, A5);
+            board.movePiece(C1, A3);
+        });
+    }
+
+    @Test
+    void 같은편_말이_없는_곳으로_이동할_수_있다2() {
+        //given
+        Board board = new Board();
+
+        assertDoesNotThrow(() -> {
+            board.movePiece(B2, B4);
+            board.movePiece(C1, A3);
+            board.movePiece(D1, C1);
+        });
     }
 }
--- src/test/java/chess/domain/PositionFixture.java
+++ src/test/java/chess/domain/PositionFixture.java
@@ -1,9 +1,11 @@
 package chess.domain;
 
 public class PositionFixture {
+
     public static final Position A1 = Position.of(File.A, Rank.ONE);
     public static final Position A2 = Position.of(File.A, Rank.TWO);
     public static final Position A3 = Position.of(File.A, Rank.THREE);
+    public static final Position A4 = Position.of(File.A, Rank.FOUR);
     public static final Position A5 = Position.of(File.A, Rank.FIVE);
     public static final Position A7 = Position.of(File.A, Rank.SEVEN);
     public static final Position B2 = Position.of(File.B, Rank.TWO);
@@ -11,6 +13,7 @@ public class PositionFixture {
     public static final Position B4 = Position.of(File.B, Rank.FOUR);
     public static final Position C1 = Position.of(File.C, Rank.ONE);
     public static final Position C3 = Position.of(File.C, Rank.THREE);
+    public static final Position D1 = Position.of(File.D, Rank.ONE);
     public static final Position D4 = Position.of(File.D, Rank.FOUR);
     public static final Position F1 = Position.of(File.F, Rank.ONE);
     public static final Position E1 = Position.of(File.E, Rank.ONE);
