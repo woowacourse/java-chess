f2d29773a36388f16d929ef3372a86f2259f8dca
eunbii0213 <eunbii0213@naver.com>
2023-03-17 13:49:45 +0900

    feat: Pawn 입력된 색깔에 따라 이동가능한 방향 목록 구성 기 능 추가

---
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -0,0 +1,40 @@
+package chess.domain.piece;
+
+import chess.domain.Color;
+import chess.practiceMove.Direction;
+
+import java.util.List;
+
+public class Pawn extends Piece {
+
+    private static final String name = "p";
+    private static final String MOVE_FORWARD_ERROR_GUIDE_MESSAGE = "앞에 기물이 있어 Pawn은 앞으로 이동할 수 없습니다";
+    private static final String MOVE_DIAGONAL_ERROR_GUIDE_MESSAGE = "Pawn은 대각선에 상대방이 있을 때만 이동할 수 있습니다";
+    private static int movableDistance = 2;
+    private final List<Direction> movableDirection;
+    private boolean isFirstMove = true;
+
+    public Pawn(Color color) {
+        super(name, color);
+        this.movableDirection = createMovableDirectionByColor(color);
+    }
+
+    private List<Direction> createMovableDirectionByColor(Color color) {
+        if (color == Color.BLACK) {
+            return List.of(Direction.BOTTOM, Direction.BOTTOM_LEFT,
+                    Direction.BOTTOM_RIGHT);
+        }
+        return List.of(Direction.TOP, Direction.TOP_LEFT,
+                Direction.TOP_RIGHT);
+    }
+
+    @Override
+    public boolean isMovableAtOnce(int absGapOfColum, int absGapOfRank) {
+        return true;
+    }
+
+    @Override
+    public boolean isMovableDirection(Direction direction) {
+        return true;
+    }
+}
