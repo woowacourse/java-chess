cb8a59be83b7856379b0818d2e29dd54277b1c6f
cpot5620 <=cpot5620@naver.com>
2023-03-17 13:22:40 +0900

    refactor(Pawn): 매직 넘버 제거

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -2,14 +2,15 @@ package chess.domain.piece;
 
 import chess.domain.board.Square;
 
-// TODO: 2023-03-16 매직넘버 제거 및 조건문 분리
 public class Pawn extends Piece {
 
     private static final int WHITE_MAX_MOVABLE_RANK = 2;
     private static final int WHITE_MIN_MOVABLE_RANK = 1;
     private static final int BLACK_MAX_MOVABLE_RANK = -2;
     private static final int BLACK_MIN_MOVABLE_RANK = -1;
-    private static final int MIN_MOVABLE_FILE = 1;
+    private static final int MIN_MOVABLE_FILE = 0;
+    private static final int MAX_MOVABLE_FILE = 1;
+
 
     public Pawn(Camp camp) {
         super(camp);
@@ -34,33 +35,31 @@ public class Pawn extends Piece {
         int rankDifference = source.calculateRankDifference(target);
         int fileDistance = source.calculateFileDistance(target);
 
-        //뒤로 가는거 + 옆으로 가는거 검증
-        if (rankDifference <= 0) {
+        if (rankDifference < WHITE_MIN_MOVABLE_RANK) {
             return false;
         }
-        //시작 위치
+
         if (source.isRankTwo()) {
-            return rankDifference <= WHITE_MAX_MOVABLE_RANK && fileDistance == 0 ||
-                    rankDifference == WHITE_MIN_MOVABLE_RANK && fileDistance == 1;
+            return rankDifference <= WHITE_MAX_MOVABLE_RANK && fileDistance == MIN_MOVABLE_FILE ||
+                    rankDifference == WHITE_MIN_MOVABLE_RANK && fileDistance == MAX_MOVABLE_FILE;
         }
 
-        return rankDifference == WHITE_MIN_MOVABLE_RANK && fileDistance <= 1;
+        return rankDifference == WHITE_MIN_MOVABLE_RANK && fileDistance <= MAX_MOVABLE_FILE;
     }
 
     private boolean canMoveBlack(Square source, Square target) {
         int rankDifference = source.calculateRankDifference(target);
         int fileDistance = source.calculateFileDistance(target);
 
-        //뒤로 가는거 + 옆으로 가는거 검증
-        if (rankDifference >= 0) {
+        if (rankDifference > BLACK_MIN_MOVABLE_RANK) {
             return false;
         }
-        //시작 위치
+
         if (source.isRankSeven()) {
-            return rankDifference >= BLACK_MAX_MOVABLE_RANK && fileDistance == 0 ||
-                    rankDifference == BLACK_MIN_MOVABLE_RANK && fileDistance == 1;
+            return rankDifference >= BLACK_MAX_MOVABLE_RANK && fileDistance == MIN_MOVABLE_FILE ||
+                    rankDifference == BLACK_MIN_MOVABLE_RANK && fileDistance == MAX_MOVABLE_FILE;
         }
 
-        return rankDifference == BLACK_MIN_MOVABLE_RANK && fileDistance <= 1;
+        return rankDifference == BLACK_MIN_MOVABLE_RANK && fileDistance <= MAX_MOVABLE_FILE;
     }
 }
