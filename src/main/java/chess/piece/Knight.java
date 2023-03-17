f6ffea16aab89ff4103a3f84d27b93476cc110a7
yujamint <woawoa0513@gmail.com>
2023-03-17 22:45:01 +0900

    refactor: Knight getPaths() 주석 작성

--- src/main/java/chess/piece/Knight.java
+++ src/main/java/chess/piece/Knight.java
@@ -24,6 +24,10 @@ public class Knight extends Piece {
 
     @Override
     public List<Position> getPaths(final Position targetPosition) {
+        /*
+        Knight는 이동 경로에 존재하는 말을 뛰어 넘을 수 있기 때문에 도착 지점에 아군 말이 존재하는지만 검사한다.
+        그러므로, 빈 리스트를 반환함으로써 Knight가 이동할 땐 경로에 말이 존재하는지 체크하지 않는다.
+         */
         return Collections.emptyList();
     }
 
