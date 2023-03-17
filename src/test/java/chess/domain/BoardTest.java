715ebb3037bcb90317d99a5d5e7438a7074d8880
jeonghoon <shb03323@naver.com>
2023-03-17 13:43:18 +0900

    feat: 폰 앞으로 이동 기능 구현

--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -1,10 +1,12 @@
 package chess.domain;
 
+import chess.domain.piece.exception.WrongDirectionException;
 import chess.domain.square.File;
 import chess.domain.square.Rank;
 import chess.domain.square.Square;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,6 +20,50 @@ class BoardTest {
     void setup() {
         board = Board.create();
     }
+
+    @Nested
+    @DisplayName("흰 폰")
+    class WhitePawn {
+
+        @Test
+        @DisplayName("폰 한 칸 앞으로 이동")
+        void pawn_move_one_square() {
+            Square current = Square.of(File.A, Rank.TWO);
+            Square destination = Square.of(File.A, Rank.THREE);
+            assertThatCode(() -> board.move(current, destination))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("더블 폰 푸시 이동")
+        void double_pawn_push() {
+            Square current = Square.of(File.A, Rank.TWO);
+            Square destination = Square.of(File.A, Rank.FOUR);
+            assertThatCode(() -> board.move(current, destination))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("더블 폰 푸시 이동 실패")
+        void double_pawn_push_fail() {
+            Square current = Square.of(File.A, Rank.TWO);
+            Square nextMove = Square.of(File.A, Rank.THREE);
+            Square destination = Square.of(File.A, Rank.FIVE);
+            assertThatThrownBy(() -> {
+                board.move(current, nextMove);
+                board.move(nextMove, destination);
+            }).isInstanceOf(WrongDirectionException.class);
+        }
+
+        @Test
+        @DisplayName("뒤로 가기 불가")
+        void back_move_fail() {
+            Square current = Square.of(File.A, Rank.TWO);
+            Square destination = Square.of(File.A, Rank.ONE);
+            assertThatThrownBy(() -> board.move(current, destination))
+                    .isInstanceOf(WrongDirectionException.class);
+        }
+     }
 
     @Test
     @DisplayName("빈자리 이동 시켰을 때 예외를 발생시킨다.")
