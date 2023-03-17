2630d7ad3507f61de3c09e5577ecf5819cfd4f87
JunH <tjdtls690@gmail.com>
2023-03-17 14:06:16 +0900

    :art: refactor : 입력 값을 스플릿해서 각 값을 받을 때, 인덱스를 상수화

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -5,6 +5,10 @@ import chess.view.InputView;
 import chess.view.OutputView;
 
 public class Application {
+    private static final int COMMAND_INDEX = 0;
+    private static final int SOURCE_COORDINATE_INDEX = 1;
+    private static final int DESTINATION_COORDINATE_INDEX = 2;
+    
     public static void main(String[] args) {
         OutputView.noticeGameStart();
         run(ChessBoard.create());
@@ -14,19 +18,20 @@ public class Application {
         String command = InputView.repeat(InputView::inputCommand);
         String[] splitedCommand = command.split(" ");
     
-        if ("move".equals(splitedCommand[0])){
-            String sourceCoordinate = splitedCommand[1];
-            String destinationCoordinate = splitedCommand[2];
+        if ("move".equals(splitedCommand[COMMAND_INDEX])){
+            String sourceCoordinate = splitedCommand[SOURCE_COORDINATE_INDEX];
+            String destinationCoordinate = splitedCommand[DESTINATION_COORDINATE_INDEX];
+            
             chessBoard.move(sourceCoordinate,destinationCoordinate);
             OutputView.printChessBoard(chessBoard.chessBoard());
             run(chessBoard);
         }
         
-        if ("end".equals(splitedCommand[0])) {
+        if ("end".equals(splitedCommand[COMMAND_INDEX])) {
             return;
         }
         
-        if ("start".equals(splitedCommand[0])){
+        if ("start".equals(splitedCommand[COMMAND_INDEX])){
             ChessBoard newChessBoard = ChessBoard.create();
             OutputView.printChessBoard(newChessBoard.chessBoard());
             run(newChessBoard);
