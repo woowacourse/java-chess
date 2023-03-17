c7107a33077d81bd961b9c7d8d0e58b4d50e4bed
eunbii0213 <eunbii0213@naver.com>
2023-03-17 14:02:24 +0900

    feat: ChessBoard를 DTO로 생성하는 기능 추가

---
+++ src/main/java/chess/dto/BoardDto.java
@@ -0,0 +1,36 @@
+package chess.dto;
+
+import chess.domain.Column;
+import chess.domain.Position;
+import chess.domain.Rank;
+import chess.domain.piece.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class BoardDto {
+
+    private final List<List<String>> chessBoardDto;
+
+    public BoardDto(Map<Position, Piece> chessBoard) {
+        chessBoardDto = createDto(chessBoard);
+    }
+
+    private List<List<String>> createDto(Map<Position, Piece> chessBoard) {
+        List<List<String>> chessBoardString = new ArrayList<>();
+        for (Rank rank : Rank.getOrderedRanks()) {
+            List<String> piecesOfRank = new ArrayList<>();
+            for (Column column : Column.getOrderedColumns()) {
+                String piece = chessBoard.get(new Position(column, rank)).findName();
+                piecesOfRank.add(piece);
+            }
+            chessBoardString.add(piecesOfRank);
+        }
+        return chessBoardString;
+    }
+
+    public List<List<String>> getDto() {
+        return new ArrayList<>(chessBoardDto);
+    }
+}
