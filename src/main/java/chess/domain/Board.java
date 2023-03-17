ae9786cca31333032cfaa92e7dca8cd51c79d07a
jaehee329 <jjhs9803@gmail.com>
2023-03-17 21:04:52 +0900

    refactor(Board): 말 움직임의 검증 로직 가독성 개선

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -1,26 +1,19 @@
 package chess.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import chess.domain.piece.Bishop;
-import chess.domain.piece.Empty;
-import chess.domain.piece.King;
-import chess.domain.piece.Knight;
-import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
-import chess.domain.piece.Queen;
-import chess.domain.piece.Rook;
 
 public class Board {
 
 	private static final String EMPTY_PIECE_IN_SOURCE_ERROR_MESSAGE = "source 위치에 조작할 수 있는 말이 없습니다.";
+	private static final String CANNOT_MOVE_PIECE_IN_SOURCE_ERROR_MESSAGE = "현재 차례의 source 위치의 말을 조작할 수 없습니다.";
 	private static final String SAME_TEAM_IN_TARGET_ERROR_MESSAGE = "같은 팀의 말의 위치로 이동할 수 없습니다.";
-	private static final String CANNOT_MOVE_TO_TARGET_DIRECTION_ERROR_MESSAGE = "말이 해당 위치로 이동할 수 없습니다.";
+	private static final String CANNOT_MOVE_TO_TARGET_DIRECTION_ERROR_MESSAGE = "말이 해당 방향으로 이동할 수 없습니다.";
 
 	private final Map<Position, Piece> board;
+
 	private Board(Map<Position, Piece> board) {
 		this.board = board;
 	}
@@ -30,88 +23,87 @@ public class Board {
 	}
 
 	public static Board create() {
-		HashMap<Position, Piece> board = new HashMap<>();
-		List<Piece> whitePieces = new ArrayList<>(
-			List.of(new Rook(Team.WHITE), new Knight(Team.WHITE), new Bishop(Team.WHITE), new Queen(Team.WHITE),
-				new King(Team.WHITE), new Bishop(Team.WHITE), new Knight(Team.WHITE), new Rook(Team.WHITE))
-		);
-		List<Piece> blackPieces = new ArrayList<>(
-			List.of(new Rook(Team.BLACK), new Knight(Team.BLACK), new Bishop(Team.BLACK), new Queen(Team.BLACK),
-				new King(Team.BLACK), new Bishop(Team.BLACK), new Knight(Team.BLACK), new Rook(Team.BLACK))
-		);
-
-		for (int i = 0; i < 8; i++) {
-			board.put(new Position(i, 0), whitePieces.get(i));
-		}
-		for (int i = 0; i < 8; i++) {
-			board.put(new Position(i, 1), new Pawn(Team.WHITE));
-		}
-		for (int i = 0; i < 8; i++) {
-			for (int j = 2; j < 6; j++) {
-				board.put(new Position(i, j), new Empty());
-			}
-		}
-		for (int i = 0; i < 8; i++) {
-			board.put(new Position(i, 6), new Pawn(Team.BLACK));
-		}
-		for (int i = 7; i >= 0; i--) {
-			board.put(new Position(i, 7), blackPieces.get(i));
-		}
+		Map<Position, Piece> board = BoardMaker.setPiece();
 		return new Board(board);
 	}
 
-	public void movePiece(Position source, Position target) {
-		if (isEmpty(source)) {
+	public void movePiece(final Team team, final Position source, final Position target) {
+		checkIsMovable(team, source, target);
+		Piece piece = board.get(source);
+		board.put(target, piece);
+		board.put(source, Piece.empty());
+	}
+
+	public void checkIsMovable(final Team team, final Position source, final Position target) {
+		checkIsSourceEmpty(source);
+		checkIsSourceCorrectTeam(team, source);
+		checkIsSameTeamExistOnTarget(source, target);
+		checkIsMovableDirection(source, target);
+		checkIsThereAnyObstacle(source, target);
+		checkIsSourcePawnMovingProperDiagonal(source, target);
+	}
+
+	private void checkIsSourceEmpty(final Position source) {
+		Piece piece = board.get(source);
+		if (piece.isGivenTeam(Team.EMPTY)) {
 			throw new IllegalArgumentException(EMPTY_PIECE_IN_SOURCE_ERROR_MESSAGE);
 		}
-		if (hasSameTeam(source, target)) {
+	}
+
+	private void checkIsSourceCorrectTeam(final Team team, final Position source) {
+		Piece piece = board.get(source);
+		if (!piece.isGivenTeam(team)) {
+			throw new IllegalArgumentException(CANNOT_MOVE_PIECE_IN_SOURCE_ERROR_MESSAGE);
+		}
+	}
+
+	private void checkIsSameTeamExistOnTarget(final Position source, final Position target) {
+		Piece sourcePiece = board.get(source);
+		Piece targetPiece = board.get(target);
+		if (sourcePiece.getTeam() == targetPiece.getTeam()) {
 			throw new IllegalArgumentException(SAME_TEAM_IN_TARGET_ERROR_MESSAGE);
 		}
-		if (isInvalidDirection(source, target)) {
+	}
+
+	private void checkIsMovableDirection(final Position source, final Position target) {
+		RelativePosition relativePosition = RelativePosition.of(source, target);
+		Piece piece = board.get(source);
+		if (!piece.isMobile(relativePosition)) {
 			throw new IllegalArgumentException(CANNOT_MOVE_TO_TARGET_DIRECTION_ERROR_MESSAGE);
 		}
+	}
+
+	private void checkIsThereAnyObstacle(final Position source, final Position target) {
 		Piece piece = board.get(source);
 		if (!piece.isKnight() && hasObstacle(source, target)) {
 			throw new IllegalArgumentException(CANNOT_MOVE_TO_TARGET_DIRECTION_ERROR_MESSAGE);
 		}
-		RelativePosition relativePosition = RelativePosition.of(source, target);
-		if (piece.isPawn() && relativePosition.isDiagonal() && isEmpty(target)) {
-			throw new IllegalArgumentException(CANNOT_MOVE_TO_TARGET_DIRECTION_ERROR_MESSAGE);
-		}
-		board.put(target, piece);
-		board.put(source, new Empty());
 	}
 
-	private boolean isEmpty(Position source) {
-		return board.get(source).isEmpty();
-	}
-
-	private boolean hasSameTeam(Position source, Position target) {
+	private void checkIsSourcePawnMovingProperDiagonal(final Position source, final Position target) {
 		Piece sourcePiece = board.get(source);
 		Piece targetPiece = board.get(target);
-		if (sourcePiece.isBlack() && targetPiece.isBlack()) {
-			return true;
-		}
-		return sourcePiece.isWhite() && targetPiece.isWhite();
-	}
-
-	private boolean isInvalidDirection(Position source, Position target) {
 		RelativePosition relativePosition = RelativePosition.of(source, target);
-		Piece piece = board.get(source);
-		return !piece.isMobile(relativePosition);
+		if (sourcePiece.isPawn() && relativePosition.isDiagonal() && targetPiece.isEmpty()) {
+			throw new IllegalArgumentException(CANNOT_MOVE_TO_TARGET_DIRECTION_ERROR_MESSAGE);
+		}
 	}
 
-	private boolean hasObstacle(Position source, Position target) {
+	private boolean hasObstacle(final Position source, final Position target) {
 		RelativePosition relativePosition = RelativePosition.of(source, target);
 		RelativePosition unitPosition = relativePosition.toUnit();
 		Position currentPosition = source.move(unitPosition);
 		while (!currentPosition.equals(target)) {
-			if (!isEmpty(currentPosition)) {
+			if (isPieceExists(currentPosition)) {
 				return true;
 			}
 			currentPosition = currentPosition.move(unitPosition);
 		}
 		return false;
+	}
+
+	private boolean isPieceExists(final Position position) {
+		return board.get(position).isEmpty();
 	}
 
 	public Map<Position, Piece> getBoard() {
