3b09a76f47a2abfef411831e6310096d9c2bc04d
Combi153 <jcm04@naver.com>
2023-03-17 12:17:47 +0900

    feat: 람다 미션2 수행

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -4,29 +4,21 @@ import java.util.List;
 
 public class Calculator {
 
-    public static int sumAll(List<Integer> numbers) {
+    public static int sumAll(List<Integer> numbers, Conditional conditional) {
         int total = 0;
         for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
+            if (conditional.test(number)) {
                 total += number;
             }
         }
         return total;
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
+    public static int sumAllEven(List<Integer> numbers, Conditional conditional) {
+        return sumAll(numbers, conditional);
+    }
 
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
+    public static int sumAllOverThree(List<Integer> numbers, final Conditional conditional) {
+        return sumAll(numbers, conditional);
     }
 }
