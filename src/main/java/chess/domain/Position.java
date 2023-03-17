998dcd7e0e8b70c3ee7c3b44ec365075306596ae
eunbii0213 <eunbii0213@naver.com>
2023-03-17 13:45:02 +0900

    feat: Position의 equals, hashCode 오버라이딩

---
+++ src/main/java/chess/domain/Position.java
@@ -0,0 +1,34 @@
+
+package chess.domain;
+
+import chess.practiceMove.Direction;
+
+import java.util.Objects;
+
+public class Position {
+    private final Rank rank;
+    private final Column column;
+
+    public Position(Column column, Rank rank) {
+        this.column = column;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return column.name() + rank.name();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return rank == position.rank && column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rank, column);
+    }
+}
