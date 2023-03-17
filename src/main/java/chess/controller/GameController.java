2ecdc331b7b9a1cd984eb700b0faa398895bbc86
yeonkkk <tjddus0302@gmail.com>
2023-03-17 15:46:05 +0900

    refactor: 재입력 로직 구현

--- src/main/java/chess/controller/GameController.java
+++ src/main/java/chess/controller/GameController.java
@@ -5,12 +5,13 @@ import static chess.domain.Command.MOVE;
 import static chess.domain.Command.START;
 import static chess.domain.Team.WHITE;
 
-import chess.ChessGame;
+import chess.domain.ChessGame;
 import chess.domain.Board;
 import chess.domain.Command;
 import chess.view.InputView;
 import chess.view.OutputView;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class GameController {
 
@@ -23,49 +24,68 @@ public class GameController {
     }
 
     public void run() {
-        startGame();
+        outputView.printGameStart();
+        Command command = readUntilValidate(this::startGame);
+        if (command == END) {
+            return;
+        }
         ChessGame chessGame = new ChessGame(new Board(), WHITE);
         Board board = chessGame.getBoard();
         outputView.printChessBoard(board.getBoardResult());
-        progressGame(chessGame);
+
+        do {
+            command = readUntilValidate(() -> progressGame(chessGame));
+        } while (command != END);
     }
 
-    private void startGame() {
-        outputView.printGameStart();
+    private Command startGame() {
         List<String> gameCommand = inputView.readGameCommand();
         Command command = Command.from(gameCommand.get(0));
-        try {
-            checkCorrectCommand(command, START);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            startGame();
-        }
+        checkWrongCommand(command, MOVE);
+        return command;
     }
 
-    private void progressGame(ChessGame chessGame) {
-        while (true) {
-            List<String> gameCommand = inputView.readGameCommand();
-            Command command = Command.from(gameCommand.get(0));
-            if (command == END) {
-                break;
-            }
-            checkCorrectCommand(command, MOVE);
-            validateMoveCommandFormat(gameCommand);
-
-            chessGame.movePiece(gameCommand.get(1), gameCommand.get(2));
-            outputView.printChessBoard(chessGame.getBoard().getBoardResult());
+    private Command progressGame(ChessGame chessGame) {
+        List<String> gameCommand = inputView.readGameCommand();
+        Command command = Command.from(gameCommand.get(0));
+        if (command == END) {
+            return command;
         }
+        checkWrongCommand(command, START);
+        validateMoveCommandFormat(gameCommand);
+
+        chessGame.movePiece(gameCommand.get(1), gameCommand.get(2));
+        outputView.printChessBoard(chessGame.getBoard().getBoardResult());
+        return command;
     }
 
-    private void checkCorrectCommand(final Command userCommand, final Command expected) {
-        if (userCommand != expected) {
-            throw new IllegalArgumentException("올바른 command를 입력해주세요. 게임 시작은 start로, 게임 진행은 move로, 게임 종료는 end로 할 수 있습니다.");
+    private void checkWrongCommand(final Command userCommand, final Command notExpected) {
+        if (userCommand == notExpected) {
+            throw new IllegalArgumentException(
+                    "올바른 command를 입력해주세요. 게임 시작은 start로, 게임 진행은 move로, 게임 종료는 end로 할 수 있습니다.");
         }
     }
 
     private void validateMoveCommandFormat(final List<String> gameCommand) {
         if (gameCommand.size() != 3) {
             throw new IllegalArgumentException("move source위치 target위치 형태로 입력해주세요. 예) move a2 a3");
+        }
+    }
+
+    private <T> T readUntilValidate(Supplier<T> supplier) {
+        T input;
+        do {
+            input = readUserInput(supplier);
+        } while (input == null);
+        return input;
+    }
+
+    private <T> T readUserInput(final Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
         }
     }
 }
