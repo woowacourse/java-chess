863d1d726b429398980673e69e86575a07dc9f15
splitube <voicesplit@daum.net>
2023-03-17 17:19:03 +0900

    refactor: DTO 네이밍 수정

--- src/main/java/domain/ValidateDto.java
+++ src/main/java/domain/ValidateDto.java
@@ -2,18 +2,18 @@ package domain;
 
 import domain.piece.Piece;
 
-public class SpecialValidateDto {
+public class ValidateDto {
 
     private final Location location;
     private final Piece piece;
 
-    private SpecialValidateDto(final Location location, final Piece piece) {
+    private ValidateDto(final Location location, final Piece piece) {
         this.location = location;
         this.piece = piece;
     }
 
-    public static SpecialValidateDto of(final Location location, final Square square) {
-        return new SpecialValidateDto(location, square.getPiece());
+    public static ValidateDto of(final Location location, final Square square) {
+        return new ValidateDto(location, square.getPiece());
     }
 
     public Location getLocation() {
