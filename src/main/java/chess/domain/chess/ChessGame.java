ee5169cc4c4ebbe6815ed0676af538bedf9ee207
jiwon <ljwon77@daum.net>
2023-03-17 14:19:32 +0900

    feat: 체스 게임을 진행하는 기능을 추가한다
    Co-authored-by: yenawee <yaena0319@naver.com>

--- src/main/java/chess/domain/chess/ChessGame.java
+++ src/main/java/chess/domain/chess/ChessGame.java
@@ -9,42 +9,104 @@ import java.util.Map;
 
 public final class ChessGame {
     private final ChessBoard chessBoard;
+    private CampType currentCamp;
 
     public ChessGame() {
         this.chessBoard = ChessBoard.getInstance(this);
     }
 
-    public void playFirstTurn(final Position source, final Position target) {
+    public void setUp(final Position source, final Position target, final CampType currentCamp) {
+        this.currentCamp = currentCamp;
+        play(source, target);
+    }
+
+    public void play(final Position source, final Position target) {
         final Piece piece = chessBoard.checkPiece(source);
-        validateMovablePiece(piece);
+        validateCamp(piece);
         if (piece.isPawn()) {
-            movePawnFirst(source, target, piece);
+            movePawn(source, target, piece);
             return;
         }
-        moveKnightFirst(source, target, piece);
-    }
-
-    private void validateMovablePiece(final Piece piece) {
-        if (piece.isSameCamp(CampType.BLACK) || !piece.isPawn() && !piece.isKnight()) {
-            throw new IllegalArgumentException("처음은 백진영의 폰과 나이트만 움직일 수 있습니다.");
+        if (piece.isKnight() && piece.canMove(source, target)) {
+            movePiece(source, target, piece);
+            return;
         }
-    }
-
-    private void movePawnFirst(final Position source, final Position target, final Piece piece) {
-        final int rankGap = target.getRank() - source.getRank();
-        if (source.getFile() != target.getFile() || rankGap < 0 || rankGap > 2) {
-            throw new IllegalArgumentException("폰은 처음에 2칸 이내만 전진할 수 있습니다.");
-        }
+        validatePossibleRoute(source, target, piece);
         movePiece(source, target, piece);
     }
 
-    private void moveKnightFirst(final Position source, final Position target, final Piece piece) {
-        final int rankGap = target.getRank() - source.getRank();
-        final int fileGap = target.getFile() - source.getFile();
-        if (Math.abs(rankGap * fileGap) != 2) {
-            throw new IllegalArgumentException("잘못된 나이트의 이동 경로입니다.");
+    private void validateCamp(final Piece piece) {
+        if (!piece.isSameCamp(currentCamp)) {
+            throw new IllegalArgumentException("현재 차례가 아닙니다.");
         }
+    }
+
+    /**
+     * 시작에서 목표 지점까지 이동이 가능한지, 그리고 그 사이에 장애물이 존재하지 않는지 체크한다.
+     *
+     * @param source 시작 지점
+     * @param target 도착 지점
+     * @param piece  체스 말
+     */
+    private void validatePossibleRoute(final Position source, final Position target, final Piece piece) {
+        if (!piece.canMove(source, target) && !chessBoard.isPossibleRoute(source, target, piece)) {
+            throw new IllegalArgumentException("움직일 수 없는 위치입니다.");
+        }
+    }
+
+    /**
+     * 폰의 이동 규칙을 관리한다.
+     * 1) 첫 시도 - 대각선 위치에 다른 편의 기물 있음 : 공격
+     * 2) 첫 시도 - 대각선 위치에 기물 없음 : 최대 2칸까지 이동 가능, 가능한 위치면 이동
+     * 3) 그 외 - 대각선 위치에 기물 있음 : 공격
+     * 4) 그 외 - 대각선 위치에 기물 없음 : 앞으로 1칸 이동
+     *
+     * @param source 시작 위치
+     * @param target 종료 위치
+     * @param piece  체스 말
+     *                                                         TODO : 여기서 책임을 가지는 건 무의미한 것 같은데, 어떻게 분리하지?
+     */
+    private void movePawn(final Position source, final Position target, final Piece piece) {
+        if ((piece.isSameCamp(CampType.WHITE) && source.getRank() == 1) || (piece.isSameCamp(CampType.BLACK) && source.getRank() == 6)) {
+            attackOrMove(source, target, piece);
+            return;
+        }
+        if (canAttack(source, target)) {
+            movePiece(source, target, piece);
+            return;
+        }
+        validatePawnOneMove(source, target);
         movePiece(source, target, piece);
+    }
+
+    private void attackOrMove(final Position source, final Position target, final Piece piece) {
+        if (canAttack(source, target)) {
+            movePiece(source, target, piece);
+            return;
+        }
+        validatePawnMoveCondition(source, target, piece);
+        movePiece(source, target, piece);
+    }
+
+    private void validatePawnOneMove(final Position source, final Position target) {
+        if (Math.abs(target.getRank() - source.getRank()) != 1 || chessBoard.contains(target)) {
+            throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+        }
+    }
+
+    private boolean canAttack(final Position source, final Position target) {
+        if (Math.abs(target.getRank() - source.getRank()) == 1 && Math.abs(target.getFile() - source.getFile()) == 1) {
+            if (!chessBoard.contains(target) || chessBoard.checkPiece(target).isSameCamp(currentCamp)) {
+                throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+            }
+        }
+        return true;
+    }
+
+    private void validatePawnMoveCondition(final Position source, final Position target, final Piece piece) {
+        if (!piece.canMove(source, target) || chessBoard.contains(target)) {
+            throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+        }
     }
 
     private void movePiece(final Position source, final Position target, final Piece piece) {
--- src/test/java/chess/domain/chess/ChessGameTest.java
+++ src/test/java/chess/domain/chess/ChessGameTest.java
@@ -1,28 +1,26 @@
 package chess.domain.chess;
 
-import chess.domain.piece.Piece;
 import chess.domain.piece.Position;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ChessGameTest {
 
     @ParameterizedTest(name = "첫 턴은 백진영의 폰과 나이트만 움직이는지 검증한다.")
     @CsvSource(value = {"6:0", "0:3"}, delimiter = ':')
-    void playFirstTurnMovableFail(final int rank, final int file) {
+    void playMovableFail(final int rank, final int file) {
         // given
         final ChessGame chessGame = new ChessGame();
         final Position source = new Position(rank, file);
         final Position target = new Position(5, 0);
 
         // when, then
-        assertThatThrownBy(() -> chessGame.playFirstTurn(source, target))
+        assertThatThrownBy(() -> chessGame.play(source, target))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("처음은 백진영의 폰과 나이트만 움직일 수 있습니다.");
+                .hasMessage("현재 차례가 아닙니다.");
     }
 
     @ParameterizedTest(name = "첫 턴의 폰은 2칸 이내만 전진할 수 있는지 검증한다.")
@@ -34,9 +32,9 @@ class ChessGameTest {
         final Position target = new Position(rank, 0);
 
         // when, then
-        assertThatThrownBy(() -> chessGame.playFirstTurn(source, target))
+        assertThatThrownBy(() -> chessGame.play(source, target))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("폰은 처음에 2칸 이내만 전진할 수 있습니다.");
+                .hasMessage("현재 차례가 아닙니다.");
     }
 
     @ParameterizedTest(name = "첫 턴의 나이트는 L자로 전진할 수 있는지 검증한다.")
@@ -48,26 +46,8 @@ class ChessGameTest {
         final Position target = new Position(rank, file);
 
         // when, then
-        assertThatThrownBy(() -> chessGame.playFirstTurn(source, target))
+        assertThatThrownBy(() -> chessGame.play(source, target))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("잘못된 나이트의 이동 경로입니다.");
-    }
-
-    @ParameterizedTest(name = "첫 턴은 백진영의 폰과 나이트만 움직이는지 검증한다.")
-    @CsvSource(value = {"1:3:2:3", "0:6:2:5"}, delimiter = ':')
-    void playFirstTurnMovableSuccess(final int sourceRank, final int sourceFile,
-                                     final int targetRank, final int targetFile) {
-        // given
-        final ChessGame chessGame = new ChessGame();
-        final Position source = new Position(sourceRank, sourceFile);
-        final Position target = new Position(targetRank, targetFile);
-        final Piece piece = chessGame.getChessBoard().get(source);
-
-        // when
-        chessGame.playFirstTurn(source, target);
-
-        // then
-        assertThat(chessGame.getChessBoard().get(target))
-                .isEqualTo(piece);
+                .hasMessage("현재 차례가 아닙니다.");
     }
 }
