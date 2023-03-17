f6394a1e738ff241d5dafe1da57afd362d3fce97
SproutMJ <mjk54565@gmail.com>
2023-03-17 14:03:22 +0900

    람다 스트림 미션 구현

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -12,21 +12,21 @@ public class Calculator {
         return total;
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
+    private static int predicateSum(List<Integer> numbers, Conditional conditional) {
         int total = 0;
         for (int number : numbers) {
-            if (number % 2 == 0) {
+            if (conditional.test(number)) {
                 total += number;
             }
         }
         return total;
     }
 
+    public static int sumAllEven(List<Integer> numbers) {
+        return predicateSum(numbers, number -> number % 2 == 0);
+    }
+
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
+        return predicateSum(numbers, number -> number > 3);
     }
 }
--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -3,54 +3,45 @@ package techcourse.fp.mission;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamStudy {
 
     public static long countWords() throws IOException {
-        String contents = Files.readString(Paths
-            .get("src/main/resources/fp/war-and-peace.txt"));
+        String contents = Files.readString(Paths.get("src/main/resources/techcourse/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter(s -> s.length() > 12).count();
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer number : numbers) {
-            result.add(2 * number);
-        }
-
-        return result;
+        return numbers.stream().map(integer -> integer * 2).collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for (Integer number : numbers) {
-            result += number;
-        }
-
-        return result;
+        return numbers.stream().reduce(Integer::sum).get();
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        // TODO: 이 부분을 구현한다.
+        return numbers.stream().filter(integer -> integer > 3).mapToInt(value -> value * 2).sum();
     }
 
     public static void printLongestWordTop100() throws IOException {
-        String contents = Files.readString(Paths
-            .get("src/main/resources/fp/war-and-peace.txt"));
+        String contents = Files.readString(Paths.get("src/main/resources/techcourse/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        System.out.println(words);
-        System.out.println(words.size());
+        //System.out.println(words);
+        //System.out.println(words.size());
         // TODO 이 부분에 구현한다.
+        words.stream().filter(s -> s.length() > 12)
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .distinct()
+                .limit(100)
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
     }
 }
