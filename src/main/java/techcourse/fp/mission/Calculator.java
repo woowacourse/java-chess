10e80eaee5b6b065cda5d1f50138ce6f039cd0f4
kim0914 <kosa0914@naver.com>
2023-03-17 16:16:13 +0900

    chore: 람다 스트림 수업 실습 코드

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -4,29 +4,33 @@ import java.util.List;
 
 public class Calculator {
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
 
-    public static int sumAllEven(List<Integer> numbers) {
+    public static int sumAll(List<Integer> numbers, Conditional numberStrategy) {
         int total = 0;
         for (int number : numbers) {
-            if (number % 2 == 0) {
+            if (numberStrategy.test(number)) {
                 total += number;
             }
         }
         return total;
     }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
-    }
+//
+//    public static int sumAllEven(List<Integer> numbers) {
+//        int total = 0;
+//        for (int number : numbers) {
+//            if (number % 2 == 0) {
+//                total += number;
+//            }
+//        }
+//        return total;
+//    }
+//
+//    public static int sumAllOverThree(List<Integer> numbers) {
+//        int total = 0;
+//
+//
+//
+//
+//        return total;
+//    }
 }
