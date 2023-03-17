0e11bd7fc871f3705e2eafc185cf71528429fff3
eunbii0213 <eunbii0213@naver.com>
2023-03-17 14:01:08 +0900

    feat: view에서 사용할 COMMAND enum 추가

---
+++ src/main/java/chess/view/Command.java
@@ -0,0 +1,23 @@
+package chess.view;
+
+public enum Command {
+    START("start"),
+    MOVE("move"),
+    END("end");
+
+    private static final String NO_COMMAND_ERROR_GUIDE_MESSAGE = "일치하는 명령이 없습니다.";
+    private String value;
+
+    Command(String value) {
+        this.value = value;
+    }
+
+    public static Command findCommandByName(String input) {
+        for (Command command : Command.values()) {
+            if (input.startsWith(command.value)) {
+                return command;
+            }
+        }
+        throw new IllegalArgumentException(NO_COMMAND_ERROR_GUIDE_MESSAGE);
+    }
+}
