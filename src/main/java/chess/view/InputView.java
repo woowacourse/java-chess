06bafc0673983b8d153720350bb36e63bd69d75d
hong-sile <gurwns9325@naver.com>
2023-03-17 17:42:51 +0900

    feat: 형식에 맞지 않는 좌표를 입력받으면 예외처리

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,21 +1,27 @@
 package chess.view;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    public static final String POSITION_FORMAT_VALIDATE_MESSAGE = "이동할 위치를 형식에 맞게 입력해주십시오. ex : b2 b4";
 
     public static String readCommand() {
         return scanner.next().trim();
     }
 
     public static List<String> readPositions() {
-        final List<String> positions = new ArrayList<>();
-        positions.add(scanner.next().trim());
-        positions.add(scanner.nextLine().trim());
+        final List<String> positions = Arrays.stream(scanner.nextLine().trim()
+                .split(" ", -1)).collect(Collectors.toList());
+
+        if (positions.size() != 2) {
+            throw new IllegalArgumentException(POSITION_FORMAT_VALIDATE_MESSAGE);
+        }
+
         return positions;
     }
 }
