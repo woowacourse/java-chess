2f2de9831a4348e002cc7dc0b357e75deb19da87
Miseong Kim <miseongk@naver.com>
2023-03-17 15:21:39 +0900

    feat(MoveStrategy): Piece의 움직임에 대한 전략 구현

---
+++ src/main/java/chess/domain/MoveStrategy.java
@@ -0,0 +1,29 @@
+package chess.domain;
+
+import java.util.function.BiFunction;
+
+public enum MoveStrategy {
+
+    ROOK((fileDiff, rankDiff) -> fileDiff == 0 || rankDiff == 0),
+    BISHOP((fileDiff, rankDiff) -> Math.abs(fileDiff) == Math.abs(rankDiff)),
+    KNIGHT((fileDiff, rankDiff) -> Math.abs(fileDiff * rankDiff) == 2),
+    KING((fileDiff, rankDiff) -> Math.abs(fileDiff) <= 1 && Math.abs(rankDiff) <= 1),
+    QUEEN((fileDiff, rankDiff) -> ROOK.moveStrategy.apply(fileDiff, rankDiff)
+            || BISHOP.moveStrategy.apply(fileDiff, rankDiff)),
+    BLACK_PAWN_FIRST((fileDiff, rankDiff) -> fileDiff == 0 && rankDiff == -2),
+    BLACK_PAWN((fileDiff, rankDiff) -> rankDiff == -1 && Math.abs(fileDiff) <= 1),
+    WHITE_PAWN_FIRST((fileDiff, rankDiff) -> fileDiff == 0 && rankDiff == 2),
+    WHITE_PAWN((fileDiff, rankDiff) -> rankDiff == 1 && Math.abs(fileDiff) <= 1);
+
+    private final BiFunction<Integer, Integer, Boolean> moveStrategy;
+
+    MoveStrategy(BiFunction<Integer, Integer, Boolean> moveStrategy) {
+        this.moveStrategy = moveStrategy;
+    }
+
+    public boolean isMovable(Position source, Position target) {
+        int fileDiff = source.fileDiff(target);
+        int rankDiff = source.rankDiff(target);
+        return moveStrategy.apply(fileDiff, rankDiff);
+    }
+}
