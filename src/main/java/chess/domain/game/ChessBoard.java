7391b486da1524e893b0a99a40b371dd4191eee4
zillionme <beatus96@naver.com>
2023-03-17 17:34:28 +0900

    refactor: 안쓰는 상수 제거 및 접근 제어자 추가

--- src/main/java/chess/domain/game/ChessBoard.java
+++ src/main/java/chess/domain/game/ChessBoard.java
@@ -3,22 +3,16 @@ package chess.domain.game;
 import chess.domain.Color;
 import chess.domain.Position;
 import chess.domain.piece.EmptyPiece;
-import chess.domain.piece.Knight;
 import chess.domain.piece.Piece;
 import chess.practiceMove.Direction;
 
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ChessBoard {
-
-    private static final String NO_PIECE_ERROR_GUIDE_MESSAGE = "이동할 수 있는 기물이 없습니다.";
-    private static final String DIRECTION_ERROR_GUIDE_MESSAGE = "기물이 해당 방향으로 이동할 수 없습니다";
-    private static final String MOVE_ERROR_GUIDE_MESSAGE = "기물이 이동할 수 없습니다";
     private static final String OTHER_PIECE_IN_ROUTE_ERROR_GUIDE_MESSAGE = "이동 경로에 기물이 있으므로 이동할 수 없습니다";
-    private static final String SAME_COLOR_PIECE_EXIST_ERROR_GUIDE_MESSAGE = "같은 팀이라 기물이 이동할 수 없습니다";
-    Map<Position, Piece> chessBoard;
+
+    private final Map<Position, Piece> chessBoard;
 
     public ChessBoard(Map<Position, Piece> chessBoard) {
         this.chessBoard = new HashMap<>(chessBoard);
@@ -61,7 +55,7 @@ public class ChessBoard {
     }
 
     private void checkOtherPieceInTravelRoute(Position currentPosition, Position end, Piece startPiece) {
-        if (!currentPosition.equals(end) && chessBoard.get(currentPosition).getColor()!=Color.NONE) {
+        if (!currentPosition.equals(end) && chessBoard.get(currentPosition).getColor() != Color.NONE) {
             throw new IllegalArgumentException(OTHER_PIECE_IN_ROUTE_ERROR_GUIDE_MESSAGE);
         }
     }
