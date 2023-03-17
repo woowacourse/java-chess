98f18122da672d616e72fce8eddcaf83b40bd702
Eunkeee <eunkeeeee@gmail.com>
2023-03-17 17:04:24 +0900

    refactor(ChessController): 메서드 분리

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.board.BoardFactory;
+
 public class ChessApplication {
     public static void main(String[] args) {
         new ChessController().run();
--- src/main/java/chess/ChessController.java
+++ src/main/java/chess/ChessController.java
@@ -3,49 +3,56 @@ package chess;
 import chess.board.Board;
 import chess.board.BoardFactory;
 import chess.piece.Color;
-import chess.piece.Piece;
 import chess.position.Position;
 import chess.view.InputView;
 import chess.view.OutputView;
 import java.util.List;
-import java.util.Map;
 
 public class ChessController {
-
     private static final String START_COMMAND = "start";
+    private final Board board;
+
+    public ChessController() {
+        BoardFactory boardFactory = new BoardFactory();
+        this.board = new Board(boardFactory);
+    }
+
 
     public void run() {
-        final BoardFactory boardFactory = new BoardFactory();
-
-        final Board board = new Board(boardFactory);
-        final Map<Position, Piece> boardMap = board.board();
-
         String command = InputView.readStartCommand();
-
         if (!command.equals(START_COMMAND)) {
             return;
         }
 
-        Color turn = Color.WHITE;
+        Color color = Color.WHITE;
         while (true) {
-            OutputView.printBoard(boardMap);
-            List<String> moveCommand = InputView.readMoveCommand();
-
-            String from = moveCommand.get(0);
-            String to = moveCommand.get(1);
-
-            int fromFile = from.charAt(0) - 'a' + 1;
-            int fromRank = from.charAt(1) - '0';
-
-            int toFile = to.charAt(0) - 'a' + 1;
-            int toRank = to.charAt(1) - '0';
-
-            Position fromPosition = new Position(fromFile, fromRank);
-            Position toPosition = new Position(toFile, toRank);
-
-            board.move(fromPosition, toPosition, turn);
-
-            turn = turn.opposite();
+            printBoard();
+            movePiece(color);
+            color = changeTurn(color);
         }
+    }
+
+    private static Position searchPosition(final List<String> moveCommand, final int index) {
+        String from = moveCommand.get(index);
+
+        int fromFile = from.charAt(0) - 'a' + 1;
+        int fromRank = from.charAt(1) - '0';
+        return new Position(fromFile, fromRank);
+    }
+
+    private void printBoard() {
+        OutputView.printBoard(board.board());
+    }
+
+    private void movePiece(final Color turn) {
+        List<String> moveCommand = InputView.readMoveCommand();
+        Position from = searchPosition(moveCommand, 0);
+        Position to = searchPosition(moveCommand, 1);
+        board.move(from, to, turn);
+    }
+
+    private static Color changeTurn(Color turn) {
+        turn = turn.opposite();
+        return turn;
     }
 }
