d2dd8472bbf61e7bd11a126790092f3d0e6f7b1c
yoondgu <doyou2ing@naver.com>
2023-03-17 14:27:16 +0900

    refactor: 체스판 초기화 책임 분리

--- src/main/java/chess/domain/ChessBoard.java
+++ src/main/java/chess/domain/ChessBoard.java
@@ -1,21 +1,13 @@
 package chess.domain;
 
-import chess.domain.piece.Bishop;
-import chess.domain.piece.King;
-import chess.domain.piece.Knight;
 import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
-import chess.domain.piece.Queen;
-import chess.domain.piece.Rook;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class ChessBoard {
 
-    private static final int FIRST_INDEX = 1;
-    private static final int RANK_SIZE = 8;
     private static final String WRONG_START_ERROR_MESSAGE = "시작 위치에 말이 없습니다.";
     private static final String OBSTACLE_IN_PATH_ERROR_MESSAGE = "경로에 다른 말이 있어서 이동할 수 없습니다.";
     private static final String WRONG_PAWN_PATH_ERROR_MESSAGE = "폰은 공격 할 때는 대각선으로만, 아닐 떄는 직진으로만 이동할 수 있습니다.";
@@ -23,30 +15,7 @@ public class ChessBoard {
     private static final String WRONG_PIECE_COLOR_ERROR_MESSAGE = "자신 팀의 말만 이동시킬 수 있습니다.";
     private static final String WRONG_ATTACK_TARGET_ERROR_MESSAGE = "상대 팀의 말만 공격할 수 있습니다.";
 
-    private final Map<Position, Piece> piecesByPosition = new HashMap<>();
-
-    public ChessBoard() {
-        initializeBoard();
-    }
-
-    private void initializeBoard() {
-        insertPiecesByColor(Camp.WHITE);
-        insertPiecesByColor(Camp.BLACK);
-    }
-
-    private void insertPiecesByColor(Camp camp) {
-        piecesByPosition.put(Position.of(2, camp.startingRank()), new Knight(camp));
-        piecesByPosition.put(Position.of(1, camp.startingRank()), new Rook(camp));
-        piecesByPosition.put(Position.of(3, camp.startingRank()), new Bishop(camp));
-        piecesByPosition.put(Position.of(4, camp.startingRank()), new King(camp));
-        piecesByPosition.put(Position.of(5, camp.startingRank()), new Queen(camp));
-        piecesByPosition.put(Position.of(6, camp.startingRank()), new Bishop(camp));
-        piecesByPosition.put(Position.of(7, camp.startingRank()), new Knight(camp));
-        piecesByPosition.put(Position.of(8, camp.startingRank()), new Rook(camp));
-        IntStream.range(FIRST_INDEX, RANK_SIZE + 1)
-                .forEach(file -> piecesByPosition.put(Position.of(file, camp.startingPawnRank()),
-                        new Pawn(camp)));
-    }
+    private final Map<Position, Piece> piecesByPosition = PieceInitializer.createPiecesWithPosition();
 
     public void move(List<Integer> sourceCoords, List<Integer> destinationCoords, final Camp camp) {
         Position source = Position.from(sourceCoords);
