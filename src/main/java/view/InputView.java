dbe01e7b07d20a1235df95e02c1277227f93e565
woosung1223 <dntjd991223@naver.com>
2023-03-17 22:37:23 +0900

    refactor: 게임 시작 문구 출력의 책임을 OutputView에게 위임

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,20 +5,12 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String GAME_START_MESSAGE = "> 체스 게임을 시작합니다." + System.lineSeparator() +
-            "> 게임 시작 : start" + System.lineSeparator() +
-            "> 게임 종료 : end" + System.lineSeparator() +
-            "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
     public static final String COMMAND_DELIMITER = " ";
 
     private final Scanner scanner;
 
     public InputView(final Scanner scanner) {
         this.scanner = scanner;
-    }
-
-    public void printGameStartMessage() {
-        System.out.println(GAME_START_MESSAGE);
     }
 
     public List<String> readCommand() {
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,6 +6,11 @@ import domain.board.Rank;
 
 public class OutputView {
 
+    private static final String GAME_START_MESSAGE = "> 체스 게임을 시작합니다." + System.lineSeparator() +
+            "> 게임 시작 : start" + System.lineSeparator() +
+            "> 게임 종료 : end" + System.lineSeparator() +
+            "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
+
     public void printBoard(Board board) {
         StringBuilder stringBuilder = new StringBuilder();
         for (Rank rank : board.getRanks()) {
@@ -21,6 +26,10 @@ public class OutputView {
         }
         stringBuilder.append(System.lineSeparator());
         return stringBuilder;
+    }
+
+    public void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
     }
 
     public void printExceptionMessage(String message) {
