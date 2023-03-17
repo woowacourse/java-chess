f8876f17c807d660b6da39003157bea77838fa37
jiwon <ljwon77@daum.net>
2023-03-17 16:53:10 +0900

    feat: 예외 발생 시 재입력을 받도록 수정한다

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -19,10 +19,20 @@ public class ChessController {
     private void start(final ChessGame chessGame) {
         Status gameStatus = new Start(chessGame);
         while (gameStatus.isRun()) {
+            gameStatus = play(chessGame, gameStatus);
+        }
+    }
+
+    private Status play(final ChessGame chessGame, Status gameStatus) {
+        try {
             List<String> commands = InputView.getCommand();
             final Command command = Command.findCommand(commands);
             gameStatus = gameStatus.checkCommand(command);
             OutputView.printBoard(chessGame.getChessBoard());
+            return gameStatus;
+        } catch (IllegalArgumentException e) {
+            OutputView.print(e.getMessage());
+            return play(chessGame, gameStatus);
         }
     }
 }
