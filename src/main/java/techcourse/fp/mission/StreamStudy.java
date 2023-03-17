45281d05a2158c215251a2adbc779300305ecf78
yenawee <yaena0319@naver.com>
2023-03-17 16:39:58 +0900

    학습 : 정렬 순서 string 길이 내림차순으로 수정

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -40,13 +40,13 @@ public class StreamStudy {
                 .get("src/main/resources/techcourse/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-//        System.out.println(words);
+        System.out.println(words);
         System.out.println(words.size());
 
         words.stream()
                 .distinct()
                 .filter(word -> word.length() > 12)
-                .sorted(Comparator.comparingInt(String::length))
+                .sorted(Comparator.comparingInt(String::length).reversed())
                 .limit(100)
                 .map(String::toLowerCase)
                 .forEach(System.out::println);
