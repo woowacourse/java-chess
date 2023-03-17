4de30760ed51b729ca6f2b176e79258471c14232
dev_kong <ahdjd5@gmail.com>
2023-03-17 15:41:18 +0900

    refa: king 상속을 통한 중복되는 코드 제거

--- src/main/java/domain/piece/type/King.java
+++ src/main/java/domain/piece/type/King.java
@@ -1,16 +1,14 @@
 package domain.piece.type;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import domain.board.Square;
 import domain.piece.Camp;
-import domain.piece.Direction;
 import domain.piece.Piece;
 
 public class King extends Piece {
-    private static final List<Direction> movableDirection = Arrays.asList(Direction.values());
+    public static final int MOVE_DISTANCE_LIMIT = 1;
 
     public King(Camp camp) {
         super(camp);
@@ -23,26 +21,16 @@ public class King extends Piece {
 
     @Override
     public List<Square> fetchMovePath(Square currentSquare, Square targetSquare) {
-        List<Integer> currentCoordinate = currentSquare.toCoordinate();
-        Integer file = currentCoordinate.get(FILE);
-        Integer rank = currentCoordinate.get(RANK);
-
-        for (Direction direction : movableDirection) {
-            ArrayList<Square> squares = new ArrayList<>();
-            int fileCoordinate = file + direction.getFile();
-            int rankCoordinate = rank + direction.getRank();
-            if (isInCoordinateRange(fileCoordinate, rankCoordinate)) {
-                squares.add(new Square(fileCoordinate, rankCoordinate));
-            }
-            if (squares.contains(targetSquare)) {
-                return squares;
-            }
-        }
-        throw new IllegalArgumentException("움직일 수 없는 경로입니다.");
+        List<Integer> gaps = calculateGap(currentSquare, targetSquare);
+        validateMovable(gaps);
+        return List.of(targetSquare);
     }
 
     @Override
     protected void validateMovable(List<Integer> gaps) {
-
+        Integer distance = Collections.max(gaps);
+        if(distance != MOVE_DISTANCE_LIMIT) {
+            throw new IllegalArgumentException("움직일 수 없는 경로입니다.");
+        }
     }
 }
