3c212b5b11e5bd20c6d5edfec1ab40976d4696a6
seokjin8678 <seokjin8678@gmail.com>
2023-03-17 23:30:40 +0900

    refactor: SquareMapper 로직 리팩터링

--- src/main/java/chess/view/SquareMapper.java
+++ src/main/java/chess/view/SquareMapper.java
@@ -6,12 +6,19 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class SquareMapper {
-    private static final Map<Role, String> mapper = new EnumMap<>(Role.class);
+    private static final Map<Team, Map<Role, String>> mapper = new EnumMap<>(Team.class);
 
     private SquareMapper() {
     }
 
     static {
+        mapper.put(Team.WHITE, createWhiteTeamRoleMap());
+        mapper.put(Team.BLACK, createBlackTeamRoleMap());
+        mapper.put(Team.NONE, createNoneTeamRoleMap());
+    }
+
+    private static Map<Role, String> createWhiteTeamRoleMap() {
+        Map<Role, String> mapper = new EnumMap<>(Role.class);
         mapper.put(Role.PAWN, "p");
         mapper.put(Role.KNIGHT, "n");
         mapper.put(Role.BISHOP, "b");
@@ -19,20 +26,34 @@ public class SquareMapper {
         mapper.put(Role.QUEEN, "q");
         mapper.put(Role.KING, "k");
         mapper.put(Role.EMPTY, ".");
+        return mapper;
+    }
+
+    private static Map<Role, String> createBlackTeamRoleMap() {
+        Map<Role, String> mapper = new EnumMap<>(Role.class);
+        mapper.put(Role.PAWN, "P");
+        mapper.put(Role.KNIGHT, "N");
+        mapper.put(Role.BISHOP, "B");
+        mapper.put(Role.ROOK, "R");
+        mapper.put(Role.QUEEN, "Q");
+        mapper.put(Role.KING, "K");
+        mapper.put(Role.EMPTY, ".");
+        return mapper;
+    }
+
+    private static Map<Role, String> createNoneTeamRoleMap() {
+        Map<Role, String> mapper = new EnumMap<>(Role.class);
+        mapper.put(Role.PAWN, ".");
+        mapper.put(Role.KNIGHT, ".");
+        mapper.put(Role.BISHOP, ".");
+        mapper.put(Role.ROOK, ".");
+        mapper.put(Role.QUEEN, ".");
+        mapper.put(Role.KING, ".");
+        mapper.put(Role.EMPTY, ".");
+        return mapper;
     }
 
     public static String map(Team team, Role role) {
-        if (!mapper.containsKey(role)) {
-            throw new IllegalArgumentException("[ERROR] 일치하는 기물이 없습니다.");
-        }
-        String value = mapper.get(role);
-        return convertString(team, value);
-    }
-
-    private static String convertString(Team team, String value) {
-        if (team == Team.BLACK) {
-            return value.toUpperCase();
-        }
-        return value;
+        return mapper.get(team).get(role);
     }
 }
