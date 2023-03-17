a8696086e225f622e47f5497ade8408c24db0159
echo <echo.backend@gmail.com>
2023-03-17 16:13:41 +0900

    style(View): 출력 문구 수정
    Co-authored-by: hanueleee <ezzeezz@naver.com>

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -7,11 +7,9 @@ import java.util.stream.Collectors;
 
 public class InputView {
     
-    private static final String COMMAND_PROMPT = "게임 시작은 start, 종료는 end 명령을 입력하세요.";
     private static final Scanner scanner = new Scanner(System.in);
     
     public List<String> readCommand() {
-        System.out.println(COMMAND_PROMPT);
         String input = scanner.nextLine();
         return Arrays.stream(input.split(" ")).collect(Collectors.toList());
     }
