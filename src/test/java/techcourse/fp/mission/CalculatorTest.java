0f0a9f2cca008de06dd2905ab0cd2c67916a4e1b
seokjin8678 <seokjin8678@gmail.com>
2023-03-17 11:38:08 +0900

    mission: 람다, 스트림 미션 2 수행

--- src/test/java/techcourse/fp/mission/CalculatorTest.java
+++ src/test/java/techcourse/fp/mission/CalculatorTest.java
@@ -11,20 +11,23 @@ class CalculatorTest {
     private final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
     @Test
-    public void sumAll() {
+    void sumAll() {
         int sum = Calculator.sumAll(numbers);
-        assertThat(sum).isEqualTo(21);
+        assertThat(sum)
+                .isEqualTo(21);
     }
 
     @Test
-    public void sumAllEven() {
+    void sumAllEven() {
         int sum = Calculator.sumAllEven(numbers);
-        assertThat(sum).isEqualTo(12);
+        assertThat(sum)
+                .isEqualTo(12);
     }
 
     @Test
-    public void sumAllOverThree() {
+    void sumAllOverThree() {
         int sum = Calculator.sumAllOverThree(numbers);
-        assertThat(sum).isEqualTo(15);
+        assertThat(sum)
+                .isEqualTo(15);
     }
 }
