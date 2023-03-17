72b5091353f32f68325e48de5ec6b3e3c7c81e7b
Choi-JJunho <junho5336@gmail.com>
2023-03-17 13:09:38 +0900

    mission: 스트림 미션 구현
    Co-authored-by: jeomxon <jeomxon@gmail.com>

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -4,29 +4,21 @@ import java.util.List;
 
 public class Calculator {
 
-    public static int sumAll(List<Integer> numbers) {
+    public static int sumAll(List<Integer> numbers, Conditional conditional) {
         int total = 0;
         for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
+            if (conditional.condition(number)) {
                 total += number;
             }
         }
         return total;
     }
 
+    public static int sumAllEven(List<Integer> numbers) {
+        return sumAll(numbers, (number) -> number % 2 == 0);
+    }
+
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
+        return sumAll(numbers, (number) -> number > 3);
     }
 }
