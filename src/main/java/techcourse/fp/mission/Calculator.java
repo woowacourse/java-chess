403d8cad6b8397ed8e3f3129c512754e3bc77cf7
kevstevie <jianna442@gmail.com>
2023-03-17 14:09:39 +0900

    스트림 람다 수업

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -5,28 +5,25 @@ import java.util.List;
 public class Calculator {
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sum(numbers, ignored -> true);
+
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sum(numbers, number -> number > 3);
+    }
+
+    public static int sum(List<Integer> numbers, Condition condition) {
         int total = 0;
-
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
+        for (Integer number : numbers) {
+            if (condition.filter(number)) {
+                total += number;
+            }
+        }
         return total;
     }
 }
