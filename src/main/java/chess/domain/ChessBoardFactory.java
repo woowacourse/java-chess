fcecc2197a6bd260112377338d9947f76498fd3e
bjk1649 <qhwns1649@naver.com>
2023-03-17 16:52:37 +0900

    refactor: 2중 for문을 flatmat으로 변경

--- src/main/java/chess/domain/ChessBoardFactory.java
+++ src/main/java/chess/domain/ChessBoardFactory.java
@@ -12,23 +12,18 @@ import chess.domain.piece.info.Team;
 import chess.domain.position.File;
 import chess.domain.position.Position;
 import chess.domain.position.Rank;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChessBoardFactory {
 
     public static ChessBoard create() {
-        List<Square> squares = new ArrayList<>();
-        for (File file : File.values()) {
-            addSquareOfEachFile(squares, file);
-        }
+        List<Square> squares = Arrays.stream(File.values())
+                .flatMap(file -> Arrays.stream(Rank.values())
+                        .map(rank -> new Square(Position.of(rank, file), createPiece(rank, file))))
+                .collect(Collectors.toUnmodifiableList());
         return new ChessBoard(squares);
-    }
-
-    private static void addSquareOfEachFile(final List<Square> squares, final File file) {
-        for (Rank rank : Rank.values()) {
-            squares.add(new Square(Position.of(rank, file), createPiece(rank, file)));
-        }
     }
 
     private static Piece createPiece(Rank rank, File file) {
