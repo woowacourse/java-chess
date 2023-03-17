0f38c3b928542bb8ce2841bfbcd6382a9bb52aee
kokodak <kokodakadokok@gmail.com>
2023-03-17 14:55:48 +0900

    feat: move 커맨드 추가
    Co-authored-by: parkmuhyeun <pjhg410@gmail.com>

--- src/main/java/chess/domain/Command.java
+++ src/main/java/chess/domain/Command.java
@@ -1,19 +1,44 @@
 package chess.domain;
 
 import java.util.Arrays;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 
 public enum Command {
 
-    START("start", ChessGame::start),
-    END("end", ChessGame::end);
+    START("start", (chessGame, ignored) -> chessGame.start()),
+
+    END("end", (chessGame, ignored) -> chessGame.end()),
+    MOVE("move", moveOrNot());
+
+    public static final int SOURCE_INDEX = 1;
+    public static final int TARGET_INDEX = 2;
+    public static final int FILE_INDEX = 0;
+    public static final int RANK_INDEX = 1;
 
     private final String name;
-    private final Consumer<ChessGame> consumer;
+    private final BiConsumer<ChessGame, String[]> consumer;
 
-    Command(final String name, final Consumer<ChessGame> consumer) {
+    Command(final String name, final BiConsumer<ChessGame, String[]> consumer) {
         this.name = name;
         this.consumer = consumer;
+    }
+
+    private static BiConsumer<ChessGame, String[]> moveOrNot() {
+        return (chessGame, splitCommand) -> {
+            final String[] source = splitPosition(splitCommand, SOURCE_INDEX);
+            final String[] target = splitPosition(splitCommand, TARGET_INDEX);
+
+            final Position sourcePosition = Position.of(File.getFile(source[FILE_INDEX]),
+                    Rank.getRank(Integer.parseInt(source[RANK_INDEX])));
+            final Position targetPosition = Position.of(File.getFile(target[FILE_INDEX]),
+                    Rank.getRank(Integer.parseInt(target[RANK_INDEX])));
+
+            chessGame.moveOrNot(sourcePosition, targetPosition);
+        };
+    }
+
+    private static String[] splitPosition(final String[] splitCommand, final int index) {
+        return splitCommand[index].split("");
     }
 
     public static Command findByString(final String name) {
@@ -23,7 +48,7 @@ public enum Command {
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 커맨드입니다."));
     }
 
-    public void execute(final ChessGame chessGame) {
-        this.consumer.accept(chessGame);
+    public void execute(final ChessGame chessGame, final String[] splitCommand) {
+        this.consumer.accept(chessGame, splitCommand);
     }
 }
