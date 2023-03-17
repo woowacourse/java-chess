abbd79600f99fe9e6dc0c993a137f4a2ccbd72dd
이건회 <rg970604@naver.com>
2023-03-17 15:17:24 +0900

    refactor : CarTest 람다식 추가

--- src/test/java/techcourse/fp/mission/CarTest.java
+++ src/test/java/techcourse/fp/mission/CarTest.java
@@ -10,6 +10,12 @@ class CarTest {
     @Test
     public void 이동() {
         Car car = new Car("pobi", 0);
+//        Car actual = car.move(new MoveStrategy() {
+//            @Override
+//            public boolean isMovable() {
+//                return true;
+//            }
+//        });
         Car actual = car.move(() -> true);
         assertThat(actual).isEqualTo(new Car("pobi", 1));
     }
@@ -17,6 +23,12 @@ class CarTest {
     @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
+//        Car actual = car.move(new MoveStrategy() {
+//            @Override
+//            public boolean isMovable() {
+//                return false;
+//            }
+//        });
         Car actual = car.move(() -> false);
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
