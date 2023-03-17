e329eb9a39cebc7f84f5d6c216239475a881437e
aiaiaiai1 <cvc100326@naver.com>
2023-03-17 15:58:16 +0900

    train: 스트림, 람다 미니 미션

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -1,32 +1,30 @@
 package techcourse.fp.mission;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Calculator {
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumBasic(numbers, ignored -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
+        return sumBasic(numbers, s -> s % 2 == 0);
+    }
+
+    public static int sumAllOverThree(List<Integer> numbers) {
+        return sumBasic(numbers, s -> s > 3);
+    }
+
+    public static int sumBasic(List<Integer> numbers, Predicate<Integer> predicate) {
         int total = 0;
         for (int number : numbers) {
-            if (number % 2 == 0) {
+            if (predicate.test(number)) {
                 total += number;
             }
         }
         return total;
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
-    }
 }
