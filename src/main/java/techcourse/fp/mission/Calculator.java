c58910599143b73a97b00758c9be70b5fd900ed5
java-saeng <gusdn85554@naver.com>
2023-03-17 11:25:10 +0900

    minimission : 미션 2 끗

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -4,29 +4,24 @@ import java.util.List;
 
 public class Calculator {
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumConditionalStrategy(List<Integer> numbers, CalculateStrategy strategy) {
+        return calculateTotal(numbers, strategy);
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
+    private static int calculateTotal(final List<Integer> numbers, final CalculateStrategy strategy) {
         int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
+
+        for (final int number : numbers) {
+            if (strategy.canCalculate(number)) {
                 total += number;
             }
         }
         return total;
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
+    @FunctionalInterface
+    interface CalculateStrategy {
 
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-
-        return total;
+        boolean canCalculate(Integer number);
     }
 }
