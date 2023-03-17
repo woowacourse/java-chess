b08b03bdd01550c1010276f5cceeb71ae42f4ae7
seokjin8678 <seokjin8678@gmail.com>
2023-03-17 12:15:31 +0900

    mission: 람다, 스트림 미션 3 수행

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -1,9 +1,11 @@
 package techcourse.fp.mission;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,46 +13,43 @@ public class StreamStudy {
 
     public static long countWords() throws IOException {
         String contents = Files.readString(Paths
-            .get("src/main/resources/fp/war-and-peace.txt"));
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+            .get("src/main/resources/techcourse/fp/war-and-peace.txt"));
+         return Arrays.stream(contents.split("[\\P{L}]+"))
+                .filter(StreamStudy::isOverThan12)
+                .count();
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer number : numbers) {
-            result.add(2 * number);
-        }
-
-        return result;
+        return numbers.stream()
+                .map(number -> number * 2)
+                .collect(toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for (Integer number : numbers) {
-            result += number;
-        }
-
-        return result;
+        return numbers.stream()
+                .reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToInt(number -> number * 2)
+                .sum();
     }
 
     public static void printLongestWordTop100() throws IOException {
         String contents = Files.readString(Paths
-            .get("src/main/resources/fp/war-and-peace.txt"));
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+            .get("src/main/resources/techcourse/fp/war-and-peace.txt"));
+        Arrays.stream(contents.split("[\\P{L}]+"))
+                .filter(StreamStudy::isOverThan12)
+                .distinct()
+                .map(String::toLowerCase)
+                .sorted(comparing(String::length).reversed())
+                .limit(100)
+                .forEach(System.out::println);
+    }
 
-        System.out.println(words);
-        System.out.println(words.size());
-        // TODO 이 부분에 구현한다.
+    private static boolean isOverThan12(String word) {
+        return word.length() > 12;
     }
 }
