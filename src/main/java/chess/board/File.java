a30c003af1db1806df947757bb6729512479438c
yujamint <woawoa0513@gmail.com>
2023-03-17 16:46:44 +0900

    feat(File): text 속성 추가 및 정적 팩터리 메서드 구현
    입력값에 따라 File 값을 반환하는 정적 팩터리 메서드 구현

--- src/main/java/chess/board/File.java
+++ src/main/java/chess/board/File.java
@@ -3,22 +3,31 @@ package chess.board;
 import java.util.Arrays;
 
 public enum File {
-    A(1),
-    B(2),
-    C(3),
-    D(4),
-    E(5),
-    F(6),
-    G(7),
-    H(8);
+    A(1, "a"),
+    B(2, "b"),
+    C(3, "c"),
+    D(4, "d"),
+    E(5, "e"),
+    F(6, "f"),
+    G(7, "g"),
+    H(8, "h");
 
     private final int value;
+    private final String text;
 
-    File(final int value) {
+    File(final int value, final String text) {
         this.value = value;
+        this.text = text;
     }
 
-    public static File findByValue(int value) {
+    public static File of(String text) {
+        return Arrays.stream(values())
+                .filter(file -> file.text.equals(text))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당하는 파일 값이 존재하지 않습니다."));
+    }
+
+    public static File of(int value) {
         return Arrays.stream(values())
                 .filter(file -> file.value == value)
                 .findAny()
