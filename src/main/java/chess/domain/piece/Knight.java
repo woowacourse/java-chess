43e0c840f564d4af3afb23d3a61a5f10c22bbec4
seokjin8678 <seokjin8678@gmail.com>
2023-03-17 19:28:13 +0900

    fix: 불필요한 static 제한자 제거

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -17,7 +17,7 @@ public class Knight extends Piece {
         return isInKnightRange(xDistance, yDistance);
     }
 
-    private static boolean isInKnightRange(int xDistance, int yDistance) {
+    private boolean isInKnightRange(int xDistance, int yDistance) {
         return (xDistance == 2 && yDistance == 1) || (xDistance == 1 && yDistance == 2);
     }
 }
