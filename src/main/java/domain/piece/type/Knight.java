4177cf78320ecd4640ff0f9f2626dc52a91cdae6
dev_kong <ahdjd5@gmail.com>
2023-03-17 16:01:03 +0900

    refa: knight 중복코드 제거

--- src/main/java/domain/piece/type/Knight.java
+++ src/main/java/domain/piece/type/Knight.java
@@ -7,16 +7,8 @@ import domain.piece.Camp;
 import domain.piece.Piece;
 
 public class Knight extends Piece {
-    private static final List<List<Integer>> movableDirection = List.of(
-            List.of(1, 2),
-            List.of(1, -2),
-            List.of(-1, 2),
-            List.of(-1, -2),
-            List.of(2, 1),
-            List.of(2, -1),
-            List.of(-2, 1),
-            List.of(-2, -1)
-    );
+    public static final int KNIGHT_GAP_SUM = 3;
+    public static final int KNIGHT_GAP_SUBTRACTION = 1;
 
     public Knight(Camp camp) {
         super(camp);
@@ -29,16 +21,19 @@ public class Knight extends Piece {
 
     @Override
     public List<Square> fetchMovePath(Square currentSquare, Square targetSquare) {
-        List<Integer> coordinateGap = List.of(
-                currentSquare.toCoordinate().get(FILE) - targetSquare.toCoordinate().get(FILE),
-                currentSquare.toCoordinate().get(RANK) - targetSquare.toCoordinate().get(RANK));
-        if (movableDirection.contains(coordinateGap)) {
-            return List.of(targetSquare);
-        }
-        throw new IllegalArgumentException("움직일 수 없는 경로입니다.");
+        List<Integer> gaps = calculateGap(currentSquare, targetSquare);
+        validateMovable(gaps);
+        return List.of(targetSquare);
     }
 
     @Override
     protected void validateMovable(List<Integer> gaps) {
+        Integer fileGapAbs = Math.abs(gaps.get(FILE));
+        Integer rankGapAbs = Math.abs(gaps.get(RANK));
+        int gapsSum = fileGapAbs + rankGapAbs;
+        int gapsSubtraction = Math.abs(fileGapAbs - rankGapAbs);
+        if(gapsSum != KNIGHT_GAP_SUM || gapsSubtraction != KNIGHT_GAP_SUBTRACTION) {
+            throw new IllegalArgumentException("움직일 수 없는 경로입니다.");
+        }
     }
 }
