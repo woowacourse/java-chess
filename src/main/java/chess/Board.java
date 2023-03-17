1d65c5721573794c6029c5e5a1c27993669b86bf
Eunkeee <eunkeeeee@gmail.com>
2023-03-17 14:11:38 +0900

    refactor(Board): 메서드 분리

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -18,20 +18,33 @@ public class Board {
     }
 
     public void move(Position from, Position to, final Color turn) {
+        validateCanMove(from, to, turn);
+        movePiece(from, to);
+    }
+
+    private void validateCanMove(final Position from, final Position to, final Color turn) {
         validateIsFromEmpty(from);
-        if (!board.get(from).isSameColor(turn)) {
-            throw new IllegalArgumentException("차례에 맞는 말을 선택해 주세요");
-        }
+        validateIsDifferentColor(from, turn);
+
         Path path = board.get(from)
                 .searchPathTo(from, to, Optional.ofNullable(board.get(to)));
         path.validateObstacle(board.keySet());
-        Piece piece = board.remove(from);
-        board.put(to, piece);
+    }
+
+    private void validateIsDifferentColor(final Position from, final Color turn) {
+        if (!board.get(from).isSameColor(turn)) {
+            throw new IllegalArgumentException("차례에 맞는 말을 선택해 주세요");
+        }
     }
 
     private void validateIsFromEmpty(final Position from) {
         if (!board.containsKey(from)) {
             throw new IllegalArgumentException("출발점에 말이 없습니다.");
         }
+    }
+
+    private void movePiece(final Position from, final Position to) {
+        Piece piece = board.remove(from);
+        board.put(to, piece);
     }
 }
