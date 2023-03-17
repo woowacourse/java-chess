c6f5f35174be3fc6cff68e10dbe81a33fd50ae20
echo <echo.backend@gmail.com>
2023-03-17 16:45:11 +0900

    refactor(EmptyPiece): 빈 피스 실행 예외 처리
    Co-authored-by: hanueleee <ezzeezz@naver.com>

--- src/main/java/chess/domain/piece/Empty.java
+++ src/main/java/chess/domain/piece/Empty.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Empty extends Piece {
     
+    public static final String EMPTY_PIECE_CANNOT_MOVE_ERROR_MESSAGE = "빈 피스는 움직일 수 없습니다.";
+    
     private Empty() {
         super(Color.NONE, PieceType.EMPTY, List.of());
     }
@@ -15,5 +17,6 @@ public class Empty extends Piece {
     
     @Override
     public void canMove(final Position start, final Position end) {
+        throw new IllegalStateException(EMPTY_PIECE_CANNOT_MOVE_ERROR_MESSAGE);
     }
 }
