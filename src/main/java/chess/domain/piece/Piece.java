68c738aeae9a588d63ba0166c43fa39ae7c67bad
eunbii0213 <eunbii0213@naver.com>
2023-03-17 13:40:32 +0900

    feat: Piece 색깔에 따라 이름을 결정하는 기능 추가

---
+++ src/main/java/chess/domain/piece/Piece.java
@@ -0,0 +1,21 @@
+package chess.domain.piece;
+
+import chess.domain.Color;
+
+public abstract class Piece {
+
+    private final String name;
+    private final Color color;
+
+    public Piece(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String findName() {
+        if (color == Color.WHITE) {
+            return name.toLowerCase();
+        }
+        return name.toUpperCase();
+    }
+}
