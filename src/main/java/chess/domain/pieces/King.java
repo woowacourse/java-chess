86fb692a297e7c0e59521129ab2f2968bc6fcdc3
sosow0212 <sosow0212@naver.com>
2023-03-17 14:52:12 +0900

    refactor: King의 이동 예외 코드를 보다 명확하게 네이밍 및 메서드명 리팩토링 진행

--- src/main/java/chess/domain/pieces/King.java
+++ src/main/java/chess/domain/pieces/King.java
@@ -1,11 +1,11 @@
 package chess.domain.pieces;
 
-import chess.domain.board.Col;
+import chess.domain.board.Column;
 import chess.domain.board.Position;
 import chess.domain.board.Row;
 import java.util.List;
 
-public class King extends Piece{
+public class King extends Piece {
 
     public King(final Name name) {
         super(name);
@@ -13,15 +13,19 @@ public class King extends Piece{
 
     @Override
     public void canMove(final Position start, final Position end) {
-        List<List<Integer>> possiblePosition = List.of(List.of(1,0), List.of(0,1), List.of(1,1));
-
         int absSubRow = Math.abs(Row.subPositionFromArrivePosition(start.getRow(), end.getRow()));
-        int absSubCol = Math.abs(Col.subPositionFromArrivePosition(start.getCol(), end.getCol()));
+        int absSubCol = Math.abs(Column.subPositionFromArrivePosition(start.getCol(), end.getCol()));
 
-        List<Integer> subPosition = List.of(absSubRow, absSubCol);
+        List<Integer> nextDirection = List.of(absSubRow, absSubCol);
 
-        if (!possiblePosition.contains(subPosition)) {
-            throw new IllegalArgumentException("올바른 위치가 아닙니다.");
+        validateMove(nextDirection);
+    }
+
+    private void validateMove(final List<Integer> nextDirection) {
+        List<List<Integer>> possibleDirectionOfKing = List.of(List.of(1, 0), List.of(0, 1), List.of(1, 1));
+
+        if (!possibleDirectionOfKing.contains(nextDirection)) {
+            throw new IllegalArgumentException("King의 이동 범위가 올바르지 않습니다.");
         }
     }
 }
