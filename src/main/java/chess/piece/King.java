1f0fcf272184752c14ecf679a2d13ccb2990607d
java-saeng <gusdn85554@naver.com>
2023-03-17 16:37:31 +0900

    refactor : King exception message 수정, 테스트 refactoring

--- src/main/java/chess/piece/King.java
+++ src/main/java/chess/piece/King.java
@@ -1,5 +1,12 @@
 package chess.piece;
 
+import chess.Movement;
+import chess.Path;
+import chess.Position;
+
+import java.util.List;
+import java.util.Optional;
+
 import static chess.Movement.D;
 import static chess.Movement.DL;
 import static chess.Movement.DR;
@@ -8,13 +15,6 @@ import static chess.Movement.R;
 import static chess.Movement.U;
 import static chess.Movement.UL;
 import static chess.Movement.UR;
-
-import chess.Movement;
-import chess.Path;
-import chess.Position;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 public class King extends Piece {
 
@@ -31,11 +31,11 @@ public class King extends Piece {
         Movement movement = to.convertMovement(from);
 
         if (!from.moveBy(movement).equals(to)) {
-            throw new IllegalStateException();
+            throw new IllegalStateException(this.getClass().getSimpleName() + "은 한 칸만 이동할 수 있습니다.");
         }
 
         if (!CAN_MOVE_DESTINATION.contains(movement)) {
-            throw new IllegalStateException();
+            throw new IllegalStateException(this.getClass().getSimpleName() + "이(가) 이동할 수 없는 경로입니다.");
         }
 
         return new Path();
