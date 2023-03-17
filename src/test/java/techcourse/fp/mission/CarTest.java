175cbd6d9abaf2e68961270805e26879ae2ae904
amaran-th <songsy405@gmail.com>
2023-03-17 11:54:04 +0900

    feat : 미니미션(Lambda&Generic) 1단계 구현

--- src/test/java/techcourse/fp/mission/CarTest.java
+++ src/test/java/techcourse/fp/mission/CarTest.java
@@ -20,6 +20,15 @@ class CarTest {
     }
 
     @Test
+    public void 이동_람다() {
+        Car car = new Car("pobi", 0);
+        MoveStrategy moveStrategy = () -> true;
+        Car actual = car.move(moveStrategy);
+        assertThat(actual).isEqualTo(new Car("pobi", 1));
+    }
+
+
+    @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
         Car actual = car.move(new MoveStrategy() {
@@ -28,6 +37,14 @@ class CarTest {
                 return false;
             }
         });
+        assertThat(actual).isEqualTo(new Car("pobi", 0));
+    }
+
+    @Test
+    public void 정지_람다() {
+        Car car = new Car("pobi", 0);
+        MoveStrategy stopStrategy = () -> false;
+        Car actual = car.move(stopStrategy);
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
 }
