be1d01a3cd98324b64d5bef974e8f4f3019b3121
bjk1649 <qhwns1649@naver.com>
2023-03-17 10:58:43 +0900

    fix: 시작 명령어 비교 조건 수정

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
 
     public void printGameGuide() {
         System.out.println("> 체스 게임을 시작합니다.");
-        System.out.println("> 게임 시작 : start");
+        System.out.println(String.format("> 게임 시작 : %s", Command.START));
         System.out.println(String.format("> 게임 종료 : %s", Command.END));
         System.out.println(String.format("> 게임 이동 : %s source위치 target위치 - 예. %s b2 b3"
                 , Command.MOVE, Command.MOVE));
