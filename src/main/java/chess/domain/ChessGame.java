eb37ed27b8b95e7ccd1ecd79f3cb369297c1e7f3
woo-chang <clllickme@naver.com>
2023-03-18 22:05:13 +0900

    refactor: Optional을 받는 시점에 처리하도록 수정

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -4,18 +4,15 @@ import chess.domain.board.Board;
 import chess.domain.board.Square;
 import chess.domain.piece.Piece;
 import java.util.List;
-import java.util.Optional;
 
 public class ChessGame {
 
     private final Board board = Board.generate();
 
     public void move(final Square source, final Square destination) {
-        final Optional<Piece> pieceOf = board.findPieceOf(source);
-        if (pieceOf.isEmpty()) {
-            throw new IllegalArgumentException("움직일 기물이 존재하지 않습니다.");
-        }
-        moveToDestination(pieceOf.get(), source, destination);
+        final Piece piece = board.findPieceOf(source)
+                .orElseThrow(() -> new IllegalArgumentException("움직일 기물이 존재하지 않습니다."));
+        moveToDestination(piece, source, destination);
     }
 
     private void moveToDestination(final Piece piece, final Square source, final Square destination) {
