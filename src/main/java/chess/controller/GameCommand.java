d213d3231d8cf2689f8a2ca89c09bcad7d70b97b
apptie <appti@naver.com>
2023-03-18 18:06:26 +0900

    refactor: 불필요한 메소드 분리 통합

--- src/main/java/chess/controller/GameCommand.java
+++ src/main/java/chess/controller/GameCommand.java
@@ -15,12 +15,8 @@ public enum GameCommand {
     }
 
     public static GameCommand from(final String input) {
-        return Arrays.stream(values()).filter(gameCommand -> gameCommand.match(input))
+        return Arrays.stream(values()).filter(gameCommand -> gameCommand.command.equalsIgnoreCase(input))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 커맨드입니다."));
-    }
-
-    private boolean match(final String input) {
-        return this.command.equalsIgnoreCase(input);
     }
 }
