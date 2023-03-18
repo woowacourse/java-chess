c8199ce524bb9de56bd81a7b4616a84c39ec6cbd
zillionme <beatus96@naver.com>
2023-03-18 19:09:50 +0900

    refactor: Knight 예외 메시지 상수화 및 메서드명 수정

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,14 +1,17 @@
 package chess.domain.piece;
 
 import chess.domain.Color;
+import chess.domain.Direction;
 import chess.domain.Position;
-import chess.practiceMove.Direction;
 
 import java.util.List;
 import java.util.Map;
 
 public class Knight extends Piece {
     private static final String name = "n";
+    public static final String DIRECTION_ERROR_MESSAGE = "Knight가 이동할 수 있는 방향이 아닙니다";
+    public static final String DISTANCE_ERROR_MESSAGE = "Knight가 한 번에 이동할 수 있는 거리가 아닙니다";
+    public static final String MOVE_ERROR_MESSAGE = "Knight는 도착점에 아군이 있으면 이동할 수 없습니다.";
     private static final Map<Integer, Integer> movableDistance = Map.of(1, 2, 2, 1);
     private static final List<Direction> movableDirection = List.of(
             Direction.KNIGHT_TOP_LEFT,
@@ -27,37 +30,39 @@ public class Knight extends Piece {
 
     @Override
     public boolean isMovable(Position start, Position end, Color colorOfDestination) {
-        Direction direction = Direction.findDirectionByGap(start, end, this);
+        Direction direction = Direction.findDirectionByGap(start, end);
+
         checkMovableDirection(direction);
-        checkMovableAtOnce(start, end);
+        checkMovableDistance(start, end);
         checkMovableToDestination(colorOfDestination);
         return true;
     }
 
-    public void checkMovableAtOnce(Position start, Position end) {
-        int absGapOfColumn = start.findGapOfColum(end);
-        int absGapOfRank = start.findGapOfRank(end);
-
-        if(!isMovableAtOnce(absGapOfColumn, absGapOfRank)) {
-            throw new IllegalArgumentException("knight이 한 번에 이동할 수 있는 거리가 아닙니다");
-        }
-    }
-
-    private boolean isMovableAtOnce(int absGapOfColumn, int absGapOfRank) {
-        return movableDistance.entrySet()
-         .stream()
-         .anyMatch((entry) -> entry.getKey() == absGapOfColumn && entry.getValue() == absGapOfRank);
-    }
-
+    @Override
     public void checkMovableDirection(Direction direction) {
         if(!movableDirection.contains(direction)){
-            throw new IllegalArgumentException("knight가 이동할 수 있는 방향이 아닙니다");
+            throw new IllegalArgumentException(DIRECTION_ERROR_MESSAGE);
         }
+    }
+
+    public void checkMovableDistance(Position start, Position end) {
+        int distanceOfColumns = Math.abs(start.findGapOfColum(end));
+        int distanceOfRanks = Math.abs(start.findGapOfRank(end));
+
+        if(!isMovableDistance(distanceOfColumns, distanceOfRanks)) {
+            throw new IllegalArgumentException(DISTANCE_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isMovableDistance(int absGapOfColumn, int absGapOfRank) {
+        return movableDistance.entrySet()
+         .stream()
+         .anyMatch(entry -> entry.getKey() == absGapOfColumn && entry.getValue() == absGapOfRank);
     }
 
     private void checkMovableToDestination(Color colorOfDestination) {
         if(this.isSameColor(colorOfDestination)) {
-            throw new IllegalArgumentException("목적지에 아군이 있으므로 Knight는 이동할 수 없습니다.");
+            throw new IllegalArgumentException(MOVE_ERROR_MESSAGE);
         }
 
     }
