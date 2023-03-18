1260701abbd997deaf41775708f004dd2b290b05
0chil <0@chll.it>
2023-03-19 01:07:14 +0900

    feat: 방향 컬렉션

---
+++ src/main/java/chess/domain/move/Directions.java
@@ -0,0 +1,117 @@
+package chess.domain.move;
+
+import static chess.domain.move.Axis.HORIZON;
+import static chess.domain.move.Axis.VERTICAL;
+import static chess.domain.move.Direction.DOWN;
+import static chess.domain.move.Direction.LEFT;
+import static chess.domain.move.Direction.RIGHT;
+import static chess.domain.move.Direction.UP;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import chess.domain.position.Position;
+import chess.util.Mathematics;
+
+public class Directions {
+
+    public static final Directions NONE = new Directions(Collections.emptyList());
+
+    private final List<Direction> directions;
+
+    public Directions(List<Direction> directions) {
+        validateVerticalBidirectional(directions);
+        validateHorizontalBidirectional(directions);
+        this.directions = directions.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public static Directions of(Direction... directions) {
+        return new Directions(List.of(directions));
+    }
+
+    private void validateHorizontalBidirectional(List<Direction> directions) {
+        if (directions.contains(UP) && directions.contains(DOWN)) {
+            throw new IllegalArgumentException("수직이나 수평으로 양방향이면 안됩니다");
+        }
+    }
+
+    private void validateVerticalBidirectional(List<Direction> directions) {
+        if (directions.contains(LEFT) && directions.contains(RIGHT)) {
+            throw new IllegalArgumentException("수직이나 수평으로 양방향이면 안됩니다");
+        }
+    }
+
+    public long count(Direction direction) {
+        return directions.stream()
+                .filter(it -> it.equals(direction))
+                .count();
+    }
+
+    public long countDirectionsIn(Axis axis) {
+        return directions.stream()
+                .filter(it -> it.isIn(axis))
+                .count();
+    }
+
+    public Position move(Position position) {
+        Position destination = position;
+        for (Direction direction : directions) {
+            destination = destination.move(direction);
+        }
+        return destination;
+    }
+
+    public Directions flipOver(Axis axis) {
+        return new Directions(directions.stream()
+                .map(direction -> direction.flipOver(axis))
+                .collect(Collectors.toList()));
+    }
+
+    public Directions join(Directions other) {
+        List<Direction> directions = new ArrayList<>(this.directions);
+        directions.addAll(other.directions);
+        return new Directions(directions);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Directions that = (Directions)o;
+
+        return Objects.equals(directions, that.directions);
+    }
+
+    @Override
+    public int hashCode() {
+        return directions != null ? directions.hashCode() : 0;
+    }
+
+    public Optional<Direction> getDirectionOf(Axis axis) {
+        return directions.stream()
+                .filter(direction -> direction.isIn(axis))
+                .findFirst();
+    }
+
+    public boolean isEmpty() {
+        return directions.isEmpty();
+    }
+
+    public long countMinimumUnits() {
+        return Mathematics.getGCD(countDirectionsIn(HORIZON), countDirectionsIn(VERTICAL));
+    }
+
+    @Override
+    public String toString() {
+        return directions.toString();
+    }
+}
---
+++ src/test/java/chess/domain/move/DirectionsTest.java
@@ -0,0 +1,100 @@
+package chess.domain.move;
+
+import static chess.domain.move.Direction.DOWN;
+import static chess.domain.move.Direction.LEFT;
+import static chess.domain.move.Direction.RIGHT;
+import static chess.domain.move.Direction.UP;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import chess.domain.AbstractTestFixture;
+
+public class DirectionsTest extends AbstractTestFixture {
+
+    @DisplayName("수직이나 수평으로 양방향이면 예외를 던진다")
+    @ParameterizedTest(name = "{0}, {1}")
+    @CsvSource({"UP,DOWN", "LEFT,RIGHT"})
+    void horizontalOrVertical_bidirectional_throws(Direction direction, Direction direction2) {
+        assertThatThrownBy(() -> new Directions(List.of(direction, direction2)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수직이나 수평으로 양방향이면 안됩니다");
+    }
+
+    @DisplayName("특정 방향이 몇 개인지 센다")
+    @Test
+    void countDirection() {
+        Directions upAndRight = new Directions(List.of(UP, UP, RIGHT, RIGHT, RIGHT));
+
+        assertThat(upAndRight.count(RIGHT)).isEqualTo(3);
+    }
+
+    @DisplayName("특정 축의 방향이 몇 개인지 센다")
+    @Test
+    void countDirectionsInAxis() {
+        Directions upAndRight = new Directions(List.of(UP, UP, RIGHT, RIGHT, RIGHT));
+
+        assertThat(upAndRight.countDirectionsIn(Axis.HORIZON)).isEqualTo(3);
+    }
+
+    @DisplayName("목적지를 찾을 수 있다")
+    @Test
+    void findDestination() {
+        var directions = new Directions(List.of(RIGHT, RIGHT, UP, UP, UP));
+        var destination = directions.move(createPosition("A,ONE"));
+
+        assertThat(destination).isEqualTo(createPosition("C,FOUR"));
+    }
+
+    @DisplayName("수평 축을 기준으로 뒤집는다")
+    @Test
+    void flipOverHorizon() {
+        var directions = new Directions(List.of(RIGHT, UP, UP));
+        var flipped = new Directions(List.of(RIGHT, DOWN, DOWN));
+
+        assertThat(directions.flipOver(Axis.HORIZON)).isEqualTo(flipped);
+    }
+
+    @DisplayName("수직 축을 기준으로 뒤집는다")
+    @Test
+    void flipOverVertical() {
+        var directions = new Directions(List.of(RIGHT, RIGHT, UP));
+        var flipped = new Directions(List.of(LEFT, LEFT, UP));
+
+        assertThat(directions.flipOver(Axis.VERTICAL)).isEqualTo(flipped);
+    }
+
+    @DisplayName("방향 컬렉션끼리 합칠 수 있다")
+    @Test
+    void joinDirections() {
+        var directions = new Directions(List.of(RIGHT, RIGHT, UP));
+        var other = new Directions(List.of(RIGHT, RIGHT, RIGHT));
+
+        assertThat(directions.join(other)).isEqualTo(new Directions(List.of(RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, UP)));
+    }
+
+    static Stream<Arguments> countMinimumUnits() {
+        return Stream.of(
+                Arguments.of(List.of(RIGHT, RIGHT, UP), 1),
+                Arguments.of(List.of(RIGHT, UP), 1),
+                Arguments.of(List.of(LEFT, LEFT), 2),
+                Arguments.of(List.of(LEFT, LEFT), 2)
+        );
+    }
+
+    @DisplayName("단위가 몇 개인지 셀 수 있다")
+    @ParameterizedTest
+    @MethodSource
+    void countMinimumUnits(List<Direction> directions, int expectedCount) {
+        assertThat(new Directions(directions).countMinimumUnits()).isEqualTo(expectedCount);
+    }
+}
