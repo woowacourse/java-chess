2da04d1ebbea82209590580a38c62ce3df98ec50
zillionme <beatus96@naver.com>
2023-03-19 00:25:50 +0900

    refactor: 체스보드 초기화 기능 -forEach를 연산으로 사용하지 않도록 수정

--- src/main/java/chess/domain/boardstrategy/InitialBoardStrategy.java
+++ src/main/java/chess/domain/boardstrategy/InitialBoardStrategy.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class InitialBoardStrategy implements BoardStrategy {
@@ -26,29 +28,39 @@ public class InitialBoardStrategy implements BoardStrategy {
         return new HashMap<>(board);
     }
 
+    /**
+     * 질문 : 처음 foreach를 사용하여 필드(Map)에 Position과 기물들을 put하였습니다.
+     * stream 사용 시, foreach는 동시성 보장이 안되기 때문에 연산에 사용하지 말라는 글을 읽고
+     * for문 대신 stream을 사용하되 아래와 같이 수정하였습니다.
+     * foreach를 사용하지 않으니 가독성이 떨어지는 점도 있는데, 차라리 for문을 사용하는 것이 나은지
+     * 현재와 같이 stream을 사용해도 좋은지
+     * 리뷰어님의 의견 궁금합니다.
+     */
     private void initEmptyPieces() {
-        for (Rank rank : Rank.getOrderedRanks()) {
-            for (Column column : Column.getOrderedColumns()) {
-                board.put(new Position(column, rank), new EmptyPiece());
-            }
-        }
+        Map<Position, Piece> emptyPieces = Column.getOrderedColumns().stream()
+                .flatMap(column -> Rank.getOrderedRanks().stream()
+                        .map(rank -> new Position(column, rank)))
+                .collect(Collectors.toMap(Function.identity(), ignored -> new EmptyPiece()));
+
+        board.putAll(emptyPieces);
     }
 
     private void initFirstRow(Rank rank, Color color) {
         List<Piece> firstRowPieces = List.of(new Rook(color), new Knight(color), new Bishop(color), new Queen(color),
                 new King(color), new Bishop(color), new Knight(color), new Rook(color));
 
-        IntStream.range(0, firstRowPieces.size())
-                .forEach(i -> board.replace(new Position(Column.findColumnByIndex(i+1), rank),
-                        firstRowPieces.get(i))
-                );
+        Map<Position, Piece> firstRow = IntStream.range(0, firstRowPieces.size())
+                .boxed()
+                .collect(Collectors.toMap(index -> new Position(Column.findColumnByIndex(index + 1), rank),
+                        firstRowPieces::get));
 
+        board.putAll(firstRow);
     }
 
     private void initSecondRow(Rank rank, Color color) {
-        Arrays.stream(Column.values())
-                .forEach(column -> board.replace(new Position(column, rank),
-                        new Pawn(color))
-                );
+        Map<Position, Piece> secondRow = Arrays.stream(Column.values())
+                .collect(Collectors.toMap(column -> new Position(column, rank), ignored -> new Pawn(color)));
+
+        board.putAll(secondRow);
     }
 }
