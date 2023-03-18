1ad8eef4e9f5511391b263aaa66b597f26a2b092
sh111-coder <ohk9134@naver.com>
2023-03-18 17:53:52 +0900

    test(PieceTest): Piece 받은 위치로 이동하는 기능 테스트

--- src/test/java/chess/piece/PieceTest.java
+++ src/test/java/chess/piece/PieceTest.java
@@ -2,11 +2,16 @@ package chess.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
+
 import chess.board.File;
 import chess.board.Position;
 import chess.board.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PieceTest {
 
@@ -34,18 +39,26 @@ class PieceTest {
         assertThat(piece.isSameSide(differentSidePiece)).isFalse();
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("moveTestCases")
     @DisplayName("받은 위치로 이동한다.")
-    void move() {
-        // given
-        final Piece piece = new Rook(new Position(File.A, Rank.ONE), Side.WHITE);
-        final Position positionToMove = new Position(File.F, Rank.ONE);
-
+    void move(Piece piece, Position positionToMove, Class<? extends Piece> pieceClass) {
         // when
-        final Piece movedPiece = piece.move(positionToMove);
+        Piece movedPiece = piece.move(positionToMove);
 
         // then
-        assertThat(movedPiece).isInstanceOf(Rook.class);
+        assertThat(piece).isInstanceOf(pieceClass);
         assertThat(movedPiece.isSamePosition(positionToMove)).isTrue();
+    }
+
+    static Stream<Arguments> moveTestCases() {
+        return Stream.of(
+                Arguments.arguments(new Rook(new Position(File.A, Rank.ONE), Side.WHITE), new Position(File.F, Rank.ONE), Rook.class),
+                Arguments.arguments(new Knight(new Position(File.B, Rank.ONE), Side.WHITE), new Position(File.C, Rank.THREE), Knight.class),
+                Arguments.arguments(new Bishop(new Position(File.C, Rank.ONE), Side.WHITE), new Position(File.E, Rank.THREE), Bishop.class),
+                Arguments.arguments(new Queen(new Position(File.D, Rank.ONE), Side.WHITE), new Position(File.H, Rank.FIVE), Queen.class),
+                Arguments.arguments(new King(new Position(File.E, Rank.ONE), Side.WHITE), new Position(File.E, Rank.TWO), King.class),
+                Arguments.arguments(new Pawn(new Position(File.B, Rank.TWO), Side.WHITE), new Position(File.B, Rank.FOUR), Pawn.class)
+        );
     }
 }
