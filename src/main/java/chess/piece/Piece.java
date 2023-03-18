0decbf8d3bb20677c5551acdf20daba3f8aa1f28
Eunkeee <eunkeeeee@gmail.com>
2023-03-19 08:37:56 +0900

    refactor(piece): 공통 메서드 분리

--- src/main/java/chess/piece/Piece.java
+++ src/main/java/chess/piece/Piece.java
@@ -1,7 +1,9 @@
 package chess.piece;
 
+import chess.path.Movement;
 import chess.path.Path;
 import chess.position.Position;
+import java.util.List;
 import java.util.Optional;
 
 public abstract class Piece {
@@ -18,13 +20,19 @@ public abstract class Piece {
 
     public abstract Path searchPathTo(Position from, Position to, Optional<Piece> destination);
 
-    protected void validateSameColor(Piece other) {
+    protected void validateMovement(final Movement movement, List<Movement> availableMovements) {
+        if (!availableMovements.contains(movement)) {
+            throw new IllegalStateException("움직일 수 없는 방향임!");
+        }
+    }
+
+    protected final void validateSameColor(Piece other) {
         if (color.isSameColor(other.color)) {
             throw new IllegalStateException("같은 색 말의 위치로 이동할 수 없습니다.");
         }
     }
 
-    public boolean isSameColor(Color turn) {
+    public final boolean isSameColor(Color turn) {
         return this.color.equals(turn);
     }
 }
