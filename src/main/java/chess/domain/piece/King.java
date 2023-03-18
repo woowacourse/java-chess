7e30071b39585d989556ac0e5da0b6e7e9a1cbd6
zillionme <beatus96@naver.com>
2023-03-18 19:13:59 +0900

    refactor: King 예외 메시지 상수화

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -1,8 +1,8 @@
 package chess.domain.piece;
 
 import chess.domain.Color;
+import chess.domain.Direction;
 import chess.domain.Position;
-import chess.practiceMove.Direction;
 
 import java.util.List;
 
@@ -10,7 +10,9 @@ public class King extends Piece {
 
     private static final String name = "k";
     private static final int MOVABLE_DISTANCE = 1;
-
+    public static final String DIRECTION_ERROR_MESSAGE = "king이 이동할 수 있는 방향이 아닙니다";
+    public static final String DISTANCE_ERROR_MESSAGE = "king이 한 번에 이동할 수 있는 거리가 아닙니다";
+    public static final String MOVE_ERROR_MESSAGE = "목적지에 아군이 있으므로 king은 이동할 수 없습니다.";
     private static final List<Direction> movableDirection = List.of(
             Direction.TOP, Direction.BOTTOM, Direction.LEFT, Direction.RIGHT,
             Direction.TOP_LEFT, Direction.TOP_RIGHT, Direction.BOTTOM_LEFT, Direction.BOTTOM_RIGHT
@@ -32,7 +34,7 @@ public class King extends Piece {
 
     public void checkMovableDirection(Direction direction) {
         if(!movableDirection.contains(direction)){
-            throw new IllegalArgumentException("king이 이동할 수 있는 방향이 아닙니다");
+            throw new IllegalArgumentException(DIRECTION_ERROR_MESSAGE);
         }
     }
 
@@ -41,13 +43,13 @@ public class King extends Piece {
         int absGapOfRank = Math.abs(start.findGapOfRank(end));
 
         if (absGapOfColumn <= MOVABLE_DISTANCE && absGapOfRank <= MOVABLE_DISTANCE) {
-            throw new IllegalArgumentException("king이 한 번에 이동할 수 있는 거리가 아닙니다");
+            throw new IllegalArgumentException(DISTANCE_ERROR_MESSAGE);
         }
     }
 
     private void checkMovableToDestination(Color colorOfDestination) {
         if(this.isSameColor(colorOfDestination)) {
-            throw new IllegalArgumentException("목적지에 아군이 있으므로 king은 이동할 수 없습니다.");
+            throw new IllegalArgumentException(MOVE_ERROR_MESSAGE);
         }
     }
 }
