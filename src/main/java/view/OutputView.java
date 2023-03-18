e75796481fe1e1f251761159d041d61c57c18e1b
junpakPark <junpak.park@gmail.com>
2023-03-18 13:10:24 +0900

    refactor : 좌표 안내 문구 추가 및 개행 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -15,10 +15,21 @@ public class OutputView {
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+        System.out.println("> 좌표 안가");
+        System.out.println("RNBQKBNR  8 (rank 8)\n" +
+                "PPPPPPPP  7\n" +
+                "........  6\n" +
+                "........  5\n" +
+                "........  4\n" +
+                "........  3\n" +
+                "pppppppp  2\n" +
+                "rnbqkbnr  1 (rank 1)\n" +
+                "\n" +
+                "abcdefgh");
     }
 
     public static void printErrorMessage(String message) {
-        System.out.printf("[ERROR] : %s", message);
+        System.out.printf("[ERROR] : %s%n", message);
     }
 
     public static void printChessBoard(ChessBoard chessBoard) {
