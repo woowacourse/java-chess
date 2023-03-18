f5ffd0e5a5806178ff40fd2abcae75df37b6c0fe
Be-student <presentsong@naver.com>
2023-03-18 19:37:44 +0900

    typo: 오타 수정

--- src/main/java/chess/domain/exception/IllegalPieceMoveException.java
+++ src/main/java/chess/domain/exception/IllegalPieceMoveException.java
@@ -3,6 +3,6 @@ package chess.domain.exception;
 public class IllegalPieceMoveException extends RuntimeException {
 
     public IllegalPieceMoveException() {
-        super("잘못 된 기물 움직임 요청입니다.");
+        super("잘못된 기물 움직임 요청입니다.");
     }
 }
