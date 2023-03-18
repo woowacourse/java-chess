6edd1c1ead5dab782dc27d3c76ea54cb951144b2
zillionme <beatus96@naver.com>
2023-03-18 19:05:52 +0900

    refactor: Rook 예외 메시지 상수화

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -1,8 +1,8 @@
 package chess.domain.piece;
 
 import chess.domain.Color;
+import chess.domain.Direction;
 import chess.domain.Position;
-import chess.practiceMove.Direction;
 
 import java.util.List;
 
@@ -10,6 +10,8 @@ import java.util.List;
 public class Rook extends Piece {
 
     private static final String name = "r";
+    public static final String MOVE_ERROR_MESSAGE = "Rook은 도착점에 아군이 있으면 이동할 수 없습니다.";
+    public static final String DIRECTION_ERROR_MESSAGE = "Rook이 이동할 수 있는 방향이 아닙니다";
     private static final List<Direction> movableDirection = List.of(Direction.TOP, Direction.BOTTOM, Direction.LEFT, Direction.RIGHT);
 
     public Rook(Color color) {
@@ -19,6 +21,7 @@ public class Rook extends Piece {
     @Override
     public boolean isMovable(Position start, Position end, Color colorOfDestination) {
         Direction direction = findDirectionToMove(start, end);
+
         checkMovableDirection(direction);
         checkMovableToDestination(colorOfDestination);
         return true;
@@ -26,13 +29,14 @@ public class Rook extends Piece {
 
     public void checkMovableDirection(Direction direction) {
         if(!movableDirection.contains(direction)){
-            throw new IllegalArgumentException("rook이 이동할 수 있는 방향이 아닙니다");
+            throw new IllegalArgumentException(DIRECTION_ERROR_MESSAGE);
         }
     }
 
     private void checkMovableToDestination(Color colorOfDestination) {
         if(this.isSameColor(colorOfDestination)) {
-           throw new IllegalArgumentException("목적지에 아군이 있으므로 rook는 이동할 수 없습니다.");
+           throw new IllegalArgumentException(MOVE_ERROR_MESSAGE);
         }
     }
+
 }
