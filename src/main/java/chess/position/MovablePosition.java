8522e037b53216ba55de14ff7868dde6e14aad22
jjongwa <troas96@naver.com>
2023-03-19 18:44:57 +0900

    refactor: 말의 이동 규칙과 방향을 enum 클래스로 만들어 분리

--- src/main/java/chess/position/MovablePosition.java
+++ src/main/java/chess/position/MovablePosition.java
@@ -1,10 +1,9 @@
 package chess.position;
 
 import chess.ChessBoard;
-import chess.piece.Shape;
 import chess.piece.ChessPiece;
-
 import chess.piece.Pawn;
+import chess.piece.Shape;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +14,6 @@ public class MovablePosition {
     private static final int MIN_CHESS_BOUNDARY = 1;
     private static final int MAX_CHESS_BOUNDARY = 8;
     private static final String BLANK = "blank";
-
-    private static final List<Position> crossVector = List.of(Position.initPosition(1, 0), Position.initPosition(0, 1),
-            Position.initPosition(-1, 0), Position.initPosition(0, -1));
-    private static final List<Position> diagonalVector = List.of(Position.initPosition(1, 1),
-            Position.initPosition(1, -1), Position.initPosition(-1, 1), Position.initPosition(-1, -1));
-    private static final List<Position> knightMoveVector = List.of(Position.initPosition(1, 2),
-            Position.initPosition(-1, 2), Position.initPosition(1, -2), Position.initPosition(-1, -2),
-            Position.initPosition(2, 1), Position.initPosition(-2, 1), Position.initPosition(2, -1),
-            Position.initPosition(-2, -1));
-    private static final List<Position> blackPawnVector = List.of(Position.initPosition(0, -1));
-    private static final List<Position> whitePawnVector = List.of(Position.initPosition(0, 1));
 
     private final List<Position> movablePosition = new ArrayList<>();
 
@@ -41,35 +29,35 @@ public class MovablePosition {
 
     private void makeRoute(ChessBoard chessBoard, Position sourcePosition, String pieceShape) {
         if (pieceShape.equals(Shape.WHITE_PAWN.getName())) {
-            addPawnPosition(chessBoard, sourcePosition, whitePawnVector, 2);
+            addPawnPosition(chessBoard, sourcePosition, Direction.HWITE_PAWN_MOVE_DIRECTION, 2);
         }
         if (pieceShape.equals(Shape.BLACK_PAWN.getName())) {
-            addPawnPosition(chessBoard, sourcePosition, blackPawnVector, 7);
+            addPawnPosition(chessBoard, sourcePosition, Direction.BLACK_PAWN_MOVE_DIRECTION, 7);
         }
         if (pieceShape.equals(Shape.WHITE_ROOK.getName()) || pieceShape.equals(Shape.BLACK_ROOK.getName())) {
-            addCrossOrDiagonalPosition(chessBoard, sourcePosition, diagonalVector, true);
+            addCrossOrDiagonalPosition(chessBoard, sourcePosition, Direction.DIAGONAL_DIRECTION, true);
         }
         if (pieceShape.equals(Shape.WHITE_BISHOP.getName()) || pieceShape.equals(Shape.BLACK_BISHOP.getName())) {
-            addCrossOrDiagonalPosition(chessBoard, sourcePosition, crossVector, true);
+            addCrossOrDiagonalPosition(chessBoard, sourcePosition, Direction.CROSS_DIRECTION, true);
         }
         if (pieceShape.equals(Shape.WHITE_KNIGHT.getName()) || pieceShape.equals(Shape.BLACK_KNIGHT.getName())) {
             addKnightPosition(chessBoard, sourcePosition);
         }
         if (pieceShape.equals(Shape.WHITE_QUEEN.getName()) || pieceShape.equals(Shape.BLACK_QUEEN.getName())) {
-            addRoyalPostion(chessBoard, sourcePosition, true);
+            addRoyalPosition(chessBoard, sourcePosition, true);
         }
         if (pieceShape.equals(Shape.WHITE_KING.getName()) || pieceShape.equals(Shape.BLACK_KING.getName())) {
-            addRoyalPostion(chessBoard, sourcePosition, false);
+            addRoyalPosition(chessBoard, sourcePosition, false);
         }
     }
 
-    private void addRoyalPostion(ChessBoard chessBoard, Position sourcePosition, boolean isInfinite) {
-        addCrossOrDiagonalPosition(chessBoard, sourcePosition, crossVector, false);
-        addCrossOrDiagonalPosition(chessBoard, sourcePosition, diagonalVector, false);
+    private void addRoyalPosition(ChessBoard chessBoard, Position sourcePosition, boolean isInfinite) {
+        addCrossOrDiagonalPosition(chessBoard, sourcePosition, Direction.CROSS_DIRECTION, isInfinite);
+        addCrossOrDiagonalPosition(chessBoard, sourcePosition, Direction.DIAGONAL_DIRECTION, isInfinite);
     }
 
-    public void addCrossOrDiagonalPosition(ChessBoard chessBoard, Position sourcePosition, List<Position> moveVector,
-                                           boolean isInfinite) {
+    public void addCrossOrDiagonalPosition(
+            ChessBoard chessBoard, Position sourcePosition, List<Position> moveVector, boolean isInfinite) {
         int limit = ONE_STEP_MOVEMENT;
         if (isInfinite) {
             limit = UNLIMIT_STEP_MOVEMENT;
@@ -80,7 +68,7 @@ public class MovablePosition {
     }
 
     public void addKnightPosition(ChessBoard chessBoard, Position sourcePosition) {
-        for (Position knightPosition : knightMoveVector) {
+        for (Position knightPosition : Direction.KNIGHT_DIRECTION) {
             findRoute(chessBoard, knightPosition, sourcePosition, ONE_STEP_MOVEMENT);
         }
     }
