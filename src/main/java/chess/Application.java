12e1597504e53fda36bce27cb53d7654dc4efa47
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-19 20:14:23 +0900

    refactor: 불필요한 예외 처리 제거

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -7,10 +7,6 @@ import chess.view.output.OutputView;
 public class Application {
     public static void main(String[] args) {
         ChessController controller = new ChessController(new InputView(), new OutputView());
-        try {
-            controller.run();
-        } catch (Exception exception) {
-            controller.printErrorMessage(exception);
-        }
+        controller.run();
     }
 }
