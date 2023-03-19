d068c8db2c8a09c26f0b6ec8d351243dbbc95318
zillionme <beatus96@naver.com>
2023-03-19 18:14:56 +0900

    refactor: Position 캐싱 기능 추가

--- src/main/java/chess/domain/board/Position.java
+++ src/main/java/chess/domain/board/Position.java
@@ -3,16 +3,42 @@ package chess.domain.board;
 
 import chess.domain.piece.Direction;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Position {
     private final Rank rank;
     private final Column column;
+    private static final List<Position> cache;
 
-    public Position(Column column, Rank rank) {
+    /** todo 4: 질문4
+     * Position에서 사용하는 값은 정해져 있어서 캐싱을 이용했는데요,
+     * stream filter을 이용하면, 객체를 생성할때마다 모든 List<Position> cache를 순회하는데,
+     * 성능적으로 이게 맞는 캐싱인지 의문이 듭니다.
+     *
+     * 이에 대한 리뷰어님의 의견이 궁금합니다!
+     */
+    static {
+        cache = Column.getOrderedColumns().stream()
+                .flatMap(column -> Rank.getOrderedRanks().stream()
+                        .map(rank -> new Position(column,rank)))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private Position(Column column, Rank rank) {
         this.column = column;
         this.rank = rank;
     }
+
+    public static Position of(Column column, Rank rank) {
+        return cache.stream()
+                .filter(position -> position.column == column && position.rank == rank)
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 Position입니다"));
+    }
+
 
     public int findGapOfRank(Position destination) {
         return destination.rank.getIndex() - rank.getIndex();
