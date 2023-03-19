fbd485132c4e5a916727fe95c629617a96a0f20e
SproutMJ <mjk54565@gmail.com>
2023-03-20 01:06:18 +0900

    refactor: ProgressCommand 예외 메시지 수정, 오류 메시지 상수화 해제

--- src/main/java/controller/ProgressCommand.java
+++ src/main/java/controller/ProgressCommand.java
@@ -4,13 +4,11 @@ public enum ProgressCommand {
     END,
     MOVE;
 
-    private static final String INVALID_GAME_OPTION = "move 또는 end만 입력해야 합니다.";
-
     public static ProgressCommand from(String command) {
         try {
             return ProgressCommand.valueOf(command.toUpperCase());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(INVALID_GAME_OPTION);
+            throw new IllegalArgumentException("move source위치 target위치 또는 end만 입력해야 합니다.");
         }
     }
 }
--- src/main/java/controller/StartCommand.java
+++ src/main/java/controller/StartCommand.java
@@ -4,13 +4,11 @@ public enum StartCommand {
     START,
     END;
 
-    private static final String INVALID_GAME_OPTION = "start 또는 end만 입력해야 합니다.";
-
     public static StartCommand from(String command) {
         try {
             return StartCommand.valueOf(command.toUpperCase());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(INVALID_GAME_OPTION);
+            throw new IllegalArgumentException("start 또는 end만 입력해야 합니다.");
         }
     }
 }
