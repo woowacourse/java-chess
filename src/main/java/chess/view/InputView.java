e06befaa1ee72aedaa47a55428541d3817290a7a
zillionme <beatus96@naver.com>
2023-03-19 18:06:37 +0900

    refactor: CommandDto 삭제 및 명령 받는 기능 수

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,16 +1,20 @@
 package chess.view;
 
-import chess.dto.CommandDto;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    public static final String DELIMITER = " ";
 
-    public static CommandDto readStateCommand() {
+    public List<String> readCommand() {
         String input = scanner.nextLine();
-        Command command = Command.findCommandByName(input);
-        return new CommandDto(command, input);
+        return Arrays.stream(input.split(DELIMITER, -1))
+                .map(String::strip)
+                .collect(Collectors.toList());
     }
+
 }
