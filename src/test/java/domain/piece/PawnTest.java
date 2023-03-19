f766c4cb17cf49f03e96e4988310ba36b4973690
splitube <voicesplit@daum.net>
2023-03-20 02:47:16 +0900

    refactor: 테스트 수정

--- src/test/java/domain/piece/PawnTest.java
+++ src/test/java/domain/piece/PawnTest.java
@@ -11,11 +11,26 @@ import org.junit.jupiter.api.TestFactory;
 
 class PawnTest {
 
-    @DisplayName("검은색 폰이 이동 가능한 경로를 반환한다.")
+    @DisplayName("폰이 이동 가능한 경로를 반환한다.")
     @TestFactory
     Stream<DynamicTest> testIsBlackMovable() {
         final Pawn pawn = Pawn.makeBlack();
         return Stream.of(
+            DynamicTest.dynamicTest("위로 1칸 움직일 수 있다.", () -> {
+                final Location start = Location.of(1, 7);
+                final Location end = Location.of(1, 6);
+                assertThat(pawn.searchPath(start, end)).containsExactly(
+                    Location.of(1, 6)
+                );
+            }),
+            DynamicTest.dynamicTest("위로 2칸 움직일 수 있다.", () -> {
+                final Location start = Location.of(1, 7);
+                final Location end = Location.of(1, 5);
+                assertThat(pawn.searchPath(start, end)).containsExactly(
+                    Location.of(1, 6),
+                    Location.of(1, 5)
+                );
+            }),
             DynamicTest.dynamicTest("아래로 1칸 움직일 수 있다.", () -> {
                 final Location start = Location.of(1, 7);
                 final Location end = Location.of(1, 6);
@@ -34,7 +49,7 @@ class PawnTest {
         );
     }
 
-    @DisplayName("검은색 폰이 이동 불가능한 경로일 때 오류를 던진다.")
+    @DisplayName("폰이 이동 불가능한 경로일 때 오류를 던진다.")
     @TestFactory
     Stream<DynamicTest> testIsBlackNotMovable() {
         final Pawn pawn = Pawn.makeBlack();
@@ -45,64 +60,9 @@ class PawnTest {
                 assertThatThrownBy(() -> pawn.searchPath(start, end))
                     .isInstanceOf(IllegalArgumentException.class);
             }),
-            DynamicTest.dynamicTest("위로 1칸 움직이면 오류를 던진다.", () -> {
-                final Location start = Location.of(1, 5);
-                final Location end = Location.of(1, 6);
-                assertThatThrownBy(() -> pawn.searchPath(start, end))
-                    .isInstanceOf(IllegalArgumentException.class);
-            }),
-            DynamicTest.dynamicTest("위로 2칸 움직이면 오류를 던진다.", () -> {
-                final Location start = Location.of(1, 5);
-                final Location end = Location.of(1, 7);
-                assertThatThrownBy(() -> pawn.searchPath(start, end))
-                    .isInstanceOf(IllegalArgumentException.class);
-            })
-        );
-    }
-
-    @DisplayName("하얀색 폰이 이동 가능한 경로를 반환한다.")
-    @TestFactory
-    Stream<DynamicTest> testIsWhiteMovable() {
-        final Pawn pawn = Pawn.makeWhite();
-        return Stream.of(
-            DynamicTest.dynamicTest("위로 1칸 움직일 수 있다.", () -> {
-                final Location start = Location.of(1, 1);
-                final Location end = Location.of(1, 2);
-                assertThat(pawn.searchPath(start, end)).containsExactly(
-                    Location.of(1, 2)
-                );
-            }),
-            DynamicTest.dynamicTest("위로 2칸 움직일 수 있다.", () -> {
-                final Location start = Location.of(1, 1);
-                final Location end = Location.of(1, 3);
-                assertThat(pawn.searchPath(start, end)).containsExactly(
-                    Location.of(1, 2),
-                    Location.of(1, 3)
-                );
-            })
-        );
-    }
-
-    @DisplayName("하얀색 폰이 이동 불가능한 경로일 때 오류를 던진다.")
-    @TestFactory
-    Stream<DynamicTest> testIsWhiteNotMovable() {
-        final Pawn pawn = Pawn.makeWhite();
-        return Stream.of(
             DynamicTest.dynamicTest("위로 3칸 움직이면 오류를 던진다.", () -> {
                 final Location start = Location.of(1, 3);
                 final Location end = Location.of(1, 6);
-                assertThatThrownBy(() -> pawn.searchPath(start, end))
-                    .isInstanceOf(IllegalArgumentException.class);
-            }),
-            DynamicTest.dynamicTest("아래로 1칸 움직이면 오류를 던진다.", () -> {
-                final Location start = Location.of(1, 3);
-                final Location end = Location.of(1, 2);
-                assertThatThrownBy(() -> pawn.searchPath(start, end))
-                    .isInstanceOf(IllegalArgumentException.class);
-            }),
-            DynamicTest.dynamicTest("아래로 2칸 움직이면 오류를 던진다.", () -> {
-                final Location start = Location.of(1, 3);
-                final Location end = Location.of(1, 1);
                 assertThatThrownBy(() -> pawn.searchPath(start, end))
                     .isInstanceOf(IllegalArgumentException.class);
             })
