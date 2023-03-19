9fc069a42dac8c5a8f1f75c2f6f07267f7623a01
zillionme <beatus96@naver.com>
2023-03-19 19:44:10 +0900

    test: 입력한 문자열과 value값이 일치하는 Rank 반환 기능 테스트

--- src/test/java/chess/domain/board/RankTest.java
+++ src/test/java/chess/domain/board/RankTest.java
@@ -1,10 +1,14 @@
 package chess.domain.board;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SuppressWarnings({"NonAsciiCharacters","SpellCheckingInspection"})
 class RankTest {
@@ -14,6 +18,21 @@ class RankTest {
         List<Rank> expected = List.of(Rank.EIGHT, Rank.SEVEN, Rank.SIX, Rank.FIVE, Rank.FOUR, Rank.THREE, Rank.TWO, Rank.ONE);
 
         assertThat(Rank.getOrderedRanks()).isEqualTo(expected);
+    }
+
+    @Test
+    void 입력한_문자열이_Rank의_value로_존재하지_않는_경우_예외() {
+        String notExistValue = "0";
+
+        assertThatThrownBy(() -> Rank.findRankByValue(notExistValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("존재하지 않는 Rank입니다");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1","2","3","4","5","6","7","8"})
+    void 입력한_문자열이_Rank의_value로_존재하면_아무일도_일어나지_않는다(String existValue) {
+        assertDoesNotThrow(() -> Rank.findRankByValue(existValue));
     }
 
 }
