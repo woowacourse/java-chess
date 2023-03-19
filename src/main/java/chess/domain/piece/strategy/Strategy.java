932d774451bee9b6b4a5dd5394e63f4eb3ec5d42
woo-chang <clllickme@naver.com>
2023-03-19 11:51:29 +0900

    refactor: 함수형 인터페이스 어노테이션 사용

--- src/main/java/chess/domain/piece/strategy/Strategy.java
+++ src/main/java/chess/domain/piece/strategy/Strategy.java
@@ -3,6 +3,7 @@ package chess.domain.piece.strategy;
 import chess.domain.board.Square;
 import java.util.List;
 
+@FunctionalInterface
 public interface Strategy {
 
     List<Square> findRoute(Square source, Square destination);
