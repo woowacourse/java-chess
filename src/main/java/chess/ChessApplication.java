3f20fd732217795a0dad0992b34b83e87f113e94
kiarakim <kiarakim413@gmail.com>
2023-03-19 21:42:27 +0900

    refactor: Controller 제거

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -1,17 +1,39 @@
 package chess;
 
-import chess.controller.ChessController;
+import chess.domain.Board;
+import chess.domain.command.Command;
+import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessApplication {
 
-    public static void main(String[] args) {
-        ChessController controller = new ChessController();
+	public static void main(String[] args) {
+		Board board = Board.create();
+		try {
+			start(board);
+			play(board);
+		} catch (IllegalArgumentException e) {
+			OutputView.printErrorMessage(e.getMessage());
+		}
+	}
 
-        try {
-            controller.run();
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-        }
-    }
+	private static void start(final Board board) {
+		String inputCommand = InputView.askStart();
+		Command.ofStart(inputCommand);
+
+		OutputView.printBoard(board);
+	}
+
+	private static void play(final Board board) {
+		String inputCommand = InputView.askNext();
+		Command command = Command.ofMoveOrEnd(inputCommand);
+		if (command.isEnd()) {
+			return;
+		}
+		if (command.isMove()) {
+			board.move(command.getSource(), command.getTarget());
+			OutputView.printBoard(board);
+			play(board);
+		}
+	}
 }
