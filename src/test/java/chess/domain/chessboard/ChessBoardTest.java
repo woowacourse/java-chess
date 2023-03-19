35b958019611d6260aa15396f0892492d35b86b3
songusika <sws981202@gmail.com>
2023-03-19 18:57:39 +0900

    test: 게임을 각 팀이 번갈아 가며 두는 기능 테스트
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- src/test/java/chess/domain/chessboard/ChessBoardTest.java
+++ src/test/java/chess/domain/chessboard/ChessBoardTest.java
@@ -1,6 +1,7 @@
 package chess.domain.chessboard;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import chess.domain.piece.state.Rook;
 import org.assertj.core.api.InstanceOfAssertFactories;
@@ -27,5 +28,36 @@ class ChessBoardTest {
                 .first()
                 .extracting("pieceState")
                 .isInstanceOf(Rook.class);
+    }
+
+    @Test
+    void 체스판은_첫_시작_시_BLACK_이_선으로_두면_예외가_발생한다() {
+        //given
+        final ChessBoard chessBoard = new ChessBoard();
+        final Coordinate BlackPawnFrom = Coordinate.of("a7");
+        final Coordinate BlackPawnTo = Coordinate.of("a6");
+
+        //when & then
+        assertThatThrownBy(()-> chessBoard.move(BlackPawnFrom, BlackPawnTo))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("현재 턴은 WHITE입니다!");
+    }
+
+    @Test
+    void 체스판은_한_팀이_연속으로_두면_예외가_발생한다() {
+        //given
+        final ChessBoard chessBoard = new ChessBoard();
+        final Coordinate WhitePawnFrom = Coordinate.of("a2");
+        final Coordinate WhitePawnTo = Coordinate.of("a3");
+        final Coordinate WhitePawnFrom2 = Coordinate.of("b2");
+        final Coordinate WhitePawnTo2 = Coordinate.of("b3");
+
+        //when
+        chessBoard.move(WhitePawnFrom, WhitePawnTo);
+
+        //when & then
+        assertThatThrownBy(()-> chessBoard.move(WhitePawnFrom2, WhitePawnTo2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("현재 턴은 BLACK입니다!");
     }
 }
