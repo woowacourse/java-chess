2240c86f884254c3e4d555dd6cbff9da3d74ace5
somsom13 <somin1113@naver.com>
2023-03-19 23:01:19 +0900

    fix: controller catch 제거 및 퀸, 킹 위치 수정

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -5,10 +5,6 @@ import chess.controller.ChessController;
 public class ChessApplication {
     public static void main(String[] args) {
         ChessController chessController = new ChessController();
-        try {
-            chessController.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        chessController.run();
     }
 }
