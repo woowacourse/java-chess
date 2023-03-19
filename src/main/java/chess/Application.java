2e2b1ea89cb8076869b1f29a4df41e9f5acfe70f
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-19 20:31:39 +0900

    style: 포맷 수정

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -5,6 +5,7 @@ import chess.view.input.InputView;
 import chess.view.output.OutputView;
 
 public class Application {
+
     public static void main(String[] args) {
         ChessController controller = new ChessController(new InputView(), new OutputView());
         controller.run();
