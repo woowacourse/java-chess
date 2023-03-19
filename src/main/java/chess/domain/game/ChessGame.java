ff2fe2d210c3e7110e8c7dc92ae1e1f9eb7c531a
zillionme <beatus96@naver.com>
2023-03-19 18:04:47 +0900

    feat: chessBoard가 초기화 되었는지에 따라, 게임의 start, move 명령에 제한 걸기 기능 추가

--- src/main/java/chess/domain/game/ChessGame.java
+++ src/main/java/chess/domain/game/ChessGame.java
@@ -1,7 +1,7 @@
 package chess.domain.game;
 
+import chess.boardstrategy.BoardStrategy;
 import chess.domain.board.Position;
-import chess.domain.board.boardstrategy.BoardStrategy;
 import chess.domain.piece.type.Piece;
 
 import java.util.Map;
@@ -9,12 +9,26 @@ import java.util.Map;
 public class ChessGame {
     private final ChessBoard chessBoard;
 
-    public ChessGame(BoardStrategy boardStrategy) {
-        this.chessBoard = new ChessBoard(boardStrategy.generate());
+    public ChessGame() {
+        this.chessBoard = new ChessBoard();
+    }
+
+    public void start(BoardStrategy boardStrategy) {
+        if(isStarted()){
+            throw new IllegalArgumentException("게임이 이미 시작되었습니다");
+        }
+        this.chessBoard.initialize(boardStrategy.generate());
     }
 
     public void move(Position start, Position end) {
+        if(!isStarted()) {
+            throw new IllegalArgumentException("게임이 시작되지 않았습니다");
+        }
         chessBoard.move(start, end);
+    }
+
+    public boolean isStarted() {
+        return chessBoard.isInitialized();
     }
 
     public Map<Position, Piece> getChessBoard() {
