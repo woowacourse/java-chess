48fc3121fec829c31b43b3bc01d363cf8686b5be
zillionme <beatus96@naver.com>
2023-03-20 08:09:46 +0900

    test: 입력된 Position의 Rank와의 차이값 구하기 기능 테스트

--- src/test/java/chess/domain/board/PositionTest.java
+++ src/test/java/chess/domain/board/PositionTest.java
@@ -1,7 +1,10 @@
 package chess.domain.board;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SuppressWarnings({"NonAsciiCharacters","SpellCheckingInspection"})
@@ -15,5 +18,16 @@ class PositionTest {
             }
         }
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"EIGHT,0","SEVEN,-1","SIX,-2","FIVE,-3","FOUR,-4","THREE,-5", "TWO,-6" ,"ONE,-7"})
+    void 입력된_Position의_Rank인덱스값에서_현재_Position의_Rank인덱스값을_뺀_값을_반환한다(Rank otherRank, int expectedGap) {
+        Position otherPosition = Position.of(Column.A, otherRank);
+        Position currentPosition = Position.of(Column.A, Rank.EIGHT);
+
+        assertThat(currentPosition.findGapOfRank(otherPosition))
+                .isEqualTo(expectedGap);
+    }
+
 
 }
