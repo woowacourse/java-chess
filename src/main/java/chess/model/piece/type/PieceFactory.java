ee88ad282c38020a4f75ea810f7eebcf1e8516c9
apptie <appti@naver.com>
2023-03-20 01:19:48 +0900

    feat: Piece의 타입에 따라 구체적인 기물을 생성하는 PieceFactory 구현

---
+++ src/main/java/chess/model/piece/type/PieceFactory.java
@@ -0,0 +1,41 @@
+package chess.model.piece.type;
+
+import chess.model.piece.Camp;
+import chess.model.piece.Piece;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public final class PieceFactory {
+
+    private static final Map<Class<? extends Piece>, Function<Camp, Piece>> factories = new HashMap<>();
+
+    static {
+        factories.put(Bishop.class, Bishop::new);
+        factories.put(Pawn.class, PieceFactory::initialPawn);
+        factories.put(King.class, King::new);
+        factories.put(Knight.class, Knight::new);
+        factories.put(Queen.class, Queen::new);
+        factories.put(Rook.class, Rook::new);
+    }
+
+    private static Piece initialPawn(final Camp camp) {
+        final Piece pawn = new Pawn(camp);
+
+        return new InitialPawn(pawn);
+    }
+
+    public static Piece create(final Class<? extends Piece> type, Camp camp) {
+        if (cannotFoundPiece(type)) {
+            throw new IllegalStateException("존재하지 않는 기물입니다.");
+        }
+        return factories.get(type).apply(camp);
+    }
+
+    private static boolean cannotFoundPiece(final Class<? extends Piece> type) {
+        return !factories.containsKey(type);
+    }
+
+    private PieceFactory() {
+    }
+}
---
+++ src/test/java/chess/model/piece/type/PieceFactoryTest.java
@@ -0,0 +1,45 @@
+package chess.model.piece.type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import chess.model.piece.Camp;
+import chess.model.piece.Piece;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PieceFactoryTest {
+
+    @ParameterizedTest(name = "create()는 {0}을 건네주면 {1}의 타입의 기물을 반환한다")
+    @CsvSource(value = {
+            "chess.model.piece.type.Bishop:chess.model.piece.type.Bishop",
+            "chess.model.piece.type.Pawn:chess.model.piece.type.InitialPawn",
+            "chess.model.piece.type.King:chess.model.piece.type.King",
+            "chess.model.piece.type.Knight:chess.model.piece.type.Knight",
+            "chess.model.piece.type.Queen:chess.model.piece.type.Queen",
+            "chess.model.piece.type.Rook:chess.model.piece.type.Rook",
+    }, delimiter = ':')
+    void create_givenPieceType_thenReturnPieceInstance(
+            final Class<? extends Piece> pieceType,
+            final Class<? extends Piece> expected
+    ) {
+        // when
+        final Piece actual = PieceFactory.create(pieceType, Camp.BLACK);
+
+        // then
+        assertThat(actual)
+                .isInstanceOf(Piece.class)
+                .isExactlyInstanceOf(expected);
+    }
+
+    @Test
+    @DisplayName("create()는 생성할 수 없는 기물 Empty 클래스를 전달하면 예외가 발생한다")
+    void create_givenEmptyClass_thenFail() {
+        // when, then
+        assertThatThrownBy(() -> PieceFactory.create(Empty.class, Camp.BLACK))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("존재하지 않는 기물입니다.");
+    }
+}
