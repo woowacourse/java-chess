4d6aa2c9046f2e76e8558f6fdeccd6b768709d63
JunDonghyuk <ehdgur4814@naver.com>
2023-03-20 00:06:06 +0900

    refactor: 같은 팀일 때 움직임 불가 검증 Piece로 이관

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static chess.domain.MoveStrategy.PAWN_STRAIGHT;
-
 public class Board {
 
     private final Map<Position, Piece> board;
@@ -27,12 +25,7 @@ public class Board {
         validateSource(source);
         validateMovable(source, target);
         Piece sourcePiece = board.get(source);
-        if (sourcePiece.isPawn()) {
-            validatePawnMoving(source, target);
-        }
-        validateSameTeam(source, target);
         validatePathBeforeTarget(sourcePiece.findPath(source, target));
-
         movePiece(source, target);
     }
 
@@ -44,10 +37,22 @@ public class Board {
 
     private void validateMovable(Position source, Position target) {
         Piece sourcePiece = board.get(source);
-
-        if (!sourcePiece.isMovable(source, target)) {
+        Team targetTeam = decideTargetTeam(target);
+        if (!sourcePiece.isMovable(source, target, targetTeam)) {
             throw new IllegalArgumentException("[ERROR] 이동할 수 없는 위치입니다.");
         }
+    }
+
+    private Team decideTargetTeam(Position target) {
+        if (haveNoPieceInPosition(target)) {
+            return Team.EMPTY;
+        }
+
+        Piece targetPiece = board.get(target);
+        if (targetPiece.isSameTeam(Team.WHITE)) {
+            return Team.WHITE;
+        }
+        return Team.BLACK;
     }
 
     private void validatePathBeforeTarget(List<Position> path) {
@@ -59,29 +64,6 @@ public class Board {
     private void validateBlocked(List<Position> path, int index) {
         if (havePieceInPosition(path.get(index))) {
             throw new IllegalArgumentException("[ERROR] 이동 경로에 기물이 있습니다.");
-        }
-    }
-
-    private void validateSameTeam(Position source, Position target) {
-        if (haveNoPieceInPosition(target)) {
-            return;
-        }
-
-        Piece sourcePiece = board.get(source);
-        Piece targetPiece = board.get(target);
-
-        if (targetPiece.isSameTeam(sourcePiece)) {
-            throw new IllegalArgumentException("[ERROR] 타겟 위치에 같은 팀 기물이 있습니다.");
-        }
-    }
-
-    private void validatePawnMoving(Position source, Position target) {
-        if (haveNoPieceInPosition(target) && !PAWN_STRAIGHT.isMovable(source, target)) {
-            throw new IllegalArgumentException("[ERROR] 폰은 상대 기물이 없을 경우, 대각선으로 움직일 수 없습니다.");
-        }
-
-        if (havePieceInPosition(target) && PAWN_STRAIGHT.isMovable(source, target)) {
-            throw new IllegalArgumentException("[ERROR] 폰은 기물이 있는 곳으로 직진할 수 없습니다.");
         }
     }
 
@@ -99,9 +81,9 @@ public class Board {
         return !board.containsKey(position);
     }
 
-    public boolean isTurn(Position source, Team turn) {
+    public boolean isCorrectTeam(Position source, Team team) {
         validateSource(source);
-        return board.get(source).isSameTeam(turn);
+        return board.get(source).isSameTeam(team);
     }
 
     public Map<Position, Piece> getBoard() {
--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -47,46 +47,4 @@ class BoardTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 이동 경로에 기물이 있습니다.");
     }
-
-    @Test
-    @DisplayName("폰이 대각선으로 이동할 경우, target 위치에 상대팀 기물이 없으면 예외가 발생한다.")
-    void validatePawnMoveCrossWhenThereIsNothingInTarget() {
-        Position source = new Position(2, 1);
-        Position target = new Position(3, 2);
-
-        assertThatThrownBy(() -> board.move(source, target))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 폰은 상대 기물이 없을 경우, 대각선으로 움직일 수 없습니다.");
-    }
-
-    @Test
-    @DisplayName("폰이 대각선으로 이동할 경우, target 위치에 상대팀 기물이 없으면 예외가 발생한다.")
-    void validateOurTeamInTarget() {
-        Position pawnSource = new Position(2, 1);
-        Position pawnTarget = new Position(4, 1);
-        Position rookSource = new Position(1, 1);
-        Position rookTarget = new Position(4, 1);
-
-        board.move(pawnSource, pawnTarget);
-        assertThatThrownBy(() -> board.move(rookSource, rookTarget))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 타겟 위치에 같은 팀 기물이 있습니다.");
-    }
-
-    @Test
-    @DisplayName("폰이 일직선으로 이동할 경우, target 위치에 기물이 있으면 예외가 발생한다.")
-    void validatePawnMoveStraightWhenThereIsSomethingInTarget() {
-        Position knightFirstSource = new Position(8, 2);
-        Position knightFirstTarget = new Position(6, 3);
-        Position knightSecondTarget = new Position(4, 4);
-        board.move(knightFirstSource, knightFirstTarget);
-        board.move(knightFirstTarget, knightSecondTarget);
-
-        Position pawnSource = new Position(2, 4);
-        Position pawnTarget = new Position(4, 4);
-
-        assertThatThrownBy(() -> board.move(pawnSource, pawnTarget))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 폰은 기물이 있는 곳으로 직진할 수 없습니다.");
-    }
 }
