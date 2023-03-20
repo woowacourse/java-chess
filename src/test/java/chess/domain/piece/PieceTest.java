07fb320970db323a288954d9330f6e46d755254e
shin-mallang <huipulci1@naver.com>
2023-03-20 22:18:05 +0900

    test: PieceTest 수정

--- src/test/java/chess/domain/piece/PieceTest.java
+++ src/test/java/chess/domain/piece/PieceTest.java
@@ -1,7 +1,6 @@
 package chess.domain.piece;
 
 import chess.domain.piece.position.Path;
-import chess.domain.piece.position.PiecePosition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -16,20 +15,26 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("Piece 은")
 class PieceTest {
 
-    static class MyPiece extends Piece {
-        public MyPiece(final Color color, final PiecePosition piecePosition) {
-            super(color, piecePosition, new AbstractPieceMovement() {
-                @Override
-                protected void validateMoveWithNoAlly(final Path path, final Piece nullableEnemy) throws IllegalArgumentException {
-                }
-            });
+    static class SuccessMovement extends AbstractPieceMovement {
+
+        @Override
+        protected void validateMoveWithNoAlly(final Path path, final Piece nullableEnemy) throws IllegalArgumentException {
+
+        }
+    }
+
+    static class FailMovement extends AbstractPieceMovement {
+
+        @Override
+        protected void validateMoveWithNoAlly(final Path path, final Piece nullableEnemy) throws IllegalArgumentException {
+            throw new IllegalArgumentException();
         }
     }
 
     @Test
     void 경유지탐색_시_같은_위치면_예외() {
         // given
-        Piece myPiece = new MyPiece(Color.BLACK, of(1, 'a'));
+        Piece myPiece = new Piece(Color.BLACK, of(1, 'a'), new SuccessMovement());
         // when & then
         assertThatThrownBy(() -> myPiece.waypoints(of(1, 'a'), null))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -38,13 +43,7 @@ class PieceTest {
     @Test
     void 경유지탐색_시_도달불가능하면_오류() {
         // given
-        Piece myPiece = new Piece(Color.BLACK, of(1, 'a'), new AbstractPieceMovement() {
-
-            @Override
-            protected void validateMoveWithNoAlly(final Path path, final Piece nullableEnemy) throws IllegalArgumentException {
-                throw new IllegalArgumentException();
-            }
-        });
+        Piece myPiece = new Piece(Color.BLACK, of(1, 'a'), new FailMovement());
 
         // when & then
         assertThatThrownBy(() -> myPiece.waypoints(of(1, 'b'), null))
@@ -54,7 +53,7 @@ class PieceTest {
     @Test
     void 단순_이동할_수_있다() {
         // given
-        final Piece pawn = new MyPiece(Color.BLACK, of("b6"));
+        final Piece pawn = new Piece(Color.BLACK, of("b6"), new SuccessMovement());
 
         // when
         final Piece next = pawn.move(of("b5"), null);
@@ -66,13 +65,7 @@ class PieceTest {
     @Test
     void 이동할_수_없는_경로로_이동하면_오류() {
         // given
-        final Piece pawn = new Piece(Color.BLACK, of("b6"), new AbstractPieceMovement() {
-
-            @Override
-            protected void validateMoveWithNoAlly(final Path path, final Piece nullableEnemy) throws IllegalArgumentException {
-                throw new IllegalArgumentException();
-            }
-        });
+        final Piece pawn = new Piece(Color.BLACK, of("b6"), new FailMovement());
         // when & then
         assertThatThrownBy(() -> pawn.move(of("b5"), null))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -81,13 +74,14 @@ class PieceTest {
     @Test
     void 죽이기_위해_이동할_수_있따() {
         // given
-        final Piece pawn = new MyPiece(Color.BLACK, of("b6"));
-        final Piece enemy = new MyPiece(Color.WHITE, of("b7"));
+        final Piece pawn = new Piece(Color.BLACK, of("b6"), new SuccessMovement());
+        final Piece enemy = new Piece(Color.WHITE, of("b7"), new SuccessMovement());
 
         // when
         final Piece next = pawn.move(enemy.piecePosition, enemy);
 
         // then
         assertThat(next.piecePosition()).isEqualTo(of("b7"));
+        assertThat(next.pieceMovement()).isInstanceOf(SuccessMovement.class);
     }
 }
