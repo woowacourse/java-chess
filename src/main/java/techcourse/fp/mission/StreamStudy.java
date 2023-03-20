3bab4c5ee1fb0a111214b61f37351a0029353e99
donghae <donghae1999@gmail.com>
2023-03-20 16:15:48 +0900

    feat: 람다, 스트림 미션 구현

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -5,52 +5,49 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamStudy {
 
     public static long countWords() throws IOException {
         String contents = Files.readString(Paths
-            .get("src/main/resources/fp/war-and-peace.txt"));
+            .get("src/main/resources/techcourse/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter(s-> s.length()>12).count();
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
         List<Integer> result = new ArrayList<>();
-        for (Integer number : numbers) {
-            result.add(2 * number);
-        }
-
+        numbers.stream().map(s->result.add(2*s));
         return result;
     }
 
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for (Integer number : numbers) {
-            result += number;
-        }
-
-        return result;
+        return numbers.stream().reduce(0,(a,b)->a+b);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        return numbers.stream()
+                .filter(s->s>3)
+                .map(s->s*2)
+                .reduce(0,(a,b)->a+b);
     }
 
     public static void printLongestWordTop100() throws IOException {
         String contents = Files.readString(Paths
-            .get("src/main/resources/fp/war-and-peace.txt"));
+            .get("src/main/resources/techcourse/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        System.out.println(words);
         System.out.println(words.size());
         // TODO 이 부분에 구현한다.
+
+        words.stream().filter(s->s.length()>12)
+                .distinct()
+                .sorted(Comparator.comparing(String::length))
+                .limit(100)
+                .map(s->s.toLowerCase())
+                .forEach(s-> System.out.println(s));
+
     }
 }
