ad574c5a253f9e7526d1f3931d89cee08392c085
amaran-th <songsy405@gmail.com>
2023-03-20 14:24:54 +0900

    refactor : start Command 관련 중복 제거

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -4,7 +4,6 @@ import chess.domain.ChessGame;
 import chess.view.InputView;
 import chess.view.OutputView;
 import java.util.List;
-import java.util.Objects;
 
 public class ChessController {
 
@@ -31,8 +30,8 @@ public class ChessController {
 
     private ChessGame createChessGame() {
         try {
-            String command = inputView.readStart();
-            validateStartCommand(command);
+            Command command = extractCommand(inputView.readCommands());
+            valicateStartCommand(command);
             return ChessGame.startNewGame();
         } catch (IllegalArgumentException e) {
             outputView.printError(e.getMessage());
@@ -40,8 +39,20 @@ public class ChessController {
         }
     }
 
-    private static void validateStartCommand(final String command) {
-        if (Objects.isNull(command) || Command.from(command) != Command.START) {
+    private Command extractCommand(List<String> commands) {
+        Command command = Command.from(commands.get(COMMAND_INDEX));
+        validateCommandSize(command, commands.size());
+        return command;
+    }
+
+    private void validateCommandSize(final Command command, final int size) {
+        if (!command.isAppropriateSize(size)) {
+            throw new IllegalArgumentException("입력된 명령어의 파라미터 개수가 올바르지 않습니다.");
+        }
+    }
+
+    private void valicateStartCommand(Command command) {
+        if (command != Command.START) {
             throw new IllegalArgumentException("입력된 명령어가 올바르지 않습니다.");
         }
     }
@@ -49,8 +60,7 @@ public class ChessController {
     private void executeTurn(final ChessGame chessGame) {
         try {
             List<String> commands = inputView.readCommands();
-            Command command = Command.from(commands.get(COMMAND_INDEX));
-            validateCommandSize(command, commands.size());
+            Command command = extractCommand(inputView.readCommands());
             executeCommand(chessGame, commands, command);
             outputView.printChessBoard(new ChessBoardDto(chessGame.getChessBoard()));
         } catch (IllegalArgumentException e) {
@@ -68,12 +78,6 @@ public class ChessController {
             String destination = commands.get(DEST_POSITION_INDEX);
             chessGame.executeMove(source, destination);
             chessGame.checkGameNotFinished();
-        }
-    }
-
-    private void validateCommandSize(final Command command, final int size) {
-        if (!command.isAppropriateSize(size)) {
-            throw new IllegalArgumentException("입력된 명령어의 파라미터 개수가 올바르지 않습니다.");
         }
     }
 }
--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -7,20 +7,16 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
+
     private final Scanner scanner;
 
     public InputView(final Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public String readStart() {
-        return scanner.nextLine()
-                .trim()
-                .toLowerCase();
-    }
 
     public List<String> readCommands() {
         return Arrays.stream(scanner.nextLine().trim().toLowerCase().split(" "))
-                .collect(Collectors.toUnmodifiableList());
+            .collect(Collectors.toUnmodifiableList());
     }
 }
