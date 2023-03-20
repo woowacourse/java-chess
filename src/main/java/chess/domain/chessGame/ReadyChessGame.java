f5e068c25a86a33a5fec9526fb29e3d253e71ec3
hum02 <ehdgma02@gmail.com>
2023-03-20 15:39:51 +0900

    refactor: 시작 전 게임의 getBoard는 빈 Map을 반환하도록 수정, 에외 메시지 상수화

--- src/main/java/chess/domain/chessGame/ReadyChessGame.java
+++ src/main/java/chess/domain/chessGame/ReadyChessGame.java
@@ -3,12 +3,16 @@ package chess.domain.chessGame;
 import chess.domain.Board;
 import chess.domain.position.Position;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class ReadyChessGame implements ChessGame {
+
+    private static final String GAME_NOT_START_ERROR_MESSAGE = "아직 게임을 시작하지 않았습니다.";
+
     @Override
     public Map<Position, String> move(String currentPosition, String nextPosition) {
-        throw new IllegalArgumentException("아직 게임을 시작하지 않았습니다.");
+        throw new IllegalArgumentException(GAME_NOT_START_ERROR_MESSAGE);
     }
 
     @Override
@@ -19,7 +23,7 @@ public class ReadyChessGame implements ChessGame {
 
     @Override
     public void end() {
-        throw new IllegalArgumentException("아직 게임을 시작하지 않았습니다.");
+        throw new IllegalArgumentException(GAME_NOT_START_ERROR_MESSAGE);
     }
 
     @Override
@@ -29,6 +33,6 @@ public class ReadyChessGame implements ChessGame {
 
     @Override
     public Map<Position, String> getPrintingBoard() {
-        return null;
+        return Collections.emptyMap();
     }
 }
