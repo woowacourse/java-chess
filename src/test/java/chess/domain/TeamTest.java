335f800c8f7c8df5fd6b7c3f92d95d2b9a2c19ff
hectick <chyeon1223@cau.ac.kr>
2023-03-20 16:23:15 +0900

    test: 팀 색깔 판단 테스트 작성

---
+++ src/test/java/chess/domain/TeamTest.java
@@ -0,0 +1,23 @@
+package chess.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class TeamTest {
+
+    @Test
+    @DisplayName("흰 팀의 색깔을 올바르게 판단하는 지 확인한다.")
+    void whiteTeamTest() {
+        assertThat(Team.WHITE.isWhite()).isTrue();
+        assertThat(Team.WHITE.isBlack()).isFalse();
+    }
+
+    @Test
+    @DisplayName("검은 팀의 색깔을 올바르게 판단하는 지 확인한다.")
+    void blackTeamTest() {
+        assertThat(Team.BLACK.isWhite()).isFalse();
+        assertThat(Team.BLACK.isBlack()).isTrue();
+    }
+}
