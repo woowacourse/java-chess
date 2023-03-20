b2435d9a8dd9848b8d2efb75f2118d6149464d25
zillionme <beatus96@naver.com>
2023-03-20 12:07:04 +0900

    test: 체스보드 초기화 상태 테스트

--- src/test/java/chess/domain/game/ChessBoardTest.java
+++ src/test/java/chess/domain/game/ChessBoardTest.java
@@ -2,9 +2,21 @@ package chess.domain.game;
 
 import boardStrategy.EmptyBoardStrategy;
 import chess.boardstrategy.InitialBoardStrategy;
+import chess.domain.board.Column;
+import chess.domain.board.Position;
+import chess.domain.board.Rank;
+import chess.domain.piece.Color;
+import chess.domain.piece.PieceType;
+import chess.domain.piece.type.Piece;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+import java.util.Map;
+
+import static chess.domain.piece.PieceType.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings({"NonAsciiCharacters","SpellCheckingInspection"})
@@ -31,4 +43,37 @@ class ChessBoardTest {
         assertThat(chessBoard.isInitialized())
                 .isFalse();
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"EIGHT, BLACK", "ONE, WHITE"})
+    void 초기화된_체스보드의_Rank8과_Rank1에_예상한_기물들이_초기화되어_있다(Rank rank, Color color) {
+        chessBoard.initialize(initialBoardStrategy.generate());
+        Map<Position, Piece> initializedChessBoard = chessBoard.getChessBoard();
+        List<PieceType> firstRowPieces = List.of(ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK);
+
+        for(int i = 0 ; i < 8 ; i++) {
+            Piece piece = initializedChessBoard.get(Position.of(Column.findColumnByIndex(i+1), rank));
+            assertThat(piece.getPieceType())
+                    .isEqualTo(firstRowPieces.get(i));
+            assertThat(piece.getColor())
+                    .isEqualTo(color);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"SEVEN, BLACK", "TWO, WHITE"})
+    void 초기화된_체스보드의_Rank7과_Rank2에_예상한_Pawn이_초기화되어_있다(Rank rank, Color color) {
+        chessBoard.initialize(initialBoardStrategy.generate());
+        Map<Position, Piece> initializedChessBoard = chessBoard.getChessBoard();
+        PieceType secondRowPiece = PAWN;
+
+        for(Column column :Column.getOrderedColumns()) {
+            Piece piece = initializedChessBoard.get(Position.of(column, rank));
+            assertThat(piece.getPieceType())
+                    .isEqualTo(secondRowPiece);
+            assertThat(piece.getColor())
+                    .isEqualTo(color);
+        }
+    }
+
 }
