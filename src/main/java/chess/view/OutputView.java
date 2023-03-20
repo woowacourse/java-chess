1b6daeed49f2fef5cb5ee4757f076b9ba5553bea
hanueleee <ezzeezz@naver.com>
2023-03-20 23:45:55 +0900

    refactor(view): 시작 메시지를 각각 단일 상수화

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -6,14 +6,16 @@ import chess.controller.RankDto;
 public class OutputView {
     
     public static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String GAME_START_MESSAGE = "> 체스 게임을 시작합니다.\n"
-            + "> 게임 시작 : start\n"
-            + "> 게임 종료 : end\n"
-            + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
-    
+    private static final String GAME_HEADER = "> 체스 게임을 시작합니다.";
+    private static final String GAME_START_INFO_MESSAGE = "> 게임 시작 : start";
+    private static final String GAME_END_INFO_MESSAGE = "> 게임 종료 : end";
+    private static final String GAME_MOVE_INFO_MESSAGE = "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
+
     public void printGameStartMessage() {
-        System.out.println(GAME_START_MESSAGE);
-        System.out.println();
+        System.out.println(GAME_HEADER);
+        System.out.println(GAME_START_INFO_MESSAGE);
+        System.out.println(GAME_END_INFO_MESSAGE);
+        System.out.println(GAME_MOVE_INFO_MESSAGE);
     }
     
     public void printBoard(final BoardDto boardDto) {
