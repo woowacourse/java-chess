fefe9bd5e55d802b70e136128c4f8cbd80a83e65
hectick <chyeon1223@cau.ac.kr>
2023-03-20 13:56:39 +0900

    test: 테스트 코드 수정

--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -14,11 +14,12 @@ import chess.domain.Team;
 class QueenTest {
 
 	@ParameterizedTest
-	@DisplayName("퀸은 팀과 관계 없이 모든 방향으로 1칸 이동할 수 있다.")
+	@DisplayName("퀸은 팀과 관계 없이 모든 방향으로 칸 수의 제약없이 이동할 수 있다.")
 	@CsvSource({"0,1","1,1","1,0","1,-1","0,-1","-1,-1","-1,0","-1,1"})
 	void queenCanMoveEveryDirectionTest(int x, int y) {
 		Random random = new Random();
-		int randomMultiplier = random.nextInt() % 9 + 1;
+		int randomMultiplier = random.nextInt(7) + 1;
+		System.out.println(randomMultiplier);
 		Queen whiteQueen = new Queen(Team.WHITE);
 		Queen blackQueen = new Queen(Team.BLACK);
 		System.out.println("x = " + randomMultiplier * x + " y = " + randomMultiplier * y);
@@ -26,5 +27,13 @@ class QueenTest {
 
 		assertTrue(whiteQueen.isMobile(relativePosition));
 		assertTrue(blackQueen.isMobile(relativePosition));
+
+/*		RelativePosition relativePosition = new RelativePosition(x, y);
+		Queen whiteQueen = new Queen(Team.WHITE);
+		Queen blackQueen = new Queen(Team.BLACK);
+
+		assertTrue(whiteQueen.isMobile(relativePosition));
+		assertTrue(blackQueen.isMobile(relativePosition));*/
+
 	}
 }
