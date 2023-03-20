f733ba5d20a459cc1504c4dea78974cb87575353
hong-sile <gurwns9325@naver.com>
2023-03-20 23:33:13 +0900

    refactor: 사용하지 않는 로직 제거

--- src/main/java/chess/domain/piece/Color.java
+++ src/main/java/chess/domain/piece/Color.java
@@ -5,18 +5,8 @@ public enum Color {
     BLACK,
     WHITE;
 
-    public boolean isWhite() {
-        return this == WHITE;
-    }
-
     public boolean isSameColor(Color color) {
         return this == color;
     }
 
-    public Color opposite() {
-        if (this == BLACK) {
-            return WHITE;
-        }
-        return BLACK;
-    }
 }
