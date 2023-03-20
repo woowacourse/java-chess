a4051d4923055dbe52e45c1813fd9f50f579b3e2
java-saeng <gusdn85554@naver.com>
2023-03-20 20:33:31 +0900

    refactor : King, Knight 를 JumperPiece 추상 클래스로 묶기

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -1,52 +1,16 @@
 package chess.domain.piece;
 
 import chess.domain.board.position.Movement;
-import chess.domain.board.position.Path;
 import chess.domain.board.position.Position;
 
-import java.util.List;
-
-import static chess.domain.board.position.Movement.D;
-import static chess.domain.board.position.Movement.DL;
-import static chess.domain.board.position.Movement.DR;
-import static chess.domain.board.position.Movement.L;
-import static chess.domain.board.position.Movement.R;
-import static chess.domain.board.position.Movement.U;
-import static chess.domain.board.position.Movement.UL;
-import static chess.domain.board.position.Movement.UR;
-
-public class King extends Piece {
-
-    private static final List<Movement> CAN_MOVE_DESTINATION = List.of(U, D, R, L, UR, UL, DR, DL);
+public class King extends JumperPiece {
 
     public King(final Color color) {
         super(color);
     }
 
     @Override
-    public Path searchPathTo(final Position from, final Position to, final Piece locatedPiece) {
-        super.validateSameColor(locatedPiece);
-
-        Movement movement = to.convertMovement(from);
-
-        if (!from.moveBy(movement).equals(to)) {
-            throw new IllegalStateException(this.getClass().getSimpleName() + "은 한 칸만 이동할 수 있습니다.");
-        }
-
-        if (!CAN_MOVE_DESTINATION.contains(movement)) {
-            throw new IllegalStateException(this.getClass().getSimpleName() + "이(가) 이동할 수 없는 경로입니다.");
-        }
-
-        return new Path();
-    }
-
-    @Override
-    protected Path moveToLocatedPiece(final Position from, final Position to, final Movement movement) {
-        return null;
-    }
-
-    @Override
-    protected boolean canNotMoveToLocatedPiece(final Movement movement) {
-        return false;
+    protected boolean canNotMovable(final Position from, final Position to, final Movement movement) {
+        return !from.moveBy(movement).equals(to);
     }
 }
--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,59 +1,19 @@
 package chess.domain.piece;
 
 import chess.domain.board.position.Movement;
-import chess.domain.board.position.Path;
 import chess.domain.board.position.Position;
 
-import java.util.List;
-
-import static chess.domain.board.position.Movement.DDL;
-import static chess.domain.board.position.Movement.DDR;
-import static chess.domain.board.position.Movement.LLD;
-import static chess.domain.board.position.Movement.LLU;
-import static chess.domain.board.position.Movement.RRD;
-import static chess.domain.board.position.Movement.RRU;
-import static chess.domain.board.position.Movement.UUL;
-import static chess.domain.board.position.Movement.UUR;
-
-public class Knight extends Piece {
+public class Knight extends JumperPiece {
 
     private static final int MANHATTAN_DISTANCE = 3;
-
-    private static final List<Movement> CAN_MOVE_DESTINATION =
-            List.of(
-                    UUR, UUL, RRU, RRD,
-                    DDR, DDL, LLU, LLD
-            );
 
     public Knight(final Color color) {
         super(color);
     }
 
     @Override
-    public Path searchPathTo(final Position from, final Position to, final Piece locatedPiece) {
-        super.validateSameColor(locatedPiece);
-
-        Movement movement = to.convertMovement(from);
-
-        if (calculateManhattanDistanceBetween(from, to) != MANHATTAN_DISTANCE) {
-            throw new IllegalStateException(this.getClass().getSimpleName() + "은 한 칸 직진, 한 칸 대각선으로만 이동할 수 있습니다.");
-        }
-
-        if (!CAN_MOVE_DESTINATION.contains(movement)) {
-            throw new IllegalStateException(this.getClass().getSimpleName() + "이(가) 이동할 수 없는 경로입니다.");
-        }
-
-        return new Path();
-    }
-
-    @Override
-    protected Path moveToLocatedPiece(final Position from, final Position to, final Movement movement) {
-        return null;
-    }
-
-    @Override
-    protected boolean canNotMoveToLocatedPiece(final Movement movement) {
-        return false;
+    protected boolean canNotMovable(final Position from, final Position to, final Movement movement) {
+        return calculateManhattanDistanceBetween(from, to) != MANHATTAN_DISTANCE;
     }
 
     private int calculateManhattanDistanceBetween(Position from, Position to) {
