27621b367b04d3ca59d03b6e547ffce4eda1c204
zillionme <beatus96@naver.com>
2023-03-20 15:48:00 +0900

    refactor: King 한번에 이동가능한 거리인지 확인 기능 수정

--- src/main/java/chess/domain/piece/type/King.java
+++ src/main/java/chess/domain/piece/type/King.java
@@ -10,9 +10,9 @@ import java.util.List;
 public class King extends Piece {
 
     private static final int MOVABLE_DISTANCE = 1;
-    public static final String DIRECTION_ERROR_MESSAGE = "king이 이동할 수 있는 방향이 아닙니다";
-    public static final String DISTANCE_ERROR_MESSAGE = "king이 한 번에 이동할 수 있는 거리가 아닙니다";
-    public static final String MOVE_ERROR_MESSAGE = "목적지에 아군이 있으므로 king은 이동할 수 없습니다.";
+    public static final String DIRECTION_ERROR_MESSAGE = "King이 이동할 수 있는 방향이 아닙니다";
+    public static final String DISTANCE_ERROR_MESSAGE = "King이 한 번에 이동할 수 있는 거리가 아닙니다";
+    public static final String MOVE_ERROR_MESSAGE = "King은 도착점에 아군이 있으면 이동할 수 없습니다";
     private static final List<Direction> movableDirection = List.of(
             Direction.TOP, Direction.BOTTOM, Direction.LEFT, Direction.RIGHT,
             Direction.TOP_LEFT, Direction.TOP_RIGHT, Direction.BOTTOM_LEFT, Direction.BOTTOM_RIGHT
@@ -26,7 +26,7 @@ public class King extends Piece {
     public boolean isMovable(Position start, Position end, Color colorOfDestination) {
         Direction direction = Direction.findDirectionByGap(start, end);
         checkMovableDirection(direction);
-        checkMovableAtOnce(start, end);
+        checkMovableDistance(start, end);
         checkMovableToDestination(colorOfDestination);
         return true;
     }
@@ -38,11 +38,11 @@ public class King extends Piece {
         }
     }
 
-    public void checkMovableAtOnce(Position start, Position end) {
+    public void checkMovableDistance(Position start, Position end) {
         int absGapOfColumn = Math.abs(start.findGapOfColumn(end));
         int absGapOfRank = Math.abs(start.findGapOfRank(end));
 
-        if (absGapOfColumn <= MOVABLE_DISTANCE && absGapOfRank <= MOVABLE_DISTANCE) {
+        if (absGapOfColumn > MOVABLE_DISTANCE || absGapOfRank > MOVABLE_DISTANCE) {
             throw new IllegalArgumentException(DISTANCE_ERROR_MESSAGE);
         }
     }
