9f5d867668494880c0acf6bcaceedde8ef6b7b19
Youngho <cyh6099@gmail.com>
2023-03-20 18:25:07 +0900

    refactor(GameCommand): 불필요한 메서드 제거

--- src/main/java/view/GameCommand.java
+++ src/main/java/view/GameCommand.java
@@ -15,12 +15,8 @@ public enum GameCommand {
 
     public static GameCommand from(String gameCommand) {
         return Arrays.stream(GameCommand.values())
-                .filter(command -> command.isExistCommand(gameCommand))
+                .filter(command -> command.gameCommand.equals(gameCommand))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게임 커맨드입니다."));
-    }
-
-    public boolean isExistCommand(String command) {
-        return this.gameCommand.equals(command);
     }
 }
