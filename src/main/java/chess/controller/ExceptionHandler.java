d92a20f3f0c57fcdffaf0dfd50f3b9c2b6ff1557
kdkdhoho <hkim4410@naver.com>
2023-03-20 19:57:49 +0900

    feat(ExceptionHandler): 예외가 터진 경우 함수를 재실행하도록 제어해주는 클래스 추가

---
+++ src/main/java/chess/controller/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package chess.controller;
+
+import static chess.view.OutputView.printError;
+
+import java.util.function.Supplier;
+
+public class ExceptionHandler {
+
+    public static <T> T repeatUntilValidInput(final Supplier<T> reader) {
+        try {
+            return reader.get();
+        } catch (IllegalArgumentException e) {
+            printError(e.getMessage());
+            return repeatUntilValidInput(reader);
+        }
+    }
+}
