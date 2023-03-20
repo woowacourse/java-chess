2c963e830ef9ebef56f5c6e671e26d145517724f
thdwoqor <celebarte01@gmail.com>
2023-03-20 22:46:38 +0900

    refactor : 에러 메시지 추가

--- src/main/java/chess/domain/state/End.java
+++ src/main/java/chess/domain/state/End.java
@@ -15,16 +15,16 @@ public class End extends State {
 
     @Override
     public State move(final Position source, final Position target) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("종료 후에 사용할 수 없는 명령어 입니다.");
     }
 
     @Override
     public State start() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("종료 후에 사용할 수 없는 명령어 입니다.");
     }
 
     @Override
     public State end() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("종료 후에 사용할 수 없는 명령어 입니다.");
     }
 }
--- src/main/java/chess/domain/state/Ready.java
+++ src/main/java/chess/domain/state/Ready.java
@@ -3,7 +3,7 @@ package chess.domain.state;
 import chess.domain.Board;
 import chess.domain.Position;
 
-public class Ready extends State{
+public class Ready extends State {
     public Ready(final Board board) {
         super(board);
     }
@@ -15,7 +15,7 @@ public class Ready extends State{
 
     @Override
     public State move(final Position source, final Position target) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("start 명령어를 먼저 입력해 주세요");
     }
 
     @Override
