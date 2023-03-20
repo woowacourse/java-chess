8f50c408d0c620ff65693f7d1209a32107449e66
hectick <chyeon1223@cau.ac.kr>
2023-03-20 13:57:38 +0900

    test: 테스트 코드 수정

--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -1,39 +1,28 @@
 package chess.domain.piece;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Random;
-
+import chess.domain.RelativePosition;
+import chess.domain.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import chess.domain.RelativePosition;
-import chess.domain.Team;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class QueenTest {
 
-	@ParameterizedTest
-	@DisplayName("퀸은 팀과 관계 없이 모든 방향으로 칸 수의 제약없이 이동할 수 있다.")
-	@CsvSource({"0,1","1,1","1,0","1,-1","0,-1","-1,-1","-1,0","-1,1"})
-	void queenCanMoveEveryDirectionTest(int x, int y) {
-		Random random = new Random();
-		int randomMultiplier = random.nextInt(7) + 1;
-		System.out.println(randomMultiplier);
-		Queen whiteQueen = new Queen(Team.WHITE);
-		Queen blackQueen = new Queen(Team.BLACK);
-		System.out.println("x = " + randomMultiplier * x + " y = " + randomMultiplier * y);
-		RelativePosition relativePosition = new RelativePosition(randomMultiplier * x, randomMultiplier * y);
+    @ParameterizedTest
+    @DisplayName("퀸은 팀과 관계 없이 모든 방향으로 칸 수의 제약없이 이동할 수 있다.")
+    @CsvSource({"0,1", "1,1", "1,0", "1,-1", "0,-1", "-1,-1", "-1,0", "-1,1"})
+    void queenCanMoveEveryDirectionTest(int x, int y) {
+        Random random = new Random();
+        int randomMultiplier = random.nextInt(7) + 1;
+        Queen whiteQueen = new Queen(Team.WHITE);
+        Queen blackQueen = new Queen(Team.BLACK);
+        RelativePosition relativePosition = new RelativePosition(randomMultiplier * x, randomMultiplier * y);
 
-		assertTrue(whiteQueen.isMobile(relativePosition));
-		assertTrue(blackQueen.isMobile(relativePosition));
-
-/*		RelativePosition relativePosition = new RelativePosition(x, y);
-		Queen whiteQueen = new Queen(Team.WHITE);
-		Queen blackQueen = new Queen(Team.BLACK);
-
-		assertTrue(whiteQueen.isMobile(relativePosition));
-		assertTrue(blackQueen.isMobile(relativePosition));*/
-
-	}
+        assertTrue(whiteQueen.isMobile(relativePosition));
+        assertTrue(blackQueen.isMobile(relativePosition));
+    }
 }
