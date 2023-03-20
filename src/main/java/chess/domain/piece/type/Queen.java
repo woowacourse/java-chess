2c6eeb272f14835319d75f9420250b43e4562b32
zillionme <beatus96@naver.com>
2023-03-20 15:40:19 +0900

    test: Queen 이동 가능한지 확인 기능 테스트

--- src/main/java/chess/domain/piece/type/Queen.java
+++ src/main/java/chess/domain/piece/type/Queen.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Queen extends Piece {
 
     public static final String DIRECTION_ERROR_MESSAGE = "Queen이 이동할 수 있는 방향이 아닙니다";
-    public static final String MOVE_ERROR_MESSAGE = "목적지에 아군이 있으므로 Queen는 이동할 수 없습니다.";
+    public static final String MOVE_ERROR_MESSAGE = "Queen은 도착점에 아군이 있으면 이동할 수 없습니다";
     private static final List<Direction> movableDirection = List.of(
             Direction.TOP, Direction.BOTTOM, Direction.LEFT, Direction.RIGHT,
             Direction.TOP_LEFT, Direction.TOP_RIGHT, Direction.BOTTOM_LEFT, Direction.BOTTOM_RIGHT);
--- src/test/java/chess/domain/piece/type/QueenTest.java
+++ src/test/java/chess/domain/piece/type/QueenTest.java
@@ -9,45 +9,46 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 @SuppressWarnings({"NonAsciiCharacters","SpellCheckingInspection"})
-class RookTest {
+class QueenTest {
 
-    private final Rook rook = new Rook(Color.BLACK);
+    private final Queen queen = new Queen(Color.BLACK);
 
     @ParameterizedTest
-    @CsvSource(value = {"C, FIVE" , "C, THREE", "E, FIVE","E,THREE"})
-    void Rook이_갈수없는_방향으로_이동하려고_하면_예외(Column endColumn, Rank endRank) {
+    @CsvSource(value = {"C, SIX" , "E, TWO", "B, FIVE","F,THREE"})
+    void Queen이_갈수없는_방향으로_이동하려고_하면_예외(Column endColumn, Rank endRank) {
         Position start = Position.of(Column.D, Rank.FOUR);
         Position end = Position.of(endColumn, endRank);
         Color colorOfDestination = Color.NONE;
 
-        assertThatThrownBy(() -> rook.isMovable(start, end, colorOfDestination))
+        assertThatThrownBy(() -> queen.isMovable(start, end, colorOfDestination))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Rook이 이동할 수 있는 방향이 아닙니다");
+                .hasMessageContaining("Queen이 이동할 수 있는 방향이 아닙니다");
 
     }
 
     @Test
     void 도착점에_기물의_색깔이_아군인_경우_예외() {
         Position start = Position.of(Column.D, Rank.FOUR);
-        Position end = Position.of(Column.D, Rank.FIVE);
+        Position end = Position.of(Column.C, Rank.FIVE);
         Color colorOfDestination = Color.BLACK;
 
-        assertThatThrownBy(() -> rook.isMovable(start, end, colorOfDestination))
+        assertThatThrownBy(() -> queen.isMovable(start, end, colorOfDestination))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Rook은 도착점에 아군이 있으면 이동할 수 없습니다");
+                .hasMessageContaining("Queen은 도착점에 아군이 있으면 이동할 수 없습니다");
 
     }
 
     @Test
     void 모든_이동_조건을_충족하면_true를_반환한다() {
         Position start = Position.of(Column.D, Rank.FOUR);
-        Position end = Position.of(Column.D, Rank.FIVE);
+        Position end = Position.of(Column.C, Rank.FIVE);
         Color colorOfDestination = Color.NONE;
 
-        assertThat(rook.isMovable(start, end, colorOfDestination))
+        assertThat(queen.isMovable(start, end, colorOfDestination))
                 .isTrue();
 
     }
