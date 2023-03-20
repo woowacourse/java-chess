74e7c11864af0e616a89e08f26ba989da91f2202
zillionme <beatus96@naver.com>
2023-03-20 15:32:52 +0900

    test: Rook 이동 가능한지 확인 기능 테스트

--- src/main/java/chess/domain/piece/type/Rook.java
+++ src/main/java/chess/domain/piece/type/Rook.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Rook extends Piece {
 
-    public static final String MOVE_ERROR_MESSAGE = "Rook은 도착점에 아군이 있으면 이동할 수 없습니다.";
+    public static final String MOVE_ERROR_MESSAGE = "Rook은 도착점에 아군이 있으면 이동할 수 없습니다";
     public static final String DIRECTION_ERROR_MESSAGE = "Rook이 이동할 수 있는 방향이 아닙니다";
     private static final List<Direction> movableDirection = List.of(Direction.TOP, Direction.BOTTOM, Direction.LEFT, Direction.RIGHT);
 
---
+++ src/test/java/chess/domain/piece/type/RookTest.java
@@ -0,0 +1,54 @@
+package chess.domain.piece.type;
+
+import chess.domain.board.Column;
+import chess.domain.board.Position;
+import chess.domain.board.Rank;
+import chess.domain.piece.Color;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class RookTest {
+
+    private final Rook rook = new Rook(Color.BLACK);
+
+    @ParameterizedTest
+    @CsvSource(value = {"C, FIVE" , "C, THREE", "E, FIVE","E,THREE"})
+    void Rook이_갈수없는_방향으로_이동하려고_하면_예외(Column endColumn, Rank endRank) {
+        Position start = Position.of(Column.D, Rank.FOUR);
+        Position end = Position.of(endColumn, endRank);
+        Color colorOfDestination = Color.NONE;
+
+        assertThatThrownBy(() -> rook.isMovable(start, end, colorOfDestination))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Rook이 이동할 수 있는 방향이 아닙니다");
+
+    }
+
+    @Test
+    void 도착점에_기물의_색깔이_아군인_경우_예외() {
+        Position start = Position.of(Column.D, Rank.FOUR);
+        Position end = Position.of(Column.D, Rank.FIVE);
+        Color colorOfDestination = Color.BLACK;
+
+        assertThatThrownBy(() -> rook.isMovable(start, end, colorOfDestination))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Rook은 도착점에 아군이 있으면 이동할 수 없습니다");
+
+    }
+
+    @Test
+    void 모든_이동_조건을_충족하면_true를_반환한다() {
+        Position start = Position.of(Column.D, Rank.FOUR);
+        Position end = Position.of(Column.D, Rank.FIVE);
+        Color colorOfDestination = Color.NONE;
+
+        assertThat(rook.isMovable(start, end, colorOfDestination))
+                .isTrue();
+
+    }
+
+}
