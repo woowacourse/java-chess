d7d310e4fbbc1d46914f6a553984fa601629064a
hanueleee <ezzeezz@naver.com>
2023-03-20 14:24:31 +0900

    refactor(PieceMapper): if 대신 Map을 사용하여 분기 줄이기

--- src/main/java/chess/controller/PieceMapper.java
+++ src/main/java/chess/controller/PieceMapper.java
@@ -2,35 +2,23 @@ package chess.controller;
 
 import chess.domain.piece.Piece;
 import chess.domain.piece.PieceType;
+import java.util.Map;
 
 public class PieceMapper {
+    private static final Map<PieceType, String> mapper = Map.of(
+            PieceType.EMPTY, ".",
+            PieceType.PAWN, "p",
+            PieceType.BISHOP, "b",
+            PieceType.KNIGHT, "n",
+            PieceType.ROOK, "r",
+            PieceType.QUEEN, "q",
+            PieceType.KING, "k"
+    );
     
     public static String map(Piece piece) {
-        String defaultPiece = "";
-        if (piece.getType() == PieceType.EMPTY) {
-            return ".";
-        }
-        if (piece.getType() == PieceType.PAWN) {
-            defaultPiece = "p";
-        }
-        if (piece.getType() == PieceType.BISHOP) {
-            defaultPiece = "b";
-        }
-        if (piece.getType() == PieceType.KNIGHT) {
-            defaultPiece = "n";
-        }
-        if (piece.getType() == PieceType.ROOK) {
-            defaultPiece = "r";
-        }
-        if (piece.getType() == PieceType.QUEEN) {
-            defaultPiece = "q";
-        }
-        if (piece.getType() == PieceType.KING) {
-            defaultPiece = "k";
-        }
         if (!piece.isWhite()) {
-            return defaultPiece.toUpperCase();
+            return mapper.get(piece.getType()).toUpperCase();
         }
-        return defaultPiece;
+        return mapper.get(piece.getType());
     }
 }
