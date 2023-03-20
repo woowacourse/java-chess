875f9357206a001c6dbe4b2a4aa3b4d6b90714e2
shin-mallang <huipulci1@naver.com>
2023-03-21 00:44:16 +0900

    test: PieceTest 에서 구현체를 사용하도록 변경

--- src/test/java/chess/domain/piece/PieceTest.java
+++ src/test/java/chess/domain/piece/PieceTest.java
@@ -1,7 +1,6 @@
 package chess.domain.piece;
 
-import chess.domain.piece.position.PiecePosition;
-import chess.domain.piece.strategy.AbstractPieceMovementStrategy;
+import chess.domain.piece.strategy.RookMovementStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -19,34 +18,10 @@ class PieceTest {
     private final Color myColor = Color.WHITE;
     private final Color enemyColor = Color.BLACK;
 
-    static class SuccessMovementStrategy extends AbstractPieceMovementStrategy {
-
-        public SuccessMovementStrategy(final Color color) {
-            super(color);
-        }
-
-        @Override
-        protected void validateMoveWithNoAlly(final PiecePosition source, final PiecePosition destination, final Piece nullableEnemy) throws IllegalArgumentException {
-
-        }
-    }
-
-    static class FailMovementStrategy extends AbstractPieceMovementStrategy {
-
-        public FailMovementStrategy(final Color color) {
-            super(color);
-        }
-
-        @Override
-        protected void validateMoveWithNoAlly(final PiecePosition source, final PiecePosition destination, final Piece nullableEnemy) throws IllegalArgumentException {
-            throw new IllegalArgumentException();
-        }
-    }
-
     @Test
     void 경유지탐색_시_같은_위치면_예외() {
         // given
-        Piece myPiece = new Piece(of(1, 'a'), new SuccessMovementStrategy(myColor));
+        Piece myPiece = new Piece(of(1, 'a'), new RookMovementStrategy(myColor));
         // when & then
         assertThatThrownBy(() -> myPiece.waypoints(of(1, 'a'), null))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -55,17 +30,17 @@ class PieceTest {
     @Test
     void 경유지탐색_시_도달불가능하면_오류() {
         // given
-        Piece myPiece = new Piece(of(1, 'a'), new FailMovementStrategy(myColor));
+        Piece myPiece = new Piece(of(1, 'a'), new RookMovementStrategy(myColor));
 
         // when & then
-        assertThatThrownBy(() -> myPiece.waypoints(of(1, 'b'), null))
+        assertThatThrownBy(() -> myPiece.waypoints(of(2, 'c'), null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 단순_이동할_수_있다() {
         // given
-        final Piece pawn = new Piece(of("b6"), new SuccessMovementStrategy(myColor));
+        final Piece pawn = new Piece(of("b6"), new RookMovementStrategy(myColor));
 
         // when
         final Piece next = pawn.move(of("b5"), null);
@@ -77,23 +52,23 @@ class PieceTest {
     @Test
     void 이동할_수_없는_경로로_이동하면_오류() {
         // given
-        final Piece pawn = new Piece(of("b6"), new FailMovementStrategy(myColor));
+        final Piece pawn = new Piece(of("b6"), new RookMovementStrategy(myColor));
         // when & then
-        assertThatThrownBy(() -> pawn.move(of("b5"), null))
+        assertThatThrownBy(() -> pawn.move(of("c5"), null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 죽이기_위해_이동할_수_있따() {
         // given
-        final Piece pawn = new Piece(of("b6"), new SuccessMovementStrategy(myColor));
-        final Piece enemy = new Piece(of("b7"), new SuccessMovementStrategy(enemyColor));
+        final Piece piece = new Piece(of("b6"), new RookMovementStrategy(myColor));
+        final Piece enemy = new Piece(of("b7"), new RookMovementStrategy(enemyColor));
 
         // when
-        final Piece next = pawn.move(enemy.piecePosition(), enemy);
+        final Piece next = piece.move(enemy.piecePosition(), enemy);
 
         // then
         assertThat(next.piecePosition()).isEqualTo(of("b7"));
-        assertThat(next.pieceMovement()).isInstanceOf(SuccessMovementStrategy.class);
+        assertThat(next.pieceMovement()).isInstanceOf(RookMovementStrategy.class);
     }
 }
