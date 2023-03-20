0a908a5ad84b15fd8e2f492fb7bd5418ee9bd89e
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-03-21 00:00:12 +0900

    refactor: GameStatus에 검증 로직 추가

--- src/main/java/chess/controller/GameStatus.java
+++ src/main/java/chess/controller/GameStatus.java
@@ -13,24 +13,28 @@ public enum GameStatus {
     public static final int SOURCE_INDEX = 1;
     public static final int TARGET_INDEX = 2;
     private static final int DEFAULT_COMMAND_INDEX = 0;
-
-    public static boolean isStart(final String input) {
-        return START.name().equalsIgnoreCase(input);
-    }
-
-    public static boolean isMove(final String input) {
-        return MOVE.name().equalsIgnoreCase(input);
-    }
-
-    public static boolean isEnd(final String input) {
-        return END.name().equalsIgnoreCase(input);
-    }
+    public static final int SOURCE_AND_TARGET_LENGTH = 2;
+    public static final int INPUT_SIZE = 3;
 
     public static GameStatus from(final List<String> input) {
-        return Arrays.stream(values())
+        GameStatus result = Arrays.stream(values())
                 .filter(value -> value.name().equalsIgnoreCase(input.get(DEFAULT_COMMAND_INDEX)))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_COMMAND_MESSAGE));
+        if (result == MOVE) {
+            validateMoveCommand(input);
+        }
+        return result;
+    }
+
+    private static void validateMoveCommand(List<String> input) {
+        if (input.size() != INPUT_SIZE) {
+            throw new IllegalArgumentException(INVALID_COMMAND_MESSAGE);
+        }
+        if (input.get(SOURCE_INDEX).length() != SOURCE_AND_TARGET_LENGTH ||
+                input.get(TARGET_INDEX).length() != SOURCE_AND_TARGET_LENGTH) {
+            throw new IllegalArgumentException(INVALID_COMMAND_MESSAGE);
+        }
     }
 
     public boolean isEnd() {
