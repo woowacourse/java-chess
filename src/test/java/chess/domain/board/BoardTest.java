c6d2f3418e986439af7ad26eb1c604621891a6e7
hong-sile <gurwns9325@naver.com>
2023-03-20 21:58:39 +0900

    Revert "refactor: turn을 board에서 관리하도록 리팩터링"
    This reverts commit 9aab79a5f99ac33eea9f58d8d878b94d3b3a01f7.

--- src/test/java/chess/domain/board/BoardTest.java
+++ src/test/java/chess/domain/board/BoardTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import chess.domain.piece.Color;
 import chess.domain.position.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,7 +22,7 @@ class BoardTest {
     void test_moveEmptyFrom() {
         Position emptyPosition = new Position(4, 4);
 
-        assertThatThrownBy(() -> board.move(emptyPosition, new Position(1, 1)))
+        assertThatThrownBy(() -> board.move(emptyPosition, new Position(1, 1), Color.WHITE))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("출발점에 말이 없습니다.");
     }
@@ -34,7 +35,7 @@ class BoardTest {
         assertTrue(board.getBoard().containsKey(from));
         assertFalse(board.getBoard().containsKey(to));
 
-        board.move(from, to);
+        board.move(from, to, Color.WHITE);
 
         assertFalse(board.getBoard().containsKey(from));
         assertTrue(board.getBoard().containsKey(to));
