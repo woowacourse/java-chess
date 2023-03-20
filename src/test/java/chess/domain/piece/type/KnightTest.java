4e1cb76826ff0bef172457ea3f0e020417217246
zillionme <beatus96@naver.com>
2023-03-20 15:54:42 +0900

    test: Knight 이동 가능한지 확인 기능 테스트

--- src/test/java/chess/domain/piece/type/KnightTest.java
+++ src/test/java/chess/domain/piece/type/KnightTest.java
@@ -12,42 +12,42 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SuppressWarnings({"NonAsciiCharacters","SpellCheckingInspection"})
-class KingTest {
+class KnightTest {
 
-    private final King king = new King(Color.BLACK);
+    private final Knight knight = new Knight(Color.BLACK);
 
     @ParameterizedTest
-    @CsvSource(value = {"C, SIX" , "E, TWO", "B, FIVE","F,THREE"})
-    void King이_갈수없는_방향으로_이동하려고_하면_예외 (Column endColumn, Rank endRank) {
+    @CsvSource(value = {"D, FIVE" , "D, THREE", "C, FOUR","E,FOUR"})
+    void Knight가_갈수없는_방향으로_이동하려고_하면_예외(Column endColumn, Rank endRank) {
         Position start = Position.of(Column.D, Rank.FOUR);
         Position end = Position.of(endColumn, endRank);
         Color colorOfDestination = Color.NONE;
 
-        assertThatThrownBy(() -> king.isMovable(start, end, colorOfDestination))
+        assertThatThrownBy(() -> knight.isMovable(start, end, colorOfDestination))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("King이 이동할 수 있는 방향이 아닙니다");
-    }
+                .hasMessageContaining("Knight가 이동할 수 있는 방향이 아닙니다");
 
+    }
 
     @Test
     void 도착점에_기물의_색깔이_아군인_경우_예외() {
         Position start = Position.of(Column.D, Rank.FOUR);
-        Position end = Position.of(Column.C, Rank.FIVE);
+        Position end = Position.of(Column.C, Rank.SIX);
         Color colorOfDestination = Color.BLACK;
 
-        assertThatThrownBy(() -> king.isMovable(start, end, colorOfDestination))
+        assertThatThrownBy(() -> knight.isMovable(start, end, colorOfDestination))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("King은 도착점에 아군이 있으면 이동할 수 없습니다");
+                .hasMessageContaining("Knight는 도착점에 아군이 있으면 이동할 수 없습니다");
 
     }
 
     @Test
     void 모든_이동_조건을_충족하면_true를_반환한다() {
         Position start = Position.of(Column.D, Rank.FOUR);
-        Position end = Position.of(Column.C, Rank.FIVE);
+        Position end = Position.of(Column.C, Rank.SIX);
         Color colorOfDestination = Color.NONE;
 
-        assertThat(king.isMovable(start, end, colorOfDestination))
+        assertThat(knight.isMovable(start, end, colorOfDestination))
                 .isTrue();
 
     }
