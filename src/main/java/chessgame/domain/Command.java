4ae669892e50572581f7edbed7031cd4ae39a943
donghae <donghae1999@gmail.com>
2023-03-20 16:16:04 +0900

    refactor: 이동 좌표 검증 수정

--- src/main/java/chessgame/domain/Command.java
+++ src/main/java/chessgame/domain/Command.java
@@ -35,12 +35,20 @@ public class Command {
     private static Command validateMove(String command) {
         String[] moveCommand = command.split(" ");
         if (moveCommand.length == 3 && MOVE.equals(moveCommand[0])) {
+            validatePoint(moveCommand[1], moveCommand[2]);
             return new Command(command);
         }
         if (MOVE.equals(moveCommand[0])) {
             throw new IllegalArgumentException("move source target을 정확히 입력해주세요");
         }
         throw new IllegalArgumentException("start, move, end만 입력 가능합니다.");
+    }
+
+    private static void validatePoint(String source, String target) {
+        String pattern = "[a-h]{1}[1-8]{1}";
+        if (!(source.matches(pattern) && target.matches(pattern))) {
+            throw new IllegalArgumentException("이동 좌표는 a-h 1-8 로 입력하여야합니다.");
+        }
     }
 
     public boolean isMove() {
