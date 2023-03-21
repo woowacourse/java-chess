4e499b66a29eb692f82b98a959b3886db51b804b
apptie <appti@naver.com>
2023-03-21 19:20:24 +0900

    refactor: InitialPawn이 가지고 있는 Pawn의 기능을 활용하도록 변경

--- src/main/java/chess/model/piece/type/InitialPawn.java
+++ src/main/java/chess/model/piece/type/InitialPawn.java
@@ -3,17 +3,12 @@ package chess.model.piece.type;
 import static chess.model.piece.Camp.BLACK;
 import static chess.model.piece.Camp.WHITE;
 import static chess.model.piece.Direction.NORTH;
-import static chess.model.piece.Direction.NORTH_EAST;
-import static chess.model.piece.Direction.NORTH_WEST;
 import static chess.model.piece.Direction.SOUTH;
-import static chess.model.piece.Direction.SOUTH_EAST;
-import static chess.model.piece.Direction.SOUTH_WEST;
 
 import chess.model.piece.Camp;
 import chess.model.piece.Direction;
 import chess.model.piece.Piece;
 import chess.model.position.Distance;
-import java.util.List;
 import java.util.Map;
 
 public class InitialPawn extends Piece {
@@ -22,12 +17,7 @@ public class InitialPawn extends Piece {
             WHITE, NORTH,
             BLACK, SOUTH
     );
-    private static final Map<Camp, List<Direction>> attackDirection = Map.of(
-            WHITE, List.of(NORTH_EAST, NORTH_WEST),
-            BLACK, List.of(SOUTH_EAST, SOUTH_WEST)
-    );
-    private static final int MINIMUM_DISTANCE = 1;
-    private static final int MAXIMUM_DISTANCE = 2;
+    private static final int INITIAL_DISTANCE = 2;
 
     private final Piece pawn;
 
@@ -38,62 +28,29 @@ public class InitialPawn extends Piece {
 
     @Override
     public boolean movable(final Distance distance, final Piece target) {
-        if (isNotAvailableStraightMove(distance, target)) {
-            return false;
-        }
-        if (isAttackAble(distance, target)) {
+        if (isEmpty(target) && isAvailableDirection(distance)) {
             return true;
         }
-        return isAvailableDirection(distance);
+        return pawn.movable(distance, target);
     }
 
-    private boolean isNotAvailableStraightMove(final Distance distance, final Piece target) {
-        return target.isNotPassable() && distance.matchByDirection(straightDirectionByCamp());
-    }
-
-    private boolean isAttackAble(final Distance distance, final Piece target) {
-        return target.isNotPassable() && isEnemy(target)
-                && isAttackDirection(distance) && isAttackAbleDistance(distance);
-    }
-
-    private boolean isEnemy(final Piece target) {
-        return !target.isSameTeam(camp());
-    }
-
-    private boolean isAttackDirection(final Distance distance) {
-        final List<Direction> availableDirections = attackDirection.get(camp());
-
-        return availableDirections.stream()
-                .anyMatch(distance::matchByDirection);
-    }
-
-    private boolean isAttackAbleDistance(final Distance distance) {
-        final int rank = Math.abs(distance.rank());
-
-        return rank == MINIMUM_DISTANCE;
+    private boolean isEmpty(final Piece target) {
+        return !target.isNotPassable();
     }
 
     @Override
     protected boolean isAvailableDirection(final Distance distance) {
-        if (isUnStraightDirection(distance)) {
-            return false;
-        }
-        return isAvailableTravelDistance(distance);
+        final Direction availableDirection = moveDirections.get(camp());
+
+        return isAvailableDistance(distance) && distance.matchByDirection(availableDirection);
     }
 
-    private boolean isUnStraightDirection(final Distance distance) {
-        return !distance.matchByDirection(straightDirectionByCamp());
-    }
-
-    private boolean isAvailableTravelDistance(final Distance distance) {
+    private boolean isAvailableDistance(final Distance distance) {
         final int rank = Math.abs(distance.rank());
 
-        return rank >= MINIMUM_DISTANCE && rank <= MAXIMUM_DISTANCE;
+        return rank == Pawn.MOVE_DISTANCE || rank == INITIAL_DISTANCE;
     }
 
-    private Direction straightDirectionByCamp() {
-        return moveDirections.get(camp());
-    }
 
     @Override
     public Piece pick() {
