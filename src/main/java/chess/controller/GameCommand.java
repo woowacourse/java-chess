b9c1fc52a13973d631aa93c536622b5a3ad218a4
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-21 15:32:36 +0900

    refactor: GameCommand 내 사용자 입력 크기와 예외 메세지 필드화

--- src/main/java/chess/controller/GameCommand.java
+++ src/main/java/chess/controller/GameCommand.java
@@ -1,40 +1,33 @@
 package chess.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 public enum GameCommand {
-    MOVE,
-    END;
+    MOVE("move", 3, "move 명령어는 '도착지'와 '출발지'에 대한 정보를 입력해야합니다."),
+    END("end", 1, "end 명령어는 값을 하나만 입력해야합니다.");
+
+    private final String gameCommand;
+    private final int inputSize;
+    private final String errorMessage;
+
+    GameCommand(String gameCommand, int inputSize, String errorMessage) {
+        this.gameCommand = gameCommand;
+        this.inputSize = inputSize;
+        this.errorMessage = errorMessage;
+    }
 
     public static GameCommand of(String input) {
-        if ("move".equalsIgnoreCase(input)) {
-            return MOVE;
-        }
-        if ("end".equalsIgnoreCase(input)) {
-            return END;
-        }
-        throw new IllegalArgumentException("잘못된 명령어를 입력했습니다.");
+        return Arrays.stream(values())
+                .filter(command -> command.gameCommand.equalsIgnoreCase(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 명령어를 입력했습니다."));
     }
 
     public static void validate(List<String> input) {
-        if (GameCommand.of(input.get(0)) == MOVE) {
-            validateMoveCommand(input);
-        }
-        if (GameCommand.of(input.get(0)) == END) {
-            validateEndCommand(input);
+        GameCommand gameCommand = GameCommand.of(input.get(0));
+        if (input.size() != gameCommand.inputSize) {
+            throw new IllegalArgumentException(gameCommand.errorMessage);
         }
     }
-
-    private static void validateMoveCommand(List<String> input) {
-        if (input.size() != 3) {
-            throw new IllegalArgumentException("move 명령어는 '도착지'와 '출발지'에 대한 정보를 입력해야합니다.");
-        }
-    }
-
-    private static void validateEndCommand(List<String> input) {
-        if (input.size() != 1) {
-            throw new IllegalArgumentException("end 명령어는 값을 하나만 입력해야합니다.");
-        }
-    }
-
 }
