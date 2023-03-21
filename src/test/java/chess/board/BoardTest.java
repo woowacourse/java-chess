ad6868abedc3031a74facc7517fc860962556f5c
sh111-coder <ohk9134@naver.com>
2023-03-21 15:44:00 +0900

    test(BoardTest): 폰 이동 테스트 추가

--- src/test/java/chess/board/BoardTest.java
+++ src/test/java/chess/board/BoardTest.java
@@ -22,28 +22,28 @@ class BoardTest {
 
     @Test
     @DisplayName("기물이 해당 위치로 갈 수 없으면 예외를 던진다.")
-    void movePiece_throws() {
+    void checkPieceMoveCondition_throws() {
         // given
         final Position sourcePosition = new Position(File.B, Rank.TWO);
         final Position targetPosition = new Position(File.B, Rank.FIVE);
 
         // when, then
-        Assertions.assertThatThrownBy(() -> board.movePiece(sourcePosition, targetPosition))
+        Assertions.assertThatThrownBy(() -> board.checkPieceMoveCondition(sourcePosition, targetPosition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 해당 기물은 대상 위치로 이동할 수 없습니다.");
     }
 
     @ParameterizedTest
-    @MethodSource("movePiece_pieceExistOnPath_TestCases")
+    @MethodSource("checkPieceMoveCondition_pieceExistOnPath_TestCases")
     @DisplayName("타겟 위치까지의 경로에 말이 존재하는 경우 예외를 던진다.")
-    void movePiece_pieceExistOnPath_throws(Position sourcePosition, Position targetPosition) {
+    void checkPieceMoveCondition_pieceExistOnPath_throws(Position sourcePosition, Position targetPosition) {
         // when, then
-        Assertions.assertThatThrownBy(() -> board.movePiece(sourcePosition, targetPosition))
+        Assertions.assertThatThrownBy(() -> board.checkPieceMoveCondition(sourcePosition, targetPosition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 타겟 위치까지의 경로에 말이 존재합니다.");
     }
 
-    static Stream<Arguments> movePiece_pieceExistOnPath_TestCases() {
+    static Stream<Arguments> checkPieceMoveCondition_pieceExistOnPath_TestCases() {
         return Stream.of(
                 Arguments.arguments(new Position(File.A, Rank.ONE), new Position(File.A, Rank.FOUR)),
                 Arguments.arguments(new Position(File.C, Rank.ONE), new Position(File.E, Rank.THREE)),
@@ -52,20 +52,55 @@ class BoardTest {
     }
 
     @ParameterizedTest
-    @MethodSource("movePiece_sameSidePieceExistOnTarget_TestCases")
+    @MethodSource("checkPieceMoveCondition_sameSidePieceExistOnTarget_TestCases")
     @DisplayName("타겟 위치에 아군 말이 존재하는 경우 예외를 던진다.")
-    void movePiece_sameSidePieceExistOnTarget_throws(Position sourcePosition, Position targetPosition) {
+    void checkPieceMoveCondition_sameSidePieceExistOnTarget_throws(Position sourcePosition, Position targetPosition) {
         // when, then
-        Assertions.assertThatThrownBy(() -> board.movePiece(sourcePosition, targetPosition))
+        Assertions.assertThatThrownBy(() -> board.checkPieceMoveCondition(sourcePosition, targetPosition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 타겟 위치에 아군 말이 존재합니다.");
     }
 
-    static Stream<Arguments> movePiece_sameSidePieceExistOnTarget_TestCases() {
+    static Stream<Arguments> checkPieceMoveCondition_sameSidePieceExistOnTarget_TestCases() {
         return Stream.of(
                 Arguments.arguments(new Position(File.A, Rank.ONE), new Position(File.A, Rank.TWO)),
                 Arguments.arguments(new Position(File.C, Rank.ONE), new Position(File.B, Rank.TWO)),
                 Arguments.arguments(new Position(File.D, Rank.ONE), new Position(File.D, Rank.TWO))
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("movePiece_Pawn1_TestCases")
+    @DisplayName("폰이 대각선에 적팀 기물이 없을 때 대각선으로 이동하면 예외를 던진다.")
+    void movePiece_Pawn_throws1(Position sourcePosition, Position targetPosition) {
+        // when, then
+        Assertions.assertThatThrownBy(() -> board.movePiece(sourcePosition, targetPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] Pawn은 대각선에 적팀 기물이 있을 때만 이동할 수 있습니다.");
+    }
+
+    static Stream<Arguments> movePiece_Pawn1_TestCases() {
+        return Stream.of(
+                Arguments.arguments(new Position(File.B, Rank.TWO), new Position(File.C, Rank.THREE)),
+                Arguments.arguments(new Position(File.B, Rank.TWO), new Position(File.A, Rank.THREE))
+        );
+    }
+
+    @Test
+    @DisplayName("폰이 대각선에 적팀 기물이 없을 때 대각선으로 이동하면 예외를 던진다.")
+    void movePiece_Pawn_throws2() {
+        // given
+        Position sourcePosition = new Position(File.B, Rank.TWO);
+        Position targetPosition = new Position(File.B, Rank.THREE);
+
+        // when
+        board.movePiece(new Position(File.B, Rank.SEVEN), new Position(File.B, Rank.FIVE));
+        board.movePiece(new Position(File.B, Rank.FIVE), new Position(File.B, Rank.FOUR));
+        board.movePiece(new Position(File.B, Rank.FOUR), new Position(File.B, Rank.THREE));
+
+        // then
+        Assertions.assertThatThrownBy(() -> board.movePiece(sourcePosition, targetPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] Pawn은 직선 상의 도착 위치 기물이 적군이어도 이동할 수 없습니다.");
     }
 }
