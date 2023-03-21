e324f6c6f09d9508fd0c2f0e5ed409bf9310c2e8
이건회 <rg970604@naver.com>
2023-03-21 16:27:18 +0900

    refactor : 상수 표현 변경

--- src/main/java/chess/board/InitialSymbols.java
+++ src/main/java/chess/board/InitialSymbols.java
@@ -3,12 +3,12 @@ package chess.board;
 import java.util.List;
 
 public enum InitialSymbols {
-    EIGHT_AND_ONE(List.of('r', 'n', 'b', 'q', 'k', 'b', 'n', 'r')),
-    SEVEN_AND_TWO(List.of('p', 'p', 'p', 'p', 'p', 'p', 'p', 'p')),
-    SIX_TO_THREE(List.of('e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'));
+    NOBLE_LINE(List.of('r', 'n', 'b', 'q', 'k', 'b', 'n', 'r')),
+    PAWN_LINE(List.of('p', 'p', 'p', 'p', 'p', 'p', 'p', 'p')),
+    EMPTY_LINE(List.of('e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'));
     
-    private static final int FIRST_INITIAL_ROW_OF_PIECES_EXCLUDING_PAWN = 1;
-    private static final int SECOND_INITIAL_ROW_OF_PIECES_EXCLUDING_PAWN = 8;
+    private static final int FIRST_INITIAL_ROW_OF_NOBLE = 1;
+    private static final int SECOND_INITIAL_ROW_OF_NOBLE = 8;
     private static final int FIRST_INITIAL_ROW_OF_PAWN = 2;
     private static final int SECOND_INITIAL_ROW_OF_PAWN = 7;
     private final List<Character> symbols;
@@ -19,14 +19,14 @@ public enum InitialSymbols {
     
     public static InitialSymbols from(int rowNum) {
         if (isOneOrEightRow(rowNum)) {
-            return EIGHT_AND_ONE;
+            return NOBLE_LINE;
         }
     
         if (isTwoOrSevenRow(rowNum)) {
-            return SEVEN_AND_TWO;
+            return PAWN_LINE;
         }
         
-        return SIX_TO_THREE;
+        return EMPTY_LINE;
     }
     
     private static boolean isTwoOrSevenRow(int rowNum) {
@@ -34,7 +34,7 @@ public enum InitialSymbols {
     }
     
     private static boolean isOneOrEightRow(int rowNum) {
-        return rowNum == FIRST_INITIAL_ROW_OF_PIECES_EXCLUDING_PAWN || rowNum == SECOND_INITIAL_ROW_OF_PIECES_EXCLUDING_PAWN;
+        return rowNum == FIRST_INITIAL_ROW_OF_NOBLE || rowNum == SECOND_INITIAL_ROW_OF_NOBLE;
     }
     
     public Character findSymbolByColumnIndex(int columnIndex) {
