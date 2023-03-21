cb287bc156a54a7f35f0e81ba131c25395a4622a
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-21 18:04:58 +0900

    test : Mock Position 추가

---
+++ src/test/java/chess/mock/MockPosition.java
@@ -0,0 +1,44 @@
+package chess.mock;
+
+import chess.domain.Position;
+
+import static chess.domain.Column.*;
+import static chess.domain.Row.*;
+
+public class MockPosition {
+    private MockPosition() {
+    }
+
+    public static final Position POSITION_0_0 = Position.of(ROW_0, COLUMN_0);
+    public static final Position POSITION_0_1 = Position.of(ROW_0, COLUMN_1);
+    public static final Position POSITION_0_2 = Position.of(ROW_0, COLUMN_2);
+    public static final Position POSITION_0_5 = Position.of(ROW_0, COLUMN_5);
+    public static final Position POSITION_0_7 = Position.of(ROW_0, COLUMN_7);
+    public static final Position POSITION_1_0 = Position.of(ROW_1, COLUMN_0);
+    public static final Position POSITION_1_1 = Position.of(ROW_1, COLUMN_1);
+    public static final Position POSITION_1_2 = Position.of(ROW_1, COLUMN_2);
+    public static final Position POSITION_1_3 = Position.of(ROW_1, COLUMN_3);
+    public static final Position POSITION_1_4 = Position.of(ROW_1, COLUMN_4);
+    public static final Position POSITION_1_5 = Position.of(ROW_1, COLUMN_5);
+    public static final Position POSITION_1_6 = Position.of(ROW_1, COLUMN_6);
+    public static final Position POSITION_2_0 = Position.of(ROW_2, COLUMN_0);
+    public static final Position POSITION_2_1 = Position.of(ROW_2, COLUMN_1);
+    public static final Position POSITION_2_2 = Position.of(ROW_2, COLUMN_2);
+    public static final Position POSITION_2_3 = Position.of(ROW_2, COLUMN_3);
+    public static final Position POSITION_2_4 = Position.of(ROW_2, COLUMN_4);
+    public static final Position POSITION_2_5 = Position.of(ROW_2, COLUMN_5);
+    public static final Position POSITION_3_1 = Position.of(ROW_3, COLUMN_1);
+    public static final Position POSITION_3_2 = Position.of(ROW_3, COLUMN_2);
+    public static final Position POSITION_3_3 = Position.of(ROW_3, COLUMN_3);
+    public static final Position POSITION_3_4 = Position.of(ROW_3, COLUMN_4);
+    public static final Position POSITION_4_1 = Position.of(ROW_4, COLUMN_1);
+    public static final Position POSITION_4_2 = Position.of(ROW_4, COLUMN_2);
+    public static final Position POSITION_4_3 = Position.of(ROW_4, COLUMN_3);
+    public static final Position POSITION_4_4 = Position.of(ROW_4, COLUMN_4);
+    public static final Position POSITION_4_5 = Position.of(ROW_4, COLUMN_5);
+    public static final Position POSITION_4_6 = Position.of(ROW_4, COLUMN_6);
+    public static final Position POSITION_5_2 = Position.of(ROW_5, COLUMN_2);
+    public static final Position POSITION_5_4 = Position.of(ROW_5, COLUMN_4);
+    public static final Position POSITION_7_0 = Position.of(ROW_7, COLUMN_0);
+    public static final Position POSITION_7_7 = Position.of(ROW_7, COLUMN_7);
+}
