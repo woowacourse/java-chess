c76b346994f8630bd9962f09c5ecff8cdb7a7029
JunDonghyuk <ehdgur4814@naver.com>
2023-03-21 10:45:47 +0900

    test: 체스게임 테스트 변경

--- src/test/java/chess/domain/game/ChessGameTest.java
+++ src/test/java/chess/domain/game/ChessGameTest.java
@@ -3,6 +3,8 @@ package chess.domain.game;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static chess.view.PositionConverter.convertToSourcePosition;
+import static chess.view.PositionConverter.convertToTargetPosition;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ChessGameTest {
@@ -13,7 +15,7 @@ class ChessGameTest {
         String command = "c7c5";
         Board board = new Board();
         ChessGame chessGame = new ChessGame(board);
-        assertThatThrownBy(() -> chessGame.progress(command))
+        assertThatThrownBy(() -> chessGame.progress(convertToSourcePosition(command), convertToTargetPosition(command)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 지금은 WHITE차례입니다.");
     }
