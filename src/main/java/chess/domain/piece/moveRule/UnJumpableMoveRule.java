557242eba1b7c8d9bc905a8d75b09c1971521033
hgo641 <hgo641@gmail.com>
2023-03-21 09:09:50 +0900

    refactor: 람다식을 메소드로 분리

--- src/main/java/chess/domain/piece/moveRule/UnJumpableMoveRule.java
+++ src/main/java/chess/domain/piece/moveRule/UnJumpableMoveRule.java
@@ -7,8 +7,12 @@ import java.util.Map;
 
 public abstract class UnJumpableMoveRule implements MoveRule {
     protected void validateRoute(Map<Position, Piece> board, List<Position> route) {
-        if (route.stream().anyMatch((position) -> board.containsKey(position))) {
+        if (isExistAnotherPiece(board, route)) {
             throw new IllegalArgumentException("경로상에 다른 기물이 있어 움직일 수 없습니다.");
         }
+    }
+
+    private boolean isExistAnotherPiece(Map<Position, Piece> board, List<Position> route){
+        return route.stream().anyMatch(position -> board.containsKey(position));
     }
 }
