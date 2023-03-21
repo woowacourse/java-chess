0ada5a77e4894c2ed12eef1ffa981245713cd6c4
Combi153 <jcm04@naver.com>
2023-03-21 14:25:05 +0900

    refactor: File 클래스에서 index 필드 삭제 및 ordinal() 사용

--- src/main/java/chess/domain/File.java
+++ src/main/java/chess/domain/File.java
@@ -1,37 +1,29 @@
 package chess.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public enum File {
-    A("a", 0),
-    B("b", 1),
-    C("c", 2),
-    D("d", 3),
-    E("e", 4),
-    F("f", 5),
-    G("g", 6),
-    H("h", 7);
+    A("a"),
+    B("b"),
+    C("c"),
+    D("d"),
+    E("e"),
+    F("f"),
+    G("g"),
+    H("h");
 
-    private static final Map<Integer, File> FILE_BY_INDEX = new HashMap<>();
     private static final Map<String, File> FILE_BY_VALUE = new HashMap<>();
 
     static {
         for (File file : values()) {
-            FILE_BY_INDEX.put(file.index, file);
             FILE_BY_VALUE.put(file.value, file);
         }
     }
 
     private final String value;
-    private final int index;
 
-    File(final String value, final int index) {
+    File(final String value) {
         this.value = value;
-        this.index = index;
     }
 
     public static File findByValue(final String value) {
@@ -39,12 +31,12 @@ public enum File {
     }
 
     public int calculateDistance(final File otherFile) {
-        return Math.abs(this.index - otherFile.index);
+        return Math.abs(this.ordinal() - otherFile.ordinal());
     }
 
     public List<File> getFilesTo(final File otherFile) {
         final List<File> ascendingFiles = generateAscendingFiles(otherFile);
-        if (this.index < otherFile.index) {
+        if (this.ordinal() < otherFile.ordinal()) {
             return ascendingFiles;
         }
         return reverse(ascendingFiles);
@@ -56,17 +48,17 @@ public enum File {
     }
 
     private List<File> generateAscendingFiles(final File otherFile) {
-        final int maxIndex = Math.max(this.index, otherFile.index);
-        final int minIndex = Math.min(this.index, otherFile.index);
+        final int maxOrder = Math.max(this.ordinal(), otherFile.ordinal());
+        final int minOrder = Math.min(this.ordinal(), otherFile.ordinal());
 
         final List<File> passingFiles = new ArrayList<>();
-        for (int index = minIndex + 1; index < maxIndex; index++) {
-            passingFiles.add(findByIndex(index));
+        for (int order = minOrder + 1; order < maxOrder; order++) {
+            passingFiles.add(findByOrdinal(order));
         }
         return passingFiles;
     }
 
-    private File findByIndex(final int index) {
-        return FILE_BY_INDEX.get(index);
+    private File findByOrdinal(final int order) {
+        return values()[order];
     }
 }
