777e73d3d9be62601779f22a4f7d7b53ff5dbae8
echo <echo.backend@gmail.com>
2023-03-21 17:50:21 +0900

    fix(BoardMapper): 거꾸로 표시되는 문제 해결

--- src/main/java/chess/controller/BoardMapper.java
+++ src/main/java/chess/controller/BoardMapper.java
@@ -3,12 +3,14 @@ package chess.controller;
 import chess.domain.Board;
 import chess.domain.position.Rank;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class BoardMapper {
     
     public static String map(Board board) {
         return Arrays.stream(Rank.values())
+                .sorted(Comparator.reverseOrder())
                 .map(rank -> RankMapper.map(board, rank))
                 .collect(Collectors.joining(System.lineSeparator()));
     }
