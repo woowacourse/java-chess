b9ae87dc55481b19d0f73701f477e66a8119ad33
BGuga <tmdwns9913@gmail.com>
2023-03-21 14:37:22 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/chess/domain/Color.java
+++ src/main/java/chess/domain/Color.java
@@ -1,17 +1,7 @@
 package chess.domain;
 
 public enum Color {
-    BLACK(-1),
-    WHITE(1),
-    NONE(0);
-
-    private final int direction;
-
-    Color(int direction) {
-        this.direction = direction;
-    }
-
-    public int colorDirection(int amount) {
-        return amount * direction;
-    }
+    BLACK,
+    WHITE,
+    NONE
 }
