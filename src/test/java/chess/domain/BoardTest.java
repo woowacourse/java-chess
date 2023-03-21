cfd6a6324febb747b5708a11c20e3c773d59fced
70825 <hello70825@gmail.com>
2023-03-21 10:56:29 +0900

    fix: 테스트가 정상적으로 작동하도록 수정

--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -5,6 +5,10 @@ import chess.domain.piece.Knight;
 import chess.domain.position.File;
 import chess.domain.position.Position;
 import chess.domain.position.Rank;
+import chess.domain.team.Team;
+import chess.domain.team.player.BlackPieceGenerator;
+import chess.domain.team.player.Player;
+import chess.domain.team.player.WhitePieceGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,10 +36,11 @@ class BoardTest {
     @Test
     @DisplayName("출발지와 도착지가 같으면 예외가 발생한다")
     void throwExcpetionWhenSamePosition() {
+        final Player player = Player.of(Team.WHITE, new WhitePieceGenerator());
         final Position source = Position.of(File.A, Rank.THREE);
         final Position target = Position.of(File.A, Rank.THREE);
 
-        assertThatThrownBy(() -> board.move(source, target))
+        assertThatThrownBy(() -> board.move(source, target, player))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("출발지와 도착지는 같을 수 없습니다");
     }
@@ -43,10 +48,11 @@ class BoardTest {
     @Test
     @DisplayName("출발점에 체스말이 존재하지 않으면 예외가 발생한다")
     void throwExceptionWhenSourceNotEmpty() {
+        final Player player = Player.of(Team.WHITE, new WhitePieceGenerator());
         final Position source = Position.of(File.B, Rank.THREE);
         final Position target = Position.of(File.B, Rank.FOUR);
 
-        assertThatThrownBy(() -> board.move(source, target))
+        assertThatThrownBy(() -> board.move(source, target, player))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("출발점에 체스말이 존재하지 않습니다");
     }
@@ -54,10 +60,11 @@ class BoardTest {
     @Test
     @DisplayName("체스말이 같은 팀을 공격할 경우 예외가 발생한다")
     void throwExceptionWhenAttackSameTeam() {
+        final Player player = Player.of(Team.WHITE, new WhitePieceGenerator());
         final Position source = Position.of(File.A, Rank.ONE);
         final Position target = Position.of(File.A, Rank.TWO);
 
-        assertThatThrownBy(() -> board.move(source, target))
+        assertThatThrownBy(() -> board.move(source, target, player))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("같은 팀은 공격할 수 없습니다");
     }
@@ -65,10 +72,11 @@ class BoardTest {
     @Test
     @DisplayName("체스말이 이동할 수 있는 방향이 아니면 예외가 발생한다")
     void throwExceptionWhenNotMovable() {
+        final Player player = Player.of(Team.WHITE, new WhitePieceGenerator());
         final Position source = Position.of(File.B, Rank.TWO);
         final Position target = Position.of(File.C, Rank.THREE);
 
-        assertThatThrownBy(() -> board.move(source, target))
+        assertThatThrownBy(() -> board.move(source, target, player))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("체스말이 이동할 수 없는 위치입니다.");
     }
@@ -76,10 +84,11 @@ class BoardTest {
     @Test
     @DisplayName("이동 경로에 체스말이 존재하면 예외가 발생한다")
     void pieceExistInPath() {
+        final Player player = Player.of(Team.WHITE, new WhitePieceGenerator());
         final Position source = Position.of(File.A, Rank.ONE);
         final Position target = Position.of(File.A, Rank.THREE);
 
-        assertThatThrownBy(() -> board.move(source, target))
+        assertThatThrownBy(() -> board.move(source, target, player))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이동할 경로에 체스말이 존재합니다.");
     }
@@ -87,12 +96,13 @@ class BoardTest {
     @Test
     @DisplayName("한 칸만 움직일 수 있는 체스말이 여러 칸을 움직이려고 할 경우, 예외가 발생한다")
     void throwExceptionWhenNotMovableByCount() {
-        board.move(Position.of(File.E, Rank.TWO), Position.of(File.E, Rank.FOUR));
+        final Player player = Player.of(Team.WHITE, new WhitePieceGenerator());
+        board.move(Position.of(File.E, Rank.TWO), Position.of(File.E, Rank.FOUR), player);
 
         final Position source = Position.of(File.E, Rank.ONE);
         final Position target = Position.of(File.E, Rank.THREE);
 
-        assertThatThrownBy(() -> board.move(source, target))
+        assertThatThrownBy(() -> board.move(source, target, player))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("한 칸만 움직일 수 있는 체스말입니다.");
     }
@@ -100,10 +110,11 @@ class BoardTest {
     @Test
     @DisplayName("체스말이 성공적으로 이동하는지 확인하는 테스트")
     void movePiece() {
+        final Player player = Player.of(Team.WHITE, new WhitePieceGenerator());
         final Position source = Position.of(File.B, Rank.ONE);
         final Position target = Position.of(File.C, Rank.THREE);
 
-        board.move(source, target);
+        board.move(source, target, player);
 
         assertThat(board.getBoard().get(source).getClass()).isEqualTo(Empty.class);
         assertThat(board.getBoard().get(target).getClass()).isEqualTo(Knight.class);
