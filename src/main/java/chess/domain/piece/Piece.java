fa066f118bc42b3a2c9ed0ace7679e9576462d5b
odo27 <odo27@naver.com>
2023-03-21 23:43:41 +0900

    refactor: 메서드 개선

--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -11,28 +11,28 @@ abstract public class Piece {
         this.team = team;
     }
 
-    public boolean isEnemy(final Piece piece) {
-        return piece.team != team;
-    }
+    public abstract Direction findDirection(final Square current, final Square destination);
 
     public boolean isAlly(final Team team) {
         return this.team == team;
+    }
+
+    public boolean isEnemy(final Piece piece) {
+        return !isAlly(piece.team);
     }
 
     public boolean isPawn() {
         return this instanceof Pawn;
     }
 
-    public abstract Direction findDirection(final Square current, final Square destination);
+    public Team getColor() {
+        return team;
+    }
 
     @Override
     public String toString() {
         return "Piece{" +
                 "color=" + team +
                 '}';
-    }
-
-    public Team getColor() {
-        return team;
     }
 }
