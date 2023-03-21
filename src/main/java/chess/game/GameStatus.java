94c718826393f5d8c065437025a65e043c0b1580
sh111-coder <ohk9134@naver.com>
2023-03-21 23:33:21 +0900

    feat(GameStatus): 커맨드를 받아서 게임 상태 반환하는 기능 테스트

--- src/main/java/chess/game/GameStatus.java
+++ src/main/java/chess/game/GameStatus.java
@@ -1,7 +1,22 @@
 package chess.game;
 
+import java.util.Arrays;
+
 public enum GameStatus {
-    INIT,
-    START,
-    END
+    START("start"),
+    MOVE("move"),
+    END("end");
+
+    private final String command;
+
+    GameStatus(final String command) {
+        this.command = command;
+    }
+
+    public static GameStatus from(String statusCommand) {
+        return Arrays.stream(values())
+                .filter(value -> value.command.equals(statusCommand))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 올바른 커맨드를 입력해주세요."));
+    }
 }
