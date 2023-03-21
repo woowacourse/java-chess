9881d9512f9bb9fc3a749fddc0b32b77966095e7
aiaiaiai1 <cvc100326@naver.com>
2023-03-21 15:59:54 +0900

    refactor(Command): Command 추상클래스를 Enum타입으로 변경

--- src/main/java/view/Command.java
+++ src/main/java/view/Command.java
@@ -1,4 +1,30 @@
 package view;
 
-public interface Command {
+import java.util.Arrays;
+import java.util.List;
+
+public enum Command {
+    INIT,
+    START,
+    MOVE,
+    END;
+
+    public static Command from(List<String> input) {
+        Command command = Arrays.stream(values())
+                .filter(s -> input.get(0).equalsIgnoreCase(s.name()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("start, end, move만 입력가능합니다."));
+
+        if (command == MOVE) {
+            validateMoveCommands(input);
+        }
+
+        return command;
+    }
+
+    private static void validateMoveCommands(List<String> input) {
+        if (input.size() != 3) {
+            throw new IllegalArgumentException("source와 target이 필요합니다.");
+        }
+    }
 }
