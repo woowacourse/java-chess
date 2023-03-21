6b4d64e14764a62f798abf321390b2c9d8ba1d9b
Combi153 <jcm04@naver.com>
2023-03-21 14:43:21 +0900

    test: 반대 색인지 확인하는 테스트 코드 추가

--- src/test/java/chess/domain/ColorTest.java
+++ src/test/java/chess/domain/ColorTest.java
@@ -1,5 +1,19 @@
 package chess.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ColorTest {
 
+    @ParameterizedTest
+    @CsvSource({"BLACK, WHITE", "WHITE, BLACK", "BLANK, BLANK"})
+    @DisplayName("반대 색 혹은 빈 색을 반환한다")
+    void getOppositeColor_test(final Color color, final Color expectedColor) {
+        final Color actualColor = color.getOppositeColor();
+
+        assertThat(actualColor).isEqualTo(expectedColor);
+    }
 }
