3a2c8aab6694782340df89fc69d817026b0d65bd
Cyma-s <ssol0319@naver.com>
2023-03-22 23:51:59 +0900

    test(Side): 상대 팀을 가져오는 기능 테스트 추가

--- src/test/java/chess/domain/square/SideTest.java
+++ src/test/java/chess/domain/square/SideTest.java
@@ -18,4 +18,16 @@ public class SideTest {
         // expected
         assertThat(side).isNotNull();
     }
+
+    @Test
+    @DisplayName("현재 진영의 반대 진영을 찾는다.")
+    void findOpponent() {
+        // given
+        Side whiteSide = Side.from(Color.WHITE);
+        Side blackSide = Side.from(Color.BLACK);
+
+        // expected
+        assertThat(whiteSide.findOpponent()).isEqualTo(Side.from(Color.BLACK));
+        assertThat(blackSide.findOpponent()).isEqualTo(Side.from(Color.WHITE));
+    }
 }
