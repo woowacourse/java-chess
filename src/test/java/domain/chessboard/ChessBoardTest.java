4fae0ff4967f2ed906ab563d181c20e2f119ff47
junpakPark <junpak.park@gmail.com>
2023-03-22 09:51:23 +0900

    feat : findPieces 메서드와 findColumnCounts 메서드 구현

--- src/test/java/domain/chessboard/ChessBoardTest.java
+++ src/test/java/domain/chessboard/ChessBoardTest.java
@@ -1,7 +1,11 @@
 package domain.chessboard;
 
+import domain.piece.*;
+import domain.squarestatus.SquareStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,4 +21,32 @@ public class ChessBoardTest {
         assertThat(sum).isEqualTo(32);
     }
 
+    @Test
+    @DisplayName("색깔을 입력하면 Pawn을 제외한 해당 색깔의 모든 기물을 리스트로 반환한다,")
+    void givenColor_thenReturnPieceList() {
+        //given
+        final ChessBoard chessBoard = ChessBoardFactory.generate();
+
+        //when
+        final List<SquareStatus> whitePieces = chessBoard.findPieces(Color.WHITE);
+
+        //then
+        assertThat(whitePieces).hasSize(8);
+    }
+
+    @Test
+    @DisplayName("색깔을 입력하면 해당 색깔의 Pawn 개수를 칼럼별로 리스트화한다.")
+    void givenColor_thenPawnCountList() {
+        //given
+        final ChessBoard chessBoard = ChessBoardFactory.generate();
+
+        //when
+        final List<Long> columnPawnCount = chessBoard.findColumnPawnCounts(Color.WHITE);
+
+
+        //then
+        assertThat(columnPawnCount)
+                .containsOnly(1L)
+                .hasSize(8);
+    }
 }
