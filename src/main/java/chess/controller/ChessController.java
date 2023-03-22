a7979d0c044e2df635606d6b2b9875cd27c7f5f3
sh111-coder <ohk9134@naver.com>
2023-03-22 15:50:15 +0900

    refactor(ChessController): 커맨드 관리 역할 CommandManager에게 위임

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,39 +1,28 @@
 package chess.controller;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 
-import chess.board.Board;
-import chess.board.Position;
-import chess.game.ChessGame;
-import chess.game.GameStatus;
-import chess.piece.Pieces;
+import chess.gamecommand.CommandManager;
+import chess.gamecommand.Commands;
+import chess.piece.Piece;
 import chess.view.InputView;
 import chess.view.OutputView;
 
 public class ChessController {
-    private static final int SOURCE_POSITION_INDEX = 1;
-    private static final int TARGET_POSITION_INDEX = 2;
-    private static final int FILE_INDEX = 0;
-    private static final int RANK_INDEX = 1;
 
     private final InputView inputView;
-    private final ChessGame chessGame;
-
+    private final CommandManager commandManager;
 
     public ChessController(final InputView inputView) {
         this.inputView = inputView;
-        this.chessGame = new ChessGame(new Board(new Pieces()));
+        this.commandManager = new CommandManager();
     }
 
     public void run() {
         printInitMessage();
-        GameStatus gameStatus = GameStatus.INIT;
-
-        while (gameStatus != GameStatus.END) {
-            final GameStatus finalGameStatus = gameStatus;
-            gameStatus = repeat(() -> handleCommand(finalGameStatus, chessGame));
+        while (!commandManager.isEnd()) {
+            executeCommandStep();
+            printBoardStep();
         }
     }
 
@@ -42,67 +31,30 @@ public class ChessController {
         OutputView.printGameCommandInputMessage();
     }
 
-    private <T> T repeat(Supplier<T> supplier) {
+    private void executeCommandStep() {
         try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
+            commandManager.execute(new Commands(inputGameCommand()));
+        } catch (Exception e) {
             OutputView.printErrorMessage(e);
-            return repeat(supplier);
+            executeCommandStep();
         }
     }
 
-    private GameStatus handleCommand(GameStatus gameStatus, final ChessGame chessGame) {
-        final List<String> splitGameCommand = inputGameCommand();
-        final String gameCommand = splitGameCommand.get(0);
-
-        if (gameCommand.equals("start")) {
-            return handleStartCommand(gameStatus, chessGame);
+    private void printBoardStep() {
+        if (!commandManager.isEnd()) {
+            List<Piece> pieces = commandManager.getPieces();
+            OutputView.printCurrentTurn(commandManager.getTurnDisplayName());
+            NameBoard nameBoard = NameBoard.generateNameBoard(pieces);
+            OutputView.printBoard(nameBoard.getNameBoard());
         }
-        if (gameCommand.equals("move")) {
-            return handleMoveCommand(gameStatus, chessGame, splitGameCommand);
-        }
-        return GameStatus.END;
     }
 
     private List<String> inputGameCommand() {
-        return repeat(inputView::inputGameCommand);
-    }
-
-    private GameStatus handleStartCommand(GameStatus gameStatus, final ChessGame chessGame) {
-        checkGameAlreadyStart(gameStatus);
-        gameStatus = GameStatus.START;
-        NameBoard nameBoard = NameBoard.generateNameBoard(chessGame.getPieces());
-        OutputView.printBoard(nameBoard.getNameBoard());
-        return gameStatus;
-    }
-
-    private void checkGameAlreadyStart(final GameStatus gameStatus) {
-        if (gameStatus == GameStatus.START) {
-            throw new IllegalArgumentException("[ERROR] 게임 플레이 중에는 다시 시작할 수 없습니다.");
-        }
-    }
-
-    private GameStatus handleMoveCommand(GameStatus gameStatus, final ChessGame chessGame, final List<String> moveCommand) {
-        checkGameNotStart(gameStatus);
-        Position sourcePosition = generatePosition(SOURCE_POSITION_INDEX, chessGame, moveCommand);
-        Position targetPosition = generatePosition(TARGET_POSITION_INDEX, chessGame, moveCommand);
-        chessGame.movePiece(sourcePosition, targetPosition);
-        NameBoard nameBoard = NameBoard.generateNameBoard(chessGame.getPieces());
-        OutputView.printBoard(nameBoard.getNameBoard());
-        return gameStatus;
-    }
-
-    private Position generatePosition(int positionIndex, ChessGame chessGame, List<String> moveCommand) {
-        String positionInput = moveCommand.get(positionIndex);
-        final List<String> splitPosition = Arrays.asList(positionInput.split(""));
-        String file = splitPosition.get(FILE_INDEX);
-        int rank = Integer.parseInt(splitPosition.get(RANK_INDEX));
-        return chessGame.generatePosition(file, rank);
-    }
-
-    private void checkGameNotStart(final GameStatus gameStatus) {
-        if (gameStatus != GameStatus.START) {
-            throw new IllegalArgumentException("[ERROR] 게임 시작 이후에 말을 이동할 수 있습니다.");
+        try {
+            return inputView.inputGameCommand();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return inputGameCommand();
         }
     }
 }
