daf80cb35a930c9b434d896e4df4a17eb47b1260
eunbii0213 <eunbii0213@naver.com>
2023-03-22 14:52:58 +0900

    refactor: 에러메세지 추가

--- src/main/java/chess/domain/game/ChessBoard.java
+++ src/main/java/chess/domain/game/ChessBoard.java
@@ -4,6 +4,7 @@ import chess.domain.Color;
 import chess.domain.Position;
 import chess.domain.piece.EmptyPiece;
 import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
 import chess.practiceMove.Direction;
 
@@ -11,13 +12,10 @@ import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
+import static chess.view.ErrorMessage.OTHER_PIECE_IN_ROUTE_ERROR_GUIDE_MESSAGE;
+
 public class ChessBoard {
 
-    private static final String NO_PIECE_ERROR_GUIDE_MESSAGE = "이동할 수 있는 기물이 없습니다.";
-    private static final String DIRECTION_ERROR_GUIDE_MESSAGE = "기물이 해당 방향으로 이동할 수 없습니다";
-    private static final String MOVE_ERROR_GUIDE_MESSAGE = "기물이 이동할 수 없습니다";
-    private static final String OTHER_PIECE_IN_ROUTE_ERROR_GUIDE_MESSAGE = "이동 경로에 기물이 있으므로 이동할 수 없습니다";
-    private static final String SAME_COLOR_PIECE_EXIST_ERROR_GUIDE_MESSAGE = "같은 팀이라 기물이 이동할 수 없습니다";
     Map<Position, Piece> chessBoard;
 
     public ChessBoard(Map<Position, Piece> chessBoard) {
@@ -50,7 +48,7 @@ public class ChessBoard {
 
         do {
             currentPosition = currentPosition.moveDirection(direction);
-            checkOtherPieceInTravelRoute(currentPosition, end, findStartPiece(start));
+            checkOtherPieceInTravelRoute(currentPosition, end);
         } while (!currentPosition.equals(end));
 
     }
@@ -58,11 +56,19 @@ public class ChessBoard {
     private void movePieceToDestination(Position start, Position end, Piece piece) {
         chessBoard.replace(start, new EmptyPiece());
         chessBoard.replace(end, piece);
+        changePawnSetting(piece);
     }
 
-    private void checkOtherPieceInTravelRoute(Position currentPosition, Position end, Piece startPiece) {
+    private void changePawnSetting(Piece piece) {
+        if(piece instanceof Pawn){
+            Pawn pawn = (Pawn) piece;
+           pawn.afterFirstMove();
+        }
+    }
+
+    private void checkOtherPieceInTravelRoute(Position currentPosition, Position end) {
         if (!currentPosition.equals(end) && chessBoard.get(currentPosition).getColor()!=Color.NONE) {
-            throw new IllegalArgumentException(OTHER_PIECE_IN_ROUTE_ERROR_GUIDE_MESSAGE);
+            throw new IllegalArgumentException(OTHER_PIECE_IN_ROUTE_ERROR_GUIDE_MESSAGE.getErrorMessage());
         }
     }
 }
