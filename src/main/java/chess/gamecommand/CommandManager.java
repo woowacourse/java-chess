5d6fc99df01c02285b70a9b408e9ad54e7059864
sh111-coder <ohk9134@naver.com>
2023-03-22 15:05:32 +0900

    feat(CommandManager): 커맨드를 관리하는 객체 구현

---
+++ src/main/java/chess/gamecommand/CommandManager.java
@@ -0,0 +1,49 @@
+package chess.gamecommand;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import chess.board.Position;
+import chess.piece.Piece;
+
+public class CommandManager {
+
+    private final Map<GameCommand, Consumer<Commands>> executionByGameCommand = Map.of(
+            GameCommand.START, none -> start(),
+            GameCommand.MOVE, this::move,
+            GameCommand.END, none -> end()
+    );
+
+    private CommandStatus commandStatus;
+
+    public CommandManager() {
+        this.commandStatus = new Init();
+    }
+
+    public void execute(Commands commands) {
+        executionByGameCommand.get(commands.getCommand()).accept(commands);
+    }
+
+    private void start() {
+        this.commandStatus = commandStatus.start();
+    }
+
+    private void move(Commands commands) {
+        Position sourcePosition = commands.generateSourcePosition();
+        Position targetPosition = commands.generateTargetPosition();
+        this.commandStatus = commandStatus.move(sourcePosition, targetPosition);
+    }
+
+    private void end() {
+        this.commandStatus = commandStatus.end();
+    }
+
+    public boolean isEnd() {
+        return commandStatus.isEnd();
+    }
+
+    public List<Piece> getPieces() {
+        return commandStatus.getPieces();
+    }
+}
