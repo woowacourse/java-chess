f00593f738cd2869f3c1db8770f8756e8a9f057c
BGuga <tmdwns9913@gmail.com>
2023-03-22 13:38:30 +0900

    refactor: boardGenerator 의 메서드들이 반환 값을 가지도록 변경

--- src/main/java/chess/domain/BoardGenerator.java
+++ src/main/java/chess/domain/BoardGenerator.java
@@ -29,15 +29,18 @@ public class BoardGenerator {
 
     public static Board makeBoard() {
         Map<Position, Piece> board = new HashMap<>();
-        enrollBlackPiece(board);
-        enrollWhitePiece(board);
-        enrollEmptyPiece(board);
+        board.putAll(makeBlackPiece());
+        board.putAll(makeWhitePiece());
+        board.putAll(makeEmptyPiece());
         return new Board(board);
     }
 
-    private static void enrollBlackPiece(Map<Position, Piece> board) {
-        enrollPiece(board, makePiecesExceptPawns(Color.BLACK, Rank.EIGHT));
-        enrollPiece(board, makePawns(Color.BLACK, Rank.SEVEN));
+    private static Map<Position, Piece> makeBlackPiece() {
+        HashMap<Position, Piece> result = new HashMap<>();
+        result.putAll(makePiecesExceptPawns(Color.BLACK, Rank.EIGHT));
+        result.putAll(makePawns(Color.BLACK, Rank.SEVEN));
+
+        return result;
     }
 
     private static Map<Position, Piece> makePiecesExceptPawns(Color color, Rank rank) {
@@ -64,24 +67,18 @@ public class BoardGenerator {
         return result;
     }
 
-    private static void enrollPiece(Map<Position, Piece> board, Map<Position, Piece> data) {
-        board.putAll(data);
-    }
-
-    private static void enrollWhitePiece(Map<Position, Piece> board) {
-        enrollPiece(board, makePiecesExceptPawns(Color.WHITE, Rank.ONE));
-        enrollPiece(board, makePawns(Color.WHITE, Rank.TWO));
-    }
-
-    private static void enrollEmptyPiece(Map<Position, Piece> board) {
-        enrollPiece(board, makeEmptyPiece());
+    private static Map<Position, Piece> makeWhitePiece() {
+        HashMap<Position, Piece> result = new HashMap<>();
+        result.putAll(makePiecesExceptPawns(Color.WHITE, Rank.ONE));
+        result.putAll(makePawns(Color.WHITE, Rank.TWO));
+        return result;
     }
 
     private static Map<Position, Piece> makeEmptyPiece() {
         HashMap<Position, Piece> result = new HashMap<>();
         for (File file : File.values()) {
             for (int i = 3; i < 7; i++) {
-                result.computeIfAbsent(Position.of(file, Rank.from(i)), (ignored) -> EmptyPiece.getInstance());
+                result.put(Position.of(file, Rank.from(i)), EmptyPiece.getInstance());
             }
         }
         return result;
