88daed0f143df3d243cbba3ce2469145fe81c613
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-03-22 09:33:04 +0900

    refactor: 클래스 이름 수정

--- src/test/java/chess/domain/ChessGameTest.java
+++ src/test/java/chess/domain/ChessGameTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class RunningChessGameTest {
+class ChessGameTest {
 
     private ChessGame chessGame;
 
