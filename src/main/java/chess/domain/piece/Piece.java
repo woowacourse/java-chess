4103556c451d60c854dac1abac90624dc543564d
waterricecake <fruturum@nate.com>
2023-03-22 11:41:34 +0900

    feat(Piece): 기물별 점수 반환 기능 구현

--- src/main/java/chess/domain/piece/Piece.java
+++ src/main/java/chess/domain/piece/Piece.java
@@ -29,4 +29,7 @@ public abstract class Piece {
         return camp.isOpposite(otherCamp);
     }
 
+    public double getScore() {
+        return this.getPieceType().getScore();
+    }
 }
--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -3,21 +3,27 @@ package chess.domain.piece;
 import java.util.function.Function;
 
 public enum PieceType {
-    PAWN(Pawn::new),
-    ROOK(Rook::new),
-    KNIGHT(Knight::new),
-    BISHOP(Bishop::new),
-    QUEEN(Queen::new),
-    KING(King::new),
-    EMPTY(Empty::new);
+    PAWN(Pawn::new, 1),
+    ROOK(Rook::new, 5),
+    KNIGHT(Knight::new, 2.5),
+    BISHOP(Bishop::new, 3),
+    QUEEN(Queen::new, 9),
+    KING(King::new, 0),
+    EMPTY(Empty::new, 0);
 
     private final Function<Camp, Piece> expression;
+    private final double score;
 
-    PieceType(Function<Camp, Piece> expression) {
+    PieceType(Function<Camp, Piece> expression, double score) {
         this.expression = expression;
+        this.score = score;
     }
 
     public Piece createPiece(Camp camp) {
         return this.expression.apply(camp);
+    }
+
+    public double getScore() {
+        return this.score;
     }
 }
