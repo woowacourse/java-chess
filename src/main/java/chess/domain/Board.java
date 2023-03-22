513cf1f3e961c2c98ccd0eeb54542a1cbe9339ca
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-22 13:06:23 +0900

    refactor : 보드 기물 이동 기능 리팩터링

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -2,6 +2,7 @@ package chess.domain;
 
 import chess.cache.BoardCache;
 import chess.cache.PieceCache;
+import chess.domain.direction.BasicDirection;
 import chess.domain.piece.Empty;
 import chess.domain.piece.Piece;
 
@@ -9,10 +10,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Board {
-    private static final int PAWN_SINGLE_MOVE = 1;
-    private static final int PAWN_DOUBLE_MOVE = 2;
+import static chess.domain.PieceType.EMPTY;
+import static chess.domain.PieceType.PAWN;
 
+public class Board {
     private final Map<Position, Piece> board;
 
     private Board(final Map<Position, Piece> board) {
@@ -27,87 +28,57 @@ public class Board {
     }
 
     public void move(final Position source, final Position target, final Color currentPlayer) {
-        validateRange(source, target);
         final Piece sourcePiece = board.get(source);
         final Piece targetPiece = board.get(target);
 
         validateInvalidColor(currentPlayer, sourcePiece, targetPiece);
-        validateInvalidPosition(source, target, sourcePiece);
-        validateBlockedPosition(source, target, sourcePiece);
+        validateTargetBlocked(source, target, sourcePiece);
         validateIfPawnUnmovable(source, target, sourcePiece);
 
         board.put(target, sourcePiece);
         board.put(source, Empty.create());
     }
 
-    private void validateInvalidPosition(final Position source, final Position target, final Piece sourcePiece) {
-        final List<Position> positions = sourcePiece.findPositions(source, target);
-
-        if (!positions.contains(target)) {
-            throw new IllegalArgumentException("이동 할 수 없는 위치 입니다.");
-        }
-    }
-
-    private void validateBlockedPosition(final Position source, final Position target, final Piece sourcePiece) {
-        final List<Position> positions = sourcePiece.findPositions(source, target);
-
-        final boolean isCrossed = positions.subList(0, positions.indexOf(target))
-                .stream()
-                .anyMatch(this::isCrossed);
-
-        if (isCrossed) {
-            throw new IllegalArgumentException("이동 위치가 다른 기물에 의해 막혀 있습니다.");
-        }
-    }
-
-    private boolean isCrossed(final Position position) {
-        return board.getOrDefault(position, Empty.create()).isNotSamePieceType(PieceType.EMPTY);
-    }
-
     private void validateInvalidColor(final Color currentPlayer, final Piece sourcePiece, final Piece targetPiece) {
         if (sourcePiece.isNotSameColor(currentPlayer)) {
             throw new IllegalArgumentException("자신의 기물을 선택해야 합니다.");
         }
-
         if (targetPiece.isSameColor(currentPlayer)) {
             throw new IllegalArgumentException("같은 색깔의 기물을 선택할 수 없습니다.");
         }
     }
 
+    private void validateTargetBlocked(final Position source, final Position target, final Piece sourcePiece) {
+        final List<Position> positions = sourcePiece.findPositions(source, target);
+
+        if (isTargetBlocked(target, positions)) {
+            throw new IllegalArgumentException("이동 위치가 다른 기물에 의해 막혀 있습니다.");
+        }
+    }
+
+    private boolean isTargetBlocked(final Position target, final List<Position> positions) {
+        return positions.subList(0, positions.indexOf(target))
+                .stream()
+                .anyMatch(this::isBlocked);
+    }
+
+    private boolean isBlocked(final Position position) {
+        return board.getOrDefault(position, Empty.create()).isNotSamePieceType(EMPTY);
+    }
+
     private void validateIfPawnUnmovable(final Position source, final Position target, final Piece sourcePiece) {
-        if (sourcePiece.isSamePieceType(PieceType.PAWN) && isPawnUnmovable(source, target)) {
+        if (sourcePiece.isSamePieceType(PAWN) && isPawnUnmovable(source, target)) {
             throw new IllegalArgumentException("폰이 이동할 수 없는 위치입니다.");
         }
     }
 
     private boolean isPawnUnmovable(final Position source, final Position target) {
-        return !(isFrontMove(source, target, PAWN_SINGLE_MOVE)
-                || isFrontMove(source, target, PAWN_DOUBLE_MOVE)
-                || isDiagonalMove(source, target));
-    }
+        final Piece targetPiece = board.get(target);
 
-    private boolean isFrontMove(final Position source, final Position target, final int moveCount) {
-        return source.getRow() == target.getRow()
-                && Math.abs(source.getColumn() - target.getColumn()) == moveCount
-                && board.get(target).isSamePieceType(PieceType.EMPTY);
-    }
-
-    private boolean isDiagonalMove(final Position source, final Position target) {
-        return Math.abs(source.getRow() - target.getRow()) == 1
-                && Math.abs(source.getColumn() - target.getColumn()) == 1
-                && board.get(target).isNotSamePieceType(PieceType.EMPTY);
-    }
-
-    private void validateRange(final Position source, final Position target) {
-        final int maxRange = (int) Math.sqrt(board.size());
-        if (isRangeOver(source, maxRange) || isRangeOver(target, maxRange)) {
-            throw new IllegalArgumentException("입력 값이 보드의 범위를 초과하였습니다.");
+        if (BasicDirection.isDiagonal(source, target)) {
+            return targetPiece.isSamePieceType(EMPTY);
         }
-    }
-
-    private boolean isRangeOver(final Position position, final int maxRange) {
-        return 0 > position.getColumn() || position.getColumn() > maxRange
-                || 0 > position.getRow() || position.getRow() > maxRange;
+        return targetPiece.isNotSamePieceType(EMPTY);
     }
 
     public Map<Position, Piece> getBoard() {
--- src/main/java/chess/domain/direction/BasicDirection.java
+++ src/main/java/chess/domain/direction/BasicDirection.java
@@ -3,6 +3,7 @@ package chess.domain.direction;
 import chess.domain.Position;
 
 import java.util.Arrays;
+import java.util.List;
 
 public enum BasicDirection implements Direction {
     NORTH(0, -1),
@@ -13,6 +14,8 @@ public enum BasicDirection implements Direction {
     NORTH_EAST(1, -1),
     SOUTH_WEST(-1, 1),
     SOUTH_EAST(1, 1);
+
+    private static final List<Direction> diagonals = List.of(NORTH_WEST, NORTH_EAST, SOUTH_WEST, SOUTH_EAST);
 
     private final int rowIndex;
     private final int columnIndex;
@@ -30,6 +33,11 @@ public enum BasicDirection implements Direction {
                 .filter(direction -> direction.isSame(rowDirection, columnDirection))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("존재할 수 없는 방향입니다."));
+    }
+
+    public static boolean isDiagonal(final Position source, final Position target) {
+        final Direction direction = from(source, target);
+        return diagonals.contains(direction);
     }
 
     private boolean isSame(final int otherRowIndex, final int otherColumnIndex) {
