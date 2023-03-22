17fd48850c410c7c8ff7f5dfdd2af23970faa1d0
sh111-coder <ohk9134@naver.com>
2023-03-22 14:50:11 +0900

    feat(Commands): 커맨드 만들어서 반환 기능 구현

--- src/main/java/chess/gamecommand/Commands.java
+++ src/main/java/chess/gamecommand/Commands.java
@@ -1,12 +1,21 @@
 package chess.gamecommand;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import chess.board.File;
+import chess.board.Position;
+import chess.board.Rank;
 
 public class Commands {
 
     private static final int COMMAND_INDEX = 0;
-    
+    private static final int SOURCE_POSITION_INDEX = 1;
+    private static final int TARGET_POSITION_INDEX = 2;
+    private static final int FILE_INDEX = 0;
+    private static final int RANK_INDEX = 1;
+
     private final List<String> commands;
 
     public Commands(List<String> commands) {
@@ -19,5 +28,9 @@ public class Commands {
         if (gameCommand == GameCommand.MOVE && commands.size() != 3) {
             throw new IllegalArgumentException("[ERROR] move 명령어는 소스 위치와 타겟 위치를 모두 입력해야 합니다.");
         }
+    }
+
+    public GameCommand getCommand() {
+        return GameCommand.from(commands.get(COMMAND_INDEX));
     }
 }
