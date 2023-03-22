65e52b10cabf7a434bb21c07ae881585612a4253
kdkdhoho <hkim4410@naver.com>
2023-03-22 14:05:52 +0900

    test(TeamTest): 팀이 중립인지 확인해주는 테스트 추가

---
+++ src/test/java/chess/domain/TeamTest.java
@@ -0,0 +1,17 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TeamTest {
+
+    @Test
+    @DisplayName("팀이 중립인지 확인해준다.")
+    void 팀이_중립인지_확인해준다() {
+        Team team = Team.BLACK;
+
+        assertThat(team.isNeutrality()).isFalse();
+    }
+}
