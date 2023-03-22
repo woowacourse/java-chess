eaeb7b520d644bc0d570ba0a76581d0b9f9f2644
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-03-22 15:15:43 +0900

    test: DirectionVector 움직임에 대한 테스트 추가

--- src/test/java/chessgame/domain/board/DirectionVectorTest.java
+++ src/test/java/chessgame/domain/board/DirectionVectorTest.java
@@ -3,6 +3,11 @@ package chessgame.domain.board;
 import chessgame.domain.coordinate.Coordinate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -89,10 +94,34 @@ class DirectionVectorTest {
     @DisplayName("북서으로 이동하는 경우에 방향 벡터를 찾을 수 있다")
     void calculateNW() {
         Coordinate startCoordinate = Coordinate.from(0, 0);
-        Coordinate endCoordinate = Coordinate.from(-3, -3);
+        Coordinate endCoordinate = Coordinate.from(3, -3);
 
         DirectionVector directionVector = DirectionVector.calculate(startCoordinate, endCoordinate);
 
-        assertThat(directionVector).isEqualTo(DirectionVector.SW);
+        assertThat(directionVector).isEqualTo(DirectionVector.NW);
+    }
+
+    @ParameterizedTest(name = "{0}은 (1, 1)에서 ({1}, {2})로 이동한다")
+    @MethodSource("moveToDirectionTestData")
+    @DisplayName("특정 위치에서 DirectionVector 방향으로 한 칸 이동할 수 있다.")
+    void moveToDirection(DirectionVector directionVector, Coordinate expectCoordinate) {
+        Coordinate startCoordinate = Coordinate.fromOnBoard(1, 1);
+
+        Coordinate resultCoordinate = directionVector.moveToDirection(startCoordinate);
+
+        assertThat(resultCoordinate).isEqualTo(expectCoordinate);
+    }
+
+    static Stream<Arguments> moveToDirectionTestData() {
+        return Stream.of(
+                Arguments.arguments(DirectionVector.N, Coordinate.fromOnBoard(2, 1)),
+                Arguments.arguments(DirectionVector.NE, Coordinate.fromOnBoard(2, 2)),
+                Arguments.arguments(DirectionVector.E, Coordinate.fromOnBoard(1, 2)),
+                Arguments.arguments(DirectionVector.SE, Coordinate.fromOnBoard(0, 2)),
+                Arguments.arguments(DirectionVector.S, Coordinate.fromOnBoard(0, 1)),
+                Arguments.arguments(DirectionVector.SW, Coordinate.fromOnBoard(0, 0)),
+                Arguments.arguments(DirectionVector.W, Coordinate.fromOnBoard(1, 0)),
+                Arguments.arguments(DirectionVector.NW, Coordinate.fromOnBoard(2, 0))
+        );
     }
 }
