9f5cadeba4d2e50a01d059287be7643ade80216c
ezzanzzan <cjsdmswjd821@naver.com>
2023-03-22 14:52:26 +0900

    style: 클래스 네이밍 수정

--- src/main/java/chess/domain/game/Game.java
+++ src/main/java/chess/domain/game/Game.java
@@ -4,7 +4,7 @@ import chess.domain.board.Board;
 import chess.domain.board.Square;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Team;
-import chess.exception.CampNotMatchException;
+import chess.exception.TeamNotMatchException;
 import java.util.List;
 
 public class Game {
@@ -22,7 +22,7 @@ public class Game {
 
     public void move(final Square source, final Square target) {
         if (this.board.isNotMyTurn(source, this.turn) || this.board.isEmptyPiece(source)) {
-            throw new CampNotMatchException(this.turn);
+            throw new TeamNotMatchException(this.turn);
         }
         this.board.move(source, target);
         this.turn = this.turn.nextTurn(this.turn);
--- src/main/java/chess/exception/TeamNotMatchException.java
+++ src/main/java/chess/exception/TeamNotMatchException.java
@@ -2,10 +2,10 @@ package chess.exception;
 
 import chess.domain.piece.Team;
 
-public class CampNotMatchException extends PieceCanNotMoveException{
+public class TeamNotMatchException extends PieceCanNotMoveException{
     private static final String ERROR_MESSAGE = "팀의 말을 선택해주세요.";
 
-    public CampNotMatchException(final Team team) {
+    public TeamNotMatchException(final Team team) {
         super(team.name()+ERROR_MESSAGE);
     }
 }
