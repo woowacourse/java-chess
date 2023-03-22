d99cd4369525b0bea0e0e8b9f2aa7f02bae1eca2
waterricecake <fruturum@nate.com>
2023-03-22 12:59:16 +0900

    test(ChessboardTest): 진영별 점수 반환 테스트

--- src/test/java/chess/ChessboardTest.java
+++ src/test/java/chess/ChessboardTest.java
@@ -5,11 +5,17 @@ import chess.domain.board.Chessboard;
 import chess.domain.board.File;
 import chess.domain.board.Rank;
 import chess.domain.board.Square;
+import chess.domain.piece.Camp;
 import chess.domain.piece.Piece;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 
 class ChessboardTest {
@@ -60,5 +66,25 @@ class ChessboardTest {
 
         Assertions.assertThat(chessboard.isEmptyInRoute(new Square(File.A, Rank.TWO), new Square(File.F, Rank.SEVEN)))
                 .isTrue();
+    }
+
+    @ParameterizedTest(name = "{0}이 한개 없을 때 {3}점을 반환한다.")
+    @MethodSource("validSquareProvider")
+    void countScoreTest(String name, Square source, Square target, double score) {
+        Chessboard chessboard = new Chessboard();
+        BoardInitializer.initializeBoard(chessboard);
+        chessboard.swapPiece(source, target);
+
+        Assertions.assertThat(chessboard.countScore(Camp.WHITE)).isEqualTo(score);
+    }
+
+    static Stream<Arguments> validSquareProvider() {
+        return Stream.of(
+                Arguments.arguments("폰", new Square(File.A, Rank.SEVEN), new Square(File.A, Rank.TWO), 37),
+                Arguments.arguments("퀸", new Square(File.A, Rank.SEVEN), new Square(File.D, Rank.ONE), 29),
+                Arguments.arguments("룩", new Square(File.A, Rank.SEVEN), new Square(File.A, Rank.ONE), 33),
+                Arguments.arguments("나이트", new Square(File.A, Rank.SEVEN), new Square(File.B, Rank.ONE), 35.5),
+                Arguments.arguments("비숍", new Square(File.A, Rank.SEVEN), new Square(File.C, Rank.ONE), 35)
+        );
     }
 }
