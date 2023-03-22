b8b19af5636a41f364fae0f2ebc20dc757cc9770
BackFoxx <backfoxxx@gmail.com>
2023-03-22 13:31:25 +0900

    feat : 가로(랭크)와 세로(파일) 값을 나타내는 객체 구현

---
+++ src/main/java/domain/File.java
@@ -0,0 +1,12 @@
+package domain;
+
+public enum File {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H
+}
---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,12 @@
+package domain;
+
+public enum Rank {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT
+}
