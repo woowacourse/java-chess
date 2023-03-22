133d0669786f574cafaddf31a262e3c009915304
sh111-coder <ohk9134@naver.com>
2023-03-22 14:49:34 +0900

    test(CommandsTest): 커맨드 만들어서 반환 테스트

--- src/test/java/chess/gamecommand/CommandsTest.java
+++ src/test/java/chess/gamecommand/CommandsTest.java
@@ -1,12 +1,17 @@
 package chess.gamecommand;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CommandsTest {
 
@@ -20,5 +25,21 @@ class CommandsTest {
         assertThatThrownBy(() -> new Commands(moveCommands))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] move 명령어는 소스 위치와 타겟 위치를 모두 입력해야 합니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCommand_TestCases")
+    @DisplayName("커맨드를 만들어서 반환한다.")
+    void getCommand(Commands commands, GameCommand expectedGameCommand) {
+        // when, then
+        assertThat(commands.getCommand()).isEqualTo(expectedGameCommand);
+    }
+
+    static Stream<Arguments> getCommand_TestCases() {
+        return Stream.of(
+                Arguments.arguments(new Commands(Arrays.asList("start")), GameCommand.START),
+                Arguments.arguments(new Commands(Arrays.asList("move", "a2", "a4")), GameCommand.MOVE),
+                Arguments.arguments(new Commands(Arrays.asList("end")), GameCommand.END)
+        );
     }
 }
