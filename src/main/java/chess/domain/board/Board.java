6ec1776c74507c9dcb033d00394d2484e5643cf8
xxeol2 <jhj01177@naver.com>
2023-03-22 11:22:49 +0900

    refactor: Board 중복 로직 줄이기

--- src/main/java/chess/domain/board/Board.java
+++ src/main/java/chess/domain/board/Board.java
@@ -17,48 +17,48 @@ public class Board {
     }
 
     public boolean checkTurn(Position position, Color turn) {
-        validatePieceToMove(position);
-        return pieces.get(position).isRightTurn(turn);
+        Piece piece = pieces.get(position);
+        validatePieceToMove(piece);
+        return piece.isRightTurn(turn);
     }
 
     public void move(Position source, Position target) {
-        validateMove(source, target);
-        movePiece(source, target);
+        Piece sourcePiece = pieces.get(source);
+        validatePieceToMove(sourcePiece);
+        Piece targetPiece = pieces.get(target);
+        validateRightTarget(sourcePiece, targetPiece);
+
+        Move move = new Move(source, target);
+        validateMove(sourcePiece, targetPiece, move);
+
+        pieces.remove(source);
+        pieces.put(target, sourcePiece);
     }
 
-    private void validateMove(Position source, Position target) {
-        validatePieceToMove(source);
-        validateRightTarget(source, target);
-        validatePieceReachable(source, target);
-        validateNotCrossOtherPiece(source, target);
-    }
-
-    private void validatePieceToMove(Position position) {
-        Piece piece = pieces.get(position);
+    private void validatePieceToMove(Piece piece) {
         if (piece == null) {
             throw new IllegalArgumentException("움직일 기물이 없습니다");
         }
     }
 
-    private void validateRightTarget(Position source, Position target) {
-        Piece sourcePiece = pieces.get(source);
-        Piece targetPiece = pieces.get(target);
-        if (!sourcePiece.isRightTarget(targetPiece)) {
+    private void validateRightTarget(Piece sourcePiece, Piece targetPiece) {
+        if (sourcePiece.isSameColor(targetPiece)) {
             throw new IllegalArgumentException("목표 위치에 같은 색 말이 있습니다");
         }
     }
 
-    private void validatePieceReachable(Position source, Position target) {
-        Piece sourcePiece = pieces.get(source);
-        Piece targetPiece = pieces.get(target);
-        Move move = new Move(source, target);
+    private void validateMove(Piece sourcePiece, Piece targetPiece, Move move) {
+        validatePieceMove(sourcePiece, targetPiece, move);
+        validateNotCrossOtherPiece(move);
+    }
+
+    private void validatePieceMove(Piece sourcePiece, Piece targetPiece, Move move) {
         if (!sourcePiece.isValidMove(move, targetPiece)) {
             throw new IllegalArgumentException("해당 기물이 이동할 수 없는 수입니다");
         }
     }
 
-    private void validateNotCrossOtherPiece(Position source, Position target) {
-        Move move = new Move(source, target);
+    private void validateNotCrossOtherPiece(Move move) {
         Set<Position> route = move.findRoute();
         for (Position position : route) {
             validateEmpty(position);
@@ -66,19 +66,9 @@ public class Board {
     }
 
     private void validateEmpty(Position position) {
-        if (!isEmpty(position)) {
+        if (pieces.get(position) != null) {
             throw new IllegalArgumentException("다른 기물을 지나칠 수 없습니다");
         }
-    }
-
-    private boolean isEmpty(Position position) {
-        return pieces.get(position) == null;
-    }
-
-    private void movePiece(Position source, Position target) {
-        Piece sourcePiece = pieces.get(source);
-        pieces.remove(source);
-        pieces.put(target, sourcePiece);
     }
 
     public Map<Position, Piece> getPieces() {
