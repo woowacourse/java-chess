ab34d94ea37c1feac15f4461f4549fb8e974a76d
SproutMJ <mjk54565@gmail.com>
2023-03-22 17:45:52 +0900

    refactor: Board의 모습을 출력형태로 변환하기 위해 존재했던 BoardView 클래스를 제거하고 그 내용물을 OutputView로 이동

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,14 +1,45 @@
 package view;
 
 import domain.piece.Piece;
+import domain.position.File;
 import domain.position.Position;
+import domain.position.Positions;
+import domain.position.Rank;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public final class OutputView {
-    private static final BoardView boardView = new BoardView();
+    private static final String EMPTY_SPACE = ".";
 
     public static void printBoard(Map<Position, Piece> board) {
-        boardView.printBoard(board);
+        Arrays.stream(Rank.values())
+                .map(rank -> makeAnRankView(board, rank))
+                .forEach(System.out::println);
+        System.out.println();
+    }
+
+    private static String makeAnRankView(final Map<Position, Piece> board, final Rank rank) {
+        return Arrays.stream(File.values())
+                .map(file -> file.getName() + rank.getName())
+                .map(position -> makePieceView(board, Positions.from(position)))
+                .collect(Collectors.joining());
+    }
+
+    private static String makePieceView(final Map<Position, Piece> board, final Position position) {
+        if (board.containsKey(position)) {
+            return convertCaseByTeam(board.get(position));
+        }
+
+        return EMPTY_SPACE;
+    }
+
+    private static String convertCaseByTeam(Piece piece) {
+        if (piece.isBlack()) {
+            return piece.getName();
+        }
+
+        return piece.getName().toLowerCase();
     }
 
     public static void printError(String message) {
