620160961d82b5e3113cec1b7d427f864e83e903
BackFoxx <backfoxxx@gmail.com>
2023-03-22 13:34:44 +0900

    feat : 하나의 위치를 나타내는 객체 구현

---
+++ src/main/java/domain/Point.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Point {
+    private final File file;
+    private final Rank rank;
+
+    public Point(File file, Rank rank) {
+        this.file = file;
+        this.rank = rank;
+    }
+}
---
+++ src/test/java/domain/PointTest.java
@@ -0,0 +1,14 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class PointTest {
+    @Test
+    @DisplayName("가로와 세로로 이루어진 하나의 위치를 나타낼 수 있다.")
+    void create() {
+        assertDoesNotThrow(() -> new Point(File.A, Rank.FIVE));
+    }
+}
