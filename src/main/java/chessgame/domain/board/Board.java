cf44a419277c5d9e50ff16c75a89a63597d487a4
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-03-22 14:57:32 +0900

    refactor: 적절한 변수 명으로 수정

--- src/main/java/chessgame/domain/board/Board.java
+++ src/main/java/chessgame/domain/board/Board.java
@@ -21,16 +21,16 @@ public class Board {
     }
 
     public boolean checkCamp(final Coordinate coordinate, final Camp camp) {
-        Piece findPiece = board.get(coordinate);
-        return findPiece.isSameCamp(camp);
+        Piece startPiece = board.get(coordinate);
+        return startPiece.isSameCamp(camp);
     }
 
     public void move(final Coordinate startCoordinate, final Coordinate endCoordinate) {
         if (isMovable(startCoordinate, endCoordinate)) {
-            Piece findPiece = board.get(startCoordinate);
+            Piece startPiece = board.get(startCoordinate);
             board.put(startCoordinate, new EmptyPiece());
-            board.put(endCoordinate, findPiece);
-            findPiece.checkMoved();
+            board.put(endCoordinate, startPiece);
+            startPiece.checkMoved();
             return;
         }
         throw new IllegalArgumentException("[ERROR] 해당 기물을 옮길 수 없습니다.");
@@ -43,24 +43,24 @@ public class Board {
 
     private boolean isMovableByRule(final Coordinate startCoordinate,
                                     final Coordinate endCoordinate) {
-        Piece findPiece = board.get(startCoordinate);
+        Piece startPiece = board.get(startCoordinate);
         Piece targetPiece = board.get(endCoordinate);
 
         if (isPieceExistsAt(endCoordinate)) {
-            return findPiece.isCatchable(targetPiece.camp(), startCoordinate, endCoordinate);
+            return startPiece.isCatchable(targetPiece.camp(), startCoordinate, endCoordinate);
         }
-        return findPiece.isMovable(startCoordinate, endCoordinate);
+        return startPiece.isMovable(startCoordinate, endCoordinate);
     }
 
     private boolean isPieceExistsAt(final Coordinate coordinate) {
-        Piece findPiece = board.get(coordinate);
-        return findPiece.isExist();
+        Piece startPiece = board.get(coordinate);
+        return startPiece.isExist();
     }
 
     private boolean isNotBlocked(final Coordinate startCoordinate, final Coordinate endCoordinate) {
-        Piece findPiece = board.get(startCoordinate);
+        Piece startPiece = board.get(startCoordinate);
 
-        if (findPiece.canReap()) {
+        if (startPiece.canReap()) {
             return true;
         }
         return isNotBlockedWhenNotReap(startCoordinate, endCoordinate);
