f96c55bb6522681aef14fc4b709acbe2a67b7957
JunDonghyuk <ehdgur4814@naver.com>
2023-03-23 01:04:52 +0900

    feat: 게임 상태 enum 구현

--- src/main/java/chess/domain/game/GameStatus.java
+++ src/main/java/chess/domain/game/GameStatus.java
@@ -2,10 +2,23 @@ package chess.domain.game;
 
 public enum GameStatus {
 
+    READY,
     RUNNING,
     TERMINATED;
 
+    public boolean isReady() {
+        return this == READY;
+    }
+
+    public boolean isRunning() {
+        return this == RUNNING;
+    }
+
     public boolean isTerminated() {
         return this == TERMINATED;
+    }
+
+    public boolean isNotRunning() {
+        return this != RUNNING;
     }
 }
