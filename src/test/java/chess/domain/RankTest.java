d7c337c510a6e0946fe781447cf7381769ec8bc7
kokodak <kokodakadokok@gmail.com>
2023-03-22 13:53:19 +0900

    test: Rank 테스트 작성

--- src/test/java/chess/domain/RankTest.java
+++ src/test/java/chess/domain/RankTest.java
@@ -1,9 +1,12 @@
 package chess.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import chess.domain.position.Rank;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,10 +14,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class RankTest {
 
+    @Test
+    void 인덱스_값으로_Rank_를_찾을_수_있다() {
+        final int index = 1;
+
+        final Rank rank = Rank.findByIndex(index);
+
+        assertThat(rank).isEqualTo(Rank.ONE);
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {0, 9})
-    void 잘못된_위치인_경우_예외를_던진다(int index) {
-        Assertions.assertThatThrownBy(() -> Rank.findByIndex(index))
+    void 잘못된_인덱스인_경우_예외를_던진다(int index) {
+        assertThatThrownBy(() -> Rank.findByIndex(index))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 위치입니다.");
     }
