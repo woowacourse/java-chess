388f83d7819cc915f3d83f03fe6d8843fcd80026
ReO <yt930310@gmail.com>
2023-03-22 20:30:58 +0900

    refactor: todo 삭제

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,6 @@
 import controller.ChessController;
 
 public class Application {
-    // TODO: 2023/03/17 기물 타겟까지만 리스트 뽑아야됨 
-    // TODO: 2023/03/17 move 중간에 continue 빼는거
-    // TODO: 2023/03/21 rook 안움직임
     public static void main(String[] args) {
         new ChessController().run();
     }
