64978e9592c422fa2c8c3370c9fd9048e4c1cfdb
ingpyo <juninpyo@naver.com>
2023-03-22 16:49:48 +0900

    refactor: 입력 함수 수정

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -3,7 +3,6 @@ package chess.view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public final class InputView {
 
@@ -11,7 +10,6 @@ public final class InputView {
     private static final String MOVE_DELIMITER = " ";
 
     public List<String> readCommand() {
-        return Arrays.stream(scanner.nextLine().split(MOVE_DELIMITER))
-                .collect(Collectors.toList());
+        return Arrays.asList(scanner.nextLine().split(MOVE_DELIMITER));
     }
 }
--- src/test/java/chess/domain/ChessBoardMakerTest.java
+++ src/test/java/chess/domain/ChessBoardMakerTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
