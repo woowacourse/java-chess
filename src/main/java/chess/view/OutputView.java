490684d516447641500983e62799061bf6f8af02
hgo641 <hgo641@gmail.com>
2023-03-22 17:20:58 +0900

    refactor: 보드 출력 방식 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,9 +1,15 @@
 package chess.view;
 
+import chess.domain.position.File;
 import chess.domain.position.Position;
+import chess.domain.position.Rank;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OutputView {
     public void printErrorMessage(String message) {
@@ -18,36 +24,20 @@ public class OutputView {
     }
 
     public void printBoard(Map<Position, String> pieceNames) {
-        List<List<String>> board = initBoard();
+        List<Rank> ranks = Arrays.asList(Rank.values());
+        Collections.reverse(ranks);
 
-        for (Position position : pieceNames.keySet()) {
-            List<Integer> coordinate = position.getCoordinate();
-            List<String> oneLine = board.get(coordinate.get(0));
-            oneLine.set(coordinate.get(1), pieceNames.get(position));
-        }
-
-        for (int i = 0; i < 8; i++) {
-            for (String name : board.get(i)) {
-                System.out.print(name);
-            }
+        for(Rank rank : ranks){
+            printPieceName(pieceNames, rank);
             System.out.println();
         }
         System.out.println();
     }
 
-    private List<List<String>> initBoard() {
-        List<List<String>> board = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            board.add(initOneLine());
+    private static void printPieceName(Map<Position, String> pieceNames, Rank rank) {
+        for(File file : File.values()){
+            String name = pieceNames.get(Position.of(file, rank));
+            System.out.print(name);
         }
-        return board;
-    }
-
-    private List<String> initOneLine() {
-        List<String> oneLine = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            oneLine.add(".");
-        }
-        return oneLine;
     }
 }
