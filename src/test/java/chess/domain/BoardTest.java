e626a8da8169dc52e5eae71479e77cbe152c8188
hectick <chyeon1223@cau.ac.kr>
2023-03-22 15:41:04 +0900

    test(Board): 테스트 코드 수정

--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -17,7 +17,7 @@ class BoardTest {
         @Test
         @DisplayName("보드가 정상적으로 생성된다.")
         void moveTest() {
-            assertDoesNotThrow(Board::create);
+            assertDoesNotThrow(BoardGenerator::createBoard);
         }
     }
 
@@ -28,7 +28,7 @@ class BoardTest {
         @Test
         @DisplayName("source 위치에 조작할 수 있는 말이 없으면 예외처리한다.")
         void moveTest1() {
-            Board board = Board.create();
+            Board board = BoardGenerator.createBoard();
             Position source = new Position(3, 3);
             Position target = new Position(3, 4);
 
@@ -39,7 +39,7 @@ class BoardTest {
         @Test
         @DisplayName("source 위치에 있는 말과 target 위치에 있는 말의 팀 색깔이 같으면 예외처리한다.")
         void moveTest2() {
-            Board board = Board.create();
+            Board board = BoardGenerator.createBoard();
             Position source = new Position(0, 0);
             Position target = new Position(0, 1);
 
@@ -50,7 +50,7 @@ class BoardTest {
         @Test
         @DisplayName("말의 이동방향이 올바르지 않을 경우 예외처리한다.")
         void moveTest3() {
-            Board board = Board.create();
+            Board board = BoardGenerator.createBoard();
             Position initial = new Position(0, 1);
             Position source = new Position(0, 2);
             Position target = new Position(0, 1);
@@ -63,7 +63,7 @@ class BoardTest {
         @Test
         @DisplayName("말이 knight 가 아닌데 다른 말을 뛰어넘으려는 경우 예외처리한다.")
         void moveTest4() {
-            Board board = Board.create();
+            Board board = BoardGenerator.createBoard();
             Position source = new Position(3, 0);
             Position target = new Position(3, 4);
 
@@ -74,7 +74,7 @@ class BoardTest {
         @Test
         @DisplayName("pawn 이 잡을 말이 없는데 대각선으로 이동하고자 할 경우 예외처리한다.")
         void moveTest5() {
-            Board board = Board.create();
+            Board board = BoardGenerator.createBoard();
             Position source = new Position(1, 6);
             Position target = new Position(2, 5);
 
@@ -85,7 +85,7 @@ class BoardTest {
         @Test
         @DisplayName("knight 는 다른 말을 건너 뛸 수 있다.")
         void moveTest6() {
-            Board board = Board.create();
+            Board board = BoardGenerator.createBoard();
             Position source = new Position(1, 0);
             Position target = new Position(2, 2);
 
