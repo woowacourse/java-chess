d27d2e24c67e28fc3b74fc79ff282eaf303e8b4b
sh111-coder <ohk9134@naver.com>
2023-03-22 21:53:53 +0900

    feat(Score): 스코어 더하는 기능 구현

---
+++ src/main/java/chess/domain/board/Score.java
@@ -0,0 +1,34 @@
+package chess.domain.board;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Score {
+
+    private final BigDecimal score;
+
+    public Score(final BigDecimal score) {
+        this.score = score;
+    }
+
+    public Score add(Score scoreToAdd) {
+        return new Score(this.score.add(scoreToAdd.score));
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Score score1 = (Score) o;
+        return Objects.equals(score, score1.score);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
+    }
+}
