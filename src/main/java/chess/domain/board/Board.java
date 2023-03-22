40530e19f1d63d6947d90f69a4f61013685b4bf9
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-03-22 17:41:58 +0900

    refactor : square에 대한 검증을 square 내부로 이동

--- src/main/java/chess/domain/board/Board.java
+++ src/main/java/chess/domain/board/Board.java
@@ -11,9 +11,6 @@ import java.util.stream.Collectors;
 public final class Board {
 
     private static final String CAN_NOT_MOVE_EXCEPTION_MESSAGE = "유효한 움직임이 아닙니다.";
-    private static final String TURN_EXCEPTION_MESSAGE = "의 턴입니다.";
-    private static final String CAN_NOT_MOVE_TO_SAME_COLOR_EXCEPTION_MESSAGE = "자신의 기물이 있는 곳으로 이동할 수 없습니다.";
-    private static final String EMPTY_SOURCE_EXCEPTION_MESSAGE = "움직이려는 기물의 위치는 빈 공간입니다.";
 
     private final List<Squares> board = new ArrayList<>();
 
@@ -31,32 +28,12 @@ public final class Board {
     public void play(Position source, Position target, Color color) {
         Square sourceSquare = getSquare(source);
         Square targetSquare = getSquare(target);
-        validateEmpty(sourceSquare);
-        validateTurn(sourceSquare, color);
-        validateSameColor(sourceSquare, targetSquare);
-        Set<Position> movablePath = sourceSquare.computePath(source, target);
-        Map<Position, Boolean> isEmptySquare = generateIsEmptySquare(movablePath);
+
+        var movablePath = sourceSquare.computePath(source, target, targetSquare, color);
+        var isEmptySquare = generateIsEmptySquare(movablePath);
 
         validateMove(source, target, sourceSquare, isEmptySquare);
         move(sourceSquare, targetSquare);
-    }
-
-    private void validateEmpty(Square sourceSquare) {
-        if (sourceSquare.equalsColor(Color.NONE)) {
-            throw new IllegalArgumentException(EMPTY_SOURCE_EXCEPTION_MESSAGE);
-        }
-    }
-
-    private void validateTurn(final Square sourceSquare, final Color color) {
-        if (!sourceSquare.equalsColor(color)) {
-            throw new IllegalArgumentException(color.name() + TURN_EXCEPTION_MESSAGE);
-        }
-    }
-
-    private void validateSameColor(final Square sourceSquare, final Square targetSquare) {
-        if (sourceSquare.equalsColor(targetSquare)) {
-            throw new UnsupportedOperationException(CAN_NOT_MOVE_TO_SAME_COLOR_EXCEPTION_MESSAGE);
-        }
     }
 
     private void validateMove(final Position source, final Position target, final Square sourceSquare, final Map<Position, Boolean> isEmptySquare) {
@@ -74,7 +51,6 @@ public final class Board {
 
     private void move(final Square sourceSquare, final Square targetSquare) {
         targetSquare.changePiece(sourceSquare);
-        sourceSquare.makeEmpty();
     }
 
     private Square getSquare(final Position source) {
