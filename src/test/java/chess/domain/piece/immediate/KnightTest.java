a153440f7434b62ee952dc52442a6d05a50d7ebb
kokodak <kokodakadokok@gmail.com>
2023-03-22 15:14:46 +0900

    test: Knight 테스트 작성

--- src/test/java/chess/domain/piece/immediate/KnightTest.java
+++ src/test/java/chess/domain/piece/immediate/KnightTest.java
@@ -1,19 +1,25 @@
-package chess.domain.piece;
+package chess.domain.piece.immediate;
 
-import static chess.domain.piece.Side.*;
-import static chess.domain.position.File.*;
-import static chess.domain.position.Rank.*;
-import static org.assertj.core.api.Assertions.*;
+import static chess.domain.piece.Side.BLACK;
+import static chess.domain.piece.Side.WHITE;
+import static chess.domain.position.File.A;
+import static chess.domain.position.File.B;
+import static chess.domain.position.File.C;
+import static chess.domain.position.Rank.ONE;
+import static chess.domain.position.Rank.THREE;
+import static chess.domain.position.Rank.TWO;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.board.Board;
-import chess.domain.piece.immediate.King;
+import chess.domain.piece.Empty;
+import chess.domain.piece.Pawn;
+import chess.domain.piece.Piece;
 import chess.domain.position.File;
 import chess.domain.position.Position;
 import chess.domain.position.Rank;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -21,14 +27,14 @@ import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class KingTest {
+public class KnightTest {
 
     private Map<Position, Piece> boardMap;
-    private King king;
+    private Knight knight;
 
     @BeforeEach
     void init() {
-        king = new King(WHITE);
+        knight = new Knight(WHITE);
         boardMap = new HashMap<>();
         for (final Rank rank : Rank.values()) {
             for (final File file : File.values()) {
@@ -38,7 +44,7 @@ public class KingTest {
     }
 
     @Test
-    void 킹은_자신이_갈_수_있는_위치들을_반환한다() {
+    void 나이트는_자신이_갈_수_있는_위치들을_반환한다() {
         /*
 
         ........
@@ -46,23 +52,23 @@ public class KingTest {
         ........
         ........
         ........
-        ........
-        pP......
-        k.......
+        .P......
+        p.......
+        n.......
 
         */
 
-        boardMap.put(Position.of(A, ONE), king);
+        boardMap.put(Position.of(A, ONE), knight);
         boardMap.put(Position.of(A, TWO), new Pawn(WHITE));
-        boardMap.put(Position.of(B, TWO), new Pawn(BLACK));
+        boardMap.put(Position.of(B, THREE), new Pawn(BLACK));
         final Board board = new Board(boardMap);
 
-        final List<Position> movablePosition = king.findMovablePosition(Position.of(A, ONE), board);
+        final List<Position> movablePosition = knight.findMovablePosition(Position.of(A, ONE), board);
 
         assertThat(movablePosition)
                 .containsOnly(
-                        Position.of(B, ONE),
-                        Position.of(B, TWO)
+                        Position.of(B, THREE),
+                        Position.of(C, TWO)
                 );
     }
 }
