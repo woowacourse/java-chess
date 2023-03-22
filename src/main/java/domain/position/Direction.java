68c2bf7eb3f13b1476568ec521492b20b1625358
MoonJeWoong <munjin0201@naver.com>
2023-03-23 00:51:17 +0900

    refactor: Direction 로직 내 불필요한 함수형 인터페이스 사용 수정

--- src/main/java/domain/position/Direction.java
+++ src/main/java/domain/position/Direction.java
@@ -1,27 +1,21 @@
 package domain.position;
 
-import java.util.Arrays;
-import java.util.function.BiFunction;
-
 public enum Direction {
 
-    CROSS((rowGap, columnGap) -> rowGap == 0 || columnGap == 0),
-    DIAGONAL((rowGap, columnGap) -> Math.abs(rowGap) == Math.abs(columnGap)),
-    OTHER((rowGap, columnGap) -> false);
-
-    private final BiFunction<Integer, Integer, Boolean> isProperDirection;
-
-    Direction(BiFunction<Integer, Integer, Boolean> isProperDirection) {
-        this.isProperDirection = isProperDirection;
-    }
+    CROSS,
+    DIAGONAL,
+    OTHER;
 
     public static Direction of(Position start, Position end) {
         int rowGap = start.calculateRowGap(end);
         int columnGap = start.calculateColumnGap(end);
 
-        return Arrays.stream(values())
-                .filter(direction -> direction.isProperDirection.apply(rowGap, columnGap))
-                .findFirst()
-                .orElse(Direction.OTHER);
+        if (rowGap == 0 || columnGap == 0) {
+            return CROSS;
+        }
+        if (Math.abs(rowGap) == Math.abs(columnGap)) {
+            return DIAGONAL;
+        }
+        return OTHER;
     }
 }
