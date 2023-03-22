fa05cdf3eb7d442536e663139009aff90acbef2d
somsom13 <somin1113@naver.com>
2023-03-22 19:05:32 +0900

    feat: King 이 잡히면 게임 종료

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -6,15 +6,24 @@ public class ChessGame {
 
     private final ChessBoard chessBoard;
     private TeamColor teamColor;
+    private boolean isPlaying;
 
     public ChessGame(final ChessBoard chessBoard) {
         this.chessBoard = chessBoard;
         this.teamColor = TeamColor.WHITE;
+        this.isPlaying = true;
     }
 
     public void move(final Position source, final Position dest) {
         chessBoard.move(source, dest, teamColor);
+        if (chessBoard.isKingDead()) {
+            isPlaying = false;
+        }
         teamColor = teamColor.transfer();
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
     }
 
     public ChessBoard getChessBoard() {
---
+++ src/test/java/chess/domain/ChessGameTest.java
@@ -0,0 +1,32 @@
+package chess.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import chess.domain.piece.Piece;
+import chess.domain.position.Position;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+class ChessGameTest {
+
+    @DisplayName("King 이 잡히면 게임이 종료된다.")
+    @Test
+    void 킹_잡히면_게임_종료() {
+        Map<Position, Piece> boardForTest = Map.of(
+            Position.from("A2"), InitialPiece.WHITE_QUEEN.getPiece(),
+            Position.from("E3"), InitialPiece.WHITE_KING.getPiece(),
+            Position.from("C4"), InitialPiece.BLACK_KING.getPiece(),
+            Position.from("B2"), InitialPiece.BLACK_PAWN.getPiece());
+
+        ChessBoard chessBoard = new ChessBoard(new HashMap<>(boardForTest));
+        ChessGame chessGame = new ChessGame(chessBoard);
+
+        chessGame.move(Position.from("A2"), Position.from("C4"));
+
+        assertThat(chessGame.isPlaying()).isFalse();
+    }
+
+}
