a95ae300a7a86d65a31f7c979f8b872938db635d
sh111-coder <ohk9134@naver.com>
2023-03-22 14:17:55 +0900

    feat(Init): 초기 상태 구현

---
+++ src/main/java/chess/gamecommand/Init.java
@@ -0,0 +1,36 @@
+package chess.gamecommand;
+
+import java.util.List;
+
+import chess.board.Board;
+import chess.board.Position;
+import chess.piece.Piece;
+import chess.piece.Pieces;
+
+public class Init implements CommandStatus {
+
+    @Override
+    public CommandStatus start() {
+        return new Play(new Board(new Pieces()));
+    }
+
+    @Override
+    public CommandStatus move(Position sourcePosition, Position targetPosition) {
+        throw new IllegalStateException("[ERROR] 초기 상태에서는 기물을 움직일 수 없습니다.");
+    }
+
+    @Override
+    public CommandStatus end() {
+        return new End();
+    }
+
+    @Override
+    public boolean isEnd() {
+        return false;
+    }
+
+    @Override
+    public List<Piece> getPieces() {
+        throw new IllegalStateException("[ERROR] 초기 상태에서는 기물들을 반환할 수 없습니다.");
+    }
+}
