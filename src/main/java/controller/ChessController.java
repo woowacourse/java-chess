336578afd1d4503e7cfe06f767ed5c54c9ae6e23
dev_kong <ahdjd5@gmail.com>
2023-03-23 22:24:19 +0900

    feat: 방을 생성하고 저장된 게임을 불러오는 기능 구현

--- src/main/java/controller/ChessController.java
+++ src/main/java/controller/ChessController.java
@@ -10,29 +10,81 @@ import static controller.Command.MOVE_COMMAND_LENGTH;
 import static controller.Command.STANDARD_COMMAND_LENGTH;
 import static controller.Command.START;
 import static controller.Command.TARGET_SQUARE_INDEX;
+import static controller.GameRoomCommand.CREATE;
+import static controller.GameRoomCommand.EXIT;
+import static controller.GameRoomCommand.JOIN;
+import static controller.GameRoomCommand.LIST;
 
 import java.util.EnumMap;
 import java.util.List;
 
+import dto.GameInfoDto;
 import dto.ScoreDto;
+import repository.JdbcChessDao;
+import repository.ProdConnector;
 import service.ChessService;
+import service.GameRoomService;
 import view.InputView;
 import view.OutputView;
 
 public class ChessController {
     private final ChessService chessService;
+    private final GameRoomService gameRoomService = new GameRoomService(new JdbcChessDao(new ProdConnector()));
     private final EnumMap<Command, Action> actions = new EnumMap<>(Command.class);
+    private final EnumMap<GameRoomCommand, GameRoomAction> gameRoomActions = new EnumMap<>(GameRoomCommand.class);
 
     public ChessController(ChessService chessService) {
         this.chessService = chessService;
         actions.put(START, this::start);
         actions.put(MOVE, this::move);
         actions.put(END, this::end);
+        gameRoomActions.put(CREATE, this::createGameRoom);
+        gameRoomActions.put(LIST, this::readGameRooms);
+        gameRoomActions.put(EXIT, this::exitGame);
+        gameRoomActions.put(JOIN, this::joinGame);
+    }
+
+
+    private void exitGame(List<String> inputs) {
     }
 
     public void run() {
+        GameRoomCommand gameRoomCommand = GameRoomCommand.NONE;
+        while (gameRoomCommand != EXIT) {
+            OutputView.printGameRoomInfo();
+            List<String> inputs = InputView.requestCommand();
+            gameRoomCommand = GameRoomCommand.find(inputs.get(0));
+            GameRoomAction gameRoomAction = gameRoomActions.get(gameRoomCommand);
+            gameRoomAction.execute(inputs);
+        }
+    }
+
+    private void createGameRoom(List<String> inputs) {
+        if(inputs.size() != 2) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        gameRoomService.createGameRoom(inputs.get(1));
+    }
+
+    private void joinGame(List<String> inputs) {
+        if(inputs.size() != 2) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        gameStart(inputs.get(1));
+    }
+
+    private void readGameRooms(List<String> inputs) {
+        List<String> gameRooms = gameRoomService.readGameRooms();
+        OutputView.printGameRooms(gameRooms);
+    }
+
+    private void gameStart(String gameName) {
+        GameInfoDto gameInfo = gameRoomService.getGameInfo(gameName);
+        OutputView.printIsSavedGame(gameInfo.getBoardDtos().size());
+        chessService.setUp(gameInfo);
+        OutputView.printChessBoard(chessService.getChessBoard());
         OutputView.printChessInfo();
-        Command command = EMPTY;
+        Command command = START;
         while (command != END) {
             command = play();
         }
@@ -62,7 +114,6 @@ public class ChessController {
 
     private void start(List<String> inputs) {
         Command.validateCommandLength(inputs.size(), STANDARD_COMMAND_LENGTH);
-        chessService.setUp();
         OutputView.printChessBoard(chessService.getChessBoard());
     }
 
@@ -76,6 +127,7 @@ public class ChessController {
 
     private void end(List<String> inputs) {
         Command.validateCommandLength(inputs.size(), STANDARD_COMMAND_LENGTH);
-        chessService.end();
+        GameInfoDto gameInfoDto = chessService.end();
+        gameRoomService.saveGameInfo(gameInfoDto);
     }
 }
---
+++ src/main/java/controller/GameRoomCommand.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.util.Arrays;
+
+public enum GameRoomCommand {
+    LIST,
+    CREATE,
+    EXIT,
+    JOIN,
+    NONE;
+
+    public static GameRoomCommand find(String command) {
+        return Arrays.stream(GameRoomCommand.values())
+                .filter(gameRoomCommand -> !gameRoomCommand.equals(NONE))
+                .filter(gameRoomCommand -> gameRoomCommand.name().equals(command))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 입력이 아닙니다."));
+    }
+}
