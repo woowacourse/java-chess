57dd98e8c802815a5b213f9a23e346060524c591
이건회 <rg970604@naver.com>
2023-03-23 22:27:17 +0900

    refactor : 동일성 비교 동등성 비교로 변경

--- src/main/java/chess/piece/Team.java
+++ src/main/java/chess/piece/Team.java
@@ -1,43 +1,46 @@
 package chess.piece;
 
+import java.util.List;
+
 public enum Team {
-    BLACK, WHITE, EMPTY;
-    
-    private static final int MAX_ROW_NUMBER_OF_BLACK_TEAM = 2;
-    private static final int MIN_ROW_NUMBER_WHITE_TEAM = 7;
-    private static final int MIN_ROW_NUM = 1;
-    private static final int MAX_ROW_NUM = 8;
-    
-    public static Team from(int rowNum) {
+    BLACK,
+    WHITE,
+    EMPTY;
+
+    private static final List<String> BLACK_TEAM_ROW_NUMS = List.of("1", "2");
+    private static final List<String> WHITE_TEAM_ROW_NUMS = List.of("7", "8");
+    private static final List<String> ROW_NUMS = List.of("1", "2", "3", "4", "5", "6", "7", "8");
+
+    public static Team from(String rowNum) {
         validateOutOfRangeRowNum(rowNum);
         if (isBlackTeamRowNum(rowNum)) {
             return BLACK;
         }
-        
+
         if (isWhiteTeamRowNum(rowNum)) {
             return WHITE;
         }
         return EMPTY;
     }
-    
-    private static boolean isBlackTeamRowNum(int rowNum) {
-        return rowNum <= MAX_ROW_NUMBER_OF_BLACK_TEAM;
+
+    private static boolean isBlackTeamRowNum(String rowNum) {
+        return BLACK_TEAM_ROW_NUMS.contains(rowNum);
     }
-    
-    private static boolean isWhiteTeamRowNum(int rowNum) {
-        return rowNum >= MIN_ROW_NUMBER_WHITE_TEAM;
+
+    private static boolean isWhiteTeamRowNum(String rowNum) {
+        return WHITE_TEAM_ROW_NUMS.contains(rowNum);
     }
-    
-    private static void validateOutOfRangeRowNum(int rowNum) {
+
+    private static void validateOutOfRangeRowNum(String rowNum) {
         if (isOutOfRangeRowNum(rowNum)) {
             throw new IllegalArgumentException("행의 번호는 1~8의 범위를 벗어날 수 없습니다.");
         }
     }
-    
-    private static boolean isOutOfRangeRowNum(int rowNum) {
-        return rowNum < MIN_ROW_NUM || rowNum > MAX_ROW_NUM;
+
+    private static boolean isOutOfRangeRowNum(String rowNum) {
+        return !ROW_NUMS.contains(rowNum);
     }
-    
+
     public boolean isSameTeam(Team otherTeam) {
         return this == otherTeam;
     }
