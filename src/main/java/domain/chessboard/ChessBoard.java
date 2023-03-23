b8a247674d6731e1a4aff448bfe9a2879c9ff836
jaeyeon kim <jakind@naver.com>
2023-03-23 19:56:19 +0900

    refactor : ColorBoard 삭제 후, ChessBoard 로 역할 위임

--- src/main/java/domain/chessboard/ChessBoard.java
+++ src/main/java/domain/chessboard/ChessBoard.java
@@ -1,9 +1,13 @@
 package domain.chessboard;
 
 import domain.coordinate.Position;
+import domain.piece.Color;
+import domain.piece.PieceType;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ChessBoard {
 
@@ -36,6 +40,52 @@ public class ChessBoard {
         return chessBoard.get(position.getY())
                 .getRank()
                 .get(position.getX());
+    }
+
+    public double calculateColorScore(Color color) {
+        List<Rank> chessBoard1 = chessBoard;
+        double sum = chessBoard1.stream()
+                .flatMap(rank -> rank.getRank().stream())
+                .filter(square -> square.isNotSameType(EmptyType.EMPTY) && square.isSameColor(color))
+                .mapToDouble(square -> square.getSquareStatus().getScore())
+                .sum();
+
+        return sum - diffScore(chessBoard1, color);
+    }
+
+    private double diffScore(List<Rank> chessBoard1, Color color) {
+        Map<Integer, Integer> numberOfPawnInColumn = new HashMap<>();
+        for (Rank value : chessBoard1) {
+            List<Square> rank = value.getRank();
+            forEachColumn(numberOfPawnInColumn, rank, color);
+        }
+        return numberOfPawnInColumn.values()
+                .stream()
+                .filter(i -> 2 <= i)
+                .mapToDouble(i -> i * 0.5)
+                .sum();
+    }
+
+    private void forEachColumn(Map<Integer, Integer> numberOfPawnInColumn, List<Square> rank, Color color) {
+        for (int column = 0; column < rank.size(); column++) {
+            putIfExistPawn(numberOfPawnInColumn, rank, column, color);
+        }
+    }
+
+    private void putIfExistPawn(Map<Integer, Integer> numberOfPawnInColumn, List<Square> rank, int column, Color color) {
+        Square square = rank.get(column);
+
+        if (square.isSameType(PieceType.PAWN) && square.isSameColor(color)) {
+            numberOfPawnInColumn.put(column,
+                    numberOfPawnInColumn.getOrDefault(column, 0) + 1);
+        }
+    }
+
+    public boolean isCheckMate() {
+        return chessBoard.stream()
+                .flatMap(rank -> rank.getRank().stream())
+                .filter(square -> square.isSameType(PieceType.KING))
+                .count() != 2;
     }
 
 }
--- src/test/java/domain/chessboard/ChessBoardTest.java
+++ src/test/java/domain/chessboard/ChessBoardTest.java
@@ -1,7 +1,8 @@
 package domain.chessboard;
 
-import domain.chessboard.ChessBoard;
-import domain.chessboard.Rank;
+import domain.coordinate.PositionFactory;
+import domain.piece.Color;
+import domain.piece.Pawn;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +23,117 @@ public class ChessBoardTest {
                 .sum();
 
         assertThat(sum).isEqualTo(64);
+    }
+
+    /*
+    RNBQKBNR
+    PPPPPPPP
+    ........
+    ........
+    ........
+    ........
+    pppppppp
+    rnbqkbnr
+    */
+    @Test
+    @DisplayName("초기 상태에는 양쪽 모두 38점이다.")
+    void scoreIsThirtyEight() {
+        // Given
+        ChessBoard chessBoard = ChessBoard.generate();
+
+        // When
+        double blackScore = chessBoard.calculateColorScore(Color.BLACK);
+        double whiteScore = chessBoard.calculateColorScore(Color.WHITE);
+
+        // Then
+        assertThat(blackScore).isEqualTo(38);
+        assertThat(whiteScore).isEqualTo(38);
+    }
+
+    /*
+    RNBQKBNR
+    PPPPPPPP
+    ........
+    ........
+    p.......
+    p.......
+    pppppppp
+    rnbqkbnr
+     */
+    @Test
+    @DisplayName("pawn이 세로로 위치해있으면 0.5점으로 판별된다, 즉, 39점이다. (WHITE)")
+    void scoreIsThirtyNine() {
+        // Given
+        ChessBoard chessBoard = ChessBoard.generate();
+        chessBoard.findSquare(PositionFactory.createPosition("a3"))
+                .bePiece(new Square(new Pawn(Color.WHITE)));
+        chessBoard.findSquare(PositionFactory.createPosition("a4"))
+                .bePiece(new Square(new Pawn(Color.WHITE)));
+
+        // When&Then
+        assertThat(chessBoard.calculateColorScore(Color.WHITE)).isEqualTo(38.5);
+    }
+
+    /*
+    RNBQKBNR
+    PPPPPPPP
+    ........
+    ........
+    ........
+    ........
+    pppppppp
+    rnbqkbnr
+    */
+    @Test
+    @DisplayName("둘다 King 이 있는 경우")
+    void allExistKing() {
+        // Given
+        ChessBoard chessBoard = ChessBoard.generate();
+
+        // When&Then
+        assertThat(chessBoard.isCheckMate()).isFalse();
+    }
+
+    /*
+    RNBQ.BNR
+    PPPPPPPP
+    ........
+    ........
+    ........
+    ........
+    pppppppp
+    rnbqkbnr
+    */
+    @Test
+    @DisplayName("black 이 king 이 없는 경우")
+    void blackNotExistKing() {
+        // Given
+        ChessBoard chessBoard = ChessBoard.generate();
+        chessBoard.findSquare(PositionFactory.createPosition("e8")).beEmpty();
+
+        // When&Then
+        assertThat(chessBoard.isCheckMate()).isTrue();
+    }
+
+    /*
+    RNBQKBNR
+    PPPPPPPP
+    ........
+    ........
+    ........
+    ........
+    pppppppp
+    rnbq.bnr
+    */
+    @Test
+    @DisplayName("White 가 king 이 없는 경우")
+    void whiteNotExistKing() {
+        // Given
+        ChessBoard chessBoard = ChessBoard.generate();
+        chessBoard.findSquare(PositionFactory.createPosition("e1")).beEmpty();
+
+        // When&Then
+        assertThat(chessBoard.isCheckMate()).isTrue();
     }
 
 }
