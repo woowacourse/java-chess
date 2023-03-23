ad78aba670d8ab2983abdfbad3eacd0e3c66fe43
apptie <appti@naver.com>
2023-03-23 11:26:24 +0900

    feat: Controller - Domain에서 사용할 PlayDto 구현

---
+++ src/main/java/chess/model/dto/PlayDto.java
@@ -0,0 +1,29 @@
+package chess.model.dto;
+
+import chess.model.game.GameCommand;
+import chess.model.position.Position;
+
+public class PlayDto {
+
+    private final GameCommand gameCommand;
+    private final Position source;
+    private final Position target;
+
+    public PlayDto(final GameCommand gameCommand, final Position source, final Position target) {
+        this.gameCommand = gameCommand;
+        this.source = source;
+        this.target = target;
+    }
+
+    public GameCommand getGameCommand() {
+        return gameCommand;
+    }
+
+    public Position getSource() {
+        return source;
+    }
+
+    public Position getTarget() {
+        return target;
+    }
+}
