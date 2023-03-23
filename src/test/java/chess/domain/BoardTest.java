206caccf6e84c454e636ae906f5dce5c4564bb9c
hectick <chyeon1223@cau.ac.kr>
2023-03-23 16:03:04 +0900

    test(Board): 킹이 죽었는지 확인하는 테스트 작성

--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -1,10 +1,12 @@
 package chess.domain;
 
+import chess.domain.piece.Team;
 import chess.domain.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -57,5 +59,61 @@ class BoardTest {
 
             assertDoesNotThrow(() -> board.movePiece(source, target));
         }
+    }
+
+    @Nested
+    @DisplayName("특정 팀의 왕이 죽었는지 판단하는 isKingDead 메서드 테스트")
+    class isKingDeadTest {
+
+        @Test
+        @DisplayName("백 팀의 왕이 죽지 않았다면 false 를 반환한다.")
+        void isKingDeadTest1() {
+            Board board = BoardGenerator.createBoard();
+            boolean isWhiteKingDead = board.isKingDead(Team.WHITE);
+
+            assertThat(isWhiteKingDead).isFalse();
+        }
+
+        @Test
+        @DisplayName("흑 팀의 왕이 죽지 않았다면 false 를 반환한다.")
+        void isKingDeadTest2() {
+            Board board = BoardGenerator.createBoard();
+            boolean isBlackKingDead = board.isKingDead(Team.BLACK);
+
+            assertThat(isBlackKingDead).isFalse();
+        }
+
+        @Test
+        @DisplayName("백 팀의 왕이 죽은 경우 true 를 반환한다.")
+        void isKingDeadTest3() {
+            Board board = BoardGenerator.createBoard();
+
+            board.movePiece(new Position(4, 6), new Position(4, 4));
+            board.movePiece(new Position(4, 1), new Position(4, 3));
+            board.movePiece(new Position(4, 4), new Position(4, 3));
+            board.movePiece(new Position(4, 3), new Position(4, 2));
+            board.movePiece(new Position(4, 2), new Position(4, 1));
+            board.movePiece(new Position(4, 1), new Position(4, 0));
+            boolean isWhiteKingDead = board.isKingDead(Team.WHITE);
+
+            assertThat(isWhiteKingDead).isTrue();
+        }
+
+        @Test
+        @DisplayName("흑 팀의 왕이 죽은 경우 true 를 반환한다.")
+        void isKingDeadTest4() {
+            Board board = BoardGenerator.createBoard();
+
+            board.movePiece(new Position(4, 1), new Position(4, 3));
+            board.movePiece(new Position(4, 6), new Position(4, 4));
+            board.movePiece(new Position(4, 3), new Position(4, 4));
+            board.movePiece(new Position(4, 4), new Position(4, 5));
+            board.movePiece(new Position(4, 5), new Position(4, 6));
+            board.movePiece(new Position(4, 6), new Position(4, 7));
+            boolean isBlackKingDead = board.isKingDead(Team.BLACK);
+
+            assertThat(isBlackKingDead).isTrue();
+        }
+
     }
 }
