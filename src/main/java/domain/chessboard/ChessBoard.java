c53bffc3cb514849c79cb2e4ed1cf2edd976e247
jaeyeon kim <jakind@naver.com>
2023-03-23 20:18:21 +0900

    refactor : 매직 넘버 상수로 치환 및 ChessBoard 메서드 분리

--- src/main/java/domain/chessboard/ChessBoard.java
+++ src/main/java/domain/chessboard/ChessBoard.java
@@ -12,6 +12,10 @@ import java.util.Map;
 public class ChessBoard {
 
     private static final int NUMBER_OF_NONE_LINES = 4;
+    private static final int NUMBER_OF_STANDARD_FOR_DECREASING_PAWN_SCORE = 2;
+    private static final double PAWN_SCORE_IS_ON_SAME_COLUMN = 0.5;
+    private static final int STANDARD_NUMBER_OF_CHECKMATE = 2; 
+    private static final int ADDITIONAL_COUNT_WHEN_FOUND_PAWN = 1;
 
     private final List<Rank> chessBoard;
 
@@ -43,49 +47,60 @@ public class ChessBoard {
     }
 
     public double calculateColorScore(Color color) {
-        List<Rank> chessBoard1 = chessBoard;
-        double sum = chessBoard1.stream()
+        double sum = chessBoard.stream()
                 .flatMap(rank -> rank.getRank().stream())
                 .filter(square -> square.isNotSameType(EmptyType.EMPTY) && square.isSameColor(color))
                 .mapToDouble(square -> square.getSquareStatus().getScore())
                 .sum();
 
-        return sum - diffScore(chessBoard1, color);
-    }
-
-    private double diffScore(List<Rank> chessBoard1, Color color) {
-        Map<Integer, Integer> numberOfPawnInColumn = new HashMap<>();
-        for (Rank value : chessBoard1) {
-            List<Square> rank = value.getRank();
-            forEachColumn(numberOfPawnInColumn, rank, color);
-        }
-        return numberOfPawnInColumn.values()
-                .stream()
-                .filter(i -> 2 <= i)
-                .mapToDouble(i -> i * 0.5)
-                .sum();
-    }
-
-    private void forEachColumn(Map<Integer, Integer> numberOfPawnInColumn, List<Square> rank, Color color) {
-        for (int column = 0; column < rank.size(); column++) {
-            putIfExistPawn(numberOfPawnInColumn, rank, column, color);
-        }
-    }
-
-    private void putIfExistPawn(Map<Integer, Integer> numberOfPawnInColumn, List<Square> rank, int column, Color color) {
-        Square square = rank.get(column);
-
-        if (square.isSameType(PieceType.PAWN) && square.isSameColor(color)) {
-            numberOfPawnInColumn.put(column,
-                    numberOfPawnInColumn.getOrDefault(column, 0) + 1);
-        }
+        return sum - diffScore(color);
     }
 
     public boolean isCheckMate() {
         return chessBoard.stream()
                 .flatMap(rank -> rank.getRank().stream())
                 .filter(square -> square.isSameType(PieceType.KING))
-                .count() != 2;
+                .count() != STANDARD_NUMBER_OF_CHECKMATE;
+    }
+
+    private double diffScore(Color color) {
+        Map<Integer, Integer> numberOfPawnInColumn = new HashMap<>();
+        for (Rank value : chessBoard) {
+            List<Square> rank = value.getRank();
+            forEachColumn(numberOfPawnInColumn, rank, color);
+        }
+        return numberOfPawnInColumn.values()
+                .stream()
+                .filter(this::isNotOnlyInColumn)
+                .mapToDouble(this::convertHalfScore)
+                .sum();
+    }
+
+    private void forEachColumn(Map<Integer, Integer> numberOfPawnInColumn, List<Square> rank, Color color) {
+        for (int column = 0; column < rank.size(); column++) {
+            Square square = rank.get(column);
+            boolean isPawnOfThisColor = isPawnOfThisColor(square, color);
+            putIfExistPawn(numberOfPawnInColumn, column, isPawnOfThisColor);
+        }
+    }
+
+    private boolean isPawnOfThisColor(Square square, Color color) {
+        return square.isSameType(PieceType.PAWN) && square.isSameColor(color);
+    }
+
+    private void putIfExistPawn(Map<Integer, Integer> numberOfPawnInColumn, int column, boolean isPawn) {
+        if (isPawn) {
+            numberOfPawnInColumn.put(column
+                    , numberOfPawnInColumn.getOrDefault(column, 0) + ADDITIONAL_COUNT_WHEN_FOUND_PAWN);
+        }
+    }
+
+    private boolean isNotOnlyInColumn(Integer numberOfPawn) {
+        return NUMBER_OF_STANDARD_FOR_DECREASING_PAWN_SCORE <= numberOfPawn;
+    }
+
+    private double convertHalfScore(Integer numberOfPawn) {
+        return numberOfPawn * PAWN_SCORE_IS_ON_SAME_COLUMN;
     }
 
 }
