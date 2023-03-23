4ffb43f620e7de7a50650f5bb34b52d7c9394ac0
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-03-23 16:49:06 +0900

    Refactor: Shape에서 Name 분리

--- src/main/java/chess/domain/dto/response/PieceResponse.java
+++ src/main/java/chess/domain/dto/response/PieceResponse.java
@@ -2,6 +2,7 @@ package chess.domain.dto.response;
 
 import chess.domain.Color;
 import chess.domain.Piece;
+import chess.domain.Shape;
 
 public class PieceResponse {
 
@@ -12,7 +13,15 @@ public class PieceResponse {
     public PieceResponse(final Piece piece, final Color color) {
         this.rank = piece.getRank();
         this.file = piece.getFile();
-        this.name = piece.getName(color);
+        this.name = namingByColor(piece, color);
+    }
+
+    private char namingByColor(Piece piece, Color color) {
+        Shape shape = piece.getShape();
+        if (color == Color.WHITE) {
+            return shape.name().toLowerCase().charAt(0);
+        }
+        return shape.name().charAt(0);
     }
 
     public boolean samePosition(final int rank, final int file) {
