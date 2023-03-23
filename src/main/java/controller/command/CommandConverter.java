fb1ff519277636b5a132cf1d7961ca58efb3c927
aiaiaiai1 <cvc100326@naver.com>
2023-03-23 15:36:53 +0900

    feat: 입력값을 통해 검증하고 CommandType 가져오는 클래스 추가

---
+++ src/main/java/controller/command/CommandConverter.java
@@ -0,0 +1,52 @@
+package controller.command;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class CommandConverter {
+    public static final int MOVE_COMMAND_SOURCE_INDEX = 1;
+    public static final int MOVE_COMMAND_DESTINATION_INDEX = 2;
+
+    private static final int COMMAND_TYPE_INDEX = 0;
+    private static final int COMMAND_FORM_MAX = 3;
+    private static final int COMMAND_FORM_MIN = 1;
+    private final CommandType commandType;
+
+    private final Map<CommandType, Consumer<List<String>>> commandValidater = Map.of(
+            CommandType.START, this::validateStartOrEndCommands,
+            CommandType.MOVE, this::validateMoveCommand,
+            CommandType.END, this::validateStartOrEndCommands
+    );
+
+    public CommandConverter(List<String> inputs) {
+        validateCommand(inputs);
+        CommandType commandType = CommandType.from(inputs.get(COMMAND_TYPE_INDEX));
+        commandValidater.get(commandType).accept(inputs);
+        this.commandType = commandType;
+    }
+
+    private void validateCommand(List<String> inputs) {
+        validateCommandForm(inputs, s -> s.size() > COMMAND_FORM_MAX);
+    }
+
+    private void validateCommandForm(List<String> inputs, Predicate<List<String>> predicate) {
+        if (predicate.test(inputs)) {
+            throw new IllegalArgumentException("잘못된 형식입니다.");
+        }
+    }
+
+    private void validateMoveCommand(List<String> inputs) {
+        validateCommandForm(inputs, s -> s.size() != COMMAND_FORM_MAX);
+    }
+
+    private void validateStartOrEndCommands(List<String> inputs) {
+        validateCommandForm(inputs, s -> s.size() != COMMAND_FORM_MIN);
+    }
+
+    public CommandType getCommandType() {
+        return commandType;
+    }
+
+}
