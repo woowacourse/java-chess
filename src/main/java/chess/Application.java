776a19f27b962531d304ab5a5ef45259f440d35d
chaewon121 <cw121@naver.com>
2023-03-23 14:22:11 +0900

    refactor: 어플리케이션 실행 구현

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,11 +1,10 @@
 package chess;
 
-import chess.domain.ChessGame;
 import chess.controller.MainController;
 
 public class Application {
     public static void main(String[] args) {
-        MainController mainController = new MainController(new ChessGame());
+        MainController mainController = new MainController();
         mainController.run();
     }
 }
