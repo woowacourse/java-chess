b9f386e8ff31f9ac074985c5e02bd90af2ef885c
parkmuhyeun <pjhg410@gmail.com>
2023-03-23 11:59:37 +0900

    test: Empty findMovablePositions 호출 테스트

--- src/test/java/chess/domain/EmptyTest.java
+++ src/test/java/chess/domain/EmptyTest.java
@@ -2,9 +2,14 @@ package chess.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import chess.domain.board.Board;
+import chess.domain.board.BoardFactory;
 import chess.domain.piece.Empty;
 import chess.domain.piece.Side;
 import chess.domain.piece.Type;
+import chess.domain.position.File;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -25,5 +30,16 @@ public class EmptyTest {
         assertThatThrownBy(() -> new Empty(Type.EMPTY, Side.WHITE))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Empty는 진영을 가질 수 없습니다.");
+    }
+
+    @Test
+    void findMovablePositions을_호출하면_예외를_던진다() {
+        final Empty empty = new Empty(Type.EMPTY, Side.NEUTRALITY);
+        final Position position = Position.of(File.getFile(3), Rank.getRank(3));
+        final Board board = BoardFactory.generateBoard();
+
+        assertThatThrownBy(() -> empty.findMovablePositions(position, board))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("지원하지 않는 메서드입니다.");
     }
 }
