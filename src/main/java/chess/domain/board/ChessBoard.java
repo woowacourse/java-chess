ea09cced45fae9116981564d1fef153f688f2fe9
zillionme <beatus96@naver.com>
2023-03-23 10:42:13 +0900

    refactor: Turn에 대한 책임을 ChessGame으로 이동한다.

--- src/main/java/chess/domain/board/ChessBoard.java
+++ src/main/java/chess/domain/board/ChessBoard.java
@@ -13,7 +13,6 @@ public class ChessBoard {
     private static final String OTHER_PIECE_IN_ROUTE_ERROR_GUIDE_MESSAGE = "이동 경로에 기물이 있으므로 이동할 수 없습니다";
 
     private final Map<Position, Piece> chessBoard;
-    private Color turn = Color.WHITE;
 
     public ChessBoard() {
         this.chessBoard = new LinkedHashMap<>();
@@ -23,14 +22,24 @@ public class ChessBoard {
         this.chessBoard.putAll(chessBoard);
     }
 
+    public Color findColorOfPieceInPosition(final Position position) {
+        Piece pieceInPosition = findPieceInBoardByPosition(position);
+        return pieceInPosition.getColor();
+    }
+
     public void move(Position start, Position end) {
         checkIfMoveToSamePosition(start, end);
         Piece pieceToMove = findPieceInBoardByPosition(start);
-        checkTurn(pieceToMove.getColor());
         checkIfPiecesExistInRoute(start, end);
         Color colorOfDestination = findPieceInBoardByPosition(end).getColor();
         if (pieceToMove.isMovable(start, end, colorOfDestination)) {
             movePieceToDestination(start, end, pieceToMove);
+        }
+    }
+
+    private static void checkIfMoveToSamePosition(Position start, Position end) {
+        if(start.equals(end)) {
+            throw new IllegalArgumentException("제자리로는 이동할 수 없습니다");
         }
     }
 
@@ -41,19 +50,9 @@ public class ChessBoard {
         return chessBoard.get(position);
     }
 
-    private static void checkIfMoveToSamePosition(Position start, Position end) {
-        if(start.equals(end)) {
-            throw new IllegalArgumentException("제자리로는 이동할 수 없습니다");
-        }
-    }
-
-
-    private void checkTurn(Color colorOfStartPiece) {
-        if(colorOfStartPiece.isOpponent(turn)) {
-            throw new IllegalArgumentException("상대편의 기물을 움직일 수 없습니다");
-        }
-    }
-
+    /**
+     * 시작 포지션 도착 포지션으로 방향을 결정하고, 루트LIst<Postion>를 만들어서 반환하기 이동 거리
+     */
     private void checkIfPiecesExistInRoute(Position start, Position end) {
         Direction direction = Direction.findDirectionByGap(start, end);
         Position currentPosition = start;
@@ -73,7 +72,6 @@ public class ChessBoard {
     private void movePieceToDestination(Position start, Position end, Piece piece) {
         chessBoard.replace(start, EmptyPiece.of());
         chessBoard.replace(end, piece);
-        turn = piece.getColor().getOpponent();
     }
 
     public boolean isInitialized() {
@@ -83,4 +81,5 @@ public class ChessBoard {
     public Map<Position, Piece> getChessBoard() {
         return new HashMap<>(chessBoard);
     }
+
 }
--- src/main/java/chess/domain/game/ChessGame.java
+++ src/main/java/chess/domain/game/ChessGame.java
@@ -3,12 +3,14 @@ package chess.domain.game;
 import chess.boardstrategy.BoardStrategy;
 import chess.domain.board.ChessBoard;
 import chess.domain.board.Position;
+import chess.domain.piece.Color;
 import chess.domain.piece.type.Piece;
 
 import java.util.Map;
 
 public class ChessGame {
     private final ChessBoard chessBoard;
+    private Color turn = Color.WHITE;
 
     public ChessGame() {
         this.chessBoard = new ChessBoard();
@@ -25,7 +27,16 @@ public class ChessGame {
         if(!isStarted()) {
             throw new IllegalArgumentException("게임이 시작되지 않았습니다");
         }
+        Color colorOfStartPiece = chessBoard.findColorOfPieceInPosition(start);
+        checkCorrectTurnByColor(colorOfStartPiece);
         chessBoard.move(start, end);
+        turn = colorOfStartPiece.getOpponent();
+    }
+
+    private void checkCorrectTurnByColor(Color colorOfStartPiece) {
+        if(colorOfStartPiece.isOpponent(turn)) {
+            throw new IllegalArgumentException("상대편의 기물을 움직일 수 없습니다");
+        }
     }
 
     private boolean isStarted() {
