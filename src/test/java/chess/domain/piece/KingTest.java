747364a55ed4eea9b4f345da591fdb0857304f61
eunbii0213 <eunbii0213@naver.com>
2023-03-23 13:32:08 +0900

    test: KingTest 구현

--- src/test/java/chess/domain/piece/KingTest.java
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -7,43 +7,56 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-class BishopTest {
+class KingTest {
 
     @Test
     void isMovable_메서드로_checkMovableToDestination_에러발생_테스트() {
         //given, when
-        Bishop bishop = new Bishop(Color.WHITE);
+        King king = new King(Color.WHITE);
         Position start = new Position("b", "2");
         Position end = new Position("a", "3");
-        Color destinationColor = Color.WHITE;//when
+        Color destinationColor = Color.WHITE;
 
         //then
-        assertThatThrownBy(() -> bishop.isMovable(start, end, destinationColor))
+        assertThatThrownBy(() -> king.isMovable(start, end, destinationColor))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void isMovable_메서드로_checkDirection_에러발생_테스트() {
         //given, when
-        Bishop bishop = new Bishop(Color.WHITE);
+        King king = new King(Color.WHITE);
         Position start = new Position("b", "2");
-        Position end = new Position("b", "3");
+        Position end = new Position("a", "5");
         Color destinationColor = Color.BLACK;
 
         //then
-        assertThatThrownBy(() -> bishop.isMovable(start, end, destinationColor))
+        assertThatThrownBy(() -> king.isMovable(start, end, destinationColor))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isMovable_메서드로_checkDistance_에러발생_테스트() {
+        //given, when
+        King king = new King(Color.WHITE);
+        Position start = new Position("b", "2");
+        Position end = new Position("b", "4");
+        Color destinationColor = Color.BLACK;
+
+        //then
+        assertThatThrownBy(() -> king.isMovable(start, end, destinationColor))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void isMovable_올바른_반환값_테스트() {
         //given, when
-        Bishop bishop = new Bishop(Color.WHITE);
+        King king = new King(Color.WHITE);
         Position start = new Position("b", "2");
         Position end = new Position("a", "3");
         Color destinationColor = Color.BLACK;
 
         //then
-        assertDoesNotThrow(() -> bishop.isMovable(start, end, destinationColor));
+        assertDoesNotThrow(() -> king.isMovable(start, end, destinationColor));
     }
 }
