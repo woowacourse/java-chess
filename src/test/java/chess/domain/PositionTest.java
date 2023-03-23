390cd31e158fba3abf51c27f5661b0d219865947
parkmuhyeun <pjhg410@gmail.com>
2023-03-23 11:50:58 +0900

    test: 패턴에 맞게 움직일 수 있는지 테스트

--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -1,10 +1,25 @@
 package chess.domain;
 
+import chess.domain.movepattern.RookMovePattern;
+import chess.domain.position.File;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PositionTest {
 
+    @Test
+    void 패턴에_맞게_움직일_수_있다() {
+        final Position target = Position.of(File.getFile(1), Rank.getRank(1));
+        final Position expect = Position.of(File.getFile(1), Rank.getRank(2));
+
+        final Position movedPosition = target.move(RookMovePattern.UP);
+
+        Assertions.assertThat(movedPosition).isEqualTo(expect);
+    }
 }
