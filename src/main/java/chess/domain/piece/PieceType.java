2910b93e1df1a15be4ad7e5a2fd57fcacb69b9a0
hanueleee <ezzeezz@naver.com>
2023-03-23 15:14:13 +0900

    feat: 각 피스별 점수 매핑

--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -1,11 +1,21 @@
 package chess.domain.piece;
 
 public enum PieceType {
-    PAWN,
-    ROOK,
-    KNIGHT,
-    BISHOP,
-    QUEEN,
-    EMPTY,
-    KING
+    PAWN(1),
+    ROOK(5),
+    KNIGHT(2.5),
+    BISHOP(3),
+    QUEEN(9),
+    EMPTY(0),
+    KING(0);
+
+    private final double point;
+
+    PieceType(double point) {
+        this.point = point;
+    }
+
+    public double getPoint() {
+        return point;
+    }
 }
