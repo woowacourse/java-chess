64b72d6ff9c046390a594c6613486203bca48bcb
amaran-th <songsy405@gmail.com>
2023-03-23 14:49:54 +0900

    feat(Score) : 테스트 코드에 자료형 변경 적용

--- src/test/java/chess/domain/ScoreTest.java
+++ src/test/java/chess/domain/ScoreTest.java
@@ -9,14 +9,14 @@ class ScoreTest {
     @Test
     void plus_메서드를_호출하면_두_Score의_합한_Score를_반환한다() {
         //given
-        Score score1 = new Score(2);
-        Score score2 = new Score(3);
+        Score score1 = new Score(2.0);
+        Score score2 = new Score(3.0);
 
         //when
         Score actual = score1.add(score2);
 
         //then
-        assertThat(actual).isEqualTo(new Score(5));
+        assertThat(actual).isEqualTo(new Score(5.0));
     }
 
 }
