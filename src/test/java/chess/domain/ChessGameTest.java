50c37a177852be718e1ffbd994e49a0523d9ff27
hectick <chyeon1223@cau.ac.kr>
2023-03-23 16:25:10 +0900

    test(ChessGame): 게임이 끝났는지 판단하는 테스트 작성

---
+++ src/test/java/chess/domain/ChessGameTest.java
@@ -0,0 +1,58 @@
+package chess.domain;
+
+import chess.domain.piece.Team;
+import chess.domain.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ChessGameTest {
+
+    @Nested
+    @DisplayName("게임이 끝났는지 판단하는 isGameEnd 메서드 테스트")
+    class isGameEndTest {
+
+        @Test
+        @DisplayName("두 팀의 왕이 모두 죽지 않았으면 false 를 반환한다.")
+        void isKingDeadTest1() {
+            Board board = BoardGenerator.createBoard();
+            boolean isGameEnd = board.isKingDead(Team.WHITE);
+
+            assertThat(isGameEnd).isFalse();
+        }
+
+        @Test
+        @DisplayName("백 팀의 왕이 죽었으면 true 를 반환한다.")
+        void isGameEndTest2() {
+            ChessGame game = ChessGame.create();
+
+            game.movePiece(new Position(4, 6), new Position(4, 4));
+            game.movePiece(new Position(4, 1), new Position(4, 3));
+            game.movePiece(new Position(4, 4), new Position(4, 3));
+            game.movePiece(new Position(4, 3), new Position(4, 2));
+            game.movePiece(new Position(4, 2), new Position(4, 1));
+            game.movePiece(new Position(4, 1), new Position(4, 0));
+            boolean isGameEnd = game.isGameEnd();
+
+            assertThat(isGameEnd).isTrue();
+        }
+
+        @Test
+        @DisplayName("흑 팀의 왕이 죽었으면 true 를 반환한다.")
+        void isGameEndTest3() {
+            ChessGame game = ChessGame.create();
+
+            game.movePiece(new Position(4, 1), new Position(4, 3));
+            game.movePiece(new Position(4, 6), new Position(4, 4));
+            game.movePiece(new Position(4, 3), new Position(4, 4));
+            game.movePiece(new Position(4, 4), new Position(4, 5));
+            game.movePiece(new Position(4, 5), new Position(4, 6));
+            game.movePiece(new Position(4, 6), new Position(4, 7));
+            boolean isGameEnd = game.isGameEnd();
+
+            assertThat(isGameEnd).isTrue();
+        }
+    }
+}
