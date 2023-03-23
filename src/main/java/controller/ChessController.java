5e53905f18f526e41d277844113fafc628b460c8
Youngho <cyh6099@gmail.com>
2023-03-24 01:08:54 +0900

    feat: 점수 및 승자 출력 흐름 추가

--- src/main/java/controller/ChessController.java
+++ src/main/java/controller/ChessController.java
@@ -3,14 +3,24 @@ package controller;
 import domain.game.Board;
 import domain.game.ChessBoardGenerator;
 import domain.game.ChessGame;
+import domain.piece.Position;
+import domain.piece.Side;
 import view.InputView;
 import view.OutputView;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class ChessController {
 
     private static final String GAME_EXIT_COMMAND = "end";
+    private static final String POSITION_DELIMITER = "";
+    private static final int SOURCE_FILE_INDEX = 0;
+    private static final int SOURCE_RANK_INDEX = 1;
+    private static final int TARGET_FILE_INDEX = 2;
+    private static final int TARGET_RANK_INDEX = 3;
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
@@ -22,27 +32,38 @@ public class ChessController {
         ChessGame chessGame = new ChessGame(chessBoard);
 
         this.outputView.printChessBoard(chessBoard.getChessBoard());
-        while (move(chessGame)) {
+        while (play(chessGame)) {
             outputView.printChessBoard(chessBoard.getChessBoard());
         }
 
     }
 
-    private boolean move(ChessGame chessGame) {
+    private boolean play(ChessGame chessGame) {
         String userCommand = repeatBySupplier(() -> inputView.requestUserCommandInGame());
-        if (isExitGame(userCommand)) {
+        if (isExitCommand(userCommand) || chessGame.isExitGame()) {
             return false;
         }
+        if (userCommand.equals("status")) {
+            Map<Side, Double> scores = chessGame.calculateScores();
+            outputView.printGameScores(scores);
+            outputView.printWinner(chessGame.calculateWinner());
+            return true;
+        }
         try {
-            chessGame.move(userCommand);
+            List<String> inputs = Arrays.asList(userCommand.split(POSITION_DELIMITER));
+
+            Position sourcePosition = Position.of(inputs.get(SOURCE_FILE_INDEX), inputs.get(SOURCE_RANK_INDEX));
+            Position targetPosition = Position.of(inputs.get(TARGET_FILE_INDEX), inputs.get(TARGET_RANK_INDEX));
+
+            chessGame.move(sourcePosition, targetPosition);
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
-            move(chessGame);
+            play(chessGame);
         }
         return true;
     }
 
-    private boolean isExitGame(String command) {
+    private boolean isExitCommand(String command) {
         return command.equals(GAME_EXIT_COMMAND);
     }
 
