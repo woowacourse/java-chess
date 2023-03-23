7a44cba5fede35c374bbd55efe632bf2b5b36698
Woojin Lee <dhkep03@gmail.com>
2023-03-23 14:53:32 +0900

    refactor(PieceCategory): 위임한 view의 정보 관련 필드 및 메서드 제거

--- src/main/java/domain/piece/PieceCategory.java
+++ src/main/java/domain/piece/PieceCategory.java
@@ -1,28 +1,17 @@
 package domain.piece;
 
 public enum PieceCategory {
-    WHITE_PAWN("p"),
-    WHITE_ROOK("r"),
-    WHITE_KNIGHT("n"),
-    WHITE_BISHOP("b"),
-    WHITE_QUEEN("q"),
-    WHITE_KING("k"),
-    BLACK_PAWN("P"),
-    BLACK_ROOK("R"),
-    BLACK_KNIGHT("N"),
-    BLACK_BISHOP("B"),
-    BLACK_QUEEN("Q"),
-    BLACK_KING("K"),
-    EMPTY_PIECE(".");
-
-
-    private final String pieceValue;
-
-    PieceCategory(String pieceValue) {
-        this.pieceValue = pieceValue;
-    }
-
-    public String getPieceValue() {
-        return pieceValue;
-    }
+    WHITE_PAWN,
+    WHITE_ROOK,
+    WHITE_KNIGHT,
+    WHITE_BISHOP,
+    WHITE_QUEEN,
+    WHITE_KING,
+    BLACK_PAWN,
+    BLACK_ROOK,
+    BLACK_KNIGHT,
+    BLACK_BISHOP,
+    BLACK_QUEEN,
+    BLACK_KING,
+    EMPTY_PIECE;
 }
