d24bbf3f91d9e639395b3880734aa5e5c7bd3946
eunbii0213 <eunbii0213@naver.com>
2023-03-23 17:07:23 +0900

    test: ScoreCalculatorTest 구현

--- src/test/java/chess/domain/score/ScoreCalculatorTest.java
+++ src/test/java/chess/domain/score/ScoreCalculatorTest.java
@@ -2,19 +2,17 @@ package chess.domain.score;
 
 import chess.domain.Position;
 import chess.domain.boardStrategy.InitialBoardStrategy;
-import chess.domain.game.ChessBoard;
 import chess.domain.game.ChessGame;
 import chess.domain.piece.Piece;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ScoreCalculatorTest {
 
     @Test
-    void calculateScores() {
+    void 초기_점수_테스트() {
         //given
         ChessGame chessGame = new ChessGame(new InitialBoardStrategy());
         Map<Position, Piece> chessBoard = chessGame.getChessBoard();
@@ -22,18 +20,12 @@ class ScoreCalculatorTest {
 
         //when
         scoreCalculator.calculateScores(chessBoard);
+        double expectedScore = 38.0;
         double blackScore = scoreCalculator.getBlackScore();
         double whiteScore = scoreCalculator.getWhiteScore();
 
         //then
-    }
-
-    @Test
-    void checkPawnScore() {
-        //given
-        ScoreCalculator scoreCalculator = new ScoreCalculator();
-        //when
-
-        //then
+        Assertions.assertEquals(expectedScore, blackScore);
+        Assertions.assertEquals(expectedScore, whiteScore);
     }
 }
