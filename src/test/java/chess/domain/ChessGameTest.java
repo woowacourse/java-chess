caf6e969e7b908e090ac8ce357200d78cdd29835
hectick <chyeon1223@cau.ac.kr>
2023-03-23 17:18:57 +0900

    test(ChessGame): 이긴 팀을 확인하는 테스트 작성

--- src/test/java/chess/domain/ChessGameTest.java
+++ src/test/java/chess/domain/ChessGameTest.java
@@ -69,4 +69,77 @@ class ChessGameTest {
             assertThat(score).isEqualTo(38.0);
         }
     }
+
+    @Nested
+    @DisplayName("이긴 팀을 반환하는 getWinTeam 메서드 테스트")
+    class getWinTeamTest {
+
+        @Test
+        @DisplayName("흑 팀 왕이 죽었을 때 백 팀을 반환한다.")
+        void getWinTeamTest1() {
+            ChessGame game = ChessGame.create();
+
+            game.movePiece(new Position(4, 1), new Position(4, 3));
+            game.movePiece(new Position(4, 6), new Position(4, 4));
+            game.movePiece(new Position(4, 3), new Position(4, 4));
+            game.movePiece(new Position(4, 4), new Position(4, 5));
+            game.movePiece(new Position(4, 5), new Position(4, 6));
+            game.movePiece(new Position(4, 6), new Position(4, 7));
+            Team winTeam = game.getWinTeam();
+
+            assertThat(winTeam).isEqualTo(Team.WHITE);
+        }
+
+        @Test
+        @DisplayName("백 팀 왕이 죽었을 때 흑 팀을 반환한다.")
+        void getWinTeamTest2() {
+            ChessGame game = ChessGame.create();
+
+            game.movePiece(new Position(4, 6), new Position(4, 4));
+            game.movePiece(new Position(4, 1), new Position(4, 3));
+            game.movePiece(new Position(4, 4), new Position(4, 3));
+            game.movePiece(new Position(4, 3), new Position(4, 2));
+            game.movePiece(new Position(4, 2), new Position(4, 1));
+            game.movePiece(new Position(4, 1), new Position(4, 0));
+            Team winTeam = game.getWinTeam();
+
+            assertThat(winTeam).isEqualTo(Team.BLACK);
+        }
+
+        @Test
+        @DisplayName("두 팀의 왕이 모두 살아있는 경우 점수가 백팀이 높다면 백 팀을 반환한다.")
+        void getWinTeamTest3() {
+            ChessGame game = ChessGame.create();
+
+            game.movePiece(new Position(4, 1), new Position(4, 3));
+            game.movePiece(new Position(4, 6), new Position(4, 4));
+            game.movePiece(new Position(4, 3), new Position(4, 4));
+            Team winTeam = game.getWinTeam();
+
+            assertThat(winTeam).isEqualTo(Team.WHITE);
+        }
+
+        @Test
+        @DisplayName("두 팀의 왕이 모두 살아있는 경우 점수가 흑 팀이 높다면 흑 팀을 반환한다.")
+        void getWinTeamTest5() {
+            ChessGame game = ChessGame.create();
+
+            game.movePiece(new Position(4, 6), new Position(4, 4));
+            game.movePiece(new Position(4, 1), new Position(4, 3));
+            game.movePiece(new Position(4, 4), new Position(4, 3));
+            Team winTeam = game.getWinTeam();
+
+            assertThat(winTeam).isEqualTo(Team.BLACK);
+        }
+
+        @Test
+        @DisplayName("두 팀의 왕이 모두 살아있는 경우 두 팀의 점수가 같으면 무승부다.")
+        void getWinTeamTest6() {
+            ChessGame game = ChessGame.create();
+
+            Team winTeam = game.getWinTeam();
+
+            assertThat(winTeam).isEqualTo(Team.EMPTY);
+        }
+    }
 }
