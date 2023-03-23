580dbb1c9befddc4dbf735bc343701e84b4ec110
dooboocookie <bkh3030@gmail.com>
2023-03-23 11:23:44 +0900

    test(Score): 더하는 테스트 추가

---
+++ src/test/java/chess/ScoreTest.java
@@ -0,0 +1,18 @@
+package chess;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ScoreTest {
+    @Test
+    @DisplayName("두 점수를 더한다.")
+    void sum() {
+        Score score1 = new Score(10);
+        Score score2 = new Score(0.5);
+
+        Score result = score1.sum(score2);
+
+        Assertions.assertThat(result).isEqualTo(new Score(10.5));
+    }
+}
