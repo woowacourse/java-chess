ecdf1cd374b217df703c3bb4f5152d7c80edfed6
sh111-coder <ohk9134@naver.com>
2023-03-23 19:22:19 +0900

    test(ScoreTest): 스코어 대소 비교 기능 테스트

--- src/test/java/chess/domain/board/ScoreTest.java
+++ src/test/java/chess/domain/board/ScoreTest.java
@@ -36,4 +36,37 @@ class ScoreTest {
         // then
         assertThat(subtractScore).isEqualTo(new Score(new BigDecimal("8.0")));
     }
+
+    @Test
+    @DisplayName("스코어를 비교하여 비교할 스코어보다 크면 true를 반환한다.")
+    void isGreaterScore() {
+        // given
+        Score score = new Score(new BigDecimal("10.0"));
+        Score scoreToCompare = new Score(new BigDecimal("8.0"));
+
+        // when, then
+        assertThat(score.isGreaterScore(scoreToCompare)).isTrue();
+    }
+
+    @Test
+    @DisplayName("스코어를 비교하여 비교할 스코어보다 작으면 true를 반환한다.")
+    void isLessScore() {
+        // given
+        Score score = new Score(new BigDecimal("8.0"));
+        Score scoreToCompare = new Score(new BigDecimal("10.0"));
+
+        // when, then
+        assertThat(score.isLessScore(scoreToCompare)).isTrue();
+    }
+
+    @Test
+    @DisplayName("스코어를 비교하여 비교할 스코어와 같으면 true를 반환한다.")
+    void isEqualScore() {
+        // given
+        Score score = new Score(new BigDecimal("8.0"));
+        Score scoreToCompare = new Score(new BigDecimal("8.0"));
+
+        // when, then
+        assertThat(score.isEqualScore(scoreToCompare)).isTrue();
+    }
 }
