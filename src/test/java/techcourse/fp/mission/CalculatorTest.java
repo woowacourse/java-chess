f7eb79ef7fffce899f158a51882e8b6c2fc7420e
jjongwa <troas96@naver.com>
2023-03-23 12:00:37 +0900

    step2 setting
    삭제된 미니미션 코드 다시 추가

---
+++ src/test/java/techcourse/fp/mission/CalculatorTest.java
@@ -0,0 +1,30 @@
+package techcourse.fp.mission;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    private final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+    @Test
+    public void sumAll() {
+        int sum = Calculator.sumAll(numbers);
+        assertThat(sum).isEqualTo(21);
+    }
+
+    @Test
+    public void sumAllEven() {
+        int sum = Calculator.sumAllEven(numbers);
+        assertThat(sum).isEqualTo(12);
+    }
+
+    @Test
+    public void sumAllOverThree() {
+        int sum = Calculator.sumAllOverThree(numbers);
+        assertThat(sum).isEqualTo(15);
+    }
+}
---
+++ src/test/java/techcourse/fp/mission/CarTest.java
@@ -0,0 +1,33 @@
+package techcourse.fp.mission;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+class CarTest {
+
+    @Test
+    public void 이동() {
+        Car car = new Car("pobi", 0);
+        Car actual = car.move(new MoveStrategy() {
+            @Override
+            public boolean isMovable() {
+                return true;
+            }
+        });
+        assertThat(actual).isEqualTo(new Car("pobi", 1));
+    }
+
+    @Test
+    public void 정지() {
+        Car car = new Car("pobi", 0);
+        Car actual = car.move(new MoveStrategy() {
+            @Override
+            public boolean isMovable() {
+                return false;
+            }
+        });
+        assertThat(actual).isEqualTo(new Car("pobi", 0));
+    }
+}
