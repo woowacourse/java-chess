e8c0e53d6be70b3e628a52bd8f9ff00360e39d81
chaewon121 <cw121@naver.com>
2023-03-23 13:52:49 +0900

    refactor: 명령 입력 관리 클래스 예외 처리

--- src/main/java/chess/view/Command.java
+++ src/main/java/chess/view/Command.java
@@ -1,22 +1,82 @@
 package chess.view;
 
+import chess.domain.Position;
+import chess.domain.board.File;
+import chess.domain.board.Rank;
+
 import java.util.Arrays;
+import java.util.List;
 
-public enum Command {
-    START("start"),
-    END("end"),
-    MOVE("move");
+import static java.util.stream.Collectors.toList;
 
-    private final String command;
+public class Command {
 
-    Command(final String command) {
-        this.command = command;
+    private final static String START = "start";
+    private final static String MOVE = "move";
+    private final static String END = "end";
+
+    private final List<String> command;
+
+    public Command(String input) {
+        List<String> inputString = Arrays.stream(input.split(" "))
+                .map(String::trim)
+                .collect(toList());
+
+        validateInput(inputString);
+        command = inputString;
     }
 
-    public static Command of(String inputCommand) {
-        return Arrays.stream(Command.values())
-                .filter(command -> command.command.equals(inputCommand))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 명령입니다."));
+    public Position getCurrentPosition(){
+        return changeStringToPosition(command.get(1));
+    }
+
+    public Position getTargetPosition(){
+        return changeStringToPosition(command.get(2));
+    }
+
+    public boolean isStart(){
+        return START.equals(command.get(0));
+    }
+
+    public boolean isMove(){
+        return MOVE.equals(command.get(0));
+    }
+
+    public boolean isEnd(){
+        return END.equals(command.get(0));
+    }
+
+    private void validateInput(List<String> inputString) {
+        if(START.equals(inputString.get(0))){
+            checkSizeOne(inputString);
+            return;
+        }
+        if(MOVE.equals(inputString.get(0))){
+            checkSizeThree(inputString);
+            return;
+        }
+        if(END.equals(inputString.get(0))){
+            checkSizeOne(inputString);
+            return;
+        }
+        throw new IllegalArgumentException("찾을 수 없는 명령어 입니다.");
+    }
+
+    private void checkSizeOne(List<String> inputString){
+        if(inputString.size()!=1){
+            throw new IllegalArgumentException("입력 형식이 잘못되었습니다.");
+        }
+    }
+
+    private void checkSizeThree(List<String> inputString){
+        if(inputString.size()!=3){
+            throw new IllegalArgumentException("입력 형식이 잘못되었습니다.");
+        }
+    }
+
+    private Position changeStringToPosition(final String inputPosition) {
+        String currentFile = String.valueOf(inputPosition.charAt(0));
+        String currentRank = String.valueOf(inputPosition.charAt(1));
+        return new Position(Rank.of(Integer.parseInt(currentRank)), File.ofByValue(currentFile));
     }
 }
