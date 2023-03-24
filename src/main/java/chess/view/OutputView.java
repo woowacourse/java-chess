fbcde921bf90560a1506e37dab50a668c7f36cc1
hgo641 <hgo641@gmail.com>
2023-03-24 17:22:06 +0900

    refactor: static 메소드로 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -3,31 +3,28 @@ package chess.view;
 import chess.domain.position.File;
 import chess.domain.position.Position;
 import chess.domain.position.Rank;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class OutputView {
-    public void printErrorMessage(String message) {
+    public static void printErrorMessage(String message) {
         System.out.println("[ERROR] " + message);
     }
 
-    public void printStartMessage() {
+    public static void printStartMessage() {
         System.out.println("> 체스 게임을 시작합니다.");
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
-    public void printBoard(Map<Position, String> pieceNames) {
+    public static void printBoard(Map<Position, String> pieceNames) {
         List<Rank> ranks = Arrays.asList(Rank.values());
         Collections.reverse(ranks);
 
-        for(Rank rank : ranks){
+        for (Rank rank : ranks) {
             printPieceName(pieceNames, rank);
             System.out.println();
         }
@@ -35,7 +32,7 @@ public class OutputView {
     }
 
     private static void printPieceName(Map<Position, String> pieceNames, Rank rank) {
-        for(File file : File.values()){
+        for (File file : File.values()) {
             String name = pieceNames.get(Position.of(file, rank));
             System.out.print(name);
         }
