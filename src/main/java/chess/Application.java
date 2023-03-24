37b9a7cb10536f266cb9391179036b326b3c8d67
be-student <presentsong@naver.com>
2023-03-24 13:34:35 +0900

    feat: frontcontroller 적용

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,13 +1,17 @@
 package chess;
 
 import chess.controller.ChessController;
+import chess.controller.FrontController;
 import chess.view.InputView;
 import chess.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        ChessController chessController = new ChessController(new OutputView(), new InputView());
-        chessController.run();
+        FrontController frontController = new FrontController(
+                new ChessController(new OutputView()),
+                new InputView(),
+                new OutputView());
+        frontController.run();
     }
 }
