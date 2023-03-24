a71259c4719f29515dec632631ccddc3cd89bd76
Eunkeee <eunkeeeee@gmail.com>
2023-03-24 14:10:46 +0900

    study(SuperClass): 상속 테스트 클래스 작성

---
+++ src/main/java/techcourse/fp/study/Super.java
@@ -0,0 +1,11 @@
+package techcourse.fp.study;
+
+public class Super {
+    public Super() {
+        overRideMe();
+    }
+
+    public void overRideMe() {
+        System.out.println("Super 클래스");
+    }
+}
