b6f2b2752dc946a27a5a02c7a8f580827494966f
Choi-JJunho <junho5336@gmail.com>
2023-03-24 17:22:10 +0900

    feat: 폰 점수 규칙 추가

--- src/main/java/chess/domain/board/ChessBoard.java
+++ src/main/java/chess/domain/board/ChessBoard.java
@@ -2,13 +2,20 @@ package chess.domain.board;
 
 import chess.domain.piece.EmptyPiece;
 import chess.domain.piece.Piece;
+import chess.domain.piece.PieceType;
 import chess.domain.piece.Team;
 import chess.exception.PieceCannotMoveException;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ChessBoard {
+
+    private static final BigDecimal PAWN_REDUCE_NUMBER = BigDecimal.valueOf(0.5);
 
     private final Map<Position, Piece> piecePosition;
 
@@ -65,11 +72,55 @@ public class ChessBoard {
     }
 
     public BigDecimal calculateScore(final Team team) {
+        BigDecimal ordinalScore = getOrdinalScore(team);
+        BigDecimal pawnDifferenceScore = getPawnSubScore(team);
+        return ordinalScore.subtract(pawnDifferenceScore);
+    }
 
-        // TODO : 폰일 경우 동일한 Rank에 같은 색의 폰이 있는 경우 0.5점으로 치환하는 메소드 생성.
-        return piecePosition.entrySet().stream()
-                .filter(entry -> entry.getValue().getTeam() == team)
-                .map(entry -> entry.getValue().getType().getScore())
+    private BigDecimal getPawnSubScore(final Team team) {
+
+        List<Position> pawnPositions = toPawnPositions(team);
+        return calculatePawnSubScore(pawnPositions);
+    }
+
+    private BigDecimal calculatePawnSubScore(final List<Position> pawnPositions) {
+        BigDecimal sum = getReduceNumber(pawnPositions);
+
+        if (sum.equals(PAWN_REDUCE_NUMBER)) {
+            return BigDecimal.ZERO;
+        }
+
+        return sum;
+    }
+
+    private BigDecimal getReduceNumber(final List<Position> pawnPositions) {
+        BigDecimal sum = PAWN_REDUCE_NUMBER;
+
+        List<File> files = pawnPositions.stream()
+                .map(Position::getFile)
+                .collect(Collectors.toList());
+
+        for (final File file : files) {
+            if (Collections.frequency(files, file) != 1) {
+                sum = sum.add(sum);
+            }
+        }
+        return sum;
+    }
+
+    private List<Position> toPawnPositions(Team team) {
+        return Arrays.stream(File.values())
+                .flatMap(file -> Arrays.stream(Rank.values())
+                        .map(rank -> Position.of(file, rank))
+                ).filter(position -> piecePosition.get(position).getType() == PieceType.PAWN)
+                .filter(position -> piecePosition.get(position).getTeam() == team)
+                .collect(Collectors.toList());
+    }
+
+    private BigDecimal getOrdinalScore(final Team team) {
+        return piecePosition.values().stream()
+                .filter(piece -> piece.getTeam() == team)
+                .map(piece -> piece.getType().getScore())
                 .reduce(BigDecimal::add)
                 .orElseThrow(() -> new IllegalStateException(team + "은 점수를 계산할 수 없습니다."));
     }
