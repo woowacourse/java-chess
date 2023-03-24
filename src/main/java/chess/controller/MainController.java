0db490a311341f7c0c0856db5e75f764ba8c4f89
kdkdhoho <hkim4410@naver.com>
2023-03-24 15:56:10 +0900

    refactor(MainController): 게임이 시작하면 Board가 생성되도록 리팩터링

--- src/main/java/chess/controller/MainController.java
+++ src/main/java/chess/controller/MainController.java
@@ -1,11 +1,11 @@
 package chess.controller;
 
-import static chess.util.ExceptionHandler.repeatUntilValidInput;
 import static chess.view.Command.END;
 import static chess.view.Command.MOVE;
 import static chess.view.Command.START;
 import static chess.view.InputView.readCommand;
 import static chess.view.OutputView.printBoard;
+import static chess.view.OutputView.printError;
 import static chess.view.OutputView.printFinishMessage;
 import static chess.view.OutputView.printGameStart;
 
@@ -15,6 +15,7 @@ import chess.domain.board.BoardMaker;
 import chess.domain.math.PositionConverter;
 import chess.view.Command;
 import java.util.List;
+import java.util.function.Supplier;
 
 public final class MainController {
 
@@ -24,18 +25,26 @@ public final class MainController {
     private static final int ONLY_COMMAND_SIZE = 1;
     private static final int MOVE_COMMAND_SIZE = 3;
 
-    private final Board board = new Board(new BoardMaker());
-
     public void run() {
         printGameStart();
-        List<String> inputs = repeatUntilValidInput(this::readValidCommand);
+        List<String> inputs = repeatUntilValidAction(this::readValidCommand);
         Command command = Command.of(inputs.get(COMMAND_INDEX));
 
         if (command == START) {
+            final Board board = new Board(new BoardMaker());
             printBoard(board.getBoard());
-            while (repeatUntilValidInput(this::playChess));
+            while (repeatUntilValidAction(() -> playChess(board)));
         }
         printFinishMessage();
+    }
+
+    private <T> T repeatUntilValidAction(final Supplier<T> reader) {
+        try {
+            return reader.get();
+        } catch (IllegalArgumentException e) {
+            printError(e.getMessage());
+            return repeatUntilValidAction(reader);
+        }
     }
 
     private List<String> readValidCommand() {
@@ -46,14 +55,14 @@ public final class MainController {
         return inputs;
     }
 
-    private boolean playChess() {
-        List<String> inputs = repeatUntilValidInput(this::readValidCommand);
+    private boolean playChess(final Board board) {
+        List<String> inputs = repeatUntilValidAction(this::readValidCommand);
         Command command = Command.of(inputs.get(COMMAND_INDEX));
 
-        return executeCommand(inputs, command);
+        return executeCommand(board, inputs, command);
     }
 
-    private boolean executeCommand(final List<String> inputs, final Command command) {
+    private boolean executeCommand(final Board board, final List<String> inputs, final Command command) {
         if (command == END) {
             return false;
         }
@@ -61,13 +70,13 @@ public final class MainController {
             throw new IllegalArgumentException("이미 게임을 실행중입니다. 다른 명령어를 입력해주세요.");
         }
         if (command == MOVE) {
-            movePiece(inputs);
+            movePiece(board, inputs);
             printBoard(board.getBoard());
         }
         return true;
     }
 
-    private void movePiece(final List<String> inputs) {
+    private void movePiece(final Board board, final List<String> inputs) {
         Position currentPosition = PositionConverter.toPosition(inputs.get(CURRENT_POSITION_INDEX));
         Position targetPosition = PositionConverter.toPosition(inputs.get(TARGET_POSITION_INDEX));
 
