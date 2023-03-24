813988c81c1e36e220bc89089bc4b9f9742b44cd
dev_kong <ahdjd5@gmail.com>
2023-03-24 17:25:17 +0900

    refa: 뷰 출력형식 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -13,6 +13,7 @@ import dto.ScoreDto;
 
 public class OutputView {
     public static void printChessBoard(Map<Square, Piece> board) {
+        System.out.println();
         List<String> messages = new ArrayList<>();
         for (Rank value : Rank.values()) {
             StringBuilder stringBuilder = new StringBuilder();
@@ -41,9 +42,10 @@ public class OutputView {
     }
 
     public static void printChessInfo() {
-        System.out.println("> 체스 게임을 시작합니다.\n"
-                + "> 게임 종료 : end\n"
-                + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 현재 점수 출력 : status");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
     public static void printErrorMessage(RuntimeException e) {
@@ -51,30 +53,34 @@ public class OutputView {
     }
 
     public static void printScores(List<ScoreDto> scoreDto) {
+        System.out.println();
         for (ScoreDto dto : scoreDto) {
             System.out.println(String.join(" : ", dto.getCamp(), String.valueOf(dto.getScore())));
         }
     }
 
     public static void printGameRoomInfo() {
-        System.out.println(">> 입장할 게임의 이름일 'JOIN' 과 함께 입력해 주세요. ex) JOIN chess1\n"
-                + ">> 목록을 보시려면 'LIST'를 입력해주세요.\n"
-                + ">> 게임을 생성하시려면 'CREATE'을 입력해주세요.\n"
-                + ">> 종료하시려면 'EXIT'를 입력해주세요.");
+        System.out.println();
+        System.out.println(">> 방 생성 : CREATE - 예. CREATE test");
+        System.out.println(">> 방 입장 : JOIN - 예. JOIN test");
+        System.out.println(">> 목록 출력 : LIST");
+        System.out.println(">> 종료 : EXIT");
     }
 
     public static void printGameRooms(List<String> gameRooms) {
-        if(gameRooms.size() == 0) {
+        System.out.println();
+        if (gameRooms.size() == 0) {
             System.out.println("생성된 게임이 없습니다.");
             return;
         }
         for (int i = 0; i < gameRooms.size(); i++) {
-            System.out.println(i + 1 + gameRooms.get(i));
+            System.out.println(i + 1 + ". " + gameRooms.get(i));
         }
     }
 
     public static void printIsSavedGame(int size) {
-        if(size>0) {
+        System.out.println();
+        if (size > 0) {
             System.out.println("저장된 게임을 불러옵니다.");
             return;
         }
