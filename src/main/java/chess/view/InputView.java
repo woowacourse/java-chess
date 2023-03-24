77f907eb4a7f8803b66a55d0ce174c8655a35fb8
java-saeng <gusdn85554@naver.com>
2023-03-24 16:21:10 +0900

    refactor : 메서드 extract

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -21,34 +21,50 @@ public class InputView {
 
         final String command = scanner.nextLine();
 
+        validateAppropriate(command);
+
+        return command;
+    }
+
+    private static void validateAppropriate(final String command) {
         if (Command.isNotAppropriate(command)) {
             throw new IllegalArgumentException("start 나 end 둘 중에 하나 입력해주세요.");
         }
-
-        return command;
     }
 
     public static List<String> readMoveCommand() {
         final List<String> commands = Arrays.stream(scanner.nextLine().split(INPUT_COMMAND_DELIM))
                                             .collect(Collectors.toList());
 
-        if (Command.isStatus(commands.get(IndexCommand.START_COMMAND_INDEX.value())) ||
-        Command.isEnd(commands.get(IndexCommand.START_COMMAND_INDEX.value()))) {
+        if (isCommandStatusOrEnd(commands)) {
             return commands;
         }
 
-        if (commands.size() > 3) {
-            throw new IllegalArgumentException("[명령어] [시작 위치] [도착 위치] 로 입력해주세요.");
-        }
+        validateCommandSize(commands);
 
         final String sourcePosition = commands.get(IndexCommand.SOURCE_POSITION.value());
         final String targetPosition = commands.get(IndexCommand.TARGET_POSITION.value());
 
+        validateCanNotAccessPosition(sourcePosition, targetPosition);
+
+        return commands;
+    }
+
+    private static boolean isCommandStatusOrEnd(final List<String> commands) {
+        return Command.isStatus(commands.get(IndexCommand.START_COMMAND_INDEX.value())) ||
+                Command.isEnd(commands.get(IndexCommand.START_COMMAND_INDEX.value()));
+    }
+
+    private static void validateCommandSize(final List<String> commands) {
+        if (commands.size() > 3) {
+            throw new IllegalArgumentException("[명령어] [시작 위치] [도착 위치] 로 입력해주세요.");
+        }
+    }
+
+    private static void validateCanNotAccessPosition(final String sourcePosition, final String targetPosition) {
         if (PositionCache.isNotCaching(sourcePosition) || PositionCache.isNotCaching(targetPosition)) {
             throw new IllegalArgumentException("접근할 수 없는 위치입니다.");
         }
-
-        return commands;
     }
 
     public static String readNewGameCommand() {
