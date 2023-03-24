6db04f0f0d3a4ddeb1ba499ec945b120552cbaaf
JunDonghyuk <ehdgur4814@naver.com>
2023-03-24 14:36:25 +0900

    feat: main 구현

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,13 +1,20 @@
 package chess;
 
-import chess.controller.ChessController;
+import chess.controller.UserAccessController;
+import chess.domain.userAccess.UserAccessService;
+import chess.domain.userAccess.room.RoomDao;
+import chess.domain.userAccess.user.UserDao;
 import chess.view.InputView;
 import chess.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        ChessController chessController = new ChessController(new InputView(), new OutputView());
-        chessController.run();
+        UserAccessController userAccessController = new UserAccessController(
+                new InputView(),
+                new OutputView(),
+                new UserAccessService(new UserDao(), new RoomDao())
+        );
+        userAccessController.run();
     }
 }
