4b226c0c63a19b4f3527d7f1bcd2b2760b8f579d
BGuga <tmdwns9913@gmail.com>
2023-03-24 21:52:39 +0900

    chore: outputView 게임 정보 반환

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -36,6 +36,7 @@ public class OutputView {
         System.out.println("> 체스 게임을 시작합니다.");
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 결과 보기 : status");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
