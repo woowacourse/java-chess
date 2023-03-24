0a00c840bd7e72ebfa30763a970b0c7479db3f09
waterricecake <fruturum@nate.com>
2023-03-24 17:49:22 +0900

    test(NotationDaoTest): 기보 테스트

---
+++ src/test/java/chess/room/NotationDaoTest.java
@@ -0,0 +1,25 @@
+package chess.room;
+
+import chess.domain.board.File;
+import chess.domain.board.Rank;
+import chess.domain.board.Square;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+class NotationDaoTest {
+    @Test
+    @DisplayName("기보를 생성하는 테스트")
+    void addNotationTest() throws SQLException {
+        Square source = new Square(File.A, Rank.TWO);
+        Square target = new Square(File.A, Rank.THREE);
+        NotationDao.addNotation(1, 1, new Move(source, target));
+    }
+
+    @Test
+    @DisplayName("기보를 가져오는 테스트")
+    void findByRoomIdTest() throws SQLException {
+        NotationDao.findByRoomId(1);
+    }
+}
