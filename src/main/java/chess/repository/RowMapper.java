beb6237bebaef68174f0fd13ff4a488894a49b5b
greeng00se <thegreengoose7@gmail.com>
2023-03-24 11:00:05 +0900

    refactor: RowMapper에 Functional Interface 적용

--- src/main/java/chess/repository/RowMapper.java
+++ src/main/java/chess/repository/RowMapper.java
@@ -3,6 +3,7 @@ package chess.repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper<T> {
     T mapRow(final ResultSet resultSet) throws SQLException;
 }
