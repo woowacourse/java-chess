9ce8fdc6dcd961f680a8983479bb39f12709174c
yoondgu <doyou2ing@naver.com>
2023-03-24 15:29:41 +0900

    feat: 강제종료 커맨드 추가에 따른 안내문구 반영

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -6,8 +6,10 @@ public class OutputView {
 
     private static final String GAME_START = "> 체스 게임을 시작합니다.";
     private static final String GAME_COMMAND_MOVE_DESCRIPTION = "move source위치 target위치 - 예. move b2 b3";
-    private static final String GAME_COMMAND_REQUEST = String.format("> 게임 시작: %s\n> 게임 종료: %s\n> 게임 이동: %s\n",
-            Command.START.getAnswer(), Command.END.getAnswer(), GAME_COMMAND_MOVE_DESCRIPTION);
+    private static final String GAME_COMMAND_REQUEST = String.format(
+            "> 게임 시작: %s\n> 게임 종료: %s\n> 게임 이동: %s\n> 어플리케이션 종료: %s",
+            Command.START.getAnswer(), Command.END.getAnswer(), GAME_COMMAND_MOVE_DESCRIPTION, Command.EXIT.getAnswer()
+    );
     private static final String ERROR_MESSAGE_FORMAT = "[입력 오류] %s" + System.lineSeparator();
 
     private OutputView() {
