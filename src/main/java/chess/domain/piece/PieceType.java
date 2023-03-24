0d8d70612fecab9e51e1f2bba54216b6b60f0146
yoondgu <doyou2ing@naver.com>
2023-03-24 22:23:39 +0900

    feat: 말 타입에 따른 점수 정의

--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -2,6 +2,21 @@ package chess.domain.piece;
 
 public enum PieceType {
 
-    ROOK, KNIGHT, BISHOP, QUEEN, KING, PAWN
+    ROOK(5),
+    KNIGHT(2.5),
+    BISHOP(3),
+    QUEEN(9),
+    KING(0),
+    PAWN(1);
 
+
+    private final double point;
+
+    PieceType(final double point) {
+        this.point = point;
+    }
+
+    public double getPoint() {
+        return point;
+    }
 }
