fb8682fe4b1992ed8aa419eb8b612ca4777f6dd2
jiwon <ljwon77@daum.net>
2023-03-24 10:44:22 +0900

    feat: 점수에 대해 관리하는 클래스를 생성한다

--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -1,10 +1,16 @@
 package chess.domain.piece;
 
 public enum PieceType {
-    QUEEN,
-    ROOK,
-    KNIGHT,
-    PAWN,
-    BISHOP,
-    KING
+    QUEEN(new Score(9)),
+    ROOK(new Score(5)),
+    KNIGHT(new Score(2.5)),
+    PAWN(new Score(1)),
+    BISHOP(new Score(3)),
+    KING(new Score(0));
+
+    private final Score score;
+
+    PieceType(final Score score) {
+        this.score = score;
+    }
 }
---
+++ src/main/java/chess/domain/piece/Score.java
@@ -0,0 +1,9 @@
+package chess.domain.piece;
+
+public class Score {
+    private final double score;
+
+    public Score(final double score) {
+        this.score = score;
+    }
+}
