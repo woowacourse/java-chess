ad3706faf10765788274a2350dea8cd8508eaa04
2014012606 <2014012606@hanyang.ac.kr>
2023-03-24 16:53:17 +0900

    feat: pieceInfo 클래스 추가 - 자신의 점수를 가진다 - 자신의 점수를 더해 반환한다

--- src/main/java/chess/domain/chesspiece/PieceInfo.java
+++ src/main/java/chess/domain/chesspiece/PieceInfo.java
@@ -1,11 +1,21 @@
 package chess.domain.chesspiece;
 
 public enum PieceInfo {
-    EMPTY_PIECE,
-    PAWN,
-    ROOK,
-    BISHOP,
-    KNIGHT,
-    QUEEN,
-    KING
+    EMPTY_PIECE(0),
+    PAWN(1),
+    ROOK(5),
+    BISHOP(3),
+    KNIGHT(2.5),
+    QUEEN(9),
+    KING(0);
+
+    private final double score;
+
+    PieceInfo(final double score) {
+        this.score = score;
+    }
+
+    public double addScore(final double score) {
+        return score + this.score;
+    }
 }
