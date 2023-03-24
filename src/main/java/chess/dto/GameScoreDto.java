a4803b87b7d7b86fbc2c366bd0b2cbfe767c66e0
somsom13 <somin1113@naver.com>
2023-03-24 16:56:48 +0900

    style: final 키워드 추가

--- src/main/java/chess/dto/GameScoreDto.java
+++ src/main/java/chess/dto/GameScoreDto.java
@@ -5,12 +5,12 @@ public class GameScoreDto {
     private final double whiteScore;
     private final double blackScore;
 
-    private GameScoreDto(double whiteScore, double blackScore) {
+    private GameScoreDto(final double whiteScore, final double blackScore) {
         this.whiteScore = whiteScore;
         this.blackScore = blackScore;
     }
 
-    public static GameScoreDto of(double whiteScore, double blackScore) {
+    public static GameScoreDto of(final double whiteScore, final double blackScore) {
         return new GameScoreDto(whiteScore, blackScore);
     }
 
