2868af187750acff0fc12278bdf2f1a22e4f42b6
Be-student <presentsong@naver.com>
2023-03-26 02:19:18 +0900

    feat: user 추가시 같은 닉네임은 2번 생성 안되도록 변경

--- src/main/java/chess/repository/UserDao.java
+++ src/main/java/chess/repository/UserDao.java
@@ -1,6 +1,7 @@
 package chess.repository;
 
 import chess.infra.connection.JdbcTemplate;
+import chess.infra.connection.RowMapper;
 
 public class UserDao {
 
@@ -11,8 +12,25 @@ public class UserDao {
     }
 
     public void addUser(String userId) {
+        if (findUserIdIfExist(userId) != null) {
+            return;
+        }
         String query = "INSERT INTO user (user_id) VALUES (?)";
         jdbcTemplate.executeUpdate(query, userId);
+    }
+
+    private String findUserIdIfExist(String userId) {
+        String query = "SELECT user_id FROM user WHERE user_id = ?";
+        return jdbcTemplate.query(query, userIdMapper(), userId);
+    }
+
+    private RowMapper<String> userIdMapper() {
+        return resultSet -> {
+            if (resultSet.next()) {
+                return resultSet.getString("user_id");
+            }
+            return null;
+        };
     }
 
     public void deleteByUserId(String userId) {
