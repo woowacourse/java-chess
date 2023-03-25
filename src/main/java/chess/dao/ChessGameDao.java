e7b201dcb4766b72b535cf062360a97833408f1e
hectick <chyeon1223@cau.ac.kr>
2023-03-25 17:14:25 +0900

    feat(DBChessGameDao): 데이터베이스 연결 기능 구현

---
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -0,0 +1,11 @@
+package chess.dao;
+
+import chess.domain.ChessGame;
+
+public interface ChessGameDao {
+    void save(ChessGame chessGame);
+
+    ChessGame select();
+
+    void update(ChessGame chessGame);
+}
---
+++ src/main/java/chess/dao/DBChessGameDao.java
@@ -0,0 +1,42 @@
+package chess.dao;
+
+import chess.domain.ChessGame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBChessGameDao implements ChessGameDao{
+
+    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
+    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USERNAME = "root"; //  MySQL 서버 아이디
+    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+        } catch (final SQLException e) {
+            System.err.println("DB 연결 오류:" + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    @Override
+    public void save(ChessGame chessGame) {
+
+    }
+
+    @Override
+    public ChessGame select() {
+        return null;
+    }
+
+    @Override
+    public void update(ChessGame chessGame) {
+
+    }
+}
