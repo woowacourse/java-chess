bb0d923d2f9efb22fb5c30321bf04ca0412f4e70
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-03-25 17:34:32 +0900

    Feat(pieces): dbPieces 정적 팩토리 메서드 추가

--- src/test/java/chess/domain/PiecesTest.java
+++ src/test/java/chess/domain/PiecesTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static chess.domain.Shape.BISHOP;
 import static chess.domain.Shape.KING;
 import static chess.domain.Shape.PAWN;
@@ -61,6 +63,16 @@ final class PiecesTest {
                     assertThat(count).isEqualTo(1);
                 }
         );
+    }
+
+    @Test
+    @DisplayName("기물 리스트로 Pieces를 생성할 수 있다.")
+    void createByPieceList() {
+        List<Piece> elements = List.of(Piece.from(1, 'a', ROOK), Piece.from(2, 'a', PAWN));
+        final var pieces = Pieces.from(elements);
+        assertThat(pieces)
+                .isNotNull()
+                .isInstanceOf(Pieces.class);
     }
 
 }
