75079e70032771715e9101a68050f9a4b4b2d065
jiwon <ljwon77@daum.net>
2023-03-25 14:10:20 +0900

    feat: EndController의 생성자에 대해 접근 제어자를 변경한다

--- src/main/java/chess/controller/status/EndController.java
+++ src/main/java/chess/controller/status/EndController.java
@@ -3,6 +3,9 @@ package chess.controller.status;
 import chess.controller.Command;
 
 public final class EndController implements Controller {
+    EndController() {
+    }
+
     @Override
     public Controller checkCommand(final Command command, final Runnable runnable) {
         throw new IllegalArgumentException("게임이 끝났습니다.");
