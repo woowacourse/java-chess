9cfdf7276c2a46c97fd54126150c07893e572570
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-03-25 17:06:47 +0900

    Feat(Piece) : HashCode&Equals 재정의
    Co-authored-by: wugawuga <dnr3655@naver.com>
    Co-authored-by: kyY00n <dev.kayoung@gmail.com>

--- src/main/java/chess/domain/Piece.java
+++ src/main/java/chess/domain/Piece.java
@@ -4,6 +4,7 @@ import chess.dto.PositionDto;
 import chess.dto.request.MoveRequest;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Piece {
 
@@ -63,15 +64,28 @@ public class Piece {
         return shape.getScore();
     }
 
+    public Shape getShape() {
+        return shape;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Piece piece = (Piece) o;
+        return Objects.equals(position, piece.position) && shape == piece.shape;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, shape);
+    }
+
     @Override
     public String toString() {
         return "Piece{" +
                 "position=" + position +
                 ", shape=" + shape +
                 '}';
-    }
-
-    public Shape getShape() {
-        return shape;
     }
 }
