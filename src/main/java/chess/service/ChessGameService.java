8deaa2966489be9ef39a4ff1c9a9f04a553ec344
iamjooon2 <iamjooon2@gmail.com>
2023-03-26 02:34:51 +0900

    refactor: Board 불변으로 변경

--- src/main/java/chess/service/ChessGameService.java
+++ src/main/java/chess/service/ChessGameService.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 public class ChessGameService {
 
-    private Board board;
+    private final Board board;
     private final MoveDao moveDao;
 
     public ChessGameService() {
