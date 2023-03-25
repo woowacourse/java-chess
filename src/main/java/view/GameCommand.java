2067a16ab0fb8f9152c8a10f6d797ae10a80dde8
Youngho <cyh6099@gmail.com>
2023-03-25 23:58:03 +0900

    refactor: game command유효성 검사 로직 수정

--- src/main/java/view/GameCommand.java
+++ src/main/java/view/GameCommand.java
@@ -1,24 +1,32 @@
 package view;
 
 import java.util.Arrays;
+import java.util.List;
 
 public enum GameCommand {
-    START("start"),
-    MOVE("move"),
-    END("end"),
-    STATUS("status"),
-    LOAD("load"),
-    NEW("new");
+    START("start", 1),
+    MOVE("move", 3),
+    END("end", 1),
+    STATUS("status", 1),
+    LOAD("load", 1),
+    NEW("new", 1);
 
     private final String gameCommand;
+    private final int commandLine;
 
-    GameCommand(String gameCommand) {
+    GameCommand(String gameCommand, int commandLine) {
         this.gameCommand = gameCommand;
+        this.commandLine = commandLine;
     }
 
-    public static GameCommand from(String gameCommand) {
+    public static GameCommand from(List<String> gameCommand) {
+        if (gameCommand == null) {
+            throw new IllegalArgumentException("아무 값도 입력되지 않았습니다.");
+        }
+
         return Arrays.stream(GameCommand.values())
-                .filter(command -> command.gameCommand.equals(gameCommand))
+                .filter(command -> command.gameCommand.equals(gameCommand.get(0)))
+                .filter(command -> command.commandLine == gameCommand.size())
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게임 커맨드입니다."));
     }
