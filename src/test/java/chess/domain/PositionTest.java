0238af3b6786db58eb483ceae79218cfee7746b9
kdkdhoho <hkim4410@naver.com>
2023-03-25 18:58:11 +0900

    test(PositionTest): 정적 팩터리 메서드 사용

--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -15,7 +15,7 @@ class PositionTest {
 
     @BeforeEach
     void setUP() {
-        position = new Position(1, 1);
+        position = Position.of(1, 1);
     }
 
     @Test
@@ -23,7 +23,7 @@ class PositionTest {
     void moveTest_up() {
         var movedPosition = position.move(UnitVector.UP);
 
-        assertThat(movedPosition.equals(new Position(0, 1))).isTrue();
+        assertThat(movedPosition.equals(Position.of(0, 1))).isTrue();
     }
 
     @Test
@@ -31,7 +31,7 @@ class PositionTest {
     void moveTest_down() {
         var movedPosition = position.move(UnitVector.DOWN);
 
-        assertThat(movedPosition.equals(new Position(2, 1))).isTrue();
+        assertThat(movedPosition.equals(Position.of(2, 1))).isTrue();
     }
 
     @Test
@@ -39,7 +39,7 @@ class PositionTest {
     void moveTest_left() {
         var movedPosition = position.move(UnitVector.LEFT);
 
-        assertThat(movedPosition.equals(new Position(1, 0))).isTrue();
+        assertThat(movedPosition.equals(Position.of(1, 0))).isTrue();
     }
 
     @Test
@@ -47,46 +47,46 @@ class PositionTest {
     void moveTest_right() {
         var movedPosition = position.move(UnitVector.RIGHT);
 
-        assertThat(movedPosition.equals(new Position(1, 2))).isTrue();
+        assertThat(movedPosition.equals(Position.of(1, 2))).isTrue();
     }
     @Test
     @DisplayName("UnitVector를 받아 Position을 이동한다.")
     void moveTest_upRight() {
         var movedPosition = position.move(UnitVector.UP_RIGHT);
 
-        assertThat(movedPosition.equals(new Position(0, 2))).isTrue();
+        assertThat(movedPosition.equals(Position.of(0, 2))).isTrue();
     }
     @Test
     @DisplayName("UnitVector를 받아 Position을 이동한다.")
     void moveTest_upLeft() {
         var movedPosition = position.move(UnitVector.UP_LEFT);
 
-        assertThat(movedPosition.equals(new Position(0, 0))).isTrue();
+        assertThat(movedPosition.equals(Position.of(0, 0))).isTrue();
     }
     @Test
     @DisplayName("UnitVector를 받아 Position을 이동한다.")
     void moveTest_downLeft() {
-        var position = new Position(1, 1);
+        var position = Position.of(1, 1);
 
         var movedPosition = position.move(UnitVector.DOWN_RIGHT);
 
-        assertThat(movedPosition.equals(new Position(2, 2))).isTrue();
+        assertThat(movedPosition.equals(Position.of(2, 2))).isTrue();
     }
     @Test
     @DisplayName("UnitVector를 받아 Position을 이동한다.")
     void moveTest_downRight() {
-        var position = new Position(1, 1);
+        var position = Position.of(1, 1);
 
         var movedPosition = position.move(UnitVector.DOWN_LEFT);
 
-        assertThat(movedPosition.equals(new Position(2, 0))).isTrue();
+        assertThat(movedPosition.equals(Position.of(2, 0))).isTrue();
     }
 
     @Test
     @DisplayName("Position 객체는 논리적으로 같으면 같은 객체이다.")
     void equalsTest() {
-        var a = new Position(1, 1);
-        var b = new Position(1, 1);
+        var a = Position.of(1, 1);
+        var b = Position.of(1, 1);
 
         assertThat(a.equals(b)).isTrue();
     }
