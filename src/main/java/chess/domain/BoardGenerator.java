40c9b9bc33640c74ce96f2a90be944aa36068042
hectick <chyeon1223@cau.ac.kr>
2023-03-26 01:15:12 +0900

    refactor(BoardGenerator): 메서드 반환값 및 변수 명 수정

--- src/main/java/chess/domain/BoardGenerator.java
+++ src/main/java/chess/domain/BoardGenerator.java
@@ -13,52 +13,58 @@ import chess.domain.position.Position;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class BoardGenerator {
 
-    private static final int BLACK_PAWN_INITIAL_ROW = 6;
-    private static final int WHITE_PAWN_INITIAL_ROW = 1;
-
     private static final int BLACK_EDGE_ROW = 7;
-    private static final int WHITE_EDGE_ROW = 0;
-    private static final int EMPTY_START_ROW = 2;
+    private static final int BLACK_PAWN_INITIAL_ROW = 6;
     private static final int EMPTY_END_ROW = 5;
-
+    private static final int EMPTY_START_ROW = 2;
+    private static final int WHITE_PAWN_INITIAL_ROW = 1;
+    private static final int WHITE_EDGE_ROW = 0;
     private static final int LINE_SIZE = 8;
 
 
     public static Board createBoard() {
-        HashMap<Position, Piece> board = new HashMap<>();
-        initializePawns(board, Team.WHITE, WHITE_PAWN_INITIAL_ROW);
-        initializePawns(board, Team.BLACK, BLACK_PAWN_INITIAL_ROW);
-        initializePiecesOfEdgeLine(board, Team.WHITE, WHITE_EDGE_ROW);
-        initializePiecesOfEdgeLine(board, Team.BLACK, BLACK_EDGE_ROW);
-        initializeEmptyPieces(board);
+        HashMap<Position, Piece> board = new LinkedHashMap<>();
+        board.putAll(initializePiecesOfEdgeLine(Team.BLACK, BLACK_EDGE_ROW));
+        board.putAll(initializePawns(Team.BLACK, BLACK_PAWN_INITIAL_ROW));
+        board.putAll(initializeEmptyPieces());
+        board.putAll(initializePawns(Team.WHITE, WHITE_PAWN_INITIAL_ROW));
+        board.putAll(initializePiecesOfEdgeLine(Team.WHITE, WHITE_EDGE_ROW));
         return new Board(board);
     }
 
-    private static void initializeEmptyPieces(final HashMap<Position, Piece> board) {
-        for (int i = 0; i < LINE_SIZE; i++) {
-            for (int j = EMPTY_START_ROW; j <= EMPTY_END_ROW; j++) {
-                board.put(new Position(i, j), new EmptyPiece());
+    private static Map<Position, Piece> initializeEmptyPieces() {
+        Map<Position, Piece> positionsAndPieces = new LinkedHashMap<>();
+        for (int column = 0; column < LINE_SIZE; column++) {
+            for (int row = EMPTY_START_ROW; row <= EMPTY_END_ROW; row++) {
+                positionsAndPieces.put(new Position(column, row), new EmptyPiece());
             }
         }
+        return positionsAndPieces;
     }
 
-    private static void initializePiecesOfEdgeLine(final HashMap<Position, Piece> board, final Team team, final int row) {
+    private static Map<Position, Piece> initializePiecesOfEdgeLine(final Team team, final int row) {
+        Map<Position, Piece> positionsAndPieces = new LinkedHashMap<>();
         List<Piece> pieces = new ArrayList<>(
                 List.of(Rook.from(team), Knight.from(team), Bishop.from(team), Queen.from(team),
                         King.from(team), Bishop.from(team), Knight.from(team), Rook.from(team))
         );
-        for (int i = 0; i < LINE_SIZE; i++) {
-            board.put(new Position(i, row), pieces.get(i));
+        for (int column = 0; column < LINE_SIZE; column++) {
+            positionsAndPieces.put(new Position(column, row), pieces.get(column));
         }
+        return positionsAndPieces;
     }
 
-    private static void initializePawns(final HashMap<Position, Piece> board, final Team team, final int row) {
-        for (int i = 0; i < LINE_SIZE; i++) {
-            board.put(new Position(i, row), Pawn.from(team));
+    private static Map<Position, Piece> initializePawns(final Team team, final int row) {
+        Map<Position, Piece> positionsAndPieces = new LinkedHashMap<>();
+        for (int column = 0; column < LINE_SIZE; column++) {
+            positionsAndPieces.put(new Position(column, row), Pawn.from(team));
         }
+        return positionsAndPieces;
     }
 }
