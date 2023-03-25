f38e176de6bb39a0043b82ab340207282b33dde1
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-25 19:45:00 +0900

    feat : 출력에 이용하기 위한 게터 추가

--- src/main/java/chess/domain/Color.java
+++ src/main/java/chess/domain/Color.java
@@ -1,7 +1,25 @@
 package chess.domain;
 
 public enum Color {
-    BLACK,
-    WHITE,
-    EMPTY
+    BLACK("검정색"),
+    WHITE("흰색"),
+    EMPTY("없음");
+
+    private final String player;
+
+    Color(final String color) {
+        this.player = color;
+    }
+
+    public boolean isBlack() {
+        return this == BLACK;
+    }
+
+    public boolean isWhite() {
+        return this == WHITE;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
 }
