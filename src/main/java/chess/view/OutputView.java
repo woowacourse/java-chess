4e45eba8a730ae99aba414ea0a6743694784c490
Be-student <presentsong@naver.com>
2023-03-26 02:19:31 +0900

    feat: 출력 메시지 추가

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -8,6 +8,10 @@ public class OutputView {
 
     public void printInitialMessage() {
         System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 로그인 : login 유저id - 예. login user1");
+        System.out.println("> 게임 목록 : games");
+        System.out.println("> 진행중인 게임 선택 : join 게임번호 - 예. join 1");
+        System.out.println("> 게임 생성 : create");
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
@@ -37,5 +41,29 @@ public class OutputView {
 
     public void printError(Exception e) {
         System.out.println(e.getMessage());
+    }
+
+    public void printLoginSuccess() {
+        System.out.println("로그인에 성공했습니다");
+        System.out.println("현재 유저의 게임들을 보려면 games 를 입력해주세요");
+
+    }
+
+    public void printGames(List<Integer> games) {
+        System.out.println("현재 유저의 게임들");
+        games.forEach(System.out::println);
+        System.out.println("게임을 시작하려면 join 게임번호 를 입력해주세요");
+        System.out.println("게임을 생성하려면 create 를 입력해주세요");
+    }
+
+    public void printCreateGameSuccess() {
+        System.out.println("게임을 생성했습니다");
+        System.out.println("게임을 시작하려면 start 를 입력해주세요");
+    }
+
+    public void printJoinGameSuccess(String boardStatus) {
+        System.out.println("게임에 참여했습니다");
+        System.out.println("현재 게임 상태");
+        System.out.println(boardStatus);
     }
 }
