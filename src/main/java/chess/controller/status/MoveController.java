ca5389e8788738cd34d2f1b1000576c1e0848717
jiwon <ljwon77@daum.net>
2023-03-25 14:12:21 +0900

    feat: 게임이 움직임 상태일 때 status를 입력하면 게임 결과를 출력하도록 제어한다

--- src/main/java/chess/controller/status/MoveController.java
+++ src/main/java/chess/controller/status/MoveController.java
@@ -12,25 +12,9 @@ public final class MoveController implements Controller {
     private final ChessGame chessGame;
     private final CampType campType;
 
-    public MoveController(final ChessGame chessGame, final CampType campType) {
+    MoveController(final ChessGame chessGame, final CampType campType) {
         this.chessGame = chessGame;
         this.campType = campType;
-    }
-
-    private Controller move(final Command command, final Runnable runnable) {
-        validateCommand(command);
-        final List<String> commands = command.getCommands();
-        final Position source = PositionConverter.convert(commands.get(1));
-        final Position target = PositionConverter.convert(commands.get(2));
-        chessGame.setUp(source, target, campType);
-        runnable.run();
-        return new MoveController(chessGame, campType.changeTurn());
-    }
-
-    private void validateCommand(final Command command) {
-        if (!command.isCorrectWhenMove()) {
-            throw new IllegalArgumentException("'move source위치 target위치 - 예. move b2 b3'와 같은 형태로 입력해 주세요.");
-        }
     }
 
     @Override
@@ -41,11 +25,33 @@ public final class MoveController implements Controller {
         if (command.isEnd()) {
             return new EndController();
         }
+        if (command.isStatus()) {
+            return new StatusController(chessGame, campType).getStatus(true);
+        }
         return move(command, runnable);
     }
 
     @Override
     public boolean isRun() {
         return true;
+    }
+
+    Controller move(final Command command, final Runnable runnable) {
+        validateCommand(command);
+        final List<String> commands = command.getCommands();
+        final Position source = PositionConverter.convert(commands.get(1));
+        final Position target = PositionConverter.convert(commands.get(2));
+        boolean isGameRun = chessGame.run(source, target, campType);
+        runnable.run();
+        if (!isGameRun) {
+            return new StatusController(chessGame, campType).getStatus(false);
+        }
+        return new MoveController(chessGame, campType.changeTurn());
+    }
+
+    private void validateCommand(final Command command) {
+        if (!command.isCorrectWhenMove()) {
+            throw new IllegalArgumentException("'move source위치 target위치 - 예. move b2 b3'와 같은 형태로 입력해 주세요.");
+        }
     }
 }
--- src/test/java/chess/controller/status/MoveControllerTest.java
+++ src/test/java/chess/controller/status/MoveControllerTest.java
@@ -4,6 +4,8 @@ import chess.controller.Command;
 import chess.controller.CommandType;
 import chess.domain.chess.CampType;
 import chess.domain.chess.ChessGame;
+import chess.domain.chess.ChessGameHelper;
+import chess.domain.piece.move.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -47,6 +49,23 @@ class MoveControllerTest {
                 .isInstanceOf(EndController.class);
     }
 
+    @Test
+    @DisplayName(value = "게임이 움직임 상태일 때 status를 입력하면 게임 결과를 출력하도록 제어한다.")
+    void checkCommandStatus() {
+        // given
+        final MoveController moveController = new MoveController(new ChessGame(), CampType.WHITE);
+        final Command command = new Command(CommandType.STATUS, List.of("status"));
+
+        // when
+        Controller controller = moveController.checkCommand(command, () -> {
+        });
+
+        // then
+        assertThat(controller)
+                .isInstanceOf(StatusController.class);
+    }
+
+
     @ParameterizedTest(name = "게임이 움직임 상태일 때 올바르지 않은 명령어 형식을 입력하면 예외가 발생한다.")
     @ValueSource(strings = {"move", "move a2", "", " move ", "move a2 a3 a5", "move a2a3"})
     void checkCommandValidate(final String commands) {
@@ -59,6 +78,26 @@ class MoveControllerTest {
         }))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("'move source위치 target위치 - 예. move b2 b3'와 같은 형태로 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("게임이 진행 중인 상태가 아니라면 end 상태로 변경한다.")
+    void move() {
+        // given
+        final Command command = new Command(CommandType.MOVE, List.of("move", "b2", "b4"));
+        final ChessGame chessGame = new ChessGame();
+        final CampType campType = CampType.WHITE;
+        chessGame.run(new Position(1, 0), new Position(3, 0), campType);
+        ChessGameHelper.playKingDie(chessGame);
+        final MoveController moveController = new MoveController(chessGame, campType);
+
+        // when
+        Controller controller = moveController.move(command, () -> {
+        });
+
+        // then
+        assertThat(controller)
+                .isInstanceOf(EndController.class);
     }
 
     @Test
