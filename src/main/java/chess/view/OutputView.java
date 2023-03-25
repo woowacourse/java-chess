43cde51ba278575209435c4dbb21a6fdff50b7d6
apptie <appti@naver.com>
2023-03-26 00:50:53 +0900

    refactor: 체스 게임 안내 메세지 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -26,6 +26,7 @@ public class OutputView {
     public void guideGameStart() {
         System.out.println("> 체스 게임을 시작합니다.");
         System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 불러오기 : load");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source 위치 target 위치 - 예. move b2 b3");
     }
