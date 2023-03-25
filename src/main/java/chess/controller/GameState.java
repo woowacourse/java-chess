b1a565b636ac0b77bbbacd44d5cf0eac13f42682
Combi153 <jcm04@naver.com>
2023-03-25 15:29:07 +0900

    feat: status 게임 상태 추가

--- src/main/java/chess/controller/GameState.java
+++ src/main/java/chess/controller/GameState.java
@@ -4,19 +4,20 @@ import java.util.Arrays;
 
 public enum GameState {
     READY("start"),
+    RUNNING("move"),
     END("end"),
-    RUNNING("move");
+    STATUS("status");
 
-    private final String command;
+    private final String commandHead;
 
-    GameState(final String command) {
-        this.command = command;
+    GameState(final String commandHead) {
+        this.commandHead = commandHead;
     }
 
-    public static GameState valueOfCommand(String inputCommand) {
+    public static GameState valueOfCommand(String input) {
         return Arrays.stream(GameState.values())
-                .filter(gameState -> gameState.command.equals(inputCommand))
+                .filter(gameState -> gameState.commandHead.equals(input))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("잘못된 명령어 입니다. 입력 값 :" + inputCommand));
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 명령어 입니다. 입력 값 :" + input));
     }
 }
