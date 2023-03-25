69cee6a244be9b7cdf4216f4b37a38f999518575
yujamint <woawoa0513@gmail.com>
2023-03-25 22:31:14 +0900

    test: Direction 정적 팩토리 메서드 기능 테스트

--- src/test/java/chess/piece/DirectionTest.java
+++ src/test/java/chess/piece/DirectionTest.java
@@ -2,8 +2,12 @@ package chess.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class DirectionTest {
 
@@ -49,5 +53,34 @@ class DirectionTest {
         // when, then
         assertThat(rightDirection.isVertical()).isFalse();
         assertThat(upDirection.isVertical()).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateDirectionByDifference")
+    @DisplayName("x좌표, y좌표 차이에 따른 방향을 반환한다.")
+    void directionOf(int xDiff, int yDiff, Direction expectedDirection) {
+        // when, then
+        assertThat(Direction.of(xDiff, yDiff)).isSameAs(expectedDirection);
+    }
+
+    static Stream<Arguments> generateDirectionByDifference() {
+        return Stream.of(
+                Arguments.of(0, 1, Direction.UP),
+                Arguments.of(1, 1, Direction.UP_RIGHT),
+                Arguments.of(1, 0, Direction.RIGHT),
+                Arguments.of(1, -1, Direction.DOWN_RIGHT),
+                Arguments.of(0, -1, Direction.DOWN),
+                Arguments.of(-1, -1, Direction.DOWN_LEFT),
+                Arguments.of(-1, 0, Direction.LEFT),
+                Arguments.of(-1, 1, Direction.UP_LEFT),
+                Arguments.of(1, 2, Direction.UP_UP_RIGHT),
+                Arguments.of(-1, 2, Direction.UP_UP_LEFT),
+                Arguments.of(2, 1, Direction.RIGHT_RIGHT_UP),
+                Arguments.of(2, -1, Direction.RIGHT_RIGHT_DOWN),
+                Arguments.of(1, -2, Direction.DOWN_DOWN_RIGHT),
+                Arguments.of(-1, -2, Direction.DOWN_DOWN_LEFT),
+                Arguments.of(-2, 1, Direction.LEFT_LEFT_UP),
+                Arguments.of(-2, -1, Direction.LEFT_LEFT_DOWN)
+        );
     }
 }
