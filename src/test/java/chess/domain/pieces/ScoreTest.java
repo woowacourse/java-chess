46a2f8d71d8d08528f702c03372800a0cbd5cd61
kdkdhoho <hkim4410@naver.com>
2023-03-25 16:21:49 +0900

    test(ScoreTest): 테스트 코드 수정

--- src/test/java/chess/domain/pieces/ScoreTest.java
+++ src/test/java/chess/domain/pieces/ScoreTest.java
@@ -20,7 +20,7 @@ class ScoreTest {
     @DisplayName("논리적으로 같은 객체가 된다")
     void 논리적으로_같은_객체가_된다() {
         Score score = new Score(30.0);
-        Score otherScore = new Score(30);
+        Score otherScore = new Score(30.0);
 
         assertThat(score).isEqualTo(otherScore);
     }
