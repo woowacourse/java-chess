20b5df53a4b2eff9924067a6ca0e1933090f884a
yujamint <woawoa0513@gmail.com>
2023-03-26 04:47:46 +0900

    feat(OutputView): 게임 결과 출력 기능 구현

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,6 +1,7 @@
 package chess.view;
 
 import chess.board.dto.BoardDto;
+import chess.piece.Side;
 import java.util.List;
 
 public class OutputView {
@@ -12,7 +13,7 @@ public class OutputView {
 
     public static void printGameCommandInputMessage() {
         printMessage("> 게임 시작 : start" + NEWLINE
-                + "> 게임 종료 : end" + NEWLINE
+                + "> 게임 중단 : end" + NEWLINE
                 + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
@@ -34,9 +35,15 @@ public class OutputView {
         System.out.println(e.getMessage());
     }
 
-    public static void printStatusCommandInputMessage() {
+    public static void printCommandAfterGameEndInputMessage() {
         printMessage("> 게임 결과 확인 : status" + NEWLINE
                 + "> 게임 종료 : end"
         );
+    }
+
+    public static void printGameResult(final Side winner, final double whiteScore, final double blackScore) {
+        System.out.printf("승자 : %s%n"
+                + "흰색 진영 : %.1f점%n"
+                + "검은색 진영 : %.1f점", winner, whiteScore, blackScore);
     }
 }
