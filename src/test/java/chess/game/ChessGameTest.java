b955760b66779024ad9b313148ca64048ae10f4f
yujamint <woawoa0513@gmail.com>
2023-03-26 03:42:55 +0900

    test(ChessGameTest): 킹 죽음에 따른 게임 종료 기능 테스트

--- src/test/java/chess/game/ChessGameTest.java
+++ src/test/java/chess/game/ChessGameTest.java
@@ -11,8 +11,11 @@ import chess.controller.GameStatus;
 import chess.piece.AllPiecesGenerator;
 import chess.piece.Pieces;
 import chess.piece.Side;
+import chess.piece.type.King;
 import chess.piece.type.Knight;
 import chess.piece.type.Pawn;
+import chess.piece.type.Queen;
+import chess.piece.type.Rook;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -69,5 +72,32 @@ class ChessGameTest {
 
         // when, then
         assertThat(chessGame.calculateScoreBySide(Side.WHITE)).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("킹이 죽으면 게임이 끝난다.")
+    void gameEnd_kingDead() {
+        // given
+        Board fixedBoard = new Board(new Pieces(() -> List.of(
+                new King(new Position(File.E, Rank.ONE), Side.WHITE),
+                new Rook(new Position(File.A, Rank.ONE), Side.WHITE),
+                new Knight(new Position(File.B, Rank.ONE), Side.WHITE),
+
+                new King(new Position(File.A, Rank.EIGHT), Side.BLACK),
+                new Queen(new Position(File.D, Rank.EIGHT), Side.BLACK),
+                new Pawn(new Position(File.B, Rank.SEVEN), Side.BLACK)
+        )));
+        final ChessGame chessGame = new ChessGame(fixedBoard, Side.WHITE, GameStatus.START);
+        final GameStatus blackKingAliveStatus = chessGame.status();
+
+        // when
+        final Position whiteRookPosition = new Position(File.A, Rank.ONE);
+        final Position blackKingPosition = new Position(File.A, Rank.EIGHT);
+        chessGame.movePiece(whiteRookPosition, blackKingPosition);
+        final GameStatus blackKingDeadStatus = chessGame.status();
+
+        // then
+        assertThat(blackKingAliveStatus).isSameAs(GameStatus.START);
+        assertThat(blackKingDeadStatus).isSameAs(GameStatus.END);
     }
 }
