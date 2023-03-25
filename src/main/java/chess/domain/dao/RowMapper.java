7905133ecd969849cbb6204c49ea57abe8544102
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-03-26 02:33:56 +0900

    Refactor(PieceDaoImpl): RowMapper 적용

---
+++ src/main/java/chess/domain/dao/RowMapper.java
@@ -0,0 +1,13 @@
+package chess.domain.dao;
+
+import chess.domain.Piece;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+//@FunctionalInterface
+public interface RowMapper {
+
+    Piece run(final ResultSet rs) throws SQLException;
+    
+}
