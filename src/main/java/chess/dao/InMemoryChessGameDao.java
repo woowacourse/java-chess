e485f046f7ba4f075abfb8c4a8460c6fabae48f1
hectick <chyeon1223@cau.ac.kr>
2023-03-26 02:18:20 +0900

    feat(InMemoryChessGameDao): 데이터베이스가 연결 안된 상태에서도 Application 을 실행할 수 있도록 DBChessGameDao 대신 바꿔 낄 클래스 생성

---
+++ src/main/java/chess/dao/InMemoryChessGameDao.java
@@ -0,0 +1,28 @@
+package chess.dao;
+
+import chess.domain.ChessGame;
+
+public class InMemoryChessGameDao implements ChessGameDao{
+
+    private ChessGame chessGame;
+
+    @Override
+    public void save(ChessGame chessGame) {
+        this.chessGame = chessGame;
+    }
+
+    @Override
+    public ChessGame select() {
+        return chessGame;
+    }
+
+    @Override
+    public void update(ChessGame chessGame) {
+        this.chessGame = chessGame;
+    }
+
+    @Override
+    public void delete() {
+        this.chessGame = null;
+    }
+}
