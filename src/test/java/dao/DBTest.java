b1e73b5d964631f99f8ce6058c84944e76b78fb5
Eunkeee <eunkeeeee@gmail.com>
2023-03-25 14:57:46 +0900

    feat(DB): 데이터베이스 연결

---
+++ src/test/java/dao/DBTest.java
@@ -0,0 +1,20 @@
+package dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class DBTest {
+
+    private final DB db = new DB();
+
+    @Test
+    public void test_getConnection() {
+        try (final var connection = db.getConnection()) {
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
