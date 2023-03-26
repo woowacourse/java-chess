2fce195b9b2b5fe08082b641575a5e04763dd410
hanueleee <ezzeezz@naver.com>
2023-03-27 01:17:15 +0900

    feat(view): 입장하고자 하는 게임방의 id를 입력받는 기능 구현

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -6,12 +6,19 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
-    
+    private static final String GAME_ID_MESSAGE = "게임방 목록 : ";
     private static final Scanner scanner = new Scanner(System.in);
     private static final String DELIMITER = " ";
     
     public static List<String> readCommand() {
         String input = scanner.nextLine();
         return Arrays.stream(input.split(DELIMITER)).collect(Collectors.toList());
+    }
+
+    public static int readGameId(List<Integer> ids) {
+        List<String> stringIds = ids.stream().map(id -> id.toString()).collect(Collectors.toList());
+        System.out.println(GAME_ID_MESSAGE + String.join(", ", stringIds));
+        String input = scanner.nextLine();
+        return Integer.parseInt(input);
     }
 }
