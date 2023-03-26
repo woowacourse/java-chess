85075d90e9258cc12b382567f4481427bcccc0ea
jyeost <pig9619@naver.com>
2023-03-27 00:40:24 +0900

    feat: db 연결

---
+++ src/test/java/db/UserDaoTest.java
@@ -0,0 +1,40 @@
+package db;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+class UserDaoTest {
+
+    private final UserDao userDao = new UserDao();
+
+    @Test
+    public void connection() throws SQLException {
+        try (final var connection = userDao.getConnection()) {
+            assertThat(connection).isNotNull();
+        }
+    }
+
+    @Test
+    public void addUser() {
+        final var user = new User("testUserId", "testUser");
+        userDao.addUser(user);
+    }
+
+    @Test
+    public void findByUserId() {
+        final var user = userDao.findByUserId("testUserId");
+
+        assertThat(user).isEqualTo(new User("testUserId", "testUser"));
+    }
+
+    @Test
+    public void updateUserId() {
+        final var user = new User("testUserId", "testUser");
+        userDao.updateUserId(user, "kero2");
+
+        assertThat(userDao.findByUserId("kero")).isNotNull();
+    }
+}
