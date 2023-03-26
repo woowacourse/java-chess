ef0c7f7e5ca34d2418b8da53a5588529108a3080
yujamint <woawoa0513@gmail.com>
2023-03-26 22:08:25 +0900

    test(MySqlPiecesDaoTest): Connection 생성 테스트

--- src/test/java/chess/dao/MySqlPiecesDaoTest.java
+++ src/test/java/chess/dao/MySqlPiecesDaoTest.java
@@ -6,13 +6,13 @@ import java.sql.Connection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class DbTest {
+class MySqlPiecesDaoTest {
 
     @Test
     @DisplayName("데이터베이스 커넥션 객체를 생성한다.")
     void getConnection() {
         // given
-        Db db = new Db();
+        MySqlPiecesDao db = new MySqlPiecesDao();
 
         // when, then
         assertThat(db.getConnection()).isInstanceOf(Connection.class);
