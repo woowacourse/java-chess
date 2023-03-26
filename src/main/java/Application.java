bc49efd40de7acdf4b8d563990a0fc8bdde725b1
splitube <voicesplit@daum.net>
2023-03-27 02:23:32 +0900

    feat: Application 작성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,12 @@
 import controller.ChessController;
 import domain.ChessGame;
-import view.InputView;
-import view.OutputView;
+import domain.board.ChessBoard;
 
 public final class Application {
 
     public static void main(String[] args) {
-        final ChessController controller = new ChessController(new ChessGame(), new InputView(), new OutputView());
-        controller.play();
+        final ChessController chessController = new ChessController(new ChessGame(new ChessBoard()));
+        chessController.ready();
+        chessController.play();
     }
 }
