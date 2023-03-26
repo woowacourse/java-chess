cc3c4a9b46c27ae72ce25b5021fc14c8f06ff4da
이건회 <rg970604@naver.com>
2023-03-27 04:58:15 +0900

    test : 테스트 추가

--- src/test/java/chess/board/ChessBoardTest.java
+++ src/test/java/chess/board/ChessBoardTest.java
@@ -1,16 +1,19 @@
 package chess.board;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import chess.domain.board.ChessBoard;
 import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Rook;
 import chess.domain.piece.Team;
 import chess.domain.piece.coordinate.Coordinate;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -24,21 +27,21 @@ class ChessBoardTest {
     
     @Test
     void 체스보드를_생성하면_8개의_행이_생성된다() {
-        Assertions.assertThat(chessBoard.chessBoard()).hasSize(8);
+        assertThat(chessBoard.chessBoard()).hasSize(8);
     }
     
     @Test
     void 경로에_기물이_있으면_이동_불가능하다() {
         chessBoard.move(Coordinate.createCoordinate("1","a"), Coordinate.createCoordinate("3","a"));
         Piece piece = chessBoard.chessBoard().get(5).pieces().get(0);
-        Assertions.assertThat(piece.isSameTeam(Team.EMPTY)).isTrue();
+        assertThat(piece.isSameTeam(Team.EMPTY)).isTrue();
     }
     
     @Test
     void 경로에_기물이_없으면_이동_가능하다() {
         chessBoard.move(Coordinate.createCoordinate("2","a"), Coordinate.createCoordinate("4","a"));
         Piece piece = chessBoard.chessBoard().get(4).pieces().get(0);
-        Assertions.assertThat(piece).isEqualTo(new Pawn(Team.BLACK, Coordinate.createCoordinate("4","a")));
+        assertThat(piece).isEqualTo(new Pawn(Team.BLACK, Coordinate.createCoordinate("4","a")));
     }
     
     @Test
@@ -48,6 +51,32 @@ class ChessBoardTest {
         chessBoard.move(Coordinate.createCoordinate("3","a"), Coordinate.createCoordinate("3","b"));
         chessBoard.move(Coordinate.createCoordinate("3","b"), Coordinate.createCoordinate("7","b"));
         Piece piece = chessBoard.chessBoard().get(1).pieces().get(1);
-        Assertions.assertThat(piece).isEqualTo(new Rook(Team.BLACK, Coordinate.createCoordinate("7", "b")));
+        assertThat(piece).isEqualTo(new Rook(Team.BLACK, Coordinate.createCoordinate("7", "b")));
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"BLACK,true","WHITE,false"})
+    void 특정_팀의_왕이_살아있는지_확인(Team team, boolean expected){
+        ChessBoard testChessBoard = ChessBoard.create();
+        testChessBoard.move(Coordinate.createCoordinate("2","d"),Coordinate.createCoordinate("4","d"));
+        testChessBoard.move(Coordinate.createCoordinate("1","d"),Coordinate.createCoordinate("3","d"));
+        testChessBoard.move(Coordinate.createCoordinate("3","d"),Coordinate.createCoordinate("3","e"));
+        testChessBoard.move(Coordinate.createCoordinate("3","e"),Coordinate.createCoordinate("7","e"));
+        testChessBoard.move(Coordinate.createCoordinate("7","e"),Coordinate.createCoordinate("8","e"));
+        assertThat(testChessBoard.isKingAlive(team)).isEqualTo(expected);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"BLACK,38","WHITE,37"})
+    void 특정_팀의_점수_계산(Team team, double expected){
+        ChessBoard testChessBoard = ChessBoard.create();
+        testChessBoard.move(Coordinate.createCoordinate("2","d"),Coordinate.createCoordinate("4","d"));
+        testChessBoard.move(Coordinate.createCoordinate("1","d"),Coordinate.createCoordinate("3","d"));
+        testChessBoard.move(Coordinate.createCoordinate("3","d"),Coordinate.createCoordinate("3","e"));
+        testChessBoard.move(Coordinate.createCoordinate("3","e"),Coordinate.createCoordinate("7","e"));
+        assertThat(testChessBoard.calculateFinalPointsByTeam(team)).isEqualTo(expected);
+    }
+
+
 }
--- src/test/java/chess/board/RowPiecesTest.java
+++ src/test/java/chess/board/RowPiecesTest.java
@@ -1,14 +1,19 @@
 package chess.board;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import chess.domain.board.RowPieces;
+import chess.domain.piece.Team;
+import chess.domain.piece.coordinate.Column;
 import chess.domain.piece.coordinate.Coordinate;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class RowPiecesTest {
 
     private RowPieces rowPieces;
@@ -39,6 +44,27 @@ class RowPiecesTest {
 
         assertThat(sourceRowPieces.isMovable(targetRowPieces, sourceCoordinate, targetCoordinate)).isTrue();
 
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"8,WHITE,30","7,WHITE,8","1,BLACK,30","2,BLACK,8"})
+    void 한_행의_전체_점수_합을_구한다(String rowNum, Team team, double point){
+        RowPieces testRowPieces = new RowPieces(rowNum);
+        assertThat(testRowPieces.sumPiecePoints(team)).isEqualTo(point);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,BLACK,false","2,BLACK,true","8,WHITE,false","7,WHITE,true"})
+    void 해당_행의_특정_열에_폰이_있는지_확인한다(String rowNum,Team team,boolean expected){
+        RowPieces testRowPieces = new RowPieces(rowNum);
+        assertThat(testRowPieces.checkPawnByColumn(Column.A,team)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,BLACK,true","1,WHITE,false","8,WHITE,true","8,BLACK,false"})
+    void 해당_행에_특정_팀의_왕이_살아있는지_확인한다(String rowNum,Team team,boolean expected){
+        RowPieces testRowPieces = new RowPieces(rowNum);
+        assertThat(testRowPieces.isContainsKing(team)).isEqualTo(expected);
     }
 
 }
