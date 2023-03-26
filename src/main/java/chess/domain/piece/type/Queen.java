edc94ec19f951095a63ba776b5157489c3631260
zillionme <beatus96@naver.com>
2023-03-26 09:10:52 +0900

    refactor: Queen의 이동가능 여부 boolean으로 판단하도록 수정

--- src/main/java/chess/domain/piece/type/Queen.java
+++ src/main/java/chess/domain/piece/type/Queen.java
@@ -2,41 +2,22 @@ package chess.domain.piece.type;
 
 import chess.domain.board.Position;
 import chess.domain.piece.Color;
-import chess.domain.piece.Direction;
 import chess.domain.piece.PieceType;
 
-import java.util.List;
-
-public class Queen extends Piece {
-
-    public static final String DIRECTION_ERROR_MESSAGE = "Queen이 이동할 수 있는 방향이 아닙니다";
-    public static final String MOVE_ERROR_MESSAGE = "Queen은 도착점에 아군이 있으면 이동할 수 없습니다";
-    private static final List<Direction> movableDirection = List.of(
-            Direction.TOP, Direction.BOTTOM, Direction.LEFT, Direction.RIGHT,
-            Direction.TOP_LEFT, Direction.TOP_RIGHT, Direction.BOTTOM_LEFT, Direction.BOTTOM_RIGHT);
-
+public class Queen extends NonPawnPiece {
 
     public Queen(Color color) {
         super(PieceType.QUEEN, color);
     }
 
     @Override
-    public void checkMovable(final Position start, final Position end, final Color destinationColor) {
-        Direction direction = Direction.findDirectionByGap(start, end);
-        checkMovableDirection(direction);
-        checkMovableToDestination(destinationColor);
+    protected boolean isMovableMove(final Position start, final Position end) {
+        int absx = Math.abs(start.findGapOfColumn(end));
+        int absy = Math.abs(start.findGapOfRank(end));
+        if(absx == 0 && absy == 0) {
+            return false;
+        }
+        return (absx == 0 || absy ==0) || absx == absy;
     }
 
-    @Override
-    protected void checkMovableDirection(Direction direction) {
-        if(!movableDirection.contains(direction)){
-            throw new IllegalArgumentException(DIRECTION_ERROR_MESSAGE);
-        }
-    }
-
-    protected void checkMovableToDestination(Color colorOfDestination) {
-        if(color.isSameColor(colorOfDestination)) {
-            throw new IllegalArgumentException(MOVE_ERROR_MESSAGE);
-        }
-    }
 }
--- src/test/java/chess/domain/piece/type/QueenTest.java
+++ src/test/java/chess/domain/piece/type/QueenTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 @SuppressWarnings({"NonAsciiCharacters","SpellCheckingInspection"})
@@ -24,9 +23,8 @@ class QueenTest {
         Position end = Position.of(endColumn, endRank);
         Color colorOfDestination = Color.NONE;
 
-        assertThatThrownBy(() -> queen.checkMovable(start, end, colorOfDestination))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Queen이 이동할 수 있는 방향이 아닙니다");
+        assertThat(queen.isMovable(start, end, colorOfDestination))
+                .isFalse();
 
     }
 
@@ -36,9 +34,8 @@ class QueenTest {
         Position end = Position.of(Column.C, Rank.FIVE);
         Color colorOfDestination = Color.BLACK;
 
-        assertThatThrownBy(() -> queen.checkMovable(start, end, colorOfDestination))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Queen은 도착점에 아군이 있으면 이동할 수 없습니다");
+        assertThat(queen.isMovable(start, end, colorOfDestination))
+                .isFalse();
 
     }
 
@@ -48,7 +45,7 @@ class QueenTest {
         Position end = Position.of(Column.C, Rank.FIVE);
         Color colorOfDestination = Color.NONE;
 
-        assertDoesNotThrow(()->queen.checkMovable(start, end, colorOfDestination));
+        assertThat(queen.isMovable(start, end, colorOfDestination))
+                .isTrue();
     }
-
 }
