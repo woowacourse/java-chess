b4961827c1d259b57a9bb8364daa96cdff1192f4
yenawee <yaena0319@naver.com>
2023-03-27 04:04:04 +0900

    feat : ChessGameDao 인터페이스 생성, DB 연결 전 InMemomryChessGameDao 사용

---
+++ src/main/java/chess/dao/InMemoryChessGameDao.java
@@ -0,0 +1,24 @@
+package chess.dao;
+
+import chess.domain.game.ChessGame;
+
+public class InMemoryChessGameDao implements ChessGameDao {
+    private ChessGame chessGame;
+
+    @Override
+    public void save(ChessGame chessGame) {
+        this.chessGame = chessGame;
+
+    }
+
+    @Override
+    public ChessGame select() {
+        return chessGame;
+    }
+
+    @Override
+    public void update(ChessGame chessGame) {
+        this.chessGame = chessGame;
+
+    }
+}
