b7b9fb432bdf0599a3a408ddf17cc85e54e90858
jjongwa <troas96@naver.com>
2023-03-26 20:17:40 +0900

    test: 보드의 좌표에 존재하는 기물의 종류를 찾아 반환하는 테스트

--- src/test/java/chess/database/ChessBoardDaoTest.java
+++ src/test/java/chess/database/ChessBoardDaoTest.java
@@ -1,5 +1,7 @@
 package chess.database;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,32 @@ class ChessBoardDaoTest {
     void addPieceInBoardTable() {
         ChessBoardDao chessBoardDao = new ChessBoardDao();
         assertDoesNotThrow(() -> chessBoardDao.addBoard(1, 2, 2, "p"));
+    }
+
+    @Test
+    void findPieceInBoardTable() {
+        ChessBoardDao chessBoardDao = new ChessBoardDao();
+        assertAll(
+                () -> assertThat(chessBoardDao.findPiece(13, 1, 1)).isEqualTo("ROOK"),
+                () -> assertThat(chessBoardDao.findPiece(13, 2, 1)).isEqualTo("KNIGHT"),
+                () -> assertThat(chessBoardDao.findPiece(13, 3, 1)).isEqualTo("BISHOP"),
+                () -> assertThat(chessBoardDao.findPiece(13, 4, 1)).isEqualTo("QUEEN"),
+                () -> assertThat(chessBoardDao.findPiece(13, 5, 1)).isEqualTo("KING"),
+                () -> assertThat(chessBoardDao.findPiece(13, 6, 1)).isEqualTo("BISHOP"),
+                () -> assertThat(chessBoardDao.findPiece(13, 7, 1)).isEqualTo("KNIGHT"),
+                () -> assertThat(chessBoardDao.findPiece(13, 8, 1)).isEqualTo("ROOK"),
+                () -> assertThat(chessBoardDao.findPiece(13, 1, 2)).isEqualTo("PAWN"),
+
+                () -> assertThat(chessBoardDao.findPiece(13, 1, 8)).isEqualTo("ROOK"),
+                () -> assertThat(chessBoardDao.findPiece(13, 2, 8)).isEqualTo("KNIGHT"),
+                () -> assertThat(chessBoardDao.findPiece(13, 3, 8)).isEqualTo("BISHOP"),
+                () -> assertThat(chessBoardDao.findPiece(13, 4, 8)).isEqualTo("QUEEN"),
+                () -> assertThat(chessBoardDao.findPiece(13, 5, 8)).isEqualTo("KING"),
+                () -> assertThat(chessBoardDao.findPiece(13, 6, 8)).isEqualTo("BISHOP"),
+                () -> assertThat(chessBoardDao.findPiece(13, 7, 8)).isEqualTo("KNIGHT"),
+                () -> assertThat(chessBoardDao.findPiece(13, 8, 1)).isEqualTo("ROOK"),
+                () -> assertThat(chessBoardDao.findPiece(13, 1, 2)).isEqualTo("PAWN")
+        );
     }
 
 }
