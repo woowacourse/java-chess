dbf5bd1a047e7b2edce205880396ab4c5dbdae61
yoondgu <doyou2ing@naver.com>
2023-03-26 14:28:26 +0900

    feat: db 수업 실습 코드 패키지 이동

--- src/main/java/techcourse/database/UserDao.java
+++ src/main/java/techcourse/database/UserDao.java
@@ -1,11 +1,11 @@
-package database;
+package techcourse.database;
 
-import chess.User;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import techcourse.User;
 
 public final class UserDao {
 
--- src/test/java/techcourse/database/UserDaoTest.java
+++ src/test/java/techcourse/database/UserDaoTest.java
@@ -1,12 +1,12 @@
-package database;
+package techcourse.database;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import chess.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import techcourse.User;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class UserDaoTest {
