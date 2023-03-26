719c3d4833c29dd308c8798cc3ea9906a68ae126
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-26 23:01:19 +0900

    feat: 체스를 움직일 때 사용하는 MoveCommand 클래스 생성

--- src/main/java/chess/controller/MoveCommand.java
+++ src/main/java/chess/controller/MoveCommand.java
@@ -1,17 +1,30 @@
 package chess.controller;
 
 import chess.domain.ChessGame;
+import chess.view.PositionMapper;
 
 import java.util.List;
 
-public class MoveCommand extends Command{
+public class MoveCommand extends Command {
 
-    public MoveCommand(ChessGame chessGame, CommandType commandType) {
-        super(chessGame, commandType);
+    private static final int SOURCE_POSITION_INDEX = 1;
+    private static final int TARGET_POSITION_INDEX = 2;
+
+    protected MoveCommand(ChessGame chessGame) {
+        super(chessGame, CommandType.MOVE);
     }
 
     @Override
     public Command execute(List<String> input) {
-        return null;
+        CommandType inputCommandType = CommandType.from(input);
+        if (inputCommandType == CommandType.MOVE) {
+            chessGame.movePiece(PositionMapper.from(input.get(SOURCE_POSITION_INDEX)),
+                    PositionMapper.from(input.get(TARGET_POSITION_INDEX)));
+            return new MoveCommand(getChessGame());
+        }
+        if (inputCommandType == CommandType.END) {
+            return null;
+        }
+        throw new IllegalArgumentException("잘못된 명령어를 입력하셨습니다.");
     }
 }
--- src/test/java/chess/controller/MoveCommandTest.java
+++ src/test/java/chess/controller/MoveCommandTest.java
@@ -1,4 +1,108 @@
+package chess.controller;
+
+import chess.domain.ChessGame;
+import chess.domain.board.BoardFactory;
+import chess.domain.board.Position;
+import chess.domain.piece.Empty;
+import chess.domain.piece.Pawn;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static chess.domain.PositionFixture.B_2;
+import static chess.domain.PositionFixture.B_3;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
 class MoveCommandTest {
-  
+
+    private ChessGame chessGame;
+
+    @BeforeEach
+    void init() {
+        chessGame = new ChessGame(BoardFactory.createBoard());
+    }
+
+    @Test
+    void MoveCommand의_타입을_확인할_수_있다() {
+
+        Command moveCommand = new MoveCommand(chessGame);
+
+        assertThat(moveCommand.isSameType(CommandType.MOVE)).isTrue();
+    }
+
+    @Test
+    void moveCommand의_ChessGame판을_확인할_수_있다() {
+        Command moveCommand = new MoveCommand(chessGame);
+
+        assertThat(moveCommand.getChessGame().getBoard().findPiece(B_2)).isInstanceOf(Pawn.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"move b2 b3", "move   b2 b3 ", " move b2 b3", "move b2   b3   ", " Move b2 b3  "})
+    void move를_입력받으면_MoveCommand_객체가_반환된다(String command) {
+        Command moveCommand = new MoveCommand(chessGame);
+        List<String> input = Arrays.stream(command.split(" "))
+                .map(String::trim)
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.toList());
+
+        Command result = moveCommand.execute(input);
+
+
+        assertAll(
+                () ->  assertThat(result.isSameType(CommandType.MOVE)).isTrue(),
+                () -> assertThat(result.getChessGame().getBoard().findPiece(B_3)).isInstanceOf(Pawn.class),
+                () -> assertThat(result.getChessGame().getBoard().findPiece(B_2)).isInstanceOf(Empty.class)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"move a2", "MOVE t3"})
+    void move명령어는_출발지와_도착지에_대한_정보를_입력받지_않으면_예외가_발생한다(String command) {
+        Command moveCommand = new MoveCommand(chessGame);
+        List<String> input = Arrays.stream(command.split(" "))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        assertThatThrownBy(() -> moveCommand.execute(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("move 명령어는 '도착지'와 '출발지'에 대한 정보를 입력해야합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"move a3 t2", "move t2 y3"})
+    void move명령어는_잘못된_열_정보를_입력받으면_예외가_발생한다(String command) {
+        Command moveCommand = new MoveCommand(chessGame);
+        List<String> input = Arrays.stream(command.split(" "))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        assertThatThrownBy(() -> moveCommand.execute(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 열 번호를 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"move aa a2", "move a9 a7"})
+    void move명령어는_잘못된_행_정보를_입력받으면_예외가_발생한다(String command) {
+        Command moveCommand = new MoveCommand(chessGame);
+        List<String> input = Arrays.stream(command.split(" "))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        assertThatThrownBy(() -> moveCommand.execute(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 행 번호를 입력해주세요.");
+    }
 }
