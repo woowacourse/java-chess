ec032ea221ecf4884dd69fc47be6d01bf9d8c8d8
jiwon <ljwon77@daum.net>
2023-03-26 17:38:29 +0900

    feat: 쿼리에 따라 1개의 결과를 조회하는 기능을 추가한다

---
+++ src/main/java/chess/database/JdbcTemplate.java
@@ -0,0 +1,49 @@
+package chess.database;
+
+import chess.dao.mapper.RowMapper;
+import chess.database.properties.ChessProperties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public final class JdbcTemplate {
+    private final ChessProperties chessProperties;
+
+    public JdbcTemplate(final ChessProperties chessProperties) {
+        this.chessProperties = chessProperties;
+    }
+
+    public <T> Optional<T> findOne(final String query,
+                                   final RowMapper<T> rowMapper,
+                                   final String... params) {
+        try (final Connection connection = getConnection();
+             final PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setString(i + 1, params[i]);
+            }
+
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                return Optional.empty();
+            }
+            return Optional.ofNullable(rowMapper.mapRow(resultSet));
+        } catch (SQLException e) {
+            System.out.println("SQL Exception! = " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private Connection getConnection() {
+        try {
+            return DriverManager.getConnection(chessProperties.getUrl(),
+                    chessProperties.getUserName(), chessProperties.getPassword());
+        } catch (final SQLException e) {
+            System.out.println("DB Connection Exception! = " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
