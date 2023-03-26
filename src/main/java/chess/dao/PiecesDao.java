17cb62d2855b56a3a1cd7291955ce101c3afafb5
yujamint <woawoa0513@gmail.com>
2023-03-26 22:07:42 +0900

    feat(PiecesDao): getConnection 추상 메서드 정의

--- src/main/java/chess/dao/PiecesDao.java
+++ src/main/java/chess/dao/PiecesDao.java
@@ -1,9 +1,12 @@
 package chess.dao;
 
 import chess.domain.piece.type.Piece;
+import java.sql.Connection;
 import java.util.List;
 
 public interface PiecesDao {
+    Connection getConnection();
+
     List<Piece> findAll();
 
     void insertAll(List<Piece> pieces);
