402f5267a77b9194a924c8c5f99fc99c00dea955
Youngho <cyh6099@gmail.com>
2023-03-27 00:56:44 +0900

    feat: Application작성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,12 @@
 import controller.ChessController;
+import dao.ChessGameDao;
+import dao.DbConnectionGenerator;
+import service.ChessGameService;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        new ChessController().run();
+        new ChessController(new InputView(), new OutputView(), new ChessGameService(new ChessGameDao(new DbConnectionGenerator()))).run();
     }
 }
