e10987ea0b491bb5179d666ed18bb7fd9515bf1e
kevstevie <jianna442@gmail.com>
2023-03-26 12:51:49 +0900

    refactor: status 출력 구문 추가

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -21,6 +21,7 @@ public final class OutputView {
         System.out.println("> 체스 게임을 시작합니다.");
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 불러오기 : load game번호 - 예. load 2");
+        System.out.println("> 게임 점수 : status");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
