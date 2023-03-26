4e6eef00d003578df473cc74f1de01811f103a6c
Youngho <cyh6099@gmail.com>
2023-03-27 00:53:46 +0900

    test: 쿼리 관련 테스트 작성

--- src/test/java/dao/ChessDaoTest.java
+++ src/test/java/dao/ChessDaoTest.java
@@ -1,13 +1,33 @@
 package dao;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 class ChessDaoTest {
-    @DisplayName("db connection확인")
+
+    private final ChessGameDao chessGameDao = new ChessGameDao(new TestDbConnectionGenerator());
+
+    @DisplayName("새로운 게임 방을 만든다.")
     @Test
-    void connectionTest() {
-        Assertions.assertThat(new ChessGameDao().getConnection()).isNotNull();
+    void newGameTest() {
+        assertThatCode(() -> chessGameDao.createRoom())
+                .doesNotThrowAnyException();
     }
+
+    @DisplayName("존재하지 않는 pk인 0으로 게임 방을 조회하려고 하면 예외가 발생한다.")
+    @Test
+    void nonExistGameTest() {
+        assertThat(chessGameDao.hasGame(0L)).isFalse();
+    }
+
+    @DisplayName("모든 게임 방을 조회한다.")
+    @Test
+    void findAllGameRoomsTest() {
+        assertThatCode(() -> chessGameDao.findAllGameRooms())
+                .doesNotThrowAnyException();
+    }
+
 }
