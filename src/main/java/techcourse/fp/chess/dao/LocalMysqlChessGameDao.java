4c97cf000e4e8f40c5fa600d97f8c55b4e44ecfb
hyunseo <ohseo8813@gmail.com>
2023-03-26 15:01:06 +0900

    refactor: dao 코드 메소드 추출로 가독성 향상

--- src/main/java/techcourse/fp/chess/dao/LocalMysqlChessGameDao.java
+++ src/main/java/techcourse/fp/chess/dao/LocalMysqlChessGameDao.java
@@ -43,83 +43,104 @@ public class LocalMysqlChessGameDao implements ChessGameDao {
 
     @Override
     public void save(final ChessGameRequest chessGameRequest) {
-        final String query = "INSERT INTO chess_game(name, turn) VALUES (?,?);";
-        final String query2 = "INSERT INTO piece(chess_game_id, piece_file, piece_rank, color, type) VALUES (?, ?, ?, ?, ?);";
         try (final Connection connection = getConnection()) {
-            final PreparedStatement pstmt1 = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            pstmt1.setString(1, chessGameRequest.getName());
-            pstmt1.setString(2, chessGameRequest.getTurn());
-            pstmt1.executeUpdate();
-
-            final ResultSet rs = pstmt1.getGeneratedKeys();
-            int chessGameId = 0;
-            if (rs.next()) {
-                chessGameId = rs.getInt(1);
-            }
-
-            final Map<Position, Piece> board = chessGameRequest.getBoard();
-
-            for (Position position : board.keySet()) {
-                final PreparedStatement pstmt2 = connection.prepareStatement(query2);
-
-                final Piece piece = board.get(position);
-
-                pstmt2.setInt(1, chessGameId);
-                pstmt2.setString(2, position.getFile().name());
-                pstmt2.setString(3, position.getRank().name());
-                pstmt2.setString(4, piece.getColor().name());
-                pstmt2.setString(5, piece.getPieceType().name());
-                pstmt2.executeUpdate();
-            }
-
+            int chessGameId = saveChessGame(chessGameRequest, connection);
+            savePiece(chessGameRequest.getBoard(), connection, chessGameId);
 
         } catch (final SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    private int saveChessGame(final ChessGameRequest chessGameRequest, final Connection connection) throws SQLException {
+        final String query = "INSERT INTO chess_game(name, turn) VALUES (?,?);";
+        final PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, chessGameRequest.getName());
+        ps.setString(2, chessGameRequest.getTurn());
+        ps.executeUpdate();
+
+        return findChessGameId(ps);
+    }
+
+    private int findChessGameId(final PreparedStatement ps) throws SQLException {
+        final ResultSet rs = ps.getGeneratedKeys();
+
+        if (rs.next()) {
+            return rs.getInt(1);
+        }
+
+        throw new IllegalStateException();
+    }
+
+    private void savePiece(final Map<Position, Piece> board,
+                                  final Connection connection, final int chessGameId) throws SQLException {
+        final String query = "INSERT INTO piece(chess_game_id, piece_file, piece_rank, color, type) VALUES (?, ?, ?, ?, ?);";
+
+        for (Position position : board.keySet()) {
+            final PreparedStatement ps = connection.prepareStatement(query);
+
+            final Piece piece = board.get(position);
+
+            ps.setInt(1, chessGameId);
+            ps.setString(2, position.getFile().name());
+            ps.setString(3, position.getRank().name());
+            ps.setString(4, piece.getColor().name());
+            ps.setString(5, piece.getPieceType().name());
+
+            ps.executeUpdate();
+        }
+    }
+
     @Override
     public ChessGame findById(final long chessGameId) {
+        try (final Connection connection = getConnection()) {
+            Map<Position, Piece> board = createBoard(chessGameId, connection);
+            final Turn turn = getTurn(chessGameId, connection);
+
+            return new ChessGame(new Board(board, turn));
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    private Map<Position, Piece> createBoard(final long chessGameId, final Connection connection) throws SQLException {
         final String query = "SELECT piece_file, piece_rank, color, type " +
                 "FROM piece " +
                 "where chess_game_id = ?";
 
-        final String query2 = "SELECT turn FROM chess_game WHERE id = ?";
+        final PreparedStatement ps = connection.prepareStatement(query);
+        ps.setLong(1, chessGameId);
+        final ResultSet resultSet = ps.executeQuery();
 
-        try (final Connection connection = getConnection()) {
-            final PreparedStatement ps = connection.prepareStatement(query);
-            ps.setLong(1, chessGameId);
-            final ResultSet resultSet = ps.executeQuery();
+        Map<Position, Piece> board = new HashMap<>();
 
-            Map<Position, Piece> board = new HashMap<>();
+        while (resultSet.next()) {
+            final String file = resultSet.getString("piece_file");
+            final String rank = resultSet.getString("piece_rank");
+            final String color = resultSet.getString("color");
+            final String type = resultSet.getString("type");
 
-            while (resultSet.next()) {
-                final String file = resultSet.getString("piece_file");
-                final String rank = resultSet.getString("piece_rank");
+            final Piece piece = PieceFactory.generate(Color.createByName(color), PieceType.createByName(type));
 
-                final String color = resultSet.getString("color");
-                final String type = resultSet.getString("type");
-
-                final Piece piece = PieceFactory.generate(Color.createByName(color), PieceType.createByName(type));
-
-                board.put(Position.createByName(file, rank), piece);
-            }
-
-            final PreparedStatement ps2 = connection.prepareStatement(query2);
-            ps2.setLong(1, chessGameId);
-            final ResultSet resultSet2 = ps2.executeQuery();
-
-
-            if (resultSet2.next()) {
-                String turn = resultSet2.getString(1);
-                return new ChessGame(new Board(board, Turn.createByStartTurn(Color.createByName(turn))));
-            }
-
-            throw new IllegalStateException();
-
-        } catch (SQLException e) {
-            return null;
+            board.put(Position.createByName(file, rank), piece);
         }
+
+        return board;
+    }
+
+    private Turn getTurn(final long chessGameId, final Connection connection) throws SQLException {
+        final String query = "SELECT turn FROM chess_game WHERE id = ?";
+
+        final PreparedStatement ps = connection.prepareStatement(query);
+        ps.setLong(1, chessGameId);
+        final ResultSet resultSet = ps.executeQuery();
+
+        if (resultSet.next()) {
+            final String color = resultSet.getString(1);
+            return Turn.createByStartTurn(Color.createByName(color));
+        }
+
+        throw new IllegalStateException();
     }
 
 
@@ -144,7 +165,6 @@ public class LocalMysqlChessGameDao implements ChessGameDao {
             }
 
             return chessGameInfos;
-
 
         } catch (SQLException e) {
             throw new IllegalArgumentException(e.getMessage());
