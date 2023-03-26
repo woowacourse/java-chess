4d636ac8a62f95cf6194e0d99889824f172324a6
thdwoqor <celebarte01@gmail.com>
2023-03-26 23:48:30 +0900

    feat : Dao 적용으로 인한 StateProcessor 수정

--- src/main/java/chess/ChessGameApplication.java
+++ src/main/java/chess/ChessGameApplication.java
@@ -3,15 +3,21 @@ package chess;
 import chess.controller.CommandProcessor;
 import chess.controller.Controller;
 import chess.controller.ResultProcessor;
+import chess.dao.GameDao;
+import chess.dao.ConnectionGenerator;
 import chess.domain.state.StateProcessor;
 
+import java.sql.SQLException;
+
 public final class ChessGameApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
         CommandProcessor commandProcessor = CommandProcessor.create();
-        StateProcessor stateProcessor = StateProcessor.create();
+        StateProcessor stateProcessor = StateProcessor.from(new GameDao(ConnectionGenerator.getConnection()));
         ResultProcessor resultProcessor = ResultProcessor.create();
 
         Controller controller = new Controller(commandProcessor, stateProcessor, resultProcessor);
         controller.run();
+
     }
 }
--- src/main/java/chess/domain/state/StateProcessor.java
+++ src/main/java/chess/domain/state/StateProcessor.java
@@ -1,24 +1,41 @@
 package chess.domain.state;
 
+import chess.cache.PieceCache;
+import chess.dao.GameDao;
+import chess.domain.Board;
 import chess.domain.ChessGame;
 import chess.domain.Color;
 import chess.domain.Position;
 import chess.domain.piece.Piece;
 import chess.dto.CommandDto;
+import chess.dto.GameDto;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 public final class StateProcessor {
     private State state;
+    private final GameDao gameDao;
+    private final int gameId;
 
-    private StateProcessor(final State state) {
+    private StateProcessor(final State state, final GameDao gameDao, final int gameId) {
         this.state = state;
+        this.gameDao = gameDao;
+        this.gameId = gameId;
     }
 
-    public static StateProcessor create() {
-        final State state = new Ready(ChessGame.create());
+    public static StateProcessor from(GameDao gameDao) throws SQLException {
+        GameDto gameDto = gameDao.findByLastGame();
 
-        return new StateProcessor(state);
+        if (gameDto.isEnd()) {
+            final State state = new Ready(ChessGame.create());
+            gameDao.createGame(Board.from(PieceCache.create()).getBoard());
+            return new StateProcessor(state, gameDao, gameDto.getId() + 1);
+        }
+
+        Board board = Board.from(gameDao.findByLastGameBoard(gameDto.getId()));
+        final State state = new Ready(ChessGame.restart(board, Color.valueOf(gameDto.getColor())));
+        return new StateProcessor(state, gameDao, gameDto.getId());
     }
 
     public Map<Position, Piece> getBoard() {
@@ -43,7 +60,13 @@ public final class StateProcessor {
     }
 
     public State move(final CommandDto commandDto) {
-        return state.move(commandDto.getSource(), commandDto.getTarget());
+        State newState = state.move(commandDto.getSource(), commandDto.getTarget());
+        if (newState.isGameEnd()) {
+            gameDao.updateGameStatus(true, gameId);
+        }
+
+        gameDao.movePiece(state.chessGame.getBoard(), gameId, state.getColor());
+        return newState;
     }
 
     public State start() {
