013480ca51370ca63c11452bdd73d3860e8b8da1
drunkenhw <drunkenhw@gmail.com>
2023-03-26 17:33:50 +0900

    refactor: 상수로 변경

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -7,10 +7,13 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final int COMMAND_INDEX = 0;
+    private static final int SKIP_COMMAND_INDEX = 1;
+    private static final String DELIMITER = " ";
 
     public ChessRequest readGameCommand() {
-        List<String> list = Arrays.stream(scanner.nextLine().split(" "))
+        List<String> list = Arrays.stream(scanner.nextLine().split(DELIMITER))
                 .collect(Collectors.toList());
-        return new ChessRequest(list.get(0), list.subList(1, list.size()));
+        return new ChessRequest(list.get(COMMAND_INDEX), list.subList(SKIP_COMMAND_INDEX, list.size()));
     }
 }
