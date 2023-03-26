2bbaff8bdad4da1d10b27b8c937de0f868b9a4cf
drunkenhw <drunkenhw@gmail.com>
2023-03-26 17:41:46 +0900

    style: 주석 제거

--- src/main/java/chess/service/ChessGameService.java
+++ src/main/java/chess/service/ChessGameService.java
@@ -29,8 +29,6 @@ public class ChessGameService {
         return chessGame.getResult();
     }
 
-    //왕이 죽었다면 true를 반환하고 db를 deleteAll 하는 메서드 입니다.
-    // 이렇게 return과 작동 2가지 일을 하는 메서드도 괜찮을까요?
     public boolean isGameEnd() {
         if (chessGame.isEnd()) {
             clear();
