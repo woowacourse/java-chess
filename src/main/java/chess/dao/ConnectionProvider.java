66012a68a7b3cded7d49349365391a8247003621
seokjin8678 <seokjin8678@gmail.com>
2023-03-26 15:50:24 +0900

    refactor: 데이터베이스 설정 DatabaseConfig 클래스로 분리

--- src/main/java/chess/dao/ConnectionProvider.java
+++ src/main/java/chess/dao/ConnectionProvider.java
@@ -1,31 +1,15 @@
 package chess.dao;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.EnumMap;
-import java.util.Map;
-import java.util.stream.Stream;
 
 public class ConnectionProvider {
     private static final String DATABASE_CONNECTION_EXCEPTION_MESSAGE = "[ERROR] 데이터베이스 연결 오류";
     private static final String ALREADY_TRANSACTION_EXCEPTION_MESSAGE = "[ERROR] 이미 진행중인 트랜잭션이 있습니다.";
     private static final String INVALID_TRANSACTION_EXCEPTION_MESSAGE = "[ERROR] 진행중인 트랜잭션이 없습니다.";
-    private static final String INVALID_DATABASE_CONFIG_EXCEPTION_MESSAGE = "[ERROR] 데이터베이스 설정 파일이 없습니다.";
-    private static final Map<ConfigKey, String> DB_CONFIG = new EnumMap<>(ConfigKey.class);
-    private static Connection txConnection;
 
-    static {
-        try (Stream<String> lines = Files.lines(Path.of("src/main/resources/db_config.txt"))) {
-            lines.map(line -> line.split("=", 2))
-                    .forEach(line -> DB_CONFIG.put(ConfigKey.valueOf(line[0]), line[1]));
-        } catch (IOException e) {
-            throw new IllegalStateException(INVALID_DATABASE_CONFIG_EXCEPTION_MESSAGE, e);
-        }
-    }
+    private static Connection txConnection;
 
     private ConnectionProvider() {
     }
@@ -36,9 +20,9 @@ public class ConnectionProvider {
         }
         try {
             return DriverManager.getConnection(
-                    DB_CONFIG.get(ConfigKey.URL),
-                    DB_CONFIG.get(ConfigKey.USERNAME),
-                    DB_CONFIG.get(ConfigKey.PASSWORD));
+                    DatabaseConfig.getUrl(),
+                    DatabaseConfig.getUsername(),
+                    DatabaseConfig.getPassword());
         } catch (SQLException e) {
             throw new IllegalStateException(DATABASE_CONNECTION_EXCEPTION_MESSAGE, e);
         }
@@ -107,11 +91,5 @@ public class ConnectionProvider {
         } catch (SQLException e) {
             throw new IllegalStateException(DATABASE_CONNECTION_EXCEPTION_MESSAGE, e);
         }
-    }
-
-    private enum ConfigKey {
-        URL,
-        USERNAME,
-        PASSWORD
     }
 }
