17dc0470856a271151e8c4c3137e4eee32ac1f89
zillionme <beatus96@naver.com>
2023-03-26 09:07:35 +0900

    refactor: Rook의 이동가능 여부 boolean으로 판단하도록 수정

--- src/test/java/chess/domain/piece/type/RookTest.java
+++ src/test/java/chess/domain/piece/type/RookTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SuppressWarnings({"NonAsciiCharacters","SpellCheckingInspection"})
 class RookTest {
@@ -23,9 +22,8 @@ class RookTest {
         Position end = Position.of(endColumn, endRank);
         Color colorOfDestination = Color.NONE;
 
-        assertThatThrownBy(() -> rook.checkMovable(start, end, colorOfDestination))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Rook이 이동할 수 있는 방향이 아닙니다");
+        assertThat(rook.isMovable(start, end, colorOfDestination))
+                .isFalse();
 
     }
 
@@ -35,9 +33,8 @@ class RookTest {
         Position end = Position.of(Column.D, Rank.FIVE);
         Color colorOfDestination = Color.BLACK;
 
-        assertThatThrownBy(() -> rook.checkMovable(start, end, colorOfDestination))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Rook은 도착점에 아군이 있으면 이동할 수 없습니다");
+        assertThat(rook.isMovable(start, end, colorOfDestination))
+                .isFalse();
 
     }
 
@@ -47,7 +44,8 @@ class RookTest {
         Position end = Position.of(Column.D, Rank.FIVE);
         Color colorOfDestination = Color.NONE;
 
-        assertDoesNotThrow(() -> rook.checkMovable(start, end, colorOfDestination));
+        assertThat(rook.isMovable(start, end, colorOfDestination))
+                .isTrue();
 
     }
 
