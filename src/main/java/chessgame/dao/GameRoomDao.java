4f1eebbc86c2413505d2897d493e95787e563814
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-03-26 16:35:48 +0900

    feat: ID를 통해 DB의 체스 게임을 조회하는 기능 추가

--- src/main/java/chessgame/dao/GameRoomDao.java
+++ src/main/java/chessgame/dao/GameRoomDao.java
@@ -1,12 +1,14 @@
 package chessgame.dao;
 
 import chessgame.domain.chessgame.Camp;
+import chessgame.dto.GameRoomDto;
 
 import java.sql.SQLException;
 
 public class GameRoomDao {
 
     private static final String INSERT_GAME_ROOM_QUERY = "INSERT INTO game_rooms(turn) VALUES(?)";
+    private static final String FIND_GAME_ROOM_QUERY = "SELECT * FROM game_rooms WHERE id = ?";
 
     private final DatabaseConnection databaseConnection = new DatabaseConnection();
 
@@ -18,5 +20,22 @@ public class GameRoomDao {
         } catch (final SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public GameRoomDto findGameRoomById(long roomId) {
+        try (final var connection = databaseConnection.getConnection();
+             final var preparedStatement = connection.prepareStatement(FIND_GAME_ROOM_QUERY)) {
+            preparedStatement.setLong(1, roomId);
+            final var result = preparedStatement.executeQuery();
+            if (result.next()) {
+                return new GameRoomDto(
+                        result.getLong("id"),
+                        result.getString("turn")
+                );
+            }
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+        throw new IllegalArgumentException("[ERROR] 해당 아이디의 게임을 찾지 못했습니다.");
     }
 }
--- src/test/java/chessgame/dao/GameRoomDaoTest.java
+++ src/test/java/chessgame/dao/GameRoomDaoTest.java
@@ -1,8 +1,12 @@
 package chessgame.dao;
 
 import chessgame.domain.chessgame.Camp;
+import chessgame.dto.GameRoomDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class GameRoomDaoTest {
 
@@ -12,5 +16,21 @@ class GameRoomDaoTest {
     @DisplayName("화이트 팀의 차례로 시작해야 하는 게임 생성")
     void addRoom() {
         gameRoomDao.addRoom(Camp.WHITE);
+    }
+
+    @Test
+    @DisplayName("특정 아이디의 게임 데이터를 조회할 수 있다")
+    void findGameRoomById() {
+        GameRoomDto gameRoomDto = gameRoomDao.findGameRoomById(1);
+
+        assertThat(gameRoomDto.getRoomId()).isEqualTo(1);
+        assertThat(gameRoomDto.getTurn()).isEqualTo("WHITE");
+    }
+
+    @Test
+    @DisplayName("DB에 없는 아이디인 경우 예외를 발생시킨다")
+    void findGameRoomByIdException() {
+        assertThatThrownBy(() -> gameRoomDao.findGameRoomById(1000))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
