dd852956de3f0dd5242e22d5cfae1f101ce2e5c1
Youngho <cyh6099@gmail.com>
2023-03-27 00:55:44 +0900

    feat: connection생성 방식 변경

--- src/main/java/dao/ChessGameDao.java
+++ src/main/java/dao/ChessGameDao.java
@@ -12,28 +12,19 @@ import java.util.List;
 import java.util.Map;
 
 public class ChessGameDao {
-    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
-    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
-    private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
 
-    public Connection getConnection() {
-        // 드라이버 연결
-        try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
-        } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
+    private final ConnectionGenerator connectionGenerator;
+
+    public ChessGameDao(ConnectionGenerator connectionGenerator) {
+        this.connectionGenerator = connectionGenerator;
     }
 
     public Long createRoom() {
-        try (Connection connection = getConnection()) {
-            final String createQuery = "INSERT INTO game_room(status) VALUES(?)";
+        try (Connection connection = connectionGenerator.getConnection()) {
+            final String createQuery = "INSERT INTO game_room(status, current_turn) VALUES(?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(createQuery, Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, GameState.RUN.name());
+            preparedStatement.setString(2, Side.WHITE.name());
 
             preparedStatement.executeUpdate();
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
@@ -46,23 +37,22 @@ public class ChessGameDao {
         throw new IllegalArgumentException("게임 방 생성 실패");
     }
 
-    public void save(Map<Position, Piece> board, Side currentTurn, Long roomId) {
-        final String saveQuery = "INSERT INTO chess_board(piece_type, side, last_turn, piece_rank, piece_file, game_room_id_fk) VALUES(?,?,?,?,?,?)";
+    public void saveChessBoard(Map<Position, Piece> board, Side currentTurn, Long roomId) {
+        final String saveQuery = "INSERT INTO chess_board(piece_type, side, piece_rank, piece_file, game_room_id_fk) VALUES(?,?,?,?,?)";
         for (Map.Entry<Position, Piece> pieces : board.entrySet()) {
             File file = pieces.getKey().getFile();
             Rank rank = pieces.getKey().getRank();
             PieceType pieceType = pieces.getValue().getPieceType();
             Side pieceSide = pieces.getValue().getSide();
 
-            try (Connection connection = getConnection()) {
+            try (Connection connection = connectionGenerator.getConnection()) {
                 PreparedStatement preparedStatement = connection.prepareStatement(saveQuery);
 
                 preparedStatement.setString(1, pieceType.name());
                 preparedStatement.setString(2, pieceSide.name());
-                preparedStatement.setString(3, currentTurn.name());
-                preparedStatement.setString(4, rank.getText());
-                preparedStatement.setString(5, file.getText());
-                preparedStatement.setLong(6, roomId);
+                preparedStatement.setString(3, rank.getText());
+                preparedStatement.setString(4, file.getText());
+                preparedStatement.setLong(5, roomId);
 
                 preparedStatement.executeUpdate();
             } catch (SQLException e) {
@@ -74,10 +64,11 @@ public class ChessGameDao {
 
     public ChessGameDaoResponseDto loadGame(Long roomId) {
         Map<Position, Piece> board = new HashMap<>();
-        final String loadQuery = "select piece_type, side, last_turn, piece_rank, piece_file from chess_board where game_room_id_fk = ?";
+        final String loadQuery = "select piece_type, side, current_turn, piece_rank, piece_file from chess_board cb join game_room gr on" +
+                " cb.game_room_id_fk = gr.game_room_id where cb.game_room_id_fk = ?";
         Side lastTurn = null;
 
-        try (Connection connection = getConnection()) {
+        try (Connection connection = connectionGenerator.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(loadQuery);
             preparedStatement.setLong(1, roomId);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -86,7 +77,7 @@ public class ChessGameDao {
                 Position position = Position.of(resultSet.getString("piece_file"), resultSet.getString("piece_rank"));
                 Piece piece = new PieceFactory().create(PieceType.valueOf(resultSet.getString("piece_type")), Side.valueOf(resultSet.getString("side")));
                 board.put(position, piece);
-                lastTurn = Side.valueOf(resultSet.getString("last_turn"));
+                lastTurn = Side.valueOf(resultSet.getString("current_turn"));
             }
 
         } catch (SQLException e) {
@@ -97,7 +88,7 @@ public class ChessGameDao {
 
     public List<Long> findAllGameRooms() {
         String selectQuery = "SELECT game_room_id, status FROM game_room";
-        try (Connection connection = getConnection()) {
+        try (Connection connection = connectionGenerator.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
 
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -111,25 +102,24 @@ public class ChessGameDao {
         }
     }
 
-    public void updateGame(Long roomId, Map<Position, Piece> board, Side currentTurn) {
-        final String saveQuery = "UPDATE chess_board SET piece_type = ?,side = ?,piece_rank = ?,piece_file = ?, last_turn = ? where game_room_id_fk = ? and piece_file = ? and piece_rank = ?";
+    public void updateChessBoard(Long roomId, Map<Position, Piece> board) {
+        final String saveQuery = "UPDATE chess_board SET piece_type = ?,side = ?,piece_rank = ?,piece_file = ? where game_room_id_fk = ? and piece_file = ? and piece_rank = ?";
         for (Map.Entry<Position, Piece> pieces : board.entrySet()) {
             File file = pieces.getKey().getFile();
             Rank rank = pieces.getKey().getRank();
             PieceType pieceType = pieces.getValue().getPieceType();
             Side pieceSide = pieces.getValue().getSide();
 
-            try (Connection connection = getConnection()) {
+            try (Connection connection = connectionGenerator.getConnection()) {
                 PreparedStatement preparedStatement = connection.prepareStatement(saveQuery);
 
                 preparedStatement.setString(1, pieceType.name());
                 preparedStatement.setString(2, pieceSide.name());
                 preparedStatement.setString(3, rank.getText());
                 preparedStatement.setString(4, file.getText());
-                preparedStatement.setString(5, currentTurn.name());
-                preparedStatement.setLong(6, roomId);
-                preparedStatement.setString(7, file.getText());
-                preparedStatement.setString(8, rank.getText());
+                preparedStatement.setLong(5, roomId);
+                preparedStatement.setString(6, file.getText());
+                preparedStatement.setString(7, rank.getText());
 
                 preparedStatement.executeUpdate();
             } catch (SQLException e) {
@@ -138,18 +128,30 @@ public class ChessGameDao {
         }
     }
 
+    public void updateGameRoom(Long roomId, Side currentTurn, GameState state) {
+        final String saveQuery = "UPDATE game_room SET status = ?,current_turn = ? where game_room_id = ?";
+        try (Connection connection = connectionGenerator.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(saveQuery);
+
+            preparedStatement.setString(1, state.name());
+            preparedStatement.setString(2, currentTurn.name());
+            preparedStatement.setLong(3, roomId);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
     public boolean hasGame(Long roomId) {
         final String loadQuery = "select piece_type from chess_board where game_room_id_fk = ?";
 
-        try (Connection connection = getConnection()) {
+        try (Connection connection = connectionGenerator.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(loadQuery);
             preparedStatement.setLong(1, roomId);
 
             ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                return true;
-            }
-            return false;
+            return resultSet.next();
 
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
