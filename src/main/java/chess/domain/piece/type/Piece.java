f2f341fd6ab06e2a5fe73e771d123ca52c079375
zillionme <beatus96@naver.com>
2023-03-26 09:02:53 +0900

    refactor: Piece의 이동가능 여부 boolean으로 판단하도록 수정

--- src/main/java/chess/domain/piece/type/Piece.java
+++ src/main/java/chess/domain/piece/type/Piece.java
@@ -2,12 +2,11 @@ package chess.domain.piece.type;
 
 import chess.domain.board.Position;
 import chess.domain.piece.Color;
-import chess.domain.piece.Direction;
 import chess.domain.piece.PieceType;
 
 public abstract class Piece {
 
-    private final PieceType pieceType;
+    protected final PieceType pieceType;
     protected final Color color;
 
     protected Piece(PieceType pieceType, Color color) {
@@ -15,9 +14,7 @@ public abstract class Piece {
         this.color = color;
     }
 
-    public abstract void checkMovable(Position start, Position end, Color destinationColor);
-
-    protected abstract void checkMovableDirection(Direction direction);
+    public abstract boolean isMovable(Position start, Position end, Color destinationColor);
 
     public Color getColor() {
         return color;
