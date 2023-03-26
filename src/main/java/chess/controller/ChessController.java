93a0b28b5bb96327452412541143de39168fffab
cpot5620 <=cpot5620@naver.com>
2023-03-27 01:17:54 +0900

    fix(BoardDao): update 로직 오류 수정

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -160,18 +160,20 @@ public class ChessController {
     }
 
     private void updateGameState(ChessGame chessGame) {
+        if (!chessGame.isBothKingAlive()) {
+            boardDao.deleteAllByRoomName(chessGame.getRoomName());
+            gameRoomDao.deleteByRoomName(chessGame.getRoomName());
+            return;
+        }
+
         if (gameRoomDao.findByRoomName(chessGame.getRoomName()).isEmpty()) {
             gameRoomDao.save(new GameRoomDto(chessGame.getRoomName(), chessGame.isWhiteTurn()));
             boardDao.save(createBoardDto(chessGame));
             return;
         }
 
-        if (chessGame.isBothKingAlive()) {
-            boardDao.update(createBoardDto(chessGame));
-            return;
-        }
-
-        boardDao.deleteAllByRoomName(chessGame.getRoomName());
+        boardDao.update(createBoardDto(chessGame));
+        gameRoomDao.update(new GameRoomDto(chessGame.getRoomName(), chessGame.isWhiteTurn()));
     }
 
     private List<BoardDto> createBoardDto(ChessGame chessGame) {
