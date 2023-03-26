6c9176209de3e0d08f172b877e02c70c93bad7c7
zillionme <beatus96@naver.com>
2023-03-26 09:34:19 +0900

    feat: PieceType 클래스에 기물 점수 추가

--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -1,12 +1,22 @@
 package chess.domain.piece;
 
 public enum PieceType {
-    EMPTY_PIECE,
-    PAWN,
-    ROOK,
-    KNIGHT,
-    BISHOP,
-    QUEEN,
-    KING
+    EMPTY_PIECE(0),
+    PAWN(0.5),
+    ROOK(5),
+    KNIGHT(2.5),
+    BISHOP(3),
+    QUEEN(9),
+    KING(0);
+
+    private final double score;
+
+    PieceType(final double score) {
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
 
 }
