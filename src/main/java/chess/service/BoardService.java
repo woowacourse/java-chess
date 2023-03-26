3d8a309d3fe32c20cb6367daa93c4bfe61e5908c
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-03-27 00:19:13 +0900

    feat: BoardService 메서드 추가

---
+++ src/main/java/chess/service/BoardService.java
@@ -0,0 +1,29 @@
+package chess.service;
+
+import chess.domain.board.Board;
+import database.dto.SquareDto;
+import database.production.BoardDao;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BoardService {
+
+    private final BoardDao boardDao = new BoardDao();
+
+    public void save(Board board, int gameId) {
+        List<SquareDto> squareDtos = convertSquareDto(board);
+        boardDao.save(squareDtos, gameId);
+    }
+
+    public void update(Board board, int gameId) {
+        List<SquareDto> squareDtos = convertSquareDto(board);
+        boardDao.update(squareDtos, gameId);
+    }
+
+    private static List<SquareDto> convertSquareDto(Board board) {
+        return board.getBoard().entrySet().stream()
+                .map(entry -> SquareDto.of(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+    }
+}
