e8e8eb0cb8a58a5a01c15bd3df1e496f394dc304
zillionme <beatus96@naver.com>
2023-03-27 08:04:41 +0900

    feat: 새로운 체스 게임 생성 기능 추가

--- src/main/java/chess/dao/ChessGameDao.java
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -5,6 +5,7 @@ import chess.dao.connection.ConnectionDriver;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 
 public class ChessGameDao {
@@ -15,12 +16,11 @@ public class ChessGameDao {
         this.connectionDriver = new ConnectionDriver();
     }
 
-    public Optional<Integer> findLastGameIdByStatus(String status, String status2) {
-        String query = "SELECT * FROM game WHERE status = ? or status = ? ORDER BY game_id DESC LIMIT 1";
+    public Optional<Integer> findLastGameIdByStatus(String status) {
+        String query = "SELECT * FROM game WHERE status = ? ORDER BY game_id DESC LIMIT 1";
 
         try (final PreparedStatement preparedStatement = connectionDriver.getConnection().prepareStatement(query)) {
             preparedStatement.setObject(1, status);
-            preparedStatement.setObject(2, status2);
 
             final ResultSet resultSet = preparedStatement.executeQuery();
             if (!resultSet.next()) {
@@ -31,4 +31,23 @@ public class ChessGameDao {
             throw new IllegalArgumentException("게임이 존재하지 않습니다."+ e.getMessage());
         }
     }
+
+    public int add(String status, String turn) {
+        String query = "INSERT INTO game (status, turn) VALUES (?, ?)";
+        try (final PreparedStatement preparedStatement = connectionDriver.getConnection().prepareStatement(query,
+                Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setObject(1, status);
+            preparedStatement.setObject(2, turn);
+            preparedStatement.executeUpdate();
+
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if(resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+            throw new IllegalArgumentException("INSERT 오류: 입력이 올바르지 않습니다");
+        } catch (final SQLException e) {
+            throw new RuntimeException("INSERT 오류:" + e.getMessage());
+        }
+    }
+
 }
