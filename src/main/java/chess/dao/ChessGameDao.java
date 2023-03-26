274c30470f9525725f7f7bd4f156e43ee1056f6b
hanueleee <ezzeezz@naver.com>
2023-03-27 01:33:40 +0900

    feat(ChessGameDao): 입장이 가능한 방을 탐색하는 기능과 입장이 불가능한 방을 탐색하는 기능 구분

--- src/main/java/chess/dao/ChessGameDao.java
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -4,8 +4,8 @@ import chess.domain.ChessGame;
 import java.util.List;
 
 public interface ChessGameDao {
-    List<Integer> findAllId();
-
+    List<Integer> findAllPossibleId();
+    List<Integer> findAllImpossibleId();
     ChessGame findById(int gameId);
     void save(int gameId, ChessGame chessGame);
     void updateById(int gameId, ChessGame chessGame);
