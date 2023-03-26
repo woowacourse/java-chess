76abc2281357db73713557b272f02f834c4ca060
zillionme <beatus96@naver.com>
2023-03-27 07:54:15 +0900

    feat: 도커 이용한 sql 테이블 생성

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,5 +1,13 @@
-CREATE TABLE user (
-	user_id VARCHAR(12) NOT NULL,
-	name VARCHAR(64) NOT NULL,
-	PRIMARY KEY (user_id)
+create table game
+(
+    game_id int primary key auto_increment,
+    status varchar(10)  not null,
+    turn varchar(10) not null
+);
+
+create table move
+(
+    game_id int not null,
+    start_position varchar(10) not null,
+    end_position varchar(10) not null
 );
