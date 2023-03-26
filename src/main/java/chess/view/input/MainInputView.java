b9d59660c8342ec4ab811f88650faa2be5628afd
greeng00se <thegreengoose7@gmail.com>
2023-03-26 21:34:16 +0900

    refactor: UI 수정

--- src/main/java/chess/view/input/MainInputView.java
+++ src/main/java/chess/view/input/MainInputView.java
@@ -13,7 +13,7 @@ public class MainInputView {
     public String readCommand(final String user, final String room) {
         System.out.println("[로그인 한 유저: " + user + ", 선택한 방 이름: " + room + "]");
         System.out.println("> 계정 관리 : user");
-        System.out.println("> 게임 관리 : room");
+        System.out.println("> 방 관리 : room");
         System.out.println("> 게임 시작 : start");
         System.out.println("> 종료 : end");
 
--- src/main/java/chess/view/input/RoomInputView.java
+++ src/main/java/chess/view/input/RoomInputView.java
@@ -18,10 +18,10 @@ public class RoomInputView {
 
     public List<String> readCommand(final String user, final String room) {
         System.out.println("[로그인 한 유저: " + user + ", 선택한 방 이름: " + room + "]");
-        System.out.println("> 게임 조회 : history");
-        System.out.println("> 게임 생성 : create");
-        System.out.println("> 게임 참가 : join");
-        System.out.println("> 종료 : end");
+        System.out.println("> 방 조회 : history");
+        System.out.println("> 방 생성 : create 방이름 - 예) create room1");
+        System.out.println("> 방 참가 : join 방번호 - 예) join 1");
+        System.out.println("> 메인 화면 : end");
 
         final String input = scanner.nextLine();
 
