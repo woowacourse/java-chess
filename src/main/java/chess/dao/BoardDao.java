d4293648108bf2f5488d7e8cc73a0c0b0b1843ee
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-03-27 06:36:41 +0900

    feat: BoardDao 클래스 생성

--- src/main/java/chess/dao/BoardDao.java
+++ src/main/java/chess/dao/BoardDao.java
@@ -1,2 +1,24 @@
-package chess.dao;public class BoardDao {
+package chess.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BoardDao {
+
+    private static final String SERVER = "localhost:3306";
+    private static final String DATABASE = "chess";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "xxxx";
+
+    public Connection getConnection() {
+        // 드라이버 연결
+        try {
+            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE, USERNAME, PASSWORD);
+        } catch (final SQLException e) {
+            System.err.println("DB 연결 오류:" + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/test/java/chess/dao/BoardDaoTest.java
+++ src/test/java/chess/dao/BoardDaoTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package chess.dao;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class BoardDaoTest {
-  
+    private final BoardDao boardDao = new BoardDao();
+
+    @Test
+    public void connection() {
+        try (final var connection = boardDao.getConnection()) {
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
