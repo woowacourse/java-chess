aa3026a6c4c5848c0561907b792f5d368f10644b
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-26 13:48:07 +0900

    feat : 컨트롤러 체스 상태 요청 기능 추가

--- src/main/java/chess/controller/Controller.java
+++ src/main/java/chess/controller/Controller.java
@@ -1,9 +1,11 @@
 package chess.controller;
 
 import chess.domain.Board;
+import chess.domain.Chess;
+import chess.domain.point.Points;
 import chess.domain.state.Start;
 import chess.domain.state.State;
-import chess.dto.ChessInputDto;
+import chess.dto.Command;
 import chess.view.GameState;
 import chess.view.InputView;
 import chess.view.OutputView;
@@ -11,24 +13,57 @@ import chess.view.OutputView;
 public final class Controller {
     public void run() {
         OutputView.printStartMessage();
-        State state = new Start(Board.create());
+        State state = new Start(Chess.create(Board.create(), Points.create()));
         while (state.isNotEnd()) {
-            final ChessInputDto dto = InputView.inputGameState();
-            state = processChess(state, dto);
-            OutputView.printChessBoard(state.getBoard());
+            state = nextState(state);
+            OutputView.printChessBoard(state.getChess());
         }
+        OutputView.printChessResult(state.getWinner());
     }
 
-    private State processChess(State state, final ChessInputDto dto) {
-        final GameState gameState = dto.getGameState();
-        if (gameState == GameState.START) {
+    private State nextState(State state) {
+        try {
+            return processChess(state);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            nextState(state);
+        }
+        return state;
+    }
+
+    private State processChess(State state) {
+        final Command command = InputView.inputGameState();
+        state = start(state, command);
+        state = move(state, command);
+        state = end(state, command);
+        state = status(state, command);
+        return state;
+    }
+
+    private State start(final State state, final Command command) {
+        if (command.getGameState() == GameState.START) {
             return state.start();
         }
-        if (gameState == GameState.MOVE) {
-            return state.move(dto);
+        return state;
+    }
+
+    private State move(final State state, final Command command) {
+        if (command.getGameState() == GameState.MOVE) {
+            return state.move(command);
         }
-        if (gameState == GameState.END) {
+        return state;
+    }
+
+    private State end(final State state, final Command command) {
+        if (command.getGameState() == GameState.END) {
             return state.end();
+        }
+        return state;
+    }
+
+    private State status(final State state, final Command command) {
+        if (command.getGameState() == GameState.STATUS) {
+            return state.status();
         }
         return state;
     }
