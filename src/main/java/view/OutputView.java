abb669efd9ac22c663c8c3418685cf552f06abf0
Youngho <cyh6099@gmail.com>
2023-03-27 00:59:06 +0900

    feat: save안내 문구 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -22,6 +22,7 @@ public class OutputView {
         System.out.println("> 게임 시작 : start");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+        System.out.println("> 게임 저장 : save");
     }
 
     public void printGameScores(Map<Side, Double> scores) {
