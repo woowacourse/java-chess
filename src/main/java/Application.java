31a285ab6932ddcd0ec9cf23701d60361fb5a35e
Choi-JJunho <junho5336@gmail.com>
2023-03-26 14:21:35 +0900

    refactor: 기본 동작방식 InMemory방식으로 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,14 @@
 import chess.controller.ChessController;
-import chess.dao.DbChessGameDao;
+import chess.dao.InMemoryChessGameDao;
 import chess.view.InputView;
 import chess.view.OutputView;
 
 public class Application {
 
     public static void main(final String[] args) {
-        final ChessController chessController = new ChessController(new InputView(), new OutputView(), new DbChessGameDao());
+        // DB와 연결해서 작업 시 아래 로직 사용
+        // final ChessController chessController = new ChessController(new InputView(), new OutputView(), new DbChessGameDao());
+        final ChessController chessController = new ChessController(new InputView(), new OutputView(), new InMemoryChessGameDao());
         chessController.run();
     }
 }
