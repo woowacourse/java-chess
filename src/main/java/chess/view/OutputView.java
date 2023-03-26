2eedf149042ce063594734d0a3728ef8f7f2b723
cpot5620 <=cpot5620@naver.com>
2023-03-27 00:57:09 +0900

    feat(OutputView): 결과 출력 로직 구현

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,26 +1,73 @@
 package chess.view;
 
+import chess.domain.ChessGame;
 import chess.domain.board.Chessboard;
 import chess.domain.board.File;
 import chess.domain.board.Rank;
 import chess.domain.board.Square;
+import chess.domain.piece.Camp;
 import chess.domain.piece.Piece;
 import chess.domain.piece.PieceType;
 
 import java.util.Arrays;
 
 public class OutputView {
+    private static final String WHITE = "WHITE";
+    private static final String BLACK = "BLACK";
     private static final String ERROR = "[ERROR] : %s";
+
+    public void printError(String message) {
+        System.out.println(String.format(ERROR, message));
+    }
 
     public void printStartMessage() {
         System.out.println(Message.GAME_START.value);
         System.out.println(Message.START_COMMAND.value);
         System.out.println(Message.END_COMMAND.value);
         System.out.println(Message.MOVE_COMMAND.value);
+        System.out.println(Message.STATUS_COMMAND.value);
     }
 
-    public void printFinishMessage() {
+    public void printScoreMessage(ChessGame chessGame) {
+        double whiteScore = chessGame.calculateScoreOf(Camp.WHITE);
+        double blackScore = chessGame.calculateScoreOf(Camp.BLACK);
+
+        printGameResult(chessGame);
+        System.out.println(String.format(Message.SCORE.value, WHITE, whiteScore));
+        System.out.println(String.format(Message.SCORE.value, BLACK, blackScore));
+        printScoreWinnerMessage(whiteScore, blackScore);
+    }
+
+    private void printGameResult(ChessGame chessGame) {
+        if (chessGame.isBothKingAlive()) {
+            return;
+        }
+
         System.out.println(System.lineSeparator() + Message.GAME_FINISH.value);
+        printGameWinner(chessGame);
+    }
+
+    private void printGameWinner(ChessGame chessGame) {
+        if (chessGame.isWhiteKingAlive()) {
+            System.out.println(String.format(Message.GAME_WINNER.value, WHITE));
+            return;
+        }
+
+        System.out.println(String.format(Message.GAME_WINNER.value, BLACK));
+    }
+
+    private void printScoreWinnerMessage(double whiteScore, double blackScore) {
+        String message = Message.SCORE_DRAW.value;
+
+        if (whiteScore > blackScore) {
+            message = String.format(Message.SCORE_WINNER.value, WHITE);
+        }
+
+        if (whiteScore < blackScore) {
+            message = String.format(Message.SCORE_WINNER.value, BLACK);
+        }
+
+        System.out.println(message);
     }
 
     public void printChessBoard(Chessboard chessboard) {
@@ -32,15 +79,13 @@ public class OutputView {
 
     private void printRankAt(Chessboard chessboard, Rank rank) {
         StringBuilder stringBuilder = new StringBuilder();
+
         for (File file : File.values()) {
             Piece piece = chessboard.getPieceAt(Square.getInstanceOf(file, rank));
             stringBuilder.append(PieceRenderer.getPieceName(piece));
         }
-        System.out.println(stringBuilder);
-    }
 
-    public void printError(String message) {
-        System.out.println(String.format(ERROR, message));
+        System.out.println(stringBuilder);
     }
 
     private enum PieceRenderer {
@@ -81,8 +126,12 @@ public class OutputView {
         START_COMMAND("> 게임 시작 : start"),
         END_COMMAND("> 게임 종료 : end"),
         MOVE_COMMAND("> 게임 이동 : move source위치 target위치 - 예. move b2 b3"),
+        STATUS_COMMAND("> 게임 점수 출력 : status"),
         GAME_FINISH("> King이 죽었기 때문에 게임을 종료합니다."),
-        ;
+        GAME_WINNER("> %s가 승리하였습니다."),
+        SCORE("> %s의 점수는 %.1f입니다."),
+        SCORE_WINNER("> %s의 점수가 더 높습니다."),
+        SCORE_DRAW("> 동점입니다.");
 
         private final String value;
 
