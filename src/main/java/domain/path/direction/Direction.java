0662c56b7ac17fe330619ed3d979b973c822fec6
splitube <voicesplit@daum.net>
2023-03-27 02:15:43 +0900

    feat: Direction 생성

---
+++ src/main/java/domain/path/direction/Direction.java
@@ -0,0 +1,50 @@
+package domain.path.direction;
+
+import domain.path.PieceMove;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public enum Direction {
+
+    RIGHT_UP(1, 1, DirectionCheckerFactory::isRightUpDiagonal),
+    RIGHT_DOWN(1, -1, DirectionCheckerFactory::isRightDownDiagonal),
+    LEFT_UP(-1, 1, DirectionCheckerFactory::isLeftUpDiagonal),
+    LEFT_DOWN(-1, -1, DirectionCheckerFactory::isLeftDownDiagonal),
+    RIGHT(1, 0, DirectionCheckerFactory::isRight),
+    LEFT(-1, 0, DirectionCheckerFactory::isLeft),
+    DOWN(0, -1, DirectionCheckerFactory::isDown),
+    UP(0, 1, DirectionCheckerFactory::isUp),
+    UP_UP_LEFT(-1, 2, DirectionCheckerFactory::isTwoUpAndLeft),
+    UP_UP_RIGHT(1, 2, DirectionCheckerFactory::isTwoUpAndRight),
+    DOWN_DOWN_LEFT(-1, -2, DirectionCheckerFactory::isTwoDownAndLeft),
+    DOWN_DOWN_RIGHT(1, -2, DirectionCheckerFactory::isTwoDownAndRight),
+    LEFT_LEFT_UP(-2, 1, DirectionCheckerFactory::isTwoLeftAndUp),
+    LEFT_LEFT_DOWN(-2, -1, DirectionCheckerFactory::isTwoLeftAndDown),
+    RIGHT_RIGHT_UP(2, 1, DirectionCheckerFactory::isTwoRightAndUp),
+    RIGHT_RIGHT_DOWN(2, -1, DirectionCheckerFactory::isTwoRightAndDown);
+
+    private final int colDiff;
+    private final int rowDiff;
+    private final Predicate<PieceMove> condition;
+
+    Direction(final int colDiff, final int rowDiff, final Predicate<PieceMove> condition) {
+        this.colDiff = colDiff;
+        this.rowDiff = rowDiff;
+        this.condition = condition;
+    }
+
+    public static Direction find(final PieceMove pieceMove) {
+        return Arrays.stream(Direction.values())
+            .filter(direction -> direction.condition.test(pieceMove))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("이동할 수 없는 경로 입니다."));
+    }
+
+    public int getColDiff() {
+        return colDiff;
+    }
+
+    public int getRowDiff() {
+        return rowDiff;
+    }
+}
---
+++ src/main/java/domain/path/direction/DirectionCheckerFactory.java
@@ -0,0 +1,114 @@
+package domain.path.direction;
+
+import domain.path.PieceMove;
+import domain.path.location.Location;
+
+public final class DirectionCheckerFactory {
+
+    private DirectionCheckerFactory() {
+    }
+
+    static boolean isRightUpDiagonal(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isDiagonal(end)
+            && end.isHigherThan(start)
+            && end.isRightThan(start);
+    }
+
+    static boolean isRightDownDiagonal(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isDiagonal(end)
+            && start.isHigherThan(end)
+            && end.isRightThan(start);
+    }
+
+    static boolean isLeftUpDiagonal(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isDiagonal(end)
+            && end.isHigherThan(start)
+            && start.isRightThan(end);
+    }
+
+    static boolean isLeftDownDiagonal(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isDiagonal(end)
+            && start.isHigherThan(end)
+            && start.isRightThan(end);
+    }
+
+    static boolean isRight(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isSameRow(end) && end.isRightThan(start);
+    }
+
+    static boolean isLeft(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isSameRow(end) && start.isRightThan(end);
+    }
+
+    static boolean isDown(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isSameCol(end) && start.isHigherThan(end);
+    }
+
+    static boolean isUp(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return start.isSameCol(end) && end.isHigherThan(start);
+    }
+
+    static boolean isTwoUpAndLeft(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == 2 && end.getCol() - start.getCol() == -1;
+    }
+
+    static boolean isTwoUpAndRight(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == 2 && end.getCol() - start.getCol() == 1;
+    }
+
+    static boolean isTwoDownAndLeft(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == -2 && end.getCol() - start.getCol() == -1;
+    }
+
+    static boolean isTwoDownAndRight(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == -2 && end.getCol() - start.getCol() == 1;
+    }
+
+    static boolean isTwoRightAndUp(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == 1 && end.getCol() - start.getCol() == 2;
+    }
+
+    static boolean isTwoRightAndDown(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == -1 && end.getCol() - start.getCol() == 2;
+    }
+
+    static boolean isTwoLeftAndUp(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == 1 && end.getCol() - start.getCol() == -2;
+    }
+
+    static boolean isTwoLeftAndDown(final PieceMove pieceMove) {
+        final Location start = pieceMove.getStart();
+        final Location end = pieceMove.getEnd();
+        return end.getRow() - start.getRow() == -1 && end.getCol() - start.getCol() == -2;
+    }
+}
