e39031ea42d3a4afc76e92553daae9d71f37d205
hectick <chyeon1223@cau.ac.kr>
2023-03-26 13:15:52 +0900

    test(DBConnection): 테스트 코드 수정

--- src/test/java/chess/dao/DBConnectionTest.java
+++ src/test/java/chess/dao/DBConnectionTest.java
@@ -2,17 +2,16 @@ package chess.dao;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-class DBChessGameDaoTest {
-
-    private final DBChessGameDao dbChessGameDao = new DBChessGameDao();
+class DBConnectionTest {
 
     @Test
     void connectionTest() {
-        try (final var connection = dbChessGameDao.getConnection()) {
+        try (final Connection connection = DBConnection.getConnection()) {
             assertThat(connection).isNotNull();
         } catch (SQLException e) {
             throw new RuntimeException(e);
