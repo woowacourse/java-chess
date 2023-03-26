078963c9a48b30d355d0fb23cdada22131406b7f
splitube <voicesplit@daum.net>
2023-03-27 02:12:07 +0900

    feat: PathValidatorFactory 생성

---
+++ src/main/java/domain/path/pathValidtor/PathValidatorFactory.java
@@ -0,0 +1,101 @@
+package domain.path.pathValidtor;
+
+import domain.board.piece.PieceType;
+import domain.path.pathValidtor.validateTarget.BlockedValidation;
+import domain.path.pathValidtor.validateTarget.DetinationValidation;
+import domain.path.pathValidtor.validateTarget.DirectionValidation;
+import domain.path.pathValidtor.validateTarget.MoveOnceValidation;
+import domain.path.pathValidtor.validateTarget.PawnMoveValidation;
+import java.util.List;
+
+public final class PathValidatorFactory {
+
+    public static PathValidator rookPathValidator() {
+        final DirectionValidation directionValidation = DirectionValidation.of(
+            PieceType.ROOK.getPossibleDirections()
+        );
+        return new PathValidator(
+            List.of(
+                directionValidation,
+                new BlockedValidation(),
+                new DetinationValidation()
+            )
+        );
+    }
+
+    public static PathValidator bishopPathValidator() {
+        final DirectionValidation directionValidation = DirectionValidation.of(
+            PieceType.BISHOP.getPossibleDirections()
+        );
+
+        return new PathValidator(
+            List.of(
+                directionValidation,
+                new BlockedValidation(),
+                new DetinationValidation()
+            )
+        );
+    }
+
+    public static PathValidator queenPathValidator() {
+        final DirectionValidation directionValidation = DirectionValidation.of(
+            PieceType.QUEEN.getPossibleDirections()
+        );
+
+        return new PathValidator(
+            List.of(
+                directionValidation,
+                new BlockedValidation(),
+                new DetinationValidation()
+            )
+        );
+    }
+
+    public static PathValidator kingPathValidator() {
+        final DirectionValidation directionValidation = DirectionValidation.of(
+            PieceType.KING.getPossibleDirections()
+        );
+
+        return new PathValidator(
+            List.of(
+                directionValidation,
+                new MoveOnceValidation(),
+                new DetinationValidation()
+            )
+        );
+    }
+
+    public static PathValidator knightPathValidator() {
+        final DirectionValidation directionValidation = DirectionValidation.of(
+            PieceType.KING.getPossibleDirections()
+        );
+
+        return new PathValidator(
+            List.of(
+                directionValidation,
+                new MoveOnceValidation(),
+                new DetinationValidation()
+            )
+        );
+    }
+
+    public static PathValidator pawnPathValidator() {
+        final DirectionValidation directionValidation = DirectionValidation.of(
+            PieceType.PAWN.getPossibleDirections()
+        );
+        return new PathValidator(
+            List.of(
+                directionValidation,
+                new PawnMoveValidation()
+            )
+        );
+    }
+
+    public static PathValidator emptyPiecePathValidator() {
+        return new PathValidator(
+            List.of(
+                DirectionValidation.of(PieceType.EMPTY.getPossibleDirections())
+            )
+        );
+    }
+}
