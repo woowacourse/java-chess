19c81422b2001399467a3fa9ae4c26d567379a18
donghae <donghae1999@gmail.com>
2023-03-27 01:09:23 +0900

    test: ConnectionGenerator, GameDao DisplayName 수정

--- src/test/java/chessgame/dao/ConnectionGeneratorTest.java
+++ src/test/java/chessgame/dao/ConnectionGeneratorTest.java
@@ -1,11 +1,13 @@
 package chessgame.dao;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ConnectionGeneratorTest {
     @Test
+    @DisplayName("DataBase Connection이 잘 수행되는지 확인 테스트한다.")
     void connection() {
         assertThat(ConnectionGenerator.getConnection()).isNotNull();
     }
--- src/test/java/chessgame/dao/GameDaoTest.java
+++ src/test/java/chessgame/dao/GameDaoTest.java
@@ -25,6 +25,7 @@ class GameDaoTest {
     }
 
     @Test
+    @DisplayName("DB에 값이 잘 저장되는지 확인한다.")
     void save() {
         Board board = new Board(ChessBoardFactory.create());
 
@@ -32,11 +33,13 @@ class GameDaoTest {
     }
 
     @Test
+    @DisplayName("DB에 읽어오는 값이 존재하지 않을 경우를 확인한다.")
     void readNull() {
         Assertions.assertThatNoException().isThrownBy(() -> assertThat(gameDao.read(gameName, connection)).isNull());
     }
 
     @Test
+    @DisplayName("DB에 읽어오는 값이 존재 할 경우를 확인한다.")
     void read() {
         Board board = new Board(ChessBoardFactory.create());
 
@@ -45,6 +48,7 @@ class GameDaoTest {
     }
 
     @Test
+    @DisplayName("DB에 있는 특정 game과 board 값을 삭제한다.")
     void remove() {
         Board board = new Board(ChessBoardFactory.create());
 
@@ -54,11 +58,12 @@ class GameDaoTest {
     }
 
     @Nested
-    @DisplayName("game의 turn을 찾는다.")
+    @DisplayName("특정 game의 현재 turn을 찾는다.")
     class findTurnByGame {
         Board board = new Board(ChessBoardFactory.create());
 
         @Test
+        @DisplayName("White팀 턴일 경우 turn이 잘 저장되었는지 확인한다.")
         void Should_FindWhite_WhenWhiteTurn() {
             gameDao.save(board, gameName, new White(), connection);
 
@@ -66,6 +71,7 @@ class GameDaoTest {
         }
 
         @Test
+        @DisplayName("Black팀 턴일 경우 turn이 잘 저장되었는지 확인한다.")
         void Should_FindBlack_WhenBlackTurn() {
             gameDao.save(board, gameName, new Black(), connection);
 
