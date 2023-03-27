3f8ce43db4a5535c642c5293a98063c3c1b45f2a
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-27 12:21:08 +0900

    feat : PieceType 기물 생성 기능 추가

--- src/main/java/chess/domain/PieceType.java
+++ src/main/java/chess/domain/PieceType.java
@@ -1,22 +1,26 @@
 package chess.domain;
 
+import chess.domain.piece.*;
 import chess.domain.point.Point;
 
 import java.util.List;
+import java.util.function.Function;
 
 public enum PieceType {
-    PAWN(Point.create(1)),
-    ROOK(Point.create(5)),
-    KNIGHT(Point.create(2.5)),
-    BISHOP(Point.create(3)),
-    QUEEN(Point.create(9)),
-    KING(Point.create(0)),
-    EMPTY(Point.create(0));
+    PAWN(Point.create(1), Pawn::from),
+    ROOK(Point.create(5), Rook::from),
+    KNIGHT(Point.create(2.5), Knight::from),
+    BISHOP(Point.create(3), Bishop::from),
+    QUEEN(Point.create(9), Queen::from),
+    KING(Point.create(0), King::from),
+    EMPTY(Point.create(0), color -> Empty.create());
 
     private final Point point;
+    private final Function<Color, Piece> createPiece;
 
-    PieceType(final Point point) {
+    PieceType(final Point point, final Function<Color, Piece> createPiece) {
         this.point = point;
+        this.createPiece = createPiece;
     }
 
     public static Point sum(final List<PieceType> pieceTypes) {
@@ -24,5 +28,9 @@ public enum PieceType {
                 .map(pieceType -> pieceType.point)
                 .reduce(Point::plus)
                 .orElse(Point.ZERO);
+    }
+
+    public Piece newInstance(final Color color) {
+        return createPiece.apply(color);
     }
 }
--- src/test/java/chess/domain/PieceTypeTest.java
+++ src/test/java/chess/domain/PieceTypeTest.java
@@ -1,5 +1,6 @@
 package chess.domain;
 
+import chess.domain.piece.*;
 import chess.domain.point.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,6 +10,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static chess.domain.Color.BLACK;
+import static chess.domain.Color.WHITE;
 import static chess.domain.PieceType.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,6 +42,36 @@ class PieceTypeTest {
                 Arguments.arguments(List.of(PAWN, PAWN, KING, BISHOP), Point.create(5)),
                 Arguments.arguments(List.of(PAWN, ROOK, KING, BISHOP), Point.create(9)),
                 Arguments.arguments(List.of(KNIGHT, ROOK, KING, BISHOP), Point.create(10.5))
+        );
+    }
+
+    @DisplayName("기물 타입과 색깔을 통해 기물 구현체를 반환한다.")
+    @ParameterizedTest
+    @MethodSource("newInstanceDummy")
+    void newInstance(final Color color, final PieceType pieceType, final Piece expected) {
+        // given & when
+        final Piece piece = pieceType.newInstance(color);
+
+        // then
+        assertThat(piece).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> newInstanceDummy() {
+        return Stream.of(
+                Arguments.arguments(WHITE, PAWN, Pawn.from(WHITE)),
+                Arguments.arguments(WHITE, ROOK, Rook.from(WHITE)),
+                Arguments.arguments(WHITE, KNIGHT, Knight.from(WHITE)),
+                Arguments.arguments(WHITE, BISHOP, Bishop.from(WHITE)),
+                Arguments.arguments(WHITE, QUEEN, Queen.from(WHITE)),
+                Arguments.arguments(WHITE, KING, King.from(WHITE)),
+                Arguments.arguments(WHITE, EMPTY, Empty.create()),
+                Arguments.arguments(BLACK, PAWN, Pawn.from(BLACK)),
+                Arguments.arguments(BLACK, ROOK, Rook.from(BLACK)),
+                Arguments.arguments(BLACK, KNIGHT, Knight.from(BLACK)),
+                Arguments.arguments(BLACK, BISHOP, Bishop.from(BLACK)),
+                Arguments.arguments(BLACK, QUEEN, Queen.from(BLACK)),
+                Arguments.arguments(BLACK, KING, King.from(BLACK)),
+                Arguments.arguments(BLACK, EMPTY, Empty.create())
         );
     }
 }
