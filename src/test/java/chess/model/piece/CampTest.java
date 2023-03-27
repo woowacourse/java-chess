72c8d2973878d79d1b1a7d3fae204ad8c1cfc79b
apptie <appti@naver.com>
2023-03-27 17:49:43 +0900

    feat: 비어 있는 기물을 표현하는 Piece.EMPTY의 진영을 표현하기 위한 Camp.EMPTY 요소 추가

--- src/test/java/chess/model/piece/CampTest.java
+++ src/test/java/chess/model/piece/CampTest.java
@@ -2,20 +2,45 @@ package chess.model.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CampTest {
 
     @ParameterizedTest(name = "isSameCamp.BLACK은 {0}이 주어지면 {1}을 반환한다")
     @DisplayName("isSameCamp() 테스트")
-    @CsvSource(value = {"BLACK:true", "WHITE:false"}, delimiter = ':')
+    @MethodSource("provideIsSameCampByBlackArguments")
     void isSameCamp_givenOtherCamp_thenReturnIsSameCamp(final Camp camp, final boolean expected) {
         // when
         final boolean actual = Camp.BLACK.isSameCamp(camp);
 
         // then
         assertThat(actual).isSameAs(expected);
+    }
+
+    private static Stream<Arguments> provideIsSameCampByBlackArguments() {
+        return Stream.of(
+                Arguments.of(Camp.BLACK, true), Arguments.of(Camp.WHITE, false), Arguments.of(Camp.EMPTY, false)
+        );
+    }
+
+    @ParameterizedTest(name = "{0}이 주어지면 {1}을 반환한다")
+    @DisplayName("isEmpty() 테스트")
+    @MethodSource("provideIsEmptyArguments")
+    void isEmpty_whenCall_thenReturnIsEmpty(final Camp camp, final boolean expected) {
+        // when
+        final boolean actual = camp.isEmpty();
+
+        // then
+        assertThat(actual).isSameAs(expected);
+    }
+
+    private static Stream<Arguments> provideIsEmptyArguments() {
+        return Stream.of(
+                Arguments.of(Camp.BLACK, false), Arguments.of(Camp.WHITE, false), Arguments.of(Camp.EMPTY, true)
+        );
     }
 }
