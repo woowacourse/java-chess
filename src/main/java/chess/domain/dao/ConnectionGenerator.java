3435af2193c876159acbf479301bd8696d1f85ec
hong-sile <gurwns9325@naver.com>
2023-03-27 21:05:51 +0900

    fix: Option 변경으로 올바른 Connection 생성하게 수정

--- src/main/java/chess/domain/dao/ConnectionGenerator.java
+++ src/main/java/chess/domain/dao/ConnectionGenerator.java
@@ -8,7 +8,7 @@ public class ConnectionGenerator {
 
     private static final String SERVER = "localhost:13306";
     private static final String DATABASE = "chess";
-    private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
+    private static final String OPTION = "?useSSL=true&serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
