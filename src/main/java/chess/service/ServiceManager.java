e8390b5219336fe39242ebab697955d599421b06
jiwon <ljwon77@daum.net>
2023-03-27 16:48:04 +0900

    feat: 서비스의 생성을 관리하는 클래스를 생성한다

---
+++ src/main/java/chess/service/ServiceManager.java
@@ -0,0 +1,27 @@
+package chess.service;
+
+import chess.dao.chess.ChessGameDao;
+import chess.dao.chess.ChessGameDaoImpl;
+import chess.dao.chess.PieceDao;
+import chess.dao.chess.PieceDaoImpl;
+import chess.dao.user.UserDaoImpl;
+import chess.database.JdbcTemplate;
+
+public final class ServiceManager {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ServiceManager(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public UserService userService() {
+        return new UserService(new UserDaoImpl(jdbcTemplate));
+    }
+
+    public ChessGameService chessGameService() {
+        final ChessGameDao chessGameDao = new ChessGameDaoImpl(jdbcTemplate);
+        final PieceDao pieceDao = new PieceDaoImpl(jdbcTemplate);
+        final ChessBoardService chessBoardService = new ChessBoardService(pieceDao);
+        return new ChessGameService(chessGameDao, chessBoardService);
+    }
+}
