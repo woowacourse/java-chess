c3de3d4cd024187a8b0c47c40b533e7d9cc2337b
waterricecake <fruturum@nate.com>
2023-03-27 23:44:52 +0900

    refactor(db): sql설정및 git.ignore 설정

---
+++ db/mysql/init/init.sql
@@ -0,0 +1,38 @@
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+CREATE SCHEMA IF NOT EXISTS `chess` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `chess` ;
+
+
+CREATE TABLE IF NOT EXISTS `chess`.`room` (
+                                              `id` INT NOT NULL AUTO_INCREMENT,
+                                              `name` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 2
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE IF NOT EXISTS `chess`.`notation` (
+                                                  `room_id` INT NOT NULL,
+                                                  `source` VARCHAR(2) NOT NULL,
+    `target` VARCHAR(2) NOT NULL,
+    `turn` INT NOT NULL,
+    INDEX `fk_notation_chess_room_idx` (`room_id` ASC) VISIBLE,
+    CONSTRAINT `fk_notation_chess_room`
+    FOREIGN KEY (`room_id`)
+    REFERENCES `chess`.`room` (`id`))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
