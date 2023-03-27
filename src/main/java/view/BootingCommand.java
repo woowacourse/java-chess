16c8e72905dab2606a05f194231d5087a4f66b0b
BackFoxx <backfoxxx@gmail.com>
2023-03-27 18:38:11 +0900

    refactor : 입력값이 어떤 BootingCommand에 해당하는 지 찾아내는 작업을 Predicate로 진행하도록 변경

--- src/main/java/view/BootingCommand.java
+++ src/main/java/view/BootingCommand.java
@@ -1,20 +1,21 @@
 package view;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 public enum BootingCommand {
-    START("start"),
-    END("end");
+    START("start"::equals),
+    END("end"::equals);
 
-    private final String command;
+    private final Predicate<String> condition;
 
-    BootingCommand(String command) {
-        this.command = command;
+    BootingCommand(Predicate<String> condition) {
+        this.condition = condition;
     }
 
     public static BootingCommand findByCommand(String command) {
         return Arrays.stream(BootingCommand.values())
-                .filter(bootingCommand -> bootingCommand.command.equals(command))
+                .filter(bootingCommand -> bootingCommand.condition.test(command))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 커멘드 입력입니다."));
     }
