a908d32deea5d32b1278c2ddd99f794c041ccc54
kokodak <kokodakadokok@gmail.com>
2023-03-27 16:37:38 +0900

    feat: 방 입장에 대한 입력 기능 구현

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -11,7 +11,13 @@ public class InputView {
         this.scanner = scanner;
     }
 
-    public List<String> readCommand() {
+    public List<String> readRoomCommand() {
+        System.out.println("새로운 게임 : new");
+        System.out.println("기존의 게임 : enter 방번호  (예시) enter 14");
+        return List.of(scanner.nextLine().split(" "));
+    }
+
+    public List<String> readGameCommand() {
         System.out.println("게임 시작     : start");
         System.out.println("기물 이동     : move source위치 target위치  (예시) move b2 b3");
         System.out.println("기물 점수 상태 : status");
