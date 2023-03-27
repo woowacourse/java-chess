128eaca1f7629fa58c6e62741a0b8c2c3d3e2cb4
somsom13 <somin1113@naver.com>
2023-03-27 15:43:31 +0900

    fix: DB 연결 오류 수정

--- src/main/java/chess/dao/ConnectionProvider.java
+++ src/main/java/chess/dao/ConnectionProvider.java
@@ -11,13 +11,15 @@ public class ConnectionProvider {
     private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
     private static final String USERNAME = "root"; //  MySQL 서버 아이디
     private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+    private static final String CUSTOM_ERROR_HANDLING = "&allowPublicKeyRetrieval=true"; // Public Key Retrieval 오류 해결
 
     private ConnectionProvider() {
     }
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" +
+                DATABASE + OPTION + CUSTOM_ERROR_HANDLING, USERNAME, PASSWORD);
         } catch (final SQLException e) {
             System.err.println("DB 연결 오류: " + e.getMessage());
             throw new RuntimeException(e);
