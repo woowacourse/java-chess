7b0459d1b0e1f7da58c334f69858de835c16a6e5
apptie <appti@naver.com>
2023-03-27 17:52:08 +0900

    refactor: AttackEvaluator의 메소드 파라미터 타입을 boolean에서 PieceType으로 변경

--- src/main/java/chess/model/piece/movement/AttackEvaluator.java
+++ src/main/java/chess/model/piece/movement/AttackEvaluator.java
@@ -1,21 +1,22 @@
 package chess.model.piece.movement;
 
 import chess.model.piece.Camp;
+import chess.model.piece.PieceType;
 
 public final class AttackEvaluator {
 
     private final Camp sourceCamp;
     private final Camp targetCamp;
-    private final boolean empty;
+    private final PieceType targetPieceType;
 
-    public AttackEvaluator(final Camp sourceCamp, final Camp targetCamp, final boolean empty) {
+    public AttackEvaluator(final Camp sourceCamp, final Camp targetCamp, final PieceType targetPieceType) {
         this.sourceCamp = sourceCamp;
         this.targetCamp = targetCamp;
-        this.empty = empty;
+        this.targetPieceType = targetPieceType;
     }
 
     public boolean isEmpty() {
-        return empty;
+        return targetPieceType.isSamePieceType(PieceType.EMPTY);
     }
 
     public boolean isEnemy() {
--- src/test/java/chess/helper/CommonAttackEvaluator.java
+++ src/test/java/chess/helper/CommonAttackEvaluator.java
@@ -6,10 +6,10 @@ import chess.model.piece.movement.AttackEvaluator;
 
 public class CommonAttackEvaluator {
 
-    protected final AttackEvaluator blackEnemyEvaluator = new AttackEvaluator(Camp.BLACK, Camp.WHITE, false);
-    protected final AttackEvaluator blackAllyEvaluator = new AttackEvaluator(Camp.BLACK, Camp.BLACK, false);
-    protected final AttackEvaluator whiteEnemyEvaluator = new AttackEvaluator(Camp.WHITE, Camp.BLACK, false);
-    protected final AttackEvaluator whiteAllyEvaluator = new AttackEvaluator(Camp.WHITE, Camp.WHITE, false);
-    protected final AttackEvaluator blackEmptyEvaluator = new AttackEvaluator(Camp.BLACK, Camp.WHITE, true);
-    protected final AttackEvaluator whiteEmptyEvaluator = new AttackEvaluator(Camp.WHITE, Camp.BLACK, true);
+    protected final AttackEvaluator blackEnemyEvaluator = new AttackEvaluator(Camp.BLACK, Camp.WHITE, PieceType.QUEEN);
+    protected final AttackEvaluator blackAllyEvaluator = new AttackEvaluator(Camp.BLACK, Camp.BLACK, PieceType.QUEEN);
+    protected final AttackEvaluator whiteEnemyEvaluator = new AttackEvaluator(Camp.WHITE, Camp.BLACK, PieceType.QUEEN);
+    protected final AttackEvaluator whiteAllyEvaluator = new AttackEvaluator(Camp.WHITE, Camp.WHITE, PieceType.QUEEN);
+    protected final AttackEvaluator blackEmptyEvaluator = new AttackEvaluator(Camp.BLACK, Camp.WHITE, PieceType.EMPTY);
+    protected final AttackEvaluator whiteEmptyEvaluator = new AttackEvaluator(Camp.WHITE, Camp.BLACK, PieceType.EMPTY);
 }
