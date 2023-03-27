74200e422ba392f6b10ca5b633d2dce4828cb27d
yenawee <yaena0319@naver.com>
2023-03-27 14:07:53 +0900

    feat : docker 실행 시 chessgame 테이블 생성되도록 설정

--- src/main/java/chess/docker/db/mysql/init/init.sql
+++ src/main/java/chess/docker/db/mysql/init/init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE chess_game
 (
-    piece_type VARCHAR(255),
+    piece_type VARCHAR(255) NOT NULL,
     piece_rank TINYINT(10) NOT NULL,
     piece_file TINYINT(10) NOT NULL,
-    team       VARCHAR(255),
+    team       VARCHAR(255) NOT NULL,
     turn       VARCHAR(255) NOT NULL
 );
