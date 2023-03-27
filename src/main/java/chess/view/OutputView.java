ff9efcf6801c739b90b659f1238280f757d15974
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-03-27 12:32:28 +0900

    feat : 게임 최신 저장 기록 출력 기능 추가

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -4,7 +4,9 @@ import chess.domain.Color;
 import chess.domain.Position;
 import chess.domain.piece.Piece;
 import chess.domain.point.Point;
+import chess.dto.GameDto;
 
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
@@ -13,10 +15,12 @@ public class OutputView {
 
     public static void printStartMessage() {
         System.out.print(
-                "> 체스 게임을 시작합니다." + System.lineSeparator() +
-                        "> 게임 시작 : start" + System.lineSeparator() +
-                        "> 게임 종료 : end" + System.lineSeparator() +
-                        "> 게임 이동 : move source위치 target위치 - 예. move b2 b3" + System.lineSeparator()
+                "> 체스 게임을 시작합니다." + System.lineSeparator()
+                        + "> 게임 시작 : start" + System.lineSeparator()
+                        + "> 게임 종료 : end" + System.lineSeparator()
+                        + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3" + System.lineSeparator()
+                        + "> 게임 상태 보기 : status" + System.lineSeparator()
+                        + "> 게임 리스트 불러오기 : load_list" + System.lineSeparator()
         );
     }
 
@@ -31,5 +35,18 @@ public class OutputView {
                 + "흰색 플레이어 점수 : " + whitePoint + System.lineSeparator()
                 + "체스 결과 : " + winner.getPlayer()
         );
+    }
+
+    public static void printFindAllGames(final List<GameDto> games) {
+        final StringBuilder gamesBuilder = new StringBuilder();
+        final String form = "%10s%15s%15s";
+        gamesBuilder.append(System.lineSeparator())
+                .append(String.format(form, "게임 번호", "게임 종료 여부", "현재 턴 소유자"))
+                .append(System.lineSeparator());
+
+        games.forEach(game -> gamesBuilder.append(String.format(form, game.getGameId(), game.isEnd(), game.getLastPlayer().getPlayer()))
+                .append(System.lineSeparator()));
+
+        System.out.println(gamesBuilder);
     }
 }
