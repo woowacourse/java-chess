554c93efec78d3301ac1ab12129299453487a9e3
songusika <sws981202@gmail.com>
2023-03-27 14:43:47 +0900

    test: 점수 덧셈 뺼셈 곱셈 기능 테스트

--- src/test/java/chess/domain/piece/ScoreTest.java
+++ src/test/java/chess/domain/piece/ScoreTest.java
@@ -3,6 +3,7 @@ package chess.domain.piece;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -34,4 +35,42 @@ class ScoreTest {
         );
     }
 
+    @Test
+    void 점수는_다른_점수를_받아_덧셈을_할_수_있다(){
+        //given
+        final Score score1 = Score.from(1);
+        final Score score2 = Score.from(2);
+
+        //when
+        final Score sumScore = score1.add(score2);
+
+        //then
+        assertThat(sumScore).isEqualTo(Score.from(3));
+    }
+
+    @Test
+    void 점수는_다른_점수를_받아_뺼샘을_할_수_있다(){
+        //given
+        final Score score1 = Score.from(1);
+        final Score score2 = Score.from(2);
+
+        //when
+        final Score subtractScore = score2.subtract(score1);
+
+        //then
+        assertThat(subtractScore).isEqualTo(Score.from(1));
+    }
+
+    @Test
+    void 점수는_비율을_받아_곱을_할_수_있다(){
+        //given
+        final Score score = Score.from(5);
+        final double ratio = 0.5;
+
+        //when
+        final Score resultScore = score.multiply(ratio);
+
+        //then
+        assertThat(resultScore).isEqualTo(Score.from(2.5));
+    }
 }
