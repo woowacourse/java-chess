d4fb82bd2bd33b120046d98cfc472bd5ac056d8f
BackFoxx <backfoxxx@gmail.com>
2023-03-27 18:49:36 +0900

    feat : move 명령과 관련된 BootingCommand 값 구현

--- src/main/java/view/BootingCommand.java
+++ src/main/java/view/BootingCommand.java
@@ -2,10 +2,12 @@ package view;
 
 import java.util.Arrays;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public enum BootingCommand {
     START("start"::equals),
-    END("end"::equals);
+    END("end"::equals),
+    MOVE(command -> Pattern.matches(Constants.MOVE_COMMAND_REGEX, command));
 
     private final Predicate<String> condition;
 
@@ -22,5 +24,9 @@ public enum BootingCommand {
 
     public boolean isStart() {
         return this == START;
+    }
+
+    private static class Constants {
+        private static final String MOVE_COMMAND_REGEX = "^move [a-z][0-9] [a-z][0-9]$";
     }
 }
--- src/test/java/view/BootingCommandTest.java
+++ src/test/java/view/BootingCommandTest.java
@@ -18,7 +18,21 @@ class BootingCommandTest {
     }
 
     @Test
-    @DisplayName("주어진 커멘드에 알맞는 BootingCommand 객체를 가져온다.")
+    @DisplayName("move 문법에 알맞는 명령어가 주어지면 알맞는 BootingCommand 객체를 가져온다.")
+    void moveCommand() {
+        assertThat(BootingCommand.findByCommand("move e3 e2"))
+                .isEqualTo(BootingCommand.MOVE);
+    }
+
+    @Test
+    @DisplayName("move 문법에 알맞지 않은 명령어가 주어지면 예외가 발생한다.")
+    void invalidMoveCommand() {
+        assertThatThrownBy(() -> BootingCommand.findByCommand("move e3e2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("주어진 커멘드에 알맞는 BootingCommand 객체가 없으면 예외를 반환한다.")
     void findByInvalidCommand() {
         assertThatThrownBy(() -> BootingCommand.findByCommand("invalid"))
                 .isInstanceOf(IllegalArgumentException.class);
