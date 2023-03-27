83a9ddc202665863d5a11e4e88fb33f0ee6edd1c
LJW25 <dbjwlee@gmail.com>
2023-03-27 09:24:11 +0900

    test: DAO 테스트 추가

--- src/main/java/chess/dao/ChessGameDao.java
+++ src/main/java/chess/dao/ChessGameDao.java
@@ -5,16 +5,20 @@ import chess.domain.piece.Team;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ChessGameDao {
+public final class ChessGameDao {
     private static final String URL = "jdbc:mysql://localhost:13306/"; // MySQL 서버 주소
     private static final String DATABASE = "chess"; // MySQL DATABASE 이름
     private static final String OPTION = "?useSSL=false&serverTimezone=UTC";
     private static final String USERNAME = "user"; //  MySQL 서버 아이디
     private static final String PASSWORD = "password"; // MySQL 서버 비밀번호
-    public static final String QUERY_INSERT_CHESS_GAME = "INSERT INTO chess_game VALUES(?, ?, ?, default)";
-    public static final String WHITE_MARK = "W";
-    public static final String BLACK_MARK = "B";
+    private static final String QUERY_INSERT_CHESS_GAME = "INSERT INTO chess_game VALUES(?, ?, ?, default)";
+    private static final String WHITE_MARK = "W";
+    private static final String BLACK_MARK = "B";
+    public static final String QUERY_SELECT_ROOM_NAMES = "SELECT room_name FROM chess_game";
+    public static final String QUERY_DELETE_CHESS_GAME = "DELETE FROM chess_game WHERE room_name = ?";
 
     public Connection getConnection() {
         // 드라이버 연결
@@ -28,9 +32,8 @@ public class ChessGameDao {
     }
 
     public void addGame(RoomName roomName, Team turn, String chessBoard) {
-        final var query = QUERY_INSERT_CHESS_GAME;
         try (final var connection = getConnection();
-             final var preparedStatement = connection.prepareStatement(query)) {
+             final var preparedStatement = connection.prepareStatement(QUERY_INSERT_CHESS_GAME)) {
             preparedStatement.setString(1, roomName.getRoomName());
             preparedStatement.setString(2, convertTurn(turn));
             preparedStatement.setString(3, chessBoard);
@@ -48,5 +51,33 @@ public class ChessGameDao {
             return BLACK_MARK;
         }
         throw new IllegalArgumentException("잘못된 팀입니다.");
+    }
+
+    public List<String> findRoomNames() {
+        final List<String> roomNames = new ArrayList<>();
+
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(QUERY_SELECT_ROOM_NAMES)) {
+
+            final var resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                roomNames.add(resultSet.getString("room_name"));
+            }
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return roomNames;
+    }
+
+    public void deleteGame(final RoomName roomName) {
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(QUERY_DELETE_CHESS_GAME)) {
+            preparedStatement.setString(1, roomName.getRoomName());
+            preparedStatement.execute();
+
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/chess/dao/ChessGameDaoTest.java
@@ -0,0 +1,43 @@
+package chess.dao;
+
+import chess.controller.ChessBoardFormatter;
+import chess.domain.chessboard.ChessBoard;
+import chess.domain.chessboard.ChessFactory;
+import chess.domain.piece.Team;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ChessGameDaoTest {
+
+    private final ChessGameDao chessGameDao = new ChessGameDao();
+
+    @Test
+    void DB_연결을_확인한다() {
+        try (final var connection = chessGameDao.getConnection()) {
+            assertThat(connection).isNotNull();
+        } catch (Exception e) {
+            System.out.println("DB 연결에 실패하였습니다");
+        }
+    }
+
+    @Test
+    void 게임을_DB에_저장한다() {
+        ChessBoardFormatter chessBoardFormatter = ChessBoardFormatter.toString(new ChessBoard(ChessFactory.create()));
+        RoomName testRoomName = new RoomName("test_room");
+
+        assertDoesNotThrow(() ->
+                chessGameDao.addGame(testRoomName, Team.WHITE, chessBoardFormatter.getOneLineChessBoard()));
+
+        chessGameDao.deleteGame(testRoomName);
+    }
+
+    @Test
+    void DB에서_방이름_목록을_불러온다() {
+        assertDoesNotThrow(() -> chessGameDao.findRoomNames());
+    }
+}
