fa1135e33497c2933be3762116ff7b8958b05129
somsom13 <somin1113@naver.com>
2023-03-27 17:17:53 +0900

    test: 체스 게임 점수 계산 테스트 추가

--- src/test/java/chess/domain/GameResultTest.java
+++ src/test/java/chess/domain/GameResultTest.java
@@ -5,27 +5,73 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.position.Position;
 import java.util.Map;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 class GameResultTest {
 
-    @DisplayName("체스말의 배치에 따른 팀별 점수를 계산한다.")
+    @DisplayName("같은 세로줄에 같은 팀의 폰이 2개 있다면 0.5점으로 계산한다.")
     @Test
-    void 팀별_점수_계산() {
+    void 같은_줄_2개_폰() {
         GameResult gameResult = GameResult.from(
             Map.of(Position.from("e6"), BLACK_QUEEN.getPiece(),
                 Position.from("e1"), WHITE_ROOK.getPiece(),
                 Position.from("f4"), WHITE_KNIGHT.getPiece(),
                 Position.from("f3"), WHITE_PAWN.getPiece(),
                 Position.from("f2"), WHITE_PAWN.getPiece(),
-                Position.from("f1"), WHITE_KING.getPiece(),
+                Position.from("g4"), WHITE_QUEEN.getPiece()));
+
+        assertThat(gameResult.calculateScoreOfTeam(TeamColor.WHITE)).isEqualTo(17.5);
+    }
+
+    @DisplayName("같은 팀의 폰이 다른 세로줄에 있을 때는 각각 1점으로 계산한다.")
+    @Test
+    void 다른_줄_2개_폰() {
+        GameResult gameResult = GameResult.from(
+            Map.of(Position.from("e6"), BLACK_QUEEN.getPiece(),
                 Position.from("g4"), WHITE_QUEEN.getPiece(),
                 Position.from("g2"), WHITE_PAWN.getPiece(),
                 Position.from("h3"), WHITE_PAWN.getPiece()));
 
-        assertThat(gameResult.calculateScoreOfTeam(TeamColor.WHITE)).isEqualTo(19.5);
+        assertThat(gameResult.calculateScoreOfTeam(TeamColor.WHITE)).isEqualTo(11);
+    }
+
+
+    @DisplayName("서로 다른 팀의 폰이 같은 줄에 각각 1개 있을 때는 1점으로 계산한다.")
+    @Test
+    void 같은_줄_상대_폰_2개() {
+        GameResult gameResult = GameResult.from(
+            Map.of(Position.from("e6"), BLACK_QUEEN.getPiece(),
+                Position.from("g4"), WHITE_QUEEN.getPiece(),
+                Position.from("h7"), BLACK_PAWN.getPiece(),
+                Position.from("h3"), WHITE_PAWN.getPiece()));
+
+        assertThat(gameResult.calculateScoreOfTeam(TeamColor.WHITE)).isEqualTo(10);
+    }
+
+    @DisplayName("King 이 살아있을 때, King 의 점수는 0점으로 계산한다.")
+    @Test
+    void King_0점() {
+        GameResult gameResult = GameResult.from(
+            Map.of(Position.from("e6"), BLACK_QUEEN.getPiece(),
+                Position.from("g4"), WHITE_QUEEN.getPiece(),
+                Position.from("e1"), WHITE_KING.getPiece(),
+                Position.from("h3"), WHITE_PAWN.getPiece()));
+
+        assertThat(gameResult.calculateScoreOfTeam(TeamColor.WHITE)).isEqualTo(10);
+    }
+
+    @DisplayName("체스판의 초기 점수는 38점이다.")
+    @Test
+    void 체스판_초기_점수() {
+        GameResult gameResult = GameResult.from(InitialPiece.getPiecesWithPosition());
+
+        Assertions.assertAll(
+            () -> assertThat(gameResult.calculateScoreOfTeam(TeamColor.WHITE)).isEqualTo(38),
+            () -> assertThat(gameResult.calculateScoreOfTeam(TeamColor.BLACK)).isEqualTo(38)
+        );
     }
 
 }
