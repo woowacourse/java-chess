5a8ae06c86245fd81a5561cde7e5497793952328
songusika <sws981202@gmail.com>
2023-03-27 17:52:23 +0900

    refactor: 코드 리포매팅

--- src/main/java/chess/domain/piece/Empty.java
+++ src/main/java/chess/domain/piece/Empty.java
@@ -43,7 +43,7 @@ public final class Empty implements PieceState {
     }
 
     @Override
-    public Score getScore(){
+    public Score getScore() {
         return Score.from(0);
     }
 }
--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -11,7 +11,7 @@ public enum PieceType {
 
     final Score score;
 
-    PieceType(final Score score){
+    PieceType(final Score score) {
         this.score = score;
     }
 }
