fb6e32a49b81a1ffe568fddd2f3be5c749f7b8f4
songusika <sws981202@gmail.com>
2023-03-28 04:40:10 +0900

    fix: 체스 게임 테스트 코드 수정

--- src/test/java/chess/domain/game/ChessGameTest.java
+++ src/test/java/chess/domain/game/ChessGameTest.java
@@ -2,11 +2,9 @@ package chess.domain.game;
 
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-import chess.controller.command.Command;
-import chess.controller.command.CommandType;
+import chess.domain.chessboard.Coordinate;
 import chess.domain.piece.Score;
 import chess.domain.piece.Team;
-import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -18,18 +16,25 @@ class ChessGameTest {
     void 체스게임은_각_팀의_점수를_계산할_수_있다() {
         //given
         final ChessGame game = new ChessGame();
-        final Command start = new Command(CommandType.START, List.of());
-        final Command command1 = new Command(CommandType.MOVE, List.of("a2", "a4"));
-        final Command command2 = new Command(CommandType.MOVE, List.of("a7", "a5"));
-        final Command command3 = new Command(CommandType.MOVE, List.of("b2", "b4"));
-        final Command command4 = new Command(CommandType.MOVE, List.of("a5", "b4"));
+
+        final Coordinate from1 = Coordinate.from("a2");
+        final Coordinate to1 = Coordinate.from("a4");
+
+        final Coordinate from2 = Coordinate.from("a7");
+        final Coordinate to2 = Coordinate.from("a5");
+
+        final Coordinate from3 = Coordinate.from("b2");
+        final Coordinate to3 = Coordinate.from("b4");
+
+        final Coordinate from4 = Coordinate.from("a5");
+        final Coordinate to4 = Coordinate.from("b4");
 
         //when
-        game.execute(start);
-        game.execute(command1);
-        game.execute(command2);
-        game.execute(command3);
-        game.execute(command4);
+        game.start();
+        game.move(from1, to1);
+        game.move(from2, to2);
+        game.move(from3, to3);
+        game.move(from4, to4);
 
         //then
         final Score black = game.getScore().get(Team.BLACK);
