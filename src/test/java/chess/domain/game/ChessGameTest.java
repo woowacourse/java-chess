62cfe50b05be9c8fad8b8a1047e3233a07ea4be5
yujamint <woawoa0513@gmail.com>
2023-03-27 12:41:04 +0900

    refactor(ChessGameTest): 생성자 수정에 따른 테스트 코드 변경

--- src/test/java/chess/domain/game/ChessGameTest.java
+++ src/test/java/chess/domain/game/ChessGameTest.java
@@ -1,21 +1,16 @@
 package chess.domain.game;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import chess.controller.GameStatus;
-import chess.domain.board.Board;
+import chess.dao.ChessGameDao;
+import chess.dao.PiecesDao;
 import chess.domain.board.File;
 import chess.domain.board.Position;
 import chess.domain.board.Rank;
-import chess.domain.piece.AllPiecesGenerator;
-import chess.domain.piece.Pieces;
 import chess.domain.piece.Side;
-import chess.domain.piece.type.King;
-import chess.domain.piece.type.Knight;
-import chess.domain.piece.type.Pawn;
-import chess.domain.piece.type.Queen;
-import chess.domain.piece.type.Rook;
+import chess.domain.piece.type.Piece;
+import java.sql.Connection;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,9 +21,8 @@ class ChessGameTest {
     @DisplayName("흰색 진영이 기물을 옮길 차례일 때, 검은색 진영의 기물을 옮기면 예외가 발생한다.")
     void checkTurnToMove_whiteTurn_throws() {
         // given
-        final ChessGame chessGame = new ChessGame(new Board(new Pieces(new AllPiecesGenerator())),
-                Side.WHITE,
-                GameStatus.START);
+        final ChessGame chessGame = new ChessGame(new FakePiecesDao(), new FakeChessGameDao(), GameStatus.INIT);
+        chessGame.startNewGame();
         final Position blackPiecePosition = new Position(File.A, Rank.SEVEN);
         final Position targetPosition = new Position(File.A, Rank.SIX);
 
@@ -38,66 +32,53 @@ class ChessGameTest {
                 .hasMessage("[ERROR] 상대방의 말은 이동시킬 수 없습니다.");
     }
 
-    @Test
-    @DisplayName("검은색 진영이 기물을 옮길 차례일 때, 흰색 진영의 기물을 옮기면 예외가 발생한다.")
-    void checkTurnToMove_blackTurn_throws() {
-        // given
-        final ChessGame chessGame = new ChessGame(new Board(new Pieces(new AllPiecesGenerator())),
-                Side.BLACK,
-                GameStatus.START);
-        final Position whitePiecePosition = new Position(File.A, Rank.TWO);
-        final Position targetPosition = new Position(File.A, Rank.THREE);
+    class FakePiecesDao implements PiecesDao {
+        @Override
+        public Connection getConnection() {
+            return null;
+        }
 
-        // when, then
-        assertThatThrownBy(() -> chessGame.movePiece(whitePiecePosition, targetPosition))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 상대방의 말은 이동시킬 수 없습니다.");
+        @Override
+        public List<Piece> findAll() {
+            return null;
+        }
+
+        @Override
+        public void insertAll(final List<Piece> pieces) {
+
+        }
+
+        @Override
+        public void delete() {
+
+        }
     }
 
-    @Test
-    @DisplayName("한 파일 위에 폰이 여러개 존재한다면, 해당 폰은 개당 0.5점으로 계산한다.")
-    void calculateScoreBySide() {
-        // given
-        Board fixedBoard = new Board(new Pieces(() -> List.of(
-                new Pawn(new Position(File.A, Rank.ONE), Side.WHITE),
-                new Pawn(new Position(File.A, Rank.THREE), Side.WHITE),
+    class FakeChessGameDao implements ChessGameDao {
+        @Override
+        public Connection getConnection() {
+            return null;
+        }
 
-                new Pawn(new Position(File.B, Rank.TWO), Side.WHITE),
-                new Pawn(new Position(File.B, Rank.THREE), Side.WHITE),
-                new Pawn(new Position(File.B, Rank.SEVEN), Side.WHITE),
+        @Override
+        public Side selectTurn() {
+            return null;
+        }
 
-                new Knight(new Position(File.B, Rank.ONE), Side.WHITE)
-        )));
-        final ChessGame chessGame = new ChessGame(fixedBoard, Side.WHITE, GameStatus.START);
+        @Override
+        public void updateTurn(final Side side) {
 
-        // when, then
-        assertThat(chessGame.calculateScoreBySide(Side.WHITE)).isEqualTo(5);
+        }
+
+        @Override
+        public void delete() {
+
+        }
+
+        @Override
+        public void insertTurn(final Side turn) {
+
+        }
     }
 
-    @Test
-    @DisplayName("킹이 죽으면 게임이 끝난다.")
-    void gameEnd_kingDead() {
-        // given
-        Board fixedBoard = new Board(new Pieces(() -> List.of(
-                new King(new Position(File.E, Rank.ONE), Side.WHITE),
-                new Rook(new Position(File.A, Rank.ONE), Side.WHITE),
-                new Knight(new Position(File.B, Rank.ONE), Side.WHITE),
-
-                new King(new Position(File.A, Rank.EIGHT), Side.BLACK),
-                new Queen(new Position(File.D, Rank.EIGHT), Side.BLACK),
-                new Pawn(new Position(File.B, Rank.SEVEN), Side.BLACK)
-        )));
-        final ChessGame chessGame = new ChessGame(fixedBoard, Side.WHITE, GameStatus.START);
-        final GameStatus blackKingAliveStatus = chessGame.status();
-
-        // when
-        final Position whiteRookPosition = new Position(File.A, Rank.ONE);
-        final Position blackKingPosition = new Position(File.A, Rank.EIGHT);
-        chessGame.movePiece(whiteRookPosition, blackKingPosition);
-        final GameStatus blackKingDeadStatus = chessGame.status();
-
-        // then
-        assertThat(blackKingAliveStatus).isSameAs(GameStatus.START);
-        assertThat(blackKingDeadStatus).isSameAs(GameStatus.END);
-    }
 }
