d1c26f503647d6a808c852debc30bcfada222059
thdwoqor <celebarte01@gmail.com>
2023-03-28 17:01:50 +0900

    test : assertDoesNotThrow 및 displayName 추가

--- src/test/java/database/GameDaoTest.java
+++ src/test/java/database/GameDaoTest.java
@@ -13,20 +13,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class GameDaoTest {
     @Test
+    @DisplayName("게임 생성 테스트")
     void create() {
         GameDao gameDao = new GameDao(ConnectionGenerator.getConnection());
         Board board = Board.from(PieceCache.create());
-        gameDao.createGame(board.getBoard());
+        assertDoesNotThrow(()->gameDao.createGame(board.getBoard()));
     }
 
     @Test
-    @DisplayName("게임보드가 정상적으로 64개 생성되었는지 테스트")
+    @DisplayName("마지막 게임보드가 정상적으로 64개 생성되었는지 테스트")
     void findByLastGameBoard() {
         GameDao gameDao = new GameDao(ConnectionGenerator.getConnection());
         GameDto gameDto = gameDao.findByLastGame();
         Map<Position, Piece> board = gameDao.findByLastGameBoard(gameDto.getId());
-        Assertions.assertThat(board.size()).isEqualTo(64);
+        Assertions.assertThat(board).hasSize(64);
     }
 }
