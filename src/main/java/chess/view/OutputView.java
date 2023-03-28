971e9e11ed746523930eb7d10cf74e6f5b0b54c5
hanueleee <ezzeezz@naver.com>
2023-03-28 17:52:49 +0900

    refactor(view): 게임 시작 안내 문구 출력 형식 수정

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -10,11 +10,12 @@ import java.util.stream.Collectors;
 public class OutputView {
     
     public static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String DECORATION = "**********************";
+    private static final String GAME_ROOM_INFO_MESSAGE = "**%d번 게임방으로 입장합니다**";
     private static final String GAME_HEADER = "> 체스 게임을 시작합니다.";
-    private static final String GAME_START_INFO_MESSAGE = "> 게임 시작 : start";
+    private static final String GAME_START_INFO_MESSAGE = "> 게임 시작 : start (이미 시작된 게임일 경우 해당 명령은 불가능합니다)";
     private static final String GAME_END_INFO_MESSAGE = "> 게임 종료 : end";
     private static final String GAME_MOVE_INFO_MESSAGE = "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
-    private static final String GAME_ROOM_INFO_MESSAGE = "***%d번 게임방으로 입장합니다***";
     private static final String STATUS_MESSAGE = "현재 %s의 점수는 %.1f입니다.";
     private static final String KING_CATCH_MESSAGE = "King을 잡았습니다. 게임이 종료됩니다.";
     private static final String WINNER_MESSAGE = "우승자는 %s입니다.";
@@ -31,12 +32,15 @@ public class OutputView {
         System.out.println(IMPOSSIBLE_ROOM_INFO + String.join(", ", stringImpossibleIds));
     }
     public static void printGameStartMessage(int gameId) {
+        System.out.println();
+        System.out.println(DECORATION);
+        System.out.println(String.format(GAME_ROOM_INFO_MESSAGE, gameId));
+        System.out.println(DECORATION);
+
         System.out.println(GAME_HEADER);
         System.out.println(GAME_START_INFO_MESSAGE);
         System.out.println(GAME_END_INFO_MESSAGE);
         System.out.println(GAME_MOVE_INFO_MESSAGE);
-        System.out.println();
-        System.out.println(String.format(GAME_ROOM_INFO_MESSAGE, gameId));
         System.out.println();
     }
 
