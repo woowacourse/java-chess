7013a35f2c0f2d27b9f92e4e4acf8354efe0e045
thdwoqor <celebarte01@gmail.com>
2023-03-28 23:51:11 +0900

    test : GameDao 테스트 수정

--- src/test/java/database/GameDaoTest.java
+++ src/test/java/database/GameDaoTest.java
@@ -1,17 +1,10 @@
 package database;
 
-import chess.cache.PieceCache;
 import chess.dao.GameDao;
 import chess.dao.ConnectionGenerator;
-import chess.domain.Board;
-import chess.domain.Position;
-import chess.domain.piece.Piece;
-import chess.dto.GameDto;
-import org.assertj.core.api.Assertions;
+import chess.dao.GameJdbcDao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -19,17 +12,7 @@ public class GameDaoTest {
     @Test
     @DisplayName("게임 생성 테스트")
     void create() {
-        GameDao gameDao = new GameDao(ConnectionGenerator.getConnection());
-        Board board = Board.from(PieceCache.create());
-        assertDoesNotThrow(()->gameDao.createGame(board.getBoard()));
-    }
-
-    @Test
-    @DisplayName("마지막 게임보드가 정상적으로 64개 생성되었는지 테스트")
-    void findByLastGameBoard() {
-        GameDao gameDao = new GameDao(ConnectionGenerator.getConnection());
-        GameDto gameDto = gameDao.findByLastGame();
-        Map<Position, Piece> board = gameDao.findByLastGameBoard(gameDto.getId());
-        Assertions.assertThat(board).hasSize(64);
+        GameDao gameDao = new GameJdbcDao(ConnectionGenerator.getConnection());
+        assertDoesNotThrow(gameDao::create);
     }
 }
