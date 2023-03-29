801b9f7f59dad27ae8e9ffb1df79ca46d6795a74
Cyma-s <ssol0319@naver.com>
2023-03-29 22:24:42 +0900

    refactor(ChessJdbcDao): 접근 제어자 변경

--- src/main/java/chess/dao/ChessJdbcDao.java
+++ src/main/java/chess/dao/ChessJdbcDao.java
@@ -15,7 +15,7 @@ public class ChessJdbcDao implements ChessDao {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
-    public Connection getConnection() {
+    private Connection getConnection() {
         try {
             return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
         } catch (final SQLException e) {
