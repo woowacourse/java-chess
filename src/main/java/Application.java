cb56902e71b7ea4a249632cbe2b7e69d94d3fa72
ReO <yt930310@gmail.com>
2023-03-29 16:06:59 +0900

    refactor: todo 삭제

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,6 @@
 import controller.ChessController;
 
 public class Application {
-    // TODO: 2023/03/25 점수 계산 테스트 코드
-    // TODO: 2023/03/25 DB 연결
     public static void main(String[] args) {
         new ChessController().run();
     }
