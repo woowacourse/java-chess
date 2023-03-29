40e1a7592b72900222da4920af39ff2748cecbfd
dhkim <akdlzmf1123@gmail.com>
2023-03-29 21:25:46 +0900

    style: 마지막 줄 개행 추가 및 주석 제거

--- src/test/java/chess/controller/CommandTypeTest.java
+++ src/test/java/chess/controller/CommandTypeTest.java
@@ -33,27 +33,4 @@ public class CommandTypeTest {
             }
         }
     }
-
-//    @Nested
-//    @DisplayName("getNextStatus 메서드는")
-//    class getNextStatus {
-//
-//        @ParameterizedTest(name = "{0}가 주어지면 예외를 던진다")
-//        @ValueSource(strings = {"", "  ", "Move", "enD"})
-//        void it_throws_exception2(String code) {
-//            assertThatThrownBy(() -> CommandType.getNextStatus(code))
-//                    .isInstanceOf(IllegalArgumentException.class)
-//                    .hasMessage("잘못된 명령어입니다.");
-//        }
-//
-//        @Nested
-//        @DisplayName("사용자 입력으로")
-//        class given_code {
-//            @ParameterizedTest(name = "{0}가 주어지면 객체 {1}을 반환한다")
-//            @CsvSource({"move,PLAYING", "end,END"})
-//            void it_returns_gameStatus(String code, CommandType commandType) {
-//                assertThat(CommandType.getNextStatus(code)).isEqualTo(commandType);
-//            }
-//        }
-//    }
 }
