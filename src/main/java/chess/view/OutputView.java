c2cb8758b9ae0082e460cec4222c0da14dc2c1a2
iamjooon2 <iamjooon2@gmail.com>
2023-03-29 20:51:26 +0900

    refactor: 불필요한 시작 명령어 안내 중복 제거

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -42,7 +42,6 @@ public class OutputView {
         System.out.println("> 체스 게임을 시작합니다.");
         System.out.println("> 새 게임 시작 : start");
         System.out.println("> 저장된 게임 시작 : load");
-        System.out.println("> 게임 시작 : start");
         System.out.println("> 점수 확인 : status");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
