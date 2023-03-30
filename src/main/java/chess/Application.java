241e8fd3c6f1200c228081b2a8fa60ce904fab95
rawfishthelgh <rg970604@naver.com>
2023-03-31 08:32:03 +0900

    refactor : 이름 변경으로인한 수정

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,13 +1,13 @@
 package chess;
 
 import chess.dao.ChessGameDao;
-import chess.dao.DBChessGameDao;
+import chess.dao.MysqlChessGameDao;
 import chess.controller.ChessController;
 import chess.domain.board.ChessBoard;
 
 public class Application {
     public static void main(String[] args) {
-        ChessGameDao chessGameDao = new DBChessGameDao();
+        ChessGameDao chessGameDao = new MysqlChessGameDao();
         ChessController chessController = new ChessController(chessGameDao);
         chessController.runChessGame(ChessBoard.create());
     }
