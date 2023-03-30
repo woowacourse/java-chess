9abf3c0e951c7e6cd34b64da69844b3ebead7d3b
hong-sile <gurwns9325@naver.com>
2023-03-31 02:35:46 +0900

    test: test용 DB의 connection을 반환해주는 TestConnectionGenerator 추가

---
+++ src/test/java/chess/model/dao/TestConnectionGenerator.java
@@ -0,0 +1,26 @@
+package chess.model.dao;
+
+import chess.model.exception.DataBaseCanNotConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnectionGenerator {
+
+    private static final String SERVER = "localhost:13307";
+    private static final String DATABASE = "chess";
+    private static final String OPTION = "?useSSL=true&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            final Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+            connection.setAutoCommit(false);
+            return connection;
+        } catch (final SQLException e) {
+            throw new DataBaseCanNotConnectException();
+        }
+    }
+}
