b27a721a4c3df82b81aa7423fa4874830fe54cef
chaewon121 <cw121@naver.com>
2023-03-31 03:50:51 +0900

    feat: team 클래스 기능 추가

--- src/main/java/chess/domain/pieces/component/Team.java
+++ src/main/java/chess/domain/pieces/component/Team.java
@@ -1,7 +1,27 @@
 package chess.domain.pieces.component;
 
+import java.util.Arrays;
+
 public enum Team {
-    BLACK,
-    WHITE,
-    NEUTRALITY
+    BLACK("블랙"),
+    WHITE("화이트"),
+    NEUTRALITY("중립");
+
+    private final String value;
+
+    Team(String value) {
+        this.value = value;
+    }
+
+    public static Team of(final String input) {
+
+        return Arrays.stream(Team.values())
+                .filter(team -> team.value.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("[ERROR] 존재하지 않는 팀입니다."));
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
