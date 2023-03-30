456dfffd22b9d13c351d3dcbaa108f7b7aa2f984
yenawee <yaena0319@naver.com>
2023-03-31 00:12:00 +0900

    feat : init.sql 추가

---
+++ src/main/java/chess/docker/db/mysql/init/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE chess_game (
+    piece_type VARCHAR(255) NOT NULL,
+    piece_rank TINYINT(10) NOT NULL,
+    piece_file VARCHAR(255) NOT NULL,
+    team VARCHAR(255) NOT NULL,
+    turn VARCHAR(255) NOT NULL
+)
