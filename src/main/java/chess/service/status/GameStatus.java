ccf5e0fd20fc57b2675c06ecb921e5912cd84369
yoondgu <doyou2ing@naver.com>
2023-03-30 20:00:38 +0900

    refactor: 상태검증 클래스 분리로 인해 사용하지 않는 메서드 삭제

--- src/main/java/chess/service/status/GameStatus.java
+++ src/main/java/chess/service/status/GameStatus.java
@@ -1,43 +1,9 @@
 package chess.service.status;
 
-import static chess.controller.CommandActionMapper.EXIT;
-import static chess.controller.CommandActionMapper.MOVE;
-import static chess.controller.CommandActionMapper.START;
-import static chess.controller.CommandActionMapper.STATUS;
-
 public enum GameStatus {
 
     READY,
     RUNNING,
-    OVER;
+    OVER
 
-    public void validateCommand(String command) {
-        if (EXIT.getCommand().equalsIgnoreCase(command)) {
-            return;
-        }
-        if (this != OVER && isStatus(command)) {
-            throw new IllegalArgumentException("게임이 시작하지 않았거나, 오버 상태가 아닙니다.");
-        }
-        if (this == OVER && isMove(command)) {
-            throw new IllegalArgumentException("오버된 게임입니다.");
-        }
-        if (this == READY && (!isStart(command))) {
-            throw new IllegalArgumentException("아직 게임이 실행중이지 않습니다.");
-        }
-        if (this != READY && (isStart(command))) {
-            throw new IllegalArgumentException("이미 게임이 실행중입니다.");
-        }
-    }
-
-    private boolean isMove(String command) {
-        return MOVE.getCommand().equalsIgnoreCase(command);
-    }
-
-    private boolean isStatus(String command) {
-        return STATUS.getCommand().equalsIgnoreCase(command);
-    }
-
-    private boolean isStart(String command) {
-        return START.getCommand().equalsIgnoreCase(command);
-    }
 }
