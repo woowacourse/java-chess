34a67e49061615982d092004f94076efcb21898b
chaewon121 <cw121@naver.com>
2023-03-31 05:19:50 +0900

    refactor: boardFatory 캐싱

--- src/main/java/chess/domain/board/BoardFactory.java
+++ src/main/java/chess/domain/board/BoardFactory.java
@@ -9,41 +9,50 @@ import java.util.Map;
 
 public class BoardFactory {
 
+    private static final Map<Position, Piece> cache = new HashMap<>();
+
     public static Map<Position, Piece> create() {
-        Map<Position, Piece> init = new HashMap<>();
-        init.put(new Position(Rank.ONE, File.A), new Rook(Team.WHITE, Type.ROOK));
-        init.put(new Position(Rank.ONE, File.B), new Knight(Team.WHITE, Type.KNIGHT));
-        init.put(new Position(Rank.ONE, File.C), new Bishop(Team.WHITE, Type.BISHOP));
-        init.put(new Position(Rank.ONE, File.D), new Queen(Team.WHITE, Type.QUEEN));
-        init.put(new Position(Rank.ONE, File.E), new King(Team.WHITE, Type.KING));
-        init.put(new Position(Rank.ONE, File.F), new Bishop(Team.WHITE, Type.BISHOP));
-        init.put(new Position(Rank.ONE, File.G), new Knight(Team.WHITE, Type.KNIGHT));
-        init.put(new Position(Rank.ONE, File.H), new Rook(Team.WHITE, Type.ROOK));
+        initBlackTeam();
+        initNeutralityTeam();
+        initWhiteTeam();
+        return cache;
+    }
+
+    private static void initBlackTeam(){
+        cache.put(new Position(Rank.ONE, File.A), new Rook(Team.WHITE, Type.ROOK));
+        cache.put(new Position(Rank.ONE, File.B), new Knight(Team.WHITE, Type.KNIGHT));
+        cache.put(new Position(Rank.ONE, File.C), new Bishop(Team.WHITE, Type.BISHOP));
+        cache.put(new Position(Rank.ONE, File.D), new Queen(Team.WHITE, Type.QUEEN));
+        cache.put(new Position(Rank.ONE, File.E), new King(Team.WHITE, Type.KING));
+        cache.put(new Position(Rank.ONE, File.F), new Bishop(Team.WHITE, Type.BISHOP));
+        cache.put(new Position(Rank.ONE, File.G), new Knight(Team.WHITE, Type.KNIGHT));
+        cache.put(new Position(Rank.ONE, File.H), new Rook(Team.WHITE, Type.ROOK));
 
         for (File file : File.values()) {
-            init.put(new Position(Rank.TWO, file), new WhitePawn(Type.PAWN));
+            cache.put(new Position(Rank.TWO, file), new WhitePawn(Type.PAWN));
         }
+    }
 
+    private static void initNeutralityTeam(){
         for (int i = 3; i < 7; i++) {
             for (File file : File.values()) {
-                init.put(new Position(Rank.of(i), file), new EmptyPiece(Team.NEUTRALITY, Type.NO_PIECE));
+                cache.put(new Position(Rank.of(i), file), new EmptyPiece(Team.NEUTRALITY, Type.NO_PIECE));
             }
         }
+    }
 
+    private static void initWhiteTeam(){
         for (File file : File.values()) {
-            init.put(new Position(Rank.SEVEN, file), new BlackPawn(Type.PAWN));
+            cache.put(new Position(Rank.SEVEN, file), new BlackPawn(Type.PAWN));
         }
-
-        init.put(new Position(Rank.EIGHT, File.A), new Rook(Team.BLACK, Type.ROOK));
-        init.put(new Position(Rank.EIGHT, File.B), new Knight(Team.BLACK, Type.KNIGHT));
-        init.put(new Position(Rank.EIGHT, File.C), new Bishop(Team.BLACK, Type.BISHOP));
-        init.put(new Position(Rank.EIGHT, File.D), new Queen(Team.BLACK, Type.QUEEN));
-        init.put(new Position(Rank.EIGHT, File.E), new King(Team.BLACK, Type.KING));
-        init.put(new Position(Rank.EIGHT, File.F), new Bishop(Team.BLACK, Type.BISHOP));
-        init.put(new Position(Rank.EIGHT, File.G), new Knight(Team.BLACK, Type.KNIGHT));
-        init.put(new Position(Rank.EIGHT, File.H), new Rook(Team.BLACK, Type.ROOK));
-
-        return init;
+        cache.put(new Position(Rank.EIGHT, File.A), new Rook(Team.BLACK, Type.ROOK));
+        cache.put(new Position(Rank.EIGHT, File.B), new Knight(Team.BLACK, Type.KNIGHT));
+        cache.put(new Position(Rank.EIGHT, File.C), new Bishop(Team.BLACK, Type.BISHOP));
+        cache.put(new Position(Rank.EIGHT, File.D), new Queen(Team.BLACK, Type.QUEEN));
+        cache.put(new Position(Rank.EIGHT, File.E), new King(Team.BLACK, Type.KING));
+        cache.put(new Position(Rank.EIGHT, File.F), new Bishop(Team.BLACK, Type.BISHOP));
+        cache.put(new Position(Rank.EIGHT, File.G), new Knight(Team.BLACK, Type.KNIGHT));
+        cache.put(new Position(Rank.EIGHT, File.H), new Rook(Team.BLACK, Type.ROOK));
     }
 }
 
