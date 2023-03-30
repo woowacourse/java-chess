e968d5cb5a99183551ebcd6d229a0635d5cf58b3
kokodak <kokodakadokok@gmail.com>
2023-03-31 01:29:14 +0900

    refactor: 사용하지 않은 import 삭제

--- src/main/java/chess/controller/action/RoomAction.java
+++ src/main/java/chess/controller/action/RoomAction.java
@@ -1,8 +1,6 @@
 package chess.controller.action;
 
 import chess.domain.game.ChessGame;
-import chess.repository.dao.ChessGameDao;
-import chess.repository.dao.PieceDao;
 import java.util.List;
 
 @FunctionalInterface
