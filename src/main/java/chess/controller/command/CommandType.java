5e60e95873ebc7f249d4549d094f2a8299a469d8
hectick <chyeon1223@cau.ac.kr>
2023-03-31 00:05:25 +0900

    refactor(CommandType): 조건문 분기 단순화

--- src/main/java/chess/controller/command/CommandType.java
+++ src/main/java/chess/controller/command/CommandType.java
@@ -1,34 +1,21 @@
 package chess.controller.command;
 
-import java.util.Collections;
 import java.util.List;
 
 public enum CommandType {
 
-    CREATE, LOAD, WAIT, START, END, MOVE, STATUS, EMPTY;
+    CREATE, LOAD, WAIT, START, END, MOVE, STATUS;
 
-    public List<CommandType> getNextPossibleCommandTypes(boolean isGameEnd){
-        if(this == CREATE || this == LOAD){
+    public List<CommandType> getNextPossibleCommandTypes(boolean isGameEnd) {
+        if (this == CREATE || this == LOAD) {
             return List.of(START, END, MOVE, STATUS);
         }
-        if(this == WAIT){
+        if (this == WAIT) {
             return List.of(START, END);
         }
-        if(this == START){
+        if (!isGameEnd) {
             return List.of(END, MOVE, STATUS);
         }
-        if(this == MOVE && !isGameEnd){
-            return List.of(END, MOVE, STATUS);
-        }
-        if(this == MOVE){
-            return List.of(END);
-        }
-        if(this == STATUS && !isGameEnd){
-            return List.of(END, MOVE, STATUS);
-        }
-        if(this == STATUS){
-            return List.of(END);
-        }
-        return Collections.emptyList();
+        return List.of(END);
     }
 }
