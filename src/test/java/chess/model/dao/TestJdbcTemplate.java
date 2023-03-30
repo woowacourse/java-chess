b56882b943061d61ceb762b4a973ef000bf39776
hong-sile <gurwns9325@naver.com>
2023-03-31 02:33:01 +0900

    test: test용 jdbc template 추가

---
+++ src/test/java/chess/model/dao/TestJdbcTemplate.java
@@ -0,0 +1,64 @@
+package chess.model.dao;
+
+import chess.model.exception.QueryFailException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestJdbcTemplate implements JdbcTemplate {
+
+    private final Connection connection;
+
+    public TestJdbcTemplate(final Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void executeUpdate(final String query, final Object... parameters) {
+        try (final PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            for (int i = 1; i <= parameters.length; i++) {
+                preparedStatement.setObject(i, parameters[i - 1]);
+            }
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new QueryFailException();
+        }
+    }
+
+    @Override
+    public <T> T executeUpdate(final String query, final RowMapper<T> rowMapper, final Object... parameters) {
+        try (final Connection connection = ConnectionGenerator.getConnection();
+             final PreparedStatement preparedStatement =
+                     connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            for (int i = 1; i <= parameters.length; i++) {
+                preparedStatement.setObject(i, parameters[i - 1]);
+            }
+            preparedStatement.executeUpdate();
+            final ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            return rowMapper.mapRow(resultSet);
+        } catch (final SQLException e) {
+            throw new QueryFailException();
+        }
+    }
+
+    @Override
+    public <T> T executeQuery(final String query, final RowMapper<T> rowMapper, final Object... parameters) {
+        try (final Connection connection = ConnectionGenerator.getConnection();
+             final PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            for (int i = 1; i <= parameters.length; i++) {
+                preparedStatement.setObject(i, parameters[i - 1]);
+            }
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            return rowMapper.mapRow(resultSet);
+        } catch (final SQLException e) {
+            throw new QueryFailException();
+        }
+    }
+
+    public void rollBack() throws SQLException {
+        connection.rollback();
+        connection.close();
+    }
+}
