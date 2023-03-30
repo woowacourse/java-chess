453374fe9ada9a91cc45ffa89aaff7a90f03c823
xxeol2 <jhj01177@naver.com>
2023-03-30 11:21:29 +0900

    refactor: CommandType에서 enum의 valueOf를 활용하도록 수정

--- src/main/java/chess/view/dto/game/GameCommandType.java
+++ src/main/java/chess/view/dto/game/GameCommandType.java
@@ -2,7 +2,6 @@ package chess.view.dto.game;
 
 import static chess.view.InputView.INVALID_INPUT_MESSAGE;
 
-import java.util.Arrays;
 import java.util.Set;
 
 public enum GameCommandType {
@@ -12,10 +11,11 @@ public enum GameCommandType {
     private static final Set<GameCommandType> SINGLE_COMMAND_TYPES = Set.of(START, END, STATUS);
 
     public static GameCommandType from(String input) {
-        return Arrays.stream(values())
-                .filter(value -> value.name().equals(input))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(INVALID_INPUT_MESSAGE));
+        try {
+            return valueOf(input);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+        }
     }
 
     public boolean isSingleCommandType() {
--- src/main/java/chess/view/dto/ready/ReadyCommandType.java
+++ src/main/java/chess/view/dto/ready/ReadyCommandType.java
@@ -2,16 +2,15 @@ package chess.view.dto.ready;
 
 import static chess.view.InputView.INVALID_INPUT_MESSAGE;
 
-import java.util.Arrays;
-
 public enum ReadyCommandType {
 
     USE, CREATE;
 
     public static ReadyCommandType from(String input) {
-        return Arrays.stream(values())
-                .filter(value -> value.name().equals(input))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(INVALID_INPUT_MESSAGE));
+        try {
+            return valueOf(input);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+        }
     }
 }
