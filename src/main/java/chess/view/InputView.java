3162a407f7720f7cbf6e38dd79df17d3c93928ef
hum02 <ehdgma02@gmail.com>
2023-03-30 23:16:55 +0900

    refactor: inputView에서부터 command의 크기를 맞춰서 보내주기로 수정

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -9,7 +9,11 @@ public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> inputCommand(){
-        String command = scanner.nextLine();
-        return Arrays.stream(command.split(" ")).collect(Collectors.toList());
+        String input = scanner.nextLine();
+        List<String> command = Arrays.stream(input.split(" ")).collect(Collectors.toList());
+        while (command.size() < 3) {
+            command.add("");
+        }
+        return command;
     }
 }
