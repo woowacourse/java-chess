c4563f093b10e85fb3d79c525bab26283c40dc32
chaewon121 <cw121@naver.com>
2023-03-31 03:54:18 +0900

    feat: dao 연동 기능 추가

--- src/main/java/chess/service/ChessGame.java
+++ src/main/java/chess/service/ChessGame.java
@@ -1,10 +1,13 @@
 package chess.service;
 
+import chess.dao.GameDao;
+import chess.dao.PieceDao;
+import chess.dao.entity.GameEntity;
+import chess.dao.entity.PieceEntity;
 import chess.domain.board.*;
-import chess.domain.entity.PieceDao;
-import chess.domain.entity.PieceEntity;
 import chess.domain.pieces.EmptyPiece;
 import chess.domain.pieces.Piece;
+import chess.domain.pieces.PieceFactory;
 import chess.domain.pieces.component.Team;
 import chess.domain.pieces.component.Type;
 import chess.domain.state.*;
@@ -16,39 +19,58 @@ public class ChessGame {
 
     private Board board;
     private State state;
-    private PieceDao pieceDao;
+    private final PieceDao pieceDao;
+    private final GameDao gameDao;
 
     public ChessGame() {
         pieceDao = new PieceDao();
+        gameDao = new GameDao();
         state = new Ready();
     }
 
     public void start() {
+        state.checkStartState();
         if (pieceDao.hasData()) {
-            System.out.println("있어");
-            Map<Position, Piece> maps = new HashMap<>();
-            for (File file : File.values()) {
-                for (Rank rank : Rank.values()) {
-                    Position position = new Position(rank, file);
-                    maps.put(position, pieceDao.findByPosition(position));
-                }
-            }
-            this.board = Board.create(maps);
+            this.board = Board.create(makeMapFromDataBase());
+            initTurnFromDataBase();
+            return;
         }
-        if (!pieceDao.hasData()) {
-            System.out.println("옶오");
-            this.board = Board.create(BoardFactory.create());
-            initPiece();
-        }
-        state.startGame();
+        this.board = Board.create(BoardFactory.create());
+        initPieceToDataBase();
+
         this.state = new WhiteTurn();
+        gameDao.addTurn(new GameEntity(1, Team.WHITE.getValue()));
     }
 
-    public void initPiece() {
+    private void initTurnFromDataBase() {
+        if (Team.WHITE.getValue().equals(gameDao.findByGameId(1))) {
+            this.state = new WhiteTurn();
+            return;
+        }
+        this.state = new BlackTurn();
+    }
+
+    private Map<Position, Piece> makeMapFromDataBase() {
+        Map<Position, Piece> maps = new HashMap<>();
+        for (File file : File.values()) {
+            maps = putMap(maps, file);
+        }
+        return maps;
+    }
+
+    private Map<Position, Piece> putMap(Map<Position, Piece> maps, File file) {
+        for (Rank rank : Rank.values()) {
+            Position position = new Position(rank, file);
+            maps.put(position, pieceDao.findByPosition(position));
+        }
+        return maps;
+    }
+
+    private void initPieceToDataBase() {
         for (Position position : board.getBoard().keySet()) {
             pieceDao.addPiece(
                     new PieceEntity(
-                            board.getBoard().get(position).getType().getName(),
+                            PieceFactory.from(board.getBoard().get(position)),
                             position.getFile(),
                             position.getRank(),
                             1));
@@ -56,7 +78,7 @@ public class ChessGame {
     }
 
     public Board getBoard() {
-        return board;
+        return this.board;
     }
 
     public Map<Team, Double> calculateScore() {
@@ -67,37 +89,35 @@ public class ChessGame {
     }
 
     public boolean isEnd() {
-        return state.isEnd();
+        return this.state.isEnd();
     }
 
-    public void move(Position a, Position b) {
+    public void move(Position current, Position target) {
         state.move(() -> {
-            checkTeam(a);
-            board.movePiece(a, b);
+            checkTeam(current);
+            board.movePiece(current, target);
         });
-        Piece piece = board.getBoard().get(b);
-        pieceDao.updateByPiecePosition(a, new EmptyPiece(Team.NEUTRALITY, Type.NO_PIECE));
-        pieceDao.updateByPiecePosition(b, piece);
+
+        Piece piece = board.getBoard().get(target);
+        pieceDao.updateByPiecePosition(current, new EmptyPiece(Team.NEUTRALITY, Type.NO_PIECE));
+        pieceDao.updateByPiecePosition(target, piece);
     }
 
-    private void checkTeam(Position a) {
-        Team team = board.getTeam(a);
+    private void checkTeam(Position position) {
+        Team team = board.getTeam(position);
         if (state.getTurn() != team) {
             throw new IllegalArgumentException("현재 차례가 아닙니다.");
         }
     }
 
     public void changeTurn() {
-        if (state.getTurn() == Team.WHITE) {
-            this.state = new BlackTurn();
-            return;
-        }
-        if (state.getTurn() == Team.BLACK) {
-            this.state = new WhiteTurn();
-        }
+        this.state = state.changeState();
+        gameDao.updateByTurn(1, Team.of(this.state.getTurn().getValue()).getValue());
     }
 
     public void changeTurnEnd() {
         this.state = new End();
+        gameDao.deleteTable();
+        pieceDao.deleteTable();
     }
 }
