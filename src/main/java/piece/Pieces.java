787a2e1826aa904fc9f14882b14b74cb82b9bfd6
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-19 15:55:43 +0900

    feat(Pieces) : 기물 일급 컬렉션 생성 기능 구현
    - 이에 따른 테스트 코드 추가

---
+++ src/main/java/piece/Pieces.java
@@ -0,0 +1,14 @@
+package piece;
+
+import piece.Piece;
+
+import java.util.List;
+
+public class Pieces {
+
+    private final List<Piece> value;
+
+    public Pieces(final List<Piece> value) {
+        this.value = value;
+    }
+}
---
+++ src/test/java/PiecesTest.java
@@ -0,0 +1,32 @@
+import fixture.PieceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import piece.Color;
+import piece.Piece;
+import piece.Pieces;
+import piece.point.File;
+import piece.point.Point;
+import piece.point.Rank;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PiecesTest {
+
+    @Test
+    @DisplayName("기물 목록을 포함한 일급컬렉션을 생성한다.")
+    void create_with_PieceList() {
+        final var point1 = new Point(File.F, Rank.ONE);
+        final var color1 = Color.BLACK;
+
+        final var point2 = new Point(File.F, Rank.ONE);
+        final var color2 = Color.BLACK;
+        List<Piece> pieceList = List.of(new PieceImpl(point1, color1), new PieceImpl(point2, color2));
+
+        final var sut = new Pieces(pieceList);
+
+        assertThat(sut).isInstanceOf(Pieces.class);
+    }
+
+}
