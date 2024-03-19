7cd3dcb860eef23d64e16c64c6b5a7210ed640e0
seunghye218 <seunghye@student.42seoul.kr>
2024-03-19 16:46:12 +0900

    feat: 좌표 기능 구현
    Co-authored-by: jcoding-play <namejgc@naver.com>

---
+++ src/main/java/chess/domain/Point.java
@@ -0,0 +1,32 @@
+package chess.domain;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Point {
+
+    private final File file;
+    private final Rank rank;
+
+    public Point(String file, int rank) {
+        this.file = new File(file);
+        this.rank = new Rank(rank);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Objects.equals(file, point.file) && Objects.equals(rank, point.rank);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(file, rank);
+    }
+}
---
+++ src/test/java/chess/domain/PointTest.java
@@ -0,0 +1,18 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PointTest {
+
+    @Test
+    @DisplayName("행과 열의 정보를 알 수 있다.")
+    void createPoint() {
+        Point point = new Point("a", 1);
+
+        assertThat(point).isEqualTo(new Point("a", 1));
+    }
+}
