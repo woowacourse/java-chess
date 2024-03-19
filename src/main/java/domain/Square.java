cd8cd8ecaeb72893cee715f2e48c232add9ee3d2
MingyeomKim <mingyum119@naver.com>
2024-03-19 14:32:42 +0900

    feat(Square): 체스판의 한 칸을 생성하고 동등성을 비교하는 기능 구현

---
+++ src/main/java/domain/Square.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.Objects;
+
+public class Square {
+    private final char piece;
+    private final Position position;
+
+    public Square(final char piece, final Position position) {
+        this.piece = piece;
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Square square = (Square) o;
+        return Objects.equals(position, square.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
---
+++ src/test/java/domain/SquareTest.java
@@ -0,0 +1,14 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SquareTest {
+    @DisplayName("같은 위치에 있는 Square 객체는 동일한 Square 객체이다.")
+    @Test
+    void generateSquare() {
+        Square square = new Square('R', new Position(0, 0));
+        Assertions.assertThat(square).isEqualTo(new Square('N', new Position(0, 0)));
+    }
+}
