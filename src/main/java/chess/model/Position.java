eec0aa93ada379aefba9c9af271090294cd2efed
eun-byeol <joeunbyeol98@gmail.com>
2024-03-19 21:53:23 +0900

    refactor: Position 클래스 필드 변수명 수정
    Co-authored-by: masonkimseoul <masonkimseoul@gmail.com>

--- src/main/java/chess/model/Position.java
+++ src/main/java/chess/model/Position.java
@@ -1,21 +1,40 @@
 package chess.model;
 
+import java.util.Objects;
+
 public class Position {
 
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 8;
-    private final int x;
-    private final int y;
+    private final int row;
+    private final int column;
 
-    public Position(int x, int y) {
-        validateRange(x, y);
-        this.x = x;
-        this.y = y;
+    public Position(int row, int column) {
+        validateRange(row, column);
+        this.row = row;
+        this.column = column;
     }
 
-    private void validateRange(int x, int y) {
-        if (x < MIN_VALUE || x > MAX_VALUE || y < MIN_VALUE || y > MAX_VALUE) {
+    private void validateRange(int row, int column) {
+        if (row < MIN_VALUE || row > MAX_VALUE || column < MIN_VALUE || column > MAX_VALUE) {
             throw new IllegalArgumentException("좌표 값은 1이상 8이하의 값만 가능합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return row == position.row && column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
     }
 }
