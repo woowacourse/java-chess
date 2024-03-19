ad60b624049796599239d5b6ef2e2a40cb4cc477
BurningFalls <sherlocky31415@gmail.com>
2024-03-19 20:28:01 +0900

    feat: 룩 움직임 가능 여부 판별 기능 구현

--- src/main/java/chess/domain/PositionDifference.java
+++ src/main/java/chess/domain/PositionDifference.java
@@ -4,4 +4,8 @@ public record PositionDifference(int xDifference, int yDifference) {
     public boolean isMagnitudeEqual() {
         return Math.abs(xDifference) == Math.abs(yDifference);
     }
+
+    public boolean isOnAxis() {
+        return xDifference == 0 || yDifference == 0;
+    }
 }
