328c60c9e756c309c2f8e2e7499eed3367ed75ac
takoyakimchi <c4nyou@gmail.com>
2024-03-19 17:39:55 +0900

    feat: Bishop 이동 가능한지 여부 반환하는 기능 구현
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/domain/piece/Bishop.java
+++ src/main/java/domain/piece/Bishop.java
@@ -1,8 +1,20 @@
 package domain.piece;
 
+import domain.Position;
+
 public class Bishop extends Piece {
 
     public Bishop(Color color) {
         super(color);
+    }
+
+    @Override
+    public boolean canMove(Position sourcePosition, Position targetPosition) {
+        if (sourcePosition.equals(targetPosition)) {
+            return false;
+        }
+        int rankDifference = sourcePosition.calculateRankDifference(targetPosition);
+        int fileDifference = sourcePosition.calculateFileDifference(targetPosition);
+        return Math.abs(rankDifference) == Math.abs(fileDifference);
     }
 }
---
+++ src/test/java/domain/piece/BishopTest.java
@@ -0,0 +1,67 @@
+package domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.File;
+import domain.Position;
+import domain.Rank;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BishopTest {
+
+    @Test
+    @DisplayName("오른쪽 위 대각선 방향으로 비숍 이동 가능")
+    void canMove_RightUp() {
+        Bishop bishop = new Bishop(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(3));
+        Position position2 = new Position(new File(6), new Rank(8));
+        assertThat(bishop.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("오른쪽 아래 대각선 방향으로 비숍 이동 가능")
+    void canMove_RightDown() {
+        Bishop bishop = new Bishop(Color.WHITE);
+        Position position1 = new Position(new File(4), new Rank(6));
+        Position position2 = new Position(new File(2), new Rank(8));
+        assertThat(bishop.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("왼쪽 위 대각선 방향으로 비숍 이동 가능")
+    void canMove_LeftUp() {
+        Bishop bishop = new Bishop(Color.WHITE);
+        Position position1 = new Position(new File(3), new Rank(5));
+        Position position2 = new Position(new File(1), new Rank(7));
+        assertThat(bishop.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("왼쪽 아래 대각선 방향으로 비숍 이동 가능")
+    void canMove_LeftDown() {
+        Bishop bishop = new Bishop(Color.WHITE);
+        Position position1 = new Position(new File(3), new Rank(5));
+        Position position2 = new Position(new File(1), new Rank(3));
+        assertThat(bishop.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("같은 대각선상이 아니면 비숍 이동 불가")
+    void cannotMove() {
+        Bishop bishop = new Bishop(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(7), new Rank(8));
+        Assertions.assertThat(bishop.canMove(position1, position2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("같은 위치로는 이동 불가")
+    void cannotMove_SamePosition() {
+        Bishop bishop = new Bishop(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(1), new Rank(1));
+        Assertions.assertThat(bishop.canMove(position1, position2)).isFalse();
+    }
+}
