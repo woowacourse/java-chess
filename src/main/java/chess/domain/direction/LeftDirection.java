222addfdd357651a718dda2b3e12938232718497
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-19 17:24:44 +0900

    feat: 좌 방향은 column 값을 최소일 경우 반환하지 않는 기능 구현
    Co-authored-by: fromitive <fromitive@gmail.com>

--- src/main/java/chess/domain/direction/LeftDirection.java
+++ src/main/java/chess/domain/direction/LeftDirection.java
@@ -4,9 +4,12 @@ import chess.domain.Position;
 import java.util.List;
 
 public class LeftDirection implements Direction {
-    
+
     @Override
     public List<Position> move(final Position from) {
-        return List.of(new Position(from.row() - 1, from.column()));
+        if (!from.isMinimumColumn()) {
+            return List.of(new Position(from.row(), from.column() - 1));
+        }
+        return List.of();
     }
 }
--- src/test/java/chess/domain/direction/LeftDirectionTest.java
+++ src/test/java/chess/domain/direction/LeftDirectionTest.java
@@ -1,7 +1,6 @@
 package chess.domain.direction;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import chess.domain.Position;
 import java.util.List;
@@ -11,11 +10,20 @@ import org.junit.jupiter.api.Test;
 class LeftDirectionTest {
 
     @Test
-    @DisplayName("왼쪽으로 이동한다.")
+    @DisplayName("좌 방향은 column을 1 감소한다.")
     void move() {
         Direction direction = new LeftDirection();
-        Position from = new Position(2, 1);
+        Position from = new Position(3, 8);
 
-        assertThat(direction.move(from)).isEqualTo(List.of(new Position(1, 1)));
+        assertThat(direction.move(from)).isEqualTo(List.of(new Position(3, 7)));
+    }
+
+    @Test
+    @DisplayName("column이 최소인 경우 이동할 수 없다.")
+    void canNotMove() {
+        Direction direction = new LeftDirection();
+        Position from = new Position(3, 1);
+
+        assertThat(direction.move(from)).isEmpty();
     }
 }
