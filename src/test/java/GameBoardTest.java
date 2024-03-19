c913ad2abfed1ac4a7b5cddfaf31e4f72bc6a61d
reddevilmidzy <midzy0228@gmail.com>
2024-03-19 21:13:52 +0900

    refactor: 테스트 코드 메서드 명 및 디스플레이 네임 수정

--- src/test/java/GameBoardTest.java
+++ src/test/java/GameBoardTest.java
@@ -17,20 +17,21 @@ class GameBoardTest {
     void rowLengthIs8() {
         GameBoard gameBoard = new GameBoard();
 
-        Assertions.assertThat(gameBoard.getBoard().size()).isEqualTo(8);
+        Assertions.assertThat(gameBoard.getBoard()).hasSize(8);
     }
 
     @DisplayName("체스판의 가로 길이는 8이다.")
     @Test
     void columnLengthIs8() {
         GameBoard gameBoard = new GameBoard();
+        List<List<Piece>> board = gameBoard.getBoard();
 
-        Assertions.assertThat(gameBoard.getBoard()).allMatch(line -> line.size() == 8);
+        Assertions.assertThat(board).allMatch(line -> line.size() == 8);
     }
 
     @Test
-    @DisplayName("Test name")
-    void methodName() {
+    @DisplayName("기물들의 시작 위치를 확인한다.")
+    void checkInitialPosition() {
         //given
         GameBoard gameBoard = new GameBoard();
 
@@ -38,14 +39,29 @@ class GameBoardTest {
         gameBoard.setting();
 
         List<List<Piece>> board = gameBoard.getBoard();
+        StringBuilder stringBuilder = new StringBuilder();
+
         for (List<Piece> pieces : board) {
-            System.out.println(pieces.toString());
+            stringBuilder.append(pieces.toString());
+            stringBuilder.append(System.lineSeparator());
         }
+
+        String expected = String.format("[R, N, B, Q, K, B, N, R]%n"
+                + "[P, P, P, P, P, P, P, P]%n"
+                + "[., ., ., ., ., ., ., .]%n"
+                + "[., ., ., ., ., ., ., .]%n"
+                + "[., ., ., ., ., ., ., .]%n"
+                + "[., ., ., ., ., ., ., .]%n"
+                + "[p, p, p, p, p, p, p, p]%n"
+                + "[r, n, b, q, k, b, n, r]%n");
+
         //then
+        Assertions.assertThat(stringBuilder.toString()).hasToString(expected);
     }
 
     @Test
-    void name() {
+    @DisplayName("위치가 주어졌을 때 해당 위치의 기물을 반환한다.")
+    void findByPosition() {
         GameBoard gameBoard = new GameBoard();
         gameBoard.setting();
 
