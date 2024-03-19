67986ba0a559894dfe1b981607274a33144fdaec
libienz <leekh10195@gmail.com>
2024-03-19 15:16:27 +0900

    feat: 체스판 행 번호 객체 구현

---
+++ src/main/java/chess/position/Row.java
@@ -0,0 +1,19 @@
+package chess.position;
+
+public class Row {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 8;
+
+    private final int rowNumber;
+
+    public Row(int rowNumber) {
+        validateNumberRange(rowNumber);
+        this.rowNumber = rowNumber;
+    }
+
+    private void validateNumberRange(int rowNumber) {
+        if (MIN_NUMBER > rowNumber || rowNumber > MAX_NUMBER) {
+            throw new IllegalStateException("체스판의 행 번호는 " + rowNumber + "가 될 수 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/chess/position/RowTest.java
@@ -0,0 +1,27 @@
+package chess.position;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RowTest {
+    @DisplayName("실패 : 지정된 범위가 아닌 행 번호가 주어지면 생성에 실패한다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 9})
+    void should_ThrowIllegaStateException_When_GiveOutRangedRowNumber(int outRangedRowNumber) {
+        assertThatThrownBy(() -> new Row(outRangedRowNumber))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("체스판의 행 번호는 " + outRangedRowNumber + "가 될 수 없습니다.");
+    }
+
+    @DisplayName("성공 : 지정된 범위의 행 번호가 주어지면 생성에 성공한다")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 8})
+    void should_MakeRowObject_When_GiveValidRangeRowNumber(int validRangedRowNumber) {
+        assertThatCode(() -> new Row(validRangedRowNumber))
+                .doesNotThrowAnyException();
+    }
+}
