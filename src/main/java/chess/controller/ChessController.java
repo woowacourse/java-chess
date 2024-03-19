cfba0fa32f2626e4bb5984eebdc420a4379f54ae
seunghye218 <seunghye@student.42seoul.kr>
2024-03-19 20:48:13 +0900

    feat: 프로그램을 실행하는 기능 구현
    Co-authored-by: jcoding-play <namejgc@naver.com>

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,5 +1,27 @@
 package chess.controller;
 
+import chess.domain.BoardFactory;
+import chess.domain.ChessGame;
+import chess.domain.Command;
+import chess.view.InputView;
+import chess.view.OutputView;
+
 public class ChessController {
 
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ChessController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        ChessGame game = new ChessGame(BoardFactory.create());
+        outputView.printGameStart();
+
+        while (Command.END != Command.from(inputView.readCommand())) {
+            outputView.printBoard(game.getBoard());
+        }
+    }
 }
--- src/main/java/chess/domain/Command.java
+++ src/main/java/chess/domain/Command.java
@@ -1,7 +1,6 @@
 package chess.domain;
 
 import java.util.Arrays;
-import java.util.Map;
 
 public enum Command {
 
