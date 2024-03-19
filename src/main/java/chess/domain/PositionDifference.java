2252eb2aa0732dacd4c9065353c90a5d0f6f94c3
BurningFalls <sherlocky31415@gmail.com>
2024-03-19 20:20:11 +0900

    feat: 비숍 움직임 가능 여부 판별 기능 구현

--- src/main/java/chess/domain/PositionDifference.java
+++ src/main/java/chess/domain/PositionDifference.java
@@ -1,5 +1,7 @@
 package chess.domain;
 
 public record PositionDifference(int xDifference, int yDifference) {
-
+    public boolean isMagnitudeEqual() {
+        return Math.abs(xDifference) == Math.abs(yDifference);
+    }
 }
