2be57b05a9296ca10720b0669067edea4f80f041
ikjo39 <wer04129@gmail.com>
2024-03-19 15:18:03 +0900

    feat: 체스 위치 추가

---
+++ src/main/java/chess/model/ChessPosition.java
@@ -0,0 +1,11 @@
+package chess.model;
+
+public class ChessPosition {
+    private final File file;
+    private final Rank rank;
+
+    public ChessPosition(final File file, final Rank rank) {
+        this.file = file;
+        this.rank = rank;
+    }
+}
---
+++ src/main/java/chess/model/File.java
@@ -0,0 +1,18 @@
+package chess.model;
+
+public enum File {
+    A(1),
+    B(2),
+    C(3),
+    D(4),
+    E(5),
+    F(6),
+    G(7),
+    H(8);
+
+    private final int coordinate;
+
+    File(int coordinate) {
+        this.coordinate = coordinate;
+    }
+}
