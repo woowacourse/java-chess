8c289cd6c93cd5afd4883d5dacd5b83de7758acb
takoyakimchi <c4nyou@gmail.com>
2024-03-19 14:03:36 +0900

    feat: File, Rank 클래스 생성
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

---
+++ src/main/java/domain/File.java
@@ -0,0 +1,39 @@
+package domain;
+
+import java.util.Objects;
+
+public final class File {
+
+    private final int value;
+
+    public File(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (File) obj;
+        return this.value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "File[" +
+            "value=" + value + ']';
+    }
+}
---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,39 @@
+package domain;
+
+import java.util.Objects;
+
+public final class Rank {
+
+    private final int value;
+
+    public Rank(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Rank) obj;
+        return this.value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Rank[" +
+            "value=" + value + ']';
+    }
+}
