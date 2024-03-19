888aeae88955c014b2c4fbf8b721dd3edf22ac9c
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-19 15:57:53 +0900

    feat: 가로, 세로 좌표값을 가지는 위치 객체 생성
    Co-authored-by: fromitive <fromitive@gmail.com>

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -1,4 +1,17 @@
 package chess.domain;
 
 public record Position(int row, int column) {
+    private static final int MIN_POSITION_RANGE = 1;
+    private static final int MAX_POSITION_RANGE = 8;
+
+    public Position {
+        validateRange(row);
+        validateRange(column);
+    }
+
+    private void validateRange(int position) {
+        if(position < MIN_POSITION_RANGE || position > MAX_POSITION_RANGE) {
+            throw new IllegalArgumentException("위치의 가로, 세로 범위는 각각 1 ~ 8이여야 합니다.");
+        }
+    }
 }
--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -1,9 +1,12 @@
 package chess.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PositionTest {
 
@@ -12,5 +15,14 @@ class PositionTest {
     void createPosition() {
         assertThatCode(() -> new Position(1, 1))
                 .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("위치는 가로, 세로 범위는 각각 1 ~ 8 이다.")
+    @CsvSource({"0,1", "1,0", "1, 9", "9, 1"})
+    void createPositionThrowException(int row, int column) {
+        assertThatThrownBy(() -> new Position(row, column))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("위치의 가로, 세로 범위는 각각 1 ~ 8이여야 합니다.");
     }
 }
