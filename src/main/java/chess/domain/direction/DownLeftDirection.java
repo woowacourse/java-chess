3d2036d30f47a0fbeef1e49a744309ed1d5bfa0f
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-19 22:29:24 +0900

    feat: 대각선 하좌 방향에 대한 기능 구현
    Co-authored-by: fromitive <fromitive@gmail.com>

---
+++ src/main/java/chess/domain/direction/DownLeftDirection.java
@@ -0,0 +1,17 @@
+package chess.domain.direction;
+
+import chess.domain.Position;
+
+public class DownLeftDirection extends Direction {
+    public DownLeftDirection(final int moveCount) {
+        super(moveCount);
+    }
+
+    @Override
+    Position next(final Position from) {
+        if (from.isMinimumRow() || from.isMinimumColumn()) {
+            return from;
+        }
+        return new Position(from.row() - 1, from.column() - 1);
+    }
+}
