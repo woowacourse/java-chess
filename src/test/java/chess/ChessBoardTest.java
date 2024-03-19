ee387d4d4663cfbb1f2316db72f2fcc31c679311
jinwoo22 <jenny010925@naver.com>
2024-03-19 17:06:05 +0900

    test: 체스 생성 테스트 추가

--- src/test/java/chess/ChessBoardTest.java
+++ src/test/java/chess/ChessBoardTest.java
@@ -1,6 +1,7 @@
 package chess;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import chess.domain.ChessBoard;
 import chess.domain.Piece;
@@ -14,16 +15,88 @@ class ChessBoardTest {
     void create() {
         final ChessBoard chessBoard = ChessBoard.init();
         final List<List<PieceWithColor>> chessBoardDetail = chessBoard.getBoard();
-        assertThat(chessBoardDetail.get(7)).containsExactly(
-                new PieceWithColor(Piece.ROOK, PieceColor.BLACK),
-                new PieceWithColor(Piece.NIGHT, PieceColor.BLACK),
-                new PieceWithColor(Piece.BISHOP, PieceColor.BLACK),
-                new PieceWithColor(Piece.QUEEN, PieceColor.BLACK),
-                new PieceWithColor(Piece.KING, PieceColor.BLACK),
-                new PieceWithColor(Piece.BISHOP, PieceColor.BLACK),
-                new PieceWithColor(Piece.NIGHT, PieceColor.BLACK),
-                new PieceWithColor(Piece.ROOK, PieceColor.BLACK)
-        );
 
+        assertAll(
+                () -> assertThat(chessBoardDetail.get(0)).containsExactly(
+                        new PieceWithColor(Piece.ROOK, PieceColor.WHITE),
+                        new PieceWithColor(Piece.NIGHT, PieceColor.WHITE),
+                        new PieceWithColor(Piece.BISHOP, PieceColor.WHITE),
+                        new PieceWithColor(Piece.QUEEN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.KING, PieceColor.WHITE),
+                        new PieceWithColor(Piece.BISHOP, PieceColor.WHITE),
+                        new PieceWithColor(Piece.NIGHT, PieceColor.WHITE),
+                        new PieceWithColor(Piece.ROOK, PieceColor.WHITE)
+                ),
+                () -> assertThat(chessBoardDetail.get(1)).containsExactly(
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE),
+                        new PieceWithColor(Piece.PAWN, PieceColor.WHITE)
+                ),
+                () -> assertThat(chessBoardDetail.get(2)).containsExactly(
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY)
+                ),
+                () -> assertThat(chessBoardDetail.get(3)).containsExactly(
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY)
+                ),
+                () -> assertThat(chessBoardDetail.get(4)).containsExactly(
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY)
+                ),
+                () -> assertThat(chessBoardDetail.get(5)).containsExactly(
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY),
+                        new PieceWithColor(Piece.EMPTY, PieceColor.EMPTY)
+                ),
+                () -> assertThat(chessBoardDetail.get(6)).containsExactly(
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.PAWN, PieceColor.BLACK)
+                ),
+                () -> assertThat(chessBoardDetail.get(7)).containsExactly(
+                        new PieceWithColor(Piece.ROOK, PieceColor.BLACK),
+                        new PieceWithColor(Piece.NIGHT, PieceColor.BLACK),
+                        new PieceWithColor(Piece.BISHOP, PieceColor.BLACK),
+                        new PieceWithColor(Piece.QUEEN, PieceColor.BLACK),
+                        new PieceWithColor(Piece.KING, PieceColor.BLACK),
+                        new PieceWithColor(Piece.BISHOP, PieceColor.BLACK),
+                        new PieceWithColor(Piece.NIGHT, PieceColor.BLACK),
+                        new PieceWithColor(Piece.ROOK, PieceColor.BLACK)
+                )
+        );
     }
 }
