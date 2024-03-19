ef577ace5bff65e698036b5f2b99cbfad24f4fa3
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-19 19:22:37 +0900

    feat(Numbering): 도메인 구현
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

---
+++ src/main/java/chess/Numbering.java
@@ -0,0 +1,23 @@
+package chess;
+
+public enum Numbering {
+
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8);
+
+    private final int number;
+
+    Numbering(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
