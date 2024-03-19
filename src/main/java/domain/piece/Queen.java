8f07f1830e58d9d1664a50c4dd4bf7947d1a4334
takoyakimchi <c4nyou@gmail.com>
2024-03-19 17:39:29 +0900

    feat: Queen 이동 가능한지 여부 반환하는 기능 구현
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/domain/piece/Queen.java
+++ src/main/java/domain/piece/Queen.java
@@ -1,8 +1,22 @@
 package domain.piece;
 
+import domain.Position;
+
 public class Queen extends Piece {
 
     public Queen(Color color) {
         super(color);
+    }
+
+    @Override
+    public boolean canMove(Position sourcePosition, Position targetPosition) {
+        if (sourcePosition.equals(targetPosition)) {
+            return false;
+        }
+        int rankDifference = sourcePosition.calculateRankDifference(targetPosition);
+        int fileDifference = sourcePosition.calculateFileDifference(targetPosition);
+        return Math.abs(rankDifference) == Math.abs(fileDifference)
+            || rankDifference == 0
+            || fileDifference == 0;
     }
 }
---
+++ src/test/java/domain/piece/QueenTest.java
@@ -0,0 +1,85 @@
+package domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.File;
+import domain.Position;
+import domain.Rank;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class QueenTest {
+
+    @Test
+    @DisplayName("오른쪽 위 대각선 방향으로 퀸 이동 가능")
+    void canMove_RightUp() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(3));
+        Position position2 = new Position(new File(6), new Rank(8));
+        assertThat(queen.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("오른쪽 아래 대각선 방향으로 퀸 이동 가능")
+    void canMove_RightDown() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(4), new Rank(6));
+        Position position2 = new Position(new File(2), new Rank(8));
+        assertThat(queen.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("왼쪽 위 대각선 방향으로 퀸 이동 가능")
+    void canMove_LeftUp() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(3), new Rank(5));
+        Position position2 = new Position(new File(1), new Rank(7));
+        assertThat(queen.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("왼쪽 아래 대각선 방향으로 퀸 이동 가능")
+    void canMove_LeftDown() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(3), new Rank(5));
+        Position position2 = new Position(new File(1), new Rank(3));
+        assertThat(queen.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("같은 대각선상이 아니면 퀸 이동 불가")
+    void cannotMove() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(7), new Rank(8));
+        Assertions.assertThat(queen.canMove(position1, position2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("Rank가 같으면 퀸 이동 가능")
+    void canMove_SameRank() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(8), new Rank(1));
+        Assertions.assertThat(queen.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("File이 같으면 퀸 이동 가능")
+    void canMove_SameFile() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(1), new Rank(8));
+        Assertions.assertThat(queen.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("같은 위치로는 이동 불가")
+    void cannotMove_SamePosition() {
+        Queen queen = new Queen(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(1), new Rank(1));
+        Assertions.assertThat(queen.canMove(position1, position2)).isFalse();
+    }
+}
