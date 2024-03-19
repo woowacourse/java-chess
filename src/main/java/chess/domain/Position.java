f5dae7bcfe506808581a6ef5bf4cceafd04a8011
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-19 17:22:06 +0900

    feat: 위치가 경계값인지 확인하는 기능 구현
    Co-authored-by: fromitive <fromitive@gmail.com>

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -10,8 +10,24 @@ public record Position(int row, int column) {
     }
 
     private void validateRange(int position) {
-        if(position < MIN_POSITION_RANGE || position > MAX_POSITION_RANGE) {
+        if (position < MIN_POSITION_RANGE || position > MAX_POSITION_RANGE) {
             throw new IllegalArgumentException("위치의 가로, 세로 범위는 각각 1 ~ 8이여야 합니다.");
         }
+    }
+
+    public boolean isMinimumColumn() {
+        return column == MIN_POSITION_RANGE;
+    }
+
+    public boolean isMaximumColumn() {
+        return column == MAX_POSITION_RANGE;
+    }
+
+    public boolean isMinimumRow() {
+        return row == MIN_POSITION_RANGE;
+    }
+
+    public boolean isMaximumRow() {
+        return row == MAX_POSITION_RANGE;
     }
 }
--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -1,7 +1,9 @@
 package chess.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,5 +26,48 @@ class PositionTest {
         assertThatThrownBy(() -> new Position(row, column))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("위치의 가로, 세로 범위는 각각 1 ~ 8이여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("위치의 column이 최소인 경우 참을 반환한다.")
+    void isMinimumColumn() {
+        Position position = new Position(3, 1);
+        assertThat(position.isMinimumColumn()).isTrue();
+    }
+    
+
+    @Test
+    @DisplayName("위치의 column이 최대인 경우 참을 반환한다.")
+    void isMaximumColumn() {
+        Position position = new Position(3, 8);
+        assertThat(position.isMaximumColumn()).isTrue();
+    }
+
+    @Test
+    @DisplayName("위치의 row가 최소인 경우 참을 반환한다.")
+    void isMinimumRow() {
+        Position position = new Position(1, 3);
+        assertThat(position.isMinimumRow()).isTrue();
+    }
+
+
+    @Test
+    @DisplayName("위치의 row가 최대인 경우 참을 반환한다.")
+    void isMaximumRow() {
+        Position position = new Position(8, 3);
+        assertThat(position.isMaximumRow()).isTrue();
+    }
+
+
+    @Test
+    @DisplayName("최대 최소가 아닌 값이 나올 경우 거짓을 반환한다.")
+    void isNotBoundaryValue() {
+        Position position = new Position(3, 3);
+        assertAll(
+                () -> assertThat(position.isMinimumColumn()).isFalse(),
+                () -> assertThat(position.isMaximumColumn()).isFalse(),
+                () -> assertThat(position.isMinimumRow()).isFalse(),
+                () -> assertThat(position.isMaximumRow()).isFalse()
+        );
     }
 }
