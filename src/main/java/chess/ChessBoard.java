3101025707c9ce67f49c26740933301dd0792afe
nayonsoso <panda0329@naver.com>
2024-03-19 20:39:32 +0900

    refactor: 초기화 순서 변경
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/ChessBoard.java
+++ src/main/java/chess/ChessBoard.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ChessBoard {
@@ -8,26 +9,6 @@ public class ChessBoard {
     private final Map<Square, Piece> chessBoard = new LinkedHashMap<>();
 
     public ChessBoard() {
-        // 백팀 말 배치
-        chessBoard.put(Square.of('a', 1), new Piece(PieceType.ROOK, Team.WHITE));
-        chessBoard.put(Square.of('b', 1), new Piece(PieceType.KNIGHT, Team.WHITE));
-        chessBoard.put(Square.of('c', 1), new Piece(PieceType.BISHOP, Team.WHITE));
-        chessBoard.put(Square.of('d', 1), new Piece(PieceType.QUEEN, Team.WHITE));
-        chessBoard.put(Square.of('e', 1), new Piece(PieceType.KING, Team.WHITE));
-        chessBoard.put(Square.of('f', 1), new Piece(PieceType.BISHOP, Team.WHITE));
-        chessBoard.put(Square.of('g', 1), new Piece(PieceType.KNIGHT, Team.WHITE));
-        chessBoard.put(Square.of('h', 1), new Piece(PieceType.ROOK, Team.WHITE));
-        for (char file = 'a'; file <= 'h'; file++) {
-            chessBoard.put(Square.of(file, 2), new Piece(PieceType.PAWN, Team.WHITE));
-        }
-
-        // 비어있는 칸들 초기화 (3~6 랭크)
-        for (int rank = 3; rank <= 6; rank++) {
-            for (char file = 'a'; file <= 'h'; file++) {
-                chessBoard.put(Square.of(file, rank), null);
-            }
-        }
-
         // 흑팀 말 배치
         chessBoard.put(Square.of('a', 8), new Piece(PieceType.ROOK, Team.BLACK));
         chessBoard.put(Square.of('b', 8), new Piece(PieceType.KNIGHT, Team.BLACK));
@@ -40,5 +21,31 @@ public class ChessBoard {
         for (char file = 'a'; file <= 'h'; file++) {
             chessBoard.put(Square.of(file, 7), new Piece(PieceType.PAWN, Team.BLACK));
         }
+
+        // 비어있는 칸들 초기화 (3~6 랭크)
+        for (int rank = 3; rank <= 6; rank++) {
+            for (char file = 'a'; file <= 'h'; file++) {
+                chessBoard.put(Square.of(file, rank), null);
+            }
+        }
+
+        // 백팀 말 배치
+        for (char file = 'a'; file <= 'h'; file++) {
+            chessBoard.put(Square.of(file, 2), new Piece(PieceType.PAWN, Team.WHITE));
+        }
+        chessBoard.put(Square.of('a', 1), new Piece(PieceType.ROOK, Team.WHITE));
+        chessBoard.put(Square.of('b', 1), new Piece(PieceType.KNIGHT, Team.WHITE));
+        chessBoard.put(Square.of('c', 1), new Piece(PieceType.BISHOP, Team.WHITE));
+        chessBoard.put(Square.of('d', 1), new Piece(PieceType.QUEEN, Team.WHITE));
+        chessBoard.put(Square.of('e', 1), new Piece(PieceType.KING, Team.WHITE));
+        chessBoard.put(Square.of('f', 1), new Piece(PieceType.BISHOP, Team.WHITE));
+        chessBoard.put(Square.of('g', 1), new Piece(PieceType.KNIGHT, Team.WHITE));
+        chessBoard.put(Square.of('h', 1), new Piece(PieceType.ROOK, Team.WHITE));
+    }
+
+    public List<Piece> findAllPieces() {
+        return chessBoard.values()
+                .stream()
+                .toList();
     }
 }
