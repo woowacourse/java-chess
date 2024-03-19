d61ad9e9ebbbc4ddca5de63e884d95c07e495b13
nayonsoso <panda0329@naver.com>
2024-03-19 20:38:11 +0900

    feat: 출력 기능 구현
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/main/java/chess/controller/ChessController.java
@@ -0,0 +1,27 @@
+package chess.controller;
+
+import chess.ChessBoard;
+import chess.view.GameStartCommand;
+import chess.view.InputView;
+import chess.view.OutputView;
+
+// TODO: 이름 고민 - er??
+public class ChessController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ChessController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        final ChessBoard chessBoard = new ChessBoard();
+
+        GameStartCommand gameStartCommand = inputView.readGameStartCommand();
+        if (GameStartCommand.START.equals(gameStartCommand)) {
+            outputView.printChessBoard(chessBoard);
+        }
+    }
+}
