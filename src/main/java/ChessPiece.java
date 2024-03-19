bf3f3e938a6884d4d7b02da7f8ab48bf7c08ee54
tackyu <tack970913@gmail.com>
2024-03-19 16:09:07 +0900

    feat(ChessPiece) : 체스 말 생성

---
+++ src/main/java/ChessPiece.java
@@ -0,0 +1,20 @@
+public enum ChessPiece {
+    BLACK_KING("K"),
+    WHITE_KING("k"),
+    BLACK_QUEEN("Q"),
+    WHITE_QUEEN("q"),
+    BLACK_ROOK("R"),
+    WHITE_ROOK("r"),
+    BLACK_BISHOP("B"),
+    WHITE_BISHOP("b"),
+    BLACK_KNIGHT("N"),
+    WHITE_KNIGHT("n"),
+    BLACK_PAWN("P"),
+    WHITE_PAWN("p");
+
+    private final String name;
+
+    ChessPiece(String name) {
+        this.name = name;
+    }
+}
