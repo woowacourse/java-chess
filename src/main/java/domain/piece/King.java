ed6b9044794da9e4235102d682042d87e8840019
takoyakimchi <c4nyou@gmail.com>
2024-03-19 17:39:47 +0900

    feat: King 이동 가능한지 여부 반환하는 기능 구현
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/domain/piece/King.java
+++ src/main/java/domain/piece/King.java
@@ -1,8 +1,21 @@
 package domain.piece;
 
+import domain.Position;
+
 public class King extends Piece {
 
     public King(Color color) {
         super(color);
+    }
+
+    @Override
+    public boolean canMove(Position sourcePosition, Position targetPosition) {
+        if (sourcePosition.equals(targetPosition)) {
+            return false;
+        }
+        int rankDifference = sourcePosition.calculateRankDifference(targetPosition);
+        int fileDifference = sourcePosition.calculateFileDifference(targetPosition);
+        return rankDifference == 1 || rankDifference == -1
+            || fileDifference == 1 || fileDifference == -1;
     }
 }
---
+++ src/test/java/domain/piece/KingTest.java
@@ -0,0 +1,85 @@
+package domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.File;
+import domain.Position;
+import domain.Rank;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KingTest {
+
+    @Test
+    @DisplayName("오른쪽 위 대각선 방향으로 킹 이동 가능")
+    void canMove_RightUp() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(3));
+        Position position2 = new Position(new File(2), new Rank(4));
+        assertThat(king.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("오른쪽 아래 대각선 방향으로 킹 이동 가능")
+    void canMove_RightDown() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(4), new Rank(6));
+        Position position2 = new Position(new File(3), new Rank(7));
+        assertThat(king.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("왼쪽 위 대각선 방향으로 킹 이동 가능")
+    void canMove_LeftUp() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(3), new Rank(5));
+        Position position2 = new Position(new File(2), new Rank(6));
+        assertThat(king.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("왼쪽 아래 대각선 방향으로 킹 이동 가능")
+    void canMove_LeftDown() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(3), new Rank(5));
+        Position position2 = new Position(new File(2), new Rank(4));
+        assertThat(king.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("1칸을 초과하면 킹 이동 불가")
+    void cannotMove() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(1), new Rank(3));
+        Assertions.assertThat(king.canMove(position1, position2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("Rank가 같고 File 차이가 한 칸이면 킹 이동 가능")
+    void canMove_SameRank() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(2), new Rank(1));
+        Assertions.assertThat(king.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("File이 같고 Rank 차이가 한 칸이면 킹 이동 가능")
+    void canMove_SameFile() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(1), new Rank(2));
+        Assertions.assertThat(king.canMove(position1, position2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("같은 위치로는 이동 불가")
+    void cannotMove_SamePosition() {
+        King king = new King(Color.WHITE);
+        Position position1 = new Position(new File(1), new Rank(1));
+        Position position2 = new Position(new File(1), new Rank(1));
+        Assertions.assertThat(king.canMove(position1, position2)).isFalse();
+    }
+}
