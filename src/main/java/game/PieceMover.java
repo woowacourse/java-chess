619da5289c6a927dc2adaba2c7f6e482962a5e83
leetaehoon <xogns1514@gmail.com>
2024-03-19 21:22:43 +0900

    feat(PieceMover): piece가 source에 위치한 piece를 target으로 이동하는 기능 구현

---
+++ src/main/java/game/PieceMover.java
@@ -0,0 +1,37 @@
+package game;
+
+import domain.chessboard.Square;
+import domain.piece.Piece;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PieceMover {
+
+    private final Map<Square, Piece> pieceBySquare;
+
+    public PieceMover() {
+        this.pieceBySquare = new HashMap<>();
+    }
+
+    public void add(Square square, Piece piece) {
+        pieceBySquare.put(square, piece);
+    }
+
+    public void move(Square sourceSquare, Square targetSquare) {
+        if (!pieceBySquare.containsKey(sourceSquare)) {
+            throw new IllegalStateException("해당 위치에 Piece가 존재하지 않습니다.");
+        }
+
+        Piece findPiece = pieceBySquare.get(sourceSquare);
+        if (pieceBySquare.containsKey(targetSquare)) {
+            pieceBySquare.remove(targetSquare);
+        }
+
+        pieceBySquare.put(targetSquare, findPiece);
+        pieceBySquare.remove(sourceSquare);
+    }
+
+    public Piece findPieceBySquare(Square targetSquare) {
+        return pieceBySquare.get(targetSquare);
+    }
+}
---
+++ src/test/java/game/PieceMoverTest.java
@@ -0,0 +1,35 @@
+package game;
+
+import domain.chessboard.Square;
+import domain.piece.Color;
+import domain.piece.Piece;
+import domain.piece.PieceRole;
+import domain.piece.PieceType;
+import domain.piece.Position;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PieceMoverTest {
+
+    @DisplayName("source에 위치한 piece를 target으로 이동한다.")
+    @Test
+    void movePieceToTarget() {
+        Position sourcePosition = new Position(1, 1);
+        Square sourceSquare = new Square(sourcePosition);
+
+        Position targetPosition = new Position(2, 2);
+        Square targetSquare = new Square(targetPosition);
+
+        PieceMover pieceMover = new PieceMover();
+
+        Piece piece = new Piece(new PieceType(PieceRole.KING, Color.BLACK), sourcePosition);
+        pieceMover.add(sourceSquare, piece);
+
+        pieceMover.move(sourceSquare, targetSquare);
+
+        Piece findPiece = pieceMover.findPieceBySquare(targetSquare);
+        Assertions.assertThat(findPiece).isEqualTo(piece);
+    }
+
+}
