f5af6aaf543b87edbef0ee83930d37e42c589ef2
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-19 22:05:35 +0900

    feat: 하 방향에 대한 기능 구현
    Co-authored-by: fromitive <fromitive@gmail.com>

--- src/test/java/chess/domain/direction/UpDirectionTest.java
+++ src/test/java/chess/domain/direction/UpDirectionTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UpDirectionTest {
-    
+
     @Test
     @DisplayName("도착 위치로 이동이 가능할 경우 참을 반환한다.")
     void canReach() {
-        UpDirection leftDirection = new UpDirection(8);
+        UpDirection direction = new UpDirection(8);
         Position from = new Position(1, 8);
         Position to = new Position(3, 8);
-        assertThat(leftDirection.canReach(from, to, List.of())).isTrue();
+        assertThat(direction.canReach(from, to, List.of())).isTrue();
     }
 
     @Test
@@ -30,19 +30,19 @@ class UpDirectionTest {
     @Test
     @DisplayName("도착위치 중간에 장애물이 있을 경우 거짓을 반환한다.")
     void canNotReachWhenObstacleExist() {
-        UpDirection leftDirection = new UpDirection(8);
+        UpDirection direction = new UpDirection(8);
         Position from = new Position(1, 3);
         Position to = new Position(8, 3);
-        assertThat(leftDirection.canReach(from, to, List.of(new Position(4, 3)))).isFalse();
+        assertThat(direction.canReach(from, to, List.of(new Position(4, 3)))).isFalse();
     }
 
     @Test
     @DisplayName("이동할 수 있는 방향의 개수를 모두 소진함에도 불구하고 도달하지 못할 경우 거짓을 반환한다.")
     void canNotReachWhenOverMoveCount() {
-        UpDirection leftDirection = new UpDirection(1);
+        UpDirection direction = new UpDirection(1);
         Position from = new Position(1, 8);
         Position to = new Position(1, 6);
-        assertThat(leftDirection.canReach(from, to, List.of())).isFalse();
+        assertThat(direction.canReach(from, to, List.of())).isFalse();
     }
 }
 
