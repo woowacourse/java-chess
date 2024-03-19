2d172ed8de357c01483876d48c58706949884369
nayonsoso <panda0329@naver.com>
2024-03-19 19:59:54 +0900

    feat: 기물과 대응되는 출력 메세지 분리
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/main/java/chess/view/PieceExpression.java
@@ -0,0 +1,42 @@
+package chess.view;
+
+import chess.Piece;
+
+import java.util.Arrays;
+
+import static chess.PieceType.*;
+import static chess.Team.BLACK;
+import static chess.Team.WHITE;
+
+public enum PieceExpression {
+
+    BLACK_KING(Piece.of(KING, BLACK), "K"),
+    BLACK_QUEEN(Piece.of(QUEEN, BLACK), "Q"),
+    BLACK_ROOK(Piece.of(ROOK, BLACK), "R"),
+    BLACK_BISHOP(Piece.of(BISHOP, BLACK), "B"),
+    BLACK_KNIGHT(Piece.of(KNIGHT, BLACK), "N"),
+    BLACK_PAWN(Piece.of(PAWN, BLACK), "P"),
+    WHITE_KING(Piece.of(KING, WHITE), "k"),
+    WHITE_QUEEN(Piece.of(QUEEN, WHITE), "q"),
+    WHITE_ROOK(Piece.of(ROOK, WHITE), "r"),
+    WHITE_BISHOP(Piece.of(BISHOP, WHITE), "b"),
+    WHITE_KNIGHT(Piece.of(KNIGHT, WHITE), "n"),
+    WHITE_PAWN(Piece.of(PAWN, WHITE), "p"),
+    EMPTY(null, ".");
+
+    private Piece piece;
+    private String expression;
+
+    PieceExpression(final Piece piece, final String expression) {
+        this.piece = piece;
+        this.expression = expression;
+    }
+
+    public static String mapToExpression(final Piece piece) {
+        return Arrays.stream(values())
+                .filter(value -> value.piece.equals(piece))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 기물입니다."))
+                .expression;
+    }
+}
