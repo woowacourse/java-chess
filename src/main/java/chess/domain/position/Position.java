b064d410e127140368cba1673422ea533974c30b
eunjungL <dldmswjd322@gmail.com>
2024-03-19 20:33:02 +0900

    refactor: Position 사용하지 않는 equals 오버라이딩 제거

--- src/main/java/chess/domain/position/Position.java
+++ src/main/java/chess/domain/position/Position.java
@@ -2,7 +2,6 @@ package chess.domain.position;
 
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -26,18 +25,5 @@ public class Position {
 
     public static Position of(File file, Rank rank) {
         return pool.get(toKey(file, rank));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Position position = (Position) o;
-        return file == position.file && rank == position.rank;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(file, rank);
     }
 }
