1bacab0e58db154c9caa7dc9264772ea76e34a93
takoyakimchi <c4nyou@gmail.com>
2024-03-19 14:03:45 +0900

    feat: Position 클래스 생성
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,39 @@
+package domain;
+
+import java.util.Objects;
+
+public class Position {
+
+    private final File file;
+    private final Rank rank;
+
+    public Position(File file, Rank rank) {
+        this.file = file;
+        this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return Objects.equals(file, position.file) && Objects.equals(rank, position.rank);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(file, rank);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+            "file=" + file +
+            ", rank=" + rank +
+            '}';
+    }
+}
