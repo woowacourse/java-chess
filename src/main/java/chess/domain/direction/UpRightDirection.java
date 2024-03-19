6dcb5045d6ea501f58b7157367b1b5042ec2c0ae
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-19 22:15:10 +0900

    feat: 대각선 상우 방향에 대한 기능 구현
    Co-authored-by: fromitive <fromitive@gmail.com>

---
+++ src/main/java/chess/domain/direction/UpRightDirection.java
@@ -0,0 +1,18 @@
+package chess.domain.direction;
+
+import chess.domain.Position;
+
+public class UpRightDirection extends Direction {
+
+    public UpRightDirection(final int moveCount) {
+        super(moveCount);
+    }
+
+    @Override
+    Position next(final Position from) {
+        if (from.isMaximumColumn() || from.isMaximumRow()) {
+            return from;
+        }
+        return new Position(from.row() + 1, from.column() + 1);
+    }
+}
---
+++ src/test/java/chess/domain/direction/UpRightDirectionTest.java
@@ -0,0 +1,50 @@
+package chess.domain.direction;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.Position;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class UpRightDirectionTest {
+
+    @Test
+    @DisplayName("도착 위치로 이동이 가능할 경우 참을 반환한다.")
+    void canReach() {
+        UpRightDirection direction = new UpRightDirection(8);
+        Position from = new Position(1, 1);
+        Position to = new Position(8, 8);
+        assertThat(direction.canReach(from, to, List.of())).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"8,7", "7,8", "8,8"})
+    @DisplayName("현재 위치에서 더이상 이동이 불가능한 경우 거짓을 반환한다.")
+    void canNotReachWhenNowIsBoundary(int row, int column) {
+        UpRightDirection direction = new UpRightDirection(8);
+        Position from = new Position(row, column);
+        Position to = new Position(1, 1);
+        assertThat(direction.canReach(from, to, List.of())).isFalse();
+    }
+
+    @Test
+    @DisplayName("도착위치 중간에 장애물이 있을 경우 거짓을 반환한다.")
+    void canNotReachWhenObstacleExist() {
+        UpRightDirection direction = new UpRightDirection(8);
+        Position from = new Position(1, 1);
+        Position to = new Position(8, 8);
+        assertThat(direction.canReach(from, to, List.of(new Position(7, 7)))).isFalse();
+    }
+
+    @Test
+    @DisplayName("이동할 수 있는 방향의 개수를 모두 소진함에도 불구하고 도달하지 못할 경우 거짓을 반환한다.")
+    void canNotReachWhenOverMoveCount() {
+        UpRightDirection direction = new UpRightDirection(1);
+        Position from = new Position(1, 1);
+        Position to = new Position(3, 3);
+        assertThat(direction.canReach(from, to, List.of())).isFalse();
+    }
+}
