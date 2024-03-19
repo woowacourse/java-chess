10c0ce978390a510e10f4208af7d4c4f298da817
BurningFalls <sherlocky31415@gmail.com>
2024-03-19 20:34:09 +0900

    feat: 퀸 움직임 가능 여부 판별 기능 구현

--- src/main/java/chess/domain/strategy/QueenMoveStrategy.java
+++ src/main/java/chess/domain/strategy/QueenMoveStrategy.java
@@ -3,12 +3,12 @@ package chess.domain.strategy;
 import chess.domain.Position;
 import chess.domain.PositionDifference;
 
-public class RookMoveStrategy implements MoveStrategy {
+public class QueenMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean canMove(Position currentPosition, Position newPosition) {
         PositionDifference positionDifference = currentPosition.calculateDifference(newPosition);
 
-        return positionDifference.isOnAxis();
+        return positionDifference.isOnAxis() || positionDifference.isMagnitudeEqual();
     }
 }
