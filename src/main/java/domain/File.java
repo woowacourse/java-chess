88ac2950ac816bdfca95f3a6ec47d46da6e271f7
SCY <helenason@naver.com>
2024-03-19 14:24:39 +0900

    feat(File, Rank): 체스판 가로세로 위치 값 생성

---
+++ src/main/java/domain/File.java
@@ -0,0 +1,6 @@
+package domain;
+
+public enum File {
+
+    A, B, C, D, E, F, G, H;
+}
---
+++ src/main/java/domain/Rank.java
@@ -0,0 +1,6 @@
+package domain;
+
+public enum Rank {
+
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT;
+}
