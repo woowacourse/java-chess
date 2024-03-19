5c688f414234d8a00a3272a08c70139bd36ff49f
SCY <helenason@naver.com>
2024-03-19 20:54:56 +0900

    refactor(SettingRule): 피스 타입 결정 역할 TypeFinder에게 위임

--- src/main/java/domain/board/SettingRule.java
+++ src/main/java/domain/board/SettingRule.java
@@ -3,56 +3,54 @@ package domain.board;
 import domain.piece.Color;
 import domain.piece.Piece;
 import domain.piece.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SettingRule {
 
     public Piece findPieceByPosition(Rank rank, File file) {
-        if (rank == Rank.TWO) {
-            return new Piece(Type.PAWN, Color.WHITE);
+        Color color = findColor(rank);
+        Type type = TypeFinder.find(rank, file);
+        return new Piece(type, color);
+    }
+
+    private Color findColor(Rank rank) {
+        if (rank == Rank.ONE || rank == Rank.TWO) {
+            return Color.WHITE;
         }
-        if (rank == Rank.SEVEN) {
-            return new Piece(Type.PAWN, Color.BLACK);
+        if (rank == Rank.SEVEN || rank == Rank.EIGHT) {
+            return Color.BLACK;
         }
-        if (file == File.E) {
-            if (rank == Rank.ONE) {
-                return new Piece(Type.KING, Color.WHITE);
-            }
-            if (rank == Rank.EIGHT) {
-                return new Piece(Type.KING, Color.BLACK);
-            }
+        return Color.NONE;
+    }
+
+    private enum TypeFinder {
+        PAWN(Type.PAWN, List.of(Rank.TWO, Rank.SEVEN), List.of(File.values())),
+        ROOK(Type.ROOK, List.of(Rank.ONE, Rank.EIGHT), List.of(File.A, File.H)),
+        KNIGHT(Type.KNIGHT, List.of(Rank.ONE, Rank.EIGHT), List.of(File.B, File.G)),
+        BISHOP(Type.BISHOP, List.of(Rank.ONE, Rank.EIGHT), List.of(File.C, File.F)),
+        QUEEN(Type.QUEEN, List.of(Rank.ONE, Rank.EIGHT), List.of(File.D)),
+        KING(Type.KING, List.of(Rank.ONE, Rank.EIGHT), List.of(File.E)),
+        NONE(Type.NONE, new ArrayList<>(), new ArrayList<>()),
+        ;
+
+        private final Type type;
+        private final List<Rank> ranks;
+        private final List<File> files;
+
+        TypeFinder(Type type, List<Rank> ranks, List<File> files) {
+            this.type = type;
+            this.ranks = ranks;
+            this.files = files;
         }
-        if (file == File.D) {
-            if (rank == Rank.ONE) {
-                return new Piece(Type.QUEEN, Color.WHITE);
-            }
-            if (rank == Rank.EIGHT) {
-                return new Piece(Type.QUEEN, Color.BLACK);
-            }
+
+        private static Type find(Rank rank, File file) {
+            return Arrays.stream(values())
+                    .filter((typeFinder -> typeFinder.ranks.contains(rank) && typeFinder.files.contains(file)))
+                    .findFirst()
+                    .orElse(TypeFinder.NONE)
+                    .type;
         }
-        if (file == File.C || file == File.F) {
-            if (rank == Rank.ONE) {
-                return new Piece(Type.BISHOP, Color.WHITE);
-            }
-            if (rank == Rank.EIGHT) {
-                return new Piece(Type.BISHOP, Color.BLACK);
-            }
-        }
-        if (file == File.B || file == File.G) {
-            if (rank == Rank.ONE) {
-                return new Piece(Type.KNIGHT, Color.WHITE);
-            }
-            if (rank == Rank.EIGHT) {
-                return new Piece(Type.KNIGHT, Color.BLACK);
-            }
-        }
-        if (file == File.A || file == File.H) {
-            if (rank == Rank.ONE) {
-                return new Piece(Type.ROOK, Color.WHITE);
-            }
-            if (rank == Rank.EIGHT) {
-                return new Piece(Type.ROOK, Color.BLACK);
-            }
-        }
-        return new Piece(Type.NONE, Color.NONE);
     }
 }
