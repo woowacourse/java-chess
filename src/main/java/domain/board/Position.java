308efc6e7b5685f5274f74eec9075accd23db894
Nam Gi Beom <rlqja0523@naver.com>
2024-03-19 19:20:57 +0900

    feat: 포지션 객체 추가
    Co-authored-by: 3juhwan <13selfesteem91@naver.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

---
+++ src/main/java/domain/board/Position.java
@@ -0,0 +1,11 @@
+package domain.board;
+
+public class Position {
+    private final int row;
+    private final int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
