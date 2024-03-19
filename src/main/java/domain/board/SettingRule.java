c76d60ca9ffc1411bdb5636c13cbd29d489552ff
SCY <helenason@naver.com>
2024-03-19 18:11:18 +0900

    feat(SettingRule): 킹을 반환하는 규칙 추가

--- src/main/java/domain/board/SettingRule.java
+++ src/main/java/domain/board/SettingRule.java
@@ -13,6 +13,14 @@ public class SettingRule {
         if (rank == Rank.SEVEN) {
             return new Piece(Type.PAWN, Color.BLACK);
         }
+        if (file == File.E) {
+            if (rank == Rank.ONE) {
+                return new Piece(Type.KING, Color.WHITE);
+            }
+            if (rank == Rank.EIGHT) {
+                return new Piece(Type.KING, Color.BLACK);
+            }
+        }
         return new Piece(Type.NONE, Color.NONE);
     }
 }
--- src/test/java/domain/board/SettingRuleTest.java
+++ src/test/java/domain/board/SettingRuleTest.java
@@ -1,26 +1,53 @@
 package domain.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import domain.piece.Color;
 import domain.piece.Piece;
 import domain.piece.Type;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@Nested
 public class SettingRuleTest {
 
-    @Test
-    @DisplayName("랭크가 2 또는 7일 경우 폰을 반환한다.")
-    void findPieceByPosition_RankTwoOrSeven_Pawn() {
-        SettingRule settingRule = new SettingRule();
+    private final SettingRule settingRule = new SettingRule();
 
-        assertAll(
-                () -> assertThat(settingRule.findPieceByPosition(Rank.TWO, File.A))
-                        .isEqualTo(new Piece(Type.PAWN, Color.WHITE)),
-                () -> assertThat(settingRule.findPieceByPosition(Rank.SEVEN, File.A))
-                        .isEqualTo(new Piece(Type.PAWN, Color.BLACK))
-        );
+    @Nested
+    class PawnTest {
+
+        @Test
+        @DisplayName("랭크가 2일 경우 흰색 폰을 반환한다.")
+        void findPieceByPosition_RankTwo_WhitePawn() {
+            assertThat(settingRule.findPieceByPosition(Rank.TWO, File.A))
+                    .isEqualTo(new Piece(Type.PAWN, Color.WHITE));
+        }
+
+        @Test
+        @DisplayName("랭크가 2일 경우 검정색 폰을 반환한다.")
+        void findPieceByPosition_RankSeven_BlackPawn() {
+            assertThat(settingRule.findPieceByPosition(Rank.SEVEN, File.A))
+                    .isEqualTo(new Piece(Type.PAWN, Color.BLACK));
+        }
     }
+
+    @Nested
+    class KingTest {
+
+        @Test
+        @DisplayName("랭크가 1, 파일이 e일 경우 흰색 킹을 반환한다.")
+        void findPieceByPosition_RankOneFileE_WhiteKing() {
+            assertThat(settingRule.findPieceByPosition(Rank.ONE, File.E))
+                    .isEqualTo(new Piece(Type.KING, Color.WHITE));
+        }
+
+        @Test
+        @DisplayName("랭크가 8, 파일이 e일 경우 검정색 킹을 반환한다.")
+        void findPieceByPosition_RankEightFileE_BlackKing() {
+            assertThat(settingRule.findPieceByPosition(Rank.EIGHT, File.E))
+                    .isEqualTo(new Piece(Type.KING, Color.BLACK));
+        }
+    }
+
 }
