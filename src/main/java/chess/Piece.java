3f31551e5708d6a07f7408a113f1ae785811eed1
nayonsoso <panda0329@naver.com>
2024-03-19 19:58:52 +0900

    refactor: 기물 캐싱
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/Piece.java
+++ src/main/java/chess/Piece.java
@@ -1,12 +1,55 @@
 package chess;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Piece {
+
+    private static final Map<String, Piece> CACHE = new HashMap<>();
 
     private final PieceType pieceType;
     private final Team team;
 
-    public Piece(PieceType pieceType, Team team) {
+    static {
+        for (final PieceType pieceType : PieceType.values()) {
+            for (Team team : Team.values()) {
+                CACHE.put(toKey(pieceType, team), new Piece(pieceType, team));
+            }
+        }
+    }
+
+    public Piece(final PieceType pieceType, final Team team) {
         this.pieceType = pieceType;
         this.team = team;
+    }
+
+    public static Piece of(final PieceType pieceType, final Team team) {
+        return CACHE.get(toKey(pieceType, team));
+    }
+
+    private static String toKey(final PieceType pieceType, final Team team) {
+        return pieceType.name() + team.name();
+    }
+
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Piece piece = (Piece) o;
+        return pieceType == piece.pieceType && team == piece.team;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceType, team);
     }
 }
