94e9b31ebcf0570206d234184c5c34f5c258d6e8
eun-byeol <joeunbyeol98@gmail.com>
2024-03-19 18:50:37 +0900

    feat: 1 이상 8 이하의 좌표 값이 아니면 예외가 발생한다
    Co-authored-by: masonkimseoul <masonkimseoul@gmail.com>

--- src/main/java/chess/model/Position.java
+++ src/main/java/chess/model/Position.java
@@ -1,11 +1,21 @@
 package chess.model;
 
 public class Position {
+
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 8;
     private final int x;
     private final int y;
 
     public Position(int x, int y) {
+        validateRange(x, y);
         this.x = x;
         this.y = y;
+    }
+
+    private void validateRange(int x, int y) {
+        if (x < MIN_VALUE || x > MAX_VALUE || y < MIN_VALUE || y > MAX_VALUE) {
+            throw new IllegalArgumentException("좌표 값은 1이상 8이하의 값만 가능합니다.");
+        }
     }
 }
--- src/test/java/chess/model/PositionTest.java
+++ src/test/java/chess/model/PositionTest.java
@@ -1,9 +1,14 @@
 package chess.model;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PositionTest {
 
@@ -11,7 +16,21 @@ class PositionTest {
     @Test
     void createValidPosition() {
         assertThatCode(() -> new Position(3, 6))
-                .doesNotThrowAnyException();
+            .doesNotThrowAnyException();
     }
 
+    @DisplayName("1 이상 8 이하의 좌표 값이 아니면 예외가 발생한다")
+    @ParameterizedTest
+    @MethodSource("provideInvalidPosition")
+    void createInvalidPosition(int x, int y) {
+        assertThatThrownBy(() -> new Position(x, y))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    public static Stream<Arguments> provideInvalidPosition() {
+        return Stream.of(
+            Arguments.of(0, 6),
+            Arguments.of(3, 9)
+        );
+    }
 }
