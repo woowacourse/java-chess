1dcbc4f67ed222a5d119ae536c8ba112e4d228a1
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-19 19:30:22 +0900

    feat(Square): 도메인 구현
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

---
+++ src/main/java/chess/Square.java
@@ -0,0 +1,12 @@
+package chess;
+
+public class Square {
+
+    private final Lettering lettering;
+    private final Numbering numbering;
+
+    public Square(Lettering lettering, Numbering numbering) {
+        this.lettering = lettering;
+        this.numbering = numbering;
+    }
+}
