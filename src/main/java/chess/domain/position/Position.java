f25b2530b4c4a93c13c7308f7bd9169c1c8cff41
libienz <leekh10195@gmail.com>
2024-03-19 16:37:39 +0900

    feat: 포지션 정적 팩토리 메서드 구현

--- src/main/java/chess/domain/position/Position.java
+++ src/main/java/chess/domain/position/Position.java
@@ -3,9 +3,14 @@ package chess.domain.position;
 public class Position {
     //TODO: ROW 클래스 이름 수정
     //TODO: 포지션 캐싱
+    //TODO: 포지션 관련 클래스들 이퀄스 해시 재정의
 
     private final RowPosition rowPosition;
     private final ColumnPosition columnPosition;
+
+    public static Position of(int rowPosition, int colPosition) {
+        return new Position(new RowPosition(rowPosition), new ColumnPosition(colPosition));
+    }
 
     public Position(RowPosition rowPosition, ColumnPosition columnPosition) {
         this.rowPosition = rowPosition;
