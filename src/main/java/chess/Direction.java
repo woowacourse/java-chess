516d83dcecbfafdcb17715ebd05866a188090569
jhon3242 <rldotnr2@gmail.com>
2024-03-19 16:08:05 +0900

    feat : Unit 객체 생성
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

---
+++ src/main/java/chess/Direction.java
@@ -0,0 +1,20 @@
+package chess;
+
+public enum Direction {
+    N(-1, 0),
+    NE(-1, 1),
+    E(0, 1),
+    SE(1, 1),
+    S(1, 0),
+    SW(1, -1),
+    W(0, -1),
+    NW(-1, -1);
+
+    private int directionX;
+    private int directionY;
+
+    Direction(int directionX, int directionY) {
+        this.directionX = directionX;
+        this.directionY = directionY;
+    }
+}
