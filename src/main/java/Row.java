53ee6795392c5ae228e2e0091b60dbca91957724
tackyu <tack970913@gmail.com>
2024-03-19 16:12:18 +0900

    feat(Row) : Row 캐싱 및 검증

---
+++ src/main/java/Row.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toMap;
+
+public class Row {
+    private final String value;
+    private static final Map<String, Row> CACHE = IntStream.rangeClosed('a', 'h')
+            .boxed()
+            .collect(toMap(
+                    i -> String.valueOf((char) i.intValue()),
+                    i -> new Row(String.valueOf((char) i.intValue()))
+            ));
+
+    private Row(String value) {
+        this.value = value;
+    }
+    public static Row valueOf(String value) {
+        validate(value);
+        return CACHE.get(value);
+    }
+    private static void validate(String value){
+        validateAlphabet(value);
+        validateSize(value);
+    }
+
+    private static void validateAlphabet(String value) {
+        //TODO : charAt 말고 다른 방법 생각해보기
+        char row = value.charAt(0);
+        if(row<'a'||row>'h'){
+            throw new IllegalArgumentException("a~h까지 가능합니다.");
+        }
+    }
+
+    private static void validateSize(String value){
+        if(value.length()!=1){
+            throw new IllegalArgumentException("a~h까지 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/RowTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toMap;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class RowTest {
+
+    @Test
+    @DisplayName("가로는 a~h의 알파벳으로 생성된다.")
+    void makeRowTest() {
+        assertThatCode(() -> {
+            Row.valueOf("a");
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("a~h가 아닌 알파벳으로 만들어진 가로는 존재하지 않는다.")
+    void validateValueOf() {
+        assertThatThrownBy(() -> Row.valueOf("z")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
