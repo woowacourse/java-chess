05b1341c16b8c9987862d0d28d9146f26a1cce38
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-19 22:42:01 +0900

    refactor: 인덴트 감소
    Co-Authored-By: H <99153215+hyeon0208@users.noreply.github.com>

--- src/main/java/chess/view/PieceView.java
+++ src/main/java/chess/view/PieceView.java
@@ -1,12 +1,14 @@
 package chess.view;
 
 import chess.domain.piece.Bishop;
+import chess.domain.piece.EmptyPiece;
 import chess.domain.piece.King;
 import chess.domain.piece.Knight;
 import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Queen;
 import chess.domain.piece.Rook;
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum PieceView {
@@ -16,9 +18,9 @@ public enum PieceView {
     Q(piece -> piece instanceof Queen, "q"),
     K(piece -> piece instanceof King, "k"),
     P(piece -> piece instanceof Pawn, "p"),
+    NONE(piece -> piece instanceof EmptyPiece, "."),
     ;
 
-    public static final String EMPTY_PIECE_SYMBOL = ".";
     private final Predicate<Piece> predicate;
     private final String symbol;
 
@@ -28,15 +30,16 @@ public enum PieceView {
     }
 
     public static String findValue(final Piece piece) {
-        for (PieceView value : PieceView.values()) {
-            if (value.predicate.test(piece)) {
-                if (piece.isWhite()) {
-                    return value.symbol;
-                }
-                return value.symbol.toUpperCase();
-            }
-        }
-        return EMPTY_PIECE_SYMBOL;
+        return Arrays.stream(PieceView.values())
+                .filter(value -> value.predicate.test(piece))
+                .map(value -> {
+                    if (piece.isWhite()) {
+                        return value.symbol;
+                    }
+                    return value.symbol.toUpperCase();
+                })
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("기물 표식을 찾을 수 없습니다."));
     }
 
 }
