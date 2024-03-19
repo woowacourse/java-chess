b4b8aee43af539042ac3fbbaac6af33917ddb4f1
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-19 21:59:22 +0900

    feat: 좌 방향에 대한 기능 구현
    Co-authored-by: fromitive <fromitive@gmail.com>

--- src/main/java/chess/domain/direction/Direction.java
+++ src/main/java/chess/domain/direction/Direction.java
@@ -2,7 +2,21 @@ package chess.domain.direction;
 
 import chess.domain.Position;
 import java.util.List;
+import java.util.stream.Stream;
 
-public interface Direction {
-    List<Position> move(Position from);
+abstract class Direction {
+    private final int moveCount;
+
+    Direction(int moveCount) {
+        this.moveCount = moveCount;
+    }
+
+    boolean canReach(Position from, Position to, List<Position> obstacle) {
+        return Stream.iterate(from, this::next)
+                .takeWhile(now -> !obstacle.contains(now))
+                .limit(moveCount)
+                .anyMatch(position -> position.equals(to));
+    }
+
+    abstract Position next(Position from);
 }
--- src/main/java/chess/domain/direction/LeftDirection.java
+++ src/main/java/chess/domain/direction/LeftDirection.java
@@ -1,15 +1,19 @@
 package chess.domain.direction;
 
 import chess.domain.Position;
-import java.util.List;
 
-public class LeftDirection implements Direction {
+public class LeftDirection extends Direction {
+
+    LeftDirection(final int moveCount) {
+        super(moveCount);
+    }
+    
 
     @Override
-    public List<Position> move(final Position from) {
-        if (!from.isMinimumColumn()) {
-            return List.of(new Position(from.row(), from.column() - 1));
+    Position next(final Position from) {
+        if (from.isMinimumColumn()) {
+            return from;
         }
-        return List.of();
+        return new Position(from.row(), from.column() - 1);
     }
 }
