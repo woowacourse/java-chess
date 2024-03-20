c64e80abfb8876a646b32aa9e6761a964be30baa
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-03-20 19:36:16 +0900

    refactor(InputView): StringTokenizer 사용

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -2,9 +2,9 @@ package chess.view;
 
 import chess.domain.Position;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -19,20 +19,23 @@ public class InputView {
 
     // move b2 b3
     public static List<Position> inputNextCommand() {
-        String command = SCANNER.nextLine();
+        String input = SCANNER.nextLine();
 
-        List<String> commands = Arrays.asList(command.split(" "));
-        List<Position> positions = new ArrayList<>();
+        StringTokenizer inputTokenizer = new StringTokenizer(input);
+        String command = inputTokenizer.nextToken();
 
-        if (Command.isStart(commands.get(0))) {
+        if (Command.isStart(command)) {
             throw new IllegalArgumentException();
         }
-
-        if (Command.isMove(commands.get(0)) && commands.size() == 3) {
-            positions.add(PositionConverter.generate(commands.get(1)));
-            positions.add(PositionConverter.generate(commands.get(2)));
+        if (Command.isEnd(command)) {
+            return new ArrayList<>();
         }
 
+        List<Position> positions = new ArrayList<>();
+        if (Command.isMove(command) && inputTokenizer.countTokens() == 2) {
+            positions.add(PositionConverter.generate(inputTokenizer.nextToken()));
+            positions.add(PositionConverter.generate(inputTokenizer.nextToken()));
+        }
         return positions;
     }
 
