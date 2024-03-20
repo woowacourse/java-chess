2189f996d0d57cf85d02386024674ef5612ebf5b
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-20 16:21:23 +0900

    feat(Point) : 파일,랭크 를 포함한 문자열을 통해 생성 기능 구현
    - 이에 따른 테스트 코드 추가

--- src/main/java/domain/piece/point/Point.java
+++ src/main/java/domain/piece/point/Point.java
@@ -1,6 +1,11 @@
 package domain.piece.point;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public record Point(File file, Rank rank) {
+
+    private static final Pattern pattern = Pattern.compile("[a-h][1-8]");
 
     public int getFileIndex() {
         return this.file.ordinal();
@@ -8,5 +13,19 @@ public record Point(File file, Rank rank) {
 
     public int getRankIndex() {
         return this.rank.ordinal();
+    }
+
+    public static Point from(String value) {
+        validate(value);
+        File file = File.from(value.charAt(0));
+        Rank rank = Rank.from(Integer.parseInt(value.substring(1)));
+        return new Point(file, rank);
+    }
+
+    private static void validate(String value) {
+        Matcher matcher = pattern.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("파일은 a~h이고, 랭크는 아래부터 위로 1~8까지입니다.");
+        }
     }
 }
--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -3,15 +3,45 @@ import org.junit.jupiter.api.Test;
 import domain.piece.point.File;
 import domain.piece.point.Point;
 import domain.piece.point.Rank;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PointTest {
     @Test
-    @DisplayName("File(행) 과 Rank(열) 을 통해 Point 를 만든다.")
+    @DisplayName("File(행) 과 Rank(열) 을 통해 포인트를 만든다.")
     void create_with_file_and_rank() {
         final var sut = new Point(File.E, Rank.ONE);
 
         assertThat(sut).isEqualTo(new Point(File.E, Rank.ONE));
+    }
+
+    @Test
+    @DisplayName("파일과 랭크가 포함된 문자열을 통해 포인트를 만든다.")
+    void create_point_with_string_that_contain_file_and_rank() {
+        final String value = "b2";
+
+        final var sut = Point.from(value);
+
+        assertThat(sut.file()).isEqualTo(File.B);
+        assertThat(sut.rank()).isEqualTo(Rank.TWO);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"b22", "c", "b3c4", ""})
+    @DisplayName("2글자가 아닌 문자열을 통해 포인트를 만들 시 예외를 발생시킨다.")
+    void throw_exception_when_string_length_is_not_two(String invalidValue) {
+        assertThatThrownBy(() -> Point.from(invalidValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"z3", "i9", "i1", "k0"})
+    @DisplayName("파일 문자가 a~h를 벗어나거나 , 랭크 문자가 1~8 을 벗어나면 예외를 발생시킨다.")
+    void throw_exception_when_file_or_rank_char_out_of_range(String invalidValue) {
+        assertThatThrownBy(() -> Point.from(invalidValue))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
