7633ca7e09f593f35ed9ca48348f2b6b25e44564
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-20 22:09:53 +0900

    feat(Bishop) : 비숍 기물이 이동 가능한지 확인하는 기능 구현
    - 이에 따른 테스트 코드 추가

--- src/main/java/domain/Bishop.java
+++ src/main/java/domain/Bishop.java
@@ -1,9 +1,17 @@
 package domain;
 
 import domain.piece.attribute.Color;
+import domain.piece.attribute.point.Direction;
 import domain.piece.attribute.point.Point;
+import factory.DirectionFactory;
+
+import java.util.List;
+
+import static domain.piece.attribute.point.Direction.*;
 
 public class Bishop extends Piece {
+    private static final List<Direction> directionList = List.of(DOWN_LEFT, DOWN_RIGHT, UP_LEFT, UP_RIGHT);
+
     public Bishop(final Point point, final Color color) {
         super(point, color);
     }
@@ -11,5 +19,10 @@ public class Bishop extends Piece {
     @Override
     public PieceStatus getStatus() {
         return PieceStatus.BISHOP;
+    }
+
+    public boolean canMove(Point point) {
+        final Direction direction = DirectionFactory.generate(this.getPoint(), point);
+        return directionList.contains(direction);
     }
 }
---
+++ src/test/java/piece/BishopTest.java
@@ -0,0 +1,63 @@
+package piece;
+
+import domain.Bishop;
+import domain.piece.attribute.Color;
+import domain.piece.attribute.point.File;
+import domain.piece.attribute.point.Point;
+import domain.piece.attribute.point.Rank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BishopTest {
+    @Test
+    @DisplayName("비숍은 아래 오른쪽 방향으로 이동할 수 있다.")
+    void can_move_up_right() {
+        final var sut = new Bishop(new Point(File.C, Rank.FOUR), Color.BLACK);
+
+        final var result = sut.canMove(new Point(File.D, Rank.THREE));
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("비숍은 아래 왼쪽 방향으로 이동할 수 있다.")
+    void can_move_down_left() {
+        final var sut = new Bishop(new Point(File.C, Rank.FOUR), Color.BLACK);
+
+        final var result = sut.canMove(new Point(File.B, Rank.THREE));
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("비숍은 위 오른쪽 방향으로 이동할 수 있다.")
+    void can_move_up_left() {
+        final var sut = new Bishop(new Point(File.C, Rank.TWO), Color.BLACK);
+
+        final var result = sut.canMove(new Point(File.D, Rank.THREE));
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("비숍은 위 왼쪽 방향으로 이동할 수 있다.")
+    void can_move_down_right() {
+        final var sut = new Bishop(new Point(File.F, Rank.FOUR), Color.BLACK);
+
+        final var result = sut.canMove(new Point(File.E, Rank.FIVE));
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("비숍은 그외 방향으로 이동할 수 없다.")
+    void can_not_move_other_direction() {
+        final var sut = new Bishop(new Point(File.F, Rank.EIGHT), Color.BLACK);
+
+        final var result = sut.canMove(new Point(File.F, Rank.ONE));
+
+        assertThat(result).isFalse();
+    }
+}
