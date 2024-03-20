145fe25c9f38b30ef73cf79adc92744960f7a411
haiseong <wjdgotjd9908@gmail.com>
2024-03-20 22:53:32 +0900

    feat(OutputView): 초기 시작시 안내 메시지를 출력하는 기능 구현

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -7,6 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
+
+    public void printWelcomeMessage() {
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+    }
+
     public void printBoard(Map<Position, PieceType> board) {
         List<String> lines = PieceDisplay.makeBoardDisplay(board);
         lines.forEach(System.out::println);
