6de8f1f83ad795a04b3afe541f3e415e06559ce5
libienz <leekh10195@gmail.com>
2024-03-21 08:44:28 +0900

    feat: 폰 일반 행마법 구현

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -9,7 +9,21 @@ public class Pawn extends Piece {
 
     @Override
     public boolean canMove(Position start, Position destination) {
-        //TODO: 폰 움직임 전략 구현 필요
+        //폰의 전진방향이 팀의 전진방향과 같고 움직이는 양이 1이면 움직일 수 있다
+        if (isForward(start, destination) && start.squaredDistanceWith(destination) == 1) {
+            return true;
+        }
+
+        //폰의 전진양이 2이고 폰이 초기위치 이면 전진할 수 있다.
+        if (isForward(start, destination) && start.squaredDistanceWith(destination) == 2 && start.isStraightWith(
+                destination) && start.rowIs(teamInitialPawnRow())) {
+            return true;
+        }
+        //잡아먹는 경우 (위 대각선 두방향도 일단 true 반환)
         return false;
+    }
+
+    private boolean isForward(Position start, Position destination) {
+        return start.directionTo(destination) == teamForwardDirection();
     }
 }
