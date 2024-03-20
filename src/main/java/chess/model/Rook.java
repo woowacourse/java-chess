86ae4ccadd372466087b7d46cd15ee5de76e5521
jongmee <ra123221@gmail.com>
2024-03-20 15:30:35 +0900

    feat: Rook 이동 가능 여부 판단 구현

--- src/main/java/chess/model/Rook.java
+++ src/main/java/chess/model/Rook.java
@@ -6,6 +6,12 @@ public class Rook extends Piece {
     }
 
     @Override
+    public boolean canMove(ChessPosition target) {
+        Distance distance = target.calculateDistance(this.chessPosition);
+        return distance.isCrossMovement();
+    }
+
+    @Override
     public String getText() {
         if (side.isWhite()) {
             return "r";
---
+++ src/test/java/chess/model/RookTest.java
@@ -0,0 +1,36 @@
+package chess.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RookTest {
+    @ParameterizedTest
+    @MethodSource("provideTargetPositionAndResult")
+    @DisplayName("Rook이 타켓 위치로 움직일 수 있는지 판단한다.")
+    void canMove(ChessPosition target, boolean expected) {
+        // given
+        ChessPosition source = new ChessPosition(File.C, Rank.TWO);
+        Rook rook = new Rook(Side.WHITE, source);
+
+        // when
+        boolean result = rook.canMove(target);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideTargetPositionAndResult() {
+        return Stream.of(
+                Arguments.of(new ChessPosition(File.C, Rank.THREE), true),
+                Arguments.of(new ChessPosition(File.D, Rank.TWO), true),
+                Arguments.of(new ChessPosition(File.F, Rank.ONE), false),
+                Arguments.of(new ChessPosition(File.A, Rank.THREE), false)
+        );
+    }
+}
