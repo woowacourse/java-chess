fb3a37b93a447f404a340674b4a55a1a3d2414a1
J-I-H-O <jeongjiho0731@gmail.com>
2024-03-20 21:04:41 +0900

    feat: 기울기 및 거리 계산 로직 구현
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -7,8 +7,8 @@ public class Position {
 
     private static final Map<String, Position> CACHE = new LinkedHashMap<>();
 
-    private final char rank;
-    private final int file;
+    private final char file;
+    private final int rank;
 
     static {
         for (char i = 'a'; i <= 'h'; i++) {
@@ -18,16 +18,37 @@ public class Position {
         }
     }
 
-    private Position(final char rank, final int file) {
-        this.rank = rank;
+    private Position(final char file, final int rank) {
         this.file = file;
+        this.rank = rank;
     }
 
-    public static Position of(final char rank, final int file) {
-        return CACHE.get(toKey(rank, file));
+    public static Position of(final char file, final int rank) {
+        return CACHE.get(toKey(file, rank));
     }
 
-    private static String toKey(final char rank, final int file) {
-        return String.valueOf(rank) + file;
+    private static String toKey(final char file, final int rank) {
+        return String.valueOf(file) + rank;
+    }
+
+
+    public double calculateGradient(final Position target) {
+        double dx = this.file - target.file;
+        double dy = this.rank - target.rank;
+
+        // TODO: 개선 필요
+        if(dx == 0) {
+            return Double.POSITIVE_INFINITY;
+        }
+        // TODO: 개선 필요
+        if(dy == 0) {
+            return 0;
+        }
+
+        return dy / dx;
+    }
+
+    public double calculateDistance(final Position target) {
+        return Math.sqrt(Math.pow(this.file - target.file, 2) + Math.pow(this.rank - target.rank, 2));
     }
 }
--- src/test/java/chess/domain/PositionTest.java
+++ src/test/java/chess/domain/PositionTest.java
@@ -1,17 +1,72 @@
 package chess.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PositionTest {
 
     @Test
     @DisplayName("칸을 생성한다.")
     void create() {
-        char rank = 'a';
-        int file = 1;
+        char file = 'a';
+        int rank = 1;
 
-        Assertions.assertThat(Position.of(rank, file)).isInstanceOf(Position.class);
+        assertThat(Position.of(file, rank)).isInstanceOf(Position.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"a,1,b,1,0", "a,1,b,2,1", "a,1,b,3,2", "a,1,c,2,0.5"})
+    @DisplayName("타겟 위치와의 기울기를 구한다.")
+    void calculateGradient(char file1, int rank1, char file2, int rank2, Double expectedGradient) {
+        Position position1 = Position.of(file1, rank1);
+        Position position2 = Position.of(file2, rank2);
+
+        double actualGradient = position1.calculateGradient(position2);
+
+        assertThat(actualGradient).isEqualTo(expectedGradient);
+    }
+
+    @Test
+    @DisplayName("기울기가 무한대가 되는 경우 정상적으로 계산되는지 테스트한다.")
+    void calculateInfiniteGradient() {
+        Position position1 = Position.of('a', 1);
+        Position position2 = Position.of('a', 2);
+
+        double actualGradient = position1.calculateGradient(position2);
+
+        assertThat(actualGradient).isEqualTo(Double.POSITIVE_INFINITY);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"a,1,b,1,1", "a,1,a,2,1"})
+    @DisplayName("타겟 위치와의 정수 거리를 구한다.")
+    void calculateIntegerDistance(char file1, int rank1, char file2, int rank2, Double expectedDistance) {
+        Position position1 = Position.of(file1, rank1);
+        Position position2 = Position.of(file2, rank2);
+
+        double actualDistance = position1.calculateDistance(position2);
+
+        assertThat(actualDistance).isEqualTo(expectedDistance);
+    }
+
+    @Test
+    @DisplayName("타겟 위치와의 제곱근 거리를 구한다.")
+    void calculateDoubleDistance() {
+        char file1 = 'a';
+        int rank1 = 1;
+        char file2 = 'b';
+        int rank2 = 2;
+        Position position1 = Position.of(file1, rank1);
+        Position position2 = Position.of(file2, rank2);
+
+        double actualDistance = position1.calculateDistance(position2);
+
+        double expectedDistance = Math.sqrt(Math.pow(file1 - file2, 2) + Math.pow(rank1 - rank2, 2));
+        assertThat(actualDistance).isEqualTo(expectedDistance);
     }
 }
