e612bbe330b9f10a47e5a55243084f930eaa1a5f
tackyu <tack970913@gmail.com>
2024-03-20 16:58:11 +0900

    feat(ChessPiece) : 각 기물은 이동할 수 있는 방향을 가진다.

--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -1,24 +1,30 @@
 package chess;
 
+import java.util.List;
+import java.util.function.BiPredicate;
+
+import static chess.Direction.*;
+
 public enum ChessPiece {
-    BLACK_KING("K"),
-    WHITE_KING("k"),
-    BLACK_QUEEN("Q"),
-    WHITE_QUEEN("q"),
-    BLACK_ROOK("R"),
-    WHITE_ROOK("r"),
-    BLACK_BISHOP("B"),
-    WHITE_BISHOP("b"),
-    BLACK_KNIGHT("N"),
-    WHITE_KNIGHT("n"),
-    BLACK_PAWN("P"),
-    WHITE_PAWN("p"),
-    NONE(".");
+    BLACK_KING("K",List.of(UP,DOWN,LEFT,RIGHT,UP_LEFT,UP_RIGHT,DOWN_LEFT,DOWN_RIGHT)),
+    WHITE_KING("k",List.of(UP,DOWN,LEFT,RIGHT,UP_LEFT,UP_RIGHT,DOWN_LEFT,DOWN_RIGHT)),
+    BLACK_QUEEN("Q",List.of(UP,DOWN,LEFT,RIGHT,UP_LEFT,UP_RIGHT,DOWN_LEFT,DOWN_RIGHT)),
+    WHITE_QUEEN("q",List.of(UP,DOWN,LEFT,RIGHT,UP_LEFT,UP_RIGHT,DOWN_LEFT,DOWN_RIGHT)),
+    BLACK_ROOK("R",List.of(UP,DOWN,LEFT,RIGHT)),
+    WHITE_ROOK("r",List.of(UP,DOWN,LEFT,RIGHT)),
+    BLACK_BISHOP("B",List.of(UP_LEFT,UP_RIGHT,DOWN_LEFT,DOWN_RIGHT)),
+    WHITE_BISHOP("b",List.of(UP_LEFT,UP_RIGHT,DOWN_LEFT,DOWN_RIGHT)),
+    BLACK_KNIGHT("N",List.of()),
+    WHITE_KNIGHT("n",List.of()),
+    BLACK_PAWN("P",List.of(DOWN,DOWN_LEFT,DOWN_RIGHT)),
+    WHITE_PAWN("p",List.of(UP,UP_LEFT,UP_RIGHT)),
+    NONE(".",List.of());
 
     private final String name;
-
-    ChessPiece(String name) {
+    private final List<Direction> movableDirection;
+    ChessPiece(String name,List<Direction> movableDirection) {
         this.name = name;
+        this.movableDirection=movableDirection;
     }
 
     public String getName() {
