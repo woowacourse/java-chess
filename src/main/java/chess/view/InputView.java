d7b60988d2df65310d678e175d651093bb004e66
masonkimseoul <masonkimseoul@gmail.com>
2024-03-20 14:54:04 +0900

    feat: 게임 명령어 입력 로직 구현
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

---
+++ src/main/java/chess/view/InputView.java
@@ -0,0 +1,15 @@
+package chess.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String NEWLINE = System.lineSeparator();
+    private static final String CHESS_GAME_INTRO = "체스 게임을 시작합니다." + NEWLINE + "게임 시작은 start, 종료는 end 명령을 입력하세요.";
+
+    public String askGameCommand() {
+        System.out.println(CHESS_GAME_INTRO);
+        return SCANNER.nextLine()
+                .strip();
+    }
+}
