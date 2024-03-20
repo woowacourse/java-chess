080a7055f95e1080bab6568096c693bc0c3b2b97
Eugene Jang <kyunellroll@gmail.com>
2024-03-20 17:43:28 +0900

    feat(BishopStrategy): Bishop 의 움직임 구현
    - Bishop이 상하좌우로 이동하려고 할 때의 에러 발생 테스트
    
    Co-authored-by: pricelees <priceelees@gmail.com>

---
+++ src/main/java/strategy/BishopStrategy.java
@@ -0,0 +1,41 @@
+package strategy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum BishopStrategy {
+
+    UP_LEFT_DIAGONAL(-1, -1, List.of(-1, -1)),
+    UP_RIGHT_DIAGONAL(-1, 1, List.of(-1, 1)),
+    DOWN_LEFT_DIAGONAL(1, -1, List.of(1, -1)),
+    DOWN_RIGHT_DIAGONAL(1, 1, List.of(1, 1));
+
+    private final int rowDifference;
+    private final int columnDifference;
+    private final List<Integer> direction;
+
+    BishopStrategy(int rowDifference, int columnDifference, List<Integer> direction) {
+        this.rowDifference = rowDifference;
+        this.columnDifference = columnDifference;
+        this.direction = direction;
+    }
+
+    public static BishopStrategy getMovingStrategy(int rowDifference, int columnDifference) {
+        return Arrays.stream(BishopStrategy.values())
+                .filter(strategy -> strategy.rowDifference == divideValueByAbs(rowDifference)
+                        && strategy.columnDifference == divideValueByAbs(columnDifference))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("invalid move"));
+    }
+
+    private static int divideValueByAbs(int value) {
+        if (value == 0) {
+            return 0;
+        }
+        return value / Math.abs(value);
+    }
+
+    public List<Integer> getDirection() {
+        return this.direction;
+    }
+}
---
+++ src/test/java/strategy/BishopStrategyTest.java
@@ -0,0 +1,43 @@
+package strategy;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import coordinate.Coordinate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import position.Column;
+import position.Row;
+
+class BishopStrategyTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"4/3", "2/3"}, delimiter = '/')
+    @DisplayName("비숍은 위, 아래로 이동할 수 없다.")
+    void upDownTest(int row, int column) {
+        Coordinate coordinate = new Coordinate(new Row(3), new Column(3));
+        Coordinate destination = new Coordinate(new Row(row), new Column(column));
+
+        int rowDiff = coordinate.checkRow(destination);
+        int columnDiff = coordinate.checkColumn(destination);
+
+        assertThatThrownBy(() -> BishopStrategy.getMovingStrategy(rowDiff, columnDiff))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("invalid move");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3/2", "3/4"}, delimiter = '/')
+    @DisplayName("비숍은 좌우로 이동할 수 없다.")
+    void leftRightTest(int row, int column) {
+        Coordinate coordinate = new Coordinate(new Row(3), new Column(3));
+        Coordinate destination = new Coordinate(new Row(row), new Column(column));
+
+        int rowDiff = coordinate.checkRow(destination);
+        int columnDiff = coordinate.checkColumn(destination);
+
+        assertThatThrownBy(() -> BishopStrategy.getMovingStrategy(rowDiff, columnDiff))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("invalid move");
+    }
+}
