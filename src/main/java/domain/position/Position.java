b12aba9bd2062d63de6a822ae3cbf2eba39477ac
SCY <helenason@naver.com>
2024-03-20 16:43:18 +0900

    feat(Position): equals & hashCode 오버라이딩

--- src/main/java/domain/position/Position.java
+++ src/main/java/domain/position/Position.java
@@ -1,5 +1,7 @@
 package domain.position;
 
+import java.util.Objects;
+
 public class Position {
 
     private final File file;
@@ -8,5 +10,22 @@ public class Position {
     public Position(File file, Rank rank) {
         this.file = file;
         this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return file == position.file && rank == position.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(file, rank);
     }
 }
