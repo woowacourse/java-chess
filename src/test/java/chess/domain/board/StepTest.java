15938223a12cd63ba3ae18e10948e90e009f4064
zangsu <zangsu_@naver.com>
2024-03-20 13:48:19 +0900

    test : 테스트 추가

--- src/test/java/chess/domain/board/StepTest.java
+++ src/test/java/chess/domain/board/StepTest.java
@@ -1,10 +1,10 @@
 package chess.domain.board;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.EnumSource.Mode;
 
@@ -15,7 +15,7 @@ class StepTest {
     @EnumSource(value = Direction.class, names = {"UP", "RIGHT", "DOWN", "LEFT"})
     void isOrthogonalTest(Direction direction) {
         Step step = new Step(direction, SquareState.EMPTY);
-        Assertions.assertThat(step.isOrthogonal()).isTrue();
+        assertThat(step.isOrthogonal()).isTrue();
     }
 
 
@@ -26,7 +26,23 @@ class StepTest {
             mode = Mode.EXCLUDE)
     void isDiagonalTest(Direction direction) {
         Step step = new Step(direction, SquareState.EMPTY);
-        Assertions.assertThat(step.isDiagonal()).isTrue();
+        assertThat(step.isDiagonal()).isTrue();
+    }
+
+    @DisplayName("경로가 비었는지 확인할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"EMPTY, true", "ENEMY, false", "ALLY, false"})
+    void isEmptyTest(SquareState state, boolean expected) {
+        Step step = new Step(Direction.DOWN, state);
+        assertThat(step.isEmpty()).isEqualTo(expected);
+    }
+
+    @DisplayName("경로에 적이 있는지 확인할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"EMPTY, false", "ENEMY, true", "ALLY, false"})
+    void isEnemyTest(SquareState state, boolean expected) {
+        Step step = new Step(Direction.DOWN, state);
+        assertThat(step.isEnemy()).isEqualTo(expected);
     }
 }
 
