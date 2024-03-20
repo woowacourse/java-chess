efd3c19c56d9f3edadfedceb77c3537eb091ce31
hyxrxn <gpfls981220@gmail.com>
2024-03-20 22:34:12 +0900

    fix(Rook): 남쪽으로 이동할 수 있도록 수정
    Co-authored-by: leegwichan <cnddkscndgus@naver.com>

--- src/main/java/chess/domain/piece/Rook.java
+++ src/main/java/chess/domain/piece/Rook.java
@@ -3,14 +3,14 @@ package chess.domain.piece;
 import chess.domain.movement.EastMovement;
 import chess.domain.movement.MovementRule;
 import chess.domain.movement.NorthMovement;
-import chess.domain.movement.SouthEastMovement;
+import chess.domain.movement.SouthMovement;
 import chess.domain.movement.WestMovement;
 import java.util.List;
 
 public final class Rook extends chess.domain.piece.Piece {
 
     private static final List<MovementRule> MOVEMENT_RULES = List.of(
-            new EastMovement(), new WestMovement(), new SouthEastMovement(), new NorthMovement());
+            new EastMovement(), new WestMovement(), new SouthMovement(), new NorthMovement());
 
     public Rook(Team team) {
         super(team, MOVEMENT_RULES);
