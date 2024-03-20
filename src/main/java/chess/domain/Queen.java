48ab23f661eb8f259a3481c9b6db28df92341bb6
haiseong <wjdgotjd9908@gmail.com>
2024-03-20 13:52:27 +0900

    feat(Queen): 퀸 말 클래스 구현

---
+++ src/main/java/chess/domain/Queen.java
@@ -0,0 +1,33 @@
+package chess.domain;
+
+import java.util.Set;
+
+public class Queen extends Piece {
+    private static Set<Direction> directions = Direction.getEightDirection();
+
+    public Queen(Position position, Color color) {
+        super(position, color);
+    }
+
+    @Override
+    public Set<Position> findMovablePositions(Position destination) {
+        Direction direction = position.findDirectionTo(destination);
+        if (!directions.contains(direction)) {
+            throw new IllegalArgumentException("이동할 수 없습니다.");
+        }
+        return position.findCourses(direction, destination);
+    }
+
+    @Override
+    public Queen update(Position destination) {
+        return new Queen(destination, color);
+    }
+
+    @Override
+    public PieceType pieceType() {
+        if (color == Color.WHITE) {
+            return PieceType.WHITE_QUEEN;
+        }
+        return PieceType.BLACK_QUEEN;
+    }
+}
---
+++ src/test/java/chess/domain/QueenTest.java
@@ -0,0 +1,47 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class QueenTest {
+
+    @Test
+    @DisplayName("(1, 1) -> (?, 1)")
+    void findMovablePositionsRight() {
+        Queen queen = new Queen(new Position(1, 1), Color.WHITE);
+        Set<Position> movablePositions = queen.findMovablePositions(new Position(4, 1));
+        assertThat(movablePositions)
+                .containsExactlyInAnyOrder(new Position(2, 1), new Position(3, 1));
+    }
+
+    @Test
+    @DisplayName("(1, 1) -> (1, ?)")
+    void findMovablePositionsUp() {
+        Queen queen = new Queen(new Position(1, 1), Color.WHITE);
+        Set<Position> movablePositions = queen.findMovablePositions(new Position(1, 4));
+        assertThat(movablePositions)
+                .containsExactlyInAnyOrder(new Position(1, 2), new Position(1, 3));
+    }
+
+    @Test
+    @DisplayName("(1, 1) -> (2 + ?, 2 + ?)")
+    void findMovablePositionsDiagonal() {
+        Queen queen = new Queen(new Position(1, 1), Color.WHITE);
+        Set<Position> movablePositions = queen.findMovablePositions(new Position(4, 4));
+        assertThat(movablePositions)
+                .containsExactlyInAnyOrder(new Position(2, 2), new Position(3, 3));
+    }
+
+    @Test
+    @DisplayName("(1, 1) -> (2, 4) 예외 발생")
+    void findMovablePositionsByInvalidDestination() {
+        Queen queen = new Queen(new Position(1, 1), Color.WHITE);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> queen.findMovablePositions(new Position(2, 4)))
+                .withMessage("이동할 수 없는 방향입니다.");
+    }
+}
