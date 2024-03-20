37bd606e618e0000b50fc767fcd82467c773f6b4
robinjoon <robin980108@naver.com>
2024-03-20 17:12:36 +0900

    feat: 안내 문구 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,6 +8,13 @@ import domain.PieceType;
 import domain.Team;
 
 public class OutputView {
+	public void printGuide() {
+		System.out.printf("> 체스 게임을 시작합니다.%n"
+			+ "> 게임 시작 : start%n"
+			+ "> 게임 종료 : end%n"
+			+ "> 게임 이동 : move source위치 target위치 - 예. move b2 b3%n");
+	}
+
 	public void printChessBoard(List<Piece> pieces) {
 		List<List<Character>> boardStatus = new ArrayList<>(8);
 		for (int i = 0; i < 9; i++) {
