17d2d372eaf50372a54d896d56406fd1c141ad51
libienz <leekh10195@gmail.com>
2024-03-21 08:19:59 +0900

    feat: 팀 열거형 도메인에 속성 추가 구현

--- src/main/java/chess/domain/piece/Team.java
+++ src/main/java/chess/domain/piece/Team.java
@@ -4,15 +4,18 @@ import static chess.domain.position.Direction.DOWN;
 import static chess.domain.position.Direction.UP;
 
 import chess.domain.position.Direction;
+import chess.domain.position.RowPosition;
 
 public enum Team {
-    WHITE(UP),
-    BLACK(DOWN);
+    WHITE(UP, RowPosition.SIX),
+    BLACK(DOWN, RowPosition.ONE);
 
     private final Direction direction;
+    private final RowPosition initialPawnRow;
 
-    Team(Direction direction) {
+    Team(Direction direction, RowPosition initialPawnRow) {
         this.direction = direction;
+        this.initialPawnRow = initialPawnRow;
     }
 
     public Direction getDirection() {
