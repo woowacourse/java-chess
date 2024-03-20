5037efa7a48fe773bf4c61e2a6d0b96886697801
jongmee <ra123221@gmail.com>
2024-03-20 17:42:03 +0900

    feat: 게임 명령어 입력 문구 변경

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,5 +1,7 @@
 package chess.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -7,8 +9,10 @@ public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
     public GameCommand readGameCommand() {
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
         String command = scanner.nextLine();
         validate(command);
         return GameCommand.from(command);
