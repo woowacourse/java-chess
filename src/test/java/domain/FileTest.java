d539ace1736444469bcbe7acc437c7e78c02a1f1
yechop <channetattoo@gmail.com>
2024-03-20 15:43:41 +0900

    fix: 랭크, 파일 필드 넘버링 시작 숫자 변경에 따른 코드 수정

--- src/test/java/domain/FileTest.java
+++ src/test/java/domain/FileTest.java
@@ -11,21 +11,21 @@ class FileTest {
     @DisplayName("가장 높은 순서 값을 찾는다.")
     @Test
     void maxTest() {
-        assertThat(File.max()).isEqualTo(7);
+        assertThat(File.max()).isEqualTo(8);
     }
 
     @DisplayName("순서를 받아 열을 찾는다.")
     @Test
     void findTest() {
         Assertions.assertAll(
-                () -> assertThat(File.find(0)).isEqualTo(File.A),
-                () -> assertThat(File.find(1)).isEqualTo(File.B),
-                () -> assertThat(File.find(2)).isEqualTo(File.C),
-                () -> assertThat(File.find(3)).isEqualTo(File.D),
-                () -> assertThat(File.find(4)).isEqualTo(File.E),
-                () -> assertThat(File.find(5)).isEqualTo(File.F),
-                () -> assertThat(File.find(6)).isEqualTo(File.G),
-                () -> assertThat(File.find(7)).isEqualTo(File.H)
+                () -> assertThat(File.find(1)).isEqualTo(File.A),
+                () -> assertThat(File.find(2)).isEqualTo(File.B),
+                () -> assertThat(File.find(3)).isEqualTo(File.C),
+                () -> assertThat(File.find(4)).isEqualTo(File.D),
+                () -> assertThat(File.find(5)).isEqualTo(File.E),
+                () -> assertThat(File.find(6)).isEqualTo(File.F),
+                () -> assertThat(File.find(7)).isEqualTo(File.G),
+                () -> assertThat(File.find(8)).isEqualTo(File.H)
         );
     }
 }
--- src/test/java/domain/RankTest.java
+++ src/test/java/domain/RankTest.java
@@ -11,21 +11,21 @@ class RankTest {
     @DisplayName("가장 높은 순서 값을 찾는다.")
     @Test
     void maxTest() {
-        assertThat(Rank.max()).isEqualTo(7);
+        assertThat(Rank.max()).isEqualTo(8);
     }
 
     @DisplayName("순서를 받아 열을 찾는다.")
     @Test
     void findTest() {
         Assertions.assertAll(
-                () -> assertThat(Rank.find(0)).isEqualTo(Rank.ONE),
-                () -> assertThat(Rank.find(1)).isEqualTo(Rank.TWO),
-                () -> assertThat(Rank.find(2)).isEqualTo(Rank.THREE),
-                () -> assertThat(Rank.find(3)).isEqualTo(Rank.FOUR),
-                () -> assertThat(Rank.find(4)).isEqualTo(Rank.FIVE),
-                () -> assertThat(Rank.find(5)).isEqualTo(Rank.SIX),
-                () -> assertThat(Rank.find(6)).isEqualTo(Rank.SEVEN),
-                () -> assertThat(Rank.find(7)).isEqualTo(Rank.EIGHT)
+                () -> assertThat(Rank.find(1)).isEqualTo(Rank.ONE),
+                () -> assertThat(Rank.find(2)).isEqualTo(Rank.TWO),
+                () -> assertThat(Rank.find(3)).isEqualTo(Rank.THREE),
+                () -> assertThat(Rank.find(4)).isEqualTo(Rank.FOUR),
+                () -> assertThat(Rank.find(5)).isEqualTo(Rank.FIVE),
+                () -> assertThat(Rank.find(6)).isEqualTo(Rank.SIX),
+                () -> assertThat(Rank.find(7)).isEqualTo(Rank.SEVEN),
+                () -> assertThat(Rank.find(8)).isEqualTo(Rank.EIGHT)
         );
     }
 }
