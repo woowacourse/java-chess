e0c24281fc9e8115657fb27dc0eeb172fd40f5d6
J-I-H-O <jeongjiho0731@gmail.com>
2024-03-20 15:16:03 +0900

    refactor: 체스판 출력 로직 클래스 분리
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -8,28 +8,8 @@ import java.util.List;
 public class OutputView {
 
     public void printChessBoard(final ChessBoard chessBoard) {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        List<Piece> values = chessBoard.findAllPieces(); //이놈은 게터가 있다!! (똥)(똥)(똥)(똥)(똥)(똥)(똥)
-
-        int idx = 1;
-        for (Piece value : values) {
-            stringBuilder.append(PieceExpression.mapToExpression(value));
-            if (idx % 8 == 0) {
-                stringBuilder.append(System.lineSeparator());
-            }
-            idx++;
-        }
-
-        System.out.println(stringBuilder);
-
-        // TODO: 고민
-//        for (char rank = 'a'; rank <= 'h'; rank++) { // 범위를 공개! 이건 캡슐화 노노(똥)
-//            for (int file = 1; file <= 8; file++) {
-//                Piece piece = chessBoard.findBySquare(Square.of(rank, file));
-//                System.out.print("이넘.of(value)");
-//            }
-//            System.out.println();
-//        }
+        List<Piece> pieces = chessBoard.findAllPieces();
+        String chessBoardExpression = ChessBoardExpression.toExpression(pieces);
+        System.out.println(chessBoardExpression);
     }
 }
