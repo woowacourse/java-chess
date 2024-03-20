349afa859e2ed399e29f742e81382fe2ada2cc71
woojong <dwax1324@gmail.com>
2024-03-20 15:44:25 +0900

    feat: 파일은 각 기물의 초기 포지션을 반환할 수 있다

--- src/main/java/domain/piece/info/File.java
+++ src/main/java/domain/piece/info/File.java
@@ -1,5 +1,8 @@
 package domain.piece.info;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum File {
     A(0),
     B(1),
@@ -19,4 +22,34 @@ public enum File {
     public int index() {
         return index;
     }
+
+    public static List<File> rookPosition() {
+        return List.of(A, H);
+    }
+
+    public static List<File> knightPosition() {
+        return List.of(B, G);
+    }
+
+    public static List<File> bishopPosition() {
+        return List.of(C, F);
+    }
+
+    public static List<File> pawnPosition() {
+        return Arrays.asList(values());
+    }
+
+    public static List<File> kingPosition() {
+        return List.of(E);
+    }
+
+    public static List<File> nonePosition() {
+        return Arrays.asList(values());
+    }
+
+    public static List<File> queenPosition() {
+        return List.of(D);
+    }
+
+
 }
