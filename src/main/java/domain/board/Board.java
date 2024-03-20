ac18eeac3c9e11ab99e480a109bc5df29e5a1435
SCY <helenason@naver.com>
2024-03-20 17:15:22 +0900

    feat(Board): 기물의 이동 기능

--- src/main/java/domain/board/Board.java
+++ src/main/java/domain/board/Board.java
@@ -1,6 +1,8 @@
 package domain.board;
 
+import domain.piece.Color;
 import domain.piece.Piece;
+import domain.piece.Type;
 import domain.position.Position;
 import java.util.List;
 import java.util.Map;
@@ -25,5 +27,13 @@ public class Board {
 
     public Piece findPieceByPosition(Position position) {
         return squares.get(position);
+    }
+
+    public void placePieceByPosition(Piece piece, Position position) {
+        squares.replace(position, piece);
+    }
+
+    public void displacePieceByPosition(Position position) {
+        squares.replace(position, new Piece(Type.NONE, Color.NONE));
     }
 }
--- src/test/java/domain/board/BoardTest.java
+++ src/test/java/domain/board/BoardTest.java
@@ -8,19 +8,49 @@ import domain.piece.Type;
 import domain.position.File;
 import domain.position.Position;
 import domain.position.Rank;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = Board.create();
+    }
 
     @Test
     @DisplayName("해당 위치의 기물을 가져온다.")
     void findPieceByPosition() {
         Position position = new Position(File.A, Rank.ONE);
-        Board board = Board.create();
 
         Piece piece = board.findPieceByPosition(position);
 
         assertThat(piece).isEqualTo(new Piece(Type.ROOK, Color.WHITE));
+    }
+
+    @Test
+    @DisplayName("해당 위치에 기물을 둔다.")
+    void placePiece() {
+        Position position = new Position(File.A, Rank.ONE);
+        Piece expected = new Piece(Type.ROOK, Color.WHITE);
+
+        board.placePieceByPosition(expected, position);
+
+        Piece actual = board.findPieceByPosition(position);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("해당 위치의 기물을 없앤다.")
+    void displacePiece() {
+        Position position = new Position(File.A, Rank.ONE);
+
+        board.displacePieceByPosition(position);
+
+        Piece actual = board.findPieceByPosition(position);
+        Piece expected = new Piece(Type.NONE, Color.NONE);
+        assertThat(actual).isEqualTo(expected);
     }
 }
