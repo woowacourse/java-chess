f195cdaafa7c0dfd28037ec7e6e4b5ad707a2b6e
zangsu <zangsu_@naver.com>
2024-03-20 13:57:47 +0900

    feat : 기물 클래스 생성

---
+++ src/main/java/chess/domain/piece/Piece.java
@@ -0,0 +1,13 @@
+package chess.domain.piece;
+
+import chess.domain.board.Path;
+
+public abstract class Piece {
+    private final Color color;
+
+    protected Piece(Color color) {
+        this.color = color;
+    }
+
+    abstract boolean canMove(Path path);
+}
