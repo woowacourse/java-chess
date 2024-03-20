a3119854dcca704f623eaaba765ffdc6aaf1b43e
seunghye218 <seunghye@student.42seoul.kr>
2024-03-20 14:42:54 +0900

    feat: Players 생성 및 ChessGame 에 추가
    Co-authored-by: jcoding-play <namejgc@naver.com>

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -1,14 +1,19 @@
 package chess.domain;
 
 import chess.domain.piece.Piece;
+import chess.domain.piece.Team;
+import chess.domain.player.Player;
+import chess.domain.player.Players;
 import java.util.Map;
 
 public class ChessGame {
 
     private final Board board;
+    private final Players players;
 
     public ChessGame(Map<Point, Piece> board) {
         this.board = new Board(board);
+        this.players = new Players(new Player(Team.WHITE), new Player(Team.BLACK));
     }
 
     public Map<Point, Piece> getBoard() {
---
+++ src/main/java/chess/domain/player/Players.java
@@ -0,0 +1,12 @@
+package chess.domain.player;
+
+public class Players {
+
+    private final Player white;
+    private final Player black;
+
+    public Players(Player white, Player black) {
+        this.white = white;
+        this.black = black;
+    }
+}
