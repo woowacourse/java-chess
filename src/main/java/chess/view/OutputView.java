89eb436aa9143483c2d68befbfbc66e9a5963f1c
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-03-20 19:47:22 +0900

    refactor(OutputView): 함수 분리

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -6,21 +6,31 @@ import java.util.Map;
 
 public class OutputView {
     public static void printStartMessage() {
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
     public static void printChessBoard(Map<Position, Character> piecesCharacter) {
-        for (int i = 8; i >= 1; i--) {
-            for (int j = 1; j <= 8; j++) {
-                if (piecesCharacter.containsKey(Position.of(i, j))) {
-                    Character character = piecesCharacter.get(Position.of(i, j));
-                    System.out.print(CharacterViewer.convertToString(character));
-                    continue;
-                }
-                System.out.print(".");
-            }
-            System.out.println();
+        for (int row = 8; row >= 1; row--) {
+            printChessRow(piecesCharacter, row);
         }
+        System.out.println();
+    }
+
+    private static void printChessRow(Map<Position, Character> piecesCharacter, int row) {
+        for (int column = 1; column <= 8; column++) {
+            System.out.print(pieceToString(piecesCharacter, Position.of(row, column)));
+        }
+        System.out.println();
+    }
+
+    private static String pieceToString(Map<Position, Character> piecesCharacter, Position position) {
+        if (piecesCharacter.containsKey(position)) {
+            Character character = piecesCharacter.get(position);
+            return CharacterViewer.convertToString(character);
+        }
+        return ".";
     }
 }
