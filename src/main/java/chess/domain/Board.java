1a0c33d4dd8b243492c9e52980794d3117eeb573
BurningFalls <sherlocky31415@gmail.com>
2024-03-20 14:17:23 +0900

    feat: Empty Piece 로 Board 를 초기화하는 기능 구현

--- src/main/java/chess/domain/Board.java
+++ src/main/java/chess/domain/Board.java
@@ -1,7 +1,11 @@
 package chess.domain;
 
+import chess.domain.piece.EmptyPiece;
 import chess.domain.piece.Piece;
+import chess.domain.strategy.EmptyMoveStrategy;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
@@ -14,6 +18,26 @@ public class Board {
     public static Board initialize() {
         Map<Position, Piece> board = new HashMap<>();
 
+        List<String> locations = createLocations();
+        for (String location : locations) {
+            Position position = Position.of(location);
+            PieceInfo pieceInfo = new PieceInfo(position, Team.NONE);
+            EmptyPiece emptyPiece = new EmptyPiece(pieceInfo, new EmptyMoveStrategy());
+            board.put(position, emptyPiece);
+        }
+
         return new Board(board);
+    }
+
+    private static List<String> createLocations() {
+        List<String> positions = new ArrayList<>();
+
+        for (int i = 0; i < 64; i++) {
+            char ch1 = (char) (i / 8 + 'a');
+            char ch2 = (char) (i % 8 + '1');
+            positions.add("" + ch1 + ch2);
+        }
+
+        return positions;
     }
 }
