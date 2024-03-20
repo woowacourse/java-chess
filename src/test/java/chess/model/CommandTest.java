5e785338529cfc55d5437c834305e8130d782360
masonkimseoul <masonkimseoul@gmail.com>
2024-03-20 14:50:25 +0900

    test: 올바른 게임 명령어가 들어오면 해당 Command 상수를 반환한다
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

--- src/test/java/chess/model/CommandTest.java
+++ src/test/java/chess/model/CommandTest.java
@@ -1,8 +1,11 @@
 package chess.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,5 +17,14 @@ class CommandTest {
         assertThatThrownBy(() -> Command.findCommand(command))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("올바르지 않은 게임 명령어입니다.");
+    }
+
+    @DisplayName("올바른 게임 명령어가 들어오면 해당 Command 상수를 반환한다")
+    @Test
+    void findValidGameCommand() {
+        assertAll(
+                () -> assertThat(Command.findCommand("start")).isEqualTo(Command.START),
+                () -> assertThat(Command.findCommand("end")).isEqualTo(Command.END)
+        );
     }
 }
