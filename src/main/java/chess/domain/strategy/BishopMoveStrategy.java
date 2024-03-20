a4262b25e3c75488f3331a11e472e32cf04380d6
BurningFalls <sherlocky31415@gmail.com>
2024-03-20 14:18:13 +0900

    refactor: 개행 수정

--- src/main/java/chess/domain/strategy/BishopMoveStrategy.java
+++ src/main/java/chess/domain/strategy/BishopMoveStrategy.java
@@ -4,6 +4,7 @@ import chess.domain.Position;
 import chess.domain.PositionDifference;
 
 public class BishopMoveStrategy implements MoveStrategy {
+    
     @Override
     public boolean canMove(Position currentPosition, Position newPosition) {
         PositionDifference positionDifference = currentPosition.calculateDifference(newPosition);
--- src/main/java/chess/domain/strategy/MoveStrategy.java
+++ src/main/java/chess/domain/strategy/MoveStrategy.java
@@ -2,7 +2,6 @@ package chess.domain.strategy;
 
 import chess.domain.Position;
 
-
 public interface MoveStrategy {
     boolean canMove(Position currentPosition, Position newPosition);
 }
