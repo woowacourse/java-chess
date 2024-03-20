e0bc209915d8de3cc39429c27a0cb286165140f3
libienz <leekh10195@gmail.com>
2024-03-20 22:43:09 +0900

    feat: Team 열거형이 전진 방향을 관리하는 기능 추가

--- src/main/java/chess/domain/piece/Team.java
+++ src/main/java/chess/domain/piece/Team.java
@@ -1,6 +1,21 @@
 package chess.domain.piece;
 
+import static chess.domain.position.Direction.DOWN;
+import static chess.domain.position.Direction.UP;
+
+import chess.domain.position.Direction;
+
 public enum Team {
-    WHITE,
-    BLACK;
+    WHITE(UP),
+    BLACK(DOWN);
+
+    private final Direction direction;
+
+    Team(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
 }
