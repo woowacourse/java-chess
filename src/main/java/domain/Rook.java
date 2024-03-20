a25c6f143ebece3709d86ccbaa46a552c5143ba4
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-20 21:58:04 +0900

    feat(Rook) : 룩 기물이 이동 가능한지 확인하는 기능 구현

--- src/main/java/domain/Rook.java
+++ src/main/java/domain/Rook.java
@@ -1,9 +1,16 @@
 package domain;
 
 import domain.piece.attribute.Color;
+import domain.piece.attribute.point.Direction;
 import domain.piece.attribute.point.Point;
+import factory.DirectionFactory;
+
+import java.util.List;
 
 public class Rook extends Piece {
+    private static final List<Direction> rookList =
+            List.of(Direction.UP, Direction.DOWN, Direction.RIGHT, Direction.LEFT);
+
     public Rook(final Point point, final Color color) {
         super(point, color);
     }
@@ -11,5 +18,10 @@ public class Rook extends Piece {
     @Override
     public PieceStatus getStatus() {
         return PieceStatus.ROOK;
+    }
+
+    public boolean canMove(final Point point) {
+        final Direction direction = DirectionFactory.generate(this.getPoint(), point);
+        return rookList.contains(direction);
     }
 }
