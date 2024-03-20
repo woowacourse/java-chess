2cc6d7fe3d6d6764fdde26f8de0cf9c2d06b9d15
masonkimseoul <masonkimseoul@gmail.com>
2024-03-20 14:49:05 +0900

    feat: 올바르지 않은 게임 명령어가 들어오면 예외가 발생한다
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

---
+++ src/main/java/chess/model/Command.java
@@ -0,0 +1,21 @@
+package chess.model;
+
+import java.util.Arrays;
+
+public enum Command {
+    START("start"),
+    END("end");
+
+    private final String displayName;
+
+    Command(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static Command findCommand(String displayName) {
+        return Arrays.stream(values())
+                .filter(command -> command.displayName.equals(displayName))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 게임 명령어입니다."));
+    }
+}
