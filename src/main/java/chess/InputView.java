960c1e3d3498704b9ec75c42b6a1853afeea0184
tackyu <tack970913@gmail.com>
2024-03-20 14:22:39 +0900

    refactor : 게임 시작 안내 문구 출력 뷰로 이동

--- src/main/java/chess/InputView.java
+++ src/main/java/chess/InputView.java
@@ -6,8 +6,6 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String readCommand(){
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
         return scanner.nextLine();
     }
 }
--- src/main/java/chess/OutputView.java
+++ src/main/java/chess/OutputView.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
 
 public class OutputView {
 
-    public static void printChessBoard(Map<Column, List<ChessPiece>> chessBoard){
+    public static void printChessBoard(Map<Column, List<ChessPiece>> chessBoard) {
         chessBoard.values()
                 .forEach(OutputView::printOneRow);
     }
@@ -14,5 +14,10 @@ public class OutputView {
     private static void printOneRow(List<ChessPiece> row) {
         row.forEach(chessPiece -> System.out.print(chessPiece.getName()));
         System.out.println();
+    }
+
+    public static void printStartMessage() {
+        System.out.println("체스 게임을 시작합니다.");
+        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
     }
 }
