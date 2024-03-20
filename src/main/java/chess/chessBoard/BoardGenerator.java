42d9bc69e07be44531a5ffdfcc03f1baa3dafcee
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-20 20:55:30 +0900

    refactor(BoardGenerator): 체스판위에 말을 올려서 생성하도록 변경
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

--- src/main/java/chess/chessBoard/BoardGenerator.java
+++ src/main/java/chess/chessBoard/BoardGenerator.java
@@ -1,21 +1,68 @@
-package chess.chessBoard;
+package chess.chessboard;
 
-import java.util.ArrayList;
+import chess.chesspiece.ChessPiece;
+import chess.chesspiece.ChessPieceGenerator;
+import chess.chesspiece.ChessPieceStartingPosition;
+import chess.chesspiece.ChessPieceType;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 public class BoardGenerator {
 
-    private static final List<Square> board = createBoard();
+    private static final BoardGenerator INSTANCE = new BoardGenerator();
+    private static final Map<Square, Optional<ChessPiece>> CHESS_BOARD_CACHE = createBoard();
 
-    private static List<Square> createBoard() {
+    private BoardGenerator() {
+    }
+
+    public static BoardGenerator getInstance() {
+        return INSTANCE;
+    }
+
+    public Map<Square, Optional<ChessPiece>> generate() {
+        return new LinkedHashMap<>(CHESS_BOARD_CACHE);
+    }
+
+    private static Map<Square, Optional<ChessPiece>> createBoard() {
+        List<Square> squares = createSquares();
+        Map<Square, Optional<ChessPiece>> chessBoard = new LinkedHashMap<>();
+        putChessPieceOnBoard(squares, chessBoard);
+
+        return chessBoard;
+    }
+
+    private static List<Square> createSquares() {
         return Arrays.stream(Lettering.values())
                 .flatMap(lettering -> Arrays.stream(Numbering.values())
                         .map(numbering -> new Square(lettering, numbering)))
                 .toList();
     }
 
-    public static List<Square> generate() {
-        return new ArrayList<>(board);
+    private static void putChessPieceOnBoard(List<Square> squares, Map<Square, Optional<ChessPiece>> chessBoard) {
+        for (Square square : squares) {
+            Optional<ChessPiece> chessPiece = createChessPieceForSquare(square);
+            chessBoard.put(square, chessPiece);
+        }
+    }
+
+    private static Optional<ChessPiece> createChessPieceForSquare(Square square) {
+        ChessPieceGenerator chessPieceGenerator = ChessPieceGenerator.getInstance();
+
+        ChessPieceType chessPieceType = determineChessPieceType(square);
+        if (chessPieceType != null) {
+            ChessPiece createdChessPiece = chessPieceGenerator.generate(chessPieceType, square.getNumbering());
+            return Optional.of(createdChessPiece);
+        }
+
+        return Optional.empty();
+    }
+
+    private static ChessPieceType determineChessPieceType(Square square) {
+        ChessPieceStartingPosition chessPieceStartingPosition = ChessPieceStartingPosition.getInstance();
+        return chessPieceStartingPosition.determineChessPieceType(square).orElse(null);
     }
 }
--- src/test/java/chess/chessBoard/BoardGeneratorTest.java
+++ src/test/java/chess/chessBoard/BoardGeneratorTest.java
@@ -1,9 +1,11 @@
-package chess.chessBoard;
+package chess.chessboard;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import chess.chesspiece.ChessPiece;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -12,25 +14,55 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class BoardGeneratorTest {
 
+    BoardGenerator boardGenerator = BoardGenerator.getInstance();
+
     @Test
     void _64개의_칸을_가진_보드를_생성한다() {
-        //given
-        List<Square> board = BoardGenerator.generate();
+        //given, when
+        Map<Square, Optional<ChessPiece>> board = boardGenerator.generate();
 
-        //when, then
+        //then
         assertThat(board).hasSize(64);
     }
 
     @Test
     void 보드의_칸은_중복되지_않는다() {
         //given
-        List<Square> board = BoardGenerator.generate();
+        Map<Square, Optional<ChessPiece>> board = boardGenerator.generate();
 
         //when
         int boardSize = board.size();
-        Set<Square> notDuplicatedBoard = new HashSet<>(board);
+        Set<Square> squares = board.keySet();
 
         //then
-        assertThat(notDuplicatedBoard).hasSize(boardSize);
+        assertThat(squares).hasSize(boardSize);
+    }
+
+    @Test
+    void 생성된_보드에_말은_32개가_올려져_있어야_한다() {
+        //given
+        Map<Square, Optional<ChessPiece>> board = boardGenerator.generate();
+
+        //when
+        int chessPieceCount = (int) board.keySet().stream()
+                .filter(square -> board.get(square).isPresent())
+                .count();
+
+        //then
+        assertThat(chessPieceCount).isEqualTo(32);
+    }
+
+    @Test
+    void 생성된_보드에_말이_없는_칸은_32개이어야_한다() {
+        //given
+        Map<Square, Optional<ChessPiece>> board = boardGenerator.generate();
+
+        //when
+        int chessPieceCount = (int) board.keySet().stream()
+                .filter(square -> board.get(square).isEmpty())
+                .count();
+
+        //then
+        assertThat(chessPieceCount).isEqualTo(32);
     }
 }
