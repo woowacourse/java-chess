73e3dc8317fada12e474d7901c3ecba6a7aa93c6
libienz <leekh10195@gmail.com>
2024-03-20 21:13:51 +0900

    feat: King 행마법 구현

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -3,13 +3,16 @@ package chess.domain.piece;
 import chess.domain.position.Position;
 
 public class King extends Piece {
+    private static final int STRAIGHT_DISTANCE = 1;
+    private static final int DIAGONAL_DISTANCE = 2;
+
     public King(Team team) {
         super(team);
     }
 
     @Override
     public boolean canMove(Position start, Position destination) {
-        //TODO: 킹 움직임 전략 구현 필요
-        return false;
+        int squaredDistance = start.squaredDistanceWith(destination);
+        return squaredDistance == STRAIGHT_DISTANCE || squaredDistance == DIAGONAL_DISTANCE;
     }
 }
