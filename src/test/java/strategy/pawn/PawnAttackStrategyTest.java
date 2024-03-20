935fb8f710360185e99941b1077738a36d1c5fe4
Eugene Jang <kyunellroll@gmail.com>
2024-03-20 17:16:24 +0900

    feat(PawnAttackStrategy): 폰이 공격할 때 가능한 전략을 구
    - 대각선을 갈 수 있도록 구현
    - 존재하는 전략이 없다면 에러를 발생하는지 테스트
    
    Co-authored-by: pricelees <priceelees@gmail.com>

---
+++ src/test/java/strategy/pawn/PawnAttackStrategyTest.java
@@ -0,0 +1,24 @@
+package strategy.pawn;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PawnAttackStrategyTest {
+
+    @DisplayName("존재하는 폰의 전략이 없다면 에러를 발생한다.")
+    @Test
+    void notExistMoveStrategy() {
+        Assertions.assertThatThrownBy(() -> PawnAttackStrategy.getMoveStrategy(3, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하는 움직임 전략이 없습니다.");
+    }
+
+    @DisplayName("폰은 뒤로 갈 수 없다.")
+    @Test
+    void pawnCanNotGoBack() {
+        Assertions.assertThatThrownBy(() -> PawnAttackStrategy.DOWN_RIGHT_DIAGONAL.validatePossibleStrategyColor(false))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("폰은 뒤로 갈 수 없습니다.");
+    }
+}
