c18f307f2c1edb83517a6af398268aa639c3e2e8
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-20 22:15:38 +0900

    feat(Queen) : 퀸 기물이 이동 가능한지 확인하는 기능 구현
    - 이에 따른 테스트 코드 추가

--- src/main/java/domain/Queen.java
+++ src/main/java/domain/Queen.java
@@ -1,9 +1,15 @@
 package domain;
 
 import domain.piece.attribute.Color;
+import domain.piece.attribute.point.Direction;
 import domain.piece.attribute.point.Point;
+import factory.DirectionFactory;
+
+import java.util.List;
 
 public class Queen extends Piece {
+    private static final List<Direction> directionList = Direction.all();
+
     public Queen(final Point point, final Color color) {
         super(point, color);
     }
@@ -11,5 +17,10 @@ public class Queen extends Piece {
     @Override
     public PieceStatus getStatus() {
         return PieceStatus.QUEEN;
+    }
+
+    public boolean canMove(final Point point) {
+        final Direction direction = DirectionFactory.generate(this.getPoint(), point);
+        return directionList.contains(direction);
     }
 }
