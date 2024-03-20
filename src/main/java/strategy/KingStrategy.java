3ecb038857d5e4a35a180e1e87412ac63c2860a8
Eugene Jang <kyunellroll@gmail.com>
2024-03-20 17:41:45 +0900

    feat(KingStrategy): King 의 움직임 구현
    - 킹은 한 칸만 이동할 수 있는지 테스트
    
    Co-authored-by: pricelees <priceelees@gmail.com>

---
+++ src/main/java/strategy/KingStrategy.java
@@ -0,0 +1,38 @@
+package strategy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum KingStrategy {
+    
+    UP(-1, 0, List.of(-1, 0)),
+    DOWN(1, 0, List.of(1, 0)),
+    LEFT(0, -1, List.of(0, -1)),
+    RIGHT(0, 1, List.of(0, 1)),
+    UP_LEFT_DIAGONAL(-1, -1, List.of(-1, -1)),
+    UP_RIGHT_DIAGONAL(-1, 1, List.of(-1, 1)),
+    DOWN_LEFT_DIAGONAL(1, -1, List.of(1, -1)),
+    DOWN_RIGHT_DIAGONAL(1, 1, List.of(1, 1));
+
+    private final int rowDifference;
+    private final int columnDifference;
+    private final List<Integer> direction;
+
+    KingStrategy(int rowDifference, int columnDifference, List<Integer> direction) {
+        this.rowDifference = rowDifference;
+        this.columnDifference = columnDifference;
+        this.direction = direction;
+    }
+
+    public static KingStrategy getMovingStrategy(int rowDifference, int columnDifference) {
+        return Arrays.stream(KingStrategy.values())
+                .filter(strategy -> strategy.rowDifference == rowDifference
+                        && strategy.columnDifference == columnDifference)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("invalid move"));
+    }
+
+    public List<Integer> getDirection() {
+        return this.direction;
+    }
+}
---
+++ src/test/java/strategy/KingStrategyTest.java
@@ -0,0 +1,28 @@
+package strategy;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import coordinate.Coordinate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import position.Column;
+import position.Row;
+
+class KingStrategyTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"0/2", "2/4", "0/0"}, delimiter = '/')
+    @DisplayName("킹은 한 칸만 이동할 수 있다.")
+    void invalidMoveTest(int row, int column) {
+        Coordinate coordinate = new Coordinate(new Row(2), new Column(2));
+        Coordinate destination = new Coordinate(new Row(row), new Column(column));
+
+        int rowDiff = coordinate.checkRow(destination);
+        int columnDiff = coordinate.checkColumn(destination);
+
+        assertThatThrownBy(() -> KingStrategy.getMovingStrategy(rowDiff, columnDiff))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("invalid move");
+    }
+}
