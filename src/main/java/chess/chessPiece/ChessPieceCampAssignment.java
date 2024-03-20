cb9d823d001d2ad9e1f034b8eb14cd5c81235603
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-20 19:29:29 +0900

    feat(ChessPieceCampAssignment): 도메인 구현
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

---
+++ src/main/java/chess/chessPiece/ChessPieceCampAssignment.java
@@ -0,0 +1,25 @@
+package chess.chessPiece;
+
+import chess.chessBoard.Numbering;
+
+public class ChessPieceCampAssignment {
+
+    private static final ChessPieceCampAssignment INSTANCE = new ChessPieceCampAssignment();
+
+    private ChessPieceCampAssignment() {
+    }
+
+    public static ChessPieceCampAssignment getInstance() {
+        return INSTANCE;
+    }
+
+    public Camp determineCamp(Numbering numbering) {
+        if (numbering == Numbering.ONE || numbering == Numbering.TWO) {
+            return Camp.WHITE;
+        }
+        if (numbering == Numbering.SEVEN || numbering == Numbering.EIGHT) {
+            return Camp.BLACK;
+        }
+        throw new IllegalArgumentException("[ERROR] 진영 배정에 유효한 위치가 아닙니다.");
+    }
+}
---
+++ src/test/java/chess/chessPiece/ChessPieceCampAssignmentTest.java
@@ -0,0 +1,53 @@
+package chess.chessPiece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import chess.chessBoard.Numbering;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ChessPieceCampAssignmentTest {
+
+    ChessPieceCampAssignment instance = ChessPieceCampAssignment.getInstance();
+
+    @Test
+    void 넘버링이_1_또는_2인_경우_진영은_흰색이다() {
+        //given, when
+        Camp camp1 = instance.determineCamp(Numbering.ONE);
+        Camp camp2 = instance.determineCamp(Numbering.TWO);
+
+        //then
+        assertAll(
+                () -> assertThat(camp1).isEqualTo(Camp.WHITE),
+                () -> assertThat(camp2).isEqualTo(Camp.WHITE)
+        );
+    }
+
+    @Test
+    void 넘버링이_7_또는_8인_경우_진영은_검정색이다() {
+        //given, when
+        Camp camp1 = instance.determineCamp(Numbering.SEVEN);
+        Camp camp2 = instance.determineCamp(Numbering.EIGHT);
+
+        //then
+        assertAll(
+                () -> assertThat(camp1).isEqualTo(Camp.BLACK),
+                () -> assertThat(camp2).isEqualTo(Camp.BLACK)
+        );
+    }
+
+    @Test
+    void 넘버링이_3_4_5_6인_경우_예외를_발생시킨다() {
+        //given, when, then
+        assertAll(
+                () -> assertThatThrownBy(() -> instance.determineCamp(Numbering.THREE)),
+                () -> assertThatThrownBy(() -> instance.determineCamp(Numbering.FOUR)),
+                () -> assertThatThrownBy(() -> instance.determineCamp(Numbering.FIVE)),
+                () -> assertThatThrownBy(() -> instance.determineCamp(Numbering.SIX))
+        );
+    }
+}
