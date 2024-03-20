850a90390edc2add248caae455546bf0e682f6c5
masonkimseoul <masonkimseoul@gmail.com>
2024-03-20 21:38:52 +0900

    feat: 퀸 상하좌우 대각선 전진 기능 구현
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

--- src/main/java/chess/model/piece/Queen.java
+++ src/main/java/chess/model/piece/Queen.java
@@ -10,6 +10,18 @@ public class Queen extends Piece {
 
     @Override
     public boolean canMove(Position source, Position target) {
+        int rowDifference = Math.abs(calculateRowDifference(source, target));
+        int columnDifference = Math.abs(calculateColumnDifference(source, target));
+
+        if (rowDifference == columnDifference) {
+            return true;
+        }
+        if (rowDifference > 0 && columnDifference == 0) {
+            return true;
+        }
+        if (rowDifference == 0 && columnDifference > 0) {
+            return true;
+        }
         return false;
     }
 }
