87a3b5d65622ca593a7ca7e242ffb77a107f87a0
tackyu <tack970913@gmail.com>
2024-03-20 14:20:26 +0900

    feat(Command) : 명령어 검증 기능 구현

---
+++ src/main/java/chess/Command.java
@@ -0,0 +1,38 @@
+package chess;
+
+import java.util.Arrays;
+
+public enum Command {
+    START("start"),
+    END("end");
+
+    private final String command;
+    Command(String command) {
+        this.command=command;
+    }
+
+    public static Command getStartCommand(String command) {
+        if(!START.command.equals(command)) {
+            throw new IllegalArgumentException("첫 명령어는 start만 입력할 수 있습니다.");
+        }
+        return START;
+    }
+
+    //TODO : 리팩토링
+    public static Command getProcessCommand(String command) {
+        Command inputCommand = Arrays.stream(values())
+                .filter(value -> value.command.equals(command))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 명령어입니다."));
+
+        if(inputCommand == START) {
+            throw new IllegalArgumentException("게임 중에 start 명령어는 사용할 수 없습니다.");
+        }
+
+        return inputCommand;
+    }
+
+    public boolean isEnd() {
+        return this == END;
+    }
+}
