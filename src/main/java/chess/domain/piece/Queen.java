2106535beb17706210ffc2abafc724f8bc625e90
le2sky <leehaneul990623@gmail.com>
2024-03-20 17:37:50 +0900

    feat: 퀸 이동 가능 좌표 계산 기능 구현

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -1,8 +1,42 @@
 package chess.domain.piece;
 
+import java.util.ArrayList;
+import java.util.List;
+import chess.domain.Coordinate;
+
 public class Queen extends AbstractPiece {
 
     public Queen(Team team) {
         super(PieceType.QUEEN, team);
+    }
+
+    @Override
+    public List<Coordinate> findAllPossibleCoordinate(Coordinate start) {
+        List<Coordinate> possibleCoordinate = new ArrayList<>();
+        for (int rankValue = 1; rankValue <= 8; rankValue++) {
+            possibleCoordinate.addAll(createOneLinePossibleCoordinate(start, rankValue));
+        }
+
+        return possibleCoordinate;
+    }
+
+    private List<Coordinate> createOneLinePossibleCoordinate(Coordinate start, int rankValue) {
+        List<Coordinate> rowCoordinate = new ArrayList<>();
+        for (char fileValue = 'a'; fileValue <= 'h'; fileValue++) {
+            if (rankValue == start.getRank() && fileValue == start.getFile()) {
+                continue;
+            }
+
+            if (rankValue == start.getRank() || fileValue == start.getFile()) {
+                rowCoordinate.add(new Coordinate(rankValue, fileValue));
+            }
+
+            if (Math.abs((start.getRank() - rankValue)) == Math.abs(start.getFile() - fileValue)) {
+                rowCoordinate.add(new Coordinate(rankValue, fileValue));
+            }
+
+        }
+
+        return rowCoordinate;
     }
 }
---
+++ src/test/java/chess/domain/QueenTest.java
@@ -0,0 +1,100 @@
+package chess.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.List;
+import chess.domain.piece.Queen;
+import chess.domain.piece.Rook;
+import chess.domain.piece.Team;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class QueenTest {
+
+    @DisplayName("생성 테스트")
+    @Test
+    void create() {
+        assertThatCode(() -> new Queen(Team.WHITE))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("퀸은 대각과 가로, 세로로 제한없이 움직일 수 있다.")
+    @Test
+    void findAllPossibleCoordinate() {
+        Coordinate start = new Coordinate(4, 'd');
+        Queen queen = new Queen(Team.WHITE);
+        List<Coordinate> expected = List.of(
+                new Coordinate(4, 'a'),
+                new Coordinate(4, 'b'),
+                new Coordinate(4, 'c'),
+                new Coordinate(4, 'e'),
+                new Coordinate(4, 'f'),
+                new Coordinate(4, 'g'),
+                new Coordinate(4, 'h'),
+                new Coordinate(5, 'd'),
+                new Coordinate(6, 'd'),
+                new Coordinate(7, 'd'),
+                new Coordinate(8, 'd'),
+                new Coordinate(3, 'd'),
+                new Coordinate(2, 'd'),
+                new Coordinate(1, 'd'),
+
+                new Coordinate(1, 'a'),
+                new Coordinate(2, 'b'),
+                new Coordinate(3, 'c'),
+                new Coordinate(3, 'e'),
+                new Coordinate(2, 'f'),
+                new Coordinate(1, 'g'),
+                new Coordinate(5, 'e'),
+                new Coordinate(6, 'f'),
+                new Coordinate(7, 'g'),
+                new Coordinate(8, 'h'),
+                new Coordinate(5, 'c'),
+                new Coordinate(6, 'b'),
+                new Coordinate(7, 'a')
+        );
+
+        List<Coordinate> result = queen.findAllPossibleCoordinate(start);
+
+        assertThat(result)
+                .containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @DisplayName("퀸이 모서리칸에 존재하는 경우를 검증한다.")
+    @Test
+    void findAllPossibleCoordinate2() {
+        Coordinate start = new Coordinate(1, 'a');
+        Queen queen = new Queen(Team.WHITE);
+        List<Coordinate> expected = List.of(
+                new Coordinate(8, 'a'),
+                new Coordinate(7, 'a'),
+                new Coordinate(6, 'a'),
+                new Coordinate(5, 'a'),
+                new Coordinate(4, 'a'),
+                new Coordinate(3, 'a'),
+                new Coordinate(2, 'a'),
+                new Coordinate(1, 'b'),
+                new Coordinate(1, 'c'),
+                new Coordinate(1, 'd'),
+                new Coordinate(1, 'e'),
+                new Coordinate(1, 'f'),
+                new Coordinate(1, 'g'),
+                new Coordinate(1, 'h'),
+
+                new Coordinate(2, 'b'),
+                new Coordinate(3, 'c'),
+                new Coordinate(4, 'd'),
+                new Coordinate(5, 'e'),
+                new Coordinate(6, 'f'),
+                new Coordinate(7, 'g'),
+                new Coordinate(8, 'h')
+        );
+
+        List<Coordinate> result = queen.findAllPossibleCoordinate(start);
+
+        assertThat(result)
+                .containsExactlyInAnyOrderElementsOf(expected);
+    }
+}
+
