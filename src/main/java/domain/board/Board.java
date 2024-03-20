f464280f3f3956d4e7bffbf1b95eb55ec8f6ee26
takoyakimchi <c4nyou@gmail.com>
2024-03-20 13:58:16 +0900

    feat: 말 움직이기 (2) - 도착점에 자신의 말이 있으면 이동 불가
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/domain/board/Board.java
+++ src/main/java/domain/board/Board.java
@@ -1,5 +1,6 @@
 package domain.board;
 
+import domain.piece.Color;
 import domain.piece.Piece;
 import java.util.Map;
 
@@ -19,10 +20,23 @@ public class Board {
         if (isNoPieceAt(sourcePosition)) {
             throw new IllegalArgumentException("source 위치에 말이 없습니다.");
         }
+        if (findPieceColorAt(sourcePosition) == findPieceColorAt(targetPosition)) {
+            throw new IllegalArgumentException("한 칸에 말이 2개 존재할 수 없습니다.");
+        }
     }
 
-    public boolean isNoPieceAt(Position position) {
+    private boolean isNoPieceAt(Position position) {
         return !squares.containsKey(position);
+    }
+
+    private Color findPieceColorAt(Position position) {
+        if (isNoPieceAt(position)) {
+            throw new IllegalArgumentException("해당 위치에 말이 없습니다.");
+        }
+        if (squares.get(position).isWhite()) {
+            return Color.WHITE;
+        }
+        return Color.BLACK;
     }
 
     // TODO: getter 사용을 지양하는 방법을 고민
--- src/test/java/domain/board/BoardTest.java
+++ src/test/java/domain/board/BoardTest.java
@@ -1,19 +1,24 @@
 package domain.board;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
 
-    /**
-     * 출발점, 도착점 === move(sourcePos, targetPos) 테스트 케이스 === - (공통) sourcePos에 말이 없으면 예외. - 모든 말은 targetPos에 자기 팀의 말이 있으면
-     * 안 됨. - 모든 말은 말의 규칙에 맞는 위치로 이동해야 한다. (sourcePos == targetPos이면 안 됨) 포함 - rook, bishop, queen은 경로에 말이 있으면 안 됨. -
-     * knight는 경로에 말이 있어도 움직일 수 있음. - pawn - pawn은 2칸 이동 시 경로에 말이 있으면 안 됨. - pawn은 바로 앞 칸에 기물이 있으면 이동 X - pawn은 대각선 앞 칸에
-     * 상대 기물이 있으면 이동 O
+    /*
+     * 출발점, 도착점
+     * == move(sourcePos, targetPos) 테스트 케이스 ===
+     * (공통) sourcePos에 말이 없으면 예외.
+     * 모든 말은 targetPos에 자기 팀의 말이 있으면 안 됨.
+     * (공통) 출발 위치 == 도착 위치면 예외
+     * 모든 말은 말의 규칙에 맞는 위치로 이동해야 한다. (sourcePos == targetPos이면 안 됨) 포함
+     * rook, bishop, queen은 경로에 말이 있으면 안 됨.
+     * knight는 경로에 말이 있어도 움직일 수 있음.
+     * pawn은 2칸 이동 시 경로에 말이 있으면 안 됨.
+     * pawn은 바로 앞 칸에 기물이 있으면 이동 X
+     * pawn은 대각선 앞 칸에 상대 기물이 있으면 이동 O
      */
 
     @Test
@@ -29,15 +34,14 @@ class BoardTest {
     }
 
     @Test
-    @DisplayName("말이 존재하지 않는지 여부 반환 가능")
-    void isNoPieceAt() {
+    @DisplayName("도착점에 본인의 말이 있으면 이동 불가")
+    void move_OwnPieceExistAtTargetPosition() {
         Board board = Board.generatedBy(new InitialBoardGenerator());
-        Position exist = new Position(new File(1), new Rank(1));
-        Position notExist = new Position(new File(4), new Rank(4));
+        Position sourcePosition = new Position(new File(1), new Rank(1));
+        Position targetPosition = new Position(new File(1), new Rank(2));
 
-        assertAll(
-            () -> assertThat(board.isNoPieceAt(exist)).isFalse(),
-            () -> assertThat(board.isNoPieceAt(notExist)).isTrue()
-        );
+        assertThatThrownBy(() -> board.move(sourcePosition, targetPosition))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("한 칸에 말이 2개 존재할 수 없습니다.");
     }
 }
