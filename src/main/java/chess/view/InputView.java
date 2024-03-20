05f35cc8b3eb493e8bd3fc2f86923b7b8ace3e27
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-20 14:55:12 +0900

    feat: 이동 명령어 입력 기능 구현
    Co-Authored-By: H <99153215+hyeon0208@users.noreply.github.com>

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,11 +1,15 @@
 package chess.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
 public class InputView {
-    private static final Set<String> COMMAND = Set.of("start", "end");
-    private static final String INVALID_COMMAND = "start 또는 end만 입력가능합니다.";
+    private static final Set<String> GAME_COMMAND = Set.of("start", "end");
+    private static final String MOVE_COMMAND = "move";
+    private static final String INVALID_GAME_COMMAND = "start 또는 end만 입력가능합니다.";
+    private static final String INVALID_MOVE_COMMAND = "부적절한 명령어입니다. move b2 b3와 같이 입력해주세요.";
     private static final String START_MESSAGE = "게임 시작은 start, 종료는 end 명령을 입력하세요.";
     private final Scanner scanner;
 
@@ -16,13 +20,29 @@ public class InputView {
     public String readStartCommand() {
         System.out.println(START_MESSAGE);
         String input = scanner.nextLine();
-        validateCommand(input);
+        validateGameCommand(input);
         return input;
     }
 
-    private void validateCommand(final String command) {
-        if (!COMMAND.contains(command)) {
-            throw new IllegalArgumentException(INVALID_COMMAND);
+    private void validateGameCommand(final String command) {
+        if (!GAME_COMMAND.contains(command)) {
+            throw new IllegalArgumentException(INVALID_GAME_COMMAND);
+        }
+    }
+
+    public List<String> readMovement() {
+        String input = scanner.nextLine();
+        List<String> splitInput = Arrays.stream(input.split(" "))
+                .map(String::trim)
+                .toList();
+        validateMoveCommand(splitInput.get(0));
+        splitInput.remove(0);
+        return splitInput;
+    }
+
+    private void validateMoveCommand(final String command) {
+        if (!command.equals(MOVE_COMMAND)) {
+            throw new IllegalArgumentException(INVALID_MOVE_COMMAND);
         }
     }
 }
