1d811564b8ec21df5364d9b636b1906867124c24
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-20 16:22:13 +0900

    feat(Point) : 파일,랭크 를 포함한 문자열을 통해 생성 기능 구현
    File,Rank 에 from 정적 팩토리 메소드 추가
    - 이에 따른 테스트 코드 추가

--- src/main/java/domain/piece/point/File.java
+++ src/main/java/domain/piece/point/File.java
@@ -1,12 +1,27 @@
 package domain.piece.point;
 
 public enum File {
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-    H
+    A('a'),
+    B('b'),
+    C('c'),
+    D('d'),
+    E('e'),
+    F('f'),
+    G('g'),
+    H('h');
+    private char value;
+
+    File(final char value) {
+        this.value = value;
+    }
+
+    public static File from(char value) {
+        for (final File file : File.values()) {
+            if (file.value == value) {
+                return file;
+            }
+        }
+        throw new IllegalArgumentException(String.format("%c는 파일에 존재하지 않습니다.", value));
+    }
+
 }
--- src/main/java/domain/piece/point/Rank.java
+++ src/main/java/domain/piece/point/Rank.java
@@ -1,12 +1,26 @@
 package domain.piece.point;
 
 public enum Rank {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8);
+    private int value;
+
+    Rank(final int value) {
+        this.value = value;
+    }
+
+    public static Rank from(int value) {
+        for (final Rank rank : Rank.values()) {
+            if (rank.value == value) {
+                return rank;
+            }
+        }
+        throw new IllegalArgumentException(String.format("%d는 랭크에 존재하지 않습니다.", value));
+    }
 }
