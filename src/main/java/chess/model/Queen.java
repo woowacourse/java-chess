66e83c2fd0c4cc50c15054b26861264e2751149a
jongmee <ra123221@gmail.com>
2024-03-20 15:28:31 +0900

    feat: Queen 이동 가능 여부 판단 구현

--- src/main/java/chess/model/Queen.java
+++ src/main/java/chess/model/Queen.java
@@ -6,6 +6,12 @@ public class Queen extends Piece {
     }
 
     @Override
+    public boolean canMove(ChessPosition target) {
+        Distance distance = target.calculateDistance(this.chessPosition);
+        return distance.isCrossMovement() || distance.isDiagonalMovement();
+    }
+
+    @Override
     public String getText() {
         if (side.isWhite()) {
             return "q";
