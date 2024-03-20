b9447a96bd60fcad638f1bff9892f1cd71e560aa
J-I-H-O <jeongjiho0731@gmail.com>
2024-03-20 19:31:55 +0900

    feat: 기물들의 이동 방향을 나타내는 enum 클래스 생성
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/main/java/chess/domain/Direction.java
@@ -0,0 +1,30 @@
+package chess.domain;
+
+public enum Direction {
+
+    LEFT(-1,0),
+    RIGHT(1,0),
+    UP(0,1),
+    DOWN(0,-1),
+    LEFT_UP(-1,1),
+    LEFT_DOWN(-1, -1),
+    RIGHT_UP(1,1),
+    RIGHT_DOWN(1,-1),
+    KNIGHT_LEFT_UP(-2, 1),
+    KNIGHT_LEFT_DOWN(-2, -1),
+    KNIGHT_RIGHT_UP(2, 1),
+    KNIGHT_RIGHT_DOWN(2, -1),
+    KNIGHT_UP_LEFT(-1, 2),
+    KNIGHT_UP_RIGHT(1, 2),
+    KNIGHT_DOWN_LEFT(-1, -2),
+    KNIGHT_DOWN_RIGHT(1, -2),
+    ;
+
+    private final int x;
+    private final int y;
+
+    Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
