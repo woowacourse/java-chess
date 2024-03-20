e8f3dd40d98c84f44401e17d8178a223af88b720
robinjoon <robin980108@naver.com>
2024-03-20 16:15:11 +0900

    refactor: PieceWrapper 추가

---
+++ src/main/java/domain/PieceWrapper.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class PieceWrapper {
+	private final Piece piece;
+
+	public PieceWrapper(Piece piece) {
+		this.piece = piece;
+	}
+
+	public boolean isOn(Position position) {
+		return piece.isOn(position);
+	}
+
+	public Team getTeam() {
+		return piece.getTeam();
+	}
+}
--- src/main/java/domain/PiecesOnChessBoard.java
+++ src/main/java/domain/PiecesOnChessBoard.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 public class PiecesOnChessBoard {
-    private final List<Piece> pieces;
+	private final List<PieceWrapper> pieces;
 
-    public PiecesOnChessBoard(List<Piece> pieces) {
-        this.pieces = pieces;
-    }
+	public PiecesOnChessBoard(List<Piece> pieces) {
+		this.pieces = pieces.stream().map(PieceWrapper::new).toList();
+	}
 
-    public Optional<Team> whichTeam(Position position) {
-        Optional<Piece> pieceOnPosition = pieces.stream().filter(piece -> piece.isOn(position))
-                .findFirst();
-        return pieceOnPosition.map(Piece::getTeam);
-    }
+	public Optional<Team> whichTeam(Position position) {
+		Optional<PieceWrapper> pieceOnPosition = pieces.stream().filter(piece -> piece.isOn(position))
+			.findFirst();
+		return pieceOnPosition.map(PieceWrapper::getTeam);
+	}
 }
