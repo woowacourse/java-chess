8ff5278e205689c72a68991f02402bbea4b5fbf2
tackyu <tack970913@gmail.com>
2024-03-20 16:56:21 +0900

    feat(Direction) : 상하좌우, 대각선에 대한 방향 생성

---
+++ src/main/java/chess/Direction.java
@@ -0,0 +1,53 @@
+package chess;
+
+public enum Direction {
+    //row, column
+    UP(0, 1),
+    DOWN(0, -1),
+    LEFT(-1, 0),
+    RIGHT(1, 0),
+    UP_LEFT(-1, 1),
+    UP_RIGHT(1, 1),
+    DOWN_LEFT(-1, -1),
+    DOWN_RIGHT(1, -1);
+
+    private int rowDirection;
+    private int columnDirection;
+
+    Direction(int rowDirection, int columnDirection) {
+        this.rowDirection = rowDirection;
+        this.columnDirection = columnDirection;
+    }
+
+    public static Direction findDirection(Position source, Position target) {
+        Row srcRow = source.getRow();
+        Column srcColumn = source.getColumn();
+        Row trgRow = target.getRow();
+        Column trgColumn = target.getColumn();
+        boolean isRowBigger = trgRow.isBigger(srcRow);
+        boolean isColumnBigger = trgColumn.isBigger(srcColumn);
+
+        if(srcRow == trgRow && isColumnBigger) {
+            return UP;
+        }
+        if(srcRow == trgRow && !isColumnBigger) {
+            return DOWN;
+        }
+        if(!isRowBigger&&srcColumn==trgColumn){
+            return LEFT;
+        }
+        if(isRowBigger&&srcColumn==trgColumn){
+            return RIGHT;
+        }
+        if(!isRowBigger&&isColumnBigger){
+            return UP_LEFT;
+        }
+        if(isRowBigger&&isColumnBigger){
+            return UP_RIGHT;
+        }
+        if(!isRowBigger&&!isColumnBigger){
+            return DOWN_LEFT;
+        }
+        return DOWN_RIGHT;
+    }
+}
