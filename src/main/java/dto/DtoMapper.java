bc9478a38d6fdfaba16130b894279f40ead3a434
woojong <dwax1324@gmail.com>
2024-03-20 15:37:06 +0900

    feat: dto 매핑을 담당하는 클래스 추가

---
+++ src/main/java/dto/DtoMapper.java
@@ -0,0 +1,40 @@
+package dto;
+
+import domain.Board;
+import domain.piece.Piece;
+import domain.piece.info.Position;
+import domain.piece.info.Type;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import view.PieceShape;
+
+public class DtoMapper {
+
+    public static List<Piece> map(final Board board, final int rank) {
+        final Map<Position, Piece> squares = board.squares();
+        return squares.entrySet().stream()
+                .filter(entry -> entry.getKey().rankIndex() == rank)
+                .sorted(Comparator.comparingInt(entry -> entry.getKey().fileIndex()))
+                .map(Entry::getValue)
+                .toList();
+    }
+
+    public static RankInfo getPieceShapeOfRank(final Board board, final int rank) {
+        final List<Piece> pieces = map(board, rank);
+        final List<String> pieceShapes = new ArrayList<>();
+
+        for (final Piece piece : pieces) {
+            if (piece.isWhite()) {
+                pieceShapes.add(PieceShape.whiteShapeOf(piece.type().name()));
+                continue;
+            }
+            pieceShapes.add(PieceShape.blackShapeOf(piece.type().name()));
+        }
+        return new RankInfo(pieceShapes);
+    }
+
+
+}
