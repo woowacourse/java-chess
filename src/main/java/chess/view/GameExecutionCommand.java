a23a7c905751a9074d421d4ec999ce6a4ce28700
libienz <leekh10195@gmail.com>
2024-03-21 15:39:07 +0900

    fix: 게임 실행 명령어 움직임 명령을 포함도록 원복

--- src/main/java/chess/view/GameExecutionCommand.java
+++ src/main/java/chess/view/GameExecutionCommand.java
@@ -5,7 +5,10 @@ import java.util.NoSuchElementException;
 
 public enum GameExecutionCommand {
     START("start"),
-    END("end");
+    END("end"),
+    MOVE("move");
+
+    private static final String ARGUMENT_SEPARATOR = " ";
 
     private final String code;
 
@@ -13,11 +16,17 @@ public enum GameExecutionCommand {
         this.code = code;
     }
 
-    public static GameExecutionCommand from(String code) {
+    //if 무브 명령어면 -> 무브 커맨드 생성
+    public static GameExecutionCommand from(String input) {
         return Arrays.stream(values())
-                .filter(code::equals)
+                .filter(command -> command.commandStartsWithCode(input))
                 .findAny()
                 .orElseThrow(() -> new NoSuchElementException("해당 커맨드를 찾을 수 없습니다"));
+    }
+
+    private boolean commandStartsWithCode(String input) {
+        String inputCommand = input.split(ARGUMENT_SEPARATOR)[0];
+        return inputCommand.equals(code);
     }
 
     public String getCode() {
