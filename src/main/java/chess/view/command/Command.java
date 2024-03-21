4d14d61c848d0f66054e2ee042cc1f3c1c8dbb1f
Zeus6768 <akaps6768@gmail.com>
2024-03-22 00:23:54 +0900

    fix(command): 명령어 검증 로직 개선
    - 명령어가 주어진 문자로 시작하는지 검증

--- src/main/java/chess/view/command/Command.java
+++ src/main/java/chess/view/command/Command.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public enum Command {
 
-    START("start", "start"),
+    START("start", "^start"),
     MOVE("move", "^move [a-h][1-8] [a-h][1-8]$"),
-    END("end", "end")
+    END("end", "^end")
     ;
 
     private final String message;
@@ -17,20 +17,8 @@ public enum Command {
         this.pattern = pattern;
     }
 
-    public static Command of(final String input) {
-        return Arrays.stream(values())
-                .filter(command -> input.matches(command.pattern))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(
-                        "존재하지 않는 명령어입니다. 제시된 명령어를 입력하세요: %s".formatted(input)));
-    }
-
     public static boolean isStart(final String input) {
         return input.matches(START.pattern);
-    }
-
-    public static boolean isEnd(final String input) {
-        return input.matches(END.pattern);
     }
 
     public static boolean isMove(final String input) {
