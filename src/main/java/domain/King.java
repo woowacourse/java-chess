f81ca87e70f51d11feaa6aa67080179361bbe13d
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-03-21 11:32:23 +0900

    feat(King) : 킹이 이동 가능한지 확인하는 기능 구현
    - 이에 따른 테스트 코드 추가

--- src/main/java/domain/King.java
+++ src/main/java/domain/King.java
@@ -1,6 +1,8 @@
 package domain;
 
 import domain.piece.attribute.Color;
+import domain.piece.attribute.point.Direction;
+import domain.piece.attribute.point.Index;
 import domain.piece.attribute.point.Point;
 
 public class King extends Piece {
@@ -11,5 +13,13 @@ public class King extends Piece {
     @Override
     public PieceStatus getStatus() {
         return PieceStatus.KING;
+    }
+
+    public boolean canMove(Point point) {
+        Direction direction = this.point.calculate(point);
+        Index index = this.point.toIndex();
+
+        return Point.fromIndex(index.move(direction))
+                    .equals(point);
     }
 }
