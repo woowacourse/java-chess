6eb232cd36507685501bdc6544676b6bcf941ab6
libienz <leekh10195@gmail.com>
2024-03-21 15:42:44 +0900

    refactor: 커맨드 정보를 이용해서 출력하도록 뷰로직 개선

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,10 +1,15 @@
 package chess.view;
 
+import static chess.view.GameExecutionCommand.END;
+import static chess.view.GameExecutionCommand.MOVE;
+import static chess.view.GameExecutionCommand.START;
+
 import chess.domain.board.ChessBoard;
 import chess.domain.piece.Piece;
 import chess.domain.position.ColumnPosition;
 import chess.domain.position.Position;
 import chess.domain.position.RowPosition;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,13 +17,21 @@ public class OutputView {
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String POSITION_EMPTY_MESSAGE = ".";
 
-    //TODO: 필요한 메서드임! 적절한 위치에 주입하기
     public void printStartMessage() {
-        System.out.println("체스 게임을 시작합니다.");
+        System.out.println(resolveStartMessage());
     }
 
     public void printChessBoardMessage(ChessBoard chessBoard) {
         System.out.println(resolveChessBoardMessage(chessBoard));
+    }
+
+    private String resolveStartMessage() {
+        return new StringJoiner(LINE_SEPARATOR)
+                .add("체스 게임을 시작합니다.")
+                .add(String.format("> 게임 시작 : %s", START.getCode()))
+                .add(String.format("> 게임 종료 : %s", END.getCode()))
+                .add(String.format("> 게임 이동 : %s source위치 target위치 - 예. %s b2 b3", MOVE.getCode(), MOVE.getCode()))
+                .toString();
     }
 
     private String resolveChessBoardMessage(ChessBoard chessBoard) {
