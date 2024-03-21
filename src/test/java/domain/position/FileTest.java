9b9e8a4b5e55bc16d2c100b283b14db02c44b479
SCY <helenason@naver.com>
2024-03-21 11:50:05 +0900

    test(File, Rank): 도메인 테스트 작성

---
+++ src/test/java/domain/position/FileTest.java
@@ -0,0 +1,50 @@
+package domain.position;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FileTest {
+
+    @Test
+    @DisplayName("두 파일 사이의 거리를 구한다.")
+    void distance_Files() {
+        File source = File.A;
+        File target = File.E;
+
+        int distance = source.distance(target);
+
+        assertThat(distance).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("두 파일 사이의 모든 파일을 반환한다.")
+    void betweenFiles() {
+        File source = File.A;
+        File target = File.E;
+
+        List<File> files = source.betweenFiles(target);
+
+        assertThat(files).containsOnly(File.A, File.B, File.C, File.D, File.E);
+    }
+
+    @Test
+    @DisplayName("두 파일이 같다면 참을 반환한다.")
+    void isSame_Files_True() {
+        File source = File.A;
+        File target = File.A;
+
+        assertThat(source.isSame(target)).isTrue();
+    }
+
+    @Test
+    @DisplayName("두 파일이 같지 않다면 거짓을 반환한다.")
+    void isSame_Files_False() {
+        File source = File.A;
+        File target = File.E;
+
+        assertThat(source.isSame(target)).isFalse();
+    }
+}
---
+++ src/test/java/domain/position/RankTest.java
@@ -0,0 +1,61 @@
+package domain.position;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RankTest {
+
+    @Test
+    @DisplayName("두 랭크 사이의 거리를 구한다.")
+    void distance_Ranks() {
+        Rank source = Rank.FIVE;
+        Rank target = Rank.ONE;
+
+        int distance = source.distance(target);
+
+        assertThat(distance).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("두 랭크 사이의 전방 거리를 구한다.")
+    void forwardDistance_Ranks() {
+        Rank source = Rank.ONE;
+        Rank target = Rank.FIVE;
+
+        int distance = source.forwardDistance(target);
+
+        assertThat(distance).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("두 랭크 사이의 모든 랭크를 반환한다.")
+    void betweenRanks() {
+        Rank source = Rank.ONE;
+        Rank target = Rank.FIVE;
+
+        List<Rank> ranks = source.betweenRanks(target);
+
+        assertThat(ranks).containsOnly(Rank.ONE, Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE);
+    }
+
+    @Test
+    @DisplayName("두 랭크가 같다면 참을 반환한다.")
+    void isSame_Ranks_True() {
+        Rank source = Rank.ONE;
+        Rank target = Rank.ONE;
+
+        assertThat(source.isSame(target)).isTrue();
+    }
+
+    @Test
+    @DisplayName("두 랭크가 같지 않다면 거짓을 반환한다.")
+    void isSame_Ranks_False() {
+        Rank source = Rank.ONE;
+        Rank target = Rank.TWO;
+
+        assertThat(source.isSame(target)).isFalse();
+    }
+}
