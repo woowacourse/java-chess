617c601fad687ee922b3f8b59dd4b0ca2dface45
DobbyMac <kimdobby@g.skku.edu>
2024-03-21 17:44:07 +0900

    feat(InputView): 사용자 게임 커맨드 입력 처리 기능 구현

--- src/main/java/view/GameCommand.java
+++ src/main/java/view/GameCommand.java
@@ -1,10 +1,10 @@
 package view;
 
 import java.util.Arrays;
+import java.util.List;
 
 public enum GameCommand {
-    START("start"),
-    END("end");
+    START("start"), END("end"), MOVE("move");
 
     private String command;
 
@@ -12,10 +12,35 @@ public enum GameCommand {
         this.command = command;
     }
 
-    public static GameCommand from(String inputCommand) {
+    public static GameCommand from(List<String> inputCommand) {
+        String command = inputCommand.get(0);
         return Arrays.stream(values())
-                .filter(gameCommand -> gameCommand.command.equals(inputCommand))
+                .filter(gameCommand -> gameCommand.command.equals(command))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("입력하신 Command가 없습니다."));
+    }
+
+    public static int toSourceFileValue(List<String> inputCommand) {
+        char file = inputCommand.get(1)
+                .charAt(0);
+        return Character.getNumericValue(file - 48);
+    }
+
+    public static int toSourceRankValue(List<String> inputCommand) {
+        char rank = inputCommand.get(1)
+                .charAt(1);
+        return Character.getNumericValue(rank);
+    }
+
+    public static int toDestinationFileValue(List<String> inputCommand) {
+        char file = inputCommand.get(2)
+                .charAt(0);
+        return Character.getNumericValue(file - 48);
+    }
+
+    public static int toDestinationRankValue(List<String> inputCommand) {
+        char rank = inputCommand.get(2)
+                .charAt(1);
+        return Character.getNumericValue(rank);
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,6 +3,8 @@ package view;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
@@ -10,9 +12,34 @@ public class InputView {
     private InputView() {
     }
 
-    public static GameCommand inputGameCommand() {
-        String inputCommand = readLine();
-        return GameCommand.from(inputCommand);
+    public static GameCommand inputInitialGameCommand() {
+        List<String> inputCommand = Arrays.asList(readLine().split(" "));
+        GameCommand initialGameCommand = GameCommand.from(inputCommand);
+        if (initialGameCommand == GameCommand.MOVE) {
+            throw new IllegalArgumentException();
+        }
+        return initialGameCommand;
+    }
+
+    public static List<String> parseCommand() {
+        List<String> inputMovement = Arrays.asList(readLine().split(" "));
+        GameCommand gameCommand = GameCommand.from(inputMovement);
+        if (gameCommand == GameCommand.MOVE) {
+            validateInputMovement(inputMovement);
+        }
+        return inputMovement;
+    }
+
+    private static void validateInputMovement(List<String> inputMovement) {
+        if (inputMovement.size() == 3) {
+            inputMovement.subList(1, inputMovement.size())
+                    .stream()
+                    .filter(position -> !position.matches("^[a-h][1-8]$"))
+                    .findAny()
+                    .ifPresent(position -> {
+                        throw new IllegalArgumentException("입력하신 " + position + "은 올바르지 않은 좌표입니다");
+                    });
+        }
     }
 
     private static String readLine() {
