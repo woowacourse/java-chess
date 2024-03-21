951cd34f9a191c633da2d0d893d018361cc48762
Choo <choo000407@naver.com>
2024-03-21 11:40:17 +0900

    test: 기물이 특정 위치로 움직일 수 있으면 움직인다

--- src/test/java/chess/model/board/BoardTest.java
+++ src/test/java/chess/model/board/BoardTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.HashMap;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
@@ -24,5 +25,27 @@ public class BoardTest {
         // when, then
         assertThatThrownBy(() -> board.move(Position.from(1, 1), Position.from(1, 3)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 기물이_특정_위치로_움직일_수_있다면_움직인다() {
+        // given
+        Board board = new InitialBoardGenerator().create();
+        board.move(Position.from(2, 2), Position.from(2, 3));
+
+        // when, then
+        List<String> boardLines = board.getLines().stream()
+                .map(line -> String.join("", line))
+                .toList();
+        assertThat(boardLines).containsExactly(
+                "RNBQKBNR",
+                "PPPPPPPP",
+                "........",
+                "........",
+                "........",
+                ".p......",
+                "p.pppppp",
+                "rnbqkbnr"
+        );
     }
 }
