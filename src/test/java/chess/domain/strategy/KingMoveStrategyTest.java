270c2a0e7b2ee9ae7233819a813a84c0b9b01b6a
jhon3242 <rldotnr2@gmail.com>
2024-03-21 13:17:16 +0900

    feat(KingMoveStrategy) : 킹 이동 전략에 따른 이동할 수 있는 위치를 계산하는 기능 구현
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

---
+++ src/test/java/chess/domain/strategy/KingMoveStrategyTest.java
@@ -0,0 +1,63 @@
+package chess.domain.strategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import chess.domain.Column;
+import chess.domain.Direction;
+import chess.domain.Position;
+import chess.domain.Row;
+import java.util.Deque;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class KingMoveStrategyTest {
+    /**
+     * ........  8 (rank 8)
+     * ........  7
+     * ........  6
+     * ........  5
+     * ........  4
+     * ........  3
+     * ...k....  2
+     * ........  1 (rank 1)
+     *
+     * abcdefgh
+     */
+    @Test
+    @DisplayName("킹이 D2 에 있을 때 방향에 따라 움직일 수 있는 후보 포지션을 차례대로 저장한다.")
+    void calculateCandidateOnEdgePosition() {
+        Position position = new Position(Row.RANK2, Column.d);
+        KingMoveStrategy kingMoveStrategy = new KingMoveStrategy();
+
+        Map<Direction, Deque<Position>> directionListMap = kingMoveStrategy.generateMovablePositions(position);
+
+        assertAll(
+                () -> assertThat(directionListMap.get(Direction.N)).containsExactly(
+                        new Position(Row.RANK3, Column.d)
+                ),
+                () -> assertThat(directionListMap.get(Direction.NE)).containsExactly(
+                        new Position(Row.RANK3, Column.e)
+                ),
+                () -> assertThat(directionListMap.get(Direction.E)).containsExactly(
+                        new Position(Row.RANK2, Column.e)
+                ),
+                () -> assertThat(directionListMap.get(Direction.SE)).containsExactly(
+                        new Position(Row.RANK1, Column.e)
+                ),
+                () -> assertThat(directionListMap.get(Direction.S)).containsExactly(
+                        new Position(Row.RANK1, Column.d)
+                ),
+                () -> assertThat(directionListMap.get(Direction.SW)).containsExactly(
+                        new Position(Row.RANK1, Column.c)
+                ),
+                () -> assertThat(directionListMap.get(Direction.W)).containsExactly(
+                        new Position(Row.RANK2, Column.c)
+                ),
+                () -> assertThat(directionListMap.get(Direction.NW)).containsExactly(
+                        new Position(Row.RANK3, Column.c)
+                )
+        );
+    }
+}
