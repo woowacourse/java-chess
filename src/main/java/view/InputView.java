aefcb2a4b23f001a5fb2c88e43fced5b7f4d2436
takoyakimchi <c4nyou@gmail.com>
2024-03-21 11:12:28 +0900

    refactor: InputView의 명령어 처리 통합
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,43 +1,25 @@
 package view;
 
-import domain.board.File;
-import domain.board.Position;
-import domain.board.Rank;
 import java.util.Scanner;
-import view.dto.MovePositionDto;
+import java.util.regex.Pattern;
 
 public class InputView {
 
     private static final String START_COMMAND = "start";
     private static final String END_COMMAND = "end";
-    private static final String MOVE_COMMAND = "move";
+    private static final Pattern MOVE_REGEX = Pattern.compile("^move [a-h][1-8] [a-h][1-8]$");
+
 
     private final Scanner scanner = new Scanner(System.in);
 
-    public boolean readStartCommand() {
-        String command = scanner.nextLine();
-        if (command.equals(START_COMMAND)) {
-            return true;
+    public String readCommand() {
+        String input = scanner.nextLine();
+        if (input.equals(START_COMMAND) || input.equals(END_COMMAND)) {
+            return input;
         }
-        if (command.equals(END_COMMAND)) {
-            return false;
+        if (MOVE_REGEX.matcher(input).matches()) {
+            return input;
         }
-        throw new IllegalArgumentException("잘못된 명령어입니다.");
-    }
-
-    public MovePositionDto readMoveCommand() {
-        String command = scanner.nextLine();
-        String[] split = command.split(" ");
-        if (split[0].equals(MOVE_COMMAND)) {
-            File sourceFile = FileResolver.resolveFile(Character.toString(split[1].charAt(0)));
-            Rank sourceRank = RankResolver.resolveRank(Character.toString(split[1].charAt(1)));
-            Position sourcePosition = new Position(sourceFile, sourceRank);
-            File targetFile = FileResolver.resolveFile(Character.toString(split[2].charAt(0)));
-            Rank targetRank = RankResolver.resolveRank(Character.toString(split[2].charAt(1)));
-            Position targetPosition = new Position(targetFile, targetRank);
-
-            return new MovePositionDto(sourcePosition, targetPosition);
-        }
-        throw new IllegalArgumentException("");
+        throw new IllegalArgumentException("올바른 명령어를 입력해 주세요.");
     }
 }
