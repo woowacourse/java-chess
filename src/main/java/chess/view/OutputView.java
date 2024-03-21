703f31561f1bd091554f847c5744f5e38ce53af6
seunghye218 <seunghye@student.42seoul.kr>
2024-03-21 15:07:17 +0900

    refactor: 시작 메세지 출력 변경
    Co-authored-by: jcoding-play <namejgc@naver.com>

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -25,6 +25,11 @@ public class OutputView {
     }
 
     public void printGameStart() {
-        System.out.printf("체스 게임을 시작합니다.%n게임 시작은 start, 종료는 end 명령을 입력하세요.%n");
+        System.out.print("""
+> 체스 게임을 시작합니다.
+> 게임 시작 : start
+> 게임 종료 : end
+> 게임 이동 : move source위치 target위치 - 예. move b2 b3        
+        """);
     }
 }
