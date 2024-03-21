63d6b08b8752906592876701fcd98a1fcba3b8c5
jhon3242 <rldotnr2@gmail.com>
2024-03-21 16:33:58 +0900

    fix(WhitePawnMoveStrategy) : 화이트 폰 이동 방향 수정
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

--- src/main/java/chess/domain/strategy/WhitePawnMoveStrategy.java
+++ src/main/java/chess/domain/strategy/WhitePawnMoveStrategy.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class WhitePawnMoveStrategy extends PawnMoveStrategy{
 
-    private static final List<Direction> DIRECTIONS = List.of(Direction.S, Direction.SE, Direction.SW);
+    private static final List<Direction> DIRECTIONS = List.of(Direction.N, Direction.NE, Direction.NW);
 
     public WhitePawnMoveStrategy() {
         super(DIRECTIONS, Row.RANK2, Direction.N);
