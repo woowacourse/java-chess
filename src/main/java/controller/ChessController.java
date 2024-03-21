aed60b7a858e586f099b6c0d022e2737015678c8
hangillee <skfcb10@naver.com>
2024-03-21 17:13:18 +0900

    feat: 잘못된 명령어 입력 시 재입력 받는 기능 구현

--- src/main/java/controller/ChessController.java
+++ src/main/java/controller/ChessController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import static constants.Bound.BOARD_LOWER_BOUND;
+import static constants.Bound.BOARD_UPPER_BOUND;
+import static view.Command.MOVE;
+import static view.Command.START;
+
 import domain.board.Board;
 import domain.board.BoardInitiator;
 import domain.board.Position;
@@ -9,31 +14,55 @@ import dto.DtoMapper;
 import dto.RankInfo;
 import java.util.ArrayList;
 import java.util.List;
+import view.Command;
 import view.InputView;
 import view.OutputView;
 
 public class ChessController {
-
-    public static final int MAX_BOARD_SIZE = 7;
-    public static final int MIN_BOARD_SIZE = 0;
+    public static final String DELIMITER = " ";
+    public static final int SOURCE_POSITION = 1;
+    public static final int TARGET_POSITION = 2;
 
     public void start() {
         OutputView.printGameStartMessage();
         final Board board = new Board(BoardInitiator.init());
+
         String command = InputView.inputCommand();
-        if (!command.equals("start")) {
-            throw new IllegalArgumentException("다른 명령어 입력 전, 체스 게임을 시작해야 합니다.");
-        }
+        readyForStartCommand(command);
         OutputView.printChessBoard(createPieceInfo(board));
-        command = InputView.inputCommand();
-        while (command.contains("move")) {
-            command = nextCommand(command, board);
+
+        move(board);
+    }
+
+    private void readyForStartCommand(String command) {
+        while (!START.equals(Command.of(command))) {
+            OutputView.printErrorMessage("게임을 시작하려면 start를 입력해주세요.");
+            command = InputView.inputCommand();
         }
     }
 
+    private void move(final Board board) {
+        String command;
+        command = InputView.inputCommand();
+        while (MOVE.equals(Command.of(command))) {
+            command = inputCommandUntilNoException(command, board);
+        }
+    }
+
+    private String inputCommandUntilNoException(String command, final Board board) {
+        try {
+            command = nextCommand(command, board);
+        } catch (Exception e) {
+            OutputView.printErrorMessage(e.getMessage());
+            command = InputView.inputCommand();
+        }
+        return command;
+    }
+
     private String nextCommand(String command, final Board board) {
-        final String[] splitCommand = command.split(" ");
+        final String[] splitCommand = command.split(DELIMITER);
         moveByCommand(board, splitCommand);
+
         OutputView.printChessBoard(createPieceInfo(board));
         command = InputView.inputCommand();
         return command;
@@ -43,10 +72,10 @@ public class ChessController {
         Position sourcePosition = null;
         Position targetPosition = null;
         if (splitCommand.length != 1) {
-            final String source = splitCommand[1];
+            final String source = splitCommand[SOURCE_POSITION];
             sourcePosition = createPosition(source);
 
-            final String target = splitCommand[2];
+            final String target = splitCommand[TARGET_POSITION];
             targetPosition = createPosition(target);
         }
         board.move(sourcePosition, targetPosition);
@@ -59,7 +88,7 @@ public class ChessController {
     }
 
     private File convertToFile(final String positionValue) {
-        final String fileName = String.valueOf(positionValue.charAt(MIN_BOARD_SIZE)).toUpperCase();
+        final String fileName = String.valueOf(positionValue.charAt(0)).toUpperCase();
         return File.of(fileName);
     }
 
@@ -70,7 +99,7 @@ public class ChessController {
 
     private List<RankInfo> createPieceInfo(final Board board) {
         final List<RankInfo> rankInfos = new ArrayList<>();
-        for (int rank = MAX_BOARD_SIZE; rank >= MIN_BOARD_SIZE; rank--) {
+        for (int rank = BOARD_UPPER_BOUND.value(); rank >= BOARD_LOWER_BOUND.value(); rank--) {
             final RankInfo pieceShapeOfRank = DtoMapper.getPieceShapeOfRank(board, rank);
             rankInfos.add(pieceShapeOfRank);
         }
