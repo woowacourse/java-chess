6c93c42ed0208171482a1cabf53434eaff70a446
hyun <osh123chm@gmail.com>
2024-03-21 14:32:38 +0900

    feat: command 입력에 따른 동작 로직
    Co-authored-by: ashsty <qkralsdk991@naver.com>

--- src/main/java/chess/controller/ChessGame.java
+++ src/main/java/chess/controller/ChessGame.java
@@ -1,16 +1,31 @@
 package chess.controller;
 
 import chess.domain.board.ChessBoard;
+import chess.domain.state.GameState;
+import chess.domain.state.Ready;
 import chess.dto.ChessBoardDto;
+import chess.view.InputView;
 import chess.view.OutputView;
+import java.util.List;
 
 public class ChessGame {
+    private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
     public void run() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.initialBoard();
-        ChessBoardDto chessBoardDto = chessBoard.convertToDto();
-        outputView.printChessBoard(chessBoardDto);
+        ChessBoardDto chessBoardDto;
+        GameState gameState = new Ready(chessBoard);
+
+        while (!gameState.isEnd()) {
+            List<String> command = inputView.readCommand();
+            gameState = gameState.play(command);
+
+            if (!gameState.isEnd()) {
+                chessBoardDto = chessBoard.convertToDto();
+                outputView.printChessBoard(chessBoardDto);
+            }
+        }
     }
 }
