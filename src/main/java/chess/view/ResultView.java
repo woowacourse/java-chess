d6b8588d69322c61478358fe52404507c6282e48
Zeus6768 <akaps6768@gmail.com>
2024-03-21 17:18:40 +0900

    feat(command): 게임 명령어 입력 로직 구현

--- src/main/java/chess/view/ResultView.java
+++ src/main/java/chess/view/ResultView.java
@@ -1,12 +1,19 @@
 package chess.view;
 
-import static chess.view.command.StartCommand.END;
+import static chess.view.command.Command.END;
 
 import java.util.List;
 
 import chess.view.dto.ChessboardDto;
 
 public class ResultView {
+
+    public void printGameStartMessage() {
+        System.out.printf("체스 게임을 시작합니다.%n"
+                + "> 게임 시작 : start%n"
+                + "> 게임 종료 : end%n"
+                + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3%n");
+    }
 
     public void printGameEnd() {
         System.out.printf(END.getMessage());
@@ -18,4 +25,3 @@ public class ResultView {
         System.out.println();
     }
 }
-
--- src/main/java/chess/view/dto/ChessboardDto.java
+++ src/main/java/chess/view/dto/ChessboardDto.java
@@ -7,25 +7,27 @@ import java.util.Map;
 import chess.domain.attribute.File;
 import chess.domain.attribute.Rank;
 import chess.domain.attribute.Square;
+import chess.domain.chessboard.Chessboard;
 import chess.domain.piece.Piece;
 
 public class ChessboardDto {
 
     private final List<List<String>> chessboard;
 
-    public ChessboardDto(final Map<Square, Piece> chessboard) {
+    public ChessboardDto(final Chessboard chessboard) {
+        Map<Square, Piece> board = chessboard.getChessboard();
         this.chessboard = new ArrayList<>();
         for (final Rank rank : Rank.values()) {
             List<String> squares = new ArrayList<>();
             this.chessboard.add(squares);
-            addSquares(chessboard, rank, squares);
+            addSquares(board, rank, squares);
         }
     }
 
-    private void addSquares(final Map<Square, Piece> chessboard, final Rank rank, final List<String> squares) {
+    private void addSquares(final Map<Square, Piece> board, final Rank rank, final List<String> squares) {
         for (final File file : File.values()) {
             Square square = Square.of(file, rank);
-            Piece piece = chessboard.get(square);
+            Piece piece = board.get(square);
             squares.add(PieceTypeDto.of(piece));
         }
     }
