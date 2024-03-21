a87e56b58b9722d1062f0f49f091ec57c58b17e3
kelly6bf <kelly6bf@gmail.com>
2024-03-21 10:59:25 +0900

    test(knightMoveStrategyTest): 나이트 움직임에 대한 테스트 코드 추가
    Co-authored-by: Hyunguk Ryu <hw0603@naver.com>

---
+++ src/test/java/domain/strategy/KnightMoveStrategyTest.java
@@ -0,0 +1,47 @@
+package domain.strategy;
+
+import domain.position.File;
+import domain.position.Position;
+import domain.position.Rank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KnightMoveStrategyTest {
+
+    @DisplayName("나이트가 이동할 수 있는 출발지/도착지면 true를 반환한다.")
+    @Test
+    void isMovableTest() {
+        // Given
+        Position source = new Position(File.C, Rank.TWO);
+        Position destination = new Position(File.D, Rank.FOUR);
+        Set<Position> otherPositions = Set.of(new Position(File.C, Rank.THREE));
+        KnightMoveStrategy knightMoveStrategy = new KnightMoveStrategy();
+
+        // When
+        boolean movable = knightMoveStrategy.isMovable(source, destination, otherPositions);
+
+        // Then
+        assertThat(movable).isTrue();
+    }
+
+    @DisplayName("나이트가 이동할 수 없는 출발지/도착지면 false를 반환한다.")
+    @Test
+    void isNotMovableTest() {
+        // Given
+        Position source = new Position(File.C, Rank.TWO);
+        Position destination = new Position(File.G, Rank.FIVE);
+        Set<Position> otherPositions = Set.of(new Position(File.C, Rank.THREE));
+        KnightMoveStrategy knightMoveStrategy = new KnightMoveStrategy();
+
+        // When
+        boolean movable = knightMoveStrategy.isMovable(source, destination, otherPositions);
+
+        // Then
+        assertThat(movable).isFalse();
+    }
+
+}
