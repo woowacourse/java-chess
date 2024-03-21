1dd44eb45585871da37744090717b8f323391919
DobbyMac <kimdobby@g.skku.edu>
2024-03-21 18:45:04 +0900

    refactor(InputView): 예외 발생 메세지 작성 및  매직 넘버 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class InputView {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    public static final String POSITION_REGEX = "^[a-h][1-8]$";
 
     private InputView() {
     }
@@ -16,7 +17,7 @@ public class InputView {
         List<String> inputCommand = Arrays.asList(readLine().split(" "));
         GameCommand initialGameCommand = GameCommand.from(inputCommand);
         if (initialGameCommand == GameCommand.MOVE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("게임을 시작해야 이동 명령을 입력할 수 있습니다.");
         }
         return initialGameCommand;
     }
@@ -34,7 +35,7 @@ public class InputView {
         if (inputMovement.size() == 3) {
             inputMovement.subList(1, inputMovement.size())
                     .stream()
-                    .filter(position -> !position.matches("^[a-h][1-8]$"))
+                    .filter(position -> !position.matches(POSITION_REGEX))
                     .findAny()
                     .ifPresent(position -> {
                         throw new IllegalArgumentException("입력하신 " + position + "은 올바르지 않은 좌표입니다");
