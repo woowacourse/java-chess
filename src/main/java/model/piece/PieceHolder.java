b8e42e80092bdabe118be902c36f12e256824c67
DobbyMac <kimdobby@g.skku.edu>
2024-03-21 17:46:23 +0900

    feat(PieceHolder): 상태패턴 기능 적용

--- src/main/java/model/piece/PieceHolder.java
+++ src/main/java/model/piece/PieceHolder.java
@@ -1,6 +1,9 @@
 package model.piece;
 
 import model.piece.state.Role;
+import model.piece.state.Square;
+import model.position.Position;
+import model.position.Route;
 
 public class PieceHolder {
     private Role role;
@@ -15,5 +18,22 @@ public class PieceHolder {
 
     public Color color() {
         return role.getColor();
+    }
+
+    public Route findRoute(Position source, Position destination) {
+        return this.role.findRoute(source, destination);
+    }
+
+    public boolean isOccupied() {
+        return role.isOccupied();
+    }
+
+    public void moveFrom(PieceHolder source) {
+        this.role.checkSameCamp(source.role);
+        this.role = source.role;
+    }
+
+    public void leave() {
+        this.role = new Square();
     }
 }
