d00940350b23e397f0edf4c2d7c3aea74eb6be0e
takoyakimchi <c4nyou@gmail.com>
2024-03-21 11:15:10 +0900

    feat: 유저의 말 이동 요청을 처리하는 기능 구현
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/ChessApplication.java
+++ src/main/java/ChessApplication.java
@@ -1,18 +1,38 @@
+import controller.ChessController;
 import domain.board.Board;
 import domain.board.InitialBoardGenerator;
 import view.InputView;
 import view.OutputView;
+import view.dto.MovePositionDto;
 
 public class ChessApplication {
 
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
+    private static final String MOVE_COMMAND = "move";
+
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
+    private static final ChessController controller = new ChessController();
 
     public static void main(String[] args) {
         outputView.printStartMessage();
-
-        while (inputView.readStartCommand()) {
+        String command = inputView.readCommand();
+        if (command.equals(START_COMMAND)) {
             outputView.printBoard(Board.generatedBy(new InitialBoardGenerator()).getSquares());
+
+            boolean isEnd = false;
+            while (!isEnd) {
+                String gameCommand = inputView.readCommand();
+                if (gameCommand.equals(END_COMMAND)) {
+                    isEnd = true;
+                }
+                if (gameCommand.startsWith(MOVE_COMMAND)) {
+                    MovePositionDto movePositionDto = MovePositionDto.from(gameCommand);
+                    Board board = controller.move(movePositionDto);
+                    outputView.printBoard(board.getSquares());
+                }
+            }
         }
     }
 }
