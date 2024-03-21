51d96d23a2e624b669bc42ef6b867fb43dff538c
tackyu <tack970913@gmail.com>
2024-03-21 16:08:21 +0900

    feat(OutputView) : 기물 역할에 따른 기물 출력 맵 생성

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -1,25 +1,47 @@
 package chess.view;
 
-import chess.domain.ChessPiece;
+import chess.domain.Line;
 import chess.domain.Column;
+import chess.domain.chessPiece.Role;
+import chess.domain.chessPiece.Piece;
 
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class OutputView {
+    private static final EnumMap<Role, String> pieceBoard = initializePiece();
 
-    public static void printChessBoard(Map<Column, List<ChessPiece>> chessBoard) {
+    public static void printChessBoard(Map<Column, Line> chessBoard) {
         chessBoard.values()
-                .forEach(OutputView::printOneRow);
+                .forEach(line -> printOneLine(line.getLine()));
     }
 
-    private static void printOneRow(List<ChessPiece> row) {
-        row.forEach(chessPiece -> System.out.print(chessPiece.getName()));
+    private static void printOneLine(List<Piece> line) {
+        line.forEach(chessPiece -> System.out.print(pieceBoard.get(chessPiece.getRole())));
         System.out.println();
     }
 
     public static void printStartMessage() {
         System.out.println("체스 게임을 시작합니다.");
         System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+    }
+
+    private static EnumMap<Role, String> initializePiece() {
+        EnumMap<Role, String> pieceBoard = new EnumMap<>(Role.class);
+        pieceBoard.put(Role.BLACK_KING, "K");
+        pieceBoard.put(Role.WHITE_KING, "k");
+        pieceBoard.put(Role.BLACK_QUEEN, "Q");
+        pieceBoard.put(Role.WHITE_QUEEN, "q");
+        pieceBoard.put(Role.BLACK_ROOK, "R");
+        pieceBoard.put(Role.WHITE_ROOK, "r");
+        pieceBoard.put(Role.BLACK_BISHOP, "B");
+        pieceBoard.put(Role.WHITE_BISHOP, "b");
+        pieceBoard.put(Role.BLACK_KNIGHT, "N");
+        pieceBoard.put(Role.WHITE_KNIGHT, "n");
+        pieceBoard.put(Role.BLACK_PAWN, "P");
+        pieceBoard.put(Role.WHITE_PAWN, "p");
+        pieceBoard.put(Role.EMPTY, ".");
+        return pieceBoard;
     }
 }
