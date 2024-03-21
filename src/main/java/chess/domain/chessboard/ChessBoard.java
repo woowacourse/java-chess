3d293e5ab7d59c5539f25445b413c4747648666c
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-21 11:11:05 +0900

    feat(ChessBoardDto): dto 클래스 구현
    Co-authored-by: lilychoibb <choiyoonseo02@gmail.com>

--- src/main/java/chess/domain/chessboard/ChessBoard.java
+++ src/main/java/chess/domain/chessboard/ChessBoard.java
@@ -1,8 +1,12 @@
 package chess.domain.chessboard;
 
 import chess.domain.chesspiece.ChessPiece;
+import chess.dto.ChessBoardDto;
+import chess.dto.ChessPieceDto;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 public class ChessBoard {
 
@@ -16,5 +20,26 @@ public class ChessBoard {
 
     public ChessBoard() {
         this(BOARD_GENERATOR.generate());
+    }
+
+    public ChessBoardDto createDto() {
+        Map<Square, Optional<ChessPieceDto>> chessBoardInfo = new LinkedHashMap<>();
+
+        for (Square square : board.keySet()) {
+            Optional<ChessPiece> chessPiece = board.get(square);
+            Optional<ChessPieceDto> chessPieceDto = createChessPieceDto(chessPiece);
+            chessBoardInfo.put(square, chessPieceDto);
+        }
+
+        return new ChessBoardDto(chessBoardInfo);
+    }
+
+    private Optional<ChessPieceDto> createChessPieceDto(Optional<ChessPiece> chessPiece) {
+        if (chessPiece.isEmpty()) {
+            return Optional.empty();
+        }
+
+        ChessPiece existChessPiece = chessPiece.get();
+        return Optional.of(existChessPiece.createDto());
     }
 }
