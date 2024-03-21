2b7ac69de886852d1ae2875af705adc66ef86f23
J-I-H-O <jeongjiho0731@gmail.com>
2024-03-21 15:42:49 +0900

    feat: 컨트롤러 초기 구현
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,9 +1,12 @@
 package chess.controller;
 
 import chess.domain.ChessBoard;
+import chess.domain.ChessBoardFactory;
+import chess.domain.Position;
 import chess.view.GameStartCommand;
 import chess.view.InputView;
 import chess.view.OutputView;
+import java.util.List;
 
 // TODO: 이름 고민 - er??
 public class ChessController {
@@ -17,11 +20,28 @@ public class ChessController {
     }
 
     public void run() {
-        final ChessBoard chessBoard = new ChessBoard();
+        final ChessBoard chessBoard = ChessBoardFactory.makeChessBoard();
 
         GameStartCommand gameStartCommand = inputView.readGameStartCommand();
         if (GameStartCommand.START.equals(gameStartCommand)) {
             outputView.printChessBoard(chessBoard);
         }
+        
+        // TODO: 검증
+        List<String> positions = inputView.readPositions();
+        String rawSourcePosition = positions.get(0);
+        String rawTargetPosition = positions.get(1);
+
+        char sourceFile = rawSourcePosition.substring(0, 1).charAt(0);
+        int sourceRank = Integer.parseInt(rawSourcePosition.substring(1, 2));
+        Position source = Position.of(sourceFile, sourceRank);
+
+        char targetFile = rawTargetPosition.substring(0, 1).charAt(0);
+        int targetRank = Integer.parseInt(rawTargetPosition.substring(1, 2));
+        Position target = Position.of(targetFile, targetRank);
+
+        chessBoard.move(source, target);
+
+        outputView.printChessBoard(chessBoard);
     }
 }
