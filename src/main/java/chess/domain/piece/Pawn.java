717d7c97efbe49ffeef5257bcd914d601f44075f
libienz <leekh10195@gmail.com>
2024-03-21 09:05:04 +0900

    feat: Pawn 킬 행마법 구현

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -3,24 +3,29 @@ package chess.domain.piece;
 import chess.domain.position.Position;
 
 public class Pawn extends Piece {
+
+    public static final int KILL_PASSING_DISTANCE = 2;
+
     public Pawn(Team team) {
         super(team);
     }
 
     @Override
     public boolean canMove(Position start, Position destination) {
-        //폰의 전진방향이 팀의 전진방향과 같고 움직이는 양이 1이면 움직일 수 있다
         if (isForward(start, destination) && start.squaredDistanceWith(destination) == 1) {
             return true;
         }
-
-        //폰의 전진양이 2이고 폰이 초기위치 이면 전진할 수 있다.
         if (isForward(start, destination) && start.squaredDistanceWith(destination) == 2 && start.isStraightWith(
                 destination) && start.rowIs(teamInitialPawnRow())) {
             return true;
         }
-        //잡아먹는 경우 (위 대각선 두방향도 일단 true 반환)
         return false;
+    }
+
+    //TODO: 테스트 구현하
+    public boolean isKillPassing(Position start, Position destination) {
+        return isForward(start, destination) && start.isDiagonalWith(destination)
+                && start.squaredDistanceWith(destination) == KILL_PASSING_DISTANCE;
     }
 
     private boolean isForward(Position start, Position destination) {
