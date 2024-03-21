68913fb752c40326e8e5d91c7a86ea74be40f4f6
Minjoo <m0522inju@gmail.com>
2024-03-21 15:55:19 +0900

    style(ColumnTest): 사용하지 않는 import 구문 제거

--- src/test/java/chess/controller/ColumnTest.java
+++ src/test/java/chess/controller/ColumnTest.java
@@ -3,7 +3,6 @@ package chess.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import chess.controller.Column;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
