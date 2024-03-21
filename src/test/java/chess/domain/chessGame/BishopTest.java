959097e4e81e1c29311be3c784675729c7e63d9a
jhon3242 <rldotnr2@gmail.com>
2024-03-21 11:52:45 +0900

    feat(BishopMoveStrategy) : 비숍 이동 전략에 따른 이동할 수 있는 위치를 계산하는 기능 구현
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

---
+++ src/test/java/chess/domain/chessGame/BishopTest.java
@@ -0,0 +1,50 @@
+package chess.domain.chessGame;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.Board;
+import chess.domain.ChessGame;
+import chess.domain.Color;
+import chess.domain.Column;
+import chess.domain.Piece;
+import chess.domain.PieceType;
+import chess.domain.Position;
+import chess.domain.Row;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BishopTest {
+    /**
+     * ........  8 (rank 8)
+     * ........  7
+     * ........  6
+     * ........  5
+     * ........  4
+     * ....P...  3
+     * .p......  2
+     * ..b.....  1 (rank 1)
+     *
+     * abcdefgh
+     */
+    @Test
+    @DisplayName("실제로 움직일 수 있는 위치를 모두 가져온다.")
+    void generateMovablePositions() {
+        Position targetPosition = new Position(Row.RANK1, Column.c);
+        ChessGame chessGame = new ChessGame(new Board(
+                Map.of(
+                        targetPosition, new Piece(PieceType.BISHOP, Color.WHITE),
+                        new Position(Row.RANK2, Column.b), new Piece(PieceType.WHITE_PAWN, Color.WHITE),
+                        new Position(Row.RANK3, Column.e), new Piece(PieceType.BLACK_PAWN, Color.BLACK)
+                )
+        ));
+
+        List<Position> result = chessGame.generateMovablePositions(targetPosition);
+
+        assertThat(result).containsExactlyInAnyOrder(
+                new Position(Row.RANK2, Column.d),
+                new Position(Row.RANK3, Column.e)
+        );
+    }
+}
---
+++ src/test/java/chess/domain/strategy/BishopMoveStrategyTest.java
@@ -0,0 +1,94 @@
+package chess.domain.strategy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import chess.domain.Column;
+import chess.domain.Direction;
+import chess.domain.Position;
+import chess.domain.Row;
+import java.util.Deque;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BishopMoveStrategyTest {
+
+    /**
+     * ........  8 (rank 8)
+     * ........  7
+     * ........  6
+     * ........  5
+     * ........  4
+     * ........  3
+     * .b......  2
+     * ........  1 (rank 1)
+     *
+     * abcdefgh
+     */
+    @Test
+    @DisplayName("비숍이 B2 에 있을 때 방향에 따라 움직일 수 있는 후보 포지션을 차례대로 저장한다.")
+    void calculateCandidateOnEdgePosition() {
+        Position position = new Position(Row.RANK2, Column.b);
+        BishopMoveStrategy bishopMoveStrategy = new BishopMoveStrategy();
+
+        Map<Direction, Deque<Position>> directionListMap = bishopMoveStrategy.generateMovablePositions(position);
+
+        assertAll(
+                () -> Assertions.assertThat(directionListMap.get(Direction.NE)).containsExactly(
+                        new Position(Row.RANK3, Column.c),
+                        new Position(Row.RANK4, Column.d),
+                        new Position(Row.RANK5, Column.e),
+                        new Position(Row.RANK6, Column.f),
+                        new Position(Row.RANK7, Column.g),
+                        new Position(Row.RANK8, Column.h)
+                ),
+                () -> Assertions.assertThat(directionListMap.get(Direction.SE)).containsExactly(
+                        new Position(Row.RANK1, Column.c)
+                ),
+                () -> Assertions.assertThat(directionListMap.get(Direction.SW)).containsExactly(
+                        new Position(Row.RANK1, Column.a)
+                ),
+                () -> Assertions.assertThat(directionListMap.get(Direction.NW)).containsExactly(
+                        new Position(Row.RANK3, Column.a)
+                )
+        );
+    }
+
+    /**
+     * ........  8 (rank 8)
+     * ........  7
+     * ........  6
+     * ........  5
+     * ........  4
+     * ........  3
+     * ........  2
+     * ..b.....  1 (rank 1)
+     *
+     * abcdefgh
+    */
+    @Test
+    @DisplayName("비숍이 C1 에 있을 때 방향에 따라 움직일 수 있는 후보 포지션을 차례대로 저장한다.")
+    void calculateCandidateOnCenterPosition() {
+        Position position = new Position(Row.RANK1, Column.c);
+        BishopMoveStrategy bishopMoveStrategy = new BishopMoveStrategy();
+
+        Map<Direction, Deque<Position>> directionListMap = bishopMoveStrategy.generateMovablePositions(position);
+
+        assertAll(
+                () -> Assertions.assertThat(directionListMap.get(Direction.NE)).containsExactly(
+                        new Position(Row.RANK2, Column.d),
+                        new Position(Row.RANK3, Column.e),
+                        new Position(Row.RANK4, Column.f),
+                        new Position(Row.RANK5, Column.g),
+                        new Position(Row.RANK6, Column.h)
+                ),
+                () -> Assertions.assertThat(directionListMap.get(Direction.SE)).isEmpty(),
+                () -> Assertions.assertThat(directionListMap.get(Direction.SW)).isEmpty(),
+                () -> Assertions.assertThat(directionListMap.get(Direction.NW)).containsExactly(
+                        new Position(Row.RANK2, Column.b),
+                        new Position(Row.RANK3, Column.a)
+                )
+        );
+    }
+}
