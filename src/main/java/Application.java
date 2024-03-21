95e5b974cd23c79f1aef486eab362a288c84134a
unifolio0 <boho82ha@gmail.com>
2024-03-21 09:46:51 +0900

    feat : 처음에 start외 다른 명령어 입력시 예외 처리 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -19,11 +19,18 @@ public class Application {
         InputController inputController = new InputController(inputView, outputView);
         outputView.printStartMessage();
         Camp camp = Camp.WHITE;
+        boolean start = false;
 
         while (true) {
             String input = inputView.readCommand();
             Command command = Command.from(input);
-            if (command == Command.MOVE) {
+            if (command == Command.START && start) {
+                throw new IllegalArgumentException("이미 게임이 진행중입니다.");
+            }
+            if (command == Command.START && !start) {
+                start = true;
+            }
+            if (command == Command.MOVE && start) {
                 List<String> cmd = List.of(input.split(" ")); // TODO 문자로 위치가 주어졌을 때 위치 찾는 테스트 추가하기
                 Moving moving = new Moving(Position.from(cmd.get(1)), Position.from(cmd.get(2)));
                 gameBoard.move(moving, camp);
@@ -32,8 +39,11 @@ public class Application {
             if (command == Command.END) {
                 break;
             }
+            if (!start) {
+                throw new IllegalArgumentException("start하고 시작하세요");
+            }
             outputView.printGameBoard(GameBoardDto.from(gameBoard));
-            System.out.println("현재 턴: " + camp.toString());
+            System.out.println("현재 턴: " + camp.toString() + "\n");
         }
     }
 
