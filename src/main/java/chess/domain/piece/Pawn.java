d9d1ac87c62e3fd069de5c6f43b9a0905c4e9743
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-21 13:49:10 +0900

    feat: 초기 폰의 위치에서 2칸 전진 기능 구현

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,6 +1,6 @@
 package chess.domain.piece;
 
-import chess.domain.square.Square;
+import chess.domain.Movement;
 
 public class Pawn extends Piece {
 
@@ -8,12 +8,52 @@ public class Pawn extends Piece {
         super(color, Type.PAWN);
     }
 
+    // TODO: 상대가 있을 경우 대각선 움직임 구현하기
     @Override
-    public boolean canMove(final Square from, final Square to) {
-        int rankDiff = to.getRankIndex() - from.getRankIndex();
+    public boolean canMove(final Movement movement) {
+        int rankSource = movement.getSourceRankIndex();
+        int moveDistance = movement.getRankDifference();
         if (isSameColor(Color.WHITE)) {
-            return rankDiff == 1;
+            return isWhiteMove(rankSource, moveDistance);
         }
-        return rankDiff == -1;
+        return isBlackMove(rankSource, moveDistance);
+    }
+
+    private boolean isWhiteMove(final int rankSource, final int moveDistance) {
+        if (isWhiteInitialSquare(rankSource)) {
+            return isWhiteFirstMove(moveDistance) || isWhiteDefaultMove(moveDistance);
+        }
+        return isWhiteDefaultMove(moveDistance);
+    }
+
+    private boolean isWhiteInitialSquare(final int moveDistance) {
+        return moveDistance == 1;
+    }
+
+    private boolean isWhiteFirstMove(final int moveDistance) {
+        return moveDistance == 2;
+    }
+
+    private boolean isWhiteDefaultMove(final int moveDistance) {
+        return moveDistance == 1;
+    }
+
+    private boolean isBlackMove(final int rankSource, final int moveDistance) {
+        if (isBlackInitialSquare(rankSource)) {
+            return isBlackFirstMove(moveDistance) || isBlackDefaultMove(moveDistance);
+        }
+        return isBlackDefaultMove(moveDistance);
+    }
+
+    private boolean isBlackInitialSquare(final int moveDistance) {
+        return moveDistance == 6;
+    }
+
+    private boolean isBlackFirstMove(final int moveDistance) {
+        return moveDistance == -2;
+    }
+
+    private boolean isBlackDefaultMove(final int moveDistance) {
+        return moveDistance == -1;
     }
 }
