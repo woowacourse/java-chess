e81841cf46207149114872203fcd56b82466a2f9
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-21 16:37:03 +0900

    refactor: 메서드 분리

--- src/main/java/chess/Game.java
+++ src/main/java/chess/Game.java
@@ -9,6 +9,8 @@ import chess.view.OutputView;
 import java.util.List;
 
 public class Game {
+    private static final String START_COMMAND = "start";
+
     private final InputView inputView;
     private final OutputView outputView;
 
@@ -21,15 +23,19 @@ public class Game {
         outputView.printGameStartMessage();
         Board board = BoardFactory.createBoard();
         String command = inputView.readStartCommand();
-
         Color nowTurn = Color.WHITE;
-        while (command.equals("start")) {
-            outputView.printBoard(board.createBoardStatus());
-            List<String> movement = inputView.readMovement();
-            Square from = Square.from(movement.get(0));
-            Square to = Square.from(movement.get(1));
-            board.move(from, to, nowTurn);
+
+        while (command.equals(START_COMMAND)) {
+            moveToCommand(board, nowTurn);
             nowTurn = nowTurn.exchangeTurn();
         }
+    }
+
+    private void moveToCommand(final Board board, final Color nowTurn) {
+        outputView.printBoard(board.createBoardStatus());
+        List<String> movement = inputView.readMovement();
+        Square from = Square.from(movement.get(0));
+        Square to = Square.from(movement.get(1));
+        board.move(from, to, nowTurn);
     }
 }
