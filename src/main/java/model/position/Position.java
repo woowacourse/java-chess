ab5551ba1c6c136dca111e8035eb4d5749fe5623
DobbyMac <kimdobby@g.skku.edu>
2024-03-21 16:14:03 +0900

    refactor(Position): 패키지 분리 리팩터링

--- src/main/java/model/position/Position.java
+++ src/main/java/model/position/Position.java
@@ -1,4 +1,4 @@
-package model;
+package model.position;
 
 import java.util.HashSet;
 import java.util.Objects;
--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import model.direction.MovingPattern;
+import model.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
