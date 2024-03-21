d1942cce318dc2f3dc0abd8f591237df9cc1c00e
ikjo39 <wer04129@gmail.com>
2024-03-21 16:08:56 +0900

    test: 체스 판 움직임 테스트 추가

---
+++ src/test/java/chess/model/ChessBoardTest.java
@@ -0,0 +1,59 @@
+package chess.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChessBoardTest {
+
+    private ChessBoard chessBoard;
+
+    @BeforeEach
+    void setUp() {
+        chessBoard = new ChessBoard(new ChessBoardInitializer().create());
+    }
+
+    @Test
+    @DisplayName("Source 위치의 기물을 Target 위치로 이동한다.")
+    void move() {
+        ChessPosition source = new ChessPosition(File.B, Rank.TWO);
+        ChessPosition target = new ChessPosition(File.B, Rank.FOUR);
+        chessBoard.move(source, target);
+
+        Map<ChessPosition, Piece> board = chessBoard.getBoard();
+
+        assertThat(board.get(source)).isNull();
+        assertThat(board.get(target)).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Source 위치에 기물이 없으면 예외가 발생한다.")
+    void moveNullSource() {
+        ChessPosition source = null;
+        ChessPosition target = new ChessPosition(File.D, Rank.TWO);
+        assertThatThrownBy(() -> chessBoard.move(source, target))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("경로가 비어있다면 예외가 발생한다.")
+    void moveWhenPathEmpty() {
+        ChessPosition source = new ChessPosition(File.A, Rank.ONE);
+        ChessPosition target = new ChessPosition(File.D, Rank.TWO);
+        assertThatThrownBy(() -> chessBoard.move(source, target))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이동 경로에 기물이 존재한다면 예외가 발생한다.")
+    void moveWhenPathContainsPiece() {
+        ChessPosition source = new ChessPosition(File.A, Rank.ONE);
+        ChessPosition target = new ChessPosition(File.A, Rank.SIX);
+        assertThatThrownBy(() -> chessBoard.move(source, target))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
