ee65fa6592dbe56b3e9a8b95b0529111dfbedcf6
reddevilmidzy <midzy0228@gmail.com>
2024-03-21 13:09:05 +0900

    test: 비숍 이동 경로 확인 테스트

--- src/test/java/model/piece/BishopTest.java
+++ src/test/java/model/piece/BishopTest.java
@@ -1,5 +1,8 @@
 package model.piece;
 
+import static model.position.Position.from;
+
+import java.util.Set;
 import java.util.stream.Stream;
 import model.Camp;
 import model.position.Moving;
@@ -34,10 +37,10 @@ class BishopTest {
 
     static Stream<Arguments> cantMovableParameterProvider() {
         return Stream.of(
-                Arguments.of(new Moving(Position.from("c8"), Position.from("a8"))),
-                Arguments.of(new Moving(Position.from("c8"), Position.from("c3"))),
-                Arguments.of(new Moving(Position.from("f8"), Position.from("e8"))),
-                Arguments.of(new Moving(Position.from("g4"), Position.from("c3")))
+                Arguments.of(new Moving(from("c8"), from("a8"))),
+                Arguments.of(new Moving(from("c8"), from("c3"))),
+                Arguments.of(new Moving(from("f8"), from("e8"))),
+                Arguments.of(new Moving(from("g4"), from("c3")))
         );
     }
 
@@ -45,19 +48,28 @@ class BishopTest {
     @DisplayName("이동할 수 있는 경로면 true를 반환한다.")
     @ParameterizedTest
     @MethodSource("canMovableParameterProvider")
-    void canMovable(Moving moving, final int size) {
+    void canMovable(Moving moving, Set<Position> expected) {
         Bishop bishop = new Bishop(Camp.BLACK);
 
         Assertions.assertThat(bishop.canMovable(moving)).isTrue();
-        Assertions.assertThat(bishop.getRoute(moving)).hasSize(size);
+    }
+
+    @DisplayName("이동할 수 있는 경로면 true를 반환한다.")
+    @ParameterizedTest
+    @MethodSource("canMovableParameterProvider")
+    void checkRoute(Moving moving, Set<Position> expected) {
+        Bishop bishop = new Bishop(Camp.BLACK);
+
+        Assertions.assertThat(bishop.getRoute(moving)).isEqualTo(expected);
     }
 
     static Stream<Arguments> canMovableParameterProvider() {
         return Stream.of(
-                Arguments.of(new Moving(Position.from("c8"), Position.from("e6")), 1),
-                Arguments.of(new Moving(Position.from("f8"), Position.from("a3")), 4),
-                Arguments.of(new Moving(Position.from("c4"), Position.from("a6")), 1),
-                Arguments.of(new Moving(Position.from("c4"), Position.from("f7")), 2)
+                Arguments.of(new Moving(from("c8"), from("e6")), Set.of(from("d7"))),
+                Arguments.of(new Moving(from("f8"), from("a3")),
+                        Set.of(from("e7"), from("d6"), from("c5"), from("b4"))),
+                Arguments.of(new Moving(from("c4"), from("a6")), Set.of(from("b5"))),
+                Arguments.of(new Moving(from("c4"), from("f7")), Set.of(from("d5"), from("e6")))
         );
     }
 }
