54deee4691530a5803940b498534ce18598d834f
J-I-H-O <jeongjiho0731@gmail.com>
2024-03-21 15:42:06 +0900

    feat: 시작 메뉴 출력 기능 구현
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -2,10 +2,16 @@ package chess.view;
 
 import chess.domain.ChessBoard;
 import chess.domain.piece.Piece;
-
 import java.util.List;
 
 public class OutputView {
+
+    public void printCommandInformation() {
+        System.out.println("> 체스 게임을 시작합니다.\n"
+                + "> 게임 시작 : start\n"
+                + "> 게임 종료 : end\n"
+                + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+    }
 
     public void printChessBoard(final ChessBoard chessBoard) {
         List<Piece> pieces = chessBoard.findAllPieces();
