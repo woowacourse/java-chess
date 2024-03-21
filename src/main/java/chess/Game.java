10166d4cac432e7bfc6c74512bf352956bdb0390
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-22 00:32:51 +0900

    refactor: DTO 생성 기능 추가

--- src/main/java/chess/Game.java
+++ src/main/java/chess/Game.java
@@ -3,10 +3,15 @@ package chess;
 import chess.domain.board.Board;
 import chess.domain.board.BoardFactory;
 import chess.domain.pieces.piece.Color;
+import chess.domain.pieces.piece.Piece;
+import chess.domain.pieces.piece.PieceResponse;
 import chess.domain.square.Square;
 import chess.view.InputView;
 import chess.view.OutputView;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Game {
     private static final String START_COMMAND = "start";
@@ -22,20 +27,26 @@ public class Game {
     public void start() {
         outputView.printGameStartMessage();
         Board board = BoardFactory.createBoard();
+
         String command = inputView.readStartCommand();
         Color nowTurn = Color.WHITE;
-
         while (command.equals(START_COMMAND)) {
+            outputView.printBoard(createBoardStatus(board.getPieces()));
             moveToCommand(board, nowTurn);
             nowTurn = nowTurn.exchangeTurn();
         }
     }
 
     private void moveToCommand(final Board board, final Color nowTurn) {
-        outputView.printBoard(board.createBoardStatus());
         List<String> movement = inputView.readMovement();
         Square from = Square.from(movement.get(0));
         Square to = Square.from(movement.get(1));
         board.move(from, to, nowTurn);
+    }
+
+    private List<PieceResponse> createBoardStatus(final Map<Square, Piece> pieces) {
+        return pieces.entrySet().stream()
+                .map(entry -> new PieceResponse(entry.getKey(), entry.getValue()))
+                .toList();
     }
 }
