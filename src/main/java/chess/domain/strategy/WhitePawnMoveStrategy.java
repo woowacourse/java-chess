cece18687ee9094d9084036f00251c1c343d8b7d
jhon3242 <rldotnr2@gmail.com>
2024-03-21 11:31:00 +0900

    feat(WhitePawnMoveStrategy) : 화이트 폰 이동 전략에 따른 이동할 수 있는 위치를 계산하는 기능 구현
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

--- src/main/java/chess/domain/strategy/WhitePawnMoveStrategy.java
+++ src/main/java/chess/domain/strategy/WhitePawnMoveStrategy.java
@@ -2,13 +2,37 @@ package chess.domain.strategy;
 
 import chess.domain.Direction;
 import chess.domain.Position;
+import chess.domain.Row;
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+// TODO 폰 추상화
 public class WhitePawnMoveStrategy implements MoveStrategy{
+
+    private static final int DEFAULT_MAX_MOVE_DISTANCE = 1;
+
+    private final List<Direction> directions = List.of(Direction.N, Direction.NE, Direction.NW);
 
     @Override
     public Map<Direction, Deque<Position>> generateMovablePositions(Position position) {
-        return null;
+        return directions.stream()
+                .collect(Collectors.toMap(
+                        direction -> direction,
+                        direction -> generateMovablePositionsByDirection(position, direction)
+                ));
+    }
+
+    private Deque<Position> generateMovablePositionsByDirection(Position currentPosition, Direction direction) {
+        int movableMaxDistance = currentPosition.calculateMaxDistance(direction, DEFAULT_MAX_MOVE_DISTANCE);
+        if (currentPosition.isSameRow(Row.RANK2) && direction == Direction.N) {
+            movableMaxDistance++;
+        }
+        return new ArrayDeque<>(IntStream.rangeClosed(1, movableMaxDistance)
+                .mapToObj(weight -> currentPosition.calculateNextPosition(direction, weight))
+                .toList());
     }
 }
---
+++ src/test/java/chess/domain/strategy/WhitePawnMoveStrategyTest.java
@@ -0,0 +1,76 @@
+package chess.domain.strategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import chess.domain.Column;
+import chess.domain.Direction;
+import chess.domain.PieceType;
+import chess.domain.Position;
+import chess.domain.Row;
+import java.util.Deque;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WhitePawnMoveStrategyTest {
+
+    /**
+     * ........  8 (rank 8)
+     * ........  7
+     * ........  6
+     * ........  5
+     * ........  4
+     * ........  3
+     * ......p.  2
+     * ........  1 (rank 1)
+     *
+     * abcdefgh
+     */
+    @Test
+    @DisplayName("화이트 폰은 시작 위치에서는 아래로 2칸을 움직일 수 있다.")
+    void canMoveTwoDistanceAtStartPositionTest() {
+        PieceType whitePawn = PieceType.WHITE_PAWN;
+
+        Map<Direction, Deque<Position>> directionListMap = whitePawn.calculateAllDirectionPositions(
+                new Position(Row.RANK2, Column.g));
+
+        assertAll(
+                () -> assertThat(directionListMap.get(Direction.N)).containsExactly(
+                        new Position(Row.RANK3, Column.g),
+                        new Position(Row.RANK4, Column.g)),
+                () -> assertThat(directionListMap.get(Direction.S)).isNull(),
+                () -> assertThat(directionListMap.get(Direction.E)).isNull(),
+                () -> assertThat(directionListMap.get(Direction.W)).isNull(),
+                () -> assertThat(directionListMap.get(Direction.NE)).containsExactly(new Position(Row.RANK3, Column.h)),
+                () -> assertThat(directionListMap.get(Direction.NW)).containsExactly(new Position(Row.RANK3, Column.f))
+        );
+    }
+
+    /**
+     * ........  8 (rank 8)
+     * ........  7
+     * ........  6
+     * ........  5
+     * ........  4
+     * .......p  3
+     * ........  2
+     * ........  1 (rank 1)
+     *
+     * abcdefgh
+     */
+    @Test
+    @DisplayName("화이트 폰은 시작 위치가 아니면 위로 1칸만 움직일 수 있다.")
+    void canMoveOneDistancePositionTest() {
+        PieceType whitePawn = PieceType.WHITE_PAWN;
+
+        Map<Direction, Deque<Position>> directionListMap = whitePawn.calculateAllDirectionPositions(
+                new Position(Row.RANK3, Column.h));
+
+        assertAll(
+                () -> assertThat(directionListMap.get(Direction.N)).containsExactly(
+                        new Position(Row.RANK4, Column.h)
+                ))
+        ;
+    }
+}
