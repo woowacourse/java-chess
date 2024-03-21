47636c855a7055f1f8a5fd7da41b383e9e089451
DobbyMac <kimdobby@g.skku.edu>
2024-03-21 17:48:49 +0900

    feat(Controller): 전체 게임 실행 로직 구현

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,10 +1,12 @@
 package controller;
 
 import static util.InputRetryHelper.inputRetryHelper;
-import static view.OutputView.*;
+import static view.OutputView.printChessBoard;
+import static view.OutputView.printInitialGamePrompt;
 
 import java.util.List;
 import model.ChessBoard;
+import model.position.Position;
 import view.GameCommand;
 import view.InputView;
 import view.dto.InfoMapper;
@@ -12,12 +14,38 @@ import view.dto.PieceInfo;
 
 public class Controller {
 
-    public void execute(){
-        printInitialGamePrompt();
-        while(inputRetryHelper(InputView::inputGameCommand) != GameCommand.END) {
-            ChessBoard chessBoard = new ChessBoard();
+    public void execute() {
+        GameCommand gameCommand = executeInitial();
+        ChessBoard chessBoard = new ChessBoard();
+        while (gameCommand != GameCommand.END) {
             List<PieceInfo> pieceInfos = InfoMapper.toPieceInfoMapper(chessBoard);
             printChessBoard(pieceInfos);
+            gameCommand = inputRetryHelper(() -> runGame(chessBoard));
         }
+    }
+
+    private GameCommand runGame(ChessBoard chessBoard) {
+        GameCommand gameCommand;
+        List<String> inputCommand = InputView.parseCommand();
+        gameCommand = inputRetryHelper(() -> GameCommand.from(inputCommand));
+        controllChessBoard(chessBoard, gameCommand, inputCommand);
+        return gameCommand;
+    }
+
+    private void controllChessBoard(ChessBoard chessBoard, GameCommand gameCommand, List<String> inputCommand) {
+        if (gameCommand == GameCommand.MOVE) {
+            int file = GameCommand.toSourceFileValue(inputCommand);
+            int rank = GameCommand.toSourceRankValue(inputCommand);
+            Position source = Position.of(file, rank);
+            int toFile = GameCommand.toDestinationFileValue(inputCommand);
+            int toRank = GameCommand.toDestinationRankValue(inputCommand);
+            Position destination = Position.of(toFile, toRank);
+            chessBoard.move(source, destination);
+        }
+    }
+
+    private GameCommand executeInitial() {
+        printInitialGamePrompt();
+        return inputRetryHelper(InputView::inputInitialGameCommand);
     }
 }
--- src/main/java/model/ChessBoard.java
+++ src/main/java/model/ChessBoard.java
@@ -3,6 +3,7 @@ package model;
 import java.util.Map;
 import model.piece.PieceHolder;
 import model.position.Position;
+import model.position.Route;
 
 public class ChessBoard {
     private final Map<Position, PieceHolder> chessBoard;
@@ -13,5 +14,21 @@ public class ChessBoard {
 
     public Map<Position, PieceHolder> getChessBoard() {
         return Map.copyOf(chessBoard);
+    }
+
+    public void move(Position source, Position destination) {
+        PieceHolder sourcePiece = chessBoard.get(source);
+        PieceHolder destinationPiece = chessBoard.get(destination);
+        Route route = sourcePiece.findRoute(source, destination);
+        route.getPositions()
+                .stream()
+                .filter(position -> chessBoard.get(position)
+                        .isOccupied())
+                .findFirst()
+                .ifPresent(position -> {
+                    throw new IllegalArgumentException("목적 지점에 기물이 위치하여 이동할 수 없습니다.");
+                });
+        destinationPiece.moveFrom(sourcePiece);
+        sourcePiece.leave();
     }
 }
