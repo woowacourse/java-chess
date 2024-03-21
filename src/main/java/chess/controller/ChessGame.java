369d7cde1f1dacb57a8e83b5f966009707f4d3f8
hyun <osh123chm@gmail.com>
2024-03-21 15:02:40 +0900

    feat: 잘못된 command 입력시 재입력 로직
    Co-authored-by: ashsty <qkralsdk991@naver.com>

--- src/main/java/chess/controller/ChessGame.java
+++ src/main/java/chess/controller/ChessGame.java
@@ -7,25 +7,56 @@ import chess.dto.ChessBoardDto;
 import chess.view.InputView;
 import chess.view.OutputView;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class ChessGame {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
     public void run() {
+        ChessBoard chessBoard = generateChessBoard();
+        GameState gameState = new Ready(chessBoard);
+        outputView.printStartMessage();
+
+        playGame(gameState, chessBoard);
+    }
+
+    private ChessBoard generateChessBoard() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.initialBoard();
-        ChessBoardDto chessBoardDto;
-        GameState gameState = new Ready(chessBoard);
+        return chessBoard;
+    }
 
+    private void playGame(GameState gameState, ChessBoard chessBoard) {
         while (!gameState.isEnd()) {
-            List<String> command = inputView.readCommand();
-            gameState = gameState.play(command);
+            GameState currentGameState = gameState;
+            gameState = repeatUntilSuccess(() -> playEachTurn(currentGameState));
 
-            if (!gameState.isEnd()) {
-                chessBoardDto = chessBoard.convertToDto();
-                outputView.printChessBoard(chessBoardDto);
-            }
+            printChessBoardInProgress(gameState, chessBoard);
+        }
+    }
+
+    private GameState playEachTurn(GameState gameState) {
+        List<String> command = inputView.readCommand();
+        gameState = gameState.play(command);
+        return gameState;
+    }
+
+
+    private void printChessBoardInProgress(GameState gameState, ChessBoard chessBoard) {
+        ChessBoardDto chessBoardDto;
+        if (!gameState.isEnd()) {
+            chessBoardDto = chessBoard.convertToDto();
+            outputView.printChessBoard(chessBoardDto);
+        }
+    }
+
+    private <T> T repeatUntilSuccess(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException | UnsupportedOperationException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return repeatUntilSuccess(supplier);
         }
     }
 }
--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -9,6 +9,18 @@ import chess.dto.ChessBoardDto;
 import java.util.Map;
 
 public class OutputView {
+    private static final String ERROR_HEADER = "[ERROR] ";
+    private static final String NEW_LINE = System.lineSeparator();
+
+    public void printStartMessage() {
+        System.out.println(
+                "> 체스 게임을 시작합니다." + NEW_LINE
+                        + "> 게임 시작 : start" + NEW_LINE
+                        + "> 게임 종료 : end" + NEW_LINE
+                        + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3"
+        );
+    }
+
     public void printChessBoard(ChessBoardDto chessBoardDto) {
         Map<Position, Piece> chessBoard = chessBoardDto.chessBoard();
 
@@ -17,6 +29,10 @@ public class OutputView {
             System.out.println();
         }
         System.out.println();
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(ERROR_HEADER + message);
     }
 
     private void printEachRank(Map<Position, Piece> chessBoard, Rank rank) {
