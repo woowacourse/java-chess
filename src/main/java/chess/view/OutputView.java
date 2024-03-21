3f92a5181704960ab7d8ed0dabe422cb0d3ed58a
hyeonjilee <skylar12200@gmail.com>
2024-03-21 16:41:01 +0900

    feat: 기물 표시 변환기 생성

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -2,12 +2,6 @@ package chess.view;
 
 import chess.domain.piece.Piece;
 import chess.domain.piece.Position;
-import chess.domain.piece.type.Bishop;
-import chess.domain.piece.type.King;
-import chess.domain.piece.type.Night;
-import chess.domain.piece.type.Pawn;
-import chess.domain.piece.type.Queen;
-import chess.domain.piece.type.Rook;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -16,7 +10,7 @@ public class OutputView {
 
     public void printChessBoard(final Set<Piece> pieces) {
         List<List<String>> board = sortByBoardOrder(pieces);
-        board.forEach(row -> printChessRow(row));
+        board.forEach(this::printChessRow);
     }
 
     private void printChessRow(final List<String> row) {
@@ -35,49 +29,9 @@ public class OutputView {
             int fileIndex = position.getFile().getIndex() - 1;
             int rankIndex = 7 - (position.getRank().getNumber() - 1);
             List<String> marks = board.get(rankIndex);
-            marks.set(fileIndex, convertToMark(piece));
+            marks.set(fileIndex, PieceMarkFormatter.convertToMark(piece));
         }
 
         return board;
-    }
-
-    private String convertToMark(final Piece piece) {
-        if (piece instanceof Rook && piece.isBlack()) {
-            return "R";
-        }
-        if (piece instanceof Rook && piece.isWhite()) {
-            return "r";
-        }
-        if (piece instanceof Night && piece.isBlack()) {
-            return "N";
-        }
-        if (piece instanceof Night && piece.isWhite()) {
-            return "n";
-        }
-        if (piece instanceof Bishop && piece.isBlack()) {
-            return "B";
-        }
-        if (piece instanceof Bishop && piece.isWhite()) {
-            return "b";
-        }
-        if (piece instanceof Queen && piece.isBlack()) {
-            return "Q";
-        }
-        if (piece instanceof Queen && piece.isWhite()) {
-            return "q";
-        }
-        if (piece instanceof King && piece.isBlack()) {
-            return "K";
-        }
-        if (piece instanceof King && piece.isWhite()) {
-            return "k";
-        }
-        if (piece instanceof Pawn && piece.isBlack()) {
-            return "P";
-        }
-        if (piece instanceof Pawn && piece.isWhite()) {
-            return "p";
-        }
-        return "";
     }
 }
