b896ea010cdeb162de933b1e46c6521ef1f4f151
kunsanglee <gosmdochee2@gmail.com>
2024-03-21 16:46:42 +0900

    refactor: 체스게임 명령어 코드 개선

--- src/main/java/chess/ChessGame.java
+++ src/main/java/chess/ChessGame.java
@@ -9,23 +9,28 @@ import chess.domain.state.GameState;
 import chess.domain.state.ReadyState;
 import chess.view.InputView;
 import chess.view.OutputView;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class ChessGame {
-    private final Map<GameCommand, CommandExecutor> commands = Map.of(
-            GameCommand.MOVE, args -> move(args.get(1), args.get(2)),
-            GameCommand.START, args -> start(),
-            GameCommand.END, args -> end());
+    private final Map<GameCommand, CommandExecutor> commands = new EnumMap<>(GameCommand.class);
     private final Board board = BoardFactory.createInitialBoard();
     private GameState gameState = new ReadyState();
 
     public void run() {
         try {
+            registerCommands();
             playChess();
         } catch (RuntimeException e) {
             OutputView.printErrorMessage(e.getMessage());
         }
+    }
+
+    private void registerCommands() {
+        commands.put(GameCommand.MOVE, args -> move(args.get(1), args.get(2)));
+        commands.put(GameCommand.START, args -> start());
+        commands.put(GameCommand.END, args -> end());
     }
 
     private void playChess() {
@@ -42,15 +47,15 @@ public class ChessGame {
         commands.get(gameCommand).execute(inputCommand);
     }
 
+    private void start() {
+        gameState = gameState.start();
+        OutputView.printChessBoard(board);
+    }
+
     private void move(String inputSource, String inputTarget) {
         Position source = Position.convert(inputSource);
         Position target = Position.convert(inputTarget);
         gameState = gameState.move(board, source, target);
-        OutputView.printChessBoard(board);
-    }
-
-    private void start() {
-        gameState = gameState.start();
         OutputView.printChessBoard(board);
     }
 
