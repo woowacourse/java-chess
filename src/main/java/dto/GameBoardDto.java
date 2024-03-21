e21cf529049642b1525c309ec8adc965d3a66fe9
reddevilmidzy <midzy0228@gmail.com>
2024-03-22 01:00:54 +0900

    refactor: dto 메서드 분리

--- src/main/java/dto/GameBoardDto.java
+++ src/main/java/dto/GameBoardDto.java
@@ -1,13 +1,15 @@
 package dto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import model.GameBoard;
-import view.message.PieceType;
 import model.piece.Piece;
+import model.position.Column;
 import model.position.Position;
+import model.position.Row;
+import view.message.PieceType;
 
 public class GameBoardDto {
 
@@ -17,35 +19,53 @@ public class GameBoardDto {
         this.value = value;
     }
 
+    // TODO 리팩터링 된건가?
     public static GameBoardDto from(final GameBoard gameBoard) {
         List<List<String>> tmp = createEmptyBoard();
-        Map<Position, Piece> board = gameBoard.getBoard();
+        Map<Position, Piece> pieceOfPosition = gameBoard.getBoard();
 
-        for (Entry<Position, Piece> entry : board.entrySet()) {
-            tmp.get(entry.getKey().getRowIndex()).set(entry.getKey().getColumnIndex(), PieceType.from(entry.getValue()).getValue());
-        }
-
-        List<String> result = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
-            StringBuilder stringBuilder = new StringBuilder();
             for (int j = 0; j < 8; j++) {
-                stringBuilder.append(tmp.get(i).get(j));
+                Position position = new Position(Column.from(j), Row.from(i));
+                tmp.get(i).set(j, convertToString(pieceOfPosition, position));
             }
-            stringBuilder.append("   ").append(8 - i);
-            result.add(stringBuilder.toString());
         }
-        result.add("");
-        result.add("abcdefgh");
+        List<List<String>> paddingBoard = paddingGuideLine(tmp);
+        List<String> result = paddingBoard.stream()
+                .map(list -> String.join("", list))
+                .toList();
         return new GameBoardDto(result);
+    }
+
+
+    private static String convertToString(Map<Position, Piece> board, Position position) {
+        if (board.containsKey(position)) {
+            return PieceType.from(board.get(position)).getValue();
+        }
+        return ".";
+    }
+
+    private static List<List<String>> paddingGuideLine(List<List<String>> board) {
+        List<List<String>> result = new ArrayList<>();
+        for (int i = 0; i < board.size(); i++) {
+            List<String> row = board.get(i);
+            row.add("   " + (board.size() - i));
+            result.add(row);
+        }
+        result.add(new ArrayList<>());
+        List<String> columnInfo = new ArrayList<>();
+        Arrays.stream(Column.values())
+                .forEach(column -> columnInfo.add(column.getValue()));
+        result.add(columnInfo);
+        return result;
     }
 
     private static List<List<String>> createEmptyBoard() {
         List<List<String>> tmp = new ArrayList<>();
-
         for (int i = 0; i < 8; i++) {
             List<String> line = new ArrayList<>();
             for (int j = 0; j < 8; j++) {
-                line.add(".");
+                line.add("");
             }
             tmp.add(line);
         }
