e206d61c945b30f33910b9d8bd8f8cacb5ffcd2d
jhon3242 <rldotnr2@gmail.com>
2024-03-21 16:15:45 +0900

    refactor(SpecialPieceMoveStrategy) : 폰을 제외한 특별 기물의 중복 로직을 추상화
    Co-authored-by: seokmyungham <gkatjraud1@gmail.com>

--- src/main/java/chess/domain/strategy/RookMoveStrategy.java
+++ src/main/java/chess/domain/strategy/RookMoveStrategy.java
@@ -1,33 +1,14 @@
 package chess.domain.strategy;
 
 import chess.domain.Direction;
-import chess.domain.Position;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class RookMoveStrategy implements MoveStrategy{
+public class RookMoveStrategy extends SpecialPieceMoveStrategy{
 
     private static final int DEFAULT_MAX_MOVE_DISTANCE = 7;
+    private static final List<Direction> DIRECTIONS = List.of(Direction.N, Direction.W, Direction.S, Direction.E);
 
-    private final List<Direction> directions = List.of(Direction.N, Direction.W, Direction.S, Direction.E);
-
-    @Override
-    public Map<Direction, Deque<Position>> generateMovablePositions(Position position) {
-        return directions.stream()
-                .collect(Collectors.toMap(
-                        direction -> direction,
-                        direction -> generateMovablePositionsByDirection(position, direction)
-                ));
-    }
-
-    private Deque<Position> generateMovablePositionsByDirection(Position currentPosition, Direction direction) {
-        int movableMaxDistance = currentPosition.calculateMaxDistance(direction, DEFAULT_MAX_MOVE_DISTANCE);
-        return new ArrayDeque<>(IntStream.rangeClosed(1, movableMaxDistance)
-                .mapToObj(weight -> currentPosition.calculateNextPosition(direction, weight))
-                .toList());
+    public RookMoveStrategy() {
+        super(DIRECTIONS, DEFAULT_MAX_MOVE_DISTANCE);
     }
 }
---
+++ src/main/java/chess/domain/strategy/SpecialPieceMoveStrategy.java
@@ -0,0 +1,37 @@
+package chess.domain.strategy;
+
+import chess.domain.Direction;
+import chess.domain.Position;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public abstract class SpecialPieceMoveStrategy implements MoveStrategy {
+
+    private List<Direction> directions;
+    private int defaultMaxMoveDistance;
+
+    public SpecialPieceMoveStrategy(List<Direction> directions, int defaultMaxMoveDistance) {
+        this.directions = directions;
+        this.defaultMaxMoveDistance = defaultMaxMoveDistance;
+    }
+
+    @Override
+    public Map<Direction, Deque<Position>> generateMovablePositions(Position position) {
+        return directions.stream()
+                .collect(Collectors.toMap(
+                        direction -> direction,
+                        direction -> generateMovablePositionsByDirection(position, direction)
+                ));
+    }
+
+    private Deque<Position> generateMovablePositionsByDirection(Position currentPosition, Direction direction) {
+        int movableMaxDistance = currentPosition.calculateMaxDistance(direction, defaultMaxMoveDistance);
+        return new ArrayDeque<>(IntStream.rangeClosed(1, movableMaxDistance)
+                .mapToObj(weight -> currentPosition.calculateNextPosition(direction, weight))
+                .toList());
+    }
+}
