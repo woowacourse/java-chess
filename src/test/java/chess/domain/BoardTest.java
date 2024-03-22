b22fdb4239c234377aaae03059524cca0eb458bf
kunsanglee <gosmdochee2@gmail.com>
2024-03-22 20:53:01 +0900

    test: BoardFactory에서 생성한 보드 결과물 검증 추가

--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -3,9 +3,13 @@ package chess.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import chess.domain.piece.Bishop;
 import chess.domain.piece.Empty;
+import chess.domain.piece.King;
 import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
+import chess.domain.piece.Queen;
 import chess.domain.piece.Rook;
 import chess.domain.position.File;
 import chess.domain.position.Position;
@@ -17,6 +21,49 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
+
+    @DisplayName("초기 보드 생성 후 존재하는 기물들을 전부 확인한다")
+    @Test
+    void createInitialBoard() {
+        Board board = BoardFactory.createInitialBoard();
+
+        Map<Position, Piece> pieces = new HashMap<>();
+        pieces.put(Position.from(File.A, Rank.ONE), Rook.WHITE);
+        pieces.put(Position.from(File.B, Rank.ONE), Knight.WHITE);
+        pieces.put(Position.from(File.C, Rank.ONE), Bishop.WHITE);
+        pieces.put(Position.from(File.D, Rank.ONE), Queen.WHITE);
+        pieces.put(Position.from(File.E, Rank.ONE), King.WHITE);
+        pieces.put(Position.from(File.F, Rank.ONE), Bishop.WHITE);
+        pieces.put(Position.from(File.G, Rank.ONE), Knight.WHITE);
+        pieces.put(Position.from(File.H, Rank.ONE), Rook.WHITE);
+        pieces.put(Position.from(File.A, Rank.TWO), Pawn.ofWhite());
+        pieces.put(Position.from(File.B, Rank.TWO), Pawn.ofWhite());
+        pieces.put(Position.from(File.C, Rank.TWO), Pawn.ofWhite());
+        pieces.put(Position.from(File.D, Rank.TWO), Pawn.ofWhite());
+        pieces.put(Position.from(File.E, Rank.TWO), Pawn.ofWhite());
+        pieces.put(Position.from(File.F, Rank.TWO), Pawn.ofWhite());
+        pieces.put(Position.from(File.G, Rank.TWO), Pawn.ofWhite());
+        pieces.put(Position.from(File.H, Rank.TWO), Pawn.ofWhite());
+
+        pieces.put(Position.from(File.A, Rank.EIGHT), Rook.BLACK);
+        pieces.put(Position.from(File.B, Rank.EIGHT), Knight.BLACK);
+        pieces.put(Position.from(File.C, Rank.EIGHT), Bishop.BLACK);
+        pieces.put(Position.from(File.D, Rank.EIGHT), Queen.BLACK);
+        pieces.put(Position.from(File.E, Rank.EIGHT), King.BLACK);
+        pieces.put(Position.from(File.F, Rank.EIGHT), Bishop.BLACK);
+        pieces.put(Position.from(File.G, Rank.EIGHT), Knight.BLACK);
+        pieces.put(Position.from(File.H, Rank.EIGHT), Rook.BLACK);
+        pieces.put(Position.from(File.A, Rank.SEVEN), Pawn.ofBlack());
+        pieces.put(Position.from(File.B, Rank.SEVEN), Pawn.ofBlack());
+        pieces.put(Position.from(File.C, Rank.SEVEN), Pawn.ofBlack());
+        pieces.put(Position.from(File.D, Rank.SEVEN), Pawn.ofBlack());
+        pieces.put(Position.from(File.E, Rank.SEVEN), Pawn.ofBlack());
+        pieces.put(Position.from(File.F, Rank.SEVEN), Pawn.ofBlack());
+        pieces.put(Position.from(File.G, Rank.SEVEN), Pawn.ofBlack());
+        pieces.put(Position.from(File.H, Rank.SEVEN), Pawn.ofBlack());
+
+        assertThat(board.getPieces()).isEqualTo(pieces);
+    }
 
     @DisplayName("해당 위치에 존재하는 기물을 반환한다")
     @Test
