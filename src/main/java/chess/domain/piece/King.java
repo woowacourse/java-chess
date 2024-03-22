688cb8cbdfaca8c4e8ed8c10b8350586ae1cb6a3
tsulocalize <tsulocalize@gmail.com>
2024-03-22 15:35:34 +0900

    refactor: 킹 이동 규칙 클래스 분리

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -1,16 +1,12 @@
 package chess.domain.piece;
 
+import chess.domain.piece.rule.KingMoveRule;
 import chess.domain.position.Position;
 
 public class King extends Piece {
 
     public King(Color color) {
-        super(color, decideRule());
-    }
-
-    private static Rule decideRule() {
-        return (fileDifference, rankDifference) -> (fileDifference.hasDistance(1) || fileDifference.hasDistance(0))
-                && (rankDifference.hasDistance(1) || rankDifference.hasDistance(0));
+        super(color, KingMoveRule.instance());
     }
 
     @Override
---
+++ src/test/java/chess/domain/piece/rule/KingMoveRuleTest.java
@@ -0,0 +1,33 @@
+package chess.domain.piece.rule;
+
+import chess.domain.position.FileDifference;
+import chess.domain.position.RankDifference;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KingMoveRuleTest {
+
+    @Test
+    @DisplayName("킹 이동 규칙 준수 테스트(성공)")
+    void should_obey_rule_when_right_condition() {
+        FileDifference fileDifference = new FileDifference(1);
+        RankDifference rankDifference = new RankDifference(-1);
+
+        KingMoveRule rule = KingMoveRule.instance();
+
+        assertThat(rule.obey(fileDifference, rankDifference)).isTrue();
+    }
+
+    @Test
+    @DisplayName("킹 이동 규칙 준수 테스트(실패)")
+    void should_not_obey_rule_when_wrong_condition() {
+        FileDifference fileDifference = new FileDifference(0);
+        RankDifference rankDifference = new RankDifference(2);
+
+        KingMoveRule rule = KingMoveRule.instance();
+
+        assertThat(rule.obey(fileDifference, rankDifference)).isFalse();
+    }
+}
