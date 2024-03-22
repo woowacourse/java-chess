6d27e06660fbccc559b8c48cccf4688f238fb806
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-22 21:43:38 +0900

    test: Queen 테스트 추가

--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -13,14 +13,13 @@ class QueenTest {
 
     private static final Queen QUEEN = new Queen(Color.BLACK);
 
-    /*
     @DisplayName("한 방향으로 이루어지지 않은 경로로 움직일 수 없다.")
     @Test
     void tooManyDirectionTest() {
-        Route manyDirectionRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.UP, SquareState.EMPTY)
-        ));
+        Route manyDirectionRoute = new Route(
+                List.of(Direction.DOWN, Direction.UP),
+                List.of(SquareState.EMPTY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(QUEEN.canMove(manyDirectionRoute))
                 .isFalse();
@@ -29,11 +28,10 @@ class QueenTest {
     @DisplayName("경로 중간에 기물이 위치한다면 움직일 수 없다.")
     @Test
     void pathHasPieceTest() {
-        Route notEmptyRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.ALLY),
-                new Step(Direction.DOWN, SquareState.EMPTY)
-        ));
+        Route notEmptyRoute = new Route(
+                List.of(Direction.DOWN, Direction.DOWN, Direction.DOWN),
+                List.of(SquareState.EMPTY, SquareState.ALLY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(QUEEN.canMove(notEmptyRoute))
                 .isFalse();
@@ -42,11 +40,9 @@ class QueenTest {
     @DisplayName("목적지에 아군이 존재한다면 움직일 수 없다.")
     @Test
     void allyLocatedAtTargetTest() {
-        Route manyDirectionRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.ALLY)
-        ));
+        Route manyDirectionRoute = new Route(
+                List.of(Direction.DOWN, Direction.DOWN, Direction.DOWN),
+                List.of(SquareState.EMPTY, SquareState.EMPTY, SquareState.ALLY));
 
         Assertions.assertThat(QUEEN.canMove(manyDirectionRoute))
                 .isFalse();
@@ -55,18 +51,12 @@ class QueenTest {
     @DisplayName("최대 7칸까지 움직일 수 있다.")
     @Test
     void maxDistanceMoveTest() {
-        Route manyDirectionRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.ENEMY)
-        ));
+        Route manyDirectionRoute = new Route(
+                List.of(Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN),
+                List.of(SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(QUEEN.canMove(manyDirectionRoute))
                 .isTrue();
     }
-     */
 }
