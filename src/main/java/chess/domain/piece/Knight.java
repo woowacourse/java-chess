34f66b678026b11444dfdb7ec077796716114130
tsulocalize <tsulocalize@gmail.com>
2024-03-22 15:31:09 +0900

    refactor: 나이트 이동 규칙 클래스 분리

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,16 +1,12 @@
 package chess.domain.piece;
 
+import chess.domain.piece.rule.KnightMoveRule;
 import chess.domain.position.Position;
 
 public class Knight extends Piece {
 
     public Knight(Color color) {
-        super(color, decideRule());
-    }
-
-    private static Rule decideRule() {
-        return ((fileDifference, rankDifference) -> (fileDifference.hasDistance(1) && rankDifference.hasDistance(2))
-                || (fileDifference.hasDistance(2) && rankDifference.hasDistance(1)));
+        super(color, KnightMoveRule.instance());
     }
 
     @Override
