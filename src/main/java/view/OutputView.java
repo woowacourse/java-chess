c6874e8bef93ff0ed4230475b701990ccbbb45f4
HyungUk Ryu <hw0603@naver.com>
2024-03-22 15:37:14 +0900

    refactor(outputView): 체스판 출력 시 File과 Rank도 함께 출력하도록 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,20 +10,41 @@ import java.util.Map;
 
 public class OutputView {
     private static final int BOARD_SIZE = 8;
+    private static final String FILE_DESCRIPTION = "A  B  C  D  E  F  G  H";
+    private static final int FILE_DESCRIPTION_PAD_SIZE = 4;
+    private static final String FILE_DESCRIPTION_SEPARATOR = "-".repeat(FILE_DESCRIPTION.length());
+    private static final String RANK_DESCRIPTION_SEPARATOR = "|";
+    private static final String PAD_CHAR = " ";
+    private static final int PIECE_CHAR_PAD_SIZE = 1;
+    public static final String EMPTY_PIECE = ".";
+    private static final String NEW_LINE = System.lineSeparator();
 
     private final OutputConvertor outputConvertor = new OutputConvertor();
 
     public void printWelcomeMessage() {
-        System.out.println("> 체스 게임을 시작합니다.");
-        System.out.println("> 게임 시작: start");
-        System.out.println("> 게임 종료: end");
-        System.out.println("> 게임 이동: move source위치 target위치 - 예. move b2 b3");
+        System.out.println("""
+                > 체스 게임을 시작합니다.
+                > 게임 시작: start
+                > 게임 종료: end
+                > 게임 이동: move source위치 target위치 - 예. move b2 b3"""
+        );
     }
 
     public void printBoard(final BoardDto boardDto) {
         List<String> boardStatus = convertBoardStatus(boardDto.piecePositions());
-        boardStatus.forEach(System.out::println);
-        System.out.println();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(pad(FILE_DESCRIPTION, FILE_DESCRIPTION_PAD_SIZE)).append(NEW_LINE);
+        sb.append(pad(FILE_DESCRIPTION_SEPARATOR, FILE_DESCRIPTION_PAD_SIZE)).append(NEW_LINE);
+        for (int rank = boardStatus.size(); rank > 0; rank--) {
+            sb.append(rank).append(PAD_CHAR).append(RANK_DESCRIPTION_SEPARATOR)
+                    .append(boardStatus.get(boardStatus.size() - rank))
+                    .append(RANK_DESCRIPTION_SEPARATOR).append(PAD_CHAR).append(rank)
+                    .append(NEW_LINE);
+        }
+        sb.append(pad(FILE_DESCRIPTION_SEPARATOR, FILE_DESCRIPTION_PAD_SIZE)).append(NEW_LINE);
+        sb.append(pad(FILE_DESCRIPTION, FILE_DESCRIPTION_PAD_SIZE));
+        System.out.println(sb);
     }
 
     private List<String> convertBoardStatus(final Map<Position, PieceType> boardStatus) {
@@ -36,10 +57,10 @@ public class OutputView {
                 .toList();
     }
 
-    private static String[][] initStringsArray() {
+    private String[][] initStringsArray() {
         String[][] strings = new String[BOARD_SIZE][BOARD_SIZE];
         for (String[] row : strings) {
-            Arrays.fill(row, ".");
+            Arrays.fill(row, pad(EMPTY_PIECE, PIECE_CHAR_PAD_SIZE));
         }
         return strings;
     }
@@ -47,6 +68,10 @@ public class OutputView {
     private void buildStrings(final String[][] strings, final Position position, final PieceType pieceType) {
         int row = position.rowIndex();
         int col = position.columnIndex();
-        strings[row][col] = outputConvertor.convertPieceTypeToString(pieceType);
+        strings[row][col] = pad(outputConvertor.convertPieceTypeToString(pieceType), PIECE_CHAR_PAD_SIZE);
+    }
+
+    private String pad(String origin, int padSize) {
+        return PAD_CHAR.repeat(padSize) + origin + PAD_CHAR.repeat(padSize);
     }
 }
