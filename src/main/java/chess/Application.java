92e18575ae088d4597a0bbb299b188b3e669f230
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-22 16:49:39 +0900

    refactor(Application): 예외 발생 시 메시지 출력하도록 수정

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -6,7 +6,11 @@ import chess.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        ChessGameController chessGameController = new ChessGameController(new InputView(), new OutputView());
-        chessGameController.start();
+        try {
+            ChessGameController chessGameController = new ChessGameController(new InputView(), new OutputView());
+            chessGameController.start();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
