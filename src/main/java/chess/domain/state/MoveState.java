ecdf3a359d44be9faf6238135b0b958ec0b6d28c
김민수 MINSOO KIM <alstn113@gmail.com>
2024-03-22 16:42:01 +0900

    refactor: 움직임 상태 오버라이드 어노테이션 추가

--- src/main/java/chess/domain/state/MoveState.java
+++ src/main/java/chess/domain/state/MoveState.java
@@ -1,14 +1,17 @@
 package chess.domain.state;
 
 public abstract class MoveState implements GameState {
+    @Override
     public GameState start() {
         throw new UnsupportedOperationException("이미 시작한 상태입니다.");
     }
 
+    @Override
     public GameState end() {
         return new EndState();
     }
 
+    @Override
     public boolean isPlaying() {
         return true;
     }
