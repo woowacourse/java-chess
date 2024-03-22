7c3488eba1eec0cfe5cb50bd6d3e3ed756c69125
SungKyum Kim <sungkyum1@naver.com>
2024-03-22 18:13:02 +0900

    test: 킹 움직임 테스트 분리

--- src/test/java/domain/piece/KingTest.java
+++ src/test/java/domain/piece/KingTest.java
@@ -16,9 +16,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 class KingTest {
     @DisplayName("킹은 상하좌우 대각선으로 한칸 움직일 수 있다.")
     @ParameterizedTest
-    @MethodSource(value = "squareArguments")
-    void canMove(final Square source, final Square target, final boolean expected) {
+    @MethodSource(value = "canMoveArguments")
+    void canMove(final Square target, final boolean expected) {
         final King king = new King(Team.BLACK);
+        final Square source = new Square(File.D, Rank.FOUR);
 
         // when
         final boolean canMove = king.canMove(source, target);
@@ -27,19 +28,39 @@ class KingTest {
         assertThat(canMove).isEqualTo(expected);
     }
 
-    static Stream<Arguments> squareArguments() {
+    static Stream<Arguments> canMoveArguments() {
         return Stream.of(
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.C, Rank.FOUR), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.E, Rank.FOUR), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.D, Rank.THREE), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.D, Rank.FIVE), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.C, Rank.THREE), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.E, Rank.THREE), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.C, Rank.FIVE), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.E, Rank.FIVE), true),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.B, Rank.FIVE), false),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.G, Rank.FIVE), false),
-                Arguments.of(new Square(File.D, Rank.FOUR), new Square(File.D, Rank.EIGHT), false)
+                Arguments.of(new Square(File.C, Rank.FOUR), true),
+                Arguments.of(new Square(File.E, Rank.FOUR), true),
+                Arguments.of(new Square(File.D, Rank.THREE), true),
+                Arguments.of(new Square(File.D, Rank.FIVE), true),
+                Arguments.of(new Square(File.C, Rank.THREE), true),
+                Arguments.of(new Square(File.E, Rank.THREE), true),
+                Arguments.of(new Square(File.C, Rank.FIVE), true),
+                Arguments.of(new Square(File.E, Rank.FIVE), true)
+        );
+    }
+
+
+    @DisplayName("킹은 상하좌우 대각선으로 한칸 움직일 수 있다.")
+    @ParameterizedTest
+    @MethodSource(value = "canNotArguments")
+    void canNotMove(final Square target, final boolean expected) {
+        final King king = new King(Team.BLACK);
+        final Square source = new Square(File.D, Rank.FOUR);
+
+        // when
+        final boolean canMove = king.canMove(source, target);
+
+        // then
+        assertThat(canMove).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> canNotArguments() {
+        return Stream.of(
+                Arguments.of(new Square(File.B, Rank.FIVE), false),
+                Arguments.of(new Square(File.G, Rank.FIVE), false),
+                Arguments.of(new Square(File.D, Rank.EIGHT), false)
         );
     }
 }
