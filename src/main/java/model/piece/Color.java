71c39692bd36171e29b61b969432033d0b9863a3
DobbyMac <kimdobby@g.skku.edu>
2024-03-22 19:45:03 +0900

    feat(Color): 자신의 다음 차례의 색깔을 반환하는 기능 구현

--- src/main/java/model/piece/Color.java
+++ src/main/java/model/piece/Color.java
@@ -1,5 +1,12 @@
 package model.piece;
 
 public enum Color {
-    BLACK, WHITE, UN_COLORED
+    BLACK, WHITE, UN_COLORED;
+
+    public Color next() {
+        if (this.equals(WHITE)) {
+            return BLACK;
+        }
+        return WHITE;
+    }
 }
