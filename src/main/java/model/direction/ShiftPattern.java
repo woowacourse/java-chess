36087501cc6f02e803ec24f652041b58a29d685d
DobbyMac <kimdobby@g.skku.edu>
2024-03-22 17:30:24 +0900

    refactor(ShiftPattern): 미사용 field인 bound 제거

--- src/main/java/model/direction/ShiftPattern.java
+++ src/main/java/model/direction/ShiftPattern.java
@@ -21,28 +21,22 @@ import java.util.Collections;
 import java.util.List;
 
 public enum ShiftPattern {
-    KING_PATTERN(List.of(N, NE, E, SE, S, SW, W, NW), 1),
-    KNIGHT_PATTERN(List.of(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS), 1),
-    WHITE_PAWN_PATTERN(List.of(N, NE, NW), 1),
-    BLACK_PAWN_PATTERN(List.of(S, SE, SW), 1),
-    QUEEN_PATTERN(List.of(W, E, S, N, NW, SW, NE, SE), 7),
-    ROOK_PATTERN(List.of(W, E, S, N), 7),
-    BISHOP_PATTERN(List.of(NW, SW, NE, SE), 7),
-    NONE(List.of(), 0);
+    KING_PATTERN(List.of(N, NE, E, SE, S, SW, W, NW)),
+    KNIGHT_PATTERN(List.of(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS)),
+    WHITE_PAWN_PATTERN(List.of(N, NE, NW)),
+    BLACK_PAWN_PATTERN(List.of(S, SE, SW)),
+    QUEEN_PATTERN(List.of(W, E, S, N, NW, SW, NE, SE)),
+    ROOK_PATTERN(List.of(W, E, S, N)),
+    BISHOP_PATTERN(List.of(NW, SW, NE, SE)),
+    NONE(List.of());
 
     private final List<Direction> directions;
-    private final int bound;
 
-    ShiftPattern(List<Direction> directions, int bound) {
+    ShiftPattern(List<Direction> directions) {
         this.directions = directions;
-        this.bound = bound;
     }
 
     public List<Direction> directions() {
         return Collections.unmodifiableList(directions);
-    }
-
-    public int bound() {
-        return bound;
     }
 }
