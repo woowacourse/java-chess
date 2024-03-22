c05e55ddfcc24b451ff1503966ea195e3d299229
PgmJun <chltmdwns96@gmail.com>
2024-03-22 17:48:36 +0900

    refactor: 체스판 생성 테스트 assertAll 검증방식으로 변경

--- src/test/java/chess/domain/board/ChessBoardGeneratorTest.java
+++ src/test/java/chess/domain/board/ChessBoardGeneratorTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ChessBoardGeneratorTest {
 
@@ -22,18 +23,20 @@ public class ChessBoardGeneratorTest {
         // then
         assertThat(pieces).hasSize(32);
 
-        assertThat(countPiece(pieces, PieceType.WHITE_PAWN)).isEqualTo(8);
-        assertThat(countPiece(pieces, PieceType.BLACK_PAWN)).isEqualTo(8);
-        assertThat(countPiece(pieces, PieceType.WHITE_ROOK)).isEqualTo(2);
-        assertThat(countPiece(pieces, PieceType.BLACK_ROOK)).isEqualTo(2);
-        assertThat(countPiece(pieces, PieceType.WHITE_KNIGHT)).isEqualTo(2);
-        assertThat(countPiece(pieces, PieceType.BLACK_KNIGHT)).isEqualTo(2);
-        assertThat(countPiece(pieces, PieceType.WHITE_BISHOP)).isEqualTo(2);
-        assertThat(countPiece(pieces, PieceType.BLACK_BISHOP)).isEqualTo(2);
-        assertThat(countPiece(pieces, PieceType.WHITE_KING)).isEqualTo(1);
-        assertThat(countPiece(pieces, PieceType.BLACK_KING)).isEqualTo(1);
-        assertThat(countPiece(pieces, PieceType.WHITE_QUEEN)).isEqualTo(1);
-        assertThat(countPiece(pieces, PieceType.BLACK_QUEEN)).isEqualTo(1);
+        assertAll(
+                () -> assertThat(countPiece(pieces, PieceType.WHITE_PAWN)).isEqualTo(8),
+                () -> assertThat(countPiece(pieces, PieceType.BLACK_PAWN)).isEqualTo(8),
+                () -> assertThat(countPiece(pieces, PieceType.WHITE_ROOK)).isEqualTo(2),
+                () -> assertThat(countPiece(pieces, PieceType.BLACK_ROOK)).isEqualTo(2),
+                () -> assertThat(countPiece(pieces, PieceType.WHITE_KNIGHT)).isEqualTo(2),
+                () -> assertThat(countPiece(pieces, PieceType.BLACK_KNIGHT)).isEqualTo(2),
+                () -> assertThat(countPiece(pieces, PieceType.WHITE_BISHOP)).isEqualTo(2),
+                () -> assertThat(countPiece(pieces, PieceType.BLACK_BISHOP)).isEqualTo(2),
+                () -> assertThat(countPiece(pieces, PieceType.WHITE_KING)).isEqualTo(1),
+                () -> assertThat(countPiece(pieces, PieceType.BLACK_KING)).isEqualTo(1),
+                () -> assertThat(countPiece(pieces, PieceType.WHITE_QUEEN)).isEqualTo(1),
+                () -> assertThat(countPiece(pieces, PieceType.BLACK_QUEEN)).isEqualTo(1)
+        );
     }
 
     private int countPiece(Map<Position, Piece> pieces, PieceType type) {
