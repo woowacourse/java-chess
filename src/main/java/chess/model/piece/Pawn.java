d9b3c197af43f48292e6c432219da16b4ff74368
JUHA <84626225+khabh@users.noreply.github.com>
2024-03-23 00:18:32 +0900

    refactor: Pawn의 필드를 더 명확히 표현하기 위해 RankDirection enum 추가

--- src/main/java/chess/model/piece/Pawn.java
+++ src/main/java/chess/model/piece/Pawn.java
@@ -3,15 +3,31 @@ package chess.model.piece;
 import chess.model.position.Movement;
 
 public class Pawn extends Piece {
-    private static final Piece WHITE_PAWN = new Pawn(Color.WHITE, 1, 2);
-    private static final Piece BLACK_PAWN = new Pawn(Color.BLACK, -1, 7);
+    private static final Piece WHITE_PAWN = new Pawn(Color.WHITE, RankDirection.UP, 2);
+    private static final Piece BLACK_PAWN = new Pawn(Color.BLACK, RankDirection.DOWN, 7);
     private static final int START_JUMP_DISTANCE = 2;
     private static final int COMMON_RANK_DISTANCE = 1;
 
-    private final int validRankDirection;
+    private enum RankDirection {
+        UP(1),
+        DOWN(-1);
+
+        private final int direction;
+
+        RankDirection(int direction) {
+            this.direction = direction;
+        }
+
+        public boolean match(Movement movement) {
+            int rankGap = movement.getRankGap();
+            return direction == Integer.signum(rankGap);
+        }
+    }
+
+    private final RankDirection validRankDirection;
     private final int startRank;
 
-    private Pawn(Color color, int validRankDirection, int startRank) {
+    private Pawn(Color color, RankDirection validRankDirection, int startRank) {
         super(color, Type.PAWN);
         this.validRankDirection = validRankDirection;
         this.startRank = startRank;
@@ -36,8 +52,7 @@ public class Pawn extends Piece {
     }
 
     private boolean isValidDirection(Movement movement) {
-        int movementRankDirection = Integer.signum(movement.getRankGap());
-        return movementRankDirection == validRankDirection;
+        return validRankDirection.match(movement);
     }
 
     private boolean isValidVerticalMove(Movement movement) {
