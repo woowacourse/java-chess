357bfdb94318f278995a705906d039ad28b1fed7
jongmee <ra123221@gmail.com>
2024-03-22 11:02:44 +0900

    refactor: MoveStrategy 인터페이스 삭제

--- src/main/java/chess/model/piece/MoveStrategy.java
+++
@@ -1,9 +0,0 @@
-package chess.model.piece;
-
-import chess.model.position.ChessPosition;
-
-import java.util.List;
-
-public interface MoveStrategy {
-    List<ChessPosition> findPath(ChessPosition source, ChessPosition target, Piece targetPiece);
-}
