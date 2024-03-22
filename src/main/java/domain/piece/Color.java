15072e4bd004893ad7556538f26c96e5482d0ad9
SCY <helenason@naver.com>
2024-03-22 16:36:52 +0900

    refactor(Color): 불필요한 메서드 제거

--- src/main/java/domain/piece/Color.java
+++ src/main/java/domain/piece/Color.java
@@ -4,10 +4,6 @@ public enum Color {
 
     WHITE, BLACK, NONE;
 
-    public boolean isWhite() {
-        return this == WHITE;
-    }
-
     public boolean isBlack() {
         return this == BLACK;
     }
