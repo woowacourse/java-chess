0b9608a9181bf1efdf71f14d3f72a6989596cbea
tsulocalize <tsulocalize@gmail.com>
2024-03-22 16:45:23 +0900

    refactor: 문자열 형식 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -12,9 +12,10 @@ public class OutputView {
 
     public void printCommandGuideMessage() {
         System.out.println(
-                "> 게임 시작 : start\n"
-                        + "> 게임 종료 : end\n"
-                        + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+                """
+                        > 게임 시작 : start
+                        > 게임 종료 : end
+                        > 게임 이동 : move source위치 target위치 - 예. move b2 b3""");
     }
 
     public void printChessBoard(ChessBoard chessBoard) {
