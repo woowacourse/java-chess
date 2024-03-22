73310ffb9436639651362619b1573dc66849187f
hyxrxn <gpfls981220@gmail.com>
2024-03-22 17:08:00 +0900

    refactor(PieceDto): 클래스로 변경
    - 주 생성자를 private으로 변경

--- src/main/java/chess/dto/PieceDto.java
+++ src/main/java/chess/dto/PieceDto.java
@@ -2,12 +2,28 @@ package chess.dto;
 
 import chess.domain.piece.Piece;
 
-public record PieceDto(PieceType type, boolean isBlack) {
+public class PieceDto {
+
+    private final PieceType type;
+    private final boolean isBlack;
+
+    private PieceDto(PieceType type, boolean isBlack) {
+        this.type = type;
+        this.isBlack = isBlack;
+    }
 
     public static PieceDto from(Piece piece) {
         PieceType type = PieceType.from(piece);
         boolean isBlackTeam = piece.isBlackTeam();
 
         return new PieceDto(type, isBlackTeam);
+    }
+
+    public PieceType type() {
+        return type;
+    }
+
+    public boolean isBlack() {
+        return isBlack;
     }
 }
