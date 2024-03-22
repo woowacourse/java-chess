f01559f4390306fd99df8ad796aed3b045e1c3e7
yoonjuho <dbswn990@naver.com>
2024-03-22 16:15:55 +0900

    feat(ShiftPattern): 각 기물이 이동할 수 있는 방향 정의

---
+++ src/main/java/model/direction/ShiftPattern.java
@@ -0,0 +1,26 @@
+package model.direction;
+
+import java.util.List;
+
+import static model.direction.Direction.*;
+import static model.direction.Direction.NW;
+
+public enum ShiftPattern {
+    KING(List.of(N, NE, E, SE, S, SW, W, NW)),
+    KNIGHT(List.of(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS)),
+    BISHOP(List.of(NW, SW, NE, SE)),
+    WHITE_PAWN(List.of(N, NW, NE)),
+    BLACK_PAWN(List.of(S, SE, SW)),
+    QUEEN(List.of(W, E, S, N, NW, SW, NE, SE)),
+    ROOK(List.of(W, E, N, S))
+    ;
+    private List<Direction> directions;
+
+    ShiftPattern(List<Direction> directions) {
+        this.directions = directions;
+    }
+
+    public List<Direction> getDirections() {
+        return directions;
+    }
+}
