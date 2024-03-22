a94b8dad014edf99726383bfa37fa9ea6ace4e39
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-22 14:13:25 +0900

    refactor(command): 생성 비용을 줄이기 위한 Pattern 사용

--- src/main/java/chess/view/command/Command.java
+++ src/main/java/chess/view/command/Command.java
@@ -1,40 +1,41 @@
 package chess.view.command;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public enum Command {
 
-    START("start", "start"),
-    MOVE("move", "^move [a-h][1-8] [a-h][1-8]$"),
-    END("end", "end")
+    START("start", Pattern.compile("start")),
+    MOVE("move", Pattern.compile("^move [a-h][1-8] [a-h][1-8]$")),
+    END("end", Pattern.compile("end"))
     ;
 
     private final String message;
-    private final String pattern;
+    private final Pattern pattern;
 
-    Command(final String message, final String pattern) {
+    Command(final String message, final Pattern pattern) {
         this.message = message;
         this.pattern = pattern;
     }
 
     public static Command of(final String input) {
         return Arrays.stream(values())
-                .filter(command -> input.matches(command.pattern))
+                .filter(command -> command.pattern.matcher(input).matches())
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(
                         "존재하지 않는 명령어입니다. 제시된 명령어를 입력하세요: %s".formatted(input)));
     }
 
     public static boolean isStart(final String input) {
-        return input.matches(START.pattern);
+        return START.pattern.matcher(input).matches();
     }
 
     public static boolean isEnd(final String input) {
-        return input.matches(END.pattern);
+        return END.pattern.matcher(input).matches();
     }
 
     public static boolean isMove(final String input) {
-        return input.matches(MOVE.pattern);
+        return MOVE.pattern.matcher(input).matches();
     }
 
     public String getMessage() {
