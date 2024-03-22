0f494bc5840c32f0c5ca55ba32739f523c7d3881
unifolio0 <boho82ha@gmail.com>
2024-03-22 18:43:06 +0900

    test : Init 테스트 코드 작성

---
+++ src/test/java/model/menu/InitTest.java
@@ -0,0 +1,31 @@
+package model.menu;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InitTest {
+
+    @DisplayName("초기 상태에서 start를 입력하면 Running을 반환한다.")
+    @Test
+    void gameSettingStart() {
+        assertThat(Init.gameSetting(List.of("start"))).isInstanceOf(Running.class);
+    }
+
+    @DisplayName("초기 상태에서 end를 입력하면 End를 반환한다.")
+    @Test
+    void gameSettingEnd() {
+        assertThat(Init.gameSetting(List.of("end"))).isInstanceOf(End.class);
+    }
+
+    @DisplayName("초기 상태에서 start, end 외에 다른 명령어를 입력하면 예외가 발생한다.")
+    @Test
+    void invalidGameSetting() {
+        assertThatThrownBy(() -> Init.gameSetting(List.of("move", "a2", "a4")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("start를 입력해야 게임이 시작됩니다.");
+    }
+}
