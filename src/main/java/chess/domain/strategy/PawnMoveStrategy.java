181132db93ff951021dd7531205e430041dbe663
Minjoo <m0522inju@gmail.com>
2024-03-23 01:05:31 +0900

    style(PawnMoveStrategy): 사용하지 않는 import 구문 제거

--- src/main/java/chess/domain/strategy/PawnMoveStrategy.java
+++ src/main/java/chess/domain/strategy/PawnMoveStrategy.java
@@ -2,7 +2,6 @@ package chess.domain.strategy;
 
 import chess.domain.color.Color;
 import chess.domain.piece.Piece;
-import chess.domain.piece.PieceType;
 import chess.domain.piece.Position;
 import chess.domain.piece.blank.Blank;
 import chess.domain.piece.pawn.Pawn;
