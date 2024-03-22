ec76a665e7675f6af11efe9440409deda70291d0
ay-eonii <naayen@naver.com>
2024-03-22 15:14:44 +0900

    fix(OutputView): 프로그램 시작 문구 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,14 +3,17 @@ package view;
 import domain.board.Board;
 import domain.piece.Piece;
 import domain.piece.Type;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class OutputView {
 
     public void printStartNotice() {
-        System.out.println("체스 게임을 시작합니다.");
-        System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
     public void printBoard(Board board) {
