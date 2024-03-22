d134dd40df25678fa59f499883ddac6ff990ab60
HyungUk Ryu <hw0603@naver.com>
2024-03-22 15:54:34 +0900

    refactor(board): 이동 위치 검증 로직 메서드 분리

--- src/main/java/domain/game/Board.java
+++ src/main/java/domain/game/Board.java
@@ -19,14 +19,32 @@ public class Board {
     }
 
     public void movePiece(final TeamColor teamColor, final Position source, final Position destination) {
+        validateMoveRequest(teamColor, source, destination);
+
+        boolean caughtEnemy = isPieceExist(destination);
+        Piece piece = chessBoard.get(source);
+        chessBoard.put(destination, piece);
+        chessBoard.remove(source);
+    }
+
+    private void validateMoveRequest(TeamColor teamColor, Position source, Position destination) {
+        validateEmpty(source);
         validateColor(teamColor, source);
         Piece piece = chessBoard.get(source);
         validateReachability(source, destination, piece);
         validateDestinationPiece(teamColor, destination);
+    }
 
-        boolean caughtEnemy = isPieceExist(destination);
-        chessBoard.put(destination, piece);
-        chessBoard.remove(source);
+    private void validateEmpty(final Position source) {
+        if (!chessBoard.containsKey(source)) {
+            throw new IllegalArgumentException("해당 위치에 기물이 없습니다.");
+        }
+    }
+
+    private void validateColor(final TeamColor teamColor, final Position source) {
+        if (!chessBoard.get(source).hasColor(teamColor)) {
+            throw new IllegalArgumentException("차례가 맞지 않습니다.");
+        }
     }
 
     private void validateReachability(Position source, Position destination, Piece piece) {
@@ -35,22 +53,16 @@ public class Board {
         }
     }
 
-    private void validateDestinationPiece(TeamColor teamColor, Position destination) {
-        if (isAllyPieceExistOnDestination(teamColor, destination)) {
-            throw new IllegalArgumentException("이동 위치에 아군 기물이 존재합니다.");
-        }
-    }
-
-    private void validateColor(final TeamColor teamColor, final Position source) {
-        if (!chessBoard.containsKey(source) || !chessBoard.get(source).hasColor(teamColor)) {
-            throw new IllegalArgumentException("차례가 맞지 않습니다.");
-        }
-    }
-
     private Set<Position> parseOtherPiecePositions(final Position source) {
         Set<Position> positions = new HashSet<>(chessBoard.keySet());
         positions.remove(source);
         return positions;
+    }
+
+    private void validateDestinationPiece(TeamColor teamColor, Position destination) {
+        if (isAllyPieceExistOnDestination(teamColor, destination)) {
+            throw new IllegalArgumentException("이동 위치에 아군 기물이 존재합니다.");
+        }
     }
 
     private boolean isAllyPieceExistOnDestination(TeamColor teamColor, Position destination) {
