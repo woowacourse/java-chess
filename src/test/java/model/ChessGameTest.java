94b52757a4255d1330d532c24c18e7b851d398c2
unifolio0 <boho82ha@gmail.com>
2024-03-22 20:21:44 +0900

    refactor : 테스트 삭제

--- src/test/java/model/ChessGameTest.java
+++ src/test/java/model/ChessGameTest.java
@@ -3,9 +3,7 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Map.Entry;
 import model.piece.Piece;
 import model.piece.Queen;
 import model.position.Moving;
@@ -22,44 +20,6 @@ class ChessGameTest {
 
         final Map<Position, Piece> board = chessGame.getBoard();
         assertThat(board.keySet()).hasSize(32);
-    }
-
-    @DisplayName("기물들의 시작 위치를 확인한다.")
-    @Test
-    void checkInitialPosition() {
-        final ChessGame chessGame = new ChessGame();
-
-        final Map<Position, Piece> board = chessGame.getBoard();
-        final StringBuilder stringBuilder = new StringBuilder();
-
-        String[][] res = new String[8][8];
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                res[i][j] = ".";
-            }
-        }
-
-        for (Entry<Position, Piece> entry : board.entrySet()) {
-            res[entry.getKey().getRowIndex()][entry.getKey().getColumnIndex()] = entry.getValue()
-                    .toString();
-        }
-
-        for (String[] ans : res) {
-            stringBuilder.append(Arrays.toString(ans));
-            stringBuilder.append(System.lineSeparator());
-        }
-
-        final String expected = String.format("[R, N, B, Q, K, B, N, R]%n"
-                + "[P, P, P, P, P, P, P, P]%n"
-                + "[., ., ., ., ., ., ., .]%n"
-                + "[., ., ., ., ., ., ., .]%n"
-                + "[., ., ., ., ., ., ., .]%n"
-                + "[., ., ., ., ., ., ., .]%n"
-                + "[p, p, p, p, p, p, p, p]%n"
-                + "[r, n, b, q, k, b, n, r]%n");
-
-        assertThat(stringBuilder.toString()).hasToString(expected);
     }
 
     @DisplayName("기물이 없는 위치가 주어졌을 때 예외가 발생한다.")
