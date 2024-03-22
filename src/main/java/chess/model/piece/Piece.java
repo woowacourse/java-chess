956b54c941ffe3a3ed632dca997611f50246ef72
jongmee <ra123221@gmail.com>
2024-03-22 11:35:16 +0900

    refactor: Piece의 메서드 접근제어자 변경

--- src/main/java/chess/model/piece/Piece.java
+++ src/main/java/chess/model/piece/Piece.java
@@ -14,11 +14,11 @@ public abstract class Piece {
     public abstract String getText();
     public abstract List<ChessPosition> findPath(ChessPosition source, ChessPosition target, Piece targetPiece);
 
-    public boolean isSameSide(Piece other) {
+    protected boolean isSameSide(Piece other) {
         return this.side == other.side;
     }
 
-    public void checkValidTargetPiece(Piece other) {
+    protected void checkValidTargetPiece(Piece other) {
         if (other != null && isSameSide(other)) {
             throw new IllegalArgumentException("타겟 위치에 아군 기물이 존재합니다.");
         }
