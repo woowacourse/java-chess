263d2309b3c9fc4340c6c58fe3be795e83eb3e4d
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-22 21:47:27 +0900

    test: Knight 테스트 추가

--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -12,26 +12,25 @@ import org.junit.jupiter.api.Test;
 class KnightTest {
     private static final Knight KNIGHT = new Knight(Color.BLACK);
 
-    /*
     @DisplayName("직선과 대각선을 모두 포함하고 있지 않은 경로로 움직일 수 없다.")
     @Test
     void orthogonalDirectionTest() {
-        Route orthogonalRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.UP, SquareState.EMPTY)
-        ));
+        Route orthogonalRoute = new Route(
+                List.of(Direction.DOWN, Direction.UP),
+                List.of(SquareState.EMPTY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(KNIGHT.canMove(orthogonalRoute))
                 .isFalse();
     }
 
-    @DisplayName("경로 중간에 기물이 위치해도 움직일 수 있다..")
+    @DisplayName("경로 중간에 기물이 위치해도 움직일 수 있다.")
     @Test
     void pathHasPieceTest() {
-        Route notEmptyRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.ALLY),
-                new Step(Direction.DOWN_LEFT, SquareState.EMPTY)
-        ));
+        Route notEmptyRoute = new Route(
+                List.of(Direction.DOWN, Direction.DOWN_LEFT),
+                List.of(SquareState.ALLY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(KNIGHT.canMove(notEmptyRoute))
                 .isTrue();
@@ -40,10 +39,10 @@ class KnightTest {
     @DisplayName("목적지에 아군이 존재한다면 움직일 수 없다.")
     @Test
     void allyLocatedAtTargetTest() {
-        Route allyTargetRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN_LEFT, SquareState.ALLY)
-        ));
+        Route allyTargetRoute = new Route(
+                List.of(Direction.DOWN, Direction.DOWN_LEFT),
+                List.of(SquareState.EMPTY, SquareState.ALLY)
+        );
 
         Assertions.assertThat(KNIGHT.canMove(allyTargetRoute))
                 .isFalse();
@@ -52,14 +51,12 @@ class KnightTest {
     @DisplayName("2칸이 아닌 경로로 움직일 수 없다.")
     @Test
     void maxDistanceMoveTest() {
-        Route wrongDistanceRoute = new Route(List.of(
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN, SquareState.EMPTY),
-                new Step(Direction.DOWN_LEFT, SquareState.EMPTY)
-        ));
+        Route wrongDistanceRoute = new Route(
+                List.of(Direction.DOWN, Direction.DOWN, Direction.DOWN_LEFT),
+                List.of(SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(KNIGHT.canMove(wrongDistanceRoute))
                 .isFalse();
     }
-     */
 }
