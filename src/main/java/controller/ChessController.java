df4706294466005f8cbb575e051a028704a4cebe
reddevilmidzy <midzy0228@gmail.com>
2024-03-22 20:42:56 +0900

    feat: 예외 시 재입력 기능 구현

--- src/main/java/controller/ChessController.java
+++ src/main/java/controller/ChessController.java
@@ -1,18 +1,23 @@
 package controller;
 
 import dto.GameBoardDto;
+import exception.CustomException;
+import java.util.Collections;
+import java.util.List;
+import model.Command;
 import model.GameBoard;
 import model.status.GameStatus;
 import model.status.Initialization;
+import view.InputView;
 import view.OutputView;
 
 public class ChessController {
 
-    private final InputController inputController;
+    private final InputView inputView;
     private final OutputView outputView;
 
-    public ChessController(final InputController inputController, final OutputView outputView) {
-        this.inputController = inputController;
+    public ChessController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
         this.outputView = outputView;
     }
 
@@ -20,20 +25,59 @@ public class ChessController {
         GameBoard gameBoard = new GameBoard();
         gameBoard.setting();
         outputView.printStartMessage();
-        play(gameBoard);
-    }
-
-    private void play(final GameBoard gameBoard) {
-        GameStatus gameStatus = Initialization.gameSetting(inputController.getCommand());
+        GameStatus gameStatus = initGame();
 
         while (gameStatus.isRunning()) {
             printCurrentStatus(gameBoard);
-            gameStatus = gameStatus.play(inputController.getCommand(), gameBoard);
+            gameStatus = play(gameStatus, gameBoard);
+        }
+    }
+
+    private GameStatus initGame() {
+        try {
+            return Initialization.gameSetting(getCommand());
+        } catch (CustomException exception) {
+            outputView.printException(exception);
+            return initGame();
+        }
+    }
+
+    private GameStatus play(GameStatus gameStatus, final GameBoard gameBoard) {
+        try {
+            return gameStatus.play(getCommand(), gameBoard);
+        } catch (CustomException exception) {
+            outputView.printException(exception);
+            return play(gameStatus, gameBoard);
         }
     }
 
     private void printCurrentStatus(final GameBoard gameBoard) {
         outputView.printGameBoard(GameBoardDto.from(gameBoard));
-        outputView.printCurrentCamp(gameBoard.getCamp()); // TODO next가 맞을까 cur이 맞을까
+        outputView.printCurrentCamp(gameBoard.getCamp());
+    }
+
+    private List<String> getCommand() {
+        List<String> command = Collections.emptyList();
+        while (command.isEmpty()) {
+            command = readCommand();
+        }
+        return command;
+    }
+
+    private List<String> readCommand() {
+        try {
+            return readLine();
+        } catch (CustomException exception) {
+            outputView.printException(exception);
+        }
+        return Collections.emptyList();
+    }
+
+    private List<String> readLine() {
+        List<String> input = inputView.readCommandList();
+        for (String value : input) {
+            Command.validate(value);
+        }
+        return input;
     }
 }
