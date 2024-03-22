eb1f9b66417bbdcefee9632701454404e74a1254
PgmJun <chltmdwns96@gmail.com>
2024-03-22 17:40:52 +0900

    refactor: PieceType 추가로 인한 네이밍 패턴 제거

--- src/main/java/chess/domain/piece/PieceColor.java
+++ src/main/java/chess/domain/piece/PieceColor.java
@@ -1,24 +1,10 @@
 package chess.domain.piece;
 
 public enum PieceColor {
-    WHITE(PieceNamePattern.WHITE),
-    BLACK(PieceNamePattern.BLACK);
-
-    private final PieceNamePattern namePattern;
-
-    PieceColor(PieceNamePattern namePattern) {
-        this.namePattern = namePattern;
-    }
-
-    public String applyNamePattern(final String name) {
-        return namePattern.apply(name);
-    }
+    WHITE,
+    BLACK;
 
     public boolean isWhite() {
         return this == WHITE;
-    }
-
-    public boolean isBlack() {
-        return this == BLACK;
     }
 }
