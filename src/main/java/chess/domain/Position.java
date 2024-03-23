f2a6bfac31c1b1b70a1a08616038982358209d72
seokmyungham <hellotjraud@gmail.com>
2024-03-23 14:30:03 +0900

    refactor(Position): 불필요한 toString 제거

--- src/main/java/chess/domain/Position.java
+++ src/main/java/chess/domain/Position.java
@@ -64,12 +64,4 @@ public class Position {
     public int hashCode() {
         return Objects.hash(column, row);
     }
-
-    @Override
-    public String toString() {
-        return "Position{" +
-                "column=" + column +
-                ", row=" + row +
-                '}';
-    }
 }
