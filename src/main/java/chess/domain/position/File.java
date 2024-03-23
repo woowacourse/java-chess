b0ea49918c9eed06e140a009f37fa4f1c9f0a5a5
libienz <leekh10195@gmail.com>
2024-03-23 13:00:39 +0900

    feat: 체스의 열 번호 File 구현

---
+++ src/main/java/chess/domain/position/File.java
@@ -0,0 +1,18 @@
+package chess.domain.position;
+
+public enum File {
+    A(0),
+    B(1),
+    C(2),
+    D(3),
+    E(4),
+    F(5),
+    G(6),
+    H(7);
+
+    private final int columnNumber;
+
+    File(int columnNumber) {
+        this.columnNumber = columnNumber;
+    }
+}
