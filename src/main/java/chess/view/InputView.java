9ed972572d4988401ae527754fd806f068eef71b
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-24 02:01:41 +0900

    feat: 명령어에 구분자를 사용했는지 검증

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -3,11 +3,10 @@ package chess.view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class InputView {
-    private static final Set<String> GAME_COMMAND = Set.of("start", "end");
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
     private static final String MOVE_COMMAND = "move";
     private static final String INVALID_GAME_COMMAND = "start 또는 end만 입력가능합니다.";
     private static final String INVALID_MOVE_COMMAND = "부적절한 명령어입니다. move b2 b3와 같이 입력해주세요.";
@@ -32,23 +31,29 @@ public class InputView {
     }
 
     private void validateGameCommand(final String command) {
-        if (!GAME_COMMAND.contains(command)) {
+        if (!command.equals(START_COMMAND) && !command.equals(END_COMMAND)) {
             throw new IllegalArgumentException(INVALID_GAME_COMMAND);
         }
     }
 
     public List<String> readMovement() {
         String input = scanner.nextLine();
-        List<String> splitInput = Arrays.stream(input.split(MOVE_COMMAND_DELIMITER))
+        validateDelimiter(input);
+        List<String> commands = Arrays.stream(input.split(MOVE_COMMAND_DELIMITER))
                 .map(String::trim)
-                .collect(Collectors.toList());
-        validateMoveCommand(splitInput.get(0));
-        splitInput.remove(0);
-        return splitInput;
+                .toList();
+        validateMoveCommand(commands.get(0));
+        return commands;
+    }
+
+    private void validateDelimiter(final String input) {
+        if (!input.equals(END_COMMAND) && !input.contains(MOVE_COMMAND_DELIMITER)) {
+            throw new IllegalArgumentException(INVALID_MOVE_COMMAND);
+        }
     }
 
     private void validateMoveCommand(final String command) {
-        if (!command.equals(MOVE_COMMAND)) {
+        if (!command.equals(MOVE_COMMAND) && !command.equals(END_COMMAND)) {
             throw new IllegalArgumentException(INVALID_MOVE_COMMAND);
         }
     }
