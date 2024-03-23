f275cfde8bdc32739fcefb103dffdf8c43935fa2
woojong <dwax1324@gmail.com>
2024-03-24 03:35:42 +0900

    test: "이동 경로에 다른 말이 있다면 예외가 발생한다"

--- src/test/java/domain/board/BoardTest.java
+++ src/test/java/domain/board/BoardTest.java
@@ -1,6 +1,7 @@
 package domain.board;
 
 import static domain.PositionFixture.*;
+import static org.assertj.core.api.Assertions.*;
 
 import domain.piece.info.File;
 import domain.piece.info.Position;
@@ -12,12 +13,22 @@ import org.junit.jupiter.api.Test;
 class BoardTest {
     @Test
     @DisplayName("현재 차례가 아니라면 예외가 발생한다")
-    void move2() {
+    void turn() {
         final Board board = new Board(BoardInitiator.init());
         board.move(B_TWO, B_THREE);
 
-        Assertions.assertThatThrownBy(() -> board.move(B_THREE, B_FOUR))
+        assertThatThrownBy(() -> board.move(B_THREE, B_FOUR))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("현재 차례가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("이동 경로에 다른 말이 있다면 예외가 발생한다")
+    void path() {
+        final Board board = new Board(BoardInitiator.init());
+
+        assertThatThrownBy(() -> board.move(A_ONE, A_THREE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이동 경로에 다른 말이 놓져 있습니다");
     }
 }
