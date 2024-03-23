adf7febed498d2bd5714d570d56dbd99626136d3
woojong <dwax1324@gmail.com>
2024-03-24 03:33:28 +0900

    test: 현재 차례가 아니라면 예외가 발생한다

--- src/test/java/domain/board/BoardTest.java
+++ src/test/java/domain/board/BoardTest.java
@@ -1,5 +1,7 @@
 package domain.board;
 
+import static domain.PositionFixture.*;
+
 import domain.piece.info.File;
 import domain.piece.info.Position;
 import domain.piece.info.Rank;
@@ -9,12 +11,13 @@ import org.junit.jupiter.api.Test;
 
 class BoardTest {
     @Test
-    @DisplayName("기물을 이동 불가능한 자리로 움직이면 예외가 발생한다")
+    @DisplayName("현재 차례가 아니라면 예외가 발생한다")
     void move2() {
         final Board board = new Board(BoardInitiator.init());
-        final Position source = new Position(File.of(0), Rank.of(0));
-        final Position next = new Position(File.of(0), Rank.of(1));
+        board.move(B_TWO, B_THREE);
 
-        Assertions.assertThatThrownBy(() -> board.move(source, next)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> board.move(B_THREE, B_FOUR))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("현재 차례가 아닙니다.");
     }
 }
