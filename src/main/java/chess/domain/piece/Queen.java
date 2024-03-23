ed32e575338930b0b432c76260b8bb0286aeb40e
le2sky <leehaneul990623@gmail.com>
2024-03-23 17:47:53 +0900

    refactor: 퀸 이동 판단 로직 개선

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -1,11 +1,24 @@
 package chess.domain.piece;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import chess.domain.board.Board;
 import chess.domain.board.Coordinate;
 import chess.domain.board.Direction;
 
 public class Queen extends AbstractPiece {
+
+    private static final List<Direction> POSSIBLE_DIRECTIONS = List.of(
+            Direction.UP,
+            Direction.DOWN,
+            Direction.LEFT,
+            Direction.RIGHT,
+            Direction.LEFT_UP,
+            Direction.LEFT_DOWN,
+            Direction.RIGHT_UP,
+            Direction.RIGHT_DOWN
+    );
 
     public Queen(Team team) {
         super(PieceType.QUEEN, team);
@@ -20,5 +33,49 @@ public class Queen extends AbstractPiece {
         }
 
         return Collections.emptyList();
+    }
+
+    @Override
+    public void validateMovable(Coordinate source, Coordinate target, Board board) {
+        Piece targetPiece = board.findByCoordinate(target);
+        if (source.equals(target)) {
+            throw new IllegalStateException("제자리 이동은 할 수 없습니다.");
+        }
+
+        if (isSameTeam(targetPiece)) {
+            throw new IllegalStateException("아군 기물은 공격할 수 없습니다.");
+        }
+
+        List<Coordinate> possiblePath = createPath(source, target, board);
+        if (!possiblePath.contains(target)) {
+            throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
+
+        List<Coordinate> movablePath = createMovablePath(possiblePath, board);
+        if (!movablePath.contains(target)) {
+            throw new IllegalStateException("기물로 막혀있어 이동할 수 없습니다.");
+        }
+    }
+
+    private List<Coordinate> createPath(Coordinate source, Coordinate target, Board board) {
+        return POSSIBLE_DIRECTIONS.stream()
+                .map(possibleDirection -> possibleDirection.createPath(source))
+                .filter(coordinates -> coordinates.contains(target))
+                .findFirst()
+                .orElse(Collections.emptyList());
+    }
+
+    private List<Coordinate> createMovablePath(List<Coordinate> possiblePath, Board board) {
+        List<Coordinate> movablePath = new ArrayList<>();
+
+        for (Coordinate coordinate : possiblePath) {
+            if (board.isPiecePresent(coordinate)) {
+                break;
+            }
+
+            movablePath.add(coordinate);
+        }
+
+        return movablePath;
     }
 }
--- src/test/java/chess/domain/piece/QueenTest.java
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -2,11 +2,17 @@ package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.HashMap;
 import java.util.List;
+import chess.domain.board.Board;
 import chess.domain.board.Coordinate;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class QueenTest {
 
@@ -17,6 +23,7 @@ class QueenTest {
                 .doesNotThrowAnyException();
     }
 
+    @Disabled
     @DisplayName("퀸은 대각과 가로, 세로로 제한없이 움직일 수 있다.")
     @Test
     void findMovablePath() {
@@ -33,6 +40,7 @@ class QueenTest {
         assertThat(result).containsExactlyElementsOf(expected);
     }
 
+    @Disabled
     @DisplayName("퀸이 목적지로 갈 수 없는 경우, 빈 컬렉션을 반환한다.")
     @Test
     void noPath() {
@@ -43,5 +51,189 @@ class QueenTest {
         List<Coordinate> result = queen.findMovablePath(start, destination);
 
         assertThat(result).isEmpty();
+    }
+
+    @DisplayName("target 좌표에 아군 기물이 있다면, 이동할 수 없다.")
+    @Test
+    void attackTeam() {
+        Queen queen = new Queen(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        Coordinate target = new Coordinate(4, 'd');
+        pieces.put(source, queen);
+        pieces.put(target, new Pawn(Team.WHITE));
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> queen.validateMovable(source, target, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("아군 기물은 공격할 수 없습니다.");
+    }
+
+    @DisplayName("제자리 이동을 할 수 없다.")
+    @Test
+    void sameCoordinate() {
+        Queen queen = new Queen(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        pieces.put(source, queen);
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> queen.validateMovable(source, source, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("제자리 이동은 할 수 없습니다.");
+    }
+
+    /***
+     * ........ 8
+     * T....... 7  T: target
+     * ........ 6
+     * ..O..... 5  O: Obstacle
+     * ........ 4
+     * ....Q... 3  Q: Queen
+     * ........ 2
+     * ........ 1
+     * --------
+     * abcdefgh
+     */
+    @DisplayName("target 으로 가는 대각선 경로에 기물이 존재하면, 이동할 수 없다.")
+    @Test
+    void obstacleDiagonal() {
+        Queen queen = new Queen(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        Coordinate obstacle = new Coordinate(5, 'c');
+        Coordinate target = new Coordinate(7, 'a');
+        pieces.put(source, queen);
+        pieces.put(obstacle, new Pawn(Team.WHITE));
+        pieces.put(target, new Pawn(Team.BLACK));
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> queen.validateMovable(source, target, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("기물로 막혀있어 이동할 수 없습니다.");
+    }
+
+    /***
+     * ....T... 8  T: target
+     * ........ 7
+     * ....O... 6  O: Obstacle
+     * ........ 5
+     * ........ 4
+     * ....Q... 3  Q: Queen
+     * ........ 2
+     * ........ 1
+     * --------
+     * abcdefgh
+     */
+    @DisplayName("target 으로 가는 세로 경로에 기물이 존재하면, 이동할 수 없다.")
+    @Test
+    void obstacleStraight() {
+        Queen queen = new Queen(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        Coordinate obstacle = new Coordinate(6, 'e');
+        Coordinate target = new Coordinate(8, 'e');
+        pieces.put(source, queen);
+        pieces.put(obstacle, new Pawn(Team.WHITE));
+        pieces.put(target, new Pawn(Team.BLACK));
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> queen.validateMovable(source, target, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("기물로 막혀있어 이동할 수 없습니다.");
+    }
+
+    @DisplayName("퀸은 대각과 가로, 세로로 제한없이 움직일 수 있다.")
+    @MethodSource("createTargetForHappyCase")
+    @ParameterizedTest
+    void canGo(Coordinate target) {
+        Queen queen = new Queen(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        pieces.put(source, queen);
+        Board board = new Board(pieces);
+
+        assertThatCode(() -> queen.validateMovable(source, target, board))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("퀸이 갈 수 없는 위치라면 이동할 수 없다.")
+    @MethodSource("createTargetForExceptionCase")
+    @ParameterizedTest
+    void cantGo(Coordinate target) {
+        Queen queen = new Queen(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        pieces.put(source, queen);
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> queen.validateMovable(source, target, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+    }
+
+    /***
+     * ....O... 8
+     * O...O... 7
+     * .O..O..O 6
+     * ..O.O.O. 5
+     * ...OOO.. 4
+     * OOOOBOOO 3
+     * ...OOO.. 2
+     * ..O.O.O. 1
+     * --------
+     * abcdefgh
+     */
+    private static Iterable<Coordinate> createTargetForHappyCase() {
+        return List.of(
+                new Coordinate(4, 'd'),
+                new Coordinate(5, 'c'),
+                new Coordinate(6, 'b'),
+                new Coordinate(7, 'a'),
+                new Coordinate(4, 'f'),
+                new Coordinate(5, 'g'),
+                new Coordinate(6, 'h'),
+                new Coordinate(2, 'd'),
+                new Coordinate(1, 'c'),
+                new Coordinate(2, 'f'),
+                new Coordinate(1, 'g'),
+                new Coordinate(3, 'a'),
+                new Coordinate(3, 'b'),
+                new Coordinate(3, 'c'),
+                new Coordinate(3, 'd'),
+                new Coordinate(3, 'f'),
+                new Coordinate(3, 'g'),
+                new Coordinate(3, 'h'),
+                new Coordinate(1, 'e'),
+                new Coordinate(2, 'e'),
+                new Coordinate(4, 'e'),
+                new Coordinate(5, 'e'),
+                new Coordinate(6, 'e'),
+                new Coordinate(7, 'e'),
+                new Coordinate(8, 'e'));
+    }
+
+    /***
+     * ....O... 8
+     * O...O... 7
+     * .O..O..O 6
+     * ..OXOXO. 5
+     * ..XOOOX. 4
+     * OOOOBOOO 3
+     * ..XOOOX. 2
+     * ..OXOXO. 1
+     * --------
+     * abcdefgh
+     */
+    private static Iterable<Coordinate> createTargetForExceptionCase() {
+        return List.of(
+                new Coordinate(5, 'd'),
+                new Coordinate(5, 'f'),
+                new Coordinate(4, 'c'),
+                new Coordinate(4, 'g'),
+                new Coordinate(2, 'c'),
+                new Coordinate(2, 'g'),
+                new Coordinate(1, 'd'),
+                new Coordinate(1, 'f'));
     }
 }
