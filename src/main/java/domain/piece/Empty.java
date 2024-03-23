e6caea47089fc9766a17dce86a8786a118bca204
KimSangHyeok <tkdgur0906@naver.com>
2024-03-24 05:02:49 +0900

    feat: 빈 칸을 의미하는 말 생성

---
+++ src/main/java/domain/piece/Empty.java
@@ -0,0 +1,15 @@
+package domain.piece;
+
+import domain.board.Position;
+
+public class Empty extends Piece {
+
+    public Empty(Color color) {
+        super(color);
+    }
+
+    @Override
+    public boolean canMove(Position source, Position target) {
+        return false;
+    }
+}
