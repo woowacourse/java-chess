e80731f0874c634d01f6c28dacb6e07ddbaed1cb
woojong <dwax1324@gmail.com>
2024-03-23 23:08:20 +0900

    test: 비어있는 칸을 움직이려고 하면 예외가 발생한다

--- src/test/java/domain/piece/NoneTest.java
+++ src/test/java/domain/piece/NoneTest.java
@@ -1,24 +1,28 @@
-//package domain.piece;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import domain.piece.info.Color;
-//import domain.piece.info.Type;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//class NoneTest {
-//
-//    @Test
-//    @DisplayName("비어있는 칸을 움직이려고 하면 예외가 발생한다")
-//    void moveNone() {
-//        final Piece none = new None(Color.NONE, Type.NONE);
-//
-//        assertAll(
-//                () -> Assertions.assertThatThrownBy(none::strategy).isInstanceOf(UnsupportedOperationException.class),
-//                () -> Assertions.assertThatThrownBy(none::movableDirections)
-//                        .isInstanceOf(UnsupportedOperationException.class)
-//        );
-//    }
-//}
+package domain.piece;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import domain.piece.info.Color;
+import domain.piece.info.File;
+import domain.piece.info.Position;
+import domain.piece.info.Rank;
+import domain.piece.info.Type;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NoneTest {
+
+    @Test
+    @DisplayName("비어있는 칸을 움직이려고 하면 예외가 발생한다")
+    void moveNone() {
+        final Piece empty = Empty.INSTANCE;
+        final Piece other = new Rook(Color.BLACK);
+        final Position source = new Position(File.D, Rank.FOUR);
+        final Position target = new Position(File.D, Rank.SEVEN);
+
+        Assertions.assertThatThrownBy(() -> empty.isReachable(source, target, other))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("비어 있는 칸입니다.");
+    }
+}
