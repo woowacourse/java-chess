6d9001c64387bdf649d0a008667675f324b10f36
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-23 21:18:05 +0900

    feat: 좌표에 기능 추가
    - 캐싱기능추가
    - 움직일 수 있는지 판단하는 기능
    - 움직이는 기능
    - 다른 좌표와 비교 기능

--- src/main/java/chess/domain/board/Coordinate.java
+++ src/main/java/chess/domain/board/Coordinate.java
@@ -1,29 +1,74 @@
 package chess.domain.board;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Coordinate {
 
-    private final Rank rank;
+    private final static Map<String, Coordinate> POOL;
+
+    static {
+        List<File> files = Arrays.stream(File.values()).toList();
+        List<Rank> ranks = Arrays.stream(Rank.values()).toList();
+
+        POOL = files.stream()
+                .flatMap(file -> ranks.stream()
+                        .map(rank -> new Coordinate(file, rank))
+                )
+                .collect(Collectors.toMap(
+                        coordinate -> toKey(coordinate.file, coordinate.rank),
+                        Function.identity()
+                ));
+    }
+
     private final File file;
+    private final Rank rank;
 
-    public Coordinate(int rankValue, char fileValue) {
-        this.rank = new Rank(rankValue);
-        this.file = new File(fileValue);
+    private Coordinate(final File file, final Rank rank) {
+        this.rank = rank;
+        this.file = file;
     }
 
-    public char getFile() {
-        return file.getValue();
+    public static Coordinate of(File file, Rank rank) {
+        if (POOL.get(toKey(file, rank)) == null) {
+            throw new IllegalArgumentException("존재하지 않는 좌표 입니다.");
+        }
+        return POOL.get(toKey(file, rank));
     }
 
-    public int getRank() {
-        return rank.getValue();
+    private static String toKey(File file, Rank rank) {
+        return file.name() + rank.name();
+
+    }
+
+    public Coordinate move(int fileWeight, int rankWeight) {
+        if (!canMove(fileWeight, rankWeight)) {
+            throw new IllegalArgumentException("해당 좌표로 움직일 수 없습니다.");
+        }
+        return Coordinate.of(file.move(fileWeight), rank.move(rankWeight));
+    }
+
+    public boolean canMove(int fileWeight, int rankWeight) {
+        return file.canMove(fileWeight) && rank.canMove(rankWeight);
+    }
+
+    public Entry<Integer, Integer> compare(Coordinate other) {
+        return Map.entry(this.file.compare(other.file), this.rank.compare(other.rank));
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Coordinate that = (Coordinate) o;
         return Objects.equals(rank, that.rank) && Objects.equals(file, that.file);
     }
@@ -31,5 +76,13 @@ public class Coordinate {
     @Override
     public int hashCode() {
         return Objects.hash(rank, file);
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "rank=" + rank +
+                ", file=" + file +
+                '}';
     }
 }
