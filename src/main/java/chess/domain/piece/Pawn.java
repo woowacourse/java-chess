18d75be81c3850413deefbad397b2509474fcc0f
le2sky <leehaneul990623@gmail.com>
2024-03-23 20:01:49 +0900

    refactor: 폰 이동 판단 로직 개선

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,16 +1,27 @@
 package chess.domain.piece;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import chess.domain.board.Board;
 import chess.domain.board.Coordinate;
 
 public class Pawn extends AbstractPiece {
 
-    private static final List<Map.Entry<Integer, Integer>> WEIGHTS = List.of(
+    private static final List<Map.Entry<Integer, Integer>> FORWARD_WEIGHTS = List.of(
             Map.entry(1, 0),
             Map.entry(2, 0)
     );
+
+    private static final List<Map.Entry<Integer, Integer>> DIAGONAL_WEIGHT = List.of(
+            Map.entry(1, -1),
+            Map.entry(1, 1)
+    );
+
+    //TODO: 상수의 올바른 위치를 생각해보기.
+    private static final int INITIAL_BLACK_PAWN_RANK = 7;
+    private static final int INITIAL_WHITE_PAWN_RANK = 2;
 
     public Pawn(Team team) {
         super(PieceType.PAWN, team);
@@ -21,7 +32,7 @@ public class Pawn extends AbstractPiece {
         int startRank = start.getRank();
         char startFile = start.getFile();
 
-        return WEIGHTS.stream()
+        return FORWARD_WEIGHTS.stream()
                 .map(weight -> mapToNextCoordinate(startRank, startFile, weight))
                 .filter(coordinate -> !Objects.isNull(coordinate))
                 .toList();
@@ -45,5 +56,82 @@ public class Pawn extends AbstractPiece {
 
     private char calculateNextFile(char startFile, Map.Entry<Integer, Integer> weight) {
         return (char) (startFile + weight.getValue());
+    }
+
+    @Override
+    public void validateMovable(Coordinate source, Coordinate target, Board board) {
+        Piece targetPiece = board.findByCoordinate(target);
+        if (source.equals(target)) {
+            throw new IllegalStateException("제자리 이동은 할 수 없습니다.");
+        }
+
+        if (isSameTeam(targetPiece)) {
+            throw new IllegalStateException("아군 기물은 공격할 수 없습니다.");
+        }
+
+        List<Coordinate> diagonalPath = createPath(source, DIAGONAL_WEIGHT);
+        List<Coordinate> forwardPath = createPath(source, FORWARD_WEIGHTS);
+        if (!(forwardPath.contains(target) || diagonalPath.contains(target))) {
+            throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
+
+        //TODO: depth 정리
+        if (diagonalPath.contains(target)) {
+            if (!board.isPiecePresent(target)) {
+                throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+            }
+
+            if (!isEnemy(targetPiece)) {
+                throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+            }
+        }
+
+        if (forwardPath.contains(target)) {
+            if (Math.abs(source.getRank() - target.getRank()) == 2 &&
+                    !(source.getRank() == INITIAL_BLACK_PAWN_RANK || source.getRank() == INITIAL_WHITE_PAWN_RANK)) {
+                throw new IllegalStateException("초기 상태의 폰이 아닌 경우, 2칸 이동할 수 없습니다.");
+            }
+
+            List<Coordinate> movablePath = createMovablePath(forwardPath, board);
+            if (!movablePath.contains(target)) {
+                throw new IllegalStateException("기물로 막혀있어 이동할 수 없습니다.");
+            }
+        }
+    }
+
+    public List<Coordinate> createPath(Coordinate source, List<Map.Entry<Integer, Integer>> weights) {
+        List<Coordinate> possibleCoordinate = new ArrayList<>();
+        int startRank = source.getRank();
+        char startFile = source.getFile();
+
+        //TODO: forwardDirection 가중치를 WEIGHTS에 곱한채로 저장하는 방법을 고려해보기
+        // 행열의 곱을 이용하는 것은 어떠한가?
+        // Weight(x, y).multifly(Weight(x, y)) = Weight(x * x, y * y)
+        for (Map.Entry<Integer, Integer> weight : weights) {
+            int nextRank = startRank + weight.getKey() * getTeam().getForwardDirection();
+            char nextFile = (char) (startFile + weight.getValue());
+
+            try {
+                Coordinate coordinate = new Coordinate(nextRank, nextFile);
+                possibleCoordinate.add(coordinate);
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
+
+        return possibleCoordinate;
+    }
+
+    private List<Coordinate> createMovablePath(List<Coordinate> possiblePath, Board board) {
+        List<Coordinate> movablePath = new ArrayList<>();
+
+        for (Coordinate coordinate : possiblePath) {
+            if (board.isPiecePresent(coordinate)) {
+                break;
+            }
+
+            movablePath.add(coordinate);
+        }
+
+        return movablePath;
     }
 }
--- src/test/java/chess/domain/piece/PawnTest.java
+++ src/test/java/chess/domain/piece/PawnTest.java
@@ -2,10 +2,15 @@ package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.HashMap;
 import java.util.List;
+import chess.domain.board.Board;
 import chess.domain.board.Coordinate;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class PawnTest {
@@ -17,6 +22,7 @@ class PawnTest {
                 .doesNotThrowAnyException();
     }
 
+    @Disabled
     @DisplayName("흰색 폰의 이동 가능한 모든 좌표를 계산한다.")
     @Test
     void findMovablePathWhite() {
@@ -32,6 +38,7 @@ class PawnTest {
         assertThat(result).containsExactlyElementsOf(expected);
     }
 
+    @Disabled
     @DisplayName("검은색 폰의 이동 가능한 모든 좌표를 계산한다.")
     @Test
     void findMovablePathBlack() {
@@ -45,5 +52,321 @@ class PawnTest {
                 new Coordinate(6, 'c'),
                 new Coordinate(5, 'c'));
         assertThat(result).containsExactlyElementsOf(expected);
+    }
+
+    @DisplayName("흰색 폰은")
+    @Nested
+    class WhitePawnTest {
+
+        private final Pawn sut = new Pawn(Team.WHITE);
+
+        @DisplayName("target 좌표에 아군 기물이 있다면, 이동할 수 없다.")
+        @Test
+        void attackTeam() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(1, 'e');
+            Coordinate target = new Coordinate(2, 'e');
+            pieces.put(source, sut);
+            pieces.put(target, new Pawn(Team.WHITE));
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("아군 기물은 공격할 수 없습니다.");
+        }
+
+        @DisplayName("제자리 이동을 할 수 없다.")
+        @Test
+        void sameCoordinate() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(3, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, source, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("제자리 이동은 할 수 없습니다.");
+        }
+
+        @DisplayName("아군 방향으로 1칸 전진할 수 없다.")
+        @Test
+        void oneBackStep() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(3, 'e');
+            Coordinate target = new Coordinate(2, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
+
+        @DisplayName("적진 방향으로 1칸 전진할 수 있다.")
+        @Test
+        void oneStep() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(3, 'e');
+            Coordinate target = new Coordinate(4, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatCode(() -> sut.validateMovable(source, target, board))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("초기 위치에 있을 경우, 적진 방향으로 2칸 움직일 수 있다.")
+        @Test
+        void twoStep() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(2, 'e');
+            Coordinate target = new Coordinate(4, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatCode(() -> sut.validateMovable(source, target, board))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("초기 위치가 아닌 경우, 적진 방향으로 2칸 움직일 수 없다.")
+        @Test
+        void noTwoStep() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(3, 'e');
+            Coordinate target = new Coordinate(5, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("초기 상태의 폰이 아닌 경우, 2칸 이동할 수 없습니다.");
+        }
+
+        /***
+         * ........ 8
+         * ........ 7
+         * ........ 6
+         * ........ 5
+         * ...T.... 4  T: target
+         * ...O.... 3  O: Obstacle
+         * ...P.... 2  P: Pawn(initial)
+         * ........ 1
+         * --------
+         * abcdefgh
+         */
+        @DisplayName("초기 위치지만, 경로에 다른 기물이 있다면 적진 방향으로 2칸 움직일 수 없다.")
+        @Test
+        void noTwoStepBecauseStuck() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(2, 'd');
+            Coordinate obstacle = new Coordinate(3, 'd');
+            Coordinate target = new Coordinate(4, 'd');
+            pieces.put(source, sut);
+            pieces.put(obstacle, new Pawn(Team.WHITE));
+            pieces.put(target, new Pawn(Team.BLACK));
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("기물로 막혀있어 이동할 수 없습니다.");
+        }
+
+        /***
+         * ........ 8
+         * ........ 7
+         * ........ 6
+         * ........ 5
+         * ........ 4
+         * ....T... 3  T: target(enemy)
+         * ...P.... 2  P: Pawn(initial)
+         * ........ 1
+         * --------
+         * abcdefgh
+         */
+        @DisplayName("적진 방향 양쪽 대각선에 적군 기물이 있으면 움직일 수 있다.")
+        @Test
+        void goDiagonal() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(2, 'd');
+            Coordinate enemy = new Coordinate(3, 'e');
+            pieces.put(source, sut);
+            pieces.put(enemy, new Pawn(Team.BLACK));
+            Board board = new Board(pieces);
+
+            assertThatCode(() -> sut.validateMovable(source, enemy, board))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("적진 방향 양쪽 대각선에 적군 기물이 없다면 움직일 수 없다.")
+        @Test
+        void noDiagonal() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(2, 'd');
+            Coordinate target = new Coordinate(3, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
+    }
+
+    @DisplayName("검정색 폰은")
+    @Nested
+    class BlackPawnTest {
+
+        private final Pawn sut = new Pawn(Team.BLACK);
+
+        @DisplayName("target 좌표에 아군 기물이 있다면 이동할 수 없다.")
+        @Test
+        void attackTeam() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(7, 'e');
+            Coordinate target = new Coordinate(6, 'e');
+            pieces.put(source, sut);
+            pieces.put(target, new Pawn(Team.BLACK));
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("아군 기물은 공격할 수 없습니다.");
+        }
+
+        @DisplayName("제자리 이동을 할 수 없다.")
+        @Test
+        void sameCoordinate() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(3, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, source, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("제자리 이동은 할 수 없습니다.");
+        }
+
+        @DisplayName("아군 방향으로 1칸 전진할 수 없다.")
+        @Test
+        void oneBackStep() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(7, 'e');
+            Coordinate target = new Coordinate(8, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
+
+        @DisplayName("적진 방향으로 1칸 전진할 수 있다.")
+        @Test
+        void oneStepBlack() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(7, 'e');
+            Coordinate target = new Coordinate(6, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatCode(() -> sut.validateMovable(source, target, board))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("초기 위치에 있을 경우, 적진 방향으로 2칸 움직일 수 있다.")
+        @Test
+        void twoStep() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(7, 'e');
+            Coordinate target = new Coordinate(5, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatCode(() -> sut.validateMovable(source, target, board))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("초기 위치가 아닌 경우, 적진 방향으로 2칸 움직일 수 없다.")
+        @Test
+        void noTwoStep() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(6, 'e');
+            Coordinate target = new Coordinate(4, 'e');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("초기 상태의 폰이 아닌 경우, 2칸 이동할 수 없습니다.");
+        }
+
+        /***
+         * ........ 8
+         * ...P.... 7  P: Pawn(initial)
+         * ...O.... 6  O: Obstacle
+         * ...T.... 5  T: target
+         * ........ 4
+         * ........ 3
+         * ........ 2
+         * ........ 1
+         * --------
+         * abcdefgh
+         */
+        @DisplayName("초기 위치지만, 경로에 다른 기물이 있다면 적진 방향으로 2칸 움직일 수 없다.")
+        @Test
+        void noTwoStepBecauseStuck() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(7, 'd');
+            Coordinate obstacle = new Coordinate(6, 'd');
+            Coordinate target = new Coordinate(5, 'd');
+            pieces.put(source, sut);
+            pieces.put(obstacle, new Pawn(Team.BLACK));
+            pieces.put(target, new Pawn(Team.WHITE));
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("기물로 막혀있어 이동할 수 없습니다.");
+        }
+
+        /***
+         * ........ 8
+         * ....P... 7 P: Pawn(initial)
+         * .....T.. 6 T: target(enemy)
+         * ........ 5
+         * ........ 4
+         * ........ 3
+         * ........ 2
+         * ........ 1
+         * --------
+         * abcdefgh
+         */
+        @DisplayName("적진 방향 양쪽 대각선에 적군 기물이 있으면 움직일 수 있다.")
+        @Test
+        void goDiagonal() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(7, 'e');
+            Coordinate enemy = new Coordinate(6, 'f');
+            pieces.put(source, sut);
+            pieces.put(enemy, new Pawn(Team.WHITE));
+            Board board = new Board(pieces);
+
+            assertThatCode(() -> sut.validateMovable(source, enemy, board))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("적진 방향 양쪽 대각선에 적군 기물이 없다면 움직일 수 없다.")
+        @Test
+        void noDiagonal() {
+            HashMap<Coordinate, Piece> pieces = new HashMap<>();
+            Coordinate source = new Coordinate(7, 'e');
+            Coordinate target = new Coordinate(6, 'f');
+            pieces.put(source, sut);
+            Board board = new Board(pieces);
+
+            assertThatThrownBy(() -> sut.validateMovable(source, target, board))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
     }
 }
