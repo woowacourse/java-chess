6f7ed772e663ad22ac9206d9493ce168d9502be1
hjk0761 <hjk0761@naver.com>
2024-03-23 21:45:38 +0900

    refactor: 사용하지 않는 메서드 제거하도록 수정

--- src/main/java/chess/domain/Team.java
+++ src/main/java/chess/domain/Team.java
@@ -1,12 +1,5 @@
 package chess.domain;
 
 public enum Team {
-    BLACK, WHITE, NONE;
-
-    public static Team takeTurn(Team team) {
-        if (team == WHITE) {
-            return BLACK;
-        }
-        return WHITE;
-    }
+    BLACK, WHITE, NONE
 }
