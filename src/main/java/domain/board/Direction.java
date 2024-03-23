c960f6e78d10bb3968cd18c3d28ee11651c7dde5
KimSangHyeok <tkdgur0906@naver.com>
2024-03-24 01:56:29 +0900

    refactor: Directon 상수 네이밍 공백추가

--- src/main/java/domain/board/Direction.java
+++ src/main/java/domain/board/Direction.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public enum Direction {
 
     UP(0, 1),
-    UPRIGHT(1, 1),
+    UP_RIGHT(1, 1),
     RIGHT(1, 0),
-    DOWNRIGHT(1, -1),
+    DOWN_RIGHT(1, -1),
     DOWN(0, -1),
-    DOWNLEFT(-1, -1),
+    DOWN_LEFT(-1, -1),
     LEFT(-1, 0),
-    UPLEFT(-1, 1);
+    UP_LEFT(-1, 1);
 
     private final int fileDirection;
     private final int rankDirection;
