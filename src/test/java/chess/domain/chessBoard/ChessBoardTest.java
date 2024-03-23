2fced2f1bd4838ea82d100dce2241d1e740e304f
tsulocalize <tsulocalizing@gmail.com>
2024-03-23 13:41:51 +0900

    test: 임시 테스트 삭제

--- src/test/java/chess/domain/chessBoard/ChessBoardTest.java
+++ src/test/java/chess/domain/chessBoard/ChessBoardTest.java
@@ -1,24 +1,4 @@
 package chess.domain.chessBoard;
 
-import chess.domain.chessBoard.generator.ChessSpaceGenerator;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 class ChessBoardTest {
-
-
-    @Test
-    @DisplayName("123")
-    void a() {
-        List<Space> spaceList = new ChessSpaceGenerator().generateSpaces();
-
-        for (int j = 0; j < 8; j++) {
-            for (int i = 0; i < 8; i++) {
-                System.out.print(spaceList.get(j * 8 + i).pieceCharacter() + " ");
-            }
-            System.out.println();
-        }
-    }
 }
