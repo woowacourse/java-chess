a2f8f49897e9ce29b361835332d8bcb9ba5bc0ef
le2sky <leehaneul990623@gmail.com>
2024-03-23 17:02:35 +0900

    refactor: 비숍 이동 판단 로직 개선

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -1,7 +1,9 @@
 package chess.domain.piece;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import chess.domain.board.Board;
 import chess.domain.board.Coordinate;
 import chess.domain.board.Direction;
 
@@ -25,5 +27,49 @@ public class Bishop extends AbstractPiece {
                 .filter(coordinates -> coordinates.contains(destination))
                 .findFirst()
                 .orElse(Collections.emptyList());
+    }
+
+    @Override
+    public void validateMovable(Coordinate source, Coordinate target, Board board) {
+        Piece targetPiece = board.findByCoordinate(target);
+        if (source.equals(target)) {
+            throw new IllegalStateException("제자리 이동은 할 수 없습니다.");
+        }
+
+        if (isSameTeam(targetPiece)) {
+            throw new IllegalStateException("아군 기물은 공격할 수 없습니다.");
+        }
+
+        List<Coordinate> possiblePath = createPath(source, target, board);
+        if (!possiblePath.contains(target)) {
+            throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
+
+        List<Coordinate> movablePath = createMovablePath(possiblePath, board);
+        if (!movablePath.contains(target)) {
+            throw new IllegalStateException("기물로 막혀있어 이동할 수 없습니다.");
+        }
+    }
+
+    private List<Coordinate> createPath(Coordinate source, Coordinate target, Board board) {
+        return POSSIBLE_DIRECTIONS.stream()
+                .map(possibleDirection -> possibleDirection.createPath(source))
+                .filter(coordinates -> coordinates.contains(target))
+                .findFirst()
+                .orElse(Collections.emptyList());
+    }
+
+    private List<Coordinate> createMovablePath(List<Coordinate> possiblePath, Board board) {
+        List<Coordinate> movablePath = new ArrayList<>();
+
+        for (Coordinate coordinate : possiblePath) {
+            if (board.isPiecePresent(coordinate)) {
+                break;
+            }
+
+            movablePath.add(coordinate);
+        }
+
+        return movablePath;
     }
 }
--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -2,11 +2,17 @@ package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.HashMap;
 import java.util.List;
+import chess.domain.board.Board;
 import chess.domain.board.Coordinate;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BishopTest {
 
@@ -17,6 +23,7 @@ class BishopTest {
                 .doesNotThrowAnyException();
     }
 
+    @Disabled
     @DisplayName("비숍은 대각으로 제한없이 움직일 수 있다.")
     @Test
     void findMovablePath() {
@@ -36,6 +43,7 @@ class BishopTest {
                 .containsExactlyElementsOf(expected);
     }
 
+    @Disabled
     @DisplayName("비숍이 목적지로 갈 수 없는 경우, 빈 컬렉션을 반환한다.")
     @Test
     void noPath() {
@@ -46,5 +54,141 @@ class BishopTest {
         List<Coordinate> result = bishop.findMovablePath(start, destination);
 
         assertThat(result).isEmpty();
+    }
+
+    @DisplayName("target 좌표에 아군 기물이 있다면, 이동할 수 없다.")
+    @Test
+    void attackTeam() {
+        Bishop bishop = new Bishop(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        Coordinate target = new Coordinate(4, 'd');
+        pieces.put(source, bishop);
+        pieces.put(target, new Pawn(Team.WHITE));
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> bishop.validateMovable(source, target, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("아군 기물은 공격할 수 없습니다.");
+    }
+
+    @DisplayName("제자리 이동을 할 수 없다.")
+    @Test
+    void sameCoordinate() {
+        Bishop bishop = new Bishop(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        pieces.put(source, bishop);
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> bishop.validateMovable(source, source, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("제자리 이동은 할 수 없습니다.");
+    }
+
+    /***
+     * ........ 8
+     * T....... 7  T: target
+     * ........ 6
+     * ..O..... 5  O: Obstacle
+     * ........ 4
+     * ....B... 3  B: Bishop
+     * ........ 2
+     * ........ 1
+     * --------
+     * abcdefgh
+     */
+    @DisplayName("target 으로 가는 경로에 기물이 존재하면, 이동할 수 없다.")
+    @Test
+    void obstacle() {
+        Bishop bishop = new Bishop(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        Coordinate obstacle = new Coordinate(5, 'c');
+        Coordinate target = new Coordinate(7, 'a');
+        pieces.put(source, bishop);
+        pieces.put(obstacle, new Pawn(Team.WHITE));
+        pieces.put(target, new Pawn(Team.BLACK));
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> bishop.validateMovable(source, target, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("기물로 막혀있어 이동할 수 없습니다.");
+    }
+
+    @DisplayName("비숍은 대각으로 제한없이 움직일 수 있다.")
+    @MethodSource("createTargetForHappyCase")
+    @ParameterizedTest
+    void canGo(Coordinate target) {
+        Bishop bishop = new Bishop(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        pieces.put(source, bishop);
+        Board board = new Board(pieces);
+
+        assertThatCode(() -> bishop.validateMovable(source, target, board))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("비숍이 갈 수 없는 위치라면 이동할 수 없다.")
+    @MethodSource("createTargetForExceptionCase")
+    @ParameterizedTest
+    void cantGo(Coordinate target) {
+        Bishop bishop = new Bishop(Team.WHITE);
+        HashMap<Coordinate, Piece> pieces = new HashMap<>();
+        Coordinate source = new Coordinate(3, 'e');
+        pieces.put(source, bishop);
+        Board board = new Board(pieces);
+
+        assertThatThrownBy(() -> bishop.validateMovable(source, target, board))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+    }
+
+    /***
+     * ........ 8
+     * O....... 7
+     * .O.....O 6
+     * ..O...O. 5
+     * ...O.O.. 4
+     * ....B... 3
+     * ...O.O.. 2
+     * ..O...O. 1
+     * --------
+     * abcdefgh
+     */
+    private static Iterable<Coordinate> createTargetForHappyCase() {
+        return List.of(
+                new Coordinate(4, 'd'),
+                new Coordinate(5, 'c'),
+                new Coordinate(6, 'b'),
+                new Coordinate(7, 'a'),
+                new Coordinate(4, 'f'),
+                new Coordinate(5, 'g'),
+                new Coordinate(6, 'h'),
+                new Coordinate(2, 'd'),
+                new Coordinate(1, 'c'),
+                new Coordinate(2, 'f'),
+                new Coordinate(1, 'g'));
+    }
+
+    /***
+     * ........ 8
+     * O....... 7
+     * .O.....O 6
+     * ..O...O. 5
+     * ...OXO.. 4
+     * ...XBX.. 3
+     * ...OXO.. 2
+     * ..O...O. 1
+     * --------
+     * abcdefgh
+     */
+    private static Iterable<Coordinate> createTargetForExceptionCase() {
+        return List.of(
+                new Coordinate(3, 'd'),
+                new Coordinate(3, 'f'),
+                new Coordinate(4, 'e'),
+                new Coordinate(2, 'e'));
     }
 }
