57d1074c3655c4f7a58ef161b77bee3ff9c4c724
Zeus6768 <akaps6768@gmail.com>
2024-03-23 12:32:41 +0900

    fix: 예외메시지 및 변수명 변경

--- src/test/java/chess/domain/attribute/FileTest.java
+++ src/test/java/chess/domain/attribute/FileTest.java
@@ -10,21 +10,22 @@ import chess.domain.chessboard.attribute.File;
 import chess.domain.chessboard.attribute.Rank;
 
 class FileTest {
+
+    @DisplayName("a~h 범위를 벗어나는 문자를 입력하면 예외가 발생한다.")
+    @ValueSource(chars = {'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'})
+    @ParameterizedTest
+    void constructor(char invalidValue) {
+        assertThatThrownBy(() -> Rank.from(invalidValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("파일은 a~h 사이로 입력해주세요: " + invalidValue);
+    }
+
     @DisplayName("1~8 범위를 벗어나는 열 번호를 입력하면 예외가 발생한다.")
     @ValueSource(ints = {0, 9})
     @ParameterizedTest
-    void constructor(int column) {
-        assertThatThrownBy(() -> File.from(column))
+    void constructor(int invalidColumn) {
+        assertThatThrownBy(() -> File.from(invalidColumn))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("파일은 1~8 사이로 입력해주세요: " + column);
-    }
-
-    @DisplayName("a~h 범위를 벗어나는 파일을 입력하면 예외가 발생한다.")
-    @ValueSource(strings = {"i", "j", "k", "l", "m", "n", "o", "p"})
-    @ParameterizedTest
-    void constructor(String value) {
-        assertThatThrownBy(() -> Rank.from(value))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("랭크는 1~8 사이로 입력해주세요: " + value);
+                .hasMessage("파일은 a~h 사이로 입력해주세요: " + invalidColumn);
     }
 }
--- src/test/java/chess/domain/attribute/RankTest.java
+++ src/test/java/chess/domain/attribute/RankTest.java
@@ -12,18 +12,18 @@ class RankTest {
     @DisplayName("0~7 범위를 벗어나는 행 번호를 입력하면 예외가 발생한다.")
     @ValueSource(ints = {-1, 8})
     @ParameterizedTest
-    void constructor(int row) {
-        assertThatThrownBy(() -> Rank.from(row))
+    void constructor(int invalidRow) {
+        assertThatThrownBy(() -> Rank.from(invalidRow))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("랭크는 1~8 사이로 입력해주세요: " + row);
+                .hasMessage("랭크는 1~8 사이로 입력해주세요: " + invalidRow);
     }
 
     @DisplayName("1~8 범위를 벗어나는 랭크를 입력하면 예외가 발생한다.")
     @ValueSource(strings = {"0", "9"})
     @ParameterizedTest
-    void constructor(String value) {
-        assertThatThrownBy(() -> Rank.from(value))
+    void constructor(String invalidRank) {
+        assertThatThrownBy(() -> Rank.from(invalidRank))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("랭크는 1~8 사이로 입력해주세요: " + value);
+                .hasMessage("랭크는 1~8 사이로 입력해주세요: " + invalidRank);
     }
 }
