23f56aa3be14e3b7bd5bf835a83671f5148f1d42
libienz <leekh10195@gmail.com>
2024-03-23 14:36:05 +0900

    test: 포지션 fixture 구현

---
+++ src/test/java/chess/fixture/PositionFixtures.java
@@ -0,0 +1,82 @@
+package chess.fixture;
+
+import chess.domain.position.File;
+import chess.domain.position.FileRankPosition;
+import chess.domain.position.Rank;
+
+public class PositionFixtures {
+    public static final FileRankPosition A1 = new FileRankPosition(File.A, Rank.ONE);
+    public static final FileRankPosition A2 = new FileRankPosition(File.A, Rank.TWO);
+    public static final FileRankPosition A3 = new FileRankPosition(File.A, Rank.THREE);
+    public static final FileRankPosition A4 = new FileRankPosition(File.A, Rank.FOUR);
+    public static final FileRankPosition A5 = new FileRankPosition(File.A, Rank.FIVE);
+    public static final FileRankPosition A6 = new FileRankPosition(File.A, Rank.SIX);
+    public static final FileRankPosition A7 = new FileRankPosition(File.A, Rank.SEVEN);
+    public static final FileRankPosition A8 = new FileRankPosition(File.A, Rank.EIGHT);
+
+    public static final FileRankPosition B1 = new FileRankPosition(File.B, Rank.ONE);
+    public static final FileRankPosition B2 = new FileRankPosition(File.B, Rank.TWO);
+    public static final FileRankPosition B3 = new FileRankPosition(File.B, Rank.THREE);
+    public static final FileRankPosition B4 = new FileRankPosition(File.B, Rank.FOUR);
+    public static final FileRankPosition B5 = new FileRankPosition(File.B, Rank.FIVE);
+    public static final FileRankPosition B6 = new FileRankPosition(File.B, Rank.SIX);
+    public static final FileRankPosition B7 = new FileRankPosition(File.B, Rank.SEVEN);
+    public static final FileRankPosition B8 = new FileRankPosition(File.B, Rank.EIGHT);
+
+    public static final FileRankPosition C1 = new FileRankPosition(File.C, Rank.ONE);
+    public static final FileRankPosition C2 = new FileRankPosition(File.C, Rank.TWO);
+    public static final FileRankPosition C3 = new FileRankPosition(File.C, Rank.THREE);
+    public static final FileRankPosition C4 = new FileRankPosition(File.C, Rank.FOUR);
+    public static final FileRankPosition C5 = new FileRankPosition(File.C, Rank.FIVE);
+    public static final FileRankPosition C6 = new FileRankPosition(File.C, Rank.SIX);
+    public static final FileRankPosition C7 = new FileRankPosition(File.C, Rank.SEVEN);
+    public static final FileRankPosition C8 = new FileRankPosition(File.C, Rank.EIGHT);
+
+    public static final FileRankPosition D1 = new FileRankPosition(File.D, Rank.ONE);
+    public static final FileRankPosition D2 = new FileRankPosition(File.D, Rank.TWO);
+    public static final FileRankPosition D3 = new FileRankPosition(File.D, Rank.THREE);
+    public static final FileRankPosition D4 = new FileRankPosition(File.D, Rank.FOUR);
+    public static final FileRankPosition D5 = new FileRankPosition(File.D, Rank.FIVE);
+    public static final FileRankPosition D6 = new FileRankPosition(File.D, Rank.SIX);
+    public static final FileRankPosition D7 = new FileRankPosition(File.D, Rank.SEVEN);
+    public static final FileRankPosition D8 = new FileRankPosition(File.D, Rank.EIGHT);
+
+    public static final FileRankPosition E1 = new FileRankPosition(File.E, Rank.ONE);
+    public static final FileRankPosition E2 = new FileRankPosition(File.E, Rank.TWO);
+    public static final FileRankPosition E3 = new FileRankPosition(File.E, Rank.THREE);
+    public static final FileRankPosition E4 = new FileRankPosition(File.E, Rank.FOUR);
+    public static final FileRankPosition E5 = new FileRankPosition(File.E, Rank.FIVE);
+    public static final FileRankPosition E6 = new FileRankPosition(File.E, Rank.SIX);
+    public static final FileRankPosition E7 = new FileRankPosition(File.E, Rank.SEVEN);
+    public static final FileRankPosition E8 = new FileRankPosition(File.E, Rank.EIGHT);
+
+    public static final FileRankPosition F1 = new FileRankPosition(File.F, Rank.ONE);
+    public static final FileRankPosition F2 = new FileRankPosition(File.F, Rank.TWO);
+    public static final FileRankPosition F3 = new FileRankPosition(File.F, Rank.THREE);
+    public static final FileRankPosition F4 = new FileRankPosition(File.F, Rank.FOUR);
+    public static final FileRankPosition F5 = new FileRankPosition(File.F, Rank.FIVE);
+    public static final FileRankPosition F6 = new FileRankPosition(File.F, Rank.SIX);
+    public static final FileRankPosition F7 = new FileRankPosition(File.F, Rank.SEVEN);
+    public static final FileRankPosition F8 = new FileRankPosition(File.F, Rank.EIGHT);
+
+    public static final FileRankPosition G1 = new FileRankPosition(File.G, Rank.ONE);
+    public static final FileRankPosition G2 = new FileRankPosition(File.G, Rank.TWO);
+    public static final FileRankPosition G3 = new FileRankPosition(File.G, Rank.THREE);
+    public static final FileRankPosition G4 = new FileRankPosition(File.G, Rank.FOUR);
+    public static final FileRankPosition G5 = new FileRankPosition(File.G, Rank.FIVE);
+    public static final FileRankPosition G6 = new FileRankPosition(File.G, Rank.SIX);
+    public static final FileRankPosition G7 = new FileRankPosition(File.G, Rank.SEVEN);
+    public static final FileRankPosition G8 = new FileRankPosition(File.G, Rank.EIGHT);
+
+    public static final FileRankPosition H1 = new FileRankPosition(File.H, Rank.ONE);
+    public static final FileRankPosition H2 = new FileRankPosition(File.H, Rank.TWO);
+    public static final FileRankPosition H3 = new FileRankPosition(File.H, Rank.THREE);
+    public static final FileRankPosition H4 = new FileRankPosition(File.H, Rank.FOUR);
+    public static final FileRankPosition H5 = new FileRankPosition(File.H, Rank.FIVE);
+    public static final FileRankPosition H6 = new FileRankPosition(File.H, Rank.SIX);
+    public static final FileRankPosition H7 = new FileRankPosition(File.H, Rank.SEVEN);
+    public static final FileRankPosition H8 = new FileRankPosition(File.H, Rank.EIGHT);
+
+    private PositionFixtures() {
+    }
+}
