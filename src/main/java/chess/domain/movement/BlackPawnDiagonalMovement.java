06d1e098af8c8792618d9c2934dfe47164ed24ec
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-03-23 15:00:26 +0900

    feat(PawnDiagonalMovement) : 폰의 대각선 움직임

---
+++ src/main/java/chess/domain/movement/BlackPawnDiagonalMovement.java
@@ -0,0 +1,25 @@
+package chess.domain.movement;
+
+import chess.domain.position.Position;
+import java.util.List;
+
+public class BlackPawnDiagonalMovement implements MovementRule {
+
+    @Override
+    public boolean isMovable(Position start, Position end, boolean isEnemyExistAtEnd) {
+        int rankDifference = start.calculateRankDifference(end);
+        int fileDifference = start.calculateFileDifference(end);
+
+        boolean isMatchDifference = rankDifference == -1 && Math.abs(fileDifference) == 1;
+        return isEnemyExistAtEnd && isMatchDifference;
+    }
+
+    @Override
+    public List<Position> findPath(Position start, Position end, boolean isEnemyExistAtEnd) {
+        if (!isMovable(start, end, isEnemyExistAtEnd)) {
+            throw new IllegalArgumentException("경로가 존재하지 않습니다.");
+        }
+
+        return List.of(end);
+    }
+}
