f0acacfad6389e24d7d8a1aaf8e8761fb847703e
SCY <helenason@naver.com>
2024-03-23 22:34:27 +0900

    feat(Chess): 이동할 수 없는 경우 에러 반환

--- src/main/java/domain/Chess.java
+++ src/main/java/domain/Chess.java
@@ -11,11 +11,23 @@ public class Chess {
     public void play(Position sourcePosition, Position targetPosition) {
         Piece sourcePiece = board.findPieceByPosition(sourcePosition);
         Piece targetPiece = board.findPieceByPosition(targetPosition);
-        if (sourcePiece.isDifferentColor(targetPiece) && sourcePiece.canMove(sourcePosition, targetPosition)
-                && board.isNotBlocked(sourcePosition, targetPosition)) {
+
+        if (!sourcePiece.isKnight() && !board.isNotBlocked(sourcePosition, targetPosition)) {
+            throw new IllegalArgumentException("[ERROR] 이동할 수 없는 위치입니다. 경로가 비어있지 않습니다.");
+        }
+        if (!sourcePiece.isDifferentColor(targetPiece)) {
+            throw new IllegalArgumentException("[ERROR] 이동할 수 없는 위치입니다. 목적지에 같은 편 기물이 있습니다.");
+        }
+        if (sourcePiece.canAttack(sourcePosition, targetPosition) && sourcePiece.isDifferentColor(targetPiece)) {
             board.placePieceByPosition(sourcePiece, targetPosition);
             board.displacePieceByPosition(sourcePosition);
+            return;
         }
+        if (!sourcePiece.canMove(sourcePosition, targetPosition)) {
+            throw new IllegalArgumentException("[ERROR] 이동할 수 없는 위치입니다. 해당 기물은 해당 위치로 움직일 수 없습니다.");
+        }
+        board.placePieceByPosition(sourcePiece, targetPosition);
+        board.displacePieceByPosition(sourcePosition);
     }
 
     public Board getBoard() {
