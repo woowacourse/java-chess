a1afebed0b237a2edb55bfab771e6d13ef8548ab
woojong <dwax1324@gmail.com>
2024-03-24 02:45:57 +0900

    test: 폰은 뒤, 옆으로 움직일 수 없다

--- src/test/java/domain/piece/PawnTest.java
+++ src/test/java/domain/piece/PawnTest.java
@@ -2,13 +2,20 @@ package domain.piece;
 
 import static domain.VectorFixture.DOWN;
 import static domain.VectorFixture.DOWN_DOWN;
+import static domain.VectorFixture.DOWN_LEFT;
+import static domain.VectorFixture.LEFT;
 import static domain.VectorFixture.LEFT_UP;
+import static domain.VectorFixture.RIGHT;
+import static domain.VectorFixture.RIGHT_DOWN;
 import static domain.VectorFixture.UP;
+import static domain.VectorFixture.UP_RIGHT;
 import static domain.VectorFixture.UP_UP;
 import static domain.piece.info.Color.*;
+import static org.assertj.core.api.AbstractSoftAssertions.assertAll;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import domain.piece.info.Vector;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -35,6 +42,26 @@ class PawnTest {
     }
 
     @Test
+    @DisplayName("폰은 뒤, 옆으로 움직일 수 없다")
+    void pawnBackward() {
+        final Piece whitePawn = new InitPawn(WHITE);
+        final Piece blackPawn = new InitPawn(BLACK);
+
+        Assertions.assertAll(
+                () -> assertThat(whitePawn.isReachable(LEFT, Empty.INSTANCE)).isFalse(),
+                () -> assertThat(whitePawn.isReachable(RIGHT, Empty.INSTANCE)).isFalse(),
+                () -> assertThat(whitePawn.isReachable(DOWN, Empty.INSTANCE)).isFalse(),
+                () -> assertThat(whitePawn.isReachable(DOWN_LEFT, Empty.INSTANCE)).isFalse(),
+                () -> assertThat(whitePawn.isReachable(RIGHT_DOWN, Empty.INSTANCE)).isFalse(),
+                () -> assertThat(blackPawn.isReachable(UP, Empty.INSTANCE)).isFalse(),
+                () -> assertThat(blackPawn.isReachable(UP_RIGHT, Empty.INSTANCE)).isFalse(),
+                () -> assertThat(blackPawn.isReachable(LEFT_UP, Empty.INSTANCE)).isFalse()
+        );
+
+
+    }
+
+    @Test
     @DisplayName("처음 움직이는 폰은 앞으로 두 칸 움직일 수 있다")
     void initPawn() {
         final Piece whitePawn = new InitPawn(WHITE);
@@ -43,6 +70,7 @@ class PawnTest {
         assertThat(whitePawn.isReachable(UP_UP, Empty.INSTANCE)).isTrue();
         assertThat(blackPawn.isReachable(DOWN_DOWN, Empty.INSTANCE)).isTrue();
     }
+
 
     /*
         firstMove => moveTwo + no enemy
