6b75092ab4d39738afab01b1149516e1e6ab6d8c
kelly6bf <kelly6bf@gmail.com>
2024-03-23 20:47:56 +0900

    feat: Queen 구현
    Co-authore현d-by: Hyunguk Ryu <hw0603@naver.com>

---
+++ src/main/java/domain/piece/Piece.java
@@ -0,0 +1,23 @@
+package domain.piece;
+
+import domain.position.Position;
+
+import java.util.Map;
+
+public abstract class Piece {
+    private final PieceColor color;
+
+    protected Piece(final PieceColor color) {
+        this.color = color;
+    }
+
+    public abstract void checkMovable(final Position source, final Position destination, final Map<Position, Piece> piecePositions);
+
+    protected boolean checkEnemy(final Piece otherPiece) {
+        return otherPiece.isEnemy(this.color);
+    }
+
+    public boolean isEnemy(final PieceColor otherColor) {
+        return this.color != otherColor;
+    }
+}
---
+++ src/main/java/domain/piece/Queen.java
@@ -0,0 +1,58 @@
+package domain.piece;
+
+import domain.position.CommonMovementDirection;
+import domain.position.Position;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static domain.position.CommonMovementDirection.*;
+
+public class Queen extends Piece {
+
+    public Queen(final PieceColor color) {
+        super(color);
+    }
+
+    @Override
+    public void checkMovable(final Position source, final Position destination, final Map<Position, Piece> piecePositions) {
+        CommonMovementDirection movementDirection = calculateDirection(source, destination);
+
+        Position alivePosition = move(source, destination, movementDirection, piecePositions);
+
+        checkAlivePosition(alivePosition, piecePositions);
+    }
+
+    private Position move(
+            final Position source,
+            final Position destination,
+            final CommonMovementDirection movementDirection,
+            final Map<Position, Piece> piecePositions
+    ) {
+        List<Position> movePaths = Stream.iterate(source, current -> current.next(movementDirection))
+                .takeWhile(current -> isContinuable(current, destination, piecePositions))
+                .toList();
+
+        return movePaths.get(movePaths.size() - 1)
+                .next(movementDirection);
+    }
+
+    private boolean isContinuable(final Position current, final Position destination, final Map<Position, Piece> piecePositions) {
+        if (current.equals(destination)) {
+            return false;
+        }
+
+        if (piecePositions.containsKey(current)) {
+            throw new IllegalArgumentException("목적지 경로에 기물이 존재하여 이동할 수 없습니다.");
+        }
+
+        return true;
+    }
+
+    private void checkAlivePosition(final Position alivePosition, final Map<Position, Piece> piecePositions) {
+        if (piecePositions.containsKey(alivePosition) && !checkEnemy(piecePositions.get(alivePosition))) {
+            throw new IllegalArgumentException("아군 기물이 위치한 칸으로는 이동할 수 없습니다.");
+        }
+    }
+}
