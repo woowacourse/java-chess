86fc5f0c42bdbb7fb93a5a72111d6975b5a843af
PgmJun <chltmdwns96@gmail.com>
2024-03-23 14:34:24 +0900

    test: between 테스트 보충

--- src/test/java/chess/domain/position/ChessFileTest.java
+++ src/test/java/chess/domain/position/ChessFileTest.java
@@ -2,8 +2,12 @@ package chess.domain.position;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -36,17 +40,20 @@ class ChessFileTest {
     }
 
     @DisplayName("파일과 파일 사이의 파일들을 조회한다.")
-    @Test
-    void findFileBetween() {
-        // given
-        ChessFile start = ChessFile.A;
-        ChessFile end = ChessFile.H;
-        List<ChessFile> expected = List.of(ChessFile.B, ChessFile.C, ChessFile.D, ChessFile.E, ChessFile.F, ChessFile.G);
-
+    @ParameterizedTest
+    @MethodSource("findFileBetweenArguments")
+    void findFileBetween(ChessFile start, ChessFile end, List<ChessFile> expected) {
         // when
         List<ChessFile> fileBetween = ChessFile.findBetween(start, end);
 
         // then
         assertThat(expected).containsAll(fileBetween);
+    }
+
+    private static Stream<Arguments> findFileBetweenArguments() {
+        return Stream.of(
+                Arguments.of(ChessFile.A, ChessFile.H, List.of(ChessFile.B, ChessFile.C, ChessFile.D, ChessFile.E, ChessFile.F, ChessFile.G)),
+                Arguments.of(ChessFile.H, ChessFile.A, List.of(ChessFile.B, ChessFile.C, ChessFile.D, ChessFile.E, ChessFile.F, ChessFile.G))
+        );
     }
 }
--- src/test/java/chess/domain/position/ChessRankTest.java
+++ src/test/java/chess/domain/position/ChessRankTest.java
@@ -2,8 +2,12 @@ package chess.domain.position;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -36,24 +40,20 @@ class ChessRankTest {
     }
 
     @DisplayName("랭크와 랭크 사이의 랭크들을 조회한다.")
-    @Test
-    void findRankBetween() {
-        // given
-        ChessRank start = ChessRank.ONE;
-        ChessRank end = ChessRank.EIGHT;
-        List<ChessRank> expected = List.of(
-                ChessRank.TWO,
-                ChessRank.THREE,
-                ChessRank.FOUR,
-                ChessRank.FIVE,
-                ChessRank.SIX,
-                ChessRank.SEVEN
-        );
-
+    @ParameterizedTest
+    @MethodSource("findRankBetweenArguments")
+    void findRankBetween(ChessRank start, ChessRank end, List<ChessRank> expected) {
         // when
         List<ChessRank> rankBetween = ChessRank.findBetween(start, end);
 
         // then
         assertThat(expected).containsAll(rankBetween);
+    }
+
+    private static Stream<Arguments> findRankBetweenArguments() {
+        return Stream.of(
+                Arguments.of(ChessRank.ONE, ChessRank.EIGHT, List.of(ChessRank.TWO, ChessRank.THREE, ChessRank.FOUR, ChessRank.FIVE, ChessRank.SIX, ChessRank.SEVEN)),
+                Arguments.of(ChessRank.EIGHT, ChessRank.ONE, List.of(ChessRank.TWO, ChessRank.THREE, ChessRank.FOUR, ChessRank.FIVE, ChessRank.SIX, ChessRank.SEVEN))
+        );
     }
 }
