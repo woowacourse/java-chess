0666c2bb2bd8d504a628e4a463e0c085ed5057dd
woojong <dwax1324@gmail.com>
2024-03-23 17:02:40 +0900

    feat: 벡터를 생성할 때 두 위치가 같으면 예외가 발생한다

--- src/test/java/domain/piece/info/VectorTest.java
+++ src/test/java/domain/piece/info/VectorTest.java
@@ -2,12 +2,19 @@ package domain.piece.info;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class VectorTest {
 
     @Test
-    void isStraight() {
+    @DisplayName("두 위치가 같으면 예외가 발생한다")
+    void isSamePosition() {
+        final Position position = new Position(File.A, Rank.ONE);
+        Assertions.assertThatThrownBy(() -> new Vector(position, position))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("두 위치가 같습니다");
     }
 
     @Test
