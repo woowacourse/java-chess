2fbf73ec9ef31db84e59e84d8d43cad0a313b60e
Hyeon0208 <dmsgktn0208@gmail.com>
2024-03-23 22:48:48 +0900

    refactor: 텍스트 블록을 사용해 메시지 출력

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -7,14 +7,10 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class InputView {
-    private static final String GAME_START_MESSAGE = "> 체스 게임을 시작합니다.";
-    private static final String START_INFO_MESSAGE = "> 게임 시작 : start";
-    private static final String END_INFO_MESSAGE = "> 게임 종료 : end";
     private static final Set<String> GAME_COMMAND = Set.of("start", "end");
     private static final String MOVE_COMMAND = "move";
     private static final String INVALID_GAME_COMMAND = "start 또는 end만 입력가능합니다.";
     private static final String INVALID_MOVE_COMMAND = "부적절한 명령어입니다. move b2 b3와 같이 입력해주세요.";
-    private static final String MOVE_INFO_MESSAGE = "> 게임 이동 : move source위치 target위치 - 예. move b2 b3";
     private static final String MOVE_COMMAND_DELIMITER = " ";
 
     private final Scanner scanner;
@@ -24,10 +20,12 @@ public class InputView {
     }
 
     public String readStartCommand() {
-        System.out.println(GAME_START_MESSAGE);
-        System.out.println(START_INFO_MESSAGE);
-        System.out.println(END_INFO_MESSAGE);
-        System.out.println(MOVE_INFO_MESSAGE);
+        System.out.print("""
+        > 체스 게임을 시작합니다.
+        > 게임 시작 : start
+        > 게임 종료 : end
+        > 게임 이동 : move source위치 target위치 - 예. move b2 b3
+        """);
         String input = scanner.nextLine();
         validateGameCommand(input);
         return input;
