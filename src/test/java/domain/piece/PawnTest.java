62618b11bef3d0d09ac519dab7fb2582affa4c00
woojong <dwax1324@gmail.com>
2024-03-23 22:41:45 +0900

    test: 폰 앞에 같은편 말이 있을 때 앞으로 한 칸 이동할 수 없다

--- src/test/java/domain/piece/PawnTest.java
+++ src/test/java/domain/piece/PawnTest.java
@@ -1,5 +1,6 @@
 package domain.piece;
 
+import static domain.piece.info.Color.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,16 +14,21 @@ class PawnTest {
     @Test
     @DisplayName("폰 앞이 비어있을 때 앞으로 한 칸 이동할 수 있다")
     void isReachable() {
-        final Piece whitePawn = new Pawn(Color.WHITE);
-        final Piece blackPawn = new Pawn(Color.BLACK);
+        final Piece whitePawn = new Pawn(WHITE);
+        final Piece blackPawn = new Pawn(BLACK);
 
         assertThat(whitePawn.isReachable(Vector.of(0, 1), Empty.INSTANCE)).isTrue();
         assertThat(blackPawn.isReachable(Vector.of(0, -1), Empty.INSTANCE)).isTrue();
     }
 
     @Test
-    @DisplayName("폰은 앞으로 한 칸 이동할 수 있다")
+    @DisplayName("폰 앞에 같은편 말이 있을 때 앞으로 한 칸 이동할 수 없다")
     void defaultMovement() {
+        final Piece whitePawn = new Pawn(WHITE);
+        final Piece blackPawn = new Pawn(BLACK);
+
+        assertThat(whitePawn.isReachable(Vector.of(0, 1), new Pawn(WHITE))).isFalse();
+        assertThat(blackPawn.isReachable(Vector.of(0, -1), new Pawn(BLACK))).isFalse();
     }
 
     @Test
