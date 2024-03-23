4b95f47cac460a3fc8ac5d1d36cc29fddeb7cb33
MingyeomKim <mingyum119@naver.com>
2024-03-23 23:41:47 +0900

    feat(DirectionTest): 방향 탐색에 대한 테스트 케이스 추가

--- src/test/java/domain/game/DirectionTest.java
+++ src/test/java/domain/game/DirectionTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class DirectionTest {
 
-    @DisplayName("(1,1) -> (2,2)로 이동하면, 북동 방향이다.")
+    @DisplayName("(b,1) -> (b,2)로 이동하면, 북동 방향이다.")
     @Test
     void moveToNorthEast() {
         Position sourcePosition = PositionFixture.generateB1Position();
@@ -16,5 +16,25 @@ class DirectionTest {
 
         Direction findDirection = Direction.findDirection(sourcePosition, targetPosition);
         Assertions.assertThat(findDirection).isEqualTo(Direction.NORTH_EAST);
+    }
+
+    @DisplayName("(b,1) -> (c, 3)로 이동하면, UP_RIGHT 방향이다.")
+    @Test
+    void moveToUpRight() {
+        Position sourcePosition = PositionFixture.generateB1Position();
+        Position targetPosition = PositionFixture.generateC3Position();
+
+        Direction findDirection = Direction.findDirection(sourcePosition, targetPosition);
+        Assertions.assertThat(findDirection).isEqualTo(Direction.UP_RIGHT);
+    }
+
+    @DisplayName("(c,4) -> (f,1)로 이동하면, 남동 방향이다.")
+    @Test
+    void moveToSouthEast() {
+        Position sourcePosition = PositionFixture.generateC4Position();
+        Position targetPosition = PositionFixture.generateF1Position();
+
+        Direction findDirection = Direction.findDirection(sourcePosition, targetPosition);
+        Assertions.assertThat(findDirection).isEqualTo(Direction.SOUTH_EAST);
     }
 }
--- src/test/java/fixture/PositionFixture.java
+++ src/test/java/fixture/PositionFixture.java
@@ -17,6 +17,10 @@ public final class PositionFixture {
         return new Position(new File('c'), new Rank(3));
     }
 
+    public static Position generateC4Position() {
+        return new Position(new File('c'), new Rank(4));
+    }
+
     public static Position generateB1Position() {
         return new Position(new File('b'), new Rank(1));
     }
@@ -25,12 +29,15 @@ public final class PositionFixture {
         return new Position(new File('b'), new Rank(2));
     }
 
+    public static Position generateB3Position() {
+        return new Position(new File('b'), new Rank(3));
+    }
+
     public static Position generateB7Position() {
         return new Position(new File('b'), new Rank(7));
     }
 
-
-    public static Position generateB3Position() {
-        return new Position(new File('b'), new Rank(3));
+    public static Position generateF1Position() {
+        return new Position(new File('f'), new Rank(1));
     }
 }
