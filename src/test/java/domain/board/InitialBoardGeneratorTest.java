5303e5c6381076d3c7973c0999eaabeea9842aa0
KimSangHyeok <tkdgur0906@naver.com>
2024-03-24 03:49:15 +0900

    refactor: 모든 체스판의 말이 초기화되었는지 테스트

--- src/test/java/domain/board/InitialBoardGeneratorTest.java
+++ src/test/java/domain/board/InitialBoardGeneratorTest.java
@@ -16,6 +16,33 @@ class InitialBoardGeneratorTest {
         Board board = Board.generatedBy(boardGenerator);
 
         assertAll(
+                () -> assertThat(board.findPieceAt(Position.of(1, 1))).isInstanceOf(Rook.class),
+                () -> assertThat(board.findPieceAt(Position.of(2, 1))).isInstanceOf(Knight.class),
+                () -> assertThat(board.findPieceAt(Position.of(3, 1))).isInstanceOf(Bishop.class),
+                () -> assertThat(board.findPieceAt(Position.of(4, 1))).isInstanceOf(Queen.class),
+                () -> assertThat(board.findPieceAt(Position.of(5, 1))).isInstanceOf(King.class),
+                () -> assertThat(board.findPieceAt(Position.of(6, 1))).isInstanceOf(Bishop.class),
+                () -> assertThat(board.findPieceAt(Position.of(7, 1))).isInstanceOf(Knight.class),
+                () -> assertThat(board.findPieceAt(Position.of(8, 1))).isInstanceOf(Rook.class),
+
+                () -> assertThat(board.findPieceAt(Position.of(1, 2))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(2, 2))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(3, 2))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(4, 2))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(5, 2))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(6, 2))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(7, 2))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(8, 2))).isInstanceOf(Pawn.class),
+
+                () -> assertThat(board.findPieceAt(Position.of(1, 7))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(2, 7))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(3, 7))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(4, 7))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(5, 7))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(6, 7))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(7, 7))).isInstanceOf(Pawn.class),
+                () -> assertThat(board.findPieceAt(Position.of(8, 7))).isInstanceOf(Pawn.class),
+
                 () -> assertThat(board.findPieceAt(Position.of(1, 8))).isInstanceOf(Rook.class),
                 () -> assertThat(board.findPieceAt(Position.of(2, 8))).isInstanceOf(Knight.class),
                 () -> assertThat(board.findPieceAt(Position.of(3, 8))).isInstanceOf(Bishop.class),
