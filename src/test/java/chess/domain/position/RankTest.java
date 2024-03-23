daf74cd5f8b0c9c16e83c48adefde4ba1182c90e
libienz <leekh10195@gmail.com>
2024-03-23 13:28:33 +0900

    test: 잘못된 행번호로 Rank를 찾을 경우 예외가 발생하는지 확인하는 테스트 추가

--- src/test/java/chess/domain/position/RankTest.java
+++ src/test/java/chess/domain/position/RankTest.java
@@ -1,10 +1,12 @@
 package chess.domain.position;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RankTest {
     @DisplayName("0부터 시작하는 행번호로 Rank를 찾을 수 있다")
@@ -12,5 +14,13 @@ class RankTest {
     @CsvSource(value = {"0, EIGHT", "1, SEVEN", "2, SIX", "3, FIVE", "4, FOUR", "5, THREE", "6, TWO", "7, ONE"})
     void should_FindRank_When_RowNumberGiven(int rowNumber, Rank rank) {
         assertThat(Rank.from(rowNumber)).isEqualTo(rank);
+    }
+
+    @DisplayName("행번호에 해당하는 Rank가 없을 경우 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 9, 10})
+    void should_ThrowIllegalArgumentException_When_WrongRowNumberIsGiven(int invalidRowNumber) {
+        assertThatThrownBy(() -> Rank.from(invalidRowNumber))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
