067b0ee0363baae218c7319301f96cb27e758b09
tsulocalize <tsulocalizing@gmail.com>
2024-03-23 18:14:30 +0900

    test: TestPiece를 PieceTest의 내부 클래스로 이동

--- src/test/java/chess/domain/piece/PieceTest.java
+++ src/test/java/chess/domain/piece/PieceTest.java
@@ -1,5 +1,6 @@
 package chess.domain.piece;
 
+import chess.domain.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +24,22 @@ class PieceTest {
         Piece piece2 = new TestPiece(Color.WHITE);
 
         assertThat(piece1.isSameColor(piece2)).isFalse();
+    }
+
+    private static class TestPiece extends Piece {
+
+        public TestPiece(Color color) {
+            super(color, ((fileDifference, rankDifference) -> false));
+        }
+
+        @Override
+        public boolean isMovable(Position from, Position to) {
+            return false;
+        }
+
+        @Override
+        public boolean isCatchable(Position from, Position to) {
+            return false;
+        }
     }
 }
