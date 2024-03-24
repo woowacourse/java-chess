fba2e822c87dcbdd974fab5dc6a7544c353e120e
le2sky <leehaneul990623@gmail.com>
2024-03-24 10:25:29 +0900

    refactor: pawn 관련 코드 가독성 개선

--- src/main/java/chess/domain/piece/Direction.java
+++ src/main/java/chess/domain/piece/Direction.java
@@ -2,27 +2,28 @@ package chess.domain.piece;
 
 enum Direction {
 
-    LEFT(new Weight(0, -1)),
-    RIGHT(new Weight(0, 1)),
-    UP(new Weight(1, 0)),
-    DOWN(new Weight(-1, 0)),
-    UP_RIGHT(new Weight(1, 1)),
-    DOWN_RIGHT(new Weight(-1, 1)),
-    UP_LEFT(new Weight(1, -1)),
-    DOWN_LEFT(new Weight(-1, -1)),
-    UP_UP_RIGHT(new Weight(2, 1)),
-    UP_UP_LEFT(new Weight(2, -1)),
-    LEFT_LEFT_UP(new Weight(1, -2)),
-    LEFT_LEFT_DOWN(new Weight(-1, -2)),
-    RIGHT_RIGHT_UP(new Weight(1, 2)),
-    RIGHT_RIGHT_DOWN(new Weight(-1, 2)),
-    DOWN_DOWN_LEFT(new Weight(-2, -1)),
-    DOWN_DOWN_RIGHT(new Weight(-2, 1));
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    UP(1, 0),
+    UP_UP(2, 0),
+    DOWN(-1, 0),
+    UP_RIGHT(1, 1),
+    DOWN_RIGHT(-1, 1),
+    UP_LEFT(1, -1),
+    DOWN_LEFT(-1, -1),
+    UP_UP_RIGHT(2, 1),
+    UP_UP_LEFT(2, -1),
+    LEFT_LEFT_UP(1, -2),
+    LEFT_LEFT_DOWN(-1, -2),
+    RIGHT_RIGHT_UP(1, 2),
+    RIGHT_RIGHT_DOWN(-1, 2),
+    DOWN_DOWN_LEFT(-2, -1),
+    DOWN_DOWN_RIGHT(-2, 1);
 
     private final Weight weight;
 
-    Direction(Weight weight) {
-        this.weight = weight;
+    Direction(int rankWeight, int fileWeight) {
+        this.weight = new Weight(rankWeight, fileWeight);
     }
 
     public Weight getWeight() {
--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,21 +1,20 @@
 package chess.domain.piece;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import chess.domain.board.Board;
 import chess.domain.board.Coordinate;
 
+// TODO: depth 정리는 했는데, 아직도 개선할 점이 많아 보임!
 public class Pawn extends AbstractPiece {
 
-    private static final List<Map.Entry<Integer, Integer>> FORWARD_WEIGHTS = List.of(
-            Map.entry(1, 0),
-            Map.entry(2, 0)
+    private final List<Direction> forwardDirections = List.of(
+            Direction.UP,
+            Direction.UP_UP
     );
 
-    private static final List<Map.Entry<Integer, Integer>> DIAGONAL_WEIGHT = List.of(
-            Map.entry(1, -1),
-            Map.entry(1, 1)
+    private final List<Direction> diagonalDirections = List.of(
+            Direction.UP_LEFT,
+            Direction.UP_RIGHT
     );
 
     //TODO: 상수의 올바른 위치를 생각해보기.
@@ -28,69 +27,75 @@ public class Pawn extends AbstractPiece {
 
     @Override
     void validatePieceMoveRule(Coordinate source, Coordinate target, Board board) {
-        List<Coordinate> diagonalPath = createPath(source, DIAGONAL_WEIGHT);
-        List<Coordinate> forwardPath = createPath(source, FORWARD_WEIGHTS);
+        List<Coordinate> diagonalPath = createPath(source, diagonalDirections);
+        List<Coordinate> forwardPath = createPath(source, forwardDirections);
+        validateReachable(target, diagonalPath, forwardPath);
+        validateDiagonal(target, board, diagonalPath);
+        validateForward(source, target, board, forwardPath);
+    }
+
+
+    // TODO: AbstractNonSlidingPiece와 중복
+    private List<Coordinate> createPath(Coordinate source, List<Direction> directions) {
+        int forwardDirection = team.getForwardDirection();
+
+        return directions.stream()
+                .map(Direction::getWeight)
+                .map(weight -> weight.multiplyAtRankWeight(forwardDirection))
+                .filter(source::isApplicable)
+                .map(source::apply)
+                .toList();
+    }
+
+    private void validateReachable(Coordinate target, List<Coordinate> diagonalPath, List<Coordinate> forwardPath) {
         if (!(forwardPath.contains(target) || diagonalPath.contains(target))) {
             throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
         }
+    }
 
-        //TODO: depth 정리
+    private void validateDiagonal(Coordinate target, Board board, List<Coordinate> diagonalPath) {
         if (diagonalPath.contains(target)) {
-            if (!board.isPiecePresent(target)) {
-                throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
-            }
+            validateEnemyExist(target, board);
+        }
+    }
 
-            if (!isEnemy(board.findByCoordinate(target))) {
-                throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
-            }
+    private void validateEnemyExist(Coordinate target, Board board) {
+        if (!board.isPiecePresent(target)) {
+            throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
         }
 
+        if (!isEnemy(board.findByCoordinate(target))) {
+            throw new IllegalStateException("해당 기물은 주어진 좌표로 이동할 수 없습니다.");
+        }
+    }
+
+    private void validateForward(Coordinate source, Coordinate target, Board board, List<Coordinate> forwardPath) {
         if (forwardPath.contains(target)) {
-            if (Math.abs(source.getRank() - target.getRank()) == 2 &&
-                    !(source.getRank() == INITIAL_BLACK_PAWN_RANK || source.getRank() == INITIAL_WHITE_PAWN_RANK)) {
-                throw new IllegalStateException("초기 상태의 폰이 아닌 경우, 2칸 이동할 수 없습니다.");
-            }
-
-            List<Coordinate> movablePath = createMovablePath(forwardPath, board);
-            if (!movablePath.contains(target)) {
-                throw new IllegalStateException("기물로 막혀있어 이동할 수 없습니다.");
-            }
+            validateInitialCoordinate(source, target);
+            validateBlocked(target, forwardPath, board);
         }
     }
 
-    public List<Coordinate> createPath(Coordinate source, List<Map.Entry<Integer, Integer>> weights) {
-        List<Coordinate> possibleCoordinate = new ArrayList<>();
-        int startRank = source.getRank();
-        char startFile = source.getFile();
-
-        //TODO: forwardDirection 가중치를 WEIGHTS에 곱한채로 저장하는 방법을 고려해보기
-        // 행열의 곱을 이용하는 것은 어떠한가?
-        // Weight(x, y).multifly(Weight(x, y)) = Weight(x * x, y * y)
-        for (Map.Entry<Integer, Integer> weight : weights) {
-            int nextRank = startRank + weight.getKey() * getTeam().getForwardDirection();
-            char nextFile = (char) (startFile + weight.getValue());
-
-            try {
-                Coordinate coordinate = new Coordinate(nextRank, nextFile);
-                possibleCoordinate.add(coordinate);
-            } catch (IllegalArgumentException ignored) {
-            }
+    private void validateInitialCoordinate(Coordinate source, Coordinate target) {
+        if (isTwoStep(source, target) &&
+                !(source.getRank() == INITIAL_BLACK_PAWN_RANK || source.getRank() == INITIAL_WHITE_PAWN_RANK)) {
+            throw new IllegalStateException("초기 상태의 폰이 아닌 경우, 2칸 이동할 수 없습니다.");
         }
-
-        return possibleCoordinate;
     }
 
-    private List<Coordinate> createMovablePath(List<Coordinate> possiblePath, Board board) {
-        List<Coordinate> movablePath = new ArrayList<>();
+    private boolean isTwoStep(Coordinate source, Coordinate target) {
+        return Math.abs(source.getRank() - target.getRank()) == 2;
+    }
 
-        for (Coordinate coordinate : possiblePath) {
-            if (board.isPiecePresent(coordinate)) {
-                break;
-            }
+    // TODO: AbstractSlidingPiece와 중복
+    private void validateBlocked(Coordinate target, List<Coordinate> slidingPath, Board board) {
+        Coordinate blockedCoordinate = slidingPath.stream()
+                .filter(board::isPiecePresent)
+                .findFirst()
+                .orElse(target);
 
-            movablePath.add(coordinate);
+        if (!blockedCoordinate.equals(target)) {
+            throw new IllegalStateException("기물로 막혀있어 이동할 수 없습니다.");
         }
-
-        return movablePath;
     }
 }
