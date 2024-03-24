cf88ed653bb5f8abbc9276fc79598ba6f13c7b25
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-03-24 16:09:25 +0900

    refactor: 테스트 코드에서 사용할 보드 초기화 방식 변경
    HashMap 생성 후 put -> Map.ofEntries로 바로 생성

--- src/test/java/chess/domain/board/BoardFactoryTest.java
+++ src/test/java/chess/domain/board/BoardFactoryTest.java
@@ -11,7 +11,6 @@ import chess.domain.square.Square;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @DisplayName("보드 팩토리")
@@ -22,42 +21,42 @@ class BoardFactoryTest {
     void createBoard() {
         final Board actual = BoardFactory.createBoard();
 
-        final Map<Square, Piece> expected = new HashMap<>();
-        expected.put(new Square(File.a, Rank.EIGHT), new Piece(PieceType.ROOK, PieceColor.BLACK));
-        expected.put(new Square(File.b, Rank.EIGHT), new Piece(PieceType.KNIGHT, PieceColor.BLACK));
-        expected.put(new Square(File.c, Rank.EIGHT), new Piece(PieceType.BISHOP, PieceColor.BLACK));
-        expected.put(new Square(File.d, Rank.EIGHT), new Piece(PieceType.QUEEN, PieceColor.BLACK));
-        expected.put(new Square(File.e, Rank.EIGHT), new Piece(PieceType.KING, PieceColor.BLACK));
-        expected.put(new Square(File.f, Rank.EIGHT), new Piece(PieceType.BISHOP, PieceColor.BLACK));
-        expected.put(new Square(File.g, Rank.EIGHT), new Piece(PieceType.KNIGHT, PieceColor.BLACK));
-        expected.put(new Square(File.h, Rank.EIGHT), new Piece(PieceType.ROOK, PieceColor.BLACK));
+        final Map<Square, Piece> expected = Map.ofEntries(
+                Map.entry(new Square(File.a, Rank.EIGHT), new Piece(PieceType.ROOK, PieceColor.BLACK)),
+                Map.entry(new Square(File.b, Rank.EIGHT), new Piece(PieceType.KNIGHT, PieceColor.BLACK)),
+                Map.entry(new Square(File.c, Rank.EIGHT), new Piece(PieceType.BISHOP, PieceColor.BLACK)),
+                Map.entry(new Square(File.d, Rank.EIGHT), new Piece(PieceType.QUEEN, PieceColor.BLACK)),
+                Map.entry(new Square(File.e, Rank.EIGHT), new Piece(PieceType.KING, PieceColor.BLACK)),
+                Map.entry(new Square(File.f, Rank.EIGHT), new Piece(PieceType.BISHOP, PieceColor.BLACK)),
+                Map.entry(new Square(File.g, Rank.EIGHT), new Piece(PieceType.KNIGHT, PieceColor.BLACK)),
+                Map.entry(new Square(File.h, Rank.EIGHT), new Piece(PieceType.ROOK, PieceColor.BLACK)),
 
-        expected.put(new Square(File.a, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
-        expected.put(new Square(File.b, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
-        expected.put(new Square(File.c, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
-        expected.put(new Square(File.d, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
-        expected.put(new Square(File.e, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
-        expected.put(new Square(File.f, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
-        expected.put(new Square(File.g, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
-        expected.put(new Square(File.h, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK));
+                Map.entry(new Square(File.a, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
+                Map.entry(new Square(File.b, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
+                Map.entry(new Square(File.c, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
+                Map.entry(new Square(File.d, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
+                Map.entry(new Square(File.e, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
+                Map.entry(new Square(File.f, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
+                Map.entry(new Square(File.g, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
+                Map.entry(new Square(File.h, Rank.SEVEN), new Piece(PieceType.PAWN, PieceColor.BLACK)),
 
-        expected.put(new Square(File.a, Rank.ONE), new Piece(PieceType.ROOK, PieceColor.WHITE));
-        expected.put(new Square(File.b, Rank.ONE), new Piece(PieceType.KNIGHT, PieceColor.WHITE));
-        expected.put(new Square(File.c, Rank.ONE), new Piece(PieceType.BISHOP, PieceColor.WHITE));
-        expected.put(new Square(File.d, Rank.ONE), new Piece(PieceType.QUEEN, PieceColor.WHITE));
-        expected.put(new Square(File.e, Rank.ONE), new Piece(PieceType.KING, PieceColor.WHITE));
-        expected.put(new Square(File.f, Rank.ONE), new Piece(PieceType.BISHOP, PieceColor.WHITE));
-        expected.put(new Square(File.g, Rank.ONE), new Piece(PieceType.KNIGHT, PieceColor.WHITE));
-        expected.put(new Square(File.h, Rank.ONE), new Piece(PieceType.ROOK, PieceColor.WHITE));
+                Map.entry(new Square(File.a, Rank.ONE), new Piece(PieceType.ROOK, PieceColor.WHITE)),
+                Map.entry(new Square(File.b, Rank.ONE), new Piece(PieceType.KNIGHT, PieceColor.WHITE)),
+                Map.entry(new Square(File.c, Rank.ONE), new Piece(PieceType.BISHOP, PieceColor.WHITE)),
+                Map.entry(new Square(File.d, Rank.ONE), new Piece(PieceType.QUEEN, PieceColor.WHITE)),
+                Map.entry(new Square(File.e, Rank.ONE), new Piece(PieceType.KING, PieceColor.WHITE)),
+                Map.entry(new Square(File.f, Rank.ONE), new Piece(PieceType.BISHOP, PieceColor.WHITE)),
+                Map.entry(new Square(File.g, Rank.ONE), new Piece(PieceType.KNIGHT, PieceColor.WHITE)),
+                Map.entry(new Square(File.h, Rank.ONE), new Piece(PieceType.ROOK, PieceColor.WHITE)),
 
-        expected.put(new Square(File.a, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
-        expected.put(new Square(File.b, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
-        expected.put(new Square(File.c, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
-        expected.put(new Square(File.d, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
-        expected.put(new Square(File.e, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
-        expected.put(new Square(File.f, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
-        expected.put(new Square(File.g, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
-        expected.put(new Square(File.h, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE));
+                Map.entry(new Square(File.a, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)),
+                Map.entry(new Square(File.b, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)),
+                Map.entry(new Square(File.c, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)),
+                Map.entry(new Square(File.d, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)),
+                Map.entry(new Square(File.e, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)),
+                Map.entry(new Square(File.f, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)),
+                Map.entry(new Square(File.g, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)),
+                Map.entry(new Square(File.h, Rank.TWO), new Piece(PieceType.PAWN, PieceColor.WHITE)));
 
         assertThat(actual).usingRecursiveComparison().isEqualTo(new Board(expected));
     }
