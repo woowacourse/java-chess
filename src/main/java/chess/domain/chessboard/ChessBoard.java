23c9560d2d01b1e5ff1758996b925003a7d99ee4
HoeSeong123 <ghltjd3@gmail.com>
2024-03-24 17:07:29 +0900

    refactor(ChessBoard) : ChessBoard의 자료구조 변경

--- src/main/java/chess/domain/chessboard/ChessBoard.java
+++ src/main/java/chess/domain/chessboard/ChessBoard.java
@@ -2,39 +2,25 @@ package chess.domain.chessboard;
 
 import chess.domain.position.Direction;
 import chess.domain.chesspiece.*;
-import chess.domain.position.Column;
 import chess.domain.position.Position;
-import chess.domain.position.Row;
 
 import java.util.*;
 
-import static chess.domain.chesspiece.Team.BLACK;
-import static chess.domain.chesspiece.Team.WHITE;
 import static chess.domain.chesspiece.Role.*;
 
 public class ChessBoard {
-    private final Map<Column, Line> chessBoard;
+    private final Map<Position, Piece> chessBoard;
 
-    private ChessBoard(Map<Column, Line> chessBoard) {
+    private ChessBoard(Map<Position, Piece> chessBoard) {
         this.chessBoard = chessBoard;
     }
 
     public static ChessBoard initializeChessBoard() {
-        Map<Column, Line> board = new LinkedHashMap<>();
-        board.put(Column.valueOf("8"), new Line(List.of(new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK), new Queen(BLACK), new King(BLACK), new Bishop(BLACK), new Knight(BLACK), new Rook(BLACK))));
-        board.put(Column.valueOf("7"), new Line(List.of(new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK))));
-        board.put(Column.valueOf("6"), new Line(List.of(new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty())));
-        board.put(Column.valueOf("5"), new Line(List.of(new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty())));
-        board.put(Column.valueOf("4"), new Line(List.of(new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty())));
-        board.put(Column.valueOf("3"), new Line(List.of(new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty(), new Empty())));
-        board.put(Column.valueOf("2"), new Line(List.of(new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE))));
-        board.put(Column.valueOf("1"), new Line(List.of(new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE), new Queen(WHITE), new King(WHITE), new Bishop(WHITE), new Knight(WHITE), new Rook(WHITE))));
-
-        return new ChessBoard(board);
+        return new ChessBoard(ChessBoardGenerator.initializeBoard());
     }
 
     public void move(Position source, Position target) {
-        Piece piece = findChessPiece(source);
+        Piece piece = chessBoard.get(source);
         piece.getRoute(source, target)
                 .forEach(this::checkObstacle);
 
@@ -42,37 +28,26 @@ public class ChessBoard {
             checkObstacle(target);
         }
         checkTeam(target, piece);
-        chessBoard.put(source.getColumn(), getUpdate(source, new Empty()));
-        chessBoard.put(target.getColumn(), getUpdate(target, piece));
+
+        chessBoard.put(source, new Empty());
+        chessBoard.put(target, piece);
     }
 
     private void checkObstacle(Position position) {
-        Piece obstacle = findChessPiece(position);
+        Piece obstacle = chessBoard.get(position);
         if (obstacle.getRole() != EMPTY) {
             throw new IllegalArgumentException("이동할 수 없습니다.");
         }
     }
 
     private void checkTeam(Position target, Piece piece) {
-        Piece targetPiece = findChessPiece(target);
+        Piece targetPiece = chessBoard.get(target);
         if (piece.isTeam(targetPiece)) {
-            throw new IllegalArgumentException("이동할 수 없습니다.");
+            throw new IllegalArgumentException("같은 팀이 있는 곳으로는 이동할 수 없습니다.");
         }
     }
 
-    private Line getUpdate(Position source, Piece piece) {
-        return chessBoard.get(source.getColumn()).update(source.getRow(), piece);
-    }
-
-
-    private Piece findChessPiece(Position source) {
-        Column column = source.getColumn();
-        Row row = source.getRow();
-        Line chessPieces = chessBoard.get(column);
-        return chessPieces.getChessPiece(row);
-    }
-
-    public Map<Column, Line> getChessBoard() {
+    public Map<Position, Piece> getChessBoard() {
         return Collections.unmodifiableMap(chessBoard);
     }
 }
---
+++ src/main/java/chess/domain/chessboard/ChessBoardGenerator.java
@@ -0,0 +1,60 @@
+package chess.domain.chessboard;
+
+import static chess.domain.chesspiece.Team.BLACK;
+import static chess.domain.chesspiece.Team.WHITE;
+
+import chess.domain.chesspiece.Bishop;
+import chess.domain.chesspiece.Empty;
+import chess.domain.chesspiece.King;
+import chess.domain.chesspiece.Knight;
+import chess.domain.chesspiece.Pawn;
+import chess.domain.chesspiece.Piece;
+import chess.domain.chesspiece.Queen;
+import chess.domain.chesspiece.Rook;
+import chess.domain.chesspiece.Team;
+import chess.domain.position.Position;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ChessBoardGenerator {
+    private static final Map<Position, Piece> board = new LinkedHashMap<>();
+    public static Map<Position, Piece> initializeBoard() {
+        initializeBackRank("8", BLACK);
+        initializePawnRank("7", BLACK);
+        initializeEmptyRanks();
+        initializePawnRank("2", WHITE);
+        initializeBackRank("1", WHITE);
+        return board;
+    }
+
+    private static void initializeBackRank(String column, Team team) {
+        board.put(createPosition("a", column), new Rook(team));
+        board.put(createPosition("b", column), new Knight(team));
+        board.put(createPosition("c", column), new Bishop(team));
+        board.put(createPosition("d", column), new Queen(team));
+        board.put(createPosition("e", column), new King(team));
+        board.put(createPosition("f", column), new Bishop(team));
+        board.put(createPosition("g", column), new Knight(team));
+        board.put(createPosition("h", column), new Rook(team));
+    }
+
+    private static void initializePawnRank(String rank, Team team) {
+        initializeFiles(rank, new Pawn(team));
+    }
+
+    private static void initializeEmptyRanks() {
+        for(int rank = 6; rank >= 3; rank--) {
+            initializeFiles(String.valueOf(rank), new Empty());
+        }
+    }
+
+    private static void initializeFiles(String rank, Piece piece) {
+        for(char file = 'a'; file <= 'h'; file++) {
+            board.put(createPosition(String.valueOf(file), String.valueOf(rank)), piece);
+        }
+    }
+
+    private static Position createPosition(String file, String column) {
+        return Position.from(file + column);
+    }
+}
