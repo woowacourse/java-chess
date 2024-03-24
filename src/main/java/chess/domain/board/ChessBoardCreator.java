c734155924b5b2e8bf2cf547897dd92f1f4bd87b
libienz <leekh10195@gmail.com>
2024-03-25 00:01:35 +0900

    refactor: 체스 보드 생성 과정에서 리플렉션 제거 및 직관적 방식으로 생성하도록 개선

--- src/main/java/chess/domain/board/ChessBoardCreator.java
+++ src/main/java/chess/domain/board/ChessBoardCreator.java
@@ -11,64 +11,56 @@ import chess.domain.piece.Team;
 import chess.domain.position.File;
 import chess.domain.position.Position;
 import chess.domain.position.Rank;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ChessBoardCreator {
-    private static final Map<Class<? extends Piece>, List<Position>> INITIAL_PIECE_ARRANGEMENT = new HashMap<>() {
-        {
-            List<Position> rookPositions = List.of(new Position(File.A, Rank.EIGHT), new Position(File.H, Rank.EIGHT));
-            put(Rook.class, rookPositions);
-
-            List<Position> knightPositions = List.of(new Position(File.B, Rank.EIGHT),
-                    new Position(File.G, Rank.EIGHT));
-            put(Knight.class, knightPositions);
-
-            List<Position> bishopPositions = List.of(new Position(File.C, Rank.EIGHT),
-                    new Position(File.F, Rank.EIGHT));
-            put(Bishop.class, bishopPositions);
-
-            List<Position> queenPositions = List.of(new Position(File.D, Rank.EIGHT));
-            put(Queen.class, queenPositions);
-
-            List<Position> kingPositions = List.of(new Position(File.E, Rank.EIGHT));
-            put(King.class, kingPositions);
-
-            List<Position> pawnPositions = Arrays.stream(File.values())
-                    .map(file -> new Position(file, Rank.SEVEN))
-                    .toList();
-            put(Pawn.class, pawnPositions);
-        }
-    };
-
     public ChessBoard create() {
-        Map<Position, Piece> positionPiece = new HashMap<>();
-        INITIAL_PIECE_ARRANGEMENT.entrySet().stream()
-                .map(entry -> mapPositionToPiece(entry.getKey(), entry.getValue()))
-                .forEach(positionPiece::putAll);
-        return new ChessBoard(positionPiece);
+        Map<Position, Piece> board = new HashMap<>();
+        placePawns(board);
+        placeRooks(board);
+        placeKnights(board);
+        placeBishops(board);
+        placeQueens(board);
+        placeKings(board);
+        return new ChessBoard(board);
     }
 
-
-    //TODO: 의미 있는 메서드명 생각해보기
-    private Map<Position, Piece> mapPositionToPiece(Class<? extends Piece> pieceType, List<Position> positions) {
-        Map<Position, Piece> positionPiece = new HashMap<>();
-        positions.forEach(position -> {
-            positionPiece.put(position, createPieceInstance(pieceType, Team.BLACK));
-            positionPiece.put(position.calculateVerticalReversedPosition(), createPieceInstance(pieceType, Team.WHITE));
-        });
-        return positionPiece;
-    }
-
-    private Piece createPieceInstance(Class<? extends Piece> clazz, Team team) {
-        try {
-            return clazz.getDeclaredConstructor(Team.class)
-                    .newInstance(team);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("[INTERNAL ERROR] 기물을 생성하여 배치하는 리플렉션에서 오류가 발생했습니다 ");
+    private void placePawns(Map<Position, Piece> board) {
+        for (File file : File.values()) {
+            board.put(new Position(file, Rank.TWO), new Pawn(Team.WHITE));
+            board.put(new Position(file, Rank.SEVEN), new Pawn(Team.BLACK));
         }
+    }
+
+    private void placeRooks(Map<Position, Piece> board) {
+        board.put(new Position(File.A, Rank.ONE), new Rook(Team.WHITE));
+        board.put(new Position(File.H, Rank.ONE), new Rook(Team.WHITE));
+        board.put(new Position(File.A, Rank.EIGHT), new Rook(Team.BLACK));
+        board.put(new Position(File.H, Rank.EIGHT), new Rook(Team.BLACK));
+    }
+
+    private void placeKnights(Map<Position, Piece> board) {
+        board.put(new Position(File.B, Rank.ONE), new Knight(Team.WHITE));
+        board.put(new Position(File.G, Rank.ONE), new Knight(Team.WHITE));
+        board.put(new Position(File.B, Rank.EIGHT), new Knight(Team.BLACK));
+        board.put(new Position(File.G, Rank.EIGHT), new Knight(Team.BLACK));
+    }
+
+    private void placeBishops(Map<Position, Piece> board) {
+        board.put(new Position(File.C, Rank.ONE), new Bishop(Team.WHITE));
+        board.put(new Position(File.F, Rank.ONE), new Bishop(Team.WHITE));
+        board.put(new Position(File.C, Rank.EIGHT), new Bishop(Team.BLACK));
+        board.put(new Position(File.F, Rank.EIGHT), new Bishop(Team.BLACK));
+    }
+
+    private void placeQueens(Map<Position, Piece> board) {
+        board.put(new Position(File.D, Rank.ONE), new Queen(Team.WHITE));
+        board.put(new Position(File.D, Rank.EIGHT), new Queen(Team.BLACK));
+    }
+
+    private void placeKings(Map<Position, Piece> board) {
+        board.put(new Position(File.E, Rank.ONE), new King(Team.WHITE));
+        board.put(new Position(File.E, Rank.EIGHT), new King(Team.BLACK));
     }
 }
