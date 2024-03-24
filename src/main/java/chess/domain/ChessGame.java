09d479265cc4b24a3a46a94712ff89f66928e234
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-03-25 00:28:56 +0900

    refactor(ChessGame): 체크 상태 확인 후 반환하도록 메서드 수정

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -18,15 +18,15 @@ public class ChessGame {
     public Map<Position, Character> movePiece(Positions positions, Consumer<CheckState> printCheck) {
         board.validateSameTeamByPosition(positions.source(), currentTeam);
         board.move(positions);
-        validateCheck(printCheck);
+        printCheck.accept(validateCheck());
         currentTeam = currentTeam.opponent();
         return board.mapPositionToCharacter();
     }
 
-    private void validateCheck(Consumer<CheckState> printCheck) {
-        printCheck.accept(board.findCheckState(currentTeam.opponent()));
+    private CheckState validateCheck() {
         if (board.findCheckState(currentTeam) != CheckState.SAFE) {
             throw new IllegalArgumentException("체크 상태를 벗어나지 않았습니다.");
         }
+        return board.findCheckState(currentTeam.opponent());
     }
 }
