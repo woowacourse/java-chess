fe042c856c5a42114eb53c7fb909717ae86933c5
Nam Gi Beom <rlqja0523@naver.com>
2024-03-24 23:22:28 +0900

    feat: 위치 정보가 없을 때 예외가 발생하는 기능 추가

--- src/main/java/domain/game/MovePosition.java
+++ src/main/java/domain/game/MovePosition.java
@@ -6,6 +6,7 @@ public class MovePosition {
     private static final int COMMAND_NAME_INDEX = 0;
     private static final int SOURCE_INDEX = 0;
     private static final int TARGET_INDEX = 1;
+    private static final int VALID_POSITION_COUNT = 2;
 
     private final String commandName;
     private final List<String> positions;
@@ -20,11 +21,19 @@ public class MovePosition {
     }
 
     public String source() {
+        validatePositionCount();
         return positions.get(SOURCE_INDEX);
     }
 
     public String target() {
+        validatePositionCount();
         return positions.get(TARGET_INDEX);
+    }
+
+    private void validatePositionCount() {
+        if (positions.size() != VALID_POSITION_COUNT) {
+            throw new IllegalStateException("위치 정보가 없습니다.");
+        }
     }
 
     public int count() {
