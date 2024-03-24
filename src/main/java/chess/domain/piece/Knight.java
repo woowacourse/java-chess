888bbac7f645949535a5fc1b302b4b7262be711b
Gyeongho Yang <chchch1213@naver.com>
2024-03-24 18:22:22 +0900

    feat: 나이트 이동 기능 구현

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -7,6 +7,8 @@ public class Knight extends Piece {
 
     private static final int SIDE_STEP = 1;
     private static final int STRAIGHT_STEP = 2;
+    private static final String ERROR_CANNOT_REACH = "나이트의 이동 방법으로 갈 수 없는 곳입니다.";
+    private static final String ERROR_FRIENDLY_ON_TARGET = "나이트의 목적지에 같은 색 기물이 존재합니다.";
 
     public Knight(PieceColor color, Square square) {
         super(color, square);
@@ -14,7 +16,29 @@ public class Knight extends Piece {
 
     @Override
     public void move(Board board, Square target) {
+        validateStraightSide(target);
+        validateFriendly(board, target);
+        square = target;
+    }
 
+    private void validateStraightSide(Square target) {
+        if (!isStraightRankSideFile(target) && !isStraightFileSideRank(target)) {
+            throw new IllegalArgumentException(ERROR_CANNOT_REACH);
+        }
+    }
+
+    private void validateFriendly(Board board, Square target) {
+        if (board.existOnSquareWithColor(target, getColor())) {
+            throw new IllegalArgumentException(ERROR_FRIENDLY_ON_TARGET);
+        }
+    }
+
+    private boolean isStraightRankSideFile(Square target) {
+        return square.distanceRankFrom(target) == STRAIGHT_STEP && square.distanceFileFrom(target) == SIDE_STEP;
+    }
+
+    private boolean isStraightFileSideRank(Square target) {
+        return square.distanceFileFrom(target) == STRAIGHT_STEP && square.distanceRankFrom(target) == SIDE_STEP;
     }
 
     @Override
--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -3,19 +3,21 @@ package chess.domain.piece;
 import chess.domain.board.Board;
 import chess.domain.square.Square;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("나이트")
 class KnightTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"a5", "a7", "e5", "e7", "b4", "d4", "b8", "d8"})
-    @DisplayName("앞으로 두칸 후 옆으로 한칸 이동한다.")
+    @DisplayName("앞으로 두칸 후 옆으로 한칸 이동한 뒤 위치 상태를 목적지로 변경한다.")
     void move(String targetInput) {
         // given
         Square source = Square.from("c6");
@@ -28,5 +30,53 @@ class KnightTest {
 
         // then
         assertThat(knight.getSquare()).isEqualTo(target);
+    }
+
+    @Test
+    @DisplayName("앞으로 두칸 후 옆 한칸이 아닌 곳으로 이동할 경우 예외가 발생한다.")
+    void validateDirection() {
+        // given
+        Square source = Square.from("c6");
+        Square target = Square.from("b5");
+        Knight knight = new Knight(PieceColor.BLACK, source);
+        Board board = new Board(Set.of(knight));
+
+        // when & then
+        assertThatCode(() -> knight.move(board, target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("나이트의 이동 방법으로 갈 수 없는 곳입니다.");
+    }
+
+    @Test
+    @DisplayName("목적지에 적 기물이 존재하면 공격이 가능하다.")
+    void attackOnTargetSquare() {
+        // given
+        Square source = Square.from("c6");
+        Square target = Square.from("a5");
+        Knight knight = new Knight(PieceColor.BLACK, source);
+        Bishop enemy = new Bishop(PieceColor.WHITE, Square.from("a5"));
+        Board board = new Board(Set.of(knight, enemy));
+
+        // when
+        knight.move(board, target);
+
+        // then
+        assertThat(knight.getSquare()).isEqualTo(target);
+    }
+
+    @Test
+    @DisplayName("목적지에 아군 기물이 존재할 경우 예외가 발생한다.")
+    void validateFriendly() {
+        // given
+        Square source = Square.from("c6");
+        Square target = Square.from("a5");
+        Knight knight = new Knight(PieceColor.BLACK, source);
+        Bishop friendly = new Bishop(PieceColor.BLACK, Square.from("a5"));
+        Board board = new Board(Set.of(knight, friendly));
+
+        // when & then
+        assertThatCode(() -> knight.move(board, target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("나이트의 목적지에 같은 색 기물이 존재합니다.");
     }
 }
