5ad6cd9a495af698a0da429ccce3678c69f87f9b
3juhwan <13selfesteem91@naver.com>
2024-03-24 10:43:10 +0900

    style: 사용하지 않는 상수 제거

--- src/main/java/domain/piece/nonpawn/King.java
+++ src/main/java/domain/piece/nonpawn/King.java
@@ -5,8 +5,6 @@ import domain.piece.Type;
 import domain.position.Position;
 
 public class King extends NonPawnPiece {
-    private static final int DISTANCE_LIMIT_COUNT = 1;
-
     public King(final Color color) {
         super(color);
     }
@@ -14,7 +12,7 @@ public class King extends NonPawnPiece {
     @Override
     protected void validateDirection(final Position source, final Position target) {
         if (!source.isAdjacentAt(target)) {
-            throw new IllegalArgumentException(String.format("킹은 한 번에 %d칸만 이동할 수 있습니다.", DISTANCE_LIMIT_COUNT));
+            throw new IllegalArgumentException("킹은 한 번에 1칸만 이동할 수 있습니다.");
         }
     }
 
