23b7324b42a5d1b1d5becf5e01a6cb44b8f63746
ikjo39 <wer04129@gmail.com>
2024-03-24 14:36:30 +0900

    refactor: 반복되던 출력 로직 하나로 통일

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -9,10 +9,12 @@ public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
     public String readGameCommand() {
-        System.out.println("> 체스 게임을 시작합니다.");
-        System.out.println("> 게임 시작 : start");
-        System.out.println("> 게임 종료 : end");
-        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+        String introductionText = """
+                > 체스 게임을 시작합니다.
+                > 게임 시작 : start
+                > 게임 종료 : end
+                > 게임 이동 : move source위치 target위치 - 예. move b2 b3""";
+        System.out.println(introductionText);
         String input = scanner.nextLine();
         validateInput(input);
         return input;
