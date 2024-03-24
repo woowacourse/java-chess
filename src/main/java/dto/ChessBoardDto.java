972eb672b5995b338e072e5fb3789b7e7f8c8cff
reddevilmidzy <midzy0228@gmail.com>
2024-03-24 22:02:46 +0900

    refactor: dto 변환 스트림 대신 메서드 분리

--- src/main/java/dto/ChessBoardDto.java
+++ src/main/java/dto/ChessBoardDto.java
@@ -1,8 +1,6 @@
 package dto;
 
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import model.ChessBoard;
 import model.piece.Piece;
 import model.position.File;
@@ -20,28 +18,36 @@ public class ChessBoardDto {
     private final String board;
     private final String currentTurn;
 
-    public ChessBoardDto(final String board, final String currentTurn) {
+    private ChessBoardDto(final String board, final String currentTurn) {
         this.board = board;
         this.currentTurn = currentTurn;
     }
 
-    public static ChessBoardDto from(final ChessBoard chessBoard) { // TODO 리팩터링
+    public static ChessBoardDto from(final ChessBoard chessBoard) {
         final Map<Position, Piece> pieceOfPosition = chessBoard.getBoard();
-        final String result = IntStream.range(0, BOARD_SIZE)
-                .mapToObj(Rank::from)
-                .map(rank -> IntStream.range(0, BOARD_SIZE)
-                        .mapToObj(File::from)
-                        .map(file -> convertToString(pieceOfPosition, file, rank))
-                        .collect(Collectors.joining()))
-                .collect(Collectors.joining(System.lineSeparator()))
-                .concat(String.format("%n%n%s%n", FILE_GUIDE_LINE));
 
-        return new ChessBoardDto(result, chessBoard.getCamp().toString());
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            stringBuilder.append(generateBoardLine(pieceOfPosition, Rank.from(i)));
+            stringBuilder.append(System.lineSeparator());
+        }
+        stringBuilder.append(String.format("%n%s%n", FILE_GUIDE_LINE));
+
+        return new ChessBoardDto(stringBuilder.toString(), chessBoard.getCamp().toString());
     }
 
-    private static String convertToString(final Map<Position, Piece> board, final File file, final Rank rank) {
-        final Position position = new Position(file, rank);
-        final Piece piece = board.get(position);
+    private static String generateBoardLine(final Map<Position, Piece> pieceOfPosition, final Rank rank) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            final File file = File.from(j);
+            final Position position = new Position(file, rank);
+            final Piece piece = pieceOfPosition.get(position);
+            stringBuilder.append(convertToString(piece, file, rank));
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String convertToString(final Piece piece, final File file, final Rank rank) {
         if (piece != null) {
             return PieceType.from(piece).getValue() + paddedRankGuidLine(file, rank);
         }
