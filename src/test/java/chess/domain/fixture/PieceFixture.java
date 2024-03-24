5617973b63e15237a8b009bcd930c03c6f442ee7
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-25 07:08:24 +0900

    test: 테스트용 fixture클래스에 폰 추가

--- src/test/java/chess/domain/fixture/PieceFixture.java
+++ src/test/java/chess/domain/fixture/PieceFixture.java
@@ -4,9 +4,12 @@ import static chess.domain.piece.Team.BLACK;
 import static chess.domain.piece.Team.WHITE;
 
 import chess.domain.piece.Bishop;
+import chess.domain.piece.InitialBlackPawn;
+import chess.domain.piece.InitialWhitePawn;
 import chess.domain.piece.King;
 import chess.domain.piece.Knight;
-import chess.domain.piece.Pawn;
+import chess.domain.piece.NormalBlackPawn;
+import chess.domain.piece.NormalWhitePawn;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Queen;
 import chess.domain.piece.Rook;
@@ -17,13 +20,15 @@ public class PieceFixture {
     public static final Piece WHITE_BISHOP = new Bishop(WHITE);
     public static final Piece WHITE_ROOK = new Rook(WHITE);
     public static final Piece WHITE_KNIGHT = new Knight(WHITE);
-    public static final Piece WHITE_PAWN = new Pawn(WHITE);
+    public static final Piece NORMAL_WHITE_PAWN = new NormalWhitePawn();
+    public static final Piece INITIAL_WHITE_PAWN = new InitialWhitePawn();
 
     public static final Piece BLACK_KING = new King(BLACK);
     public static final Piece BLACK_QUEEN = new Queen(BLACK);
     public static final Piece BLACK_BISHOP = new Bishop(BLACK);
     public static final Piece BLACK_ROOK = new Rook(BLACK);
     public static final Piece BLACK_KNIGHT = new Knight(BLACK);
-    public static final Piece BLACK_PAWN = new Pawn(BLACK);
+    public static final Piece NORMAL_BLACK_PAWN = new NormalBlackPawn();
+    public static final Piece INITIAL_BLACK_PAWN = new InitialBlackPawn();
 
 }
