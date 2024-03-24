015f27a6932af511e1b9c14c20917a84b4ded448
coli-geonwoo <kkwoo001021@naver.com>
2024-03-25 01:47:27 +0900

    refactor: 중복코드 메서드 분리

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,11 +1,19 @@
 package chess.domain.piece;
 
 import chess.domain.board.ChessBoard;
+import chess.domain.position.Direction;
 import chess.domain.position.DirectionJudge;
 import chess.domain.position.Position;
 
+import java.util.List;
+
+import static chess.domain.position.Direction.*;
+
 public class Pawn extends Piece {
     public static final int KILL_PASSING_DISTANCE = 2;
+    public static final int FORWARDING_PASSING_DISTANCE = 1;
+    public static final int FIRST_FORWADING_PASSING_DISTANCE = 4;
+    private static List<Direction> KILL_PASSING = List.of(NW, NE, SW, SE);
 
     public Pawn(Team team) {
         super(team);
@@ -14,21 +22,30 @@ public class Pawn extends Piece {
     @Override
     public boolean canMove(Position start, Position destination, ChessBoard board) {
         //TODO: 조건식 가독성 있게 추상화 및 매직넘버 상수화
-        if (isForward(start, destination) && start.squaredDistanceWith(destination) == 1 && board.positionIsEmpty(destination)) {
-            return true;
-        }
-        if (isForward(start, destination) && start.squaredDistanceWith(destination) == 4 && start.isStraightWith(
-                destination) && start.rowIs(teamInitialPawnRow()) && board.positionIsEmpty(destination)) {
-            return true;
-        }
-
-        return isKillPassing(start, destination) && !board.positionIsEmpty(destination) && this.isOtherTeam(board.findPieceByPosition(destination));
+        return isFowardPassing(start, destination, board)
+                || isFirstFowardPassing(start, destination, board)
+                || isKillPassing(start, destination, board);
     }
 
     //TODO: 테스트 구현하기
-    private boolean isKillPassing(Position start, Position destination) {
-        return isForward(start, destination) && start.isDiagonalWith(destination)
-                && start.squaredDistanceWith(destination) == KILL_PASSING_DISTANCE;
+    private boolean isKillPassing(Position start, Position destination, ChessBoard board) {
+        return isForward(start, destination)
+                && KILL_PASSING.contains(DirectionJudge.judge(start, destination))
+                && start.squaredDistanceWith(destination) == KILL_PASSING_DISTANCE
+                && board.piecesIsOtherTeam(start, destination);
+    }
+
+    private boolean isFowardPassing(Position start, Position destination, ChessBoard board) {
+        return isForward(start, destination)
+                && start.squaredDistanceWith(destination) == FORWARDING_PASSING_DISTANCE
+                && board.positionIsEmpty(destination);
+    }
+
+    private boolean isFirstFowardPassing(Position start, Position destination, ChessBoard board) {
+        return isForward(start, destination)
+                && start.squaredDistanceWith(destination) == FIRST_FORWADING_PASSING_DISTANCE
+                && start.rowIs(teamInitialPawnRow())
+                && board.pathIsAllEmpty(start.findPath(destination));
     }
 
     private boolean isForward(Position start, Position destination) {
