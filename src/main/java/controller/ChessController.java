deb4e4bc1736e4613f9090d3ed7c83748f41e34b
hangillee <skfcb10@naver.com>
2024-03-25 03:24:16 +0900

    refactor: 상태 패턴과 커맨드 패턴 도입을 통한 컨트롤러 간소화

--- src/main/java/controller/ChessController.java
+++ src/main/java/controller/ChessController.java
@@ -1,101 +1,47 @@
 package controller;
 
-import static constants.Bound.BOARD_LOWER_BOUND;
-import static constants.Bound.BOARD_UPPER_BOUND;
-import static view.Command.END;
-import static view.Command.MOVE;
-import static view.Command.START;
-
+import controller.command.Command;
+import controller.command.Commands;
 import domain.board.Board;
 import domain.board.BoardInitiator;
-import domain.board.Position;
-import java.util.ArrayList;
-import java.util.List;
-import view.Command;
+import domain.game.Game;
+import view.CommandShape;
 import view.InputView;
 import view.OutputView;
-import view.dto.DtoMapper;
-import view.dto.RankInfo;
 
 public class ChessController {
     public void start() {
-        OutputView.printGameStartMessage();
         final Board board = new Board(BoardInitiator.init());
+        Game game = new Game(board);
 
-        String command = InputView.inputCommand();
-        if (END.equals(Command.of(command))) {
-            return;
+        while (game.isNotEnded()) {
+            game = new Game(board);
+            Commands commands = Commands.of(game);
+            readyForStart(game, commands);
+            runningGame(game, commands);
         }
-
-        checkCommandIsStart(command, board);
-        movePiecesByCommandOn(board);
     }
 
-    private void checkCommandIsStart(final String command, final Board board) {
-        readyForStartCommand(command);
-        OutputView.printChessBoard(createRankInfo(board));
+    private void readyForStart(final Game game, final Commands commands) {
+        OutputView.printGameStartMessage();
+        while (game.isInit()) {
+            executeCommand(commands);
+        }
     }
 
-    private void readyForStartCommand(String command) {
+    private void runningGame(final Game game, final Commands commands) {
+        while (game.isStarted()) {
+            executeCommand(commands);
+        }
+    }
+
+    private void executeCommand(final Commands commands) {
         try {
-            inputUntilStartCommand(command);
+            final String value = InputView.inputCommand();
+            Command command = commands.find(CommandShape.of(value));
+            command.execute();
         } catch (Exception e) {
             OutputView.printErrorMessage(e.getMessage());
         }
-    }
-
-    private static void inputUntilStartCommand(String command) {
-        while (!START.equals(Command.of(command))) {
-            OutputView.printErrorMessage("게임을 시작하려면 start를 입력해주세요.");
-            InputView.clearBuffer();
-            command = InputView.inputCommand();
-        }
-    }
-
-    private void movePiecesByCommandOn(final Board board) {
-        String command = InputView.inputCommand();
-        while (MOVE.equals(Command.of(command))) {
-            command = inputCommandUntilNoException(board);
-        }
-    }
-
-    private String inputCommandUntilNoException(final Board board) {
-        String command;
-        try {
-            command = nextCommand(board);
-        } catch (Exception e) {
-            OutputView.printErrorMessage(e.getMessage());
-            command = InputView.inputCommand();
-        }
-        return command;
-    }
-
-    private String nextCommand(final Board board) {
-        moveByCommand(board);
-
-        OutputView.printChessBoard(createRankInfo(board));
-        return InputView.inputCommand();
-    }
-
-    private void moveByCommand(final Board board) {
-        final String source = InputView.inputCommand();
-        final String target = InputView.inputCommand();
-        move(source, target, board);
-    }
-
-    public void move(final String source, final String target, final Board board) {
-        final Position sourcePosition = Position.createPosition(source);
-        final Position targetPosition = Position.createPosition(target);
-
-        board.moveByPosition(sourcePosition, targetPosition);
-    }
-
-    private List<RankInfo> createRankInfo(final Board board) {
-        final List<RankInfo> rankInfos = new ArrayList<>();
-        for (int rank = BOARD_UPPER_BOUND.value(); rank >= BOARD_LOWER_BOUND.value(); rank--) {
-            final RankInfo pieceShapeOfRank = DtoMapper.getPieceShapeOn(board, rank);
-            rankInfos.add(pieceShapeOfRank);
-        }
-        return rankInfos;
     }
 }
