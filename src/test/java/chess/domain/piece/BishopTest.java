8cb5def962593ae6ef01fbfdd079a6c41a16e95a
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-03-24 21:04:35 +0900

    test: Bishop 테스트 추가

--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -11,14 +11,14 @@ import org.junit.jupiter.api.Test;
 
 class BishopTest {
     private static final Bishop BISHOP = new Bishop(Color.BLACK);
-    /*
+
     @DisplayName("한 방향으로 이루어지지 않은 경로로 움직일 수 없다.")
     @Test
     void tooManyDirectionTest() {
-        Route manyDirectionRoute = new Route(List.of(
-                new Step(Direction.DOWN_LEFT, SquareState.EMPTY),
-                new Step(Direction.DOWN_RIGHT, SquareState.EMPTY)
-        ));
+        Route manyDirectionRoute = new Route(
+                List.of(Direction.DOWN_LEFT, Direction.DOWN_RIGHT),
+                List.of(SquareState.EMPTY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(BISHOP.canMove(manyDirectionRoute))
                 .isFalse();
@@ -27,11 +27,10 @@ class BishopTest {
     @DisplayName("경로 중간에 기물이 위치한다면 움직일 수 없다.")
     @Test
     void pathHasPieceTest() {
-        Route notEmptyRoute = new Route(List.of(
-                new Step(Direction.DOWN_LEFT, SquareState.EMPTY),
-                new Step(Direction.DOWN_LEFT, SquareState.ALLY),
-                new Step(Direction.DOWN_LEFT, SquareState.EMPTY)
-        ));
+        Route notEmptyRoute = new Route(
+                List.of(Direction.DOWN_LEFT, Direction.DOWN_LEFT, Direction.DOWN_LEFT),
+                List.of(SquareState.EMPTY, SquareState.ALLY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(BISHOP.canMove(notEmptyRoute))
                 .isFalse();
@@ -40,11 +39,11 @@ class BishopTest {
     @DisplayName("목적지에 아군이 존재한다면 움직일 수 없다.")
     @Test
     void allyLocatedAtTargetTest() {
-        Route manyDirectionRoute = new Route(List.of(
-                new Step(Direction.DOWN_LEFT, SquareState.EMPTY),
-                new Step(Direction.DOWN_LEFT, SquareState.EMPTY),
-                new Step(Direction.DOWN_LEFT, SquareState.ALLY)
-        ));
+        Route manyDirectionRoute = new Route(
+                List.of(Direction.DOWN_LEFT, Direction.DOWN_LEFT, Direction.DOWN_LEFT),
+                List.of(SquareState.EMPTY, SquareState.EMPTY, SquareState.ALLY)
+
+        );
 
         Assertions.assertThat(BISHOP.canMove(manyDirectionRoute))
                 .isFalse();
@@ -53,11 +52,10 @@ class BishopTest {
     @DisplayName("수직 방향으로 움직일 수 없다.")
     @Test
     void canNotMoveDiagonalTest() {
-        Route diagonalDirectionRoute = new Route(List.of(
-                new Step(Direction.UP, SquareState.EMPTY),
-                new Step(Direction.UP, SquareState.EMPTY),
-                new Step(Direction.UP, SquareState.EMPTY)
-        ));
+        Route diagonalDirectionRoute = new Route(
+                List.of(Direction.UP, Direction.UP, Direction.UP),
+                List.of(SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(BISHOP.canMove(diagonalDirectionRoute))
                 .isFalse();
@@ -66,18 +64,12 @@ class BishopTest {
     @DisplayName("최대 7칸까지 움직일 수 있다.")
     @Test
     void maxDistanceMoveTest() {
-        Route manyDirectionRoute = new Route(List.of(
-                new Step(Direction.DOWN_RIGHT, SquareState.EMPTY),
-                new Step(Direction.DOWN_RIGHT, SquareState.EMPTY),
-                new Step(Direction.DOWN_RIGHT, SquareState.EMPTY),
-                new Step(Direction.DOWN_RIGHT, SquareState.EMPTY),
-                new Step(Direction.DOWN_RIGHT, SquareState.EMPTY),
-                new Step(Direction.DOWN_RIGHT, SquareState.EMPTY),
-                new Step(Direction.DOWN_RIGHT, SquareState.ENEMY)
-        ));
+        Route manyDirectionRoute = new Route(
+                List.of(Direction.DOWN_RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT),
+                List.of(SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY)
+        );
 
         Assertions.assertThat(BISHOP.canMove(manyDirectionRoute))
                 .isTrue();
     }
-    */
 }
