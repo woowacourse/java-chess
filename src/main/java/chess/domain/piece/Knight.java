83137f306ef9880111c409055455525ced4a3ea6
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-24 16:42:30 +0900

    feat(Knight): 나이트가 이동할 수 있는 위치를 반환하는 기능 구현

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -3,6 +3,7 @@ package chess.domain.piece;
 import chess.domain.attribute.Color;
 import chess.domain.attribute.Square;
 import chess.domain.chessboard.attribute.Direction;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -20,5 +21,35 @@ public class Knight extends UnslidingPiece {
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.toUnmodifiableSet());
+    }
+
+    public Set<Square> findLegalMoves(Set<Piece> entirePieces) {
+        Set<Square> candidateSquares = candidateSquares();
+        for (Piece other : entirePieces) {
+            removeIfAllyExist(other, candidateSquares);
+        }
+        return candidateSquares;
+    }
+
+    public void removeIfAllyExist(Piece other, Set<Square> candidateSquares) {
+        if (candidateSquares.contains(other.currentSquare())) {
+            if (isAllyOf(other)) {
+                candidateSquares.remove(other.currentSquare());
+            }
+        }
+    }
+
+    public Set<Square> candidateSquares() {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        squares.add(currentSquare.moveUp().moveLeftUp());
+        squares.add(currentSquare.moveUp().moveRightUp());
+        squares.add(currentSquare.moveDown().moveLeftDown());
+        squares.add(currentSquare.moveDown().moveRightDown());
+        squares.add(currentSquare.moveRight().moveRightUp());
+        squares.add(currentSquare.moveRight().moveRightDown());
+        squares.add(currentSquare.moveLeft().moveLeftUp());
+        squares.add(currentSquare.moveLeft().moveLeftDown());
+        return squares;
     }
 }
---
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -0,0 +1,144 @@
+package chess.domain.piece;
+
+import static chess.domain.attribute.Color.*;
+import static chess.domain.attribute.File.*;
+import static chess.domain.attribute.Rank.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.attribute.Square;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KnightTest {
+    /*
+     * * * * * * * * 8    * * * * * * * * 8
+     * * * * * * * * 7    * * * * * * * * 7
+     * * * * * * * * 6    * * * o * o * * 6
+     * * * * * * * * 5    * * o * * * o * 5
+     * * * * n * * * 4 -> * * * * n * * * 4
+     * * * * * * * * 3    * * o * * * o * 3
+     * * * * * * * * 2    * * * o * o * * 2
+     * * * * * * * * 1    * * * * * * * * 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_기물이 존재하지 않는 경우")
+    @Test
+    void movableSquareAllDirection() {
+        Knight knight = new Knight(WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(knight);
+        Set<Square> squares = knight.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(D, SIX),
+                        Square.of(F, SIX),
+                        Square.of(C, FIVE),
+                        Square.of(G, FIVE),
+                        Square.of(C, THREE),
+                        Square.of(G, THREE),
+                        Square.of(D, TWO),
+                        Square.of(F, TWO));
+    }
+
+    /*
+     * * * * * * * * 8    * * * * * * * * 8
+     * * * * * * * * 7    * * * * * * * * 7
+     * * * * * p * * 6    * * * o * x * * 6
+     * * * * * * * * 5    * * o * * * o * 5
+     * * * * n * * * 4 -> * * * * n * * * 4
+     * * * * * * p * 3    * * o * * * x * 3
+     * * * * * * * * 2    * * * o * o * * 2
+     * * * * * * * * 1    * * * * * * * * 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_같은 편의 기물이 존재하는 경우")
+    @Test
+    void movableSquare() {
+        Knight knight = new Knight(WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(
+                knight,
+                new Pawn(WHITE, Square.of(F, SIX)),
+                new Pawn(WHITE, Square.of(G, THREE))
+        );
+        Set<Square> squares = knight.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(D, SIX),
+                        Square.of(C, FIVE),
+                        Square.of(G, FIVE),
+                        Square.of(C, THREE),
+                        Square.of(D, TWO),
+                        Square.of(F, TWO));
+    }
+
+    /*
+     * * * * * * * * 8    * * * * * * * * 8
+     * * * * * * * * 7    * * * * * * * * 7
+     * * * p * P * * 6    * * * x * o * * 6
+     * * * * * * * * 5    * * o * * * o * 5
+     * * * * n * * * 4 -> * * * * n * * * 4
+     * * * * * * p * 3    * * o * * * x * 3
+     * * * * * * * * 2    * * * o * o * * 2
+     * * * * * * * * 1    * * * * * * * * 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_다른 편의 기물이 존재하는 경우")
+    @Test
+    void movableSquareEnemyExist() {
+        Knight knight = new Knight(WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(
+                knight,
+                new Pawn(WHITE, Square.of(D, SIX)),
+                new Pawn(WHITE, Square.of(G, THREE)),
+                new Pawn(BLACK, Square.of(F, SIX))
+        );
+        Set<Square> squares = knight.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(F, SIX),
+                        Square.of(C, FIVE),
+                        Square.of(G, FIVE),
+                        Square.of(C, THREE),
+                        Square.of(D, TWO),
+                        Square.of(F, TWO));
+    }
+
+    /*
+     * * * * * * * * 8    * * * * * * * * 8
+     * * * * * * * * 7    * * * * * * * * 7
+     * * * * * * * * 6    * * * o * o * * 6
+     * * * p p p * * 5    * * o * * * o * 5
+     * * * p n p * * 4 -> * * * * n * * * 4
+     * * * p p p * * 3    * * o * * * o * 3
+     * * * * * * * * 2    * * * o * o * * 2
+     * * * * * * * * 1    * * * * * * * * 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동하는 중에 기물이 존재해도 건너뛸 수 있다.")
+    @Test
+    void movableSquareCanPassOver() {
+        Knight knight = new Knight(WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(
+                knight,
+                new Pawn(WHITE, Square.of(D, FIVE)),
+                new Pawn(WHITE, Square.of(E, FIVE)),
+                new Pawn(WHITE, Square.of(F, FIVE)),
+                new Pawn(WHITE, Square.of(D, FOUR)),
+                new Pawn(WHITE, Square.of(F, FOUR)),
+                new Pawn(WHITE, Square.of(D, THREE)),
+                new Pawn(WHITE, Square.of(E, THREE)),
+                new Pawn(WHITE, Square.of(F, THREE))
+        );
+        Set<Square> squares = knight.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(D, SIX),
+                        Square.of(F, SIX),
+                        Square.of(C, FIVE),
+                        Square.of(G, FIVE),
+                        Square.of(C, THREE),
+                        Square.of(G, THREE),
+                        Square.of(D, TWO),
+                        Square.of(F, TWO));
+    }
+}
