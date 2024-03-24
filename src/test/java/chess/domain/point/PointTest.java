ec09bdcad4a274d63bd42f5345abaeb8ecb0b871
seunghye218 <seunghye@student.42seoul.kr>
2024-03-24 23:21:55 +0900

    chore: 미사용 import 문 제거

--- src/test/java/chess/domain/point/PointTest.java
+++ src/test/java/chess/domain/point/PointTest.java
@@ -2,9 +2,6 @@ package chess.domain.point;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import chess.domain.point.File;
-import chess.domain.point.Point;
-import chess.domain.point.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/chess/domain/point/RankTest.java
+++ src/test/java/chess/domain/point/RankTest.java
@@ -3,7 +3,6 @@ package chess.domain.point;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import chess.domain.point.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
