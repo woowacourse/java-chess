7bbafdb1882b1d2f3ef636f356c07afd389942fc
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-25 13:50:40 +0900

    feat(Queen): 퀸이 이동할 수 있는 위치를 반환하는 기능 구현

--- src/main/java/chess/domain/piece/Queen.java
+++ src/main/java/chess/domain/piece/Queen.java
@@ -3,7 +3,11 @@ package chess.domain.piece;
 import chess.domain.attribute.Color;
 import chess.domain.attribute.Square;
 import chess.domain.chessboard.attribute.Direction;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Queen extends SlidingPiece {
     public Queen(final Color color, final Square square) {
@@ -13,5 +17,174 @@ public class Queen extends SlidingPiece {
     @Override
     public Set<Square> movableSquaresFrom(final Square source) {
         return movableSquaresOf(Direction.all(), source);
+    }
+
+    public Set<Square> findLegalMoves(Set<Piece> existPieces) {
+        return Stream.of(candidateUpSquares(existPieces),
+                        candidateDownSquares(existPieces),
+                        candidateLeftSquares(existPieces),
+                        candidateRightSquares(existPieces),
+                        candidateLeftUpSquares(existPieces),
+                        candidateLeftDownSquares(existPieces),
+                        candidateRightDownSquares(existPieces),
+                        candidateRightUpSquares(existPieces))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+    }
+
+    private Set<Square> candidateUpSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveUp()) {
+            currentSquare = currentSquare.moveUp();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateDownSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveDown()) {
+            currentSquare = currentSquare.moveDown();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateRightSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveRight()) {
+            currentSquare = currentSquare.moveRight();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateLeftSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveLeft()) {
+            currentSquare = currentSquare.moveLeft();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateLeftUpSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveLeftUp()) {
+            currentSquare = currentSquare.moveLeftUp();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateLeftDownSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveLeftDown()) {
+            currentSquare = currentSquare.moveLeftDown();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateRightUpSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveRightUp()) {
+            currentSquare = currentSquare.moveRightUp();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateRightDownSquares(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveRightDown()) {
+            currentSquare = currentSquare.moveRightDown();
+            if (isOccupied(existPieces, currentSquare)) {
+                Piece piece = getPiece(existPieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Piece getPiece(Set<Piece> entirePieces, Square existPieces) { //todo 예외를 던지는 것이 아닌, Blank를 반환하기
+        return entirePieces.stream()
+                .filter(piece -> piece.currentSquare() == existPieces)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 위치에 기물이 없습니다."));
+    }
+
+    private boolean isOccupied(Set<Piece> entirePieces, Square currentSquare) {
+        return entirePieces.stream()
+                .anyMatch(piece -> piece.currentSquare() == currentSquare);
     }
 }
---
+++ src/test/java/chess/domain/piece/QueenTest.java
@@ -0,0 +1,126 @@
+package chess.domain.piece;
+
+import static chess.domain.attribute.File.*;
+import static chess.domain.attribute.Rank.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.attribute.Color;
+import chess.domain.attribute.Square;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class QueenTest {
+
+    /*
+     * * * * * * * * 8    o * * * o * * * 8
+     * * * * * * * * 7    * o * * o * * o 7
+     * * * * * * * * 6    * * o * o * o * 6
+     * * * * * * * * 5    * * * o o o * * 5
+     * * * * q * * * 4 -> o o o o q o o o 4
+     * * * * * * * * 3    * * * o o o * * 3
+     * * * * * * * * 2    * * o * o * o * 2
+     * * * * * * * * 1    * o * * o * * o 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_기물이 존재하지 않는 경우")
+    @Test
+    void movableSquareAllDirection() {
+        Queen queen = new Queen(Color.WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(queen);
+        Set<Square> squares = queen.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(E, ONE), Square.of(E, TWO),
+                        Square.of(E, THREE), Square.of(E, FIVE),
+                        Square.of(E, SIX), Square.of(E, SEVEN),
+                        Square.of(E, EIGHT), Square.of(A, FOUR),
+                        Square.of(B, FOUR), Square.of(C, FOUR),
+                        Square.of(D, FOUR), Square.of(F, FOUR),
+                        Square.of(G, FOUR), Square.of(H, FOUR),
+
+                        Square.of(A, EIGHT), Square.of(B, ONE),
+                        Square.of(B, SEVEN), Square.of(C, TWO),
+                        Square.of(C, SIX), Square.of(D, THREE),
+                        Square.of(D, FIVE), Square.of(F, FIVE),
+                        Square.of(F, THREE), Square.of(G, SIX),
+                        Square.of(G, TWO), Square.of(H, SEVEN),
+                        Square.of(H, ONE));
+    }
+
+    /*
+     * * * * * * * * 8    o * * * x * * * 8
+     * * * * * * * * 7    * o * * x * * x 7
+     * * * * p * p * 6    * * o * x * x * 6
+     * * * * * * * * 5    * * * o o o * * 5
+     p * * * q * * * 4 -> x o o o q o o o 4
+     * * * p * * * * 3    * * * x o o * * 3
+     * * * * * * p * 2    * * x * o * x * 2
+     * * * * * * * * 1    * x * * o * * x 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_같은 편 기물이 존재하는 경우")
+    @Test
+    void movableSquareExistAlly() {
+        Queen queen = new Queen(Color.WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(
+                queen,
+                new Pawn(Color.WHITE, Square.of(E, SIX)),
+                new Pawn(Color.WHITE, Square.of(G, SIX)),
+                new Pawn(Color.WHITE, Square.of(A, FOUR)),
+                new Pawn(Color.WHITE, Square.of(D, THREE)),
+                new Pawn(Color.WHITE, Square.of(G, TWO))
+        );
+        Set<Square> squares = queen.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(E, ONE), Square.of(E, TWO),
+                        Square.of(E, THREE), Square.of(E, FIVE),
+                        Square.of(B, FOUR), Square.of(C, FOUR),
+                        Square.of(D, FOUR), Square.of(F, FOUR),
+                        Square.of(G, FOUR), Square.of(H, FOUR),
+
+                        Square.of(A, EIGHT), Square.of(B, SEVEN),
+                        Square.of(C, SIX), Square.of(D, FIVE),
+                        Square.of(F, FIVE), Square.of(F, THREE));
+    }
+
+    /*
+     * * * * * * * * 8    o * * * x * * * 8
+     * * * * * * * * 7    * o * * x * * x 7
+     * * * * P * * * 6    * * o * o * x * 6
+     * * * * * p * * 5    * * * o o x * * 5
+     P * * * q * * * 4 -> o o o o q o o o 4
+     * * * * * * * * 3    * * * o o o * * 3
+     * * * * P * * * 2    * * o * o * o * 2
+     * * * * * * * * 1    * o * * x * * o 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_적의 기물이 존재하는 경우")
+    @Test
+    void movableSquareExistEnemy() {
+        Queen queen = new Queen(Color.WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(
+                queen,
+                new Pawn(Color.BLACK, Square.of(E, SIX)),
+                new Pawn(Color.BLACK, Square.of(E, TWO)),
+                new Pawn(Color.BLACK, Square.of(A, FOUR)),
+                new Pawn(Color.WHITE, Square.of(F, FIVE))
+        );
+        Set<Square> squares = queen.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(E, TWO), Square.of(E, THREE),
+                        Square.of(E, FIVE), Square.of(E, SIX),
+                        Square.of(A, FOUR), Square.of(B, FOUR),
+                        Square.of(C, FOUR), Square.of(D, FOUR),
+                        Square.of(F, FOUR), Square.of(G, FOUR),
+                        Square.of(H, FOUR),
+
+                        Square.of(A, EIGHT), Square.of(B, ONE),
+                        Square.of(B, SEVEN), Square.of(C, TWO),
+                        Square.of(C, SIX), Square.of(D, THREE),
+                        Square.of(D, FIVE), Square.of(F, THREE),
+                        Square.of(G, TWO), Square.of(H, ONE));
+    }
+}
