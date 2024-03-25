4900e395e4802fdd050a35e9334ed5dac5b271a7
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-25 21:40:49 +0900

    fix: 맵정보 LinkedHashMap으로 변경

--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -2,11 +2,14 @@ package chess.domain.piece;
 
 import static chess.domain.fixture.CoordinateFixture.A1;
 import static chess.domain.fixture.CoordinateFixture.A7;
+import static chess.domain.fixture.CoordinateFixture.B2;
+import static chess.domain.fixture.CoordinateFixture.C3;
 import static chess.domain.fixture.CoordinateFixture.D4;
 import static chess.domain.fixture.CoordinateFixture.E5;
 import static chess.domain.fixture.CoordinateFixture.F6;
 import static chess.domain.fixture.CoordinateFixture.G7;
 import static chess.domain.fixture.CoordinateFixture.H8;
+import static chess.domain.fixture.PieceFixture.BLACK_KNIGHT;
 import static chess.domain.fixture.PieceFixture.EMPTY_PIECE;
 import static chess.domain.fixture.PieceFixture.INITIAL_BLACK_PAWN;
 import static chess.domain.fixture.PieceFixture.NORMAL_WHITE_PAWN;
@@ -18,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import chess.domain.board.Coordinate;
 import chess.domain.piece.directionmove.Bishop;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -51,7 +55,7 @@ class BishopTest {
     @DisplayName("비숍이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다.")
     @Test
     void canMove() {
-        HashMap<Coordinate, Piece> boardInformation = new HashMap<>();
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
         boardInformation.put(G7, EMPTY_PIECE);
 
         assertThat(WHITE_BISHOP.canMove(A1, G7, boardInformation)).isTrue();
@@ -60,7 +64,7 @@ class BishopTest {
     @DisplayName("비숍이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다(기물은 잡으며 이동하는 경우).")
     @Test
     void canMoveCaseTakeDown() {
-        HashMap<Coordinate, Piece> boardInformation = new HashMap<>();
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
         boardInformation.put(G7, INITIAL_BLACK_PAWN);
 
         assertThat(WHITE_BISHOP.canMove(A1, G7, boardInformation)).isTrue();
@@ -69,8 +73,19 @@ class BishopTest {
     @DisplayName("비숍이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다(목적지에 같은 팀이 있는 경우).")
     @Test
     void canMoveCaseStuckCuzSameTeamPiece() {
-        HashMap<Coordinate, Piece> boardInformation = new HashMap<>();
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
         boardInformation.put(D4, NORMAL_WHITE_PAWN);
+
+        assertThat(WHITE_BISHOP.canMove(A1, D4, boardInformation)).isFalse();
+    }
+
+    @DisplayName("비숍이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다(중간에 기물이 있는 경우).")
+    @Test
+    void canMoveCaseStuck() {
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
+        boardInformation.put(B2, EMPTY_PIECE);
+        boardInformation.put(C3, BLACK_KNIGHT);
+        boardInformation.put(D4, EMPTY_PIECE);
 
         assertThat(WHITE_BISHOP.canMove(A1, D4, boardInformation)).isFalse();
     }
--- src/test/java/chess/domain/piece/RookTest.java
+++ src/test/java/chess/domain/piece/RookTest.java
@@ -1,6 +1,9 @@
 package chess.domain.piece;
 
 import static chess.domain.fixture.CoordinateFixture.A1;
+import static chess.domain.fixture.CoordinateFixture.A2;
+import static chess.domain.fixture.CoordinateFixture.A3;
+import static chess.domain.fixture.CoordinateFixture.A4;
 import static chess.domain.fixture.CoordinateFixture.A7;
 import static chess.domain.fixture.CoordinateFixture.D4;
 import static chess.domain.fixture.CoordinateFixture.E4;
@@ -9,6 +12,7 @@ import static chess.domain.fixture.CoordinateFixture.G4;
 import static chess.domain.fixture.CoordinateFixture.H1;
 import static chess.domain.fixture.CoordinateFixture.H4;
 import static chess.domain.fixture.CoordinateFixture.H8;
+import static chess.domain.fixture.PieceFixture.BLACK_KNIGHT;
 import static chess.domain.fixture.PieceFixture.BLACK_ROOK;
 import static chess.domain.fixture.PieceFixture.EMPTY_PIECE;
 import static chess.domain.fixture.PieceFixture.WHITE_ROOK;
@@ -19,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import chess.domain.board.Coordinate;
 import chess.domain.piece.directionmove.Rook;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -51,7 +56,7 @@ class RookTest {
     @DisplayName("룩이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다.")
     @Test
     void canMove() {
-        HashMap<Coordinate, Piece> boardInformation = new HashMap<>();
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
         boardInformation.put(H1, EMPTY_PIECE);
 
         assertThat(WHITE_ROOK.canMove(A1, H1, boardInformation)).isTrue();
@@ -60,7 +65,7 @@ class RookTest {
     @DisplayName("룩이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다(기물은 잡으며 이동하는 경우).")
     @Test
     void canMoveCaseTakeDown() {
-        HashMap<Coordinate, Piece> boardInformation = new HashMap<>();
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
         boardInformation.put(A7, BLACK_ROOK);
 
         assertThat(WHITE_ROOK.canMove(A1, A7, boardInformation)).isTrue();
@@ -69,10 +74,20 @@ class RookTest {
     @DisplayName("룩이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다(목적지에 같은 팀이 있는 경우).")
     @Test
     void canMoveCaseStuckCuzSameTeamPiece() {
-        HashMap<Coordinate, Piece> boardInformation = new HashMap<>();
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
         boardInformation.put(H1, WHITE_ROOK);
 
         assertThat(WHITE_ROOK.canMove(A1, H1, boardInformation)).isFalse();
     }
 
+    @DisplayName("룩이 경로 정보를 토대로 목적지로 갈 수 있는지 판단한다(중간에 기물이 있는 경우).")
+    @Test
+    void canMoveCaseStuck() {
+        HashMap<Coordinate, Piece> boardInformation = new LinkedHashMap<>();
+        boardInformation.put(A2, EMPTY_PIECE);
+        boardInformation.put(A3, BLACK_KNIGHT);
+        boardInformation.put(A4, EMPTY_PIECE);
+
+        assertThat(WHITE_ROOK.canMove(A1, A4, boardInformation)).isFalse();
+    }
 }
