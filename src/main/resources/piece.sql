9a3592a6a13e21dafa4d71042b3fb423349321a7
3juhwan <13selfesteem91@naver.com>
2024-03-25 16:27:01 +0900

    rename: SQL 스크립트명 수정

--- src/main/resources/piece.sql
+++ src/main/resources/piece.sql
@@ -1,7 +1,8 @@
-create table piece (
+create table piece
+(
     board_file varchar(1) not null,
     board_rank varchar(1) not null,
-    color varchar(5) not null,
-    type varchar(6) not null,
+    color      varchar(5) not null,
+    type       varchar(6) not null,
     primary key (board_file, board_rank)
 )
