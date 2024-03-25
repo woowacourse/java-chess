3ad041a3cb7d18f6980122f47cca32f52288e42d
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-25 14:27:18 +0900

    feat: 초기에 놓여지는 폰 InitialPawn으로 변경

--- src/main/java/chess/domain/board/BoardFactory.java
+++ src/main/java/chess/domain/board/BoardFactory.java
@@ -1,67 +1,61 @@
 package chess.domain.board;
 
+import chess.domain.piece.Piece;
+import chess.domain.piece.Team;
+import chess.domain.piece.directionmove.Bishop;
+import chess.domain.piece.directionmove.Queen;
+import chess.domain.piece.directionmove.Rook;
+import chess.domain.piece.fixedmove.King;
+import chess.domain.piece.fixedmove.Knight;
+import chess.domain.piece.pawn.InitialBlackPawn;
+import chess.domain.piece.pawn.InitialWhitePawn;
 import java.util.HashMap;
 import java.util.Map;
-import chess.domain.piece.Bishop;
-import chess.domain.piece.King;
-import chess.domain.piece.Knight;
-import chess.domain.piece.Pawn;
-import chess.domain.piece.Piece;
-import chess.domain.piece.Queen;
-import chess.domain.piece.Rook;
-import chess.domain.piece.Team;
 
 public class BoardFactory {
+
+    private static final int INITIAL_WHITE_SPECIAL_RANK = 1;
+    private static final int INITIAL_WHITE_PAWN_RANK = 2;
+    private static final int INITIAL_BLACK_SPECIAL_RANK = 8;
+    private static final int INITIAL_BLACK_PAWN_RANK = 7;
+    private static final char MIN_FILE_RANGE = 'a';
+    private static final char MAX_FILE_RANGE = 'h';
+    private static final HashMap<Coordinate, Piece> INITIAL_BOARD = new HashMap<>();
 
     private BoardFactory() {
     }
 
-    public static Map<Coordinate, Piece> createInitialPieces(
-            int initialWhiteSpecialRank,
-            int initialWhitePawnRank,
-            int initialBlackSpecialRank,
-            int initialBlackPawnRank) {
+    public static Map<Coordinate, Piece> createInitialPieces() {
 
-        HashMap<Coordinate, Piece> pieces = new HashMap<>();
-        initializeWhitePiece(initialWhiteSpecialRank, initialWhitePawnRank, pieces);
-        initializeBlackPiece(initialBlackSpecialRank, initialBlackPawnRank, pieces);
-
-        return pieces;
+        initializeWhitePiece();
+        initializeBlackPiece();
+        return INITIAL_BOARD;
     }
 
-    private static void initializeWhitePiece(
-            int initialWhiteSpecialRank,
-            int initialWhitePawnRank,
-            HashMap<Coordinate, Piece> pieces) {
-        initializeSpecialPiece(initialWhiteSpecialRank, Team.WHITE, pieces);
-        initializePawn(initialWhitePawnRank, Team.WHITE, pieces);
+    private static void initializeWhitePiece() {
+        initializeSpecialPiece(INITIAL_WHITE_SPECIAL_RANK, Team.WHITE);
+        initializePawn(INITIAL_WHITE_PAWN_RANK, new InitialWhitePawn());
     }
 
-    private static void initializeBlackPiece(
-            int initialBlackSpecialRank,
-            int initialBlackPawnRank,
-            HashMap<Coordinate, Piece> pieces) {
-        initializeSpecialPiece(initialBlackSpecialRank, Team.BLACK, pieces);
-        initializePawn(initialBlackPawnRank, Team.BLACK, pieces);
+    private static void initializeBlackPiece() {
+        initializeSpecialPiece(INITIAL_BLACK_SPECIAL_RANK, Team.BLACK);
+        initializePawn(INITIAL_BLACK_PAWN_RANK, new InitialBlackPawn());
     }
 
-    private static void initializePawn(
-            int rankValue,
-            Team team,
-            HashMap<Coordinate, Piece> pieces) {
-        for (char fileValue = 'a'; fileValue <= 'h'; fileValue++) {
-            pieces.put(new Coordinate(rankValue, fileValue), new Pawn(team));
+    private static void initializePawn(int rankValue, Piece pawn) {
+        for (int fileValue = MIN_FILE_RANGE; fileValue <= MAX_FILE_RANGE; fileValue++) {
+            INITIAL_BOARD.put(Coordinate.of(File.from(fileValue), Rank.from(rankValue)), pawn);
         }
     }
 
-    private static void initializeSpecialPiece(int rankValue, Team team, HashMap<Coordinate, Piece> pieces) {
-        pieces.put(new Coordinate(rankValue, 'e'), new King(team));
-        pieces.put(new Coordinate(rankValue, 'd'), new Queen(team));
-        pieces.put(new Coordinate(rankValue, 'c'), new Bishop(team));
-        pieces.put(new Coordinate(rankValue, 'f'), new Bishop(team));
-        pieces.put(new Coordinate(rankValue, 'b'), new Knight(team));
-        pieces.put(new Coordinate(rankValue, 'g'), new Knight(team));
-        pieces.put(new Coordinate(rankValue, 'a'), new Rook(team));
-        pieces.put(new Coordinate(rankValue, 'h'), new Rook(team));
+    private static void initializeSpecialPiece(int rankValue, Team team) {
+        INITIAL_BOARD.put(Coordinate.of(File.A, Rank.from(rankValue)), new Rook(team));
+        INITIAL_BOARD.put(Coordinate.of(File.B, Rank.from(rankValue)), new Knight(team));
+        INITIAL_BOARD.put(Coordinate.of(File.C, Rank.from(rankValue)), new Bishop(team));
+        INITIAL_BOARD.put(Coordinate.of(File.D, Rank.from(rankValue)), new Queen(team));
+        INITIAL_BOARD.put(Coordinate.of(File.E, Rank.from(rankValue)), new King(team));
+        INITIAL_BOARD.put(Coordinate.of(File.F, Rank.from(rankValue)), new Bishop(team));
+        INITIAL_BOARD.put(Coordinate.of(File.G, Rank.from(rankValue)), new Knight(team));
+        INITIAL_BOARD.put(Coordinate.of(File.H, Rank.from(rankValue)), new Rook(team));
     }
 }
