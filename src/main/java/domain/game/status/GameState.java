c962c8a6f1b50af4022225e3a34d84899f79175a
HyungUk Ryu <hw0603@naver.com>
2024-03-26 00:55:33 +0900

    feat(gameState): 게임 상태를 표현하는 인터페이스 정의

---
+++ src/main/java/domain/game/status/GameState.java
@@ -0,0 +1,18 @@
+package domain.game.status;
+
+import domain.game.Board;
+import domain.game.MoveResponse;
+import domain.game.TeamColor;
+import domain.position.Position;
+
+public interface GameState {
+    boolean isContinuable();
+
+    boolean isTurnOf(TeamColor teamColor);
+
+    TeamColor currentTurn();
+
+    MoveResponse move(Board board, Position source, Position destination);
+
+    TeamColor getWinner();
+}
