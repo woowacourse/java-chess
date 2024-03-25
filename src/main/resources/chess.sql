e4e7951ad1bc7bf04ee94b9a0a1d27acf70fefdb
3juhwan <13selfesteem91@naver.com>
2024-03-25 15:10:21 +0900

    feat: chess SQL문 추가

---
+++ src/main/resources/chess.sql
@@ -0,0 +1,7 @@
+create table piece (
+    board_file varchar(1) not null,
+    board_rank varchar(1) not null,
+    color varchar(5) not null,
+    type varchar(6) not null,
+    primary key (board_file, board_rank)
+)
