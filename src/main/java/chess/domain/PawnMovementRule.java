f415247c82e85e5dbb43e98bc670bade16626ce5
nayonsoso <panda0329@naver.com>
2024-03-25 22:24:28 +0900

    refactor: 빌드를 상수화

--- src/main/java/chess/domain/PawnMovementRule.java
+++ src/main/java/chess/domain/PawnMovementRule.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 public class PawnMovementRule {
 
-    private final List<Position> pawnInitialPositions = List.of(
+    private final static List<Position> PAWN_INITIAL_POSITIONS = List.of(
             Position.of(File.A, Rank.TWO), Position.of(File.B, Rank.TWO), Position.of(File.C, Rank.TWO), Position.of(File.D, Rank.TWO),
             Position.of(File.E, Rank.TWO), Position.of(File.F, Rank.TWO), Position.of(File.G, Rank.TWO), Position.of(File.H, Rank.TWO),
             Position.of(File.A, Rank.SEVEN), Position.of(File.B, Rank.SEVEN), Position.of(File.C, Rank.SEVEN), Position.of(File.D, Rank.SEVEN),
             Position.of(File.E, Rank.SEVEN), Position.of(File.F, Rank.SEVEN), Position.of(File.G, Rank.SEVEN), Position.of(File.H, Rank.SEVEN)
     );
-    private final List<Direction> pawnAttackDirections = List.of(
+    private final static List<Direction> PAWN_ATTACK_DIRECTIONS = List.of(
             Direction.LEFT_UP, Direction.LEFT_DOWN, Direction.RIGHT_UP, Direction.RIGHT_DOWN
     );
 
@@ -21,11 +21,11 @@ public class PawnMovementRule {
         Position nextPosition = sourcePosition.moveTowardDirection(direction);
         Position nextNextPosition = nextPosition.moveTowardDirection(direction);
 
-        return pawnInitialPositions.contains(sourcePosition) &&
+        return PAWN_INITIAL_POSITIONS.contains(sourcePosition) &&
                 (nextPosition.equals(targetPosition) || nextNextPosition.equals(targetPosition));
     }
 
     public boolean canMoveTowardDiagonal(Piece sourcePiece, Piece targetPiece, Direction direction) {
-        return pawnAttackDirections.contains(direction) && targetPiece.isEnemy(sourcePiece);
+        return PAWN_ATTACK_DIRECTIONS.contains(direction) && targetPiece.isEnemy(sourcePiece);
     }
 }
