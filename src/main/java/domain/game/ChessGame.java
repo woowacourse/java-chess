ac5ef1ab2c578587d16dd4f4f4141b9abd3f84a5
HyungUk Ryu <hw0603@naver.com>
2024-03-26 01:07:36 +0900

    feat(chessGame): 킹이 잡혔을 때 게임을 종료하고 승자를 판단하는 기능 구현

--- src/main/java/domain/game/ChessGame.java
+++ src/main/java/domain/game/ChessGame.java
@@ -1,11 +1,16 @@
 package domain.game;
 
 import domain.game.state.BlackTurn;
+import domain.game.state.GameEnd;
 import domain.game.state.GameState;
 import domain.game.state.WhiteTurn;
 import domain.position.Position;
+import java.util.Map;
+import java.util.Set;
 
 public class ChessGame {
+    private static final Set<PieceType> GAME_END_WHEN_CAUGHT = Set.of(PieceType.WHITE_KING, PieceType.BLACK_KING);
+
     private GameState state;
     private final Board board;
 
@@ -19,10 +24,27 @@ public class ChessGame {
     }
 
     public void move(Position source, Position destination) {
-        state.move(board, source, destination);
+        MoveResponse moveResponse = state.move(board, source, destination);
+
+        if (shouldGameEnd(moveResponse)) {
+            state = winStateOf(state);
+            return;
+        }
         state = nextStateOf(state);
     }
 
+    private boolean shouldGameEnd(MoveResponse moveResponse) {
+        if (!moveResponse.hasCaught()) {
+            return false;
+        }
+        PieceType caughtPieceType = moveResponse.caughtPieceType();
+        return GAME_END_WHEN_CAUGHT.contains(caughtPieceType);
+    }
+
+    private GameState winStateOf(GameState state) {
+        TeamColor winner = state.currentTurn();
+        return new GameEnd(winner);
+    }
 
     private GameState nextStateOf(GameState state) {
         if (state.isTurnOf(TeamColor.WHITE)) {
@@ -31,8 +53,19 @@ public class ChessGame {
         return new WhiteTurn();
     }
 
+    public boolean isGameEnd() {
+        return !state.isContinuable();
+    }
+
+    public TeamColor getWinner() {
+        return state.getWinner();
+    }
 
     public TeamColor currentPlayingTeam() {
         return state.currentTurn();
+    }
+
+    public Map<Position, Piece> getPositionsOfPieces() {
+        return board.getPositionsOfPieces();
     }
 }
