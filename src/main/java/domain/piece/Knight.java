b2398f8adcfd3ecee5dbc61b4df3df0555f6a899
SCY <helenason@naver.com>
2024-03-25 16:14:03 +0900

    refactor(Knight): 불필요한 방향 체크 로직 제거

--- src/main/java/domain/piece/Knight.java
+++ src/main/java/domain/piece/Knight.java
@@ -13,42 +13,7 @@ public class Knight extends Piece {
 
     @Override
     public boolean canMove(Position source, Position target) {
-        return isRightUpUp(source, target) || isLeftUpUp(source, target)
-                || isRightDownDown(source, target) || isLeftDownDown(source, target)
-                || isRightRightUp(source, target) || isLeftLeftUp(source, target)
-                || isRightRightDown(source, target) || isLeftLeftDown(source, target);
-    }
-
-    private boolean isRightUpUp(Position source, Position target) {
-        return source.isRightUp(target) && distanceOneFileTwoRank(source, target);
-    }
-
-    private boolean isLeftUpUp(Position source, Position target) {
-        return source.isLeftUp(target) && distanceOneFileTwoRank(source, target);
-    }
-
-    private boolean isRightDownDown(Position source, Position target) {
-        return source.isRightDown(target) && distanceOneFileTwoRank(source, target);
-    }
-
-    private boolean isLeftDownDown(Position source, Position target) {
-        return source.isLeftDown(target) && distanceOneFileTwoRank(source, target);
-    }
-
-    private boolean isRightRightUp(Position source, Position target) {
-        return source.isRightUp(target) && distanceTwoFileOneRank(source, target);
-    }
-
-    private boolean isLeftLeftUp(Position source, Position target) {
-        return source.isLeftUp(target) && distanceTwoFileOneRank(source, target);
-    }
-
-    private boolean isRightRightDown(Position source, Position target) {
-        return source.isRightDown(target) && distanceTwoFileOneRank(source, target);
-    }
-
-    private boolean isLeftLeftDown(Position source, Position target) {
-        return source.isLeftDown(target) && distanceTwoFileOneRank(source, target);
+        return distanceOneFileTwoRank(source, target) || distanceTwoFileOneRank(source, target);
     }
 
     private boolean distanceOneFileTwoRank(Position source, Position target) {
