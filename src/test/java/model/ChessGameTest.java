2366abd1e104d2abd0e295e3c8eedb261e9cbcef
unifolio0 <boho82ha@gmail.com>
2024-03-25 14:26:18 +0900

    test : 기능 변경에 따른 테스트 추가 작성

--- src/test/java/model/ChessGameTest.java
+++ src/test/java/model/ChessGameTest.java
@@ -13,14 +13,25 @@ import org.junit.jupiter.api.Test;
 
 class ChessGameTest {
 
-    @DisplayName("초기에는 32개의 기물이 생성된다.")
+    @DisplayName("처음에 start를 입력하면 32개의 기물이 생성된다.")
     @Test
-    void initPieces() {
+    void start() {
         final ChessGame chessGame = new ChessGame();
         chessGame.start();
 
         final Map<Position, Piece> board = chessGame.getBoard();
         assertThat(board.keySet()).hasSize(32);
+    }
+
+    @DisplayName("INIT 상태가 아닐 때 start를 입력하면 예외가 발생한다.")
+    @Test
+    void invalidStart() {
+        final ChessGame chessGame = new ChessGame();
+        chessGame.start();
+
+        assertThatThrownBy(chessGame::start)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 게임이 진행중입니다.");
     }
 
     @DisplayName("기물이 없는 위치가 주어졌을 때 예외가 발생한다.")
@@ -79,5 +90,35 @@ class ChessGameTest {
         assertThatThrownBy(() -> chessGame.move(moving))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자신의 기물만 이동 가능합니다.");
+    }
+
+    @DisplayName("RUNNING 상태가 아닐 때 move 명령어를 사용하면 예외가 발생한다.")
+    @Test
+    void invalidMove() {
+        final ChessGame chessGame = new ChessGame();
+
+        final Moving moving = new Moving(Position.from("a7"), Position.from("a6"));
+
+        assertThatThrownBy(() -> chessGame.move(moving))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("start를 입력해야 게임이 시작됩니다.");
+    }
+
+    @DisplayName("상태가 END일 때 true값을 반환한다.")
+    @Test
+    void end() {
+        final ChessGame chessGame = new ChessGame();
+        chessGame.end();
+
+        assertThat(chessGame.isNotEnd()).isFalse();
+    }
+
+    @DisplayName("상태가 END가 아닐 때 false값을 반환한다.")
+    @Test
+    void notEnd() {
+        final ChessGame chessGame = new ChessGame();
+        chessGame.start();
+
+        assertThat(chessGame.isNotEnd()).isTrue();
     }
 }
