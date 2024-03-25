09fe9c2dd44e618aad3942c4c464736dee0a9697
takoyakimchi <c4nyou@gmail.com>
2024-03-25 14:08:58 +0900

    test: Knight 테스트에 ParameterizedTest 적용

--- src/test/java/chess/domain/piece/KnightTest.java
+++ src/test/java/chess/domain/piece/KnightTest.java
@@ -5,97 +5,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.board.Position;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class KnightTest {
 
-    @Test
-    @DisplayName("위로 한칸 이동 후 오른쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_UpUpRight() {
+    @DisplayName("나이트 이동 가능")
+    @ParameterizedTest(name = "(4,4)에서 ({0},{1})로 이동할 수 있다.")
+    @CsvSource({"5,6", "3,6", "6,5", "6,3", "5,2", "3,2", "2,3", "2,5"})
+    void canMove(int file, int rank) {
         Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(5, 6);
-        assertThat(knight.canMove(position1, position2)).isTrue();
+        Position source = Position.of(4, 4);
+        Position target = Position.of(file, rank);
+        assertThat(knight.canMove(source, target)).isTrue();
     }
 
-    @Test
-    @DisplayName("위로 한칸 이동 후 왼쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_UpUpLeft() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(3, 6);
-        assertThat(knight.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("오른쪽으로 한칸 이동 후 위쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_RightRightUp() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(6, 5);
-        assertThat(knight.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("오른쪽으로 한칸 이동 후 아래쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_RightRightDown() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(6, 3);
-        assertThat(knight.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("아래로 한칸 이동 후 오른쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_DownDownRight() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(5, 2);
-        assertThat(knight.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("아래로 한칸 이동 후 왼쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_DownDownLeft() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(3, 2);
-        assertThat(knight.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("왼쪽으로 한칸 이동 후 아래쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_LeftLeftDown() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(2, 3);
-        assertThat(knight.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("위로 한칸 이동 후 오른쪽 대각선 방향으로 나이트 이동 가능")
-    void canMove_LeftLeftUp() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(2, 5);
-        assertThat(knight.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
     @DisplayName("나이트 이동 불가")
-    void cannotMove() {
+    @ParameterizedTest(name = "(4,4)에서 ({0},{1})로 이동할 수 없다.")
+    @CsvSource({"3,3", "4,4"})
+    void cannotMove(int file, int rank) {
         Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(4, 4);
-        Position position2 = Position.of(3, 3);
-        assertThat(knight.canMove(position1, position2)).isFalse();
-    }
-
-    @Test
-    @DisplayName("같은 위치로는 이동 불가")
-    void cannotMove_SamePosition() {
-        Knight knight = new Knight(Color.WHITE);
-        Position position1 = Position.of(1, 1);
-        Position position2 = Position.of(1, 1);
-        assertThat(knight.canMove(position1, position2)).isFalse();
+        Position source = Position.of(4, 4);
+        Position target = Position.of(file, rank);
+        assertThat(knight.canMove(source, target)).isFalse();
     }
 }
