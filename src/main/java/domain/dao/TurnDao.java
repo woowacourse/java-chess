e7f427e8f274050f04b02b70a5fe17bfa2551870
3juhwan <13selfesteem91@naver.com>
2024-03-25 16:55:54 +0900

    feat: TurnDao 추가

---
+++ src/main/java/domain/dao/TurnDao.java
@@ -0,0 +1,48 @@
+package domain.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TurnDao {
+    private static final String tableName = "turns";
+    private final ConnectionManager connectionManager;
+
+    private TurnDao(final ConnectionManager connectionManager) {
+        this.connectionManager = connectionManager;
+    }
+
+    public TurnDao() {
+        this(new ConnectionManager());
+    }
+
+    public String find() {
+        final var query = "SELECT * FROM " + tableName;
+        try (final var connection = connectionManager.getConnection();
+             final var prepareStatement = connection.prepareStatement(query)) {
+            ResultSet resultSet = prepareStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getString("color");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public void update(String color) {
+        final String deleteQuery = "DELETE FROM " + tableName;
+        final String insertQuery = "INSERT INTO " + tableName + " (color) VALUES (?)";
+        try (final Connection connection = connectionManager.getConnection();
+             final PreparedStatement deleteStatement = connection.prepareStatement(deleteQuery);
+             final PreparedStatement insertStatement = connection.prepareStatement(insertQuery)) {
+            deleteStatement.executeUpdate();
+
+            insertStatement.setString(1, color);
+            insertStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/domain/dao/TurnDaoTest.java
@@ -0,0 +1,17 @@
+package domain.dao;
+
+import domain.piece.Color;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TurnDaoTest {
+    @Test
+    void 턴을_저장한다() {
+        TurnDao turnDao = new TurnDao();
+
+        turnDao.update(Color.WHITE.name());
+
+        assertThat(turnDao.find()).isEqualTo("WHITE");
+    }
+}
