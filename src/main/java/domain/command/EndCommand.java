2bbad9974aa4ab08d6c0c615e32b17f201a57b88
slimsha2dy <chy2581@naver.com>
2024-03-25 20:46:23 +0900

    refactor: 사용하지 않는 import 문 삭제

--- src/main/java/domain/command/EndCommand.java
+++ src/main/java/domain/command/EndCommand.java
@@ -1,7 +1,5 @@
 package domain.command;
 
-import java.util.List;
-
 public class EndCommand implements Command {
     public static EndCommand END_COMMAND = new EndCommand();
 
--- src/main/java/domain/command/StartCommand.java
+++ src/main/java/domain/command/StartCommand.java
@@ -1,9 +1,8 @@
 package domain.command;
 
-import java.util.List;
-
 public class StartCommand implements Command {
     public static StartCommand START_COMMAND = new StartCommand();
 
-    private StartCommand() {}
+    private StartCommand() {
+    }
 }
