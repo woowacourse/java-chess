fa55eed5b201018045e2df8edbd22e4cc12f1166
Eugene Jang <kyunellroll@gmail.com>
2024-03-25 13:47:27 +0900

    refactor(ChessBoardInitializer): 초기 보드를 Blank를 추가해 세팅하는 메서드 추가

--- src/main/java/domain/chessboard/ChessBoardInitializer.java
+++ src/main/java/domain/chessboard/ChessBoardInitializer.java
@@ -25,22 +25,44 @@ public class ChessBoardInitializer {
     public static Map<Coordinate, ChessPiece> createInitialBoard() {
         Map<Coordinate, ChessPiece> board = new HashMap<>();
 
+        settingFirstRow(board);
+        settingAllBlankRows(board);
+        settingSecondRow(board);
+
+        return board;
+    }
+
+    private static void settingFirstRow(Map<Coordinate, ChessPiece> board) {
         for (int i = 1; i <= CHESS_BOARD_COLUMNS.length(); i++) {
             board.put(Coordinate.from(CHESS_BOARD_COLUMNS.charAt(i - 1) + BLACK_FIRST_RANK),
                     createFirstRank(Color.BLACK).get(i));
             board.put(Coordinate.from(CHESS_BOARD_COLUMNS.charAt(i - 1) + WHITE_FIRST_RANK),
                     createFirstRank(Color.WHITE).get(i));
         }
+    }
+
+    private static void settingAllBlankRows(Map<Coordinate, ChessPiece> board) {
+        for (int row = 3; row <= 6; row++) {
+            settingBlankRow(board, String.valueOf(row));
+        }
+    }
+
+    private static void settingBlankRow(Map<Coordinate, ChessPiece> board, String row) {
+        for (int i = 1; i <= CHESS_BOARD_COLUMNS.length(); i++) {
+            board.put(Coordinate.from(CHESS_BOARD_COLUMNS.charAt(i - 1) + row), new Blank());
+        }
+    }
+
+    private static void settingSecondRow(Map<Coordinate, ChessPiece> board) {
         for (int i = 1; i <= CHESS_BOARD_COLUMNS.length(); i++) {
             board.put(Coordinate.from(CHESS_BOARD_COLUMNS.charAt(i - 1) + BLACK_SECOND_RANK), new Pawn(Color.BLACK));
             board.put(Coordinate.from(CHESS_BOARD_COLUMNS.charAt(i - 1) + WHITE_SECOND_RANK), new Pawn(Color.WHITE));
         }
-        return board;
     }
 
     private static List<ChessPiece> createFirstRank(Color color) {
         return List.of(
-                new Blank(color),
+                new Blank(),
                 new Rook(color),
                 new Knight(color),
                 new Bishop(color),
