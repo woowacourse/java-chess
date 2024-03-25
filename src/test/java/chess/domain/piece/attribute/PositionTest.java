22c09143afa9dfa70afe154be7244aea2bffaca3
Zeus6768 <akaps6768@gmail.com>
2024-03-26 03:21:50 +0900

    style: 탭 문자 대신 공백 사용

--- src/test/java/chess/domain/piece/attribute/PositionTest.java
+++ src/test/java/chess/domain/piece/attribute/PositionTest.java
@@ -22,49 +22,49 @@ import chess.domain.chessboard.attribute.Direction;
 
 class PositionTest {
 
-	private static final String INITIAL_POSITION = "d5";
+    private static final String INITIAL_POSITION = "d5";
 
-	static Stream<Arguments> moveTo() {
-		return Stream.of(
-				Arguments.of(UP, Position.from("d6")),
-				Arguments.of(UP_RIGHT, Position.from("e6")),
-				Arguments.of(RIGHT, Position.from("e5")),
-				Arguments.of(DOWN_RIGHT, Position.from("e4")),
-				Arguments.of(DOWN, Position.from("d4")),
-				Arguments.of(DOWN_LEFT, Position.from("c4")),
-				Arguments.of(LEFT, Position.from("c5")),
-				Arguments.of(UP_LEFT, Position.from("c6"))
-		);
-	}
+    static Stream<Arguments> moveTo() {
+        return Stream.of(
+                Arguments.of(UP, Position.from("d6")),
+                Arguments.of(UP_RIGHT, Position.from("e6")),
+                Arguments.of(RIGHT, Position.from("e5")),
+                Arguments.of(DOWN_RIGHT, Position.from("e4")),
+                Arguments.of(DOWN, Position.from("d4")),
+                Arguments.of(DOWN_LEFT, Position.from("c4")),
+                Arguments.of(LEFT, Position.from("c5")),
+                Arguments.of(UP_LEFT, Position.from("c6"))
+        );
+    }
 
-	@DisplayName("현재 위치에서 주어진 방향으로 이동한다.")
-	@MethodSource
-	@ParameterizedTest
-	void moveTo(Direction direction, Position expected) {
-		Position sut = Position.from(INITIAL_POSITION);
-		Position actual = sut.moveTo(direction).get();
-		assertThat(actual).isEqualTo(expected);
-	}
+    @DisplayName("현재 위치에서 주어진 방향으로 이동한다.")
+    @MethodSource
+    @ParameterizedTest
+    void moveTo(Direction direction, Position expected) {
+        Position sut = Position.from(INITIAL_POSITION);
+        Position actual = sut.moveTo(direction).get();
+        assertThat(actual).isEqualTo(expected);
+    }
 
-	static Stream<Arguments> after() {
-		return Stream.of(
-				Arguments.of(Movement.of(UP, UP_LEFT), Position.from("c7")),
-				Arguments.of(Movement.of(UP, UP_RIGHT), Position.from("e7")),
-				Arguments.of(Movement.of(RIGHT, UP_RIGHT), Position.from("f6")),
-				Arguments.of(Movement.of(RIGHT, DOWN_RIGHT), Position.from("f4")),
-				Arguments.of(Movement.of(DOWN, DOWN_LEFT), Position.from("c3")),
-				Arguments.of(Movement.of(DOWN, DOWN_RIGHT), Position.from("e3")),
-				Arguments.of(Movement.of(LEFT, DOWN_LEFT), Position.from("b4")),
-				Arguments.of(Movement.of(LEFT, UP_LEFT), Position.from("b6"))
-		);
-	}
+    static Stream<Arguments> after() {
+        return Stream.of(
+                Arguments.of(Movement.of(UP, UP_LEFT), Position.from("c7")),
+                Arguments.of(Movement.of(UP, UP_RIGHT), Position.from("e7")),
+                Arguments.of(Movement.of(RIGHT, UP_RIGHT), Position.from("f6")),
+                Arguments.of(Movement.of(RIGHT, DOWN_RIGHT), Position.from("f4")),
+                Arguments.of(Movement.of(DOWN, DOWN_LEFT), Position.from("c3")),
+                Arguments.of(Movement.of(DOWN, DOWN_RIGHT), Position.from("e3")),
+                Arguments.of(Movement.of(LEFT, DOWN_LEFT), Position.from("b4")),
+                Arguments.of(Movement.of(LEFT, UP_LEFT), Position.from("b6"))
+        );
+    }
 
-	@DisplayName("현재 위치에서 주어진 이동을 수행한다.")
-	@MethodSource
-	@ParameterizedTest
-	void after(Movement movement, Position expected) {
-		Position sut = Position.from(INITIAL_POSITION);
-		Position actual = sut.after(movement).get();
-		assertThat(actual).isEqualTo(expected);
-	}
+    @DisplayName("현재 위치에서 주어진 이동을 수행한다.")
+    @MethodSource
+    @ParameterizedTest
+    void after(Movement movement, Position expected) {
+        Position sut = Position.from(INITIAL_POSITION);
+        Position actual = sut.after(movement).get();
+        assertThat(actual).isEqualTo(expected);
+    }
 }
