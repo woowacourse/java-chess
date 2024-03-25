c951572d154c5307edb41b26881e16711d41ca25
takoyakimchi <c4nyou@gmail.com>
2024-03-25 14:14:28 +0900

    test: King 테스트에 ParameterizedTest 적용

--- src/test/java/chess/domain/piece/KingTest.java
+++ src/test/java/chess/domain/piece/KingTest.java
@@ -4,70 +4,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.board.Position;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class KingTest {
 
-    @Test
-    @DisplayName("오른쪽 위 대각선 방향으로 킹 이동 가능")
-    void canMove_RightUp() {
+    @DisplayName("킹 이동 가능")
+    @ParameterizedTest(name = "(4,4)에서 ({0},{1})로 이동할 수 있다.")
+    @CsvSource({"4,5", "4,3", "5,4", "3,4", "5,5", "3,5", "5,3", "3,3"})
+    void canMove(int file, int rank) {
         King king = new King(Color.WHITE);
-        Position position1 = Position.of(1, 3);
-        Position position2 = Position.of(2, 4);
-        assertThat(king.canMove(position1, position2)).isTrue();
+        Position source = Position.of(4, 4);
+        Position target = Position.of(file, rank);
+        assertThat(king.canMove(source, target)).isTrue();
     }
 
-    @Test
-    @DisplayName("오른쪽 아래 대각선 방향으로 킹 이동 가능")
-    void canMove_RightDown() {
+    @DisplayName("킹 이동 불가")
+    @ParameterizedTest(name = "(4,4)에서 ({0},{1})로 이동할 수 없다.")
+    @CsvSource({"4,4", "2,4"})
+    void cannotMove(int file, int rank) {
         King king = new King(Color.WHITE);
-        Position position1 = Position.of(4, 6);
-        Position position2 = Position.of(3, 7);
-        assertThat(king.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("왼쪽 위 대각선 방향으로 킹 이동 가능")
-    void canMove_LeftUp() {
-        King king = new King(Color.WHITE);
-        Position position1 = Position.of(3, 5);
-        Position position2 = Position.of(2, 6);
-        assertThat(king.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("왼쪽 아래 대각선 방향으로 킹 이동 가능")
-    void canMove_LeftDown() {
-        King king = new King(Color.WHITE);
-        Position position1 = Position.of(3, 5);
-        Position position2 = Position.of(2, 4);
-        assertThat(king.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("1칸을 초과하면 킹 이동 불가")
-    void cannotMove() {
-        King king = new King(Color.WHITE);
-        Position position1 = Position.of(1, 1);
-        Position position2 = Position.of(1, 3);
-        assertThat(king.canMove(position1, position2)).isFalse();
-    }
-
-    @Test
-    @DisplayName("Rank가 같고 File 차이가 한 칸이면 킹 이동 가능")
-    void canMove_SameRank() {
-        King king = new King(Color.WHITE);
-        Position position1 = Position.of(1, 1);
-        Position position2 = Position.of(2, 1);
-        assertThat(king.canMove(position1, position2)).isTrue();
-    }
-
-    @Test
-    @DisplayName("File이 같고 Rank 차이가 한 칸이면 킹 이동 가능")
-    void canMove_SameFile() {
-        King king = new King(Color.WHITE);
-        Position position1 = Position.of(1, 1);
-        Position position2 = Position.of(1, 2);
-        assertThat(king.canMove(position1, position2)).isTrue();
+        Position source = Position.of(4, 4);
+        Position target = Position.of(file, rank);
+        assertThat(king.canMove(source, target)).isFalse();
     }
 }
