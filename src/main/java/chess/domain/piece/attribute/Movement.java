adc25f97d5d5230b89aede26d8caa5ae383bfc9b
Zeus6768 <akaps6768@gmail.com>
2024-03-26 03:14:10 +0900

    style: 탭 문자 대신 공백 사용

--- src/main/java/chess/domain/piece/attribute/Movement.java
+++ src/main/java/chess/domain/piece/attribute/Movement.java
@@ -9,21 +9,21 @@ import chess.domain.chessboard.attribute.Direction;
 
 public class Movement {
 
-	private final Queue<Direction> directions;
+    private final Queue<Direction> directions;
 
-	private Movement(final Queue<Direction> directions) {
-		this.directions = directions;
-	}
+    private Movement(final Queue<Direction> directions) {
+        this.directions = directions;
+    }
 
-	public static Movement of(final Direction... directions) {
-		if (directions == null || directions.length == 0) {
-			throw new IllegalArgumentException("이동을 위한 방향은 1개 이상입니다.");
-		}
-		return new Movement(new LinkedList<>(Arrays.asList(directions)));
-	}
+    public static Movement of(final Direction... directions) {
+        if (directions == null || directions.length == 0) {
+            throw new IllegalArgumentException("이동을 위한 방향은 1개 이상입니다.");
+        }
+        return new Movement(new LinkedList<>(Arrays.asList(directions)));
+    }
 
-	public List<Direction> directions() {
-		return directions.stream()
-				.toList();
-	}
+    public List<Direction> directions() {
+        return directions.stream()
+                .toList();
+    }
 }
--- src/main/java/chess/view/dto/ChessboardDto.java
+++ src/main/java/chess/view/dto/ChessboardDto.java
@@ -7,25 +7,25 @@ import chess.domain.chessboard.attribute.Square;
 
 public class ChessboardDto {
 
-	private final List<List<String>> chessboard;
+    private final List<List<String>> chessboard;
 
-	public ChessboardDto(final Chessboard chessboard) {
-		this.chessboard = initialize(chessboard.getSquares());
-	}
+    public ChessboardDto(final Chessboard chessboard) {
+        this.chessboard = initialize(chessboard.getSquares());
+    }
 
-	private List<List<String>> initialize(List<List<Square>> squares) {
-		return squares.stream()
-				.map(this::initializeRow)
-				.toList();
-	}
+    private List<List<String>> initialize(List<List<Square>> squares) {
+        return squares.stream()
+                .map(this::initializeRow)
+                .toList();
+    }
 
-	private List<String> initializeRow(List<Square> row) {
-		return row.stream()
-				.map(SquareCharacter::from)
-				.toList();
-	}
+    private List<String> initializeRow(List<Square> row) {
+        return row.stream()
+                .map(SquareCharacter::from)
+                .toList();
+    }
 
-	public List<List<String>> get() {
-		return List.copyOf(chessboard);
-	}
+    public List<List<String>> get() {
+        return List.copyOf(chessboard);
+    }
 }
