6ee33ddc3701a058b6236833a22138ca3d5d1685
MingyeomKim <mingyum119@naver.com>
2024-03-25 20:35:47 +0900

    refactor: 컨트롤러 내부에서 뷰 객체를 생성하도록 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,8 @@
 import controller.ChessController;
-import view.InputView;
-import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        ChessController chessController = new ChessController(new InputView(), new OutputView());
+        ChessController chessController = new ChessController();
         chessController.runChessGame();
     }
 }
