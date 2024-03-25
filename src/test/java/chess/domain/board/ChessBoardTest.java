a3c551cc1cc76d88af0df695aab924513432479b
coli-geonwoo <kkwoo001021@naver.com>
2024-03-25 15:23:20 +0900

    test: 체스보드 메서드 테스트 추가

--- src/test/java/chess/domain/board/ChessBoardTest.java
+++ src/test/java/chess/domain/board/ChessBoardTest.java
@@ -1,63 +1,92 @@
 package chess.domain.board;
 
-import chess.domain.piece.King;
-import chess.domain.piece.Piece;
+import chess.domain.piece.Pawn;
 import chess.domain.piece.Team;
 import chess.domain.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+import static chess.domain.position.Fixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ChessBoardTest {
-    @DisplayName("체스보드의 특정위치에 기물이 없는 지 확인할 수 있다")
+    /*
+    ........  8
+    ........  7
+    ........  6
+    ........  5
+    ........  4
+    ...P....  3
+    ........  2
+    ........  1
+    abcdefgh
+    */
+    @DisplayName("체스보드의 특정위치에 기물이 없는지 확인할 수 있다")
     @Test
     void should_CheckPositionEmptiness_When_GivenPosition() {
-        Map<Position, Piece> positionPiece = new HashMap<>();
-        Position position = Position.of(1, 1);
-        positionPiece.put(position, new King(Team.BLACK));
-        ChessBoard chessBoard = new ChessBoard(positionPiece);
-
-        assertThat(chessBoard.positionIsEmpty(Position.of(2, 2))).isTrue();
+        ChessBoard board = new ChessBoard(Map.ofEntries(Map.entry(D3, new Pawn(Team.BLACK))));
+        assertAll(
+                () -> assertThat(board.positionIsEmpty(D3)).isFalse(),
+                () -> assertThat(board.positionIsEmpty(D2)).isTrue()
+        );
     }
 
-    @DisplayName("체스보드의 특정위치에 기물이 있는지 확인할 수 있다")
-    @Test
-    void should_CheckPositionNotEmptiness_When_GivenPosition() {
-        Map<Position, Piece> positionPiece = new HashMap<>();
-        Position position = Position.of(1, 1);
-        positionPiece.put(position, new King(Team.BLACK));
-        ChessBoard chessBoard = new ChessBoard(positionPiece);
-
-        assertThat(chessBoard.positionIsEmpty(Position.of(1, 1))).isFalse();
-    }
-
+    /*
+    ........  8
+    ........  7
+    ........  6
+    ........  5
+    ........  4
+    ...P....  3
+    ........  2
+    ........  1
+    abcdefgh
+    */
     @DisplayName("체스보드 특정 위치의 기물을 가져올 수 있다")
     @Test
     void should_GetPieceByPosition_When_GiveTargetPosition() {
-        Map<Position, Piece> positionPiece = new HashMap<>();
-        Position position = Position.of(1, 1);
-        positionPiece.put(position, new King(Team.BLACK));
-        ChessBoard chessBoard = new ChessBoard(positionPiece);
+        ChessBoard board = new ChessBoard(Map.ofEntries(Map.entry(D3, new Pawn(Team.BLACK))));
 
-        assertThat(chessBoard.findPieceByPosition(position)).isInstanceOf(King.class);
+        assertThat(board.findPieceByPosition(D3)).isInstanceOf(Pawn.class);
     }
 
-    @DisplayName("체스보드 특정 위치의 기물을 가져올 때 없으면 예외를 발생시킨다")
+    @DisplayName("체스보드 특정 위치의 기물을 가져올 때, 위치에 기물이 없으면 예외를 발생시킨다")
     @Test
     void should_ThrowNoSuchElementException_When_TargetPositionIsEmpty() {
-        Map<Position, Piece> positionPiece = new HashMap<>();
-        Position position = Position.of(1, 1);
-        positionPiece.put(position, new King(Team.BLACK));
-        ChessBoard chessBoard = new ChessBoard(positionPiece);
+        ChessBoard board = new ChessBoard(Map.ofEntries(Map.entry(D3, new Pawn(Team.BLACK))));
 
-        assertThatThrownBy(() -> chessBoard.findPieceByPosition(Position.of(1, 2)))
+        assertThatThrownBy(() -> board.findPieceByPosition(D2))
                 .isInstanceOf(NoSuchElementException.class)
                 .hasMessage("해당 위치에 기물이 존재하지 않습니다.");
+    }
+
+    /*
+    ........  8
+    .......0  7
+    ......x.  6
+    .....x..  5
+    ....x...  4
+    ...b....  3
+    ....p...  2
+    .....x..  1
+    abcdefgh
+    */
+    @DisplayName("체스보드 경유경로에 기물이 없는지 확인할 수 있다")
+    @Test
+    void should_CheckNoOtherPiecesInPath() {
+        ChessBoard board = new ChessBoard(Map.ofEntries(Map.entry(E2, new Pawn(Team.BLACK))));
+        List<Position> cleanPath = D3.findPath(H7);
+        List<Position> blockedPath = D3.findPath(F1);
+
+        assertAll(
+                () -> assertThat(board.pathIsAllEmpty(cleanPath)).isTrue(),
+                () -> assertThat(board.pathIsAllEmpty(blockedPath)).isFalse()
+        );
     }
 }
