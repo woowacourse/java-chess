6209d4360e8e9dc2eb61d6bb39b80679e466d03c
fram1998 <koust6u@pusan.ac.kr>
2024-03-25 15:20:52 +0900

    feat(File): File 이 직접 이동하는 책임을 가지도록 기능 구현

--- src/main/java/domain/piece/attribute/point/File.java
+++ src/main/java/domain/piece/attribute/point/File.java
@@ -32,4 +32,44 @@ public enum File {
         return values()[ordinalIndex];
     }
 
+
+
+    public boolean isFarLeft() {
+        return ordinal() == 0;
+    }
+
+    public boolean isFarRight() {
+        return ordinal() == values().length - 1;
+    }
+
+    public boolean canMoveLeft(final int step) {
+        return ordinal() - step >= 0;
+    }
+
+    public boolean canMoveRight(final int step) {
+        return ordinal() + step < values().length;
+    }
+
+    public File moveLeft(final int step) {
+        if (canMoveLeft(step)) {
+            return values()[ordinal() - step];
+        }
+        throw new IllegalStateException("움직일 수 없는 위치입니다.");
+    }
+
+    public File moveLeft() {
+        return moveLeft(1);
+    }
+
+    public File moveRight(final int step) {
+        if (canMoveRight(step)) {
+            return values()[ordinal() + step];
+        }
+        throw new IllegalStateException("움직일 수 없는 위치입니다.");
+    }
+
+    public File moveRight() {
+        return moveRight(1);
+    }
+
 }
