fe70782c784bf211047d841ac36739b86f5d266f
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-25 10:27:24 +0900

    feat(Bishop): 비숍이 이동할 수 있는 위치를 반환하는 기능 구현

--- src/main/java/chess/domain/piece/Bishop.java
+++ src/main/java/chess/domain/piece/Bishop.java
@@ -3,7 +3,11 @@ package chess.domain.piece;
 import chess.domain.attribute.Color;
 import chess.domain.attribute.Square;
 import chess.domain.chessboard.attribute.Direction;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Bishop extends SlidingPiece {
 
@@ -14,5 +18,99 @@ public class Bishop extends SlidingPiece {
     @Override
     public Set<Square> movableSquaresFrom(final Square source) {
         return movableSquaresOf(Direction.ofBishop(), source);
+    }
+
+    public Set<Square> findLegalMoves(Set<Piece> entirePieces) {
+        return Stream.of(candidateLeftUpSquares(entirePieces),
+                        candidateLeftDownSquares(entirePieces),
+                        candidateRightDownSquares(entirePieces),
+                        candidateRightUpSquares(entirePieces))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+    }
+
+    private Set<Square> candidateLeftUpSquares(Set<Piece> entirePieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveLeftUp()) {
+            currentSquare = currentSquare.moveLeftUp();
+            if (isOccupied(entirePieces, currentSquare)) {
+                Piece piece = getPiece(entirePieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    // 현재 위치에 해당하는 실제 기물을 가져오기
+    private Piece getPiece(Set<Piece> entirePieces, Square currentSquare) { //todo 예외를 던지는 것이 아닌, Blank를 반환하기
+        return entirePieces.stream()
+                .filter(piece -> piece.currentSquare() == currentSquare)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 기물이 없습니다."));
+    }
+
+    private boolean isOccupied(Set<Piece> entirePieces, Square currentSquare) {
+        return entirePieces.stream()
+                .anyMatch(piece -> piece.currentSquare() == currentSquare);
+    }
+
+    private Set<Square> candidateLeftDownSquares(Set<Piece> entirePieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveLeftDown()) {
+            currentSquare = currentSquare.moveLeftDown();
+            if (isOccupied(entirePieces, currentSquare)) {
+                Piece piece = getPiece(entirePieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateRightUpSquares(Set<Piece> entirePieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveRightUp()) {
+            currentSquare = currentSquare.moveRightUp();
+            if (isOccupied(entirePieces, currentSquare)) {
+                Piece piece = getPiece(entirePieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
+    }
+
+    private Set<Square> candidateRightDownSquares(Set<Piece> entirePieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        while (currentSquare.canMoveRightDown()) {
+            currentSquare = currentSquare.moveRightDown();
+            if (isOccupied(entirePieces, currentSquare)) {
+                Piece piece = getPiece(entirePieces, currentSquare);
+                if (isAllyOf(piece)) {
+                    break;
+                }
+                squares.add(currentSquare);
+                break;
+            }
+            squares.add(currentSquare);
+        }
+        return squares;
     }
 }
---
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -0,0 +1,119 @@
+package chess.domain.piece;
+
+import static chess.domain.attribute.Color.*;
+import static chess.domain.attribute.File.*;
+import static chess.domain.attribute.Rank.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.domain.attribute.Square;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BishopTest {
+
+    /*
+     * * * * * * * * 8    o * * * * * * * 8
+     * * * * * * * * 7    * o * * * * * o 7
+     * * * * * * * * 6    * * o * * * o * 6
+     * * * * * * * * 5    * * * o * o * * 5
+     * * * * b * * * 4 -> * * * * b * * * 4
+     * * * * * * * * 3    * * * o * o * * 3
+     * * * * * * * * 2    * * o * * * o * 2
+     * * * * * * * * 1    * o * * * * * o 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_기물이 존재하지 않는 경우")
+    @Test
+    void movableSquareAllDirection() {
+        Bishop bishop = new Bishop(WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(bishop);
+        Set<Square> squares = bishop.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(B, ONE),
+                        Square.of(C, TWO),
+                        Square.of(D, THREE),
+                        Square.of(F, FIVE),
+                        Square.of(G, SIX),
+                        Square.of(H, SEVEN),
+                        Square.of(A, EIGHT),
+                        Square.of(B, SEVEN),
+                        Square.of(C, SIX),
+                        Square.of(D, FIVE),
+                        Square.of(F, THREE),
+                        Square.of(G, TWO),
+                        Square.of(H, ONE));
+    }
+
+    /*
+     * * * * * * * * 8    o * * * * * * * 8
+     * * * * * * * * 7    * o * * * * * x 7
+     * * * * * * p * 6    * * o * * * x * 6
+     * * * * * * * * 5    * * * o * o * * 5
+     * * * * b * * * 4 -> * * * * b * * * 4
+     * * * * * * * * 3    * * * o * o * * 3
+     * * * * * * * * 2    * * o * * * o * 2
+     * * * * * * * * 1    * o * * * * * o 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_같은 색 기물이 존재하는 경우")
+    @Test
+    void movableSquareExistAlly() {
+        Bishop bishop = new Bishop(WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(
+                bishop,
+                new Pawn(WHITE, Square.of(G, SIX))
+        );
+        Set<Square> squares = bishop.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(B, ONE),
+                        Square.of(C, TWO),
+                        Square.of(D, THREE),
+                        Square.of(F, FIVE),
+                        Square.of(A, EIGHT),
+                        Square.of(B, SEVEN),
+                        Square.of(C, SIX),
+                        Square.of(D, FIVE),
+                        Square.of(F, THREE),
+                        Square.of(G, TWO),
+                        Square.of(H, ONE));
+    }
+
+    /*
+     * * * * * * * * 8    o * * * * * * * 8
+     * * * * * * * * 7    * o * * * * * x 7
+     * * * * * * P * 6    * * o * * * o * 6
+     * * * * * * * * 5    * * * o * o * * 5
+     * * * * b * * * 4 -> * * * * b * * * 4
+     * * * * * * * * 3    * * * o * o * * 3
+     * * * * * * * * 2    * * o * * * o * 2
+     * * * * * * * * 1    * o * * * * * o 1
+     a b c d e f g h      a b c d e f g h
+     */
+    @DisplayName("이동 가능한 모든 위치를 반환한다_기물이 존재하지 않는 경우")
+    @Test
+    void movableSquareExistEnemy() {
+        Bishop bishop = new Bishop(WHITE, Square.of(E, FOUR));
+        Set<Piece> existPieces = Set.of(
+                bishop,
+                new Pawn(BLACK, Square.of(G, SIX))
+        );
+        Set<Square> squares = bishop.findLegalMoves(existPieces);
+        assertThat(squares)
+                .containsExactlyInAnyOrder(
+                        Square.of(B, ONE),
+                        Square.of(C, TWO),
+                        Square.of(D, THREE),
+                        Square.of(F, FIVE),
+                        Square.of(G, SIX),
+                        Square.of(A, EIGHT),
+                        Square.of(B, SEVEN),
+                        Square.of(C, SIX),
+                        Square.of(D, FIVE),
+                        Square.of(F, THREE),
+                        Square.of(G, TWO),
+                        Square.of(H, ONE));
+    }
+}
