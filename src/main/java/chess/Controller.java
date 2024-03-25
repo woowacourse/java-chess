942f91a6900b9605f988f580992d2670d676345b
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-25 16:12:44 +0900

    feat: Command에 관한 view 로직을 관리하는 기능 구현
    - move 커맨드의 경우 source, target을 coordinate 객체로 변환한다.

--- src/main/java/chess/Controller.java
+++ src/main/java/chess/Controller.java
@@ -1,10 +1,10 @@
 package chess;
 
-import java.util.List;
 import chess.domain.board.Board;
-import chess.domain.board.Coordinate;
+import chess.view.Command;
 import chess.view.InputView;
 import chess.view.OutputView;
+import java.util.List;
 
 class Controller {
 
@@ -18,17 +18,14 @@ class Controller {
 
     public void run() {
         outputView.printStartMessage();
-        play(new Board());
-    }
-
-    private void play(Board board) {
-        String command = inputView.readCommand();
-        if (command.equals("start")) {
-            playChess(board);
+        List<String> commandInput = inputView.readCommand();
+        Command command = Command.from(commandInput.get(0));
+        if (command.isStart()) {
+            playChess(new Board());
             return;
         }
 
-        if (command.equals("end")) {
+        if (command.isEnd()) {
             return;
         }
 
@@ -37,33 +34,18 @@ class Controller {
 
     private void playChess(Board board) {
         outputView.printBoard(board);
-        String read = inputView.readCommand();
-        if (read.startsWith("move ")) {
-            List<Coordinate> coordinates = parseToCoordinates(read);
-            board.move(coordinates.get(0), coordinates.get(1));
+        List<String> commandInput = inputView.readCommand();
+        Command command = Command.from(commandInput.get(0));
+        if (command.isMove()) {
+            board.move(command.sourceCoordinate(commandInput.get(1)), command.targetCoordinate(commandInput.get(2)));
             playChess(board);
             return;
         }
 
-        if (read.equals("end")) {
+        if (command.isEnd()) {
             return;
         }
 
         throw new IllegalArgumentException("잘못된 입력입니다.");
-    }
-
-    private List<Coordinate> parseToCoordinates(String read) {
-        String[] moveCommand = read.split("move ");
-        String[] coordinates = moveCommand[1].split(" ");
-        if (coordinates.length != 2) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
-        }
-
-        return List.of(parseToCoordinate(coordinates[0]), parseToCoordinate(coordinates[1]));
-    }
-
-    private Coordinate parseToCoordinate(String input) {
-        String[] split = input.split("");
-        return new Coordinate(Integer.parseInt(split[1]), split[0].charAt(0));
     }
 }
--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,12 +1,27 @@
 package chess.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
+    private static final int START_AND_END_COMMAND_SIZE = 1;
+    private static final int MOVE_COMMAND_SIZE = 3;
+    
     private final Scanner scanner = new Scanner(System.in);
 
-    public String readCommand() {
-        return scanner.nextLine().trim();
+
+    public List<String> readCommand() {
+        String command = scanner.nextLine().trim();
+        List<String> splitCommand = Arrays.stream(command.split(" ")).toList();
+        validateCommandSize(splitCommand);
+        return Arrays.stream(command.split(" ")).toList();
+    }
+
+    private void validateCommandSize(final List<String> splitInput) {
+        if (splitInput.size() != START_AND_END_COMMAND_SIZE && splitInput.size() != MOVE_COMMAND_SIZE) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
     }
 }
