a18bd133afdf1690e2c8400d28af59097d13c773
DobbyMac <kimdobby@g.skku.edu>
2024-03-26 15:43:02 +0900

    refactor(Color): Enum 클래스 비교 로직 중 equals 제거

--- src/main/java/model/piece/Color.java
+++ src/main/java/model/piece/Color.java
@@ -4,7 +4,7 @@ public enum Color {
     BLACK, WHITE, UN_COLORED;
 
     public Color next() {
-        if (this.equals(WHITE)) {
+        if (this == WHITE) {
             return BLACK;
         }
         return WHITE;
