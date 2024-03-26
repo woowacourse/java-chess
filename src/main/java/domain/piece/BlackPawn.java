87cd35f6cd00b5575edd470337dac903415d6af2
이상진 <priceelees@gmail.com>
2024-03-26 18:34:29 +0900

    refactor(BlackPawn): PawnBase 를 바탕으로 기능 재정의
    - 이동할 수 없는 방향에 대한 테스트 추가

--- src/main/java/domain/piece/BlackPawn.java
+++ src/main/java/domain/piece/BlackPawn.java
@@ -5,46 +5,26 @@ import domain.coordinate.Position;
 import domain.direction.DiagonalDirection;
 import domain.direction.Direction;
 import domain.direction.StraightDirection;
-import domain.piece.base.ChessPieceBase;
+import domain.piece.base.PawnBase;
 
-public class BlackPawn extends ChessPieceBase {
+public class BlackPawn extends PawnBase {
 
-    private static final int INITIAL_ROW = 1;
+    private static final int BLACK_PAWN_INITIAL_ROW = 1;
 
     public BlackPawn() {
         super(Color.BLACK);
     }
 
     @Override
-    public Direction getDirection(Coordinate start, Coordinate destination) {
-        int rowDifference = start.calculateRowDifference(destination);
-        int columnDifference = start.calculateColumnDifference(destination);
-        boolean isInitialPawn = isInitialPawn(start);
-
-        return getBlackPawnDirection(rowDifference, columnDifference, isInitialPawn);
-    }
-
-    private Direction getBlackPawnDirection(int rowDifference, int columnDifference, boolean isInitialPawn) {
-        if (rowDifference == 1 && Math.abs(columnDifference) == 1) {
-            return DiagonalDirection.getDirection(rowDifference, columnDifference);
-        }
-        if (canMoveDown(rowDifference, columnDifference, isInitialPawn)) {
-            return StraightDirection.DOWN;
-        }
-        throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
-    }
-
-    private boolean canMoveDown(int rowDifference, int columnDifference, boolean isInitialPawn) {
-        if (columnDifference != 0) {
-            return false;
-        }
-        if (rowDifference == 2) {
-            return isInitialPawn;
-        }
-        return rowDifference == 1;
-    }
-
     public boolean isInitialPawn(Coordinate start) {
-        return start.hasSameRowPosition(Position.of(INITIAL_ROW));
+        return start.hasSameRowPosition(Position.of(BLACK_PAWN_INITIAL_ROW));
+    }
+
+    @Override
+    public boolean canMove(Direction direction, int distance, boolean isInitialPawn) {
+        if (direction == StraightDirection.DOWN) {
+            return isMovableDistance(distance, isInitialPawn);
+        }
+        return direction == DiagonalDirection.DOWN_RIGHT || direction == DiagonalDirection.DOWN_LEFT;
     }
 }
--- src/test/java/domain/piece/BlackPawnTest.java
+++ src/test/java/domain/piece/BlackPawnTest.java
@@ -6,9 +6,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import domain.coordinate.Coordinate;
 import domain.direction.DiagonalDirection;
 import domain.direction.StraightDirection;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BlackPawnTest {
 
@@ -17,6 +21,19 @@ class BlackPawnTest {
     void cantMoveWhenNotInitialPawn() {
         Coordinate start = new Coordinate(3, 3);
         Coordinate destination = new Coordinate(5, 3);
+        BlackPawn blackPawn = new BlackPawn();
+
+        assertThatThrownBy(() -> blackPawn.getDirection(start, destination))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이동할 수 없는 위치입니다.");
+    }
+
+    @DisplayName("올바르지 않은 방향으로 이동할 수 없다.")
+    @ParameterizedTest(name = "{1}")
+    @MethodSource("provideBlackPawnInvalidDirection")
+    void notMovableDirection(Coordinate destination, String direction) {
+        Coordinate start = new Coordinate(2, 2);
+
         BlackPawn blackPawn = new BlackPawn();
 
         assertThatThrownBy(() -> blackPawn.getDirection(start, destination))
@@ -66,5 +83,17 @@ class BlackPawnTest {
 
             assertThat(blackPawn.getDirection(start, destination)).isEqualTo(DiagonalDirection.DOWN_RIGHT);
         }
+    }
+
+    // 흑색 폰이 (2, 2)에 있다고 가정하고, 이동할 수 없는 방향을 반환한다.
+    private static Stream<Arguments> provideBlackPawnInvalidDirection() {
+        return Stream.of(
+                Arguments.arguments(new Coordinate(1, 2), "위"),
+                Arguments.arguments(new Coordinate(4, 2), "시작 폰이 아닐 때 아래로 두 칸"),
+                Arguments.arguments(new Coordinate(2, 1), "왼쪽"),
+                Arguments.arguments(new Coordinate(2, 3), "오른쪽"),
+                Arguments.arguments(new Coordinate(1, 3), "오른쪽 위 대각선"),
+                Arguments.arguments(new Coordinate(1, 1), "왼쪽 위 대각선")
+        );
     }
 }
