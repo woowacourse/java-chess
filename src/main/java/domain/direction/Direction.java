95ddb1b4292ce01866201eabaea55be8ac260322
Eugene Jang <kyunellroll@gmail.com>
2024-03-26 21:44:21 +0900

    refactor(Direction): 클래스로 변경

--- src/main/java/domain/direction/Direction.java
+++ src/main/java/domain/direction/Direction.java
@@ -2,10 +2,18 @@ package domain.direction;
 
 import java.util.List;
 
-public record Direction(int rowDirection, int columnDirection) {
+public class Direction {
 
     private static final int ROW_DIRECTION = 0;
     private static final int COLUMN_DIRECTION = 1;
+
+    private final int getRowDirection;
+    private final int columnDirection;
+
+    private Direction(int getRowDirection, int columnDirection) {
+        this.getRowDirection = getRowDirection;
+        this.columnDirection = columnDirection;
+    }
 
     public static Direction from(List<Integer> direction) {
         return new Direction(direction.get(ROW_DIRECTION), direction.get(COLUMN_DIRECTION));
