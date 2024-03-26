a92ba082bcdacfeec8ee94a86b9dc30b7c4b535c
Zeus6768 <akaps6768@gmail.com>
2024-03-26 17:01:44 +0900

    style: final 키워드 추가

--- src/main/java/chess/view/dto/ChessboardDto.java
+++ src/main/java/chess/view/dto/ChessboardDto.java
@@ -13,13 +13,13 @@ public class ChessboardDto {
         this.chessboard = initialize(chessboard.getSquares());
     }
 
-    private List<List<String>> initialize(List<List<Square>> squares) {
+    private List<List<String>> initialize(final List<List<Square>> squares) {
         return squares.stream()
                 .map(this::initializeRow)
                 .toList();
     }
 
-    private List<String> initializeRow(List<Square> row) {
+    private List<String> initializeRow(final List<Square> row) {
         return row.stream()
                 .map(SquareCharacter::from)
                 .toList();
