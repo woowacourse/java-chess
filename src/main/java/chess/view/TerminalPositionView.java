0ae3645d37f7b5c375d3db3828afebfd9b1f5c14
nhlee98 <e5slnh9876@gmail.com>
2024-03-26 09:14:13 +0900

    fix: move 커맨드의 인자에 대한 검증 로직 추가

--- src/main/java/chess/view/TerminalPositionView.java
+++ src/main/java/chess/view/TerminalPositionView.java
@@ -18,6 +18,9 @@ public class TerminalPositionView {
     private static final int FILE_END_INDEX = 1;
     private static final int RANK_BEGIN_INDEX = 1;
     private static final int RANK_END_INDEX = 2;
+    private static final int POSITION_TEXT_LENGTH =
+            (FILE_END_INDEX - FILE_BEGIN_INDEX) + (RANK_END_INDEX - RANK_BEGIN_INDEX);
+    private static final int ARGUMENTS_SIZE = 2;
     private static final Map<String, File> FILE_TEXTS = Map.of(
             "a", File.A,
             "b", File.B,
@@ -40,10 +43,29 @@ public class TerminalPositionView {
     );
 
     public static TerminalPosition of(List<String> arguments) {
+        validate(arguments);
         Position start = toStartPosition(arguments.get(START_POSITION_INDEX));
         Position end = toEndPosition(arguments.get(END_POSITION_INDEX));
 
         return new TerminalPosition(start, end);
+    }
+
+    private static void validate(List<String> arguments) {
+        validateArgumentsSize(arguments);
+        validatePositionLength(arguments.get(START_POSITION_INDEX));
+        validatePositionLength(arguments.get(END_POSITION_INDEX));
+    }
+
+    private static void validateArgumentsSize(List<String> arguments) {
+        if (arguments.size() != ARGUMENTS_SIZE) {
+            throw new IllegalArgumentException("인자는 source와 target 위치 2개만 존재해야 합니다.");
+        }
+    }
+
+    private static void validatePositionLength(String positionText) {
+        if (positionText.length() != POSITION_TEXT_LENGTH) {
+            throw new IllegalArgumentException("source와 taget은 각각 2글자이어야 합니다.");
+        }
     }
 
     public static Position toStartPosition(String startPositionText) {
