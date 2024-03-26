d8e52b41b772f93b613ba1e7df0635cd3751570e
DobbyMac <kimdobby@g.skku.edu>
2024-03-26 17:38:47 +0900

    refactor(Position): Position 클래스 static final int 제거 및 메서드 예외 처리 수정

--- src/main/java/model/position/Position.java
+++ src/main/java/model/position/Position.java
@@ -1,31 +1,43 @@
 package model.position;
 
+import static util.FileConverter.A;
+import static util.FileConverter.H;
+import static util.RankConverter.EIGHT;
+import static util.RankConverter.ONE;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import model.direction.Direction;
 
 public class Position {
-    public static final int MIN_POSITION = 1;
-    public static final int MAX_POSITION = 8;
+    private static final Set<Position> cache = new HashSet<>();
     private final int file;
     private final int rank;
+
+    static {
+        for (int file = A.getValue(); file <= H.getValue(); file++) {
+            initRank(file);
+        }
+    }
 
     private Position(int file, int rank) {
         this.file = file;
         this.rank = rank;
     }
 
-    private static final Set<Position> cache = new HashSet<>();
-
-    static {
-        for (int file = MIN_POSITION; file <= MAX_POSITION; file++) {
-            initRank(file);
+    public static Position of(int file, int rank) {
+        if (A.getValue() <= file && file <= H.getValue() && ONE.getValue() <= rank && rank <= EIGHT.getValue()) {
+            return cache.stream()
+                    .filter(position -> position.file == file && position.rank == rank)
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("캐싱된 Position 확인이 필요합니다."));
         }
+        throw new IllegalArgumentException("존재하지 않는 Position 입니다.");
     }
 
     private static void initRank(int file) {
-        for (int rank = MIN_POSITION; rank <= MAX_POSITION; rank++) {
+        for (int rank = ONE.getValue(); rank <= EIGHT.getValue(); rank++) {
             cache.add(new Position(file, rank));
         }
     }
@@ -33,19 +45,12 @@ public class Position {
     public boolean isAvailablePosition(Direction direction) {
         int movedFile = file + direction.fileDifferential();
         int movedRank = rank + direction.rankDifferential();
-        return MIN_POSITION <= movedFile && movedFile <= MAX_POSITION && MIN_POSITION <= movedRank
-                && movedRank <= MAX_POSITION;
+        return A.getValue() <= movedFile && movedFile <= H.getValue() && ONE.getValue() <= movedRank
+                && movedRank <= EIGHT.getValue();
     }
 
     public Position getNextPosition(Direction direction) {
         return Position.of(file + direction.fileDifferential(), rank + direction.rankDifferential());
-    }
-
-    public static Position of(int file, int rank) {
-        return cache.stream()
-                .filter(position -> position.file == file && position.rank == rank)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Position 입니다."));
     }
 
     public int file() {
