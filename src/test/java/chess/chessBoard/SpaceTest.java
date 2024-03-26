41acf3c2b53d255176f7bd5ee5cc098b5077ec43
ehtjsv2 <ehtjsv2@naver.com>
2024-03-26 09:59:53 +0900

    test: 없어진 함수 테스트 중인 테스트 삭제

--- src/test/java/chess/chessBoard/SpaceTest.java
+++ src/test/java/chess/chessBoard/SpaceTest.java
@@ -33,7 +33,23 @@ class SpaceTest {
     }
 
     @Test
-    @DisplayName("피스를 움직이면 이동하려는 위치에 해당피스가 있다")
+    @DisplayName("피스가 없는 지 체크 할 수 있다.")
+    void should_check_piece_non_exist() {
+        Space space = new Space(new EmptyPiece(), new Position(File.a, Rank.ONE));
+
+        assertThat(space.hasPiece()).isFalse();
+    }
+
+    @Test
+    @DisplayName("피스가 있는 지 체크 할 수 있다.")
+    void should_check_piece_exist() {
+        Space space = new Space(new Pawn(Color.WHITE), new Position(File.a, Rank.ONE));
+
+        assertThat(space.hasPiece()).isTrue();
+    }
+
+    @Test
+    @DisplayName("피스를 움직이면 시작 위치의 피스는 비어진다.")
     void should_be_empty_starting_space_when_move() {
         Piece piece1 = new Pawn(Color.WHITE);
         Piece piece2 = new EmptyPiece();
@@ -42,7 +58,7 @@ class SpaceTest {
 
         space1.movePiece(space2);
 
-        assertThat(space1.doesNotHavePiece()).isTrue();
+        assertThat(space1.hasPiece()).isFalse();
     }
 
     @Test
@@ -56,14 +72,6 @@ class SpaceTest {
         assertThatThrownBy(() -> space1.movePiece(space2))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이동 규칙을 위반한 움직임입니다.");
-    }
-
-    @Test
-    @DisplayName("피스가 없는 지 체크 할 수 있다.")
-    void should_check_piece_exist() {
-        Space space = new Space(new EmptyPiece(), new Position(File.a, Rank.ONE));
-
-        assertThat(space.doesNotHavePiece()).isTrue();
     }
 
     @Test
