4c9e77cfb567611b9608113bc988bd80a92e4646
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-26 17:57:27 +0900

    refactor: 위치에 대한 테스트를 위해 픽스처 생성

--- src/test/java/chess/PositionFixture.java
+++ src/test/java/chess/PositionFixture.java
@@ -1,2 +1,22 @@
-package chess;public enum PositionFixture {
+package chess;
+
+import chess.domain.position.File;
+import chess.domain.position.Position;
+import chess.domain.position.Rank;
+
+public enum PositionFixture {
+    WHITE_PAWN_FIRST_MOVE_POSITION(new Position(File.A, Rank.TWO)),
+    BLACK_PAWN_FIRST_MOVE_POSITION(new Position(File.A, Rank.SEVEN)),
+    PAWN_NOT_FIRST_MOVE_POSITION(new Position(File.A, Rank.FIVE)),
+    ;
+
+    private final Position position;
+
+    PositionFixture(final Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
 }
