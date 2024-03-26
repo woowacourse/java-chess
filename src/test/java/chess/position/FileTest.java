2d1a2630d23cb265756baa36db2dffe3280bdac9
ehtjsv2 <ehtjsv2@naver.com>
2024-03-26 14:58:42 +0900

    test: file과 rank 루트구하는 테스트 추가

--- src/test/java/chess/position/FileTest.java
+++ src/test/java/chess/position/FileTest.java
@@ -1,8 +1,17 @@
 package chess.position;
 
+import static chess.domain.position.File.a;
+import static chess.domain.position.File.b;
+import static chess.domain.position.File.c;
+import static chess.domain.position.File.d;
+import static chess.domain.position.File.e;
+import static chess.domain.position.File.f;
+import static chess.domain.position.File.g;
+import static chess.domain.position.File.h;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.position.File;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +20,20 @@ class FileTest {
     @Test
     @DisplayName("FIle과 FIle간 거리를 계산할 수 있다.")
     void should_calculate_distance() {
-        File fileA = File.a;
-        File fileB = File.b;
+        File fileA = a;
+        File fileB = b;
 
         assertThat(fileA.calculateDifferenceTo(fileB)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("File과 FIle간 루트를 구할 수 있다")
+    void should_find_route() {
+        File from = a;
+        File to = h;
+
+        List<File> fileRouteToTargetFile = from.findFileRouteToTargetFile(to);
+
+        assertThat(fileRouteToTargetFile).containsExactly(b, c, d, e, f, g);
     }
 }
--- src/test/java/chess/position/RankTest.java
+++ src/test/java/chess/position/RankTest.java
@@ -1,8 +1,17 @@
 package chess.position;
 
+import static chess.domain.position.Rank.EIGHT;
+import static chess.domain.position.Rank.FIVE;
+import static chess.domain.position.Rank.FOUR;
+import static chess.domain.position.Rank.ONE;
+import static chess.domain.position.Rank.SEVEN;
+import static chess.domain.position.Rank.SIX;
+import static chess.domain.position.Rank.THREE;
+import static chess.domain.position.Rank.TWO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import chess.domain.position.Rank;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +20,20 @@ class RankTest {
     @Test
     @DisplayName("Rank와 Rank간 거리를 계산할 수 있다.")
     void should_calculate_distance() {
-        Rank rankOne = Rank.ONE;
+        Rank rankOne = ONE;
         Rank rankTwo = Rank.TWO;
 
         assertThat(rankOne.calculateDifferenceTo(rankTwo)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("rank와 rank간 루트를 구할 수 있다")
+    void should_find_route() {
+        Rank from = ONE;
+        Rank to = EIGHT;
+
+        List<Rank> rankRouteToTargetRank = from.findRankRouteToTargetRank(to);
+
+        assertThat(rankRouteToTargetRank).containsExactly(TWO, THREE, FOUR, FIVE, SIX, SEVEN);
     }
 }
