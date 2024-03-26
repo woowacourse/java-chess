a2aa693e0b8f40ff807aaf8b7d00c1e8e7ce2a7b
gyungchan Jo <namejgc@naver.com>
2024-03-26 19:59:06 +0900

    refactor: 중복으로 사용되는 에러 메시지 상수로 분리

--- src/main/java/chess/domain/game/state/End.java
+++ src/main/java/chess/domain/game/state/End.java
@@ -4,15 +4,16 @@ import chess.domain.board.Board;
 import chess.domain.point.Point;
 
 public class End implements State {
+    private static final String END_GAME_EXCEPTION_MESSAGE = "이미 게임이 종료되었습니다.";
 
     @Override
     public State start() {
-        throw new UnsupportedOperationException("이미 게임이 종료되었습니다.");
+        throw new UnsupportedOperationException(END_GAME_EXCEPTION_MESSAGE);
     }
 
     @Override
     public State finish() {
-        throw new UnsupportedOperationException("이미 게임이 종료되었습니다.");
+        throw new UnsupportedOperationException(END_GAME_EXCEPTION_MESSAGE);
     }
 
     @Override
@@ -22,6 +23,6 @@ public class End implements State {
 
     @Override
     public State move(final Board board, final Point departure, final Point destination) {
-        throw new UnsupportedOperationException("이미 게임이 종료되었습니다.");
+        throw new UnsupportedOperationException(END_GAME_EXCEPTION_MESSAGE);
     }
 }
