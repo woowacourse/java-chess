62cac80102c447a02452c89f1f124a2fcc0e3125
Minjoo <m0522inju@gmail.com>
2024-03-27 00:42:46 +0900

    feat(TurnDto): TurnDto 정적 팩토리 메서드 추가

--- src/main/java/chess/dto/TurnDto.java
+++ src/main/java/chess/dto/TurnDto.java
@@ -4,6 +4,10 @@ import chess.domain.color.Color;
 
 public record TurnDto(String color) {
 
+    public static TurnDto of(Color color) {
+        return new TurnDto(color.name());
+    }
+
     public Color getColor() {
         return Color.valueOf(color);
     }
