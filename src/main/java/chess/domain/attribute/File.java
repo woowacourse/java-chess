62519ae84df14a6dbcca2717a4e3e0d082e1e026
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-26 14:30:05 +0900

    refactor(PieceType): PieceType에서 기물의 위치를 초기화하지 않도록 수정

--- src/main/java/chess/domain/attribute/File.java
+++ src/main/java/chess/domain/attribute/File.java
@@ -46,59 +46,6 @@ public enum File {
                         "파일은 %d~%d 사이로 입력해주세요: %s".formatted(FILE_MIN, FILE_MAX, column)));
     }
 
-    public static File startKingFile() {
-        return E;
-    }
-
-    public static File startQueenFile() {
-        return D;
-    }
-
-    public static File startBishopFileOf(final int index) {
-        validateIndexExceptPawn(index);
-        if (index == 0) {
-            return C;
-        }
-        return F;
-    }
-
-    public static File startKnightFileOf(final int index) {
-        validateIndexExceptPawn(index);
-        if (index == 0) {
-            return B;
-        }
-        return G;
-    }
-
-    public static File startRookFileOf(final int index) {
-        validateIndexExceptPawn(index);
-        if (index == 0) {
-            return A;
-        }
-        return H;
-    }
-
-    private static void validateIndexExceptPawn(final int index) {
-        List<Integer> validIndices = List.of(0, 1);
-        if (!validIndices.contains(index)) {
-            throw new IllegalArgumentException("이미 존재하는 기물의 개수는 0 또는 1개입니다: %d".formatted(index));
-        }
-    }
-
-    public static File startPawnFileOf(final int index) {
-        validatePawnIndex(index);
-        return values()[index];
-    }
-
-    private static void validatePawnIndex(final int index) {
-        List<Integer> validIndices = IntStream.range(0, PAWN.getCount())
-                .boxed()
-                .toList();
-        if (!validIndices.contains(index)) {
-            throw new IllegalArgumentException("폰의 개수는 0~8개입니다: %d".formatted(index));
-        }
-    }
-
     public File left() {
         int leftOrdinal = this.ordinal() - 1;
         File[] files = File.values();
--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -1,39 +1,10 @@
 package chess.domain.piece;
 
-import static chess.domain.attribute.File.startBishopFileOf;
-import static chess.domain.attribute.File.startKingFile;
-import static chess.domain.attribute.File.startKnightFileOf;
-import static chess.domain.attribute.File.startPawnFileOf;
-import static chess.domain.attribute.File.startQueenFile;
-import static chess.domain.attribute.File.startRookFileOf;
-import static chess.domain.attribute.Rank.startPawnRankOf;
-import static chess.domain.attribute.Rank.startRankOf;
-
-import chess.domain.attribute.Color;
-import chess.domain.attribute.Square;
-import java.util.function.BiFunction;
-
 public enum PieceType {
-    KING(1, (color, index) -> Square.of(startKingFile(), startRankOf(color))),
-    QUEEN(1, (color, index) -> Square.of(startQueenFile(), startRankOf(color))),
-    BISHOP(2, (color, index) -> Square.of(startBishopFileOf(index), startRankOf(color))),
-    KNIGHT(2, (color, index) -> Square.of(startKnightFileOf(index), startRankOf(color))),
-    ROOK(2, (color, index) -> Square.of(startRookFileOf(index), startRankOf(color))),
-    PAWN(8, (color, index) -> Square.of(startPawnFileOf(index), startPawnRankOf(color)));
-
-    private final int count;
-    private final BiFunction<Color, Integer, Square> startSquare;
-
-    PieceType(final int count, final BiFunction<Color, Integer, Square> startSquare) {
-        this.count = count;
-        this.startSquare = startSquare;
-    }
-
-    public Square startSquareOf(final Color color, final int index) {
-        return startSquare.apply(color, index);
-    }
-
-    public int getCount() {
-        return count;
-    }
+    KING,
+    QUEEN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    PAWN;
 }
