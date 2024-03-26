ff624217c11eaa0722e9eb088b5567e10ad9e5f7
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-03-26 15:29:33 +0900

    refactor(Pawn): 함수 인덴트 줄이기

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -4,6 +4,8 @@ import chess.domain.attribute.Color;
 import chess.domain.attribute.Square;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Pawn extends Piece {
     public Pawn(final Color color, final Square square) {
@@ -19,102 +21,103 @@ public class Pawn extends Piece {
     }
 
     private Set<Square> findBlackPawnLegalMoves(Set<Piece> existPieces) {
-        Set<Square> squares = new HashSet<>();
-        Set<Square> capableOfAttack = findBlackCapableOfAttack(existPieces);
-        squares.addAll(capableOfAttack);
+        Set<Square> squares = new HashSet<>(findBlackCapableOfAttack(existPieces));
         Square currentSquare = currentSquare();
-
         if (currentSquare.isStartRankOfBlackPawn()) {
-            for (int i = 0; i < 2 && currentSquare.canMoveDown(); i++) {
-                currentSquare = currentSquare.moveDown();
-                if (isOccupied(existPieces, currentSquare)) {
-                    break;
-                }
-                squares.add(currentSquare);
-            }
+            addToMovableSquare(existPieces, currentSquare, squares);
             return squares;
         }
         if (currentSquare.canMoveDown()) {
             currentSquare = currentSquare.moveDown();
-            if (!isOccupied(existPieces, currentSquare)) {
-                squares.add(currentSquare);
-            }
+            addToMovableSquareIfBlank(existPieces, currentSquare, squares);
         }
         return squares;
     }
 
     private Set<Square> findBlackCapableOfAttack(Set<Piece> existPieces) {
+        Set<Square> leftDownAttack = findLeftDownAttack(existPieces);
+        Set<Square> rightDownAttack = rightDownAttack(existPieces);
+        return Stream.concat(leftDownAttack.stream(), rightDownAttack.stream())
+                .collect(Collectors.toSet());
+    }
+
+    private Set<Square> findLeftDownAttack(Set<Piece> existPieces) {
         Set<Square> squares = new HashSet<>();
         Square currentSquare = currentSquare();
         if (currentSquare.canMoveLeftDown()) {
             currentSquare = currentSquare.moveLeftDown();
-            if (isOccupied(existPieces, currentSquare)) {
-                Piece piece = getPiece(existPieces, currentSquare);
-                if (isEnemyOf(piece)) {
-                    squares.add(currentSquare);
-                }
-            }
-        }
-
-        currentSquare = currentSquare();
-        if (currentSquare.canMoveRightDown()) {
-            currentSquare = currentSquare.moveRightDown();
-            if (isOccupied(existPieces, currentSquare)) {
-                Piece piece = getPiece(existPieces, currentSquare);
-                if (isEnemyOf(piece)) {
-                    squares.add(currentSquare);
-                }
-            }
+            addMovableSquareIfOccupiedEnemy(existPieces, currentSquare, squares);
         }
         return squares;
     }
 
+    private Set<Square> rightDownAttack(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        if (currentSquare.canMoveRightDown()) {
+            currentSquare = currentSquare.moveRightDown();
+            addMovableSquareIfOccupiedEnemy(existPieces, currentSquare, squares);
+        }
+        return squares;
+    }
+
+    private void addToMovableSquare(Set<Piece> existPieces, Square currentSquare, Set<Square> squares) {
+        for (int i = 0; i < 2 && currentSquare.canMoveDown(); i++) {
+            currentSquare = currentSquare.moveDown();
+            if (isOccupied(existPieces, currentSquare)) {
+                break;
+            }
+            squares.add(currentSquare);
+        }
+    }
+
     private Set<Square> findWhiteCapableOfAttack(Set<Piece> existPieces) {
+        Set<Square> leftUpAttack = leftUpAttack(existPieces);
+        Set<Square> rightUpAttack = rightUpAttack(existPieces);
+        return Stream.concat(leftUpAttack.stream(), rightUpAttack.stream())
+                .collect(Collectors.toSet());
+    }
+
+    private Set<Square> rightUpAttack(Set<Piece> existPieces) {
+        Set<Square> squares = new HashSet<>();
+        Square currentSquare = currentSquare();
+        if (currentSquare.canMoveRightUp()) {
+            currentSquare = currentSquare.moveRightUp();
+            addMovableSquareIfOccupiedEnemy(existPieces, currentSquare, squares);
+        }
+        return squares;
+    }
+
+    private Set<Square> leftUpAttack(Set<Piece> existPieces) {
         Set<Square> squares = new HashSet<>();
         Square currentSquare = currentSquare();
         if (currentSquare.canMoveLeftUp()) {
             currentSquare = currentSquare.moveLeftUp();
-            if (isOccupied(existPieces, currentSquare)) {
-                Piece piece = getPiece(existPieces, currentSquare);
-                if (isEnemyOf(piece)) {
-                    squares.add(currentSquare);
-                }
-            }
-        }
-
-        currentSquare = currentSquare();
-        if (currentSquare.canMoveRightUp()) {
-            currentSquare = currentSquare.moveRightUp();
-            if (isOccupied(existPieces, currentSquare)) {
-                Piece piece = getPiece(existPieces, currentSquare);
-                if (isEnemyOf(piece)) {
-                    squares.add(currentSquare);
-                }
-            }
+            addMovableSquareIfOccupiedEnemy(existPieces, currentSquare, squares);
         }
         return squares;
     }
 
     private Set<Square> findWhitePawnLegalMoves(Set<Piece> existPieces) {
-        Set<Square> squares = new HashSet<>();
-        Set<Square> capableOfAttack = findWhiteCapableOfAttack(existPieces);
-        squares.addAll(capableOfAttack);
+        Set<Square> squares = new HashSet<>(findWhiteCapableOfAttack(existPieces));
         Square currentSquare = currentSquare();
         if (currentSquare.isStartRankOfWhitePawn()) {
-            for (int i = 0; i < 2 && currentSquare.canMoveUp(); i++) {
-                currentSquare = currentSquare.moveUp();
-                if (isOccupied(existPieces, currentSquare)) {
-                    break;
-                }
-                squares.add(currentSquare);
-            }
-            return squares;
+            return findStartWhitePawnLegalMoves(existPieces, currentSquare, squares);
         }
         if (currentSquare.canMoveUp()) {
             currentSquare = currentSquare.moveUp();
-            if (!isOccupied(existPieces, currentSquare)) {
-                squares.add(currentSquare);
+            addToMovableSquareIfBlank(existPieces, currentSquare, squares);
+        }
+        return squares;
+    }
+
+    private Set<Square> findStartWhitePawnLegalMoves(Set<Piece> existPieces, Square currentSquare, Set<Square> squares) {
+        for (int i = 0; i < 2 && currentSquare.canMoveUp(); i++) {
+            currentSquare = currentSquare.moveUp();
+            if (isOccupied(existPieces, currentSquare)) {
+                break;
             }
+            squares.add(currentSquare);
         }
         return squares;
     }
@@ -126,9 +129,33 @@ public class Pawn extends Piece {
                 .orElseThrow(() -> new IllegalArgumentException("해당 위치에 기물이 없습니다."));
     }
 
+    private void addMovableSquareIfOccupiedEnemy(Set<Piece> existPieces, Square currentSquare, Set<Square> squares) {
+        if (isOccupied(existPieces, currentSquare)) {
+            Piece piece = getPiece(existPieces, currentSquare);
+            addToMovableSquareIfEnemy(piece, squares, currentSquare);
+        }
+    }
+
+    private void addToMovableSquareIfEnemy(Piece piece, Set<Square> squares, Square currentSquare) {
+        if (isEnemyOf(piece)) {
+            squares.add(currentSquare);
+        }
+    }
+
+    private void addToMovableSquareIfBlank(Set<Piece> existPieces, Square currentSquare, Set<Square> squares) {
+        if (isBlank(existPieces, currentSquare)) {
+            squares.add(currentSquare);
+        }
+    }
+
     private boolean isOccupied(Set<Piece> entirePieces, Square currentSquare) {
         return entirePieces.stream()
                 .anyMatch(piece -> piece.currentSquare() == currentSquare);
+    }
+
+    private boolean isBlank(Set<Piece> entirePieces, Square currentSquare) {
+        return entirePieces.stream()
+                .noneMatch(piece -> piece.currentSquare() == currentSquare);
     }
 
 }
