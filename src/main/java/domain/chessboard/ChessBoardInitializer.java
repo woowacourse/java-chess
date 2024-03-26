0d96623f3571e591bf46db5e0bae97d52608da07
이상진 <priceelees@gmail.com>
2024-03-26 19:28:37 +0900

    refactor(ChessBoardInitializer): 수정된 ChessBoard 에 맞도록 보드 판을 초기화하는 방법 수정
    - 만들어진 보드 판의 구성에 대한 테스트 작성

--- src/main/java/domain/chessboard/ChessBoardInitializer.java
+++ src/main/java/domain/chessboard/ChessBoardInitializer.java
@@ -1,8 +1,8 @@
 package domain.chessboard;
 
-import domain.coordinate.Coordinate;
 import domain.piece.Bishop;
 import domain.piece.BlackPawn;
+import domain.piece.Blank;
 import domain.piece.Color;
 import domain.piece.King;
 import domain.piece.Knight;
@@ -10,34 +10,29 @@ import domain.piece.Queen;
 import domain.piece.Rook;
 import domain.piece.WhitePawn;
 import domain.piece.base.ChessPiece;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class ChessBoardInitializer {
 
-    private static final int BLACK_FIRST_RANK = 0;
-    private static final int BLACK_SECOND_RANK = 1;
-    private static final int WHITE_FIRST_RANK = 7;
-    private static final int WHITE_SECOND_RANK = 6;
     private static final int CHESS_BOARD_SIZE = 8;
 
-    public static Map<Coordinate, ChessPiece> createInitialBoard() {
-        Map<Coordinate, ChessPiece> board = new HashMap<>();
-
-        for (int i = 0; i < CHESS_BOARD_SIZE; i++) {
-            board.put(new Coordinate(BLACK_FIRST_RANK, i), createFirstRank(Color.BLACK).get(i));
-            board.put(new Coordinate(WHITE_FIRST_RANK, i), createFirstRank(Color.WHITE).get(i));
-        }
-        for (int i = 0; i < CHESS_BOARD_SIZE; i++) {
-            board.put(new Coordinate(BLACK_SECOND_RANK, i), new BlackPawn());
-            board.put(new Coordinate(WHITE_SECOND_RANK, i), new WhitePawn());
-        }
-        return board;
+    public static List<Row> createInitialBoard() {
+        return List.of(
+                createFirstRank(Color.BLACK),
+                createSamePiecesRow(getPawnByColor(Color.BLACK)),
+                createSamePiecesRow(Blank.getInstance()),
+                createSamePiecesRow(Blank.getInstance()),
+                createSamePiecesRow(Blank.getInstance()),
+                createSamePiecesRow(Blank.getInstance()),
+                createSamePiecesRow(getPawnByColor(Color.WHITE)),
+                createFirstRank(Color.WHITE)
+        );
     }
 
-    private static List<ChessPiece> createFirstRank(Color color) {
-        return List.of(
+    private static Row createFirstRank(Color color) {
+        return new Row(Arrays.asList(
                 new Rook(color),
                 new Knight(color),
                 new Bishop(color),
@@ -46,6 +41,21 @@ public class ChessBoardInitializer {
                 new Bishop(color),
                 new Knight(color),
                 new Rook(color)
-        );
+        ));
+    }
+
+    private static Row createSamePiecesRow(ChessPiece piece) {
+        List<ChessPiece> row = new ArrayList<>();
+        for (int i = 0; i < CHESS_BOARD_SIZE; i++) {
+            row.add(piece);
+        }
+        return new Row(row);
+    }
+
+    private static ChessPiece getPawnByColor(Color color) {
+        if (color == Color.WHITE) {
+            return new WhitePawn();
+        }
+        return new BlackPawn();
     }
 }
