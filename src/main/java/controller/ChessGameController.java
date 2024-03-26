761e78041c37b19cbc15a53fa7e7095b768e6d53
이상진 <priceelees@gmail.com>
2024-03-26 19:34:17 +0900

    refactor(ChessGameController): ChessGame 사용 및 말 이름을 직접 문자열로 변환하도록 수정

--- src/main/java/controller/ChessGameController.java
+++ src/main/java/controller/ChessGameController.java
@@ -1,12 +1,17 @@
 package controller;
 
-import domain.chessboard.ChessBoard;
+import domain.chessboard.Row;
 import domain.coordinate.Coordinate;
+import domain.game.ChessGame;
+import domain.piece.Color;
+import domain.piece.base.ChessPiece;
+import java.util.List;
 import view.InputView;
 import view.OutputView;
 import view.dto.Commands;
 import view.dto.CoordinateRequest;
 import view.util.Command;
+import view.util.PieceTranslator;
 
 public class ChessGameController {
 
@@ -21,41 +26,41 @@ public class ChessGameController {
     public void run() {
         outputView.printGameGuide();
 
-        ChessBoard chessBoard = initializeBoard();
-        outputView.printBoard(chessBoard.getBoard());
+        ChessGame chessGame = initializeBoard();
+        printCurrentBoardStatus(chessGame);
 
-        startGame(chessBoard);
+        start(chessGame);
     }
 
-    private ChessBoard initializeBoard() {
+    private ChessGame initializeBoard() {
         Commands commands = inputView.receiveCommands();
 
         if (Command.START != commands.command()) {
             throw new IllegalArgumentException("게임을 먼저 시작하세요.");
         }
-        return new ChessBoard();
+        return new ChessGame();
     }
 
-    private void startGame(ChessBoard chessBoard) {
+    private void start(ChessGame chessGame) {
         boolean isPlaying;
 
         do {
-            isPlaying = playGame(chessBoard);
+            isPlaying = playGame(chessGame);
         } while (isPlaying);
     }
 
-    private boolean playGame(ChessBoard chessBoard) {
+    private boolean playGame(ChessGame chessGame) {
         Commands commands = inputView.receiveCommands();
 
         if (isCommandMove(commands)) {
-            move(chessBoard, commands);
-            outputView.printBoard(chessBoard.getBoard());
+            move(chessGame, commands);
+            printCurrentBoardStatus(chessGame);
             return true;
         }
         return false;
     }
 
-    private static boolean isCommandMove(Commands commands) {
+    private boolean isCommandMove(Commands commands) {
         if (commands.command() == Command.END) {
             return false;
         }
@@ -65,14 +70,37 @@ public class ChessGameController {
         return true;
     }
 
-    private void move(ChessBoard chessBoard, Commands commands) {
+    private void move(ChessGame chessGame, Commands commands) {
         Coordinate start = createCoordinate(commands.startCoordinate());
         Coordinate destination = createCoordinate(commands.destinationCoordinate());
 
-        chessBoard.playTurn(start, destination);
+        chessGame.startTurn(start, destination);
     }
 
     private Coordinate createCoordinate(CoordinateRequest coordinateRequest) {
         return new Coordinate(coordinateRequest.row(), coordinateRequest.column());
+    }
+
+    private void printCurrentBoardStatus(ChessGame chessGame) {
+        List<List<String>> boardStatus = chessGame.getCurrentBoard().stream()
+                .map(this::getRowStatus)
+                .toList();
+        outputView.printBoard(boardStatus);
+    }
+
+    private List<String> getRowStatus(Row row) {
+        return row.getPieces().stream()
+                .map(this::getPieceName)
+                .toList();
+    }
+
+    private String getPieceName(ChessPiece chessPiece) {
+        PieceTranslator pieceTranslator = PieceTranslator.from(chessPiece);
+        String name = pieceTranslator.getName();
+
+        if (chessPiece.hasSameColor(Color.WHITE)) {
+            return name;
+        }
+        return name.toUpperCase();
     }
 }
