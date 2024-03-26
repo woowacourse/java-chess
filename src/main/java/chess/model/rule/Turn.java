5fa8f800b4b7c2a463c3525c0faa51c312317512
jongmee <ra123221@gmail.com>
2024-03-26 18:43:25 +0900

    feat(Turn): 게임 순서 도메인 클래스 생성, 캐싱한다

---
+++ src/main/java/chess/model/rule/Turn.java
@@ -0,0 +1,28 @@
+package chess.model.rule;
+
+import chess.model.piece.Side;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+public class Turn {
+    private static final Map<Side, Turn> CACHE = Arrays.stream(Side.values())
+            .collect(toMap(identity(), Turn::new));
+
+    private final Side side;
+
+    private Turn(Side side) {
+        this.side = side;
+    }
+
+    public static Turn from(Side side) {
+        return CACHE.get(side);
+    }
+
+    public Turn getNextTurn() {
+        return Turn.from(side.getOppositeSide());
+    }
+}
---
+++ src/test/java/chess/model/rule/TurnTest.java
@@ -0,0 +1,25 @@
+package chess.model.rule;
+
+import chess.model.piece.Side;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TurnTest {
+
+    @Test
+    @DisplayName("캐싱되어 항상 동일한 객체를 반환한다.")
+    void from() {
+        // given
+        Turn difference = Turn.from(Side.WHITE);
+        Turn otherDifference = Turn.from(Side.WHITE);
+
+        // when
+        boolean result = difference.equals(otherDifference);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+}
