e01f0c2a4c2c61e826abc354ac900b81c1a12ff5
yoonseo choi <choiyoonseo02@gmail.com>
2024-03-27 14:22:48 +0900

    test: Lettering 테스트 추가

---
+++ src/test/java/chess/domain/chessboard/LetteringTest.java
@@ -0,0 +1,24 @@
+package chess.domain.chessboard;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class LetteringTest {
+
+    @Test
+    void 현재_레터링이_제일_오른쪽에_위치해_있으면_다음_레터링이_없어_예외를_발생시킨다() {
+        assertThatThrownBy(() ->
+                Lettering.findNextLettering(Lettering.H)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 현재_레터링이_제일_왼쪽에_위치해_있으면_이전_레터링이_없어_예외를_발생시킨다() {
+        assertThatThrownBy(() ->
+                Lettering.findPreviousLettering(Lettering.A)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
