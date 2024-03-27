92d98fde273f47d8b877d4f51c4b11596b28eacb
tsulocalize <tsulocalize@gmail.com>
2024-03-27 10:52:51 +0900

    feat: 가이드 메시지 Command 추가

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -18,7 +18,9 @@ public class OutputView {
                 """
                         > 게임 시작 : start
                         > 게임 종료 : end
-                        > 게임 이동 : move source위치 target위치 - 예. move b2 b3""");
+                        > 게임 이동 : move source위치 target위치 - 예. move b2 b3
+                        > 점수 확인 : status
+                        """);
     }
 
     public void printChessBoard(ChessGame chessGame) {
