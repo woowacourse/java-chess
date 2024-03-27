6aee1c4e7cc74fc8f59c40b119f4bbf7844fd74e
DobbyMac <kimdobby@g.skku.edu>
2024-03-27 15:11:00 +0900

    feat(Route): 특정 Route의 반대 방향으로 향하는 Route를 구하는 기능 구현

--- src/main/java/model/position/Route.java
+++ src/main/java/model/position/Route.java
@@ -1,6 +1,7 @@
 package model.position;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import model.direction.Direction;
@@ -14,21 +15,33 @@ public class Route {
         this.positions = new ArrayList<>(positions);
     }
 
-    public List<Position> getPositions() {
-        return List.copyOf(positions);
-    }
-
     public boolean contains(Position position) {
         return positions.contains(position);
     }
 
     public Route directRouteTo(Position destination) {
-        List<Position> subList = positions.subList(0, positions.indexOf(destination) + 1);
+        List<Position> subList = new ArrayList<>(positions.subList(0, positions.indexOf(destination) + 1));
         return new Route(direction, subList);
     }
 
-    public Direction getDirection(){
+    public Route reverseRouteTowardSource() {
+        List<Position> reversedPositions = new ArrayList<>(positions);
+        Direction oppositeDirection = direction.toOppositeDirection();
+        Collections.reverse(reversedPositions);
+        reversedPositions.remove(0);
+        Position beforeLastPosition = positions.get(0);
+        if (beforeLastPosition.hasAvailableNextPostitionToDirection(oppositeDirection)) {
+            reversedPositions.add(beforeLastPosition.getNextPosition(oppositeDirection));
+        }
+        return new Route(oppositeDirection, reversedPositions);
+    }
+
+    public Direction getDirection() {
         return direction;
+    }
+
+    public List<Position> getPositions() {
+        return List.copyOf(positions);
     }
 
     @Override
--- src/test/java/model/position/RouteTest.java
+++ src/test/java/model/position/RouteTest.java
@@ -1,8 +1,11 @@
 package model.position;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import model.direction.Direction;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +14,7 @@ import org.junit.jupiter.api.Test;
 class RouteTest {
 
     @Test
-    @DisplayName("출발 지점부터 도착 지점 이전까지의 Route를 반환한다.")
+    @DisplayName("출발 지점부터 도착 지점까지의 Route를 반환한다.")
     void subRoute_ShouldReturnSubListOfHoleRoute_WhenGivenDestination() {
         // When
         List<Position> wholePositions = new ArrayList<>();
@@ -29,4 +32,61 @@ class RouteTest {
 
         assertEquals(subPositions, subRoute.getPositions());
     }
+
+    @Test
+    @DisplayName("주어진 루트를 뒤집어야 한다")
+    void reverseRouteTowardSource_Should_ReverseBasicRoute_When_RouteIsGiven() {
+        // Given
+        List<Position> positions = Arrays.asList(Position.of(2, 3), Position.of(3, 4), Position.of(4, 5));
+        Direction direction = Direction.NE;
+        List<Position> reversedPositions = Arrays.asList(Position.of(3, 4), Position.of(2, 3), Position.of(1, 2));
+        Direction reversedDirection = direction.toOppositeDirection();
+        Route expectedReversedRoute = new Route(reversedDirection, reversedPositions);
+
+        // When
+        Route route = new Route(direction, positions);
+        Route reversedRoute = route.reverseRouteTowardSource();
+
+        // Then
+        assertEquals(expectedReversedRoute, reversedRoute);
+    }
+
+    @Test
+    @DisplayName("체스판 경계의 포지션을 포함하는 루트를 뒤집어야 한다")
+    void reverseRouteTowardSource_Should_HandleEdgePositions_When_RouteIncludesBoardEdge() {
+        // Given
+        List<Position> positions = Arrays.asList(Position.of(1, 2), Position.of(1, 3));
+        Route route = new Route(Direction.N, positions);
+
+        // When & Then
+        assertDoesNotThrow(route::reverseRouteTowardSource);
+    }
+
+    @Test
+    @DisplayName("Single Shift로 이루어진 루트를 뒤집을 수 있어야 한다.")
+    void reverseRouteTowardSource_Should_Fail_When_RouteHasSinglePosition() {
+        // Given
+        List<Position> positions = Arrays.asList(Position.of(4, 4));
+        Direction direction = Direction.E;
+        Direction reversedDirection = direction.toOppositeDirection();
+        List<Position> reversedPositions = Arrays.asList(Position.of(3, 4));
+
+        List<Position> positions2 = Arrays.asList(Position.of(4, 4));
+        Direction direction2 = Direction.NNE;
+        Direction reversedDirection2 = direction2.toOppositeDirection();
+        List<Position> reversedPositions2 = Arrays.asList(Position.of(3, 2));
+
+        Route route = new Route(direction, positions);
+        Route route2 = new Route(direction2, positions2);
+
+        Route reveredRoute1 = new Route(reversedDirection, reversedPositions);
+        Route reveredRoute2 = new Route(reversedDirection2, reversedPositions2);
+        // When & Then
+        assertAll(() -> {
+            assertEquals(reveredRoute1, route.reverseRouteTowardSource(), "E 반대 방향으로 한칸 움직이는 루트를 구하지 못했습니다.");
+            assertEquals(reveredRoute2, route2.reverseRouteTowardSource(), "NNE 반대 방향으로 한칸 움직이는 루트를 구하지 못했습니다.");
+
+        });
+    }
+
 }
