7b0aead85b57773254c541cebb548e97583fea4e
Eugene Jang <kyunellroll@gmail.com>
2024-03-27 21:10:49 +0900

    refactor(Direction): 잘못된 방향 설정 수정

--- src/main/java/domain/direction/Direction.java
+++ src/main/java/domain/direction/Direction.java
@@ -8,8 +8,8 @@ public enum Direction {
 
     UP(1, 0),
     DOWN(-1, 0),
-    LEFT(0, 1),
-    RIGHT(0, -1),
+    LEFT(0, -1),
+    RIGHT(0, 1),
 
     UP_RIGHT_DIAGONAL(1, 1),
     UP_LEFT_DIAGONAL(1, -1),
@@ -17,13 +17,13 @@ public enum Direction {
     DOWN_RIGHT_DIAGONAL(-1, 1),
 
     UP_UP_RIGHT_DIAGONAL(2, 1),
-    UP_UP_LEFT_DIAGONAL(2, -1),
+    UP_UP_LEFT_DIAGONAL(-2, -1),
     LEFT_LEFT_UP_DIAGONAL(1, -2),
-    LEFT_LEFT_DOWN_DIAGONAL(1, -2),
-    RIGHT_RIGHT_UP_DIAGONAL(-1, -2),
-    RIGHT_RIGHT_DOWN_DIAGONAL(-1, -2),
+    LEFT_LEFT_DOWN_DIAGONAL(-1, -2),
+    RIGHT_RIGHT_UP_DIAGONAL(1, 2),
+    RIGHT_RIGHT_DOWN_DIAGONAL(-1, 2),
     DOWN_DOWN_RIGHT_DIAGONAL(-2, 1),
-    DOWN_DOWN_LEFT_DIAGONAL(-2, -1);
+    DOWN_DOWN_LEFT_DIAGONAL(2, -1);
 
     private final int rowDirection;
     private final int columnDirection;
