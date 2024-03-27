2ba0d88f8da63a23232848cfd42bb7a04ec6b446
hyxrxn <gpfls981220@gmail.com>
2024-03-27 22:29:50 +0900

    refactor(PieceTypeTest): import 변경

--- src/test/java/chess/domain/piece/PieceTypeTest.java
+++ src/test/java/chess/domain/piece/PieceTypeTest.java
@@ -1,7 +1,7 @@
 package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
