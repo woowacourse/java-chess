1e88cccd9e290d616fa1d54ff060fd81d0d68ad4
yoonjuho <dbswn990@naver.com>
2024-03-27 13:43:46 +0900

    refactor(ShiftPattern): final 처리

--- src/main/java/model/direction/ShiftPattern.java
+++ src/main/java/model/direction/ShiftPattern.java
@@ -3,7 +3,6 @@ package model.direction;
 import java.util.List;
 
 import static model.direction.Direction.*;
-import static model.direction.Direction.NW;
 
 public enum ShiftPattern {
     KING(List.of(N, NE, E, SE, S, SW, W, NW)),
@@ -12,9 +11,8 @@ public enum ShiftPattern {
     WHITE_PAWN(List.of(N, NW, NE)),
     BLACK_PAWN(List.of(S, SE, SW)),
     QUEEN(List.of(W, E, S, N, NW, SW, NE, SE)),
-    ROOK(List.of(W, E, N, S))
-    ;
-    private List<Direction> directions;
+    ROOK(List.of(W, E, N, S));
+    private final List<Direction> directions;
 
     ShiftPattern(List<Direction> directions) {
         this.directions = directions;
