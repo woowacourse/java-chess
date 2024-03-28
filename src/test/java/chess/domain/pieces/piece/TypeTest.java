556c4ef12dff781494a9592b89edb071ae326c86
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-29 00:55:57 +0900

    test: 모든 기물 점수 검증

--- src/test/java/chess/domain/pieces/piece/TypeTest.java
+++ src/test/java/chess/domain/pieces/piece/TypeTest.java
@@ -1,6 +1,7 @@
 package chess.domain.pieces.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,14 @@ class TypeTest {
     @DisplayName("기물별 점수를 검증한다.")
     @Test
     void score() {
-        //given
-        assertThat(Type.KING.getScore().getValue()).isEqualTo(0);
-
-        //when
-
-        //then
-
+        //given & when & then
+        assertAll(
+                () -> assertThat(Type.KING.getScore().getValue()).isEqualTo(0),
+                () -> assertThat(Type.QUEEN.getScore().getValue()).isEqualTo(9),
+                () -> assertThat(Type.BISHOP.getScore().getValue()).isEqualTo(3),
+                () -> assertThat(Type.KNIGHT.getScore().getValue()).isEqualTo(2.5),
+                () -> assertThat(Type.ROOK.getScore().getValue()).isEqualTo(5),
+                () -> assertThat(Type.PAWN.getScore().getValue()).isEqualTo(1)
+        );
     }
 }
