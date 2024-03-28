f098b80db7aea761da398df64964a60898a1d214
juha <khabh@naver.com>
2024-03-28 15:34:12 +0900

    test: import 순서 재정렬

--- src/test/java/chess/model/position/MovementTest.java
+++ src/test/java/chess/model/position/MovementTest.java
@@ -1,11 +1,11 @@
 package chess.model.position;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MovementTest {
     @Test
