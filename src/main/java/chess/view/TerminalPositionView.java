b6597dac7605e908c3bdb50a2b98bef0c46079a3
nhlee98 <e5slnh9876@gmail.com>
2024-03-28 16:19:31 +0900

    refactor: File과 Rank에 대한 뷰 로직 분리

--- src/main/java/chess/view/TerminalPositionView.java
+++ src/main/java/chess/view/TerminalPositionView.java
@@ -1,12 +1,9 @@
 package chess.view;
 
-import chess.domain.position.File;
 import chess.domain.position.Position;
-import chess.domain.position.Rank;
 import chess.domain.position.TerminalPosition;
 
 import java.util.List;
-import java.util.Map;
 
 public class TerminalPositionView {
     private TerminalPositionView() {
@@ -21,26 +18,6 @@ public class TerminalPositionView {
     private static final int POSITION_TEXT_LENGTH =
             (FILE_END_INDEX - FILE_BEGIN_INDEX) + (RANK_END_INDEX - RANK_BEGIN_INDEX);
     private static final int ARGUMENTS_SIZE = 2;
-    private static final Map<String, File> FILE_TEXTS = Map.of(
-            "a", File.A,
-            "b", File.B,
-            "c", File.C,
-            "d", File.D,
-            "e", File.E,
-            "f", File.F,
-            "g", File.G,
-            "h", File.H
-    );
-    private static final Map<String, Rank> RANK_TEXTS = Map.of(
-            "1", Rank.FIRST,
-            "2", Rank.SECOND,
-            "3", Rank.THIRD,
-            "4", Rank.FOURTH,
-            "5", Rank.FIFTH,
-            "6", Rank.SIXTH,
-            "7", Rank.SEVENTH,
-            "8", Rank.EIGHTH
-    );
 
     public static TerminalPosition of(List<String> arguments) {
         validate(arguments);
@@ -70,13 +47,13 @@ public class TerminalPositionView {
 
     public static Position toStartPosition(String startPositionText) {
         return new Position(
-                FILE_TEXTS.get(startPositionText.substring(FILE_BEGIN_INDEX, FILE_END_INDEX)),
-                RANK_TEXTS.get(startPositionText.substring(RANK_BEGIN_INDEX, RANK_END_INDEX)));
+                FileView.from(startPositionText.substring(FILE_BEGIN_INDEX, FILE_END_INDEX)),
+                RankView.from(startPositionText.substring(RANK_BEGIN_INDEX, RANK_END_INDEX)));
     }
 
     public static Position toEndPosition(String endPositionText) {
         return new Position(
-                FILE_TEXTS.get(endPositionText.substring(FILE_BEGIN_INDEX, FILE_END_INDEX)),
-                RANK_TEXTS.get(endPositionText.substring(RANK_BEGIN_INDEX, RANK_END_INDEX)));
+                FileView.from(endPositionText.substring(FILE_BEGIN_INDEX, FILE_END_INDEX)),
+                RankView.from(endPositionText.substring(RANK_BEGIN_INDEX, RANK_END_INDEX)));
     }
 }
