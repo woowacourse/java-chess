2f81746bee6078fd4bc4cb0789623d2b8119aa5f
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 16:30:15 +0900

    test(Rook): 장애물이 존재하는 경우 테스트 케이스 추가

--- src/test/java/chess/piece/RookTest.java
+++ src/test/java/chess/piece/RookTest.java
@@ -48,4 +48,69 @@ class RookTest {
         // then
         assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
     }
+
+    @DisplayName("이동경로에 같은 팀 기물이 존재하는 경우, 해당 기물들을 마주하기 전까지의 위치로 움직일 수 있다.")
+    @Test
+    void existSameTeamHurdle() {
+        // given
+        Position rookPosition = new Position(Row.FOUR, Column.D);
+        Rook rook = new Rook(Color.BLACK);
+        Map<Position, ChessPiece> positions = Map.of(
+                rookPosition, rook,
+                new Position(Row.SIX, Column.D), new Bishop(Color.BLACK),
+                new Position(Row.FOUR, Column.H), new Bishop(Color.BLACK),
+                new Position(Row.THREE, Column.D), new Knight(Color.BLACK)
+        );
+        List<Position> expected = List.of(
+                new Position(Row.FIVE, Column.D),
+                new Position(Row.FOUR, Column.A),
+                new Position(Row.FOUR, Column.B),
+                new Position(Row.FOUR, Column.C),
+                new Position(Row.FOUR, Column.E),
+                new Position(Row.FOUR, Column.F),
+                new Position(Row.FOUR, Column.G)
+        );
+
+        // when
+        List<Position> destinations = rook.getAvailableDestinations(
+                new Position(Row.FOUR, Column.D), positions
+        );
+
+        // then
+        assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @DisplayName("이동경로에 다른 팀 기물이 존재하는 경우, 해당 기물들의 위치로 움직일 수 있다.")
+    @Test
+    void existOtherTeamHurdle() {
+        // given
+        Position rookPosition = new Position(Row.FOUR, Column.D);
+        Rook rook = new Rook(Color.BLACK);
+        Map<Position, ChessPiece> positions = Map.of(
+                rookPosition, rook,
+                new Position(Row.SIX, Column.D), new Bishop(Color.WHITE),
+                new Position(Row.FOUR, Column.H), new Bishop(Color.WHITE),
+                new Position(Row.THREE, Column.D), new Knight(Color.WHITE)
+        );
+        List<Position> expected = List.of(
+                new Position(Row.SIX, Column.D),
+                new Position(Row.FIVE, Column.D),
+                new Position(Row.THREE, Column.D),
+                new Position(Row.FOUR, Column.A),
+                new Position(Row.FOUR, Column.B),
+                new Position(Row.FOUR, Column.C),
+                new Position(Row.FOUR, Column.E),
+                new Position(Row.FOUR, Column.F),
+                new Position(Row.FOUR, Column.G),
+                new Position(Row.FOUR, Column.H)
+        );
+
+        // when
+        List<Position> destinations = rook.getAvailableDestinations(
+                new Position(Row.FOUR, Column.D), positions
+        );
+
+        // then
+        assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
+    }
 }
