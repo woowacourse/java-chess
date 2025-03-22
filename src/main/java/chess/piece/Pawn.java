fcd814032a44525a42ea30f59745ac03db5a1a01
kjyyjk <rlawnsdud920@gmail.com>
2025-03-22 15:08:26 +0900

    feat: 처음 움직이는 Pawn만 앞으로 두 칸까지 이동할 수 있다

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -1,5 +1,11 @@
 package chess.piece;
 
+import static chess.Movement.LEFT_UP;
+import static chess.Movement.RIGHT_UP;
+import static chess.Movement.UP;
+import static chess.Movement.UP_UP;
+import static chess.Row.TWO;
+
 import chess.Color;
 import chess.Movement;
 import chess.Position;
@@ -11,10 +17,10 @@ public class Pawn extends Piece {
     public static List<List<Movement>> allMovements = new ArrayList<>();
 
     static {
-        allMovements.add(List.of(Movement.UP));
-        allMovements.add(List.of(Movement.UP_UP));
-        allMovements.add(List.of(Movement.LEFT_UP));
-        allMovements.add(List.of(Movement.RIGHT_UP));
+        allMovements.add(List.of(UP));
+        allMovements.add(List.of(UP_UP));
+        allMovements.add(List.of(LEFT_UP));
+        allMovements.add(List.of(RIGHT_UP));
     }
 
     public Pawn(final Color color) {
@@ -23,11 +29,28 @@ public class Pawn extends Piece {
 
     @Override
     protected List<Position> findRoot(Position start, Position goal) {
-        return List.of();
+        for (List<Movement> movements : allMovements) {
+            List<Position> root = new ArrayList<>();
+            Position now = start;
+            root.add(now);
+            for (Movement movement : movements) {
+                if (now.canNotMove(movement)) {
+                    break;
+                }
+                now = now.move(movement);
+                root.add(now);
+            }
+            if (now.equals(goal)) {
+                if (start.row() != TWO && movements.contains(UP_UP)) {
+                    throw new IllegalArgumentException("처음 움직이는 폰만 앞으로 두 칸을 이동할 수 있습니다.");
+                }
+                return root;
+            }
+        }
+        throw new IllegalArgumentException("경로가 존재하지 않습니다.");
     }
 
     @Override
     protected void validateMiddlePath(Board board, List<Position> root) {
-
     }
 }
