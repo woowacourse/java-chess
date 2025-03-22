9f608fdf9b0016b2458a3aedac0c726d5031b0b9
Dompoo <dlckdrms0517@gmail.com>
2025-03-22 14:37:02 +0900

    feat: 퀸 움직이는 기능 개발

--- src/main/java/chess/piece/Queen.java
+++ src/main/java/chess/piece/Queen.java
@@ -2,6 +2,7 @@ package chess.piece;
 
 import chess.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Queen extends ChessPiece {
@@ -12,6 +13,35 @@ public class Queen extends ChessPiece {
 
     @Override
     protected List<Position> calculateCanMovePositions() {
-        return List.of();
+        final List<Position> positions = new ArrayList<>();
+        for (int i = 1; i <= 7; i++) {
+            if (position.canMoveVertical(i)) {
+                positions.add(position.moveVertical(i));
+            }
+            if (position.canMoveHorizontal(i)) {
+                positions.add(position.moveHorizontal(i));
+            }
+
+            if (position.canMoveVertical(-i)) {
+                positions.add(position.moveVertical(-i));
+            }
+            if (position.canMoveHorizontal(-i)) {
+                positions.add(position.moveHorizontal(-i));
+            }
+            if (position.canMoveDiagonal(i, i)) {
+                positions.add(position.moveDiagonal(i, i));
+            }
+            if (position.canMoveDiagonal(i, -i)) {
+                positions.add(position.moveDiagonal(i, -i));
+            }
+            if (position.canMoveDiagonal(-i, i)) {
+                positions.add(position.moveDiagonal(-i, i));
+            }
+            if (position.canMoveDiagonal(-i, -i)) {
+                positions.add(position.moveDiagonal(-i, -i));
+            }
+        }
+
+        return positions;
     }
 }
---
+++ src/test/java/chess/piece/QueenTest.java
@@ -0,0 +1,83 @@
+package chess.piece;
+
+import chess.Position;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static chess.Fixtures.*;
+import static org.assertj.core.api.Assertions.*;
+
+class QueenTest {
+
+    @ParameterizedTest
+    @MethodSource("provideValidPositions")
+    void 퀸은_십자방향과_대각선으로_움직일_수_있다(Position validPosition) {
+        // given
+        final Queen queen = new Queen(D5);
+
+        // expected
+        assertThatCode(() -> queen.move(validPosition))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidPositions")
+    void 퀸은_그_외의_위치로는_움직일_수_없다(Position invalidPosition) {
+        // given
+        final Queen queen = new Queen(D5);
+
+        // expected
+        assertThatThrownBy(() -> queen.move(invalidPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("움직일 수 없는 위치입니다.");
+    }
+
+    public static Stream<Arguments> provideValidPositions() {
+        return Stream.of(
+                Arguments.of(C4),
+                Arguments.of(B3),
+                Arguments.of(A2),
+                Arguments.of(C6),
+                Arguments.of(B7),
+                Arguments.of(A8),
+                Arguments.of(E4),
+                Arguments.of(F3),
+                Arguments.of(G2),
+                Arguments.of(H1),
+                Arguments.of(E6),
+                Arguments.of(F7),
+                Arguments.of(G8),
+                Arguments.of(D1),
+                Arguments.of(D2),
+                Arguments.of(D3),
+                Arguments.of(D4),
+                Arguments.of(D6),
+                Arguments.of(D7),
+                Arguments.of(D8),
+                Arguments.of(A5),
+                Arguments.of(B5),
+                Arguments.of(C5),
+                Arguments.of(E5),
+                Arguments.of(F5),
+                Arguments.of(G5),
+                Arguments.of(H5)
+        );
+    }
+
+    public static Stream<Arguments> provideInvalidPositions() {
+        return Stream.of(
+                Arguments.of(F4),
+                Arguments.of(F6),
+                Arguments.of(E3),
+                Arguments.of(E7),
+                Arguments.of(B4),
+                Arguments.of(B6),
+                Arguments.of(C3),
+                Arguments.of(C7)
+        );
+    }
+
+}
