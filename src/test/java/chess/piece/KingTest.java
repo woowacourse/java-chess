7d8696fcd8b16f77c765331a309fb5ab0c106039
ykmxxi <ykmxxi97@gmail.com>
2025-03-22 14:03:34 +0900

    refactor: 사용하지 않는 import 제거

--- src/test/java/chess/piece/KingTest.java
+++ src/test/java/chess/piece/KingTest.java
@@ -4,10 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import chess.Column;
