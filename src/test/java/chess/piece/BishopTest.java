f0611a727b43090612a0f4913b074585aada5e49
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 14:53:19 +0900

    fix: 비숍의 장애물 존재 테스트 수정 - 같은 팀의 기물이 존재하는 경우와 다른 팀으 기물이 존재하는 경우 분리

--- src/test/java/chess/piece/BishopTest.java
+++ src/test/java/chess/piece/BishopTest.java
@@ -44,12 +44,12 @@ class BishopTest {
         assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
     }
 
-    @DisplayName("장애물이 존재하는 경우, 보드를 벗어나지 않는 영역 내에서 장애물을 마주하기 전까지의 모든 대각선 방향으로 움직일 수 있다.")
+    @DisplayName("이동경로에 같은 팀 기물이 존재하는 경우, 해당 기물들을 마주하기 전까지의 위치로 움직일 수 있다.")
     @Test
-    void existHurdle() {
+    void existSameTeamHurdle() {
         // given
         Map<Position, ChessPiece> positions = Map.of(
-                new Position(Row.SIX, Column.F), new Bishop(Color.WHITE),
+                new Position(Row.SIX, Column.F), new Bishop(Color.BLACK),
                 new Position(Row.ONE, Column.G), new Bishop(Color.BLACK)
         );
         Bishop bishop = new Bishop(Color.BLACK);
