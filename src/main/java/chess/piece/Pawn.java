1f4591aab66f20aa60a0661af86b2baf08966257
kjyyjk <rlawnsdud920@gmail.com>
2025-03-22 15:01:30 +0900

    feat: Pawn의 이동 가능한 경우의 수를 초기화한다

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -1,11 +1,21 @@
 package chess.piece;
 
 import chess.Color;
+import chess.Movement;
 import chess.Position;
 import chess.board.Board;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece {
+    public static List<List<Movement>> allMovements = new ArrayList<>();
+
+    static {
+        allMovements.add(List.of(Movement.UP));
+        allMovements.add(List.of(Movement.UP_UP));
+        allMovements.add(List.of(Movement.LEFT_UP));
+        allMovements.add(List.of(Movement.RIGHT_UP));
+    }
 
     public Pawn(final Color color) {
         super(color);
