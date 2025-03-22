432c4cddeac9e40576a4a342b2f85de0020d4ada
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 14:42:52 +0900

    🔴red: 장애물이 존재하는 경우 왕의 목적지를 반환할 수 있다 테스트 작성

--- src/test/java/chess/piece/KingTest.java
+++ src/test/java/chess/piece/KingTest.java
@@ -38,4 +38,31 @@ class KingTest {
         // then
         assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
     }
+
+    @DisplayName("장애물이 있는 경우, 8개 방향 중에서 장애물이 없는 곳으로 한 칸 이동할 수 있다.")
+    @Test
+    void existHurdles() {
+        // given
+        Map<Position, ChessPiece> positions = Map.of(
+                new Position(Row.FIVE, Column.D), new Bishop(Color.BLACK),
+                new Position(Row.THREE, Column.C), new King(Color.WHITE)
+        );
+        King king = new King(Color.BLACK);
+        List<Position> expected = List.of(
+                new Position(Row.FIVE, Column.C),
+                new Position(Row.FIVE, Column.E),
+                new Position(Row.FOUR, Column.C),
+                new Position(Row.FOUR, Column.E),
+                new Position(Row.THREE, Column.D),
+                new Position(Row.THREE, Column.E)
+        );
+
+        // when
+        List<Position> destinations = king.getAvailableDestinations(
+                new Position(Row.FOUR, Column.D), positions
+        );
+
+        // then
+        assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
+    }
 }
