e6ce925064ae6950c513b32356467244d5abade6
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 15:43:39 +0900

    🔴red: 나이트는 목적지에 같은 팀의 기물이 없는 경로만 선택할 수 있다 테스트 작성

--- src/test/java/chess/piece/KnightTest.java
+++ src/test/java/chess/piece/KnightTest.java
@@ -40,4 +40,30 @@ class KnightTest {
         // then
         assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
     }
+
+    @DisplayName("목적지에 같은 팀의 기물이 있는 경우, 목적지 후보에서 제외된다.")
+    @Test
+    void existSameTeamAtDestination() {
+        Map<Position, ChessPiece> positions = Map.of(
+                new Position(Row.SIX, Column.C), new King(Color.BLACK),
+                new Position(Row.THREE, Column.F), new Bishop(Color.BLACK)
+        );
+        Knight knight = new Knight(Color.BLACK);
+        List<Position> expected = List.of(
+                new Position(Row.FIVE, Column.B),
+                new Position(Row.SIX, Column.E),
+                new Position(Row.FIVE, Column.F),
+                new Position(Row.THREE, Column.B),
+                new Position(Row.TWO, Column.C),
+                new Position(Row.TWO, Column.E)
+        );
+
+        // when
+        List<Position> destinations = knight.getAvailableDestinations(
+                new Position(Row.FOUR, Column.D), positions
+        );
+
+        // then
+        assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
+    }
 }
