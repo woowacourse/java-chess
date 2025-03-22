679ac1b6d054d91ec08691a4b4b93d56901d2d9c
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 15:42:38 +0900

    🟢green: 나이트는 목적지를 제외한 경로 상의 장애물을 신경쓰지 않는다 테스트 통과 로직 구현

--- src/main/java/chess/piece/Knight.java
+++ src/main/java/chess/piece/Knight.java
@@ -1,13 +1,25 @@
 package chess.piece;
 
 import chess.Color;
+import chess.Movement;
 import chess.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Knight implements ChessPiece {
     private final Color color;
+    private final List<Movement> movements = List.of(
+            Movement.UP_UP_LEFT,
+            Movement.UP_UP_RIGHT,
+            Movement.DOWN_DOWN_LEFT,
+            Movement.DOWN_DOWN_RIGHT,
+            Movement.LEFT_LEFT_UP,
+            Movement.LEFT_LEFT_DOWN,
+            Movement.RIGHT_RIGHT_UP,
+            Movement.RIGHT_RIGHT_DOWN
+    );
 
     public Knight(Color color) {
         this.color = color;
@@ -15,7 +27,13 @@ public class Knight implements ChessPiece {
 
     @Override
     public List<Position> getAvailableDestinations(Position startPosition, Map<Position, ChessPiece> positions) {
-        return null;
+        List<Position> destinations = new ArrayList<>();
+        for (Movement movement : movements) {
+            if (startPosition.canMove(movement)) {
+                destinations.add(startPosition.move(movement));
+            }
+        }
+        return destinations;
     }
 
     @Override
