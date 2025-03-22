9e40a45043569d1ebee686b832ef0628288e805b
yesjuhee <juhee0924@g.skku.edu>
2025-03-22 19:34:10 +0900

    feat: 출력 추가

--- src/main/java/chess/view/BoardView.java
+++ src/main/java/chess/view/BoardView.java
@@ -5,12 +5,24 @@ import chess.board.Color;
 import chess.position.Column;
 import chess.position.Position;
 import chess.position.Row;
+import java.util.Map;
 
 public final class BoardView {
+
+    private final Map<Row, Character> rowMap = Map.of(
+            Row.ONE, '1',
+            Row.TWO, '2',
+            Row.THREE, '3',
+            Row.FOUR, '4',
+            Row.FIVE, '5',
+            Row.SIX, '6',
+            Row.SEVEN, '7',
+            Row.EIGHT, '8');
 
     public void display(Board board) {
         System.out.println();
         for (Row row : Row.values()) {
+            System.out.printf("%c ", rowMap.get(row));
             for (Column column : Column.values()) {
                 Position position = new Position(row, column);
                 Color color = board.colorAt(position);
@@ -22,6 +34,7 @@ public final class BoardView {
             }
             System.out.println();
         }
+        System.out.println("  ABCDEFGH");
         System.out.print("> ");
     }
 }
