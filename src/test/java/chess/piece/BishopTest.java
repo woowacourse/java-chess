46b1bc42db2bb75afc07c23b217bb2484848682e
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 14:13:33 +0900

    🔴red: 장애물이 존재하는 경우의 비숍 목적지 리스트 반환 테스트 작성

--- src/test/java/chess/piece/BishopTest.java
+++ src/test/java/chess/piece/BishopTest.java
@@ -43,4 +43,34 @@ class BishopTest {
         // then
         assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
     }
+
+    @DisplayName("장애물이 존재하는 경우, 보드를 벗어나지 않는 영역 내에서 장애물을 마주하기 전까지의 모든 대각선 방향으로 움직일 수 있다.")
+    @Test
+    void existHurdle() {
+        // given
+        Map<Position, ChessPiece> positions = Map.of(
+                new Position(Row.SIX, Column.F), new Bishop(Color.WHITE),
+                new Position(Row.ONE, Column.G), new Bishop(Color.BLACK)
+        );
+        Bishop bishop = new Bishop(Color.BLACK);
+        List<Position> expected = List.of(
+                new Position(Row.FIVE, Column.C),
+                new Position(Row.SIX, Column.B),
+                new Position(Row.SEVEN, Column.A),
+                new Position(Row.THREE, Column.E),
+                new Position(Row.TWO, Column.F),
+                new Position(Row.FIVE, Column.E),
+                new Position(Row.THREE, Column.C),
+                new Position(Row.TWO, Column.B),
+                new Position(Row.ONE, Column.A)
+        );
+
+        // when
+        List<Position> destinations = bishop.getAvailableDestinations(
+                new Position(Row.FOUR, Column.D), positions
+        );
+
+        // then
+        assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
+    }
 }
