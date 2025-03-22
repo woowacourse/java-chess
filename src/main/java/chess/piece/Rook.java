6701ea0d44356f08ee098a50bf5c55ebbc98f1b3
eoehd1ek <eoehd2ek@gmail.com>
2025-03-22 19:29:36 +0900

    feat: 룩 이동 경로에 말이 있는 경우 예외 추가

--- src/main/java/chess/piece/Rook.java
+++ src/main/java/chess/piece/Rook.java
@@ -30,7 +30,34 @@ public final class Rook extends Piece {
 
     @Override
     public boolean isPathAble(Position start, Position end, Pieces pieces) {
-        // TODO: 경로상에 기물 있으면 안됨,
+        // 경로상에 기물 있으면 안됨
+        int moveAmount = Math.max(
+                Math.abs(end.i() - start.i()),
+                Math.abs(end.j() - start.j())
+        );
+        int direction_i = 0;
+        if (end.i() - start.i() != 0) {
+            direction_i = 1;
+            if (start.i() > end.i()) {
+                direction_i = -1;
+            }
+        }
+        int direction_j = 0;
+        if (end.j() - start.j() != 0) {
+            direction_j = 1;
+            if (start.j() > end.j()) {
+                direction_j = -1;
+            }
+        }
+        
+        for (int i = 1; i < moveAmount; ++i) {
+            Position p = new Position(
+                    start.i() + (i * direction_i),
+                    start.j() + (i * direction_j));
+            if (!pieces.isEmpty(p)) {
+                return false;
+            }
+        }
         return true;
     }
 }
