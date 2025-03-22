a58d317588e03da8dab7981647696c795b9e27e4
wogus <ind07152@naver.com>
2025-03-22 15:50:14 +0900

    feat: Rock 구현

---
+++ src/test/java/chess/piece/RockTest.java
@@ -0,0 +1,117 @@
+package chess.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.Color;
+import chess.Column;
+import chess.Position;
+import chess.Row;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class RockTest {
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("주위에 비어있는 경우")
+    void moveCheckTest(Position movePosition, boolean expected) {
+        Position position = new Position(Row.THREE, Column.B);
+        Rook rook = new Rook(Color.BLACK, position);
+
+        boolean canMove = rook.canMove(movePosition, List.of());
+
+        assertThat(canMove).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> moveCheckTest() {
+        return Stream.of(
+                Arguments.of(
+                        new Position(Row.THREE, Column.A), true
+                ),
+                Arguments.of(
+                        new Position(Row.THREE, Column.C), true
+                ),
+                Arguments.of(
+                        new Position(Row.THREE, Column.D), true
+                ),
+                Arguments.of(
+                        new Position(Row.THREE, Column.E), true
+                ),
+                Arguments.of(
+                        new Position(Row.THREE, Column.F), true
+                ),
+                Arguments.of(
+                        new Position(Row.THREE, Column.G), true
+                ),
+                Arguments.of(
+                        new Position(Row.THREE, Column.H), true
+                ),
+                Arguments.of(
+                        new Position(Row.ONE, Column.B), true
+                ),
+                Arguments.of(
+                        new Position(Row.TWO, Column.B), true
+                ),
+                Arguments.of(
+                        new Position(Row.FOUR, Column.B), true
+                ),
+                Arguments.of(
+                        new Position(Row.FIVE, Column.B), true
+                ),
+                Arguments.of(
+                        new Position(Row.SIX, Column.B), true
+                ),
+                Arguments.of(
+                        new Position(Row.SEVEN, Column.B), true
+                ),
+                Arguments.of(
+                        new Position(Row.EIGHT, Column.B), true
+                ),
+                Arguments.of(
+                        new Position(Row.EIGHT, Column.E), false
+                ),
+                Arguments.of(
+                        new Position(Row.EIGHT, Column.F), false
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("주위에 아군이 있으면 못 움직인다.")
+    void canMoveTest2(){
+        Position position = new Position(Row.THREE, Column.C);
+        Rook rook = new Rook(Color.BLACK, position);
+        Position toPosition = new Position(Row.THREE, Column.D);
+        Pawn pawn = new Pawn(Color.BLACK,toPosition);
+        boolean canMove = rook.canMove(toPosition, List.of(pawn));
+        assertThat(canMove).isFalse();
+    }
+
+    @Test
+    @DisplayName("주위에 적이면 이동가능")
+    void canMoveTest3(){
+        Position position = new Position(Row.THREE, Column.C);
+        Rook rook = new Rook(Color.BLACK, position);
+        Position toPosition = new Position(Row.THREE, Column.D);
+        Pawn pawn = new Pawn(Color.WHITE,toPosition);
+        boolean canMove = rook.canMove(toPosition, List.of(pawn));
+        assertThat(canMove).isTrue();
+    }
+
+    @Test
+    @DisplayName("중간에 누가 있으면 갈 수 없다.")
+    void canMoveTest4(){
+        Position position = new Position(Row.THREE, Column.C);
+        Knight knight = new Knight(Color.BLACK, position);
+        Position pawnPosition = new Position(Row.THREE, Column.B);
+        Position toPosition = new Position(Row.THREE, Column.D);
+        Pawn pawn = new Pawn(Color.WHITE,pawnPosition);
+        boolean canMove = knight.canMove(toPosition, List.of(pawn));
+        assertThat(canMove).isFalse();
+    }
+}
