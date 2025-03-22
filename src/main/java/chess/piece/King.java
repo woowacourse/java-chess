f60b4de9ea8020aacc893a62fee389bfb4e6fc7a
Dompoo <dlckdrms0517@gmail.com>
2025-03-22 14:00:48 +0900

    feat: 킹 움직이는 기능 개발

--- src/main/java/chess/piece/King.java
+++ src/main/java/chess/piece/King.java
@@ -1,5 +1,54 @@
 package chess.piece;
 
+import chess.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class King {
 
+    private final Position position;
+
+    public King(final Position position) {
+        this.position = position;
+    }
+
+    public King move(final Position newPosition) {
+        if (!calculateCanMovePositions().contains(newPosition)) {
+            throw new IllegalArgumentException("움직일 수 없는 위치입니다.");
+        }
+        return new King(newPosition);
+    }
+
+    private List<Position> calculateCanMovePositions() {
+        final List<Position> positions = new ArrayList<>();
+        if (position.canMoveLeft()) {
+            positions.add(position.moveLeft());
+        }
+        if (position.canMoveRight()) {
+            positions.add(position.moveRight());
+        }
+        if (position.canMoveUp()) {
+            positions.add(position.moveUp());
+        }
+        if (position.canMoveDown()) {
+            positions.add(position.moveDown());
+        }
+        if (position.canMoveLeftUp()) {
+            positions.add(position.moveLeftUp());
+        }
+        if (position.canMoveLeftDown()) {
+            positions.add(position.moveLeftDown());
+        }
+        if (position.canMoveRightUp()) {
+            positions.add(position.moveRightUp());
+        }
+        if (position.canMoveRightDown()) {
+            positions.add(position.moveRightDown());
+        }
+        if (position.canMoveRightUp()) {
+            positions.add(position.moveRightUp());
+        }
+        return positions;
+    }
 }
---
+++ src/test/java/chess/piece/KingTest.java
@@ -0,0 +1,64 @@
+package chess.piece;
+
+import chess.Column;
+import chess.Position;
+import chess.Row;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static chess.Fixtures.D5;
+import static org.assertj.core.api.Assertions.*;
+
+class KingTest {
+
+    @ParameterizedTest
+    @MethodSource("provide십자와대각선Positions")
+    void 킹은_십자방향과_대각선으로_움직일_수_있다(Position validPosition) {
+        // given
+        final King king = new King(D5);
+
+        // expected
+        assertThatCode(() -> king.move(validPosition))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidPositions")
+    void 킹은_그_외의_위치로는_움직일_수_없다(Position invalidPosition) {
+        // given
+        final King king = new King(D5);
+
+        // expected
+        assertThatThrownBy(() -> king.move(invalidPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("움직일 수 없는 위치입니다.");
+    }
+
+    public static Stream<Arguments> provide십자와대각선Positions() {
+        return Stream.of(
+                Arguments.of(new Position(Row.FIVE, Column.C)),
+                Arguments.of(new Position(Row.FIVE, Column.E)),
+                Arguments.of(new Position(Row.FOUR, Column.D)),
+                Arguments.of(new Position(Row.SIX, Column.D)),
+                Arguments.of(new Position(Row.FOUR, Column.C)),
+                Arguments.of(new Position(Row.FOUR, Column.E)),
+                Arguments.of(new Position(Row.SIX, Column.C)),
+                Arguments.of(new Position(Row.SIX, Column.E))
+        );
+    }
+
+    public static Stream<Arguments> provideInvalidPositions() {
+        return Stream.of(
+                Arguments.of(new Position(Row.FIVE, Column.B)),
+                Arguments.of(new Position(Row.FIVE, Column.F)),
+                Arguments.of(new Position(Row.SEVEN, Column.D)),
+                Arguments.of(new Position(Row.SEVEN, Column.D)),
+                Arguments.of(new Position(Row.SEVEN, Column.E)),
+                Arguments.of(new Position(Row.THREE, Column.C)),
+                Arguments.of(new Position(Row.THREE, Column.D))
+        );
+    }
+}
