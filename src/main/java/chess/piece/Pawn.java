2f1d4b9d842b350387d4020fd86627bd0df9f86e
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 17:13:24 +0900

    🟢green: 검은색 폰이 처음 움직이는 경우, 아래 방향으로 두 칸까지 이동할 수 있다 테스트 통과 코드 작성

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -1,8 +1,10 @@
 package chess.piece;
 
 import chess.Color;
+import chess.Movement;
 import chess.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,18 +18,36 @@ public class Pawn implements ChessPiece {
     }
 
     @Override
-    public List<Position> getAvailableDestinations(Position startPosition, Map<Position, ChessPiece> positions) {
+    public Position move(Position from, Position to, Map<Position, ChessPiece> positions) {
+        isFirstMove = false;
         return null;
+    }
+
+    @Override
+    public List<Position> getAvailableDestinations(Position startPosition, Map<Position, ChessPiece> positions) {
+        List<Position> destinations = new ArrayList<>();
+        for (Movement movement : getMovements()) {
+            if (startPosition.canMove(movement)) {
+                destinations.add(startPosition.move(movement));
+            }
+        }
+        return destinations;
+    }
+
+    private List<Movement> getMovements() {
+        if (isFirstMove) {
+            return List.of(
+                    Movement.DOWN,
+                    Movement.DOWN_DOWN
+            );
+        }
+        return List.of(
+                Movement.DOWN
+        );
     }
 
     @Override
     public Color getColor() {
         return color;
-    }
-
-    @Override
-    public Position move(Position from, Position to, Map<Position, ChessPiece> positions) {
-        isFirstMove = false;
-        return null;
     }
 }
