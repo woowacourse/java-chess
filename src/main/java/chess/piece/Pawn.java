b56e8a1bba6f71c15d2da54820c9816e9e203152
sung-june27 <kmymy27@gmail.com>
2025-03-22 15:44:36 +0900

    feat(Pawn): 첫 번째 이동 시, 2칸 이동 가능

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -2,31 +2,38 @@ package chess.piece;
 
 import chess.Color;
 import chess.Position;
+import chess.Row;
 
 public class Pawn extends Piece {
 
-    public Pawn(Color color, Position position) {
+    public Pawn(final Color color, final Position position) {
         super(color, position);
     }
 
     public void move(final int x, final int y) {
         boolean canMove = false;
-        if (color == Color.WHITE && y > 0) {
+        if (color == Color.WHITE && y > 0 && y <= 2) {
             canMove = position.canMoveUp();
         }
-        if (color == Color.BLACK && y < 0) {
+        if (color == Color.BLACK && y < 0 && y >= -2) {
             canMove = position.canMoveDown();
         }
 
+        boolean isNotFirstMove = true;
+        if (color == Color.WHITE && position.row() == Row.TWO) {
+            isNotFirstMove = false;
+        }
+        if (color == Color.BLACK && position.row() == Row.SEVEN) {
+            isNotFirstMove = false;
+        }
+
+        if (isNotFirstMove && y != 1) {
+            canMove = false;
+        }
+
         if (canMove) {
-            if (color == Color.WHITE) {
-                position = position.moveUp();
-                return;
-            }
-            if (color == Color.BLACK) {
-                position = position.moveDown();
-                return;
-            }
+            position = position.moveVertical(y);
+            return;
         }
         throw new IllegalArgumentException("이동할 수 없습니다.");
     }
--- src/test/java/chess/piece/PawnTest.java
+++ src/test/java/chess/piece/PawnTest.java
@@ -30,6 +30,44 @@ class PawnTest {
         assertThat(pawn.getPosition()).isEqualTo(Fixtures.D6);
     }
 
+    @DisplayName("백팀이라면 처음 이동할 때, 위로 두 칸 이동 가능하다.")
+    @Test
+    void move_up_two_step_white_color_first_move() {
+        Pawn pawn = new Pawn(Color.WHITE, Fixtures.D2);
+
+        pawn.move(0, 2);
+
+        assertThat(pawn.getPosition()).isEqualTo(Fixtures.D4);
+    }
+
+    @DisplayName("흑팀이라면 처음 이동할 때 아래로 두 칸 이동 가능하다.")
+    @Test
+    void move_down_two_step_black_color_first_move() {
+        Pawn pawn = new Pawn(Color.BLACK, Fixtures.D7);
+
+        pawn.move(0, -2);
+
+        assertThat(pawn.getPosition()).isEqualTo(Fixtures.D5);
+    }
+
+    @DisplayName("처음 이동 아니라면, 백팀은 위로 두 칸 이동할 수 없다.")
+    @Test
+    void cant_move_up_two_step_white_color_not_first_move() {
+        Pawn pawn = new Pawn(Color.WHITE, Fixtures.D3);
+
+        assertThatThrownBy(()->pawn.move(0, 2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("처음 이동 아니라면, 흑팀은 아래로 두 칸 이동할 수 없다.")
+    @Test
+    void cnat_move_down_two_step_black_color_not_first_move() {
+        Pawn pawn = new Pawn(Color.BLACK, Fixtures.D6);
+
+        assertThatThrownBy(()->pawn.move(0, -2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("백팀이라면 아래로 이동 불가능하다.")
     @Test
     void cant_move_down_white_color() {
@@ -45,6 +83,24 @@ class PawnTest {
         Pawn pawn = new Pawn(Color.BLACK, Fixtures.D7);
 
         assertThatThrownBy(()->pawn.move(0, 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("백팀은 위로 두 칸 이상 이동할 수 없다.")
+    @Test
+    void cant_move_up_over_two_step_white_color() {
+        Pawn pawn = new Pawn(Color.WHITE, Fixtures.D3);
+
+        assertThatThrownBy(()->pawn.move(0, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("흑팀은 아래로 두 칸 이상 이동할 수 없다.")
+    @Test
+    void cnat_move_down_over_two_step_black_color() {
+        Pawn pawn = new Pawn(Color.BLACK, Fixtures.D6);
+
+        assertThatThrownBy(()->pawn.move(0, -3))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
