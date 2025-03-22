baa4793052ed88561698dc700c753106f6faab47
ykmxxi <ykmxxi97@gmail.com>
2025-03-22 13:59:27 +0900

    feat: 킹의 움직임 구현

--- src/main/java/chess/piece/King.java
+++ src/main/java/chess/piece/King.java
@@ -1,5 +1,45 @@
 package chess.piece;
 
+import java.util.List;
+import java.util.Objects;
+
+import chess.Movement;
+import chess.Position;
+
 public class King {
+
+    private static final List<Movement> MOVEMENTS = List.of(
+            Movement.UP, Movement.LEFT, Movement.DOWN, Movement.RIGHT,
+            Movement.LEFT_UP, Movement.RIGHT_UP, Movement.LEFT_DOWN, Movement.RIGHT_DOWN
+    );
+
+    private final Position position;
+
+    public King(final Position position) {
+        this.position = position;
+    }
+
+    public King move(final Movement movement) {
+        if (MOVEMENTS.contains(movement)) {
+            return new King(position.move(movement));
+        }
+        throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof King king)) {
+            return false;
+        }
+        return Objects.equals(position, king.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(position);
+    }
 
 }
---
+++ src/test/java/chess/piece/KingTest.java
@@ -0,0 +1,54 @@
+package chess.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import chess.Column;
+import chess.Movement;
+import chess.Position;
+import chess.Row;
+
+class KingTest {
+
+    @MethodSource
+    @ParameterizedTest
+    void 킹은_직선을_한칸_움직일_수_있다(Movement movement, Position target) {
+        King king = new King(new Position(Row.TWO, Column.D));
+
+        assertThat(king.move(movement)).isEqualTo(new King(target));
+    }
+
+    private static Stream<Arguments> 킹은_직선을_한칸_움직일_수_있다() {
+        return Stream.of(
+                Arguments.of(Movement.UP, new Position(Row.THREE, Column.D)),
+                Arguments.of(Movement.DOWN, new Position(Row.ONE, Column.D)),
+                Arguments.of(Movement.LEFT, new Position(Row.TWO, Column.C)),
+                Arguments.of(Movement.RIGHT, new Position(Row.TWO, Column.E))
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void 킹은_대각선을_한칸_움직일_수_있다(Movement movement, Position target) {
+        King king = new King(new Position(Row.TWO, Column.D));
+
+        assertThat(king.move(movement)).isEqualTo(new King(target));
+    }
+
+    private static Stream<Arguments> 킹은_대각선을_한칸_움직일_수_있다() {
+        return Stream.of(
+                Arguments.of(Movement.LEFT_UP, new Position(Row.THREE, Column.C)),
+                Arguments.of(Movement.RIGHT_UP, new Position(Row.THREE, Column.E)),
+                Arguments.of(Movement.LEFT_DOWN, new Position(Row.ONE, Column.C)),
+                Arguments.of(Movement.RIGHT_DOWN, new Position(Row.ONE, Column.E))
+        );
+    }
+
+}
