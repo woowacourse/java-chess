3a39ac278370096943478bde08f4568e30fa0545
Dompoo <dlckdrms0517@gmail.com>
2025-03-22 14:11:07 +0900

    feat: 나이트 움직이는 기능 개발

--- src/main/java/chess/piece/Knight.java
+++ src/main/java/chess/piece/Knight.java
@@ -1,5 +1,52 @@
 package chess.piece;
 
+import chess.Movement;
+import chess.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Knight {
 
+    private final Position position;
+
+    public Knight(final Position position) {
+        this.position = position;
+    }
+
+    public Knight move(final Position newPosition) {
+        if (!calculateCanMovePositions().contains(newPosition)) {
+            throw new IllegalArgumentException("움직일 수 없는 위치입니다.");
+        }
+        return new Knight(newPosition);
+    }
+
+    private List<Position> calculateCanMovePositions() {
+        final List<Position> positions = new ArrayList<>();
+        if (position.canMove(Movement.DOWN_DOWN_RIGHT)) {
+            positions.add(position.move(Movement.DOWN_DOWN_RIGHT));
+        }
+        if (position.canMove(Movement.DOWN_DOWN_LEFT)) {
+            positions.add(position.move(Movement.DOWN_DOWN_LEFT));
+        }
+        if (position.canMove(Movement.UP_UP_LEFT)) {
+            positions.add(position.move(Movement.UP_UP_LEFT));
+        }
+        if (position.canMove(Movement.UP_UP_RIGHT)) {
+            positions.add(position.move(Movement.UP_UP_RIGHT));
+        }
+        if (position.canMove(Movement.LEFT_LEFT_DOWN)) {
+            positions.add(position.move(Movement.LEFT_LEFT_DOWN));
+        }
+        if (position.canMove(Movement.LEFT_LEFT_UP)) {
+            positions.add(position.move(Movement.LEFT_LEFT_UP));
+        }
+        if (position.canMove(Movement.RIGHT_RIGHT_DOWN)) {
+            positions.add(position.move(Movement.RIGHT_RIGHT_DOWN));
+        }
+        if (position.canMove(Movement.RIGHT_RIGHT_UP)) {
+            positions.add(position.move(Movement.RIGHT_RIGHT_UP));
+        }
+        return positions;
+    }
 }
--- src/test/java/chess/piece/KnightTest.java
+++ src/test/java/chess/piece/KnightTest.java
@@ -9,35 +9,48 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static chess.Fixtures.D5;
+import static chess.Fixtures.*;
 import static org.assertj.core.api.Assertions.*;
 
-class KingTest {
+class KnightTest {
 
     @ParameterizedTest
     @MethodSource("provideValidPositions")
-    void 킹은_십자방향과_대각선으로_움직일_수_있다(Position validPosition) {
+    void 나이트는_십자방향과_대각선으로_움직일_수_있다(Position validPosition) {
         // given
-        final King king = new King(D5);
+        final Knight knight = new Knight(D5);
 
         // expected
-        assertThatCode(() -> king.move(validPosition))
+        assertThatCode(() -> knight.move(validPosition))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @MethodSource("provideInvalidPositions")
-    void 킹은_그_외의_위치로는_움직일_수_없다(Position invalidPosition) {
+    void 나이트는_그_외의_위치로는_움직일_수_없다(Position invalidPosition) {
         // given
-        final King king = new King(D5);
+        final Knight knight = new Knight(D5);
 
         // expected
-        assertThatThrownBy(() -> king.move(invalidPosition))
+        assertThatThrownBy(() -> knight.move(invalidPosition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("움직일 수 없는 위치입니다.");
     }
 
     public static Stream<Arguments> provideValidPositions() {
+        return Stream.of(
+                Arguments.of(F4),
+                Arguments.of(F6),
+                Arguments.of(E3),
+                Arguments.of(E7),
+                Arguments.of(B4),
+                Arguments.of(B6),
+                Arguments.of(C3),
+                Arguments.of(C7)
+        );
+    }
+
+    public static Stream<Arguments> provideInvalidPositions() {
         return Stream.of(
                 Arguments.of(new Position(Row.FIVE, Column.C)),
                 Arguments.of(new Position(Row.FIVE, Column.E)),
@@ -50,15 +63,4 @@ class KingTest {
         );
     }
 
-    public static Stream<Arguments> provideInvalidPositions() {
-        return Stream.of(
-                Arguments.of(new Position(Row.FIVE, Column.B)),
-                Arguments.of(new Position(Row.FIVE, Column.F)),
-                Arguments.of(new Position(Row.SEVEN, Column.D)),
-                Arguments.of(new Position(Row.SEVEN, Column.D)),
-                Arguments.of(new Position(Row.SEVEN, Column.E)),
-                Arguments.of(new Position(Row.THREE, Column.C)),
-                Arguments.of(new Position(Row.THREE, Column.D))
-        );
-    }
 }
