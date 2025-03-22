6ba197799228503a2557fdb2d6bdacdacf25760e
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 16:05:26 +0900

    🟢green: 퀸 - 장애물이 존재하지 않는 경우 목적지 반환 테스트 통과 코드 작성

--- src/main/java/chess/piece/Queen.java
+++ src/main/java/chess/piece/Queen.java
@@ -1,13 +1,25 @@
 package chess.piece;
 
 import chess.Color;
+import chess.Movement;
 import chess.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Queen implements ChessPiece {
     private final Color color;
+    private final List<Movement> movements = List.of(
+            Movement.UP,
+            Movement.DOWN,
+            Movement.LEFT,
+            Movement.RIGHT,
+            Movement.LEFT_UP,
+            Movement.RIGHT_UP,
+            Movement.LEFT_DOWN,
+            Movement.RIGHT_DOWN
+    );
 
     public Queen(Color color) {
         this.color = color;
@@ -15,7 +27,15 @@ public class Queen implements ChessPiece {
 
     @Override
     public List<Position> getAvailableDestinations(Position startPosition, Map<Position, ChessPiece> positions) {
-        return null;
+        List<Position> destinations = new ArrayList<>();
+        for (Movement movement : movements) {
+            Position currentPosition = startPosition;
+            while (currentPosition.canMove(movement)) {
+                currentPosition = currentPosition.move(movement);
+                destinations.add(currentPosition);
+            }
+        }
+        return destinations;
     }
 
     @Override
