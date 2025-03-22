2b073767a89430060d369544f385ebf64db690db
eoehd1ek <eoehd2ek@gmail.com>
2025-03-22 14:04:15 +0900

    feat: 체스판에서 쓸 살아있는 기물 클래스 추가

---
+++ src/main/java/chess/piece/LivePiece.java
@@ -0,0 +1,16 @@
+package chess.piece;
+
+import chess.Position;
+
+// NOTE: 살아있는 기물정보를 다룸
+public class LivePiece {
+    private final Piece piece;
+    private Position position;
+
+    public LivePiece(Piece piece, Position position) {
+        this.piece = piece;
+        this.position = position;
+    }
+
+
+}
