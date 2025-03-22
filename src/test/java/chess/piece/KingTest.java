bf6346aab69d0d3fee0eec32d3e41d66bddc1df3
minSsan <msp1125@hanyang.ac.kr>
2025-03-22 14:27:51 +0900

    🔴red: 장애물이 없는 경우 왕은 8개의 방향을 목적지로 선택할 수 있다 테스트 작성

---
+++ src/test/java/chess/piece/KingTest.java
@@ -0,0 +1,42 @@
+package chess.piece;
+
+import chess.Color;
+import chess.Column;
+import chess.Position;
+import chess.Row;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class KingTest {
+    @DisplayName("장애물이 없는 경우, 8개 방향으로 한 칸씩 이동할 수 있다.")
+    @Test
+    void notExistHurdles() {
+        // given
+        Map<Position, ChessPiece> positions = Map.of();
+        King king = new King(Color.BLACK);
+        List<Position> expected = List.of(
+                new Position(Row.FIVE, Column.C),
+                new Position(Row.FIVE, Column.D),
+                new Position(Row.FIVE, Column.E),
+                new Position(Row.FOUR, Column.C),
+                new Position(Row.FOUR, Column.E),
+                new Position(Row.THREE, Column.C),
+                new Position(Row.THREE, Column.D),
+                new Position(Row.THREE, Column.E)
+        );
+
+        // when
+        List<Position> destinations = king.getAvailableDestinations(
+                new Position(Row.FOUR, Column.D), positions
+        );
+
+        // then
+        assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
+    }
+}
